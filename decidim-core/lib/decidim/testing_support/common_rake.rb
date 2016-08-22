43480ed9fe93830372bd9ffd2efda6790ae650c8
Marc Riera <mrc2407@gmail.com>
2016-08-22 11:28:49 +0200

    Remove spree reference

--- decidim-core/lib/decidim/testing_support/common_rake.rb
+++ decidim-core/lib/decidim/testing_support/common_rake.rb
@@ -3,8 +3,6 @@ require_relative '../../../../decidim-core/lib/generators/decidim/dummy_generato
 desc "Generates a dummy app for testing"
 namespace :common do
   task :test_app do |_t, args|
-    args.with_defaults(user_class: "Spree::LegacyUser")
-
     Decidim::Generators::DummyGenerator.start [
       "--lib_name=#{ENV['LIB_NAME']}",
       "--migrate=true",
