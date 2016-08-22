9ef93a2bb6b94ce88ff49805c5223370e481d9b6
Marc Riera <mrc2407@gmail.com>
2016-08-22 10:25:17 +0200

    Add rails5 needed dependencies for tests

--- common_gemfile.rb
+++ common_gemfile.rb
@@ -2,6 +2,14 @@ source 'https://rubygems.org'
 
 gem 'pg'
 
+# Rails 5 deps
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.2'
+gem 'jquery-rails'
+gem 'turbolinks', '~> 5'
+gem 'jbuilder', '~> 2.5'
+
 group :development do
   gem 'byebug'
   gem 'listen'
