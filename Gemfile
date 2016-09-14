cee691c4fe19d3e6ef2d5dd0c688fae2c03f3047
Oriol Gual <oriol.gual@gmail.com>
2016-09-14 12:54:00 +0200

    Add Codecov

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ eval(File.read(File.dirname(__FILE__) + '/common_gemfile.rb'))
 
 # Specify your gem's dependencies in decidim.gemspec
 gemspec
+gem 'codecov', require: false, group: :test
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
+if ENV["CI"]
+  require "simplecov"
+  SimpleCov.start
+
+  require "codecov"
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'decidim'
