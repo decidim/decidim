c6800d476c3e50c0556a7e367ee3b1644eaf648e
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-09-22 15:23:03 +0200

    Add features

--- decidim-system/decidim-system.gemspec
+++ decidim-system/decidim-system.gemspec
@@ -39,4 +39,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency "wisper-rspec"
   s.add_development_dependency "pg"
   s.add_development_dependency "listen"
+  s.add_development_dependency "launchy"
 end
---
+++ decidim-system/spec/features/sessions_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+require "spec_helper"
+
+describe "sessions", type: :feature do
+  let!(:admin) do
+    Decidim::System::Admin.create!(email: "admin@example.org",
+                                   password: "123456",
+                                   password_confirmation: "123456")
+  end
+
+  before(:each) do
+    visit decidim_system.root_path
+  end
+
+  context "when using a correct username and password" do
+    it "lets you into the system panel" do
+      within ".new_admin" do
+        fill_in :admin_email, with: "admin@example.org"
+        fill_in :admin_password, with: "123456"
+        find("*[type=submit]").click
+      end
+
+      expect(page).to have_content("Dashboard")
+    end
+  end
+
+  context "when using an incorrectusername and password" do
+    it "doesn't let you in the admin panel" do
+      within ".new_admin" do
+        fill_in :admin_email, with: "admin@example.org"
+        fill_in :admin_password, with: "forged_password"
+        find("*[type=submit]").click
+      end
+
+      expect(page).not_to have_content("Dashboard")
+    end
+  end
+end
