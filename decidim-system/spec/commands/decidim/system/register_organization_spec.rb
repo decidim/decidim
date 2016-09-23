fbd89e795bbea61ee471294ac92f26fe4b1d8f74
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-09-23 14:32:44 +0200

    Fix styles

--- decidim-system/spec/commands/decidim/system/register_organization_spec.rb
+++ decidim-system/spec/commands/decidim/system/register_organization_spec.rb
@@ -4,54 +4,54 @@ require "spec_helper"
 module Decidim
   module System
     describe RegisterOrganization, :db do
-      describe 'call' do
+      describe "call" do
         let(:form) do
           RegisterOrganizationForm.new(params)
         end
 
         let(:command) { described_class.new(form) }
 
-        context 'when the form is valid' do
+        context "when the form is valid" do
           let(:params) do
             {
-              name: 'Gotham City',
-              host: 'decide.gotham.gov',
-              organization_admin_email: 'f.laguardia@gotham.gov'
+              name: "Gotham City",
+              host: "decide.gotham.gov",
+              organization_admin_email: "f.laguardia@gotham.gov"
             }
           end
 
-          it 'returns a valid response' do
+          it "returns a valid response" do
             expect { command.call }.to broadcast(:ok)
           end
 
-          it 'creates a new organization' do
+          it "creates a new organization" do
             expect { command.call }.to change { Organization.count }.by(1)
             organization = Organization.last
 
-            expect(organization.name).to eq('Gotham City')
-            expect(organization.host).to eq('decide.gotham.gov')
+            expect(organization.name).to eq("Gotham City")
+            expect(organization.host).to eq("decide.gotham.gov")
           end
 
-          it 'invites a user as organization admin' do
+          it "invites a user as organization admin" do
             expect { command.call }.to change { User.count }.by(1)
             admin = User.last
 
-            expect(admin.email).to eq('f.laguardia@gotham.gov')
-            expect(admin.organization.name).to eq('Gotham City')
+            expect(admin.email).to eq("f.laguardia@gotham.gov")
+            expect(admin.organization.name).to eq("Gotham City")
             expect(admin).to be_admin
             expect(admin).to be_created_by_invite
           end
         end
 
-        context 'when the form is invalid' do
+        context "when the form is invalid" do
           let(:params) do
             {
               name: nil,
-              host: 'foo.com'
+              host: "foo.com"
             }
           end
 
-          it 'returns an invalid response' do
+          it "returns an invalid response" do
             expect { command.call }.to broadcast(:invalid)
           end
         end
