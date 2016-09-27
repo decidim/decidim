e99bf79f3c2a0af6f65a95c44f3b59d3d95d97dd
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-09-27 16:06:33 +0200

    Fix constant collision
    We have to manually specify the top level constant as eager loading in production vs lazy loading on development are inconsistent. YAY.

--- decidim-system/app/controllers/decidim/system/devise/passwords_controller.rb
+++ decidim-system/app/controllers/decidim/system/devise/passwords_controller.rb
@@ -3,7 +3,7 @@ module Decidim
   module System
     module Devise
       # A controller so admins can recover their passwords.
-      class PasswordsController < Devise::PasswordsController
+      class PasswordsController < ::Devise::PasswordsController
         layout "decidim/system/login"
       end
     end
