4430b20b47d5ce938597fc46b02522af059060dd
David Morcillo Muñoz <david.morcillo@gmail.com>
2016-09-28 12:53:41 +0200

    Correctly configure devise controller

--- decidim-core/app/controllers/decidim/devise/passwords_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-module Decidim
-  module Devise
-    # A controller so admins can recover their passwords.
-    class PasswordsController < Devise::PasswordsController
-    end
-  end
-end
