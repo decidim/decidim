e6a8f5a7699179acce6a56449636200e022b1439
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-10-08 11:19:47 +0200

    Put DeviseFailureApp into an autoload module

--- decidim-core/config/initializers/devise.rb
+++ decidim-core/config/initializers/devise.rb
@@ -2,8 +2,6 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 
-require "decidim/devise_failure_app"
-
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
