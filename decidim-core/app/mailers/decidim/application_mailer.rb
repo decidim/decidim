d33341b15ef7e8c40e3d84d5a1595fbf0e6ff9e3
Oriol Gual <oriol.gual@gmail.com>
2016-10-11 16:10:21 +0200

    Use a custom email layout

--- decidim-core/app/mailers/decidim/application_mailer.rb
+++ decidim-core/app/mailers/decidim/application_mailer.rb
@@ -3,6 +3,8 @@ module Decidim
   # Main application mailer configuration. Inherit from this to create new
   # mailers.
   class ApplicationMailer < ActionMailer::Base
+    include Roadie::Rails::Automatic
+
     default from: "from@example.com"
     layout "mailer"
   end
