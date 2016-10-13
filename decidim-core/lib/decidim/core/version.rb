a49a8dee8ef6a27565dc82dc93b5ea8cfbfb178e
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-10-13 09:59:55 +0200

    Version bump

--- decidim-core/lib/decidim/core/version.rb
+++ decidim-core/lib/decidim/core/version.rb
@@ -2,7 +2,7 @@
 # This holds Decidim's version and the Rails version on which it depends.
 module Decidim
   def self.version
-    "0.0.1.alpha4"
+    "0.0.1.alpha5"
   end
 
   def self.rails_version
