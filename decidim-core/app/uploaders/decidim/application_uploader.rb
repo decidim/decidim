903fa235451552f58adf3f206302aacf3f7e40c3
Marc Riera <mrc2407@gmail.com>
2016-10-13 09:11:40 +0200

    Add ApplicationUploader

---
+++ decidim-core/app/uploaders/decidim/application_uploader.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+module Decidim
+  # This class deals with uploading images to Decidim It is intended to just
+  # hold the uploads configuration, so you should inherit from this class and
+  # then tweak any configuration you need.
+  class ApplicationUploader < CarrierWave::Uploader::Base
+    storage :file
+
+    # Override the directory where uploaded files will be stored.
+    # This is a sensible default for uploaders that are meant to be mounted:
+    def store_dir
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
+  end
+end
