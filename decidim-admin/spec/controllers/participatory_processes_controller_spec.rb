6ea1b09095dd70a9fcc0eef6580fe04b7bbf6cd2
Marc Riera <mrc2407@gmail.com>
2016-10-27 10:50:04 +0200

    Add controller specs

---
+++ decidim-admin/spec/controllers/participatory_processes_controller_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+require "spec_helper"
+
+module Decidim
+  module Admin
+    describe ParticipatoryProcessesController, type: :controller do
+      routes { Decidim::Admin::Engine.routes }
+      let(:organization) { create(:organization) }
+      let!(:external_process) { create :participatory_process }
+
+      before do
+        @request.env["decidim.current_organization"] = organization
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+      end
+
+      describe "GET show" do
+        context "process in another organization" do
+          let(:user) { create(:user, :admin, :confirmed, organization: organization) }
+
+          before do
+            sign_in user, scope: :user
+          end
+
+          it "is not visible to the user" do
+            expect do
+              get :show, params: { id: external_process.id }
+            end.to raise_error(ActiveRecord::RecordNotFound)
+          end
+        end
+      end
+    end
+  end
+end
