21013054d7fbbf30454cb36f4abf66ea21d228f2
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-10-28 20:05:39 +0200

    Add a fake "dev" language

--- decidim-dev/lib/decidim/test/rspec_support/i18n.rb
+++ decidim-dev/lib/decidim/test/rspec_support/i18n.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 RSpec.configure do |config|
   config.before(:suite) do
+    I18n.available_locales = [:dev, :en]
+    I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
+    I18n.locale = :dev
+    I18n.fallbacks.map("dev" => "en")
     I18n.config.enforce_available_locales = false
   end
 
