a51cdb43ce65cf5aa139d58ca2324ce0b8c4a962
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-10-30 22:08:52 +0100

    Fix specs

--- decidim-dev/lib/decidim/test/rspec_support/i18n.rb
+++ decidim-dev/lib/decidim/test/rspec_support/i18n.rb
@@ -1,16 +1,13 @@
 # frozen_string_literal: true
 RSpec.configure do |config|
   config.before(:suite) do
-    I18n.available_locales = [:dev, :en]
     I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
-    I18n.locale = :dev
     I18n.fallbacks.map("dev" => "en")
     I18n.config.enforce_available_locales = false
   end
 
-  config.around(:each) do |example|
-    previous_locale = I18n.locale
-    example.run
-    I18n.locale = previous_locale
+  config.before(:each) do
+    I18n.available_locales = [:dev, :en]
+    I18n.locale = :dev
   end
 end
