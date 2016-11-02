806612fbedf620d20a17223d7eff96e926605c10
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-11-02 11:15:05 +0100

    Move decidim config to core

--- decidim-core/lib/decidim/core.rb
+++ decidim-core/lib/decidim/core.rb
@@ -7,4 +7,25 @@ module Decidim
   autoload :TranslatableAttributes, "decidim/translatable_attributes"
   autoload :FormBuilder, "decidim/form_builder"
   autoload :DeviseFailureApp, "decidim/devise_failure_app"
+  include ActiveSupport::Configurable
+
+  # Loads seeds from all engines.
+  def self.seed!
+    Rails.application.railties.select do |railtie|
+      railtie.respond_to?(:load_seed) && railtie.class.name.include?("Decidim::")
+    end.each(&:load_seed)
+  end
+
+  # Exposes a configuration option: The application name String.
+  config_accessor :application_name
+
+  # Exposes a configuration option: The email String to use as sender in all
+  # the mails.
+  config_accessor :mailer_sender
+
+  # Exposes a configuration option: an Array of `cancancan`'s Ability classes
+  # that will be automatically included to the base `Decidim::Ability` class.
+  config_accessor :abilities do
+    []
+  end
 end
--- lib/decidim.rb
+++ lib/decidim.rb
@@ -5,25 +5,4 @@ require "decidim/admin"
 
 # Module declaration.
 module Decidim
-  include ActiveSupport::Configurable
-
-  # Loads seeds from all engines.
-  def self.seed!
-    Rails.application.railties.select do |railtie|
-      railtie.respond_to?(:load_seed) && railtie.class.name.include?("Decidim::")
-    end.each(&:load_seed)
-  end
-
-  # Exposes a configuration option: The application name String.
-  config_accessor :application_name
-
-  # Exposes a configuration option: The email String to use as sender in all
-  # the mails.
-  config_accessor :mailer_sender
-
-  # Exposes a configuration option: an Array of `cancancan`'s Ability classes
-  # that will be automatically included to the base `Decidim::Ability` class.
-  config_accessor :abilities do
-    []
-  end
 end
