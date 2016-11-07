9ec3564fbc33e9ead62426e30614edeeb9d65250
Marc Riera <mrc2407@gmail.com>
2016-11-07 14:21:06 +0100

    Add missing specs

---
+++ decidim-core/spec/models/decidim/participatory_process_user_role_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+require "spec_helper"
+
+module Decidim
+  describe ParticipatoryProcessUserRole do
+    let(:participatory_process_user_role) { build(:participatory_process_user_role, role: role) }
+    let(:role) { "admin" }
+
+    subject { participatory_process_user_role }
+
+    it { is_expected.to be_valid }
+
+    context "when the role is not admin" do
+      let(:role) { "fake_role" }
+
+      it { is_expected.not_to be_valid }
+    end
+  end
+end
