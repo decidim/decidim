5f3fcf404dbdfb1fdcea69fa43e831ac9e07ad54
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-11-08 14:33:42 +0100

    Document initializer option

--- lib/generators/decidim/templates/initializer.rb
+++ lib/generators/decidim/templates/initializer.rb
@@ -3,5 +3,7 @@ Decidim.configure do |config|
   config.application_name = "My Application Name"
   config.mailer_sender    = "change-me@domain.org"
   config.authorization_handlers = [ExampleAuthorizationHandler]
+
+  # Uncomment this lines to set your preferred locales
   # config.available_locales = %{en ca es}
 end
