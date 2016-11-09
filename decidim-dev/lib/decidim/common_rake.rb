b2dba9edb8ee4f534ce66f6a75bbf3c2db2ad6e1
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-11-09 12:05:02 +0100

    Add notice

--- decidim-dev/lib/decidim/common_rake.rb
+++ decidim-dev/lib/decidim/common_rake.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 require_relative "../generators/decidim/dummy_generator"
 
+
+begin
+  require "./spec/#{ENV["ENGINE_NAME"]}_dummy_app/config/application"
+  Rails.application.load_tasks
+rescue LoadError
+  puts "Could not load dummy application. Please ensure you have run `bundle exec rake test_app`"
+  puts "Dummy app's rake tasks won't be available."
+  puts "Tried to load it from #{dummy_app_path}"
+end
+
+
 desc "Generates a dummy app for testing"
 namespace :common do
   task :test_app do |_t, _args|
