860dc9e94fdf9f06fefd66c70037eb45de19feef
Josep Jaume Rey <josepjaume@gmail.com>
2016-11-09 06:55:49 -0800

    Precompile assets before running tests on CI (#193)
    * Precompile assets before running tests on CI
    
    * Add notice
    
    * Exit with a failed status
    
    * Precompile assets when generating the app
    
    * Remove extra lines
    
    * Fix typo
    
    * Dramatically simplify Rake Task
    
    * Fix error message

--- decidim-dev/lib/decidim/common_rake.rb
+++ decidim-dev/lib/decidim/common_rake.rb
@@ -1,14 +1,28 @@
 # frozen_string_literal: true
+require "bundler/gem_tasks"
+require "rspec/core/rake_task"
 require_relative "../generators/decidim/dummy_generator"
 
+engine_path = Dir.pwd
+engine_name = engine_path.split("/").last
+dummy_app_path = File.expand_path(File.join(engine_path, "spec", "#{engine_name}_dummy_app"))
+
 desc "Generates a dummy app for testing"
-namespace :common do
-  task :test_app do |_t, _args|
-    Decidim::Generators::DummyGenerator.start [
-      "--engine_path=#{ENV["ENGINE_PATH"]}",
+task :generate_test_app do
+  Decidim::Generators::DummyGenerator.start(
+    [
+      "--engine_path=#{engine_path}",
       "--migrate=true",
       "--quiet"
     ]
-    FileUtils.cd(ENV["ENGINE_PATH"])
-  end
+  )
+
+  require File.join(dummy_app_path, "config", "application")
+  Rails.application.load_tasks
+  Rake.application["assets:precompile"].invoke
+
+  FileUtils.cd(engine_path)
 end
+
+RSpec::Core::RakeTask.new(:spec)
+task default: [:generate_test_app, :spec]
