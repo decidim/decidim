cf644c67e1139d65cc7e1ec7118d3802ec0c54df
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-11-09 14:51:25 +0100

    Remove extra lines

--- decidim-dev/lib/decidim/common_rake.rb
+++ decidim-dev/lib/decidim/common_rake.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 require_relative "../generators/decidim/dummy_generator"
 
-begin
-rescue LoadError
-  puts "Could not load dummy application. Please ensure you have run `bundle exec rake common:test_app`"
-  puts "Dummy app's rake tasks won't be available."
-  puts "Tried to load it from #{dummy_app_path}"
-end
-
 desc "Generates a dummy app for testing"
 namespace :common do
   task :test_app do
@@ -18,12 +11,13 @@ namespace :common do
         "--quiet"
       ]
     )
-    FileUtils.cd(ENV["ENGINE_PATH"])
 
     dummy_app_path = Dir.glob("./spec/*_dummy_app").first
     require File.join(dummy_app_path, "config", "application")
     Rails.application.load_tasks
 
     Rake.application["assets:precompile"].invoke
+
+    FileUtils.cd(ENV["ENGINE_PATH"])
   end
 end
