1ba8154d094bdcb087112e9060aab3ea51c2eb94
David Morcillo Muñoz <david.morcillo@gmail.com>
2016-11-15 16:26:01 +0100

    Fix rubocop issues

--- decidim-core/lib/decidim/form_builder.rb
+++ decidim-core/lib/decidim/form_builder.rb
@@ -57,7 +57,7 @@ module Decidim
     #           :label   - The Boolean value to create or not the input label (optional) (default: true)
     #           :toolbar - The String value to configure WYSIWYG toolbar. It should be 'basic' or
     #                      or 'full' (optional) (default: 'basic')
-    #           :lines   - The Integer to indicate how many lines should editor have (optional) (default: 10) 
+    #           :lines   - The Integer to indicate how many lines should editor have (optional) (default: 10)
     #
     # Renders a container with both hidden field and editor container
     def editor(name, options = {})
