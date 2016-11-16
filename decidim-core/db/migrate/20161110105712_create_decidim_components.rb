b9a67d71be4c42b867401cf0c9ba74844a5f0031
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-11-16 10:59:19 +0100

    Use jsonb

--- decidim-core/db/migrate/20161110105712_create_decidim_components.rb
+++ decidim-core/db/migrate/20161110105712_create_decidim_components.rb
@@ -2,8 +2,8 @@ class CreateDecidimComponents < ActiveRecord::Migration[5.0]
   def change
     create_table :decidim_components do |t|
       t.references :decidim_participatory_process, foreign_key: true
-      t.hstore :name
-      t.hstore :configuration
+      t.jsonb :name
+      t.jsonb :configuration
       t.string :component_type
 
       t.timestamps
