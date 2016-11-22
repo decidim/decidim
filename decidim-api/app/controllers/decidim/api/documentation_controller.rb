ccf5575cf5af9d7f0706eb3f46bdf2b8f4ada1e2
Marc Riera <mrc2407@gmail.com>
2016-11-22 16:21:35 +0100

    Fix constant lookup (#262)

--- decidim-api/app/controllers/decidim/api/documentation_controller.rb
+++ decidim-api/app/controllers/decidim/api/documentation_controller.rb
@@ -3,7 +3,7 @@ module Decidim
   module Api
     # This controller takes queries from an HTTP endpoint and sends them out to
     # the Schema to be executed, later returning the response as JSON.
-    class DocumentationController < ApplicationController
+    class DocumentationController < Api::ApplicationController
       layout "decidim/api/documentation"
     end
   end
