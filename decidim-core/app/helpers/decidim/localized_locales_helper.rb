fab5bb3c749eb9d30665bb2fd2c400ff503aba3c
Marc Riera <mrc2407@gmail.com>
2016-11-22 10:01:34 +0100

    Add missing docs

--- decidim-core/app/helpers/decidim/localized_locales_helper.rb
+++ decidim-core/app/helpers/decidim/localized_locales_helper.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 module Decidim
-  # Custom helpers, scoped to the admin panel.
+  # A helper that converts an array of locale IDs (two-letter identifiers, e.g.
+  # `"en"`) to an array of Objects that have both the ID and their name in
+  # their own language (e.g., `"English"`).
+  #
+  # This is intended to be used in forms, when selecting the default locale
+  # form a given list, or when creating a list of radio buttons, for example.
   #
   module LocalizedLocalesHelper
+    # Converts a given array of strings to an array of Objects representing
+    # locales.
+    #
+    # collection - an Array of Strings. By default it uses all the available
+    #   locales in Decidim, but you can passa nother collection of locales (for
+    #   example, the available locales for an organization)
     def localized_locales(collection = Decidim.available_locales)
       klass = Class.new do
         def initialize(locale)
