d00f058b0072fa63e87e07fbde7f8263891a2e03
Oriol Gual <oriol.gual@gmail.com>
2016-11-23 12:04:36 +0100

    Copy the demo authorization handler to the target app. Closes #256

--- lib/generators/decidim/demo_generator.rb
+++ lib/generators/decidim/demo_generator.rb
@@ -7,20 +7,20 @@ require_relative "install_generator"
 
 module Decidim
   module Generators
-    # Generates a Rails app and installs decidim to it. Uses the default Rails
-    # generator for most of the work.
-    #
-    # Remember that, for how generators work, actions are executed based on the
-    # definition order of the public methods.
+    # Modifies an existing decidim app so it can be used as a demo of Dcidim.
     class DemoGenerator < Rails::Generators::Base
+      def source_paths
+        [
+          File.expand_path("../../../../decidim-dev/lib/decidim", __FILE__)
+        ]
+      end
+
       def authorization_handlers
         remove_file "app/services/example_authorization_handler.rb"
-        inject_into_file "config/initializers/decidim.rb", before: "Decidim" do
-          "require \"decidim/dummy_authorization_handler\" \n"
-        end
         gsub_file "config/initializers/decidim.rb",
                   /ExampleAuthorizationHandler/,
                   "Decidim::DummyAuthorizationHandler"
+        template "dummy_authorization_handler.rb", "app/services/decidim/dummy_authorization_handler.rb"
       end
     end
   end
