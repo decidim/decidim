ef9c91245e5d71571a7f5f76ca31a708314196d3
Marc Riera <mrc2407@gmail.com>
2016-11-25 11:22:56 +0100

    Add i18n for errors

--- decidim-core/spec/models/decidim/category_spec.rb
+++ decidim-core/spec/models/decidim/category_spec.rb
@@ -19,6 +19,11 @@ module Decidim
       let(:category) { build(:category, parent: subcategory) }
 
       it { is_expected.not_to be_valid }
+
+      it "adds an error" do
+        subject.valid?
+        expect(subject.errors[:parent_id]).to eq ["can't be inside of a subcategory"]
+      end
     end
 
     context "before saving a subcategory" do
