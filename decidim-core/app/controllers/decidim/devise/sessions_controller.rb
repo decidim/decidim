82a0db16aec886ac32822d428d6923d5bb38146a
Marc Riera <mrc2407@gmail.com>
2016-11-28 11:11:18 +0100

    Redirect admins to admin on login (#288)
    Fix login redirect

--- decidim-core/app/controllers/decidim/devise/sessions_controller.rb
+++ decidim-core/app/controllers/decidim/devise/sessions_controller.rb
@@ -8,11 +8,23 @@ module Decidim
       layout "layouts/decidim/application"
 
       def after_sign_in_path_for(user)
-        if user.is_a?(User) && user.sign_in_count == 1 && Decidim.authorization_handlers.any?
+        if !pending_redirect?(user) && first_login_and_not_authorized?(user)
           authorizations_path
         else
           super
         end
+      end
+
+      # Calling the `stored_location_for` method removes the key, so in order
+      # to check if there's any pending redirect after login I need to call
+      # this method and use the value to set a pending redirect. This is the
+      # only way to do this without checking the session directly.
+      def pending_redirect?(user)
+        store_location_for(user, stored_location_for(user))
+      end
+
+      def first_login_and_not_authorized?(user)
+        user.is_a?(User) && user.sign_in_count == 1 && Decidim.authorization_handlers.any?
       end
     end
   end
--- decidim-core/spec/controllers/sessions_controller_spec.rb
+++ decidim-core/spec/controllers/sessions_controller_spec.rb
@@ -8,30 +8,42 @@ module Decidim
         subject { controller.after_sign_in_path_for(user) }
 
         context "when the given resource is a user" do
-          context "when it is the first time to log in" do
-            let(:user) { build(:user, sign_in_count: 1) }
+          context "and is an admin" do
+            let(:user) { build(:user, :admin, sign_in_count: 1) }
 
-            context "when there are authorization handlers" do
-              before do
-                Decidim.authorization_handlers = [Decidim::DummyAuthorizationHandler]
-              end
-
-              it { is_expected.to eq("/authorizations") }
+            before do
+              controller.session[:"user_return_to"] = account_path
             end
 
-            context "otherwise" do
-              before do
-                Decidim.authorization_handlers = []
+            it { is_expected.to eq account_path }
+          end
+
+          context "and is not an admin" do
+            context "when it is the first time to log in" do
+              let(:user) { build(:user, sign_in_count: 1) }
+
+              context "when there are authorization handlers" do
+                before do
+                  Decidim.authorization_handlers = [Decidim::DummyAuthorizationHandler]
+                end
+
+                it { is_expected.to eq("/authorizations") }
               end
+
+              context "otherwise" do
+                before do
+                  Decidim.authorization_handlers = []
+                end
+
+                it { is_expected.to eq("/") }
+              end
+            end
+
+            context "and it's not the first time to log in" do
+              let(:user) { build(:user, sign_in_count: 2) }
 
               it { is_expected.to eq("/") }
             end
-          end
-
-          context "otherwise" do
-            let(:user) { build(:user, sign_in_count: 2) }
-
-            it { is_expected.to eq("/") }
           end
         end
       end
