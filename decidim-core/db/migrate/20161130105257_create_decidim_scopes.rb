04f476a74e77d721cc0331493b7a24a7e70123e2
Marc Riera Casals <mrc2407@gmail.com>
2016-12-02 14:20:18 +0100

    Address comments

--- decidim-core/db/migrate/20161130105257_create_decidim_scopes.rb
+++ decidim-core/db/migrate/20161130105257_create_decidim_scopes.rb
@@ -1,7 +1,7 @@
 class CreateDecidimScopes < ActiveRecord::Migration[5.0]
   def change
     create_table :decidim_scopes do |t|
-      t.string :name, null: false
+      t.string :name, null: false, index: :uniqueness
       t.references :decidim_organization, foreign_key: true, index: true
       t.timestamps
     end
