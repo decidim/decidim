5720b71bac7a9be81a5967a1ccbc71d18b089988
David Morcillo Muñoz <david.morcillo@gmail.com>
2016-12-02 10:21:35 +0100

    Add es6-object-assign but not working

--- decidim-comments/app/frontend/entry.js
+++ decidim-comments/app/frontend/entry.js
@@ -1,6 +1,9 @@
-import Comments from './comments/comments.component';
+/* eslint-disable import/first */
 
+require('es6-object-assign').polyfill();
 require('react-dom');
+
+import Comments from './comments/comments.component';
 
 // Expose globals for react-rails
 window.Comments = Comments;
