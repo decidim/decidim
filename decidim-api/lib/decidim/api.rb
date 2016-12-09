31420e948ab444e1c3b9cffa9ca33b82498e2bbe
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-12-09 14:13:16 +0100

    Fix requires

--- decidim-api/lib/decidim/api.rb
+++ decidim-api/lib/decidim/api.rb
@@ -2,7 +2,7 @@
 require "decidim/api/engine"
 require "decidim/api/schema"
 require "decidim/api/query_type"
-require "decidim/api/translated_field"
+require "decidim/api/translated_field_type"
 
 module Decidim
   # This module holds all business logic related to exposing a Public API for
