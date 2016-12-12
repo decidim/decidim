9c27a282da2a98722bb40b7d0097ed6e68d8b9c6
David Morcillo Muñoz <david.morcillo@gmail.com>
2016-12-12 13:09:04 +0100

    Add reply button and form to comments form

--- decidim-comments/app/frontend/comments/comment.component.jsx
+++ decidim-comments/app/frontend/comments/comment.component.jsx
@@ -1,45 +1,90 @@
-import { propType }    from 'graphql-anywhere';
-import gql             from 'graphql-tag';
-import UserAvatar      from 'react-user-avatar';
-import moment          from 'moment';
+import { Component, PropTypes } from 'react';
+import { propType }             from 'graphql-anywhere';
+import gql                      from 'graphql-tag';
+import UserAvatar               from 'react-user-avatar';
+import moment                   from 'moment';
 
-import commentFragment from './comment.fragment.graphql'
+import AddCommentForm           from './add_comment_form.component';
+
+import commentFragment          from './comment.fragment.graphql';
 
 /**
- * @returns {ReactComponent} - A single comment component with the author info and the comment's body
+ * A single comment component with the author info and the comment's body
  */
-const Comment = ({ comment: { author, body, createdAt } }) => {
-  let authorInitialLetter = " ";
-  const formattedCreatedAt = ` ${moment(createdAt, "YYYY-MM-DD HH:mm:ss z").format("LLL")}`;
-  
-  if (author.name.length > 0) {
-    authorInitialLetter = author.name[0];
+class Comment extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showReplyForm: false
+    };
   }
 
-  return (
-    <article className="comment">
-      <div className="comment__header">
-        <div className="author-data">
-          <div className="author-data__main">
-            <div className="author author--inline">
-              <span style={{ color: "#fff" }} className="author__avatar">
-                <UserAvatar size="20" name={authorInitialLetter} />
-              </span>
-              <a className="author__name">{author.name}</a>
-              <time dateTime={createdAt}>{formattedCreatedAt}</time>
+  render() {
+    const { comment: { author, body, createdAt } } = this.props;
+
+    let authorInitialLetter = " ";
+    const formattedCreatedAt = ` ${moment(createdAt, "YYYY-MM-DD HH:mm:ss z").format("LLL")}`;
+    
+    if (author.name.length > 0) {
+      authorInitialLetter = author.name[0];
+    }
+
+    return (
+      <article className="comment">
+        <div className="comment__header">
+          <div className="author-data">
+            <div className="author-data__main">
+              <div className="author author--inline">
+                <span style={{ color: "#fff" }} className="author__avatar">
+                  <UserAvatar size="20" name={authorInitialLetter} />
+                </span>
+                <a className="author__name">{author.name}</a>
+                <time dateTime={createdAt}>{formattedCreatedAt}</time>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="comment__content">
-        <p>{ body }</p>
-      </div>
-      <div className="comment__foter">
-        &nbsp;
-      </div>
-    </article>
-  );
-};
+        <div className="comment__content">
+          <p>{ body }</p>
+        </div>
+        <div className="comment__foter">
+          {this._renderReplyButton()}
+        </div>
+        {this._renderReplyForm()}
+      </article>
+    );
+  }
+
+  _renderReplyButton() {
+    const { currentUser } = this.props;
+    const { showReplyForm } = this.state;
+
+    if (currentUser) {
+      return (
+        <button 
+          className="comment__reply muted-link"
+          aria-controls="comment1-reply"
+          onClick={() => this.setState({ showReplyForm: !showReplyForm })}
+        >
+          Responder
+        </button>
+      );
+    }
+
+    return <div>&nbsp</div>;
+  }
+
+  _renderReplyForm() {
+    const { showReplyForm } = this.state;
+
+    if (showReplyForm) {
+      return <AddCommentForm />;
+    }
+
+    return null;
+  }
+}
 
 Comment.fragments = {
   comment: gql`
@@ -48,7 +93,10 @@ Comment.fragments = {
 };
 
 Comment.propTypes = {
-  comment: propType(Comment.fragments.comment).isRequired
+  comment: propType(Comment.fragments.comment).isRequired,
+  currentUser: PropTypes.shape({
+    name: PropTypes.string.isRequired
+  })
 };
 
 export default Comment;
--- decidim-comments/app/frontend/comments/comment.component.test.jsx
+++ decidim-comments/app/frontend/comments/comment.component.test.jsx
@@ -1,48 +1,79 @@
-import { shallow }           from 'enzyme';
-import { filter }            from 'graphql-anywhere';
-import gql                   from 'graphql-tag';
+import { shallow }             from 'enzyme';
+import { filter }              from 'graphql-anywhere';
+import gql                     from 'graphql-tag';
 
-import Comment               from './comment.component';
+import Comment                 from './comment.component';
+import AddCommentForm          from './add_comment_form.component';
 
-import commentFragment       from './comment.fragment.graphql'
+import commentFragment         from './comment.fragment.graphql'
 
-import generateCommentsData  from '../support/generate_comments_data';
+import stubComponent           from '../support/stub_component';
+import generateCommentsData    from '../support/generate_comments_data';
+import generateCurrentUserData from '../support/generate_current_user_data';
 
-describe("<Comment />", () => {
+describe("<Comment currentUser={currentUser} />", () => {
   let comment = {};
+  let currentUser = null;
+
+  stubComponent(AddCommentForm);
 
   beforeEach(() => {
     const commentsData = generateCommentsData(1);
+    const currentUserData = generateCurrentUserData();
     
     const fragment = gql`
       ${commentFragment}
     `;
 
     comment = filter(fragment, commentsData[0]);
+    currentUser = currentUserData;
   });
 
   it("should render an article with class comment", () => {
-    const wrapper = shallow(<Comment comment={comment} />);
+    const wrapper = shallow(<Comment comment={comment} currentUser={currentUser} />);
     expect(wrapper.find('article.comment')).to.present();
   });
 
   it("should render a time tag with comment's created at", () => {
-    const wrapper = shallow(<Comment comment={comment} />);
+    const wrapper = shallow(<Comment comment={comment} currentUser={currentUser} />);
     expect(wrapper.find('time')).to.have.text(comment.created_at);
   });
 
   it("should render author's name in a link with class author__name", () => {
-    const wrapper = shallow(<Comment comment={comment} />);
+    const wrapper = shallow(<Comment comment={comment} currentUser={currentUser} />);
     expect(wrapper.find('a.author__name')).to.have.text(comment.author.name);
   });
 
   it("should render author's avatar using the UserAvatar component and the author's name first letter", () => {
-    const wrapper = shallow(<Comment comment={comment} />);
+    const wrapper = shallow(<Comment comment={comment} currentUser={currentUser} />);
     expect(wrapper.find('UserAvatar')).to.have.prop('name', comment.author.name[0]);
   });
 
   it("should render comment's body on a div with class comment__content", () => {
-    const wrapper = shallow(<Comment comment={comment} />);
+    const wrapper = shallow(<Comment comment={comment} currentUser={currentUser} />);
     expect(wrapper.find('div.comment__content')).to.have.text(comment.body);
+  });
+
+  it("should initialize with a state property showReplyForm as false", () => {
+    const wrapper = shallow(<Comment comment={comment} currentUser={currentUser} />);
+    expect(wrapper).to.have.state('showReplyForm', false);
+  });
+
+  it("should render a AddCommentForm component when clicking the reply button", () => {
+    const wrapper = shallow(<Comment comment={comment} currentUser={currentUser} />);
+    expect(wrapper.find(AddCommentForm)).not.to.be.present();
+    wrapper.find('button.comment__reply').simulate('click');
+    expect(wrapper.find(AddCommentForm)).to.be.present();
+  });
+
+  describe("when user is not logged in", () => {
+    beforeEach(() => {
+      currentUser = null;
+    });
+
+    it("should not render reply button", () => {
+      const wrapper = shallow(<Comment comment={comment} currentUser={currentUser} />);
+      expect(wrapper.find('button.comment__reply')).not.to.be.present();
+    });
   });
 });
