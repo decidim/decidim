b1d3143f1e91990b55d726c5fc2cba87c522ee25
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-12-13 14:01:01 +0100

    Fix Admin::BaseController

---
+++ decidim-admin/lib/decidim/admin/features.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+module Decidim
+  module Admin
+    # This module contains all logic related to Features and how they're handled
+    # at the admin panel level.
+    module Features
+      autoload :BaseController, "decidim/admin/features/base_controller"
+    end
+  end
+end
