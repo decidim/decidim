1042c4fa775407ea753e389b7c6eb7d84ed54c5d
David Morcillo <david.morcillo@gmail.com>
2016-12-13 09:27:01 +0100

    Use webpack 2 (#337)
    * Use webpack-2 and tweak config a bit
    
    * Enable tree shaking
    
    * Enable test coverage for karma
    
    * Transpile webpack config using babel
    
    * Ignore coverage folder eslint
    
    * Travis is not babel friendly

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,67 @@
-const path = require('path');
-const webpack = require('webpack');
+const webpack            = require('webpack');
+const webpackValidator   = require('webpack-validator');
+const webpackConfigUtils = require('webpack-config-utils');
+const getIfUtils         = webpackConfigUtils.getIfUtils;
+const ProgressBarPlugin  = require('progress-bar-webpack-plugin');
 
-module.exports = {
+module.exports = env => {
+  const envUtils = getIfUtils(env);
+  const ifProd = envUtils.ifProd;
+
+  const config = webpackValidator({
     entry: {
-        comments: './decidim-comments/app/frontend/entry.js'
+      comments: './decidim-comments/app/frontend/entry.js'
     },
     output: {
-        path: __dirname,
-        filename: 'decidim-[name]/app/assets/javascripts/decidim/[name]/bundle.js'
+      path: __dirname,
+      filename: 'decidim-[name]/app/assets/javascripts/decidim/[name]/bundle.js'
     },
     resolve: {
-        extensions: ['', '.js', '.jsx', '.graphql', '.yml']
+      extensions: ['.js', '.jsx', '.graphql', '.yml']
+    },
+    devtool: ifProd('source-map', 'eval'),
+    module: {
+      noParse: [
+        /\/sinon\.js/
+      ],
+      loaders: [
+        { 
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loaders: ['babel-loader', 'eslint-loader'] 
+        },
+        {
+          test: /\.(yml|yaml)$/,
+          loaders: ['json-loader', 'yaml-loader']
+        },
+        {
+          test: /\.(graphql|gql)$/,
+          loaders: ['raw-loader']
+        },
+        { 
+          test: require.resolve("react"),
+          loader: "expose-loader?React"
+        },
+        { 
+          test: require.resolve("react-dom"),
+          loader: "expose-loader?ReactDOM"
+        }
+      ]
     },
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-        }),
+      new ProgressBarPlugin(),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: ifProd('"production"', '"development"')
+        }
+      })
     ],
-    module: {
-        noParse: [
-          /\/sinon\.js/
-        ],
-        loaders: [
-            { 
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loaders: ['babel', 'eslint'] 
-            },
-            {
-                test: /\.(graphql|gql)$/,
-                exclude: /node_modules/,
-                loaders: ['raw']
-            },
-            {
-                test: /\.(jpg|png)$/,
-                loader: 'url'
-            },
-            {
-                test: /\.(yml|yaml)$/,
-                loaders: ['json', 'yaml']
-            },
-            { 
-                test: require.resolve("react"),
-                loader: "expose?React"
-            },
-            { 
-                test: require.resolve("react-dom"),
-                loader: "expose?ReactDOM"
-            }
-        ]
-    },
     externals: {
-        'cheerio': 'window',
-        'react/lib/ExecutionEnvironment': true,
-        'react/lib/ReactContext': true,
-        'react/addons': true
-    },
-    eslint: {
-        configFile: '.eslintrc.json'
+      'cheerio': 'window',
+      'react/addons': 'react',
+      'react/lib/ExecutionEnvironment': 'react',
+      'react/lib/ReactContext': 'react'
     }
+  });
+  return config;
 };
-
-      
