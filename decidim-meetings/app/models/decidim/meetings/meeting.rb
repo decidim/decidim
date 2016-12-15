8f3bf9a6e7e3956a14941ebef38d68f6705c1323
Marc Riera Casals <mrc2407@gmail.com>
2016-12-15 15:03:37 +0100

    Reduce unused arguments

--- decidim-meetings/app/models/decidim/meetings/meeting.rb
+++ decidim-meetings/app/models/decidim/meetings/meeting.rb
@@ -4,9 +4,10 @@ module Decidim
     # The data store for a Meeting in the Decidim::Meetings component. It stores a
     # title, description and any other useful information to render a custom meeting.
     class Meeting < Meetings::ApplicationRecord
-      validates :title, presence: true
       belongs_to :feature, foreign_key: "decidim_feature_id", class_name: Decidim::Feature
       belongs_to :author, foreign_key: "decidim_author_id", class_name: Decidim::User
+
+      validates :title, presence: true
     end
   end
 end
