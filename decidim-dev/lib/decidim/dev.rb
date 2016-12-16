816d676b7066695250706f7675858506bac896f9
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-12-16 14:42:33 +0100

    Add documentation

--- decidim-dev/lib/decidim/dev.rb
+++ decidim-dev/lib/decidim/dev.rb
@@ -2,6 +2,9 @@
 require "decidim/dev/assets"
 
 module Decidim
+  # Decidim::Dev holds all the convenience logic and libraries to be able to
+  # create external libraries that create test apps and test themselves against
+  # them.
   module Dev
   end
 end
