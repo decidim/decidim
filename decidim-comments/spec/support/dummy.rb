7d450ddf0a55a109bf53cf1679cfd46da825f8ec
David Morcillo Muñoz <david.morcillo@gmail.com>
2016-12-19 12:08:58 +0100

    Include application.js to dummy controller

--- decidim-comments/spec/support/dummy.rb
+++ decidim-comments/spec/support/dummy.rb
@@ -12,6 +12,9 @@ class Decidim::DummyController < Decidim::ApplicationController
     @participatory_process = Decidim::ParticipatoryProcess.find(params[:id])
     @options = params.slice(:arguable)
     @options.each { |key, val| @options[key] = val === 'true' }
-    render inline: "<%= comments_for(@participatory_process, @options) %>".html_safe
+    render inline: %{
+      <%= javascript_include_tag 'application' %>
+      <%= comments_for(@participatory_process, @options) %>
+    }.html_safe
   end
 end
