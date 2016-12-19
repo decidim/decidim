0080357514cb1f882357d00535ced3c634868b8c
David Morcillo Muñoz <david.morcillo@gmail.com>
2016-12-19 09:37:16 +0100

    Fix rubocop issues

--- decidim-comments/app/helpers/decidim/comments/comments_helper.rb
+++ decidim-comments/app/helpers/decidim/comments/comments_helper.rb
@@ -16,34 +16,32 @@ module Decidim
         commentable_id = resource.id.to_s
         node_id = "comments-for-#{commentable_type.demodulize}-#{commentable_id}"
 
-        react_comments_component(node_id, {
-          commentableType: commentable_type,
-          commentableId: commentable_id,
-          options: options.slice(:arguable),
-          locale: I18n.locale
-        })
+        react_comments_component(node_id, commentableType: commentable_type,
+                                          commentableId: commentable_id,
+                                          options: options.slice(:arguable),
+                                          locale: I18n.locale)
       end
-      
+
       # Private: Render Comments component using inline javascript
       #
       # node_id - The id of the DOMElement to render the React component
       # props   - A hash corresponding to Comments component props
       def react_comments_component(node_id, props)
-        content_tag('div', '', id: node_id) +
-        javascript_tag(%{
-          $.getScript('#{asset_path('decidim/comments/comments')}')
-            .then(function () {
-              window.DecidimComments.renderCommentsComponent(
-                '#{node_id}',
-                {
-                  commentableType: "#{props[:commentableType]}",
-                  commentableId: "#{props[:commentableId]}",
-                  options: JSON.parse("#{j(props[:options].to_json)}"),
-                  locale: "#{props[:locale]}"
-                }
-              );
-            });
-        })
+        content_tag("div", "", id: node_id) +
+          javascript_tag(%{
+            $.getScript('#{asset_path("decidim/comments/comments")}')
+              .then(function () {
+                window.DecidimComments.renderCommentsComponent(
+                  '#{node_id}',
+                  {
+                    commentableType: "#{props[:commentableType]}",
+                    commentableId: "#{props[:commentableId]}",
+                    options: JSON.parse("#{j(props[:options].to_json)}"),
+                    locale: "#{props[:locale]}"
+                  }
+                );
+              });
+          })
       end
     end
   end
