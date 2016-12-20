ae980dad4ed152fcf77206b0edad9fa6f0ffe74b
Oriol Gual <oriol.gual@gmail.com>
2016-12-20 11:17:03 +0100

    Link to static pages should use page title (#383)

--- decidim-core/spec/features/homepage_spec.rb
+++ decidim-core/spec/features/homepage_spec.rb
@@ -13,5 +13,27 @@ describe "Homepage", type: :feature do
     it "welcomes the user" do
       expect(page).to have_content(organization.name)
     end
+
+    context "when there are static pages" do
+      let!(:static_pages) { create_list(:static_page, 3, organization: organization) }
+      before do
+        visit current_path
+      end
+
+      it "includes links to them" do
+        within ".main-footer" do
+          expect(page).to have_css("li a", count: 3)
+          static_pages.each do |static_page|
+            expect(page).to have_content(static_page.title["en"])
+          end
+        end
+
+        static_page = static_pages.first
+        click_link static_page.title["en"]
+        expect(page).to have_i18n_content(static_page.title, locale: "en")
+
+        expect(page).to have_i18n_content(static_page.content, locale: "en")
+      end
+    end
   end
 end
