e475bbc8ebf6a4e5f5ee731fe95ecd01ef515176
Oriol Gual <oriol.gual@gmail.com>
2017-01-03 16:43:36 +0100

    Add something

--- decidim-core/spec/commands/decidim/invite_user_again_spec.rb
+++ decidim-core/spec/commands/decidim/invite_user_again_spec.rb
@@ -2,12 +2,18 @@ require "spec_helper"
 
 module Decidim
   describe InviteUserAgain do
+    command { described_class.new(user, "invite_admin") }
     context "when the user was invited" do
+      let(:user) { build(:user).invite! }
+
       it "sends the invitation instructions"
-      it "broadcasts ok"
+      it "broadcasts ok" do
+        expect { command.call }.to broadcast(:valid)
     end
 
     context "when the user was not invited initially" do
+      let(:user) { create(:user) }
+
       it "does not send an email"
       it "broadcasts invalid"
     end
