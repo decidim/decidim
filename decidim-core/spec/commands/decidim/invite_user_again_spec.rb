6c2eed8afb90dda6efe27d8f8ec9a0fb40469896
Oriol Gual <oriol.gual@gmail.com>
2017-01-09 11:21:38 +0100

    specs

--- decidim-core/spec/commands/decidim/invite_user_again_spec.rb
+++ decidim-core/spec/commands/decidim/invite_user_again_spec.rb
@@ -2,20 +2,40 @@ require "spec_helper"
 
 module Decidim
   describe InviteUserAgain do
-    command { described_class.new(user, "invite_admin") }
+    let(:command) { described_class.new(user, "invite_admin") }
     context "when the user was invited" do
-      let(:user) { build(:user).invite! }
+      let(:user) { build(:user) }
 
-      it "sends the invitation instructions"
+      before do
+        user.invite!
+        clear_enqueued_jobs
+      end
+
+      it "sends the invitation instructions" do
+        command.call
+        expect(ActionMailer::DeliveryJob).to have_been_enqueued.on_queue("mailers")
+      end
+
       it "broadcasts ok" do
-        expect { command.call }.to broadcast(:valid)
+        expect { command.call }.to broadcast(:ok)
+      end
     end
 
     context "when the user was not invited initially" do
-      let(:user) { create(:user) }
+      let!(:user) { create(:user) }
 
-      it "does not send an email"
-      it "broadcasts invalid"
+      before do
+        clear_enqueued_jobs
+      end
+
+      it "does not send an email" do
+        command.call
+        expect(ActionMailer::DeliveryJob).to_not have_been_enqueued.on_queue("mailers")
+      end
+
+      it "broadcasts invalid" do
+        expect { command.call }.to broadcast(:invalid)
+      end
     end
   end
 end
