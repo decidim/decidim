ecb712541471770ad09eed2eda1987d5ebbd146b
David Morcillo Muñoz <david.morcillo@gmail.com>
2017-01-11 12:32:10 +0100

    Add missing documentation

--- decidim-meetings/app/services/decidim/meetings/meeting_search.rb
+++ decidim-meetings/app/services/decidim/meetings/meeting_search.rb
@@ -5,14 +5,20 @@ module Decidim
     # `current_feature` param with a `Decidim::Feature` in order to
     # find the meetings.
     class MeetingSearch < ResourceSearch
+      # Public: Initializes the service.
+      # feature     - A Decidim::Feature to get the meetings from.
+      # page        - The page number to paginate the results.
+      # per_page    - The number of proposals to return per page.
       def initialize(options = {})
-        super(Meeting, options)
+        super(Meeting.all, options)
       end
 
+      # Handle the order_start_time filter
       def search_order_start_time
         query.order(start_time: order_start_time)
       end
 
+      # Handle the scope_id filter
       def search_scope_id
         query.where(decidim_scope_id: scope_id)
       end
--- decidim-proposals/app/services/decidim/proposals/proposal_search.rb
+++ decidim-proposals/app/services/decidim/proposals/proposal_search.rb
@@ -7,10 +7,10 @@ module Decidim
       # Public: Initializes the service.
       # feature     - A Decidim::Feature to get the proposals from.
       # page        - The page number to paginate the results.
-      # random_seed - A random flaot number between -1 and 1 to be used as a random seed at the database.
       # per_page    - The number of proposals to return per page.
+      # random_seed - A random flaot number between -1 and 1 to be used as a random seed at the database.
       def initialize(options = {})
-        super(Proposal, options)
+        super(Proposal.all, options)
         @random_seed = options[:random_seed].to_f
       end
 
