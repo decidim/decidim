911b8267dd2a59ce58a392ceadeeebcaf91ec219
David Morcillo Muñoz <david.morcillo@gmail.com>
2017-01-12 16:06:25 +0100

    Add same features to proposal show

--- decidim-proposals/app/controllers/decidim/proposals/proposal_votes_controller.rb
+++ decidim-proposals/app/controllers/decidim/proposals/proposal_votes_controller.rb
@@ -8,7 +8,37 @@ module Decidim
 
       def create
         @proposal = Proposal.find(params[:proposal_id])
+
+        set_votes_count_classes
+        set_vote_button_classes
+
         @proposal.votes.create!(author: current_user)
+      end
+
+      private
+
+      def set_votes_count_classes
+        if request_from_list_page?
+          @number_class = "card__support__number"
+          @label_class = ""
+        else
+          @number_class = "extra__suport-number"
+          @label_class = "extra__suport-text"
+        end
+      end
+
+      def set_vote_button_classes
+        @button_class = if request_from_list_page?
+                          "small"
+                        else
+                          "expanded button--sc"
+                        end
+      end
+
+      def request_from_list_page?
+        URI(request.referer).path ==
+          URI(decidim_proposals.root_path(feature: @proposal.feature)).path.chomp("/") ||
+          URI(decidim_proposals.proposals_path(feature: @proposal.feature)).path.chomp("/")
       end
     end
   end
