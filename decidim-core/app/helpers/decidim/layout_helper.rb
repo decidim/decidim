9c9ef7f5b8a9774f043612ea1aa380fe1da87be2
Genís Matutes Pujol <genis.matutes@gmail.com>
2017-01-16 10:11:48 +0100

    Fix icon helper

--- decidim-core/app/helpers/decidim/layout_helper.rb
+++ decidim-core/app/helpers/decidim/layout_helper.rb
@@ -33,7 +33,9 @@ module Decidim
       html_properties["aria-label"] = options[:aria_label]
       html_properties["role"] = options[:role]
       html_properties["aria-hidden"] = options[:aria_hidden]
-      html_properties["class"] = "icon icon--#{name} #{options[:class]}"
+
+      icon_class = (options[:remove_icon_class] ? "" : "icon")
+      html_properties["class"] = "icon--#{name} #{icon_class}"
 
       content_tag :svg, html_properties do
         content_tag :use, nil, "xlink:href" => "#{asset_url("decidim/icons.svg")}#icon-#{name}"
