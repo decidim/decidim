7f0ebdd3c792db7c4edf59aaff800b600f44ec99
David Morcillo Muñoz <david.morcillo@gmail.com>
2017-01-16 15:35:31 +0100

    Add omniauth config to secrets defaults

--- decidim-core/config/initializers/devise.rb
+++ decidim-core/config/initializers/devise.rb
@@ -299,9 +299,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  if Rails.application.secrets.dig(:omniauth, :facebook).present?
-    config.omniauth :facebook, Rails.application.secrets.omniauth.facebook.app_id, Rails.application.secrets.omniauth.facebook.app_secret
-  end
+  config.omniauth :facebook, Rails.application.secrets.omniauth.facebook.app_id, Rails.application.secrets.omniauth.facebook.app_secret
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
