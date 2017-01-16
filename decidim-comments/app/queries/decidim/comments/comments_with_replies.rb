0f17732f93d8366be3442978a2f454bee8711445
lastpotion <89.aitor.porro@gmail.com>
2017-01-16 09:30:13 +0100

    Fix Hound warnings

--- decidim-comments/app/queries/decidim/comments/comments_with_replies.rb
+++ decidim-comments/app/queries/decidim/comments/comments_with_replies.rb
@@ -35,15 +35,15 @@ module Decidim
           )
 
         scope = case @options[:order_by]
-          when "older"
-            order_by_older(scope)
-          when "recent"
-            order_by_recent(scope)
-          when "best_rated"
-            order_by_best_rated(scope)
-          when "most_discussed"
-            order_by_most_discussed(scope)
-        end
+                  when "older"
+                    order_by_older(scope)
+                  when "recent"
+                    order_by_recent(scope)
+                  when "best_rated"
+                    order_by_best_rated(scope)
+                  when "most_discussed"
+                    order_by_most_discussed(scope)
+                end
 
         scope
       end
@@ -71,7 +71,7 @@ module Decidim
       end
 
       def count_replies(comment)
-        if comment.replies.size > 0
+        if comment.replies.size.positive?
           comment.replies.size + comment.replies.map { |reply| count_replies(reply) }.sum
         else
           0
