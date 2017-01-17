f01f291a4c758e6b5c800768af547ae4f113979d
Marc Riera <mrc2407@gmail.com>
2017-01-17 09:02:09 +0100

    Fix constant lookup

--- decidim-comments/app/models/decidim/comments/seed.rb
+++ decidim-comments/app/models/decidim/comments/seed.rb
@@ -17,7 +17,7 @@ module Decidim
           random = rand(Decidim::User.count)
           Comment.create(
             commentable: resource,
-            body: Faker::Lorem.sentence,
+            body: ::Faker::Lorem.sentence,
             author: Decidim::User.where(organization: organization).offset(random).first
           )
         end
