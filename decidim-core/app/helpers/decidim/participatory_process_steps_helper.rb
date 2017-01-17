0d24dd09fbe9343c91317d84300fbfb55abb328f
Genís Matutes Pujol <genis.matutes@gmail.com>
2017-01-17 15:12:18 +0100

    Refactor conditionals for steps

--- decidim-core/app/helpers/decidim/participatory_process_steps_helper.rb
+++ decidim-core/app/helpers/decidim/participatory_process_steps_helper.rb
@@ -4,11 +4,13 @@ module Decidim
   # ParticipatoryProcessStep depending on their date.
   module ParticipatoryProcessStepsHelper
     # Returns the class for the given step depending on their end_date.
+    #
     # step - the given ParticipatoryProcessStep
+    # past - a Boolean indicating if the step is past or not
     #
     # Returns a String.
-    def status_class_for(step)
-      status = step.end_date > Time.current ? "timeline__item--inactive" : ""
+    def step_class(step, past)
+      status = past ? "" : "timeline__item--inactive"
       step.active? ? "timeline__item--current" : status
     end
   end
