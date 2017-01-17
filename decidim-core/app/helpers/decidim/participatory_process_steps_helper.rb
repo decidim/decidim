95532ffb942e7f8080916f927feacdb6ea87d179
Genís Matutes Pujol <genis.matutes@gmail.com>
2017-01-17 11:27:31 +0100

    Move logic to a helper

---
+++ decidim-core/app/helpers/decidim/participatory_process_steps_helper.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+module Decidim
+  # Helper that provides a single method to give a class to a
+  # ParticipatoryProcessStep depending on their date.
+  module ParticipatoryProcessStepsHelper
+    # Returns the class for the given step depending on their end_date.
+    # step - the given ParticipatoryProcessStep
+    #
+    # Returns a String.
+    def status_class_for(step)
+      status = step.end_date > Time.current  ? "timeline__item--inactive" : ""
+      step.active? ? "timeline__item--current" : status
+    end
+  end
+end
