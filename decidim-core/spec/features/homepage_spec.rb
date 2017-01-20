131b15f4959707759010d382f4b46d351cf46e6d
lastpotion <89.aitor.porro@gmail.com>
2017-01-20 12:01:24 +0100

    Add statistics tests

--- decidim-core/spec/features/homepage_spec.rb
+++ decidim-core/spec/features/homepage_spec.rb
@@ -35,5 +35,51 @@ describe "Homepage", type: :feature do
         expect(page).to have_i18n_content(static_page.content, locale: "en")
       end
     end
+
+    describe "statistics" do
+      let!(:users) { create_list(:user, 4, :confirmed, organization: organization) }
+      let!(:participatory_process){
+          create_list(
+            :participatory_process,
+            2,
+            :published,
+            organization: organization,
+            description: { en: "Description", ca: "Descripció", es: "Descripción" },
+            short_description: { en: "Short description", ca: "Descripció curta", es: "Descripción corta" }
+          )
+      }
+
+      context "when organization show_statistics attribute is false" do
+        let(:organization) { create(:organization, show_statistics: false) }
+        it "should not show the statistics block" do
+          expect(page).to_not have_content("Current state of #{organization.name}")
+        end
+      end
+      
+      context "when organization show_statistics attribute is true" do
+        let(:organization) { create(:organization, show_statistics: true) }
+
+        before do
+          visit current_path
+        end
+        
+        it "should show the statistics block" do
+          within "#statistics" do
+            expect(page).to have_content("Current state of #{organization.name}")
+            expect(page).to have_content("PROCESSES")
+            expect(page).to have_content("USERS")
+          end
+        end
+
+        it "should have the correct values for the statistics" do
+          within ".users-count" do
+            expect(page).to have_content("4")
+          end     
+          within ".processes-count" do
+            expect(page).to have_content("2")
+          end 
+        end
+      end
+    end
   end
 end
