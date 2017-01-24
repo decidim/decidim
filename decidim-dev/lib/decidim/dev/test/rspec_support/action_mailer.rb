5a870be70cb96e6c6d75bff45cec72f8cc5490a5
Marc Riera <mrc2407@gmail.com>
2017-01-24 11:55:53 +0100

    Fix tests

--- decidim-dev/lib/decidim/dev/test/rspec_support/action_mailer.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/action_mailer.rb
@@ -20,7 +20,7 @@ module MailerHelpers
   end
 
   def last_email_body
-    last_email.html_part.body.encoded
+    (last_email.try(:html_part).try(:body) || last_email.body).encoded
   end
 
   def last_email_link
