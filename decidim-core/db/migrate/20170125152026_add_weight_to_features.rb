2bfab811a9619eb28a110d450e62944c561f4013
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-01-25 17:36:08 +0100

    Add default value

--- decidim-core/db/migrate/20170125152026_add_weight_to_features.rb
+++ decidim-core/db/migrate/20170125152026_add_weight_to_features.rb
@@ -1,5 +1,5 @@
 class AddWeightToFeatures < ActiveRecord::Migration[5.0]
   def change
-    add_column :decidim_features, :weight, :integer
+    add_column :decidim_features, :weight, :integer, default: 0
   end
 end
