ef9d698169cd82a18d3c0632894bf51587ed636c
David Morcillo Muñoz <david.morcillo@gmail.com>
2017-01-26 16:24:07 +0100

    Create folder and some files for the new feature

---
+++ decidim-budgets/lib/decidim/results/admin_engine.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+module Decidim
+  module Budgets
+    # This is the engine that runs on the public interface of `decidim-budgets`.
+    # It mostly handles rendering the created reuslts associated to a participatory
+    # process.
+    class AdminEngine < ::Rails::Engine
+      isolate_namespace Decidim::Results::Admin
+
+      paths["db/migrate"] = nil
+
+      routes do
+        resources :results
+        root to: "results#index"
+      end
+
+      def load_seed
+        nil
+      end
+    end
+  end
+end
---
+++ decidim-budgets/lib/decidim/results/test/factories.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+require "decidim/faker/localized"
+require "decidim/dev"
+
+FactoryGirl.define do
+  # factory :result, class: Decidim::Results::Result do
+  #   title { Decidim::Faker::Localized.sentence(3) }
+  #   description { Decidim::Faker::Localized.wrapped("<p>", "</p>") { Decidim::Faker::Localized.sentence(4) } }
+  #   short_description { Decidim::Faker::Localized.wrapped("<p>", "</p>") { Decidim::Faker::Localized.sentence(4) } }
+  #   feature
+  # end
+end
