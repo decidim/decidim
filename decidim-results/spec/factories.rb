425729ffdbf1beb2f4549323a4231d4f16bb6cdf
Marc Riera Casals <mrc2407@gmail.com>
2017-01-26 09:34:03 +0100

    Move factories to lib so that they can be included

--- decidim-results/spec/factories.rb
+++ decidim-results/spec/factories.rb
@@ -1,12 +1,4 @@
 require "decidim/core/test/factories"
 require "decidim/admin/test/factories"
 require "decidim/comments/test/factories"
-
-FactoryGirl.define do
-  factory :result, class: Decidim::Results::Result do
-    title { Decidim::Faker::Localized.sentence(3) }
-    description { Decidim::Faker::Localized.wrapped("<p>", "</p>") { Decidim::Faker::Localized.sentence(4) } }
-    short_description { Decidim::Faker::Localized.wrapped("<p>", "</p>") { Decidim::Faker::Localized.sentence(4) } }
-    feature
-  end
-end
+require "decidim/results/test/factories"
