b577c9159147aa86343588302439156b67812c2a
lastpotion <89.aitor.porro@gmail.com>
2017-01-26 16:19:34 +0100

    Add extra records migration

---
+++ decidim-core/db/migrate/20170126151123_add_extra_info_to_processes.rb
@@ -0,0 +1,8 @@
+class AddExtraInfoToProcesses < ActiveRecord::Migration[5.0]
+  def change
+      add_column :decidim_participatory_processes, :domain, :jsonb
+      add_column :decidim_participatory_processes, :end_date, :date
+      add_column :decidim_participatory_processes, :developer_group, :string
+      add_column :decidim_participatory_processes, :decidim_scope_id, :integer
+  end
+end
