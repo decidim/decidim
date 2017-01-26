d9916d8721a9ae917cdda07bd2a5afe86e76bc89
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-01-26 12:49:47 +0100

    Add missing documentation
    [ci skip]

--- decidim-admin/app/helpers/decidim/admin/feature_settings_helper.rb
+++ decidim-admin/app/helpers/decidim/admin/feature_settings_helper.rb
@@ -10,6 +10,16 @@ module Decidim
         text: :text_area
       }
 
+      # Public: Renders a form field that matches a settings attribute's
+      # type.
+      #
+      # form      - The form in which to render the field.
+      # attribute - The FeatureSettings::Attribute instance with the
+      #             description of the attribute.
+      # name      - The name of the field.
+      # options   - Extra options to be passed to the field helper.
+      #
+      # Returns a rendered form field.
       def settings_attribute_input(form, attribute, name, options = {})
         form.send(TYPES[attribute.type.to_sym], name, options)
       end
