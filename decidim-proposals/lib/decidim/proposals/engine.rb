55c156d0e63a28d94d387a40891c1b56b4fe3fc9
Josep Jaume Rey <josepjaume@gmail.com>
2017-01-30 18:51:34 +0100

    Features can register assets (#666)
    * Features can register assets
    
    * Re-add js
    
    * Fix asset

--- decidim-proposals/lib/decidim/proposals/engine.rb
+++ decidim-proposals/lib/decidim/proposals/engine.rb
@@ -18,7 +18,7 @@ module Decidim
       end
 
       initializer "decidim_proposals.assets" do |app|
-        app.config.assets.precompile += %w(decidim_proposals_manifest.js decidim_proposals_manifest.css)
+        app.config.assets.precompile += %w(decidim_proposals_manifest.js)
       end
 
       initializer "decidim_proposals.inject_abilities_to_user" do |_app|
