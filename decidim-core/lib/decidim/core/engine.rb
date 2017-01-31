25c8d2c1ae62533efc43e2244187fb3b7d1db64f
Marc Riera <mrc2407@gmail.com>
2017-01-31 14:28:53 +0100

    Fix abilities reloading (#701)

--- decidim-core/lib/decidim/core/engine.rb
+++ decidim-core/lib/decidim/core/engine.rb
@@ -73,7 +73,7 @@ module Decidim
 
       initializer "decidim_admin.inject_abilities_to_user" do |_app|
         Decidim.configure do |config|
-          config.abilities << Abilities::Everyone
+          config.abilities << "Decidim::Abilities::Everyone"
         end
       end
 
