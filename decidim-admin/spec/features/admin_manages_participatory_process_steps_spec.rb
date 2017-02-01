9835dd6941a54ab04e830f797255d01dbcc84e11
Oriol Gual <oriol.gual@gmail.com>
2017-02-01 17:05:37 +0100

    Always load shared examples & support files from engines

--- decidim-admin/spec/features/admin_manages_participatory_process_steps_spec.rb
+++ decidim-admin/spec/features/admin_manages_participatory_process_steps_spec.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-require_relative "../shared/participatory_admin_shared_context"
-require_relative "../shared/manage_process_steps_examples"
 
 describe "Admin manages participatory process steps", type: :feature do
   include_context "participatory process admin"
--- decidim-proposals/spec/features/admin_manages_proposals_spec.rb
+++ decidim-proposals/spec/features/admin_manages_proposals_spec.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-require_relative "../shared/admin_shared_context"
-require_relative "../shared/manage_proposals_examples"
 
 describe "Admin manages proposals", type: :feature do
   include_context "admin"
