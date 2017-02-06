15bd52faaf0ede880bd7768d7e03cf70a563bca2
Oriol Gual <oriol.gual@gmail.com>
2017-02-06 11:23:40 +0100

    Add Vary header so Chrome doesn't cache the wrong response (#798)

--- decidim-core/app/controllers/decidim/application_controller.rb
+++ decidim-core/app/controllers/decidim/application_controller.rb
@@ -6,11 +6,19 @@ module Decidim
     include Decidim::LocaleSwitcher
     include NeedsAuthorization
     protect_from_forgery with: :exception, prepend: true
+    after_action :add_vary_header
 
     layout "layouts/decidim/application"
 
     def user_not_authorized_path
       decidim.root_path
+    end
+
+    # Make sure Chrome doesn't use the cache from a different format. This
+    # prevents a bug where clicking the back button of the browser
+    # displays the JS response instead of the HTML one.
+    def add_vary_header
+      response.headers["Vary"] = "Accept"
     end
   end
 end
