df0120d38304e8e397da7e018fed2dcee4db9cc3
Oriol Gual <oriol.gual@gmail.com>
2017-02-07 19:44:14 +0100

    Add specs

--- decidim-core/spec/features/homepage_spec.rb
+++ decidim-core/spec/features/homepage_spec.rb
@@ -23,7 +23,7 @@ describe "Homepage", type: :feature do
 
       it "includes links to them" do
         within ".main-footer" do
-          expect(page).to have_css("li a", count: 3)
+          expect(page).to have_css("ul.footer-nav li a", count: 3)
           static_pages.each do |static_page|
             expect(page).to have_content(static_page.title["en"])
           end
@@ -52,19 +52,19 @@ describe "Homepage", type: :feature do
 
       context "when organization show_statistics attribute is false" do
         let(:organization) { create(:organization, show_statistics: false) }
-        
+
         it "should not show the statistics block" do
           expect(page).not_to have_content("Current state of #{organization.name}")
         end
       end
-      
+
       context "when organization show_statistics attribute is true" do
         let(:organization) { create(:organization, show_statistics: true) }
 
         before do
           visit current_path
         end
-        
+
         it "should show the statistics block" do
           within "#statistics" do
             expect(page).to have_content("Current state of #{organization.name}")
@@ -76,12 +76,25 @@ describe "Homepage", type: :feature do
         it "should have the correct values for the statistics" do
           within ".users-count" do
             expect(page).to have_content("4")
-          end     
+          end
 
           within ".processes-count" do
             expect(page).to have_content("2")
-          end 
+          end
         end
+      end
+    end
+
+    describe "social links" do
+      before do
+        visit current_path
+      end
+
+      it "includes the linsk to social networks" do
+        expect(page).to have_xpath("//a[@href = 'https://twitter.com/#{organization.twitter_handler}']")
+        expect(page).to have_xpath("//a[@href = 'https://www.facebook.com/#{organization.facebook_handler}']")
+        expect(page).to have_xpath("//a[@href = 'https://www.youtube.com/#{organization.youtube_handler}']")
+        expect(page).to have_xpath("//a[@href = 'https://www.github.com/#{organization.github_handler}']")
       end
     end
   end
