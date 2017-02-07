e848aeba4a6937842753a464d006a3fbdb5aef4f
Oriol Gual <oriol.gual@gmail.com>
2017-02-07 21:04:55 +0100

    Double the resolution for official uploaders because retina displays

--- decidim-core/app/uploaders/decidim/official_image_footer_uploader.rb
+++ decidim-core/app/uploaders/decidim/official_image_footer_uploader.rb
@@ -3,7 +3,7 @@ module Decidim
   # This class deals with uploading hero images to ParticipatoryProcesses.
   class OfficialImageFooterUploader < ImageUploader
     include CarrierWave::MiniMagick
-    process resize_to_limit: [150, 45]
+    process resize_to_limit: [300, 90]
 
     def max_image_height_or_width
       300
--- decidim-core/app/uploaders/decidim/official_image_header_uploader.rb
+++ decidim-core/app/uploaders/decidim/official_image_header_uploader.rb
@@ -3,7 +3,7 @@ module Decidim
   # This class deals with uploading hero images to ParticipatoryProcesses.
   class OfficialImageHeaderUploader < ImageUploader
     include CarrierWave::MiniMagick
-    process resize_to_limit: [40, 40]
+    process resize_to_limit: [80, 80]
 
     def max_image_height_or_width
       300
