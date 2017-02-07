8c6e132a81ba9e6d1a399c7ba2182e08deaf22f0
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-02-07 14:03:35 +0100

    Fix specs

--- decidim-budgets/spec/shared/manage_projects_examples.rb
+++ decidim-budgets/spec/shared/manage_projects_examples.rb
@@ -50,13 +50,6 @@ RSpec.shared_examples "manage projects" do
         ca: "El meu projecte"
       )
       fill_in_i18n_editor(
-        :project_short_description,
-        "#short_description-tabs",
-        en: "Short description",
-        es: "Descripción corta",
-        ca: "Descripció curta"
-      )
-      fill_in_i18n_editor(
         :project_description,
         "#description-tabs",
         en: "A longer description",
