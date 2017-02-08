4096213a72d4c86443246a5cce3c77bde1ad1b58
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-02-08 17:51:54 +0100

    Add docs and locales

--- decidim-core/app/validators/etiquette_validator.rb
+++ decidim-core/app/validators/etiquette_validator.rb
@@ -1,4 +1,7 @@
 # coding: utf-8
+# frozen_string_literal: true
+# This validator takes care of ensuring the validated content is
+# respectful, doesn't use caps, and overall is meaningful.
 class EtiquetteValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
@@ -12,32 +15,27 @@ class EtiquetteValidator < ActiveModel::EachValidator
   private
 
   def validate_caps(record, attribute, value)
-    if value.scan(/[A-Z]/).length > value.length / 3
-      record.errors.add(attribute, options[:message] || :too_much_caps)
-    end
+    return if value.scan(/[A-Z]/).length < value.length / 3
+    record.errors.add(attribute, options[:message] || :too_much_caps)
   end
 
   def validate_marks(record, attribute, value)
-    if value.scan(/[!?¡¿]{2,}/).length > 0
-      record.errors.add(attribute, options[:message] || :too_many_marks)
-    end
+    return if value.scan(/[!?¡¿]{2,}/).empty?
+    record.errors.add(attribute, options[:message] || :too_many_marks)
   end
 
   def validate_long_words(record, attribute, value)
-    if value.scan(/[A-z]{30,}/).length > 0
-      record.errors.add(attribute, options[:message] || :long_words)
-    end
+    return if value.scan(/[A-z]{30,}/).empty?
+    record.errors.add(attribute, options[:message] || :long_words)
   end
 
   def validate_caps_first(record, attribute, value)
-    if value.scan(/^[a-z]{1}/).length > 0
-      record.errors.add(attribute, options[:message] || :must_start_with_caps)
-    end
+    return if value.scan(/^[a-z]{1}/).empty?
+    record.errors.add(attribute, options[:message] || :must_start_with_caps)
   end
 
   def validate_length(record, attribute, value)
-    if value.length < 15
-      record.errors.add(attribute, options[:message] || :too_short)
-    end
+    return if value.length > 15
+    record.errors.add(attribute, options[:message] || :too_short)
   end
 end
