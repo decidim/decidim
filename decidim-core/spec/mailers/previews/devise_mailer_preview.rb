6fee2b93f63ba5f165d9e91b3a93fa6e844dea08
Josep Jaume Rey <josepjaume@gmail.com>
2017-02-08 10:36:41 +0100

    Fix logo size (#853)
    * Fix logo size
    
    * Fix logo size
    
    * Update mailers

---
+++ decidim-core/spec/mailers/previews/devise_mailer_preview.rb
@@ -0,0 +1,11 @@
+module Decidim
+  class DeviseMailerPreview < ActionMailer::Preview
+    def confirmation_instructions
+      DecidimDeviseMailer.confirmation_instructions(User.first, "faketoken", {})
+    end
+
+    def reset_password_instructions
+      DecidimDeviseMailer.reset_password_instructions(User.first, "faketoken", {})
+    end
+  end
+end
