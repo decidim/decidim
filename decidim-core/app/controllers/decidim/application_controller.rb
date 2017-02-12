f1519ab494c051aa6d911d67885b15cd9a8354fb
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-02-12 17:45:26 +0100

    Make devise redirect to the right path after signing in

--- decidim-core/app/controllers/decidim/application_controller.rb
+++ decidim-core/app/controllers/decidim/application_controller.rb
@@ -9,10 +9,25 @@ module Decidim
     helper Decidim::DecidimFormHelper
     helper Decidim::LanguageChooserHelper
 
+    # saves the location before loading each page so we can return to the
+    # right page. If we're on a devise page, we don't want to store that as the
+    # place to return to (for example, we don't want to return to the sign in page
+    # after signing in), which is what the :unless prevents
+    before_filter :store_current_location, :unless => :devise_controller?
+
     protect_from_forgery with: :exception, prepend: true
     after_action :add_vary_header
 
     layout "layouts/decidim/application"
+
+    private
+
+    # override the devise helper to store the current location so we can
+    # redirect to it after loggin in or out. This override makes signing in
+    # and signing up work automatically.
+    def store_current_location
+      store_location_for(:user, request.url)
+    end
 
     def user_not_authorized_path
       decidim.root_path
