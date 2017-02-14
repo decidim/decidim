cffde71947afefccd56c470d9d70bb1fe73ee1bb
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-02-14 16:26:33 +0100

    Fix newsletter

--- decidim-core/app/models/decidim/newsletter.rb
+++ decidim-core/app/models/decidim/newsletter.rb
@@ -4,7 +4,7 @@ module Decidim
     belongs_to :author, class_name: User
     belongs_to :organization
 
-    validates :title, :body, translatable_presence: true
+    validates :subject, :body, translatable_presence: true
     validate :author_belongs_to_organization
 
     # Returns true if this newsletter was already sent.
