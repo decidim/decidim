57f8a3e09fc2945a01cbe4106e6210b196b6f1f2
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-02-14 19:37:36 +0100

    Add specs

---
+++ decidim-admin/spec/jobs/newsletter_job_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+require "spec_helper"
+
+module Decidim
+  module Admin
+    describe NewsletterJob do
+      let!(:newsletter) { create(:newsletter, organization: organization, total_deliveries: 0) }
+      let!(:organization) { create(:organization) }
+      let!(:deliverable_user) { create(:user, :confirmed, newsletter_notifications: true, organization: organization) }
+      let!(:non_deliverable_user) { create(:user, :confirmed, newsletter_notifications: false, organization: organization) }
+
+      it "delivers a newsletter to a the eligible users" do
+        expect(NewsletterDeliveryJob).to receive(:perform_later).with(deliverable_user, newsletter)
+
+        NewsletterJob.perform_now(newsletter)
+      end
+
+      it "updates the recipients count" do
+        NewsletterJob.perform_now(newsletter)
+        expect(newsletter.reload.total_recipients).to eq(1)
+      end
+
+      it "updates the deliveries count" do
+        NewsletterJob.perform_now(newsletter)
+        expect(newsletter.reload.total_deliveries).to eq(0)
+      end
+    end
+  end
+end
