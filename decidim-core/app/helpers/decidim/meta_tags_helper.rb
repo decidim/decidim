6e30cf59f13b5df9ab638613088056ef16e11154
Marc Riera Casals <mrc2407@gmail.com>
2017-02-16 10:57:48 +0100

    Improve meta titles and descriptions

--- decidim-core/app/helpers/decidim/meta_tags_helper.rb
+++ decidim-core/app/helpers/decidim/meta_tags_helper.rb
@@ -8,22 +8,40 @@ module Decidim
     # values.
     #
     # Returns nothing.
-    def add_meta_tags(tags)
+    def add_decidim_meta_tags(tags)
       add_decidim_page_title(tags[:title])
-
-      provide(:meta_description, tags[:description])
-      provide(:meta_url, tags[:url])
-      provide(:twitter_handler, tags[:twitter_handler])
-      provide(:meta_image_url, tags[:image_url])
+      add_decidim_meta_description(tags[:description])
+      add_decidim_meta_url(tags[:url])
+      add_decidim_meta_twitter_handler(tags[:twitter_handler])
+      add_decidim_meta_image_url(tags[:image_url])
     end
 
     def add_decidim_page_title(title)
-      @title ||= []
-      @title << title
+      @decidim_page_title ||= []
+      @decidim_page_title << title
     end
 
     def decidim_page_title
-      (@title || []).join(" - ")
+      (@decidim_page_title || []).join(" - ")
+    end
+
+    attr_reader :decidim_meta_description, :decidim_meta_url, :decidim_meta_image_url,
+                :decidim_meta_twitter_handler
+
+    def add_decidim_meta_description(description)
+      @decidim_meta_description ||= strip_tags(description)
+    end
+
+    def add_decidim_meta_twitter_handler(twitter_handler)
+      @decidim_meta_twitter_handler ||= twitter_handler
+    end
+
+    def add_decidim_meta_url(url)
+      @decidim_meta_url ||= url
+    end
+
+    def add_decidim_meta_image_url(image_url)
+      @decidim_meta_image_url ||= image_url
     end
   end
 end
