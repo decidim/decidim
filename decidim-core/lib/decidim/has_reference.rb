76e6f0f43b580f5622932925b002ca0d4fec4d58
Genís Matutes Pujol <genis.matutes@gmail.com>
2017-02-16 11:02:58 +0100

    Add docs and more specs

--- decidim-core/lib/decidim/has_reference.rb
+++ decidim-core/lib/decidim/has_reference.rb
@@ -12,6 +12,17 @@ module Decidim
 
       private
 
+      # Internal: Sets a unique reference for the model after it is saved, in
+      # the following format:
+      #
+      # "BCN-DPP-2017-02-6589" which in this example translates to:
+      #
+      # BCN: A setting configured at the organization to be prepended to each reference.
+      # PROP: Unique name identifier for a resource: Decidim::Proposals::Proposal (MEET for meetings or PROJ for projects).
+      # 2017-02: Year-Month of the resource creation date
+      # 6589: ID of the resource
+      #
+      # Returns nothing.
       def set_reference
         ref = organization.custom_reference
         class_identifier = self.class.name.demodulize[0..3].upcase
