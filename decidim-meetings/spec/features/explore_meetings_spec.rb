ab38590b7b4e0ac94d59f33a0b130fe4d148fda6
lastpotion <89.aitor.porro@gmail.com>
2017-02-17 09:52:03 +0100

    Add tests

--- decidim-meetings/spec/features/explore_meetings_spec.rb
+++ decidim-meetings/spec/features/explore_meetings_spec.rb
@@ -9,12 +9,9 @@ describe "Explore meetings", type: :feature do
     create_list(:meeting, meetings_count, feature: feature)
   end
 
-  before do
-    visit_feature
-  end
-
   context "index" do
     it "shows all meetings for the given process" do
+      visit_feature
       expect(page).to have_selector("article.card", count: meetings_count)
 
       meetings.each do |meeting|
@@ -24,6 +21,7 @@ describe "Explore meetings", type: :feature do
 
     context "filtering" do
       it "allows searching by text" do
+        visit_feature
         within ".filters" do
           fill_in :filter_search_text, with: translated(meetings.first.title)
         end
@@ -66,8 +64,34 @@ describe "Explore meetings", type: :feature do
       end
     end
 
-    context "shows past meetins if isn't any upcoming one.'"
-    
+    context "No upcoming meetings scheduled" do
+      let!(:meetings) do
+        create_list(:meeting, 2, feature: feature, start_time: Time.current - 4.days, end_time: Time.current - 2.days)
+      end
+
+      it "only shows the past meetings" do
+        visit_feature
+        expect(page).to have_css(".card--meeting", count: 2)
+      end
+
+      it "shows the correct warning" do
+        visit_feature
+        within ".callout" do
+          expect(page).to have_content("no scheduled meetings")
+        end
+      end
+    end
+
+    context "No meetings scheduled" do
+      let!(:meetings){[]}
+
+      it "shows the correct warning" do
+        visit_feature
+        within ".callout" do
+          expect(page).to have_content("any meeting scheduled")
+        end
+      end
+    end
   end
 
   context "show" do
@@ -80,11 +104,10 @@ describe "Explore meetings", type: :feature do
         start_time: date.beginning_of_day,
         end_time: date.end_of_day
       )
-
-      visit decidim_meetings.meeting_path(participatory_process_id: participatory_process.id, feature_id: feature.id, id: meeting.id)
     end
 
     it "shows all meeting info" do
+      visit decidim_meetings.meeting_path(participatory_process_id: participatory_process.id, feature_id: feature.id, id: meeting.id)
       expect(page).to have_i18n_content(meeting.title)
       expect(page).to have_i18n_content(meeting.description)
       expect(page).to have_i18n_content(meeting.location)
@@ -99,6 +122,7 @@ describe "Explore meetings", type: :feature do
 
     context "without category or scope" do
       it "does not show any tag" do
+        visit decidim_meetings.meeting_path(participatory_process_id: participatory_process.id, feature_id: feature.id, id: meeting.id)
         expect(page).not_to have_selector("ul.tags.tags--meeting")
       end
     end
@@ -112,6 +136,7 @@ describe "Explore meetings", type: :feature do
       end
 
       it "shows tags for category" do
+        visit decidim_meetings.meeting_path(participatory_process_id: participatory_process.id, feature_id: feature.id, id: meeting.id)
         expect(page).to have_selector("ul.tags.tags--meeting")
         within "ul.tags.tags--meeting" do
           expect(page).to have_content(translated(meeting.category.name))
@@ -119,6 +144,7 @@ describe "Explore meetings", type: :feature do
       end
 
       it "links to the filter for this category" do
+        visit decidim_meetings.meeting_path(participatory_process_id: participatory_process.id, feature_id: feature.id, id: meeting.id)
         within "ul.tags.tags--meeting" do
           click_link translated(meeting.category.name)
         end
@@ -135,6 +161,7 @@ describe "Explore meetings", type: :feature do
       end
 
       it "shows tags for scope" do
+        visit decidim_meetings.meeting_path(participatory_process_id: participatory_process.id, feature_id: feature.id, id: meeting.id)
         expect(page).to have_selector("ul.tags.tags--meeting")
         within "ul.tags.tags--meeting" do
           expect(page).to have_content(meeting.scope.name)
@@ -142,6 +169,7 @@ describe "Explore meetings", type: :feature do
       end
 
       it "links to the filter for this scope" do
+        visit decidim_meetings.meeting_path(participatory_process_id: participatory_process.id, feature_id: feature.id, id: meeting.id)
         within "ul.tags.tags--meeting" do
           click_link meeting.scope.name
         end
@@ -157,11 +185,11 @@ describe "Explore meetings", type: :feature do
 
       before do
         meeting.link_resources(proposals, "proposals_from_meeting")
-        visit_feature
-        click_link translated(meeting.title)
       end
 
       it "shows related proposals" do
+        visit_feature
+        click_link translated(meeting.title)
         proposals.each do |proposal|
           expect(page).to have_content(proposal.title)
           expect(page).to have_content(proposal.author_name)
@@ -178,11 +206,11 @@ describe "Explore meetings", type: :feature do
 
       before do
         meeting.link_resources(results, "meetings_through_proposals")
-        visit_feature
-        click_link translated(meeting.title)
       end
 
       it "shows related results" do
+        visit_feature
+        click_link translated(meeting.title)
         results.each do |result|
           expect(page).to have_i18n_content(result.title)
         end
@@ -195,13 +223,9 @@ describe "Explore meetings", type: :feature do
     context "when the meeting is closed" do
       let!(:meeting) { create(:meeting, :closed, feature: feature) }
 
-      before do
-        meeting
+      it "shows the closing report" do
         visit_feature
         click_link translated(meeting.title)
-      end
-
-      it "shows the closing report" do
         expect(page).to have_i18n_content(meeting.closing_report)
 
         within ".definition-data" do
