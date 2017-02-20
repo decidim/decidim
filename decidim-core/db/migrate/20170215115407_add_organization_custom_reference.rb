1e077a7a7ab0ef59cf17dcab01d9d7d2b42cd089
Genís Matutes Pujol <genis.matutes@gmail.com>
2017-02-20 12:47:01 +0100

    Hide reference if unexistant

--- decidim-core/db/migrate/20170215115407_add_organization_custom_reference.rb
+++ decidim-core/db/migrate/20170215115407_add_organization_custom_reference.rb
@@ -1,5 +1,5 @@
 class AddOrganizationCustomReference < ActiveRecord::Migration[5.0]
   def change
-    add_column :decidim_organizations, :custom_reference, :string
+    add_column :decidim_organizations, :custom_reference, :string, null: false
   end
 end
