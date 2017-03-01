2e58973f7fca581980814471def5477a80c37784
Marc Riera Casals <mrc2407@gmail.com>
2017-03-01 14:32:25 +0100

    Fix typos

--- decidim-core/app/services/decidim/resource_search.rb
+++ decidim-core/app/services/decidim/resource_search.rb
@@ -30,8 +30,8 @@ module Decidim
 
     # Handles the scope_id filter. When we want to show only those that do not
     # have a scope_id set, we cannot pass an empty String or nil because Searchlight
-    # will automatically filter out these params, se the method will not be used.
-    # Instead, we need to passa fake ID and then convert it inside. In this case,
+    # will automatically filter out these params, so the method will not be used.
+    # Instead, we need to pass a fake ID and then convert it inside. In this case,
     # in order to select those elements that do not have a scope_id set we use
     # `"global"` as parameter, and in the method we do the needed changes to search
     # properly.
