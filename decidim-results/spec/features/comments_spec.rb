0e3c708836d0a28f0becb36fafec7739e2b1b27f
lastpotion <89.aitor.porro@gmail.com>
2017-03-02 13:05:18 +0100

    Remove author from budgets and results

--- decidim-results/spec/features/comments_spec.rb
+++ decidim-results/spec/features/comments_spec.rb
@@ -3,8 +3,7 @@ require "spec_helper"
 
 describe "Comments", type: :feature, perform_enqueued: true do
   let!(:feature) { create(:result_feature, organization: organization) }
-  let!(:author) { create(:user, :confirmed, organization: organization) }
-  let!(:commentable) { create(:result, feature: feature, author: author) }
+  let!(:commentable) { create(:result, feature: feature) }
 
   let(:resource_path) { decidim_results.result_path(commentable, feature_id: feature, participatory_process_id: feature.participatory_process) }
   include_examples "comments"
