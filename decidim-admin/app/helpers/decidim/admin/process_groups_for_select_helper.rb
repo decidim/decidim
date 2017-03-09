32cb5e7a82d69b3eb2f55d03b90524cd155b976a
Genís Matutes Pujol <genis.matutes@gmail.com>
2017-03-09 11:24:27 +0100

    Use prompt instead of tweaking the select collection

--- decidim-admin/app/helpers/decidim/admin/process_groups_for_select_helper.rb
+++ decidim-admin/app/helpers/decidim/admin/process_groups_for_select_helper.rb
@@ -11,7 +11,6 @@ module Decidim
       # Returns an Array.
       def process_groups_for_select
         @process_groups_for_select ||=
-          [[I18n.t("decidim.participatory_processes.participatory_process_groups.none"), 0]] +
           current_organization.participatory_process_groups.map do |group|
             [translated_attribute(group.name), group.id]
           end
