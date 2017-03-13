57832dd186ed861e2671cdf5789fbd6ad41d1a81
Genís Matutes Pujol <genis.matutes@gmail.com>
2017-03-13 15:00:16 +0100

    Add append payload at app controller

--- decidim-admin/app/controllers/decidim/admin/application_controller.rb
+++ decidim-admin/app/controllers/decidim/admin/application_controller.rb
@@ -25,6 +25,18 @@ module Decidim
       def current_ability_klass
         Decidim::Admin::Abilities::Base
       end
+
+      def append_info_to_payload(payload)
+        super
+        payload[:user_id] = current_user.id
+        payload[:organization_id] = current_organization.id
+        payload[:app] = current_organization.name
+        payload[:remote_ip] = request.remote_ip
+        payload[:referer] = request.referer.to_s
+        payload[:request_id] = request.uuid
+        payload[:user_agent] = request.user_agent
+        payload[:xhr] = request.xhr? ? 'true' : 'false'
+      end
     end
   end
 end
--- decidim-core/app/controllers/decidim/application_controller.rb
+++ decidim-core/app/controllers/decidim/application_controller.rb
@@ -47,5 +47,17 @@ module Decidim
     def redirect_to_404
       raise ActionController::RoutingError, "Not Found"
     end
+
+    def append_info_to_payload(payload)
+      super
+      payload[:user_id] = current_user.id
+      payload[:organization_id] = current_organization.id
+      payload[:app] = current_organization.name
+      payload[:remote_ip] = request.remote_ip
+      payload[:referer] = request.referer.to_s
+      payload[:request_id] = request.uuid
+      payload[:user_agent] = request.user_agent
+      payload[:xhr] = request.xhr? ? 'true' : 'false'
+    end
   end
 end
