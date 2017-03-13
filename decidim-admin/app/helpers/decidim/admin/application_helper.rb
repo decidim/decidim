0cb9cb72dcdad0e8002dad7c917da316b7b0e0bf
David Morcillo Muñoz <david.morcillo@gmail.com>
2017-03-13 12:12:03 +0100

    Add basic layout

--- decidim-admin/app/helpers/decidim/admin/application_helper.rb
+++ decidim-admin/app/helpers/decidim/admin/application_helper.rb
@@ -14,6 +14,14 @@ module Decidim
       def title
         current_organization.name
       end
+
+      def admin_menu_item_to(name, url, options={})
+        content_tag :li, class: active_link_to_class(url, active: options[:active], class_active: "is-active") do
+          active_link_to url, active: options[:active], class_active: "is-active" do
+            icon(options[:icon_name] || name) + I18n.t("menu.#{name}", scope: "decidim.admin")
+          end
+        end
+      end
     end
   end
 end
