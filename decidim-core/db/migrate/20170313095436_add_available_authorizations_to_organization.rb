d8b1475a8d2e17328d17c485376cafddbad5f0b1
Marc Riera Casals <mrc2407@gmail.com>
2017-03-14 11:51:45 +0100

    Fix migration

--- decidim-core/db/migrate/20170313095436_add_available_authorizations_to_organization.rb
+++ decidim-core/db/migrate/20170313095436_add_available_authorizations_to_organization.rb
@@ -2,6 +2,9 @@ class AddAvailableAuthorizationsToOrganization < ActiveRecord::Migration[5.0]
   def change
     add_column :decidim_organizations, :available_authorizations, :string, array: true, default: []
 
-    Decidim::Organization.update_all(available_authorizations: Decidim.authorization_handlers.map(&:name))
+    handlers = Decidim.authorization_handlers.map(&:name)
+    Decidim::Organization.find_each do |org|
+      org.update_attributes(available_authorizations: handlers)
+    end
   end
 end
