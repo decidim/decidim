5b41318c33dee2b78242b902570cd0a850e87a92
Marc Riera Casals <mrc2407@gmail.com>
2017-03-14 11:42:57 +0100

    Add default values in migration

--- decidim-core/db/migrate/20170313095436_add_available_authorizations_to_organization.rb
+++ decidim-core/db/migrate/20170313095436_add_available_authorizations_to_organization.rb
@@ -1,5 +1,7 @@
 class AddAvailableAuthorizationsToOrganization < ActiveRecord::Migration[5.0]
   def change
     add_column :decidim_organizations, :available_authorizations, :string, array: true, default: []
+
+    Decidim::Organization.update_all(available_authorizations: Decidim.authorization_handlers.map(&:name))
   end
 end
