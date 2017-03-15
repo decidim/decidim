fbdeef1cc84ba1577bfe6832d2fa7a6ac1e73c91
Marc Riera Casals <mrc2407@gmail.com>
2017-03-15 10:03:17 +0100

    Fix seeds for review apps

--- lib/generators/decidim/install_generator.rb
+++ lib/generators/decidim/install_generator.rb
@@ -31,7 +31,14 @@ module Decidim
       end
 
       def add_seeds
-        append_file "db/seeds.rb", "\n# You can remove the 'faker' gem if you don't want Decidim seeds.\nDecidim.seed!"
+        append_file("db/seeds.rb", <<-SEEDS_CONTENT)
+# You can remove the 'faker' gem if you don't want Decidim seeds.
+if ENV["HEROKU_APP_NAME"].present?
+  ENV["DECIDIM_HOST"] = ENV["HEROKU_APP_NAME"] + ".herokuapp.com"
+  ENV["SEED"] = true
+end
+Decidim.seed!
+        SEEDS_CONTENT
       end
 
       def copy_initializer
