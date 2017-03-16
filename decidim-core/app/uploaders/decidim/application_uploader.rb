a1e614bb6739eaf48d0e596e104f52ca0c331e4d
Marc Riera Casals <mrc2407@gmail.com>
2017-03-16 09:16:57 +0100

    Add Decidim.base_uploads_path

--- decidim-core/app/uploaders/decidim/application_uploader.rb
+++ decidim-core/app/uploaders/decidim/application_uploader.rb
@@ -7,7 +7,10 @@ module Decidim
     # Override the directory where uploaded files will be stored.
     # This is a sensible default for uploaders that are meant to be mounted:
     def store_dir
-      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      default_path = "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+
+      return File.join(Decidim.base_uploads_path, default_path) if Decidim.base_uploads_path.present?
+      default_path
     end
   end
 end
