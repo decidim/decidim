9855bdefd84003dc5b6b150098be8cf41f526b6a
Genís Matutes Pujol <genis.matutes@gmail.com>
2017-03-21 11:14:52 +0100

    Add icon link helper

---
+++ decidim-admin/app/helpers/decidim/admin/icon_link_helper.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+module Decidim
+  module Admin
+    module IconLinkHelper
+      def icon_link_to(icon_name, link, title, klass)
+        link_to(link,
+                class: "action-icon " + klass,
+                data: { tooltip: true, disable_hover: false },
+                tooltip: true,
+                disable_hover: false,
+                title: title) do
+          icon(icon_name)
+        end
+      end
+    end
+  end
+end
