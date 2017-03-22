49633f056d317f55056fa37db01ace6afe71bac7
Genís Matutes Pujol <genis.matutes@gmail.com>
2017-03-22 09:29:51 +0100

    Add docs and refactor helper call

--- decidim-admin/app/helpers/decidim/admin/icon_link_helper.rb
+++ decidim-admin/app/helpers/decidim/admin/icon_link_helper.rb
@@ -2,11 +2,22 @@
 module Decidim
   module Admin
     module IconLinkHelper
-      def icon_link_to(icon_name, link, title, klass, method, data={})
+      # This helper adds the necessary boilerplate for the admin icon links.
+      # 
+      # icon_name - A String representing the icon name from Iconic
+      #             http://useiconic.com/open
+      # link      - The path or url where the link should point to.
+      # title     - A String that will be shown when hovering the icon.
+      # options   - An optional Hash containing extra data for the link:
+      #             method - Symbol of HTTP verb. Supported verbs are :post, :get, :delete, :patch, and :put.
+      #             class  - Any extra class that will be added to the link.
+      #             data   - This option can be used to add custom data attributes.
+      #
+      def icon_link_to(icon_name, link, title, options = {})
         link_to(link,
-                method: method,
-                class: "action-icon " + klass,
-                data: { tooltip: true, disable_hover: false }.merge(data),
+                method: @options[:method],
+                class: "action-icon " + @options[:class],
+                data: { tooltip: true, disable_hover: false }.merge(@options[:data] || {}),
                 tooltip: true,
                 disable_hover: false,
                 title: title) do
@@ -15,4 +26,3 @@ module Decidim
       end
     end
   end
-end
