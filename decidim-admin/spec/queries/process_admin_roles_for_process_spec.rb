b5681120e7e7505053309bacb6f0234f869efd2b
Genís Matutes <genis.matutes@gmail.com>
2017-03-27 11:40:04 +0200

    List collaborators at admin (#1176)

--- decidim-admin/spec/queries/process_admin_roles_for_process_spec.rb
+++ decidim-admin/spec/queries/process_admin_roles_for_process_spec.rb
@@ -5,13 +5,14 @@ describe Decidim::Admin::ProcessAdminRolesForProcess do
   let(:organization) { create :organization }
   let!(:process1) { create :participatory_process, organization: organization }
   let!(:process2) { create :participatory_process, organization: organization }
-  let!(:user_role1) { create :participatory_process_user_role, participatory_process: process1 }
+  let!(:user_role1) { create :participatory_process_user_role, participatory_process: process1, role: :admin }
   let!(:user_role2) { create :participatory_process_user_role, participatory_process: process2 }
+  let!(:user_role3) { create :participatory_process_user_role, participatory_process: process1, role: :collaborator }
 
   subject { described_class.new(process1) }
 
   it "returns only the user roles for the given process" do
-    expect(subject.query)
-      .to eq [user_role1]
+    expect(subject.query).to include user_role1
+    expect(subject.query).to include user_role3
   end
 end
