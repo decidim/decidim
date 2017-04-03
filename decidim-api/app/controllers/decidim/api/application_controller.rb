64073cbd1f972e7c58ca8b5b0a2b9ecb38463c01
David Morcillo <david.morcillo@gmail.com>
2017-04-03 14:36:24 +0200

    Skip csrf token in api controller (#1215)
    * Skip csrf token in api controller
    
    * Allow forgery protection on tests
    
    * Include csrf_meta_tags for dummy

--- decidim-api/app/controllers/decidim/api/application_controller.rb
+++ decidim-api/app/controllers/decidim/api/application_controller.rb
@@ -3,6 +3,7 @@ module Decidim
   module Api
     # Base controller for `decidim-api`. All other controllers inherit from this.
     class ApplicationController < ::DecidimController
+      skip_before_filter :verify_authenticity_token
       include NeedsOrganization
     end
   end
