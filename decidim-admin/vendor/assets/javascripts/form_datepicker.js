4d1042f64fc3e4813a52a8b29b2bd62295f3f8de
lastpotion <89.aitor.porro@gmail.com>
2017-04-05 11:49:25 +0200

    fix lint errors

--- decidim-admin/vendor/assets/javascripts/form_datepicker.js
+++ decidim-admin/vendor/assets/javascripts/form_datepicker.js
@@ -1,29 +1,26 @@
-function formDatePicker(){
-  $('[data-datepicker]').each(
-    function(){
-      const initialDate = $(this).attr('data-startdate') || '';
-      const language = $("html").attr('lang') || 'en';
-      const pickTime = $(this).attr('data-timepicker') === '';
-      const format = $(this).fdatepicker.dates[language] && $(this).fdatepicker.dates[language].format || 'mm/dd/yyyy';
+const formDatePicker = () =>
+  $('[data-datepicker]').each(() => {
+    const initialDate = $(this).attr('data-startdate') || '';
+    const language = $("html").attr('lang') || 'en';
+    const pickTime = $(this).attr('data-timepicker') === '';
+    const format = $(this).fdatepicker.dates[language] && $(this).fdatepicker.dates[language].format || 'mm/dd/yyyy';
 
-      if (pickTime) {
-         format = `${format}, hh:ii`;
-      };
-
-      $(this).fdatepicker({
-        format,
-    		initialDate,
-        language,
-    		disableDblClickSelection: true,
-    		leftArrow:'<<',
-    		rightArrow:'>>',
-        pickTime
-    	})
-      .on('changeDate', function (ev) {
-        $(ev.target).siblings("input").val(moment.utc(ev.date));
-      });
+    if (pickTime) {
+        format = `${format}, hh:ii`;
     }
-  );
-}
 
+    $(this).fdatepicker({
+      format,
+      initialDate,
+      language,
+      disableDblClickSelection: true,
+      leftArrow:'<<',
+      rightArrow:'>>',
+      pickTime
+    })
+    .on('changeDate', (ev)=>  {
+      $(ev.target).siblings("input").val(moment.utc(ev.date));
+    })
+  }
+)
 
