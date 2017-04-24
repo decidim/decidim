8c12a72d87c6a5d8d042e12d8f224263b4d16b67
David Morcillo <david.morcillo@gmail.com>
2017-04-24 09:59:31 +0200

    Test chrome version

--- run_ci.sh
+++ run_ci.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-set -ev
-if [ "$GEM" == "." ]; then
-  yarn test:ci
-  docker build --rm=false -t codegram/decidim .
-elif [ -f "run_ci.sh" ]; then
-  sh ./run_ci.sh
-fi
+# set -ev
+# if [ "$GEM" == "." ]; then
+#   yarn test:ci
+#   docker build --rm=false -t codegram/decidim .
+# elif [ -f "run_ci.sh" ]; then
+#   sh ./run_ci.sh
+# fi
 
-bundle exec rake
+# bundle exec rake
+
+$CAPYBARA_CHROMIUM_BIN --version
