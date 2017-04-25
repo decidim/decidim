57e85c7cbcc329e38b50e4e869512eb6e98d2931
David Morcillo <david.morcillo@gmail.com>
2017-04-25 09:01:50 +0200

    Don't accept alerts with phantom

--- decidim-dev/lib/decidim/dev/test/rspec_support/alert_helper.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/alert_helper.rb
@@ -1,5 +1,5 @@
 module AlertHelper
   def accept_alert
-    page.driver.browser.switch_to.alert.accept if page.driver.class == Capybara::Selenium::Driver
+    page.driver.browser.switch_to.alert.accept unless page.driver.browser.browser == :phantomjs
   end
 end
