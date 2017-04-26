d4c9ded08856ebb3f11b7d728d57203635977eec
Marc Riera Casals <mrc2407@gmail.com>
2017-04-26 12:01:40 +0200

    Fix tests

--- decidim-proposals/spec/helpers/proposal_order_helper_spec.rb
+++ decidim-proposals/spec/helpers/proposal_order_helper_spec.rb
@@ -10,10 +10,20 @@ module Decidim
 
       describe "#order_fields" do
         context "with votes enabled" do
-          let(:current_settings) { double(:current_settings, { votes_enabled?: true }) }
+          context "with votes hidden" do
+            let(:current_settings) { double(:current_settings, { votes_enabled?: true, votes_hidden?: true }) }
 
-          it "shows most_voted option to sort" do
-            expect(helper.order_fields).to include(:most_voted)
+            it "does not show most_voted option to sort" do
+              expect(helper.order_fields).not_to include(:most_voted)
+            end
+          end
+
+          context "with votes not hidden" do
+            let(:current_settings) { double(:current_settings, { votes_enabled?: true, votes_hidden?: false }) }
+
+            it "shows most_voted option to sort" do
+              expect(helper.order_fields).to include(:most_voted)
+            end
           end
         end
 
