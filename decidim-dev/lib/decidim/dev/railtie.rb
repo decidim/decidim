2cb092a5829319ac9f2ba4048949ca35b4f81274
Josep Jaume Rey <josepjaume@gmail.com>
2017-04-27 14:44:43 +0200

    Decouple decidim generator task from core (#1298)
    * Decouple decidim generator task from core
    
    * Fix rails task

---
+++ decidim-dev/lib/decidim/dev/railtie.rb
@@ -0,0 +1,13 @@
+module Decidim
+  module Dev
+    class Railtie < Rails::Railtie
+      railtie_name :decidim_dev
+
+      rake_tasks do
+        Dir[File.join(File.dirname(__FILE__), "../../tasks/*.rake")].each do |file|
+          load file
+        end
+      end
+    end
+  end
+end
