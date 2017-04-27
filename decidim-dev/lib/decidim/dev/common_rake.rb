4b796ebd79cf8270b66f6780292205fb80beb411
David Rodríguez <deivid.rodriguez@riseup.net>
2017-04-27 11:15:36 -0300

    Don't autorun i18n specs when requiring test helper

--- decidim-dev/lib/decidim/dev/common_rake.rb
+++ decidim-dev/lib/decidim/dev/common_rake.rb
@@ -2,5 +2,7 @@
 require "bundler/gem_tasks"
 require "rspec/core/rake_task"
 
-RSpec::Core::RakeTask.new(:spec)
+RSpec::Core::RakeTask.new(:spec) do |t|
+  t.rspec_opts = "--pattern **/*_spec.rb,#{__dir__}/test/i18n_spec.rb"
+end
 task default: [:spec]
