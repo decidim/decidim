12339292b322c18edfda8c6e5018807dabda267c
David Rodríguez <deivid.rodriguez@riseup.net>
2017-05-02 12:09:21 -0300

    Make sure i18n specs are actually run

--- decidim-dev/lib/decidim/dev/common_rake.rb
+++ decidim-dev/lib/decidim/dev/common_rake.rb
@@ -3,6 +3,8 @@ require "bundler/gem_tasks"
 require "rspec/core/rake_task"
 
 RSpec::Core::RakeTask.new(:spec) do |t|
-  t.rspec_opts = "--pattern **/*_spec.rb,#{__dir__}/test/i18n_spec.rb"
+  # The i18n_spec part needs to go first or it won't be include. It seems like a
+  # bug in RSpec to me... https://github.com/rspec/rspec-core/issues/2418
+  t.rspec_opts = "--pattern #{__dir__}/test/i18n_spec.rb,**/*_spec.rb"
 end
 task default: [:spec]
