23358b9a921d05e86a4cc26251ecd882c754d62e
Marc Riera <mrc2407@gmail.com>
2017-05-02 12:17:01 +0200

    Remove skip_before_filter deprecation warnings (#1320)

--- decidim-api/app/controllers/decidim/api/application_controller.rb
+++ decidim-api/app/controllers/decidim/api/application_controller.rb
@@ -3,7 +3,7 @@ module Decidim
   module Api
     # Base controller for `decidim-api`. All other controllers inherit from this.
     class ApplicationController < ::DecidimController
-      skip_before_filter :verify_authenticity_token
+      skip_before_action :verify_authenticity_token
       include NeedsOrganization
     end
   end
