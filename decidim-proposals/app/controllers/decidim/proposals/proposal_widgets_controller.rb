e3ffaf6c06183fc11f5501eac7512f0ad38a5a82
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-05-09 12:13:03 +0200

    Refactor widgets

--- decidim-proposals/app/controllers/decidim/proposals/proposal_widgets_controller.rb
+++ decidim-proposals/app/controllers/decidim/proposals/proposal_widgets_controller.rb
@@ -3,17 +3,12 @@
 module Decidim
   module Proposals
     class ProposalWidgetsController < Decidim::WidgetsController
-      helper_method :model, :current_participatory_process
       helper Proposals::ApplicationHelper
 
       private
 
       def model
         @model ||= Proposal.where(feature: params[:feature_id]).find(params[:proposal_id])
-      end
-
-      def current_participatory_process
-        @current_participatory_process ||= model.feature.participatory_process
       end
 
       def iframe_url
