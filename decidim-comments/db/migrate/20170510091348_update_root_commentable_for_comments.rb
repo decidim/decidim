f117e3b14d6ec8e48c82578c435e12135ec0e9d4
David Morcillo <david.morcillo@gmail.com>
2017-05-10 11:23:26 +0200

    Fix error on second migration

--- decidim-comments/db/migrate/20170510091348_update_root_commentable_for_comments.rb
+++ decidim-comments/db/migrate/20170510091348_update_root_commentable_for_comments.rb
@@ -1,7 +1,7 @@
 class UpdateRootCommentableForComments < ActiveRecord::Migration[5.0]
   def change
     Decidim::Comments::Comment.where(depth: 0).update_all(
-      "decidim_root_commentable_id = #{decidim_commentable_id}, decidim_root_commentable_type = #{decidim_commentable_type}"
+      "decidim_root_commentable_id = decidim_commentable_id, decidim_root_commentable_type = decidim_commentable_type"
     )
 
     Decidim::Comments::Comment.where("depth > 0").find_each do |comment|
