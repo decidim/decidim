268b2b5d2b490918dfd1f8a458ad1078b557a6b5
David Morcillo <david.morcillo@gmail.com>
2017-05-10 11:29:06 +0200

    Empty down step for migration

--- decidim-comments/db/migrate/20170510091348_update_root_commentable_for_comments.rb
+++ decidim-comments/db/migrate/20170510091348_update_root_commentable_for_comments.rb
@@ -1,5 +1,5 @@
 class UpdateRootCommentableForComments < ActiveRecord::Migration[5.0]
-  def change
+  def up
     Decidim::Comments::Comment.where(depth: 0).update_all(
       "decidim_root_commentable_id = decidim_commentable_id, decidim_root_commentable_type = decidim_commentable_type"
     )
@@ -8,6 +8,9 @@ class UpdateRootCommentableForComments < ActiveRecord::Migration[5.0]
       comment.root_commentable = root_commentable(comment)
       comment.save(validate: false)
     end
+  end
+
+  def down
   end
 
   private
