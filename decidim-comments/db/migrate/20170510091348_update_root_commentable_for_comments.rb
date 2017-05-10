72056d0f2af115bdf9ef769dc83a61cf836eee72
David Morcillo <david.morcillo@gmail.com>
2017-05-10 11:22:24 +0200

    Update migrations

--- decidim-comments/db/migrate/20170510091348_update_root_commentable_for_comments.rb
+++ decidim-comments/db/migrate/20170510091348_update_root_commentable_for_comments.rb
@@ -1,9 +1,4 @@
 class UpdateRootCommentableForComments < ActiveRecord::Migration[5.0]
-  def root_commentable(comment)
-    return comment.commentable if comment.depth.zero?
-    root_commentable comment.commentable
-  end
-
   def change
     Decidim::Comments::Comment.where(depth: 0).update_all(
       "decidim_root_commentable_id = #{decidim_commentable_id}, decidim_root_commentable_type = #{decidim_commentable_type}"
@@ -13,5 +8,12 @@ class UpdateRootCommentableForComments < ActiveRecord::Migration[5.0]
       comment.root_commentable = root_commentable(comment)
       comment.save
     end
+  end
+
+  private
+
+  def root_commentable(comment)
+    return comment.commentable if comment.depth.zero?
+    root_commentable comment.commentable
   end
 end
--- decidim-comments/db/migrate/20170510091409_set_root_commentable_null_constraints.rb
+++ decidim-comments/db/migrate/20170510091409_set_root_commentable_null_constraints.rb
@@ -1,6 +1,6 @@
 class SetRootCommentableNullConstraints < ActiveRecord::Migration[5.0]
   def change
-    change_column :decidim_comments_comments, :decidim_root_commentable_id, :integer, null: false
-    change_column :decidim_comments_comments, :decidim_root_commentable_type, :string, null: false
+    change_column_null(:decidim_comments_comments, :decidim_root_commentable_id, false)
+    change_column_null(:decidim_comments_comments, :decidim_root_commentable_type, false)
   end
 end
