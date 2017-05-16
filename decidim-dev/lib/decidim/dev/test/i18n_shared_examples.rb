ed260b73cc5edca1da7d0af72b49026e30cd6a9a
David Rodríguez <deivid.rodriguez@riseup.net>
2017-05-16 09:26:28 -0300

    More readable test failures
    With colors and all.

--- decidim-dev/lib/decidim/dev/test/i18n_shared_examples.rb
+++ decidim-dev/lib/decidim/dev/test/i18n_shared_examples.rb
@@ -8,13 +8,11 @@ RSpec.shared_examples_for "I18n sanity" do
   let(:unused_keys) { i18n.unused_keys }
 
   it "does not have missing keys" do
-    expect(missing_keys).to be_empty,
-                            "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing` to show them"
+    expect(missing_keys).to be_empty, "#{missing_keys.inspect} are missing"
   end
 
   it "does not have unused keys" do
-    expect(unused_keys).to be_empty,
-                           "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused` to show them"
+    expect(unused_keys).to be_empty, "#{unused_keys.inspect} are unused"
   end
 
   it "is normalized" do
