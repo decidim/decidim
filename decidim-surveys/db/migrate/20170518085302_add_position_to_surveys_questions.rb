fa929350734c653ad900fa2415e8475b94cde5a3
David Morcillo <david.morcillo@gmail.com>
2017-05-18 14:18:18 +0200

    Add missing index

--- decidim-surveys/db/migrate/20170518085302_add_position_to_surveys_questions.rb
+++ decidim-surveys/db/migrate/20170518085302_add_position_to_surveys_questions.rb
@@ -1,5 +1,5 @@
 class AddPositionToSurveysQuestions < ActiveRecord::Migration[5.0]
   def change
-    add_column :decidim_surveys_survey_questions, :position, :integer
+    add_column :decidim_surveys_survey_questions, :position, :integer, index: true
   end
 end
