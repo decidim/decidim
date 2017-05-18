733aad899918d0437de3d1d9e8eed4f9923503ce
Aitor Porro <89.aitor.porro@gmail.com>
2017-05-18 15:21:39 +0200

    Add order to results (#1365)
    * Add order to results
    
    * Add test to results order
    
    * Remove sleep on tests
    
    * Rubocop fixes
    
    * Add controller test
    
    * Add missing space

--- decidim-results/spec/features/explore_results_spec.rb
+++ decidim-results/spec/features/explore_results_spec.rb
@@ -3,30 +3,71 @@ require "spec_helper"
 
 describe "Explore results", type: :feature do
   include_context "feature"
-  let(:manifest_name) { "results" }
 
+  let(:titles) { %w(Biure Atque Delectus Quia Fuga) }
+  let(:manifest_name) { "results" }
   let(:results_count) { 5 }
   let!(:scope) { create :scope, organization: organization }
   let!(:results) do
-    create_list(
-      :result,
-      results_count,
-      feature: feature
-    )
-  end
-
-  before do
-    visit path
+    Array.new(results_count) do |n|
+      create(:result, title: { en: titles[n] }, feature: feature)
+    end
   end
 
   context "index" do
-    let(:path) { decidim_results.results_path(participatory_process_id: participatory_process.id, feature_id: feature.id) }
+    it "shows all results ordered alphabetically" do
+      visit_feature
 
-    it "shows all results for the given process" do
-      expect(page).to have_selector("article.card", count: results_count)
+      expect(page).to have_selector(".card--result", count: results_count)
 
       results.each do |result|
         expect(page).to have_content(translated(result.title))
+      end
+    end
+
+    context "when filtering" do
+      before do
+        create(:result, feature: feature, scope: scope)
+      end
+
+      context "when the process has a linked scope" do
+        before do
+          participatory_process.update_attributes(scope: scope)
+        end
+
+        it "enables filtering by scope" do
+          visit_feature
+
+          within ".filters" do
+            expect(page).not_to have_content(/Scopes/i)
+          end
+        end
+      end
+
+      context "when the process has no linked scope" do
+        before do
+          participatory_process.update_attributes(scope: nil)
+        end
+
+        it "enables filtering by scope" do
+          visit_feature
+
+          within ".filters" do
+            expect(page).to have_content(/Scopes/i)
+          end
+        end
+      end
+
+      context "when filtering by scope" do
+        it "lists the filtered results" do
+          visit_feature
+
+          within ".filters" do
+            check scope.name
+          end
+
+          expect(page).to have_css(".card--result", count: 1)
+        end
       end
     end
   end
@@ -37,6 +78,8 @@ describe "Explore results", type: :feature do
     let(:result) { results.first }
 
     it "shows all result info" do
+      visit path
+
       expect(page).to have_i18n_content(result.title)
       expect(page).to have_i18n_content(result.description)
       expect(page).to have_content(result.reference)
@@ -53,6 +96,8 @@ describe "Explore results", type: :feature do
 
     context "without category or scope" do
       it "does not show any tag" do
+        visit path
+
         expect(page).not_to have_selector("ul.tags.tags--result")
       end
     end
@@ -66,6 +111,8 @@ describe "Explore results", type: :feature do
       end
 
       it "shows tags for category" do
+        visit path
+
         expect(page).to have_selector("ul.tags.tags--result")
         within "ul.tags.tags--result" do
           expect(page).to have_content(translated(result.category.name))
@@ -73,6 +120,8 @@ describe "Explore results", type: :feature do
       end
 
       it "links to the filter for this category" do
+        visit path
+
         within "ul.tags.tags--result" do
           click_link translated(result.category.name)
         end
@@ -89,6 +138,8 @@ describe "Explore results", type: :feature do
       end
 
       it "shows tags for scope" do
+        visit path
+
         expect(page).to have_selector("ul.tags.tags--result")
         within "ul.tags.tags--result" do
           expect(page).to have_content(result.scope.name)
@@ -96,6 +147,8 @@ describe "Explore results", type: :feature do
       end
 
       it "links to the filter for this scope" do
+        visit path
+
         within "ul.tags.tags--result" do
           click_link result.scope.name
         end
@@ -108,11 +161,9 @@ describe "Explore results", type: :feature do
       let(:author) { create(:user, :confirmed, organization: feature.organization) }
       let!(:comments) { create_list(:comment, 3, commentable: result) }
 
-      before do
-        visit current_path
-      end
-
       it "shows the comments" do
+        visit path
+
         comments.each do |comment|
           expect(page).to have_content(comment.body)
         end
@@ -127,10 +178,11 @@ describe "Explore results", type: :feature do
 
       before do
         result.link_resources(proposals, "included_proposals")
-        visit current_path
       end
 
       it "shows related proposals" do
+        visit path
+
         proposals.each do |proposal|
           expect(page).to have_content(proposal.title)
           expect(page).to have_content(proposal.author_name)
@@ -147,66 +199,14 @@ describe "Explore results", type: :feature do
 
       before do
         result.link_resources(meetings, "meetings_through_proposals")
-        visit current_path
       end
 
       it "shows related meetings" do
+        visit path
+
         meetings.each do |meeting|
           expect(page).to have_i18n_content(meeting.title)
           expect(page).to have_i18n_content(meeting.description)
-        end
-      end
-    end
-
-    context "when filtering" do
-      before do
-        create(:result, feature: feature, scope: scope)
-        visit_feature
-      end
-
-      context "when the process has a linked scope" do
-        before do
-          participatory_process.update_attributes(scope: scope)
-          visit current_path
-        end
-
-        it "enables filtering by scope" do
-          within ".filters" do
-            expect(page).not_to have_content(/Scopes/i)
-          end
-        end
-      end
-
-      context "when the process has no linked scope" do
-        before do
-          participatory_process.update_attributes(scope: nil)
-          visit current_path
-        end
-
-        it "enables filtering by scope" do
-          within ".filters" do
-            expect(page).to have_content(/Scopes/i)
-          end
-        end
-      end
-
-      context "by origin 'official'" do
-        it "lists the filtered results" do
-          within ".filters" do
-            check scope.name
-          end
-
-          expect(page).to have_css(".card--result", count: 1)
-        end
-      end
-
-      context "by origin 'citizenship'" do
-        it "lists the filtered results" do
-          within ".filters" do
-            check scope.name
-          end
-
-          expect(page).to have_css(".card--result", count: results.size)
         end
       end
     end
