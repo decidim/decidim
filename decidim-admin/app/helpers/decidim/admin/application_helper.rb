0f57ff81054818c5e33610d7de6618832b7b8b01
David Rodríguez <deivid.rodriguez@riseup.net>
2017-05-29 14:59:32 -0300

    Don't generate empty CSS classes

--- decidim-admin/app/helpers/decidim/admin/application_helper.rb
+++ decidim-admin/app/helpers/decidim/admin/application_helper.rb
@@ -17,7 +17,7 @@ module Decidim
       end
 
       def admin_menu_item_to(name, url, options = {})
-        content_tag :li, class: active_link_to_class(url, active: options[:active], class_active: "is-active") do
+        content_tag :li, class: active_link_to_class(url, active: options[:active], class_active: "is-active").presence do
           active_link_to url do
             icon(options[:icon_name]) + I18n.t("menu.#{name}", scope: "decidim.admin")
           end
