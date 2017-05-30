6e0689b44de43176f90587c6de08ed1c8ce3884e
David Morcillo <david.morcillo@gmail.com>
2017-05-30 10:58:11 +0200

    Add answer option default value

--- decidim-surveys/db/migrate/20170525132233_add_answer_options_to_surveys_questions.rb
+++ decidim-surveys/db/migrate/20170525132233_add_answer_options_to_surveys_questions.rb
@@ -1,5 +1,5 @@
 class AddAnswerOptionsToSurveysQuestions < ActiveRecord::Migration[5.0]
   def change
-    add_column :decidim_surveys_survey_questions, :answer_options, :jsonb
+    add_column :decidim_surveys_survey_questions, :answer_options, :jsonb, default: []
   end
 end
