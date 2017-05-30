e9d35695c06d7c38198da8fd9da7af2f0016ead9
David Morcillo <david.morcillo@gmail.com>
2017-05-30 10:58:11 +0200

    Add answer option default value

--- decidim-surveys/app/models/decidim/surveys/survey_question.rb
+++ decidim-surveys/app/models/decidim/surveys/survey_question.rb
@@ -6,10 +6,6 @@ module Decidim
       TYPES = %w(short_answer long_answer single_option multiple_option).freeze
 
       belongs_to :survey, class_name: Survey, foreign_key: "decidim_survey_id"
-
-      def answer_options
-        self[:answer_options] || []
-      end
     end
   end
 end
