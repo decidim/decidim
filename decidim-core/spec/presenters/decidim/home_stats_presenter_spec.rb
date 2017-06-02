660e557385c295023bfe1c05df194690c3b354aa
David Rodríguez <deivid.rodriguez@riseup.net>
2017-06-02 14:28:19 -0300

    Enforce Style/AlignArray in specs too

--- decidim-core/spec/presenters/decidim/home_stats_presenter_spec.rb
+++ decidim-core/spec/presenters/decidim/home_stats_presenter_spec.rb
@@ -34,24 +34,22 @@ module Decidim
     describe "#highlighted" do
       it "renders a collection of high priority stats including users and proceses" do
         expect(subject.highlighted).to eq(
-          [
-            "<div class=\"home-pam__highlight\">",
-              "<div class=\"home-pam__data\">",
-                "<h4 class=\"home-pam__title\">Users</h4>",
-                "<span class=\"home-pam__number users_count\"> 1</span>",
-              "</div>",
-              "<div class=\"home-pam__data\">",
-                "<h4 class=\"home-pam__title\">Processes</h4>",
-                "<span class=\"home-pam__number processes_count\"> 1</span>",
-              "</div>",
-            "</div>",
-            "<div class=\"home-pam__highlight\">",
-              "<div class=\"home-pam__data\">",
-                "<h4 class=\"home-pam__title\">Foo</h4>",
-                "<span class=\"home-pam__number foo\"> 10</span>",
-              "</div>",
-            "</div>"
-          ].join("")
+          "<div class=\"home-pam__highlight\">" \
+            "<div class=\"home-pam__data\">" \
+              "<h4 class=\"home-pam__title\">Users</h4>" \
+              "<span class=\"home-pam__number users_count\"> 1</span>" \
+            "</div>" \
+            "<div class=\"home-pam__data\">" \
+              "<h4 class=\"home-pam__title\">Processes</h4>" \
+              "<span class=\"home-pam__number processes_count\"> 1</span>" \
+            "</div>" \
+          "</div>" \
+          "<div class=\"home-pam__highlight\">" \
+            "<div class=\"home-pam__data\">" \
+              "<h4 class=\"home-pam__title\">Foo</h4>" \
+              "<span class=\"home-pam__number foo\"> 10</span>" \
+            "</div>" \
+          "</div>"
         )
       end
     end
@@ -59,18 +57,16 @@ module Decidim
     describe "#not_highlighted" do
       it "renders a collection of medium priority stats" do
         expect(subject.not_highlighted).to eq(
-          [
-            "<div class=\"home-pam__lowlight\">",
-              "<div class=\"home-pam__data\">",
-                "<h4 class=\"home-pam__title\">Comments</h4>",
-                "<span class=\"home-pam__number comments_count\"> 0</span>",
-              "</div>",
-              "<div class=\"home-pam__data\">",
-                "<h4 class=\"home-pam__title\">Bar</h4>",
-                "<span class=\"home-pam__number bar\"> 20</span>",
-              "</div>",
-            "</div>"
-          ].join("")
+          "<div class=\"home-pam__lowlight\">" \
+            "<div class=\"home-pam__data\">" \
+              "<h4 class=\"home-pam__title\">Comments</h4>" \
+              "<span class=\"home-pam__number comments_count\"> 0</span>" \
+            "</div>" \
+            "<div class=\"home-pam__data\">" \
+              "<h4 class=\"home-pam__title\">Bar</h4>" \
+              "<span class=\"home-pam__number bar\"> 20</span>" \
+            "</div>" \
+          "</div>"
         )
       end
     end
