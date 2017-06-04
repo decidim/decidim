7b4bc5710a1210e76371ac396054702da77c60ef
David Rodríguez <deivid.rodriguez@riseup.net>
2017-06-04 18:30:43 -0300

    Remove unnecessary requires

--- decidim-budgets/spec/features/process_admin_manages_project_attachments_spec.rb
+++ decidim-budgets/spec/features/process_admin_manages_project_attachments_spec.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-require_relative "../shared/admin_shared_context"
-require_relative "../shared/manage_attachments_examples"
 
 describe "Process admin manages project attachments", type: :feature do
   let(:user) { process_admin }
