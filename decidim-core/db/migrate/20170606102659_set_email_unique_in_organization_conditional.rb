ce72a3392531c162aaff48872c4617565f1dd52c
David Rodríguez <deivid.rodriguez@riseup.net>
2017-06-06 12:30:12 -0300

    Normalize index creation
    This is the only `using: :btree` usage. This is automatic.

--- decidim-core/db/migrate/20170606102659_set_email_unique_in_organization_conditional.rb
+++ decidim-core/db/migrate/20170606102659_set_email_unique_in_organization_conditional.rb
@@ -3,6 +3,6 @@
 class SetEmailUniqueInOrganizationConditional < ActiveRecord::Migration[5.0]
   def change
     remove_index :decidim_users, %w(email decidim_organization_id)
-    add_index :decidim_users, %w(email decidim_organization_id), where: "(deleted_at IS NULL)", name: "index_decidim_users_on_email_and_decidim_organization_id", unique: true, using: :btree
+    add_index :decidim_users, %w(email decidim_organization_id), where: "(deleted_at IS NULL)", name: "index_decidim_users_on_email_and_decidim_organization_id", unique: true
   end
 end
