819275f5393ce5ad0ffa0c22ff69d818b35af2f0
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-06-09 09:12:11 +0200

    Fix failing spec

--- decidim-budgets/app/models/decidim/budgets/order.rb
+++ decidim-budgets/app/models/decidim/budgets/order.rb
@@ -11,8 +11,8 @@ module Decidim
 
       belongs_to :user, class_name: "Decidim::User", foreign_key: "decidim_user_id"
 
-      has_many :projects, through: :line_items, class_name: "Decidim::Budgets::Project", foreign_key: "decidim_project_id"
       has_many :line_items, class_name: "Decidim::Budgets::LineItem", foreign_key: "decidim_order_id", dependent: :destroy
+      has_many :projects, through: :line_items, class_name: "Decidim::Budgets::Project", foreign_key: "decidim_project_id"
 
       validates :user, uniqueness: { scope: :feature }
       validate :user_belongs_to_organization
