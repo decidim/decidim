006f1c85ea868e55f6aa0a415d75d73cc73d70a0
David Rodríguez <deivid.rodriguez@riseup.net>
2017-06-11 20:46:20 -0300

    Improve resource locator presenter tests
    These tests were not very good in my opinion because they were using the
    tested functionality in the test itself.

--- decidim-core/spec/presenters/decidim/resource_locator_presenter_spec.rb
+++ decidim-core/spec/presenters/decidim/resource_locator_presenter_spec.rb
@@ -4,35 +4,30 @@ require "spec_helper"
 
 module Decidim
   describe ResourceLocatorPresenter, type: :helper do
-    let(:feature) { create(:feature) }
-    let(:resource) { create(:dummy_resource, feature: feature) }
+    let(:organization) { create(:organization, host: "1.lvh.me") }
+
+    let(:participatory_process) do
+      create(:participatory_process, id: 1, organization: organization)
+    end
+
+    let(:feature) do
+      create(:feature, id: 1, participatory_process: participatory_process)
+    end
+
+    let(:resource) do
+      create(:dummy_resource, id: 1, feature: feature)
+    end
 
     describe "#url" do
       subject { described_class.new(resource).url }
 
-      let(:expected_resource_url) do
-        helper.decidim_dummy.dummy_resource_url(
-          participatory_process_id: feature.participatory_process.id,
-          feature_id: feature.id,
-          id: resource.id,
-          host: feature.organization.host
-        )
-      end
-
-      it { is_expected.to eq(expected_resource_url) }
+      it { is_expected.to eq("http://1.lvh.me/processes/1/f/1/dummy_resources/1") }
     end
 
     describe "#path" do
       subject { described_class.new(resource).path }
 
-      let(:expected_resource_path) do
-        helper.decidim_dummy.dummy_resource_path(
-          participatory_process_id: feature.participatory_process.id,
-          feature_id: feature.id, id: resource.id
-        )
-      end
-
-      it { is_expected.to eq(expected_resource_path) }
+      it { is_expected.to eq("/processes/1/f/1/dummy_resources/1") }
     end
   end
 end
