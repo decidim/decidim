0bf52e938623f8762e7867db0d312c87bbe7144f
David Morcillo <david.morcillo@gmail.com>
2017-06-12 16:34:30 +0200

    Migrations almost working

--- decidim-budgets/db/migrate/20170612101846_migrate_projects_category.rb
+++ decidim-budgets/db/migrate/20170612101846_migrate_projects_category.rb
@@ -3,12 +3,13 @@
 class MigrateProjectsCategory < ActiveRecord::Migration[5.1]
   def change
     records = ActiveRecord::Base.connection.execute("SELECT id, decidim_category_id FROM decidim_budgets_projects")
-    values = records.map do |record|
-      "(#{record[:id]}, #{record[:decidim_category_id]}, 'Decidim::Budgets::Project')"
+    now = Time.current
+    values = records.to_a.reject!{ |record| record["decidim_category_id"].blank? }.map do |record|
+      "(#{record["id"]}, #{record["decidim_category_id"]}, 'Decidim::Budgets::Project', '#{now}', '#{now}')"
     end
     if values.any?
       ActiveRecord::Base.connection.execute(
-        "INSERT INTO decidim_categorizations(decidim_category_id, categorizable_id, categorizable_type) VALUES #{values.join(', ')}"
+        "INSERT INTO decidim_categorizations(decidim_category_id, categorizable_id, categorizable_type, created_at, updated_at) VALUES #{values.join(', ')}"
       )
     end
     remove_column :decidim_budgets_projects, :decidim_category_id
--- decidim-meetings/db/migrate/20170612101925_migrate_meetings_category.rb
+++ decidim-meetings/db/migrate/20170612101925_migrate_meetings_category.rb
@@ -3,12 +3,13 @@
 class MigrateMeetingsCategory < ActiveRecord::Migration[5.1]
   def change
     records = ActiveRecord::Base.connection.execute("SELECT id, decidim_category_id FROM decidim_meetings_meetings")
-    values = records.map do |record|
-      "(#{record[:id]}, #{record[:decidim_category_id]}, 'Decidim::Meetings::Meeting')"
+    now = Time.current
+    values = records.to_a.reject!{ |record| record["decidim_category_id"].blank? }.map do |record|
+      "(#{record["id"]}, #{record["decidim_category_id"]}, 'Decidim::Meetings::Meeting', '#{now}', '#{now}')"
     end
     if values.any?
       ActiveRecord::Base.connection.execute(
-        "INSERT INTO decidim_categorizations(decidim_category_id, categorizable_id, categorizable_type) VALUES #{values.join(', ')}"
+        "INSERT INTO decidim_categorizations(decidim_category_id, categorizable_id, categorizable_type, created_at, updated_at) VALUES #{values.join(', ')}"
       )
     end
     remove_column :decidim_meetings_meetings, :decidim_category_id
