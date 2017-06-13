6b60fc3c48e028f6fb023a91939e3e5e7a67524d
David Morcillo <david.morcillo@gmail.com>
2017-06-13 15:47:14 +0200

    Better migrations

--- decidim-budgets/db/migrate/20170612101846_migrate_projects_category.rb
+++ decidim-budgets/db/migrate/20170612101846_migrate_projects_category.rb
@@ -2,20 +2,12 @@
 
 class MigrateProjectsCategory < ActiveRecord::Migration[5.1]
   def change
-    # Ensure database integrity updating projects with an invalid category
-    ActiveRecord::Base.connection.execute('
-      UPDATE decidim_budgets_projects SET decidim_category_id = NULL where id IN (
-        SELECT t.id FROM decidim_budgets_projects as t WHERE t.decidim_category_id NOT IN (
-          SELECT c.id FROM decidim_categories as c
-        )
-      )
-    ')
-    # Create categorizations
-    ActiveRecord::Base.connection.execute('
+    # Create categorizations ensuring database integrity
+    execute('
       INSERT INTO decidim_categorizations(decidim_category_id, categorizable_id, categorizable_type, created_at, updated_at)
-        SELECT decidim_category_id, id, \'Decidim::Budgets::Project\', NOW(), NOW()
+        SELECT decidim_category_id, decidim_budgets_projects.id, \'Decidim::Budgets::Project\', NOW(), NOW()
         FROM decidim_budgets_projects
-        WHERE decidim_category_id IS NOT NULL
+        INNER JOIN decidim_categories ON decidim_categories.id = decidim_budgets_projects.decidim_category_id
     ')
     # Remove unused column
     remove_column :decidim_budgets_projects, :decidim_category_id
--- decidim-meetings/db/migrate/20170612101925_migrate_meetings_category.rb
+++ decidim-meetings/db/migrate/20170612101925_migrate_meetings_category.rb
@@ -2,20 +2,12 @@
 
 class MigrateMeetingsCategory < ActiveRecord::Migration[5.1]
   def change
-    # Ensure database integrity updating meetings with an invalid category
-    ActiveRecord::Base.connection.execute('
-      UPDATE decidim_meetings_meetings SET decidim_category_id = NULL where id IN (
-        SELECT t.id FROM decidim_meetings_meetings as t WHERE t.decidim_category_id NOT IN (
-          SELECT c.id FROM decidim_categories as c
-        )
-      )
-    ')
-    # Create categorizations
-    ActiveRecord::Base.connection.execute('
+    # Create categorizations ensuring database integrity
+    execute('
       INSERT INTO decidim_categorizations(decidim_category_id, categorizable_id, categorizable_type, created_at, updated_at)
-        SELECT decidim_category_id, id, \'Decidim::Meetings::Meeting\', NOW(), NOW()
+        SELECT decidim_category_id, decidim_meetings_meetings.id, \'Decidim::Meetings::Meeting\', NOW(), NOW()
         FROM decidim_meetings_meetings
-        WHERE decidim_category_id IS NOT NULL
+        INNER JOIN decidim_categories ON decidim_categories.id = decidim_meetings_meetings.decidim_category_id
     ')
     # Remove unused column
     remove_column :decidim_meetings_meetings, :decidim_category_id
