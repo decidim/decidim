ba4e496d8e7d38cbd069f07a85fa04f7667ee513
lastpotion <89.aitor.porro@gmail.com>
2017-06-13 11:47:51 +0200

    User groups belong to an org

--- decidim-core/app/models/decidim/user_group.rb
+++ decidim-core/app/models/decidim/user_group.rb
@@ -3,6 +3,8 @@
 module Decidim
   # A UserGroup is an organization of citizens
   class UserGroup < ApplicationRecord
+    belongs_to :organization, foreign_key: "decidim_organization_id", class_name: "Decidim::Organization"
+
     has_many :memberships, class_name: "Decidim::UserGroupMembership", foreign_key: :decidim_user_group_id
     has_many :users, through: :memberships, class_name: "Decidim::User", foreign_key: :decidim_user_id
 
