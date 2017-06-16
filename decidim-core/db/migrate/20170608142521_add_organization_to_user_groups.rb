c044ea40e7d74fe3b4f3000c0aab48644e449009
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-06-16 11:46:27 +0200

    Fix migration

--- decidim-core/db/migrate/20170608142521_add_organization_to_user_groups.rb
+++ decidim-core/db/migrate/20170608142521_add_organization_to_user_groups.rb
@@ -6,7 +6,7 @@ class AddOrganizationToUserGroups < ActiveRecord::Migration[5.0]
 
     Decidim::UserGroup.includes(:users).find_each do |user_group|
       user_group.organization = user_group.users.first.organization
-      user_group.save!
+      user_group.save!(validate: false)
     end
 
     change_column :decidim_user_groups, :decidim_organization_id, :integer, null: false
