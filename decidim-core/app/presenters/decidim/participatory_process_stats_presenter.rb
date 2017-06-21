290af959625109a1e59edfa216bd14ab71a0af64
lastpotion <89.aitor.porro@gmail.com>
2017-06-21 16:19:55 +0200

    Improved layout

--- decidim-core/app/presenters/decidim/participatory_process_stats_presenter.rb
+++ decidim-core/app/presenters/decidim/participatory_process_stats_presenter.rb
@@ -8,12 +8,29 @@ module Decidim
     # Public: Render a collection of primary stats.
     def highlighted
       highlighted_stats = Decidim.stats.only([:comments_count]).with_context(published_features).map { |name, data| [:global, name, data] }
-      highlighted_stats = highlighted_stats.concat(feature_stats)
+      highlighted_stats = highlighted_stats.concat(feature_stats(feature_stats_list))
       highlighted_stats = highlighted_stats.reject(&:empty?)
 
       safe_join(
-        highlighted_stats.in_groups_of(2, false).map do |stats|
-          content_tag :div, class: "column small-3" do
+        highlighted_stats.in_groups_of(4, false).map do |stats|
+          content_tag :div, class: "home-pam__highlight" do
+            safe_join(
+              stats.map do |scope, name, data|
+                render_stats_data(scope, name, data)
+              end
+            )
+          end
+        end
+      )
+    end
+
+    def not_highlighted
+      not_highlighted_stats = feature_stats(stats_list)
+      not_highlighted_stats = not_highlighted_stats.reject(&:empty?)
+
+      safe_join(
+        not_highlighted_stats.in_groups_of(4, false).map do |stats|
+          content_tag :div, class: "home-pam__lowlight" do
             safe_join(
               stats.map do |scope, name, data|
                 render_stats_data(scope, name, data)
@@ -26,7 +43,7 @@ module Decidim
 
     private
 
-    def which_stats
+    def feature_stats_list
       {
         :proposals => [:proposals_count],
         :meetings => [:meetings_count],
@@ -36,20 +53,29 @@ module Decidim
       }
     end
 
-    def feature_stats
-      Decidim.feature_manifests.to_a.select { |feature| which_stats.keys.include? feature.name }.map do |feature|
+    def stats_list
+      {
+        :proposals => [:comments_count],
+        :meetings => [:comments_count],
+        :pages => [:comments_count],
+        :results => [:comments_count]
+      }
+    end
+
+    def feature_stats(list)
+      Decidim.feature_manifests.to_a.select { |feature| list.keys.include? feature.name }.map do |feature|
         feature.stats
-          .only(which_stats[feature.name])
+          .only(list[feature.name])
           .with_context(published_features)
           .map { |name, data| [feature.name, name, data] }
       end.flatten(1)
     end
 
     def render_stats_data(scope, name, data)
-      content_tag :div, "", class: "card" do
+      content_tag :div, "", class: "home-pam__data" do
         safe_join([
-                    content_tag(:h1, "#{number_with_delimiter(data)} ", class: "#{scope} #{name} text-center"),
-                    content_tag(:p, I18n.t("#{scope}.#{name}", scope: "decidim.participatory_processes.statistics"), class: "text-center card-divider")
+                    content_tag(:h4, I18n.t("#{scope}.#{name}", scope: "decidim.participatory_processes.statistics"), class: "text-center home-pam__title"),
+                    content_tag(:span, " #{number_with_delimiter(data)} ", class: "#{scope} #{name} home-pam__number")
                   ])
       end
     end
