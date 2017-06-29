aa771e0366c0943266255d3a8b6442ecf50fc61b
David Rodríguez <deivid.rodriguez@riseup.net>
2017-06-29 12:48:20 +0200

    Use a more map-ish fake URL

--- decidim-dev/lib/decidim/dev/test/rspec_support/geocoder.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/geocoder.rb
@@ -4,7 +4,7 @@ RSpec.configure do |config|
   config.before(:suite) do
     # Set geocoder configuration in test mode
     Decidim.geocoder = {
-      static_map_url: "http://www.example.org",
+      static_map_url: "https://www.example.org/my_static_map",
       here_app_id: "1234",
       here_app_code: "5678"
     }
