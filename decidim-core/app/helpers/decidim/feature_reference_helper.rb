142569412857e6cdcc229207afb86cfb13ca4237
David Morcillo <david.morcillo@gmail.com>
2017-06-30 10:51:22 +0200

    Add reference to proposals#index

--- decidim-core/app/helpers/decidim/feature_reference_helper.rb
+++ decidim-core/app/helpers/decidim/feature_reference_helper.rb
@@ -6,12 +6,14 @@ module Decidim
     # Displays the localized reference for the given feature.
     #
     # feature - the Feature that has the reference to display.
+    # options - An optional hash of options
+    #         * class: A string of extra css classes
     #
     # Returns a String.
-    def feature_reference(feature)
+    def feature_reference(feature, options = {})
       return unless feature.reference
       @reference = feature.reference
-      "<div class='reference'>#{localized_reference}</div>".html_safe
+      "<div class='reference #{options[:class]}'>#{localized_reference}</div>".html_safe
     end
 
     private
