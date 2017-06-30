4e27d187ca30aa39fb2004aee2d220d9153dc093
David Rodríguez <deivid.rodriguez@riseup.net>
2017-06-30 12:03:12 +0200

    Fix code style of commented configuration

--- lib/generators/decidim/templates/initializer.rb
+++ lib/generators/decidim/templates/initializer.rb
@@ -7,7 +7,7 @@ Decidim.configure do |config|
   config.authorization_handlers = [ExampleAuthorizationHandler]
 
   # Uncomment this lines to set your preferred locales
-  # config.available_locales = %i{en ca es}
+  # config.available_locales = [:en, :ca, :es]
 
   # Geocoder configuration
   # config.geocoder = {
