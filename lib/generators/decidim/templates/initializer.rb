b4f9f86747e6d82a9f31c744de826b186778c056
David Rodríguez <deivid.rodriguez@riseup.net>
2017-06-30 12:46:05 +0200

    Fix locale configuration
    Due to the order Rails loads things, locale were not actually
    configurable via `Decidim.available_locales` at the moment, because when
    the assignment to Rails native locales was done, customizations had not
    yet been loaded.
    
    This change fixes it by loading locale customizations _before_ setting
    `config.i18n.available_locales`.

--- lib/generators/decidim/templates/initializer.rb
+++ lib/generators/decidim/templates/initializer.rb
@@ -28,3 +28,6 @@ Decidim.configure do |config|
   # The number of reports which an object can receive before hiding it
   # config.max_reports_before_hiding = 3
 end
+
+Rails.application.config.i18n.available_locales = Decidim.available_locales
+Rails.application.config.i18n.default_locale = Decidim.default_locale
