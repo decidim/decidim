0d0dcb4dde4102a020c70b3987245092b6cc1269
David Morcillo <david.morcillo@gmail.com>
2017-07-19 10:25:54 +0200

    Fix renaming problems

--- decidim-core/lib/decidim/core/engine.rb
+++ decidim-core/lib/decidim/core/engine.rb
@@ -79,11 +79,11 @@ module Decidim
 
       initializer "decidim.inject_abilities_to_user" do |_app|
         Decidim.configure do |config|
-          config.abilities << "Decidim::Abilities::EveryoneAbililty"
-          config.abilities << "Decidim::Abilities::AdminUser"
-          config.abilities << "Decidim::Abilities::ParticipatoryProcessAdminUser"
-          config.abilities << "Decidim::Abilities::ParticipatoryProcessCollaboratorUser"
-          config.abilities << "Decidim::Abilities::ParticipatoryProcessModeratorUser"
+          config.abilities << "Decidim::Abilities::EveryoneAbility"
+          config.abilities << "Decidim::Abilities::AdminAbility"
+          config.abilities << "Decidim::Abilities::ParticipatoryProcessAdminAbility"
+          config.abilities << "Decidim::Abilities::ParticipatoryProcessCollaboratorAbility"
+          config.abilities << "Decidim::Abilities::ParticipatoryProcessModeratorAbility"
         end
       end
 
