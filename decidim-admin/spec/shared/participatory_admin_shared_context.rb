778b039a79971456feb470108b4a291850127fac
David Rodríguez <deivid.rodriguez@riseup.net>
2017-07-20 22:04:08 +0200

    Reuse trait

--- decidim-admin/spec/shared/participatory_admin_shared_context.rb
+++ decidim-admin/spec/shared/participatory_admin_shared_context.rb
@@ -3,9 +3,10 @@
 RSpec.shared_context "participatory process admin" do
   let(:organization) { create(:organization) }
   let!(:user) { create(:user, :admin, :confirmed, organization: organization) }
+
   let(:participatory_process) { create(:participatory_process, organization: organization) }
-  let(:process_admin) { create :user, :confirmed, organization: organization }
-  let!(:user_role) { create :participatory_process_user_role, user: process_admin, participatory_process: participatory_process }
+  let!(:process_admin) { create :user, :process_admin, :confirmed, organization: organization, participatory_process: participatory_process }
+
   let(:image1_filename) { "city.jpeg" }
   let(:image1_path) do
     File.expand_path(File.join(__dir__, "..", "..", "..", "decidim-dev", "spec", "support", image1_filename))
