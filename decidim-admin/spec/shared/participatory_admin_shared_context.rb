03959e49546b78c62372ea627f5f6ad25a22606e
David Rodríguez <deivid.rodriguez@riseup.net>
2017-07-21 00:00:32 +0200

    Simplify image test setup

--- decidim-admin/spec/shared/participatory_admin_shared_context.rb
+++ decidim-admin/spec/shared/participatory_admin_shared_context.rb
@@ -8,15 +8,9 @@ RSpec.shared_context "participatory process admin" do
   let!(:process_admin) { create :user, :process_admin, :confirmed, organization: organization, participatory_process: participatory_process }
 
   let(:image1_filename) { "city.jpeg" }
-  let(:image1_path) do
-    File.expand_path(File.join(__dir__, "..", "..", "..", "decidim-dev", "spec", "support", image1_filename))
-  end
+  let(:image1_path) { Decidim::Dev.asset(image1_filename) }
   let(:image2_filename) { "city2.jpeg" }
-  let(:image2_path) do
-    File.expand_path(File.join(__dir__, "..", "..", "..", "decidim-dev", "spec", "support", image2_filename))
-  end
+  let(:image2_path) { Decidim::Dev.asset(image2_filename) }
   let(:image3_filename) { "city3.jpeg" }
-  let(:image3_path) do
-    File.expand_path(File.join(__dir__, "..", "..", "..", "decidim-dev", "spec", "support", image3_filename))
-  end
+  let(:image3_path) { Decidim::Dev.asset(image3_filename) }
 end
