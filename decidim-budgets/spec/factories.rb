1a09a415389d5e59347d9a7012b5ed7890883753
David Rodríguez <deivid.rodriguez@riseup.net>
2017-07-21 19:17:21 +0200

    Move user role factories to core
    Since the class has been moved directly under `Decidim`.

--- decidim-budgets/spec/factories.rb
+++ decidim-budgets/spec/factories.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "decidim/core/test/factories"
-require "decidim/admin/test/factories"
 require "decidim/proposals/test/factories"
 require "decidim/comments/test/factories"
 
