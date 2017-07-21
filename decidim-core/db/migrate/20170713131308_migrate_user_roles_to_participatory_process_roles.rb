4e15a44955ddf3f7087c082892ad8479645adf93
David Morcillo <david.morcillo@gmail.com>
2017-07-21 14:25:17 +0200

    Use sql batch insert

--- decidim-core/db/migrate/20170713131308_migrate_user_roles_to_participatory_process_roles.rb
+++ decidim-core/db/migrate/20170713131308_migrate_user_roles_to_participatory_process_roles.rb
@@ -4,13 +4,20 @@ class MigrateUserRolesToParticipatoryProcessRoles < ActiveRecord::Migration[5.1]
   def up
     participatory_processes = Decidim::ParticipatoryProcess.includes(:organization).all
     Decidim::User.find_each do |user|
-      next if user.roles.include? "admin"
+      next if user.roles.empty? || user.roles.include?("admin")
+
       processes = participatory_processes.select { |process| process.organization == user.organization }
-      processes.each do |process|
-        user.roles.each do |role|
-          Decidim::ParticipatoryProcessUserRole.find_or_create_by(user: user, participatory_process: process, role: role)
+      values = processes.map do |process|
+        user.roles.map do |role|
+          "(#{user.id}, #{process.id}, '#{role}', NOW(), NOW())"
         end
       end
+
+      execute("
+        INSERT INTO decidim_admin_participatory_process_user_roles
+        (decidim_user_id, decidim_participatory_process_id, role, created_at, updated_at)
+        VALUES #{values.join(", ")}
+      ")
     end
     remove_column :decidim_users, :roles
   end
