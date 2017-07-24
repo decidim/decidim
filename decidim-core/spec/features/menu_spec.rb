a9de1c3f600aba5f4e35abf0bbc3e4e544137ca5
David Rodríguez <deivid.rodriguez@riseup.net>
2017-07-24 22:41:27 +0200

    Fix menu active options
    Also moved the tests to proper integration tests since testing the
    helper in isolation was too hard and required to stub out other
    libraries' internals.

---
+++ decidim-core/spec/features/menu_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Menu", type: :feature do
+  let(:organization) { create(:organization) }
+
+  before do
+    switch_to_host(organization.host)
+    visit decidim.root_path
+  end
+
+  matcher :have_selected_option do |expected|
+    match do |page|
+      page.has_selector?(".main-nav__link--active", count: 1) &&
+        page.has_selector?(".main-nav__link--active", text: expected)
+    end
+  end
+
+  it "renders the default main menu" do
+    within ".main-nav" do
+      expect(page).to \
+        have_selector("li", count: 3) &
+        have_link("Home", href: "/") &
+        have_link("Processes", href: "/processes") &
+        have_link("More information", href: "/pages")
+    end
+  end
+
+  it "selects the correct default active option" do
+    within ".main-nav" do
+      expect(page).to have_selected_option("Home")
+    end
+  end
+
+  context "when clicking on a menu entry" do
+    before do
+      click_link "More information"
+    end
+
+    it "switches the active option" do
+      expect(page).to have_selected_option("More information")
+    end
+
+    context "and clicking on a subpage of that entry" do
+      before do
+        page = create(:static_page, organization: organization)
+
+        visit current_path
+
+        click_link page.title["en"]
+      end
+
+      it "switches the active option" do
+        expect(page).to have_selected_option("More information")
+      end
+    end
+  end
+end
