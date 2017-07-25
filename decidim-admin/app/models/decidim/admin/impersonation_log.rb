9b63150e2916c9f9c961f77a385bfe00153f7ffc
David Morcillo <david.morcillo@gmail.com>
2017-07-25 12:35:35 +0200

    [WIP] Impersonate managed user command

---
+++ decidim-admin/app/models/decidim/admin/impersonation_log.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Admin
+    # TODO
+    class ImpersonationLog < ApplicationRecord
+      belongs_to :admin, foreign_key: "decidim_admin_id", class_name: "Decidim::User"
+      belongs_to :user, foreign_key: "decidim_user_id", class_name: "Decidim::User"
+    end
+  end
+end
--- decidim-admin/spec/commands/impersonate_managed_user_spec.rb
+++ decidim-admin/spec/commands/impersonate_managed_user_spec.rb
@@ -4,10 +4,38 @@ require "spec_helper"
 
 describe Decidim::Admin::ImpersonateManagedUser do
   let(:organization) { create :organization }
-  let(:current_user) { create :user, :admin, organization: organization}
+  let(:current_user) { create :user, :admin, organization: organization }
+  let(:document_number) { "12345678X" }
+  let(:form_params) do
+    {
+      authorization: {
+        handler_name: "Decidim::DummyAuthorizationHandler",
+        document_number: document_number
+      }
+    }
+  end
+  let(:form) do
+    Decidim::Admin::ImpersonateManagedUserForm.from_params(
+      form_params
+    ).with_context(
+      current_organization: organization
+    )
+  end
   let(:user) { create :user, :managed, organization: organization }
+  let(:handler_document_number) { document_number }
+  let(:handler) do
+    Decidim::DummyAuthorizationHandler.from_params(
+      document_number: handler_document_number
+    )
+  end
+  let!(:authorization) do
+    create(:authorization,
+           user: user,
+           name: handler.handler_name,
+           attributes: { unique_id: handler.unique_id })
+  end
 
-  subject { described_class.new(current_user, user) }
+  subject { described_class.new(form, user, current_user) }
 
   context "when everything is ok" do
     it "broadcasts ok" do
@@ -15,14 +43,22 @@ describe Decidim::Admin::ImpersonateManagedUser do
     end
 
     it "creates a impersonation log" do
-      expect {
+      expect do
         subject.call
-      }.to change { ImpersonationLog.count }.by(1)
+      end.to change { Decidim::Admin::ImpersonationLog.count }.by(1)
     end
   end
 
   context "when the user is not managed" do
     let(:user) { create :user }
+
+    it "is not valid" do
+      expect { subject.call }.to broadcast(:invalid)
+    end
+  end
+
+  context "when the authorization is not valid" do
+    let(:handler_document_number) { "98765432X" }
 
     it "is not valid" do
       expect { subject.call }.to broadcast(:invalid)
