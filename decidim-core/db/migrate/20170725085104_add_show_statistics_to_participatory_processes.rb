6c0206788651c5daa16241c44d4e180381c90628
David Morcillo <david.morcillo@gmail.com>
2017-07-25 12:06:03 +0200

    Fix another rubocop issue

--- decidim-core/db/migrate/20170725085104_add_show_statistics_to_participatory_processes.rb
+++ decidim-core/db/migrate/20170725085104_add_show_statistics_to_participatory_processes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddShowStatisticsToParticipatoryProcesses < ActiveRecord::Migration[5.1]
   def change
     add_column :decidim_participatory_processes, :show_statistics, :boolean, default: true
