89e72068a3813f749e0b41aaf65c7a3a0089b1dd
David Rodríguez <deivid.rodriguez@riseup.net>
2017-08-01 15:17:41 +0200

    Extract a publicable concern (#1672)
    * Extract a Publicable concern
    
    * Extract more logic to the Publicable concern

---
+++ decidim-core/lib/decidim/publicable.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require "active_support/concern"
+
+module Decidim
+  # This concern contains the logic related to publication and promotion.
+  module Publicable
+    extend ActiveSupport::Concern
+
+    class_methods do
+      # Public: Scope to return only published records.
+      #
+      # Returns an ActiveRecord::Relation.
+      def published
+        where.not(published_at: nil)
+      end
+
+      # Public: Scope to return only unpublished records.
+      #
+      # Returns an ActiveRecord::Relation.
+      def unpublished
+        where(published_at: nil)
+      end
+    end
+
+    # Public: Checks whether the record has been published or not.
+    #
+    # Returns true if published, false otherwise.
+    def published?
+      published_at.present?
+    end
+
+    #
+    # Public: Publishes this feature
+    #
+    # Returns true if the record was properly saved, false otherwise.
+    def publish!
+      update_attribute(:published_at, Time.current)
+    end
+
+    #
+    # Public: Unpublishes this feature
+    #
+    # Returns true if the record was properly saved, false otherwise.
+    def unpublish!
+      update_attribute(:published_at, nil)
+    end
+  end
+end
