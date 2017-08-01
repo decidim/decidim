cd7e1ed45de80b9817b67af40ebb2d7410d4297e
David Rodríguez <deivid.rodriguez@riseup.net>
2017-08-01 09:51:02 +0200

    Decouple global ability context (#1665)
    The global ability context shouldn't always try to look for the current
    participatory process.
    
    As a result, we can make the `current_participatory_process` method in
    the participatory processes admin controller private. Public methods in
    controllers should match the public controller route actions, so having
    to have this method public was weird.

--- decidim-core/app/controllers/concerns/decidim/needs_authorization.rb
+++ decidim-core/app/controllers/concerns/decidim/needs_authorization.rb
@@ -30,8 +30,7 @@ module Decidim
           current_settings: try(:current_settings),
           feature_settings: try(:feature_settings),
           current_organization: try(:current_organization),
-          current_feature: try(:current_feature),
-          current_participatory_process: try(:current_participatory_process)
+          current_feature: try(:current_feature)
         }
       end
 
