b46d252a78956be958646ac75e85f6c269e3d570
David Rodríguez <deivid.rodriguez@riseup.net>
2017-08-02 14:46:49 +0200

    Make seeds quasi-idempotent (#1675)
    Some objects like votes or comments are created randomly, so re-seeding
    will create other objects. But the main objects (users, organizations)
    will be the same and reseeding without dropping the DB first will not
    crash.

--- decidim-system/db/seeds.rb
+++ decidim-system/db/seeds.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 if !Rails.env.production? || ENV["SEED"]
-  Decidim::System::Admin.create!(
-    email: "system@example.org",
+  Decidim::System::Admin.find_or_initialize_by(email: "system@example.org").update!(
     password: "decidim123456",
     password_confirmation: "decidim123456"
   )
