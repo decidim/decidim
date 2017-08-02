cba0660742abcb368b13c3bb761fd36bad5d5d16
David Morcillo <david.morcillo@gmail.com>
2017-08-02 14:57:27 +0200

    Fix constant autoload problem

--- decidim-core/lib/decidim/abilities.rb
+++ decidim-core/lib/decidim/abilities.rb
@@ -3,5 +3,6 @@
 module Decidim
   module Abilities
     autoload :ParticipatoryProcessRoleAbility, "decidim/abilities/participatory_process_role_ability"
+    autoload :UserManagerAbility, "decidim/abilities/user_manager_ability"
   end
 end
