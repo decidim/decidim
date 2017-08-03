ca6bf869532325c8fcf08b78c61f71ce4479be4d
David Rodríguez <deivid.rodriguez@riseup.net>
2017-08-03 18:30:23 +0200

    Track coverage for spec files themselves too

--- decidim-dev/lib/decidim/dev/test/base_spec_helper.rb
+++ decidim-dev/lib/decidim/dev/test/base_spec_helper.rb
@@ -13,8 +13,7 @@ if ENV["SIMPLECOV"]
 
   SimpleCov.start do
     filters.clear
-    add_filter "/test/"
-    add_filter "/spec/"
+    add_filter "/spec/decidim_dummy_app/"
     add_filter "bundle.js"
     add_filter "/vendor/"
 
