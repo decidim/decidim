154635e8702f80e7c271fdb971d688516e06d1c5
David Morcillo <david.morcillo@gmail.com>
2017-08-04 10:32:16 +0200

    Remove unnecessary file

--- decidim-dev/lib/decidim/dev/test/rspec_support/i18n.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.configure do |config|
-  config.before(:suite) do
-    I18n.config.enforce_available_locales = false
-    Decidim.available_locales = %w(en ca es)
-    I18n.available_locales = Decidim.available_locales
-  end
-end
