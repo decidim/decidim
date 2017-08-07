64a5ad1a6aa1bc8d58eb53d9cc1319883862789f
David Morcillo <david.morcillo@gmail.com>
2017-08-07 10:56:36 +0200

    Fix rubocop issues

--- decidim-core/db/migrate/20170804125402_attachment_description_nullable.rb
+++ decidim-core/db/migrate/20170804125402_attachment_description_nullable.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AttachmentDescriptionNullable < ActiveRecord::Migration[5.1]
   def change
     change_column :decidim_attachments, :description, :jsonb, null: true
