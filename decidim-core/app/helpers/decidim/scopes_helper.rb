73302c67fa8f47de49dafab632aea3875d17fe21
David Rodríguez <deivid.rodriguez@riseup.net>
2017-08-08 14:20:26 +0200

    `subscopes_for` tweaks
    For further independency from participatory processes.

--- decidim-core/app/helpers/decidim/scopes_helper.rb
+++ decidim-core/app/helpers/decidim/scopes_helper.rb
@@ -5,17 +5,17 @@ module Decidim
   module ScopesHelper
     Option = Struct.new(:id, :name)
 
-    # Public: Returns a collection of the given participatory process scopes,
-    # prepending a global scope. The global scope is at the beginning because it's
-    # easier to be found there. Use this helper in places like `select` elements in
-    # forms.
+    # Public: Returns a collection of the given "scopable object" scopes,
+    # prepending a global scope. The global scope is at the beginning because
+    # it's easier to be found there. Use this helper in places like `select`
+    # elements in forms.
     #
-    # participatory_process - a Decidim::ParticipatoryProcess.
+    # scopable - an object including the Decidim::Scopable concern.
     #
     # Returns an Array.
-    def subscopes_for(participatory_process)
+    def subscopes_for(scopable)
       [Option.new("", I18n.t("decidim.scopes.global"))] +
-        participatory_process.subscopes.map do |scope|
+        scopable.subscopes.map do |scope|
           Option.new(scope.id, translated_attribute(scope.name))
         end
     end
