1f2dd8da812d3b64bd9c95ea1154794c31e840c0
David Rodríguez <deivid.rodriguez@riseup.net>
2017-08-10 03:33:32 +0200

    Enable factory linting

---
+++ decidim-core/spec/factory_lint_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Lint factories", processing_uploads_for: Decidim::AttachmentUploader do
+  it "has 100% valid factories" do
+    expect { FactoryGirl.lint }.not_to raise_error
+  end
+end
