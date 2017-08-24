798f917a85c28c7acf5a789cba7b03579712d01b
David Rodríguez <deivid.rodriguez@riseup.net>
2017-08-24 15:42:47 +0200

    Review application generation (#1774)
    * More readable indentation
    
    * Use standard `gemspec` in Gemfile
    
    * Some whitespace for the generated Gemfile
    
    * Normalize quotes in generated Gemfile
    
    * Refactor decidim gem spec in generated Gemfile
    
    * Prefer `Decidim.version`
    
    The alternative has caused problems in the past (with the faker gem), so
    let's stick to this.
    
    * Fix requiring assemblies
    
    LoadError does not inherit from StandardError, so we need to manually
    rescue it. In addition, this doesn't swallow other unrelated errors.
    
    * Test application generation
    
    * Make it easier to use decidim-assemblies
    
    By including a tip in the generated Gemfile.

---
+++ spec/generator_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+describe "Application generation" do
+  let(:status) { system(command, out: File::NULL) }
+
+  after { FileUtils.rm_rf("tmp/test_app") }
+
+  context "with edge argument" do
+    let(:command) { "bin/decidim --edge tmp/test_app" }
+
+    it "successfully generates application" do
+      expect(status).to eq(true)
+    end
+  end
+
+  context "with branch argument" do
+    let(:command) { "bin/decidim --branch master tmp/test_app" }
+
+    it "successfully generates application" do
+      expect(status).to eq(true)
+    end
+  end
+
+  context "with path argument" do
+    let(:command) { "bin/decidim --path #{File.expand_path("..", __dir__)} tmp/test_app" }
+
+    it "successfully generates application" do
+      expect(status).to eq(true)
+    end
+  end
+end
