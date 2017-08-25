224c793997c1eeb0319e47946b25fd75d1a1b625
Marc Riera Casals <mrc2407@gmail.com>
2017-08-25 13:47:33 +0200

    Customize email texts per event

--- decidim-meetings/app/events/decidim/meetings/close_meeting_event.rb
+++ decidim-meetings/app/events/decidim/meetings/close_meeting_event.rb
@@ -5,6 +5,18 @@ module Decidim
     class CloseMeetingEvent < Decidim::Events::BaseEvent
       include Decidim::Events::EmailEvent
       include Decidim::Events::NotificationEvent
+
+      def email_subject
+        I18n.t("decidim.meetings.events.close_meeting_event.email_subject", resource_title: resource_title)
+      end
+
+      def email_intro
+        I18n.t("decidim.meetings.events.close_meeting_event.email_intro", resource_title: resource_title)
+      end
+
+      def email_outro
+        I18n.t("decidim.meetings.events.close_meeting_event.email_outro", resource_title: resource_title)
+      end
     end
   end
 end
