7cff5eef2aa86ab0bb1273b2c6d7c9ffd678bc50
Marc Riera Casals <mrc2407@gmail.com>
2017-08-28 14:31:26 +0200

    Find participatory space manifest for processes and assemblies

--- decidim-core/app/helpers/decidim/icon_helper.rb
+++ decidim-core/app/helpers/decidim/icon_helper.rb
@@ -11,19 +11,19 @@ module Decidim
     #
     # Returns an HTML tag with the icon.
     def feature_icon(feature, options = {})
-      feature_manifest_icon(feature.manifest, options)
+      manifest_icon(feature.manifest, options)
     end
 
-    # Public: Returns an icon given an instance of a Feature Manifest. It defaults to
+    # Public: Returns an icon given an instance of a Manifest. It defaults to
     # a question mark when no icon is found.
     #
-    # feature_manifest - The feature manifest to generate the icon for.
+    # manifest - The manifest to generate the icon for.
     # options - a Hash with options
     #
     # Returns an HTML tag with the icon.
-    def feature_manifest_icon(feature_manifest, options = {})
-      if feature_manifest.icon
-        external_icon feature_manifest.icon, options
+    def manifest_icon(manifest, options = {})
+      if manifest.icon
+        external_icon manifest.icon, options
       else
         icon "question-mark", options
       end
@@ -31,7 +31,7 @@ module Decidim
 
     # Public: Finds the correct icon for the given resource. If the resource has a
     # Feature then it uses it to find the icon, otherwise checks for the resource
-    # type to find the icon.
+    # manifest to find the icon.
     #
     # resource - The resource to generate the icon for.
     # options - a Hash with options
@@ -41,7 +41,7 @@ module Decidim
       if resource.respond_to?(:feature)
         feature_icon(resource.feature, options)
       else
-        external_icon "decidim/participatory_processes/process.svg", options
+        manifest_icon(resource.manifest, options)
       end
     end
   end
