e934a583c29cbad9fcabebf341342aeea551020c
Marc Riera Casals <mrc2407@gmail.com>
2017-08-28 12:35:40 +0200

    Rename notification fields

--- decidim-core/app/models/decidim/notification.rb
+++ decidim-core/app/models/decidim/notification.rb
@@ -2,13 +2,13 @@
 
 module Decidim
   class Notification < ApplicationRecord
-    belongs_to :followable, foreign_key: "decidim_followable_id", foreign_type: "decidim_followable_type", polymorphic: true
+    belongs_to :resource, foreign_key: "decidim_resource_id", foreign_type: "decidim_resource_type", polymorphic: true
     belongs_to :user, foreign_key: "decidim_user_id", class_name: "Decidim::User"
 
-    validates :user, :followable, presence: true
+    validates :user, :resource, presence: true
 
     def event_class_instance
-      @event_class_instance ||= event_class.constantize.new(resource: followable, event_name: notification_type, user: user)
+      @event_class_instance ||= event_class.constantize.new(resource: resource, event_name: event_name, user: user)
     end
   end
 end
