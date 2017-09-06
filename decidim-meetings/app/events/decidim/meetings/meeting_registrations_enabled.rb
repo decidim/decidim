3523896ab055eab768928afc79d660cd59386b4e
David Morcillo <david.morcillo@gmail.com>
2017-09-06 18:11:20 +0200

    Notify followers when registrations are enabled

---
+++ decidim-meetings/app/events/decidim/meetings/meeting_registrations_enabled.rb
@@ -0,0 +1,39 @@
+# frozen-string_literal: true
+
+module Decidim
+  module Meetings
+    class MeetingRegistrationsEnabled < Decidim::Events::BaseEvent
+      include Decidim::Events::EmailEvent
+      include Decidim::Events::NotificationEvent
+
+      def email_subject
+        I18n.t(
+          "decidim.meetings.events.meeting_registrations_enabled.email_subject",
+          resource_title: resource_title
+        )
+      end
+
+      def email_intro
+        I18n.t(
+          "decidim.meetings.events.meeting_registrations_enabled.email_intro",
+          resource_title: resource_title
+        )
+      end
+
+      def email_outro
+        I18n.t(
+          "decidim.meetings.events.meeting_registrations_enabled.email_outro",
+          resource_title: resource_title
+        )
+      end
+
+      def notification_title
+        I18n.t(
+          "decidim.meetings.events.meeting_registrations_enabled.notification_title",
+          resource_title: resource_title,
+          resource_path: resource_path
+        ).html_safe
+      end
+    end
+  end
+end
--- decidim-meetings/spec/commands/update_registrations_spec.rb
+++ decidim-meetings/spec/commands/update_registrations_spec.rb
@@ -45,4 +45,35 @@ describe Decidim::Meetings::Admin::UpdateRegistrations do
       expect(translated(meeting.registration_terms)).to eq "A legal text"
     end
   end
+
+  context "events" do
+    let(:user) { create :user, :confirmed, organization: meeting.organization }
+    let!(:follow) { create :follow, followable: meeting, user: user }
+
+    context "when registrations are enabled" do
+      it "notifies the change" do
+        expect(Decidim::EventsManager)
+          .to receive(:publish)
+          .with(
+            event: "decidim.events.meetings.registrations_enabled",
+            event_class: Decidim::Meetings::MeetingRegistrationsEnabled,
+            resource: meeting,
+            recipient_ids: [user.id]
+          )
+
+        subject.call
+      end
+    end
+
+    context "when registrations are already enabled and something else changes" do
+      let(:meeting) { create(:meeting, :with_registrations_enabled) }
+
+      it "doesn't notify the change" do
+        expect(Decidim::EventsManager)
+          .not_to receive(:publish)
+
+        subject.call
+      end
+    end
+  end
 end
