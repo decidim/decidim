bfb0b6e90d23ed77b1b666feef40859308621094
David Morcillo <david.morcillo@gmail.com>
2017-09-07 16:27:42 +0200

    Clean up

--- decidim-meetings/app/mailers/decidim/meetings/registration_mailer.rb
+++ decidim-meetings/app/mailers/decidim/meetings/registration_mailer.rb
@@ -6,6 +6,7 @@ module Decidim
     # they join a meeting.
     class RegistrationMailer < Decidim::ApplicationMailer
       include Decidim::TranslationsHelper
+      include ActionView::Helpers::SanitizeHelper
 
       helper Decidim::ResourceHelper
       helper Decidim::TranslationsHelper
@@ -15,21 +16,30 @@ module Decidim
           @user = user
           @meeting = meeting
           @organization = @meeting.organization
+          @locator = Decidim::ResourceLocatorPresenter.new(@meeting)
 
-          cal = Icalendar::Calendar.new
-          cal.event do |e|
-            e.dtstart = Icalendar::Values::DateTime.new(@meeting.start_time)
-            e.dtend = Icalendar::Values::DateTime.new(@meeting.end_time)
-            e.summary = translated_attribute @meeting.title
-            e.description = translated_attribute @meeting.description
-            e.geo = [@meeting.latitude, @meeting.longitude]
-          end
-
-          attachments["meeting-calendar-info.ics"] = cal.to_ical
+          add_calendar_attachment
 
           subject = I18n.t("confirmation.subject", scope: "decidim.meetings.mailer.registration_mailer")
           mail(to: user.email, subject: subject)
         end
+      end
+
+      private
+
+      def add_calendar_attachment
+        calendar = Icalendar::Calendar.new
+        calendar.event do |event|
+          event.dtstart = Icalendar::Values::DateTime.new(@meeting.start_time)
+          event.dtend = Icalendar::Values::DateTime.new(@meeting.end_time)
+          event.summary = translated_attribute @meeting.title
+          event.description = strip_tags(translated_attribute(@meeting.description))
+          event.location = @meeting.address
+          event.geo = [@meeting.latitude, @meeting.longitude]
+          event.url = @locator.url
+        end
+
+        attachments["meeting-calendar-info.ics"] = calendar.to_ical
       end
     end
   end
