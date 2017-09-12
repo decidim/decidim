b5a3f9d9e3da11e9bf82bb4b4188c65037ae37c0
David Morcillo <david.morcillo@gmail.com>
2017-09-12 10:28:30 +0200

    Add upcoming meeting notification job

---
+++ decidim-meetings/app/events/decidim/meetings/upcoming_meeting_event.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Meetings
+    class UpcomingMeetingEvent < Decidim::Events::BaseEvent
+      include Decidim::Events::EmailEvent
+      include Decidim::Events::NotificationEvent
+
+      def email_subject
+        I18n.t("decidim.meetings.events.upcoming_meeting_event.email_subject", resource_title: resource_title)
+      end
+
+      def email_intro
+        I18n.t("decidim.meetings.events.upcoming_meeting_event.email_intro", resource_title: resource_title)
+      end
+
+      def email_outro
+        I18n.t("decidim.meetings.events.upcoming_meeting_event.email_outro", resource_title: resource_title)
+      end
+
+      def notification_title
+        I18n.t(
+          "decidim.meetings.events.upcoming_meeting_event.notification_title",
+          resource_title: resource_title,
+          resource_path: resource_path
+        ).html_safe
+      end
+    end
+  end
+end
---
+++ decidim-meetings/spec/jobs/decidim/meetings/upcoming_meeting_notification_job_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Decidim::Meetings::UpcomingMeetingNotificationJob do
+  let(:organization) { create :organization }
+  let(:user) { create :user, organization: organization }
+  let(:start_time) { 1.day.from_now }
+  let(:participatory_space) { create :participatory_process, organization: organization }
+  let(:feature) { create :feature, manifest_name: :meetings, participatory_space: participatory_space }
+  let(:meeting) { create :meeting, start_time: start_time }
+  let!(:checksum) { subject.generate_checksum(meeting) }
+  let!(:follow) { create :follow, followable: meeting, user: user }
+  subject { described_class }
+
+  context "when the checksum is correct" do
+    it "notifies the upcoming meeting" do
+      expect(Decidim::EventsManager)
+        .to receive(:publish)
+        .with(
+          event: "decidim.events.meetings.upcoming_meeting",
+          event_class: Decidim::Meetings::UpcomingMeetingEvent,
+          resource: meeting,
+          recipient_ids: [user.id]
+        )
+
+      subject.perform_now(meeting.id, checksum)
+    end
+  end
+
+  context "when the checksum is not correct" do
+    let(:checksum) { "1234" }
+
+    it "doesn't notify the upcoming meeting" do
+      expect(Decidim::EventsManager)
+        .not_to receive(:publish)
+
+      subject.perform_now(meeting.id, checksum)
+    end
+  end
+end
