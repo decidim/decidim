4dce4c7b7127ed611eb331f3fb1ad09d02d0d331
David Morcillo <david.morcillo@gmail.com>
2017-09-13 11:50:52 +0200

    Create shared examples for follows

---
+++ decidim-core/lib/decidim/core/test/shared_examples/follows_examples.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+shared_examples "follows" do
+  include_context "feature"
+
+  before do
+    login_as user, scope: :user
+  end
+
+  context "when not following the followable" do
+    context "when user clicks the Follow button" do
+      it "makes the user follow the followable" do
+        visit resource_locator(followable).path
+        expect do
+          click_button "Follow"
+          expect(page).to have_content "Stop following"
+        end.to change(Decidim::Follow, :count).by(1)
+      end
+    end
+  end
+
+  context "when the user is following the followable" do
+    before do
+      create(:follow, followable: followable, user: user)
+    end
+
+    context "when user clicks the Follow button" do
+      it "makes the user follow the followable" do
+        visit resource_locator(followable).path
+        expect do
+          click_button "Stop following"
+          expect(page).to have_content "Follow"
+        end.to change(Decidim::Follow, :count).by(-1)
+      end
+    end
+  end
+end
--- decidim-meetings/spec/features/follow_meetings_spec.rb
+++ decidim-meetings/spec/features/follow_meetings_spec.rb
@@ -3,42 +3,11 @@
 require "spec_helper"
 
 describe "Follow meetings", type: :feature do
-  include_context "feature"
   let(:manifest_name) { "meetings" }
 
-  let!(:meeting) do
+  let!(:followable) do
     create(:meeting, feature: feature)
   end
 
-  before do
-    login_as user, scope: :user
-  end
-
-  context "when not following the meeting" do
-    context "when user clicks the Follow button" do
-      it "makes the user follow the meeting" do
-        visit resource_locator(meeting).path
-        expect do
-          click_button "Follow"
-          expect(page).to have_content "Stop following"
-        end.to change(Decidim::Follow, :count).by(1)
-      end
-    end
-  end
-
-  context "when the user is following the meeting" do
-    before do
-      create(:follow, followable: meeting, user: user)
-    end
-
-    context "when user clicks the Follow button" do
-      it "makes the user follow the meeting" do
-        visit resource_locator(meeting).path
-        expect do
-          click_button "Stop following"
-          expect(page).to have_content "Follow"
-        end.to change(Decidim::Follow, :count).by(-1)
-      end
-    end
-  end
+  include_examples "follows"
 end
