d7e3d52c9107e971bf3c00dd29e19b1d6af791e8
David Morcillo <david.morcillo@gmail.com>
2017-09-14 12:09:19 +0200

    Fix rubocop issues

--- decidim-core/db/migrate/20170914092116_remove_comment_and_replies_notifications_from_users.rb
+++ decidim-core/db/migrate/20170914092116_remove_comment_and_replies_notifications_from_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveCommentAndRepliesNotificationsFromUsers < ActiveRecord::Migration[5.1]
   def change
     remove_column :decidim_users, :comments_notifications
