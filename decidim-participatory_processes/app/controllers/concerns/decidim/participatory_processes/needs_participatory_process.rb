fb56e705934cd23aa5274c977a6e399507067145
Marc Riera Casals <mrc2407@gmail.com>
2017-09-15 14:50:47 +0200

    Fix some specs

--- decidim-participatory_processes/app/controllers/concerns/decidim/participatory_processes/needs_participatory_process.rb
+++ decidim-participatory_processes/app/controllers/concerns/decidim/participatory_processes/needs_participatory_process.rb
@@ -42,7 +42,7 @@ module Decidim
 
         def detect_participatory_process
           request.env["current_participatory_process"] ||
-            organization_processes.where(slug: params["participatory_process_slug"]).or(
+            organization_processes.where(slug: params[:participatory_process_slug] || params[:slug]).or(
               organization_processes.where(id: params["participatory_process_id"])
             ).first
         end
