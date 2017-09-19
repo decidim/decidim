8cdd02140951f877f539e0cec0a38e404b700c09
David Morcillo <david.morcillo@gmail.com>
2017-09-19 15:27:33 +0200

    Add feature specs

--- decidim-meetings/spec/shared/manage_registrations_examples.rb
+++ decidim-meetings/spec/shared/manage_registrations_examples.rb
@@ -1,10 +1,26 @@
 # frozen_string_literal: true
 
+def visit_edit_registrations_page
+  within find("tr", text: translated(meeting.title)) do
+    page.find("a.action-icon--registrations").click
+  end
+end
+
+def fill_in_meeting_registration_invite(with:)
+  click_button "Invite user"
+
+  within "form.new_meeting_registration_invite" do
+    fill_in :meeting_registration_invite_email, with: with
+  end
+
+  click_button "Invite"
+
+  expect(page).to have_content("successfully")
+end
+
 shared_examples "manage registrations" do
   it "enable and configure registrations" do
-    within find("tr", text: translated(meeting.title)) do
-      page.find("a.action-icon--registrations").click
-    end
+    visit_edit_registrations_page
 
     within ".edit_meeting_registrations" do
       check :meeting_registrations_enabled
@@ -24,17 +40,68 @@ shared_examples "manage registrations" do
     end
   end
 
+  context "when registrations are not enabled" do
+    it "cannot invite people to join a meeting" do
+      visit_edit_registrations_page
+      expect(page).to have_selector("button:disabled", text: "Invite user")
+    end
+  end
+
   context "when registrations are enabled" do
     let!(:meeting) { create :meeting, scope: scope, feature: current_feature, registrations_enabled: true }
     let!(:registrations) { create_list :registration, 10, meeting: meeting }
 
     context "and a few registrations have been created" do
       it "can verify the number of registrations" do
-        within find("tr", text: translated(meeting.title)) do
-          page.find("a.action-icon--registrations").click
+        visit_edit_registrations_page
+        expect(page).to have_content("#{registrations.length} registrations")
+      end
+    end
+
+    context "when inviting a unregistered user" do
+      it "the invited user sign up into the application and join the meeting" do
+        visit_edit_registrations_page
+
+        fill_in_meeting_registration_invite with: "foo@example.org"
+
+        logout :user
+
+        visit last_email_link
+
+        within "form.new_user" do
+          fill_in :user_password, with: "123456"
+          fill_in :user_password_confirmation, with: "123456"
+          find("*[type=submit]").click
         end
 
-        expect(page).to have_content("#{registrations.length} registrations")
+        expect(page).to have_content("successfully")
+
+        visit resource_locator(meeting).path
+
+        within ".card.extra" do
+          expect(page).to have_css(".button", text: "GOING")
+        end
+      end
+    end
+
+    context "when inviting a registered user" do
+      let!(:registered_user) { create(:user, :confirmed, organization: organization) }
+
+      it "the invited user sign up into the application and join the meeting" do
+        visit_edit_registrations_page
+
+        fill_in_meeting_registration_invite with: registered_user.email
+
+        logout :user
+
+        visit last_email_link
+
+        expect(page).to have_content("successfully")
+        expect(current_path).to eq(resource_locator(meeting).path)
+
+        within ".card.extra" do
+          expect(page).to have_css(".button", text: "GOING")
+        end
       end
     end
   end
@@ -43,9 +110,7 @@ shared_examples "manage registrations" do
     let!(:registrations) { create_list :registration, 10, meeting: meeting }
 
     it "exports a CSV" do
-      within find("tr", text: translated(meeting.title)) do
-        page.find("a.action-icon--registrations").click
-      end
+      visit_edit_registrations_page
 
       find(".exports.dropdown").click
 
@@ -56,9 +121,7 @@ shared_examples "manage registrations" do
     end
 
     it "exports a JSON" do
-      within find("tr", text: translated(meeting.title)) do
-        page.find("a.action-icon--registrations").click
-      end
+      visit_edit_registrations_page
 
       find(".exports.dropdown").click
 
