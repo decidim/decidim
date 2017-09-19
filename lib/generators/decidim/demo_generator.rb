bcb37f24f50ce66c4859ccead2e0709f6abc349c
David Rodríguez <deivid.rodriguez@riseup.net>
2017-09-19 16:54:19 +0200

    Remove unused `require_relative`

--- lib/generators/decidim/demo_generator.rb
+++ lib/generators/decidim/demo_generator.rb
@@ -3,7 +3,6 @@
 require "rails/generators"
 require "rails/generators/rails/app/app_generator"
 require "decidim/core/version"
-require_relative "install_generator"
 
 module Decidim
   module Generators
