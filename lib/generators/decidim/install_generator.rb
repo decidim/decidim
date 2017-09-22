c96b06bd58bbfcf2e0dd385f07cbbc45fa279a93
David Rodríguez <deivid.rodriguez@riseup.net>
2017-09-22 21:25:37 +0200

    Don't swallow errors in app generation
    The `rails_command` method shells out the target command, but swallows
    any errors that may happen. In my opinion, this is bad because we really
    want these commands to succeed, and since the output is usually long,
    you might not notice that they have failed. In addition, if errors are
    swallowed, it's really hard to unit test generators!

--- lib/generators/decidim/install_generator.rb
+++ lib/generators/decidim/install_generator.rb
@@ -37,7 +37,7 @@ module Decidim
       end
 
       def copy_migrations
-        rails_command "railties:install:migrations"
+        rails "railties:install:migrations"
         recreate_db if options[:recreate_db]
       end
 
@@ -118,16 +118,20 @@ module Decidim
       private
 
       def recreate_db
-        rails_command "db:environment:set db:drop" unless ENV["CI"]
-        rails_command "db:create"
+        rails "db:environment:set", "db:drop" unless ENV["CI"]
+        rails "db:create"
 
         if options[:seed_db]
-          rails_command "db:migrate db:seed"
+          rails "db:migrate", "db:seed"
         else
-          rails_command "db:migrate"
+          rails "db:migrate"
         end
 
-        rails_command "db:test:prepare"
+        rails "db:test:prepare"
+      end
+
+      def rails(*args)
+        abort unless system("bin/rails", *args)
       end
 
       def scss_variables
