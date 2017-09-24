1ddd1646b07f498e9b40312a6ec1e7d26ff1e34e
David Rodríguez <deivid.rodriguez@riseup.net>
2017-09-24 18:42:09 +0200

    Extract a shared example

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -5,35 +5,33 @@ describe "Application generation" do
 
   after { FileUtils.rm_rf("tmp/test_app") }
 
-  context "with edge argument" do
-    let(:command) { "bin/decidim --edge tmp/test_app" }
-
+  shared_examples_for "a sane generator" do
     it "successfully generates application" do
       expect(status).to eq(true)
     end
+  end
+
+  context "with edge argument" do
+    let(:command) { "bin/decidim --edge tmp/test_app" }
+
+    it_behaves_like "a sane generator"
   end
 
   context "with branch argument" do
     let(:command) { "bin/decidim --branch master tmp/test_app" }
 
-    it "successfully generates application" do
-      expect(status).to eq(true)
-    end
+    it_behaves_like "a sane generator"
   end
 
   context "with path argument" do
     let(:command) { "bin/decidim --path #{File.expand_path("..", __dir__)} tmp/test_app" }
 
-    it "successfully generates application" do
-      expect(status).to eq(true)
-    end
+    it_behaves_like "a sane generator"
   end
 
   context "development application" do
     let(:command) { "rake development_app" }
 
-    it "successfully generates application" do
-      expect(status).to eq(true)
-    end
+    it_behaves_like "a sane generator"
   end
 end
