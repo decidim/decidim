098b276dc1a6a3d74c259632f27f25fcccbbf181
David Morcillo <david.morcillo@gmail.com>
2017-09-25 15:10:53 +0200

    Remove decidim-results files

--- decidim-results/db/migrate/20170410074358_remove_not_null_reference_results.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveNotNullReferenceResults < ActiveRecord::Migration[5.0]
-  def change
-    change_column_null :decidim_results_results, :reference, true
-  end
-end
