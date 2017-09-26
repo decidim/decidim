8ab6edf23cde9f6ec3b44b33281fbcaf6dc095f2
David Morcillo <david.morcillo@gmail.com>
2017-09-26 14:10:33 +0200

    Fix lint errors

--- decidim-accountability/app/assets/javascripts/decidim/accountability/admin/accountability_admin.js
+++ decidim-accountability/app/assets/javascripts/decidim/accountability/admin/accountability_admin.js
@@ -1,8 +1,9 @@
 // = require_self
 
 $("#result_decidim_accountability_status_id").change(function () {
-  progress = $(this).find(':selected').data('progress')
-  if (progress || progress == 0) {
+  /* eslint-disable no-invalid-this */
+  const progress = $(this).find(':selected').data('progress')
+  if (progress || progress === 0) {
     $("#result_progress").val(progress);
   }
 });
