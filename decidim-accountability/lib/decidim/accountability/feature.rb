5016453a6790233795f07f221cb92a76126be1ff
David Morcillo <david.morcillo@gmail.com>
2017-09-26 15:29:01 +0200

    Fix seeds

--- decidim-accountability/lib/decidim/accountability/feature.rb
+++ decidim-accountability/lib/decidim/accountability/feature.rb
@@ -25,61 +25,57 @@ Decidim.register_feature(:accountability) do |feature|
     settings.attribute :comments_blocked, type: :boolean, default: false
   end
 
-  feature.seeds do
-    Decidim::ParticipatoryProcess.all.each do |process|
-      next unless process.steps.any?
+  feature.seeds do |participatory_space|
+    feature = Decidim::Feature.create!(
+      name: Decidim::Features::Namer.new(participatory_space.organization.available_locales, :accountability).i18n_name,
+      manifest_name: :accountability,
+      published_at: Time.current,
+      participatory_space: participatory_space
+    )
 
-      feature = Decidim::Feature.create!(
-        name: Decidim::Features::Namer.new(process.organization.available_locales, :accountability).i18n_name,
-        manifest_name: :accountability,
-        published_at: Time.current,
-        participatory_space: process
+    5.times do |i|
+      Decidim::Accountability::Status.create!(
+        feature: feature,
+        name: Decidim::Faker::Localized.word,
+        key: "status_#{i}"
+      )
+    end
+
+    3.times do
+      result = Decidim::Accountability::Result.create!(
+        feature: feature,
+        scope: participatory_space.organization.scopes.sample,
+        category: participatory_space.categories.sample,
+        title: Decidim::Faker::Localized.sentence(2),
+        description: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
+          Decidim::Faker::Localized.paragraph(3)
+        end
       )
 
-      5.times do |i|
-        Decidim::Accountability::Status.create!(
-          feature: feature,
-          name: Decidim::Faker::Localized.word,
-          key: "status_#{i}"
-        )
-      end
+      Decidim::Comments::Seed.comments_for(result)
 
       3.times do
-        result = Decidim::Accountability::Result.create!(
+        child_result = Decidim::Accountability::Result.create!(
           feature: feature,
-          scope: process.organization.scopes.sample,
-          category: process.categories.sample,
+          parent: result,
+          start_date: Time.zone.today,
+          end_date: Time.zone.today + 10,
+          status: Decidim::Accountability::Status.all.sample,
+          progress: rand(1..100),
           title: Decidim::Faker::Localized.sentence(2),
           description: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
             Decidim::Faker::Localized.paragraph(3)
           end
         )
 
-        Decidim::Comments::Seed.comments_for(result)
-
-        3.times do
-          child_result = Decidim::Accountability::Result.create!(
-            feature: feature,
-            parent: result,
-            start_date: Time.zone.today,
-            end_date: Time.zone.today + 10,
-            status: Decidim::Accountability::Status.all.sample,
-            progress: rand(1..100),
-            title: Decidim::Faker::Localized.sentence(2),
-            description: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
-              Decidim::Faker::Localized.paragraph(3)
-            end
+        rand(0..5).times do |i|
+          child_result.timeline_entries.create!(
+            entry_date: child_result.start_date + i.days,
+            description: Decidim::Faker::Localized.sentence(2)
           )
-
-          rand(0..5).times do |i|
-            child_result.timeline_entries.create!(
-              entry_date: child_result.start_date + i.days,
-              description: Decidim::Faker::Localized.sentence(2)
-            )
-          end
-
-          Decidim::Comments::Seed.comments_for(child_result)
         end
+
+        Decidim::Comments::Seed.comments_for(child_result)
       end
     end
   end
