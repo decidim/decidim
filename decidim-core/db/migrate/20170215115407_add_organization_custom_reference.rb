f1ca99fde7191c095ae64fce03046267f7160ab7
David Rodríguez <deivid.rodriguez@riseup.net>
2017-09-27 18:44:12 +0200

    Fix more model usages in migrations

--- decidim-core/db/migrate/20170215115407_add_organization_custom_reference.rb
+++ decidim-core/db/migrate/20170215115407_add_organization_custom_reference.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class AddOrganizationCustomReference < ActiveRecord::Migration[5.0]
+  class Organization < ApplicationRecord
+    self.table_name = :decidim_organizations
+  end
+
   def change
     add_column :decidim_organizations, :reference_prefix, :string
 
-    Decidim::Organization.find_each do |organization|
+    Organization.find_each do |organization|
       organization.update_attributes!(reference_prefix: organization.name[0])
     end
 
