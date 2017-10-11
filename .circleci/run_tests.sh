23689559b12e2045d48c6d97208d2eff14508cac
Marc Riera Casals <mrc2407@gmail.com>
2017-10-11 15:37:12 +0200

    f

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -3,10 +3,11 @@
 engine=$1
 testCommand=$2
 branch=`git rev-parse --abbrev-ref HEAD`
+modifiedFiles="git diff --name-only $branch master"
 
 if git diff --name-only $branch master | grep "^${engine}" ; then
   echo "YES"
-  echo $testCommand
+  eval $testCommand
 else
   echo "NO"
 fi
