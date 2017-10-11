e3ae3082fdc62249e8ee93963f0754c37cb0b681
David Rodríguez <deivid.rodriguez@riseup.net>
2017-10-11 13:01:07 +0200

    Go minimal

--- decidim-dev/lib/decidim/dev/test/rspec_support/capybara_select2.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/capybara_select2.rb
@@ -4,22 +4,13 @@
 
 module Capybara
   module Select2
-    def select2(value, options = {})
-      raise "Must pass a hash containing 'from' or 'xpath' or 'css'" unless options.is_a?(Hash) && [:from, :xpath, :css].any? { |k| options.has_key? k }
-
-      if options.has_key? :xpath
-        select2_container = find(:xpath, options[:xpath])
-      elsif options.has_key? :css
-        select2_container = find(:css, options[:css])
-      else
-        select_name = options[:from]
-        select2_container = find("label", text: select_name).find(:xpath, "..").find(".select2-container")
-      end
+    def select2(value, xpath:, search:)
+      select2_container = find(:xpath, xpath)
 
       # Open select2 field
       select2_container.find(".select2-selection").click
 
-      if options.has_key? :search
+      if search
         find(:xpath, "//body").find(".select2-search input.select2-search__field").set(value)
         page.execute_script(%|$("input.select2-search__field:visible").keyup();|)
         drop_container = ".select2-results"
@@ -29,9 +20,7 @@ module Capybara
 
       expect(page).to have_no_content("Searching...")
 
-      [value].flatten.each do |val|
-        find(:xpath, "//body").find("#{drop_container} li.select2-results__option", text: val).click
-      end
+      find(:xpath, "//body").find("#{drop_container} li.select2-results__option", text: value).click
     end
   end
 end
