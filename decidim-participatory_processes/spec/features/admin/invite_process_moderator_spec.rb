dc2ad3282c4111ffee9f5d06877b57ff5b1c9fd5
Marc Riera Casals <mrc2407@gmail.com>
2017-10-11 10:56:03 +0200

    Show processes for moderators
    Moderators could not see the processes link in admin, so they couldn't really
    manage them.

--- decidim-participatory_processes/spec/features/admin/invite_process_moderator_spec.rb
+++ decidim-participatory_processes/spec/features/admin/invite_process_moderator_spec.rb
@@ -21,6 +21,71 @@ describe "Invite process moderator", type: :feature do
 
   before do
     switch_to_host organization.host
+  end
+
+  context "when the user does not exist" do
+    describe "Accept an invitation", perform_enqueued: true do
+      before do
+        invite_user
+      end
+
+      it "asks for a password and redirects to the admin dashboard" do
+        visit last_email_link
+
+        within "form.new_user" do
+          fill_in :user_password, with: "123456"
+          fill_in :user_password_confirmation, with: "123456"
+          find("*[type=submit]").click
+        end
+
+        expect(current_path).to eq "/admin/"
+        expect(page).to have_content("DASHBOARD")
+
+        click_link "Processes"
+
+        within "#processes" do
+          expect(page).to have_i18n_content(participatory_process.title)
+          click_link translated(participatory_process.title)
+        end
+
+        within ".secondary-nav" do
+          expect(page.text).to eq "Moderations"
+        end
+      end
+    end
+  end
+
+  context "when the user already exists" do
+    describe "Accept an invitation", perform_enqueued: true do
+      let(:email) { "moderator@example.org" }
+      let(:moderator) { @moderator }
+
+      before do
+        @moderator = create :user, :confirmed, email: email, organization: organization
+        invite_user
+      end
+
+      it "redirects the moderator to the admin dashboard" do
+        login_as moderator, scope: :user
+
+        visit decidim_admin.root_path
+        expect(page).to have_content("DASHBOARD")
+
+        click_link "Processes"
+
+        within "#processes" do
+          expect(page).to have_i18n_content(participatory_process.title)
+          click_link translated(participatory_process.title)
+        end
+
+        within ".secondary-nav" do
+          expect(page.text).to eq "Moderations"
+        end
+      end
+    end
+  end
+
+  def invite_user
     login_as user, scope: :user
 
     visit decidim_admin_participatory_processes.participatory_process_user_roles_path(participatory_process)
@@ -33,22 +98,5 @@ describe "Invite process moderator", type: :feature do
     select role, from: "Role"
     click_button "Create"
     logout :user
-  end
-
-  context "when the user does not exist" do
-    describe "Accept an invitation", perform_enqueued: true do
-      it "asks for a password and redirects to the admin dashboard" do
-        visit last_email_link
-
-        within "form.new_user" do
-          fill_in :user_password, with: "123456"
-          fill_in :user_password_confirmation, with: "123456"
-          find("*[type=submit]").click
-        end
-
-        expect(current_path).to eq "/admin/"
-        expect(page).to have_content("DASHBOARD")
-      end
-    end
   end
 end
