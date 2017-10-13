29a289050cfa8f50436d2924b0724a0ee339c70b
Marc Riera Casals <mrc2407@gmail.com>
2017-10-13 09:19:22 +0200

    Try to keep the code simpler
    Should only run main,proposals

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -3,21 +3,19 @@
 engine=$1
 testCommand=$2
 branch=`git rev-parse --abbrev-ref HEAD`
-echo $branch
-modifiedFiles="git diff --name-only $branch master"
 
-git checkout master
-git reset --hard origin/master
-git checkout -
+# git checkout master
+# git reset --hard origin/master
+# git checkout -
 
 if [ "$branch" = "master" ]; then
-   echo "YES, IT'S MASTER!!"
-   eval $testCommand
-elif git diff --name-only $branch master | grep "^decidim-core" ; then
-  echo "YES"
+  echo "YES, IT'S MASTER!!"
   eval $testCommand
-elif git diff --name-only $branch master | grep "^${engine}" ; then
-  echo "YES"
+elif git diff --name-only $branch origin/master | grep "^decidim-core" ; then
+  echo "YES, CORE IS BEING MODIFIED"
+  eval $testCommand
+elif git diff --name-only $branch origin/master | grep "^${engine}" ; then
+  echo "YES, THIS ENGINE IS BEING MODIFIED"
   eval $testCommand
 else
   echo "NO"
