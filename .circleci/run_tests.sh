07098df685529bfdc4ee1ccb177434484cb01e54
Marc Riera Casals <mrc2407@gmail.com>
2017-10-13 09:52:15 +0200

    Improve command to check for files
    Runs main,proposals

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -4,17 +4,13 @@ engine=$1
 testCommand=$2
 branch=`git rev-parse --abbrev-ref HEAD`
 
-# git checkout master
-# git reset --hard origin/master
-# git checkout -
-
 if [ "$branch" = "master" ]; then
   echo "YES, IT'S MASTER!!"
   eval $testCommand
-elif git diff --name-only $branch origin/master | grep "^decidim-core" ; then
+elif git diff --name-only origin/master...$branch  | grep "^decidim-core" ; then
   echo "YES, CORE IS BEING MODIFIED"
   eval $testCommand
-elif git diff --name-only $branch origin/master | grep "^${engine}" ; then
+elif git diff --name-only origin/master...$branch  | grep "^${engine}" ; then
   echo "YES, THIS ENGINE IS BEING MODIFIED"
   eval $testCommand
 else
