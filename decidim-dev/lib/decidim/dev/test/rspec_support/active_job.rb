895bbb9e94492163875fcbb6892e16229d9558ec
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-10-16 10:03:50 +0200

    Fix job enququeing

--- decidim-dev/lib/decidim/dev/test/rspec_support/active_job.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/active_job.rb
@@ -3,11 +3,7 @@
 RSpec.configure do |config|
   config.include ActiveJob::TestHelper
 
-  config.around :example, perform_enqueued: true do |example|
-    perform_enqueued_jobs { example.run }
-  end
-
-  config.after(:each) do
+  config.before(:each) do
     clear_enqueued_jobs
   end
 end
