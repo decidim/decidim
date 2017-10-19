9de7249aab489dcc68d5b04bdbc3c10f91f3bf60
Marc Riera Casals <mrc2407@gmail.com>
2017-10-19 10:38:30 +0200

    Ensure CTA button path is a path

--- decidim-admin/app/forms/decidim/admin/organization_appearance_form.rb
+++ decidim-admin/app/forms/decidim/admin/organization_appearance_form.rb
@@ -29,8 +29,10 @@ module Decidim
       translatable_attribute :description, String
       translatable_attribute :welcome_text, String
 
+      validates :cta_button_path, format: { with: %r{\A[a-zA-Z]+[a-zA-Z0-9-/]+\z} }, allow_blank: true
       validates :official_img_header,
                 :official_img_footer,
+                :homepage_image,
                 :logo,
                 file_size: { less_than_or_equal_to: ->(_record) { Decidim.maximum_attachment_size } },
                 file_content_type: { allow: ["image/jpeg", "image/png"] }
--- decidim-admin/spec/forms/organization_appearance_form_spec.rb
+++ decidim-admin/spec/forms/organization_appearance_form_spec.rb
@@ -21,6 +21,7 @@ module Decidim
         }
       end
       let(:organization) { create(:organization) }
+      let(:cta_button_path) { nil }
       let(:homepage_image_path) { Decidim::Dev.asset("city.jpeg") }
       let(:attributes) do
         {
@@ -33,6 +34,7 @@ module Decidim
             "description_ca" => description[:ca],
             "homepage_image" => Rack::Test::UploadedFile.new(homepage_image_path, "image/jpeg"),
             "show_statics" => false,
+            "cta_button_path" => cta_button_path,
             "header_snippets" => header_snippets
           }
         }
@@ -51,6 +53,18 @@ module Decidim
       end
 
       context "when everything is OK" do
+        it { is_expected.to be_valid }
+      end
+
+      context "when cta_button_path is a full URL" do
+        let(:cta_button_path) { "http://example.org" }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context "when cta_button_path is a valid path" do
+        let(:cta_button_path) { "processes/my-process/" }
+
         it { is_expected.to be_valid }
       end
     end
