a58303816c7e80195755b57906a4ff3eee6723d3
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-10-23 14:41:24 +0200

    Constantize authorization handlers in migrations

--- decidim-core/db/migrate/20170313095436_add_available_authorizations_to_organization.rb
+++ decidim-core/db/migrate/20170313095436_add_available_authorizations_to_organization.rb
@@ -8,7 +8,7 @@ class AddAvailableAuthorizationsToOrganization < ActiveRecord::Migration[5.0]
   def change
     add_column :decidim_organizations, :available_authorizations, :string, array: true, default: []
 
-    handlers = Decidim.authorization_handlers
+    handlers = Decidim.authorization_handlers.map(&:constantize)
     Organization.find_each do |org|
       org.update_attributes!(available_authorizations: handlers)
     end
