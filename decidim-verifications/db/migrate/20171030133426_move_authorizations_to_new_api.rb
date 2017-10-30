1f3b1185f2de06909b20c0e7c89aac0fbfdebb61
David Rodríguez <deivid.rodriguez@riseup.net>
2017-10-30 13:16:11 +0100

    Migrate to new API

---
+++ decidim-verifications/db/migrate/20171030133426_move_authorizations_to_new_api.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+#
+# Assumes to authorizations in the old format (as rectify form classes) will be
+# registered as the underscored class name using the new API. For example, a
+# previous
+#
+# ```
+# config.authorization_handlers = ["Decidim::ExampleCensusHandler"]
+# ```
+#
+# will now be
+#
+# ```
+# Decidim::Verifications.register_workflow(:example_census_handler) do |auth|
+#   auth.form = "Decidim::ExampleCensusHandler"
+# end
+# ```
+#
+class MoveAuthorizationsToNewApi < ActiveRecord::Migration[5.1]
+  class Organization < ApplicationRecord
+    self.table_name = :decidim_organizations
+  end
+
+  class Feature < ApplicationRecord
+    self.table_name = :decidim_features
+  end
+
+  def up
+    Organization.find_each do |organization|
+      migrated_authorizations = organization.available_authorizations.map do |authorization|
+        authorization.demodulize.underscore
+      end
+
+      organization.update!(available_authorizations: migrated_authorizations)
+    end
+
+    Feature.find_each do |feature|
+      feature.permissions.transform_values! do |value|
+        value["authorization_handler_name"].classify.demodulize.underscore
+      end
+
+      feature.save!
+    end
+  end
+
+  def down
+    Organization.find_each do |organization|
+      migrated_authorizations = organization.available_authorizations.map do |authorization|
+        Decidim::Verifications.find_workflow_manifest(authorization).form
+      end
+
+      organization.update!(available_authorizations: migrated_authorizations)
+    end
+
+    Feature.find_each do |feature|
+      feature.permissions.transform_values! do |value|
+        workflow = Decidim::Verifications.find_workflow_manifest(value)
+
+        workflow.form.underscore
+      end
+
+      feature.save!
+    end
+  end
+end
