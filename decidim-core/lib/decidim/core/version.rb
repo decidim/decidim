0a32e81c69ce1a2785ad0be14cc941a16390ed5b
Marc Riera Casals <mrc2407@gmail.com>
2017-11-06 11:11:46 +0100

    Bump version and CHANGELOG

--- decidim-core/lib/decidim/core/version.rb
+++ decidim-core/lib/decidim/core/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-core version.
   module Core
     def self.version
-      "0.7.1"
+      "0.7.2"
     end
   end
 end
--- decidim-system/lib/decidim/system/version.rb
+++ decidim-system/lib/decidim/system/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-system version.
   module System
     def self.version
-      "0.7.1"
+      "0.7.2"
     end
   end
 end
