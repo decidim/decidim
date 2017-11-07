71b512ffd12063d4435b613583a3f9b7cc09454e
David Rodríguez <deivid.rodriguez@riseup.net>
2017-11-07 08:54:11 -0300

    Create a testing docker image
    And bundle test dependencies in there.

--- .circleci/install-dockerize.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
-tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
-rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
