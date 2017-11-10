bb44b8c0cf9df120fb77546e4ed8cdc42c80ee83
David Rodríguez <deivid.rodriguez@riseup.net>
2017-11-10 18:37:24 -0300

    Fix RSpec/MultipleDescribes offenses

---
+++ decidim-accountability/spec/features/process_admin_manages_accountability_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Process admin manages accountability", type: :feature do
+  let(:manifest_name) { "accountability" }
+
+  include_context "admin"
+  include_context "feature admin"
+
+  let(:user) { process_admin }
+
+  before do
+    switch_to_host(organization.host)
+    login_as user, scope: :user
+    visit_feature_admin
+  end
+
+  describe "Process admin manages results" do
+    it_behaves_like "manage results"
+    it_behaves_like "export results"
+  end
+
+  describe "child results" do
+    it_behaves_like "manage child results"
+
+    before do
+      click_link translated(result.title)
+    end
+  end
+
+  describe "statuses" do
+    it_behaves_like "manage statuses"
+
+    before do
+      click_link "Statuses"
+    end
+  end
+end
