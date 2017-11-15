cc5c9ff8640106712329506d5fdb2704acc3a2fb
David Rodríguez <deivid.rodriguez@riseup.net>
2017-11-15 11:09:21 -0300

    Fix another potential flakyness in select2 helper

--- decidim-dev/lib/decidim/dev/test/rspec_support/capybara_select2.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/capybara_select2.rb
@@ -4,9 +4,9 @@
 
 module Capybara
   module Select2
-    def select2(value, xpath:, search:)
-      expect(page).to have_xpath(xpath)
-      select2_container = find(:xpath, xpath)
+    def select2(value, id:, search:)
+      expect(page).to have_xpath("//select[@id='#{id}']/..")
+      select2_container = find(:xpath, "//select[@id='#{id}']/..")
 
       expect(select2_container).to have_selector(".select2-selection")
       select2_container.find(".select2-selection").click
@@ -26,7 +26,9 @@ module Capybara
 
       body = find(:xpath, "//body")
       expect(body).to have_selector("#{drop_container} li.select2-results__option", text: value)
+
       body.find("#{drop_container} li.select2-results__option", text: value).click
+      expect(page).to have_select(id, with_options: [value])
     end
   end
 end
--- decidim-proposals/spec/features/proposals_spec.rb
+++ decidim-proposals/spec/features/proposals_spec.rb
@@ -74,7 +74,7 @@ describe "Proposals", type: :feature do
             fill_in :proposal_title, with: "Oriol for president"
             fill_in :proposal_body, with: "He will solve everything"
             select translated(category.name), from: :proposal_category_id
-            select2 translated(scope.name), xpath: '//select[@id="proposal_scope_id"]/..', search: true
+            select2 translated(scope.name), id: "proposal_scope_id", search: true
 
             find("*[type=submit]").click
           end
@@ -109,7 +109,7 @@ describe "Proposals", type: :feature do
 
               fill_in :proposal_address, with: address
               select translated(category.name), from: :proposal_category_id
-              select2 translated(scope.name), xpath: '//select[@id="proposal_scope_id"]/..', search: true
+              select2 translated(scope.name), id: "proposal_scope_id", search: true
 
               find("*[type=submit]").click
             end
@@ -139,7 +139,7 @@ describe "Proposals", type: :feature do
               fill_in :proposal_title, with: "Oriol for president"
               fill_in :proposal_body, with: "He will solve everything"
               select translated(category.name), from: :proposal_category_id
-              select2 translated(scope.name), xpath: '//select[@id="proposal_scope_id"]/..', search: true
+              select2 translated(scope.name), id: "proposal_scope_id", search: true
               select user_group.name, from: :proposal_user_group_id
 
               find("*[type=submit]").click
@@ -174,7 +174,7 @@ describe "Proposals", type: :feature do
 
                 fill_in :proposal_address, with: address
                 select translated(category.name), from: :proposal_category_id
-                select2 translated(scope.name), xpath: '//select[@id="proposal_scope_id"]/..', search: true
+                select2 translated(scope.name), id: "proposal_scope_id", search: true
                 select user_group.name, from: :proposal_user_group_id
 
                 find("*[type=submit]").click
@@ -665,7 +665,7 @@ describe "Proposals", type: :feature do
         context "selecting the global scope" do
           it "lists the filtered proposals" do
             within ".filters" do
-              select2("Global scope", xpath: '//select[@id="filter_scope_id"]/..', search: false)
+              select2("Global scope", id: "filter_scope_id", search: false)
             end
 
             expect(page).to have_css(".card--proposal", count: 1)
@@ -676,7 +676,7 @@ describe "Proposals", type: :feature do
         context "selecting one scope" do
           it "lists the filtered proposals" do
             within ".filters" do
-              select2(translated(scope.name), xpath: '//select[@id="filter_scope_id"]/..', search: true)
+              select2(translated(scope.name), id: "filter_scope_id", search: true)
             end
 
             expect(page).to have_css(".card--proposal", count: 2)
@@ -687,8 +687,8 @@ describe "Proposals", type: :feature do
         context "selecting the global scope and another scope" do
           it "lists the filtered proposals" do
             within ".filters" do
-              select2(translated(scope.name), xpath: '//select[@id="filter_scope_id"]/..', search: true)
-              select2("Global scope", xpath: '//select[@id="filter_scope_id"]/..', search: false)
+              select2(translated(scope.name), id: "filter_scope_id", search: true)
+              select2("Global scope", id: "filter_scope_id", search: false)
             end
 
             expect(page).to have_css(".card--proposal", count: 3)
