d22b60c7bec8b5ddbfd5d2782d3d9078e966ae77
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-11-21 15:53:52 +0100

    Fix specs

--- decidim-assemblies/spec/presenters/decidim/assemblies/assembly_stats_presenter_spec.rb
+++ decidim-assemblies/spec/presenters/decidim/assemblies/assembly_stats_presenter_spec.rb
@@ -8,36 +8,35 @@ module Decidim
     let!(:user) { create(:user, :confirmed, organization: organization) }
     let!(:assembly) { create(:assembly, organization: organization) }
     let!(:feature) { create(:feature, participatory_space: assembly) }
-    let!(:feature2) { create(:feature, participatory_space: assembly) }
-    let!(:feature3) { create(:feature, participatory_space: assembly) }
 
     subject { described_class.new(assembly: assembly) }
 
+    let(:manifest) do
+      Decidim::FeatureManifest.new.tap do |manifest|
+        manifest.name = "Test"
+      end
+    end
+
     before do
-      feature.manifest.stats.register :foo, priority: StatsRegistry::HIGH_PRIORITY, &proc { 10 }
-      feature2.manifest.stats.register :bar, priority: StatsRegistry::MEDIUM_PRIORITY, &proc { 20 }
-      feature3.manifest.stats.register :baz, priority: StatsRegistry::LOW_PRIORITY, &proc { 30 }
+      manifest.stats.register :foo, priority: StatsRegistry::HIGH_PRIORITY, &proc { 10 }
+
       I18n.backend.store_translations(
         :en,
         decidim: {
           assemblies: {
             statistics: {
               foo: "Foo",
-              bar: "Bar",
-              baz: "Baz"
             }
           }
         }
       )
 
-      allow(Decidim).to receive(:feature_manifests).and_return([feature.manifest, feature2.manifest, feature3.manifest])
+      allow(Decidim).to receive(:feature_manifests).and_return([manifest])
     end
 
     describe "#highlighted" do
       it "renders a collection of stats including users and proceses" do
         expect(subject.highlighted).to include("10 Foo")
-        expect(subject.highlighted).to include("20 Bar")
-        expect(subject.highlighted).to_not include("30 Baz")
       end
     end
   end
