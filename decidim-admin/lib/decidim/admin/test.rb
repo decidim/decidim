4e9aef7749b8fa6654876fd6e21668fb99953093
David Rodríguez <deivid.rodriguez@riseup.net>
2017-11-21 19:41:49 -0300

    Move moderations examples to decidim-admin

---
+++ decidim-admin/lib/decidim/admin/test.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require "decidim/admin/test/manage_attachments_examples"
+require "decidim/admin/test/manage_feature_permissions_examples"
+require "decidim/admin/test/manage_moderations_examples"
--- decidim-budgets/spec/shared/manage_attachments_examples.rb
+++ decidim-budgets/spec/shared/manage_attachments_examples.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "decidim/admin/test/manage_attachments_examples"
+require "spec_helper"
 
 shared_examples "manage project attachments" do
   let(:attached_to) { project }
