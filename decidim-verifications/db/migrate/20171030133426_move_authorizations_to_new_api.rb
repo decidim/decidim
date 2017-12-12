1fa8a9823e1d68f4f505fcf786d95044549e21e9
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-12-12 12:33:05 +0100

    Use safe navigator

--- decidim-verifications/db/migrate/20171030133426_move_authorizations_to_new_api.rb
+++ decidim-verifications/db/migrate/20171030133426_move_authorizations_to_new_api.rb
@@ -39,7 +39,7 @@ class MoveAuthorizationsToNewApi < ActiveRecord::Migration[5.1]
       next if feature.permissions.nil?
 
       feature.permissions.transform_values! do |value|
-        value["authorization_handler_name"].classify.demodulize.underscore
+        value["authorization_handler_name"]&.classify&.demodulize&.underscore
       end
 
       feature.save!
