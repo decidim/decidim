957179a19e954addfd613e3b54a2c99ff61b0fe2
Isaac Massot <isaac.mg@coditramuntana.com>
2017-12-12 16:10:54 +0100

    [FEAT] improve unsusbscribe links, utm codes

--- decidim-core/app/controllers/decidim/notifications_settings_controller.rb
+++ decidim-core/app/controllers/decidim/notifications_settings_controller.rb
@@ -15,7 +15,8 @@ module Decidim
       @unsubscribe_notifications_settings = false
       @notifications_settings = form(NotificationsSettingsForm).from_params(params)
 
-      if current_user.newsletter_notifications
+      email_user = SignedGlobalID.find(params[:u], for: :unsubscribe_user)
+      if email_user == current_user && current_user.newsletter_notifications
         UnsubscribeNotificationsSettings.call(current_user, @unsubscribe_notifications_settings) do
           on(:ok) do
             flash.now[:notice] = t("notifications_settings.update.success", scope: "decidim")
