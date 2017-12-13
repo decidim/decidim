22800876f6982fd39abedf62422f4d968fa50976
Leonardo Diez <leiodd@gmail.com>
2017-12-13 17:10:25 +0100

    Allow to configure global scope selection value, by default is ''. Used for resources filters.

--- decidim-core/app/controllers/decidim/scopes_controller.rb
+++ decidim-core/app/controllers/decidim/scopes_controller.rb
@@ -43,7 +43,7 @@ module Decidim
         parent_scopes = [root].compact
       end
       render :picker, layout: nil, locals: { required: required, title: title, root: root, current: current, scopes: scopes.order(name: :asc),
-                                             parent_scopes: parent_scopes, context: context }
+                                             parent_scopes: parent_scopes, global_id: params[:global_id], context: context }
     end
   end
 end
