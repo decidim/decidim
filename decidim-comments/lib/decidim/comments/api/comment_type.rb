0cf3aedef14f7033e894731cd695555f8f768bae
David Rodríguez <deivid.rodriguez@riseup.net>
2017-12-15 14:16:59 -0300

    Simplify GraphQL field

--- decidim-comments/lib/decidim/comments/api/comment_type.rb
+++ decidim-comments/lib/decidim/comments/api/comment_type.rb
@@ -27,11 +27,7 @@ module Decidim
         }
       end
 
-      field :formattedCreatedAt, !types.String, "The creation date of the comment in relative format" do
-        resolve lambda { |obj, _args, _ctx|
-          obj.friendly_created_at
-        }
-      end
+      field :formattedCreatedAt, !types.String, "The creation date of the comment in relative format", property: :friendly_created_at
 
       field :author, !Decidim::AuthorInterface, "The comment's author" do
         resolve lambda { |obj, _args, _ctx|
