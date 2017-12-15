cb387e717e1e9e63cc2defcf5446687ae931dd37
Leonardo Diez <leiodd@gmail.com>
2017-12-15 13:11:14 +0100

    Moved scopes picker ERB code for forms and filters to a helper. Related translations moved to core engine. Changelog file updated.

--- decidim-core/app/helpers/decidim/scopes_helper.rb
+++ decidim-core/app/helpers/decidim/scopes_helper.rb
@@ -12,8 +12,26 @@ module Decidim
       current_participatory_space.scopes_enabled? && current_participatory_space.scope.blank? && resource.scope.present?
     end
 
-    def scope_name_with_type(scope)
-      scope ? "#{translated_attribute(scope.name)} (#{translated_attribute(scope.scope_type.name)})" : t("decidim.scopes.global")
+    def scope_name_for_picker(scope, global_name)
+      if scope
+        "#{translated_attribute(scope.name)} (#{translated_attribute(scope.scope_type.name)})"
+      else
+        global_name
+      end
+    end
+
+    def scopes_picker_field(form, name)
+      form.scopes_picker name do |scope|
+        { url: decidim.scopes_picker_path(root: try(:current_participatory_space)&.scope, current: scope&.id, field: form.label_for(name)),
+          text: scope_name_for_picker(scope, I18n.t("decidim.scopes.global")) }
+      end
+    end
+
+    def scopes_picker_filter(form, name)
+      form.scopes_picker name, multiple: true, legend_title: I18n.t("decidim.scopes.scopes"), label: false do |scope|
+        { url: decidim.scopes_picker_path(root: current_participatory_space.scope, current: scope&.id, title: I18n.t("decidim.scopes.prompt"), global_value: "global"),
+          text: scope_name_for_picker(scope, I18n.t("decidim.scopes.prompt")) }
+      end
     end
   end
 end
