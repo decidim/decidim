f9a3c3e5f3006dac8787800cb0e668f8de4e972e
David Rodríguez <deivid.rodriguez@riseup.net>
2017-12-18 21:43:29 -0300

    Centralize user group presentation data

--- decidim-core/lib/decidim/core/api/user_group_type.rb
+++ decidim-core/lib/decidim/core/api/user_group_type.rb
@@ -15,17 +15,17 @@ module Decidim
     field :name, !types.String, "The user group's name"
 
     field :nickname, !types.String, "User groups have no nickname" do
-      resolve ->(_obj, _args, _ctx) { "" }
+      resolve ->(obj, _args, _ctx) { UserGroupPresenter.new(obj).nickname }
     end
 
     field :avatarUrl, !types.String, "The user's avatar url" do
-      resolve ->(obj, _args, _ctx) { obj.avatar.url }
+      resolve ->(obj, _args, _ctx) { UserGroupPresenter.new(obj).avatar_url }
     end
 
     field :isVerified, !types.Boolean, "Whether the user group is verified or not", property: :verified?
 
     field :deleted, !types.Boolean, "Whether the user group's has been deleted or not" do
-      resolve ->(_obj, _args, _ctx) { false }
+      resolve ->(obj, _args, _ctx) { UserGroupPresenter.new(obj).deleted? }
     end
 
     field :isUser, !types.Boolean, "User groups are not users" do
