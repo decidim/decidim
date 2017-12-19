775941630ac20125f090f91743575f59f6d67577
David Rodríguez <deivid.rodriguez@riseup.net>
2017-12-19 07:32:13 -0300

    Document public methods in `Nicknamizable`

--- decidim-core/lib/decidim/nicknamizable.rb
+++ decidim-core/lib/decidim/nicknamizable.rb
@@ -3,14 +3,27 @@
 module Decidim
   # This concern contains the logic related to nicknames.
   module Nicknamizable
+    #
+    # Allowed range for nickname length
+    #
     def nickname_length_range
       (0...nickname_max_length)
     end
 
+    #
+    # Maximum allowed nickname length
+    #
     def nickname_max_length
       20
     end
 
+    #
+    # Converts any string into a valid nickname
+    #
+    # * Parameterizes it so it's valid as a URL.
+    # * Trims length so it fits validation constraints.
+    # * Disambiguates it so it's unique.
+    #
     def nicknamize(name)
       disambiguate(name.parameterize(separator: "_")[nickname_length_range])
     end
