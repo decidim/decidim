e27f9675aeda794fac65159dfee6126f71b29d6c
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-12-20 12:19:11 +0100

    Fix links

--- decidim-design/app/helpers/application_helper.rb
+++ decidim-design/app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def current_page
-    controller.send(:current_page)
-  end
-
-  def partial(name)
-    render partial: "#{name.gsub("partials/_", "partials/")}"
-  end
 end
