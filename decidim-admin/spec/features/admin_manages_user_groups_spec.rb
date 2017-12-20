3bed4edec2b994e3b5f70223b851c80320283cca
David Rodríguez <deivid.rodriguez@riseup.net>
2017-12-20 09:35:01 -0300

    Curate user group verification spec
    * Don't need that many objects.
    * Don't need match: :first since the link is unique.
    * Test the whole user flow. Admins won't go directly to this page but
      navigate to it.

--- decidim-admin/spec/features/admin_manages_user_groups_spec.rb
+++ decidim-admin/spec/features/admin_manages_user_groups_spec.rb
@@ -7,19 +7,21 @@ describe "Admin manages user groups", type: :feature do
 
   let!(:user) { create(:user, :admin, :confirmed, organization: organization) }
 
-  let!(:user_groups) { create_list(:user_group, 10, users: [create(:user, organization: organization)]) }
+  let!(:user_groups) { create_list(:user_group, 3, users: [create(:user, organization: organization)]) }
 
   let(:user_group) { user_groups.first }
 
   before do
     switch_to_host(organization.host)
     login_as user, scope: :user
-    visit decidim_admin.user_groups_path
+    visit decidim_admin.root_path
+    click_link "Users"
+    click_link "User groups"
   end
 
   it "verifies a user group" do
     within "tr[data-user-group-id=\"#{user_group.id}\"]" do
-      click_link "Verify", match: :first
+      click_link "Verify"
     end
 
     expect(page).to have_content("verified successfully")
@@ -31,7 +33,7 @@ describe "Admin manages user groups", type: :feature do
 
   it "reject a user group" do
     within "tr[data-user-group-id=\"#{user_group.id}\"]" do
-      click_link "Reject", match: :first
+      click_link "Reject"
     end
 
     expect(page).to have_content("rejected successfully")
