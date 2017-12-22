f34675a8ae349b458fda9328d9113b0e5e86c678
David Rodríguez <deivid.rodriguez@riseup.net>
2017-12-22 10:35:27 -0300

    Fix index on nicknames (#2417)
    * Add conditions on nicknames index
    
    * Nickname index should be scoped per organization

--- decidim-core/db/migrate/20171212103803_create_unique_nicknames.rb
+++ decidim-core/db/migrate/20171212103803_create_unique_nicknames.rb
@@ -14,10 +14,16 @@ class CreateUniqueNicknames < ActiveRecord::Migration[5.1]
       user.update!(nickname: User.nicknamize(user.name))
     end
 
-    add_index :decidim_users, :nickname, unique: true
+    add_index :decidim_users,
+              %w(nickname decidim_organization_id),
+              where: "(deleted_at IS NULL) AND (managed = 'f')",
+              name: "index_decidim_users_on_nickame_and_decidim_organization_id",
+              unique: true
   end
 
   def down
+    remove_index :decidim_users, %w(nickname decidim_organization_id)
+
     remove_column :decidim_users, :nickname
   end
 end
