414acb0005c3a0a6996ecf62916d8088a28f634c
David Rodríguez <deivid.rodriguez@riseup.net>
2017-12-22 09:34:39 -0300

    Nickname index should be scoped per organization

--- decidim-core/db/migrate/20171212103803_create_unique_nicknames.rb
+++ decidim-core/db/migrate/20171212103803_create_unique_nicknames.rb
@@ -15,12 +15,15 @@ class CreateUniqueNicknames < ActiveRecord::Migration[5.1]
     end
 
     add_index :decidim_users,
-              :nickname,
+              %w(nickname decidim_organization_id),
               where: "(deleted_at IS NULL) AND (managed = 'f')",
+              name: "index_decidim_users_on_nickame_and_decidim_organization_id",
               unique: true
   end
 
   def down
+    remove_index :decidim_users, %w(nickname decidim_organization_id)
+
     remove_column :decidim_users, :nickname
   end
 end
