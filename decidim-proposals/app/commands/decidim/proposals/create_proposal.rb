27fa313e227340e0196a9b207314f92cfa81be85
Oriol Gual <oriolgual@users.noreply.github.com>
2017-12-22 14:58:40 +0100

    Follow users and get notifications for their actions (#2401)
    * Update Changelog
    
    * Add button to follow users
    
    * Notify followers when creating a proposal
    
    * Send a notification to the comment's author followers too

--- decidim-proposals/app/commands/decidim/proposals/create_proposal.rb
+++ decidim-proposals/app/commands/decidim/proposals/create_proposal.rb
@@ -35,12 +35,24 @@ module Decidim
         transaction do
           create_proposal
           create_attachment if process_attachments?
+          send_notification
         end
 
         broadcast(:ok, proposal)
       end
 
       private
+
+      def send_notification
+        return if proposal.author.blank?
+
+        Decidim::EventsManager.publish(
+          event: "decidim.events.proposals.proposal_created",
+          event_class: Decidim::Proposals::CreateProposalEvent,
+          resource: proposal,
+          recipient_ids: proposal.author.followers.pluck(:id)
+        )
+      end
 
       attr_reader :form, :proposal, :attachment
 
--- decidim-proposals/spec/commands/decidim/proposals/create_proposal_spec.rb
+++ decidim-proposals/spec/commands/decidim/proposals/create_proposal_spec.rb
@@ -8,6 +8,49 @@ module Decidim
       let(:form_klass) { ProposalForm }
 
       it_behaves_like "create a proposal", true
+
+      describe "events" do
+        subject do
+          described_class.new(form, author)
+        end
+
+        let(:feature) { create(:proposal_feature) }
+        let(:organization) { feature.organization }
+        let(:form) do
+          form_klass.from_params(
+            form_params
+          ).with_context(
+            current_organization: organization,
+            current_feature: feature
+          )
+        end
+        let(:form_params) do
+          {
+            title: "A reasonable proposal title",
+            body: "A reasonable proposal body",
+            address: nil,
+            has_address: false,
+            attachment: nil,
+            user_group_id: nil
+          }
+        end
+        let(:author) { create(:user, organization: organization) }
+        let(:follower) { create(:user, organization: organization) }
+        let!(:follow) { create :follow, followable: author, user: follower }
+
+        it "notifies the change" do
+          expect(Decidim::EventsManager)
+            .to receive(:publish)
+            .with(
+              event: "decidim.events.proposals.proposal_created",
+              event_class: Decidim::Proposals::CreateProposalEvent,
+              resource: kind_of(Decidim::Proposals::Proposal),
+              recipient_ids: [follower.id]
+            )
+
+          subject.call
+        end
+      end
     end
   end
 end
