8074bcd82668ebcfcbcd24b3214e8b6e5dcb2fc4
Ludivine Coipel <coipel.ludivine@gmail.com>
2017-12-28 12:04:31 +0100

    add availables locales to avoid seeds errors

--- custom_decidim_app/config/initializers/decidim.rb
+++ custom_decidim_app/config/initializers/decidim.rb
@@ -7,7 +7,7 @@ Decidim.configure do |config|
 
   # Change these lines to set your preferred locales
   config.default_locale = :en
-  config.available_locales = [:en, :fr]
+  config.available_locales = [:en, :fr, :ca, :es]
 
   # Geocoder configuration
   # config.geocoder = {
