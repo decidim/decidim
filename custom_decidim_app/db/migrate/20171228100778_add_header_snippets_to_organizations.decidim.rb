8a19d1f07bf09d9791850e86b25c7a540ed6fdfd
Ludivine Coipel <coipel.ludivine@gmail.com>
2018-01-03 10:05:34 +0100

    Remove custom decidom app

--- custom_decidim_app/db/migrate/20171228100778_add_header_snippets_to_organizations.decidim.rb
+++
@@ -1,8 +0,0 @@
-# This migration comes from decidim (originally 20170913092351)
-# frozen_string_literal: true
-
-class AddHeaderSnippetsToOrganizations < ActiveRecord::Migration[5.1]
-  def change
-    add_column :decidim_organizations, :header_snippets, :text
-  end
-end
