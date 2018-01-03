60348c69ab25599463747ce343407947d4e1e338
Ludivine Coipel <coipel.ludivine@gmail.com>
2018-01-03 15:20:55 +0100

    Edit config files for development mailer + edit front mailer

--- development_app/config/environments/development.rb
+++ development_app/config/environments/development.rb
@@ -28,8 +28,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :letter_opener_web
-  config.action_mailer.default_url_options = { port: 3000 }
+  config.action_mailer.delivery_method = :sendmail
+  # config.action_mailer.default_url_options = { port: 3000 }
 
 
   config.action_mailer.perform_caching = false
