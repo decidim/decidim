19eff03b9105026c66a8d2ce48566ce2fe8fc4d9
Agustí B.R <agusti.br@coditramuntana.com>
2018-01-09 12:37:06 +0100

    add index to decidim_proposals_proposals published_at

--- decidim-proposals/db/migrate/20171220084719_add_published_at_to_proposals.rb
+++ decidim-proposals/db/migrate/20171220084719_add_published_at_to_proposals.rb
@@ -2,6 +2,6 @@
 
 class AddPublishedAtToProposals < ActiveRecord::Migration[5.1]
   def change
-    add_column :decidim_proposals_proposals, :published_at, :datetime
+    add_column :decidim_proposals_proposals, :published_at, :datetime, index: true
   end
 end
