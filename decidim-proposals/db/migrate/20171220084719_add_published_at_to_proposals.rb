792eb5a765f6f3e750bddae8a10e0936f7037413
Agustí B.R <agusti.br@coditramuntana.com>
2018-01-09 13:15:56 +0100

    Add published_at value to proposal in migration

--- decidim-proposals/db/migrate/20171220084719_add_published_at_to_proposals.rb
+++ decidim-proposals/db/migrate/20171220084719_add_published_at_to_proposals.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class AddPublishedAtToProposals < ActiveRecord::Migration[5.1]
-  def change
+  def up
     add_column :decidim_proposals_proposals, :published_at, :datetime, index: true
+    Decidim::Proposals::Proposal.update_all("published_at = updated_at")
+  end
+  def down
+    remove_column :decidim_proposals_proposals, :published_at
   end
 end
