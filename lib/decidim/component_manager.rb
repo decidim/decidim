0a61d0355512a5d6e01fa27d58c5797953c8b90f
David Rodríguez <deivid.rodriguez@riseup.net>
2018-01-12 05:07:45 -0300

    Extract a component manager class (#2427)
    * Remove unused .dockerignore
    
    * Extract Rake utils to a module
    
    * Refactor Rakefile
    
    * Make test_all more similar to Circle CI setup
    
    * Remove docker_development_app generator
    
    This generator was not working as expected since it would use th
    decidim's code inside codegram's image, so development changes would not
    be picked app by the development application.
    
    * Move common logic to a component manager class
    
    * Switch folder back after dummy app generation
    
    Otherwise running things like `rake test_app test_all` fails.
    
    * Rename decidim-design
    
    This is not an engine/component, so component manager tasks fail on it.
    Renaming also helps clarifying this.
    
    * Add docs for `ComponentManager` class

---
+++ lib/decidim/component_manager.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+module Decidim
+  #
+  # Handles a decidim components.
+  #
+  # Allows to perform custom operations on all plugins in a given folder, or on
+  # specific plugin folders.
+  #
+  # Potential operations are:
+  #
+  # * Running custom commands, via the `run` method, such as releasing it, or
+  #   installing it locally.
+  #
+  # * Updating version files from the main `.decidim-version` file in the root
+  #   of the repository.
+  #
+  class ComponentManager
+    def initialize(dir)
+      @dir = File.expand_path(dir)
+    end
+
+    def run(command, out: STDOUT)
+      command = command.gsub("%version", version).gsub("%name", name)
+      status = system(command, out: out)
+      abort unless status || ENV["FAIL_FAST"] == "false"
+    end
+
+    def replace_version
+      self.class.replace_file(
+        "lib/#{name.tr("-", "/")}/version.rb",
+        /def self\.version(\s*)"[^"]*"/,
+        "def self.version\\1\"#{version}\""
+      )
+    end
+
+    class << self
+      def replace_versions
+        replace_file(
+          "package.json",
+          /^  "version": "[^"]*"/,
+          "  \"version\": \"#{version.gsub(/\.pre/, "-pre")}\""
+        )
+
+        in_all_dirs do |dir|
+          new(dir).replace_version
+        end
+      end
+
+      def run_all(command, out: STDOUT, include_root: true)
+        in_all_dirs(include_root: include_root) do |dir|
+          new(dir).run(command, out: out)
+        end
+      end
+
+      def version
+        File.read(version_file).strip
+      end
+
+      def version_file
+        File.expand_path(File.join("..", "..", ".decidim-version"), __dir__)
+      end
+
+      def replace_file(name, regexp, replacement)
+        new_content = File.read(name).gsub(regexp, replacement)
+
+        File.open(name, "w") { |f| f.write(new_content) }
+      end
+
+      private
+
+      def all_dirs(include_root: true)
+        Dir.glob(include_root ? "{decidim-*,.}" : "decidim-*")
+           .select { |f| File.directory?(f) }
+      end
+
+      def in_all_dirs(include_root: true)
+        all_dirs(include_root: include_root).each do |dir|
+          Dir.chdir(dir) { yield(dir) }
+        end
+      end
+    end
+
+    private
+
+    def name
+      File.basename(@dir)
+    end
+
+    def short_name
+      name.gsub(/decidim-/, "")
+    end
+
+    def version
+      self.class.version
+    end
+  end
+end
