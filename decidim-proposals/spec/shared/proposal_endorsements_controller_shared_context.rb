36e2139445434888528a7087174bce36b67f44a9
oliver <oliver.hv@coditramuntana.com>
2018-01-12 12:51:35 +0100

    [REFACTOR|TEST] Split proposal_endorsements_controller_spec in two and extract a shared context.

---
+++ decidim-proposals/spec/shared/proposal_endorsements_controller_shared_context.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+RSpec.shared_context "when in a proposal" do
+  routes { Decidim::Proposals::Engine.routes }
+
+  let(:proposal) { create(:proposal, feature: feature) }
+  let(:user) { create(:user, :confirmed, organization: feature.organization) }
+  let(:params) do
+    {
+      proposal_id: proposal.id,
+      feature_id: feature.id,
+      participatory_process_slug: feature.participatory_space.slug
+    }
+  end
+
+  before do
+    request.env["decidim.current_organization"] = feature.organization
+    request.env["decidim.current_feature"] = feature
+    sign_in user
+  end
+end
