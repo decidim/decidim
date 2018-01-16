6ab0a29fc6d63a51555701d3b38051b4d5861549
Marc Riera Casals <mrc2407@gmail.com>
2018-01-16 11:08:37 +0100

    Unify test files

---
+++ decidim-debates/lib/decidim/debates/test/factories.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :debate, class: Decidim::Debates::Debate do
+    title { Decidim::Faker::Localized.sentence(3) }
+    description { Decidim::Faker::Localized.wrapped("<p>", "</p>") { Decidim::Faker::Localized.sentence(4) } }
+    instructions { Decidim::Faker::Localized.wrapped("<p>", "</p>") { Decidim::Faker::Localized.sentence(4) } }
+    start_time { 1.day.from_now }
+    end_time { start_time.advance(hours: 2) }
+    feature { build(:feature, manifest_name: "debates") }
+  end
+
+  factory :debates_feature, parent: :feature do
+    name { Decidim::Features::Namer.new(participatory_space.organization.available_locales, :debates).i18n_name }
+    manifest_name :debates
+    participatory_space { create(:participatory_process, :with_steps, organization: organization) }
+    settings do
+      {
+        comments_enabled: true
+      }
+    end
+  end
+end
--- decidim-debates/spec/spec_helper.rb
+++ decidim-debates/spec/spec_helper.rb
@@ -2,18 +2,8 @@
 
 require "decidim/dev"
 
-ENV["ENGINE_NAME"] = File.dirname(File.dirname(__FILE__)).split("/").last
+ENV["ENGINE_NAME"] = File.dirname(__dir__).split("/").last
 
 Decidim::Dev.dummy_app_path = File.expand_path(File.join("..", "spec", "decidim_dummy_app"))
 
 require "decidim/dev/test/base_spec_helper"
-
-RSpec.configure do |config|
-  config.before(:each) do
-    I18n.available_locales = %i(en ca es)
-    I18n.default_locale = :en
-    I18n.locale = :en
-    Decidim.available_locales = %i(en ca es)
-    Decidim.default_locale = :en
-  end
-end
