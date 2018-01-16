0889c3eba9aba46e9f039d141b51705ca29c3e3e
Agustí B.R <agusti.br@coditramuntana.com>
2018-01-16 11:21:27 +0100

    fix proposal feature comments spec

--- decidim-proposals/spec/features/comments_spec.rb
+++ decidim-proposals/spec/features/comments_spec.rb
@@ -5,7 +5,7 @@ require "spec_helper"
 describe "Comments", type: :feature do
   let!(:feature) { create(:proposal_feature, organization: organization) }
   let!(:author) { create(:user, :confirmed, organization: organization) }
-  let!(:commentable) { create(:proposal, feature: feature, author: author) }
+  let!(:commentable) { create(:proposal, :published, feature: feature, author: author) }
 
   let(:resource_path) { resource_locator(commentable).path }
 
