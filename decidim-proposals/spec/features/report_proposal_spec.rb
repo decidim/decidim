a79b0cac3e1d33a5e368320dc459024f99d4aa5f
Agustí B.R <agusti.br@coditramuntana.com>
2018-01-16 11:24:56 +0100

    fix proposal feature report spec

--- decidim-proposals/spec/features/report_proposal_spec.rb
+++ decidim-proposals/spec/features/report_proposal_spec.rb
@@ -6,7 +6,7 @@ describe "Report Proposal", type: :feature do
   include_context "with a feature"
 
   let(:manifest_name) { "proposals" }
-  let!(:proposals) { create_list(:proposal, 3, feature: feature) }
+  let!(:proposals) { create_list(:proposal, 3, :published, feature: feature) }
   let(:reportable) { proposals.first }
   let(:reportable_path) { resource_locator(reportable).path }
   let!(:user) { create :user, :confirmed, organization: organization }
