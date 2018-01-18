59401af401fdd582c119813e575b7f8be038128c
Marc Riera Casals <mrc2407@gmail.com>
2018-01-18 10:19:12 +0100

    Add current user ability class

---
+++ decidim-debates/app/models/decidim/debates/abilities/current_user_ability.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Debates
+    module Abilities
+      # Defines the abilities related to debates for a logged in user.
+      # Intended to be used with `cancancan`.
+      class CurrentUserAbility
+        include CanCan::Ability
+
+        attr_reader :user, :context
+
+        def initialize(user, context)
+          return unless user
+
+          @user = user
+          @context = context
+
+          can :create, Debate if authorized?(:create) && creation_enabled?
+        end
+
+        private
+
+        def authorized?(action)
+          return unless feature
+
+          ActionAuthorizer.new(user, feature, action).authorize.ok?
+        end
+
+        def creation_enabled?
+          true
+        end
+
+        def current_settings
+          context.fetch(:current_settings, nil)
+        end
+
+        def feature
+          feature = context.fetch(:current_feature, nil)
+          return nil unless feature && feature.manifest.name == :debates
+
+          feature
+        end
+      end
+    end
+  end
+end
--- decidim-debates/lib/decidim/debates/engine.rb
+++ decidim-debates/lib/decidim/debates/engine.rb
@@ -15,6 +15,12 @@ module Decidim
         resources :debates, only: [:index, :show]
         root to: "debates#index"
       end
+
+      initializer "decidim_debates.inject_abilities_to_user" do |_app|
+        Decidim.configure do |config|
+          config.abilities += ["Decidim::Debates::Abilities::CurrentUserAbility"]
+        end
+      end
     end
   end
 end
