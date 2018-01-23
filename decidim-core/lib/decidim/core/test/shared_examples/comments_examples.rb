653c483ad25c06df8d10f3717c9904bf0456cfad
Marc Riera Casals <mrc2407@gmail.com>
2018-01-23 16:17:12 +0100

    Fix home stats

--- decidim-core/lib/decidim/core/test/shared_examples/comments_examples.rb
+++ decidim-core/lib/decidim/core/test/shared_examples/comments_examples.rb
@@ -101,7 +101,7 @@ shared_examples "comments" do
 
         within "#comments #comment_#{comment.id}" do
           click_button "Reply"
-          fill_in "add-comment-Decidim::Comments::Comment-#{comment.id}", with: "This is a reply"
+          fill_in "#add-comment-Decidim::Comments::Comment-#{comment.id}", with: "This is a reply"
           click_button "Send"
         end
       end
