a460ea56c310d904d268bc7daf20f3294a0c2c9a
Oriol Gual <oriol.gual@gmail.com>
2018-01-24 16:42:57 +0100

    Use ExtendedEvent in decidim-proposals events

--- decidim-proposals/spec/events/decidim/proposals/accepted_proposal_event_spec.rb
+++ decidim-proposals/spec/events/decidim/proposals/accepted_proposal_event_spec.rb
@@ -3,27 +3,11 @@
 require "spec_helper"
 
 describe Decidim::Proposals::AcceptedProposalEvent do
-  subject do
-    described_class.new(resource: proposal, event_name: event_name, user: user, extra: {})
-  end
+  let(:resource) { create :proposal }
+  let(:event_name) { "decidim.events.proposals.proposal_accepted" }
 
-  let(:organization) { proposal.organization }
-  let(:proposal) { create :proposal }
-  let(:event_name) { "decidim.events.proposals.proposal_created" }
-  let(:user) { create :user, organization: organization }
-  let(:resource_path) { resource_locator(proposal).path }
-
-  describe "types" do
-    subject { described_class }
-
-    it "supports notifications" do
-      expect(subject.types).to include :notification
-    end
-
-    it "supports emails" do
-      expect(subject.types).to include :email
-    end
-  end
+  include_context "extended event"
+  it_behaves_like "an extended event"
 
   describe "email_subject" do
     it "is generated correctly" do
@@ -34,21 +18,21 @@ describe Decidim::Proposals::AcceptedProposalEvent do
   describe "email_intro" do
     it "is generated correctly" do
       expect(subject.email_intro)
-        .to eq("The proposal \"#{proposal.title}\" has been accepted. You can read the answer in this page:")
+        .to eq("The proposal \"#{resource.title}\" has been accepted. You can read the answer in this page:")
     end
   end
 
   describe "email_outro" do
     it "is generated correctly" do
       expect(subject.email_outro)
-        .to eq("You have received this notification because you are following \"#{proposal.title}\". You can unfollow it from the previous link.")
+        .to eq("You have received this notification because you are following \"#{resource.title}\". You can unfollow it from the previous link.")
     end
   end
 
   describe "notification_title" do
     it "is generated correctly" do
       expect(subject.notification_title)
-        .to include("The <a href=\"#{resource_path}\">#{proposal.title}</a> proposal has been accepted")
+        .to include("The <a href=\"#{resource_path}\">#{resource.title}</a> proposal has been accepted")
     end
   end
 end
--- decidim-proposals/spec/events/decidim/proposals/create_proposal_event_spec.rb
+++ decidim-proposals/spec/events/decidim/proposals/create_proposal_event_spec.rb
@@ -2,57 +2,44 @@
 
 require "spec_helper"
 
-describe Decidim::Proposals::CreateProposalEvent do
-  subject do
-    described_class.new(resource: proposal, event_name: event_name, user: user, extra: {})
-  end
+module Decidim
+  module Proposals
+    describe CreateProposalEvent do
+      let(:resource) { create :proposal }
+      let(:event_name) { "decidim.events.proposals.proposal_created" }
 
-  let(:organization) { proposal.organization }
-  let(:proposal) { create :proposal }
-  let(:proposal_author) { proposal.author }
-  let(:event_name) { "decidim.events.proposals.proposal_created" }
-  let(:user) { create :user, organization: organization }
-  let(:resource_path) { resource_locator(proposal).path }
+      include_context "extended event"
+      it_behaves_like "an extended event"
 
-  describe "types" do
-    subject { described_class }
+      describe "email_subject" do
+        it "is generated correctly" do
+          expect(subject.email_subject).to eq("New proposal by @#{author.nickname}")
+        end
+      end
 
-    it "supports notifications" do
-      expect(subject.types).to include :notification
-    end
+      describe "email_intro" do
+        it "is generated correctly" do
+          expect(subject.email_intro)
+            .to eq("#{author.name} @#{author.nickname}, who you are following, has created a new proposal, check it out and contribute:")
+        end
+      end
 
-    it "supports emails" do
-      expect(subject.types).to include :email
-    end
-  end
+      describe "email_outro" do
+        it "is generated correctly" do
+          expect(subject.email_outro)
+            .to eq("You have received this notification because you are following @#{author.nickname}. You can stop receiving notifications following the previous link.")
+        end
+      end
 
-  describe "email_subject" do
-    it "is generated correctly" do
-      expect(subject.email_subject).to eq("New proposal by @#{proposal_author.nickname}")
-    end
-  end
+      describe "notification_title" do
+        it "is generated correctly" do
+          expect(subject.notification_title)
+            .to include("The <a href=\"#{resource_path}\">#{resource.title}</a> proposal was created by ")
 
-  describe "email_intro" do
-    it "is generated correctly" do
-      expect(subject.email_intro)
-        .to eq("Hi,\n#{proposal_author.name} @#{proposal_author.nickname}, who you are following, has created a new proposal, check it out and contribute:")
-    end
-  end
-
-  describe "email_outro" do
-    it "is generated correctly" do
-      expect(subject.email_outro)
-        .to eq("You have received this notification because you are following @#{proposal_author.nickname}. You can stop receiving notifications following the previous link.")
-    end
-  end
-
-  describe "notification_title" do
-    it "is generated correctly" do
-      expect(subject.notification_title)
-        .to include("The <a href=\"#{resource_path}\">#{proposal.title}</a> proposal was created by ")
-
-      expect(subject.notification_title)
-        .to include("<a href=\"/profiles/#{proposal_author.nickname}\">#{proposal_author.name} @#{proposal_author.nickname}</a>.")
+          expect(subject.notification_title)
+            .to include("<a href=\"/profiles/#{author.nickname}\">#{author.name} @#{author.nickname}</a>.")
+        end
+      end
     end
   end
 end
