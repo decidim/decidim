f2c4c8f20605810491876411d7765c5d7bdf9155
Agustí B.R <agusti.br@coditramuntana.com>
2018-01-24 13:20:07 +0100

    category: track changes of categorisations

--- decidim-core/lib/decidim/has_category.rb
+++ decidim-core/lib/decidim/has_category.rb
@@ -13,6 +13,12 @@ module Decidim
 
       validate :category_belongs_to_organization
 
+      def previous_category
+        return if self.categorization.versions.count <= 1
+        category_id = self.categorization.paper_trail.previous_version.decidim_category_id
+        Decidim::Category.find_by_id category_id
+      end
+
       private
 
       def category_belongs_to_organization
