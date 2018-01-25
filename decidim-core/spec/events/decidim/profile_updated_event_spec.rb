c399117a5b212545e544ad19f575235fe2eff3e1
Oriol Gual <oriol.gual@gmail.com>
2018-01-25 15:03:33 +0100

    Use ExtendedEvent in decidim-admin events

--- decidim-core/spec/events/decidim/profile_updated_event_spec.rb
+++ decidim-core/spec/events/decidim/profile_updated_event_spec.rb
@@ -3,50 +3,37 @@
 require "spec_helper"
 
 describe Decidim::ProfileUpdatedEvent do
-  subject do
-    described_class.new(resource: user, event_name: event_name, user: follower, extra: {})
-  end
+  include_context "extended event"
 
-  let(:organization) { follower.organization }
-  let(:follower) { create :user }
   let(:event_name) { "decidim.events.users.profile_updated" }
-  let(:user) { create :user, organization: organization }
+  let(:resource) { create :user }
+  let(:author) { resource }
 
-  describe "types" do
-    subject { described_class }
-
-    it "supports notifications" do
-      expect(subject.types).to include :notification
-    end
-
-    it "supports emails" do
-      expect(subject.types).to include :email
-    end
-  end
+  it_behaves_like "an extended event"
 
   describe "email_subject" do
     it "is generated correctly" do
-      expect(subject.email_subject).to eq("@#{user.nickname} updated their profile")
+      expect(subject.email_subject).to eq("@#{resource.nickname} updated their profile")
     end
   end
 
   describe "email_intro" do
     it "is generated correctly" do
       expect(subject.email_intro)
-        .to eq("The <a href=\"/profiles/#{user.nickname}\">profile page</a> of #{user.name} (@#{user.nickname}), who you are following, has been updated.")
+        .to eq("The <a href=\"#{author_presenter.profile_url}\">profile page</a> of #{resource.name} (@#{resource.nickname}), who you are following, has been updated.")
     end
   end
 
   describe "email_outro" do
     it "is generated correctly" do
       expect(subject.email_outro)
-        .to eq("You have received this notification because you are following @#{user.nickname}. You can stop receiving notifications following the previous link.")
+        .to eq("You have received this notification because you are following @#{resource.nickname}. You can stop receiving notifications following the previous link.")
     end
   end
 
   describe "notification_title" do
     it "is generated correctly" do
-      expect(subject.notification_title).to include("<a href=\"/profiles/#{user.nickname}\">profile page</a>")
+      expect(subject.notification_title).to include("<a href=\"#{author_presenter.profile_path}\">profile page</a>")
     end
   end
 end
