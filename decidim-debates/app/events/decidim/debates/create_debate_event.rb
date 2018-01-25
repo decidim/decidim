c703842e46179ae36ea63465f571c9f866c06f0b
Oriol Gual <oriol.gual@gmail.com>
2018-01-25 16:09:53 +0100

    Use ExtendedEvent in decidim-debates events

--- decidim-debates/app/events/decidim/debates/create_debate_event.rb
+++ decidim-debates/app/events/decidim/debates/create_debate_event.rb
@@ -9,52 +9,12 @@ module Decidim
     #          author
     # "participatory_space" - The event is being sento to the followers
     #                         of the event's participatory space.
-    class CreateDebateEvent < Decidim::Events::BaseEvent
-      include Decidim::Events::EmailEvent
-      include Decidim::Events::NotificationEvent
+    class CreateDebateEvent < Decidim::Events::ExtendedEvent
+      include Decidim::Events::AuthorEvent
 
-      def email_subject
-        I18n.t(
-          "email_subject",
-          scope: i18n_scope,
-          space_title: space_title,
-          author_nickname: author.nickname
-        )
-      end
+      i18n_attributes :space_title, :space_path
 
-      def email_intro
-        I18n.t(
-          "email_intro",
-          scope: i18n_scope,
-          resource_title: resource_title,
-          space_title: space_title,
-          author_nickname: author.nickname,
-          author_name: author.name
-        )
-      end
-
-      def email_outro
-        I18n.t(
-          "email_outro",
-          scope: i18n_scope,
-          space_title: space_title,
-          author_nickname: author.nickname
-        )
-      end
-
-      def notification_title
-        I18n.t(
-          "notification_title",
-          scope: i18n_scope,
-          resource_title: resource_title,
-          resource_path: resource_path,
-          space_title: space_title,
-          space_path: space_path,
-          author_nickname: author.nickname,
-          author_name: author.name,
-          author_path: author.profile_path
-        ).html_safe
-      end
+      delegate :author, to: :resource
 
       private
 
@@ -70,15 +30,11 @@ module Decidim
         space.title.is_a?(Hash) ? space.title[I18n.locale.to_s] : space.title
       end
 
-      def author
-        @author ||= Decidim::UserPresenter.new(resource.author)
-      end
-
       def i18n_scope
         @scope ||= if extra[:type].to_s == "user"
-                     "decidim.debates.events.create_debate_event.user_followers"
+                     "decidim.events.debates.create_debate_event.user_followers"
                    else
-                     "decidim.debates.events.create_debate_event.space_followers"
+                     "decidim.events.debates.create_debate_event.space_followers"
                    end
       end
     end
