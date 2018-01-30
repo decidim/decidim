5f908364d510c5b5c7b96bbc764cb9ef421e1c1d
Marc Riera Casals <mrc2407@gmail.com>
2018-01-30 14:35:31 +0100

    Add ActionLogger class to create logs

---
+++ decidim-core/app/services/decidim/action_logger.rb
@@ -0,0 +1,97 @@
+# frozen_string_literal: true
+
+module Decidim
+  # Use this class to log actions by any user.
+  #
+  # Usage:
+  #
+  #    ActionLogger.log(:create, user, new_proposal, extra_data)
+  class ActionLogger
+    # Public: Logs the given `action` by the given `user` on the given `resource`.
+    # Delegates the work to the instance method.
+    #
+    # action - a String representing the name of the action
+    # user - the Decidim::User that performed the action
+    # resource - the resource onn which the action was performed
+    # resource_extra - a Hash with resource_extra info to be recorded
+    #
+    # Returns the newly created `Decidim::ActionLog` resource.
+    def self.log(action, user, resource, resource_extra = {})
+      new(action, user, resource, resource_extra).log!
+    end
+
+    # Public: Initializes the instance.
+    #
+    # action - a String representing the name of the action
+    # user - the Decidim::User that performed the action
+    # resource - the resource onn which the action was performed
+    # resource_extra - a Hash with resource_extra info to be recorded
+    def initialize(action, user, resource, resource_extra = {})
+      @action = action
+      @user = user
+      @resource = resource
+      @resource_extra = resource_extra
+    end
+
+    # Public: Logs the given `action` by the given `user` on the given
+    # `resource`.
+    #
+    # Returns the newly created `Decidim::ActionLog` resource.
+    def log!
+      Decidim::ActionLog.create!(
+        user: user,
+        organization: organization,
+        action: action,
+        resource: resource,
+        participatory_space: participatory_space,
+        feature: feature,
+        extra: extra_data
+      )
+    end
+
+    private
+
+    attr_reader :action, :user, :resource, :resource_extra
+
+    def organization
+      user.organization
+    end
+
+    def feature
+      resource.feature if resource.respond_to?(:feature)
+    end
+
+    def participatory_space
+      feature.participatory_space if resource.respond_to?(:participatory_space)
+    end
+
+    def get_title(resource)
+      resource.try(:title) || resource.try(:name)
+    end
+
+    def participatory_space_manifest_name
+      participatory_space.try(:class).try(:participatory_space_manifest).try(:name)
+    end
+
+    def extra_data
+      {
+        feature: {
+          manifest_name: feature.try(:manifest_name),
+          title: get_title(feature)
+        }.compact,
+        participatory_space: {
+          manifest_name: participatory_space_manifest_name,
+          title: get_title(participatory_space)
+        }.compact,
+        resource: {
+          title: get_title(resource)
+        }.compact,
+        user: {
+          ip: user.current_sign_in_ip,
+          name: user.name,
+          nickname: user.nickname
+        }.compact
+      }.deep_merge(resource_extra)
+    end
+  end
+end
