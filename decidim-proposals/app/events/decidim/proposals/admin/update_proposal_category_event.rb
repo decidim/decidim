15ee3867eea189631133b8105325730217f04887
Agustí B.R <agusti.br@coditramuntana.com>
2018-01-31 15:35:19 +0100

    using `SimpleEvent` and `AuthorEvent`on `UpdateProposalCategoryEvent`

--- decidim-proposals/app/events/decidim/proposals/admin/update_proposal_category_event.rb
+++ decidim-proposals/app/events/decidim/proposals/admin/update_proposal_category_event.rb
@@ -3,45 +3,8 @@
 module Decidim
   module Proposals
     module Admin
-      class UpdateProposalCategoryEvent < Decidim::Events::BaseEvent
-        include Decidim::Events::EmailEvent
-        include Decidim::Events::NotificationEvent
-
-        def email_subject
-          I18n.t(
-            "decidim.proposals.events.update_proposal_category_event.email_subject",
-            resource_title: resource_title
-          )
-        end
-
-        def email_intro
-          I18n.t(
-            "decidim.proposals.events.update_proposal_category_event.email_intro",
-            resource_title: resource_title,
-            author_nickname: author.nickname,
-            author_name: author.name
-          )
-        end
-
-        def email_outro
-          I18n.t(
-            "decidim.proposals.events.update_proposal_category_event.email_outro"
-          )
-        end
-
-        def notification_title
-          I18n.t(
-            "decidim.proposals.events.update_proposal_category_event.notification_title",
-            resource_title: resource_title,
-            resource_path: resource_path
-          ).html_safe
-        end
-
-        private
-
-        def author
-          @author ||= Decidim::UserPresenter.new(resource.author)
-        end
+      class UpdateProposalCategoryEvent < Decidim::Events::SimpleEvent
+        include Decidim::Events::AuthorEvent
       end
     end
   end
