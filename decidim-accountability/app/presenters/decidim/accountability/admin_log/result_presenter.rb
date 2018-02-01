c93940f04295751332acce9d72b4a16bf6064d38
Marc Riera Casals <mrc2407@gmail.com>
2018-02-01 14:45:57 +0100

    Improve log data presentation

--- decidim-accountability/app/presenters/decidim/accountability/admin_log/result_presenter.rb
+++ decidim-accountability/app/presenters/decidim/accountability/admin_log/result_presenter.rb
@@ -30,22 +30,19 @@ module Decidim
         # view_helpers - An object holding the view helpers at the render time.
         #   Most probably should come automatically from the views.
         def render
-          h.content_tag(:li) do
-            [
-              action_log.extra["user"]["name"],
-              " (@",
-              action_log.extra["user"]["nickname"],
-              ")",
-              " ",
-              action_log.action,
-              " ",
-              action_log.extra["resource"]["title"]["ca"],
-              " (",
-              action_log.resource_type,
-              " ",
-              action_log.resource_id,
-              ")"
-            ].join("")
+          case action
+          when "create"
+            h.content_tag(:li) do
+              I18n.t(
+                "decidim.accountability.admin_log.result.create",
+                user_name: render_user,
+                result_name: render_resource,
+                space_name: render_space
+              ).html_safe +
+                " (#{h.localize(action_log.created_at, format: :decidim_short)})"
+            end
+          when "update"
+            "TBD"
           end
         end
 
@@ -53,6 +50,47 @@ module Decidim
 
         attr_reader :action_log, :view_helpers
         alias h view_helpers
+
+        delegate :action, :user, :resource, :feature, :participatory_space, to: :action_log
+
+        def render_space
+          return render_space_name if participatory_space.blank?
+
+          h.link_to(render_space_name, space_path)
+        end
+
+        def render_resource
+          return render_resource_name if resource.blank?
+
+          h.link_to(render_resource_name, resource_path)
+        end
+
+        def space_path
+          Decidim::ResourceLocatorPresenter.new(participatory_space).path
+        end
+
+        def resource_path
+          Decidim::ResourceLocatorPresenter.new(resource).path
+        end
+
+        def render_space_name
+          action_log.extra["participatory_space"]["title"]["ca"]
+        end
+
+        def render_resource_name
+          action_log.extra["resource"]["title"]["ca"]
+        end
+
+        def render_user
+          return render_user_name if user.blank?
+          h.link_to(render_user_name, h.decidim.profile_path(action_log.extra["user"]["nickname"]))
+        end
+
+        def render_user_name
+          name = action_log.extra["user"]["name"]
+          nickname = action_log.extra["user"]["nickname"]
+          "#{name} @#{nickname}"
+        end
       end
     end
   end
