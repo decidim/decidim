34aa749d1fe5cb68705224e6c833696796138c58
Isaac Massot <isaac.mg@coditramuntana.com>
2018-02-01 08:37:09 +0100

    fix migration name

--- decidim-assemblies/db/migrate/20180131083235_create_assembly_private_users.rb
+++ decidim-assemblies/db/migrate/20180131083235_create_assembly_private_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateParticipatoryProcessPrivateUsers < ActiveRecord::Migration[5.1]
+class CreateAssemblyPrivateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :decidim_assembly_private_users do |t|
       t.references :decidim_user, index: { name: "index_decidim_assemblies_users_on_decidim_user_id" }
