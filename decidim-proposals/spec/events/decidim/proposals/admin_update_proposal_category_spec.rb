6971daa7a8725412da9dd1333cd44b0718e597f6
Agustí B.R <agusti.br@coditramuntana.com>
2018-02-01 17:49:31 +0100

    refactor spect to use shared examples and context for simple events

--- decidim-proposals/spec/events/decidim/proposals/admin_update_proposal_category_spec.rb
+++ decidim-proposals/spec/events/decidim/proposals/admin_update_proposal_category_spec.rb
@@ -3,54 +3,36 @@
 require "spec_helper"
 
 describe Decidim::Proposals::Admin::UpdateProposalCategoryEvent do
-  subject do
-    described_class.new(resource: proposal, event_name: event_name, user: user, extra: {})
-  end
-
-  let(:organization) { proposal.organization }
-  let(:proposal) { create :proposal }
-  let(:proposal_author) { proposal.author }
+  let(:resource) { create :proposal }
   let(:event_name) { "decidim.events.proposals.proposal_update_category" }
-  let(:user) { create :user, organization: organization }
-  let(:resource_path) { resource_locator(proposal).path }
 
-  describe "types" do
-    subject { described_class }
-
-    it "supports notifications" do
-      expect(subject.types).to include :notification
-    end
-
-    it "supports emails" do
-      expect(subject.types).to include :email
-    end
-  end
+  include_context "simple event"
+  it_behaves_like "an simple event"
 
   describe "email_subject" do
     it "is generated correctly" do
-      expect(subject.email_subject)
-      .to eq("The #{proposal.title} proposal category has been updated")
+      expect(subject.email_subject).to eq("The #{resource.title} proposal category has been updated")
     end
   end
 
   describe "email_intro" do
     it "is generated correctly" do
       expect(subject.email_intro)
-        .to eq("Hi,\n#{proposal_author.name} @#{proposal_author.nickname}, an admin has updated your proposal #{proposal.title}, check it out:")
+        .to eq("An admin has updated the category of your proposal \"#{resource.title}\", check it out:")
     end
   end
 
   describe "email_outro" do
     it "is generated correctly" do
       expect(subject.email_outro)
-        .to eq("You have received this notification because you are the author of the proposal. You can stop receiving notifications following the previous link.")
+        .to eq("You have received this notification because you are the author of the proposal.")
     end
   end
 
   describe "notification_title" do
     it "is generated correctly" do
       expect(subject.notification_title)
-        .to include("The <a href=\"#{resource_path}\">#{proposal.title}</a> proposal has been updated by an admin.")
+        .to include("The <a href=\"#{resource_path}\">#{resource.title}</a> proposal category has been updated by an admin.")
     end
   end
 end
