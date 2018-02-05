22a75d35a64ca3658b89504072ffb0021622fa97
Marc Riera Casals <mrc2407@gmail.com>
2018-02-05 11:09:45 +0100

    Add admin log menu in the admin section

--- decidim-admin/lib/decidim/admin/engine.rb
+++ decidim-admin/lib/decidim/admin/engine.rb
@@ -76,6 +76,13 @@ module Decidim
                     position: 7,
                     active: [%w(decidim/admin/organization decidim/admin/scopes decidim/admin/scope_types), []],
                     if: can?(:read, current_organization)
+
+          menu.item I18n.t("menu.admin_log", scope: "decidim.admin"),
+                    decidim_admin.root_path,
+                    icon_name: "dashboard",
+                    position: 10,
+                    active: [%w(decidim/admin/admin_log), []],
+                    if: can?(:read, :admin_log)
         end
       end
     end
