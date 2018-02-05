bb351de2408573091f22db0aaaa796e9a4cc7feb
moustachu <git@moustachu.net>
2018-02-05 09:14:34 +0100

    fixing migrations

--- decidim-comments/db/migrate/20171128091519_add_upstream_moderation_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddUpstreamModerationToComments < ActiveRecord::Migration[5.1]
-  def change
-    add_column :decidim_moderations, :upstream_moderation, :string, default: "unmoderate"
-  end
-end
