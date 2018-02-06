6b55ee8bcd137d0802f539ab03378a63e854f65b
Oriol Gual <oriol.gual@gmail.com>
2018-02-06 16:53:33 +0100

    Fix ProcessStepActivatedEvent urls

--- decidim-admin/app/events/decidim/participatory_process_step_activated_event.rb
+++ decidim-admin/app/events/decidim/participatory_process_step_activated_event.rb
@@ -5,15 +5,19 @@ module Decidim
     include Rails.application.routes.mounted_helpers
 
     def resource_path
-      @resource_path ||= decidim_participatory_processes.participatory_process_participatory_process_steps_path(participatory_space)
+      @resource_path ||= decidim_participatory_processes.participatory_process_participatory_process_steps_path(participatory_process_slug: participatory_space.slug)
     end
 
     def resource_url
       @resource_url ||= decidim_participatory_processes
                         .participatory_process_participatory_process_steps_url(
-                          participatory_space,
+                          participatory_process_slug: participatory_space.slug,
                           host: participatory_space.organization.host
                         )
+    end
+
+    def participatory_space
+      resource.participatory_process
     end
   end
 end
