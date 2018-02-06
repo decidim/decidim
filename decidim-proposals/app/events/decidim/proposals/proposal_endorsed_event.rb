768158e3bab15fbb4613830b086a2976e5317f59
Oliver Valls <oliver.hv@coditramuntana.com>
2018-02-06 13:47:58 +0100

    [REFACTOR] use the shared examples for proposal_endorsed_event_spec.

--- decidim-proposals/app/events/decidim/proposals/proposal_endorsed_event.rb
+++ decidim-proposals/app/events/decidim/proposals/proposal_endorsed_event.rb
@@ -11,6 +11,36 @@ module Decidim
         endorser.profile_path
       end
 
+      def email_subject
+        I18n.t(
+          "decidim.events.proposals.proposal_endorsed.email_subject",
+          endorser_nickname: endorser.nickname
+        )
+      end
+
+      def email_intro
+        I18n.t(
+          "decidim.events.proposals.proposal_endorsed.email_intro",
+          endorser_nickname: endorser.nickname,
+          endorser_name: endorser.name
+        )
+      end
+
+      def notification_title
+        I18n.t(
+          "decidim.events.proposals.proposal_endorsed.notification_title",
+          resource_title: resource_title,
+          resource_path: resource_path,
+          endorser_nickname: endorser.nickname,
+          endorser_name: endorser.name,
+          endorser_path: endorser.profile_path
+        ).html_safe
+      end
+
+      def i18n_options
+        super().merge(nickname: endorser_nickname)
+      end
+
       private
 
       def endorser
--- decidim-proposals/spec/events/decidim/proposals/proposal_endorsed_event_spec.rb
+++ decidim-proposals/spec/events/decidim/proposals/proposal_endorsed_event_spec.rb
@@ -3,20 +3,20 @@
 require "spec_helper"
 
 describe Decidim::Proposals::ProposalEndorsedEvent do
-  subject do
-    extra = {
-      endorser: endorsement_author
-    }
-    described_class.new(resource: proposal, event_name: event_name, user: follower, extra: extra)
-  end
+  include_context "simple event"
 
+  let(:event_name) { "decidim.events.users.profile_updated" }
+  let(:resource) { proposal }
+  let(:author) { create :user, organization: proposal.organization }
+
+  let(:extra) { { endorser: author } }
   let(:proposal) { create :proposal }
-  let(:endorsement_author) { create :user, organization: proposal.organization }
-  let(:endorsement) { create :proposal_endorsement, proposal: proposal, author: endorsement_author }
-  let(:event_name) { "decidim.events.proposals.proposal_endorsed" }
+  let(:endorsement) { create :proposal_endorsement, proposal: proposal, author: author }
   let(:resource_path) { resource_locator(proposal).path }
   let(:follower) { create(:user, organization: proposal.organization) }
-  let(:follow) { create(:follow, followable: endorsement_author, user: follower) }
+  let(:follow) { create(:follow, followable: author, user: follower) }
+
+  it_behaves_like "an simple event"
 
   describe "types" do
     subject { described_class }
@@ -32,22 +32,15 @@ describe Decidim::Proposals::ProposalEndorsedEvent do
 
   describe "email_subject" do
     it "is generated correctly" do
-      expect(subject.email_subject).to eq("@#{endorsement_author.nickname} has endorsed a new proposal")
+      expect(subject.email_subject).to eq("#{author_presenter.nickname} has endorsed a new proposal")
     end
   end
 
   describe "email_intro" do
     it "is generated correctly" do
       expect(subject.email_intro)
-        .to eq("Hi,\n#{endorsement_author.name} @#{endorsement_author.nickname}, who you are following," \
+        .to eq("Hi,\n#{author.name} #{author_presenter.nickname}, who you are following," \
          " has just endorsed a proposal that may be interesting to you, check it out and contribute:")
-    end
-  end
-
-  describe "email_outro" do
-    it "is generated correctly" do
-      expect(subject.email_outro)
-        .to eq("You have received this notification because you are following @#{endorsement_author.nickname}. You can stop receiving notifications following the previous link.")
     end
   end
 
@@ -57,7 +50,7 @@ describe Decidim::Proposals::ProposalEndorsedEvent do
         .to include("The <a href=\"#{resource_path}\">#{proposal.title}</a> proposal has been endorsed by ")
 
       expect(subject.notification_title)
-        .to include("<a href=\"/profiles/#{endorsement_author.nickname}\">#{endorsement_author.name} @#{endorsement_author.nickname}</a>.")
+        .to include("<a href=\"/profiles/#{author.nickname}\">#{author.name} #{author_presenter.nickname}</a>.")
     end
   end
 end
