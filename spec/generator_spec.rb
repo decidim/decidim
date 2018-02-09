dd8e5ca7eb0ea382ee59aff72c7d9c7e007fcc75
David Rodríguez <deivid.rodriguez@riseup.net>
2018-02-09 14:32:40 -0300

    Get external test app generation tested

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -7,8 +7,6 @@ describe "Application generation" do
 
   let(:test_app) { "spec/generator_test_app" }
 
-  after { FileUtils.rm_rf(test_app) }
-
   shared_examples_for "a sane generator" do
     it "successfully generates application" do
       expect(status).to eq(true)
@@ -29,34 +27,57 @@ describe "Application generation" do
   end
   # rubocop:enable RSpec/BeforeAfterAll
 
-  context "without flags" do
-    let(:command) { "decidim #{test_app}" }
+  context "when directly invoking decidim executable" do
+    after { FileUtils.rm_rf(test_app) }
 
-    it_behaves_like "a sane generator"
-  end
+    context "without flags" do
+      let(:command) { "decidim #{test_app}" }
 
-  context "with --edge flag" do
-    let(:command) { "decidim --edge #{test_app}" }
-
-    it_behaves_like "a sane generator"
-  end
-
-  context "with --branch flag" do
-    let(:command) { "decidim --branch master #{test_app}" }
-
-    it_behaves_like "a sane generator"
-  end
-
-  context "with --path flag" do
-    let(:command) { "decidim --path #{File.expand_path("..", __dir__)} #{test_app}" }
-
-    it_behaves_like "a sane generator"
-  end
-
-  context "with development application" do
-    let(:command) do
-      "decidim --path #{File.expand_path("..", __dir__)} #{test_app} --recreate_db --seed_db"
+      it_behaves_like "a sane generator"
     end
+
+    context "with --edge flag" do
+      let(:command) { "decidim --edge #{test_app}" }
+
+      it_behaves_like "a sane generator"
+    end
+
+    context "with --branch flag" do
+      let(:command) { "decidim --branch master #{test_app}" }
+
+      it_behaves_like "a sane generator"
+    end
+
+    context "with --path flag" do
+      let(:command) { "decidim --path #{File.expand_path("..", __dir__)} #{test_app}" }
+
+      it_behaves_like "a sane generator"
+    end
+
+    context "with development application" do
+      let(:command) do
+        "decidim --path #{File.expand_path("..", __dir__)} #{test_app} --recreate_db --seed_db"
+      end
+
+      it_behaves_like "a sane generator"
+    end
+  end
+
+  context "when run from external application" do
+    around do |example|
+      Bundler.with_original_env do
+        system(
+          "decidim --path #{File.expand_path("..", __dir__)} #{test_app}",
+          out: File::NULL
+        )
+      end
+
+      Dir.chdir(test_app) { example.run }
+
+      FileUtils.rm_rf(test_app)
+    end
+
+    let(:command) { "bundle exec rake decidim:generate_external_test_app" }
 
     it_behaves_like "a sane generator"
   end
