338110fed06c0694400a90f3c66606d1a0fc5e5f
Hugoren Martinako <aumpfbahn@gmail.com>
2018-02-13 16:31:14 +0100

    table selected & fake data

--- decidim_app-design/app/helpers/application_helper.rb
+++ decidim_app-design/app/helpers/application_helper.rb
@@ -12,14 +12,4 @@ module ApplicationHelper
   def is(mod)
     request.path_info.include?(mod)
   end
-
-  def random_string(length, include_uppercase = true, include_lowercase = true, include_numbers = false)
-    o = []
-    o.push ('a'..'z') if include_uppercase
-    o.push ('A'..'Z') if include_lowercase
-
-    o.push (0..9) if include_numbers
-    o = o.map { |i| i.to_a }.flatten
-    string = (0...length).map { o[rand(o.length)] }.join
-  end
 end
