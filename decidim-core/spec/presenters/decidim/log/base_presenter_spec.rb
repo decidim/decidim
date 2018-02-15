82a58ead451436b19b4185548b2d8e90aa2e0530
Marc Riera Casals <mrc2407@gmail.com>
2018-02-15 16:07:57 +0100

    Check diff is presented when conditions apply

--- decidim-core/spec/presenters/decidim/log/base_presenter_spec.rb
+++ decidim-core/spec/presenters/decidim/log/base_presenter_spec.rb
@@ -3,21 +3,24 @@
 require "spec_helper"
 
 describe Decidim::Log::BasePresenter, type: :helper do
-  subject { described_class.new(action_log, helper) }
+  subject { presenter }
 
+  let(:presenter) { described_class.new(action_log, helper) }
   let(:action_log) { create :action_log, action: action, created_at: Date.new(2018, 1, 2).at_midnight }
   let(:user) { action_log.user }
   let(:participatory_space) { action_log.participatory_space }
   let(:resource) { action_log.resource }
   let(:action) { :create }
+  let(:version_double) { double(present?: false) }
 
   before do
     helper.extend(Decidim::ApplicationHelper)
     helper.extend(Decidim::TranslationsHelper)
+    allow(presenter).to receive(:version).and_return(version_double)
   end
 
   describe "#present" do
-    subject { described_class.new(action_log, helper).present }
+    subject { presenter.present }
 
     it "shows the date of the action" do
       expect(subject).to include("02/01/2018 00:00")
@@ -40,6 +43,25 @@ describe Decidim::Log::BasePresenter, type: :helper do
         expect(subject).to include(user.nickname)
         expect(subject).to include(resource.title)
         expect(subject).to include(participatory_space.title["en"])
+      end
+
+      context "when version exists" do
+        let(:version_double) { double(present?: true, changeset: {}) }
+
+        it "renders a dropdown" do
+          expect(subject).to include("class=\"logs__log__actions-dropdown\"")
+        end
+
+        it "renders the diff" do
+          diff_presenter_double = double(present: true)
+          allow(Decidim::Log::DiffPresenter)
+            .to receive(:new).and_return(diff_presenter_double)
+
+          expect(diff_presenter_double)
+            .to receive(:present)
+
+          subject
+        end
       end
     end
 
