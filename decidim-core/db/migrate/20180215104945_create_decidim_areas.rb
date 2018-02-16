1f300fb62fe6d72a5c00a3251d0c2ee5aa1431bd
Isaac Massot <isaac.mg@coditramuntana.com>
2018-02-16 13:37:49 +0100

    fix migration

--- decidim-core/db/migrate/20180215104945_create_decidim_areas.rb
+++ decidim-core/db/migrate/20180215104945_create_decidim_areas.rb
@@ -4,7 +4,7 @@ class CreateDecidimAreas < ActiveRecord::Migration[5.1]
   def change
     create_table :decidim_areas do |t|
       t.jsonb :name
-      t.references :decidim_area_type, foreign_key: { to_table: :decidim_area_types }, index: true
+      t.references :area_type, foreign_key: { to_table: :decidim_area_types }, index: true
       t.references :decidim_organization, foreign_key: true, index: true
       t.timestamps
     end
