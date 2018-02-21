8b5952a93a28e8994946bf9caa5f419ed514b3f1
Agustí B.R <agusti.br@coditramuntana.com>
2018-02-21 11:02:54 +0100

    refactor spec manage proposal category examples to match layout

--- decidim-proposals/spec/shared/manage_proposals_category_examples.rb
+++ decidim-proposals/spec/shared/manage_proposals_category_examples.rb
@@ -9,11 +9,11 @@ shared_examples "when managing proposals category as an admin" do
 
   context "when in the Proposals list page" do
     it "shows a checkbox to select each proposal" do
-      expect(page).to have_css("#js-form-recategorize-proposals tbody .js-check-all-proposal", count: 4)
+      expect(page).to have_css(".table-list tbody .js-proposal-list-check", count: 4)
     end
 
     it "shows a checkbox to (des)select all proposal" do
-      expect(page).to have_css("#js-form-recategorize-proposals thead .js-check-all", count: 1)
+      expect(page).to have_css(".table-list thead .js-check-all", count: 1)
     end
 
     context "when selecting proposals" do
@@ -25,16 +25,45 @@ shared_examples "when managing proposals category as an admin" do
         expect(page).to have_css("span#js-recategorize-proposals-count", count: 1)
       end
 
-      it "shows a category select" do
-        expect(page).to have_css("select#category_id", count: 1)
+      it "shows the bulk actions button" do
+        expect(page).to have_css("#js-bulk-actions-button", count: 1)
       end
 
-      it "shows a submit button" do
-        expect(page).to have_css("button#js-submit-edit-category", count: 1)
+      context "when click the bulk action button" do
+        before do
+          click_button "Actions"
+        end
+
+        it "shows the bulk actions dropdown" do
+          expect(page).to have_css("#js-bulk-actions-dropdown", count: 1)
+        end
+
+        it "shows the change action option" do
+          expect(page).to have_selector(:link_or_button, "Change category")
+        end
       end
+
+
+      context "when change category is selected from actions dropdown" do
+        before do
+          click_button "Actions"
+          click_button "Change category"
+        end
+
+        it "shows the category select" do
+          expect(page).to have_css("select#category_id", count: 1)
+        end
+
+        it "shows an update button" do
+          expect(page).to have_css("button#js-submit-edit-category", count: 1)
+        end
+      end
+
 
       context "when submiting form" do
         before do
+          click_button "Actions"
+          click_button "Change category"
           within "#js-form-recategorize-proposals" do
             select translated(category.name), from: :category_id
             page.find("button#js-submit-edit-category").click
