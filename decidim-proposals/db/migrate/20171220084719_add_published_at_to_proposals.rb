42f5b4c4e13ec021253e2af736652c4714161184
Agustí B.R <agusti.br@coditramuntana.com>
2018-02-21 16:56:07 +0100

    disable rubocop in the AddPublishedAtToProposals migration

--- decidim-proposals/db/migrate/20171220084719_add_published_at_to_proposals.rb
+++ decidim-proposals/db/migrate/20171220084719_add_published_at_to_proposals.rb
@@ -3,7 +3,9 @@
 class AddPublishedAtToProposals < ActiveRecord::Migration[5.1]
   def up
     add_column :decidim_proposals_proposals, :published_at, :datetime, index: true
+    # rubocop:disable Rails/SkipsModelValidations
     Decidim::Proposals::Proposal.update_all("published_at = updated_at")
+    # rubocop:enable Rails/SkipsModelValidations
   end
 
   def down
