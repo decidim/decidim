2ddb21a3dfeaecb10259b94371a618d6a9af5e7b
Agustí B.R <agusti.br@coditramuntana.com>
2018-02-21 16:23:07 +0100

    fix migration AddPublishedAtToProposals

--- decidim-proposals/db/migrate/20171220084719_add_published_at_to_proposals.rb
+++ decidim-proposals/db/migrate/20171220084719_add_published_at_to_proposals.rb
@@ -3,9 +3,7 @@
 class AddPublishedAtToProposals < ActiveRecord::Migration[5.1]
   def up
     add_column :decidim_proposals_proposals, :published_at, :datetime, index: true
-    Decidim::Proposals::Proposal.find_each do |proposal|
-      proposal.update_attributes!(published_at: proposal.updated_at)
-    end
+    Decidim::Proposals::Proposal.update_all("published_at = updated_at")
   end
 
   def down
