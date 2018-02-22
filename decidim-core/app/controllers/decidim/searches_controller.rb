36c4d02b7d46b72b1dea1a8b9e01400c745b3f78
Oliver Valls <oliver.hv@coditramuntana.com>
2018-02-22 15:40:23 +0100

    Apply Search command into SearchController.

--- decidim-core/app/controllers/decidim/searches_controller.rb
+++ decidim-core/app/controllers/decidim/searches_controller.rb
@@ -2,14 +2,21 @@
 
 module Decidim
   class SearchesController < Decidim::ApplicationController
+    include Rectify::ControllerHelpers
     skip_authorization_check
     helper_method :term
 
     def index
-      @results = SearchableRsrc.global_search(term)
+      Search.call(term) do
+        on(:ok) do |results|
+          expose(results: results)
+        end
+      end
     end
 
+    #--------------------------------------------------------------
     private
+    #--------------------------------------------------------------
 
     def term
       @term ||= params[:term]
