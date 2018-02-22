66782f02d3b03b008e0099884ad333054bfe9d6e
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2018-02-22 15:30:34 +0100

    Fix style

--- decidim-core/spec/types/date_time_type_spec.rb
+++ decidim-core/spec/types/date_time_type_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "spec_helper"
 require "decidim/api/test/type_context"
 
@@ -5,7 +7,7 @@ module Decidim
   module Core
     describe DateTimeType, type: :graphql do
       include_context "with a graphql type"
-      let(:model) { DateTime.civil(2018, 2, 22, 9, 47, 00, "+1") }
+      let(:model) { DateTime.civil(2018, 2, 22, 9, 47, 0, "+1") }
       let(:query) { nil }
 
       it "returns the formatted date" do
