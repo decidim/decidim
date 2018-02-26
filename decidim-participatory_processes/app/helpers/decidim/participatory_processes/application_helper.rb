29363e7130425d3b61e19758227470131308e605
Ruben Gonzalez <ruben@fanatikcoders.com>
2018-02-26 16:17:11 +0100

    Document why deep_copy in the method

--- decidim-participatory_processes/app/helpers/decidim/participatory_processes/application_helper.rb
+++ decidim-participatory_processes/app/helpers/decidim/participatory_processes/application_helper.rb
@@ -9,9 +9,12 @@ module Decidim
 
       # Public: Renders all hooks registered as `hook_name`.
       #
-      # hook_name - a Symbol representing the name of the hook.
+      #   Note: We're passing a deep copy of the view context to allow
+      #   us to extend it without polluting the original view context
       #
-      # Returns an HTML safe String.
+      # @param hook_name [Symbol] representing the name of the hook.
+      #
+      # @return [String] an HTML safe String
       def render_participatory_processes_hook(hook_name)
         Decidim::ParticipatoryProcesses.view_hooks.render(hook_name, deep_dup)
       end
