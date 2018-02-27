8d4bdab075e740c92fc0f845340130fb627739b3
Marc Riera Casals <mrc2407@gmail.com>
2018-02-27 09:16:54 +0100

    Fix offenses

--- decidim-core/db/migrate/20180226140756_add_version_to_action_logs.rb
+++ decidim-core/db/migrate/20180226140756_add_version_to_action_logs.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddVersionToActionLogs < ActiveRecord::Migration[5.1]
   def up
     add_column :decidim_action_logs, :version_id, :integer, index: true
@@ -5,7 +7,9 @@ class AddVersionToActionLogs < ActiveRecord::Migration[5.1]
     Decidim::ActionLog.find_each do |action_log|
       version_id = action_log.extra.dig("version", "id")
       next unless version_id
+      # rubocop:disable Rails/SkipsModelValidations
       action_log.update_column(:version_id, version_id)
+      # rubocop:enable Rails/SkipsModelValidations
     end
   end
 
