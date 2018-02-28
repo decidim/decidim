b083468dec050d05838bcff772655b8d39ec6c1a
Marc Riera Casals <mrc2407@gmail.com>
2018-02-28 14:22:49 +0100

    Track assembly creation

--- decidim-assemblies/app/models/decidim/assembly.rb
+++ decidim-assemblies/app/models/decidim/assembly.rb
@@ -13,6 +13,7 @@ module Decidim
     include Decidim::Followable
     include Decidim::HasReference
     include Decidim::Traceable
+    include Decidim::Loggable
 
     belongs_to :organization,
                foreign_key: "decidim_organization_id",
@@ -44,6 +45,10 @@ module Decidim
     # Returns an ActiveRecord::Relation.
     def self.promoted
       where(promoted: true)
+    end
+
+    def self.log_presenter_class_for(_log)
+      Decidim::Assemblies::AdminLog::AssemblyPresenter
     end
 
     def hashtag
