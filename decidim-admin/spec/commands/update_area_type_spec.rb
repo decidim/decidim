815b645856792bfff209edbd618b019144c7d312
isaacmg410 <isaac.mg@coditramuntana.com>
2018-02-28 16:20:33 +0100

    BUGFIX Area and AreaType command specs (#2859)
    * BUGFIX Area and AreaType command specs are actually testing scope commands
    
    * add changelog line

--- decidim-admin/spec/commands/update_area_type_spec.rb
+++ decidim-admin/spec/commands/update_area_type_spec.rb
@@ -3,11 +3,11 @@
 require "spec_helper"
 
 module Decidim::Admin
-  describe UpdateScopeType do
-    subject { described_class.new(scope_type, form) }
+  describe UpdateAreaType do
+    subject { described_class.new(area_type, form) }
 
     let(:organization) { create :organization }
-    let(:scope_type) { create :scope_type, organization: organization }
+    let(:area_type) { create :area_type, organization: organization }
     let(:name) { Decidim::Faker::Localized.literal("new name") }
     let(:plural) { Decidim::Faker::Localized.literal("new names") }
 
@@ -31,15 +31,15 @@ module Decidim::Admin
     context "when the form is valid" do
       before do
         subject.call
-        scope_type.reload
+        area_type.reload
       end
 
       it "updates the name of the scope" do
-        expect(translated(scope_type.name)).to eq("new name")
+        expect(translated(area_type.name)).to eq("new name")
       end
 
       it "updates the plural of the scope" do
-        expect(translated(scope_type.plural)).to eq("new names")
+        expect(translated(area_type.plural)).to eq("new names")
       end
     end
   end
