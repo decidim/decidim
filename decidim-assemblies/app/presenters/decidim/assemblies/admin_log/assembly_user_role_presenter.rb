77c7e3b7d8f18c9597abb8c57a34e34d14ac0271
Marc Riera Casals <mrc2407@gmail.com>
2018-03-01 12:21:40 +0100

    Trace assembly user roles updates

--- decidim-assemblies/app/presenters/decidim/assemblies/admin_log/assembly_user_role_presenter.rb
+++ decidim-assemblies/app/presenters/decidim/assemblies/admin_log/assembly_user_role_presenter.rb
@@ -27,7 +27,7 @@ module Decidim
 
         def action_string
           case action
-          when "create"
+          when "create", "update"
             "decidim.admin_log.assembly_user_role.#{action}"
           else
             super
