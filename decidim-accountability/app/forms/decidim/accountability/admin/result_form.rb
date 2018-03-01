d1c05b3c3a3f6f855ac79c4ecf1d3dc46333a53d
Oliver Valls <oliver.hv@coditramuntana.com>
2018-03-01 14:47:08 +0100

    [I18N] Pending translations! [REFACTOR] Remove useless method and use its name in another method for better undestanding.

--- decidim-accountability/app/forms/decidim/accountability/admin/result_form.rb
+++ decidim-accountability/app/forms/decidim/accountability/admin/result_form.rb
@@ -39,15 +39,11 @@ module Decidim
           self.decidim_category_id = model.category.try(:id)
         end
 
-        def proposal_objs
-          @proposal_objs ||= Decidim.find_resource_manifest(:proposals)
+        def proposals
+          @proposals ||= Decidim.find_resource_manifest(:proposals)
                                     .try(:resource_scope, current_feature)
                                     &.where(id: proposal_ids)
                                     &.order(title: :asc)
-        end
-
-        def proposals
-          @proposals ||= proposal_objs&.pluck(:title, :id)
         end
 
         def projects
