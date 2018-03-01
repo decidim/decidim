bc2b4f825d1db3fe35757b1c4fe405f291574ba6
Oliver Valls <oliver.hv@coditramuntana.com>
2018-03-01 14:29:38 +0100

    [REFACTOR] Extract Javascript to javascript files, as it should be.

--- decidim-accountability/app/assets/javascripts/decidim/accountability/admin/accountability_admin.js
+++ decidim-accountability/app/assets/javascripts/decidim/accountability/admin/accountability_admin.js
@@ -7,3 +7,31 @@ $("#result_decidim_accountability_status_id").change(function () {
     $("#result_progress").val(progress);
   }
 });
+
+$(function() {
+  $(document).on("open.zf.reveal", "#data_picker-modal", function () {
+    $('#data_picker-autocomplete').autoComplete({
+      minChars: 2,
+      source: function(term, response){
+        try { xhr.abort(); } catch(e){}
+        $.getJSON('proposals.json', { q: term }, function(data){ console.log(data); response(data); });
+      },
+      renderItem: function (item, search){
+        search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+        var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
+        var title= item[0]
+        var modelId= item[1]
+        var val= '#' + modelId + '- ' + title;
+        return '<div class="autocomplete-suggestion" data-model-id="' + modelId + '" data-val="'+title+'">' + val.replace(re, "<b>$1</b>") + '</div>';
+      },
+      onSelect: function(e, term, item){
+        let choose= $('#proposal-picker-choose')
+        var modelId= item.data('modelId')
+        var val= '#' + modelId + '- ' + item.data('val');
+        choose.data('picker-value', modelId)
+        choose.data('picker-text', val )
+        choose.data('picker-choose', '')
+      }
+    })
+  } );
+})
