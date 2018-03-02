8306fbe13a8b6877eb317f374785ddbfa904bad2
Oliver Valls <oliver.hv@coditramuntana.com>
2018-03-02 13:29:45 +0100

    [TEST] Make the test for the Command cover both ways of invocation, by id and by url.

--- decidim-proposals/spec/commands/decidim/comments/create_comment_with_proposal_links_spec.rb
+++ decidim-proposals/spec/commands/decidim/comments/create_comment_with_proposal_links_spec.rb
@@ -15,19 +15,40 @@ module Decidim
         let(:parser_context) { { current_organization: organization } }
         let(:body) { ::Faker::Lorem.paragraph + " ~#{linked_proposal.id}" }
 
-        it "creates a new comment with proposal url replaced" do
-          expect(Comment).to receive(:create!).with(
-            author: author,
-            commentable: commentable,
-            root_commentable: commentable,
-            body: Decidim::ContentProcessor.parse(body, parser_context).rewrite,
-            alignment: alignment,
-            decidim_user_group_id: user_group_id
-          ).and_call_original
+        context "when the link is a Proposal id" do
+          it "creates a new comment with proposal url replaced" do
+            expect(Comment).to receive(:create!).with(
+              author: author,
+              commentable: commentable,
+              root_commentable: commentable,
+              body: Decidim::ContentProcessor.parse(body, parser_context).rewrite,
+              alignment: alignment,
+              decidim_user_group_id: user_group_id
+            ).and_call_original
 
-          expect do
-            command.call
-          end.to change(Comment, :count).by(1)
+            expect do
+              command.call
+            end.to change(Comment, :count).by(1)
+          end
+        end
+
+        context "when the link is a url" do
+          let(:body) { ::Faker::Lorem.paragraph + " #{Decidim::ResourceLocatorPresenter.new(linked_proposal).path}" }
+
+          it "creates a new comment with proposal url replaced" do
+            expect(Comment).to receive(:create!).with(
+              author: author,
+              commentable: commentable,
+              root_commentable: commentable,
+              body: Decidim::ContentProcessor.parse(body, parser_context).rewrite,
+              alignment: alignment,
+              decidim_user_group_id: user_group_id
+            ).and_call_original
+
+            expect do
+              command.call
+            end.to change(Comment, :count).by(1)
+          end
         end
 
         it "sends a notification to linked proposal's authors" do
