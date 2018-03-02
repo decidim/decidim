ba9da170d804bf87b882ac3e19186049795811c7
David Rodríguez <deivid.rodriguez@riseup.net>
2018-03-02 20:57:13 -0300

    Remove unnecessary defensiveness

--- decidim-core/app/helpers/decidim/scopes_helper.rb
+++ decidim-core/app/helpers/decidim/scopes_helper.rb
@@ -34,7 +34,7 @@ module Decidim
     #
     # Returns nothing.
     def scopes_picker_field(form, name, root: false, options: {})
-      root = try(:current_participatory_space)&.scope if root == false
+      root = current_participatory_space.scope if root == false
       form.scopes_picker name, options do |scope|
         { url: decidim.scopes_picker_path(root: root, current: scope&.id, field: form.label_for(name)),
           text: scope_name_for_picker(scope, I18n.t("decidim.scopes.global")) }
