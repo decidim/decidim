17f55230e29f5a9e69bae29e6ab04728938e5142
Marc Riera Casals <mrc2407@gmail.com>
2018-03-03 18:37:02 +0100

    Add Proposal M card cell

---
+++ decidim-proposals/app/cells/decidim/proposals/proposal_m_cell.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require "cell/partial"
+
+module Decidim
+  module Proposals
+    # This cell renders a proposal with its M-size card.
+    class ProposalMCell < Decidim::ViewModel
+      self.view_paths << "#{Decidim::Core::Engine.root}/app/views"
+      self.view_paths << "#{Decidim::Proposals::Engine.root}/app/cells"
+      self.view_paths << "#{Decidim::Proposals::Engine.root}/app/views"
+
+      include Decidim::LayoutHelper
+      include Decidim::ApplicationHelper
+      include Decidim::ActionAuthorizationHelper
+      include Decidim::TranslationsHelper
+      include Decidim::ResourceHelper
+      include Decidim::Proposals::ApplicationHelper
+      include Decidim::ResourceReferenceHelper
+
+      include Decidim::Proposals::Engine.routes.url_helpers
+
+      include Partial
+
+      def model_path
+        resource_locator(model).path
+      end
+
+      def current_settings
+        controller.current_settings
+      end
+
+      def current_user
+        controller.current_user
+      end
+    end
+  end
+end
