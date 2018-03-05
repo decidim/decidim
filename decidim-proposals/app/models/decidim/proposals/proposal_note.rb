169a04cbba539ece364953e579793468c03250fc
Marc Riera Casals <mrc2407@gmail.com>
2018-03-05 12:18:49 +0100

    Make proposal notes traceable

--- decidim-proposals/app/models/decidim/proposals/proposal_note.rb
+++ decidim-proposals/app/models/decidim/proposals/proposal_note.rb
@@ -4,6 +4,8 @@ module Decidim
   module Proposals
     # A proposal can include a notes created by admins.
     class ProposalNote < ApplicationRecord
+      include Decidim::Traceable
+
       belongs_to :proposal, foreign_key: "decidim_proposal_id", class_name: "Decidim::Proposals::Proposal", counter_cache: true
       belongs_to :author, foreign_key: "decidim_author_id", class_name: "Decidim::User"
 
