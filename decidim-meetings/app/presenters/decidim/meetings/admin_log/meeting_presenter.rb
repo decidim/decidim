0eb306b8a2b11bffaf99968976fca3b64fa06e28
Marc Riera Casals <mrc2407@gmail.com>
2018-03-05 14:55:57 +0100

    Trace meeting updates

--- decidim-meetings/app/presenters/decidim/meetings/admin_log/meeting_presenter.rb
+++ decidim-meetings/app/presenters/decidim/meetings/admin_log/meeting_presenter.rb
@@ -29,7 +29,7 @@ module Decidim
 
         def action_string
           case action
-          when "create"
+          when "create", "update"
             "decidim.meetings.admin_log.meeting.#{action}"
           else
             super
