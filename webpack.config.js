96869b7c36ee73ba4c339c3c87c6c129e13d3ec9
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2018-03-06 09:21:42 +0100

    Fix tslint style

--- webpack.config.js
+++ webpack.config.js
@@ -12,14 +12,15 @@ module.exports = env => {
 
   const config = {
     entry: {
-      comments: "./decidim-comments/app/frontend/entry.ts"
+      comments: "./decidim-comments/app/frontend/entry.ts",
     },
     output: {
       path: __dirname,
-      filename: "decidim-[name]/app/assets/javascripts/decidim/[name]/bundle.js"
+      filename:
+        "decidim-[name]/app/assets/javascripts/decidim/[name]/bundle.js",
     },
     resolve: {
-      extensions: [".js", ".jsx", ".ts", ".tsx", ".yml"]
+      extensions: [".js", ".jsx", ".ts", ".tsx", ".yml"],
     },
     devtool: ifProd("source-map", "eval"),
     module: {
@@ -27,52 +28,52 @@ module.exports = env => {
         {
           test: /\.jsx?$/,
           exclude: /node_modules/,
-          loaders: ["babel-loader", "eslint-loader"]
+          loaders: ["babel-loader", "eslint-loader"],
         },
         {
           test: /\.tsx?$/,
-          loaders: ["babel-loader", "awesome-typescript-loader"]
+          loaders: ["babel-loader", "awesome-typescript-loader"],
         },
         {
           test: /\.js.es6$/,
-          loaders: ["babel-loader", "eslint-loader"]
+          loaders: ["babel-loader", "eslint-loader"],
         },
         {
           test: /\.(yml|yaml)$/,
-          loaders: ["json-loader", "yaml-loader"]
+          loaders: ["json-loader", "yaml-loader"],
         },
         {
           test: /\.(graphql|gql)$/,
           exclude: /node_modules/,
-          loader: "graphql-tag/loader"
+          loader: "graphql-tag/loader",
         },
         {
           test: /\.json$/,
-          loaders: ["json-loader"]
+          loaders: ["json-loader"],
         },
         {
           test: require.resolve("react"),
-          loader: "expose-loader?React"
+          loader: "expose-loader?React",
         },
         {
           test: require.resolve("jquery"),
-          loader: "expose-loader?$"
-        }
-      ]
+          loader: "expose-loader?$",
+        },
+      ],
     },
     plugins: [
       new ProgressBarPlugin(),
       new webpack.DefinePlugin({
         "process.env": {
-          NODE_ENV: ifProd('"production"', '"development"')
-        }
+          NODE_ENV: ifProd('"production"', '"development"'),
+        },
       }),
       new BundleAnalyzerPlugin({
         analyzerMode: ifTest("disabled", "static"),
         reportFilename: "webpack.report.html",
-        openAnalyzer: false
-      })
-    ]
+        openAnalyzer: false,
+      }),
+    ],
   };
   return config;
 };
