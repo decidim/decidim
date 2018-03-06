08d380aa988c90bafc4e5785ade76521b2846e6a
Agustí B.R <agusti.br@coditramuntana.com>
2018-03-06 13:16:53 +0100

    Refactor cell_proposal_m w/ action `footer` context aware.

--- decidim-proposals/app/cells/decidim/proposals/proposal_m_cell.rb
+++ decidim-proposals/app/cells/decidim/proposals/proposal_m_cell.rb
@@ -6,15 +6,42 @@ module Decidim
   module Proposals
     # This cell renders a proposal with its M-size card.
     class ProposalMCell < Decidim::Proposals::ViewModel
+      property :title
 
-      def model_path
+      def show
+        render
+      end
+
+      def footer
+        render
+      end
+
+      private
+
+      def resource_path
         resource_locator(model).path
       end
 
-      def current_settings
-        controller.current_settings
+      def from_inside?
+        true if context[:from].to_s.include?(:inside.to_s)
       end
 
+      def from_outside?
+        true if context[:from].to_s.include?(:outside.to_s)
+      end
+
+      def body
+        truncate(model.body, length: 100)
+      end
+
+      def controller
+        context[:controller] || controller
+      end
+
+      def current_settings
+        model.feature.current_settings
+      end
+      #
       def feature_settings
         controller.feature_settings
       end
