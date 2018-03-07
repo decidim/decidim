2e640c5547b74ee6f687d868c6ffb18eb2402d8b
Ruben Gonzalez <ruben@fanatikcoders.com>
2018-03-07 12:23:03 +0100

    Add specs for admin child assemblies

--- decidim-assemblies/spec/system/admin/assembly_admin_manages_assemblies_spec.rb
+++ decidim-assemblies/spec/system/admin/assembly_admin_manages_assemblies_spec.rb
@@ -5,18 +5,22 @@ require "spec_helper"
 describe "Assembly admin manages assemblies", type: :system do
   include_context "when assembly admin administrating an assembly"
 
-  it_behaves_like "manage assemblies"
-
-  it "cannot create a new assembly" do
-    switch_to_host(organization.host)
-    login_as user, scope: :user
-    visit decidim_admin_assemblies.assemblies_path
-
-    expect(page).to have_no_selector(".actions .new")
+  shared_context "creating an assembly" do
+    it "cannot create a new assembly" do
+      expect(page).to have_no_selector(".actions .new")
+    end
   end
 
-  context "when deleting an assembly" do
-    let!(:assembly2) { create(:assembly, organization: organization) }
+  shared_context "deleting an assembly" do
+    it "cannot delete an assembly" do
+      within find("tr", text: translated(assembly.title)) do
+        expect(page).to have_no_content("Destroy")
+      end
+    end
+  end
+
+  context "managing parent assemblies" do
+    let(:parent_assembly) { nil }
 
     before do
       switch_to_host(organization.host)
@@ -24,10 +28,27 @@ describe "Assembly admin manages assemblies", type: :system do
       visit decidim_admin_assemblies.assemblies_path
     end
 
-    it "cannot delete an assembly" do
-      within find("tr", text: translated(assembly2.title)) do
-        expect(page).to have_no_content("Destroy")
+    it_behaves_like "manage assemblies"
+    it_behaves_like "creating an assembly"
+    it_behaves_like "deleting an assembly"
+  end
+
+  context "managing child assemblies" do
+    let!(:parent_assembly) { create :assembly, organization: organization }
+    let!(:child_assembly) { create :assembly, organization: organization, parent: parent_assembly }
+    let(:assembly) { child_assembly }
+
+    before do
+      switch_to_host(organization.host)
+      login_as user, scope: :user
+      visit decidim_admin_assemblies.assemblies_path
+      within find("tr", text: translated(parent_assembly.title)) do
+        click_link "Assemblies"
       end
     end
+
+    it_behaves_like "manage assemblies"
+    it_behaves_like "creating an assembly"
+    it_behaves_like "deleting an assembly"
   end
 end
