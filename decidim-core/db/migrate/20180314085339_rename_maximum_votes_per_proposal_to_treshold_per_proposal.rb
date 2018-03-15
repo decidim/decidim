0bed5d8008fa0cab36f6bb64e62a233b81c70cc2
Isaac Massot <isaac.mg@coditramuntana.com>
2018-03-15 08:46:34 +0100

    fix rubocop

--- decidim-core/db/migrate/20180314085339_rename_maximum_votes_per_proposal_to_treshold_per_proposal.rb
+++ decidim-core/db/migrate/20180314085339_rename_maximum_votes_per_proposal_to_treshold_per_proposal.rb
@@ -3,25 +3,25 @@
 class RenameMaximumVotesPerProposalToTresholdPerProposal < ActiveRecord::Migration[5.1]
   def up
     execute <<~SQL
-    UPDATE decidim_components
-    SET settings = jsonb_set(
-      settings::jsonb,
-      array['global'],
-      (settings->'global')::jsonb - 'maximum_votes_per_proposal' || jsonb_build_object('treshold_per_proposal', settings->'global'->'maximum_votes_per_proposal')
-      )
-    WHERE manifest_name = 'proposals'
+      UPDATE decidim_components
+      SET settings = jsonb_set(
+        settings::jsonb,
+        array['global'],
+        (settings->'global')::jsonb - 'maximum_votes_per_proposal' || jsonb_build_object('treshold_per_proposal', settings->'global'->'maximum_votes_per_proposal')
+        )
+      WHERE manifest_name = 'proposals'
     SQL
   end
 
   def down
     execute <<~SQL
-    UPDATE decidim_components
-    SET settings = jsonb_set(
-      settings::jsonb,
-      array['global'],
-      (settings->'global')::jsonb - 'treshold_per_proposal' || jsonb_build_object('maximum_votes_per_proposal', settings->'global'->'treshold_per_proposal')
-      )
-    WHERE manifest_name = 'proposals'
+      UPDATE decidim_components
+      SET settings = jsonb_set(
+        settings::jsonb,
+        array['global'],
+        (settings->'global')::jsonb - 'treshold_per_proposal' || jsonb_build_object('maximum_votes_per_proposal', settings->'global'->'treshold_per_proposal')
+        )
+      WHERE manifest_name = 'proposals'
     SQL
   end
 end
