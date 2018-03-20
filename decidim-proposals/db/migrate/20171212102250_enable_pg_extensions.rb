73e9ab33fa1cfb3188af175c61f8b449a3f2aaa9
Oliver Valls <oliver.hv@coditramuntana.com>
2018-03-20 15:25:03 +0100

    [TEST] enabling_extension is required so that test suite works in ci env.

--- decidim-proposals/db/migrate/20171212102250_enable_pg_extensions.rb
+++ decidim-proposals/db/migrate/20171212102250_enable_pg_extensions.rb
@@ -3,11 +3,16 @@
 class EnablePgExtensions < ActiveRecord::Migration[5.1]
   def change
     unless extension_enabled?("pg_trgm")
-      raise <<-MSG.squish
-        Decidim requires the pg_trgm extension to be enabled in your PostgreSQL.
-        You can do so by running `CREATE EXTENSION IF NOT EXISTS "pg_trgm";` on the current DB as a PostgreSQL
-        super user.
-      MSG
+      begin
+        # required so that test suite works in ci env
+        enable_extension "pg_trgm"
+      rescue
+        raise <<-MSG.squish
+          Decidim requires the pg_trgm extension to be enabled in your PostgreSQL.
+          You can do so by running `CREATE EXTENSION IF NOT EXISTS "pg_trgm";` on the current DB as a PostgreSQL
+          super user.
+        MSG
+      end
     end
   end
 end
