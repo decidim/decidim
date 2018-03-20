7ed66daceb52c67bbe42923045fed17cfc8a0ade
Oliver Valls <oliver.hv@coditramuntana.com>
2018-03-20 15:09:21 +0100

    Do not force Postgresql user to be admin when enabling trigram extension.

--- decidim-proposals/db/migrate/20171212102250_enable_pg_extensions.rb
+++ decidim-proposals/db/migrate/20171212102250_enable_pg_extensions.rb
@@ -2,6 +2,12 @@
 
 class EnablePgExtensions < ActiveRecord::Migration[5.1]
   def change
-    enable_extension "pg_trgm"
+    unless extension_enabled?("pg_trgm")
+      raise <<-MSG.squish
+        Decidim requires the pg_trgm extension to be enabled in your PostgreSQL.
+        You can do so by running `CREATE EXTENSION IF NOT EXISTS "pg_trgm";` on the current DB as a PostgreSQL
+        super user.
+      MSG
+    end
   end
 end
