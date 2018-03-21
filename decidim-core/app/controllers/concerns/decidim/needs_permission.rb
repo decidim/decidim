5aa69aa08b0ad4f0a1a7961b7b41ebbb14f3f24d
Marc Riera Casals <mrc2407@gmail.com>
2018-03-21 12:57:59 +0100

    Fix moderations specs

---
+++ decidim-core/app/controllers/concerns/decidim/needs_permission.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "active_support/concern"
+
+module Decidim
+  # Common logic to work with the permissions system
+  module NeedsPermission
+    extend ActiveSupport::Concern
+
+    included do
+      helper_method :allowed_to?
+
+      class ::Decidim::ActionForbidden < StandardError
+      end
+
+      rescue_from Decidim::ActionForbidden, with: :user_not_authorized
+
+      alias_method :permissions_context, :ability_context
+
+      def check_permission_to(action, subject, extra_context = {})
+        raise Decidim::MissingPermission unless allowed_to?(action, subject, extra_context)
+      end
+
+      def allowed_to?(action, subject, extra_context = {})
+        permission_klass.new(
+          current_user,
+          Decidim::PermissionAction.new(scope: permission_scope, action: action, subject: subject),
+          permissions_context.merge(extra_context)
+        ).allowed?
+      end
+
+      def permission_klass
+        raise "Please, make this method return a class"
+      end
+
+      def permission_scope
+        raise "Please, make this method return a symbol"
+      end
+    end
+  end
+end
