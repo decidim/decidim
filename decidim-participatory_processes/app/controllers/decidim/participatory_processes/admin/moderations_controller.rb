39adc5792df7fe62b675178046dd43d41dcb1ea8
Marc Riera Casals <mrc2407@gmail.com>
2018-03-21 12:57:59 +0100

    Fix moderations specs

--- decidim-participatory_processes/app/controllers/decidim/participatory_processes/admin/moderations_controller.rb
+++ decidim-participatory_processes/app/controllers/decidim/participatory_processes/admin/moderations_controller.rb
@@ -6,6 +6,20 @@ module Decidim
       # This controller allows admins to manage moderations in a participatory process.
       class ModerationsController < Decidim::Admin::ModerationsController
         include Concerns::ParticipatoryProcessAdmin
+        include NeedsPermission
+
+        def permission_scope
+          :admin
+        end
+
+        def permission_klass
+          Decidim::ParticipatoryProcesses::Permissions
+        end
+
+        # Overrides the method to use the permissions system logic.
+        def ensure_access_to(action, _subject = reportable)
+          check_permission_to action, :moderation
+        end
       end
     end
   end
