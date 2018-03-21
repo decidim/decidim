eaa7c5829ed42008155473198b002f35916f2935
Ruben Gonzalez <ruben@fanatikcoders.com>
2018-03-21 13:04:50 +0100

    Do not force Postgresql user to be admin when enabling ltree extension

--- decidim-assemblies/db/migrate/20180226103942_add_parent_child_relation_to_assemblies.rb
+++ decidim-assemblies/db/migrate/20180226103942_add_parent_child_relation_to_assemblies.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
-class AddParentChildRelationToAssemblies < ActiveRecord::Migration[5.0]
+class AddParentChildRelationToAssemblies < ActiveRecord::Migration[5.1]
   def change
-    enable_extension "ltree"
+    unless extension_enabled?("ltree")
+      begin
+        # required so that test suite works in ci env
+        enable_extension "ltree"
+      rescue StandardError
+        raise <<-MSG.squish
+        Decidim requires the ltree extension to be enabled in your PostgreSQL.
+        You can do so by running `CREATE EXTENSION IF NOT EXISTS "ltree";` on the current DB as a PostgreSQL
+        super user.
+        MSG
+      end
+    end
 
     add_reference :decidim_assemblies, :parent, index: { name: :decidim_assemblies_assemblies_on_parent_id }
     add_column :decidim_assemblies, :parents_path, :ltree
