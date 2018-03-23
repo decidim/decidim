e243c09166753d7762ce9110f297f8f9c68ea630
Agustí B.R <agusti.br@coditramuntana.com>
2018-03-23 12:33:47 +0100

    fix offending rubocop indentation

--- decidim-core/db/migrate/20180323102631_change_event_name_and_class_to_rename_to_publish_proposal_event.rb
+++ decidim-core/db/migrate/20180323102631_change_event_name_and_class_to_rename_to_publish_proposal_event.rb
@@ -4,14 +4,14 @@ class ChangeEventNameAndClassToRenameToPublishProposalEvent < ActiveRecord::Migr
   def up
     # rubocop:disable Rails/SkipsModelValidations
     Decidim::Notification.where(event_name: "decidim.events.proposals.proposal_created")
-      .update_all(event_name: "decidim.events.proposals.proposal_published", event_class: "Decidim::Proposals::PublishProposalEvent")
+                         .update_all(event_name: "decidim.events.proposals.proposal_published", event_class: "Decidim::Proposals::PublishProposalEvent")
     # rubocop:enable Rails/SkipsModelValidations
   end
 
   def down
     # rubocop:disable Rails/SkipsModelValidations
     Decidim::Notification.where(event_name: "decidim.events.proposals.proposal_published")
-      .update_all(event_name: "decidim.events.proposals.proposal_created", event_class: "Decidim::Proposals::CreateProposalEvent")
+                         .update_all(event_name: "decidim.events.proposals.proposal_created", event_class: "Decidim::Proposals::CreateProposalEvent")
     # rubocop:enable Rails/SkipsModelValidations
   end
 end
