9562f1ac186c1003590af901a746c339880be235
Marc Riera <mrc2407@gmail.com>
2018-04-03 15:22:34 +0200

    Fine grained permissions: comments (#3116)
    * Remove abilities and redundant specs from comments
    
    Permissions are handled by the space
    
    * Add changelog

--- decidim-comments/lib/decidim/comments/admin_engine.rb
+++ decidim-comments/lib/decidim/comments/admin_engine.rb
@@ -8,16 +8,6 @@ module Decidim
 
       paths["db/migrate"] = nil
 
-      initializer "decidim_comments.inject_abilities_to_user" do |_app|
-        Decidim.configure do |config|
-          config.admin_abilities += [
-            "Decidim::Comments::Abilities::AdminAbility",
-            "Decidim::Comments::Abilities::ParticipatoryProcessAdminAbility",
-            "Decidim::Comments::Abilities::ParticipatoryProcessModeratorAbility"
-          ]
-        end
-      end
-
       def load_seed
         nil
       end
