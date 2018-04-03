0eaa7e04268e186bbd80db09addc426305c43857
Marc Riera Casals <mrc2407@gmail.com>
2018-04-03 10:00:45 +0200

    Add permissions

---
+++ decidim-budgets/app/permissions/decidim/budgets/admin/permissions.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Budgets
+    module Admin
+      class Permissions < Decidim::DefaultPermissions
+        def allowed?
+          # Stop checks if the user is not authorized to perform the
+          # permission_action for this space
+          return false unless spaces_allows_user?
+
+          # The public part needs to be implemented yet
+          return false if permission_action.scope != :admin
+
+          return false if permission_action.subject != :project
+
+          return true if case permission_action.action
+                        when :create
+                          true
+                        else
+                          false
+                        end
+
+          false
+        end
+      end
+    end
+  end
+end
