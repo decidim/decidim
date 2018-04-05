4ecca835685097c6ed35f39e9141ec7c0bdb861f
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2018-04-05 15:55:02 +0200

    Fix typo

--- decidim-core/app/types/decidim/core/scope_api_type.rb
+++ decidim-core/app/types/decidim/core/scope_api_type.rb
@@ -9,8 +9,8 @@ module Decidim
       field :id, !types.ID
       field :name, !TranslatedFieldType, "The name of this scope."
 
-      field :children, !types[Decidim::Core::ScopeType], "Descendants of this scope"
-      field :parent, Decidim::Core::ScopeType, "This scope's parent scope."
+      field :children, !types[Decidim::Core::ScopeApiType], "Descendants of this scope"
+      field :parent, Decidim::Core::ScopeApiType, "This scope's parent scope."
     end
   end
 end
