14c7b94c12addcfefc73ab66b95f7a95bab2c2eb
Josep Jaume Rey <josepjaume@gmail.com>
2018-04-16 15:59:05 +0200

    Api improvements (#3132)
    * Expose proposal author and comments
    
    * Extend API
    
    * Fix style
    
    * Add attachment specs
    
    * Add specs
    
    * Fix offenses
    
    * Fix typo
    
    * Move logic
    
    * Fix documentation
    
    * Endorsements should be authorable
    
    * Delegate organization on endorsement
    
    * Fix specs
    
    * Fix specs
    
    * Fix offense
    
    * Fix API

---
+++ decidim-core/spec/types/scope_api_type_spec.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "decidim/api/test/type_context"
+
+module Decidim
+  module Core
+    describe ScopeApiType do
+      include_context "with a graphql type"
+
+      let!(:subscope) { create(:subscope) }
+      let!(:parent) { subscope.parent }
+
+      let(:model) { subscope }
+
+      describe "name" do
+        let(:query) { '{ name { translation(locale: "en") } }' }
+
+        it "returns the scope's name" do
+          expect(response["name"]["translation"]).to eq(model.name["en"])
+        end
+      end
+
+      context "when it's a subscope" do
+        let(:model) { subscope }
+
+        describe "parent" do
+          let(:query) { "{ parent { id } }" }
+
+          it "returns its parent" do
+            expect(response["parent"]).to eq("id" => parent.id.to_s)
+          end
+        end
+
+        describe "children" do
+          let(:query) { "{ children { id } }" }
+
+          it "returns an empty array" do
+            expect(response["children"]).to eq([])
+          end
+        end
+      end
+
+      context "when it's a parent scope" do
+        let(:model) { parent }
+
+        describe "parent" do
+          let(:query) { "{ parent { id } }" }
+
+          it "returns nil" do
+            expect(response["parent"]).to be_nil
+          end
+        end
+
+        describe "children" do
+          let(:query) { "{ children { id } }" }
+
+          it "returns its children" do
+            expect(response["children"]).to eq([{ "id" => subscope.id.to_s }])
+          end
+        end
+      end
+    end
+  end
+end
