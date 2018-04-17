123edd4e331cc867cb36b6ee44e841f6e2fd15aa
Rubén González <ruben@fanatikcoders.com>
2018-04-17 09:53:25 +0200

    [Backport] Fix showing not visible proposals in process home page (#3199)
    * Fix view hooks returning proposals that should not be shown
    
    * Refactor to improve specs
    
    * Add changelog entry

--- decidim-proposals/spec/system/participatory_process_groups_view_hooks_spec.rb
+++ decidim-proposals/spec/system/participatory_process_groups_view_hooks_spec.rb
@@ -5,7 +5,8 @@ require "spec_helper"
 describe "Proposals in process group home", type: :system do
   include_context "with a feature"
   let(:manifest_name) { "proposals" }
-  let(:proposals_count) { 5 }
+  let(:proposals_count) { 2 }
+  let(:highlighted_proposals) { proposals_count * 2 }
 
   let!(:participatory_process_group) do
     create(
@@ -16,6 +17,12 @@ describe "Proposals in process group home", type: :system do
     )
   end
 
+  before do
+    allow(Decidim::Proposals.config)
+      .to receive(:process_group_highlighted_proposals_limit)
+      .and_return(highlighted_proposals)
+  end
+
   context "when there are no proposals" do
     it "does not show the highlighted proposals section" do
       visit decidim_participatory_processes.participatory_process_group_path(participatory_process_group)
@@ -24,19 +31,43 @@ describe "Proposals in process group home", type: :system do
   end
 
   context "when there are proposals" do
-    let!(:proposals) do
-      create_list(:proposal, proposals_count, feature: feature)
-    end
+    let!(:proposals) { create_list(:proposal, proposals_count, feature: feature) }
+    let!(:drafted_proposals) { create_list(:proposal, proposals_count, :draft, feature: feature) }
+    let!(:hidden_proposals) { create_list(:proposal, proposals_count, :hidden, feature: feature) }
+    let!(:withdrawn_proposals) { create_list(:proposal, proposals_count, :withdrawn, feature: feature) }
 
     it "shows the highlighted proposals section" do
       visit decidim_participatory_processes.participatory_process_group_path(participatory_process_group)
 
       within ".highlighted_proposals" do
-        expect(page).to have_css(".card--proposal", count: 3)
+        expect(page).to have_css(".card--proposal", count: proposals_count)
 
         proposals_titles = proposals.map(&:title)
+        drafted_proposals_titles = drafted_proposals.map(&:title)
+        hidden_proposals_titles = hidden_proposals.map(&:title)
+        withdrawn_proposals_titles = withdrawn_proposals.map(&:title)
+
         highlighted_proposals = page.all(".card--proposal .card__title").map(&:text)
         expect(proposals_titles).to include(*highlighted_proposals)
+        expect(drafted_proposals_titles).not_to include(*highlighted_proposals)
+        expect(hidden_proposals_titles).not_to include(*highlighted_proposals)
+        expect(withdrawn_proposals_titles).not_to include(*highlighted_proposals)
+      end
+    end
+
+    context "and there are more proposals than those that can be shown" do
+      let!(:proposals) { create_list(:proposal, highlighted_proposals + 2, feature: feature) }
+
+      it "shows the amount of proposals configured" do
+        visit decidim_participatory_processes.participatory_process_group_path(participatory_process_group)
+
+        within ".highlighted_proposals" do
+          expect(page).to have_css(".card--proposal", count: highlighted_proposals)
+
+          proposals_titles = proposals.map(&:title)
+          highlighted_proposals = page.all(".card--proposal .card__title").map(&:text)
+          expect(proposals_titles).to include(*highlighted_proposals)
+        end
       end
     end
 
@@ -52,7 +83,7 @@ describe "Proposals in process group home", type: :system do
       it "shows a tag with the proposals scope" do
         visit decidim_participatory_processes.participatory_process_group_path(participatory_process_group)
 
-        expect(page).to have_selector(".tags", text: child_scope.name["en"], count: 3)
+        expect(page).to have_selector(".tags", text: child_scope.name["en"], count: proposals_count)
       end
     end
   end
