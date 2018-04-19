355c98dfb284111d18c3caf8bc276df8a230e37f
Marc Riera Casals <mrc2407@gmail.com>
2018-04-19 09:35:34 +0200

    Fix admin specs

--- decidim-admin/spec/helpers/menu_helper_spec.rb
+++ decidim-admin/spec/helpers/menu_helper_spec.rb
@@ -15,20 +15,50 @@ module Decidim
           allow(view).to receive(:can?).and_return(true)
         end
 
-        it "renders the default main menu" do
-          expect(default_main_menu).to \
-            have_selector("li", count: 11) &
-            have_link("Dashboard", href: "/admin/") &
-            have_link("Processes", href: "/admin/participatory_processes") &
-            have_link("Process groups", href: "/admin/participatory_process_groups") &
-            have_link("Assemblies", href: "/admin/assemblies") &
-            have_link("Consultations", href: "/admin/consultations") &
-            have_link("Pages", href: "/admin/static_pages") &
-            have_link("Users", href: "/admin/users") &
-            have_link("Newsletters", href: "/admin/newsletters") &
-            have_link("Settings", href: "/admin/organization/edit") &
-            have_link("Admin activity log", href: "/admin/logs") &
-            have_link("OAuth applications", href: "/admin/oauth_applications")
+        RSpec::Matchers.define_negated_matcher :have_no_link, :have_link
+
+        context "when all spaces are inactive" do
+          it "renders the default main menu" do
+            expect(default_main_menu).to \
+              have_selector("li", count: 7) &
+              have_link("Dashboard", href: "/admin/") &
+              have_link("Pages", href: "/admin/static_pages") &
+              have_link("Users", href: "/admin/users") &
+              have_link("Newsletters", href: "/admin/newsletters") &
+              have_link("Settings", href: "/admin/organization/edit") &
+              have_link("Admin activity log", href: "/admin/logs") &
+              have_link("OAuth applications", href: "/admin/oauth_applications")
+
+            expect(default_main_menu).to \
+              have_no_link("Processes", href: "/admin/participatory_processes") &
+              have_no_link("Process groups", href: "/admin/participatory_process_groups") &
+              have_no_link("Assemblies", href: "/admin/assemblies") &
+              have_no_link("Consultations", href: "/admin/consultations")
+          end
+        end
+
+        context "when all spaces are active" do
+          before do
+            create :participatory_space, :active, organization: current_organization, manifest_name: :participatory_processes
+            create :participatory_space, :active, organization: current_organization, manifest_name: :assemblies
+            create :participatory_space, :active, organization: current_organization, manifest_name: :consultations
+          end
+
+          it "renders the default main menu" do
+            expect(default_main_menu).to \
+              have_selector("li", count: 11) &
+              have_link("Dashboard", href: "/admin/") &
+              have_link("Processes", href: "/admin/participatory_processes") &
+              have_link("Process groups", href: "/admin/participatory_process_groups") &
+              have_link("Assemblies", href: "/admin/assemblies") &
+              have_link("Consultations", href: "/admin/consultations") &
+              have_link("Pages", href: "/admin/static_pages") &
+              have_link("Users", href: "/admin/users") &
+              have_link("Newsletters", href: "/admin/newsletters") &
+              have_link("Settings", href: "/admin/organization/edit") &
+              have_link("Admin activity log", href: "/admin/logs") &
+              have_link("OAuth applications", href: "/admin/oauth_applications")
+          end
         end
 
         it "selects the correct default active option" do
