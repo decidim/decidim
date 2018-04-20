f19c226ef4b1e685545cb2fa35c8f5bf80587c62
Marc Riera Casals <mrc2407@gmail.com>
2018-04-20 11:28:24 +0200

    Separate migration into two

---
+++ decidim-core/db/migrate/20180420092713_publish_participatory_spaces.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class PublishParticipatorySpaces < ActiveRecord::Migration[5.1]
+  class ParticipatorySpace < ApplicationRecord
+    self.table_name = :decidim_participatory_spaces
+  end
+
+  class Organization < ApplicationRecord
+    self.table_name = :decidim_organizations
+  end
+
+  def up
+    %w(participatory_processes assemblies).each do |space_name|
+      Organization.find_each do |organization|
+        space = ParticipatorySpace.find_or_create_by(
+          decidim_organization_id: organization.id,
+          manifest_name: space_name
+        )
+        space.update!(activated_at: Time.current, published_at: Time.current)
+      end
+    end
+  end
+
+  def down; end
+end
