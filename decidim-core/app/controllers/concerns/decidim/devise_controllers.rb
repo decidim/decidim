8b8cd514eb170c601634ddb420e8d3ed57103bb4
Marc Riera Casals <mrc2407@gmail.com>
2018-04-23 15:03:55 +0200

    Fix specs

--- decidim-core/app/controllers/concerns/decidim/devise_controllers.rb
+++ decidim-core/app/controllers/concerns/decidim/devise_controllers.rb
@@ -28,20 +28,23 @@ module Decidim
       # Saves the location before loading each page so we can return to the
       # right page.
       before_action :store_current_location
-    end
 
-    def permission_class_chain
-      [Decidim::Permissions]
-    end
+      def permission_class_chain
+        [
+          Decidim::Admin::Permissions,
+          Decidim::Permissions
+        ]
+      end
 
-    def permission_scope
-      :public
-    end
+      def permission_scope
+        :public
+      end
 
-    def store_current_location
-      return if params[:redirect_url].blank? || !request.format.html?
+      def store_current_location
+        return if params[:redirect_url].blank? || !request.format.html?
 
-      store_location_for(:user, params[:redirect_url])
+        store_location_for(:user, params[:redirect_url])
+      end
     end
   end
 end
