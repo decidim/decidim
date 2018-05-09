10b870c537a3ee6a25fa93fd4feb9a596803fc34
David Rodríguez <deivid.rodriguez@riseup.net>
2018-05-09 11:43:16 -0300

    Use optional gems in development app (#3362)
    * Add a rake task to bundle all gemfiles at once
    
    * Normalize optional gems paths
    
    * Make sure migrations pick the right Gemfile
    
    And thus, optional gem migrations are installed and run.
    
    * Use optional gems in "demo apps"

--- decidim-generators/lib/decidim/generators/app_generator.rb
+++ decidim-generators/lib/decidim/generators/app_generator.rb
@@ -98,8 +98,14 @@ module Decidim
 
         if current_gem == "decidim"
           gsub_file "Gemfile", /gem "decidim-dev".*/, "gem \"decidim-dev\", #{gem_modifier}"
-          gsub_file "Gemfile", /gem "decidim-consultations".*/, "# gem \"decidim-consultations\", #{gem_modifier}"
-          gsub_file "Gemfile", /gem "decidim-initiatives".*/, "# gem \"decidim-initiatives\", #{gem_modifier}"
+
+          if options[:demo]
+            gsub_file "Gemfile", /gem "decidim-consultations".*/, "gem \"decidim-consultations\", #{gem_modifier}"
+            gsub_file "Gemfile", /gem "decidim-initiatives".*/, "gem \"decidim-initiatives\", #{gem_modifier}"
+          else
+            gsub_file "Gemfile", /gem "decidim-consultations".*/, "# gem \"decidim-consultations\", #{gem_modifier}"
+            gsub_file "Gemfile", /gem "decidim-initiatives".*/, "# gem \"decidim-initiatives\", #{gem_modifier}"
+          end
         end
 
         Bundler.with_original_env { run "bundle install" }
@@ -125,6 +131,7 @@ module Decidim
           [
             "--recreate_db=#{options[:recreate_db]}",
             "--seed_db=#{options[:seed_db]}",
+            "--skip_gemfile=#{options[:skip_gemfile]}",
             "--app_name=#{app_name}"
           ]
         )
