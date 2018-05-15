7d92875e60b3a91d182c50bc2d03e0abffaa4f5d
Marc Riera Casals <mrc2407@gmail.com>
2018-05-15 15:23:40 +0200

    Register users as a resource

---
+++ decidim-core/app/cells/decidim/user_profile/show.erb
@@ -0,0 +1,1 @@
+<p>USER PROFILE</p>
---
+++ decidim-core/app/cells/decidim/user_profile_cell.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Decidim
+  # This cell renders the profile of the given user.
+  class UserProfileCell < Decidim::ViewModel
+    def show
+      render
+    end
+  end
+end
