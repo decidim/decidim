031a8e85b827a54251732b562aa65c0e59ff6eef
Agustí B.R <agusti.br@coditramuntana.com>
2018-05-21 12:09:23 +0200

    disable "create as" in edit

--- decidim-proposals/app/forms/decidim/proposals/collaborative_draft_form.rb
+++ decidim-proposals/app/forms/decidim/proposals/collaborative_draft_form.rb
@@ -4,6 +4,13 @@ module Decidim
   module Proposals
     # A form object to be used when public users want to create a Collaborative Draft.
     class CollaborativeDraftForm < Decidim::Proposals::ProposalForm
+
+      def map_model(model)
+        self.user_group_id = nil
+        return unless model.categorization
+
+        self.category_id = model.categorization.decidim_category_id
+      end
     end
   end
 end
