6b7c6ba48c81c9cc1709052195cb89b339d32b4e
Marc Riera <mrc2407@gmail.com>
2018-05-21 11:37:01 +0200

    Rename migrations to avoid duplicated names (#3452)

---
+++ decidim-proposals/db/migrate/20171212102250_enable_pg_trgm_extension_for_proposals.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class EnablePgTrgmExtensionForProposals < ActiveRecord::Migration[5.1]
+  def change
+    return if extension_enabled?("pg_trgm")
+
+    begin
+      # required so that test suite works in ci env
+      enable_extension "pg_trgm"
+    rescue StandardError
+      raise <<-MSG.squish
+        Decidim requires the pg_trgm extension to be enabled in your PostgreSQL.
+        You can do so by running `CREATE EXTENSION IF NOT EXISTS "pg_trgm";` on the current DB as a PostgreSQL
+        super user.
+      MSG
+    end
+  end
+end
