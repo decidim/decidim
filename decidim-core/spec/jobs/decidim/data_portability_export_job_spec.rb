bae0fb18df0b9fc965ce94df66c1fec11d16abb9
Isaac Massot <isaac.mg@coditramuntana.com>
2018-05-24 12:14:59 +0200

    fix codeclimate complains

--- decidim-core/spec/jobs/decidim/data_portability_export_job_spec.rb
+++ decidim-core/spec/jobs/decidim/data_portability_export_job_spec.rb
@@ -4,7 +4,6 @@ require "spec_helper"
 
 module Decidim
   describe DataPortabilityExportJob do
-    # let!(:component) { create(:component, manifest_name: "dummy") }
     let(:organization) { create :organization }
     let!(:user) { create(:user, organization: organization) }
 
@@ -19,26 +18,5 @@ module Decidim
       expect(attachment.mime_type).to eq("application/zip")
       expect(attachment.filename).to match(/^exporter.*\.zip$/)
     end
-
-    # describe "CSV" do
-    #   # it "exports the data portability entities" do
-    #   #   exporter_double = double(export: true)
-    #   #   class_double = double(new: exporter_double)
-    #   #   expect(Decidim::Exporters::CSV)
-    #   #     .to(receive(:new).with(anything, DummySerializer))
-    #   #     .and_return(double(export: export_data))
-    #   #
-    #   # end
-    #
-    #   # let(:export_data) { [ ["contents1", Decidim::Exporters::CSV.new("content", "csv")],["contents2", Decidim::Exporters::CSV.new("content2", "csv")]] }
-    #   # it "uses the CSV exporter" do
-    #   #   #TODO make the test exporter
-    #   #
-    #   #   expect(ExportMailer)
-    #   #     .to(receive(:data_portability_export).with(user, anything, export_data))
-    #   #     .and_return(double(deliver_now: true))
-    #   #
-    #   # end
-    # end
   end
 end
