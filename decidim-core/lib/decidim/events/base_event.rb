dd23e3156e9579ff68e0260e2a79e388c9e92f02
Rubén González <rbngzlv@gmail.com>
2018-05-30 11:02:03 +0200

    Don't send notifications to users without access to the private space

--- decidim-core/lib/decidim/events/base_event.rb
+++ decidim-core/lib/decidim/events/base_event.rb
@@ -68,11 +68,14 @@ module Decidim
       # event to decide based on the params.
       #
       # It returns false when the resource or any element in the chain is a
-      # `Decidim::Publicable` and it isn't published.
+      # `Decidim::Publicable` and it isn't published or participatory_space
+      # is a `Decidim::Participable` and the user can't participate.
       def notifiable?
         return false if resource.is_a?(Decidim::Publicable) && !resource.published?
         return false if participatory_space.is_a?(Decidim::Publicable) && !participatory_space&.published?
         return false unless component&.published?
+
+        return false if participatory_space.is_a?(Decidim::Participable) && !participatory_space.can_participate?(user)
 
         true
       end
