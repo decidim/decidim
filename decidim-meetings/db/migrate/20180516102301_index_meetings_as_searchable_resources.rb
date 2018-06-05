75010d4623a3c251d172238bd7ffcbc674600d43
Oliver Valls <oliver.hv@coditramuntana.com>
2018-06-05 11:17:50 +0200

    Use a Fake Meeting class to avoid model changes to affect this migration.

--- decidim-meetings/db/migrate/20180516102301_index_meetings_as_searchable_resources.rb
+++ decidim-meetings/db/migrate/20180516102301_index_meetings_as_searchable_resources.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
 class IndexMeetingsAsSearchableResources < ActiveRecord::Migration[5.1]
+  class Meeting < ApplicationRecord
+    self.table_name = :decidim_meetings_meetings
+    include Decidim::Searchable
+
+    searchable_fields(
+      scope_id: :decidim_scope_id,
+      participatory_space: { component: :participatory_space },
+      A: :title,
+      D: [:description, :address],
+      datetime: :start_time
+    )
+  end
+
   def up
-    Decidim::Meetings::Meeting.find_each(&:add_to_index_as_search_resource)
+    Meeting.find_each(&:add_to_index_as_search_resource)
   end
 
   def down
