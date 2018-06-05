3b77adc41f4be0647b3a56da62ccab305b774682
Oliver Valls <oliver.vh@coditramuntana.com>
2018-06-05 17:25:10 +0200

    Try to fix the `ParseError at [row,col]:[2,1] Message: Content is not allowed in prolog.` from circle ci.

--- decidim-dev/lib/decidim/dev/common_rake.rb
+++ decidim-dev/lib/decidim/dev/common_rake.rb
@@ -5,7 +5,7 @@ require "rspec/core/rake_task"
 require "decidim/dev"
 
 RSpec::Core::RakeTask.new(:spec) do |t|
-  t.rspec_opts = "--format=doc -o ~/rspec/rspec.xml" if ENV["CI"]
+  t.rspec_opts = "--format doc -o ~/rspec/rspec.xml" if ENV["CI"]
 end
 task default: [:spec]
 
