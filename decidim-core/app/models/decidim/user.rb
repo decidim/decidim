92021dcf0984d1eaac615a2d121eaca196f046b4
Marc Riera <mrc2407@gmail.com>
2018-06-07 10:47:04 +0200

    Let users invite friends (#3593)
    * Let users invite friends
    
    * Add specs to the form
    
    * Add specs for the commands
    
    * Use `flash.now`
    
    * Use correct email
    
    * Use plain textarea for custom message in invitations
    
    * Add system specs
    
    * Normalize locales file
    
    * Rubocop lints

--- decidim-core/app/models/decidim/user.rb
+++ decidim-core/app/models/decidim/user.rb
@@ -30,7 +30,7 @@ module Decidim
     has_many :following_follows, foreign_key: "decidim_user_id", class_name: "Decidim::Follow", dependent: :destroy
 
     validates :name, presence: true, unless: -> { deleted? }
-    validates :nickname, presence: true, unless: -> { deleted? || managed? }, length: { maximum: Decidim::User.nickname_max_length }
+    validates :nickname, presence: true, unless: -> { deleted? || managed? || name.blank? }, length: { maximum: Decidim::User.nickname_max_length }
     validates :locale, inclusion: { in: :available_locales }, allow_blank: true
     validates :tos_agreement, acceptance: true, allow_nil: false, on: :create
     validates :tos_agreement, acceptance: true, if: :user_invited?
