6a528357ddf3855d9ad085a983207407a7bfbeef
Marc Riera Casals <mrc2407@gmail.com>
2018-06-07 11:01:41 +0200

    Fix processes specs

--- decidim-participatory_processes/spec/system/admin/admin_manages_participatory_processes_spec.rb
+++ decidim-participatory_processes/spec/system/admin/admin_manages_participatory_processes_spec.rb
@@ -108,7 +108,7 @@ describe "Admin manages participatory processes", versioning: true, type: :syste
 
     it "deletes a participatory_process" do
       click_link translated(participatory_process2.title)
-      accept_confirm { click_link "Destroy" }
+      accept_confirm { click_link "Delete" }
 
       expect(page).to have_admin_callout("successfully")
 
