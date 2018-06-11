ce09ae6bba871a7dbdfa647b08f3ebf48df277a3
Julie Simon <juliesimon.js@gmail.com>
2018-06-11 14:47:25 +0200

    Comment votes_weight

--- decidim-proposals/lib/decidim/extends/models/abilities/current_user_ability_extend.rb
+++ decidim-proposals/lib/decidim/extends/models/abilities/current_user_ability_extend.rb
@@ -1,10 +1,10 @@
-module CurrentUserAbilityExtend
-  def voting_enabled?
-    return unless current_settings
-    (current_settings.votes_enabled? || current_settings.votes_weight_enabled?) && !current_settings.votes_blocked?
-  end
-end
+# module CurrentUserAbilityExtend
+#   def voting_enabled?
+#     return unless current_settings
+#     (current_settings.votes_enabled? || current_settings.votes_weight_enabled?) && !current_settings.votes_blocked?
+#   end
+# end
 
-Decidim::Proposals::Abilities::CurrentUserAbility.class_eval do
-  prepend(CurrentUserAbilityExtend)
-end
+# Decidim::Proposals::Abilities::CurrentUserAbility.class_eval do
+#   prepend(CurrentUserAbilityExtend)
+# end
