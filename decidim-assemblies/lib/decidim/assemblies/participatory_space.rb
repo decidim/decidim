84bd4d615e025dd2654a0d835856b54c383b6f8e
Julie Simon <juliesimon.js@gmail.com>
2018-06-11 18:24:26 +0200

    Comment seed not working

--- decidim-assemblies/lib/decidim/assemblies/participatory_space.rb
+++ decidim-assemblies/lib/decidim/assemblies/participatory_space.rb
@@ -170,31 +170,31 @@ Decidim.register_participatory_space(:assemblies) do |participatory_space|
           )
         end
 
-        Decidim::AssemblyMember::POSITIONS.each do |position|
-          Decidim::AssemblyMember.create!(
-            full_name: Faker::Name.name,
-            gender: Faker::Lorem.word,
-            birthday: Faker::Date.birthday(18, 65),
-            birthplace: Faker::Demographic.demonym,
-            designation_date: Faker::Date.between(1.year.ago, 1.month.ago),
-            designation_mode: Faker::Lorem.word,
-            position: position,
-            position_other: position == "other" ? Faker::Job.position : nil,
-            assembly: current_assembly
-          )
-        end
+        # Decidim::AssemblyMember::POSITIONS.each do |position|
+        #   Decidim::AssemblyMember.create!(
+        #     full_name: Faker::Name.name,
+        #     gender: Faker::Lorem.word,
+        #     birthday: Faker::Date.birthday(18, 65),
+        #     birthplace: Faker::Demographic.demonym,
+        #     designation_date: Faker::Date.between(1.year.ago, 1.month.ago),
+        #     designation_mode: Faker::Lorem.word,
+        #     position: position,
+        #     position_other: position == "other" ? Faker::Job.position : nil,
+        #     assembly: current_assembly
+        #   )
+        # end
 
-        Decidim::AssemblyMember.create!(
-          user: current_assembly.organization.users.first,
-          gender: Faker::Lorem.word,
-          birthday: Faker::Date.birthday(18, 65),
-          birthplace: Faker::Demographic.demonym,
-          designation_date: Faker::Date.between(1.year.ago, 1.month.ago),
-          designation_mode: Faker::Lorem.word,
-          position: "other",
-          position_other: Faker::Job.position,
-          assembly: current_assembly
-        )
+        # Decidim::AssemblyMember.create!(
+        #   user: current_assembly.organization.users.first,
+        #   gender: Faker::Lorem.word,
+        #   birthday: Faker::Date.birthday(18, 65),
+        #   birthplace: Faker::Demographic.demonym,
+        #   designation_date: Faker::Date.between(1.year.ago, 1.month.ago),
+        #   designation_mode: Faker::Lorem.word,
+        #   position: "other",
+        #   position_other: Faker::Job.position,
+        #   assembly: current_assembly
+        # )
 
         Decidim.component_manifests.each do |manifest|
           manifest.seed!(current_assembly.reload)
