c7a2f2a959e4622e01c7ca79a883ca65bb20666b
Marc Riera Casals <mrc2407@gmail.com>
2018-06-26 10:58:27 +0200

    Add new profile avatar version so that images do not look scaled

--- decidim-core/app/uploaders/decidim/avatar_uploader.rb
+++ decidim-core/app/uploaders/decidim/avatar_uploader.rb
@@ -7,11 +7,15 @@ module Decidim
 
     process :validate_dimensions
 
-    version :big do
+    version :profile do
+      process resize_to_fill: [268, 320]
+    end
+
+    version :big, from_version: :profile do
       process resize_to_fit: [80, 80]
     end
 
-    version :thumb do
+    version :thumb, from_version: :big do
       process resize_to_fit: [40, 40]
     end
 
