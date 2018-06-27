a2c88e757578ae140579de5498c1dceeaed31e23
Marc Riera <mrc2407@gmail.com>
2018-06-27 11:33:55 +0200

    Fix Coauthorships migration to handle official proposals (#3721)

--- decidim-proposals/db/migrate/20180529110230_move_authorships_to_coauthorships.rb
+++ decidim-proposals/db/migrate/20180529110230_move_authorships_to_coauthorships.rb
@@ -14,6 +14,9 @@ class MoveAuthorshipsToCoauthorships < ActiveRecord::Migration[5.1]
     proposals.each do |proposal|
       author_id = proposal.attributes["decidim_author_id"]
       user_group_id = proposal.attributes["decidim_user_group_id"]
+
+      next if author_id.nil?
+
       Coauthorship.create!(
         coauthorable_id: proposal.id,
         coauthorable_type: "Decidim::Proposals::Proposal",
