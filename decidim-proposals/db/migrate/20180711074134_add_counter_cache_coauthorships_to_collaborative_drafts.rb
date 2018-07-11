f6fdf88cb93705280a799ca116613e04b5c703ba
Agustí B.R <agusti.br@coditramuntana.com>
2018-07-11 09:48:04 +0200

    Add Counter Cache Coauthorships To CollaborativeDrafts

---
+++ decidim-proposals/db/migrate/20180711074134_add_counter_cache_coauthorships_to_collaborative_drafts.rb
@@ -0,0 +1,5 @@
+class AddCounterCacheCoauthorshipsToCollaborativeDrafts < ActiveRecord::Migration[5.2]
+  def change
+    add_column :decidim_proposals_collaborative_drafts, :coauthorships_count, :integer, null: false, default: 0
+  end
+end
