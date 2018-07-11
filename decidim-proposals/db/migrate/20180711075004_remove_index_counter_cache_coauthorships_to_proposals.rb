2eb878fe17a964bdced5074e5cc2dd018be2fbcd
Agustí B.R <agusti.br@coditramuntana.com>
2018-07-11 09:52:57 +0200

    Remove Index Counter Cache Coauthorships To Proposals

---
+++ decidim-proposals/db/migrate/20180711075004_remove_index_counter_cache_coauthorships_to_proposals.rb
@@ -0,0 +1,5 @@
+class RemoveIndexCounterCacheCoauthorshipsToProposals < ActiveRecord::Migration[5.2]
+  def change
+    remove_index :decidim_proposals_proposals, name: "idx_decidim_proposals_proposals_on_proposal_coauthorships_count"
+  end
+end
