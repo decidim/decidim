09315b83010cd70b59b5193b86d0799791e5a415
armandfardeau <fardeauarmand@gmail.com>
2018-07-16 14:43:33 +0200

    :green_heart: Fix NotificationMailer on Decidim-core
    Revert commits squashed in 14d130f9c52b73b9e37f0e1f2a3e4d8016b146de :
    + ./spec/services/decidim/email_notification_generator_spec.rb:39
    \# Decidim::EmailNotificationGenerator
    \# generate when the event_class supports emails
    \# when the user wants emails for notifications
    \# schedules a job for each recipient
    --> refactoring NotificationMailer extends

--- decidim-core/lib/decidim/extends/notification_mailer_extend.rb
+++ decidim-core/lib/decidim/extends/notification_mailer_extend.rb
@@ -1,86 +1,8 @@
 Decidim::NotificationMailer.class_eval do
-
-  def event_received(event, event_class_name, resource, user, extra)
-    return unless user.email
-
-    with_user(user) do
-
-      if @extra[:new_content]
-        @organization = user.organization
-        event_class = event_class_name.constantize
-        @event_instance = event_class.new(resource: resource, event_name: event, user: user, extra: extra)
-        subject = @event_instance.email_subject
-      else
-        @organization = resource.organization
-        event_class = event_class_name.constantize
-        @event_instance = event_class.new(resource: resource, event_name: event, user: user, extra: extra)
-        @slug = extra[:process_slug]
-        @locale = locale.to_s
-        subject = @event_instance.email_subject
-        @parent_title = parent_title(resource, event_class)
-        @resource_title = resource.try(:title)
-        @body = body(event_class, resource, extra)
-        @is_comment = is_comment?(event_class)
-        @translatable =
-          if resource.is_a?(Decidim::Budgets::Project) || resource.is_a?(Decidim::Meetings::Meeting)
-            true
-          elsif resource.is_a?(Decidim::Proposals::Proposal)
-            if event_class_name == "Decidim::Proposals::ProposalCreatedEvent"
-              true
-            else
-              false
-            end
-          end
-      end
-
-      mail(from: Decidim.config.mailer_sender, to: user.email, subject: subject)
-      
-    end
-  end
-
   def new_content_received(event, event_class_name, resource, user, extra)
     with_user(user) do
       @organization = resource.organization
-      event_class = event_class_name.constantize
-      @event_instance = event_class.new(resource: resource, event_name: event, user: user, extra: extra)
-      @slug = extra[:process_slug]
-      @locale = locale.to_s
-      subject = @event_instance.email_subject
-      @parent_title = parent_title(resource, event_class)
-      @resource_title = resource.try(:title)
-      @body = body(event_class, resource, extra)
-      @is_comment = is_comment?(event_class)
-      @translatable =
-        if resource.is_a?(Decidim::Budgets::Project) || resource.is_a?(Decidim::Meetings::Meeting)
-          true
-        elsif resource.is_a?(Decidim::Proposals::Proposal)
-          if event_class_name == "Decidim::Proposals::ProposalCreatedEvent"
-            true
-          else
-            false
-          end
-        end
-      mail(from: Decidim.config.mailer_sender, to: user.email, subject: subject)
-    end
-  end
 
-  def is_comment?(event_class)
-    event_class == Decidim::Comments::CommentCreatedEvent
-  end
-
-  def parent_title(resource, event_class)
-    if is_comment?(event_class)
-      resource.title
-    else
-      resource.feature.participatory_space.title
-    end
-  end
-
-  def body(event_class, resource, extra)
-    if is_comment?(event_class)
-      Decidim::Comments::Comment.find(extra[:comment_id]).body
-    else
-      resource.body
     end
   end
 end
