f04da6cbb6ed1f4f0e976fa2a78b6df746949443
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2018-07-16 14:07:36 +0200

    Order by ID by default

--- decidim-surveys/app/models/decidim/surveys/survey_answer_option.rb
+++ decidim-surveys/app/models/decidim/surveys/survey_answer_option.rb
@@ -3,7 +3,7 @@
 module Decidim
   module Surveys
     class SurveyAnswerOption < Surveys::ApplicationRecord
-      default_scope order(arel_table[:created_at].asc)
+      default_scope { order(arel_table[:id].asc) }
 
       belongs_to :question, class_name: "SurveyQuestion", foreign_key: "decidim_survey_question_id"
     end
