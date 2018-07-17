f8a27bcb42a2f40d6ad97eccc6e49b11630d8a38
Agustí B.R <agusti.br@coditramuntana.com>
2018-07-17 17:35:22 +0200

    fix create proposal spec

--- decidim-proposals/spec/commands/decidim/proposals/admin_create_proposal_spec.rb
+++ decidim-proposals/spec/commands/decidim/proposals/admin_create_proposal_spec.rb
@@ -26,14 +26,6 @@ module Decidim
         let(:longitude) { 2.1234 }
         let(:attachment_params) { nil }
 
-        if with_author
-          let(:author) { create(:user, organization: organization) }
-
-          let(:user_group) do
-            create(:user_group, :verified, organization: organization, users: [author])
-          end
-        end
-
         describe "call" do
           let(:form_params) do
             {
@@ -42,16 +34,12 @@ module Decidim
               address: address,
               has_address: has_address,
               attachment: attachment_params,
-              user_group_id: (with_author ? user_group.try(:id) : nil)
+              user_group_id: nil
             }
           end
 
           let(:command) do
-            if with_author
-              described_class.new(form, author)
-            else
-              described_class.new(form)
-            end
+            described_class.new(form)
           end
 
           describe "when the form is not valid" do
@@ -81,112 +69,15 @@ module Decidim
               end.to change(Decidim::Proposals::Proposal, :count).by(1)
             end
 
-            if with_author
-              context "with an author" do
-                let(:user_group) { nil }
+            it "traces the action", versioning: true do
+              expect(Decidim.traceability)
+                .to receive(:create!)
+                .with(Decidim::Proposals::Proposal, kind_of(Decidim::User), kind_of(Hash))
+                .and_call_original
 
-                it "sets the author" do
-                  command.call
-                  proposal = Decidim::Proposals::Proposal.last
-                  creator = proposal.creator
-
-                  expect(creator.author).to eq(author)
-                  expect(creator.user_group).to eq(nil)
-                end
-
-                it "adds the author as a follower" do
-                  command.call
-                  proposal = Decidim::Proposals::Proposal.last
-
-                  expect(proposal.followers).to include(author)
-                end
-
-                context "with a proposal limit" do
-                  let(:component) do
-                    create(:proposal_component, settings: { "proposal_limit" => 2 })
-                  end
-
-                  it "checks the author doesn't exceed the amount of proposals" do
-                    expect { command.call }.to broadcast(:ok)
-                    expect { command.call }.to broadcast(:ok)
-                    expect { command.call }.to broadcast(:invalid)
-                  end
-                end
-              end
-
-              context "with a user group" do
-                it "sets the user group" do
-                  command.call
-                  proposal = Decidim::Proposals::Proposal.last
-                  creator = proposal.creator
-
-                  expect(creator.author).to eq(author)
-                  expect(creator.user_group).to eq(user_group)
-                end
-
-                context "with a proposal limit" do
-                  let(:component) do
-                    create(:proposal_component, settings: { "proposal_limit" => 2 })
-                  end
-
-                  before do
-                    create_list(:proposal, 2, component: component, users: [author])
-                  end
-
-                  it "checks the user group doesn't exceed the amount of proposals independently of the author" do
-                    expect { command.call }.to broadcast(:ok)
-                    expect { command.call }.to broadcast(:ok)
-                    expect { command.call }.to broadcast(:invalid)
-                  end
-                end
-              end
-
-              describe "the proposal limit excludes withdrawn proposals" do
-                let(:component) do
-                  create(:proposal_component, settings: { "proposal_limit" => 1 })
-                end
-
-                describe "when the author is a user" do
-                  let(:user_group) { nil }
-
-                  before do
-                    create(:proposal, :withdrawn, users: [author], component: component)
-                  end
-
-                  it "checks the user doesn't exceed the amount of proposals" do
-                    expect { command.call }.to broadcast(:ok)
-                    expect { command.call }.to broadcast(:invalid)
-
-                    user_proposal_count = Decidim::Coauthorship.where(author: author, coauthorable_type: "Decidim::Proposals::Proposal").count
-                    expect(user_proposal_count).to eq(2)
-                  end
-                end
-
-                describe "when the author is a user_group" do
-                  before do
-                    create(:proposal, :withdrawn, users: [author], user_groups: [user_group], component: component)
-                  end
-
-                  it "checks the user_group doesn't exceed the amount of proposals" do
-                    expect { command.call }.to broadcast(:ok)
-                    expect { command.call }.to broadcast(:invalid)
-
-                    user_group_proposal_count = Decidim::Coauthorship.where(user_group: user_group, coauthorable_type: "Decidim::Proposals::Proposal").count
-                    expect(user_group_proposal_count).to eq(2)
-                  end
-                end
-              end
-            else
-              it "traces the action", versioning: true do
-                expect(Decidim.traceability)
-                  .to receive(:create!)
-                  .with(Decidim::Proposals::Proposal, kind_of(Decidim::User), kind_of(Hash))
-                  .and_call_original
-
-                expect { command.call }.to change(Decidim::ActionLog, :count)
-                action_log = Decidim::ActionLog.last
-                expect(action_log.version).to be_present
-              end
+              expect { command.call }.to change(Decidim::ActionLog, :count)
+              action_log = Decidim::ActionLog.last
+              expect(action_log.version).to be_present
             end
 
             context "when geocoding is enabled" do
--- decidim-proposals/spec/commands/decidim/proposals/create_proposal_spec.rb
+++ decidim-proposals/spec/commands/decidim/proposals/create_proposal_spec.rb
@@ -20,12 +20,10 @@ module Decidim
         )
       end
 
-      if with_author
-        let(:author) { create(:user, organization: organization) }
+      let(:author) { create(:user, organization: organization) }
 
-        let(:user_group) do
-          create(:user_group, :verified, organization: organization, users: [author])
-        end
+      let(:user_group) do
+        create(:user_group, :verified, organization: organization, users: [author])
       end
 
       describe "call" do
@@ -33,16 +31,12 @@ module Decidim
           {
             title: "A reasonable proposal title",
             body: "A reasonable proposal body",
-            user_group_id: (with_author ? user_group.try(:id) : nil)
+            user_group_id: user_group.try(:id)
           }
         end
 
         let(:command) do
-          if with_author
-            described_class.new(form, author)
-          else
-            described_class.new(form)
-          end
+          described_class.new(form, author)
         end
 
         describe "when the form is not valid" do
@@ -72,111 +66,98 @@ module Decidim
             end.to change(Decidim::Proposals::Proposal, :count).by(1)
           end
 
-          if with_author
-            context "with an author" do
+          context "with an author" do
+            let(:user_group) { nil }
+
+            it "sets the author" do
+              command.call
+              proposal = Decidim::Proposals::Proposal.last
+              creator = proposal.creator
+
+              expect(creator.author).to eq(author)
+              expect(creator.user_group).to eq(nil)
+            end
+
+            it "adds the author as a follower" do
+              command.call
+              proposal = Decidim::Proposals::Proposal.last
+
+              expect(proposal.followers).to include(author)
+            end
+
+            context "with a proposal limit" do
+              let(:component) do
+                create(:proposal_component, settings: { "proposal_limit" => 2 })
+              end
+
+              it "checks the author doesn't exceed the amount of proposals" do
+                expect { command.call }.to broadcast(:ok)
+                expect { command.call }.to broadcast(:ok)
+                expect { command.call }.to broadcast(:invalid)
+              end
+            end
+          end
+
+          context "with a user group" do
+            it "sets the user group" do
+              command.call
+              proposal = Decidim::Proposals::Proposal.last
+              creator = proposal.creator
+
+              expect(creator.author).to eq(author)
+              expect(creator.user_group).to eq(user_group)
+            end
+
+            context "with a proposal limit" do
+              let(:component) do
+                create(:proposal_component, settings: { "proposal_limit" => 2 })
+              end
+
+              before do
+                create_list(:proposal, 2, component: component, users: [author])
+              end
+
+              it "checks the user group doesn't exceed the amount of proposals independently of the author" do
+                expect { command.call }.to broadcast(:ok)
+                expect { command.call }.to broadcast(:ok)
+                expect { command.call }.to broadcast(:invalid)
+              end
+            end
+          end
+
+          describe "the proposal limit excludes withdrawn proposals" do
+            let(:component) do
+              create(:proposal_component, settings: { "proposal_limit" => 1 })
+            end
+
+            describe "when the author is a user" do
               let(:user_group) { nil }
 
-              it "sets the author" do
-                command.call
-                proposal = Decidim::Proposals::Proposal.last
-                creator = proposal.creator
-
-                expect(creator.author).to eq(author)
-                expect(creator.user_group).to eq(nil)
+              before do
+                create(:proposal, :withdrawn, users: [author], component: component)
               end
 
-              it "adds the author as a follower" do
-                command.call
-                proposal = Decidim::Proposals::Proposal.last
+              it "checks the user doesn't exceed the amount of proposals" do
+                expect { command.call }.to broadcast(:ok)
+                expect { command.call }.to broadcast(:invalid)
 
-                expect(proposal.followers).to include(author)
-              end
-
-              context "with a proposal limit" do
-                let(:component) do
-                  create(:proposal_component, settings: { "proposal_limit" => 2 })
-                end
-
-                it "checks the author doesn't exceed the amount of proposals" do
-                  expect { command.call }.to broadcast(:ok)
-                  expect { command.call }.to broadcast(:ok)
-                  expect { command.call }.to broadcast(:invalid)
-                end
+                user_proposal_count = Decidim::Coauthorship.where(author: author, coauthorable_type: "Decidim::Proposals::Proposal").count
+                expect(user_proposal_count).to eq(2)
               end
             end
 
-            context "with a user group" do
-              it "sets the user group" do
-                command.call
-                proposal = Decidim::Proposals::Proposal.last
-                creator = proposal.creator
-
-                expect(creator.author).to eq(author)
-                expect(creator.user_group).to eq(user_group)
+            describe "when the author is a user_group" do
+              before do
+                create(:proposal, :withdrawn, users: [author], user_groups: [user_group], component: component)
               end
 
-              context "with a proposal limit" do
-                let(:component) do
-                  create(:proposal_component, settings: { "proposal_limit" => 2 })
-                end
+              it "checks the user_group doesn't exceed the amount of proposals" do
+                expect { command.call }.to broadcast(:ok)
+                expect { command.call }.to broadcast(:invalid)
 
-                before do
-                  create_list(:proposal, 2, component: component, users: [author])
-                end
-
-                it "checks the user group doesn't exceed the amount of proposals independently of the author" do
-                  expect { command.call }.to broadcast(:ok)
-                  expect { command.call }.to broadcast(:ok)
-                  expect { command.call }.to broadcast(:invalid)
-                end
+                user_group_proposal_count = Decidim::Coauthorship.where(user_group: user_group, coauthorable_type: "Decidim::Proposals::Proposal").count
+                expect(user_group_proposal_count).to eq(2)
               end
-            end
-
-            describe "the proposal limit excludes withdrawn proposals" do
-              let(:component) do
-                create(:proposal_component, settings: { "proposal_limit" => 1 })
-              end
-
-              describe "when the author is a user" do
-                let(:user_group) { nil }
-
-                before do
-                  create(:proposal, :withdrawn, users: [author], component: component)
-                end
-
-                it "checks the user doesn't exceed the amount of proposals" do
-                  expect { command.call }.to broadcast(:ok)
-                  expect { command.call }.to broadcast(:invalid)
-
-                  user_proposal_count = Decidim::Coauthorship.where(author: author, coauthorable_type: "Decidim::Proposals::Proposal").count
-                  expect(user_proposal_count).to eq(2)
-                end
-              end
-
-              describe "when the author is a user_group" do
-                before do
-                  create(:proposal, :withdrawn, users: [author], user_groups: [user_group], component: component)
-                end
-
-                it "checks the user_group doesn't exceed the amount of proposals" do
-                  expect { command.call }.to broadcast(:ok)
-                  expect { command.call }.to broadcast(:invalid)
-
-                  user_group_proposal_count = Decidim::Coauthorship.where(user_group: user_group, coauthorable_type: "Decidim::Proposals::Proposal").count
-                  expect(user_group_proposal_count).to eq(2)
-                end
-              end
-            end
-          else
-            it "traces the action", versioning: true do
-              expect(Decidim.traceability)
-                .to receive(:create!)
-                .with(Decidim::Proposals::Proposal, kind_of(Decidim::User), kind_of(Hash))
-                .and_call_original
-
-              expect { command.call }.to change(Decidim::ActionLog, :count)
-              action_log = Decidim::ActionLog.last
-              expect(action_log.version).to be_present
             end
           end
         end
