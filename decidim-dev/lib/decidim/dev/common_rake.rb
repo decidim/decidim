b0d4706fca5dacb21abe6b1f756561f1ec43af28
Rubén González <rbngzlv@gmail.com>
2018-07-25 08:26:12 +0200

    Save runtime logs to cache

--- decidim-dev/lib/decidim/dev/common_rake.rb
+++ decidim-dev/lib/decidim/dev/common_rake.rb
@@ -6,6 +6,7 @@ require "parallel_tests/tasks"
 require "decidim/dev"
 
 RSpec::Core::RakeTask.new(:spec) do |t|
+  # This options aren't loaded in CI (we are using parallel_tests gem)
   t.rspec_opts = "--format progress --format RspecJunitFormatter -o ~/rspec/rspec.xml" if ENV["CI"]
 end
 task default: [:spec]
