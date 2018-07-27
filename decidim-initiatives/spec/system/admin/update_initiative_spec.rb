f2da769cc2a737cac343a91dfe9c0764883ff381
Oliver Valls <tramuntanal@gmail.com>
2018-07-27 08:38:43 +0200

    Fix initiative edition when state is not published. (#3930)
    * Fix initiative edition when state is not published.
    
    * Added Changelog entry.

--- decidim-initiatives/spec/system/admin/update_initiative_spec.rb
+++ decidim-initiatives/spec/system/admin/update_initiative_spec.rb
@@ -5,23 +5,47 @@ require "spec_helper"
 describe "User prints the initiative", type: :system do
   include_context "when admins initiative"
 
+  def submit_and_validate
+    find("*[type=submit]").click
+
+    within ".callout-wrapper" do
+      expect(page).to have_content("successfully")
+    end
+  end
+
   context "when initiative update" do
     before do
       switch_to_host(organization.host)
       login_as user, scope: :user
       visit decidim_admin_initiatives.initiatives_path
-      page.find(".action-icon--edit").click
     end
 
-    it "Updates initiative data" do
+    it "Updates published initiative data" do
+      page.find(".action-icon--edit").click
       within ".edit_initiative" do
         fill_in :initiative_hashtag, with: "#hashtag"
       end
+      submit_and_validate
+    end
 
-      find("*[type=submit]").click
+    context "when initiative is in accepted state" do
+      before do
+        initiative.accepted!
+      end
 
-      within ".callout-wrapper" do
-        expect(page).to have_content("successfully")
+      it "updates accepted initiative data" do
+        page.find(".action-icon--edit").click
+        within ".edit_initiative" do
+          fill_in_i18n_editor(
+            :initiative_answer,
+            "#initiative-answer-tabs",
+            ca: "Alguna resposta",
+            en: "Some answer",
+            es: "Alguna respuesta"
+          )
+          fill_in :initiative_answer_url, with: "http://meta.decidim.org"
+        end
+        submit_and_validate
       end
     end
   end
