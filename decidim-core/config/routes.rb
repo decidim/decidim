e205b6c6da4efc605078a690b05a3111cba4f6af
Agustí B.R <agusti.br@coditramuntana.com>
2018-07-27 15:02:00 +0200

    Amendment model, Amendable concern, amendments_list, emendation_announcement, emendation_actions ...

--- decidim-core/config/routes.rb
+++ decidim-core/config/routes.rb
@@ -108,6 +108,10 @@ Decidim::Core::Engine.routes.draw do
     collection do
       post :create
     end
+    member do
+      get :review
+      patch :accept
+    end
   end
 
   namespace :gamification do
