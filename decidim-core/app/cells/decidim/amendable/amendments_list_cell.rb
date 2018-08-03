e4c21cfd95bad964df1650321282f5b56c5c3716
Agustí B.R <agusti.br@coditramuntana.com>
2018-08-03 13:43:37 +0200

    Refactored due to review

---
+++ decidim-core/app/cells/decidim/amendable/amendments_list_cell.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Decidim::Amendable
+  # This cell renders the button to amend the given resource.
+  class AmendmentsListCell < Decidim::ViewModel
+    include Decidim::LayoutHelper
+    include Decidim::CardHelper
+
+    private
+
+    def has_actions?
+      false
+    end
+
+    def model_name
+      model.model_name.human
+    end
+
+    def current_user
+      context[:current_user]
+    end
+
+    def current_component
+      model.first.component
+    end
+  end
+end
---
+++ decidim-core/app/forms/decidim/create_amend_form.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Decidim
+  # A form object to be used when users want to amend an amendable resource.
+  class CreateAmendForm < Decidim::Form
+    mimic :amend
+
+    attribute :amendable_gid, String
+    attribute :title, String
+    attribute :body, String
+
+    validates :amendable_gid, :amendable_type, :amender, presence: true
+
+    def amendable
+      @amendable ||= GlobalID::Locator.locate_signed amendable_gid
+    end
+
+    def amendable_type
+      amendable.resource_manifest.model_class_name
+    end
+
+    def emendation_type
+      amendable_type
+    end
+
+    def amender
+      current_user
+    end
+
+    # def title
+    #   amendable.title
+    # end
+    #
+    # def body
+    #   amendable.body
+    # end
+  end
+end
