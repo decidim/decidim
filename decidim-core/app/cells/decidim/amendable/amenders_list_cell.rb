03dc80cc37e4ff1015e2097344ff5035c8715762
Agustí B.R <agusti.br@coditramuntana.com>
2018-08-09 18:05:01 +0200

    Add Amended by list in amendable detail

---
+++ decidim-core/app/cells/decidim/amendable/amenders_list_cell.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Decidim::Amendable
+  # This cell renders a list with amenders of the given resource.
+  class AmendersListCell < Decidim::ViewModel
+  end
+end
