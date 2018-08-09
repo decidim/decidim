997667f758a14839495450273b9986ba4a55f693
Marc Riera Casals <mrc2407@gmail.com>
2018-08-09 15:26:04 +0200

    Fix specs

--- decidim-core/spec/models/decidim/content_block_spec.rb
+++ decidim-core/spec/models/decidim/content_block_spec.rb
@@ -14,21 +14,32 @@ module Decidim
       end
     end
 
-    describe ".images" do
-      context "when the related attachment does not exist" do
+    describe ".images_container" do
+      it "responds to the image names" do
+        expect(subject.images_container).to respond_to(:background_image)
+      end
+
+      context "when the image has not been uploaded" do
         it "returns nil" do
-          expect(subject.images.hero_background_image).to be_nil
+          expect(subject.images_container.background_image.url).to be_nil
         end
       end
 
       context "when the related attachment exists" do
+        let(:original_image) do
+          Rack::Test::UploadedFile.new(
+            Decidim::Dev.test_file("city.jpeg", "image/jpeg"),
+            "image/jpg"
+          )
+        end
+
         before do
-          create(:attachment, attached_to: subject, title: { name: :hero_background_image })
+          subject.images_container.background_image = original_image
+          subject.save
         end
 
         it "returns nil" do
-          expect(subject.images.hero_background_image).to be_kind_of(Decidim::Attachment)
-          expect(subject.attachments.count).to eq 1
+          expect(subject.images_container.background_image).not_to be_nil
         end
       end
     end
