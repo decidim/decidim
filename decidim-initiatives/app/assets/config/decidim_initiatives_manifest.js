3eb0578ca8825f2bfec5f1f9786d9df6288f870e
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2018-08-28 17:37:40 +0200

    Fix asset linking

--- decidim-initiatives/app/assets/config/decidim_initiatives_manifest.js
+++ decidim-initiatives/app/assets/config/decidim_initiatives_manifest.js
@@ -3,3 +3,4 @@
 //= link decidim/initiatives/identity_selector_dialog.js
 //= link decidim/initiatives/scoped_type.js
 //= link decidim/initiatives/admin/invite_users.js
+//= link_tree ../images/decidim
