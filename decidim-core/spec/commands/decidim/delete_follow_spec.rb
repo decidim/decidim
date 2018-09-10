a030c098b4c41f9e940f315ca6d5359db9f0c1af
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2018-09-10 14:50:32 +0200

    Ad followers badge

---
+++ decidim-core/spec/commands/decidim/delete_follow_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  describe DeleteFollow do
+    let!(:organization) { create(:organization) }
+    let!(:user1) { create(:user, organization: organization) }
+    let!(:user2) { create(:user, organization: organization) }
+    let!(:follow) { create(:follow, user: user1, followable: user2) }
+
+    let(:form) { double(follow: follow, invalid?: false) }
+
+    it "destroys a follow" do
+      expect { described_class.new(form, user1).call }.to broadcast(:ok)
+
+      expect(user2.reload.followers).not_to include(user1)
+    end
+
+    context "gamification" do
+      before do
+        Decidim::Gamification.set_score(user1, :followers, 10)
+        Decidim::Gamification.set_score(user2, :followers, 10)
+      end
+
+      it "decrements the user's score" do
+        described_class.new(form, user1).call
+
+        expect(Decidim::Gamification.status_for(user1, :followers).score).to eq(10)
+        expect(Decidim::Gamification.status_for(user2, :followers).score).to eq(9)
+      end
+    end
+  end
+end
