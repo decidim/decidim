8910de2335beef6a2867aa9652c1ce6f4fa0669a
armandfardeau <fardeauarmand@gmail.com>
2018-09-10 12:26:22 +0200

    Import extends

--- decidim-budgets/config/initializers/extends.rb
+++ decidim-budgets/config/initializers/extends.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
-require "decidim/extends/project_extend.rb"
+require "decidim/extends/models/project_extend.rb"
+require "decidim/extends/models/order_extend.rb"
+require "decidim/extends/commands/create_project_extend.rb"
+require "decidim/extends/commands/update_project_extend.rb"
+require "decidim/extends/forms/project_form_extend.rb"
