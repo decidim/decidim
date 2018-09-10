e5208e0d8cfc8ef4a0b8c0cbdedf5873c8c1d08b
Marc Riera Casals <mrc2407@gmail.com>
2018-09-10 11:26:41 +0200

    Fix class name

--- decidim-core/db/migrate/20180810092428_move_organization_fields_to_hero_content_block.rb
+++ decidim-core/db/migrate/20180810092428_move_organization_fields_to_hero_content_block.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class MoveOrganizationFieldsToHeroContentBlock < ActiveRecord::Migration[5.2]
-  class ::Decidim::Organization < ApplicationRecord
+  class ::Decidim::Organization < ::Decidim::ApplicationRecord
     mount_uploader :homepage_image, ::Decidim::HomepageImageUploader
   end
 
