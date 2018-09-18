e01e5c4300cbf1807ab09c48cba438fa640d2679
armandfardeau <fardeauarmand@gmail.com>
2018-09-18 11:19:32 +0200

    fix typo

--- decidim-proposals/app/forms/decidim/proposals/admin/proposal_form.rb
+++ decidim-proposals/app/forms/decidim/proposals/admin/proposal_form.rb
@@ -1,74 +1,76 @@
 # frozen_string_literal: true
 
 module Decidim
-  module Admin
-    # A form object to be used when admin users want to create a proposal.
-    class ProposalForm < Decidim::Form
-      mimic :proposal
+  module Proposals
+    module Admin
+      # A form object to be used when admin users want to create a proposal.
+      class ProposalForm < Decidim::Form
+        mimic :proposal
 
-      include Decidim::Proposals::Concerns::FormattedAttributes
+        include Decidim::Proposals::Concerns::FormattedAttributes
 
-      attribute :title, String
-      attribute :body, String
-      attribute :address, String
-      attribute :latitude, Float
-      attribute :longitude, Float
-      attribute :category_id, Integer
-      attribute :scope_id, Integer
-      attribute :attachment, AttachmentForm
+        attribute :title, String
+        attribute :body, String
+        attribute :address, String
+        attribute :latitude, Float
+        attribute :longitude, Float
+        attribute :category_id, Integer
+        attribute :scope_id, Integer
+        attribute :attachment, AttachmentForm
 
-      validates :title, :body, presence: true
-      validates :address, geocoding: true, if: -> { current_component.settings.geocoding_enabled? }
-      validates :category, presence: true, if: ->(form) { form.category_id.present? }
-      validates :scope, presence: true, if: ->(form) { form.scope_id.present? }
+        validates :title, :body, presence: true
+        validates :address, geocoding: true, if: -> {current_component.settings.geocoding_enabled?}
+        validates :category, presence: true, if: ->(form) {form.category_id.present?}
+        validates :scope, presence: true, if: ->(form) {form.scope_id.present?}
 
-      validate :scope_belongs_to_participatory_space_scope
+        validate :scope_belongs_to_participatory_space_scope
 
-      validate :notify_missing_attachment_if_errored
+        validate :notify_missing_attachment_if_errored
 
-      delegate :categories, to: :current_component
+        delegate :categories, to: :current_component
 
-      def map_model(model)
-        return unless model.categorization
+        def map_model(model)
+          return unless model.categorization
 
-        self.category_id = model.categorization.decidim_category_id
-      end
+          self.category_id = model.categorization.decidim_category_id
+        end
 
-      alias component current_component
+        alias component current_component
 
-      # Finds the Category from the category_id.
-      #
-      # Returns a Decidim::Category
-      def category
-        @category ||= categories.find_by(id: category_id)
-      end
+        # Finds the Category from the category_id.
+        #
+        # Returns a Decidim::Category
+        def category
+          @category ||= categories.find_by(id: category_id)
+        end
 
-      # Finds the Scope from the given decidim_scope_id, uses participatory space scope if missing.
-      #
-      # Returns a Decidim::Scope
-      def scope
-        @scope ||= @scope_id ? current_participatory_space.scopes.find_by(id: @scope_id) : current_participatory_space.scope
-      end
+        # Finds the Scope from the given decidim_scope_id, uses participatory space scope if missing.
+        #
+        # Returns a Decidim::Scope
+        def scope
+          @scope ||= @scope_id ? current_participatory_space.scopes.find_by(id: @scope_id) : current_participatory_space.scope
+        end
 
-      # Scope identifier
-      #
-      # Returns the scope identifier related to the proposal
-      def scope_id
-        @scope_id || scope&.id
-      end
+        # Scope identifier
+        #
+        # Returns the scope identifier related to the proposal
+        def scope_id
+          @scope_id || scope&.id
+        end
 
-      private
+        private
 
-      def scope_belongs_to_participatory_space_scope
-        errors.add(:scope_id, :invalid) if current_participatory_space.out_of_scope?(scope)
-      end
+        def scope_belongs_to_participatory_space_scope
+          errors.add(:scope_id, :invalid) if current_participatory_space.out_of_scope?(scope)
+        end
 
-      # This method will add an error to the `attachment` field only if there's
-      # any error in any other field. This is needed because when the form has
-      # an error, the attachment is lost, so we need a way to inform the user of
-      # this problem.
-      def notify_missing_attachment_if_errored
-        errors.add(:attachment, :needs_to_be_reattached) if errors.any? && attachment.present?
+        # This method will add an error to the `attachment` field only if there's
+        # any error in any other field. This is needed because when the form has
+        # an error, the attachment is lost, so we need a way to inform the user of
+        # this problem.
+        def notify_missing_attachment_if_errored
+          errors.add(:attachment, :needs_to_be_reattached) if errors.any? && attachment.present?
+        end
       end
     end
   end
