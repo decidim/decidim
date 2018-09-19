8dc31359a28d3bdd04e188ec0c5557cf77b1fc63
Oriol Gual <oriol@codegram.com>
2018-09-19 12:55:03 +0200

    Add all activities view

---
+++ decidim-core/app/controllers/decidim/last_activities_controller.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Decidim
+  # The controller to handle the user's public profile page.
+  class LastActivitiesController < Decidim::ApplicationController
+    include FilterResource
+    include Paginable
+
+    helper Decidim::ResourceHelper
+    helper Decidim::FiltersHelper
+    helper_method :activities
+
+    def activities
+      @activities ||= paginate(search.results)
+    end
+
+    def search_klass
+      ActivitySearch
+    end
+
+    def context_params
+      { organization: current_organization }
+    end
+
+    def default_filter_params
+      {
+        search_text: "",
+        resource_type: "all"
+      }
+    end
+
+    def default_search_params
+      {
+        scope: ActionLog.public.includes(:resource)
+      }
+    end
+  end
+end
