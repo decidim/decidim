25d4018b1e347f582784cc1f813f797641c6557f
Marc Riera Casals <mrc2407@gmail.com>
2018-09-26 12:51:02 +0200

    Fix specs

--- decidim-participatory_processes/db/migrate/20180926082635_add_cta_url_and_text_to_steps.rb
+++ decidim-participatory_processes/db/migrate/20180926082635_add_cta_url_and_text_to_steps.rb
@@ -2,7 +2,7 @@
 
 class AddCtaUrlAndTextToSteps < ActiveRecord::Migration[5.2]
   def change
-    add_column :decidim_participatory_process_steps, :cta_text, :jsonb
+    add_column :decidim_participatory_process_steps, :cta_text, :jsonb, default: {}
     add_column :decidim_participatory_process_steps, :cta_path, :string
   end
 end
