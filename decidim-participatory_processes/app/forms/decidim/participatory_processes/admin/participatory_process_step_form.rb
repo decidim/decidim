a7fa6120cf128acee3e314b2f68ff06cc469880b
Marc Riera Casals <mrc2407@gmail.com>
2018-09-26 11:05:46 +0200

    Set step CTA from the admin

--- decidim-participatory_processes/app/forms/decidim/participatory_processes/admin/participatory_process_step_form.rb
+++ decidim-participatory_processes/app/forms/decidim/participatory_processes/admin/participatory_process_step_form.rb
@@ -11,13 +11,16 @@ module Decidim
 
         translatable_attribute :title, String
         translatable_attribute :description, String
+        translatable_attribute :cta_text, String
 
         mimic :participatory_process_step
 
         attribute :start_date, Decidim::Attributes::LocalizedDate
         attribute :end_date, Decidim::Attributes::LocalizedDate
+        attribute :cta_path, String
 
         validates :title, translatable_presence: true
+        validates :cta_path, format: { with: %r{\A[a-zA-Z]+[a-zA-Z0-9-\_/]+\z} }, allow_blank: true
 
         validates :start_date, date: { before: :end_date, allow_blank: true, if: proc { |obj| obj.end_date.present? } }
         validates :end_date, date: { after: :start_date, allow_blank: true, if: proc { |obj| obj.start_date.present? } }
