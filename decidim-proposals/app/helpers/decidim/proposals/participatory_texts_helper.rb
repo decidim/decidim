fa5dc9e5dd444e888a6af0bc53f874f2bc53b579
Oliver Valls <oliver.vh@coditramuntana.com>
2018-10-05 11:55:14 +0200

    Rubocop and i18n normalization.

--- decidim-proposals/app/helpers/decidim/proposals/participatory_texts_helper.rb
+++ decidim-proposals/app/helpers/decidim/proposals/participatory_texts_helper.rb
@@ -10,10 +10,9 @@ module Decidim
       #
       # Returns a string with the title of the section, subsection or article.
       def preview_participatory_text_section_title(proposal)
-        translated= t(proposal.participatory_text_level, scope: "decidim.proposals.admin.participatory_texts.sections", title: proposal.title)
+        translated = t(proposal.participatory_text_level, scope: "decidim.proposals.admin.participatory_texts.sections", title: proposal.title)
         translated.html_safe
       end
-
     end
   end
 end
