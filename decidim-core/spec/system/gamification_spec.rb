c0a32c7e023da025f1cdae0df3ed323764b88170
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2018-10-09 10:27:02 +0200

    Add specs

--- decidim-core/spec/system/gamification_spec.rb
+++ decidim-core/spec/system/gamification_spec.rb
@@ -11,7 +11,7 @@ describe "Gamification", type: :system do
     login_as user, scope: :user
   end
 
-  describe "badges" do
+  describe "profile badges" do
     before do
       Decidim::Gamification.set_score(user, :test, 5)
     end
@@ -22,6 +22,24 @@ describe "Gamification", type: :system do
       within ".badge-test" do
         expect(page).to have_content "LEVEL 2"
       end
+    end
+  end
+
+  describe "badges info page" do
+    it "can be reached from the profile's badges page" do
+      visit decidim.profile_path(user.nickname)
+      click_link "Badges"
+      within ".tabs-panel.is-active" do
+        click_link "See all available badges"
+      end
+      expect(page).to have_current_path(decidim.gamification_badges_path)
+    end
+
+    it "shows a list of available badges" do
+      visit decidim.gamification_badges_path
+      expect(page).to have_content "Test badge"
+      expect(page).to have_content "This is a dummy badge"
+      expect(page).to have_content "Use a test environment for decidim"
     end
   end
 end
