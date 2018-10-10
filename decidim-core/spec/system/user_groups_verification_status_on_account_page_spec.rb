15a026e91cbf9c8b36369ec28f8e5cd48bf5e058
Marc Riera Casals <mrc2407@gmail.com>
2018-10-10 15:08:54 +0200

    Rename specs for a better understanding

--- decidim-core/spec/system/user_groups_verification_status_on_account_page_spec.rb
+++ decidim-core/spec/system/user_groups_verification_status_on_account_page_spec.rb
@@ -2,7 +2,7 @@
 
 require "spec_helper"
 
-describe "User groups", type: :system do
+describe "User group verification status on account page", type: :system do
   let(:organization) { create(:organization) }
   let(:user) { create(:user, :confirmed, organization: organization) }
 
