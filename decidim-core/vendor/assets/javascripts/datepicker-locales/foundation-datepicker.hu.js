a51c6232662b09cda4f7fdc5c616fdac0a8ca808
Enzo Angiulli <enzo@linkensphere.com>
2018-10-24 09:00:00 +0200

    added Hungarian datepicker js configs (#4358)

---
+++ decidim-core/vendor/assets/javascripts/datepicker-locales/foundation-datepicker.hu.js
@@ -0,0 +1,16 @@
+/**
+ * Hungarian translation for foundation-datepicker
+ * Enzo Angiulli <enzo@linkensphere.com>
+ */
+;(function($){
+	$.fn.fdatepicker.dates['es'] = {
+		days: ["Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap"],
+		daysShort: ["Vas", "Hét", "Kedd", "Szer", "Csüt", "Pént", "Szom", "Vas"],
+		daysMin: ["V", "H", "K", "Sze", "Cs", "P", "Szo", "V"],
+		months: ["Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"],
+		monthsShort: ["Jan", "Feb", "Már", "Ápr", "Máj", "Jún", "Júl", "Aug", "Szep", "Okt", "Nov", "Dec"],
+		today: "Ma",
+    weekStart: 1,
+    format: "yyyy.mm.dd."
+  };
+}(jQuery));
