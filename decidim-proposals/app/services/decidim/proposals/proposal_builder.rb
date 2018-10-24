093451b82aef3cce6226506f00ebfe9761f8484c
Oriol Gual <oriol@codegram.com>
2018-10-24 10:01:52 +0200

    Add docs

--- decidim-proposals/app/services/decidim/proposals/proposal_builder.rb
+++ decidim-proposals/app/services/decidim/proposals/proposal_builder.rb
@@ -5,6 +5,13 @@ module Decidim
     # A factory class to ensure we always create Proposals the same way since it involves some logic.
     module ProposalBuilder
       # Public: Creates a new Proposal.
+      #
+      # attributes        - The Hash of attributes to create the Proposal with.
+      # author            - An Authorable the will be the first coauthor of the Proposal.
+      # user_group_author - A User Group to, optionally, set it as the author too.
+      # action_user       - The User to be used as the user who is creating the proposal in the traceability logs.
+      #
+      # Returns a Proposal.
       def create(attributes:, author:, action_user:, user_group_author: nil)
         Decidim.traceability.perform_action!(:create, Proposal, action_user, visibility: "all") do
           proposal = Proposal.new(attributes)
@@ -17,6 +24,16 @@ module Decidim
       module_function :create
 
       # Public: Creates a new Proposal by copying the attributes from another one.
+      #
+      # original_proposal - The Proposal to be used as base to create the new one.
+      # author            - An Authorable the will be the first coauthor of the Proposal.
+      # user_group_author - A User Group to, optionally, set it as the author too.
+      # action_user       - The User to be used as the user who is creating the proposal in the traceability logs.
+      # extra_attributes  - A Hash of attributes to create the new proposal, will overwrite the original ones.
+      # skip_link         - Whether to skip linking the two proposals or not (default false).
+      #
+      # Returns a Proposal
+      #
       # rubocop:disable Metrics/ParameterLists
       def copy(original_proposal, author:, action_user:, user_group_author: nil, extra_attributes: {}, skip_link: false)
         origin_attributes = original_proposal.attributes.except(
