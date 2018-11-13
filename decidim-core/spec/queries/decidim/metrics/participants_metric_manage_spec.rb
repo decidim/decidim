46d9a9c658d687691f37645373c32f692531c6d3
MarcReniu <marc.rs@coditramuntana.com>
2018-11-13 15:27:36 +0100

    Participants metric spec modified to use moduled strategy

--- decidim-core/spec/queries/decidim/metrics/participants_metric_manage_spec.rb
+++ decidim-core/spec/queries/decidim/metrics/participants_metric_manage_spec.rb
@@ -1,48 +1,20 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-require "decidim/debates/test/factories"
-require "decidim/surveys/test/factories"
-require "decidim/budgets/test/factories"
-require "decidim/comments/test/factories"
 
 describe Decidim::Metrics::ParticipantsMetricManage do
   let(:day) { Time.zone.today - 1.day }
   let(:organization) { create(:organization) }
-
-  # Create a proposal (Proposals)
-  # Give support to a proposal (Proposals)
-  # Endorse (Proposals)
-  # TOTAL Participants for Proposals: 16 ( 1 proposal, 10 votes, 5 endorsements )
-  let(:participatory_space) { create(:participatory_process, :with_steps, organization: organization) }
-  let(:proposals_component) { create(:proposal_component, :published, participatory_space: participatory_space) }
-  let(:proposal) { create(:proposal, :with_endorsements, published_at: day, component: proposals_component) }
-  let(:proposal_votes) { create_list(:proposal_vote, 10, created_at: day, proposal: proposal) }
-  let(:proposal_endorsements) { create_list(:proposal_endorsement, 5, created_at: day, proposal: proposal) }
-
-  # Create a debate (Debates)
-  # TOTAL Participants for Debates: 5
-  let(:debates_component) { create(:debates_component, :published, participatory_space: participatory_space) }
-  let(:debates) { create_list(:debate, 5, :with_author, component: debates_component, created_at: day) }
-
-  # Answer a survey (Surveys)
-  # TOTAL Participants for Surveys: 5
-  let(:survey_component) { create(:surveys_component, :published, participatory_space: participatory_space) }
-  let(:survey) { create(:survey, component: survey_component) }
-  let(:questionnaire) { create(:questionnaire, questionnaire_for: survey) }
-  let(:answers) { create_list(:answer, 5, questionnaire: questionnaire, created_at: day) }
-
-  # Vote a participatory budgeting project (Budgets)
-  # TOTAL Participants for Budgets: 5
-  let(:budget_component) { create(:budget_component, :published, participatory_space: participatory_space, settings: { vote_threshold_percent: 0 }) }
-  let(:orders) { create_list(:order, 5, component: budget_component, checked_out_at: day) }
-
-  # Leave a comment (Comments)
-  # TOTAL Participants for Comments: 2
-  let(:comments) { create_list(:comment, 2, root_commentable: proposal, commentable: proposal, created_at: day) }
-
-  # TOTAL Participants: 33
-  let(:all) { proposal && proposal_votes && proposal_endorsements && debates && answers && orders && comments }
+  let!(:participatory_space) { create(:participatory_process, organization: organization) }
+  let(:key) { [participatory_space.class.name, participatory_space.id] }
+  let(:query) do
+    q = {}
+    q[key] = {
+      cumulative_users: [1, 2, 3, 4, 5, 6, 7, 8],
+      quantity_users: [1, 2]
+    }
+    q
+  end
 
   context "when executing" do
     context "without data" do
@@ -54,21 +26,18 @@ describe Decidim::Metrics::ParticipantsMetricManage do
     end
 
     context "with data" do
-      before { all }
+      before do
+        # rubocop:disable RSpec/AnyInstance
+        allow_any_instance_of(described_class).to receive(:query).and_return(query)
+        # rubocop:enable RSpec/AnyInstance
+      end
 
       it "creates new metric records" do
         registry = generate_metric_registry
 
         expect(registry.collect(&:day)).to eq([day])
-        expect(registry.collect(&:cumulative)).to eq([33])
-        expect(registry.collect(&:quantity)).to eq([33])
-      end
-
-      it "does not create any record if there is no data" do
-        registry = generate_metric_registry("2017-01-01")
-
-        expect(Decidim::Metric.count).to eq(0)
-        expect(registry).to be_empty
+        expect(registry.collect(&:cumulative)).to eq([8])
+        expect(registry.collect(&:quantity)).to eq([2])
       end
 
       it "updates metric records" do
@@ -76,8 +45,8 @@ describe Decidim::Metrics::ParticipantsMetricManage do
         registry = generate_metric_registry
 
         expect(Decidim::Metric.count).to eq(1)
-        expect(registry.collect(&:cumulative)).to eq([33])
-        expect(registry.collect(&:quantity)).to eq([33])
+        expect(registry.collect(&:cumulative)).to eq([8])
+        expect(registry.collect(&:quantity)).to eq([2])
       end
     end
   end
