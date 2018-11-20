35051e89638268d9fc649a50c81f8acc61f09a6f
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2018-11-20 14:33:02 +0100

    Fix specs

--- decidim-core/app/events/decidim/welcome_notification_event.rb
+++ decidim-core/app/events/decidim/welcome_notification_event.rb
@@ -11,10 +11,6 @@ module Decidim
     delegate :organization, to: :user, prefix: false
     delegate :url_helpers, to: "Decidim::Core::Engine.routes"
 
-    def resource
-      nil
-    end
-
     def subject
       interpolate(organization.welcome_notification_subject.symbolize_keys[I18n.locale])
     end
@@ -37,6 +33,14 @@ module Decidim
 
     def notification_title
       ("<p><strong>#{subject}</strong></p>" + body).html_safe
+    end
+
+    def resource_path
+      nil
+    end
+
+    def resource_title
+      nil
     end
 
     private
