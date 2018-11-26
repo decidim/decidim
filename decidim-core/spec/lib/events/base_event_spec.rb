cd572fa812fcba663a23d9461db1a7bc31ba6dfd
armandfardeau <fardeauarmand@gmail.com>
2018-11-26 18:22:01 +0100

    Fix notifications sending when there's no component

--- decidim-core/spec/lib/events/base_event_spec.rb
+++ decidim-core/spec/lib/events/base_event_spec.rb
@@ -120,5 +120,16 @@ module Decidim
         end
       end
     end
+
+    context "when the resource is a participatory space" do
+      let(:resource) { build(:participatory_process) }
+
+      it { is_expected.to be_notifiable }
+      context "when it is not published" do
+        let(:resource) { build(:participatory_process, :unpublished) }
+
+        it { is_expected.not_to be_notifiable }
+      end
+    end
   end
 end
