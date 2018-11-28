f2a728accb28c1186b0c3141e8d4e6cd5c66d817
Marc Riera Casals <mrc2407@gmail.com>
2018-11-28 11:15:11 +0100

    Trace ID documents config updates

--- decidim-verifications/app/commands/decidim/verifications/id_documents/admin/update_config.rb
+++ decidim-verifications/app/commands/decidim/verifications/id_documents/admin/update_config.rb
@@ -22,9 +22,15 @@ module Decidim
           attr_reader :form
 
           def update_config
-            form.current_organization.id_documents_methods = form.selected_methods
-            form.current_organization.id_documents_explanation_text = form.offline_explanation
-            form.current_organization.save!
+            Decidim.traceability.perform_action!(
+              :update_id_documents_config,
+              form.current_organization,
+              form.current_user
+            ) do
+              form.current_organization.id_documents_methods = form.selected_methods
+              form.current_organization.id_documents_explanation_text = form.offline_explanation
+              form.current_organization.save!
+            end
           end
         end
       end
