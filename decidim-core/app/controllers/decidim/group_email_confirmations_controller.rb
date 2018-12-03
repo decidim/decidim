0969400b7202e0b41bc651d7cbaffa4f4f008e91
Marc Riera Casals <mrc2407@gmail.com>
2018-12-03 12:23:29 +0100

    Lint files

--- decidim-core/app/controllers/decidim/group_email_confirmations_controller.rb
+++ decidim-core/app/controllers/decidim/group_email_confirmations_controller.rb
@@ -11,7 +11,7 @@ module Decidim
       enforce_permission_to :manage, :user_group, user_group: user_group
       if user_group.email.blank?
         flash.keep[:alert] = t("decidim.profiles.user.fill_in_email_to_confirm_it")
-        redirect_to edit_group_path(user_group.nickname) and return
+        redirect_to(edit_group_path(user_group.nickname)) && return
       end
 
       user_group.send_confirmation_instructions
