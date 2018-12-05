b3f0a1c5e542e2323abc5ffe961cfe11936849bd
ailobe <aitorlopezbeltran@gmail.com>
2018-12-05 16:47:58 +0100

    Add validation pending/confirmation notifications to conferences registrations

---
+++ decidim-conferences/app/events/decidim/conferences/conference_registration_notification_event.rb
@@ -0,0 +1,21 @@
+# frozen-string_literal: true
+
+module Decidim
+  module Conferences
+    class ConferenceRegistrationNotificationEvent < Decidim::Events::BaseEvent
+      include Decidim::Events::NotificationEvent
+
+      def notification_title
+        I18n.t("notification_title", i18n_options).html_safe
+      end
+
+      def i18n_options
+        {
+          resource_title: resource_title,
+          resource_url: resource_url,
+          scope: event_name
+        }
+      end
+    end
+  end
+end
