f4ed4566e3d8a9bd96e5e0bfd51760c678dde466
Marc Riera Casals <mrc2407@gmail.com>
2018-12-14 10:28:39 +0100

    Make EmailNotificationGenerator handle the new API

--- decidim-core/app/jobs/decidim/email_notification_generator_job.rb
+++ decidim-core/app/jobs/decidim/email_notification_generator_job.rb
@@ -7,8 +7,7 @@ module Decidim
     # rubocop:disable Metrics/ParameterLists
     def perform(event, event_class_name, resource, followers, affected_users, extra)
       event_class = event_class_name.constantize
-      recipient_ids = followers.compact.map(&:id) + affected_users.compact.map(&:id)
-      EmailNotificationGenerator.new(event, event_class, resource, recipient_ids, extra).generate
+      EmailNotificationGenerator.new(event, event_class, resource, followers, affected_users, extra).generate
     end
     # rubocop:enable Metrics/ParameterLists
   end
--- decidim-core/app/services/decidim/email_notification_generator.rb
+++ decidim-core/app/services/decidim/email_notification_generator.rb
@@ -12,14 +12,21 @@ module Decidim
     # event - A String with the name of the event.
     # event_class - A class that wraps the event.
     # resource - an instance of a class implementing the `Decidim::Resource` concern.
+    # followers - a collection of Users that receive the notification because
+    #   they're following it
+    # affected_users - a collection of Users that receive the notification because
+    #   they're affected by it
     # extra - a Hash with extra information to be included in the notification.
-    def initialize(event, event_class, resource, recipient_ids, extra)
+    # rubocop:disable Metrics/ParameterLists
+    def initialize(event, event_class, resource, followers, affected_users, extra)
       @event = event
       @event_class = event_class
       @resource = resource
-      @recipient_ids = recipient_ids
+      @followers = followers
+      @affected_users = affected_users
       @extra = extra
     end
+    # rubocop:enable Metrics/ParameterLists
 
     # Schedules a job for each recipient to send the email. Returns `nil`
     # if the resource is not resource or if it is not present.
@@ -29,23 +36,26 @@ module Decidim
       return unless resource
       return unless event_class.types.include?(:email)
 
-      recipient_ids.each do |recipient_id|
-        send_email_to(recipient_id)
+      followers.each do |recipient|
+        send_email_to(recipient)
+      end
+
+      affected_users.each do |recipient|
+        send_email_to(recipient)
       end
     end
 
     private
 
-    attr_reader :event, :event_class, :resource, :recipient_ids, :extra
+    attr_reader :event, :event_class, :resource, :followers, :affected_users, :extra
 
     # Private: sends the notification email to the user if they have the
     # `email_on_notification` flag active.
     #
-    # recipient_id - The ID of the user that will receive the email.
+    # recipient - The user that will receive the email.
     #
     # Returns nothing.
-    def send_email_to(recipient_id)
-      recipient = Decidim::User.find_by(id: recipient_id)
+    def send_email_to(recipient)
       return unless recipient
       return unless recipient.email_on_notification?
 
