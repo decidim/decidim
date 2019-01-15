8ac680cf526a06cf6add1c1f71738bc0124bfef4
Eduardo Martínez <eduardomech@gmail.com>
2019-01-15 15:53:06 +0100

    Rename helper method

--- decidim-initiatives/spec/system/initiative_signing_spec.rb
+++ decidim-initiatives/spec/system/initiative_signing_spec.rb
@@ -7,26 +7,26 @@ describe "Initiative signing", type: :system do
   let(:initiative) do
     create(:initiative, :published, organization: organization)
   end
-  let(:authorized_user) { create(:user, :confirmed, organization: organization) }
+  let(:confirmed_user) { create(:user, :confirmed, organization: organization) }
 
   before do
     allow(Decidim::Initiatives)
       .to receive(:do_not_require_authorization)
       .and_return(true)
     switch_to_host(organization.host)
-    login_as authorized_user, scope: :user
+    login_as confirmed_user, scope: :user
   end
 
   context "when the user has not signed the initiative yet and signs it" do
     context "when the user has a verified user group" do
-      let!(:user_group) { create :user_group, :verified, users: [authorized_user], organization: authorized_user.organization }
+      let!(:user_group) { create :user_group, :verified, users: [confirmed_user], organization: confirmed_user.organization }
 
       it "votes as a user group" do
         vote_initiative(user_name: user_group.name)
       end
 
       it "votes as themselves" do
-        vote_initiative(user_name: authorized_user.name)
+        vote_initiative(user_name: confirmed_user.name)
       end
     end
 
@@ -37,7 +37,7 @@ describe "Initiative signing", type: :system do
 
   context "when the user has signed the initiative and unsigns it" do
     context "when the user has a verified user group" do
-      let!(:user_group) { create :user_group, :verified, users: [authorized_user], organization: authorized_user.organization }
+      let!(:user_group) { create :user_group, :verified, users: [confirmed_user], organization: confirmed_user.organization }
 
       it "removes the signature" do
         vote_initiative(user_name: user_group.name)
