cceff5d149e99ac758b94215eeacf2f421fe85d8
moustachu <git@moustachu.net>
2019-01-17 19:06:43 +0100

    :sparkles: #226 Survey answer date

--- decidim-surveys/lib/decidim/surveys/survey_user_answers_serializer.rb
+++ decidim-surveys/lib/decidim/surveys/survey_user_answers_serializer.rb
@@ -15,6 +15,7 @@ module Decidim
       # Public: Exports a hash with the serialized data for the user answers.
       def serialize
         @survey_answers.each_with_index.inject({}) do |serialized, (answer, idx)|
+          serialized.update(:date => answer.created_at.strftime("%Y-%m-%d %H:%M:%S"))
           serialized.update("#{idx + 1}. #{translated_attribute(answer.question.body)}" => normalize_body(answer))
         end
       end
