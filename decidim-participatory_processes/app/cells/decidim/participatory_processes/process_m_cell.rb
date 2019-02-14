6a43bfc4e17eb86a638afbaba26a7b74170e1186
Armand <fardeauarmand@gmail.com>
2019-02-14 15:29:37 +0100

    Fix empty action btn text

--- decidim-participatory_processes/app/cells/decidim/participatory_processes/process_m_cell.rb
+++ decidim-participatory_processes/app/cells/decidim/participatory_processes/process_m_cell.rb
@@ -37,7 +37,7 @@ module Decidim
       end
 
       def step_action_btn_text
-        if model.active_step&.action_btn_text
+        if translated_in_current_locale(model.active_step&.action_btn_text)
           translated_attribute(model.active_step.action_btn_text)
         else
           t("participatory_processes.participatory_process.take_part", scope: "layouts.decidim")
