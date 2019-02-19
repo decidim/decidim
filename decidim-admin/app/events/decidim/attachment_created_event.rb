8d3fd98fd11c78431f37dff3da1c763dfc451048
ailobe <aitorlopezbeltran@gmail.com>
2019-02-19 15:56:39 +0100

    Refactor previous change

--- decidim-admin/app/events/decidim/attachment_created_event.rb
+++ decidim-admin/app/events/decidim/attachment_created_event.rb
@@ -2,14 +2,14 @@
 
 module Decidim
   class AttachmentCreatedEvent < Decidim::Events::SimpleEvent
-    i18n_attributes :resource_url
+    i18n_attributes :attached_to_url
 
     def resource_path
       @resource.url
     end
 
     def resource_url
-      resource_locator.url
+      attached_to_url
     end
 
     def resource_text
@@ -20,6 +20,10 @@ module Decidim
     end
 
     private
+
+    def attached_to_url
+      resource_locator.url
+    end
 
     def resource
       @resource.attached_to
