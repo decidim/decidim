c96381d715cb6dcf41f64db9fe58db06b73da00c
Mijail Rondon <mijail@riseup.net>
2019-02-19 23:43:53 -0400

    Fix magic comment

--- decidim-core/db/migrate/20190220023422_add_colors_to_decidim_organization.rb
+++ decidim-core/db/migrate/20190220023422_add_colors_to_decidim_organization.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddColorsToDecidimOrganization < ActiveRecord::Migration[5.2]
   def change
     add_column :decidim_organizations, :colors, :jsonb, default: {}
