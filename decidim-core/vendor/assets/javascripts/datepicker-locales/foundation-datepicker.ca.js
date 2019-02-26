d14c1ee23a4712586cd65f77f2ad6e16bb88a43e
Marc Riera Casals <mrc2407@gmail.com>
2019-02-26 11:53:48 +0100

    Fix dates in Catalan

--- decidim-core/vendor/assets/javascripts/datepicker-locales/foundation-datepicker.ca.js
+++ decidim-core/vendor/assets/javascripts/datepicker-locales/foundation-datepicker.ca.js
@@ -2,13 +2,13 @@
  * Catalan translation for foundation-datepicker, language file from bootstrap-datepicker
  * J. Garcia <jogaco.en@gmail.com>
  */
-;(function($){
+; (function ($) {
 	$.fn.fdatepicker.dates['ca'] = {
-		days: ["Diumenge", "Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte"],
-		daysShort: ["Diu",  "Dil", "Dmt", "Dmc", "Dij", "Div", "Dis"],
-		daysMin: ["dg", "dl", "dt", "dc", "dj", "dv", "ds"],
-		months: ["Gener", "Febrer", "Març", "Abril", "Maig", "Juny", "Juliol", "Agost", "Setembre", "Octubre", "Novembre", "Desembre"],
-		monthsShort: ["Gen", "Feb", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Oct", "Nov", "Des"],
+		days: ["diumenge", "dilluns", "dimarts", "dimecres", "dijous", "divendres", "dissabte"],
+		daysShort: ["dg.", "dl.", "dt.", "dc.", "dj.", "dv.", "ds."],
+		daysMin: ["dg.", "dl.", "dt.", "dc.", "dj.", "dv.", "ds."],
+		months: ["gener", "febrer", "març", "abril", "maig", "juny", "juliol", "agost", "setembre", "octubre", "novembre", "desembre"],
+		monthsShort: ["gen.", "febr.", "març", "abr.", "maig", "juny", "jul.", "ag.", "set.", "oct.", "nov.", "des."],
 		today: "Avui",
 		clear: "Esborrar",
 		weekStart: 1
