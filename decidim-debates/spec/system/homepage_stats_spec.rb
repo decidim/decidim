198bbf716aca923dd3c42c43865cdf1d313f8e38
moustachu <git@moustachu.net>
2019-03-02 01:05:25 +0100

    :white_check_mark: repair tests for #356

--- decidim-debates/spec/system/homepage_stats_spec.rb
+++ decidim-debates/spec/system/homepage_stats_spec.rb
@@ -3,17 +3,21 @@
 require "spec_helper"
 
 describe "Homepage", type: :system do
-  include_context "with a component"
   let(:manifest_name) { "debates" }
+  let(:component) { create(:debates_component) }
+  let(:organization) { component.organization }
 
   let!(:debates) { create_list(:debate, 3, component: component) }
   let!(:moderation) { create :moderation, reportable: debates.first, hidden_at: 1.day.ago }
 
   before do
-    visit decidim.root_path
+    create :content_block, organization: organization, scope: :homepage, manifest_name: :stats
+    switch_to_host(organization.host)
   end
 
   it "display unhidden debates count" do
+    visit decidim.root_path
+
     within(".debates_count") do
       expect(page).to have_content(2)
     end
--- decidim-proposals/spec/system/homepage_stats_spec.rb
+++ decidim-proposals/spec/system/homepage_stats_spec.rb
@@ -3,17 +3,21 @@
 require "spec_helper"
 
 describe "Homepage", type: :system do
-  include_context "with a component"
   let(:manifest_name) { "proposals" }
+  let(:component) { create(:proposal_component) }
+  let(:organization) { component.organization }
 
   let!(:proposals) { create_list(:proposal, 3, component: component) }
   let!(:moderation) { create :moderation, reportable: proposals.first, hidden_at: 1.day.ago }
 
   before do
-    visit decidim.root_path
+    create :content_block, organization: organization, scope: :homepage, manifest_name: :stats
+    switch_to_host(organization.host)
   end
 
   it "displays unhidden proposals count" do
+    visit decidim.root_path
+
     within(".proposals_count") do
       expect(page).to have_content(2)
     end
