44e7c14a09a77023bed64bbcffc3fab3f4aef0a6
ailobe <aitorlopezbeltran@gmail.com>
2019-04-03 16:03:49 +0200

    Refactor filter specs

--- decidim-assemblies/spec/system/filter_assemblies_spec.rb
+++ decidim-assemblies/spec/system/filter_assemblies_spec.rb
@@ -2,14 +2,14 @@
 
 require "spec_helper"
 
-describe "Assemblies", type: :system do
+describe "Filter Assemblies", type: :system do
   let(:organization) { create(:organization) }
 
   before do
     switch_to_host(organization.host)
   end
 
-  context "when there are published assemblies" do
+  context "when filtering parent assemblies by assembly_type" do
     let!(:assembly_1) { create(:assembly, organization: organization, assembly_type: "others") }
     let!(:assembly_2) { create(:assembly, organization: organization, assembly_type: "government") }
     let!(:assembly_3) { create(:assembly, organization: organization, assembly_type: "consultative_advisory") }
@@ -18,98 +18,96 @@ describe "Assemblies", type: :system do
     let!(:assembly_6) { create(:assembly, organization: organization, assembly_type: "working_group") }
     let!(:assembly_7) { create(:assembly, organization: organization, assembly_type: "commission") }
 
-    context "and filtering parent assemblies by assembly_type" do
-      before do
-        visit decidim_assemblies.assemblies_path
-        click_button "All types of assemblies"
-      end
-
-      it "filters by All types" do
-        click_link "All types of assemblies"
-        expect(page).to have_selector("article.card.card--assembly", count: 7)
-      end
-
-      it "filters by Government type" do
-        click_link "Government"
-        expect(page).to have_selector("article.card.card--assembly", count: 1)
-        expect(page).to have_selector("#button-text", text: "Government")
-      end
-
-      it "filters by Executive type" do
-        click_link "Executive"
-        expect(page).to have_selector("article.card.card--assembly", count: 1)
-        expect(page).to have_selector("#button-text", text: "Executive")
-      end
-
-      it "filters by Consultative/Advisory type" do
-        click_link "Consultative/Advisory"
-        expect(page).to have_selector("article.card.card--assembly", count: 1)
-        expect(page).to have_selector("#button-text", text: "Consultative/Advisory")
-      end
-
-      it "filters by Participatory type" do
-        click_link "Participatory"
-        expect(page).to have_selector("article.card.card--assembly", count: 1)
-        expect(page).to have_selector("#button-text", text: "Participatory")
-      end
-
-      it "filters by Working group type" do
-        click_link "Working group"
-        expect(page).to have_selector("article.card.card--assembly", count: 1)
-        expect(page).to have_selector("#button-text", text: "Working group")
-      end
-
-      it "filters by Commission type" do
-        click_link "Commission"
-        expect(page).to have_selector("article.card.card--assembly", count: 1)
-        expect(page).to have_selector("#button-text", text: "Commission")
-      end
-
-      it "filters by Others type" do
-        click_link "Others"
-        expect(page).to have_selector("article.card.card--assembly", count: 1)
-        expect(page).to have_selector("#button-text", text: "Others")
-      end
+    before do
+      visit decidim_assemblies.assemblies_path
+      click_button "All types of assemblies"
     end
 
-    context "and filtering parent assemblies by scope" do
-      let!(:scope) { create :scope, organization: organization }
-      let!(:assembly_with_scope) { create(:assembly, scope: scope, organization: organization) }
-      let!(:assembly_without_scope) { create(:assembly, organization: organization) }
+    it "filters by All types" do
+      click_link "All types of assemblies"
+      expect(page).to have_selector("article.card.card--assembly", count: 7)
+    end
 
-      context "and choosing a scope" do
-        before do
-          visit decidim_assemblies.assemblies_path(filter: { scope_id: scope.id })
-        end
+    it "filters by Government type" do
+      click_link "Government"
+      expect(page).to have_selector("article.card.card--assembly", count: 1)
+      expect(page).to have_selector("#button-text", text: "Government")
+    end
 
-        it "lists all processes belonging to that scope" do
-          within "#parent-assemblies" do
-            expect(page).to have_content(translated(assembly_with_scope.title))
-            expect(page).not_to have_content(translated(assembly_without_scope.title))
-          end
+    it "filters by Executive type" do
+      click_link "Executive"
+      expect(page).to have_selector("article.card.card--assembly", count: 1)
+      expect(page).to have_selector("#button-text", text: "Executive")
+    end
+
+    it "filters by Consultative/Advisory type" do
+      click_link "Consultative/Advisory"
+      expect(page).to have_selector("article.card.card--assembly", count: 1)
+      expect(page).to have_selector("#button-text", text: "Consultative/Advisory")
+    end
+
+    it "filters by Participatory type" do
+      click_link "Participatory"
+      expect(page).to have_selector("article.card.card--assembly", count: 1)
+      expect(page).to have_selector("#button-text", text: "Participatory")
+    end
+
+    it "filters by Working group type" do
+      click_link "Working group"
+      expect(page).to have_selector("article.card.card--assembly", count: 1)
+      expect(page).to have_selector("#button-text", text: "Working group")
+    end
+
+    it "filters by Commission type" do
+      click_link "Commission"
+      expect(page).to have_selector("article.card.card--assembly", count: 1)
+      expect(page).to have_selector("#button-text", text: "Commission")
+    end
+
+    it "filters by Others type" do
+      click_link "Others"
+      expect(page).to have_selector("article.card.card--assembly", count: 1)
+      expect(page).to have_selector("#button-text", text: "Others")
+    end
+  end
+
+  context "when filtering parent assemblies by scope" do
+    let!(:scope) { create :scope, organization: organization }
+    let!(:assembly_with_scope) { create(:assembly, scope: scope, organization: organization) }
+    let!(:assembly_without_scope) { create(:assembly, organization: organization) }
+
+    context "and choosing a scope" do
+      before do
+        visit decidim_assemblies.assemblies_path(filter: { scope_id: scope.id })
+      end
+
+      it "lists all processes belonging to that scope" do
+        within "#parent-assemblies" do
+          expect(page).to have_content(translated(assembly_with_scope.title))
+          expect(page).not_to have_content(translated(assembly_without_scope.title))
         end
       end
     end
+  end
 
-    context "and filtering parent assemblies by area" do
-      let!(:area) { create :area, organization: organization }
-      let!(:assembly_with_area) { create(:assembly, area: area, organization: organization) }
-      let!(:assembly_without_area) { create(:assembly, organization: organization) }
+  context "when filtering parent assemblies by area" do
+    let!(:area) { create :area, organization: organization }
+    let!(:assembly_with_area) { create(:assembly, area: area, organization: organization) }
+    let!(:assembly_without_area) { create(:assembly, organization: organization) }
 
+    before do
+      visit decidim_assemblies.assemblies_path
+    end
+
+    context "and choosing an area" do
       before do
-        visit decidim_assemblies.assemblies_path
+        select translated(area.name), from: "filter[area_id]"
       end
 
-      context "and choosing an area" do
-        before do
-          select translated(area.name), from: "filter[area_id]"
-        end
-
-        it "lists all processes belonging to that area" do
-          within "#parent-assemblies" do
-            expect(page).to have_content(translated(assembly_with_area.title))
-            expect(page).not_to have_content(translated(assembly_without_area.title))
-          end
+      it "lists all processes belonging to that area" do
+        within "#parent-assemblies" do
+          expect(page).to have_content(translated(assembly_with_area.title))
+          expect(page).not_to have_content(translated(assembly_without_area.title))
         end
       end
     end
