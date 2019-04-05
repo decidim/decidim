955ead0ea7a230cc9187045c98aa9a6b77e21d6e
ailobe <aitorlopezbeltran@gmail.com>
2019-04-05 13:43:36 +0200

    Fix unused locales

--- decidim-participatory_processes/db/migrate/20190404091040_add_metadata_fields_to_participatory_processes.rb
+++ decidim-participatory_processes/db/migrate/20190404091040_add_metadata_fields_to_participatory_processes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddMetadataFieldsToParticipatoryProcesses < ActiveRecord::Migration[5.2]
   def change
     add_column :decidim_participatory_processes, :cost, :decimal
