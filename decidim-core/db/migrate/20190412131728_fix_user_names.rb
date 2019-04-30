264f61f3721cf413f43680e91756e8d04d1d6193
Oriol Gual <oriol@codegram.com>
2019-04-30 10:59:31 +0200

    Don't use puts

--- decidim-core/db/migrate/20190412131728_fix_user_names.rb
+++ decidim-core/db/migrate/20190412131728_fix_user_names.rb
@@ -9,10 +9,10 @@ class FixUserNames < ActiveRecord::Migration[5.2]
 
     weird_characters.each do |character|
       Decidim::UserBaseEntity.where("name like '%#{character}%' escape '\' OR nickname like '%#{character}%' escape '\'").find_each do |entity|
-        p "detected character: #{character}"
-        p "UserBaseEntity ID: #{entity.id}"
-        p "#{entity.name} => #{entity.name.delete(characters_to_remove).strip}"
-        p "#{entity.nickname} => #{entity.nickname.delete(characters_to_remove).strip}"
+        Rails.logger.log "detected character: #{character}"
+        Rails.logger.log "UserBaseEntity ID: #{entity.id}"
+        Rails.logger.log "#{entity.name} => #{entity.name.delete(characters_to_remove).strip}"
+        Rails.logger.log "#{entity.nickname} => #{entity.nickname.delete(characters_to_remove).strip}"
 
         entity.name = entity.name.delete(characters_to_remove).strip
         entity.nickname = entity.nickname.delete(characters_to_remove).strip
