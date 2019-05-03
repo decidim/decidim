efd62cabe312d2d1164358e952a16d497c68c94a
moustachu <git@moustachu.net>
2019-05-03 19:22:08 +0200

    :bug: Restore Debates export form component manifest #632

--- decidim-debates/spec/services/decidim/debates/debate_serializer_spec.rb
+++ decidim-debates/spec/services/decidim/debates/debate_serializer_spec.rb
@@ -25,14 +25,14 @@ module Decidim
           expect(serialized).to include(id: debate.id)
         end
 
-        it "serializes the author id" do
-          expect(serialized).to include(author_id: debate.author.id)
+        it "serializes the author profile url" do
+          expect(serialized[:author_url]).to match(%r{http.*/profiles})
         end
 
         context "when there is no author id" do
           it "serializes the author id with nilll" do
             debate.author = nil
-            expect(serialized).to include(author_id: nil)
+            expect(serialized).to include(author_url: "")
           end
         end
 
@@ -52,12 +52,35 @@ module Decidim
           expect(serialized).to include(created_at: debate.created_at)
         end
 
+        context "when start and end time are set" do
+          it "serializes the start and time" do
+            debate.start_time = 1.day.ago
+            debate.end_time = 1.day.from_now
+            expect(serialized).to include(start_time: debate.start_time)
+            expect(serialized).to include(end_time: debate.end_time)
+          end
+        end
+
+        context "when start and end time are not set" do
+          it "serializes the start and time as nilll" do
+            debate.start_time = nil
+            debate.end_time = nil
+            expect(serialized).to include(start_time: nil)
+            expect(serialized).to include(end_time: nil)
+          end
+        end
+
         it "serializes the url" do
           expect(serialized[:url]).to include("http", debate.id.to_s)
         end
 
         it "serializes the component" do
           expect(serialized[:component]).to include(id: debate.component.id)
+        end
+
+        it "serializes the participatory space" do
+          expect(serialized[:participatory_space]).to include(id: participatory_process.id)
+          expect(serialized[:participatory_space][:url]).to include("http", participatory_process.slug)
         end
       end
     end
