ceceb35beae811c61e08dad568ad0dcb08e97b6b
Ivan Vergés <ivan@platoniq.net>
2019-05-08 17:25:09 +0200

    add docs and tests for new metrics commands

--- decidim-core/spec/tasks/decidim_tasks_metrics_spec.rb
+++ decidim-core/spec/tasks/decidim_tasks_metrics_spec.rb
@@ -48,4 +48,37 @@ describe "Executing Decidim Metrics tasks" do
       end
     end
   end
+
+  describe "rake decidim:metrics:rebuild", type: :task do
+    let!(:organizations) { create_list(:organization, 2) }
+
+    context "when executing task" do
+      after do
+        clear_enqueued_jobs
+      end
+
+      it "does nos create jobs if no date given" do
+        Rake::Task[:"decidim:metrics:rebuild"].reenable
+        expect { Rake::Task[:"decidim:metrics:rebuild"].invoke }.to have_enqueued_job(Decidim::MetricJob).exactly(0).times
+      end
+
+      it "have to be executed without failures" do
+        Rake::Task[:"decidim:metrics:rebuild"].reenable
+        expect { Rake::Task[:"decidim:metrics:rebuild"].invoke(Time.zone.yesterday.to_s) }.not_to raise_error
+        expect { Rake::Task[:"decidim:metrics:rebuild"].invoke(Decidim.metrics_registry.all.first.metric_name, Time.zone.yesterday.to_s) }.not_to raise_error
+      end
+
+      it "creates jobs for each organization and time range" do
+        Rake::Task[:"decidim:metrics:rebuild"].reenable
+        expect { Rake::Task[:"decidim:metrics:rebuild"].invoke(Decidim.metrics_registry.all.first.metric_name, Time.zone.yesterday.to_s) }.to have_enqueued_job(Decidim::MetricJob).at_least(Decidim::Organization.count * 2).times
+      end
+    end
+  end
+
+  describe "rake decidim:metrics:list", type: :task do
+    it "have to be executed without failures" do
+      Rake::Task[:"decidim:metrics:list"].reenable
+      expect { Rake::Task[:"decidim:metrics:rebuild"].invoke }.not_to raise_error
+    end
+  end
 end
