b336bc6bd220e03b00ece1dc9017e45458fa8cae
ailobe <aitorlopezbeltran@gmail.com>
2019-05-13 14:31:46 +0200

    Uncomment code left commented by mistake

--- decidim-meetings/spec/system/admin_manages_meetings_spec.rb
+++ decidim-meetings/spec/system/admin_manages_meetings_spec.rb
@@ -8,12 +8,12 @@ describe "Admin manages meetings", type: :system, serves_map: true do
 
   include_context "when managing a component as an admin"
 
-  # it_behaves_like "manage meetings"
-  # it_behaves_like "manage registrations"
-  # it_behaves_like "manage announcements"
-  # it_behaves_like "manage agenda"
-  # it_behaves_like "manage minutes"
-  # it_behaves_like "manage invites"
-  # it_behaves_like "export meetings"
+  it_behaves_like "manage meetings"
+  it_behaves_like "manage registrations"
+  it_behaves_like "manage announcements"
+  it_behaves_like "manage agenda"
+  it_behaves_like "manage minutes"
+  it_behaves_like "manage invites"
+  it_behaves_like "export meetings"
   it_behaves_like "duplicate meetings"
 end
