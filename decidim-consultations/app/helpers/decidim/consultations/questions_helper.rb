a7d5cf06174f0d877f3739af0fbb1f6f879ef077
ailobe <aitorlopezbeltran@gmail.com>
2019-05-14 09:58:28 +0200

    Refactor display_next_previous_button helper method

--- decidim-consultations/app/helpers/decidim/consultations/questions_helper.rb
+++ decidim-consultations/app/helpers/decidim/consultations/questions_helper.rb
@@ -4,23 +4,23 @@ module Decidim
   module Consultations
     # Helper for questions controller
     module QuestionsHelper
-      # Returns a link to the next/previous question
-      # depending on whether the user has voted it or not.
+      # Returns a link to the next/previous question if found.
+      # Else, returns a disabled link to the current question.
       def display_next_previous_button(direction, optional_classes = "")
         css = "card__button button hollow " + optional_classes
 
         case direction
         when :previous
           i18n_text = t("previous_button", scope: "decidim.questions")
-          url = decidim_consultations.question_path(previous_question || current_question)
-          css << " disabled" if previous_question.blank?
+          question = previous_question || current_question
+          css << " disabled" if previous_question.nil?
         when :next
           i18n_text = t("next_button", scope: "decidim.questions")
-          url = decidim_consultations.question_path(next_question || current_question)
-          css << " disabled" if next_question.blank?
+          question = next_question || current_question
+          css << " disabled" if next_question.nil?
         end
 
-        link_to(i18n_text, url, class: css)
+        link_to(i18n_text, decidim_consultations.question_path(question), class: css)
       end
     end
   end
