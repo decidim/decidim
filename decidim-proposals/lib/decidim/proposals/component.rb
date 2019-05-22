ef2902fc7f64bdd11c12b646b239137515d55326
ailobe <aitorlopezbeltran@gmail.com>
2019-05-22 18:23:54 +0200

    Add update hook validation

--- decidim-proposals/lib/decidim/proposals/component.rb
+++ decidim-proposals/lib/decidim/proposals/component.rb
@@ -11,6 +11,12 @@ Decidim.register_component(:proposals) do |component|
     raise "Can't destroy this component when there are proposals" if Decidim::Proposals::Proposal.where(component: instance).any?
   end
 
+  component.on(:update) do |instance|
+    update_setting_value = instance.settings.participatory_texts_enabled
+    return unless update_setting_value == instance.reload.settings.participatory_texts_enabled
+    raise "Can't update ParticipatoryText setting when there are proposals" if Decidim::Proposals::Proposal.where(component: instance).any?
+  end
+
   component.data_portable_entities = ["Decidim::Proposals::Proposal"]
 
   component.newsletter_participant_entities = ["Decidim::Proposals::Proposal"]
