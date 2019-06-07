22f84689ed70c9a3d33fa467be0d15254fff3fc9
Isaac Massot <isaac.mg@coditramuntana.com>
2019-06-07 12:56:49 +0200

    add authorization to amend action

--- decidim-proposals/lib/decidim/proposals/component.rb
+++ decidim-proposals/lib/decidim/proposals/component.rb
@@ -15,7 +15,7 @@ Decidim.register_component(:proposals) do |component|
 
   component.newsletter_participant_entities = ["Decidim::Proposals::Proposal"]
 
-  component.actions = %w(endorse vote create withdraw)
+  component.actions = %w(endorse vote create withdraw amend)
 
   component.query_type = "Decidim::Proposals::ProposalsType"
 
@@ -64,7 +64,7 @@ Decidim.register_component(:proposals) do |component|
     resource.model_class_name = "Decidim::Proposals::Proposal"
     resource.template = "decidim/proposals/proposals/linked_proposals"
     resource.card = "decidim/proposals/proposal"
-    resource.actions = %w(endorse vote)
+    resource.actions = %w(endorse vote amend)
     resource.searchable = true
   end
 
