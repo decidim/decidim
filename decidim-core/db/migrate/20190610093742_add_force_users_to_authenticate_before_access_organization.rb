3f00c4ebc2107119c9a60be6cc74fac76291846a
Isaac Massot <isaac.mg@coditramuntana.com>
2019-06-13 14:54:36 +0200

    change migration

--- decidim-core/db/migrate/20190610093742_add_force_users_to_authenticate_before_access_organization.rb
+++ decidim-core/db/migrate/20190610093742_add_force_users_to_authenticate_before_access_organization.rb
@@ -5,25 +5,6 @@ class AddForceUsersToAuthenticateBeforeAccessOrganization < ActiveRecord::Migrat
     add_column :decidim_organizations,
                  :force_users_to_authenticate_before_access_organization,
                  :boolean,
-                 default: :false
+                 default: false
   end
-  
-  # def up
-  #   ActiveRecord::Base.transaction do
-  #     add_column :decidim_organizations,
-  #                :force_users_to_authenticate_before_access_organization,
-  #                :boolean,
-  #                default: :false
-  #   end
-
-  #   # Decidim::Organization.find_each do |organization|
-  #   #   organization.update(force_users_to_authenticate_before_access_organization: false)
-  #   # end
-  # end
-
-  # def down
-  #   ActiveRecord::Base.transaction do
-  #     remove_column :decidim_organizations, :force_users_to_authenticate_before_access_organization
-  #   end
-  # end
 end
