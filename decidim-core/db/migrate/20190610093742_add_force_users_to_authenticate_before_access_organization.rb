531699f79b7c04f418f2c3b258f2a794a43731b1
Isaac Massot <isaac.mg@coditramuntana.com>
2019-06-13 16:07:28 +0200

    fix rubocop

--- decidim-core/db/migrate/20190610093742_add_force_users_to_authenticate_before_access_organization.rb
+++ decidim-core/db/migrate/20190610093742_add_force_users_to_authenticate_before_access_organization.rb
@@ -3,8 +3,8 @@
 class AddForceUsersToAuthenticateBeforeAccessOrganization < ActiveRecord::Migration[5.2]
   def change
     add_column :decidim_organizations,
-                 :force_users_to_authenticate_before_access_organization,
-                 :boolean,
-                 default: false
+               :force_users_to_authenticate_before_access_organization,
+               :boolean,
+               default: false
   end
 end
