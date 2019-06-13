567a600a8ebbdd218e656142ffd835819656219d
Isaac Massot <isaac.mg@coditramuntana.com>
2019-06-13 14:27:51 +0200

    fix migration

--- decidim-core/db/migrate/20190610093742_add_force_users_to_authenticate_before_access_organization.rb
+++ decidim-core/db/migrate/20190610093742_add_force_users_to_authenticate_before_access_organization.rb
@@ -1,32 +1,29 @@
 # frozen_string_literal: true
 
 class AddForceUsersToAuthenticateBeforeAccessOrganization < ActiveRecord::Migration[5.2]
-  def up
-    ActiveRecord::Base.transaction do
-      add_column :decidim_organizations,
+  def change
+    add_column :decidim_organizations,
                  :force_users_to_authenticate_before_access_organization,
                  :boolean,
-                 default: nil
-
-      execute <<~SQL
-        ALTER TABLE decidim_organizations ALTER COLUMN force_users_to_authenticate_before_access_organization SET DEFAULT false;
-      SQL
-    end
-
-    Decidim::Organization.find_each do |organization|
-      organization.update(force_users_to_authenticate_before_access_organization: false)
-    end
-
-    ActiveRecord::Base.transaction do
-      execute <<~SQL
-        ALTER TABLE decidim_organizations ALTER COLUMN force_users_to_authenticate_before_access_organization SET NOT NULL;
-      SQL
-    end
+                 default: :false
   end
+  
+  # def up
+  #   ActiveRecord::Base.transaction do
+  #     add_column :decidim_organizations,
+  #                :force_users_to_authenticate_before_access_organization,
+  #                :boolean,
+  #                default: :false
+  #   end
 
-  def down
-    ActiveRecord::Base.transaction do
-      remove_column :decidim_organizations, :force_users_to_authenticate_before_access_organization
-    end
-  end
+  #   # Decidim::Organization.find_each do |organization|
+  #   #   organization.update(force_users_to_authenticate_before_access_organization: false)
+  #   # end
+  # end
+
+  # def down
+  #   ActiveRecord::Base.transaction do
+  #     remove_column :decidim_organizations, :force_users_to_authenticate_before_access_organization
+  #   end
+  # end
 end
