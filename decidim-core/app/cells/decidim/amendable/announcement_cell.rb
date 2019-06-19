365503eb1b23b7a5db140fae967ea41e21fbbda5
Oriol Gual <oriol@codegram.com>
2019-06-19 10:54:41 +0200

    fix Rubocop

--- decidim-core/app/cells/decidim/amendable/announcement_cell.rb
+++ decidim-core/app/cells/decidim/amendable/announcement_cell.rb
@@ -24,6 +24,7 @@ module Decidim::Amendable
 
     def promoted_message
       return "" unless model.amendment.promoted?
+
       proposal = model.linked_promoted_resource
       text = message(:promoted, amendable_type)
       %(<br><strong>#{proposal_link(proposal, text)}</strong>)
