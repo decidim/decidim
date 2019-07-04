bdcb74ab2c0aa1da34780ebd86929c36cb4154a9
Ivan Vergés <ivan@platoniq.net>
2019-07-04 14:38:42 +0200

    fix results count

--- decidim-consultations/app/models/decidim/consultations/question.rb
+++ decidim-consultations/app/models/decidim/consultations/question.rb
@@ -68,12 +68,19 @@ module Decidim
         @most_voted_response ||= responses.order(votes_count: :desc).first
       end
 
+      # Total number of votes, on multiple votes questions does not match users voting
       def total_votes
         @total_votes ||= responses.sum(&:votes_count)
       end
 
+      # Total number of users voting
+      def total_participants
+        @total_participants ||= votes.select(:decidim_author_id).distinct.count
+      end
+
       # Multiple answers allowed?
       def multiple?
+        return false if external_voting
         max_responses&.> 1
       end
 
