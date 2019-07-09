6486d2a3a4acbb0be754c05f6aa50743df80f14b
Ivan Vergés <ivan@platoniq.net>
2019-07-10 00:22:03 +0200

    grid fixes

--- decidim-consultations/app/controllers/decidim/consultations/admin/responses_controller.rb
+++ decidim-consultations/app/controllers/decidim/consultations/admin/responses_controller.rb
@@ -7,7 +7,7 @@ module Decidim
       class ResponsesController < Decidim::Consultations::Admin::ApplicationController
         include QuestionAdmin
 
-        helper QuestionsHelper
+        helper Admin::QuestionsHelper
         helper_method :current_response
 
         def index
