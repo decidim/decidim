ea815c99cdca29f2024194be94f06cc80dfc7457
Ivan Vergés <ivan@platoniq.net>
2019-07-10 18:54:14 +0200

    add asset to javascript

--- decidim-consultations/app/assets/config/decidim_consultations_manifest.js
+++ decidim-consultations/app/assets/config/decidim_consultations_manifest.js
@@ -1,5 +1,6 @@
 //= link_directory ../images/decidim/consultations
 //= link decidim/consultations/social_share.js
 //= link decidim/consultations/utils.js
+//= link decidim/consultations/utils_multiple.js
 //= link decidim/consultations/vote_dialog.js
 //= link decidim/consultations/show_more.js
