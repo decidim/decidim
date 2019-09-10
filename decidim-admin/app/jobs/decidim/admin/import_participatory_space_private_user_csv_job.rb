2bafcf3e5d43c60387ad67096385c17d649f8712
Isaac Massot <isaac.mg@coditramuntana.com>
2019-09-10 12:16:46 +0200

    Update decidim-admin/app/jobs/decidim/admin/import_participatory_space_private_user_csv_job.rb
    Co-Authored-By: Oriol Gual <oriolgual@users.noreply.github.com>

--- decidim-admin/app/jobs/decidim/admin/import_participatory_space_private_user_csv_job.rb
+++ decidim-admin/app/jobs/decidim/admin/import_participatory_space_private_user_csv_job.rb
@@ -8,7 +8,7 @@ module Decidim
       queue_as :default
 
       def perform(email, user_name, privatable_to, current_user)
-        return if email.blank? && user_name.blank?
+        return if email.blank? || user_name.blank?
 
         params = {
           name: user_name,
