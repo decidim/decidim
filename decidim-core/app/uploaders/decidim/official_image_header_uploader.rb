604f84812eeb83db070dabdc2ca2c484775da981
Oriol Gual <oriol@codegram.com>
2019-09-23 15:21:07 +0200

    Limit allowed file extensions to be uploaded and strip image metadata

--- decidim-core/app/uploaders/decidim/official_image_header_uploader.rb
+++ decidim-core/app/uploaders/decidim/official_image_header_uploader.rb
@@ -3,7 +3,6 @@
 module Decidim
   # This class deals with uploading hero images to ParticipatoryProcesses.
   class OfficialImageHeaderUploader < ImageUploader
-    include CarrierWave::MiniMagick
     process resize_to_fit: [160, 160]
   end
 end
