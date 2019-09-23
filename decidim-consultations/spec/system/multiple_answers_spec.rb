41642899a94cc61177cbf41247cedb0a22f6ab6f
Ivan Vergés <ivan@platoniq.net>
2019-09-23 18:40:30 +0200

    multiple answers ui specs (wip)

--- decidim-consultations/spec/system/multiple_answers_spec.rb
+++ decidim-consultations/spec/system/multiple_answers_spec.rb
@@ -4,10 +4,11 @@ require "spec_helper"
 
 describe "Multiple Answers Question", type: :system do
   let(:organization) { create(:organization) }
-  let(:consultation) { create(:consultation, :published, organization: organization) }
+  let(:consultation) { create(:consultation, :active, organization: organization) }
   let(:question) { create :question, :multiple, :published, consultation: consultation }
+  let(:user) { create :user, :confirmed, organization: organization }
 
-  context "when shows question information" do
+  context "and guest user" do
     before do
       switch_to_host(organization.host)
       visit decidim_consultations.question_path(question)
@@ -23,15 +24,37 @@ describe "Multiple Answers Question", type: :system do
     end
 
     it "Page contains a vote button" do
-      expect(page).to have_link(id: "vote_button")
+      expect(page).to have_button(id: "vote_button")
+      expect(page).not_to have_link(id: "multivote_button")
     end
 
-    # it "unvote button appears after voting" do
-    #   click_link(id: "vote_button")
-    #   click_button translated(response.title)
-    #   click_button "Confirm"
-    #   expect(page).to have_button(id: "unvote_button")
-    # end
+    it "Page do not contains an unvote button" do
+      expect(page).not_to have_button(id: "unvote_button")
+    end
+  end
+
+  context "and authenticated user" do
+    # let!(:response) { create :response, question: question }
+
+    context "and never voted before" do
+      before do
+        switch_to_host(organization.host)
+        login_as user, scope: :user
+        visit decidim_consultations.question_path(question)
+      end
+
+      it "Page contains a vote button" do
+        expect(page).to have_link(id: "multivote_button")
+      end
+
+      # it "unvote button appears after voting" do
+      #   click_link(id: "vote_button")
+      #   click_button translated(response.title)
+      #   click_button "Confirm"
+      #   expect(page).to have_button(id: "unvote_button")
+      # end
+
+    end
   end
 
 end
