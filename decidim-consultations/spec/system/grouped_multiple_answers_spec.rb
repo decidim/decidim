0f50bd65e6b3584dea7f05ecb4d2c764cabac3c8
Ivan Vergés <ivan@platoniq.net>
2019-09-24 21:20:01 +0200

    grouped answers user behavior specs

---
+++ decidim-consultations/spec/system/grouped_multiple_answers_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Grouped Multiple Answers Question", type: :system do
+  let(:organization) { create(:organization) }
+  let(:consultation) { create(:consultation, :active, organization: organization) }
+  let(:question) { create :question, :multiple, :published, consultation: consultation }
+  let(:response_group) { create :response_group }
+  let(:user) { create :user, :confirmed, organization: organization }
+
+  context "when response is grouped" do
+    let!(:response) { create :response, question: question, response_group: response_group }
+
+    before do
+      switch_to_host(organization.host)
+      login_as user, scope: :user
+      visit decidim_consultations.question_question_multiple_votes_path(question)
+    end
+
+    it "has group title" do
+      within ".form .card" do
+        expect(page).to have_unchecked_field("vote_id_#{response.id}")
+        expect(page).to have_i18n_content(response_group.title)
+      end
+    end
+  end
+
+  context "when response is not grouped" do
+    let!(:response) { create :response, question: question }
+
+    before do
+      switch_to_host(organization.host)
+      login_as user, scope: :user
+      visit decidim_consultations.question_question_multiple_votes_path(question)
+    end
+
+    it "do not have group title" do
+      within ".form .card" do
+        expect(page).to have_unchecked_field("vote_id_#{response.id}")
+        expect(page).not_to have_i18n_content(response_group.title)
+      end
+    end
+  end
+end
