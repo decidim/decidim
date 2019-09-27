4ff5cac32871829494c3667ae8c79e44333b650f
Oliver Valls <oliver.vh@coditramuntana.com>
2019-09-27 11:41:24 +0200

    [REFACTOR] More consistent Proposal's presenters and fix escaping order.

--- decidim-proposals/app/presenters/decidim/proposals/proposal_presenter.rb
+++ decidim-proposals/app/presenters/decidim/proposals/proposal_presenter.rb
@@ -43,19 +43,21 @@ module Decidim
       #
       # Returns a String.
       def title(links: false, extras: true, html_escape: false)
-        renderer = Decidim::ContentRenderers::HashtagRenderer.new(proposal.title)
-        text = renderer.render(links: links, extras: extras).html_safe
+        text = proposal.title
         text = decidim_html_escape(text) if html_escape
-        text
+
+        renderer = Decidim::ContentRenderers::HashtagRenderer.new(text)
+        renderer.render(links: links, extras: extras).html_safe
       end
 
       def body(links: false, extras: true, strip_tags: false)
-        renderer = Decidim::ContentRenderers::HashtagRenderer.new(proposal.body)
+        text = proposal.body
+        text = strip_tags(text) if strip_tags
+
+        renderer = Decidim::ContentRenderers::HashtagRenderer.new(text)
         text = renderer.render(links: links, extras: extras).html_safe
-        if strip_tags
-          text = strip_tags(text)
-          text = Anchored::Linker.auto_link(text, target: "_blank", rel: "noopener")
-        end
+
+        text = Anchored::Linker.auto_link(text, target: "_blank", rel: "noopener") if links
         text
       end
     end
