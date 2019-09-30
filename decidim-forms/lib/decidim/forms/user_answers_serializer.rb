f061449914bbddea93099c707f96ee5d6960c988
Ivan Vergés <ivan@platoniq.net>
2019-09-30 16:45:40 +0200

    Add user status to answers export

--- decidim-forms/lib/decidim/forms/user_answers_serializer.rb
+++ decidim-forms/lib/decidim/forms/user_answers_serializer.rb
@@ -18,6 +18,8 @@ module Decidim
           serialized.update(
             answer_translated_attribute_name(:id) => answer.id,
             answer_translated_attribute_name(:created_at) => answer.created_at.to_s(:db),
+            answer_translated_attribute_name(:ip_hash) => answer.ip_hash,
+            answer_translated_attribute_name(:user_status) => answer_translated_attribute_name(answer.decidim_user_id.present? ? "registered" : "unregistered"),
             "#{idx + 1}. #{translated_attribute(answer.question.body)}" => normalize_body(answer)
           )
         end
