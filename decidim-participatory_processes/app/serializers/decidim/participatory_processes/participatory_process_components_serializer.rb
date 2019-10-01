c247196f03ff94c58771b7c32d11aa579ae7b6eb
Oliver Valls <oliver.vh@coditramuntana.com>
2019-10-01 18:53:41 +0200

    [WIP] First idea to serialize components.

---
+++ decidim-participatory_processes/app/serializers/decidim/participatory_processes/participatory_process_components_serializer.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module Decidim
+  module ParticipatoryProcesses
+    # This class serializes a ParticipatoryProcesses so can be exported to CSV, JSON or other
+    # formats.
+    class ParticipatoryProcessComponentsSerializer < Decidim::Exporters::Serializer
+      include Decidim::ApplicationHelper
+      include Decidim::ResourceHelper
+      include Decidim::TranslationsHelper
+
+      # Public: Initializes the serializer with a participatory_process.
+      def initialize(participatory_process)
+        @participatory_process = participatory_process
+      end
+
+      # Public: Exports a hash with the serialized data for this participatory_process.
+      def serialize
+        response= []
+        participatory_process.components.each do |component|
+          jcomponent = {
+            component_class: component.class.name, # do we really need it?
+            manifest_name: component.manifest_name,
+            name: component.name,
+            participatory_space_id: component.participatory_space_id,
+            participatory_space_type: component.participatory_space_type,
+            settings: serialize_settings(component),
+            weight: component.weight,
+            permissions: component.permissions,
+          }
+          fill_component_specific_fields(jcomponent) if has_component_specific_fields?(component)
+          response << jcomponent
+        end
+        jcomponent
+      end
+
+      private
+
+      attr_reader :participatory_process
+
+      def has_component_specific_fields?(component)
+        component.manifest.serializes_specific_fields?
+      end
+    end
+  end
+end
