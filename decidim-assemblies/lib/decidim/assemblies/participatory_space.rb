df18119cb1074547fdea5589d1720643c136c3f5
Oliver Valls <oliver.vh@coditramuntana.com>
2019-11-01 18:50:39 +0100

    [FEATURE] Make assemblies searchable.

--- decidim-assemblies/lib/decidim/assemblies/participatory_space.rb
+++ decidim-assemblies/lib/decidim/assemblies/participatory_space.rb
@@ -13,6 +13,7 @@ Decidim.register_participatory_space(:assemblies) do |participatory_space|
   participatory_space.register_resource(:assembly) do |resource|
     resource.model_class_name = "Decidim::Assembly"
     resource.card = "decidim/assemblies/assembly"
+    resource.searchable = true
   end
 
   participatory_space.context(:public) do |context|
