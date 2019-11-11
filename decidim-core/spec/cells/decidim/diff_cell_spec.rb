d97cc8496fa19a32dc2ee7823f701a778928fda1
ailobe <aitorlopezbeltran@gmail.com>
2019-11-11 16:58:50 +0100

    Add option to view HTML unescaped for better user experience in diff cell

--- decidim-core/spec/cells/decidim/diff_cell_spec.rb
+++ decidim-core/spec/cells/decidim/diff_cell_spec.rb
@@ -27,4 +27,61 @@ describe Decidim::DiffCell, versioning: true, type: :cell do
       expect(subject).to have_css(".diff-for-scope .diff-data .ins")
     end
   end
+
+  describe "view unescaped html" do
+    include_context "with content"
+
+    let(:component) { create(:proposal_component, settings: settings) }
+    let(:item) { create(:proposal, component: component, body: content) }
+    let(:settings) { {} }
+
+    context "when rich text editor is enabled on the frontend" do
+      let(:settings) { { rich_editor_public_view: true } }
+
+      it "shows the HTML view dropdown menu" do
+        expect(subject).to have_css(".diff-view-html .dropdown .menu")
+      end
+    end
+
+    context "when rich text editor is NOT enabled on the frontend" do
+      let(:settings) { { rich_editor_public_view: false } }
+
+      it "does NOT show the HTML view dropdown menu" do
+        expect(subject).not_to have_css(".diff-view-html .dropdown .menu")
+      end
+    end
+
+    context "with diff_view_unified_unescaped" do
+      let(:html) { subject.find(".diff-for-body #diff_view_unified_unescaped") }
+
+      it "renders potentially safe HTML tags unescaped" do
+        expect(html).to have_selector("em", text: "em")
+        expect(html).to have_selector("u", text: "u")
+        expect(html).to have_selector("strong", text: "strong")
+      end
+
+      it "sanitizes potentially malicious HTML tags" do
+        expect(html).not_to have_selector("script", visible: false)
+        expect(html).to have_content("alert('SCRIPT')")
+      end
+    end
+
+    context "with diff_view_unified_escaped" do
+      let(:html) { subject.find(".diff-for-body #diff_view_unified_escaped") }
+
+      it "sanitizes potentially safe HTML tags" do
+        expect(html).not_to have_selector("em")
+        expect(html).to have_content("em")
+        expect(html).not_to have_selector("u")
+        expect(html).to have_content("u")
+        expect(html).not_to have_selector("strong")
+        expect(html).to have_content("strong")
+      end
+
+      it "sanitizes potentially malicious HTML tags" do
+        expect(html).not_to have_selector("script", visible: false)
+        expect(html).to have_content("alert('SCRIPT')")
+      end
+    end
+  end
 end
