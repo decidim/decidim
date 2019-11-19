b41c67df7050d98793e5b1a1834e235ed0dfbb88
Ivan Vergés <ivan@platoniq.net>
2019-11-19 13:19:56 +0100

    unscope category database queries in tests

--- decidim-core/lib/decidim/core/test/factories.rb
+++ decidim-core/lib/decidim/core/test/factories.rb
@@ -55,7 +55,7 @@ FactoryBot.define do
   factory :category, class: "Decidim::Category" do
     name { generate_localized_title }
     description { Decidim::Faker::Localized.wrapped("<p>", "</p>") { generate_localized_title } }
-    weight { Faker::Number.number(1) }
+    weight { 0 }
 
     association :participatory_space, factory: :participatory_process
   end
