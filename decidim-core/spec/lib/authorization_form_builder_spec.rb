836288e4402e9dd05d24b262bce8f6e2519c676f
Oriol Gual <oriol@codegram.com>
2019-11-19 11:42:11 +0100

    fix scope factory

--- decidim-core/spec/lib/authorization_form_builder_spec.rb
+++ decidim-core/spec/lib/authorization_form_builder_spec.rb
@@ -30,13 +30,24 @@ module Decidim
         expect(find("input#authorization_handler_date_of_birth")["data-date-format"]).to eq("dd/mm/yyyy")
         expect(find("input#authorization_handler_postal_code")["type"]).to eq("text")
         expect(find("input#authorization_handler_document_number")["type"]).to eq("text")
-        expect(find("input#authorization_handler_scope_id")["type"]).to eq("text")
         expect(find("input#authorization_handler_name_and_surname")["type"]).to eq("text")
       end
 
       it "does not include other handler attributes" do
         expect(find("input#authorization_handler_id")).to eq(nil)
         expect(find("input#authorization_handler_user")).to eq(nil)
+      end
+
+      context "when there are scopes" do
+        let(:user) { create(:user) }
+        let!(:scope) { create(:scope, organization: user.organization) }
+        let(:record) do
+          DummyAuthorizationHandler.new(user: user)
+        end
+
+        it "includes a scopes selector" do
+          expect(find("select#authorization_handler_scope_id").children.first["value"]).to eq(scope.id.to_s)
+        end
       end
     end
 
@@ -69,11 +80,13 @@ module Decidim
           handler_name: String,
           document_number: String,
           postal_code: String,
-          date_of_birth: Date
+          date_of_birth: Date,
+          scope_id: Integer,
+          name_and_surname: String
         }
       end
 
-      it { is_expected.to eq(public_attributes) }
+      it { is_expected.to match_array(public_attributes) }
     end
   end
 end
