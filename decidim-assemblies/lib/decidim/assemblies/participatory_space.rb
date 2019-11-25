eb9d72cdb8ab5bd6f8c8b91decaaeb5eebf89a2b
Oliver Valls <oliver.vh@coditramuntana.com>
2019-11-25 13:16:31 +0100

    In assemblies' seeds index both parent and child assemblies.

--- decidim-assemblies/lib/decidim/assemblies/participatory_space.rb
+++ decidim-assemblies/lib/decidim/assemblies/participatory_space.rb
@@ -99,7 +99,6 @@ Decidim.register_participatory_space(:assemblies) do |participatory_space|
       ) do
         Decidim::Assembly.create!(params)
       end
-      assembly.add_to_index_as_search_resource
 
       # Create users with specific roles
       Decidim::AssemblyUserRole::ROLES.each do |role|
@@ -151,6 +150,7 @@ Decidim.register_participatory_space(:assemblies) do |participatory_space|
       )
 
       [assembly, child].each do |current_assembly|
+        current_assembly.add_to_index_as_search_resource
         attachment_collection = Decidim::AttachmentCollection.create!(
           name: Decidim::Faker::Localized.word,
           description: Decidim::Faker::Localized.sentence(5),
