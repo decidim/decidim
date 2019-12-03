8d49f57c7511ec5d29dbfec1443940e927b4463a
Ivan Vergés <ivan@platoniq.net>
2019-12-03 16:53:45 +0100

    add translatable fields to meetings. Add services interface

--- decidim-meetings/app/types/decidim/meetings/meeting_type.rb
+++ decidim-meetings/app/types/decidim/meetings/meeting_type.rb
@@ -10,12 +10,14 @@ module Decidim
         -> { Decidim::Comments::CommentableInterface },
         -> { Decidim::Core::CategorizableInterface },
         -> { Decidim::Core::ScopableInterface },
-        -> { Decidim::Core::AttachableInterface }
+        -> { Decidim::Core::AttachableInterface },
+        -> { Decidim::Meetings::ServicesInterface }
       ]
 
       field :id, !types.ID
       field :reference, !types.String
       field :title, !Decidim::Core::TranslatedFieldType, "The title of this meeting."
+      field :description, Decidim::Core::TranslatedFieldType, "The description of this meeting."
       field :startTime, !Decidim::Core::DateTimeType, "The time this meeting starts", property: :start_time
       field :endTime, !Decidim::Core::DateTimeType, "The time this meeting ends", property: :end_time
 
@@ -23,11 +25,14 @@ module Decidim
       field :closingReport, Decidim::Core::TranslatedFieldType, "The closing report of this meeting.", property: :closing_report
 
       field :registrationsEnabled, !types.Boolean, "Whether the registrations are enabled or not", property: :registrations_enabled
+      field :registrationTerms, Decidim::Core::TranslatedFieldType, "The registration terms", property: :registration_terms
       field :remainingSlots, types.Int, "Amount of slots available for this meeting", property: :remaining_slots
       field :attendeeCount, types.Int, "Amount of attendees to this meeting", property: :attendees_count
       field :contributionCount, types.Int, "Amount of contributions to this meeting", property: :contributions_count
 
-      field :address, types.String, "The physical address of this meeting"
+      field :location, Decidim::Core::TranslatedFieldType, "The location of this meeting (free format)"
+      field :locationHints, Decidim::Core::TranslatedFieldType, "The location of this meeting (free format)", property: :location_hints
+      field :address, types.String, "The physical address of this meeting (used for geolocation)"
       field :coordinates, Decidim::Core::CoordinatesType, "Physical coordinates for this meeting" do
         resolve ->(meeting, _args, _ctx) {
           [meeting.latitude, meeting.longitude]
