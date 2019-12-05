9da6ae888e982d618dce84d10620c3f4ec2c25ef
Agusti B.R <agusti.br@coditramuntana.com>
2019-12-05 13:42:04 +0100

    fix and simplify admin terms permissions

--- decidim-admin/app/controllers/decidim/admin/admin_terms_controller.rb
+++ decidim-admin/app/controllers/decidim/admin/admin_terms_controller.rb
@@ -5,13 +5,7 @@ module Decidim
     # The controller to handle the Admin
     # Terms of use agreement.
     class AdminTermsController < Decidim::Admin::ApplicationController
-      def show
-        enforce_permission_to :read, :admin_terms_of_use
-      end
-
       def accept
-        enforce_permission_to :accept, :admin_terms_of_use
-
         current_user.admin_terms_accepted_at = Time.current
         if current_user.save!
           flash[:notice] = t("accept.success", scope: "decidim.admin.admin_terms_of_use")
@@ -23,8 +17,6 @@ module Decidim
       end
 
       def refuse
-        enforce_permission_to :refuse, :admin_terms_of_use
-
         current_user.admin_terms_accepted_at = nil
         if current_user.save!
           flash[:notice] = t("refuse.success", scope: "decidim.admin.admin_terms_of_use")
--- decidim-admin/app/permissions/decidim/admin/permissions.rb
+++ decidim-admin/app/permissions/decidim/admin/permissions.rb
@@ -4,7 +4,6 @@ module Decidim
   module Admin
     class Permissions < Decidim::DefaultPermissions
       def permissions
-        return admin_terms_permissions if permission_action.scope == :admin && !user.admin_terms_accepted?
         return permission_action if managed_user_action?
 
         unless permission_action.scope == :admin
@@ -19,12 +18,12 @@ module Decidim
 
         return user_manager_permissions if user_manager?
 
-        allow! if user_can_enter_space_area?
+        allow! if user_can_enter_space_area?(require_admin_terms_accepted: true)
 
         read_admin_dashboard_action?
         apply_newsletter_permissions_for_admin!
 
-        if user.admin? && user.admin_terms_accepted?
+        if user.admin? && admin_terms_accepted?
           allow! if read_admin_log_action?
           allow! if static_page_action?
           allow! if organization_action?
@@ -54,7 +53,7 @@ module Decidim
       private
 
       def user_manager?
-        return false unless user.admin_terms_accepted?
+        return unless admin_terms_accepted?
 
         user && !user.admin? && user.role?("user_manager")
       end
@@ -69,6 +68,7 @@ module Decidim
       end
 
       def apply_newsletter_permissions_for_admin!
+        return unless admin_terms_accepted?
         return unless permission_action.subject == :newsletter
         return allow! if user.admin?
         return unless space_allows_admin_access?
@@ -122,6 +122,7 @@ module Decidim
       end
 
       def managed_user_action?
+        return unless admin_terms_accepted?
         return unless permission_action.subject == :managed_user
         return user_manager_permissions if user_manager?
         return unless user&.admin?
@@ -160,15 +161,19 @@ module Decidim
         @organization ||= context.fetch(:organization, nil) || context.fetch(:current_organization, nil)
       end
 
-      def user_can_enter_space_area?
+      def user_can_enter_space_area?(**args)
         return unless permission_action.action == :enter &&
                       permission_action.subject == :space_area
 
-        space_allows_admin_access_to_current_action?
+        space_allows_admin_access_to_current_action?(args)
       end
 
-      def space_allows_admin_access_to_current_action?
+      def space_allows_admin_access_to_current_action?(require_admin_terms_accepted: false)
         Decidim.participatory_space_manifests.any? do |manifest|
+          if manifest.name != :initiatives && require_admin_terms_accepted
+            next unless admin_terms_accepted?
+          end
+
           new_permission_action = Decidim::PermissionAction.new(
             action: permission_action.action,
             scope: permission_action.scope,
@@ -184,8 +189,10 @@ module Decidim
         Decidim::Admin::UserManagerPermissions.new(user, permission_action, context).permissions
       end
 
-      def admin_terms_permissions
-        Decidim::Admin::AdminTermsPermissions.new(user, permission_action, context).permissions
+      def admin_terms_accepted?
+        return unless permission_action.scope == :admin
+
+        user&.admin_terms_accepted?
       end
 
       def available_authorization_handlers?
