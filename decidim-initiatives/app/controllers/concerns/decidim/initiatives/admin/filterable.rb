009ae2a76a77ba083b1e0b88b371bc870e75248b
ailobe <aitorlopezbeltran@gmail.com>
2019-12-10 15:57:03 +0100

    Add filterable to initiatives

---
+++ decidim-initiatives/app/controllers/concerns/decidim/initiatives/admin/filterable.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "active_support/concern"
+
+module Decidim
+  module Initiatives
+    module Admin
+      module Filterable
+        extend ActiveSupport::Concern
+
+        included do
+          include Decidim::Admin::Filterable
+
+          private
+
+          def base_query
+            collection
+          end
+
+          def search_field_predicate
+            :title_or_description_cont
+          end
+
+          def filters
+            [:state_eq]
+          end
+
+          def filters_with_values
+            {
+              state_eq: %w(
+                created
+                validating
+                discarded
+                published
+                rejected
+                accepted
+              )
+            }
+          end
+        end
+      end
+    end
+  end
+end
