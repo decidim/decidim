3d6651c07bc7c3c1b55847e1c7c0f429189dc0b6
jarvisct <javier.jd@coditramuntana.com>
2019-12-10 16:59:23 +0100

    Add filterable to participants

--- decidim-admin/app/controllers/decidim/admin/officializations_controller.rb
+++ decidim-admin/app/controllers/decidim/admin/officializations_controller.rb
@@ -5,7 +5,7 @@ module Decidim
     # Controller that allows managing user officializations at the admin panel.
     #
     class OfficializationsController < Decidim::Admin::ApplicationController
-      include Decidim::Admin::Paginable
+      include Decidim::Admin::Officializations::Filterable
 
       layout "decidim/admin/users"
 
@@ -14,10 +14,7 @@ module Decidim
 
       def index
         enforce_permission_to :read, :officialization
-        @query = params[:q]
-        @state = params[:state]
-        users = Decidim::Admin::UserFilter.for(current_organization.users.not_deleted, @query, @state)
-        @users = paginate(users)
+        @users = filtered_collection
       end
 
       def new
@@ -53,6 +50,10 @@ module Decidim
       end
 
       private
+
+      def collection
+        @collection ||= current_organization.users.not_deleted
+      end
 
       def user
         @user ||= Decidim::User.find_by(
