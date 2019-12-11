04883d5543d7cc294acc9188a91d1feec58c141c
jarvisct <javier.jd@coditramuntana.com>
2019-12-11 17:03:26 +0100

    Extract endorser identities partial into a generic identities cell. Check if some already exists.

---
+++ decidim-core/app/cells/decidim/endorsers_list_cell.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "cell/partial"
+
+module Decidim
+  # This cell renders the list of endorsers.
+  #
+  # Example:
+  #
+  #    cell("decidim/endorsers_list", my_component)
+  class EndorsersListCell < Decidim::ViewModel
+    include ApplicationHelper
+
+    def show
+      return unless endorsers.any?
+
+      render
+    end
+
+    private
+
+    # Finds the correct author for each endorsement.
+    #
+    # Returns an Array of presented Users/UserGroups
+    def endorsers
+      @endorsers ||= model.endorsements.for_listing.map { |identity| present(identity.normalized_author) }
+    end
+  end
+end
