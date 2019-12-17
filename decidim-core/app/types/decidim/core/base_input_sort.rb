5bea9ef9c8e6186d61664614733eafef36436f15
Ivan Vergés <ivan@platoniq.net>
2019-12-17 12:40:44 +0100

    refactor locale argument. use it only when needed

--- decidim-core/app/types/decidim/core/base_input_sort.rb
+++ decidim-core/app/types/decidim/core/base_input_sort.rb
@@ -3,16 +3,6 @@
 module Decidim
   module Core
     class BaseInputSort < GraphQL::Schema::InputObject
-      argument :locale,
-               type: String,
-               description: "Specify the locale to use when ordering translated fields, otherwise default organization language will be used",
-               required: false,
-               prepare: ->(locale, ctx) do
-                 raise GraphQL::ExecutionError, "#{locale} locale is not used in the organization" unless ctx[:current_organization].available_locales.include?(locale)
-
-                 locale
-               end
-
       # Overwrite the prepare method to allow 2 possible values only
       def prepare
         arguments.each do |key, value|
