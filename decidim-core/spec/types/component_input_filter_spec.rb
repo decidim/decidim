9ba802fef8890e4b067eff823f651ec4a95ff3de
Ivan Vergés <ivan@platoniq.net>
2019-12-18 13:20:34 +0100

    fix tests

--- decidim-core/spec/types/component_input_filter_spec.rb
+++ decidim-core/spec/types/component_input_filter_spec.rb
@@ -10,72 +10,81 @@ module Decidim
       include_context "with a graphql type"
       let(:type_class) { Decidim::ParticipatoryProcesses::ParticipatoryProcessType }
 
-      let(:model) { create(:proposal_component) }
+      let(:model) { create(:participatory_process, organization: current_organization) }
+      let!(:proposal) { create(:proposal_component, :published, participatory_space: model) }
+      let!(:dummy) { create(:component, :published, participatory_space: model) }
 
 
-      describe "type is Proposal" do
-        let(:query) {  "{ components(filter: { type: \"Proposal\"}) { id } }" }
-        before do
-          model.participatory_space.organization = current_organization
-          model.participatory_space.save!
-        end
-
-        it "returns the component" do
-          expect(response["components"]).to eq(model.id)
+      context "when no filters are applied" do
+        let(:query) {  %[{ components(filter: {}) { id } }] }
+        it "returns all the types" do
+          ids = response["components"].map { |component| component["id"].to_i }
+          expect(ids).to include(*proposal.id)
+          expect(ids).to include(*dummy.id)
         end
       end
 
-      describe "type is not Proposal" do
-        let(:query) {  "{ components(filter: { type: \"Accountability\"}) { id } }" }
+      context "when filtering by type" do
+        let(:query) {  %[{ components(filter: { type: "proposals"}) { id } }] }
 
-        it "returns the component" do
+        it "returns the types requested" do
+          ids = response["components"].map { |component| component["id"].to_i }
+          expect(ids).to include(*proposal.id)
+          expect(ids).not_to include(*dummy.id)
+        end
+      end
+
+      context "when type is not present" do
+        let(:query) {  %[{ components(filter: { type: "other_type"}) { id } }] }
+
+        it "returns and empty array" do
           expect(response["components"]).to eq([])
         end
       end
 
-      context "when searching components with comments" do
+      # context "when searching components with comments" do
 
-        let(:model_with_comments_enabled) { create(:proposal_component)}
-        let(:model_with_comments_disabled) { create(:proposal_component, :with_comments_disabled)}
+      #   let(:model_with_comments_enabled) { create(:proposal_component)}
+      #   let(:model_with_comments_disabled) { create(:proposal_component, :with_comments_disabled)}
 
-        describe "comments enabled" do
-          let(:query) {  "{ components(filter: { withCommentsEnabled: true} ) { id } }" }
+      #   describe "comments enabled" do
+      #     let(:query) {  "{ components(filter: { withCommentsEnabled: true} ) { id } }" }
 
-          it "returns the component" do
-            expect(response["components"]).to eq(model_with_comments_enabled.id)
-          end
-        end
+      #     it "returns the component" do
+      #       expect(response["components"]).to eq(model_with_comments_enabled.id)
+      #     end
+      #   end
 
-        describe "comments not enabled" do
-          let(:query) { "{ components(filter: { withCommentsEnabled: true } ) { id } }" }
+      #   describe "comments not enabled" do
+      #     let(:query) { "{ components(filter: { withCommentsEnabled: true } ) { id } }" }
 
-          it "returns the component" do
-            expect(response["components"]).to eq(model_with_comments_disabled.id)
-          end
-        end
-      end
+      #     it "returns the component" do
+      #       expect(response["components"]).to eq(model_with_comments_disabled.id)
+      #     end
+      #   end
+      # end
 
-      context "when searching components with geocoding" do
+      # context "when searching components with geocoding" do
 
-        let(:model_with_geocoding_enabled) { create(:proposal_component, :with_geocoding_enabled)}
-        let(:model_with_geocoding_disabled) { create(:proposal_component)}
+      #   let(:model_with_geocoding_enabled) { create(:proposal_component, :with_geocoding_enabled)}
+      #   let(:model_with_geocoding_disabled) { create(:proposal_component)}
 
-        describe "comments enabled" do
-          let(:query) {  "{ components(filter: { withCommentsEnabled: true} ) { id } }" }
+      #   describe "comments enabled" do
+      #     let(:query) {  "{ components(filter: { withCommentsEnabled: true} ) { id } }" }
 
-          it "returns the component" do
-            expect(response["components"]).to eq(model_with_geocoding_enabled.id)
-          end
-        end
+      #     it "returns the component" do
+      #       expect(response["components"]).to eq(model_with_geocoding_enabled.id)
+      #     end
+      #   end
 
-        describe "comments not enabled" do
-          let(:query) { "{ components(filter: { withCommentsEnabled: false } ) { id } }" }
+      #   describe "comments not enabled" do
+      #     let(:query) { "{ components(filter: { withCommentsEnabled: false } ) { id } }" }
 
-          it "returns the component" do
-            expect(response["components"]).to eq(model_with_geocoding_disabled.id)
-          end
-        end
-      end
+      #     it "returns the component" do
+      #       expect(response["components"]).to eq(model_with_geocoding_disabled.id)
+      #     end
+      #   end
+      # end
     end
   end
 end
