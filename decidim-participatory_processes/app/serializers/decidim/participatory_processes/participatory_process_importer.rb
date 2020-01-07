a5bffcb2be7b3b32b4d0429d989ff90b5c5c4927
Víctor Ortega <victor.ol@coditramuntana.com>
2020-01-07 09:39:04 +0100

    [FIX] Scopes are activated even if not activated in the orginal process

--- decidim-participatory_processes/app/serializers/decidim/participatory_processes/participatory_process_importer.rb
+++ decidim-participatory_processes/app/serializers/decidim/participatory_processes/participatory_process_importer.rb
@@ -41,6 +41,7 @@ module Decidim
             end_date: attributes["end_date"],
             announcement: attributes["announcement"],
             private_space: attributes["private_space"],
+            scopes_enabled: attributes["scopes_enabled"],
             participatory_process_group: import_process_group(attributes["participatory_process_group"])
           )
           @imported_process.remote_hero_image_url = attributes["remote_hero_image_url"] if remote_file_exists?(attributes["remote_hero_image_url"])
