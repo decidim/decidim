9ee0c0458624a0ad6d8d4706f8c4e5aa35a6f909
Ivan Vergés <ivan@platoniq.net>
2020-01-09 18:59:12 +0100

    assemblies commands tests

--- decidim-assemblies/spec/commands/create_assembly_spec.rb
+++ decidim-assemblies/spec/commands/create_assembly_spec.rb
@@ -8,7 +8,7 @@ module Decidim::Assemblies
 
     let(:organization) { create :organization }
     let(:current_user) { create :user, :admin, :confirmed, organization: organization }
-    let(:assembly_type) { create :assemblies_type, organisation: organisation }
+    let(:assembly_type) { create :assemblies_type, organization: organization }
     let(:scope) { create :scope, organization: organization }
     let(:area) { create :area, organization: organization }
     let(:errors) { double.as_null_object }
@@ -132,6 +132,22 @@ module Decidim::Assemblies
         expect { subject.call }.to change(Decidim::ActionLog, :count)
         action_log = Decidim::ActionLog.last
         expect(action_log.version).to be_present
+      end
+
+      it "links to assembly type" do
+        subject.call
+
+        expect(assembly.assembly_type).to eq(assembly_type)
+      end
+
+      context "when no assembly type is set" do
+        let(:assembly_type) { nil }
+
+        it "assembly type is null" do
+          subject.call
+
+          expect(assembly.assembly_type).to eq(nil)
+        end
       end
 
       it "links participatory processes" do
