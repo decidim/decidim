589fe8c676e28511fe380c9ba714e296d3d9b213
Vera Rojman <vrojman@protonmail.com>
2020-01-16 18:17:26 +0100

    Remove answer-related methods from general forms helper

--- decidim-forms/app/helpers/decidim/forms/admin/application_helper.rb
+++ decidim-forms/app/helpers/decidim/forms/admin/application_helper.rb
@@ -13,26 +13,6 @@ module Decidim
         def tabs_id_for_question_answer_option(question, answer_option)
           "questionnaire_question_#{question.to_param}_answer_option_#{answer_option.to_param}"
         end
-  
-        # TODO: Should we move this to new helper?
-        def questionnaire_answer_body(answer)
-          return answer.body if answer.body.present?
-  
-          choices = answer.choices.map do |choice|
-            choice.try(:custom_body) || choice.try(:body)
-          end
-  
-          choices.join(", ") # TODO: is this separator defined as a locale or similar?
-        end
-  
-        def questionnaire_answer_user_status(answer)
-          answer.decidim_user_id.present? ? "registered" : "unregistered" # TODO: translate
-        end
-  
-        def questionnaire_answer_session_token_mask(answer)
-          token = answer.session_token
-          [token[0..1], token[-2..-1]].join("...")
-        end
       end
     end
   end
