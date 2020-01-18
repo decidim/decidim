388864a2d7f83da6a24b784e73118dfb14bc7071
Vera Rojman <vrojman@protonmail.com>
2020-01-18 15:10:41 +0100

    Return QuestionnaireUserAnswers to its previous state

--- decidim-forms/app/queries/decidim/forms/questionnaire_user_answers.rb
+++ decidim-forms/app/queries/decidim/forms/questionnaire_user_answers.rb
@@ -20,11 +20,7 @@ module Decidim
 
       # Returns query with all questionnaire answers
       def query
-        Answer.where(questionnaire: @questionnaire)
-      end
-
-      # Finds and group answers by user for each questionnaire's question.
-      def grouped_by_user
+        query = Answer.where(questionnaire: @questionnaire)
         query.sort_by { |answer| answer.question.position }.group_by { |a| a.user || a.session_token }.values
       end
     end
