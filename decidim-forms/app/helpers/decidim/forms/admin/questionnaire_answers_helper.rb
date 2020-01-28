c77eeb0bdf0503e84951c8b3a1a1bc25957e9ad2
Vera Rojman <vrojman@protonmail.com>
2020-01-28 17:25:30 +0100

    Create QuestionnaireAnswerPresenter

--- decidim-forms/app/helpers/decidim/forms/admin/questionnaire_answers_helper.rb
+++ decidim-forms/app/helpers/decidim/forms/admin/questionnaire_answers_helper.rb
@@ -6,21 +6,6 @@ module Decidim
       # Custom helpers for questionnaire answers
       #
       module QuestionnaireAnswersHelper
-        def questionnaire_answer_body(answer)
-          return answer.body if answer.body.present?
-          return "-" if answer.choices.empty?
-
-          choices = answer.choices.map do |choice|
-            choice.try(:custom_body) || choice.try(:body)
-          end
-
-          choices.join(", ")
-        end
-
-        def questionnaire_participant_status(registered)
-          t(registered ? "registered" : "unregistered", scope: "decidim.forms.user_answers_serializer")
-        end
-
         def display_percentage(number)
           number_to_percentage(number, precision: 0, strip_insignificant_zeros: true, locale: I18n.locale)
         end
