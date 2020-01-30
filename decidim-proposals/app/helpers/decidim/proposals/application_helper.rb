d72744c8d3fce86fb8d416337cb37bb6303f32d1
Agustí B.R <agusti@codegram.com>
2020-01-30 16:09:25 +0100

    apply participatory process scope to scope filter

--- decidim-proposals/app/helpers/decidim/proposals/application_helper.rb
+++ decidim-proposals/app/helpers/decidim/proposals/application_helper.rb
@@ -228,7 +228,7 @@ module Decidim
       end
 
       def filter_scopes_values
-        main_scopes = current_participatory_space.scopes.top_level
+        main_scopes = [current_participatory_space.scope] || current_participatory_space.scopes.top_level
 
         scopes_values = main_scopes.flat_map do |scope|
           TreeNode.new(
