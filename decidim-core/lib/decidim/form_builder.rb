97f93c405b2316e879b2761b3f73093784b85464
Ivan Molinero <ivan.mr@coditramuntana.com>
2020-02-05 16:55:27 +0100

    Fix: Display values on translated fields with hashtaggable option on edit forms

--- decidim-core/lib/decidim/form_builder.rb
+++ decidim-core/lib/decidim/form_builder.rb
@@ -95,7 +95,7 @@ module Decidim
     end
 
     def translated_one_locale(type, name, locale, options = {})
-      return hashtaggable_text_field(type, name, locale, options.merge(value: options[:value])) if options[:hashtaggable]
+      return hashtaggable_text_field(type, name, locale, options) if options[:hashtaggable]
 
       send(
         type,
