0d8862fcc68f0cbd7c30e63dfa78761bf608a505
Víctor Ortega <victor.ol@coditramuntana.com>
2020-02-27 10:57:50 +0100

    Refactored code and removed all renderers but link renderer

--- decidim-core/spec/models/decidim/messaging/message_spec.rb
+++ decidim-core/spec/models/decidim/messaging/message_spec.rb
@@ -3,7 +3,7 @@
 require "spec_helper"
 
 describe Decidim::Messaging::Message do
-  describe "#formatted_body" do
+  describe "#sanitized_body" do
     let!(:originator) { create(:user) }
     let!(:recipient) { create(:user) }
 
@@ -33,16 +33,16 @@ describe Decidim::Messaging::Message do
       before { allow(Decidim).to receive(:content_processors).and_return([:link]) }
 
       let(:body) do
-        %(Content with <a href="http://urls.net" onmouseover="alert('hello')">URLs</a> of anchor type and text urls like https://decidim.org. And a malicous <a href="javascript:document.cookies">click me</a>)
+        %(Content with <a href="http://urls.net" onmouseover="alert('hello')">URLs</a> of anchor type and text urls like https://decidim.org.)
       end
 
       let(:result) do
-        %(<div><p>Content with URLs of anchor type and text urls like <a href="https://decidim.org" target="_blank" rel="nofollow noopener">https://decidim.org</a>. And a malicous click me</p></div>)
+        %(Content with <a href="http://urls.net">URLs</a> of anchor type and text urls like <a href="https://decidim.org" target="_blank" rel="nofollow noopener">https://decidim.org</a>.)
       end
 
       it "converts all URLs to links and strips attributes in anchors" do
         conversation.add_message(sender: create(:user), body: body)
-        expect(conversation.messages.last.formatted_body).to eq(result)
+        expect(conversation.messages.last.sanitized_body).to eq(result)
       end
     end
   end
