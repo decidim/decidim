c51d337e522cce5f204411a394940d03f5a9cf1d
Vera Rojman <vrojman@protonmail.com>
2020-03-02 12:42:21 +0100

    Add factory spec for conditioned questions

--- decidim-forms/spec/models/decidim/forms/question_spec.rb
+++ decidim-forms/spec/models/decidim/forms/question_spec.rb
@@ -27,6 +27,31 @@ module Decidim
 
         it { is_expected.not_to be_valid }
       end
+
+      describe "scopes" do
+        let(:question_not_conditioned) { create(:questionnaire_question, questionnaire: questionnaire) }
+        let(:question_conditioned) { create(:questionnaire_question, :conditioned, questionnaire: questionnaire) }
+
+        describe "#conditioned" do
+          it "includes questions that have display conditions" do
+            expect(subject.class.conditioned).to contain_exactly(question_conditioned)
+          end
+
+          it "doesn't include questions without display conditions" do
+            expect(subject.class.conditioned).not_to include(question_not_conditioned)
+          end
+        end
+
+        describe "#not_conditioned" do
+          it "includes questions that don't have display conditions" do
+            expect(subject.class.not_conditioned).to contain_exactly(question_not_conditioned)
+          end
+
+          it "doesn't include questions that have display conditions" do
+            expect(subject.class.not_conditioned).not_to include(question_conditioned)
+          end
+        end
+      end
     end
   end
 end
