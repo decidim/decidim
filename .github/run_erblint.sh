f6f8da9bb76c4e7d0b21fd61feed8251c73e8d9f
Marc Riera <mrc2407@gmail.com>
2020-03-19 12:52:15 +0100

    Move test suite to GitHub Actions (#5843)
    * Lint code through GitHub Actions
    
    * Actually lint files
    
    * Ignore ruby gems when linting JS files
    
    * Properly cache NPM deps
    
    * Set cache path for npm
    
    * Run rubocop in parallel
    
    * Don't lint ERB files in deps
    
    * Don't make npm verbose
    
    * Test npm caching works
    
    * Remove npm cache
    
    It'sa ctually not working, so it just adds useless seconds to thew
    workflow
    
    * Run tests for main folder
    
    * Add tests for core module
    
    * Add missing workflows
    
    Created from a template
    
    * Simplify generators workflow
    
    * Automatically upload screenshots if tests fail
    
    * Ensure path exists before uploading
    
    * Create the screenshots path before uploading it
    
    * Install Ruby deps from inside each module folder
    
    This is how it works on CircleCI.
    
    * Revert "Install Ruby deps from inside each module folder"
    
    This reverts commit ad43682e643645e0fb6daca4e3d2bf3657ac2dfe.
    
    * Add README
    
    * Fix screenshots uploads
    
    They should be uploaded always...
    
    * Improve generators workflow
    
    * Try to fix generators workflow
    
    * Try to install ruby deps for generators
    
    * Install gems globally
    
    * Increase max wait time for slow specs
    
    * Run JS tests from comments workflow
    
    * Split proposals workflow in three
    
    * Install codecov
    
    * Set needed ENV vars for Codecov uploads
    
    * Use GitHub Action to upload coverage reports
    
    * Send coverage for main folder
    
    * Use custom names for the parallel proposals workflows
    
    * Delete decidim-dev workflow
    
    This folder doesn't have any test
    
    * Disable Codecov comments
    
    * Remove unused file
    
    * Don't report to CodeClimate from CircleCI
    
    * Revert "Don't report to CodeClimate from CircleCI"
    
    This reverts commit 5a6a087075a66ddc9efa96d246b794c21e61e9f7.
    
    * Don't change the formatter for CircleCI
    
    * Remove CircleCI config
    
    * Remove CircleCI build status badge
    
    * Add codecov status badge
    
    * Add test suite badges
    
    * Fix badges
    
    * Fix workflows README
    
    * Update .github/workflows/README.md
    
    Co-Authored-By: Oliver Valls <oliver.vh@coditramuntana.com>
    
    * Remove CircleCI env variable
    
    Co-authored-by: Oliver Valls <oliver.vh@coditramuntana.com>

--- .github/run_erblint.sh
+++ .github/run_erblint.sh
@@ -2,7 +2,7 @@
 
 shopt -s globstar
 
-bundle exec erblint **/app/{cells,views}/**/*.erb
+bundle exec erblint decidim**/app/{cells,views}/**/*.erb
 
 # Store the return code of the erblint execution
 EXIT_CODE=$?
