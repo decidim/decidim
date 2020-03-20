64b567693ea48fb8827f4875c104343cdf4d0b6b
anagha <anagha1996@gmail.com>
2020-03-21 00:58:12 +0530

    indentatiotion

--- decidim-assemblies/app/commands/decidim/assemblies/admin/notify_role_assigned_assembly.rb
+++ decidim-assemblies/app/commands/decidim/assemblies/admin/notify_role_assigned_assembly.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
 module Decidim
-    module Assemblies
-      module Admin
-        # A command to notify users when a role is assigned for a participatory process
-        class NotifyRoleAssignedAssembly < Rectify::Command
-          def send_notification user
-              Decidim::EventsManager.publish(
-                event: "decidim.events.assembly.role_assigned",
-                event_class: Decidim::AssemblyRoleAssignedEvent,
-                resource: @assembly,
-                affected_users: [user],
-                extra: {
-                  role: form.role
-                }
-              )
-          end
+  module Assemblies
+    module Admin
+      # A command to notify users when a role is assigned for a participatory process
+      class NotifyRoleAssignedAssembly < Rectify::Command
+        def send_notification user
+            Decidim::EventsManager.publish(
+              event: "decidim.events.assembly.role_assigned",
+              event_class: Decidim::AssemblyRoleAssignedEvent,
+              resource: @assembly,
+              affected_users: [user],
+              extra: {
+                role: form.role
+              }
+            )
         end
       end
     end
   end
+end
