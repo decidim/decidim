90e429d0554a2eef01f249e797281504ab86e955
Vera Rojman <vrojman@protonmail.com>
2020-03-25 15:53:35 +0100

    Commit answers only for questions with fulfilled conditions

--- decidim-forms/app/forms/decidim/forms/answer_form.rb
+++ decidim-forms/app/forms/decidim/forms/answer_form.rb
@@ -45,21 +45,21 @@ module Decidim
         choices.select(&:body)
       end
 
-      private
-
-      def conditions_fulfilled
+      def conditions_fulfilled?
         question.display_conditions.all? do |condition|
           answer = question.questionnaire.answers.find_by(question: condition.condition_question)
           condition.fulfilled?(answer)
         end
       end
 
+      private
+
       def mandatory_body?
-        question.mandatory_body? if conditions_fulfilled
+        question.mandatory_body? if conditions_fulfilled?
       end
 
       def mandatory_choices?
-        question.mandatory_choices? if conditions_fulfilled
+        question.mandatory_choices? if conditions_fulfilled?
       end
 
       def max_choices
