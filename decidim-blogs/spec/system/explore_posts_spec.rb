bbbeaa40b1481f0847d9163552b2fc98a1a81eb2
Marc Riera Casals <mrc2407@gmail.com>
2020-03-31 10:00:46 +0200

    Remove wrong check

--- decidim-blogs/spec/system/explore_posts_spec.rb
+++ decidim-blogs/spec/system/explore_posts_spec.rb
@@ -45,7 +45,6 @@ describe "Explore posts", type: :system do
       expect(page).to have_i18n_content(post.title)
       expect(page).to have_i18n_content(post.body)
       expect(page).to have_content(post.author.name)
-      expect(page).to have_content(post.created_at.day)
     end
 
     it "shows the back button" do
