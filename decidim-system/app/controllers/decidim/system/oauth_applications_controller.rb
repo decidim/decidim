467c0bc02a2c3bfe4aa5fb61043ef4b640f8d8f9
Vera Rojman <vrojman@protonmail.com>
2020-04-07 13:22:16 +0200

    Update index for oauth_applications

--- decidim-system/app/controllers/decidim/system/oauth_applications_controller.rb
+++ decidim-system/app/controllers/decidim/system/oauth_applications_controller.rb
@@ -5,6 +5,8 @@ module Decidim
     # Controller that allows managing OAuth applications at the admin panel.
     #
     class OAuthApplicationsController < Admin::ApplicationController
+      helper Decidim::Admin::AttributesDisplayHelper
+
       def index
         @oauth_applications = collection.page(params[:page]).per(15)
       end
@@ -22,12 +24,12 @@ module Decidim
 
         CreateOAuthApplication.call(@form) do
           on(:ok) do
-            flash[:notice] = I18n.t("oauth_applications.create.success", scope: "decidim.admin")
+            flash[:notice] = I18n.t("oauth_applications.create.success", scope: "decidim.system")
             redirect_to action: :index
           end
 
           on(:invalid) do
-            flash.now[:alert] = I18n.t("oauth_applications.create.error", scope: "decidim.admin")
+            flash.now[:alert] = I18n.t("oauth_applications.create.error", scope: "decidim.system")
             render :new
           end
         end
@@ -44,13 +46,13 @@ module Decidim
 
         UpdateOAuthApplication.call(@oauth_application, @form, current_user) do
           on(:ok) do |_application|
-            flash[:notice] = I18n.t("oauth_applications.update.success", scope: "decidim.admin")
+            flash[:notice] = I18n.t("oauth_applications.update.success", scope: "decidim.system")
             redirect_to action: :index
           end
 
           on(:invalid) do |application|
             @oauth_application = application
-            flash.now[:error] = I18n.t("oauth_applications.update.error", scope: "decidim.admin")
+            flash.now[:error] = I18n.t("oauth_applications.update.error", scope: "decidim.system")
             render action: :edit
           end
         end
@@ -61,12 +63,12 @@ module Decidim
 
         DestroyOAuthApplication.call(@oauth_application, current_user) do
           on(:ok) do
-            flash[:notice] = I18n.t("oauth_applications.destroy.success", scope: "decidim.admin")
+            flash[:notice] = I18n.t("oauth_applications.destroy.success", scope: "decidim.system")
             redirect_to action: :index
           end
 
           on(:invalid) do
-            flash.now[:error] = I18n.t("oauth_applications.destroy.error", scope: "decidim.admin")
+            flash.now[:error] = I18n.t("oauth_applications.destroy.error", scope: "decidim.system")
             redirect_to :back
           end
         end
@@ -75,7 +77,7 @@ module Decidim
       private
 
       def collection
-        @collection ||= current_organization.oauth_applications
+        @collection ||= Decidim::OAuthApplication.all
       end
     end
   end
