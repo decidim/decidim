f2758f4c0fa781061926a20a9edb99fee3ae5097
anagha <anagha1996@gmail.com>
2020-04-12 03:16:47 +0530

    Added tests for events

--- decidim-assemblies/app/events/decidim/assembly_role_assigned_event.rb
+++ decidim-assemblies/app/events/decidim/assembly_role_assigned_event.rb
@@ -10,14 +10,17 @@ module Decidim
     end
 
     def i18n_role
-      I18n.t(extra["role"], "decidim.admin.models.conference_user_role.roles", default: extra["role"])
+      I18n.t(extra["role"], "decidim.admin.models.assembly_user_role.roles", default: extra["role"])
     end
 
     def i18n_options
       {
+        resource_path: resource_path,
         resource_title: resource_title,
         resource_url: resource_url,
         scope: event_name,
+        participatory_space_title: participatory_space_title,
+        participatory_space_url: participatory_space_url,
         role: i18n_role
       }
     end
