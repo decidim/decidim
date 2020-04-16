d16eafe0dbb974e7bcc62e96684f888b44a7f3d8
Leonardo Diez <leiodd@gmail.com>
2020-04-16 10:48:04 +0200

    fix: replace + with coalesce operator (//)

--- .github/upload_coverage.sh
+++ .github/upload_coverage.sh
@@ -3,8 +3,8 @@
 REPORT_NAME=$1
 EVENT_PAYLOAD_FILE=$2
 
-PRID=`jq ".number + .check_run.pull_requests[0].number" $EVENT_PAYLOAD_FILE`
-SHA=`jq -r ".pull_request.head.sha + .check_run.head_sha + .after" $EVENT_PAYLOAD_FILE`
+PRID=`jq ".number // .check_run.pull_requests[0].number" $EVENT_PAYLOAD_FILE`
+SHA=`jq -r ".pull_request.head.sha // .check_run.head_sha // .after" $EVENT_PAYLOAD_FILE`
 
 if [ $PRID = "null" ]
 then
