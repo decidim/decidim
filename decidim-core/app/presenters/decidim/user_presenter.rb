babe95f908dae09dddef7c62ff469a8bb18042ed
Ramon Costa <ramon.costa@gmail.com>
2020-04-16 14:16:44 +0200

    Fixes linter issue

--- decidim-core/app/presenters/decidim/user_presenter.rb
+++ decidim-core/app/presenters/decidim/user_presenter.rb
@@ -38,6 +38,7 @@ module Decidim
 
     def direct_messages_enabled?(context)
       return false unless __getobj__.respond_to?(:accepts_conversation?)
+
       __getobj__.accepts_conversation?(context[:current_user])
     end
 
