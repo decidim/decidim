2093fe11090331d650327bb0374a7eb840d6723e
faithngetich <faithngetich188@gmail.com>
2020-05-10 14:50:03 +0300

    Add email to user object

--- decidim-admin/spec/controllers/organizations_contoller_spec.rb
+++ decidim-admin/spec/controllers/organizations_contoller_spec.rb
@@ -16,7 +16,7 @@ module Decidim
       end
 
       describe "GET users in json format" do
-        let!(:user) { create(:user, name: "Daisy Miller", nickname: "daisy_m", email: "daisy@gl.com", organization: organization) }
+        let!(:user) { create(:user, name: "Daisy Miller", nickname: "daisy_m", organization: organization) }
         let!(:other_user) { create(:user, name: "Daisy O'connor", nickname: "daisy_o") }
 
         let(:parsed_response) { JSON.parse(response.body).map(&:symbolize_keys) }
@@ -51,7 +51,7 @@ module Decidim
 
         context "when searching by email" do
           it "returns the id, name, email and nickname for filtered users" do
-            get :users, format: :json, params: { term: "daisy@gl.com" }
+            get :users, format: :json, params: { term: user.email }
             expect(parsed_response).to eq([{ value: user.id, label: "#{user.name} (@#{user.nickname}) #{user.email}" }])
           end
         end
