28bc595caeade2d1f26c096e8b741ce713fae4d9
anagha <anagha1996@gmail.com>
2020-05-11 13:13:16 +0530

    fixing assets

--- decidim-assemblies/app/assets/config/decidim_assemblies_manifest.js
+++ decidim-assemblies/app/assets/config/decidim_assemblies_manifest.js
@@ -1,3 +1,3 @@
 //= link_directory ../images/decidim/assemblies
 //= link_directory ../javascripts/decidim/assemblies
-//= require ../javascripts/decidim/assemblies/orgchart
+//= require decidim/assemblies/orgchart
