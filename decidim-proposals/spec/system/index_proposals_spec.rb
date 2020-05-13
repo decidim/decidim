478b2f5047d07c1e98856b9ea8c3595148f9358a
Gemma <gemmatm@coditramuntana.com>
2020-05-13 16:34:50 +0200

    Update spec for proposals index

--- decidim-proposals/spec/system/index_proposals_spec.rb
+++ decidim-proposals/spec/system/index_proposals_spec.rb
@@ -9,28 +9,30 @@ describe "Index proposals", type: :system do
   context "when there are proposals" do
 
     let!(:proposals) { create_list(:proposal, 3, component: component) }
-		it "doesn't display empty message" do
-			visit_component
-			
-			expect(page).to have_no_content("There isn't any proposal")
-  	end
+      it "doesn't display empty message" do
+        visit_component
+
+        expect(page).to have_no_content("There isn't any proposal")
+    end
   end
 
   context "when there are no proposals" do
-		context "when there are no filters" do
-			it "shows generic empty message" do
-				visit_component
+    context "when there are no filters" do
+      it "shows generic empty message" do
+        visit_component
 
-				expect(page).to have_content("There isn't any proposal")
-			end
-		end
-		context "when there are filters" do
-			let!(:proposals) { create(:proposal, :with_answer, :accepted, component: component) }
-			it "shows filters empty message" do
-				visit_component
+        expect(page).to have_content("There isn't any proposal")
+      end
+    end
+    context "when there are filters" do
+      let!(:proposals) { create(:proposal, :with_answer, :accepted, component: component) }
+      it "shows filters empty message" do
+        visit_component
 
-				expect(page).to have_content("There isn't any proposal with this criteria")
-			end
-		end
-	end
+        uncheck "Accepted"
+
+        expect(page).to have_content("There isn't any proposal with this criteria")
+      end
+    end
+  end
 end
