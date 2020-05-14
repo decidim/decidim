d660519828894431bb1a2ae90b47c7ff46e15542
Leonardo Diez <leiodd@gmail.com>
2020-05-14 10:14:14 +0200

    feat: improve navigation flow when checking out or deleting orders

--- decidim-budgets/app/controllers/decidim/budgets/orders_controller.rb
+++ decidim-budgets/app/controllers/decidim/budgets/orders_controller.rb
@@ -5,6 +5,7 @@ module Decidim
     # Exposes the order resource so users can checkout it.
     class OrdersController < Decidim::Budgets::ApplicationController
       include NeedsCurrentOrder
+      include Decidim::ComponentPathHelper
 
       def checkout
         enforce_permission_to :vote, :project, order: current_order, parent_component_context: parent_component_context
@@ -12,12 +13,12 @@ module Decidim
         Checkout.call(current_order, current_component) do
           on(:ok) do
             flash[:notice] = I18n.t("orders.checkout.success", scope: "decidim")
-            redirect_to projects_path
+            redirect_to return_path
           end
 
           on(:invalid) do
             flash.now[:alert] = I18n.t("orders.checkout.error", scope: "decidim")
-            redirect_to projects_path
+            redirect_to return_path
           end
         end
       end
@@ -26,13 +27,21 @@ module Decidim
         CancelOrder.call(current_order) do
           on(:ok) do
             flash[:notice] = I18n.t("orders.destroy.success", scope: "decidim")
-            redirect_to projects_path
+            redirect_to return_path
           end
 
           on(:invalid) do
             flash.now[:alert] = I18n.t("orders.destroy.error", scope: "decidim")
-            redirect_to projects_path
+            redirect_to return_path
           end
+        end
+      end
+
+      def return_path
+        if params[:return_path]
+          params[:return_path]
+        else
+          main_component_path(current_component.parent || current_component)
         end
       end
     end
