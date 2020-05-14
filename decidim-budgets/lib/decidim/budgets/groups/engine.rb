09f8c40d7fa71e3e5d03291489d846aebd866c3d
Leonardo Diez <leiodd@gmail.com>
2020-05-14 10:14:08 +0200

    refactor: use `view_hooks` to avoid children dependencies with parents

--- decidim-budgets/lib/decidim/budgets/groups/engine.rb
+++ decidim-budgets/lib/decidim/budgets/groups/engine.rb
@@ -13,6 +13,14 @@ module Decidim
         routes do
           root to: "budgets#index"
         end
+
+        initializer "decidim_budgets.groups.view_hooks" do
+          Decidim.view_hooks.register(:budgets_parent_information, priority: Decidim::ViewHooks::MEDIUM_PRIORITY) do |view_context|
+            view_context.render(
+              partial: "decidim/budgets/groups/hooks/more_information_modal"
+            )
+          end
+        end
       end
     end
   end
