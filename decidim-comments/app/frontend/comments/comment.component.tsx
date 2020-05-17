03ed4f58cec12d7b1a498f7bcc14b130c3a5c51b
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-05-18 00:11:37 +0300

    [TAC025] Validate HTML for the comments component
    Original commit: 52654a12392a3df3ee6b8b57388136e55a4d70b4

--- decidim-comments/app/frontend/comments/comment.component.tsx
+++ decidim-comments/app/frontend/comments/comment.component.tsx
@@ -51,7 +51,7 @@ class Comment extends React.Component<CommentProps, CommentState> {
     votable: false
   };
 
-  public commentNode: HTMLElement;
+  public commentNode: HTMLDivElement;
 
   constructor(props: CommentProps) {
     super(props);
@@ -95,7 +95,7 @@ class Comment extends React.Component<CommentProps, CommentState> {
     }
   }
 
-  public getNodeReference = (commentNode: HTMLElement) => this.commentNode = commentNode;
+  public getNodeReference = (commentNode: HTMLDivElement) => this.commentNode = commentNode;
 
   public render(): JSX.Element {
     const { session, comment: { id, author, formattedBody, createdAt, formattedCreatedAt }, articleClassName } = this.props;
@@ -112,7 +112,7 @@ class Comment extends React.Component<CommentProps, CommentState> {
     }
 
     return (
-      <article id={`comment_${id}`} className={articleClassName} ref={this.getNodeReference}>
+      <div id={`comment_${id}`} className={articleClassName} ref={this.getNodeReference}>
         <div className="comment__header">
           <div className="author-data">
             <div className="author-data__main">
@@ -146,7 +146,7 @@ class Comment extends React.Component<CommentProps, CommentState> {
         {this._renderReplies()}
         {this._renderAdditionalReplyButton()}
         {this._renderReplyForm()}
-      </article>
+      </div>
     );
   }
 
