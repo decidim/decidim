6bf2d5bbe6a0d016a1b81b7ca17edf2512d3bae0
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-05-17 23:38:47 +0300

    [TAC033] Fix scopes picker spec with updated output
    Original commit: af9b34d280dff92356152a673414a4d4e7382df5

--- decidim-core/spec/helpers/decidim/scopes_helper_spec.rb
+++ decidim-core/spec/helpers/decidim/scopes_helper_spec.rb
@@ -14,13 +14,8 @@ module Decidim
           <div id="my_scope_input" class="data-picker picker-single" data-picker-name="my_scope_input">
             <div class="picker-values">
               <div>
-                <a href="/scopes/picker?current=#{scope.id}&amp;field=my_scope_input" data-picker-value="#{scope.id}">
-                  #{scope.name["en"]} (#{scope.scope_type.name["en"]})
-                </a>
+                <a href="/scopes/picker?current=#{scope.id}&amp;field=my_scope_input" data-picker-value="#{scope.id}">#{scope.name["en"]} (#{scope.scope_type.name["en"]})</a>
               </div>
-            </div>
-            <div class="picker-prompt">
-              <a href="/scopes/picker?field=my_scope_input">Global scope</a>
             </div>
           </div>
         HTML
