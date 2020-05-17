9148ddd83f8d1b7fcebbb4addc1f497e75d71da6
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-05-18 00:17:35 +0300

    [TAC025] Fix admin test after required label change on the forms
    Original commit: 65a47142a360be4d6b9ab3f52c381caee0790018

--- decidim-admin/spec/shared/manage_impersonations_examples.rb
+++ decidim-admin/spec/shared/manage_impersonations_examples.rb
@@ -39,7 +39,7 @@ shared_examples "manage impersonations examples" do
       let(:document_number) { "123456789Y" }
 
       it "shows the errors in the form" do
-        expect(page).to have_selector("label", text: "Document number*\nnRequired\nis invalid")
+        expect(page).to have_selector("label", text: "Document number*\nRequired\nis invalid")
       end
     end
 
