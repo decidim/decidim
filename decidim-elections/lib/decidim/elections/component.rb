53cba052c14c09365da9528ff31f950026884a3a
Leonardo Diez <leiodd@gmail.com>
2020-05-19 15:39:02 +0200

    feat: add seeds

--- decidim-elections/lib/decidim/elections/component.rb
+++ decidim-elections/lib/decidim/elections/component.rb
@@ -31,11 +31,46 @@ Decidim.register_component(:elections) do |component|
     Decidim::Elections::Election.where(component: components).count
   end
 
-  # component.register_stat :some_stat do |context, start_at, end_at|
-  #   # Register some stat number to the application
-  # end
+  component.seeds do |participatory_space|
+    admin_user = Decidim::User.find_by(
+      organization: participatory_space.organization,
+      email: "admin@example.org"
+    )
 
-  # component.seeds do |participatory_space|
-  #   # Add some seeds for this component
-  # end
+    params = {
+      name: Decidim::Components::Namer.new(participatory_space.organization.available_locales, :elections).i18n_name,
+      manifest_name: :elections,
+      published_at: Time.current,
+      participatory_space: participatory_space
+    }
+
+    component = Decidim.traceability.perform_action!(
+      "publish",
+      Decidim::Component,
+      admin_user,
+      visibility: "all"
+    ) do
+      Decidim::Component.create!(params)
+    end
+
+    3.times do
+      params = {
+        component: component,
+        title: Decidim::Faker::Localized.sentence(2),
+        subtitle: Decidim::Faker::Localized.sentence(2),
+        description: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
+          Decidim::Faker::Localized.paragraph(3)
+        end,
+        start_time: 3.weeks.from_now,
+        end_time: 3.weeks.from_now + 4.hours
+      }
+
+      Decidim.traceability.create!(
+        Decidim::Elections::Election,
+        admin_user,
+        params,
+        visibility: "all"
+      )
+    end
+  end
 end
