6888f583cc2f094bebbaf98b250054d800fc1240
Leonardo Diez <leiodd@gmail.com>
2020-05-19 15:39:10 +0200

    test: fix datetime comparisons

--- decidim-elections/spec/commands/decidim/elections/admin/create_election_spec.rb
+++ decidim-elections/spec/commands/decidim/elections/admin/create_election_spec.rb
@@ -37,8 +37,8 @@ describe Decidim::Elections::Admin::CreateElection do
     expect(translated(election.title)).to eq "title"
     expect(translated(election.subtitle)).to eq "subtitle"
     expect(translated(election.description)).to eq "description"
-    expect(election.start_time).to eq start_time
-    expect(election.end_time).to eq end_time
+    expect(election.start_time).to be_within(1.second).of start_time
+    expect(election.end_time).to be_within(1.second).of end_time
   end
 
   it "sets the component" do
