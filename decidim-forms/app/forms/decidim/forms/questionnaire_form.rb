eff6f7b43861c9e5cd75551df94f707ae4bf5449
Marc Riera Casals <mrc2407@gmail.com>
2020-05-19 16:05:41 +0200

    Handle survey submissions

--- decidim-forms/app/forms/decidim/forms/questionnaire_form.rb
+++ decidim-forms/app/forms/decidim/forms/questionnaire_form.rb
@@ -23,10 +23,28 @@ module Decidim
         end
       end
 
+      # Public: Splits reponses by step, keeping the separator. Ruby doesn't
+      # seem to give a way to do this in the standard library, so we had to
+      # write our own version.
+      #
+      # Returns an array of steps. Each step is a list of the questions in that
+      # step, including the separator.
       def responses_by_step
-        @responses_by_step ||= responses.split do |response|
-          response.question.separator?
-        end
+        @responses_by_step ||=
+          begin
+            steps_acc = [[]]
+            step_index = 0
+
+            responses.each do |response|
+              steps_acc[step_index] << response
+
+              if response.question.separator?
+                step_index += 1
+                steps_acc[step_index] = []
+              end
+            end
+            steps_acc
+          end
       end
 
       def total_steps
