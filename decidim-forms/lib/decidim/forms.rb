7d5c40d2246a74f489034dfaa071848eb0df7bd5
Vera Rojman <vrojman@protonmail.com>
2020-05-26 15:10:26 +0200

    Autoload FormPDF exporter, not base PDF

--- decidim-forms/lib/decidim/forms.rb
+++ decidim-forms/lib/decidim/forms.rb
@@ -10,6 +10,6 @@ module Decidim
   module Forms
     autoload :UserAnswersSerializer, "decidim/forms/user_answers_serializer"
     autoload :DataPortabilityUserAnswersSerializer, "decidim/forms/data_portability_user_answers_serializer"
-    autoload :PDF, "decidim/exporters/pdf"
+    autoload :FormPDF, "decidim/exporters/form_pdf"
   end
 end
