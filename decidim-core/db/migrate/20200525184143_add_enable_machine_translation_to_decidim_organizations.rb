ca866ac6028fc69b180885bd79b761b8dffa433c
anagha <anagha1996@gmail.com>
2020-05-27 13:48:45 +0530

    Fixing lint issue in migration file

--- decidim-core/db/migrate/20200525184143_add_enable_machine_translation_to_decidim_organizations.rb
+++ decidim-core/db/migrate/20200525184143_add_enable_machine_translation_to_decidim_organizations.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddEnableMachineTranslationToDecidimOrganizations < ActiveRecord::Migration[5.2]
   def change
     add_column :decidim_organizations, :enable_machine_translations, :boolean, default: false
