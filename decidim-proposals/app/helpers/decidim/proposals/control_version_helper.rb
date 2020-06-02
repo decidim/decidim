755eed3a84432959a037921bb48a2fa943d39b98
Marc Riera Casals <mrc2407@gmail.com>
2020-06-02 08:18:57 +0200

    Simplify codee

--- decidim-proposals/app/helpers/decidim/proposals/control_version_helper.rb
+++ decidim-proposals/app/helpers/decidim/proposals/control_version_helper.rb
@@ -7,15 +7,6 @@ module Decidim
       def item_name
         versioned_resource.model_name.singular_route_key.to_sym
       end
-
-      def back_to_resource_path_text_scope
-        case item_name
-        when :proposal
-          "decidim.proposals.versions.proposals"
-        when :collaborative_draft
-          "decidim.proposals.versions.collaborative_drafts"
-        end
-      end
     end
   end
 end
