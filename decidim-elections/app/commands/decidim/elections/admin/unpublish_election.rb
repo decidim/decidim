a78e58c3bc1f8c67bbaa80716c654c92dc34290c
Gladys Chacartegui <gladys@codegram.com>
2020-06-03 13:14:51 +0200

    Rubocop format

--- decidim-elections/app/commands/decidim/elections/admin/unpublish_election.rb
+++ decidim-elections/app/commands/decidim/elections/admin/unpublish_election.rb
@@ -2,42 +2,42 @@
 
 module Decidim
   module Elections
-  module Admin
-    # This command gets called when a election is unpublished from the admin panel.
-    class UnpublishElection < Rectify::Command
-      # Public: Initializes the command.
-      #
-      # election - The election to unpublish.
-      # current_user - the user performing the action
-      def initialize(election, current_user)
-        @election = election
-        @current_user = current_user
-      end
+    module Admin
+      # This command gets called when a election is unpublished from the admin panel.
+      class UnpublishElection < Rectify::Command
+        # Public: Initializes the command.
+        #
+        # election - The election to unpublish.
+        # current_user - the user performing the action
+        def initialize(election, current_user)
+          @election = election
+          @current_user = current_user
+        end
 
-      # Public: Unpublishes the Election.
-      #
-      # Broadcasts :ok if unpublished, :invalid otherwise.
-      def call
-        unpublish_election
+        # Public: Unpublishes the Election.
+        #
+        # Broadcasts :ok if unpublished, :invalid otherwise.
+        def call
+          unpublish_election
 
-        broadcast(:ok)
-      end
+          broadcast(:ok)
+        end
 
-      private
+        private
 
-      attr_reader :election, :current_user
+        attr_reader :election, :current_user
 
-      def unpublish_election
-        Decidim.traceability.perform_action!(
-          :unpublish,
-          election,
-          current_user
-        ) do
-          election.unpublish!
-          election
+        def unpublish_election
+          Decidim.traceability.perform_action!(
+            :unpublish,
+            election,
+            current_user
+          ) do
+            election.unpublish!
+            election
+          end
         end
       end
     end
   end
-end
 end
