ea09abe0251ed832857807d9573e86d81c1ca47d
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-06-05 00:27:55 +0300

    HTML-validation: invisible captcha styles to the page head

--- decidim-core/config/initializers/invisible_captcha.rb
+++ decidim-core/config/initializers/invisible_captcha.rb
@@ -5,6 +5,7 @@ InvisibleCaptcha.setup do |config|
   config.visual_honeypots = false
   config.timestamp_threshold = 4
   config.timestamp_enabled = false
+  config.injectable_styles = true
   # Leave these unset if you want to use I18n (see below)
   # config.sentence_for_humans     = 'If you are a human, ignore this field'
   # config.timestamp_error_message = 'Sorry, that was too quick! Please resubmit.'
