12414007ea2138947640fbfd26d2c11dbff9b549
Vera Rojman <vrojman@protonmail.com>
2020-06-04 20:56:13 +0200

    Put template model inside Templates module

--- decidim-templates/app/models/decidim/templates/template.rb
+++ decidim-templates/app/models/decidim/templates/template.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 module Decidim
-  class Template < ApplicationRecord
-    belongs_to :organization,
-               foreign_key: "decidim_organization_id",
-               class_name: "Decidim::Organization"
+  module Templates
+    class Template < ApplicationRecord
+      belongs_to :organization,
+                 foreign_key: "decidim_organization_id",
+                 class_name: "Decidim::Organization"
 
-    belongs_to :author, class_name: "Decidim::User", foreign_key: "decidim_author_id"
-    belongs_to :model, foreign_key: "decidim_model_id", foreign_type: "decidim_model_type", polymorphic: true
+      belongs_to :author, class_name: "Decidim::User", foreign_key: "decidim_author_id"
+      belongs_to :model, foreign_key: "decidim_model_id", foreign_type: "decidim_model_type", polymorphic: true
+    end
   end
 end
--- decidim-templates/db/migrate/20200518093819_create_decidim_templates.rb
+++ decidim-templates/db/migrate/20200518093819_create_decidim_templates.rb
@@ -2,7 +2,7 @@
 
 class CreateDecidimTemplates < ActiveRecord::Migration[5.2]
   def change
-    create_table :decidim_templates do |t|
+    create_table :decidim_templates_templates do |t|
       t.integer :decidim_organization_id, null: false, index: { name: "index_decidim_questionnaire_templates_organization" }
       t.references :author, null: false, index: { name: "decidim_templates_author" }
       t.references :model, null: false, polymorphic: true, index: { name: "decidim_templates_model" }
