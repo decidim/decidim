58ce579105a6f966452617f30d7330f3fa7f604b
Vera Rojman <vrojman@protonmail.com>
2020-06-08 15:18:52 +0200

    Update templates table

--- decidim-templates/db/migrate/20200518093819_create_decidim_templates.rb
+++ decidim-templates/db/migrate/20200518093819_create_decidim_templates.rb
@@ -3,9 +3,9 @@
 class CreateDecidimTemplates < ActiveRecord::Migration[5.2]
   def change
     create_table :decidim_templates_templates do |t|
-      t.integer :decidim_organization_id, null: false, index: { name: "index_decidim_questionnaire_templates_organization" }
-      t.references :author, null: false, index: { name: "decidim_templates_author" }
-      t.references :model, null: false, polymorphic: true, index: { name: "decidim_templates_model" }
+      t.integer :decidim_organization_id, null: false, index: { name: "index_decidim_templates_organization" }
+      t.references :templatable, null: false, polymorphic: true, index: { name: "index_decidim_templates_templatable" }
+      t.jsonb :name
       t.timestamps
     end
   end
