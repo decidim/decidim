769712ddde87779a82a1afe387f7cf382da365f4
Vera Rojman <vrojman@protonmail.com>
2020-06-08 11:18:53 +0200

    Add error message to geocoder controller when geocoder blank

--- decidim-api/app/controllers/decidim/api/geocoder_controller.rb
+++ decidim-api/app/controllers/decidim/api/geocoder_controller.rb
@@ -4,9 +4,13 @@ module Decidim
   module Api
     class GeocoderController < Api::ApplicationController
       def search
-        @search_string = params[:term]
-        results = Geocoder.search(@search_string, language: params.dig(:locale) || I18n.locale)
-        render json: format_results(results)
+        if Decidim.geocoder.blank?
+          render json: { error: { message: t("decidim.geocoder.not_configured") } }
+        else
+          @search_string = params[:term]
+          results = Geocoder.search(@search_string, language: params.dig(:locale) || I18n.locale)
+          render json: format_results(results)
+        end
       end
 
       private
