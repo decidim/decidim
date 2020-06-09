b8442dd537341cc3551106f946c0218f78539828
Gladys Chacartegui <gladys@codegram.com>
2020-06-09 09:40:24 +0200

    Two more tests

--- decidim-elections/spec/events/decidim/elections/election_published_event_spec.rb
+++ decidim-elections/spec/events/decidim/elections/election_published_event_spec.rb
@@ -3,47 +3,39 @@
 require "spec_helper"
 
 describe Decidim::Elections::ElectionPublishedEvent do
-  # include Decidim::ElectionPathHelper
-
   include_context "when a simple event"
-  def main_election_path(component)
-    current_params = try(:params) || {}
-    EngineRouter.main_proxy(component).root_path(locale: current_params[:locale])
-  end
 
   let(:event_name) { "decidim.events.elections.election_published" }
   let(:resource) { create(:election) }
-  # let(:participatory_space) { resource.participatory_space }
-  let(:resource_path) { main_election_path(resource) }
-
-  # participatory_space = resource.participatory_space
+  let(:participatory_space_title) { resource.participatory_space.title["en"] }
+  let(:resource_title) { resource.title["en"] }
 
   it_behaves_like "a simple event"
 
   describe "email_subject" do
     it "is generated correctly" do
-      expect(subject.email_subject).to eq("The #{resource.title["en"]} election is now active for #{resource.participatory_space.title["en"]}.")
+      expect(subject.email_subject).to eq("The #{resource_title} election is now active for #{participatory_space_title}.")
     end
   end
 
   describe "email_intro" do
     it "is generated correctly" do
       expect(subject.email_intro)
-        .to eq("The #{resource.title["en"]} election is now active for #{resource.participatory_space.title["en"]}. You can see it from this page:")
+        .to eq("The #{resource_title} election is now active for #{participatory_space_title}. You can see it from this page:")
     end
   end
 
-  # describe "email_outro" do
-  #   it "is generated correctly" do
-  #     expect(subject.email_outro)
-  #       .to include("You have received this notification because you are following #{participatory_space.title["en"]}")
-  #   end
-  # end
+  describe "email_outro" do
+    it "is generated correctly" do
+      expect(subject.email_outro)
+        .to eq("You have received this notification because you are following #{participatory_space_title}. You can stop receiving notifications following the previous link.")
+    end
+  end
 
-  # describe "notification_title" do
-  #   it "is generated correctly" do
-  #     expect(subject.notification_title)
-  #       .to eq("The #{resource.name["en"]} election is now active for <a href=\"#{resource_path}\">#{participatory_space.title["en"]}</a>")
-  #   end
-  # end
+  describe "notification_title" do
+    it "is generated correctly" do
+      expect(subject.notification_title)
+        .to eq("The <a href=\"#{resource_path}\">#{resource_title}</a> election is now active for #{participatory_space_title}.")
+    end
+  end
 end
--- decidim-elections/spec/system/admin_manages_elections_spec.rb
+++ decidim-elections/spec/system/admin_manages_elections_spec.rb
@@ -109,6 +109,62 @@ describe "Admin manages elections", type: :system do
     end
   end
 
+  describe "publishing an election" do
+    context "when the election is unpublished" do
+      let!(:election) { create(:election, :unpublished, component: current_component) }
+
+      it "publishes the election" do
+        within find("tr", text: translated(election.title)) do
+          page.find(".action-icon--publish").click
+        end
+
+        within ".callout-wrapper" do
+          expect(page).to have_content("successfully")
+        end
+
+        within find("tr", text: translated(election.title)) do
+          expect(page).to have_no_selector(".action-icon--publish")
+        end
+      end
+    end
+  end
+
+  describe "unpublishing an election" do
+    it "unpublishes an election" do
+      within find("tr", text: translated(election.title)) do
+        page.find(".action-icon--unpublish").click
+      end
+
+      within ".callout-wrapper" do
+        expect(page).to have_content("successfully")
+      end
+
+      within find("tr", text: translated(election.title)) do
+        expect(page).to have_no_selector(".action-icon--unpublish")
+      end
+    end
+
+    context "when the election has started and has not finished" do
+      let!(:election) { create(:election, :started, component: current_component) }
+
+      it "cannot unpublish the election" do
+        within find("tr", text: translated(election.title)) do
+          expect(page).to have_no_selector(".action-icon--unpublish")
+        end
+      end
+    end
+
+    context "when the election is published and has finished" do
+      let!(:election) { create(:election, :finished, component: current_component) }
+
+      it "can unpublish the election" do
+        within find("tr", text: translated(election.title)) do
+          expect(page).to have_selector(".action-icon--unpublish")
+        end
+      end
+    end
+  end
+
   describe "deleting an election" do
     it "deletes an election" do
       within find("tr", text: translated(election.title)) do
