5c0b3dd03ff9a345da91550c36ad309c723e1e1b
Jesús Di Bari <jesus.db@coditramuntana.com>
2020-06-15 16:01:47 +0200

    Add line feeds at the end of paragraphs and lists
    Now it detects whether if a list is ordered or unordered, and adds numberings or bullet points respectively.

--- decidim-proposals/app/presenters/decidim/proposals/proposal_presenter.rb
+++ decidim-proposals/app/presenters/decidim/proposals/proposal_presenter.rb
@@ -52,10 +52,7 @@ module Decidim
       def body(links: false, extras: true, strip_tags: false)
         text = proposal.body
 
-        if strip_tags
-          text = text.gsub(%r{<\/p>}, "\n\n")
-          text = strip_tags(text)
-        end
+        text = strip_tags(sanitize_text(text)) if strip_tags
 
         renderer = Decidim::ContentRenderers::HashtagRenderer.new(text)
         text = renderer.render(links: links, extras: extras).html_safe
@@ -92,6 +89,45 @@ module Decidim
 
       def resource_manifest
         proposal.class.resource_manifest
+      end
+
+      private
+
+      def sanitize_unordered_lists(text)
+        text.gsub(%r{(?=.*<\/ul>)(?!.*?<li>.*?<\/ol>.*?<\/ul>)<li>}) { |li| li + '• ' }
+      end
+
+      def sanitize_ordered_lists(text)
+        i = 0
+
+        text.gsub(%r{(?=.*<\/ol>)(?!.*?<li>.*?<\/ul>.*?<\/ol>)<li>}) do |li|
+          i += 1
+
+          li + "#{i}. "
+        end
+      end
+
+      def add_line_feeds_to_paragraphs(text)
+        text.gsub("</p>") { |p| p + "\n\n" }
+      end
+
+      def add_line_feeds_to_list_items(text)
+        text.gsub("</li>") { |li| li + "\n" }
+      end
+
+      # Adds line feeds after the paragraph and list item closing tags.
+      #
+      # Returns a String.
+      def add_line_feeds(text)
+        add_line_feeds_to_paragraphs(add_line_feeds_to_list_items(text))
+      end
+
+      # Maintains the paragraphs and lists separations with their bullet points and
+      # list numberings where appropriate.
+      #
+      # Returns a String.
+      def sanitize_text(text)
+        add_line_feeds(sanitize_ordered_lists(sanitize_unordered_lists(text)))
       end
     end
   end
