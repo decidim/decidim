60c59e6a92700a51d7f702c9d8039331d20282d2
anagha <anagha1996@gmail.com>
2020-06-24 11:20:35 +0530

    [WIP] tests for the jobs

--- decidim-core/spec/jobs/decidim/machine_translation_updated_resource_job_spec.rb
+++ decidim-core/spec/jobs/decidim/machine_translation_updated_resource_job_spec.rb
@@ -4,22 +4,16 @@ require "spec_helper"
 
 module Decidim
   describe MachineTranslationUpdatedResourceJob do
-    subject { dummy_resource.class }
-
-    let(:organization) { create(:organization) }
-    let(:available_locales) { organization.available_locales }
     let(:dummy_resource) { build :dummy_resource }
 
     context "when the address changes" do
-      let(:title) { { en: "title" } }
+      before do
+        updated_title = Decidim::Faker::Localized.name
+        dummy_resource.update(title: updated_title)      
+      end
 
       it "creates jobs for combination of each field and locale" do
-        translatable_fields = subject.translatable_fields_list.map(&:to_s)
-
-        translatable_fields.each do |field|
-        end
-
-        expect(Decidim::MachineTranslationUpdateFieldsJob).to have_been_enqueued.on_queue("default").at_least(translatable_fields.size * available_locales.size).times
+        expect(Decidim::MachineTranslationUpdateFieldsJob).to have_been_enqueued.on_queue("default")
       end
     end
   end
--- decidim-core/spec/lib/translatable_resource_spec.rb
+++ decidim-core/spec/lib/translatable_resource_spec.rb
@@ -4,22 +4,32 @@ require "spec_helper"
 
 module Decidim
   describe TranslatableResource do
-    subject { dummy_resource.class }
-
     let(:dummy_resource) { build :dummy_resource }
 
     describe "translatable_fields_list" do
       it "gets the list of defined translatable fields" do
-        expect(subject.translatable_fields_list).to eq([:title, :body])
+        expect(dummy_resource.class.translatable_fields_list).to eq([:title])
       end
     end
 
-    describe "after create" do
-      let(:new_resource) { dummy_resource.new }
-
-      it "when following a resource" do
+    describe "when new resource is created" do
+      let(:dummy_resource) { create :dummy_resource }
+      let(:new_resource) { create :dummy_resource}
+      it " enqueues machine translation new resource job" do
         expect(Decidim::MachineTranslationNewResourceJob).to have_been_enqueued.on_queue("default")
       end
     end
+
+    context "when new resource is updated" do
+      before do
+        updated_title = Decidim::Faker::Localized.name
+        dummy_resource.update(title: updated_title)      
+      end
+      it "enqueues machine translation update resource job" do
+        expect(Decidim::MachineTranslationUpdatedResourceJob).to have_been_enqueued.on_queue("default")
+      end
+    end
+
+
   end
 end
