40d97edef945c021628474da1afc8c4f9a63568e
anagha <anagha1996@gmail.com>
2020-06-29 15:28:39 +0530

    [WIP] sending I18n.locale to jobs

--- decidim-core/app/jobs/decidim/machine_translation_new_resource_job.rb
+++ decidim-core/app/jobs/decidim/machine_translation_new_resource_job.rb
@@ -4,13 +4,15 @@ module Decidim
   class MachineTranslationNewResourceJob < ApplicationJob
     queue_as :default
 
-    def perform(resource)
+    def perform(resource, source_locale)
       class_name = resource.class.name
       id = resource.id
       translatable_fields = resource.class.translatable_fields_list.map(&:to_s)
       translatable_fields.each do |field|
         locales = Decidim.available_locales.map(&:to_s)
         locales.each do |locale|
+          next unless locale != source_locale
+          byebug
           MachineTranslationCreateFieldsJob.perform_later(id, class_name, field, resource[field], locale)
         end
       end
--- decidim-core/lib/decidim/translatable_resource.rb
+++ decidim-core/lib/decidim/translatable_resource.rb
@@ -19,11 +19,11 @@ module Decidim
     end
 
     def machine_translation_new_resource
-      Decidim::MachineTranslationNewResourceJob.perform_later(self)
+      Decidim::MachineTranslationNewResourceJob.perform_later(self, I18n.locale.to_s)
     end
 
     def machine_translation_updated_resource
-      Decidim::MachineTranslationUpdatedResourceJob.perform_later(self)
+      Decidim::MachineTranslationUpdatedResourceJob.perform_later(self, I18n.locale.to_s)
     end
   end
 end
