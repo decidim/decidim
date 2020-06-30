d014886349420e35d367ed09ca07310be06a3158
Oliver Valls <oliver.vh@coditramuntana.com>
2020-06-30 08:23:16 +0200

    Fix mistyped table name

--- decidim-proposals/db/migrate/20200120230130_drop_proposal_endorsements.rb
+++ decidim-proposals/db/migrate/20200120230130_drop_proposal_endorsements.rb
@@ -3,6 +3,6 @@
 class DropProposalEndorsements < ActiveRecord::Migration[5.2]
   def change
     drop_table :decidim_proposals_proposal_endorsements, if_exists: true, force: :restrict
-    remove_column :decidim_proposals_proposal, :proposal_endorsements_count
+    remove_column :decidim_proposals_proposals, :proposal_endorsements_count
   end
 end
