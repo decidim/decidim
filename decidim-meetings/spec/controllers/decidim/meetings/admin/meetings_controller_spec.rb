8dddee21a95606eda3ea6e6a4d4ba1f049149dc9
Jesús Di Bari <jesus.db@coditramuntana.com>
2020-07-01 14:10:41 +0200

    Add tests to meetings controller (destroy)

---
+++ decidim-meetings/spec/controllers/decidim/meetings/admin/meetings_controller_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  module Meetings
+    module Admin
+      describe MeetingsController, type: :controller do
+        routes { Decidim::Meetings::AdminEngine.routes }
+
+        let!(:meeting) { create :meeting, component: meeting_component }
+        let(:user) { create :user, :admin, :confirmed, organization: organization }
+
+        let(:time_zone) { "UTC" }
+        let(:organization) { create(:organization, time_zone: time_zone) }
+        let(:participatory_process) { create :participatory_process, organization: organization }
+        let(:meeting_component) { create(:meeting_component, participatory_space: participatory_process) }
+        let(:proposal_component) { create(:proposal_component, participatory_space: participatory_process) }
+        let(:proposal) { create(:proposal, component: proposal_component) }
+        let(:meeting_proposals) { meeting.authored_proposals }
+
+        describe "POST #destroy" do
+          before do
+            request.env["decidim.current_organization"] = organization
+            request.env["decidim.current_component"] = meeting_component
+            sign_in user, scope: :user
+            proposal.coauthorships.clear
+            proposal.add_coauthor(meeting)
+          end
+
+          context "when having at least one proposal (invalid)" do
+            it "flashes an alert message" do
+              post :destroy, params: { id: meeting.id }
+
+              expect(flash[:alert]).not_to be_empty
+            end
+
+            it "renders the index view" do
+              post :destroy, params: { id: meeting.id }
+
+              expect(subject).to render_template(:index)
+            end
+          end
+        end
+      end
+    end
+  end
+end
