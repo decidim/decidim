321ccd8a974c63316056bb473b374a910103fdc7
Vera Rojman <vrojman@protonmail.com>
2020-07-03 14:47:03 +0200

    Add permission spec for share_token

--- decidim-admin/spec/permissions/decidim/admin/permissions_spec.rb
+++ decidim-admin/spec/permissions/decidim/admin/permissions_spec.rb
@@ -141,6 +141,14 @@ describe Decidim::Admin::Permissions do
       it { is_expected.to eq true }
     end
   end
+  
+  describe "share tokens" do
+    let(:action_subject) { :share_token }
+
+    context "when any action" do
+      it { is_expected.to eq true }
+    end
+  end
 
   describe "organization" do
     let(:action_subject) { :organization }
