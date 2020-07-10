b0b6137a6c02443fbc00c8a9addeadfb55f7029c
Agustí B.R <hola@agusti.cat>
2020-07-10 15:19:33 +0200

    feat(graphql): add/fix gaphQL types

---
+++ decidim-budgets/app/types/decidim/budgets/budget_type.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Budgets
+    BudgetType = GraphQL::ObjectType.define do
+      interfaces [
+        -> { Decidim::Core::TraceableInterface }
+      ]
+
+      name "Budget"
+      description "A budget"
+
+      field :id, !types.ID, "The internal ID of this election"
+      field :title, !Decidim::Core::TranslatedFieldType, "The title for this budget"
+      field :description, !Decidim::Core::TranslatedFieldType, "The description for this budget"
+      field :total_budget, !types.Int, "The total budget"
+      field :createdAt, Decidim::Core::DateTimeType, "When this budget was created", property: :created_at
+      field :updatedAt, Decidim::Core::DateTimeType, "When this budget was updated", property: :updated_at
+
+      field :projects, !types[Decidim::Budgets::ProjectType], "The projects for this budget"
+    end
+  end
+end
