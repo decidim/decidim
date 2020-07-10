d3e9c853a578f77bdba05cbd31df5ce9fa1461eb
Vera Rojman <vrojman@protonmail.com>
2020-07-10 18:05:08 +0200

    Present matrix questions in pdf

--- decidim-forms/app/presenters/decidim/forms/admin/questionnaire_answer_presenter.rb
+++ decidim-forms/app/presenters/decidim/forms/admin/questionnaire_answer_presenter.rb
@@ -19,15 +19,17 @@ module Decidim
           return answer.body if answer.body.present?
           return "-" if answer.choices.empty?
 
-          choices = answer.choices.map do |choice|
-            choice.try(:custom_body) || choice.try(:body)
-          end
+          return answer.choices.first if answer.question.question_type == "single_option"
 
-          return choices.first if answer.question.question_type == "single_option"
+          present_choices
+        end
 
-          content_tag(:ul) do
-            safe_join(choices.map { |c| choice(c) })
-          end
+        def separator?
+          answer.question.separator?
+        end
+        
+        def matrix?
+          answer.question.matrix?
         end
 
         private
@@ -36,9 +38,31 @@ module Decidim
           answer.questionnaire.questionnaire_for&.component&.organization
         end
 
-        def choice(choice_body)
-          content_tag :li do
-            choice_body
+        def choice_body(choice)
+          choice.try(:custom_body) || choice.try(:body)
+        end
+
+        def present_choices
+          if matrix?
+            content_tag :dl do
+              safe_join(
+                answer.choices.map do |c|
+                  matrix_row = answer.question.matrix_rows.find_by(id: c.matrix_row.id)
+                  safe_join([
+                    content_tag(:dt, translated_attribute(matrix_row.body)),
+                    content_tag(:dd, choice_body(c))
+                  ])
+                end
+              )
+            end
+          else
+            content_tag :ul do
+              safe_join(
+                answer.choices.map do |c|
+                  content_tag(:li, choice_body(c))
+                end
+              )
+            end
           end
         end
       end
