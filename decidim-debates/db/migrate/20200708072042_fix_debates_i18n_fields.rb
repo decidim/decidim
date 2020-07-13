4768b00040b3ac9c5e059791e978eaa85f5b63ef
Marc Riera Casals <mrc2407@gmail.com>
2020-07-13 10:37:26 +0200

    Improve code

--- decidim-debates/db/migrate/20200708072042_fix_debates_i18n_fields.rb
+++ decidim-debates/db/migrate/20200708072042_fix_debates_i18n_fields.rb
@@ -16,9 +16,7 @@ class FixDebatesI18nFields < ActiveRecord::Migration[5.2]
   def change
     reset_column_information
 
-    Debate.find_each do |debate|
-      next if debate.decidim_author_type == "Decidim::Organization"
-
+    debates.find_each do |debate|
       locale, org_id = User.where(id: debate.decidim_author_id).pluck(:locale, :decidim_organization_id).first
       locale = locale.presence || Organization.find(org_id).default_locale
       locale = locale.to_s
@@ -30,10 +28,16 @@ class FixDebatesI18nFields < ActiveRecord::Migration[5.2]
         locale => debate.description[locale]
       }
 
-      debate.save!
+      debate.save!(validate: false)
     end
 
     reset_column_information
+  end
+
+  def debates
+    Debate
+      .where.not(decidim_author_type: "Decidim::Organization")
+      .select(:id, :decidim_author_id, :title, :description)
   end
 
   def reset_column_information
