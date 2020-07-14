0d94db4cdc3b941e75985a29764ec652d89a6090
Leonardo Diez <leiodd@gmail.com>
2020-07-14 15:01:30 +0200

    feat: order answers randomly when needed

--- decidim-elections/app/helpers/decidim/elections/votes_helper.rb
+++ decidim-elections/app/helpers/decidim/elections/votes_helper.rb
@@ -5,6 +5,14 @@ module Decidim
     # Custom helpers for the voting booth views.
     #
     module VotesHelper
+      def ordered_answers(question)
+        if question.random_answers_order
+          question.answers.shuffle
+        else
+          question.answers
+        end
+      end
+
       def more_information?(answer)
         answer.description || answer.proposals.any? || answer.photos.any?
       end
