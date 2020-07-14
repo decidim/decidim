472d661da7122670d34d18e1687ff4b52cf19351
anagha <anagha1996@gmail.com>
2020-07-14 14:08:20 +0530

    [WIP] changing approach to machine translation, removing translated field

--- decidim-core/app/models/decidim/translated_field.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Decidim
-  class TranslatedField < ApplicationRecord
-    belongs_to :translated_resource, foreign_key: "translated_resource_id", foreign_type: "translated_resource_type", polymorphic: true, optional: true
-  end
-end
