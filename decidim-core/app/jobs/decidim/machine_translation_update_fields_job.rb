63f9dfe55a07adfc1a3805e795c06470c51c9903
Marc Riera Casals <mrc2407@gmail.com>
2020-07-16 15:36:21 +0200

    Let DummyTranslator know how to update values

--- decidim-core/app/jobs/decidim/machine_translation_update_fields_job.rb
+++ decidim-core/app/jobs/decidim/machine_translation_update_fields_job.rb
@@ -5,16 +5,8 @@ module Decidim
     queue_as :default
 
     def perform(resource, field_name, field_value, locale, source_locale)
-      translation_value = Decidim.machine_translation_service.to_s.safe_constantize.new(field_value, source_locale, locale).translate
+      Decidim.machine_translation_service.to_s.safe_constantize.new(field_value, source_locale, locale).translate
 
-      #Refactor code
-      if resource[field_name]["machine_translations"].present?
-        resource[field_name]["machine_translations"] = resource[field_name]["machine_translations"].merge({locale => translation_value})
-      else
-        resource[field_name] = resource[field_name].merge({"machine_translations" =>{ locale => translation_value }})
-      end
-
-      resource.update_attribute field_name, resource[field_name]
     end
   end
 end
--- decidim-core/lib/decidim/dummy_translator.rb
+++ decidim-core/lib/decidim/dummy_translator.rb
@@ -7,16 +7,34 @@ module Decidim
   # This is for testing only.
   class DummyTranslator
 
-    attr_reader :text, :original_locale, :target_locale
-  
-    def initialize(text, original_locale, target_locale)
+    attr_reader :text, :original_locale, :target_locale, :resource, :field_name
+
+    def initialize(resource, field_name, text, original_locale, target_locale)
+      @resource = resource
+      @field_name = field_name
       @text = text
       @original_locale = original_locale
       @target_locale = target_locale
     end
 
     def translate
-       "#{target_locale} - #{text}"
+      translated_text = "#{target_locale} - #{text}"
+
+      #Refactor code
+      if resource[field_name]["machine_translations"].present?
+        resource[field_name]["machine_translations"] = resource[field_name]["machine_translations"].merge({locale => translation_value})
+      else
+        resource[field_name] = resource[field_name].merge({"machine_translations" =>{ locale => translation_value }})
+      end
+
+      resource.update_attribute field_name, resource[field_name]
+
+      # DummyTranslatorJob.perform_later(
+      #   resource,
+      #   field_name,
+      #   translated_text,
+      #   target_locale
+      # )
     end
   end
 end
