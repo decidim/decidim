f5373224e9fb3f3eadcd8d607710d7775def29f5
Marc Riera Casals <mrc2407@gmail.com>
2020-07-16 15:20:47 +0200

    Override translatable attribute setter

--- decidim-core/lib/decidim/translatable_resource.rb
+++ decidim-core/lib/decidim/translatable_resource.rb
@@ -9,8 +9,33 @@ module Decidim
     included do
       after_create :machine_translation_updated_resource
       after_update :machine_translation_updated_resource
+
       def self.translatable_fields(*list)
         @translatable_fields = list
+
+        @translatable_fields.each do |field|
+          method_name = "#{field}="
+
+          # We're overriding the attribute setter method so that we can reuse the
+          # machine translations. This is to fix a bug encoutered when updating
+          # the resource from a FormObject. The FormObject on the `#create`
+          # action in the controller doesn't have the machine translations loaded,
+          # so they're effectively lost whenever a resource is updated.
+          #
+          # This overriding allows us to keep the old machine translations, so
+          # that we skip some translations requests.
+          define_method(method_name) do |new_value|
+            original_value = attributes[field.to_s]
+
+            return super(new_value) if new_value.has_key?("machine_translations")
+
+            if original_value.has_key?("machine_translations")
+              new_value = new_value.merge("machine_translations" => original_value["machine_translations"])
+            end
+
+            super(new_value)
+          end
+        end
       end
 
       def self.translatable_fields_list
@@ -21,7 +46,7 @@ module Decidim
     def machine_translation_new_resource
       return unless Decidim.machine_translation_service.present?
 
-      Decidim::MachineTranslationCreateResourceJob.perform_later(
+      Decidim::MachineTranslationCreateResourceJob.perform_now(
         self,
         I18n.locale.to_s
       )
@@ -29,7 +54,8 @@ module Decidim
 
     def machine_translation_updated_resource
       return unless Decidim.machine_translation_service.present?
-      Decidim::MachineTranslationUpdatedResourceJob.perform_later(
+
+      Decidim::MachineTranslationUpdatedResourceJob.perform_now(
         self,
         translatable_previous_changes,
         I18n.locale.to_s
