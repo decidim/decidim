b717c79221a187260185b942229a327dd64b27c3
Armand Fardeau <fardeauarmand@gmail.com>
2020-07-21 15:30:32 +0200

    Reintroduce time between renewals

--- decidim-generators/lib/decidim/generators/app_templates/verifications_initializer.rb
+++ decidim-generators/lib/decidim/generators/app_templates/verifications_initializer.rb
@@ -3,7 +3,9 @@
 Decidim::Verifications.register_workflow(:dummy_authorization_handler) do |workflow|
   workflow.form = "DummyAuthorizationHandler"
   workflow.action_authorizer = "DummyAuthorizationHandler::DummyActionAuthorizer"
-  workflow.expires_in = 1.month
+  workflow.expires_in = 1.hour
+  workflow.renewable = true
+  workflow.time_between_renewals = 5.minutes
 
   workflow.options do |options|
     options.attribute :postal_code, type: :string, default: "08001", required: false
