e643d4a89236094e823c4d4c517ad8d0e3c79283
Ramon Costa <ramon.costa@gmail.com>
2020-07-22 09:35:36 +0200

    Clean comments

--- decidim-core/app/helpers/decidim/sanitize_helper.rb
+++ decidim-core/app/helpers/decidim/sanitize_helper.rb
@@ -30,7 +30,6 @@ module Decidim
       decidim_html_escape(text).sub(/^javascript:/, "")
     end
 
-    # Security fix - Avoid CSV injection - https://owasp.org/www-community/attacks/CSV_Injection
     def csv_sanitize(value)
       return value unless value.instance_of?(String) && invalid_first_chars.include?(value.first)
 
