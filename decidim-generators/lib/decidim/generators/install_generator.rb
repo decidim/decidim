decidim/decidim

commit c096524bd35874c7344f81bfa4ebf8a4b445d330

Setup profiling tooling (#6281)
* Add --profiling flag to configure to dev app

This configures bullet and rack-mini-profiler for development apps to
perform all sorts of profiling: analyze memory, database, and call stack
with flamegraphs.

More details:
* https://github.com/flyerhzm/bullet,
* https://github.com/MiniProfiler/rack-mini-profiler.

* Replace gem list with decidim's gem name pattern

Bullet treats `stacktrace_includes` as a pattern to match against
backtrace lines so there's no need to specify all gems. This works
already.

decidim-generators/lib/decidim/generators/install_generator.rb
@@ -30,6 +30,10 @@ module Decidim
                                   default: false,
                                   desc: "Don't generate a Gemfile for the application"
 
+      class_option :profiling, type: :boolean,
+                               default: false,
+                               desc: "Add the necessary gems to profile the app"
+
       def install
         route "mount Decidim::Core::Engine => '/'"
       end
@@ -100,6 +104,25 @@ module Decidim
             |  }
           RUBY
         end
+      end
+
+      def profiling_gems
+        return unless options[:profiling]
+
+        append_file "Gemfile", <<~RUBY
+
+          group :development do
+            # Profiling gems
+            gem "bullet"
+            gem "flamegraph"
+            gem "memory_profiler"
+            gem "rack-mini-profiler", require: false
+            gem "stackprof"
+          end
+        RUBY
+
+        copy_file "bullet_initializer.rb", "config/initializers/bullet.rb"
+        copy_file "rack_profiler_initializer.rb", "config/initializers/rack_profiler.rb"
       end
 
       def copy_migrations

decidim-dev/lib/tasks/generators.rake
@@ -44,7 +44,8 @@ namespace :decidim do
         "..",
         "--recreate_db",
         "--seed_db",
-        "--demo"
+        "--demo",
+        "--profiling"
       )
     end
   end

decidim-generators/lib/decidim/generators/app_generator.rb
@@ -61,6 +61,10 @@ module Decidim
                           default: false,
                           desc: "Generate demo authorization handlers"
 
+      class_option :profiling, type: :boolean,
+                               default: false,
+                               desc: "Add the necessary gems to profile the app"
+
       def database_yml
         template "database.yml.erb", "config/database.yml", force: true
       end
@@ -192,7 +196,8 @@ module Decidim
             "--recreate_db=#{options[:recreate_db]}",
             "--seed_db=#{options[:seed_db]}",
             "--skip_gemfile=#{options[:skip_gemfile]}",
-            "--app_name=#{app_name}"
+            "--app_name=#{app_name}",
+            "--profiling=#{options[:profiling]}"
           ]
         )
       end

decidim-generators/lib/decidim/generators/app_templates/bullet_initializer.rb
@@ -1,0 +1,10 @@
+# frozen_string_literal: true
+
+if defined?(Bullet)
+  Rails.application.config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    Bullet.rails_logger = true
+    Bullet.stacktrace_includes = %w(decidim-)
+  end
+end

decidim-generators/lib/decidim/generators/app_templates/rack_profiler_initializer.rb
@@ -1,0 +1,8 @@
+# frozen_string_literal: true
+
+if Rails.env.development?
+  require "rack-mini-profiler"
+
+  # initialization is skipped so trigger it
+  Rack::MiniProfilerRails.initialize!(Rails.application)
+end

commit cd14dd19f01ae19bde2214dfb2d3a65554595849

Fix development_app rake task
Adding the profiling gems to the Gemfile midway through the install
generator breaks the next steps because they are still missing. That's
because we don't run `bundle install` in the generator. Moving this to the end makes it work.

This got broken in https://github.com/decidim/decidim/pull/6281. Thanks
to @microstudi for the heads-up!

decidim-generators/lib/decidim/generators/install_generator.rb
@@ -106,25 +106,6 @@ module Decidim
         end
       end
 
-      def profiling_gems
-        return unless options[:profiling]
-
-        append_file "Gemfile", <<~RUBY
-
-          group :development do
-            # Profiling gems
-            gem "bullet"
-            gem "flamegraph"
-            gem "memory_profiler"
-            gem "rack-mini-profiler", require: false
-            gem "stackprof"
-          end
-        RUBY
-
-        copy_file "bullet_initializer.rb", "config/initializers/bullet.rb"
-        copy_file "rack_profiler_initializer.rb", "config/initializers/rack_profiler.rb"
-      end
-
       def copy_migrations
         rails "decidim:upgrade"
         recreate_db if options[:recreate_db]
@@ -146,6 +127,25 @@ module Decidim
             |  config.action_mailer.default_url_options = { port: 3000 }
           RUBY
         end
+      end
+
+      def profiling_gems
+        return unless options[:profiling]
+
+        append_file "Gemfile", <<~RUBY
+
+          group :development do
+            # Profiling gems
+            gem "bullet"
+            gem "flamegraph"
+            gem "memory_profiler"
+            gem "rack-mini-profiler", require: false
+            gem "stackprof"
+          end
+        RUBY
+
+        copy_file "bullet_initializer.rb", "config/initializers/bullet.rb"
+        copy_file "rack_profiler_initializer.rb", "config/initializers/rack_profiler.rb"
       end
 
       private