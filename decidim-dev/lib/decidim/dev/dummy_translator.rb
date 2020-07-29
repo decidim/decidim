5df1991df5c95a7fb32cc2a4ed303391e08baba8
Marc Riera Casals <mrc2407@gmail.com>
2020-07-29 16:05:09 +0200

    Fix docs and path

--- decidim-dev/lib/decidim/dev/dummy_translator.rb
+++ decidim-dev/lib/decidim/dev/dummy_translator.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module Decidim
-  # This Dummy translator recieves the field value
-  # and the locale of the field which has to be
-  # translated. It returns the appended value for both.
-  # This is for testing only.
   module Dev
+    # This Dummy translator recieves the field value
+    # and the locale of the field which has to be
+    # translated. It returns the appended value for both.
+    # This is for testing only.
     class DummyTranslator
       attr_reader :text, :original_locale, :target_locale, :resource, :field_name
 
