fa89fc5c03f482e5c5687a25f803059a3fc14004
Marc Riera Casals <mrc2407@gmail.com>
2020-07-29 14:46:38 +0200

    Fix linter complaints

--- decidim-core/spec/lib/dummy_translator_spec.rb
+++ decidim-core/spec/lib/dummy_translator_spec.rb
@@ -4,13 +4,13 @@ require "spec_helper"
 
 module Decidim
   describe DummyTranslator do
+    subject { described_class.new(resource, field_name, original_value, target_locale, source_locale) }
+
     let(:resource) { create :participatory_process, title: { source_locale => original_value } }
     let(:source_locale) { :en }
     let(:target_locale) { :ca }
     let(:field_name) { :title }
     let(:original_value) { "My title" }
-
-    subject { described_class.new(resource, field_name, original_value, target_locale, source_locale) }
 
     it "schedules a job to save the attribute" do
       expect(MachineTranslationSaveJob)
