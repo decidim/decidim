11422df3159b64a9676db3fe14dd74cb7333be1b
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-07-31 23:57:09 +0300

    Fix gallery methods when current organization is not available

--- decidim-core/app/commands/decidim/gallery_methods.rb
+++ decidim-core/app/commands/decidim/gallery_methods.rb
@@ -7,16 +7,13 @@ module Decidim
     private
 
     def build_gallery(attached_to = nil)
-      attached_to = @attached_to if attached_to.blank?
-      attached_to = form.current_organization if attached_to.blank? && form.respond_to?(:current_organization)
-
       @gallery = []
       @form.add_photos.each do |photo|
         next unless image? photo
 
         @gallery << Attachment.new(
           title: photo.original_filename,
-          attached_to: attached_to,
+          attached_to: attached_to || gallery_attached_to,
           file: photo # Define attached_to before this
         )
       end
@@ -40,19 +37,19 @@ module Decidim
 
     def create_gallery
       @gallery.map! do |photo|
-        photo.attached_to = @attached_to
+        photo.attached_to = gallery_attached_to
         photo.save!
         @form.photos << photo.id.to_s
       end
     end
 
     def photo_cleanup!
-      @attached_to.photos.each do |photo|
+      gallery_attached_to.photos.each do |photo|
         photo.destroy! if @form.photos.exclude? photo.id.to_s
       end
       # manually reset cached photos
-      @attached_to.reload
-      @attached_to.instance_variable_set(:@photos, nil)
+      gallery_attached_to.reload
+      gallery_attached_to.instance_variable_set(:@photos, nil)
     end
 
     # maybe a custom settings options would be nice
@@ -62,6 +59,13 @@ module Decidim
 
     def process_gallery?
       gallery_allowed? && @form.add_photos.any?
+    end
+
+    def gallery_attached_to
+      return @attached_to if @attached_to.present?
+      return form.current_organization if form.respond_to?(:current_organization)
+
+      form.current_component.organization if form.respond_to?(:current_component)
     end
   end
 end
