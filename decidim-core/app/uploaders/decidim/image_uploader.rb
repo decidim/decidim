bb60c6299c464c16ff46d8ce772a8bc859baf739
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-08-02 21:54:31 +0300

    Rename the method in order to unify naming with the other classes

--- decidim-core/app/uploaders/decidim/image_uploader.rb
+++ decidim-core/app/uploaders/decidim/image_uploader.rb
@@ -60,7 +60,7 @@ module Decidim
 
     def validate_size
       manipulate! do |image|
-        validation_error!(I18n.t("carrierwave.errors.image_too_big")) if image.size > maximum_attachment_size
+        validation_error!(I18n.t("carrierwave.errors.image_too_big")) if image.size > maximum_upload_size
         image
       end
     end
@@ -83,7 +83,7 @@ module Decidim
       raise CarrierWave::IntegrityError, text
     end
 
-    def maximum_attachment_size
+    def maximum_upload_size
       Decidim.organization_settings(model).upload_maximum_file_size
     end
   end
