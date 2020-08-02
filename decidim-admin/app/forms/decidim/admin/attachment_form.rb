3d050048e6cb569a40fe07cd8179e9a1aa24dce2
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-08-02 22:04:24 +0300

    Pass through the admin attachemnt form validators to the record

--- decidim-admin/app/forms/decidim/admin/attachment_form.rb
+++ decidim-admin/app/forms/decidim/admin/attachment_form.rb
@@ -16,6 +16,7 @@ module Decidim
       mimic :attachment
 
       validates :file, presence: true, unless: :persisted?
+      validates :file, passthru: { to: Decidim::Attachment }
       validates :title, :description, translatable_presence: true
       validates :attachment_collection, presence: true, if: ->(form) { form.attachment_collection_id.present? }
       validates :attachment_collection_id, inclusion: { in: :attachment_collection_ids }, allow_blank: true
