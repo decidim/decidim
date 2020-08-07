7f2f9a5ce1f15698f5994257fbff5ae93848090e
slickepinne <svenjaschaefer@gmail.com>
2020-08-07 09:27:20 +0200

    fix: remove unnecessary cell data options

--- decidim-elections/app/cells/decidim/elections/election_m_cell.rb
+++ decidim-elections/app/cells/decidim/elections/election_m_cell.rb
@@ -25,25 +25,6 @@ module Decidim
         icon "elections", class: "icon--big"
       end
 
-      def spans_multiple_dates?
-        start_date != end_date
-      end
-
-      def election_date
-        return unless start_date && end_date
-        return render(:multiple_dates) if spans_multiple_dates?
-
-        render(:single_date)
-      end
-
-      def formatted_start_time
-        model.start_time.strftime("%H:%M")
-      end
-
-      def formatted_end_time
-        model.end_time.strftime("%H:%M")
-      end
-
       def start_date
         return unless model.start_time
 
