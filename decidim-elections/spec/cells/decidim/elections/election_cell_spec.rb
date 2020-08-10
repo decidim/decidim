45df4c003ece0dd6d1087a3191ba136e875e7794
slickepinne <svenjaschaefer@gmail.com>
2020-08-10 15:03:32 +0200

    add tests for election cell

---
+++ decidim-elections/spec/cells/decidim/elections/election_cell_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Decidim::Elections::ElectionCell, type: :cell do
+  controller Decidim::Elections::ElectionsController
+
+  subject { my_cell.call }
+
+  let(:my_cell) { cell("decidim/elections/election", model) }
+  let!(:election) { create(:election, :complete, :published, :ongoing) }
+  let!(:current_user) { create(:user, :confirmed, organization: model.participatory_space.organization) }
+
+  before do
+    allow(controller).to receive(:current_user).and_return(current_user)
+  end
+
+  context "when rendering an election" do
+    let(:model) { election }
+
+    it "renders the card" do
+      expect(subject).to have_css(".card--election")
+    end
+  end
+end
