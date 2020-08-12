550f727b6570f251f5ec90b9244a57c2b5b44ca8
Vera Rojman <vrojman@protonmail.com>
2020-08-12 12:20:57 +0200

    Refactor translations in helper

--- decidim-templates/app/helpers/decidim/templates/admin/templates_helper.rb
+++ decidim-templates/app/helpers/decidim/templates/admin/templates_helper.rb
@@ -10,12 +10,21 @@ module Decidim
           prompt_options = {
             url: decidim_admin_templates.questionnaire_templates_url(format: :json),
             change_url: decidim_admin_templates.preview_questionnaire_templates_url(format: :js),
-            placeholder: t(".choose_template")
+            placeholder: t("placeholder", scope: "decidim.templates.admin.questionnaire_templates.choose")
           }
 
           default_options = templates.last(5).map { |questionnaire_template| { value: questionnaire_template.id, label: translated_attribute(questionnaire_template.name) } }
 
-          form.autocomplete_select(:questionnaire_template_id, false, { multiple: false, label: t(".choose_template"), default_options: default_options }, prompt_options)
+          form.autocomplete_select(
+            :questionnaire_template_id,
+            false,
+            {
+              multiple: false,
+              label: t("label", scope: "decidim.templates.admin.questionnaire_templates.choose"),
+              default_options: default_options
+            },
+            prompt_options
+          )
         end
       end
     end
