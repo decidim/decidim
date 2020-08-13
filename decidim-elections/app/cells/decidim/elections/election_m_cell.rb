0a358a53127e71e9d94c9ed549d660aa64a79a8a
slickepinne <svenjaschaefer@gmail.com>
2020-08-13 14:44:38 +0200

    add statuses to cards and update footer text

--- decidim-elections/app/cells/decidim/elections/election_m_cell.rb
+++ decidim-elections/app/cells/decidim/elections/election_m_cell.rb
@@ -13,16 +13,51 @@ module Decidim
 
       private
 
+      def has_state?
+        true
+      end
+
       def title
         present(model).title
       end
 
-      def description
-        present(model).description(strip_tags: true)
-      end
-
       def resource_icon
         icon "elections", class: "icon--big"
+      end
+
+      # Even though we need to render the badge, we can't do it in the normal
+      # way, because the paragraph comes from a user input and contains HTML.
+      # This causes the badge and the paragraph to appear in different lines.
+      # In order to fix it, check the `description` method.
+      def has_badge?
+        false
+      end
+
+      def badge_name
+        text = model.voting_period_status
+        return unless text
+
+        I18n.t(text, scope: "decidim.elections.election_m.badge_name")
+      end
+
+      # In order to render the badge inline with the paragraph text we need to
+      # find the opening `<p>` tag and include the badge right after it. This
+      # makes the layout look good.
+      def description
+        text = super
+        text.gsub!(/^<p>/, "<p>#{render :badge}")
+        html_truncate(text, length: 100)
+      end
+
+      def state_classes
+        case model.voting_period_status
+        when :ongoing
+          ["success"]
+        when :upcoming
+          ["warning"]
+        else
+          ["muted"]
+        end
       end
 
       def start_date
@@ -37,8 +72,23 @@ module Decidim
         model.end_time.to_date
       end
 
+      def questions_count_status
+        content_tag(
+          :strong,
+          t("decidim.elections.election_m.questions", count: model.questions.count)
+        )
+      end
+
+      def footer_button_text
+        if model.ongoing?
+          t("decidim.elections.election_m.footer.vote")
+        else
+          t("decidim.elections.election_m.footer.view")
+        end
+      end
+
       def statuses
-        []
+        [:questions_count]
       end
     end
   end
