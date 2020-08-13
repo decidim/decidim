ebbc1d46cb53b221968aaae43297dbd758a24254
Oliver Valls <oliver.vh@coditramuntana.com>
2020-08-13 10:36:02 +0200

    Bugfix 0.22.0.rc1 (#6381)
    * Declare colors for filter button
    
    * Call organization_colors inside decidim-admin
    
    * Different translations for proposals' states and initiatives' state
    
    * Re-fix by having totally different i18n scopes for proposals and initiatives
    
    * Fix color links on author_name and hide/show replies and reply links
    
    * Fix misaligned error on proposal cards
    
    * Fix lint code on filterable_helper
    
    * Fix truncate not applied in Last Activity cards
    
    * Fix missing username in last activity cards for proposals
    
    * Fix endorsements list 'and X more people' doesn't behave correctly
    
    * Fix random indent style in conversations and notifications
    
    * Let i18n_ctx be optional
    
    * Delete commented tags
    
    * Remove "for %{resource_name}" in Version Control and Fingerprint
    
    * Remove trailing ")" in related assemblies
    
    * Fix unexpected whitespace before "{" in scss
    
    * Remove argument due to method signature
    
    * Fix [CI] Proposals (unit tests) / Tests (pull_request)
    
    * Add last activity card shorten text spec
    
    Co-authored-by: Ivan Molinero <ivan.mr@coditramuntana.com>
    Co-authored-by: Ramon Costa <ramon.costa@gmail.com>

--- decidim-admin/app/helpers/decidim/admin/filterable_helper.rb
+++ decidim-admin/app/helpers/decidim/admin/filterable_helper.rb
@@ -5,44 +5,46 @@ module Decidim
     # Helper that provides methods related to Decidim::Admin::Filterable concern.
     module FilterableHelper
       # Renders the filters selector with tags in the admin panel.
-      def admin_filter_selector
-        render partial: "decidim/admin/shared/filters"
+      def admin_filter_selector(i18n_ctx = nil)
+        render partial: "decidim/admin/shared/filters", locals: { i18n_ctx: i18n_ctx }
       end
 
       # Builds a tree of links from Decidim::Admin::Filterable::filters_with_values
-      def submenu_options_tree
+      def submenu_options_tree(i18n_ctx = nil)
+        i18n_scope = filterable_i18n_scope_from_ctx(i18n_ctx)
+
         filters_with_values.each_with_object({}) do |(filter, values), hash|
-          link = filter_link_label(filter)
+          link = filter_link_label(filter, i18n_scope)
           hash[link] = if values.is_a?(Array)
-                         build_submenu_options_tree_from_array(filter, values)
+                         build_submenu_options_tree_from_array(filter, values, i18n_scope)
                        elsif values.is_a?(Hash)
-                         build_submenu_options_tree_from_hash(filter, values)
+                         build_submenu_options_tree_from_hash(filter, values, i18n_scope)
                        end
         end
       end
 
       # Builds a tree of links from an array. The tree will have only one level.
-      def build_submenu_options_tree_from_array(filter, values)
+      def build_submenu_options_tree_from_array(filter, values, i18n_scope)
         links = []
-        links += extra_dropdown_submenu_options_items(filter)
-        links += values.map { |value| filter_link_value(filter, value) }
+        links += extra_dropdown_submenu_options_items(filter, i18n_scope)
+        links += values.map { |value| filter_link_value(filter, value, i18n_scope) }
         links.each_with_object({}) { |link, hash| hash[link] = nil }
       end
 
       # To be overriden. Useful for adding links that do not match with the filter.
       # Must return an Array.
-      def extra_dropdown_submenu_options_items(_filter)
+      def extra_dropdown_submenu_options_items(_filter, _i18n_scope)
         []
       end
 
       # Builds a tree of links from an Hash. The tree can have many levels.
-      def build_submenu_options_tree_from_hash(filter, values)
+      def build_submenu_options_tree_from_hash(filter, values, i18n_scope)
         values.each_with_object({}) do |(key, value), hash|
-          link = filter_link_value(filter, key)
+          link = filter_link_value(filter, key, i18n_scope)
           hash[link] = if value.nil?
                          nil
                        elsif value.is_a?(Hash)
-                         build_submenu_options_tree_from_hash(filter, value)
+                         build_submenu_options_tree_from_hash(filter, value, i18n_scope)
                        end
         end
       end
@@ -63,21 +65,21 @@ module Decidim
         end
       end
 
-      def filter_link_label(filter)
-        link_to(i18n_filter_label(filter), href: "#")
+      def filter_link_label(filter, i18n_scope)
+        link_to(i18n_filter_label(filter, i18n_scope), href: "#")
       end
 
-      def filter_link_value(filter, value)
-        link_to(i18n_filter_value(filter, value), query_params_with(filter => value))
+      def filter_link_value(filter, value, i18n_scope)
+        link_to(i18n_filter_value(filter, value, i18n_scope), query_params_with(filter => value))
       end
 
-      def i18n_filter_label(filter)
-        t("decidim.admin.filters.#{filter}.label")
+      def i18n_filter_label(filter, i18n_scope)
+        t("#{i18n_scope}.#{filter}.label")
       end
 
-      def i18n_filter_value(filter, value)
-        if I18n.exists?("decidim.admin.filters.#{filter}.values.#{value}")
-          t(value, scope: "decidim.admin.filters.#{filter}.values")
+      def i18n_filter_value(filter, value, i18n_scope)
+        if I18n.exists?("#{i18n_scope}.#{filter}.values.#{value}")
+          t(value, scope: "#{i18n_scope}.#{filter}.values")
         else
           find_dynamic_translation(filter, value)
         end
@@ -94,16 +96,16 @@ module Decidim
         html.join.html_safe
       end
 
-      def applied_filters_tags
+      def applied_filters_tags(i18n_ctx)
         ransack_params.slice(*filters).map do |filter, value|
-          applied_filter_tag(filter, value)
+          applied_filter_tag(filter, value, filterable_i18n_scope_from_ctx(i18n_ctx))
         end.join.html_safe
       end
 
-      def applied_filter_tag(filter, value)
+      def applied_filter_tag(filter, value, i18n_scope)
         content_tag(:span, class: "label secondary") do
-          concat "#{i18n_filter_label(filter)}: "
-          concat i18n_filter_value(filter, value)
+          concat "#{i18n_filter_label(filter, i18n_scope)}: "
+          concat i18n_filter_value(filter, value, i18n_scope)
           concat remove_filter_icon_link(filter)
         end
       end
@@ -115,6 +117,12 @@ module Decidim
           t("decidim.admin.actions.cancel"),
           class: "action-icon--remove"
         )
+      end
+
+      def filterable_i18n_scope_from_ctx(i18n_ctx)
+        i18n_scope = "decidim.admin.filters"
+        i18n_scope += ".#{i18n_ctx}" if i18n_ctx
+        i18n_scope
       end
     end
   end
