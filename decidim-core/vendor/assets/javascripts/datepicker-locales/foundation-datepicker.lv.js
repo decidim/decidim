c81609568bb05ab73b2da2b5c7cdd7bbb2ce56ff
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2020-08-17 19:00:27 +0200

    add latvian support

---
+++ decidim-core/vendor/assets/javascripts/datepicker-locales/foundation-datepicker.lv.js
@@ -0,0 +1,15 @@
+/**
+ * Latvian translation for foundation-datepicker
+ */
+;(function($){
+	$.fn.fdatepicker.dates['lv'] = {
+		days: ["Svētdiena", "Pirmdiena", "Otrdiena", "Trešdiena", "Ceturtdiena", "Piektdiena", "Sestdiena", "Svētdiena"],
+		daysShort: ["Svētd", "Pirmd", "Otrd", "Trešd", "Ceturtd", "Piektd", "Sestd", "Svētd"],
+		daysMin: ["S", "P", "O", "T", "C", "P", "S", "S"],
+		months: ["Janvāris", "Februāris", "Marts", "Aprīlis", "Maijs", "Jūnijs", "Jūlijs", "Augusts", "Septembris", "Oktobris", "Novembris", "Décembris"],
+		monthsShort: ["Janv", "Febr", "Marts", "Apr", "Maijs", "Jūn", "Jūl", "Aug", "Sept", "Okt", "Nov", "Dec"],
+		today: "Šodien",
+		clear: "Izdzēst",
+		weekStart: 1
+	};
+}(jQuery));
