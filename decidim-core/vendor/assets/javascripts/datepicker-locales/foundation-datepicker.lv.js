e00089aa101e74822356c9c9f78ef721d93a31b7
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2020-08-18 13:51:08 +0200

    remove duplicated 'sunday' in datepicker

--- decidim-core/vendor/assets/javascripts/datepicker-locales/foundation-datepicker.lv.js
+++ decidim-core/vendor/assets/javascripts/datepicker-locales/foundation-datepicker.lv.js
@@ -3,9 +3,9 @@
  */
 ;(function($){
 	$.fn.fdatepicker.dates['lv'] = {
-		days: ["Svētdiena", "Pirmdiena", "Otrdiena", "Trešdiena", "Ceturtdiena", "Piektdiena", "Sestdiena", "Svētdiena"],
-		daysShort: ["Svētd", "Pirmd", "Otrd", "Trešd", "Ceturtd", "Piektd", "Sestd", "Svētd"],
-		daysMin: ["S", "P", "O", "T", "C", "P", "S", "S"],
+		days: ["Svētdiena", "Pirmdiena", "Otrdiena", "Trešdiena", "Ceturtdiena", "Piektdiena", "Sestdiena"],
+		daysShort: ["Svētd", "Pirmd", "Otrd", "Trešd", "Ceturtd", "Piektd", "Sestd"],
+		daysMin: ["S", "P", "O", "T", "C", "P", "S"],
 		months: ["Janvāris", "Februāris", "Marts", "Aprīlis", "Maijs", "Jūnijs", "Jūlijs", "Augusts", "Septembris", "Oktobris", "Novembris", "Décembris"],
 		monthsShort: ["Janv", "Febr", "Marts", "Apr", "Maijs", "Jūn", "Jūl", "Aug", "Sept", "Okt", "Nov", "Dec"],
 		today: "Šodien",
