4022e7ec760187041aaae320b61fa4c46981b906
Oriol Gual <oriol@codegram.com>
2020-08-24 12:26:12 +0200

    Add comments metadata to debates

--- decidim-debates/app/presenters/decidim/debates/debate_presenter.rb
+++ decidim-debates/app/presenters/decidim/debates/debate_presenter.rb
@@ -33,6 +33,39 @@ module Decidim
         content = strip_tags(content) if strip_tags
         content
       end
+
+      def last_comment_by
+        return unless comments_authors.any?
+
+        comment_author = comments.order("created_at DESC").first.normalized_author
+        return unless comment_author
+
+        if comment_author.is_a?(Decidim::User)
+          Decidim::UserPresenter.new(comment_author)
+        elsif comment_author.is_a?(Decidim::UserGroup)
+          Decidim::UserGroupPresenter.new(comment_author)
+        elsif comment_author.is_a?(Decidim::Organization)
+          Decidim::Debates::OfficialAuthorPresenter.new
+        end
+      end
+
+      def participants_count
+        comments_authors.count do |author|
+          author.is_a?(Decidim::User)
+        end
+      end
+
+      def groups_count
+        comments_authors.count do |author|
+          author.is_a?(Decidim::UserGroup)
+        end
+      end
+
+      private
+
+      def comments_authors
+        @comments_authors ||= debate.comments.includes(:author, :user_group).map(&:normalized_author).uniq
+      end
     end
   end
 end
