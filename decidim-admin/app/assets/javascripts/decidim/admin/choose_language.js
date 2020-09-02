d8294e53b20e557dfb61ac300d3550cd3acb9ad5
anagha <anagha1996@gmail.com>
2020-09-02 19:34:22 +0530

    adding dropdown option to locales list

---
+++ decidim-admin/app/assets/javascripts/decidim/admin/choose_language.js
@@ -0,0 +1,17 @@
+$(() => {
+  $("select.language-change").change(function () {
+    var selectedValue = this.value;
+    var $select = $(this);
+    var options = $select[0].children;
+    var arr = Array.from(options);
+    var selectedOption = arr.filter(function(opt) { 
+      return opt.value == selectedValue 
+    })
+    var targetTabPane = selectedOption[0]["attributes"].href["value"];
+    var $tabsContent = $select.parent().parent().siblings();
+    $previousTab = $tabsContent.children('.is-active');
+    $previousTab.removeClass('is-active');
+    $newTab = $tabsContent.children(targetTabPane);
+    $newTab.addClass("is-active");
+  })
+});
