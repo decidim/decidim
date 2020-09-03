32c4839263b099cfbab1215b4b3b6c06e296ce9b
anagha <anagha1996@gmail.com>
2020-09-04 01:21:31 +0530

    disable no-invalid-this error

--- decidim-admin/app/assets/javascripts/decidim/admin/choose_language.js
+++ decidim-admin/app/assets/javascripts/decidim/admin/choose_language.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-invalid-this */
+
 $(() => {
   $("select.language-change").change(function () {
     let $select = $(this);
-    let selectedValue = $select[0].value;
+    let selectedValue = this.value;
     let options = $select[0].children;
     let arr = Array.from(options);
     let selectedOption = arr.filter(function(opt) {
@@ -9,9 +11,7 @@ $(() => {
     })
     let targetTabPane = selectedOption[0].attributes.href.value;
     let $tabsContent = $select.parent().parent().siblings();
-    let $previousTab = $tabsContent.children(".is-active");
-    $previousTab.removeClass("is-active");
-    let $newTab = $tabsContent.children(targetTabPane);
-    $newTab.addClass("is-active");
+    $tabsContent.children(".is-active").removeClass("is-active");
+    $tabsContent.children(targetTabPane).addClass("is-active");
   })
 });
