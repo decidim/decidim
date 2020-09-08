cefcc59fe126274fe76438524b8183341182aa22
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-09-08 09:47:40 +0300

    Add backend functionality for answering a comment

--- decidim-comments/app/controllers/decidim/comments/comments_controller.rb
+++ decidim-comments/app/controllers/decidim/comments/comments_controller.rb
@@ -5,9 +5,39 @@ module Decidim
     # Controller that manages the comments for a commentable object.
     #
     class CommentsController < Decidim::Comments::ApplicationController
+      before_action :authenticate_user!
+      before_action :set_commentable
+
       def create
-        # TODO: Check permissions
-        # TODO: Add comment
+        raise ActionController::RoutingError, "Not Found" unless commentable
+
+        form = Decidim::Comments::CommentForm.from_params(params).with_context(
+          current_organization: current_organization,
+          current_component: commentable.component
+        )
+        Decidim::Comments::CreateComment.call(form, current_user) do
+          on(:ok) do |comment|
+            @comment = comment
+            render :create
+          end
+
+          on(:invalid) do
+            @error = t("create.error", scope: "decidim.comments.comments")
+            render :error
+          end
+        end
+      end
+
+      private
+
+      attr_reader :commentable
+
+      def set_commentable
+        @commentable = GlobalID::Locator.locate_signed(commentable_gid)
+      end
+
+      def commentable_gid
+        params.require(:comment).fetch(:commentable_gid)
       end
     end
   end
