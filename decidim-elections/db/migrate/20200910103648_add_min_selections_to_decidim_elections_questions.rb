63f4e83c28e04b6ccfbfd75e5fddb39fec80b07f
slickepinne <svenjaschaefer@gmail.com>
2020-09-10 16:01:20 +0200

    add min_selections type and lint migration

--- decidim-elections/db/migrate/20200910103648_add_min_selections_to_decidim_elections_questions.rb
+++ decidim-elections/db/migrate/20200910103648_add_min_selections_to_decidim_elections_questions.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddMinSelectionsToDecidimElectionsQuestions < ActiveRecord::Migration[5.2]
   def change
     add_column :decidim_elections_questions, :min_selections, :integer, null: false, default: 1
