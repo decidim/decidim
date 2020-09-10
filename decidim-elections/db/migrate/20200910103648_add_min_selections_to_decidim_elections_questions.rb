20891283673cd5e092298f2b5262dfc6d337ae11
slickepinne <svenjaschaefer@gmail.com>
2020-09-10 12:44:46 +0200

    add min_selections to questions

---
+++ decidim-elections/db/migrate/20200910103648_add_min_selections_to_decidim_elections_questions.rb
@@ -0,0 +1,5 @@
+class AddMinSelectionsToDecidimElectionsQuestions < ActiveRecord::Migration[5.2]
+  def change
+    add_column :decidim_elections_questions, :min_selections, :integer, null: false, default: 1
+  end
+end
