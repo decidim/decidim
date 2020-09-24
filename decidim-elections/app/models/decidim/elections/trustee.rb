499149824d03171326158a25e184f9628c67ba7a
slickepinne <svenjaschaefer@gmail.com>
2020-09-24 12:58:38 +0200

    fix relations between trustee, election and participatory spaces

--- decidim-elections/app/models/decidim/elections/trustee.rb
+++ decidim-elections/app/models/decidim/elections/trustee.rb
@@ -6,10 +6,10 @@ module Decidim
     # public key and has a reference to Decidim::User.
     class Trustee < ApplicationRecord
       belongs_to :user, foreign_key: "decidim_user_id", class_name: "Decidim::User"
-      has_many :elections_trustees, dependent: :destroy
-      has_many :elections, through: :elections_trustees, foreign_key: "decidim_elections_election_id", class_name: "Decidim::Elections::Election"
-      has_many :trustees_participatory_spaces, dependent: :destroy
-      has_many :participatory_spaces, through: :trustees_participatory_spaces, foreign_key: "participatory_space_id", foreign_type: "participatory_space_type"
+      has_many :elections_trustees, foreign_key: "decidim_elections_trustee_id", dependent: :destroy
+      has_many :elections, through: :elections_trustees
+      has_many :trustees_participatory_spaces, foreign_key: "decidim_elections_trustee_id", dependent: :destroy
+      has_many :participatory_spaces, through: :trustees_participatory_spaces, source_type: name
     end
   end
 end
