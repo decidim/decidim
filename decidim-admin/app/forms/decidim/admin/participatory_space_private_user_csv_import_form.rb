95bbf45b0de72d358132a8aff79729a2aa08229b
Ramon Costa <ramon.costa@gmail.com>
2020-09-25 09:17:58 +0200

    Fix csv form validate

--- decidim-admin/app/forms/decidim/admin/participatory_space_private_user_csv_import_form.rb
+++ decidim-admin/app/forms/decidim/admin/participatory_space_private_user_csv_import_form.rb
@@ -1,19 +1,25 @@
 # frozen_string_literal: true
 
+require "csv"
+
 module Decidim
   module Admin
     # A form object used to upload CSV to batch participatory space private users.
     #
     class ParticipatorySpacePrivateUserCsvImportForm < Form
       attribute :file
+      attribute :name, String
+      attribute :email, String
 
       validates :file, presence: true
       validate :validate_csv
 
       def validate_csv
-        CSV.foreach(file.path) do |_email, user_name|
-          errors.add(:user_name, "user_name not valid!") unless user_name.match?(/\A(?!.*[<>?%&\^*#@\(\)\[\]\=\+\:\;\"\{\}\\\|])/)
-          errors.add(:email, :taken) if context && context.current_organization && context.current_organization.admins.where(email: email).exists?
+        if file.present?
+          CSV.foreach(file.path) do |email, user_name|
+            errors.add(:user_name, "user_name not valid!") unless user_name.match?(/\A(?!.*[<>?%&\^*#@\(\)\[\]\=\+\:\;\"\{\}\\\|])/)
+            errors.add(:email, :taken) if context && context.current_organization && context.current_organization.admins.where(email: email).exists?
+          end
         end
       end
     end
