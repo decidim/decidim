613874fb78bd4e789a389f5a6f7a75fe27dd7c9c
slickepinne <svenjaschaefer@gmail.com>
2020-09-30 00:07:37 +0200

    update and fix trustee relations

--- decidim-elections/app/models/decidim/elections/trustee.rb
+++ decidim-elections/app/models/decidim/elections/trustee.rb
@@ -8,8 +8,7 @@ module Decidim
       belongs_to :user, foreign_key: "decidim_user_id", class_name: "Decidim::User"
       has_many :elections_trustees, foreign_key: "decidim_elections_trustee_id", dependent: :destroy
       has_many :elections, through: :elections_trustees
-      has_many :trustees_participatory_spaces, foreign_key: "decidim_elections_trustee_id", dependent: :destroy
-      has_many :participatory_spaces, through: :trustees_participatory_spaces, source_type: name
+      has_many :trustees_participatory_spaces, inverse_of: :trustee, foreign_key: "decidim_elections_trustee_id", dependent: :destroy
     end
   end
 end
