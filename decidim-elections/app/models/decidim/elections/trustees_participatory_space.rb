5ebfcd3dd7d7cdabf1a914b3cb63207dda0d6aec
slickepinne <svenjaschaefer@gmail.com>
2020-09-30 00:07:37 +0200

    update and fix trustee relations

--- decidim-elections/app/models/decidim/elections/trustees_participatory_space.rb
+++ decidim-elections/app/models/decidim/elections/trustees_participatory_space.rb
@@ -5,7 +5,7 @@ module Decidim
     # The data store for a trustee in the Decidim::Elections component. It stores a
     # public key and has a reference to Decidim::User.
     class TrusteesParticipatorySpace < ApplicationRecord
-      belongs_to :trustee, foreign_key: "decidim_elections_trustee_id", class_name: "Decidim::Elections::Trustee"
+      belongs_to :trustee, foreign_key: "decidim_elections_trustee_id", class_name: "Decidim::Elections::Trustee", inverse_of: :trustees_participatory_spaces
       belongs_to :participatory_space, foreign_key: "participatory_space_id", foreign_type: "participatory_space_type", polymorphic: true
     end
   end
