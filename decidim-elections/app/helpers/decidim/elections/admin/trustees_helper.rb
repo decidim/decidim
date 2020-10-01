daec8b63ee07ee0e90ef181dd4e5c4c5175107b4
Agustí B.R <hola@agusti.cat>
2020-10-01 17:33:38 +0200

    chore(elections): enable/disable TrusteesParticipatorySpace considered attribute

---
+++ decidim-elections/app/helpers/decidim/elections/admin/trustees_helper.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Elections
+    module Admin
+      # Custom helpers for trustees admin.
+      #
+      module TrusteesHelper
+        def trustee_current_participatory_space(trustee)
+          trustee.trustees_participatory_spaces.where(participatory_space: current_participatory_space).first
+        end
+
+        def considered_icon_action_for(trustee)
+          if trustee_current_participatory_space(trustee).considered
+            "x"
+          else
+            "check"
+          end
+        end
+
+        def considered_label_action_for(trustee)
+          if trustee_current_participatory_space(trustee).considered
+            t("trustee_participatory_space.actions.disable", scope: "decidim.elections.admin")
+          else
+            t("trustee_participatory_space.actions.enable", scope: "decidim.elections.admin")
+          end
+        end
+      end
+    end
+  end
+end
