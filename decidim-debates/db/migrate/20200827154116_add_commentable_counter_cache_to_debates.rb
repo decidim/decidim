a63b4f884e13a9a8b0ff779046e180d33fa854b6
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-10-01 12:05:40 +0300

    Fix debates comments count migration (#6564)

--- decidim-debates/db/migrate/20200827154116_add_commentable_counter_cache_to_debates.rb
+++ decidim-debates/db/migrate/20200827154116_add_commentable_counter_cache_to_debates.rb
@@ -4,6 +4,11 @@ class AddCommentableCounterCacheToDebates < ActiveRecord::Migration[5.2]
   def change
     add_column :decidim_debates_debates, :comments_count, :integer, null: false, default: 0, index: true
     Decidim::Debates::Debate.reset_column_information
-    Decidim::Debates::Debate.find_each(&:update_comments_count)
+
+    # rubocop:disable Rails/SkipsModelValidations
+    Decidim::Debates::Debate.includes(:comments).find_each do |debate|
+      debate.update_columns(comments_count: debate.comments.not_hidden.count)
+    end
+    # rubocop:enable Rails/SkipsModelValidations
   end
 end
