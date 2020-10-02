167146340d43fcdf2a70f33234ea1ea7174c3173
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-10-02 08:44:30 +0300

    Add specs for the cells and the controllers

---
+++ decidim-comments/spec/cells/decidim/comments/comments_cell_spec.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim::Comments
+  describe CommentsCell, type: :cell do
+    controller Decidim::Comments::CommentsController
+
+    subject { my_cell.call }
+
+    let(:my_cell) { cell("decidim/comments/comments", comment.commentable) }
+    let(:organization) { create(:organization) }
+    let(:participatory_process) { create :participatory_process, organization: organization }
+    let(:component) { create(:component, participatory_space: participatory_process) }
+    let(:commentable) { create(:dummy_resource, component: component) }
+    let(:comment) { create(:comment, commentable: commentable) }
+
+    context "when rendering" do
+      it "renders the thread" do
+        expect(subject).to have_css(".section-heading", text: "1 comment")
+        expect(subject).to have_css(".callout.primary.loading-comments p", text: "Loading comments ...")
+        expect(subject).to have_content(comment.body.values.first)
+        expect(subject).to have_css(".add-comment")
+        expect(subject).to have_content("Sign in with your account or sign up to add your comment.")
+
+        {
+          best_rated: "Best rated",
+          recent: "Recent",
+          older: "Older",
+          most_discussed: "Most discussed"
+        }.each do |key, title|
+          expect(subject).to have_css("a[href^='/comments?'][href$='&order=#{key}&reload=1']", text: title)
+        end
+      end
+
+      context "with the single comment defined" do
+        let(:my_cell) { cell("decidim/comments/comments", comment.commentable, single_comment: comment.id) }
+        let(:other_comments) { create_list(:comment, 10, commentable: commentable) }
+
+        it "renders only the single comment" do
+          expect(subject).to have_css(".section-heading", text: "Comment details")
+          expect(subject).to have_content(comment.body.values.first)
+
+          other_comments.each do |other_comment|
+            expect(subject).not_to have_content(other_comment.body.values.first)
+          end
+        end
+
+        it "renders the single comment warning" do
+          expect(subject).to have_css(".callout.secondary", text: "You are seeing a single comment")
+          expect(subject).to have_css(".callout.secondary", text: "You can check the rest of the comments here.")
+        end
+      end
+
+      context "when signed in" do
+        let(:current_user) { create(:user, :confirmed, organization: component.organization) }
+
+        before do
+          allow(controller).to receive(:current_user).and_return(current_user)
+          allow(controller).to receive(:user_signed_in?).and_return(true)
+        end
+
+        it "renders the add comment form" do
+          expect(subject).to have_css(".add-comment #new_comment_for_DummyResource_#{commentable.id}")
+        end
+
+        context "when alignment is enabled" do
+          before do
+            allow(commentable).to receive(:comments_have_alignment?).and_return(true)
+          end
+
+          it "renders the alignment buttons" do
+            expect(subject).to have_css(".opinion-toggle.button-group .opinion-toggle--ok")
+            expect(subject).to have_css(".opinion-toggle.button-group .opinion-toggle--meh")
+            expect(subject).to have_css(".opinion-toggle.button-group .opinion-toggle--ko")
+          end
+        end
+
+        context "when comments are blocked" do
+          before do
+            comment # Create the comment before disabling comments
+            allow(commentable).to receive(:accepts_new_comments?).and_return(false)
+            allow(commentable).to receive(:user_allowed_to_comment?).with(current_user).and_return(false)
+          end
+
+          it "renders the comments blocked warning" do
+            expect(subject).to have_css(".callout.warning", text: I18n.t("decidim.components.comments.blocked_comments_warning"))
+            expect(subject).not_to have_css(".callout.warning", text: I18n.t("decidim.components.comments.blocked_comments_for_user_warning"))
+          end
+        end
+
+        context "when user comments are blocked" do
+          before do
+            allow(commentable).to receive(:user_allowed_to_comment?).with(current_user).and_return(false)
+          end
+
+          it "renders the user comments blocked warning" do
+            expect(subject).not_to have_css(".callout.warning", text: I18n.t("decidim.components.comments.blocked_comments_warning"))
+            expect(subject).to have_css(".callout.warning", text: I18n.t("decidim.components.comments.blocked_comments_for_user_warning"))
+          end
+        end
+      end
+    end
+  end
+end
