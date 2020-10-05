dea136d530e053128242e40847b7363a8c330f15
Ramon Costa <ramon.costa@gmail.com>
2020-10-05 14:17:40 +0200

    Fix form validations for user's invitation (#6556)
    * Fix form validations for user's invitation
    
    * Add form's validation for email uniqueness
    
    * Add forms spec tests
    
    * Add csv's form spec sample test file
    
    * Fix form's rubocop offenses
    
    * Fix spec's rubocop offenses
    
    * Fix csv form validate
    
    * Fix shared rspec test for assembly and participatory process
    
    * Move form's validation regex to UserBaseEntity & fix not localized error message
    
    * Fix rubocop offense
    
    * Add user roles controller spec for Assemblies and Participatory Processess
    
    * Fix rubocop offenses

--- decidim-admin/spec/forms/participatory_space_private_user_csv_import_form_spec.rb
+++ decidim-admin/spec/forms/participatory_space_private_user_csv_import_form_spec.rb
@@ -5,7 +5,17 @@ require "spec_helper"
 module Decidim
   module Admin
     describe ParticipatorySpacePrivateUserCsvImportForm do
-      subject { described_class.from_params(attributes) }
+      subject do
+        described_class.from_params(
+          attributes
+        ).with_context(
+          current_user: current_user,
+          current_organization: current_organization
+        )
+      end
+
+      let(:current_organization) { create(:organization) }
+      let(:current_user) { create(:user, organization: current_organization) }
 
       let(:attributes) do
         {
@@ -20,6 +30,19 @@ module Decidim
 
       context "when file is missing" do
         let(:file) { nil }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context "when user name contains invalid chars" do
+        let(:file) { File.new Decidim::Dev.asset("import_participatory_space_private_users_nok.csv") }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context "when email is already being used" do
+        let(:email) { "my_user@example.org" }
+        let!(:user) { create(:user, name: "Daisy Miller", nickname: "daisy_m", email: email, organization: current_organization, admin: true) }
 
         it { is_expected.to be_invalid }
       end
