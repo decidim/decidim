56b0726b6ac9765180d6aa4c9032be51a005d49d
Eduardo Martínez <eduardomech@gmail.com>
2020-10-06 23:32:04 +0200

    Add system specs for participatory process group edition
    * Checks secondary nav elements
    * Check management of participatory process group homepage content
      blocks

---
+++ decidim-participatory_processes/spec/system/admin/admin_manages_participatory_process_group_homepage_spec.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Admin manages participatory process group homepage", type: :system do
+  include_context "when admin administrating a participatory process"
+  let!(:participatory_process_group) { create(:participatory_process_group, organization: organization) }
+
+  before do
+    switch_to_host(organization.host)
+    login_as user, scope: :user
+  end
+
+  context "when editing a participatory process group homepage" do
+    it "has sub nav with Homepage active" do
+      visit decidim_admin_participatory_processes.edit_participatory_process_group_homepage_path(participatory_process_group)
+      within "div.secondary-nav" do
+        expect(page).to have_content("Info")
+        expect(page).to have_content("Homepage")
+        active_secondary_nav = find(:xpath, ".//li[@class='is-active']")
+        expect(active_secondary_nav.text).to eq("Homepage")
+      end
+    end
+  end
+
+  context "when editing a non-persisted content block" do
+    it "creates the content block to the db before editing it" do
+      visit decidim_admin_participatory_processes.edit_participatory_process_group_homepage_path(participatory_process_group)
+
+      expect(Decidim::ContentBlock.for_scope(
+        :participatory_process_group_homepage,
+        organization: organization
+      ).where(scoped_resource_id: participatory_process_group.id).count).to eq 0
+
+      within ".js-list-availables" do
+        within find("li", text: "Hero image") do
+          find("svg.icon--pencil").click
+        end
+      end
+
+      expect(Decidim::ContentBlock.for_scope(
+        :participatory_process_group_homepage,
+        organization: organization
+      ).where(scoped_resource_id: participatory_process_group.id).count).to eq 1
+    end
+  end
+
+  context "when editing a persisted content block" do
+    let!(:content_block) do
+      create(
+        :content_block,
+        organization: organization,
+        manifest_name: :hero,
+        scope_name: :participatory_process_group_homepage,
+        scoped_resource_id: participatory_process_group.id
+      )
+    end
+
+    it "updates the settings of the content block" do
+      visit decidim_admin_participatory_processes.edit_participatory_process_group_homepage_content_block_path(participatory_process_group, :hero)
+
+      fill_in(
+        :content_block_settings_welcome_text_en,
+        with: "Custom welcome text!"
+      )
+
+      click_button "Update"
+      visit decidim_admin_participatory_processes.edit_participatory_process_group_homepage_content_block_path(participatory_process_group, :hero)
+      expect(page).to have_selector("input[value='Custom welcome text!']")
+
+      content_block.reload
+
+      expect(content_block.settings.to_json).to match(/Custom welcome text!/)
+    end
+  end
+end
