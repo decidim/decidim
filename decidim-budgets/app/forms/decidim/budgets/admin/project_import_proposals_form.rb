fc8e492259c892e37170fe57fdee78a2b61da067
Marina <marina@codegram.com>
2020-10-07 11:14:05 +0200

    Rename remaining decidim_scope_id vars to scope_id

--- decidim-budgets/app/forms/decidim/budgets/admin/project_import_proposals_form.rb
+++ decidim-budgets/app/forms/decidim/budgets/admin/project_import_proposals_form.rb
@@ -9,14 +9,14 @@ module Decidim
         mimic :proposals_import
 
         attribute :origin_component_id, Integer
-        attribute :decidim_scope_id, Integer
+        attribute :scope_id, Integer
         attribute :default_budget, Integer
         attribute :import_all_accepted_proposals, Boolean
 
         validates :origin_component_id, :origin_component, :current_component, presence: true
         validates :import_all_accepted_proposals, allow_nil: false, acceptance: true
         validates :default_budget, presence: true, numericality: { greater_than: 0 }
-        validates :scope, presence: true, if: ->(form) { form.decidim_scope_id.present? }
+        validates :scope, presence: true, if: ->(form) { form.scope_id.present? }
         validates :scope_id, scope_belongs_to_component: true, if: ->(form) { form.scope_id.present? }
 
         def origin_component
@@ -34,7 +34,7 @@ module Decidim
         end
 
         def scope
-          @scope ||= @scope_id ? current_component.scopes.find_by(id: @decidim_scope_id) : current_component.scope
+          @scope ||= @scope_id ? current_component.scopes.find_by(id: @scope_id) : current_component.scope
         end
 
         def scope_id
