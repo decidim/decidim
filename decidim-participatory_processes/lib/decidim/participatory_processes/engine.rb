74872c0c4ffb74168bf61d12406e99c2fbd3698d
Eduardo Martínez <eduardomech@gmail.com>
2020-10-09 16:49:44 +0200

    Add html content block and remove hero

--- decidim-participatory_processes/lib/decidim/participatory_processes/engine.rb
+++ decidim-participatory_processes/lib/decidim/participatory_processes/engine.rb
@@ -88,22 +88,14 @@ module Decidim
           content_block.default!
         end
 
-        Decidim.content_blocks.register(:participatory_process_group_homepage, :hero) do |content_block|
-          content_block.cell = "decidim/content_blocks/hero"
-          content_block.settings_form_cell = "decidim/content_blocks/hero_settings_form"
-          content_block.public_name_key = "decidim.content_blocks.hero.name"
-
-          content_block.images = [
-            {
-              name: :background_image,
-              uploader: "Decidim::HomepageImageUploader"
-            }
-          ]
+        Decidim.content_blocks.register(:participatory_process_group_homepage, :html) do |content_block|
+          content_block.cell = "decidim/content_blocks/html"
+          content_block.public_name_key = "decidim.content_blocks.html.name"
+          content_block.settings_form_cell = "decidim/content_blocks/html_settings_form"
 
           content_block.settings do |settings|
-            settings.attribute :welcome_text, type: :text, translated: true
+            settings.attribute :html_content, type: :text, translated: true
           end
-
           content_block.default!
         end
       end
