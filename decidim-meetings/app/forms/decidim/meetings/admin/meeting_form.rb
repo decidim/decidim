26d9e66d15858088837c0f17cda38bfd781645fd
Marc Riera Casals <mrc2407@gmail.com>
2020-10-09 16:17:50 +0200

    Validate URLs

--- decidim-meetings/app/forms/decidim/meetings/admin/meeting_form.rb
+++ decidim-meetings/app/forms/decidim/meetings/admin/meeting_form.rb
@@ -34,7 +34,7 @@ module Decidim
 
         validates :address, presence: true, if: ->(form) { form.needs_address? }
         validates :address, geocoding: true, if: ->(form) { form.has_address? && !form.geocoded? && form.needs_address? }
-        validates :online_meeting_url, presence: true, if: ->(form) { form.online_meeting? }
+        validates :online_meeting_url, presence: true, url: true, if: ->(form) { form.online_meeting? }
         validates :start_time, presence: true, date: { before: :end_time }
         validates :end_time, presence: true, date: { after: :start_time }
 
