26b63311adc26242734785d9a778c5aa64b47ecf
Marc Riera Casals <mrc2407@gmail.com>
2020-10-12 15:05:40 +0200

    Fix lints

--- decidim-meetings/app/presenters/decidim/meetings/meeting_presenter.rb
+++ decidim-meetings/app/presenters/decidim/meetings/meeting_presenter.rb
@@ -82,11 +82,11 @@ module Decidim
       def handle_locales(content, all_locales, &block)
         if all_locales
           content.each_with_object({}) do |(key, value), parsed_content|
-            if key == "machine_translations"
-              parsed_content[key] = handle_locales(value, all_locales, &block)
-            else
-              parsed_content[key] = block.call(value)
-            end
+            parsed_content[key] = if key == "machine_translations"
+                                    handle_locales(value, all_locales, &block)
+                                  else
+                                    block.call(value)
+                                  end
           end
         else
           yield(translated_attribute(content))
