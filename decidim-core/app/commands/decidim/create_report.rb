182c10b6dd50124754f837d671faeeb5b50d401f
Andrea Orler <andrea@codegram.com>
2020-10-13 16:45:32 +0200

    send notification to authors when a report is created for their content

--- decidim-core/app/commands/decidim/create_report.rb
+++ decidim-core/app/commands/decidim/create_report.rb
@@ -30,6 +30,7 @@ module Decidim
       end
 
       send_report_notification_to_moderators
+      send_report_notification_to_author
 
       if hideable?
         hide!
@@ -68,6 +69,19 @@ module Decidim
       participatory_space_moderators.each do |moderator|
         ReportedMailer.report(moderator, @report).deliver_later
       end
+    end
+
+    def send_report_notification_to_author
+      data = {
+        event: "decidim.events.reports.report_created",
+        event_class: Decidim::ReportCreatedEvent,
+        resource: @report.moderation.reportable,
+        extra: {
+          report_reason: @report.reason
+        },
+        affected_users: @report.moderation.reportable.try(:authors) || [@report.moderation.reportable.try(:author)]
+      }
+      Decidim::EventsManager.publish(data)
     end
 
     def hideable?
