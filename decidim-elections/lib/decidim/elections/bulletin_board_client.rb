a87d5e72548b66181224a037b4a7d3a98ae8940e
slickepinne <svenjaschaefer@gmail.com>
2020-10-13 15:13:10 +0200

    set graphql_client to private

--- decidim-elections/lib/decidim/elections/bulletin_board_client.rb
+++ decidim-elections/lib/decidim/elections/bulletin_board_client.rb
@@ -7,7 +7,7 @@ module Decidim
         @server = params[:server].presence
         @api_key = params[:api_key].presence
         @identification_private_key = params[:identification_private_key]&.strip.presence
-
+        @graphql_client = graphql_client
         @private_key = OpenSSL::PKey::RSA.new(identification_private_key_content) if identification_private_key
       end
 
@@ -19,23 +19,8 @@ module Decidim
         private_key && server && api_key
       end
 
-      def graphql_client
-        Graphlient::Client.new(server,
-                               headers: {
-                                 "api_key" => api_key
-                               })
-      end
-
       def encode_data(setup_election_data)
-        JWT.encode setup_election_data, private_key, 'RS256'
-      end
-
-      def create_election(election_data)
-        graphql_client.query do
-          mutation do
-            createElection(signedData: encode_data(election_data), apiKey: api_key)
-          end
-        end
+        JWT.encode setup_election_data, private_key, "RS256"
       end
 
       private
@@ -48,6 +33,13 @@ module Decidim
                                                 else
                                                   File.read(Rails.application.root.join(identification_private_key))
                                                 end
+      end
+
+      def graphql_client
+        @graphql_client ||= Graphlient::Client.new(server,
+                                                   headers: {
+                                                     "api_key" => api_key
+                                                   })
       end
     end
   end
