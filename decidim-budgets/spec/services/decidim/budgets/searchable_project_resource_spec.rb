a85ca66f4fa4280a892a5f3458b4cce1756be8c3
Marc Riera Casals <mrc2407@gmail.com>
2020-10-14 16:36:06 +0200

    Fix budgets tests

--- decidim-budgets/spec/services/decidim/budgets/searchable_project_resource_spec.rb
+++ decidim-budgets/spec/services/decidim/budgets/searchable_project_resource_spec.rb
@@ -49,7 +49,7 @@ module Decidim
 
               organization.available_locales.each do |locale|
                 searchable = SearchableResource.find_by(resource_type: resource.class.name, resource_id: resource.id, locale: locale)
-                expect(searchable.content_a).to eq I18n.transliterate(updated_title[locale])
+                expect(searchable.content_a).to eq translated(updated_title, locale: locale)
                 expect(searchable.updated_at).to be > created_at
               end
             end
@@ -115,10 +115,10 @@ module Decidim
 
     def expected_searchable_resource_attrs(resource, locale)
       {
-        "content_a" => I18n.transliterate(resource.title[locale]),
+        "content_a" => I18n.transliterate(translated(resource.title, locale: locale)),
         "content_b" => "",
         "content_c" => "",
-        "content_d" => I18n.transliterate(resource.description[locale]),
+        "content_d" => I18n.transliterate(translated(resource.description, locale: locale)),
         "locale" => locale,
 
         "decidim_organization_id" => resource.component.organization.id,
