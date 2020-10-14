2d6867650290f8dcbf9152de0308e3bda5c0a07b
Andrea Orler <andrea@codegram.com>
2020-10-14 11:10:14 +0200

    improve notification message

--- decidim-core/app/events/decidim/report_created_event.rb
+++ decidim-core/app/events/decidim/report_created_event.rb
@@ -4,25 +4,26 @@ module Decidim
   class ReportCreatedEvent < Decidim::Events::SimpleEvent
     delegate :url_helpers, to: "Decidim::Core::Engine.routes"
 
-    i18n_attributes :resource_path, :report_reason, :resource_title
+    i18n_attributes :resource_path, :report_reason, :resource_type
 
     def resource_path
-      # TODO: use url_helpers to get the path to the reported resource
-      "/reported_resource"
+      @resource.moderation.reportable.reported_content_url
     end
 
     def resource_url
-      # TODO: use url_helpers to get the url to the reported resource
-      "localhost:3000/reported_resource"
+      @resource.moderation.reportable.reported_content_url
     end
 
     def resource_title
-      # TODO: figure out what to use as resource title
-      "miao"
+      @resource.moderation.reportable.try(:title) || resource_type
     end
 
     def report_reason
-      @resource.reason
+      I18n.t("decidim.admin.moderations.report.reasons.#{@resource.reason}").downcase
+    end
+
+    def resource_type
+      @resource.moderation.reportable.model_name.human.downcase
     end
   end
 end
