269cf5f73195cc5a5634b2edcaaada53d9138485
anagha vl <44900292+anaghavl@users.noreply.github.com>
2020-10-14 17:23:48 +0530

    Add online meetings (#6572)
    * Add config to allow online meetings
    
    * Add online_meeting_url fields to DB
    
    * Add fields to forms and commands
    
    * Use `data-` attributes instead of specific field names
    
    * Remove unused locales
    
    * Fix JS
    
    * Fix locale
    
    * Fix forms specs
    
    * Fix specs
    
    * Hide meeting type and online url fields if online meetings are not allowed
    
    * Show online meeting URL
    
    * Ensure fields are hidden
    
    * Fix meeting form
    
    * Fix tests
    
    * Add `type_of`meeting` column to meetings
    
    It stores whether the meeting is in-person or online
    
    * Filter meetings by type
    
    * Add missing i18n string
    
    * Validate URLs
    
    * Address feedback
    
    * Fix i18n
    
    * Remove config option
    
    * test: select the type of meeting before filling in location
    
    * Move URL details to cell
    
    * Fix form
    
    * Fix icon
    
    * Fix edit form
    
    Co-authored-by: Marc Riera Casals <mrc2407@gmail.com>
    Co-authored-by: David Morcillo <david.morcillo@gmail.com>

--- decidim-meetings/app/forms/decidim/meetings/admin/meeting_form.rb
+++ decidim-meetings/app/forms/decidim/meetings/admin/meeting_form.rb
@@ -17,6 +17,10 @@ module Decidim
         attribute :decidim_category_id, Integer
         attribute :private_meeting, Boolean
         attribute :transparent, Boolean
+        attribute :online_meeting_url, String
+        attribute :type_of_meeting, String
+
+        TYPE_OF_MEETING = %w(in_person online).freeze
 
         translatable_attribute :title, String
         translatable_attribute :description, String
@@ -25,10 +29,12 @@ module Decidim
 
         validates :title, translatable_presence: true
         validates :description, translatable_presence: true
-        validates :location, translatable_presence: true
+        validates :type_of_meeting, presence: true
+        validates :location, translatable_presence: true, if: ->(form) { form.in_person_meeting? }
 
-        validates :address, presence: true
-        validates :address, geocoding: true, if: ->(form) { form.has_address? && !form.geocoded? }
+        validates :address, presence: true, if: ->(form) { form.needs_address? }
+        validates :address, geocoding: true, if: ->(form) { form.has_address? && !form.geocoded? && form.needs_address? }
+        validates :online_meeting_url, presence: true, url: true, if: ->(form) { form.online_meeting? }
         validates :start_time, presence: true, date: { before: :end_time }
         validates :end_time, presence: true, date: { after: :start_time }
 
@@ -36,6 +42,7 @@ module Decidim
         validates :category, presence: true, if: ->(form) { form.decidim_category_id.present? }
         validates :scope, presence: true, if: ->(form) { form.decidim_scope_id.present? }
         validates :decidim_scope_id, scope_belongs_to_component: true, if: ->(form) { form.decidim_scope_id.present? }
+        validates :clean_type_of_meeting, presence: true
 
         delegate :categories, to: :current_component
 
@@ -49,6 +56,11 @@ module Decidim
 
           self.title = presenter.title(all_locales: title.is_a?(Hash))
           self.description = presenter.description(all_locales: description.is_a?(Hash))
+          self.type_of_meeting = if model.online_meeting?
+                                   "online"
+                                 else
+                                   "in_person"
+                                 end
         end
 
         def services_to_persist
@@ -89,8 +101,33 @@ module Decidim
           geocoding_enabled? && address.present?
         end
 
+        def needs_address?
+          in_person_meeting?
+        end
+
         def geocoded?
           latitude.present? && longitude.present?
+        end
+
+        def online_meeting?
+          type_of_meeting == "online"
+        end
+
+        def in_person_meeting?
+          type_of_meeting == "in_person"
+        end
+
+        def clean_type_of_meeting
+          type_of_meeting.presence
+        end
+
+        def type_of_meeting_select
+          TYPE_OF_MEETING.map do |type|
+            [
+              I18n.t("type_of_meeting.#{type}", scope: "decidim.meetings"),
+              type
+            ]
+          end
         end
       end
     end
