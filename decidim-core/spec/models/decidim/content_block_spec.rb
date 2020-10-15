a5986ea5a76188c36e1ed564d636e635cdec6008
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-10-15 09:36:10 +0300

    Fix content block image updates

--- decidim-core/spec/models/decidim/content_block_spec.rb
+++ decidim-core/spec/models/decidim/content_block_spec.rb
@@ -15,6 +15,16 @@ module Decidim
     end
 
     describe ".images_container" do
+      before do
+        # Enable processing for the test in order to catch validation errors
+        Decidim::HomepageImageUploader.enable_processing = true
+      end
+
+      after do
+        Decidim::HomepageImageUploader.enable_processing = false
+        content_block.images_container.background_image.remove! if content_block.images_container.background_image
+      end
+
       it "responds to the image names" do
         expect(subject.images_container).to respond_to(:background_image)
       end
@@ -38,8 +48,35 @@ module Decidim
           subject.save
         end
 
-        it "returns nil" do
+        it "returns the image" do
           expect(subject.images_container.background_image).not_to be_nil
+        end
+      end
+
+      context "when the image is larger in size than the organization allows" do
+        let(:original_image) do
+          Rack::Test::UploadedFile.new(
+            Decidim::Dev.test_file("city.jpeg", "image/jpeg"),
+            "image/jpeg"
+          )
+        end
+
+        before do
+          content_block.organization.settings.tap do |settings|
+            settings.upload.maximum_file_size.default = 1.kilobyte.to_f / 1.megabyte
+          end
+        end
+
+        it "returns fails to save the image with validation errors" do
+          subject.test = true
+          subject.images_container.background_image = original_image
+          subject.save
+          expect(subject.valid?).to be(false)
+          expect(subject.errors[:images_container]).to eq(["is invalid"])
+          expect(subject.images_container.errors.full_messages).to eq(
+            ["Background image The image is too big"]
+          )
+          expect(subject.images).to eq({})
         end
       end
     end
