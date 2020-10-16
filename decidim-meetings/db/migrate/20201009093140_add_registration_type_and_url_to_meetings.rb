7bf17142b462c97b9c431fcdb80464abc9d1f1c6
anagha <anagha1996@gmail.com>
2020-10-16 17:04:09 +0530

    Changing migration file

--- decidim-meetings/db/migrate/20201009093140_add_registration_type_and_url_to_meetings.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddRegistrationTypeAndUrlToMeetings < ActiveRecord::Migration[5.2]
-  def change
-    add_column :decidim_meetings_meetings, :registration_type, :string, null: false, default: "registration_disabled"
-    add_column :decidim_meetings_meetings, :registration_url, :string
-  end
-end
