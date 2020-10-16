6eb44ed761e487e272d4d626de2fe581a7423641
Andrea Orler <andrea@codegram.com>
2020-10-16 17:29:46 +0200

    send email when a reported content is translated to the default lan guage

--- decidim-core/spec/jobs/decidim/machine_translation_save_job_spec.rb
+++ decidim-core/spec/jobs/decidim/machine_translation_save_job_spec.rb
@@ -68,5 +68,29 @@ module Decidim
         )
       end
     end
+
+    describe "when the resource is reported" do
+      let(:moderation) { create(:moderation, report_count: 2) }
+      let!(:report) { create(:report, moderation: moderation) }
+      let(:comment) { build(:comment, body: { "ca" => "Comentari" }, moderation: moderation) }
+      let(:target_locale) { "en" }
+      let(:translated_value) { "Comment" }
+
+      it "sends emails to the moderators when the target language is the organization's default" do
+        allow(ReportedMailer).to receive(:send_report_notification_to_users).and_call_original
+        
+        comment.save
+        MachineTranslationSaveJob.perform_now(
+          comment,
+          "body",
+          target_locale,
+          translated_value
+        )
+
+        expect(ReportedMailer)
+            .to have_received(:send_report_notification_to_users)
+            .with(moderation.participatory_space.moderators, report)
+      end
+    end
   end
 end
