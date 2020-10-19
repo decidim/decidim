727f2092654db2fd4c636fef8abfa68c103a527f
Gladys Chacartegui <gladys@codegram.com>
2020-10-19 12:30:29 +0200

    Add permissions and form fields

--- decidim-elections/app/controllers/decidim/elections/admin/setup_controller.rb
+++ decidim-elections/app/controllers/decidim/elections/admin/setup_controller.rb
@@ -7,28 +7,13 @@ module Decidim
       class SetupController < Admin::ApplicationController
         helper_method :elections, :election, :trustees
 
-        def index
-          flash.now[:alert] ||= I18n.t("elections.index.no_bulletin_board", scope: "decidim.elections.admin").html_safe unless Decidim::Elections.bulletin_board.configured?
-        end
-
         def show
-          @form = form(SetupForm).from_model(election)
+          enforce_permission_to :setup, :election, election: election
+          @form = form(SetupForm).from_model(election, number_of_trustees: 2)
         end
 
         def update
-          # @form = form(SetupForm).from_params(params, election: election)
-
-          # SetupElection.call(@form, trustees, current_user) do
-          # on(:ok) do
-          #   flash[:notice] = I18n.t("setup.success", scope: "decidim.elections.admin")
-          # end
-
-          # on(:invalid) do
-          #   flash.now[:alert] = I18n.t("setup.invalid", scope: "decidim.elections.admin")
-          ##   render action: "edit"
-          # end
-          redirect_to election_questions_path(election)
-          # end
+          @form = form(SetupForm).from_params(params, election: election, trustee_ids: params[:setup][:trustee_ids])
         end
 
         private
@@ -39,11 +24,6 @@ module Decidim
 
         def election
           @election ||= elections.find_by(id: params[:id])
-        end
-
-        def trustees
-          trustees_space = TrusteesParticipatorySpace.where(participatory_space: current_participatory_space).includes(:trustee)
-          @trustees ||= Trustee.where(trustees_participatory_spaces: trustees_space).includes([:user]).page(params[:page]).per(15)
         end
       end
     end
