aa4c4474237c564fd74f7a019330648308b03ae1
Andrea Orler <andrea@codegram.com>
2020-10-19 09:47:27 +0200

    add test

--- decidim-core/spec/jobs/decidim/machine_translation_save_job_spec.rb
+++ decidim-core/spec/jobs/decidim/machine_translation_save_job_spec.rb
@@ -73,23 +73,45 @@ module Decidim
       let(:moderation) { create(:moderation, report_count: 2) }
       let!(:report) { create(:report, moderation: moderation) }
       let(:comment) { build(:comment, body: { "ca" => "Comentari" }, moderation: moderation) }
-      let(:target_locale) { "en" }
       let(:translated_value) { "Comment" }
 
-      it "sends emails to the moderators when the target language is the organization's default" do
-        allow(ReportedMailer).to receive(:send_report_notification_to_users).and_call_original
+      describe "when the target language is the organization's default" do
+        let(:target_locale) { "en" }
 
-        comment.save
-        MachineTranslationSaveJob.perform_now(
-          comment,
-          "body",
-          target_locale,
-          translated_value
-        )
+        it "sends emails to the moderators" do
+          allow(ReportedMailer).to receive(:send_report_notification_to_users).and_call_original
 
-        expect(ReportedMailer)
-          .to have_received(:send_report_notification_to_users)
-          .with(moderation.participatory_space.moderators, report)
+          comment.save
+          MachineTranslationSaveJob.perform_now(
+            comment,
+            "body",
+            target_locale,
+            translated_value
+          )
+
+          expect(ReportedMailer)
+            .to have_received(:send_report_notification_to_users)
+            .with(moderation.participatory_space.moderators, report)
+        end
+      end
+
+      describe "when the target language is NOT the organization's default" do
+        let(:target_locale) { "fi" }
+
+        it "doesn't send emails" do
+          allow(ReportedMailer).to receive(:send_report_notification_to_users).and_call_original
+
+          comment.save
+          MachineTranslationSaveJob.perform_now(
+            comment,
+            "body",
+            target_locale,
+            translated_value
+          )
+
+          expect(ReportedMailer)
+            .not_to have_received(:send_report_notification_to_users)
+        end
       end
     end
   end
