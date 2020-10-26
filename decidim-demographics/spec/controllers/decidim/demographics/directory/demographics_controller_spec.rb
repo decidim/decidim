c95ef674bcf33d3c0e2c8c6f6ea0b1af0ab3d05f
Cristian Georgescu <georgescu.cristi@gmail.com>
2020-10-26 14:55:06 +0200

    wip demographics_controller_spec

--- decidim-demographics/spec/controllers/decidim/demographics/directory/demographics_controller_spec.rb
+++ decidim-demographics/spec/controllers/decidim/demographics/directory/demographics_controller_spec.rb
@@ -8,18 +8,28 @@ describe Decidim::Demographics::Directory::DemographicsController, type: :contro
   let(:time_zone) { "UTC" }
   let(:organization) { create(:organization, time_zone: time_zone) }
   let(:participatory_process) { create :participatory_process, organization: organization }
-  let(:meeting_component) { create(:meeting_component, participatory_space: participatory_process) }
-
   let(:user) { create :user, :admin, :confirmed, organization: organization }
+
+  let(:demographic_form ) do
+    double(
+      gender: "male",
+      age: "< 15",
+      nationalities: [1,4],
+      postal_code: "111222",
+      background: "self-employed"
+    )
+  end
 
   before do
     request.env["decidim.current_organization"] = organization
     request.env["decidim.current_participatory_process"] = participatory_process
-    request.env["decidim.current_component"] = meeting_component
     sign_in user
   end
 
-  pending
+  it "tests new action" do
+    get :new
+    expect(demographic_form).to receive(:from_model)
+  end
   # new
   # create
 end
