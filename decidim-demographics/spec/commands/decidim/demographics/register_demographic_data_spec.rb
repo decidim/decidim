9761d6611f6449f1c3d198f8aa8b33b31e166507
Cristian Georgescu <georgescu.cristi@gmail.com>
2020-10-26 14:04:03 +0200

    demographic factory and model tests

--- decidim-demographics/spec/commands/decidim/demographics/register_demographic_data_spec.rb
+++ decidim-demographics/spec/commands/decidim/demographics/register_demographic_data_spec.rb
@@ -5,13 +5,40 @@ require "spec_helper"
 module Decidim
   module Demographics
     describe RegisterDemographicsData do
+      subject { described_class.new(demographic, form) }
+
+      let(:form) do
+        double(
+          invalid?: validity,
+          attributes: {
+            age: " < 15"
+          }
+        )
+      end
+      let(:demographic) do
+        double(
+          data: {
+            age: "< 15"
+          }
+        )
+      end
+
       describe "call" do
         describe "when invalid" do
-          pending
+          let(:validity) { true }
+
+          it "broadcasts invalid" do
+            expect { subject.call }.to broadcast(:invalid)
+          end
         end
 
         describe "when valid" do
-          pending
+          let(:validity) { false }
+
+          it "broadcasts ok" do
+            expect(demographic).to receive(:save!)
+            expect { subject.call }.to broadcast(:ok)
+          end
         end
       end
     end
