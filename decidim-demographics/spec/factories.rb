d00c36c91c8d0c2f8c978b1dc04381483f4b853a
Cristian Georgescu <georgescu.cristi@gmail.com>
2020-10-27 20:35:10 +0200

    tested

--- decidim-demographics/spec/factories.rb
+++ decidim-demographics/spec/factories.rb
@@ -1,20 +1,3 @@
 # frozen_string_literal: true
 
 require "decidim/demographics/test/factories"
-
-FactoryBot.define do
-  factory :data, class: OpenStruct do
-    send :'@type', "data"
-    age { Decidim::Demographics::Demographic::AGE_GROUPS.sample }
-    background { Decidim::Demographics::Demographic::PROFESSIONAL_CATEGORIES.sample }
-    gender { Decidim::Demographics::Demographic::AVAILABLE_GENDERS.sample }
-    nationality { Decidim::Demographics::Demographic::PROFESSIONAL_CATEGORIES.sample(Random.rand(1...3)) }
-    postal_code { Faker::Address.zip_code }
-  end
-
-  factory :demographic, class: "Decidim::Demographics::Demographic" do
-    user
-    organization
-    data
-  end
-end
--- decidim-demographics/spec/serializers/decidim/demographics/data_portability_demographic_serializer_spec.rb
+++ decidim-demographics/spec/serializers/decidim/demographics/data_portability_demographic_serializer_spec.rb
@@ -5,7 +5,7 @@ require "spec_helper"
 module Decidim
   module Demographics
     describe DataPortabilityDemographicSerializer do
-      let(:resource) { create(:demographic_component) }
+      let(:resource) { create(:encrypted_demographic) }
       let(:subject) { described_class.new(resource) }
 
       describe "#serialize" do
@@ -13,7 +13,11 @@ module Decidim
           serialized = subject.serialize
 
           expect(serialized).to be_a(Hash)
-          expect(serialized).to include(id)
+          expect(serialized).to include(:age)
+          expect(serialized).to include(:nationality)
+          expect(serialized).to include(:gender)
+          expect(serialized).to include(:postal_code)
+          expect(serialized).to include(:background)
         end
       end
     end
