95ec86fb212f96a71f722a3731fdf31dcbabc0da
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2020-10-28 12:54:58 +0100

    Avoid using File storage specific methods

--- decidim-assemblies/app/presenters/decidim/assemblies/assembly_presenter.rb
+++ decidim-assemblies/app/presenters/decidim/assemblies/assembly_presenter.rb
@@ -10,17 +10,11 @@ module Decidim
       delegate :url, to: :banner_image, prefix: true
 
       def hero_image_url
-        uri = URI(assembly.hero_image.file.file)
-        return uri unless uri.scheme.nil?
-
         URI.join(decidim.root_url(host: assembly.organization.host), assembly.hero_image_url).to_s
       end
 
       def banner_image_url
-        uri = URI(assembly.hero_image.file.file)
-        return uri unless uri.scheme.nil?
-
-        URI.join(decidim.root_url(host: assembly.organization.host), assembly.hero_image_url).to_s
+        URI.join(decidim.root_url(host: assembly.organization.host), assembly.banner_image_url).to_s
       end
 
       def assembly
