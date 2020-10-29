a38bc97602459b922663995c9f00428180fc0cba
slickepinne <svenjaschaefer@gmail.com>
2020-10-29 19:49:16 +0100

    admin handles answer selected

---
+++ decidim-elections/spec/commands/decidim/elections/admin/mark_answer_as_selected_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Decidim::Elections::Admin::MarkAnswerAsSelected do
+  subject(:command) { described_class.new(answer, user) }
+
+  let(:election) { create(:election, :published, :finished) }
+  let(:question) { create :question, :with_votes, election: election }
+  let(:answer) { question.answers.first }
+  let(:component) { election.component }
+  let(:organization) { component.organization }
+  let(:user) { create :user, :admin, :confirmed, organization: organization }
+
+  it "updates the selected answer attribute" do
+    subject.call
+    expect(answer.selected).to eq(true)
+  end
+
+  context "when answer has no votes" do
+    let(:answer) { create :election_answer }
+
+    it "is not valid" do
+      expect { subject.call }.to broadcast(:invalid)
+    end
+  end
+end
