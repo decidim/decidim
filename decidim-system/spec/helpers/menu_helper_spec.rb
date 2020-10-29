b448830b7d19d69283dd1b3c20f568e9c0ecbfb0
Alexandru-Emil Lupu <contact@alecslupu.ro>
2020-10-29 10:24:51 +0200

    Fixing menu issue

--- decidim-system/spec/helpers/menu_helper_spec.rb
+++ decidim-system/spec/helpers/menu_helper_spec.rb
@@ -2,28 +2,25 @@
 
 require "spec_helper"
 
-module Decidim
-  module System
-    describe MenuHelper do
-      describe "main_menu" do
-        let(:default_main_menu) { helper.main_menu.render }
+describe Decidim::System::MenuHelper do
 
-        it "renders the default main menu" do
-          expect(default_main_menu).to \
-            have_selector("li", count: 4) &
-            have_link("Dashboard", href: "/system/") &
-            have_link("Organizations", href: "/system/organizations") &
-            have_link("Admins", href: "/system/admins") &
-            have_link("OAuth applications", href: "/system/oauth_applications")
-        end
+  describe "main_menu" do
+    let(:default_main_menu) { helper.main_menu.render }
 
-        it "selects the correct default active option" do
-          allow(view).to \
-            receive(:params).and_return(controller: "decidim/system/dashboard", action: "show")
+    it "renders the default main menu" do
+      expect(default_main_menu).to \
+        have_selector("li", count: 4) &
+        have_link("Dashboard", href: "/system/") &
+        have_link("Organizations", href: "/system/organizations") &
+        have_link("Admins", href: "/system/admins") &
+        have_link("OAuth applications", href: "/system/oauth_applications")
+    end
 
-          expect(default_main_menu).to have_selector(".active", text: "Dashboard")
-        end
-      end
+    it "selects the correct default active option" do
+      allow(view).to \
+        receive(:params).and_return(controller: "decidim/system/dashboard", action: "show")
+
+      expect(default_main_menu).to have_selector(".active", text: "Dashboard")
     end
   end
 end
