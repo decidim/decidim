40116150bff484fef23edf021db026cdccdc7b39
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2020-10-30 14:24:35 +0100

    Add test

--- decidim-meetings/spec/cells/decidim/meetings/meeting_m_cell_spec.rb
+++ decidim-meetings/spec/cells/decidim/meetings/meeting_m_cell_spec.rb
@@ -3,7 +3,7 @@
 require "spec_helper"
 
 module Decidim::Meetings
-  describe MeetingCell, type: :cell do
+  describe MeetingMCell, type: :cell do
     controller Decidim::Meetings::MeetingsController
 
     let!(:meeting) { create(:meeting) }
@@ -17,6 +17,22 @@ module Decidim::Meetings
 
       it "renders the card" do
         expect(cell_html).to have_css(".card--meeting")
+      end
+    end
+
+    context "when title contains special html entities" do
+      let(:show_space) { true }
+
+      before do
+        @original_title = meeting.title["en"]
+        meeting.update!(title: { en: "#{meeting.title["en"]} &'<" })
+        meeting.reload
+      end
+
+      it "escapes them correclty" do
+        # as the `cell` test helper wraps conent in a Capybara artifact that already converts html entities
+        # we should compare with the expected visual result, as we were checking the DOM instead of the html
+        expect(cell_html).to have_content("#{@original_title} &'<")
       end
     end
   end
