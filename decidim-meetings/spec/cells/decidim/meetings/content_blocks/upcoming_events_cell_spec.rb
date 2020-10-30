d617593d6a823a3dc5010ef145011468cfd779eb
Ramon Costa <ramon.costa@gmail.com>
2020-10-30 11:28:42 +0100

    Fix in upcoming meetings show only visible meetings

--- decidim-meetings/spec/cells/decidim/meetings/content_blocks/upcoming_events_cell_spec.rb
+++ decidim-meetings/spec/cells/decidim/meetings/content_blocks/upcoming_events_cell_spec.rb
@@ -10,6 +10,8 @@ module Decidim
 
         let(:html) { cell("decidim/meetings/content_blocks/upcoming_events").call }
         let(:organization) { create(:organization) }
+        let(:current_user) { create :user, :admin, :confirmed, organization: organization }
+        let(:invited_by) { create(:user, :admin, :confirmed, organization: organization ) }
 
         before do
           expect(controller).to receive(:current_organization).at_least(:once).and_return(organization)
@@ -42,6 +44,46 @@ module Decidim
               expect(subject.length).to eq(2)
               expect(subject.first).to eq(meeting)
               expect(subject.last).to eq(second_meeting)
+            end
+
+            context "with upcoming private events" do
+              let!(:meeting) do
+                create(:meeting, start_time: 1.week.from_now, private_meeting: true, transparent: false)
+              end
+              let!(:second_meeting) do
+                create(:meeting, start_time: meeting.start_time.advance(weeks: 1), component: meeting.component, private_meeting: true, transparent: false)
+              end
+
+              it "renders nothing" do
+                expect(subject.length).to eq(0)
+              end
+            end
+
+            context "with upcoming private events but invited user" do
+              let!(:meeting) do
+                create(:meeting, start_time: 1.week.from_now, private_meeting: true, transparent: false)
+              end
+              let!(:second_meeting) do
+                create(:meeting, start_time: meeting.start_time.advance(weeks: 1), component: meeting.component, private_meeting: true, transparent: false)
+              end
+              let(:invite_form_params) do
+                {
+                  name: current_user.name,
+                  email: current_user.email,
+                  user_id: current_user.id,
+                  existing_user: true
+                }
+              end
+              let(:invite_user_form) do
+                Decidim::Meetings::Admin::MeetingRegistrationInviteForm.from_params(invite_form_params)
+              end
+              let!(:invite_user_to_meeting) do
+                Decidim::Meetings::Admin::InviteUserToJoinMeeting.call(invite_user_form, second_meeting, invited_by)
+              end
+
+              it "renders only upcoming not private meeting correctly" do
+                expect(subject.length).to eq(1)
+              end
             end
           end
         end
