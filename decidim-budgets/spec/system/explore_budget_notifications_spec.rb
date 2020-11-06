1272a02e5e973ac0b9852141e2b3bc89d81414eb
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-11-06 17:21:24 +0200

    Fix broken notifications page due to multi-budget changes (#6815)
    * Fix event paths/urls for resources with polymorphic paths
    
    * Add a test for the budgeting project comment notifications

---
+++ decidim-budgets/spec/system/explore_budget_notifications_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Explore budget notifications", :slow, type: :system do
+  include_context "with a component"
+  let(:manifest_name) { "budgets" }
+  let(:budget) { create :budget, component: component }
+  let(:projects_count) { 5 }
+  let!(:projects) do
+    create_list(:project, projects_count, budget: budget)
+  end
+  let!(:project) { projects.first }
+
+  describe "index" do
+    context "when a budgeting project was commented in a followed space" do
+      let(:user) { create(:user, :confirmed, organization: component.organization) }
+      let(:other_user) { create(:user, :confirmed, organization: component.organization) }
+
+      before do
+        switch_to_host(organization.host)
+        login_as(user, scope: :user)
+
+        # Create a notification for the follower
+        create(:follow, followable: component.participatory_space, user: user)
+        comment = create(:comment, commentable: project, author: other_user)
+        perform_enqueued_jobs do
+          Decidim::Comments::NewCommentNotificationCreator.new(comment, [], []).create
+        end
+      end
+
+      it "displays the notification for the comment" do
+        visit_notifications
+
+        within "#notifications" do
+          expect(page).to have_content(translated(project.title))
+        end
+      end
+    end
+  end
+
+  private
+
+  def visit_notifications
+    page.visit decidim.notifications_path
+  end
+end
