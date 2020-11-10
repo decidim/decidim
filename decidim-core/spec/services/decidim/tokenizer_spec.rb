ef7a5766cec135cd3a7d9159d213fd543a196cd6
Ivan Vergés <ivan@platoniq.net>
2020-11-10 17:42:46 +0100

    add tokenizer class for generating tokens

---
+++ decidim-core/spec/services/decidim/tokenizer_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Decidim::Tokenizer do
+  subject { described_class.new(salt: secret, length: length) }
+
+  let(:secret) { "secret" }
+  let(:length) { 10 }
+  let(:string) { "Poems everybody!" }
+
+  describe "digest" do
+    it "generates an integer key" do
+      expect(subject.int_digest(string)).to be_a(Integer)
+      expect(subject.int_digest(string).size).to eq(10)
+    end
+
+    it "generates an hexadecimal key" do
+      expect(subject.hex_digest(string)).to be_a(String)
+      expect(subject.hex_digest(string).size <= 20).to eq(true)
+    end
+
+    it "generates random secrets" do
+      expect(described_class.random_salt).not_to eq(described_class.random_salt)
+      expect(described_class.random_salt.size).to eq(64)
+    end
+
+    context "when the size is different" do
+      let(:length) { 15 }
+
+      it "generates a digest of the propser size" do
+        expect(subject.int_digest(string).size).to eq(15)
+      end
+    end
+  end
+end
