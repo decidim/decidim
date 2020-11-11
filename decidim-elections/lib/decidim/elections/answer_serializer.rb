567c7e2203d988680477457dce20c1fe271390c6
slickepinne <svenjaschaefer@gmail.com>
2020-11-11 12:36:02 +0100

    create answer serializer

---
+++ decidim-elections/lib/decidim/elections/answer_serializer.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Elections
+    # This class serializes an Answer so it can be exported to CSV, JSON or other
+    # formats.
+    class AnswerSerializer < Decidim::Exporters::Serializer
+      include Decidim::ApplicationHelper
+      include Decidim::ResourceHelper
+      include Decidim::TranslationsHelper
+
+      # Public: Initializes the serializer with an answer.
+      def initialize(answer)
+        @answer = answer
+      end
+
+      # Public: Exports a hash with the serialized data for this answer.
+      def serialize
+        {
+          id: answer.id,
+          answer_title: answer.title,
+          answer_votes: answer.votes,
+          election_id: election.id,
+          election_title: election.title,
+          question_id: question.id,
+          question_title: question.title
+        }
+      end
+
+      private
+
+      attr_reader :answer
+
+      def election
+        answer.question.election
+      end
+
+      def question
+        answer.question
+      end
+
+      def organization
+        answer.question.election.component.organization
+      end
+    end
+  end
+end
