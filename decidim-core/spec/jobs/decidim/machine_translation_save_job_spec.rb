98a27097d17460b015c2eaf65b2bdcf9eae0f11c
Andrea Orler <orlera@users.noreply.github.com>
2020-11-16 09:39:39 +0100

    Send report email when reported resource is translated to default org language (#6726)
    * feat: add extra columns to the moderation panel
    
    * feat: sort moderations by report count and creation date
    
    * test: prevent random failures not using random numbers
    
    * feat: filter moderations by reportable type
    
    * chore: add reported_content to moderations migration
    
    * feat: filter moderations by reportable id or reported content
    
    * feat: update reported content when a report is created
    
    * feat: add pagination links
    
    * chore: fix some minor problems
    
    * refactor: simplify Reportable concern API methods
    
    * fix: rubocop issues and test using old method
    
    * feat: store report's locale
    
    * feat: add reported content cell
    
    * chore(proposals): add reported content cell
    
    * chore(meetings): add reported content cell
    
    * chore(debates): add reported content cell
    
    * feat: add reports index and show for moderations panel
    
    * chore: rubocop and eslint fixes
    
    * chore: normalize decidim-admin locales
    
    * chore: fix some erb linter problems
    
    * fix: codeclimate issues
    
    * refactor: remove reported content column from reports list
    
    * feat: render reportable authors with conversation links
    
    * feat: show reportable excerpt on hover
    
    * chore: fix codeclimate issues
    
    * send email when a reported content is translated to the default language
    
    * fix: send email to moderators
    
    Co-authored-by: David Morcillo <david.morcillo@gmail.com>

--- decidim-core/spec/jobs/decidim/machine_translation_save_job_spec.rb
+++ decidim-core/spec/jobs/decidim/machine_translation_save_job_spec.rb
@@ -68,5 +68,76 @@ module Decidim
         )
       end
     end
+
+    describe "when the resource is reported" do
+      let(:organization) { create(:organization) }
+      let(:participatory_space) { create(:participatory_process, organization: organization) }
+      let!(:moderators) { create_list(:user, 3, :admin, organization: organization) }
+      let(:moderation) { create(:moderation, report_count: 2, participatory_space: participatory_space) }
+      let!(:report) { create(:report, moderation: moderation) }
+      let(:title) { { "ca" => "Títol" } }
+      let(:proposal) { build(:proposal, title: title, body: { "ca" => "Proposta" }, moderation: moderation) }
+      let(:translated_value) { "Proposal" }
+
+      describe "and the target language is the organization's default" do
+        let(:target_locale) { "en" }
+
+        describe "and the resource is completely translated" do
+          let(:title) { { "ca" => "Títol", "machine_translations" => { "en" => "Title" } } }
+
+          it "sends emails to the moderators" do
+            allow(ReportedMailer).to receive(:report).and_call_original
+
+            proposal.save
+            MachineTranslationSaveJob.perform_now(
+              proposal,
+              "body",
+              target_locale,
+              translated_value
+            )
+
+            expect(ReportedMailer)
+              .to have_received(:report)
+              .exactly(moderation.participatory_space.moderators.count).times
+          end
+        end
+
+        describe "and the resource is NOT completely translated" do
+          it "doesn't send emails" do
+            allow(ReportedMailer).to receive(:report).and_call_original
+
+            proposal.save
+            MachineTranslationSaveJob.perform_now(
+              proposal,
+              "body",
+              target_locale,
+              translated_value
+            )
+
+            expect(ReportedMailer)
+              .not_to have_received(:report)
+          end
+        end
+      end
+
+      describe "and the target language is NOT the organization's default" do
+        let(:target_locale) { "fi" }
+
+        it "doesn't send emails" do
+          allow(ReportedMailer).to receive(:report).and_call_original
+
+          proposal.save
+          MachineTranslationSaveJob.perform_now(
+            proposal,
+            "body",
+            target_locale,
+            translated_value
+          )
+
+          expect(ReportedMailer)
+            .not_to have_received(:report)
+        end
+      end
+    end
   end
 end
