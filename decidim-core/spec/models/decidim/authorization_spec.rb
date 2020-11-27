7300cb218e7f95bf6f5efc5ca0907d5db540bdfb
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-11-27 17:58:37 +0200

    Add specs for authorization metadata encryption

--- decidim-core/spec/models/decidim/authorization_spec.rb
+++ decidim-core/spec/models/decidim/authorization_spec.rb
@@ -31,5 +31,123 @@ module Decidim
         expect(authorization.metadata_cell).to be_kind_of String
       end
     end
+
+    context "with metadata" do
+      let!(:authorization) { create(:authorization, :granted, metadata: { foo: "bar" }) }
+      let(:database_metadata) do
+        JSON.parse(
+          ActiveRecord::Base.connection.select_one(
+            "SELECT metadata FROM #{described_class.table_name} WHERE id = #{authorization.id}"
+          )["metadata"]
+        )
+      end
+      let(:expected_foo) { "bar" }
+
+      shared_examples_for "encrypted authorization metadata" do
+        it "encrypts the metadata to the database" do
+          expect(
+            Decidim::AttributeEncryptor.decrypt(database_metadata["foo"])
+          ).to eq(expected_foo)
+        end
+
+        it "decrypts the final values automatically" do
+          final = Decidim::Authorization.find(authorization.id)
+          expect(final.metadata["foo"]).to eq(expected_foo)
+        end
+      end
+
+      it_behaves_like "encrypted authorization metadata"
+
+      context "when storing with update!" do
+        let(:expected_foo) { "baz" }
+
+        before do
+          authorization.update!(metadata: { foo: "baz" })
+        end
+
+        it_behaves_like "encrypted authorization metadata"
+      end
+
+      context "when storing with attribute update" do
+        let(:expected_foo) { "biz" }
+
+        before do
+          authorization.metadata = { foo: "biz" }
+          authorization.save!
+        end
+
+        it_behaves_like "encrypted authorization metadata"
+      end
+
+      context "when storing with attributes=" do
+        let(:expected_foo) { "buz" }
+
+        before do
+          authorization.attributes = { metadata: { foo: "buz" } }
+          authorization.save!
+        end
+
+        it_behaves_like "encrypted authorization metadata"
+      end
+    end
+
+    context "with verification metadata" do
+      let!(:authorization) { create(:authorization, :pending, verification_metadata: { foo: "bar" }) }
+      let(:database_metadata) do
+        JSON.parse(
+          ActiveRecord::Base.connection.select_one(
+            "SELECT verification_metadata FROM #{described_class.table_name} WHERE id = #{authorization.id}"
+          )["verification_metadata"]
+        )
+      end
+      let(:expected_foo) { "bar" }
+
+      shared_examples_for "encrypted authorization verification metadata" do
+        it "encrypts the metadata to the database" do
+          expect(
+            Decidim::AttributeEncryptor.decrypt(database_metadata["foo"])
+          ).to eq(expected_foo)
+        end
+
+        it "decrypts the final values automatically" do
+          final = Decidim::Authorization.find(authorization.id)
+          expect(final.verification_metadata["foo"]).to eq(expected_foo)
+        end
+      end
+
+      it_behaves_like "encrypted authorization verification metadata"
+
+      context "when storing with update!" do
+        let(:expected_foo) { "baz" }
+
+        before do
+          authorization.update!(verification_metadata: { foo: "baz" })
+        end
+
+        it_behaves_like "encrypted authorization verification metadata"
+      end
+
+      context "when storing with attribute update" do
+        let(:expected_foo) { "biz" }
+
+        before do
+          authorization.verification_metadata = { foo: "biz" }
+          authorization.save!
+        end
+
+        it_behaves_like "encrypted authorization verification metadata"
+      end
+
+      context "when storing with attributes=" do
+        let(:expected_foo) { "buz" }
+
+        before do
+          authorization.attributes = { verification_metadata: { foo: "buz" } }
+          authorization.save!
+        end
+
+        it_behaves_like "encrypted authorization verification metadata"
+      end
+    end
   end
 end
