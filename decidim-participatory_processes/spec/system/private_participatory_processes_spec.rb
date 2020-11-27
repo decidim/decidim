3eda98a63e6eeddcea587bc9d84b3eab7cae64c5
Eduardo Martínez <eduardomech@gmail.com>
2020-11-27 21:30:22 +0100

    Add system test for registrations with share token

--- decidim-participatory_processes/spec/system/private_participatory_processes_spec.rb
+++ decidim-participatory_processes/spec/system/private_participatory_processes_spec.rb
@@ -12,6 +12,7 @@ describe "Private Participatory Processes", type: :system do
   let!(:other_user_2) { create :user, :confirmed, organization: organization }
   let!(:participatory_space_private_user) { create :participatory_space_private_user, user: other_user, privatable_to: private_participatory_process }
   let!(:participatory_space_private_user_2) { create :participatory_space_private_user, user: other_user_2, privatable_to: private_participatory_process }
+  let!(:share_token) { create :share_token, user: admin, organization: organization, token_for: private_participatory_process }
 
   context "when there are private participatory processes" do
     context "and no user is loged in" do
@@ -99,6 +100,46 @@ describe "Private Participatory Processes", type: :system do
 
         expect(page).to have_current_path decidim_participatory_processes.participatory_process_path(private_participatory_process)
         expect(page).to have_content "This is a private process"
+      end
+    end
+
+    context "when a private participatory space has a valid share token and the sign up path is used with the token" do
+      let(:sign_in_path) { decidim.new_user_registration_path(share_token: share_token.token) }
+
+      before do
+        switch_to_host(organization.host)
+      end
+
+      context "and no user is logged in" do
+        it "redirects new registered user to space as private user" do
+          visit sign_in_path
+
+          fill_in :registration_user_name, with: "Nikola Tesla"
+          fill_in :registration_user_nickname, with: "the-greatest-genius-in-history"
+          fill_in :registration_user_email, with: "nikola.tesla@example.org"
+          fill_in :registration_user_password, with: "sekritpass123"
+          fill_in :registration_user_password_confirmation, with: "sekritpass123"
+          page.check("registration_user_newsletter")
+          page.check("registration_user_tos_agreement")
+          within "form.new_user" do
+            find("*[type=submit]").click
+          end
+
+          new_user = organization.users.last
+
+          expect(page).to have_current_path decidim_participatory_processes.participatory_process_path(private_participatory_process)
+          expect(private_participatory_process.users).to include(new_user)
+        end
+      end
+
+      context "when user is loged in and is not a participatory space private user" do
+        it "redirects new registered user to space as private user" do
+          login_as user, scope: :user
+          visit sign_in_path
+
+          expect(page).to have_current_path decidim_participatory_processes.participatory_process_path(private_participatory_process)
+          expect(private_participatory_process.users).to include(user)
+        end
       end
     end
   end
