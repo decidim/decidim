af6e35fe2b6ddd8dbe1c4df04b7305757f20d14e
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-11-29 19:08:43 +0200

    Add specs to check for public access pages visibility

--- decidim-core/spec/controllers/pages_controller_spec.rb
+++ decidim-core/spec/controllers/pages_controller_spec.rb
@@ -13,9 +13,7 @@ module Decidim
         request.env["decidim.current_organization"] = organization
       end
 
-      context "when a page exists" do
-        let(:page) { create(:static_page, organization: organization) }
-
+      shared_examples "accessible static page" do
         render_views
 
         it "renders the page contents" do
@@ -26,6 +24,12 @@ module Decidim
           expect(response.body).to include(page.title[I18n.locale.to_s])
           expect(response.body).to include(page.content[I18n.locale.to_s])
         end
+      end
+
+      context "when a page exists" do
+        let(:page) { create(:static_page, organization: organization) }
+
+        it_behaves_like "accessible static page"
 
         context "when asking the page in other formats" do
           it "ignores them" do
@@ -40,6 +44,50 @@ module Decidim
         it "redirects to the 404" do
           expect { get :show, params: { id: "some-page" } }
             .to raise_error(ActiveRecord::RecordNotFound)
+        end
+      end
+
+      context "when organization forces users to authenticate before access" do
+        before do
+          organization.force_users_to_authenticate_before_access_organization = true
+          organization.save
+        end
+
+        context "with a publicly accessible page" do
+          let(:page) { create(:static_page, organization: organization, allow_public_access: true) }
+
+          it_behaves_like "accessible static page"
+        end
+
+        context "with a publicly hidden page" do
+          let(:page) { create(:static_page, organization: organization, allow_public_access: false) }
+
+          it "redirects to sign in path" do
+            get :show, params: { id: page.slug }
+
+            expect(response).to redirect_to("/users/sign_in")
+            expect(flash[:warning]).to include("Please, login with your account before access")
+          end
+        end
+
+        context "when authenticated" do
+          let!(:user) { create :user, :confirmed, organization: organization }
+
+          before do
+            sign_in user
+          end
+
+          context "with a publicly accessible page" do
+            let(:page) { create(:static_page, organization: organization, allow_public_access: true) }
+
+            it_behaves_like "accessible static page"
+          end
+
+          context "with a publicly hidden page" do
+            let(:page) { create(:static_page, organization: organization, allow_public_access: false) }
+
+            it_behaves_like "accessible static page"
+          end
         end
       end
     end
