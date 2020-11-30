b5c33564b3f53d63676bc8f0d99eb5b6cc7d5397
Eduardo Martínez <eduardomech@gmail.com>
2020-11-30 13:56:02 +0100

    Register highlighted_meetings content block in participatory process groups homepage

--- decidim-participatory_processes/lib/decidim/participatory_processes/engine.rb
+++ decidim-participatory_processes/lib/decidim/participatory_processes/engine.rb
@@ -147,6 +147,12 @@ module Decidim
             settings.attribute :order, type: :enum, default: "random", choices: %w(random recent)
           end
         end
+
+        Decidim.content_blocks.register(:participatory_process_group_homepage, :highlighted_meetings) do |content_block|
+          content_block.cell = "decidim/meetings/content_blocks/highlighted_meetings"
+          content_block.public_name_key = "decidim.meetings.content_blocks.upcoming_events.name"
+          content_block.default!
+        end
       end
 
       initializer "decidim_participatory_processes.stats" do
