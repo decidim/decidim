d87be7fa7afe2b6b6f292347c4a964be0a84ab75
roxana.oprescu <roxana.oprescu@tremend.com>
2020-12-04 14:52:54 +0200

    Remove comment

--- decidim-core/app/models/decidim/user_suspension.rb
+++ decidim-core/app/models/decidim/user_suspension.rb
@@ -6,7 +6,5 @@ module Decidim
 
     belongs_to :user, class_name: "Decidim::User", foreign_key: :decidim_user_id
     belongs_to :suspending_user, class_name: "Decidim::User"
-
-    # validates :justification, presence: true, length: { minimum: MINIMUM_JUSTIFICATION_LENGTH }
   end
 end
