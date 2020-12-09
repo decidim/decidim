488fdbc34c01e34e75d53e3b95d77b844a7cdfa0
Eduardo Martínez <eduardomech@gmail.com>
2020-12-09 13:45:09 +0100

    Improvements in Process Groups and processes block (#6853)
    * Make groups editable for admins
    
    * Add test to check groups editable for admins
    * Add admin edit link to group landing page
    
    * Update info.html.erb
    
    * Add a link to group in the process page if present
    
    * Add migration to include promoted flag in process groups
    
    * Add promoted flag management of groups from admin panel
    
    * Display promoted process groups if present in processes index
    
    * Display cta on higlighted groups if there is an associated content block defined in the group landing page
    
    * Add system tests for highlighted groups section in processes index
    
    * Include promoted groups in highlighted processes section
    
    It removes the promoted groups section and merge the promoted processes
    and groups in the same section for promoted processes
    
    * Fix test and include example
    
    * Fix query to return groups when current_user is admin
    
    * Extract views to cards for featured processes in homepage content block
    
    * Include promoted groups in highlighted processes content block
    
    * Reuse big partial from processes index if there is only a highlighted group
    
    * Add tests
    
    Co-authored-by: Oliver Valls <199462+tramuntanal@users.noreply.github.com>

--- decidim-participatory_processes/app/cells/decidim/participatory_processes/content_blocks/highlighted_processes_cell.rb
+++ decidim-participatory_processes/app/cells/decidim/participatory_processes/content_blocks/highlighted_processes_cell.rb
@@ -4,32 +4,33 @@ module Decidim
   module ParticipatoryProcesses
     module ContentBlocks
       class HighlightedProcessesCell < Decidim::ViewModel
+        include Decidim::ApplicationHelper
         include Decidim::SanitizeHelper
+        include Decidim::CardHelper
+        include Cell::ViewModel::Partial
+        include ParticipatoryProcessHelper
+        include Decidim::ParticipatoryProcesses::Engine.routes.url_helpers
 
         delegate :current_user, to: :controller
 
         def show
-          if single_process?
+          if single_item?
             render "single_process"
-          elsif highlighted_processes.any?
+          elsif highlighted_items.any?
             render
           end
         end
 
-        def single_process?
-          highlighted_processes.to_a.length == 1
+        def single_item?
+          highlighted_items.length == 1
         end
 
         def max_results
           model.settings.max_results
         end
 
-        def highlighted_processes
-          @highlighted_processes ||= (
-            OrganizationPublishedParticipatoryProcesses.new(current_organization, current_user) |
-            HighlightedParticipatoryProcesses.new |
-            FilteredParticipatoryProcesses.new("active")
-          ).query.includes([:organization]).limit(max_results)
+        def highlighted_items
+          @highlighted_items ||= promoted_groups + highlighted_processes
         end
 
         def i18n_scope
@@ -38,6 +39,28 @@ module Decidim
 
         def decidim_participatory_processes
           Decidim::ParticipatoryProcesses::Engine.routes.url_helpers
+        end
+
+        private
+
+        def highlighted_processes
+          @highlighted_processes ||= if highlighted_processes_max_results.zero?
+                                       []
+                                     else
+                                       (
+                                         OrganizationPublishedParticipatoryProcesses.new(current_organization, current_user) |
+                                         HighlightedParticipatoryProcesses.new |
+                                         FilteredParticipatoryProcesses.new("active")
+                                       ).query.includes([:organization]).limit(highlighted_processes_max_results)
+                                     end
+        end
+
+        def promoted_groups
+          @promoted_groups ||= (OrganizationParticipatoryProcessGroups.new(current_organization) | PromotedParticipatoryProcessGroups.new).query.limit(max_results)
+        end
+
+        def highlighted_processes_max_results
+          @highlighted_processes_max_results ||= max_results - promoted_groups.count
         end
       end
     end
