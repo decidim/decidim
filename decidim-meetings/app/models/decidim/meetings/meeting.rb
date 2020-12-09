35aee56c0c97864aab1421add6c42ffdafef09ca
ivan-mr <ivan.mr@coditramuntana.com>
2020-12-09 13:08:18 +0100

    Fix visible_meeting_for to increase cases where an user can see meetings (#6925)
    * Fix visible_meeting_for to increase cases where an user can see meetings
    
    * Fix visible_meeting_for to get polymorphic participatory spaces user roles and participatory_space_private_users
    
    * Fix codeclimate hard tabs
    
    * Fix when there's no user on visible_meeting_for scope
    
    * Adding decidim_meetings_registrations to visible_for scope
    
    * Modify test to execute with a normal user and not and admin user who can see all meetings
    
    * Allow access to private meetings to admins, process admins and private participants
    
    * Admins can not participate in participatory_spaces
    
    * Simplify `current_user_can_visit_meeting?(user)` method
    
    * Rubocopify
    
    * Make test succeed
    
    * Tests for meeting access visibility
    
    * Fix visible_meeting_for scope when participatory_space is not defined
    
    * Change current_user_visit_meeting? method to improve performance
    
    * Refactor begin rescue block on visible_meeting_for scope
    
    * Change of append function on an array to look in a better way
    
    Co-authored-by: Oliver Valls <199462+tramuntanal@users.noreply.github.com>

--- decidim-meetings/app/models/decidim/meetings/meeting.rb
+++ decidim-meetings/app/models/decidim/meetings/meeting.rb
@@ -45,11 +45,40 @@ module Decidim
       scope :upcoming, -> { where(arel_table[:end_time].gteq(Time.current)) }
 
       scope :visible_meeting_for, lambda { |user|
-                                    joins("LEFT JOIN decidim_meetings_registrations ON
-                                    decidim_meetings_registrations.decidim_meeting_id = #{table_name}.id")
-                                      .where("(private_meeting = ? and decidim_meetings_registrations.decidim_user_id = ?)
-                                    or private_meeting = ? or (private_meeting = ? and transparent = ?)", true, user, false, true, true).distinct
-                                  }
+        (all.distinct if user&.admin?) ||
+          if user.present?
+            spaces = %w(assembly participatory_process)
+            spaces << "conference" if defined?(Decidim::Conference)
+            user_role_queries = spaces.map do |participatory_space_name|
+              "SELECT decidim_components.id FROM decidim_components
+              WHERE CONCAT(decidim_components.participatory_space_id, '-', decidim_components.participatory_space_type)
+              IN
+              (SELECT CONCAT(decidim_#{participatory_space_name}_user_roles.decidim_#{participatory_space_name}_id, '-Decidim::#{participatory_space_name.classify}')
+              FROM decidim_#{participatory_space_name}_user_roles WHERE decidim_#{participatory_space_name}_user_roles.decidim_user_id = ?)
+              "
+            end
+
+            where("decidim_meetings_meetings.private_meeting = ?
+            OR decidim_meetings_meetings.transparent = ?
+            OR decidim_meetings_meetings.id IN
+              (SELECT decidim_meetings_registrations.decidim_meeting_id FROM decidim_meetings_registrations WHERE decidim_meetings_registrations.decidim_user_id = ?)
+            OR decidim_meetings_meetings.decidim_component_id IN
+              (SELECT decidim_components.id FROM decidim_components
+                WHERE CONCAT(decidim_components.participatory_space_id, '-', decidim_components.participatory_space_type)
+                IN
+                  (SELECT CONCAT(decidim_participatory_space_private_users.privatable_to_id, '-', decidim_participatory_space_private_users.privatable_to_type)
+                  FROM decidim_participatory_space_private_users WHERE decidim_participatory_space_private_users.decidim_user_id = ?)
+              )
+            OR decidim_meetings_meetings.decidim_component_id IN
+              (
+                #{user_role_queries.compact.join(" UNION ")}
+              )
+            ", false, true, user.id, user.id, *user_role_queries.compact.map { user.id })
+              .distinct
+          else
+            visible
+          end
+      }
 
       scope :visible, -> { where("decidim_meetings_meetings.private_meeting != ? OR decidim_meetings_meetings.transparent = ?", true, true) }
 
@@ -153,9 +182,8 @@ module Decidim
         can_participate_in_space?(user) && can_participate_in_meeting?(user)
       end
 
-      def current_user_can_visit_meeting?(current_user)
-        (private_meeting? && registrations.exists?(decidim_user_id: current_user.try(:id))) ||
-          !private_meeting? || (private_meeting? && transparent?)
+      def current_user_can_visit_meeting?(user)
+        Decidim::Meetings::Meeting.visible_meeting_for(user).exists?(id: id)
       end
 
       # Return the duration of the meeting in minutes
