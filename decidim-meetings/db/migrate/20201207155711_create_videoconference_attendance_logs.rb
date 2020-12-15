48c154d54e4adc5a920e3731d82dc5e56f7b6ff1
Vera Rojman <vrojman@protonmail.com>
2020-12-15 22:48:02 +0100

    Make rubocop happy

--- decidim-meetings/db/migrate/20201207155711_create_videoconference_attendance_logs.rb
+++ decidim-meetings/db/migrate/20201207155711_create_videoconference_attendance_logs.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateVideoconferenceAttendanceLogs < ActiveRecord::Migration[5.2]
   def change
     create_table :decidim_meetings_videoconference_attendance_logs do |t|
