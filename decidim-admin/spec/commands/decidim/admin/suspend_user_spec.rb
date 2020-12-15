108870485d7c375e635bec3015ae8ec41661523e
roxana.oprescu <roxana.oprescu@tremend.com>
2020-12-15 18:40:57 +0200

    Add more test cases for suspend_user.rb

--- decidim-admin/spec/commands/decidim/admin/suspend_user_spec.rb
+++ decidim-admin/spec/commands/decidim/admin/suspend_user_spec.rb
@@ -8,7 +8,7 @@ module Decidim::Admin
 
     let(:organization) { create :organization }
     let(:current_user) { create :user, :admin, organization: organization }
-    let(:suspendable) { create :user, :managed, organization: organization }
+    let(:suspendable) { create :user, :managed, name: "Testingname", organization: organization }
     let(:justification) { "justification for suspending the user" }
     let(:user_suspension) { create :justification, :user, :current_user }
 
@@ -24,6 +24,18 @@ module Decidim::Admin
 
       it "broadcasts ok" do
         expect { subject.call }.to broadcast(:ok, suspendable)
+      end
+
+      it "user is notified" do
+        subject.call
+        expect(Decidim::UserSuspensionJob).to have_been_enqueued.on_queue("user_suspension")
+      end
+
+      it "user is updated" do
+        subject.call
+        expect(form.user.suspended).to be(true)
+        expect(form.user.extended_data["user_name"]).to eq("Testingname")
+        expect(form.user.name).to eq("Blocked user")
       end
 
       it "tracks the changes" do
