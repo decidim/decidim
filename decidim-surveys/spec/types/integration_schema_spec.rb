f7895bf34d89d991b5756336172d27e579042b89
Alexandru-Emil Lupu <contact@alecslupu.ro>
2020-12-19 08:59:20 +0200

    Enhance test on survey

--- decidim-surveys/spec/types/integration_schema_spec.rb
+++ decidim-surveys/spec/types/integration_schema_spec.rb
@@ -22,7 +22,26 @@ describe "Decidim::Api::QueryType" do
         "description" => { "translation" => survey.questionnaire.description[locale] },
         "forType" => "Decidim::Surveys::Survey",
         "id" => survey.questionnaire.id.to_s,
-        "questions" => survey.questionnaire.questions.map { |q| { "id" => q.id.to_s } },
+        "questions" => survey.questionnaire.questions.map do |q|
+          {
+            "answerOptions" => q.answer_options.map do |a|
+              {
+                "body"=>{"translation"=>a.body[locale]},
+                "freeText"=>a.free_text?,
+                "id"=>a.id.to_s
+              }
+            end,
+            "body"=>{"translation"=> q.body[locale]},
+            "createdAt" => q.created_at.iso8601.to_s.gsub("Z", "+00:00"),
+            "description"=>{"translation"=> q.description[locale]},
+            "id" => q.id.to_s,
+            "mandatory"=>q.mandatory?,
+            "maxChoices"=>q.max_choices,
+            "position"=>q.position,
+            "questionType"=>q.question_type,
+            "updatedAt"=>q.updated_at.iso8601.to_s.gsub("Z", "+00:00"),
+          }
+        end,
         "title" => { "translation" => survey.questionnaire.title[locale] },
         "tos" => { "translation" => survey.questionnaire.tos[locale] },
         "updatedAt" => survey.questionnaire.updated_at.iso8601.to_s.gsub("Z", "+00:00")
@@ -68,11 +87,22 @@ describe "Decidim::Api::QueryType" do
                 forType
                 id
                 questions {
+                  answerOptions {
+                    id
+                    body { translation(locale:"#{locale}") }
+                    freeText
+                  }
+                  body { translation(locale:"#{locale}") }
+                  createdAt
+                  description { translation(locale:"#{locale}") }
                   id
+                  mandatory
+                  maxChoices
+                  position
+                  questionType
+                  updatedAt
                 }
-                title {
-                  translation(locale:"#{locale}")
-                }
+                title { translation(locale:"#{locale}") }
                 tos {
                   translation(locale:"#{locale}")
                 }
@@ -112,7 +142,20 @@ describe "Decidim::Api::QueryType" do
             forType
             id
             questions {
+              answerOptions {
+                id
+                body { translation(locale:"#{locale}") }
+                freeText
+              }
+              body { translation(locale:"#{locale}") }
+              createdAt
+              description { translation(locale:"#{locale}") }
               id
+              mandatory
+              maxChoices
+              position
+              questionType
+              updatedAt
             }
             title {
               translation(locale:"#{locale}")
