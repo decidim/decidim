ff98796f25c44215160f6634df038869fe6132b6
Leonardo Diez <leiodd@gmail.com>
2020-12-20 16:11:27 +0100

    feat: add scheduled tasks rake for open and close the ballot box

---
+++ decidim-elections/spec/lib/tasks/decidim_election_scheduled_tasks_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "decidim_elections:scheduled_tasks", type: :task do
+  it "runs gracefully" do
+    expect { task.execute }.not_to raise_error
+  end
+
+  context "with elections to open", :vcr do
+    let!(:election) { create :election, :ready, :bb_test_election }
+
+    before { task.execute }
+
+    it "opens the Ballot Box" do
+      check_message_printed("Opening Election ##{election.id}:")
+      check_message_printed("Ballot Box opened. New bulletin board status: vote")
+    end
+  end
+
+  context "with elections to close", :vcr do
+    let!(:election) { create :election, :vote, :finished, :bb_test_election }
+
+    before { task.execute }
+
+    it "closes the Ballot Box" do
+      check_message_printed("Closing Election ##{election.id}:")
+      check_message_printed("Ballot Box closed. New bulletin board status: tally")
+    end
+  end
+
+  context "with elections that shouldn't be affected" do
+    let!(:election1) { create :election, :ready, start_time: 1.day.from_now }
+    let!(:election2) { create :election, :vote }
+
+    before { task.execute }
+
+    it "don't modify them" do
+      expect(election1.reload).to be_bb_ready
+      expect(election2.reload).to be_bb_vote
+    end
+  end
+end
