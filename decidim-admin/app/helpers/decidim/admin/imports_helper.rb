89885a129a99b887881954cabc7840bec8221ffa
lahdeero <eero.lahdenpera@gmail.com>
2020-12-23 17:36:01 +0200

    Class/method descriptions and helper test

--- decidim-admin/app/helpers/decidim/admin/imports_helper.rb
+++ decidim-admin/app/helpers/decidim/admin/imports_helper.rb
@@ -3,21 +3,31 @@
 module Decidim
   module Admin
     module ImportsHelper
-      # Renders an import dropdown for the provided component
+      # Renders an import dropdown for the provided component. Additional dropdown items
+      # can be given as block.
       #
       # component - The component to render the export dropdown for. Defaults to the
       #           current component.
       #
       # Returns a rendered dropdown.
       def import_dropdown(component = current_component, resource_id = nil)
-        locals = block_given? ? { component: component, resource_id: resource_id, block: yield } : { component: component, resource_id: resource_id }
+        locals = { component: component, resource_id: resource_id }
+        locals[:block] = yield if block_given?
         render partial: "decidim/admin/imports/dropdown", locals: locals
       end
 
+      # Routes to the correct importer for a component.
+      #
+      # component - The component to be routed.
+      # options - Extra options that need to be passed to the route.
+      #
+      # Returns the path to the component importer.
       def admin_imports_path(component, options)
         EngineRouter.admin_proxy(component.participatory_space).new_component_imports_path(options.merge(component_id: component))
       end
 
+      # Public: A formatted collection of mime_type to be used
+      # in forms.
       def mime_types
         types = ""
         accepted_mime_types = Decidim::Admin::Import::Readers::ACCEPTED_MIME_TYPES.keys
@@ -28,6 +38,11 @@ module Decidim
         types
       end
 
+      # Renders a user_group select field in a form.
+      # form - FormBuilder object
+      # name - attribute user_group_id
+      #
+      # Returns nothing.
       def user_group_select_field(form, name)
         selected = @import.user_group_id.presence
         user_groups = Decidim::UserGroups::ManageableUserGroups.for(current_user).verified
---
+++ decidim-admin/spec/helpers/imports_helper_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  module Admin
+    describe ImportsHelper do
+      describe "#render" do
+        subject do
+          Nokogiri::HTML(helper.import_dropdown(component))
+        end
+
+        let!(:component) { create(:component, manifest_name: "dummy") }
+
+        it "creates an import dropdown" do
+          expect(subject.css("li").length).to eq(1)
+        end
+
+        it "creates a link" do
+          link = subject.css("li.imports--dummy").css("a")[0].attributes["href"]
+          # raise link["href"].value.inspect
+          expect(link.value).to eq("/admin/participatory_processes/#{component.participatory_space.slug}/components/#{component.id}/imports/new")
+        end
+      end
+
+      # it "creates links for each format" do
+      #   link = subject.css("li.exports--format--csv.exports--dummies a")[0]
+      #   expect(link["href"]).to eq("/admin/participatory_processes/#{component.participatory_space.slug}/components/#{component.id}/exports.CSV?id=dummies")
+      #   expect(link["data-method"]).to eq("post")
+      # end
+      describe "#mime_types" do
+        before do
+          allow(helper).to receive(:t).with(
+            ".accepted_mime_types.json"
+          ).and_return("JSON")
+          allow(helper).to receive(:t).with(
+            ".accepted_mime_types.csv"
+          ).and_return("CSV")
+          allow(helper).to receive(:t).with(
+            ".accepted_mime_types.xls"
+          ).and_return("XLS")
+        end
+
+        it "returns the expected mime types" do
+          expect(helper.mime_types).to eq("JSON, CSV, XLS")
+        end
+
+        it "asd" do
+          raise helper.user_group_select_field(nil, nil).inspect
+        end
+      end
+    end
+  end
+end
