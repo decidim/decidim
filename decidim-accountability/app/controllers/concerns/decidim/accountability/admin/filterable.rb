8cd9bde5327f55852b007582780aa0a3010f6f63
Simon Tharby <jinjamail@gmail.com>
2021-01-01 17:37:36 +0000

    Add filter by status functionality.

--- decidim-accountability/app/controllers/concerns/decidim/accountability/admin/filterable.rb
+++ decidim-accountability/app/controllers/concerns/decidim/accountability/admin/filterable.rb
@@ -26,21 +26,33 @@ module Decidim
           def filters
             [
               :category_id_eq,
-              :scope_id_eq
+              :scope_id_eq,
+              :status_id_eq
             ]
           end
 
           def filters_with_values
             {
               scope_id_eq: scope_ids_hash(scopes.top_level),
-              category_id_eq: category_ids_hash(categories.first_class)
+              category_id_eq: category_ids_hash(categories.first_class),
+              status_id_eq: status_ids_hash(statuses)
             }
           end
 
           # Can't user `super` here, because it does not belong to a superclass
           # but to a concern.
           def dynamically_translated_filters
-            [:scope_id_eq, :category_id_eq]
+            [:scope_id_eq, :category_id_eq, :status_id_eq]
+          end
+
+          def status_ids_hash(statuses)
+            statuses.each_with_object({}) do |status, hash|
+              hash[status.id] = status.id
+            end
+          end
+
+          def translated_status_id_eq(id)
+            translated_attribute(statuses.find_by(id: id).name)
           end
         end
       end
