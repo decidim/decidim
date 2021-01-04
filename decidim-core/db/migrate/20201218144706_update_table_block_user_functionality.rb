73f8247b8cb0862f2c4af6204a1db0eae5474ea2
roxana.oprescu <roxana.oprescu@tremend.com>
2021-01-04 16:19:07 +0200

    Fix linting errors

--- decidim-core/db/migrate/20201218144706_update_table_block_user_functionality.rb
+++ decidim-core/db/migrate/20201218144706_update_table_block_user_functionality.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UpdateTableBlockUserFunctionality < ActiveRecord::Migration[5.2]
   def change
     rename_column :decidim_user_suspensions, :suspending_user_id, :blocking_user_id
--- decidim-core/db/migrate/20201218145252_rename_decidim_user_fields_for_block_functionality.rb
+++ decidim-core/db/migrate/20201218145252_rename_decidim_user_fields_for_block_functionality.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameDecidimUserFieldsForBlockFunctionality < ActiveRecord::Migration[5.2]
   def change
     rename_column :decidim_users, :suspended, :blocked
