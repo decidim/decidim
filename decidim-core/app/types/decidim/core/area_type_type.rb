b16833e286e7fb08b6b4735eb66e6059c153f23b
Alexandru Emil Lupu <contact@alecslupu.ro>
2021-01-12 11:48:01 +0200

    Partial update of GraphQL syntax on Core module (#7049)

--- decidim-core/app/types/decidim/core/area_type_type.rb
+++ decidim-core/app/types/decidim/core/area_type_type.rb
@@ -2,13 +2,12 @@
 
 module Decidim
   module Core
-    AreaTypeType = GraphQL::ObjectType.define do
-      name "AreaType"
+    class AreaTypeType < Decidim::Api::Types::BaseObject
       description "An area type."
 
-      field :id, !types.ID, "Internal ID for this area type"
-      field :name, !TranslatedFieldType, "The name of this area type."
-      field :plural, !TranslatedFieldType, "The plural name of this area type"
+      field :id, GraphQL::Types::ID, "Internal ID for this area type", null: false
+      field :name, Decidim::Core::TranslatedFieldType, "The name of this area type.", null: false
+      field :plural, Decidim::Core::TranslatedFieldType, "The plural name of this area type", null: false
     end
   end
 end
--- decidim-core/app/types/decidim/core/scope_api_type.rb
+++ decidim-core/app/types/decidim/core/scope_api_type.rb
@@ -2,15 +2,14 @@
 
 module Decidim
   module Core
-    ScopeApiType = GraphQL::ObjectType.define do
-      name "Scope"
+    class ScopeApiType < Decidim::Api::Types::BaseObject
       description "A scope"
 
-      field :id, !types.ID
-      field :name, !TranslatedFieldType, "The name of this scope."
+      field :id, GraphQL::Types::ID, null: false
+      field :name, Decidim::Core::TranslatedFieldType, "The graphql_name of this scope.", null: false
 
-      field :children, !types[Decidim::Core::ScopeApiType], "Descendants of this scope"
-      field :parent, Decidim::Core::ScopeApiType, "This scope's parent scope."
+      field :children, [Decidim::Core::ScopeApiType, { null: true }], "Descendants of this scope", null: false
+      field :parent, Decidim::Core::ScopeApiType, "This scope's parent scope.", null: true
     end
   end
 end
