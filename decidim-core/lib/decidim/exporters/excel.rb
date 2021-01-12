67cfc50820eaa2e44f2074d1ffec64e57b12973b
lahdeero <eero.lahdenpera@gmail.com>
2021-01-12 15:18:18 +0200

    Fix tests and removed comments

--- decidim-core/lib/decidim/exporters/excel.rb
+++ decidim-core/lib/decidim/exporters/excel.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-# require "spreadsheet"
 require "rubyXL"
 
 require "rubyXL/convenience_methods/cell"
-# require "rubyXL/convenience_methods/color"
 require "rubyXL/convenience_methods/font"
 require "rubyXL/convenience_methods/workbook"
 require "rubyXL/convenience_methods/worksheet"
@@ -26,23 +24,7 @@ module Decidim
       # Returns an ExportData instance.
       def export
         workbook = RubyXL::Workbook.new
-        # book = Spreadsheet::Workbook.new
-        # sheet = book.create_worksheet
         worksheet = workbook[0]
-        # sheet.name = "Export"
-
-        # sheet.row(0).default_format = Spreadsheet::Format.new(
-        #   weight: :bold,
-        #   pattern: 1,
-        #   pattern_fg_color: :xls_color_14,
-        #   horizontal_align: :center
-        # )
-
-        # sheet.row(0).replace headers
-
-        # headers.length.times.each do |index|
-        #   sheet.column(index).width = 20
-        # end
 
         headers.each_with_index.map do |header, index|
           worksheet.change_column_width(index, 20)
@@ -54,37 +36,15 @@ module Decidim
         worksheet.change_row_horizontal_alignment(0, "center")
 
         processed_collection.each_with_index do |resource, index|
-          # sheet.row(index + 1).replace(headers.map { |header| custom_sanitize(resource[header]) })
           headers.each_with_index do |header, j|
-            # raise custom_sanitize(resource[header]).class.inspect
-
             if resource[header].class == ActiveSupport::TimeWithZone
-              # c = worksheet.add_cell(index + 1, j)
-              # c.set_number_format("dd.mm.yyyy HH:MM:SS")
-              # c.change_contents(resource[header].to_datetime)
-              # # raise c.value.to_time.utc.iso8601.inspect
-              # raise c.raw_value.inspect
               cell = worksheet.add_cell(index + 1, j, custom_sanitize(resource[header]))
               cell.set_number_format("dd.mm.yyyy HH:MM:SS")
-              # raise cell.value.inspect
               next
             end
             worksheet.add_cell(index + 1, j, custom_sanitize(resource[header]))
-            # c.change_contents(custom_sanitize(resource[header])) if resource[header].class == ActiveSupport::TimeWithZone
-            # if j == 5
-              # foo = custom_sanitize(resource[header])
-              # raise foo.to_date.inspect
-              # raise worksheet[1][5].inspect
-            # end
           end
         end
-
-        # output = StringIO.new
-        # book.write output
-
-        # ExportData.new(output.string, "xls")
-
-        # raise workbook.stream.string.inspect
 
         ExportData.new(workbook.stream.string, "xlsx")
       end
--- decidim-core/spec/lib/exporters/excel_spec.rb
+++ decidim-core/spec/lib/exporters/excel_spec.rb
@@ -39,57 +39,48 @@ module Decidim
     describe "export" do
       it "exports the collection using the right serializer" do
         exported = StringIO.new(subject.export.read)
-        # book = Spreadsheet.open(exported)
         workbook = RubyXL::Parser.parse_buffer(exported)
-        # worksheet = book.worksheet(0)
         worksheet = workbook[0]
-        # expect(worksheet.rows.length).to eq(7)
         expect(worksheet.sheet_data.rows.length).to eq(7)
 
-        # headers = worksheet.rows[0]
         headers = worksheet[0].cells.map(&:value)
         expect(headers).to eq(["id", "serialized_name/ca", "serialized_name/es", "other_ids", "float", "date"])
 
-        # expect(worksheet.rows[1][0..4]).to eq([1, "foocat", "fooes", "1, 2, 3", 1.66])
-        # raise worksheet[1][0..4].map(&:value).inspect
-        # raise worksheet[1][0..4].map { |o| o.value }.inspect
         expect(worksheet[1][0..4].map(&:value)).to eq([1, "foocat", "fooes", "1, 2, 3", 1.66])
 
-        # expect(worksheet.rows[1].datetime(5)).to eq(Time.zone.local(2017, 10, 1, 5, 0))
-        # raise worksheet[1][5].value.inspect
-        expect(worksheet[1][5].value).to eq(Time.zone.local(2017, 10, 1, 5, 0))
+        expect(Time.zone.parse(worksheet[1][5].value.to_s)).to eq(Time.zone.local(2017, 10, 1, 5, 0))
 
         expect(worksheet[2][0..4].map(&:value)).to eq([2, "barcat", "bares", "2, 3, 4", 0.55])
-        # expect(worksheet.rows[2].datetime(5)).to eq(Time.zone.local(2017, 9, 20))
-        expect(worksheet[2][5].value).to eq(Time.zone.local(2017, 9, 20))
+        expect(Time.zone.parse(worksheet[2][5].value.to_s)).to eq(Time.zone.local(2017, 9, 20))
       end
     end
 
     describe "export sanitizer" do
       it "exports the collection sanitizing invalid first chars correctly" do
         exported = StringIO.new(subject.export.read)
-        book = Spreadsheet.open(exported)
-        worksheet = book.worksheet(0)
+        workbook = RubyXL::Parser.parse_buffer(exported)
+        worksheet = workbook[0]
 
-        headers = worksheet.rows[0]
+        headers = worksheet[0].cells.map(&:value)
         expect(headers).to eq(["id", "serialized_name/ca", "serialized_name/es", "other_ids", "float", "date"])
-        expect(worksheet.rows[1][0..4]).to eq([1, "foocat", "fooes", "1, 2, 3", 1.66])
-        expect(worksheet.rows[1].datetime(5)).to eq(Time.zone.local(2017, 10, 1, 5, 0))
+        expect(worksheet[1][0..4].map(&:value)).to eq([1, "foocat", "fooes", "1, 2, 3", 1.66])
 
-        expect(worksheet.rows[2][0..4]).to eq([2, "barcat", "bares", "2, 3, 4", 0.55])
-        expect(worksheet.rows[2].datetime(5)).to eq(Time.zone.local(2017, 9, 20))
+        expect(Time.zone.parse(worksheet[1][5].value.to_s)).to eq(Time.zone.local(2017, 10, 1, 5, 0))
 
-        expect(worksheet.rows[3][0..4]).to eq([3, "'@atcat", "'@ates", "1, 2, 3", 0.35])
-        expect(worksheet.rows[3].datetime(5)).to eq(Time.zone.local(2020, 7, 20))
+        expect(worksheet[2][0..4].map(&:value)).to eq([2, "barcat", "bares", "2, 3, 4", 0.55])
+        expect(Time.zone.parse(worksheet[2][5].value.to_s)).to eq(Time.zone.local(2017, 9, 20))
 
-        expect(worksheet.rows[4][0..4]).to eq([4, "'=equalcat", "'=equales", "1, 2, 3", 0.45])
-        expect(worksheet.rows[4].datetime(5)).to eq(Time.zone.local(2020, 6, 24))
+        expect(worksheet[3][0..4].map(&:value)).to eq([3, "'@atcat", "'@ates", "1, 2, 3", 0.35])
+        expect(Time.zone.parse(worksheet[3][5].value.to_s)).to eq(Time.zone.local(2020, 7, 20))
 
-        expect(worksheet.rows[5][0..4]).to eq([5, "'+pluscat", "'+pluses", "1, 2, 3", 0.65])
-        expect(worksheet.rows[5].datetime(5)).to eq(Time.zone.local(2020, 7, 15))
+        expect(worksheet[4][0..4].map(&:value)).to eq([4, "'=equalcat", "'=equales", "1, 2, 3", 0.45])
+        expect(Time.zone.parse(worksheet[4][5].value.to_s)).to eq(Time.zone.local(2020, 6, 24))
 
-        expect(worksheet.rows[6][0..4]).to eq([6, "'-minuscat", "'-minuses", "1, 2, 3", 0.75])
-        expect(worksheet.rows[6].datetime(5)).to eq(Time.zone.local(2020, 6, 27))
+        expect(worksheet[5][0..4].map(&:value)).to eq([5, "'+pluscat", "'+pluses", "1, 2, 3", 0.65])
+        expect(Time.zone.parse(worksheet[5][5].value.to_s)).to eq(Time.zone.local(2020, 7, 15))
+
+        expect(worksheet[6][0..4].map(&:value)).to eq([6, "'-minuscat", "'-minuses", "1, 2, 3", 0.75])
+        expect(Time.zone.parse(worksheet[6][5].value.to_s)).to eq(Time.zone.local(2020, 6, 27))
       end
     end
   end
