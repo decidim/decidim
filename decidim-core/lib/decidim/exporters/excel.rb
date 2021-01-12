43e7182fea5e98611e3e9c714e2295aa8ae4d9a1
lahdeero <eero.lahdenpera@gmail.com>
2021-01-12 07:33:07 +0200

    Export excel creates xlsx

--- decidim-core/lib/decidim/exporters/excel.rb
+++ decidim-core/lib/decidim/exporters/excel.rb
@@ -23,29 +23,39 @@ module Decidim
         # book = Spreadsheet::Workbook.new
         workbook.add_worksheet("Export")
         # sheet = book.create_worksheet
+        worksheet = workbook[0]
         # sheet.name = "Export"
 
-        sheet.row(0).default_format = Spreadsheet::Format.new(
-          weight: :bold,
-          pattern: 1,
-          pattern_fg_color: :xls_color_14,
-          horizontal_align: :center
-        )
+        # sheet.row(0).default_format = Spreadsheet::Format.new(
+        #   weight: :bold,
+        #   pattern: 1,
+        #   pattern_fg_color: :xls_color_14,
+        #   horizontal_align: :center
+        # )
 
-        sheet.row(0).replace headers
+        # sheet.row(0).replace headers
 
-        headers.length.times.each do |index|
-          sheet.column(index).width = 20
-        end
+        # headers.length.times.each do |index|
+        #   sheet.column(index).width = 20
+        # end
+
+        headers.each_with_index.map { |header, index| worksheet.add_cell(0, index, header) }
 
         processed_collection.each_with_index do |resource, index|
-          sheet.row(index + 1).replace(headers.map { |header| custom_sanitize(resource[header]) })
+          # sheet.row(index + 1).replace(headers.map { |header| custom_sanitize(resource[header]) })
+          j = 0
+          headers.map do |header|
+            worksheet.add_cell(index + 1, j, custom_sanitize(resource[header]))
+            j += 1
+          end
         end
 
-        output = StringIO.new
-        book.write output
+        # output = StringIO.new
+        # book.write output
 
-        ExportData.new(output.string, "xls")
+        # ExportData.new(output.string, "xls")
+
+        ExportData.new(workbook.stream.string, "xlsx")
       end
     end
   end
