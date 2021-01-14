12085121c6d4f2fee66c3f003a9fd2b25ad2560e
Andrea Orler <andrea@codegram.com>
2021-01-14 15:13:33 +0100

    fix: linting

--- decidim-elections/db/migrate/20210114100749_add_published_at_to_decidim_votings_votings.rb
+++ decidim-elections/db/migrate/20210114100749_add_published_at_to_decidim_votings_votings.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddPublishedAtToDecidimVotingsVotings < ActiveRecord::Migration[5.2]
   def change
     add_column :decidim_votings_votings, :published_at, :datetime
