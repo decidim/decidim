05371a66601d3fed6a8ce4fa97e40ebabaf9c21b
Alexandru-Emil Lupu <contact@alecslupu.ro>
2021-01-14 19:37:41 +0200

    Remove GraphQL helper module that helped during transition

--- decidim-dev/lib/decidim/dev/test/rspec_support/component.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/component.rb
@@ -72,7 +72,6 @@ module Decidim
       include Decidim::HasAttachments
       include Decidim::ShareableWithToken
       include Decidim::TranslatableResource
-      include Decidim::Core::GraphQLApiTransition
 
       translatable_fields :title
       searchable_fields(
