26f5107d9e451614cc5ae541650400ca38645130
Agustí B.R <hola@agusti.cat>
2021-01-15 13:09:22 +0100

    chore(votings): update graphQL

--- decidim-elections/app/types/decidim/votings/voting_type.rb
+++ decidim-elections/app/types/decidim/votings/voting_type.rb
@@ -3,21 +3,18 @@
 module Decidim
   module Votings
     # This type represents a voting space.
-    VotingType = GraphQL::ObjectType.define do
-      interfaces [
-        -> { Decidim::Core::ParticipatorySpaceInterface },
-        -> { Decidim::Core::TraceableInterface }
-      ]
+    class VotingType < Decidim::Api::Types::BaseObject
+      implements Decidim::Core::ParticipatorySpaceInterface
+      implements Decidim::Core::ScopableInterface
 
-      name "Voting"
       description "A voting space"
 
-      field :description, Decidim::Core::TranslatedFieldType, "The description of this voting"
-      field :slug, !types.String, "Slug of this voting"
-      field :createdAt, Decidim::Core::DateTimeType, "When this voting was created", property: :created_at
-      field :updatedAt, Decidim::Core::DateTimeType, "When this voting was updated", property: :updated_at
-      field :startTime, !Decidim::Core::DateTimeType, "The start time for this voting", property: :start_time
-      field :endTime, !Decidim::Core::DateTimeType, "The end time for this voting", property: :end_time
+      field :slug, String, null: false
+      field :description, Decidim::Core::TranslatedFieldType, "The description of this voting space.", null: true
+      field :start_time, Decidim::Core::DateTimeType, "The start time for this voting space.", null: false
+      field :end_time, Decidim::Core::DateTimeType, "The end time for this voting space", null: false
+      field :created_at, Decidim::Core::DateTimeType, "The time this voting was created", null: false
+      field :updated_at, Decidim::Core::DateTimeType, "The time this voting was updated", null: false
     end
   end
 end
