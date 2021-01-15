6037e3b822be1ec90de65c814c9089c16195f89f
Agustí B.R <hola@agusti.cat>
2021-01-15 10:10:36 +0100

    feat(votings): add presenter and permissions specs

--- decidim-admin/spec/helpers/menu_helper_spec.rb
+++ decidim-admin/spec/helpers/menu_helper_spec.rb
@@ -22,7 +22,7 @@ module Decidim
             have_link("Processes", href: "/admin/participatory_processes") &
             have_link("Conferences", href: "/admin/conferences") &
             have_link("Assemblies", href: "/admin/assemblies") &
-            have_link("Consultations", href: "/admin/consultations") &
+            have_link("Votings", href: "/admin/votings") & have_link("Consultations", href: "/admin/consultations") &
             have_link("Initiatives", href: "/admin/initiatives") &
             have_link("Global moderation", href: "/admin/moderations") &
             have_link("Pages", href: "/admin/static_pages") &
--- decidim-elections/app/permissions/decidim/votings/permissions.rb
+++ decidim-elections/app/permissions/decidim/votings/permissions.rb
@@ -4,7 +4,33 @@ module Decidim
   module Votings
     class Permissions < Decidim::DefaultPermissions
       def permissions
+        allowed_public_anonymous_action?
+
+        return permission_action unless user
+
+        return Decidim::Votings::Admin::Permissions.new(user, permission_action, context).permissions if permission_action.scope == :admin
+
         permission_action
+      end
+
+      private
+
+      def voting
+        @voting ||= context.fetch(:voting, nil)
+      end
+
+      def allowed_public_anonymous_action?
+        return unless permission_action.action == :read
+        return unless permission_action.scope == :public
+
+        case permission_action.subject
+        when :votings
+          allow!
+        when :voting
+          toggle_allow(voting.published? || user&.admin?)
+        when :participatory_space
+          allow!
+        end
       end
     end
   end
