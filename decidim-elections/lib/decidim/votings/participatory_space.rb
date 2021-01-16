ce4d12bd968908e1e81ccfe847719fcf917d4e53
Agustí B.R <hola@agusti.cat>
2021-01-16 17:56:33 +0100

    feat(votings): add seeds

--- decidim-elections/lib/decidim/votings/participatory_space.rb
+++ decidim-elections/lib/decidim/votings/participatory_space.rb
@@ -32,6 +32,33 @@ Decidim.register_participatory_space(:votings) do |participatory_space|
   #   Decidim::VotingsUserRole.where(user: user).destroy_all
   # end
 
-  # participatory_space.seeds do
-  # end
+  participatory_space.seeds do
+    organization = Decidim::Organization.first
+    # seeds_root = File.join(__dir__, "..", "..", "..", "db", "seeds")
+
+    2.times do |n|
+      params = {
+        organization: organization,
+        title: Decidim::Faker::Localized.sentence(word_count: 5),
+        slug: Decidim::Faker::Internet.unique.slug(words: nil, glue: "-"),
+        description: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
+          Decidim::Faker::Localized.paragraph(sentence_count: 3)
+        end,
+        published_at: 2.weeks.ago,
+        start_time: 3.weeks.from_now,
+        end_time: 3.weeks.from_now + 4.hours,
+        scope: n.positive? ? nil : Decidim::Scope.reorder(Arel.sql("RANDOM()")).first
+      }
+
+      voting = Decidim.traceability.perform_action!(
+        "publish",
+        Decidim::Votings::Voting,
+        organization.users.first,
+        visibility: "all"
+      ) do
+        Decidim::Votings::Voting.create!(params)
+      end
+      voting.add_to_index_as_search_resource
+    end
+  end
 end
