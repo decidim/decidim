76ae6052bf4119252b7366fd05866a63ddd28168
Alexandru-Emil Lupu <contact@alecslupu.ro>
2021-01-21 23:19:56 +0200

    Fixing Sortitions Api autoloader

--- decidim-sortitions/lib/decidim/api/sortition_type.rb
+++ decidim-sortitions/lib/decidim/api/sortition_type.rb
@@ -9,21 +9,21 @@ module Decidim
 
       description "A sortition"
 
-      field :id, ID, "The internal ID for this sortition", null: false
-      field :dice, Integer, "The dice for this sortition", null: true
-      field :target_items, Integer, "The target items for this sortition", null: true
+      field :id, GraphQL::Types::ID, "The internal ID for this sortition", null: false
+      field :dice, GraphQL::Types::Int, "The dice for this sortition", null: true
+      field :target_items, GraphQL::Types::Int, "The target items for this sortition", null: true
       field :request_timestamp, Decidim::Core::DateType, "The request time stamp for this request", null: true
-      field :selected_proposals, [Integer, { null: true }], "The selected proposals for this sortition", null: true
+      field :selected_proposals, [GraphQL::Types::Int, { null: true }], "The selected proposals for this sortition", null: true
       field :created_at, Decidim::Core::DateTimeType, "When this sortition was created", null: true
       field :updated_at, Decidim::Core::DateTimeType, "When this sortition was updated", null: true
       field :witnesses, Decidim::Core::TranslatedFieldType, "The witnesses for this sortition", null: true
       field :additional_info, Decidim::Core::TranslatedFieldType, "The additional info for this sortition", null: true
-      field :reference, String, "The reference for this sortition", null: true
+      field :reference, GraphQL::Types::String, "The reference for this sortition", null: true
       field :title, Decidim::Core::TranslatedFieldType, "The title for this sortition", null: true
       field :cancel_reason, Decidim::Core::TranslatedFieldType, "The cancel reason for this sortition", null: true
       field :cancelled_on, Decidim::Core::DateType, "When this sortition was cancelled", null: true
       field :cancelled_by_user, Decidim::Core::UserType, "Who cancelled this sortition", null: true
-      field :candidate_proposals, [Integer, { null: true }], "The candidate proposal for this sortition", null: true
+      field :candidate_proposals, [GraphQL::Types::Int, { null: true }], "The candidate proposal for this sortition", null: true
     end
   end
 end
