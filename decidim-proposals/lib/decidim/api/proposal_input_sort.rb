88d97e000642698de5f1af870e71692c2618aa64
Alexandru-Emil Lupu <contact@alecslupu.ro>
2021-01-21 23:29:53 +0200

    Fixing Proposals Api autoloader

--- decidim-proposals/lib/decidim/api/proposal_input_sort.rb
+++ decidim-proposals/lib/decidim/api/proposal_input_sort.rb
@@ -9,9 +9,9 @@ module Decidim
       graphql_name "ProposalSort"
       description "A type used for sorting proposals"
 
-      argument :id, String, "Sort by ID, valid values are ASC or DESC", required: false
+      argument :id, GraphQL::Types::String, "Sort by ID, valid values are ASC or DESC", required: false
       argument :vote_count,
-               type: String,
+               type: GraphQL::Types::String,
                description: "Sort by number of votes, valid values are ASC or DESC. Will be ignored if votes are hidden",
                required: false,
                as: :proposal_votes_count
