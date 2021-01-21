45aa19bb319c8e92f8775d00983b738b00cbbd57
Alexandru-Emil Lupu <contact@alecslupu.ro>
2021-01-21 23:11:18 +0200

    Fixing Surveys Api autoloader

--- decidim-surveys/lib/decidim/api/survey_type.rb
+++ decidim-surveys/lib/decidim/api/survey_type.rb
@@ -5,7 +5,7 @@ module Decidim
     class SurveyType < Decidim::Api::Types::BaseObject
       description "A survey"
 
-      field :id, ID, "The internal ID for this survey", null: false
+      field :id, GraphQL::Types::ID, "The internal ID for this survey", null: false
       field :created_at, Decidim::Core::DateTimeType, "The time this survey was created", null: true
       field :updated_at, Decidim::Core::DateTimeType, "The time this survey was updated", null: true
       field :questionnaire, Decidim::Forms::QuestionnaireType, "The questionnaire for this survey", null: true
