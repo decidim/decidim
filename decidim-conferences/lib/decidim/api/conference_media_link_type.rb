eeeab0d2db17960d23ad5f96f873e2bacb358d35
Alexandru-Emil Lupu <contact@alecslupu.ro>
2021-01-22 07:45:36 +0200

    Fixing Conferences Api autoloader

--- decidim-conferences/lib/decidim/api/conference_media_link_type.rb
+++ decidim-conferences/lib/decidim/api/conference_media_link_type.rb
@@ -6,11 +6,11 @@ module Decidim
     class ConferenceMediaLinkType < Decidim::Api::Types::BaseObject
       description "A conference media link"
 
-      field :id, ID, "Internal ID for this media link", null: false
+      field :id, GraphQL::Types::ID, "Internal ID for this media link", null: false
       field :title, Decidim::Core::TranslatedFieldType, "Title for this media link", null: true
-      field :link, String, "URL for this media link", null: true
+      field :link, GraphQL::Types::String, "URL for this media link", null: true
       field :date, Decidim::Core::DateType, "Relevant date for the media link", null: true
-      field :weight, Integer, "Order of appearance in which it should be presented", null: true
+      field :weight, GraphQL::Types::Int, "Order of appearance in which it should be presented", null: true
       field :created_at, Decidim::Core::DateTimeType, "The time this entry was created", null: true
       field :updated_at, Decidim::Core::DateTimeType, "The time this entry was updated", null: true
     end
