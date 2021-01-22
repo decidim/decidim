921a6ce89f9d967163d9404bfe41491c342ca9a3
Alexandru-Emil Lupu <contact@alecslupu.ro>
2021-01-22 08:33:08 +0200

    Fixing Initiatives Api autoloader

--- decidim-initiatives/lib/decidim/api/initiative_committee_member_type.rb
+++ decidim-initiatives/lib/decidim/api/initiative_committee_member_type.rb
@@ -7,10 +7,10 @@ module Decidim
       graphql_name "InitiativeCommitteeMemberType"
       description "A initiative committee member"
 
-      field :id, ID, "Internal ID for this member of the committee", null: false
+      field :id, GraphQL::Types::ID, "Internal ID for this member of the committee", null: false
       field :user, Decidim::Core::UserType, "The decidim user for this initiative committee member", null: true
 
-      field :state, String, "Type of the committee member", null: true
+      field :state, GraphQL::Types::String, "Type of the committee member", null: true
       field :created_at, Decidim::Core::DateTimeType, "The date this initiative committee member was created", null: true
       field :updated_at, Decidim::Core::DateTimeType, "The date this initiative committee member was updated", null: true
     end
