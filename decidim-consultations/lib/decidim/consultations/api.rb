f7ea949d8ee2203848e612fca2fa31788a0be15d
Alexandru-Emil Lupu <contact@alecslupu.ro>
2021-01-22 07:48:23 +0200

    Fixing Consultations Api autoloader

--- decidim-consultations/lib/decidim/consultations/api.rb
+++ decidim-consultations/lib/decidim/consultations/api.rb
@@ -3,5 +3,6 @@
 module Decidim
   module Consultations
     autoload :ConsultationType, "decidim/api/consultation_type"
+    autoload :ConsultationQuestionType, "decidim/api/consultation_question_type"
   end
 end
