da85d0fba39933a575d96760341d4837e37731ef
Alexandru-Emil Lupu <contact@alecslupu.ro>
2021-01-22 07:36:48 +0200

    Fixing Budgets Api autoloader

--- decidim-budgets/lib/decidim/api/budget_type.rb
+++ decidim-budgets/lib/decidim/api/budget_type.rb
@@ -8,10 +8,10 @@ module Decidim
 
       description "A budget"
 
-      field :id, ID, "The internal ID of this budget", null: false
+      field :id, GraphQL::Types::ID, "The internal ID of this budget", null: false
       field :title, Decidim::Core::TranslatedFieldType, "The title for this budget", null: false
       field :description, Decidim::Core::TranslatedFieldType, "The description for this budget", null: false
-      field :total_budget, Integer, "The total budget", null: false, camelize: false
+      field :total_budget, GraphQL::Types::Int, "The total budget", null: false, camelize: false
       field :created_at, Decidim::Core::DateTimeType, "When this budget was created", null: true
       field :updated_at, Decidim::Core::DateTimeType, "When this budget was updated", null: true
 
--- decidim-budgets/lib/decidim/api/project_type.rb
+++ decidim-budgets/lib/decidim/api/project_type.rb
@@ -10,14 +10,14 @@ module Decidim
 
       description "A project"
 
-      field :id, ID, "The internal ID for this project", null: false
+      field :id, GraphQL::Types::ID, "The internal ID for this project", null: false
       field :title, Decidim::Core::TranslatedFieldType, "The title for this project", null: true
       field :description, Decidim::Core::TranslatedFieldType, "The description for this project", null: true
-      field :budget_amount, Integer, "The budget amount for this project", null: true, camelize: false
-      field :selected, Boolean, "Whether this proposal is selected or not", method: :selected?, null: true
+      field :budget_amount, GraphQL::Types::Int, "The budget amount for this project", null: true, camelize: false
+      field :selected, GraphQL::Types::Boolean, "Whether this proposal is selected or not", method: :selected?, null: true
       field :created_at, Decidim::Core::DateTimeType, "When this project was created", null: true
       field :updated_at, Decidim::Core::DateTimeType, "When this project was updated", null: true
-      field :reference, String, "The reference for this project", null: true
+      field :reference, GraphQL::Types::String, "The reference for this project", null: true
     end
   end
 end
