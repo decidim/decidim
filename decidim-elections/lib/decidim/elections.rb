4244c9acf34af69450da5a0bf930c71768224ac6
Alexandru Emil Lupu <contact@alecslupu.ro>
2021-01-22 11:47:22 +0200

    API paths refactor (#7199)

--- decidim-elections/lib/decidim/elections.rb
+++ decidim-elections/lib/decidim/elections.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "decidim/elections/admin"
+require "decidim/elections/api"
 require "decidim/elections/trustee_zone"
 require "decidim/elections/engine"
 require "decidim/elections/admin_engine"
