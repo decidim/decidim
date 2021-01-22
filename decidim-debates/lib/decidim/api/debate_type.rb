a2a917881022cd758c29ad316f06ae4a8eeda7d7
Alexandru-Emil Lupu <contact@alecslupu.ro>
2021-01-22 07:50:59 +0200

    Fixing Debates Api autoloader

--- decidim-debates/lib/decidim/api/debate_type.rb
+++ decidim-debates/lib/decidim/api/debate_type.rb
@@ -10,17 +10,17 @@ module Decidim
 
       description "A debate"
 
-      field :id, ID, "The internal ID for this debate", null: false
+      field :id, GraphQL::Types::ID, "The internal ID for this debate", null: false
       field :title, Decidim::Core::TranslatedFieldType, "The title for this debate", null: true
       field :description, Decidim::Core::TranslatedFieldType, "The description for this debate", null: true
       field :instructions, Decidim::Core::TranslatedFieldType, "The instructions for this debate", null: true
       field :start_time, Decidim::Core::DateTimeType, "The start time for this debate", null: true
       field :end_time, Decidim::Core::DateTimeType, "The end time for this debate", null: true
-      field :image, String, "The image of this debate", null: true
+      field :image, GraphQL::Types::String, "The image of this debate", null: true
       field :created_at, Decidim::Core::DateTimeType, "When this debate was created", null: true
       field :updated_at, Decidim::Core::DateTimeType, "When this debate was updated", null: true
       field :information_updates, Decidim::Core::TranslatedFieldType, "The information updates for this debate", null: true
-      field :reference, String, "The reference for this debate", null: true
+      field :reference, GraphQL::Types::String, "The reference for this debate", null: true
     end
   end
 end
