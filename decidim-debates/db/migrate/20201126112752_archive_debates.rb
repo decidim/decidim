e8437f9081eb00be7f4a7d31c3c9160a8f0ff1c8
Marc Riera Casals <mrc2407@gmail.com>
2021-01-25 11:00:35 +0100

    Revert "Archive Debates (#6940)"
    This reverts commit 1fdc3deb6191717a30656024cbaa6ca7524d18ec.

--- decidim-debates/db/migrate/20201126112752_archive_debates.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class ArchiveDebates < ActiveRecord::Migration[5.2]
-  def change
-    add_column :decidim_debates_debates, :archived_at, :datetime
-    add_index :decidim_debates_debates, :archived_at
-  end
-end
