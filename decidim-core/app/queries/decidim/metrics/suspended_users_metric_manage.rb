575b9f1a887770f91ccd715963a456d902cffc42
roxana.oprescu <roxana.oprescu@tremend.com>
2021-02-02 17:52:19 +0200

    Update metrics to take into account the date of report/block actions

--- decidim-core/app/queries/decidim/metrics/suspended_users_metric_manage.rb
+++ decidim-core/app/queries/decidim/metrics/suspended_users_metric_manage.rb
@@ -14,11 +14,12 @@ module Decidim
         return @query if @query
 
         @query = Decidim::User.blocked.where(organization: @organization)
+        @query = @query.where("blocked_at <= ?", end_time)
         @query
       end
 
       def quantity
-        @quantity ||= @query.count
+        @quantity ||= @query.where("blocked_at >= ?", start_time).count
       end
     end
   end
