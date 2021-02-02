4be1986226a269ec2703844a435a7465f9aa1bcf
Agustí B.R <hola@agusti.cat>
2021-02-02 10:21:48 +0100

    fix(election): add closing <strong> tag

--- decidim-elections/app/controllers/decidim/elections/trustee_zone/application_controller.rb
+++ decidim-elections/app/controllers/decidim/elections/trustee_zone/application_controller.rb
@@ -21,7 +21,7 @@ module Decidim
           announcement_args = {
             callout_class: "alert",
             announcement: {
-              title: "<strong>#{t("no_bulletin_board.title", scope: "decidim.elections.trustee_zone")}",
+              title: "<strong>#{t("no_bulletin_board.title", scope: "decidim.elections.trustee_zone")}</strong>",
               body: t("no_bulletin_board.body", scope: "decidim.elections.trustee_zone")
             }
           }
