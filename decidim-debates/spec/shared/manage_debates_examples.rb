14fd7963644f4a9b71a8fc0009507604b65bff41
Ivan Vergés <ivan@platoniq.net>
2021-02-03 20:36:15 +0100

    Fix datepicker tests to not match upcoming month's days (#7256)
    # Conflicts:
    #	decidim-elections/spec/system/admin_manages_votings_spec.rb

--- decidim-debates/spec/shared/manage_debates_examples.rb
+++ decidim-debates/spec/shared/manage_debates_examples.rb
@@ -91,12 +91,12 @@ RSpec.shared_examples "manage debates" do
     end
 
     page.execute_script("$('#debate_start_time').focus()")
-    page.find(".datepicker-dropdown .day", text: "12").click
+    page.find(".datepicker-dropdown .day:not(.new)", text: "12").click
     page.find(".datepicker-dropdown .hour", text: "10:00").click
     page.find(".datepicker-dropdown .minute", text: "10:50").click
 
     page.execute_script("$('#debate_end_time').focus()")
-    page.find(".datepicker-dropdown .day", text: "12").click
+    page.find(".datepicker-dropdown .day:not(.new)", text: "12").click
     page.find(".datepicker-dropdown .hour", text: "12:00").click
     page.find(".datepicker-dropdown .minute", text: "12:50").click
 
