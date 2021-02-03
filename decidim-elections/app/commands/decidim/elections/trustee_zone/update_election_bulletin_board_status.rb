7c18a10d5fc7400f3acb03414f1308fdea64cfa7
slickepinne <svenjaschaefer@gmail.com>
2021-02-03 12:05:54 +0100

    show results when tally ended

--- decidim-elections/app/commands/decidim/elections/trustee_zone/update_election_bulletin_board_status.rb
+++ decidim-elections/app/commands/decidim/elections/trustee_zone/update_election_bulletin_board_status.rb
@@ -19,7 +19,10 @@ module Decidim
         def call
           return broadcast(:ok, election) unless election.bb_status.to_sym == required_status.to_sym
 
-          update_election_status!
+          transaction do
+            update_election_status!
+            fetch_election_results if election.bb_tally_ended?
+          end
 
           broadcast(:ok, election)
         end
@@ -27,6 +30,34 @@ module Decidim
         private
 
         attr_reader :election, :required_status
+
+        def decoded_data
+          @decoded_data ||= begin
+            signed_data = Decidim::Elections.bulletin_board.get_election_log_entries_by_types(election.id, ["end_tally"])
+            public_key = Rails.application.secrets.bulletin_board[:server_public_key]
+            public_key_rsa = JWT::JWK::RSA.import(public_key).public_key
+            JWT.decode(signed_data.first.signed_data, public_key_rsa, true, algorithm: "RS256")
+          rescue JWT::VerificationError, JWT::DecodeError, JWT::InvalidIatError, JWT::InvalidPayload => e
+            { error: e.message }
+          end
+        end
+
+        def results
+          @results ||= decoded_data.first["results"]
+        end
+
+        def fetch_election_results
+          answers = []
+          results.values.map do |values|
+            values.each do |key, value|
+              answers = Decidim::Elections::Answer.where(id: key)
+              answers.each do |answer|
+                answer.votes_count = value
+                answer.save!
+              end
+            end
+          end
+        end
 
         def update_election_status!
           status = Decidim::Elections.bulletin_board.get_election_status(election.id)
