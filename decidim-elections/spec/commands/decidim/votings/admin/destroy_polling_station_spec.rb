2529a93299bb1e33664bedce028cf542b07fa936
Andrea Orler <orlera@users.noreply.github.com>
2021-02-08 10:32:41 +0100

    Add Polling Stations to Votings (#7300)

---
+++ decidim-elections/spec/commands/decidim/votings/admin/destroy_polling_station_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  module Votings
+    module Admin
+      describe DestroyPollingStation do
+        subject { described_class.new(polling_station, user) }
+
+        let(:polling_station) { create :polling_station }
+        let(:user) { create :user, :admin, organization: organization }
+        let(:organization) { create(:organization) }
+
+        context "when everything is ok" do
+          it "destroys the meeting" do
+            subject.call
+
+            expect { polling_station.reload }.to raise_error(ActiveRecord::RecordNotFound)
+          end
+
+          it "traces the action", versioning: true do
+            expect(Decidim.traceability)
+              .to receive(:perform_action!)
+              .with(:delete, polling_station, user, visibility: "all")
+              .and_call_original
+
+            expect { subject.call }.to change(Decidim::ActionLog, :count)
+            action_log = Decidim::ActionLog.last
+            expect(action_log.version).to be_present
+          end
+        end
+      end
+    end
+  end
+end
