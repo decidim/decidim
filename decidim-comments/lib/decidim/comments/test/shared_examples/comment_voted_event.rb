6a121a8de1e5dd09253c3c5924a226665e59f301
Vera Rojman <vera@platoniq.net>
2021-02-09 10:02:21 +0100

    Update decidim-comments/lib/decidim/comments/test/shared_examples/comment_voted_event.rb
    Co-authored-by: Marc Riera <mrc2407@gmail.com>

--- decidim-comments/lib/decidim/comments/test/shared_examples/comment_voted_event.rb
+++ decidim-comments/lib/decidim/comments/test/shared_examples/comment_voted_event.rb
@@ -4,7 +4,6 @@ require "spec_helper"
 
 shared_examples_for "a comment voted event" do
   include_context "when it's a comment event"
-  # it_behaves_like "a simple event"
 
   let(:resource) { comment.commentable }
 
