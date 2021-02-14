42a2eee3e7ef32b9575857a83c8d2009527439a3
Alexandru-Emil Lupu <contact@alecslupu.ro>
2021-02-14 21:24:54 +0200

    Add Assemblies menu

--- decidim-assemblies/app/helpers/decidim/assemblies/admin/assemblies_admin_menu_helper.rb
+++ decidim-assemblies/app/helpers/decidim/assemblies/admin/assemblies_admin_menu_helper.rb
@@ -1,26 +1,13 @@
 # frozen_string_literal: true
 
 module Decidim
-  module Initiatives
+  module Assemblies
     module Admin
-      module InitiativeAdminMenuHelper
+      module AssembliesAdminMenuHelper
         include Decidim::Admin::SidebarMenuHelper
 
-        def admin_initiatives_menu
-          Decidim::MenuRegistry.destroy(:admin_initiatives_menu)
-          Decidim.menu :admin_initiatives_menu do |menu|
-            menu.item I18n.t("menu.initiatives", scope: "decidim.admin"),
-                      decidim_admin_initiatives.initiatives_path,
-                      position: 1.0,
-                      active: is_active_link?(decidim_admin_initiatives.initiatives_path),
-                      if: allowed_to?(:index, :initiative)
-
-            menu.item I18n.t("menu.initiatives_types", scope: "decidim.admin"),
-                      decidim_admin_initiatives.initiatives_types_path,
-                      active: is_active_link?(decidim_admin_initiatives.initiatives_types_path),
-                      if: allowed_to?(:manage, :initiative_type)
-          end
-          @admin_initiatives_menu ||= sidebar_menu(:admin_initiatives_menu)
+        def admin_assemblies_menu
+          @admin_initiatives_menu ||= sidebar_menu(:admin_assemblies_menu)
         end
       end
     end
