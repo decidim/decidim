b1c98cc5d32169bb03a7b957c0f76ba29390d31d
Ramon Costa <ramon.costa@gmail.com>
2021-02-17 14:46:48 +0100

    Add assemblies & processes weight field (#7283)
    Co-authored-by: Laura Jaime <laura.jv@coditramuntana.com>

--- decidim-assemblies/spec/commands/create_assembly_spec.rb
+++ decidim-assemblies/spec/commands/create_assembly_spec.rb
@@ -26,6 +26,7 @@ module Decidim::Assemblies
         invalid?: invalid,
         title: { en: "title" },
         subtitle: { en: "subtitle" },
+        weight: 1,
         slug: "slug",
         hashtag: "hashtag",
         meta_scope: { en: "meta scope" },
