cd8a259ca4ff8ce636bbfce05c65085e5153324f
lahdeero <eero.lahdenpera@gmail.com>
2021-02-17 11:16:34 +0200

    Rename proposalscreator to proposalcreator

--- decidim-proposals/lib/decidim/proposals.rb
+++ decidim-proposals/lib/decidim/proposals.rb
@@ -12,7 +12,7 @@ module Decidim
   # allows users to create proposals in a participatory process.
   module Proposals
     autoload :ProposalSerializer, "decidim/proposals/proposal_serializer"
-    autoload :ProposalCreator, "decidim/proposals/proposals_creator"
+    autoload :ProposalCreator, "decidim/proposals/proposal_creator"
     autoload :CommentableProposal, "decidim/proposals/commentable_proposal"
     autoload :CommentableCollaborativeDraft, "decidim/proposals/commentable_collaborative_draft"
     autoload :MarkdownToProposals, "decidim/proposals/markdown_to_proposals"
