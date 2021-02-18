1ae4639c0ae0334ea013bc7f432619d18c3650b8
lahdeero <eero.lahdenpera@gmail.com>
2021-02-18 14:03:34 +0200

    Fix test

--- decidim-core/spec/helpers/decidim/external_domain_helper_spec.rb
+++ decidim-core/spec/helpers/decidim/external_domain_helper_spec.rb
@@ -4,15 +4,15 @@ require "spec_helper"
 
 module Decidim
   describe ExternalDomainHelper do
-    let(:url) { Faker::Internet.url }
-
     context "when everything is OK" do
       it { expect(helper.highlight_domain("http://decidim.barcelona")).to include('class="alert">decidim.barcelona') }
     end
 
     context "when invalid URL" do
+      let(:invalid_url) { "foo" }
+
       it "raises InvalidUrlError" do
-        expect { helper.highlight_domain(Faker::Internet.slug) }.to raise_error(Decidim::ExternalDomainHelper::InvalidUrlError)
+        expect { helper.highlight_domain(invalid_url) }.to raise_error(Decidim::InvalidUrlError)
       end
     end
   end
