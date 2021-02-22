6758619bb2238505951e934f03cd1508865a84e9
slickepinne <svenjaschaefer@gmail.com>
2021-02-22 10:18:51 +0100

    export voting as open data

--- decidim-core/spec/system/homepage_spec.rb
+++ decidim-core/spec/system/homepage_spec.rb
@@ -433,6 +433,7 @@ describe "Homepage", type: :system do
             expect(zipfile.glob("*open-data-results.csv").length).to eq(1)
             expect(zipfile.glob("*open-data-meetings.csv").length).to eq(1)
             expect(zipfile.glob("*open-data-elections.csv").length).to eq(1)
+            expect(zipfile.glob("*open-data-votings.csv").length).to eq(1)
           end
         end
       end
