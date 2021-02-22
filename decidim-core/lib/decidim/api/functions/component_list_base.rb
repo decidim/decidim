a033c3129492659a48f7b0207d83cbb068e58281
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-02-22 15:34:04 +0200

    Move API default order to own module and apply to components list

--- decidim-core/lib/decidim/api/functions/component_list_base.rb
+++ decidim-core/lib/decidim/api/functions/component_list_base.rb
@@ -27,6 +27,7 @@ module Decidim
     #
     class ComponentListBase
       include NeedsApiFilterAndOrder
+      include NeedsApiDefaultOrder
       attr_reader :model_class
 
       def initialize(model_class:)
@@ -52,21 +53,6 @@ module Decidim
       end
 
       private
-
-      # Add default order to the query in order to avoid random PostgreSQL
-      # ordering between the queries for different pages of results. If some of
-      # the queried records are updated or new records are added between the
-      # API calls to different pages of records, PostgreSQL can randomly change
-      # the order causing duplicates to appear or some records to disappear from
-      # the results unless the order of the records is explicitly defined.
-      #
-      # Note that this needs to be called as the last method before returning
-      # the query so that it won't affect the desired ordering specified in the
-      # GraphQ query. In that case, ordering by ID will be the secondary order
-      # of the records.
-      def add_default_order
-        @query = @query.order(:id)
-      end
 
       def filter_keys_by_settings(kwargs, component)
         kwargs.select do |key, _value|
