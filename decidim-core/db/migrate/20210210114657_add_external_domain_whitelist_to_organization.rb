6a890abaf44146a3a69927b64debc6aa59414a6f
lahdeero <eero.lahdenpera@gmail.com>
2021-02-22 12:50:49 +0200

    Combine two migrations into one

--- decidim-core/db/migrate/20210210114657_add_external_domain_whitelist_to_organization.rb
+++ decidim-core/db/migrate/20210210114657_add_external_domain_whitelist_to_organization.rb
@@ -3,5 +3,13 @@
 class AddExternalDomainWhitelistToOrganization < ActiveRecord::Migration[5.2]
   def change
     add_column :decidim_organizations, :external_domain_whitelist, :string, array: true, default: []
+
+    reversible do |direction|
+      direction.up do
+        Decidim::Organization.find_each do |organization|
+          organization.update!(external_domain_whitelist: ["decidim.org", "github.com"])
+        end
+      end
+    end
   end
 end
