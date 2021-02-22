e0a1edc9a373bde33ed33c62bd568624739c34de
slickepinne <svenjaschaefer@gmail.com>
2021-02-22 10:17:14 +0100

    export votings

--- decidim-elections/app/presenters/decidim/votings/voting_presenter.rb
+++ decidim-elections/app/presenters/decidim/votings/voting_presenter.rb
@@ -2,20 +2,34 @@
 
 module Decidim
   module Votings
-    #
-    # Decorator for voting
-    #
     class VotingPresenter < SimpleDelegator
+      include Rails.application.routes.mounted_helpers
+      include ActionView::Helpers::UrlHelper
       include Decidim::SanitizeHelper
       include Decidim::TranslatableAttributes
 
-      def voting
-        __getobj__
-      end
+      delegate :url, to: :introductory_image, prefix: true
+      delegate :url, to: :banner_image, prefix: true
 
       def title
         content = translated_attribute(voting.title)
         decidim_html_escape(content)
+      end
+
+      def introductory_image_url
+        return if voting.introductory_image.blank?
+
+        URI.join(decidim.root_url(host: voting.organization.host), voting.introductory_image_url).to_s
+      end
+
+      def banner_image_url
+        return if voting.banner_image.blank?
+
+        URI.join(decidim.root_url(host: voting.organization.host), voting.banner_image_url).to_s
+      end
+
+      def voting
+        __getobj__
       end
     end
   end
