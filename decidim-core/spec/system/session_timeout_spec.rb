f843965e4660d03be0502bc9c695d502ba1f7049
lahdeero <eero.lahdenpera@gmail.com>
2021-02-24 14:10:45 +0200

    Change config in tests

--- decidim-core/spec/system/session_timeout_spec.rb
+++ decidim-core/spec/system/session_timeout_spec.rb
@@ -11,7 +11,7 @@ describe "Session timeout", type: :system do
   context "when session is about to timeout" do
     before do
       Devise.timeout_in = 2.minutes
-      Rails.application.config.session_timeouter_interval = 1000
+      Decidim.config.session_timeouter_interval = 1000
       switch_to_host(organization.host)
       login_as current_user, scope: :user
     end
@@ -19,7 +19,7 @@ describe "Session timeout", type: :system do
     it "timeouts if the user idles for too long" do
       visit decidim.root_path
       travel 1.minute
-      expect(page).to have_content("You were inactive for too long", wait: 2)
+      expect(page).to have_content("You were inactive for too long", wait: 3)
     end
 
     it "doesnt timeout when user is active in another window" do
@@ -29,13 +29,13 @@ describe "Session timeout", type: :system do
       switch_to_window(open_new_window)
       visit decidim.root_path
       2.times.each do
-        expect(page).to have_content("If you continue being inactive", wait: 2)
+        expect(page).to have_content("If you continue being inactive", wait: 3)
         find("#continueSession").click
         travel 20.seconds
       end
 
       switch_to_window(win1)
-      expect(page).to have_content("If you continue being inactive", wait: 2)
+      expect(page).to have_content("If you continue being inactive", wait: 3)
       find("#continueSession").click
       expect(page).not_to have_content("You were inactive for too long")
     end
