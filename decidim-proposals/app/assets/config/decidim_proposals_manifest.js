67cd4db27b4c121ece363588fb2c38b856cf3d13
lahdeero <eero.lahdenpera@gmail.com>
2021-02-24 10:00:34 +0200

    Remove js from proposals manifest

--- decidim-proposals/app/assets/config/decidim_proposals_manifest.js
+++ decidim-proposals/app/assets/config/decidim_proposals_manifest.js
@@ -1,5 +1,4 @@
 //= link decidim/proposals/add_proposal.js
 //= link decidim/proposals/admin/proposals_form.js
-//= link decidim/proposals/admin/component_settings.js
 //= link_tree ../images/decidim
 //= link_tree ../documents/decidim
