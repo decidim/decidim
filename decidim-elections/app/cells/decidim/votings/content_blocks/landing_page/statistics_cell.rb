41c795c4a292ad7f6483d608130ed4fcc805615f
Agustí B.R <hola@agusti.cat>
2021-02-25 16:21:23 +0100

    chore(votings): wrap landing StatisticsCell in a .section

--- decidim-elections/app/cells/decidim/votings/content_blocks/landing_page/statistics_cell.rb
+++ decidim-elections/app/cells/decidim/votings/content_blocks/landing_page/statistics_cell.rb
@@ -5,6 +5,12 @@ module Decidim
     module ContentBlocks
       module LandingPage
         class StatisticsCell < Decidim::StatisticsCell
+          def show
+            content_tag(:div, super, class: "section")
+          end
+
+          private
+
           def stats
             @stats ||= begin
               voting = Decidim::Votings::Voting.find_by(id: model.scoped_resource_id)
