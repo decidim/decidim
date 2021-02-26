ea8effbb68e355df8e6c2093bcd68032271690b4
lahdeero <eero.lahdenpera@gmail.com>
2021-02-26 11:10:56 +0200

    Add test

--- decidim-core/spec/system/session_timeout_spec.rb
+++ decidim-core/spec/system/session_timeout_spec.rb
@@ -10,8 +10,8 @@ describe "Session timeout", type: :system do
 
   context "when session is about to timeout" do
     before do
-      Devise.timeout_in = 2.minutes
-      Decidim.config.session_timeouter_interval = 1000
+      Decidim.expire_session_after = 2.minutes
+      Decidim.session_timeout_interval = 1000
       switch_to_host(organization.host)
       login_as current_user, scope: :user
     end
@@ -19,7 +19,7 @@ describe "Session timeout", type: :system do
     it "timeouts if the user idles for too long" do
       visit decidim.root_path
       travel 1.minute
-      expect(page).to have_content("You were inactive for too long", wait: 3)
+      expect(page).to have_content("You were inactive for too long", wait: 4)
     end
 
     it "doesnt timeout when user is active in another window" do
@@ -29,14 +29,22 @@ describe "Session timeout", type: :system do
       switch_to_window(open_new_window)
       visit decidim.root_path
       2.times.each do
-        expect(page).to have_content("If you continue being inactive", wait: 3)
+        expect(page).to have_content("If you continue being inactive", wait: 4)
         find("#continueSession").click
         travel 20.seconds
       end
 
       switch_to_window(win1)
-      expect(page).to have_content("If you continue being inactive", wait: 3)
+      expect(page).to have_content("If you continue being inactive", wait: 4)
       find("#continueSession").click
+      expect(page).not_to have_content("You were inactive for too long")
+    end
+
+    it "doesnt timeout user who wants to be remembered" do
+      current_user.remember_created_at = Time.current
+      visit decidim.root_path
+      travel 1.minute
+      sleep 1
       expect(page).not_to have_content("You were inactive for too long")
     end
   end
