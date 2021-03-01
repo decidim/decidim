a47ee48c21bda226911335bec30c2a8591e61dae
Fernando Blat <fernando@blat.es>
2021-03-01 15:14:15 +0100

    Migrate decidim-conferences

---
+++ decidim-conferences/app/packs/entrypoints/decidim_conferences_admin.js
@@ -0,0 +1,4 @@
+import "../src/decidim/conferences/admin/conferences"
+import "../src/decidim/conferences/admin/conference_speakers"
+import "../src/decidim/conferences/admin/conference_invite_form"
+import '../../../../decidim-core/app/packs/src/decidim/slug_form'
