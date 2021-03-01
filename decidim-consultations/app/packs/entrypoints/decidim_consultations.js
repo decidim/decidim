b160be6da87b52a7b95b5d09621c6495f96809fe
Fernando Blat <fernando@blat.es>
2021-03-01 15:20:34 +0100

    Migrate decidim-consultations

---
+++ decidim-consultations/app/packs/entrypoints/decidim_consultations.js
@@ -0,0 +1,7 @@
+import '../src/decidim/consultations/show_more'
+import '../src/decidim/consultations/utils'
+import '../src/decidim/consultations/utils_multiple'
+import '../src/decidim/consultations/vote_dialog'
+import '../../../../decidim-core/app/packs/src/decidim/filters'
+import '../../../../decidim-core/app/packs/src/decidim/results_listing'
+import '../../../../decidim-core/app/packs/src/decidim/slug_form'
