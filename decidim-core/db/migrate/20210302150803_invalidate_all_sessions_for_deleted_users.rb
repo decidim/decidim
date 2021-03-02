201acee89adcc7b52316e1048e663fbfe186577a
Marc Riera Casals <mrc2407@gmail.com>
2021-03-02 16:15:57 +0100

    Lint code

--- decidim-core/db/migrate/20210302150803_invalidate_all_sessions_for_deleted_users.rb
+++ decidim-core/db/migrate/20210302150803_invalidate_all_sessions_for_deleted_users.rb
@@ -4,9 +4,7 @@ class InvalidateAllSessionsForDeletedUsers < ActiveRecord::Migration[5.2]
   def up
     Decidim::User.reset_column_information
 
-    Decidim::User.where.not(deleted_at: nil).find_each do |user|
-      user.invalidate_all_sessions!
-    end
+    Decidim::User.where.not(deleted_at: nil).find_each(&:invalidate_all_sessions!)
   end
 
   def down; end
