c401cfe32e12fd14c19fa6ffa260b60dc7ae973c
Fernando Blat <fernando@blat.es>
2021-03-03 06:58:48 +0100

    Migrate initiatives

---
+++ decidim-initiatives/app/packs/entrypoints/decidim_initiatives_admin.js
@@ -0,0 +1,2 @@
+import '../src/decidim/initiatives/admin/initiatives_types'
+import '../src/decidim/initiatives/admin/invite_users'
---
+++ decidim-initiatives/app/packs/src/decidim/initiatives/application.js
@@ -0,0 +1,1 @@
+import '../../../../../../decidim-core/app/packs/src/decidim/editor'
