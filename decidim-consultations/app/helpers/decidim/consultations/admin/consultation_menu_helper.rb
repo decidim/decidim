1202cf5fce004f0d213096d844888cd86c4fd704
Alexandru-Emil Lupu <contact@alecslupu.ro>
2021-03-03 15:55:30 +0200

    Migrate de Consultation/Question menu to MenuRegistry

--- decidim-consultations/app/helpers/decidim/consultations/admin/consultation_menu_helper.rb
+++ decidim-consultations/app/helpers/decidim/consultations/admin/consultation_menu_helper.rb
@@ -6,6 +6,18 @@ module Decidim
       module ConsultationMenuHelper
         include Decidim::Admin::SidebarMenuHelper
 
+        def admin_consultation_components_menu
+          @admin_consultation_components_menu ||= simple_menu(:admin_consultation_components_menu)
+        end
+
+        def admin_questions_menu
+          @admin_questions_menu ||= simple_menu(:admin_questions_menu)
+        end
+
+        def admin_consultation_question_menu
+          @admin_consultation_question_menu ||= sidebar_menu(:admin_consultation_question_menu)
+        end
+
         def admin_consultation_menu
           @admin_consultation_menu ||= sidebar_menu(:admin_consultation_menu)
         end
