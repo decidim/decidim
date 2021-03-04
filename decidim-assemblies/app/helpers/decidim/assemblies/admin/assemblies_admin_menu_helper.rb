97d5cc13ba365b17219f42985367669641ba98cb
Alexandru-Emil Lupu <contact@alecslupu.ro>
2021-03-04 22:49:22 +0200

    Migrate the assembly menus to Menu registry

--- decidim-assemblies/app/helpers/decidim/assemblies/admin/assemblies_admin_menu_helper.rb
+++ decidim-assemblies/app/helpers/decidim/assemblies/admin/assemblies_admin_menu_helper.rb
@@ -6,6 +6,18 @@ module Decidim
       module AssembliesAdminMenuHelper
         include Decidim::Admin::SidebarMenuHelper
 
+        def assemblies_admin_attachments_menu
+          @assemblies_admin_attachments_menu ||= simple_menu(:assemblies_admin_attachments_menu)
+        end
+
+        def admin_assemblies_components_menu
+          @admin_assemblies_components_menu ||= simple_menu(:admin_assemblies_components_menu)
+        end
+
+        def assemblies_admin_menu
+          @assemblies_admin_menu ||= sidebar_menu(:assemblies_admin_menu)
+        end
+
         def admin_assemblies_menu
           @admin_assemblies_menu ||= sidebar_menu(:admin_assemblies_menu)
         end
