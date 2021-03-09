1aa6b4d00ec85f35a3fb7f401783cedae53e7ccf
Marc Riera Casals <mrc2407@gmail.com>
2021-03-09 10:22:18 +0100

    Generalize solution for all announcements

---
+++ decidim-dev/lib/decidim/dev/test/rspec_support/cell_matchers.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Decidim
+  module CellMatchers
+    RSpec::Matchers.define :render_nothing do |_expected_value|
+      match do |actual_value|
+        expect(actual_value).to have_no_selector("html")
+      end
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include Decidim::CellMatchers, type: :cell
+end
