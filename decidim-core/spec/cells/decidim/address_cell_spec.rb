b0d9d24a085a609a699accd3ae5283248488ec6b
Marc Riera <mrc2407@gmail.com>
2021-03-10 08:36:44 +0100

    Sanitize address inputs (#7573)

--- decidim-core/spec/cells/decidim/address_cell_spec.rb
+++ decidim-core/spec/cells/decidim/address_cell_spec.rb
@@ -6,33 +6,30 @@ describe Decidim::AddressCell, type: :cell do
   subject { my_cell.call }
 
   let(:my_cell) { cell("decidim/address", model) }
-  let(:address) { "Carrer de Pepe Rubianes, 1" }
+  let(:address_text) { "Foo bar Street, 1" }
+  let(:js_alert) { "<script>alert(1)</script>" }
+  let(:address) { "#{address_text}#{js_alert}" }
   let(:latitude) { 41.378481 }
   let(:longitude) { 2.1879618 }
   let(:model) { create(:dummy_resource, address: address, latitude: latitude, longitude: longitude) }
+  let(:hint_text) { "Lorem ipsum dolor sit amet consectetur" }
+  let(:location_hints) { "#{hint_text}#{js_alert}" }
+  let(:location_text) { "This is my location" }
+  let(:location) { "#{location_text}#{js_alert}" }
 
   let(:icondata_address) { subject.find(".card__icondata--address") }
 
-  context "when rendering a model with address" do
-    it "renders a resource address" do
-      expect(icondata_address).to have_content(model.address)
-      expect(icondata_address).to have_no_content(model.latitude)
-      expect(icondata_address).to have_no_content(model.longitude)
-    end
+  before do
+    allow(model).to receive(:location_hints).and_return location_hints
+    allow(model).to receive(:location).and_return location
   end
 
-  context "when rendering a model with location hints" do
-    let(:location_hints) { "Lorem ipsum dolor sit amet consectetur" }
-
-    before do
-      allow(model).to receive(:location_hints).and_return location_hints
-    end
-
-    it "renders a resource location_hints" do
-      expect(icondata_address).to have_content(model.address)
-      expect(icondata_address).to have_content(model.location_hints)
-      expect(icondata_address).to have_no_content(model.latitude)
-      expect(icondata_address).to have_no_content(model.longitude)
-    end
+  it "renders a resource address and related fields" do
+    expect(icondata_address).to have_content(address_text)
+    expect(icondata_address).to have_content(hint_text)
+    expect(icondata_address).to have_content(location_text)
+    expect(icondata_address.to_s).not_to match("<script>")
+    expect(icondata_address).to have_no_content(model.latitude)
+    expect(icondata_address).to have_no_content(model.longitude)
   end
 end
