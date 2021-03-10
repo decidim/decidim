d8eb4cdbec91a5f4302427953e1dbe77388d629b
Marc Riera Casals <mrc2407@gmail.com>
2021-03-10 10:52:25 +0100

    Bump versions

--- decidim-api/lib/decidim/api/version.rb
+++ decidim-api/lib/decidim/api/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-api version.
   module Api
     def self.version
-      "0.24.0.rc1"
+      "0.24.0.rc2"
     end
   end
 end
--- decidim-core/lib/decidim/core/version.rb
+++ decidim-core/lib/decidim/core/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-core version.
   module Core
     def self.version
-      "0.24.0.rc1"
+      "0.24.0.rc2"
     end
   end
 end
