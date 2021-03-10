50bb0309e77ce0e6262fcd439731cc9e17bbe8ed
Vera Rojman <vrojman@protonmail.com>
2021-03-10 15:36:00 +0100

    Use first photo attachment as card image for meetings

--- decidim-meetings/app/cells/decidim/meetings/meeting_m_cell.rb
+++ decidim-meetings/app/cells/decidim/meetings/meeting_m_cell.rb
@@ -31,6 +31,14 @@ module Decidim
 
       private
 
+      def resource_image_path
+        model.photo&.url
+      end
+
+      def has_image?
+        true
+      end
+
       def spans_multiple_dates?
         start_date != end_date
       end
