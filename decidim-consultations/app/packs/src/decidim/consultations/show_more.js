265a9caebf6c3a6d30735f8a734c6b22d68a0b1f
Fernando Blat <fernando@blat.es>
2021-03-12 10:19:59 +0100

    Typo

--- decidim-consultations/app/packs/src/decidim/consultations/show_more.js
+++ decidim-consultations/app/packs/src/decidim/consultations/show_more.js
@@ -1,12 +1,10 @@
 /* eslint-disable no-invalid-this */
 
-(function() {
-  $(() => {
+$(() => {
 
-    $(".show-more").on("click", function() {
-      $(this).hide();
-      $(".show-more-panel").removeClass("hide");
-    });
+  $(".show-more").on("click", function() {
+    $(this).hide();
+    $(".show-more-panel").removeClass("hide");
+  });
 
-  })
-}(window));
+})
