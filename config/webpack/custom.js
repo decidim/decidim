f5e301a310e6134c8300d370d5c551fc902b23d7
Fernando Blat <fernando@blat.es>
2021-03-15 17:04:29 +0100

    Fix load order to bind tributes properly

--- config/webpack/custom.js
+++ config/webpack/custom.js
@@ -19,7 +19,7 @@ module.exports = {
       },
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        exclude: /node_modules\/(?!tributejs)/,
         loader: "babel-loader"
       },
       {
