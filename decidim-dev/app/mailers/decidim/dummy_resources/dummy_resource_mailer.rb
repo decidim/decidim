291972d76046abcc7ae77e18dc07799623b2e844
lahdeero <eero.lahdenpera@gmail.com>
2021-03-18 13:21:14 +0200

    Mailer can set reply_to

--- decidim-dev/app/mailers/decidim/dummy_resources/dummy_resource_mailer.rb
+++ decidim-dev/app/mailers/decidim/dummy_resources/dummy_resource_mailer.rb
@@ -3,11 +3,17 @@
 module Decidim
   module DummyResources
     class DummyResourceMailer < ApplicationMailer
-      def send_email(user, organization)
+      def send_email(user, organization, subject, reply_to)
         @user = user
         @organization = organization
 
-        mail(to: "#{user.name} <#{user.email}>") do |format|
+        args = { to: "#{user.name} <#{user.email}>" }
+        args[:subject] = subject if subject
+        args[:reply_to] = reply_to if reply_to
+
+        mail(
+          args
+        ) do |format|
           format.text { "This is the test" }
           format.html { "<p>This is a mail </p>" }
         end
