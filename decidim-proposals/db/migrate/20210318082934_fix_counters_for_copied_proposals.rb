b13a3830e8067d70157bd4352679ed25f491d49a
Marc Riera Casals <mrc2407@gmail.com>
2021-03-18 13:44:57 +0100

    Lint file

--- decidim-proposals/db/migrate/20210318082934_fix_counters_for_copied_proposals.rb
+++ decidim-proposals/db/migrate/20210318082934_fix_counters_for_copied_proposals.rb
@@ -3,11 +3,11 @@
 class FixCountersForCopiedProposals < ActiveRecord::Migration[5.2]
   def up
     copies_ids = Decidim::ResourceLink
-      .where(
-        name: "copied_from_component",
-        from_type: "Decidim::Proposals::Proposal",
-        to_type: "Decidim::Proposals::Proposal"
-      ).pluck(:to_id)
+                 .where(
+                   name: "copied_from_component",
+                   from_type: "Decidim::Proposals::Proposal",
+                   to_type: "Decidim::Proposals::Proposal"
+                 ).pluck(:to_id)
 
     Decidim::Proposals::Proposal.where(id: copies_ids).find_each do |record|
       record.class.reset_counters(record.id, :follows)
