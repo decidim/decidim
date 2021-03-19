129655570f43ecb15ec66a17c1aa293b81cb7d11
Agustí B.R <hola@agusti.cat>
2021-03-19 13:10:47 +0100

    fix lint issues

--- decidim-elections/spec/commands/decidim/votings/census/admin/create_datum_spec.rb
+++ decidim-elections/spec/commands/decidim/votings/census/admin/create_datum_spec.rb
@@ -8,8 +8,9 @@ module Decidim::Votings::Census::Admin
 
     let(:dataset) { create(:dataset, organization: user.organization) }
     let(:user) { create(:user, :admin) }
-    let(:params) { {
-      document_number: document_number,
+    let(:params) do
+      {
+        document_number: document_number,
         document_type: "DNI",
         birthdate: "20010414",
         full_name: "Jane Doe",
@@ -17,17 +18,21 @@ module Decidim::Votings::Census::Admin
         postal_code: "12345",
         mobile_phone_number: "123456789",
         email: email
-     } }
+      }
+    end
 
-    let(:context) { {
-      current_user: user,
-      dataset: dataset,
-      voting: dataset.voting,
-      organization: user.organization} }
+    let(:context) do
+      {
+        current_user: user,
+        dataset: dataset,
+        voting: dataset.voting,
+        organization: user.organization
+      }
+    end
 
     let(:form) { DatumForm.from_params(params).with_context(context) }
 
-    let(:document_number) {"123456789Y"}
+    let(:document_number) { "123456789Y"}
     let(:email) {"example@test.org"}
 
     context "when the form is not valid" do
@@ -54,7 +59,7 @@ module Decidim::Votings::Census::Admin
         .and_call_original
 
       expect { subject.call }.to change(Decidim::ActionLog, :count)
-      action_log = Decidim::ActionLog.where(resource_type:"Decidim::Votings::Census::Datum").last
+      action_log = Decidim::ActionLog.where(resource_type: "Decidim::Votings::Census::Datum").last
       expect(action_log.version).to be_present
       expect(action_log.version.event).to eq "create"
     end
