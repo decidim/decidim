976f23cec3464781072b61e874bc002e666434fe
Fernando Blat <fernando@blat.es>
2021-03-19 07:51:45 +0100

    Fix system assets

--- decidim-system/app/packs/src/decidim/system/application.js
+++ decidim-system/app/packs/src/decidim/system/application.js
@@ -5,13 +5,21 @@ import $ from "jquery"
 import Quill from "quill"
 import "foundation-sites"
 
-import "../../../../../../decidim-core/app/packs/src/decidim/editor"
+import createQuillEditor from "../../../../../../decidim-core/app/packs/src/decidim/editor"
+import Configuration from "../../../../../../decidim-core/app/packs/src/decidim/configuration"
+
+window.Decidim = window.Decidim || {};
+window.Decidim.config = new Configuration()
+
 import "../../../../../../decidim-core/app/packs/src/decidim/input_tags"
-import "../../../../../../decidim-core/app/packs/src/decidim/configuration"
 import "../../../../../../decidim-core/app/packs/src/decidim/confirm"
 
 $(() => {
   $(document).foundation();
+
+  $(".editor-container").each((_idx, container) => {
+    createQuillEditor(container);
+  });
 });
 
 // This needs to be loaded after confirm dialog to bind properly
