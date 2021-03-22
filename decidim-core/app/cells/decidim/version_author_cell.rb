8dd327d4a66dbe98059c333f9f52ece9be522021
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-03-22 12:34:55 +0200

    Fix ARIA roles for the SVG images (icons)

--- decidim-core/app/cells/decidim/version_author_cell.rb
+++ decidim-core/app/cells/decidim/version_author_cell.rb
@@ -2,8 +2,17 @@
 
 module Decidim
   class VersionAuthorCell < Decidim::ViewModel
+    include Decidim::SanitizeHelper
+
     def author
       model
+    end
+
+    def author_name
+      return nil unless author
+      return t(".deleted") if author.deleted?
+
+      author.name
     end
   end
 end
