e933eb5d3e4076c163c11b97a4e3eb7a37511bec
Fernando Blat <fernando@blat.es>
2021-03-23 12:16:08 +0100

    Adapt part of the design app

---
+++ decidim_app-design/app/packs/entrypoints/public.js
@@ -0,0 +1,14 @@
+import $ from "jquery"
+import "core-js/stable";
+
+// = require decidim
+import 'evote_check_nota'
+// = require_tree ./vizzs
+// */
+
+
+// This needs to be loaded after confirm dialog to bind properly
+import Rails from "@rails/ujs"
+Rails.start()
+
+
