86d0a4a33fe197cf2298d2418e83585342ff20d3
Leonardo Diez <leiodd@gmail.com>
2021-03-23 16:26:35 +0100

    docs: apply suggestions from review

--- .github/workflows/dependencies.sh
+++ .github/workflows/dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# This scripts uses the .gemspec files to gather all the dependencies for each decidim module in the repo.
+# This script gathers all the dependencies for each decidim module in the repository.
+# Run it located on the root folder of the repo to get the list of paths to add to each workflow.
 # The output must be manually added to each module workflow, in the on.pull_request.paths zone.
 
 for module in $(ls | grep decidim-)
