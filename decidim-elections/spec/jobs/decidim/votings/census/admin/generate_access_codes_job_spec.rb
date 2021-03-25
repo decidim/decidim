fcaa996e01ee139a7c4a89f8d48997272817b22f
Andrea Orler <andrea@codegram.com>
2021-03-25 09:58:57 +0100

    fix: adapt ot changes in base branch

--- decidim-elections/spec/jobs/decidim/votings/census/admin/generate_access_codes_job_spec.rb
+++ decidim-elections/spec/jobs/decidim/votings/census/admin/generate_access_codes_job_spec.rb
@@ -25,7 +25,7 @@ describe Decidim::Votings::Census::Admin::GenerateAccessCodesJob do
       end
 
       context "when the dataset is not in the correct status" do
-        let(:dataset) { create(:dataset, organization: organization, status: :codes_generated) }
+        let(:dataset) { create(:dataset, status: :codes_generated) }
 
         it "does not update the dataset nor the data" do
           expect(Decidim::Votings::Census::Admin::UpdateDataset).not_to receive(:call)
