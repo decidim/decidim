239e3afaace34029aa60acbf5d35f5da6b77daea
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-03-25 13:01:57 +0200

    Strip the `<p>` tags from inside the heading elements

--- decidim-core/app/cells/decidim/content_blocks/sub_hero_cell.rb
+++ decidim-core/app/cells/decidim/content_blocks/sub_hero_cell.rb
@@ -11,6 +11,16 @@ module Decidim
 
         render
       end
+
+      private
+
+      def organization_description
+        desc = decidim_sanitize(translated_attribute(current_organization.description))
+
+        # Strip the surrounding paragraph tag because it is not allowed within
+        # a <hN> element.
+        desc.sub(/^<p>/, "").sub(%r{</p>$}, "")
+      end
     end
   end
 end
