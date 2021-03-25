5ff4e80855c917d04019ecb3a62a0830225f2232
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-03-25 21:50:28 +0200

    Add the accessibility shared examples

---
+++ decidim-dev/lib/decidim/dev/test/rspec_support/accessibility_examples.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+shared_examples_for "accessible page" do
+  it "passes accessibility tests" do
+    expect(page).to be_axe_clean
+  end
+
+  it "passes HTML validation" do
+    # Capybara is stripping the doctype out of the HTML which is required for
+    # the validation. If it doesn't exist, add it there.
+    html = page.source
+    html = "<!DOCTYPE html>\n#{html}" unless html.strip.match?(/^<!DOCTYPE/i)
+
+    # This would cause a validation error (added by Foundation JS).
+    html = html.gsub(/<meta class="foundation-mq">/, "")
+
+    expect(html).to be_valid_html
+  end
+end
