aafd76cb3710fd6a03884deaaff5ef59e991d324
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-03-26 18:56:09 +0200

    Modify the view_context_class instead
    If we override the `view_context` method on the controller, the
    view assignment variables won't be available because the view
    context won't be re-created after the view action.
    
    This ensures the view assignments are available while keeping the
    controller class untouched.

--- decidim-core/lib/decidim/core/test/shared_examples/controller_render_views.rb
+++ decidim-core/lib/decidim/core/test/shared_examples/controller_render_views.rb
@@ -3,13 +3,29 @@
 shared_context "with controller rendering the view" do
   # Fix "No route matches" errors with the view.
   before do
-    # Needed for the form_for to work (through decidim_form_for)
-    # The path shouldn't matter in the controller specs.
-    allow(controller.view_context).to receive(:polymorphic_path).and_return("/")
+    view_context_class = controller.view_context_class
+    view_context_class.class_eval do
+      # Needed for the form_for to work (through decidim_form_for)
+      # The path shouldn't matter in the controller specs.
+      def polymorphic_path(_record, _options)
+        "/"
+      end
 
-    # Needed for the head and link_to helpers to work
-    # The URL shouldn't matter in the controller specs.
-    allow(controller.view_context).to receive(:url_for).and_return("/")
+      # Needed for the head and link_to helpers to work
+      # The URL shouldn't matter in the controller specs.
+      def url_for(_options)
+        "/"
+      end
+    end
+
+    allow(controller).to receive(:view_context_class).and_return(view_context_class)
+  end
+
+  after do
+    # Ensure that the customized view context class is not being kept in the
+    # cache variable. Otherwise we might mess up the following specs using the
+    # same controller in the same run.
+    controller.class.remove_instance_variable(:@view_context_class)
   end
 
   # Rendering of the view is necessary to see the view renders correctly
