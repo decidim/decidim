14f9b8e922c3a8566b5674e8fc0483e05835bd89
Marc Riera <mrc2407@gmail.com>
2021-03-26 08:58:17 +0100

    Fix report mailers when author is a meeting (#7683)

--- decidim-core/spec/mailers/reported_mailer_spec.rb
+++ decidim-core/spec/mailers/reported_mailer_spec.rb
@@ -98,6 +98,18 @@ module Decidim
             expect(email_body(mail)).to match(author.name)
           end
         end
+
+        context "when the author is a meeting" do
+          let(:meetings_component) { create :component, manifest_name: :meetings, organization: reportable.organization }
+          let!(:meeting) { create :meeting, component: meetings_component }
+
+          it "includes the title of the meeting" do
+            reportable.coauthorships.destroy_all
+            create :coauthorship, coauthorable: reportable, author: meeting
+
+            expect(email_body(mail)).to match(translated(meeting.title))
+          end
+        end
       end
     end
 
