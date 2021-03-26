f79259b20673d69470d73c348abd00f4a0ad034a
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-03-26 16:45:54 +0200

    Do not modify the controller class in the shared context

--- decidim-core/lib/decidim/core/test/shared_examples/controller_render_views.rb
+++ decidim-core/lib/decidim/core/test/shared_examples/controller_render_views.rb
@@ -3,21 +3,17 @@
 shared_context "with controller rendering the view" do
   # Fix "No route matches" errors with the view.
   before do
-    controller.class_eval do
-      helper_method :polymorphic_path, :url_for
+    # Needed for the form_for to work (through decidim_form_for)
+    # The path shouldn't matter in the controller specs.
+    allow(controller.view_context).to receive(:polymorphic_path).and_return("/")
 
-      # Needed for the form_for to work (through decidim_form_for)
-      # The path shouldn't matter in the controller specs.
-      def polymorphic_path(_record, _options)
-        "/"
-      end
+    # Needed for the head and link_to helpers to work
+    # The URL shouldn't matter in the controller specs.
+    allow(controller.view_context).to receive(:url_for).and_return("/")
+  end
 
-      # Needed for the head and link_to helpers to work
-      # The URL shouldn't matter in the controller specs.
-      def url_for(_options)
-        "/"
-      end
-    end
+  after do
+    controller.class_eval { clear_helpers }
   end
 
   # Rendering of the view is necessary to see the view renders correctly
