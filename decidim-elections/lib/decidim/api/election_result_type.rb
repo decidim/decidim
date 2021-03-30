353c0570db7f919eb68810ac8bdb31b5f59a36d4
Agustí B.R <hola@agusti.cat>
2021-03-30 19:40:10 +0200

    feat(votings): add :polling_station and :answer reference to ElectionResultType

--- decidim-elections/lib/decidim/api/election_result_type.rb
+++ decidim-elections/lib/decidim/api/election_result_type.rb
@@ -7,7 +7,11 @@ module Decidim
       description "A voting result for an answer"
 
       field :id, GraphQL::Types::ID, "The internal ID of this result", null: false
+      field :created_at, Decidim::Core::DateTimeType, "When this result was created", null: true
+      field :updated_at, Decidim::Core::DateTimeType, "When this result was updated", null: true
       field :votes_count, GraphQL::Types::Int, "The vote count", null: false
+      field :answer, Decidim::Elections::ElectionAnswerType, "The answer for this result", null: false
+      field :polling_station, Decidim::Votings::PollingStationType, null: true
     end
   end
 end
--- decidim-elections/spec/types/decidim/elections/election_result_type_spec.rb
+++ decidim-elections/spec/types/decidim/elections/election_result_type_spec.rb
@@ -10,21 +10,53 @@ module Decidim
       include_context "with a graphql class type"
 
       let(:polling_station) { create(:polling_station) }
-      let(:model) { create(:election_result) }
+      let(:model) { create(:election_result, polling_station: polling_station) }
 
       describe "id" do
         let(:query) { "{ id }" }
 
         it "returns all the required fields" do
-          expect(response).to include("id" => model.id.to_s)
+          expect(response["id"]).to eq(model.id.to_s)
         end
       end
 
       describe "votes_count" do
         let(:query) { "{ votesCount }" }
 
-        it "returns the votes for this answer" do
+        it "returns the votes count" do
           expect(response["votesCount"]).to eq(model.votes_count)
+        end
+      end
+
+      describe "createdAt" do
+        let(:query) { "{ createdAt }" }
+
+        it "returns when the voting was created" do
+          expect(response["createdAt"]).to eq(model.created_at.to_time.iso8601)
+        end
+      end
+
+      describe "updatedAt" do
+        let(:query) { "{ updatedAt }" }
+
+        it "returns when the voting was updated" do
+          expect(response["updatedAt"]).to eq(model.updated_at.to_time.iso8601)
+        end
+      end
+
+      describe "answer" do
+        let(:query) { "{ answer { id } }" }
+
+        it "returns the answer for this result" do
+          expect(response["answer"]["id"]).to eq(model.answer.id.to_s)
+        end
+      end
+
+      describe "polling station" do
+        let(:query) { "{ pollingStation { id } }" }
+
+        it "returns the polling station for this result" do
+          expect(response["pollingStation"]["id"]).to eq(model.polling_station.id.to_s)
         end
       end
     end
