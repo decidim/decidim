c711d1e85da82c2e4e5fdd4e3d74d89fa356aaea
Agustí B.R <hola@agusti.cat>
2021-03-30 19:04:37 +0200

    fix(votings): census admin menu item identifier

--- decidim-elections/lib/decidim/votings/census_admin_engine.rb
+++ decidim-elections/lib/decidim/votings/census_admin_engine.rb
@@ -9,7 +9,7 @@ module Decidim
       paths["lib/tasks"] = nil
 
       initializer "decidim_votings.decidim_voting_menu" do
-        Decidim.menu :decidim_voting_menu do |menu|
+        Decidim.menu :admin_voting_menu do |menu|
           menu.add_item :voting_census,
                         I18n.t("census", scope: "decidim.votings.admin.menu.votings_submenu"),
                         decidim_admin_votings.voting_census_path(current_participatory_space),
