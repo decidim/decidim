54d7c210270f321de0eb4710d6caf3be642a89c9
slickepinne <svenjaschaefer@gmail.com>
2021-03-31 14:27:30 +0200

    add rack-attack to avoid attacks on census check post requests

---
+++ decidim-elections/config/initializers/rack_attack.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# if Rails.env.production? || Rails.env.test?
+  require "rack/attack"
+
+  Rails.application.configure do |config|
+    config.middleware.use Rack::Attack
+  end
+
+  # Throttle check census attempts by IP to 6 reqs/minute
+  # Return the IP as a discriminator on POST /check_census requests
+  Rack::Attack.throttle("limit check census data attempts per request by IP", limit: 5, period: 60.seconds) do |request|
+    request.ip if request.path.include?("/check_census") && request.post?
+  end
+# end
