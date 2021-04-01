eea3a52b95f857bc2e86e2d8f06e66072f39e18c
slickepinne <svenjaschaefer@gmail.com>
2021-04-01 18:18:37 +0200

    add organization to trustee migration

--- decidim-elections/db/migrate/20210322164620_remove_index_from_decidim_elections_trustees_name.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveIndexFromDecidimElectionsTrusteesName < ActiveRecord::Migration[5.2]
-  def change
-    remove_index :decidim_elections_trustees, name: "index_decidim_elections_trustees_on_name"
-  end
-end
