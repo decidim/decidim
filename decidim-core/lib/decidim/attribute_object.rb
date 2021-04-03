f2b7f83fd49af7bbb2a3305cd8c78fa6e859fa11
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-04-03 18:07:51 +0300

    Remove unnecessary method

--- decidim-core/lib/decidim/attribute_object.rb
+++ decidim-core/lib/decidim/attribute_object.rb
@@ -7,15 +7,5 @@ module Decidim
     autoload :NestedAttributes, "decidim/attribute_object/nested_attributes"
     autoload :NestedValidator, "decidim/attribute_object/nested_validator"
     autoload :TypeMap, "decidim/attribute_object/type_map"
-
-    def self.model
-      # This provides the super method for the form attributes to provide
-      # backwards compatibility with virtus.
-      Module.new do
-        extend ActiveSupport::Concern
-
-        include Decidim::AttributeObject::Model
-      end
-    end
   end
 end
