327941cc4dff4ff5c43f86f890c4b04685ad016d
slickepinne <svenjaschaefer@gmail.com>
2021-04-05 12:31:19 +0200

    configure rack attack in votings

--- decidim-elections/config/initializers/rack_attack.rb
+++ decidim-elections/config/initializers/rack_attack.rb
@@ -3,13 +3,13 @@
 if Rails.env.production? || Rails.env.test?
   require "rack/attack"
 
-  Rails.application.configure do |config|
-    config.middleware.use Rack::Attack
-  end
-
   # Throttle check census attempts by IP to 6 reqs/minute
   # Return the IP as a discriminator on POST /check_census requests
-  Rack::Attack.throttle("limit check census data attempts per request by IP", limit: 5, period: 60.seconds) do |request|
+  Rack::Attack.throttle(
+    "limit check census data attempts per request by IP",
+    limit: Decidim::Votings.check_census_max_requests,
+    period: Decidim::Votings.throttling_period
+  ) do |request|
     request.ip if request.path.include?("/check_census") && request.post?
   end
 end
