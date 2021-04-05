3c2afd1676c15442380be152c68e6bada8369f11
Svenja Schäfer <svenjaschaefer@gmail.com>
2021-04-05 20:33:25 +0200

    fix: remove vote.js from elections manifest (#7795)

--- decidim-elections/app/assets/config/decidim_elections_manifest.js
+++ decidim-elections/app/assets/config/decidim_elections_manifest.js
@@ -3,7 +3,6 @@
 //= link decidim/elections/trustee/key_ceremony.js
 //= link decidim/elections/trustee/tally.js
 //= link decidim/elections/trustee/trustee_zone.js
-//= link decidim/elections/voter/vote.js
 //= link decidim/elections/voter/casting-vote.js
 //= link decidim/elections/voter/new-vote.js
 //= link decidim/elections/voter/setup-preview.js
