fda32745e5557dc82ff8eae499b2f1220cfa11b8
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-04-06 15:31:21 +0300

    Add categories parent filter to API (#7609)

---
+++ decidim-core/lib/decidim/api/input_filters/category_input_filter.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Core
+    class CategoryInputFilter < BaseInputFilter
+      graphql_name "CategoryFilter"
+      description "A type used for filtering any category objects"
+
+      argument :parent_id,
+               type: [ID, { null: true }],
+               description: "Returns the sub-categories for the given parent category or top-level categories if set to `null`",
+               required: false,
+               prepare: lambda { |parent_id, _ctx|
+                          { parent_id: parent_id }
+                        }
+    end
+  end
+end
