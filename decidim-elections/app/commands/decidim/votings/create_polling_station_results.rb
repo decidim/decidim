56dfb6df60706e4f8773088a3b26afbac9b1afba
Agustí B.R <hola@agusti.cat>
2021-04-07 14:48:39 +0200

    feat(votings): polling station officer blank questions

--- decidim-elections/app/commands/decidim/votings/create_polling_station_results.rb
+++ decidim-elections/app/commands/decidim/votings/create_polling_station_results.rb
@@ -7,6 +7,7 @@ module Decidim
       # Public: Initializes the command.
       #
       # form - A form object with the params.
+      # polling_officer - A polling_officer.
       def initialize(form, polling_officer)
         @form = form
         @polling_officer = polling_officer
@@ -25,6 +26,10 @@ module Decidim
           create_answer_result_for(answer_result)
         end
 
+        form.question_results.each do |question_result|
+          create_question_result_for(question_result)
+        end
+
         broadcast(:ok)
       end
 
@@ -35,10 +40,29 @@ module Decidim
       def create_answer_result_for(answer_result)
         params = {
           decidim_votings_polling_station_id: form.polling_station_id,
+          decidim_elections_election_id: form.election_id,
           votes_count: answer_result.votes_count,
-          decidim_elections_answer_id: answer_result.id
+          decidim_elections_question_id: answer_result.question_id,
+          decidim_elections_answer_id: answer_result.id,
+          result_type: "valid_answer"
         }
 
+        create_result!(params)
+      end
+
+      def create_question_result_for(question_result)
+        params = {
+          decidim_votings_polling_station_id: form.polling_station_id,
+          decidim_elections_election_id: form.election_id,
+          votes_count: question_result.votes_count,
+          decidim_elections_question_id: question_result.id,
+          result_type: "blank_question"
+        }
+
+        create_result!(params)
+      end
+
+      def create_result!(params)
         Decidim.traceability.create!(
           Decidim::Elections::Result,
           polling_officer.user,
--- decidim-elections/app/forms/decidim/votings/answer_result_form.rb
+++ decidim-elections/app/forms/decidim/votings/answer_result_form.rb
@@ -4,14 +4,16 @@ module Decidim
   module Votings
     class AnswerResultForm < Decidim::Form
       attribute :id, Integer
+      attribute :question_id, Integer
       attribute :votes_count, Integer
 
-      validates :id, :votes_count, presence: true
+      validates :id, :question_id, :votes_count, presence: true
       validates :votes_count, numericality: true
 
       def map_model(model)
         answer = model[:answer]
         self.id = answer.id
+        self.question_id = answer.question.id
         self.votes_count = Decidim::Elections::Result.find_by(answer: answer, polling_station: model[:polling_station])
       end
     end
