c233a45cd173802570b3d395ac651ebe4266f4ff
Vera Rojman <vrojman@protonmail.com>
2021-04-19 13:44:40 +0200

    Refactor spec

--- decidim-proposals/spec/system/authorized_comments_spec.rb
+++ decidim-proposals/spec/system/authorized_comments_spec.rb
@@ -13,7 +13,8 @@ describe "Authorized comments", type: :system do
     }
   end
   let(:comment_permission) { { comment: authorization_handlers } }
-  let(:vote_comment_permission) { { vote_comment: authorization_handlers }}
+  let(:vote_comment_permission) { { vote_comment: authorization_handlers } }
+  let(:permissions) { {} }
   let!(:author) { create(:user, :confirmed, organization: organization) }
   let!(:commentable) { create(:proposal, component: component, users: [author]) }
   let!(:organization) { create(:organization) }
@@ -31,34 +32,108 @@ describe "Authorized comments", type: :system do
     organization.available_authorizations = [authorization_handler_name]
     organization.save!
     sign_in user
+    commentable.create_resource_permission(permissions: permissions)
   end
 
-  context "when the proposal requires permissions to comment" do
-    before do
-      commentable.create_resource_permission(permissions: comment_permission)
-      visit resource_path
+  shared_examples_for "allowed to comment" do
+    it do
+      expect(page).not_to have_content("You need to be verified to comment at this moment")
+      expect(page).to have_selector("form.new_comment")
     end
+  end
 
-    it "shows a modal with a warning message" do
+  shared_examples_for "not allowed to comment" do
+    it do
       expect(page).to have_content("You need to be verified to comment at this moment")
     end
   end
 
-  context "when the proposal requires permissions to vote a comment" do
-    before do
-      commentable.create_resource_permission(permissions: vote_comment_permission)
-      visit resource_path
+  shared_examples_for "allowed to vote a comment" do
+    it do
+      within "#comment_#{comments[0].id}" do
+        page.find(".comment__votes--up").click
+      end
+
+      expect(page).to have_selector(".comment__votes--up", text: /1/)
     end
+  end
 
-    it "shows a modal with a warning message" do
-      allow(commentable).to receive(:comments_have_votes?).and_return(true)
-
+  shared_examples_for "not allowed to vote a comment" do
+    it do
       within "#comment_#{comments[0].id}" do
         page.find(".comment__votes--up").click
       end
 
       expect(page).to have_selector(".comment__votes--up", text: /0/)
       expect(page).to have_content("Authorization required")
+    end
+  end
+
+  shared_context "with restricted comment action" do
+    let(:permissions) { comment_permission }
+    before do
+      visit resource_path
+    end
+  end
+
+  shared_context "with restricted vote_comment action" do
+    let(:permissions) { vote_comment_permission }
+    before do
+      visit resource_path
+    end
+  end
+
+  shared_context "with restricted comment and vote_comment action" do
+    let(:permissions) { comment_permission.merge(vote_comment_permission) }
+    before do
+      visit resource_path
+    end
+  end
+
+  context "when the proposal has no restriction on commenting and voting comments" do
+    before do
+      visit resource_path
+    end
+
+    it_behaves_like "allowed to comment"
+    it_behaves_like "allowed to vote a comment"
+  end
+
+  context "when the proposal has restrictions on commenting and/or voting comments" do
+    context "and user is not verified" do
+      include_context "with restricted comment action" do
+        it_behaves_like "not allowed to comment"
+        it_behaves_like "allowed to vote a comment"
+      end
+
+      include_context "with restricted vote_comment action" do
+        it_behaves_like "allowed to comment"
+        it_behaves_like "not allowed to vote a comment"
+      end
+
+      include_context "with restricted comment and vote_comment action" do
+        it_behaves_like "not allowed to comment"
+        it_behaves_like "not allowed to vote a comment"
+      end
+    end
+
+    context "and user is verified" do
+      let!(:authorization) { create(:authorization, user: user, name: "dummy_authorization_handler") }
+
+      include_context "with restricted comment action" do
+        it_behaves_like "allowed to comment"
+        it_behaves_like "allowed to vote a comment"
+      end
+
+      include_context "with restricted vote_comment action" do
+        it_behaves_like "allowed to comment"
+        it_behaves_like "allowed to vote a comment"
+      end
+
+      include_context "with restricted comment and vote_comment action" do
+        it_behaves_like "allowed to comment"
+        it_behaves_like "allowed to vote a comment"
+      end
     end
   end
 end
