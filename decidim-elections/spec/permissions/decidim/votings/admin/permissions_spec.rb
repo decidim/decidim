1196fb259cbee3b07e4f0c076d2d1b5fc1d4f60f
Andrea Orler <orlera@users.noreply.github.com>
2021-04-21 14:40:09 +0200

    Give admin panel access to monitoring committee members (#7843)

--- decidim-elections/spec/permissions/decidim/votings/admin/permissions_spec.rb
+++ decidim-elections/spec/permissions/decidim/votings/admin/permissions_spec.rb
@@ -40,8 +40,8 @@ describe Decidim::Votings::Admin::Permissions do
   end
 
   describe "components" do
-    let(:action_subject) { :component }
-    let(:action_name) { :manage }
+    let(:action_subject) { :components }
+    let(:action_name) { :read }
     let(:extra_context) do
       { participatory_space: voting }
     end
@@ -72,6 +72,80 @@ describe Decidim::Votings::Admin::Permissions do
       let(:action_name) { :manage }
 
       it { is_expected.to eq true }
+    end
+  end
+
+  describe "monitoring committee" do
+    context "when the user is an admin" do
+      context "when reading the Committee Members" do
+        let(:action_subject) { :monitoring_committee_members }
+        let(:action_name) { :read }
+
+        it { is_expected.to eq true }
+      end
+
+      context "when reading the Certificates" do
+        let(:action_subject) { :monitoring_committee_certificates }
+        let(:action_name) { :read }
+
+        it { is_expected.to eq false }
+      end
+    end
+
+    context "when the user is a Monitoring Committee Member" do
+      let(:user) { create :user, organization: organization }
+      let!(:monitoring_committee_member) { create(:monitoring_committee_member, user: user, voting: voting) }
+
+      context "when accessing the admin panel" do
+        let(:action_subject) { :admin_dashboard }
+        let(:action_name) { :read }
+
+        it { is_expected.to eq true }
+      end
+
+      context "when reading the votings" do
+        let(:action_subject) { :votings }
+        let(:action_name) { :read }
+
+        it { is_expected.to eq true }
+      end
+
+      context "when listing their voting" do
+        let(:action_subject) { :voting }
+        let(:action_name) { :list }
+
+        it { is_expected.to eq true }
+      end
+
+      context "when listing another voting" do
+        let(:other_voting) { create(:voting, organization: organization) }
+        let!(:monitoring_committee_member) { create(:monitoring_committee_member, user: user, voting: other_voting) }
+        let(:action_subject) { :voting }
+        let(:action_name) { :list }
+
+        it { is_expected.to eq false }
+      end
+
+      context "when reading their voting's Polling Officers" do
+        let(:action_subject) { :polling_officers }
+        let(:action_name) { :read }
+
+        it { is_expected.to eq false }
+      end
+
+      context "when reading their voting's Committee Members" do
+        let(:action_subject) { :monitoring_committee_members }
+        let(:action_name) { :read }
+
+        it { is_expected.to eq false }
+      end
+
+      context "when reading their voting's Certificates" do
+        let(:action_subject) { :monitoring_committee_certificates }
+        let(:action_name) { :read }
+
+        it { is_expected.to eq true }
+      end
     end
   end
 
