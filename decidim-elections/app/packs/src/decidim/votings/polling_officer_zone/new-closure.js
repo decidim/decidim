52fe051a306e1d9474c83b516a379b920f9d5cf0
Agustí B.R <hola@agusti.cat>
2021-04-22 10:39:37 +0200

    feat(votings/elections): PollingStationClosure and BulletinBoardClosure

--- decidim-elections/app/packs/src/decidim/votings/polling_officer_zone/new-closure.js
+++ decidim-elections/app/packs/src/decidim/votings/polling_officer_zone/new-closure.js
@@ -1,17 +1,34 @@
 $(() => {
+  const $submitBtn = $("#submit-verify-votes");
+  const $modalBtn = $("#btn-modal-closure-count-error");
+  const $totalBallotsInput = $("#envelopes_result_total_ballots_count");
+  const $electionVotesInput = $("#envelopes_result_election_votes_count");
+
+  const checkValues = () => {
+    const totalBallotsInputValue = parseInt($totalBallotsInput.val(), 10);
+    const electionVotesInputValue = parseInt($electionVotesInput.val(), 10);
+
+
+    if (totalBallotsInputValue === electionVotesInputValue) {
+      $submitBtn.removeClass("disabled hide");
+      $modalBtn.addClass("hide");
+    } else {
+      $submitBtn.addClass("hide");
+      $modalBtn.removeClass("hide");
+    }
+  };
+
+  $totalBallotsInput.on("blur", checkValues);
+
   $("#submit-verify-votes").addClass("disabled");
 
-  $("#envelopes_result_total_ballots_count").on("keyup", function() {
-    $("#modal-total-ballots-value").html($("#envelopes_result_total_ballots_count").val());
+  $totalBallotsInput.on("keyup", function() {
+    $("#modal-total-ballots-value").html(parseInt($totalBallotsInput.val(), 10));
   });
 
   $("#envelopes_result_polling_officer_notes").on("keyup", function() {
     let modalPollingOfficerNotes = $("#envelopes_result_polling_officer_notes").val()
 
-    if (modalPollingOfficerNotes.length > 0) {
-      $("#btn-submit-from-modal").removeClass("disabled");
-    } else {
-      $("#btn-submit-from-modal").addClass("disabled");
-    }
+    $("#btn-submit-from-modal").toggleClass("disabled", !modalPollingOfficerNotes.trim());
   });
 });
