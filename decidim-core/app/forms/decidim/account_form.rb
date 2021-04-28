a6caa09d50347eb029757efcf945b03406979a66
Marc Riera Casals <mrc2407@gmail.com>
2021-04-28 14:20:31 +0200

    Fix constant name

--- decidim-core/app/forms/decidim/account_form.rb
+++ decidim-core/app/forms/decidim/account_form.rb
@@ -20,7 +20,7 @@ module Decidim
 
     validates :name, presence: true
     validates :email, presence: true, 'valid_email_2/email': { disposable: true }
-    validates :nickname, presence: true, format: Decidim::UserBaseEntity::REGEXP_NICKNAME
+    validates :nickname, presence: true, format: Decidim::User::REGEXP_NICKNAME
 
     validates :nickname, length: { maximum: Decidim::User.nickname_max_length, allow_blank: true }
     validates :password, confirmation: true
