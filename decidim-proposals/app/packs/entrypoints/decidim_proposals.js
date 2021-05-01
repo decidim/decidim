e89fc94d0e2f536eff926d37db26caf40070e50f
Fernando Blat <fernando@blat.es>
2021-05-01 08:36:31 +0200

    Fix images requires

--- decidim-proposals/app/packs/entrypoints/decidim_proposals.js
+++ decidim-proposals/app/packs/entrypoints/decidim_proposals.js
@@ -2,8 +2,7 @@ import "src/decidim/proposals/utils"
 import "src/decidim/proposals/add_proposal"
 
 // Images
-require.context("../images/decidim/proposals", true)
-require.context("../images/decidim/gamification", true)
+require.context("../images", true)
 
 // Documents
 require.context("../documents", true)
