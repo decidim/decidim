c5a5b181dac34d2a3c2afcf2a5494f0fbbcb532f
Eduardo Martínez <eduardomech@gmail.com>
2021-05-04 22:51:51 +0200

    Add system tests for public attending participants

--- decidim-meetings/spec/system/meeting_registrations_spec.rb
+++ decidim-meetings/spec/system/meeting_registrations_spec.rb
@@ -178,6 +178,8 @@ describe "Meeting registrations", type: :system do
 
             within "#meeting-registration-confirm-#{meeting.id}" do
               expect(page).to have_content "A legal text"
+              expect(page).to have_content "Show my attendance publicly"
+              expect(page).to have_field("public_participation", checked: false)
               page.find(".button.expanded").click
             end
 
@@ -186,6 +188,33 @@ describe "Meeting registrations", type: :system do
             expect(page).to have_css(".button", text: "GOING")
             expect(page).to have_text("19 slots remaining")
             expect(page).to have_text("Stop following")
+            expect(page).to have_no_text("ATTENDING PARTICIPANTS")
+            expect(page).to have_no_css("#list-of-public-participants")
+          end
+
+          it "they can join the meeting and configure their participation to be shown publicly" do
+            visit_meeting
+
+            within ".card.extra" do
+              click_button "Join meeting"
+            end
+
+            within "#meeting-registration-confirm-#{meeting.id}" do
+              expect(page).to have_content "Show my attendance publicly"
+              expect(page).to have_field("public_participation", checked: false)
+              page.find("input#public_participation").click
+              page.find(".button.expanded").click
+            end
+
+            expect(page).to have_content("successfully")
+
+            expect(page).to have_css(".button", text: "GOING")
+            expect(page).to have_text("19 slots remaining")
+            expect(page).to have_text("Stop following")
+            expect(page).to have_text("ATTENDING PARTICIPANTS")
+            within "#list-of-public-participants" do
+              expect(page).to have_text(user.name)
+            end
           end
 
           it "they can join the meeting if they are already following it" do
@@ -199,6 +228,8 @@ describe "Meeting registrations", type: :system do
 
             within "#meeting-registration-confirm-#{meeting.id}" do
               expect(page).to have_content "A legal text"
+              expect(page).to have_content "Show my attendance publicly"
+              expect(page).to have_field("public_participation", checked: false)
               page.find(".button.expanded").click
             end
 
@@ -213,7 +244,7 @@ describe "Meeting registrations", type: :system do
         context "and they ARE part of a verified user group" do
           let!(:user_group) { create :user_group, :verified, users: [user], organization: organization }
 
-          it "they can join the meeting representing a group" do
+          it "they can join the meeting representing a group and appear in the attending organizations list" do
             visit_meeting
 
             within ".card.extra" do
@@ -222,8 +253,12 @@ describe "Meeting registrations", type: :system do
 
             within "#meeting-registration-confirm-#{meeting.id}" do
               expect(page).to have_content "I represent a group"
+              expect(page).to have_content "Show my attendance publicly"
+              expect(page).to have_field("public_participation", checked: false)
+              page.find("input#public_participation").click
               page.find("input#user_group").click
               select user_group.name, from: :join_meeting_user_group_id
+              page.find("input#public_participation").click
               page.find(".button.expanded").click
             end
 
@@ -234,6 +269,8 @@ describe "Meeting registrations", type: :system do
 
             expect(page).to have_text("ATTENDING ORGANIZATIONS")
             expect(page).to have_text(user_group.name)
+            expect(page).to have_no_text("ATTENDING PARTICIPANTS")
+            expect(page).to have_no_css("#list-of-public-participants")
           end
         end
       end
@@ -261,6 +298,8 @@ describe "Meeting registrations", type: :system do
 
           expect(page).to have_i18n_content(questionnaire.title, upcase: true)
           expect(page).to have_i18n_content(questionnaire.description)
+          expect(page).to have_content "Show my attendance publicly"
+          expect(page).to have_field("public_participation", checked: false)
 
           expect(page).to have_no_i18n_content(question.body)
 
