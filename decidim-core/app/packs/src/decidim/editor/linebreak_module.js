420f44e9b53128b50619af88a8504ef6342abf98
lahdeero <eero.lahdenpera@gmail.com>
2021-05-11 11:36:33 +0300

    Override scroll optimize optimize

--- decidim-core/app/packs/src/decidim/editor/linebreak_module.js
+++ decidim-core/app/packs/src/decidim/editor/linebreak_module.js
@@ -12,6 +12,8 @@ Quill.debug("error");
 const Delta = Quill.import("delta");
 const Break = Quill.import("blots/break");
 const Embed = Quill.import("blots/embed");
+const Scroll = Quill.import("blots/scroll");
+const Parchment = Quill.import("parchment");
 Quill.register({"modules/history": HistoryOverride}, true);
 let icons = Quill.import("ui/icons");
 icons.linebreak = "⏎";
@@ -31,6 +33,101 @@ class SmartBreak extends Break {
   }
 }
 Quill.register(SmartBreak);
+
+class ScrollOvderride extends Scroll {
+  optimize(mutations = [], context = {}) {
+    if (this.batch === true) {
+      return;
+    }
+
+    this.parchmentOptimize(mutations, context);
+
+    if (mutations.length > 0) {
+      this.emitter.emit("scroll-optimize", mutations, context);
+    }
+  }
+
+  parchmentOptimize(mutations = [], context = {}) {
+    // super.optimize(context);
+    Reflect.apply(Parchment.Container.prototype.optimize, this, [context]);
+
+    // We must modify mutations directly, cannot make copy and then modify
+    // let records = [].slice.call(this.observer.takeRecords());
+    let records = [...this.observer.takeRecords()];
+    // Array.push currently seems to be implemented by a non-tail recursive function
+    // so we cannot just mutations.push.apply(mutations, this.observer.takeRecords());
+    while (records.length > 0) {
+      mutations.push(records.pop());
+    }
+    let mark = (blot, markParent) => {
+      if (!blot || blot === this) {
+        return;
+      }
+      if (!blot.domNode.parentNode) {
+        return;
+      }
+      if (blot.domNode.__blot && blot.domNode.__blot.mutations === null) {
+        blot.domNode.__blot.mutations = [];
+      }
+      if (markParent) {
+        mark(blot.parent);
+      }
+    };
+    let optimize = (blot) => {
+      // Post-order traversal
+      if (
+        blot.domNode.__blot === null &&
+        blot.domNode.__blot.mutations === null
+      ) {
+        return;
+      }
+      if (blot instanceof Parchment.Container) {
+        blot.children.forEach(optimize);
+      }
+      blot.optimize(context);
+    };
+    let remaining = mutations;
+    for (let ind = 0; remaining.length > 0; ind += 1) {
+      // MAX_OPTIMIZE_ITERATIONS = 100
+      if (ind >= 100) {
+        throw new Error("[Parchment] Maximum optimize iterations reached");
+      }
+      remaining.forEach((mutation) => {
+        let blot = Parchment.find(mutation.target, true);
+        if (!blot) {
+          return;
+        }
+        if (blot.domNode === mutation.target) {
+          if (mutation.type === "childList") {
+            mark(Parchment.find(mutation.previousSibling, false));
+
+            mutation.addedNodes.forEach((node) => {
+              let child = Parchment.find(node, false);
+              mark(child, false);
+              if (child instanceof Parchment.Container) {
+                child.children.forEach(function(grandChild) {
+                  mark(grandChild, false);
+                });
+              }
+            });
+          } else if (mutation.type === "attributes") {
+            mark(blot.prev);
+          }
+        }
+        mark(blot);
+      });
+      this.children.forEach(optimize);
+      remaining = [...this.observer.takeRecords()];
+      records = remaining.slice();
+      while (records.length > 0) {
+        mutations.push(records.pop());
+      }
+    }
+  }
+};
+Quill.register("blots/scroll", ScrollOvderride, true);
+Parchment.register(ScrollOvderride);
+
 
 export default function lineBreakButtonHandler(quill) {
   let range = quill.selection.getRange()[0];
