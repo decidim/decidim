ba16228bffc5f01ab1b6da647cc15816b6898f8b
Eduardo Martínez <eduardomech@gmail.com>
2021-05-13 00:29:37 +0200

    Redirect to new organization form if none exists after login as system admin

--- decidim-system/app/controllers/decidim/system/dashboard_controller.rb
+++ decidim-system/app/controllers/decidim/system/dashboard_controller.rb
@@ -3,6 +3,13 @@
 module Decidim
   module System
     class DashboardController < Decidim::System::ApplicationController
+      before_action :check_organizations_presence
+
+      def check_organizations_presence
+        return if Organization.exists?
+
+        redirect_to new_organization_path
+      end
     end
   end
 end
