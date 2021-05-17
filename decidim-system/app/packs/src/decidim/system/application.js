88ab2345b960c9190d1114be18dee8a682d2db1d
Eduardo Martínez <eduardomech@gmail.com>
2021-05-17 17:21:45 +0200

    Move advanced organization settings to a partial and hide by default

--- decidim-system/app/packs/src/decidim/system/application.js
+++ decidim-system/app/packs/src/decidim/system/application.js
@@ -23,6 +23,18 @@ $(() => {
   $(".editor-container").each((_idx, container) => {
     createQuillEditor(container);
   });
+
+  $("button.collapse").on("click", () => {
+    $(".collapsible").addClass("hide");
+    $("button.expand").removeClass("hide");
+    $("button.collapse").addClass("hide");
+  });
+
+  $("button.expand").on("click", () => {
+    $(".collapsible").removeClass("hide");
+    $("button.collapse").removeClass("hide");
+    $("button.expand").addClass("hide");
+  });
 });
 
 // This needs to be loaded after confirm dialog to bind properly
