719aac415cc2e7f64aafd26bd1347ae36941b238
Eduardo Martínez <eduardomech@gmail.com>
2021-05-20 13:02:43 +0200

    Update test

--- decidim-meetings/spec/system/meeting_minutes_spec.rb
+++ decidim-meetings/spec/system/meeting_minutes_spec.rb
@@ -13,21 +13,25 @@ describe "Meeting minutes", type: :system do
     visit resource_locator(meeting).path
   end
 
-  context "when meeting minutes is not visible" do
+  context "when meeting closing is not visible" do
     let(:visible) { false }
 
-    it "the section minutes is not visible" do
+    it "the closing ingformation is not visible" do
       visit_meeting
 
       expect(page).to have_no_content("MEETING MINUTES")
-      expect(page).not_to have_css(".minutes-section")
+      expect(page).to have_no_content("RELATED INFORMATION")
+      expect(page).to have_no_css("div.card.card--secondary.extra")
+      expect(page).to have_no_content("CONTRIBUTIONS COUNT")
+      expect(page).to have_no_content("ATTENDEES COUNT")
+      expect(page).to have_no_content("ATTENDING ORGANIZATIONS")
+      expect(page).to have_no_css("div.minutes-section")
     end
   end
 
   context "when meeting minutes is visible" do
     it "shows the minutes section" do
       visit_meeting
-      expect(page).to have_content("MEETING MINUTES")
       expect(page).to have_css(".minutes-section")
 
       within ".minutes-section" do
@@ -38,14 +42,25 @@ describe "Meeting minutes", type: :system do
       end
     end
 
-    context "and minutes data is missing" do
-      it "hides the section minutes" do
+    it "shows information related with closing action" do
+      visit_meeting
+
+      expect(page).to have_content("MEETING MINUTES")
+      expect(page).to have_css("div.card.card--secondary.extra")
+      expect(page).to have_content("CONTRIBUTIONS COUNT")
+      expect(page).to have_content("ATTENDEES COUNT")
+      expect(page).to have_content("ATTENDING ORGANIZATIONS")
+      expect(page).to have_css("div.minutes-section")
+    end
+
+    context "and audio and video data is missing" do
+      it "hides the minutes section with related information" do
         meeting.update(
           video_url: nil,
           audio_url: nil
         )
         visit_meeting
-        expect(page).to have_no_content("MEETING MINUTES")
+        expect(page).to have_no_content("RELATED INFORMATION")
         expect(page).not_to have_css(".minutes-section")
       end
     end
