4aaaa9790bc9986f1cd79b747c25922a9c6727ba
Alexandru-Emil Lupu <contact@alecslupu.ro>
2021-05-26 06:29:55 +0300

    Wrong Statistics Metrics being computed

---
+++ decidim-comments/db/migrate/20210402124533_add_participatory_process_id.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddParticipatoryProcessId < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :decidim_comments_comments, :decidim_participatory_process,
+                  foreign_key: true,
+                  index: { name: "index_comments_on_participatory_process" }
+  end
+end
