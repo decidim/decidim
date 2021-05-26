d7c3e50e86c4039e27a812e0b9c6597c5a5095ab
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-05-26 21:06:58 +0300

    Configure the entrypoints per gem

--- decidim-api/config/assets.rb
+++ decidim-api/config/assets.rb
@@ -3,3 +3,7 @@
 base_path = File.expand_path("..", __dir__)
 
 Decidim::Webpacker.register_path("#{base_path}/app/packs")
+Decidim::Webpacker.register_entrypoint(
+  :decidim_api_docs,
+  "#{base_path}/app/packs/entrypoints/decidim_api_docs.js"
+)
--- decidim-blogs/config/assets.rb
+++ decidim-blogs/config/assets.rb
@@ -3,3 +3,7 @@
 base_path = File.expand_path("..", __dir__)
 
 Decidim::Webpacker.register_path("#{base_path}/app/packs")
+Decidim::Webpacker.register_entrypoint(
+  :decidim_blogs,
+  "#{base_path}/app/packs/entrypoints/decidim_blogs.js"
+)
