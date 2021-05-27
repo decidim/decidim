cddb2a2f7eefc4bfd229e83f886c0521d6699241
Fernando Blat <fernando@blat.es>
2021-05-27 11:20:14 +0200

    Show answers results

---
+++ decidim-meetings/app/cells/decidim/meetings/question_responses_cell.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Meetings
+    # This cell renders the percentage of responses which chose
+    # the given answer option in a meeting poll question
+    class QuestionResponsesCell < Decidim::ViewModel
+      include ActionView::Helpers::NumberHelper
+
+      def show
+        render
+      end
+
+      private
+
+      def answer_percentage
+        total = answers.count
+        option = answers.where(decidim_answer_option_id: model.id).count
+        "#{number_with_precision((option.to_f / total) * 100, precision: 1, significant: true)}%"
+      end
+
+      def answers
+        @answers ||= Decidim::Meetings::AnswerChoice.joins(:answer).where("#{Decidim::Meetings::Answer.table_name}.decidim_question_id" => model.question.id)
+      end
+    end
+  end
+end
