b6f1688aa8afcc95647453a001bbe6f718874c69
Fernando Blat <fernando@blat.es>
2021-05-31 15:53:07 +0200

    Mock rails assets

---
+++ decidim-generators/lib/decidim/generators/app_templates/assets.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# This class is necessary because Sprockets dependency can't be removed from Decidim
+# until some dependencies upgrade and remove it.
+#
+# List of gems using Sprockets:
+#   - graphiql-rails: PR to remove it from oct 2019 - https://github.com/rmosolgo/graphiql-rails/pull/76
+module Decidim
+  class AssetsToSilenceSprocketsDependency
+    def precompile; [] end
+    def precompile=(value); end
+  end
+end
+
+Rails.application.config.assets = Decidim::AssetsToSilenceSprocketsDependency.new
