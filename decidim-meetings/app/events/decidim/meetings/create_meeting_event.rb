92d8a8fe4cf769e12c42394b60b85d960ce9b958
Fernando Blat <fernando@blat.es>
2021-06-01 05:08:16 +0200

    Add buttons to new meeting event

--- decidim-meetings/app/events/decidim/meetings/create_meeting_event.rb
+++ decidim-meetings/app/events/decidim/meetings/create_meeting_event.rb
@@ -3,8 +3,18 @@
 module Decidim
   module Meetings
     class CreateMeetingEvent < Decidim::Events::SimpleEvent
+      delegate :organization, to: :user, prefix: false
+
       def resource_text
         translated_attribute(resource.description)
+      end
+
+      def button_text
+        I18n.t("meeting_created.button_text", scope: "decidim.events.meetings") if resource.can_be_joined_by?(user)
+      end
+
+      def button_url
+        Decidim::EngineRouter.main_proxy(component).join_meeting_registration_url(meeting_id: resource.id, host: organization.host) if resource.can_be_joined_by?(user)
       end
     end
   end
--- decidim-meetings/spec/events/decidim/meetings/create_meeting_event_spec.rb
+++ decidim-meetings/spec/events/decidim/meetings/create_meeting_event_spec.rb
@@ -40,4 +40,50 @@ describe Decidim::Meetings::CreateMeetingEvent do
       expect(subject.resource_text).to eq translated(resource.description)
     end
   end
+
+  describe "button text" do
+    it "returns the nil" do
+      expect(subject.button_text).to be_nil
+    end
+  end
+
+  describe "button url" do
+    it "returns the nil" do
+      expect(subject.button_url).to be_nil
+    end
+  end
+
+  context "when registration is enabled" do
+    let(:organization) { create :organization }
+    let(:participatory_process) { create :participatory_process, organization: organization }
+    let(:component) { create :component, manifest_name: :meetings, participatory_space: participatory_process }
+
+    let(:registrations_enabled) { true }
+    let(:available_slots) { 10 }
+    let(:questionnaire) { nil }
+
+    let(:resource) do
+      create(:meeting,
+             component: component,
+             registrations_enabled: registrations_enabled,
+             available_slots: available_slots,
+             questionnaire: questionnaire)
+    end
+
+    let(:user) { create :user, :confirmed, organization: organization, email_on_notification: false }
+
+    let(:registration_form) { Decidim::Meetings::JoinMeetingForm.new }
+
+    describe "button text" do
+      it "returns a register to meeting call to action" do
+        expect(subject.button_text).to eq("Register to the meeting")
+      end
+    end
+
+    describe "button url" do
+      it "returns the link to join the meeting" do
+        expect(subject.button_url).to eq(Decidim::EngineRouter.main_proxy(component).join_meeting_registration_url(meeting_id: resource.id, host: organization.host))
+      end
+    end
+  end
 end
