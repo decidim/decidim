a3f129ba83bb0805ace9e247700092aeec3da5b6
leio10 <leiodd@gmail.com>
2021-06-01 10:03:04 +0200

    Use correct newsletter cell for web view (#8081)
    * Use correct newsletter cell for web view (#8025)
    
    * Use correct newsletter cell for web view
    
    * Add spec
    
    * Fix rubocop offenses
    
    * fix: downgrade Rails 6 method used
    
    Co-authored-by: Vera Rojman <vrojman@protonmail.com>

---
+++ decidim-core/spec/system/newsletter_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Newsletters (view in web)", type: :system do
+  let(:organization) { newsletter.organization }
+  let!(:newsletter) { create :newsletter, :sent }
+  let!(:content_block) do
+    content_block = Decidim::ContentBlock.find_by(organization: organization, scope_name: :newsletter_template, scoped_resource_id: newsletter.id)
+    content_block.destroy!
+    content_block = create(
+      :content_block,
+      :newsletter_template,
+      organization: organization,
+      scoped_resource_id: newsletter.id,
+      manifest_name: "image_text_cta",
+      settings: {
+        body: Decidim::Faker::Localized.wrapped("<p>", "</p>") { generate_localized_title },
+        introduction: Decidim::Faker::Localized.wrapped("<p>", "</p>") { generate_localized_title },
+        cta_text: Decidim::Faker::Localized.word,
+        cta_url: I18n.available_locales.map { |locale| [locale, Faker::Internet.url] }.to_h
+      }
+    )
+    content_block
+  end
+
+  before do
+    switch_to_host organization.host
+  end
+
+  describe "accessing the newsletter page" do
+    before do
+      page.visit decidim.newsletter_path(newsletter)
+    end
+
+    it "renders the correct template" do
+      within ".content" do
+        expect(page).to have_link(translated(content_block.settings.cta_text), href: translated(content_block.settings.cta_url))
+      end
+    end
+  end
+end
