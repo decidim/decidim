d4a21d5edacc766a348467c22f9069a96427dde2
Eduardo Martínez <eduardomech@gmail.com>
2021-06-02 09:49:52 +0200

    Add groups as assembly members (#7993)

--- decidim-admin/app/controllers/decidim/admin/organization_controller.rb
+++ decidim-admin/app/controllers/decidim/admin/organization_controller.rb
@@ -30,10 +30,20 @@ module Decidim
       end
 
       def users
+        search(current_organization.users)
+      end
+
+      def user_entities
+        search(current_organization.user_entities)
+      end
+
+      private
+
+      def search(relation)
         respond_to do |format|
           format.json do
             if (term = params[:term].to_s).present?
-              query = current_organization.users.order(name: :asc)
+              query = relation.order(name: :asc)
               query = if term.start_with?("@")
                         query.where("nickname ILIKE ?", "#{term.delete("@")}%")
                       else
--- decidim-core/app/models/decidim/organization.rb
+++ decidim-core/app/models/decidim/organization.rb
@@ -29,6 +29,7 @@ module Decidim
     has_many :admins, -> { where(admin: true) }, foreign_key: "decidim_organization_id", class_name: "Decidim::User"
     has_many :users_with_any_role, -> { where.not(roles: []) }, foreign_key: "decidim_organization_id", class_name: "Decidim::User"
     has_many :users, foreign_key: "decidim_organization_id", class_name: "Decidim::User", dependent: :destroy
+    has_many :user_entities, foreign_key: "decidim_organization_id", class_name: "Decidim::UserBaseEntity", dependent: :destroy
     has_many :oauth_applications, foreign_key: "decidim_organization_id", class_name: "Decidim::OAuthApplication", inverse_of: :organization, dependent: :destroy
     has_many :hashtags, foreign_key: "decidim_organization_id", class_name: "Decidim::Hashtag", dependent: :destroy
 
