38f6557b96535eb32d30c0c8596248484d1e09b1
IvanOrtiz4 <ivanortizp4@gmail.com>
2021-06-02 12:49:11 +0200

    add a message regarding accesibility in color pick

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -1,32 +1,48 @@
 # frozen_string_literal: true
 
-require 'i18n/tasks'
+require "i18n/tasks"
 
-RSpec.describe I18n do
-  let(:i18n) { I18n::Tasks::BaseTask.new }
+describe "I18n sanity" do
+  let(:locales) do
+    ENV["ENFORCED_LOCALES"].presence || "en"
+  end
+
+  let(:i18n) { I18n::Tasks::BaseTask.new(locales: locales.split(",")) }
   let(:missing_keys) { i18n.missing_keys }
   let(:unused_keys) { i18n.unused_keys }
+  let(:non_normalized_paths) { i18n.non_normalized_paths }
   let(:inconsistent_interpolations) { i18n.inconsistent_interpolations }
 
-  it 'does not have missing keys' do
+  it "correct Norwegian locale keys should be surrounded by quotation marks" do
+    # otherwise psych evaluates `no:` to `false`
+    # see https://makandracards.com/makandra/24809-yaml-keys-like-yes-or-no-evaluate-to-true-and-false
+    i18n = I18n::Tasks::BaseTask.new(locales: "no")
+    forest = i18n.data_forest(["no"])
+    stats = i18n.forest_stats(forest)
+    expect(stats[:locales]).to eq("no")
+  end
+
+  it "does not have missing keys" do
     expect(missing_keys).to be_empty,
                             "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
   end
 
-  it 'does not have unused keys' do
+  it "does not have unused keys" do
     expect(unused_keys).to be_empty,
                            "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
   end
 
-  it 'files are normalized' do
-    non_normalized = i18n.non_normalized_paths
-    error_message = "The following files need to be normalized:\n" \
-                    "#{non_normalized.map { |path| "  #{path}" }.join("\n")}\n" \
-                    "Please run `i18n-tasks normalize' to fix"
-    expect(non_normalized).to be_empty, error_message
+  unless ENV["SKIP_NORMALIZATION"]
+    it "is normalized" do
+      error_message = "The following files need to be normalized:\n" \
+                      "#{non_normalized_paths.map { |path| "  #{path}" }.join("\n")}\n" \
+                      "Please run `bundle exec i18n-tasks normalize --locales #{locales}` to fix them"
+
+      expect(non_normalized_paths).to be_empty, error_message
+    end
   end
 
-  it 'does not have inconsistent interpolations' do
+  it "does not have inconsistent interpolations" do
     error_message = "#{inconsistent_interpolations.leaves.count} i18n keys have inconsistent interpolations.\n" \
                     "Run `i18n-tasks check-consistent-interpolations' to show them"
     expect(inconsistent_interpolations).to be_empty, error_message
