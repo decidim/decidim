bef94097e05730a478869469c8900fd420956bbc
Fernando Blat <fernando@blat.es>
2021-06-11 12:52:57 +0200

    Register to meeting via email (#7947)

--- decidim-core/spec/lib/events/email_event_spec.rb
+++ decidim-core/spec/lib/events/email_event_spec.rb
@@ -15,5 +15,33 @@ module Decidim
         expect(subject.types).to include :email
       end
     end
+
+    context "when the event behaves like email event" do
+      subject do
+        TestEvent.new(resource: resource, event_name: "test", user: user)
+      end
+
+      let(:organization) { create(:organization, name: "O'Connor") }
+      let(:user) { create(:user, name: "Sarah Connor", organization: organization) }
+      let(:resource) { user }
+
+      describe ".button_url" do
+        it "responds and returns nil" do
+          expect(subject.button_url).to be_nil
+        end
+      end
+
+      describe ".button_text" do
+        it "responds and returns nil" do
+          expect(subject.button_text).to be_nil
+        end
+      end
+
+      describe ".has_button?" do
+        it "responds and returns false" do
+          expect(subject).not_to have_button
+        end
+      end
+    end
   end
 end
