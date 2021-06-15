4b103e74cc30890cb38a04a9d0a280061b798d16
roxana.oprescu <roxana.oprescu@tremend.com>
2021-06-15 15:11:44 +0300

    Adjust comment_stats from conference

--- decidim-conferences/app/presenters/decidim/conferences/conference_stats_presenter.rb
+++ decidim-conferences/app/presenters/decidim/conferences/conference_stats_presenter.rb
@@ -11,7 +11,7 @@ module Decidim
       def highlighted
         highlighted_stats = component_stats(priority: StatsRegistry::HIGH_PRIORITY)
         highlighted_stats.concat(component_stats(priority: StatsRegistry::MEDIUM_PRIORITY))
-        highlighted_stats.concat(comments_stats(:conferences, tag: :comments))
+        highlighted_stats.concat(comments_stats(:conferences))
         highlighted_stats = highlighted_stats.reject(&:empty?)
         highlighted_stats = highlighted_stats.reject { |_manifest, _name, data| data.zero? }
         grouped_highlighted_stats = highlighted_stats.group_by(&:first)
