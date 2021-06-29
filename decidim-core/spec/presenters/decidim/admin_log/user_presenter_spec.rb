737413597d519bf3270183148205814a69b71042
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-06-30 00:10:07 +0300

    Fix the user presenter spec

--- decidim-core/spec/presenters/decidim/admin_log/user_presenter_spec.rb
+++ decidim-core/spec/presenters/decidim/admin_log/user_presenter_spec.rb
@@ -13,6 +13,7 @@ describe Decidim::AdminLog::UserPresenter, type: :helper do
   context "when action is block" do
     include_examples "present admin log entry" do
       let(:admin_log_resource) { create(:user, :blocked, organization: organization) }
+      let(:admin_log_extra_data) { { resource: { title: "John Doe" } } }
       let(:action) { "block" }
 
       describe "#present" do
@@ -20,7 +21,7 @@ describe Decidim::AdminLog::UserPresenter, type: :helper do
 
         it "presents the blocked user's name prior to blocking" do
           expect(subject).not_to include("Blocked user")
-          expect(subject).to include(admin_log_resource.user_name)
+          expect(subject).to include(admin_log_extra_data[:resource][:title])
         end
       end
     end
