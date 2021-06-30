fa5425d3da7d18dbd3cf4d9341fcd8dc4e660496
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-06-30 11:25:02 +0300

    Fix admin log blocked user name (#8156)

---
+++ decidim-core/spec/db/migrate/fix_blocked_user_names_in_action_log_spec.rb
@@ -0,0 +1,114 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "FixBlockedUserNamesInActionLog", type: :migration do
+  let(:organization) { create(:organization) }
+  let(:admin) { create(:user, :admin, :confirmed) }
+  let(:users) { create_list(:user, 4, :blocked, organization: organization) }
+
+  let(:user_logs_extra) { {} }
+  let!(:dummy_logs) { create_list(:action_log, 10, organization: organization) }
+  let!(:user_logs) do
+    users.map do |u|
+      create(
+        :action_log,
+        organization: organization,
+        resource: u,
+        resource_type: u.class.name,
+        action: "block",
+        user: admin,
+        extra_data: user_logs_extra
+      )
+    end
+  end
+
+  shared_examples "working migration" do |direction|
+    subject { migration.migrate(direction) }
+
+    context "when there are no matching log entries" do
+      let!(:user_logs) { [] }
+
+      it "does not change any log entries" do
+        expect { subject }.not_to(change { Decidim::ActionLog.order(:id).pluck(:extra) })
+      end
+    end
+
+    context "when there are matching log entries" do
+      let(:user_logs_extra) do
+        if direction == :up
+          {}
+        else
+          { resource: { title: "User name" } }
+        end
+      end
+
+      it "updates the entries that match" do
+        matching_before = Decidim::ActionLog.where(resource_type: "Decidim::User").order(:id).pluck(:extra)
+        not_matching_before = Decidim::ActionLog.where.not(resource_type: "Decidim::User").order(:id).pluck(:extra)
+
+        expect { subject }.not_to change(Decidim::ActionLog, :count)
+
+        expect(matching_before).not_to eq(
+          Decidim::ActionLog.where(resource_type: "Decidim::User").order(:id).pluck(:extra)
+        )
+        expect(not_matching_before).to eq(
+          Decidim::ActionLog.where.not(resource_type: "Decidim::User").order(:id).pluck(:extra)
+        )
+      end
+
+      it "updates the correct resource titles" do
+        subject
+
+        block_logs = Decidim::ActionLog.where(resource_type: "Decidim::User", action: "block").map do |log|
+          [log.resource_id, log.extra["resource"]["title"]]
+        end.to_h
+        expected_logs = users.map do |user|
+          if direction == :up
+            [user.id, user.extended_data["user_name"]]
+          else
+            [user.id, "Blocked user"]
+          end
+        end.to_h
+        expect(block_logs).to match(expected_logs)
+      end
+
+      context "with the user's extended data being empty" do
+        before do
+          users.each do |u|
+            # rubocop:disable Rails/SkipsModelValidations
+            u.update_columns(
+              name: generate(:name),
+              extended_data: nil
+            )
+            # rubocop:enable Rails/SkipsModelValidations
+          end
+        end
+
+        it "updates the log entries with the expected user names" do
+          subject
+
+          block_logs = Decidim::ActionLog.where(resource_type: "Decidim::User", action: "block").map do |log|
+            [log.resource_id, log.extra["resource"]["title"]]
+          end.to_h
+          expected_logs = users.map do |user|
+            if direction == :up
+              [user.id, user.name]
+            else
+              [user.id, "Blocked user"]
+            end
+          end.to_h
+          expect(block_logs).to match(expected_logs)
+        end
+      end
+    end
+  end
+
+  describe "#migrate :up" do
+    it_behaves_like "working migration", :up
+  end
+
+  describe "#migrate :down" do
+    it_behaves_like "working migration", :down
+  end
+end
