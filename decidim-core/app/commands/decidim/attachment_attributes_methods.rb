4d9a42100cff48094512c0f2ab904d32cda65b7a
Eduardo Martínez <eduardomech@gmail.com>
2021-07-19 17:49:08 +0200

    Fix AttachmentAttributesMethods command comment
    Co-authored-by: leio10 <leiodd@gmail.com>

--- decidim-core/app/commands/decidim/attachment_attributes_methods.rb
+++ decidim-core/app/commands/decidim/attachment_attributes_methods.rb
@@ -7,8 +7,8 @@ module Decidim
     private
 
     # This method set the attributes to assign. The attribute is included if a
-    # value exists in the form or remmove_attribute_name is true in te form (in
-    # this case the value is nil
+    # value exists in the form or remove_[attribute_name] is true in the form
+    # (in this case the value is nil)
     def attachment_attributes(*attrs)
       attrs.each_with_object({}) do |attribute, attributes|
         attributes[attribute] = form.send(attribute) if form.try("remove_#{attribute}") || form.send(attribute).present?
