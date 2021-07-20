72dc3433405790cbf76a31f435fcd0e94cbb71e7
Leonardo Diez <leiodd@gmail.com>
2021-07-20 12:51:37 +0200

    test: complete tests

--- decidim-assemblies/spec/queries/admin/assembly_admins_spec.rb
+++ decidim-assemblies/spec/queries/admin/assembly_admins_spec.rb
@@ -2,19 +2,30 @@
 
 require "spec_helper"
 
-module Decidim::Assemblies
-  describe Admin::AdminUsers do
+module Decidim::Assemblies::Admin
+  describe AdminUsers do
     subject { described_class.new(assembly) }
 
     let(:organization) { create :organization }
-    let(:assembly) { create :assembly, organization: organization }
+    let!(:assembly) { create :assembly, organization: organization }
     let!(:admin) { create(:user, :admin, :confirmed, organization: organization) }
-    let!(:assembly_admin) do
-      create(:user, :admin, :confirmed, organization: organization)
-    end
+    let!(:assembly_admin_role) { create(:assembly_user_role, assembly: assembly, user: assembly_admin) }
+    let(:assembly_admin) { create(:user, organization: organization) }
+    let!(:other_assembly_admin_role) { create(:assembly_user_role, user: other_assembly_admin) }
+    let(:other_assembly_admin) { create(:user, organization: organization) }
+    let!(:normal_user) { create(:user, :confirmed, organization: organization) }
+    let!(:other_organization_user) { create(:user, :confirmed) }
 
     it "returns the organization admins and assembly admins" do
       expect(subject.query).to match_array([admin, assembly_admin])
+    end
+
+    context "when asking for organization admin users" do
+      subject { described_class.new(nil, organization) }
+
+      it "returns all the organization admins and assembly admins" do
+        expect(subject.query).to match_array([admin, assembly_admin, other_assembly_admin])
+      end
     end
   end
 end
