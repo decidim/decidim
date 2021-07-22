f80c3a8cb1f064c91111d4a8f30f996e3f79bf4e
Fernando Blat <fernando@blat.es>
2021-07-22 13:26:03 +0200

    Online meetings iframe visibility with time (#8097)

--- decidim-meetings/app/cells/decidim/meetings/online_meeting_cell.rb
+++ decidim-meetings/app/cells/decidim/meetings/online_meeting_cell.rb
@@ -11,14 +11,19 @@ module Decidim
         @embedder ||= MeetingIframeEmbedder.new(model.online_meeting_url)
       end
 
+      delegate :live?, to: :model
       delegate :embeddable?, to: :embedder
 
       def live_event_url
-        if !model.show_embedded_iframe? && embeddable?
+        if embeddable?
           Decidim::EngineRouter.main_proxy(model.component).meeting_live_event_path(meeting_id: model.id)
         else
           model.online_meeting_url
         end
+      end
+
+      def future?
+        Time.current <= model.start_time && !live?
       end
     end
   end
--- decidim-meetings/app/cells/decidim/meetings/online_meeting_link_cell.rb
+++ decidim-meetings/app/cells/decidim/meetings/online_meeting_link_cell.rb
@@ -15,17 +15,19 @@ module Decidim
         model.online_meeting_url.present?
       end
 
-      delegate :live?, to: :model
       delegate :embed_code, to: :embedder
 
       private
 
-      def future?
-        Time.current <= model.start_time && !live?
-      end
-
       def show_embed?
         model.show_embedded_iframe? && embedder.embeddable?
+      end
+
+      def live?
+        model.start_time &&
+          model.end_time &&
+          Time.current >= (model.start_time - 10.minutes) &&
+          Time.current <= model.end_time
       end
     end
   end
