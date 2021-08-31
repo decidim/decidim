3c0abf52900182b198d67edad2c54056953260e2
Andrés Pereira de Lucena <andreslucena@gmail.com>
2021-08-31 10:27:42 +0200

    Add lb to available locales

--- decidim-core/lib/decidim/core.rb
+++ decidim-core/lib/decidim/core.rb
@@ -152,7 +152,7 @@ module Decidim
 
   # Exposes a configuration option: The application available locales.
   config_accessor :available_locales do
-    %w(en bg ar ca cs da de el eo es es-MX es-PY et eu fi-pl fi fr fr-CA ga gl hr hu id is it ja ko lt lv mt nl no pl pt pt-BR ro ru sk sl sr sv tr uk vi zh-CN zh-TW)
+    %w(en bg ar ca cs da de el eo es es-MX es-PY et eu fi-pl fi fr fr-CA ga gl hr hu id is it ja ko lb lt lv mt nl no pl pt pt-BR ro ru sk sl sr sv tr uk vi zh-CN zh-TW)
   end
 
   # Exposes a configuration option: The application default locale.
