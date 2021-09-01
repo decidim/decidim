f7a664611a73c01ecd701893ecad1bb9b7d550f6
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-09-01 21:58:57 +0300

    Refactor the create import spec

--- decidim-admin/spec/commands/decidim/admin/create_import_spec.rb
+++ decidim-admin/spec/commands/decidim/admin/create_import_spec.rb
@@ -27,9 +27,9 @@ module Decidim::Admin
     describe "when everything is ok" do
       it "returns broadcast ok" do
         record = double("record", save!: double("record"))
-        importer = instance_double("importer", prepare: [record], invalid_lines: [], import!: record.save!)
-        allow(subject).to receive(:importer_for).and_return(importer)
-        allow(form).to receive(:check_invalid_lines).and_return([])
+        importer = instance_double("importer", prepare: [record], invalid_indexes: [], import!: record.save!)
+        allow(importer).to receive(:invalid_indexes_message).and_return(nil)
+        allow(subject).to receive(:importer).and_return(importer)
         allow(form).to receive(:importer).and_return(importer)
         expect do
           subject.call
@@ -40,12 +40,8 @@ module Decidim::Admin
     describe "when something unexpected happens" do
       it "returns broadcast invalid" do
         importer = double
-        allow(subject).to receive(:importer_for).with(file.tempfile.path, file.content_type).and_return(importer)
-        expect(importer).to receive(:prepare)
-        allow(importer).to receive(:invalid_lines).and_return([])
-        allow(importer).to receive(:import!).and_raise(StandardError)
-        allow(form).to receive(:check_invalid_lines).and_return([])
-        allow(form).to receive(:importer).and_return(importer)
+        allow(importer).to receive(:invalid_indexes_message).and_return("Invalid")
+        expect(importer).not_to receive(:prepare)
         expect { subject.call }.to broadcast(:invalid)
       end
     end
