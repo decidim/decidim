2bf92f69bd3240fa9af2d6f9d5b2d2cf699efea4
Andrés Pereira de Lucena <andreslucena@gmail.com>
2021-09-02 11:57:25 +0200

    Add foundation-datepicker lb (Luxembourgish) locale in vendor/

---
+++ decidim-core/vendor/assets/javascripts/datepicker-locales/foundation-datepicker.lb.js
@@ -0,0 +1,13 @@
+/**
+ * Luxembourgish localisation
+ */
+;(function($){
+	$.fn.fdatepicker.dates['lb'] = {
+		days: ["Sonndeg", "Méindeg", "Dënschdeg", "Mëttwoch", "Donneschdeg", "Freideg", "Samschdeg", "Sonndeg"],
+		daysShort: ["Son", "Méi", "Dën", "Mët", "Don", "Fre", "Sam", "Son"],
+		daysMin: ["So", "Mé", "Dë", "Më", "Do", "Fr", "Sa", "So"],
+		months: ["Januar", "Februar", "Mäerz", "Abrëll", "Mee", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"], 
+		monthsShort: ["Jan", "Febr", "Mrz", "Abr", "Mee", "Jun", "Jul", "Aug", "Sept", "Okt", "Nov", "Dez"],
+		today: "Haut"
+	};
+}(jQuery));
