27f43a5ffab1daba0b50579a50c459e04440d0b5
Alexandru-Emil Lupu <contact@alecslupu.ro>
2021-09-05 21:57:50 +0300

    Adding test suite for sorting and filtering

--- decidim-admin/app/controllers/decidim/admin/user_groups_controller.rb
+++ decidim-admin/app/controllers/decidim/admin/user_groups_controller.rb
@@ -54,14 +54,20 @@ module Decidim
 
       private
 
+      def filtered_collection
+        paginate(query.result)
+      end
+
       def base_query
         Decidim::Admin::UserGroupsEvaluation.for(collection, @query, @state)
       end
 
       def collection
         UserGroup
-          .includes(:memberships)
+          .left_outer_joins(:memberships)
+          .select("decidim_users.*, COUNT(decidim_user_group_memberships.decidim_user_group_id) as users_count")
           .where(decidim_user_group_memberships: { decidim_user_id: current_organization.users })
+          .group(Arel.sql("decidim_users.id"))
       end
     end
   end
