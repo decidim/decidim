ece89b2727d19c61fae9cb034358f770cec19fee
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2021-09-06 21:34:39 +0200

    Restrict iframe online meetings visibility depending on iframe_access_level configuration

--- decidim-meetings/app/cells/decidim/meetings/online_meeting_link_cell.rb
+++ decidim-meetings/app/cells/decidim/meetings/online_meeting_link_cell.rb
@@ -8,6 +8,8 @@ module Decidim
       include Decidim::LayoutHelper
 
       def show
+        return unless iframe_access_level_allowed?
+
         render
       end
 
@@ -21,6 +23,19 @@ module Decidim
 
       def show_embed?
         model.show_embedded_iframe? && embedder.embeddable?
+      end
+
+      def iframe_access_level_allowed?
+        return unless model.show_embedded_iframe?
+
+        case model.iframe_access_level
+        when "all"
+          true
+        when "signed_in"
+          current_user.present?
+        else
+          model.has_registration_for?(current_user)
+        end
       end
     end
   end
