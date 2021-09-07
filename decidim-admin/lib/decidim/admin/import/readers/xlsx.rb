afecc2118ce1831f0488bec5ecba003765f4bd1a
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-09-07 10:31:41 +0300

    Fix comment

--- decidim-admin/lib/decidim/admin/import/readers/xlsx.rb
+++ decidim-admin/lib/decidim/admin/import/readers/xlsx.rb
@@ -29,7 +29,7 @@ module Decidim
             raise Decidim::Admin::Import::InvalidFileError, "The provided XLSX file is not valid"
           end
 
-          # Returns a Zip::OutputStream
+          # Returns a StringIO
           def example_file(data)
             workbook = RubyXL::Workbook.new
             sheet = workbook.worksheets[0]
