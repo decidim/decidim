48656720e0cc1cae56aa563c9a4a311ba285da76
Andrés Pereira de Lucena <andreslucena@gmail.com>
2021-09-08 12:19:29 +0200

    Point 'View public page' in Assembly Members to its public page

--- decidim-assemblies/app/controllers/decidim/assemblies/admin/assembly_members_controller.rb
+++ decidim-assemblies/app/controllers/decidim/assemblies/admin/assembly_members_controller.rb
@@ -7,6 +7,7 @@ module Decidim
       #
       class AssemblyMembersController < Decidim::Assemblies::Admin::ApplicationController
         include Concerns::AssemblyAdmin
+        layout "decidim/admin/assembly_members"
 
         def index
           enforce_permission_to :index, :assembly_member
