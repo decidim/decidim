75fcf4481816acc368540fa5f4e4135c5a530c97
roxana.oprescu <roxana.oprescu@tremend.com>
2021-09-13 16:09:16 +0300

    Exclude withdrawn meetings from "upcoming meetings" section from homepage

--- decidim-meetings/app/cells/decidim/meetings/content_blocks/upcoming_events_cell.rb
+++ decidim-meetings/app/cells/decidim/meetings/content_blocks/upcoming_events_cell.rb
@@ -18,6 +18,7 @@ module Decidim
                                .where(component: meeting_components)
                                .visible_meeting_for(current_user)
                                .where("end_time >= ?", Time.current)
+                               .except_withdrawn
                                .order(start_time: :asc)
                                .limit(limit)
         end
