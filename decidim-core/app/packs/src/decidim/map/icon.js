f24e0ef16baf6d2e4131349af066e7a348cac5eb
Leonardo Diez <leiodd@gmail.com>
2021-09-15 12:01:41 +0200

    fix: use a more updated library for svgicons

--- decidim-core/app/packs/src/decidim/map/icon.js
+++ decidim-core/app/packs/src/decidim/map/icon.js
@@ -1,10 +1,10 @@
 import * as L from "leaflet";
-import { SVGIcon } from "leaflet-svgicon"
+import "@decidim/leaflet-svgicon";
 
-L.DivIcon.SVGIcon = SVGIcon;
 L.DivIcon.SVGIcon.DecidimIcon = L.DivIcon.SVGIcon.extend({
   options: {
     fillColor: "#ef604d",
+    fillOpacity: 1,
     opacity: 0
   },
   _createPathDescription: function() {
