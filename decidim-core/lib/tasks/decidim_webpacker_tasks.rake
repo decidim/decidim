decidim/decidim

commit bfcd80e6c03cf6184598d27e85e6655554956286

Remove npm decidim packages with dependencies from other decidim packages (#8330)
* chore: remove interpackages dependencies

* chore: don't publish root package

* fix: add all dependencies directly to root package.json

* chore: read npm dependencies from root json

* chore: regenerate webpacker binstubs instead of reinstalling it

Rails installs webpacker when creating the app, so we were installing webpacker twice.

* refactor: improve npm dependencies installation semantics

* fix: apply suggestions

* chore: update npm lock files

* test: add missing tests for tasks

* chore: remove unused package from lock file

* test: fix tests broken by webpacker changes

decidim-core/lib/tasks/decidim_webpacker_tasks.rake
@@ -55,12 +55,8 @@ namespace :decidim do
     end
 
     def install_decidim_npm
-      decidim_npm_packages.each do |type, package|
-        if type == :dev
-          system! "npm i -D #{package}"
-        else
-          system! "npm i #{package}"
-        end
+      decidim_npm_packages.each do |type, packages|
+        system! "npm i --save-#{type} #{packages.join(" ")}"
       end
     end
 
@@ -73,32 +69,55 @@ namespace :decidim do
     end
 
     def decidim_npm_packages
+      gem_path = unreleased_gem_path
+
+      if gem_path
+        package_spec = "./packages/%s"
+
+        # The packages folder needs to be copied to the application folder
+        # because the linked dependencies are not installed when packages
+        # are installed using file references outside the application root
+        # where the `package.json` is located at. For more information, see:
+        # https://github.com/npm/cli/issues/2339
+        FileUtils.rm_rf(rails_app_path.join("packages"))
+        FileUtils.cp_r(gem_path.join("packages"), rails_app_path)
+      else
+        package_spec = "@decidim/%s@~#{Decidim::GemManager.semver_friendly_version(decidim_gemspec.version.to_s)}"
+      end
+
+      local_npm_dependencies.transform_values { |names| names.map { |name| format(package_spec, name) } }
+    end
+
+    def unreleased_gem_path
       if decidim_gemspec.source.is_a?(Bundler::Source::Rubygems)
-        if released_version?
-          return {
-            dev: "@decidim/dev@~#{Decidim::GemManager.semver_friendly_version(decidim_gemspec.version.to_s)}",
-            prod: "@decidim/all@~#{Decidim::GemManager.semver_friendly_version(decidim_gemspec.version.to_s)}"
-          }
-        else
-          gem_path = Pathname(decidim_gemspec.full_gem_path)
-        end
+        return if released_version?
+
+        gem_path = Pathname(decidim_gemspec.full_gem_path)
       else
         gem_path = decidim_gemspec.source.path
         gem_path = Pathname(ENV["BUNDLE_GEMFILE"]).dirname.join(gem_path) if gem_path.relative?
       end
 
-      # The packages folder needs to be copied to the application folder
-      # because the linked dependencies are not installed when packages
-      # are installed using file references outside the application root
-      # where the `package.json` is located at. For more information, see:
-      # https://github.com/npm/cli/issues/2339
-      FileUtils.rm_rf(rails_app_path.join("packages"))
-      FileUtils.cp_r(gem_path.join("packages"), rails_app_path)
+      gem_path
+    end
 
-      {
-        dev: "./packages/dev",
-        prod: "./packages/all"
-      }
+    def local_npm_dependencies
+      @local_npm_dependencies ||= begin
+        package_json = JSON.parse(File.read(decidim_path.join("package.json")))
+
+        {
+          prod: local_npm_dependencies_list(package_json["dependencies"]),
+          dev: local_npm_dependencies_list(package_json["devDependencies"])
+        }.freeze
+      end
+    end
+
+    def local_npm_dependencies_list(deps)
+      return [] unless deps
+
+      deps.values
+          .select { |ref| ref.starts_with?("file:packages/") }
+          .map { |ref| ref.delete_prefix("file:packages/") }
     end
 
     def decidim_path
@@ -151,8 +170,8 @@ namespace :decidim do
       File.write(file, contents)
     end
 
-    def system!(*args)
-      system(*args) || abort("\n== Command #{args} failed ==")
+    def system!(command)
+      system("cd #{rails_app_path} && #{command}") || abort("\n== Command #{args} failed ==")
     end
   end
 end

decidim-core/spec/lib/map/dynamic_map/builder_spec.rb
@@ -19,7 +19,7 @@ module Decidim
       describe "#stylesheet_snippets" do
         it "returns the expected stylesheet assets" do
           expect(subject.stylesheet_snippets).to match(
-            %r{<link rel="stylesheet" media="screen" href="/packs-test/css/decidim_map-[^.]*\.css" />}
+            %r{<link rel="stylesheet" media="screen" href="/packs-test/css/decidim_map(-[^.]*)?\.css" />}
           )
         end
       end
@@ -27,7 +27,7 @@ module Decidim
       describe "#javascript_snippets" do
         it "returns the expected JavaScript assets" do
           expect(subject.javascript_snippets).to match(
-            %r{<script src="/packs-test/js/decidim_map_provider_default-[^.]*\.js"></script>}
+            %r{<script src="/packs-test/js/decidim_map_provider_default(-[^.]*)?\.js"></script>}
           )
         end
       end

decidim-core/spec/lib/map/provider/autocomplete/here_builder_spec.rb
@@ -14,7 +14,7 @@ module Decidim
           describe "#javascript_snippets" do
             it "returns the expected JavaScript assets" do
               expect(subject.javascript_snippets).to match(
-                %r{<script src="/packs-test/js/decidim_geocoding_provider_here-[^.]*\.js"></script>}
+                %r{<script src="/packs-test/js/decidim_geocoding_provider_here(-[^.]*)?\.js"></script>}
               )
             end
           end

decidim-core/spec/lib/map/provider/autocomplete/osm_builder_spec.rb
@@ -14,7 +14,7 @@ module Decidim
           describe "#javascript_snippets" do
             it "returns the expected JavaScript assets" do
               expect(subject.javascript_snippets).to match(
-                %r{<script src="/packs-test/js/decidim_geocoding_provider_photon-[^.]*\.js"></script>}
+                %r{<script src="/packs-test/js/decidim_geocoding_provider_photon(-[^.]*)?\.js"></script>}
               )
             end
           end

decidim-core/spec/lib/map/provider/dynamic_map/here_builder_spec.rb
@@ -31,7 +31,7 @@ module Decidim
           describe "#javascript_snippets" do
             it "returns the expected JavaScript assets" do
               expect(subject.javascript_snippets).to match(
-                %r{<script src="/packs-test/js/decidim_map_provider_here-[^.]*\.js"></script>}
+                %r{<script src="/packs-test/js/decidim_map_provider_here(-[^.]*)?\.js"></script>}
               )
             end
           end

decidim-core/spec/tasks/decidim_tasks_webpacker_spec.rb
@@ -1,0 +1,20 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "rake decidim:webpacker:install", type: :task do
+  it "preloads the Rails environment" do
+    expect(task.prerequisites).to include "environment"
+  end
+
+  it "have changed the app's package.json file" do
+    package_json = Rails.root.join("package.json")
+    FileUtils.rm(package_json)
+
+    task.execute
+
+    package_json_content = JSON.parse(File.read(package_json))
+    expect(package_json_content["dependencies"].keys).to match_array(["@decidim/browserslist-config", "@decidim/core", "@decidim/elections", "@decidim/webpacker"])
+    expect(package_json_content["devDependencies"].keys).to match_array(["@decidim/dev", "@decidim/eslint-config", "@decidim/stylelint-config"])
+  end
+end

decidim-generators/lib/decidim/generators/install_generator.rb
@@ -89,8 +89,9 @@ module Decidim
         # Create empty directory for images
         empty_directory "app/packs/images"
 
-        # Run webpacker installation
-        rails "webpacker:install"
+        # Regenerate webpacker binstubs
+        remove_file "bin/yarn"
+        rails "webpacker:binstubs"
 
         # Run Decidim custom webpacker installation
         rails "decidim:webpacker:install"

lib/decidim/gem_manager.rb
@@ -165,7 +165,6 @@ module Decidim
 
       def package_dirs
         dirs = Dir.glob("#{root}/packages/*")
-        dirs << "./"
 
         dirs.each { |dir| yield(dir) }
       end

commit 33533a76e29bd961bfcf555ceced6599d866b9c3

Fix problems introduced by #8330 (#8341)
* chore: bump problematic JS dependency version

* fix: replace changed variable name

decidim-core/lib/tasks/decidim_webpacker_tasks.rake
@@ -171,7 +171,7 @@ namespace :decidim do
     end
 
     def system!(command)
-      system("cd #{rails_app_path} && #{command}") || abort("\n== Command #{args} failed ==")
+      system("cd #{rails_app_path} && #{command}") || abort("\n== Command #{command} failed ==")
     end
   end
 end