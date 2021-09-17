40300ef822bdc82154690d7799cfd7d586d4649d
Eduardo Martínez <eduardomech@gmail.com>
2021-09-17 16:47:46 +0200

    Non participant assembly members avatar (#8277)
    * Define not_user_avatar attachment for AssemblyMember model
    
    * Add management of not user avatar in assembly members admin
    
    * Add passthru validation in assembly member form for not user avatar
    
    * Define remove_not_user_avatar in AssemblyMember model
    
    This method is created to avoid errors generating the form of an
    assembly member. Although the form is built using a form object
    with the method defined the form builder looks for the method in
    the model directly and fails if not present.
    
    * Display not user avatar for members not associated with a user
    
    * Update tests
    
    * Add consent warning to new assembly member form on avatar
    
    * Rename attribute
    
    * Add tests for assembly member presenter
    
    * Remove duplicated help message
    
    * Fix unused key warning

--- decidim-assemblies/app/models/decidim/assembly_member.rb
+++ decidim-assemblies/app/models/decidim/assembly_member.rb
@@ -6,6 +6,7 @@ module Decidim
   class AssemblyMember < ApplicationRecord
     include Decidim::Traceable
     include Decidim::Loggable
+    include Decidim::HasUploadValidations
 
     POSITIONS = %w(president vice_president secretary other).freeze
 
@@ -13,12 +14,21 @@ module Decidim
     belongs_to :assembly, foreign_key: "decidim_assembly_id", class_name: "Decidim::Assembly"
     alias participatory_space assembly
 
+    has_one_attached :non_user_avatar
+    validates_avatar :non_user_avatar, uploader: Decidim::AvatarUploader
+
+    delegate :organization, to: :assembly
+
     default_scope { order(weight: :asc, created_at: :asc) }
 
     scope :not_ceased, -> { where("ceased_date >= ? OR ceased_date IS NULL", Time.zone.today) }
 
     def self.log_presenter_class_for(_log)
       Decidim::Assemblies::AdminLog::AssemblyMemberPresenter
+    end
+
+    def remove_non_user_avatar
+      false
     end
   end
 end
