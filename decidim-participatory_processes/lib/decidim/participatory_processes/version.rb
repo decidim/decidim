30239b30c757820facbdebd3ac8c519cd4913d53
leio10 <leiodd@gmail.com>
2021-09-20 14:07:42 +0200

    Backport "Update NPM version" (#8344)
    * chore: bump npm version to 7.24.0
    
    * chore: add webpack rake task to sync npm files
    
    * chore: bump node version

--- decidim-participatory_processes/lib/decidim/participatory_processes/version.rb
+++ decidim-participatory_processes/lib/decidim/participatory_processes/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-participatory_processes version.
   module ParticipatoryProcesses
     def self.version
-      "0.25.0.rc3"
+      "0.25.0.dev"
     end
   end
 end
