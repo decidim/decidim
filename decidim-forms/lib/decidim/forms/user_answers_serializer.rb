a75ffafa05cea7c20085285c56071353546a50af
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-09-20 17:12:29 +0200

    Add nil safety in UserAnswersSerializer

--- decidim-forms/lib/decidim/forms/user_answers_serializer.rb
+++ decidim-forms/lib/decidim/forms/user_answers_serializer.rb
@@ -32,13 +32,15 @@ module Decidim
       def hash_for(answer)
         {
           answer_translated_attribute_name(:id) => answer&.session_token,
-          answer_translated_attribute_name(:created_at) => answer&.created_at.to_s(:db),
+          answer_translated_attribute_name(:created_at) => answer&.created_at&.to_s(:db),
           answer_translated_attribute_name(:ip_hash) => answer&.ip_hash,
           answer_translated_attribute_name(:user_status) => answer_translated_attribute_name(answer&.decidim_user_id.present? ? "registered" : "unregistered")
         }
       end
 
       def questions_hash
+        return {} if questionnaire&.questions.blank?
+
         questionnaire.questions.each.inject({}) do |serialized, question|
           serialized.update(
             translated_question_key(question.position, question.body) => ""
