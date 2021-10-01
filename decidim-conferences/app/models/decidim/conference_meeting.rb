506c0ac61fed3a04364860467cce3873bf80faa9
leio10 <leiodd@gmail.com>
2021-10-01 09:28:53 +0200

    Fix error when accessing the meetings of a conference with speakers related (#8369) (#8371)
    * fix: add missing avatar attributes to conference speakers
    
    * fix: add missing information to ConferenceMeeting model
    
    * test: add regression test for this fix
    
    * feat(seed): publish most of the meetings
    
    * feat(seeds): add speakers to some meetings in conferences
    
    * style: fix lint issue

--- decidim-conferences/app/models/decidim/conference_meeting.rb
+++ decidim-conferences/app/models/decidim/conference_meeting.rb
@@ -3,9 +3,11 @@
 module Decidim
   # It represents a meeting of the conference
   class ConferenceMeeting < Decidim::Meetings::Meeting
+    translatable_fields(*Decidim::Meetings::Meeting.translatable_fields_list)
+
     has_many :conference_speaker_conference_meetings, dependent: :destroy
     has_many :conference_speakers, through: :conference_speaker_conference_meetings, class_name: "Decidim::ConferenceSpeaker"
-    has_many :conference_meeting_registration_types, dependent: :destroy
+    has_many :conference_meeting_registration_types, dependent: :destroy, class_name: "Decidim::Conferences::ConferenceMeetingRegistrationType"
     has_many :registration_types, through: :conference_meeting_registration_types, foreign_key: "registration_type_id", class_name: "Decidim::Conferences::RegistrationType"
   end
 end
