de2bebb124c807dec729d472d7feefa72340c8bc
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2021-10-04 20:22:18 +0200

    Move visibility methods to parent cell class

--- decidim-meetings/app/cells/decidim/meetings/online_meeting_cell.rb
+++ decidim-meetings/app/cells/decidim/meetings/online_meeting_cell.rb
@@ -5,6 +5,14 @@ module Decidim
     # This cell defines methods required for other cells to render
     # online meeting urls
     class OnlineMeetingCell < Decidim::ViewModel
+      def show
+        return if model.iframe_embed_type_none?
+        return unless iframe_access_level_allowed?
+        return unless assembly_privacy_allowed?
+
+        render
+      end
+
       protected
 
       def embedder
@@ -30,6 +38,29 @@ module Decidim
 
       def future?
         Time.current <= model.start_time && !live?
+      end
+
+      def iframe_access_level_allowed?
+        case model.iframe_access_level
+        when "all"
+          true
+        when "signed_in"
+          current_user.present?
+        else
+          model.has_registration_for?(current_user)
+        end
+      end
+
+      def assembly_privacy_allowed?
+        return true if !private_transparent_assembly? || current_user&.admin?
+
+        model.participatory_space.users.include?(current_user)
+      end
+
+      def private_transparent_assembly?
+        return unless model.participatory_space.is_a?(Decidim::Assembly)
+
+        model.participatory_space.private_space? && model.participatory_space.is_transparent?
       end
     end
   end
--- decidim-meetings/app/cells/decidim/meetings/online_meeting_link_cell.rb
+++ decidim-meetings/app/cells/decidim/meetings/online_meeting_link_cell.rb
@@ -7,14 +7,6 @@ module Decidim
     class OnlineMeetingLinkCell < Decidim::Meetings::OnlineMeetingCell
       include Decidim::LayoutHelper
 
-      def show
-        return if model.iframe_embed_type_none?
-        return unless iframe_access_level_allowed?
-        return unless assembly_privacy_allowed?
-
-        render
-      end
-
       def online_meeting_url?
         model.online_meeting_url.present?
       end
@@ -25,29 +17,6 @@ module Decidim
 
       def show_embed?
         model.iframe_embed_type_embed_in_meeting_page? && embedder.embeddable?
-      end
-
-      def iframe_access_level_allowed?
-        case model.iframe_access_level
-        when "all"
-          true
-        when "signed_in"
-          current_user.present?
-        else
-          model.has_registration_for?(current_user)
-        end
-      end
-
-      def assembly_privacy_allowed?
-        return true if !private_transparent_assembly? || current_user&.admin?
-
-        model.participatory_space.users.include?(current_user)
-      end
-
-      def private_transparent_assembly?
-        return unless model.participatory_space.is_a?(Decidim::Assembly)
-
-        model.participatory_space.private_space? && model.participatory_space.is_transparent?
       end
     end
   end
