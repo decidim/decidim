9673ce2f5ae2860b8bb80388d7993d49f63e3009
leio10 <leiodd@gmail.com>
2021-10-26 17:50:04 +0200

    Fix webpacker issue when using zeitwerk (#8444)
    * fix: ignore webpacker paths when using zeitwerk
    
    * test: add regression test
    
    * fix: prevent failing when used outside the rails app

--- decidim-core/lib/decidim/webpacker.rb
+++ decidim-core/lib/decidim/webpacker.rb
@@ -15,6 +15,8 @@ module Decidim
       else
         configuration.additional_paths.push(path)
       end
+
+      Rails.autoloaders.main.ignore(path) if defined?(Rails) && Rails.configuration.autoloader == :zeitwerk
     end
 
     def self.register_entrypoints(entrypoints)
