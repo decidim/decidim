5c5e07bf72ee6ffe6dc993658b3813fdf91442d4
lahdeero <eero.lahdenpera@gmail.com>
2021-10-28 17:04:35 +0300

    Fix comments get link in project view

--- decidim-comments/app/cells/decidim/comments/comments_cell.rb
+++ decidim-comments/app/cells/decidim/comments/comments_cell.rb
@@ -61,6 +61,8 @@ module Decidim
       end
 
       def commentable_path(params = {})
+        return resource_locator(options[:polymorphic]).path(params) if options[:polymorphic]
+
         resource_locator(model).path(params)
       end
 
