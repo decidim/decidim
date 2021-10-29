6f8a8c542afb73067497ba7bbc208c240f924d7f
lahdeero <eero.lahdenpera@gmail.com>
2021-10-29 12:00:23 +0300

    Copy system test html screenshots from gem

--- decidim-dev/lib/decidim/dev/test/rspec_support/capybara.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/capybara.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "selenium-webdriver"
-require "system_test_html_screenshots"
+require_relative "system_test_html_screenshots"
 
 module Decidim
   # Helpers meant to be used only during capybara test runs.
