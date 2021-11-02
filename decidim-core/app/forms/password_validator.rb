11d3ce1759fa6d69ef36f837b02386f670652550
lahdeero <eero.lahdenpera@gmail.com>
2021-11-02 12:02:27 +0200

    Add tests and fix bugs

--- decidim-core/app/forms/password_validator.rb
+++ decidim-core/app/forms/password_validator.rb
@@ -5,11 +5,12 @@ class PasswordValidator < ActiveModel::EachValidator
   MINIMUM_LENGTH = 10
   MAX_LENGTH = 256
   MIN_UNIQUE_CHARACTERS = 5
+  IGNORE_SIMILARITY_SHORTER_THAN = 4
   VALIDATION_METHODS = [
     :password_too_short?,
     :password_too_long?,
     :not_enough_unique_characters?,
-    :username_included_in_password?,
+    :name_included_in_password?,
     :email_included_in_password?,
     :domain_included_in_password?,
     :password_too_common
@@ -68,8 +69,14 @@ class PasswordValidator < ActiveModel::EachValidator
     value.split("").uniq.length < MIN_UNIQUE_CHARACTERS
   end
 
-  def username_included_in_password?
-    record.name.split(" ").include?(value) || record.name.downcase.split(" ").include?(value)
+  def name_included_in_password?
+    record.name.split(" ").each do |part|
+      next if part.length < IGNORE_SIMILARITY_SHORTER_THAN
+
+      return true if value.include?(part)
+    end
+
+    false
   end
 
   def email_included_in_password?
@@ -79,10 +86,11 @@ class PasswordValidator < ActiveModel::EachValidator
   end
 
   def domain_included_in_password?
-    return false unless record&.context&.current&.organization&.host
+    return false unless record&.context&.current_organization&.host
+    return true if value.include?(record.context.current_organization.host)
 
-    record.context.current.organization.host.split(".").each do |part|
-      next if part.length < 4
+    record.context.current_organization.host.split(".").each do |part|
+      next if part.length < IGNORE_SIMILARITY_SHORTER_THAN
 
       return true if value.include?(part)
     end
