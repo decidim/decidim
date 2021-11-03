971a488f4ae28a2b688b45857b1d1e33d03c879a
lahdeero <eero.lahdenpera@gmail.com>
2021-11-03 18:50:50 +0200

    Update test again

--- decidim-core/spec/lib/common_passwords_spec.rb
+++ decidim-core/spec/lib/common_passwords_spec.rb
@@ -5,10 +5,18 @@ require "spec_helper"
 module Decidim
   describe CommonPasswords do
     let(:subject) { described_class.instance }
-    let(:data) do
+    let(:dummy_data) do
       double(
-        read: "qwertyuiop 1234567890 q1w2e3r4t5"
+        read: example_passwords
       )
+    end
+    let(:example_url) { "www.example.org/passwords.txt" }
+    let(:example_passwords) { "qwertyuiop 1234567890 q1w2e3r4t5 tooshort" }
+    let(:test_password_list_path) { Rails.root.join("tmp/common-passwords.txt") }
+
+    before do
+      stub_const "Decidim::CommonPasswords::URLS", [example_url]
+      allow(URI).to receive(:open).and_yield(dummy_data)
     end
 
     describe "#passwords" do
@@ -18,27 +26,20 @@ module Decidim
     end
 
     describe "#update_passwords!" do
-      let(:example_url) { "www.example.org/passwords.txt" }
-      let(:example_passwords) { "qwertyuiop 1234567890 q1w2e3r4t5 tooshort" }
-
-      before do
-        allow(subject).to receive(:password_list_path).and_return(test_password_list_path)
-        stub_const "Decidim::CommonPasswords::URLS", [example_url]
-        allow(URI).to receive(:open).with(example_url).and_return(example_passwords)
-      end
-
       it "opens file for writing" do
-        expect(File).to receive(:open).with(test_password_list_path, "w")
-
         subject.update_passwords!
 
         expect(subject.passwords).to eq(example_passwords.split.slice(0..-2))
       end
     end
+  end
+end
 
+module Decidim
+  class CommonPasswords
     private
 
-    def test_password_list_path
+    def password_list_path
       Rails.root.join("tmp/common-passwords.txt")
     end
   end
