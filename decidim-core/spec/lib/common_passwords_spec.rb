e804475cac1ec222cb3dc52a369509d183938f42
lahdeero <eero.lahdenpera@gmail.com>
2021-11-04 16:36:29 +0200

    Stub requests

--- decidim-core/spec/lib/common_passwords_spec.rb
+++ decidim-core/spec/lib/common_passwords_spec.rb
@@ -4,21 +4,26 @@ require "spec_helper"
 
 module Decidim
   describe CommonPasswords do
+    let(:organization) { create(:organization) }
     let(:dummy_data) do
       double(
         read: example_passwords
       )
     end
-    let(:example_url) { "www.example.org/passwords.txt" }
     let(:example_passwords) { "VJHT29061987 1234567890 q1w2e3r4t5 tooshort" }
     let(:test_password_list_path) { Rails.root.join("tmp/common-passwords.txt") }
+    let(:urls) { Decidim::CommonPasswords::URLS }
 
     before do
+      urls.each do |request_url|
+        stub_request(:get, request_url)
+          .with(
+            headers: { "Accept" => "*/*", "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3", "User-Agent" => "Ruby" }
+          ).to_return(status: 200, body: example_passwords, headers: {})
+      end
       # rubocop:disable RSpec/AnyInstance
       allow_any_instance_of(described_class).to receive(:password_list_path).and_return(test_password_list_path)
       # rubocop:enable RSpec/AnyInstance
-      stub_const "Decidim::CommonPasswords::URLS", [example_url]
-      allow(URI).to receive(:open).and_yield(dummy_data)
     end
 
     describe "#passwords" do
