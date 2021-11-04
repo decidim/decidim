b5c50e9416f286adabe355a24c60ee2401ac6857
lahdeero <eero.lahdenpera@gmail.com>
2021-11-04 18:33:02 +0200

    Override class dynamically in test

--- decidim-core/spec/lib/common_passwords_spec.rb
+++ decidim-core/spec/lib/common_passwords_spec.rb
@@ -4,6 +4,13 @@ require "spec_helper"
 
 module Decidim
   describe CommonPasswords do
+    let(:subject) do
+      Class.new(described_class) do
+        def password_list_path
+          Rails.root.join("tmp/common-passwords.txt")
+        end
+      end.instance
+    end
     let(:organization) { create(:organization) }
     let(:dummy_data) do
       double(
@@ -11,7 +18,6 @@ module Decidim
       )
     end
     let(:example_passwords) { "VJHT29061987 1234567890 q1w2e3r4t5 tooshort" }
-    let(:test_password_list_path) { Rails.root.join("tmp/common-passwords.txt") }
     let(:urls) { Decidim::CommonPasswords::URLS }
 
     before do
@@ -21,22 +27,19 @@ module Decidim
             headers: { "Accept" => "*/*", "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3", "User-Agent" => "Ruby" }
           ).to_return(status: 200, body: example_passwords, headers: {})
       end
-      # rubocop:disable RSpec/AnyInstance
-      allow_any_instance_of(described_class).to receive(:password_list_path).and_return(test_password_list_path)
-      # rubocop:enable RSpec/AnyInstance
     end
 
     describe "#passwords" do
       it "returns passwords" do
-        expect(described_class.instance.passwords).to be_kind_of(Array)
+        expect(subject.passwords).to be_kind_of(Array)
       end
     end
 
     describe "#update_passwords!" do
       it "opens file for writing" do
-        described_class.instance.update_passwords!
+        subject.update_passwords!
 
-        expect(described_class.instance.passwords).to eq(example_passwords.split.slice(0..-2))
+        expect(subject.passwords).to eq(example_passwords.split.slice(0..-2))
       end
     end
   end
