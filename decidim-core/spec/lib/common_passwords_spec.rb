70185e1c601670d1ceb721f3d6d2b21e1c775f60
lahdeero <eero.lahdenpera@gmail.com>
2021-11-04 13:56:45 +0200

    Update test vol 5

--- decidim-core/spec/lib/common_passwords_spec.rb
+++ decidim-core/spec/lib/common_passwords_spec.rb
@@ -4,14 +4,13 @@ require "spec_helper"
 
 module Decidim
   describe CommonPasswords do
-    let(:subject) { described_class.instance }
     let(:dummy_data) do
       double(
         read: example_passwords
       )
     end
     let(:example_url) { "www.example.org/passwords.txt" }
-    let(:example_passwords) { "qwertyuiop 1234567890 q1w2e3r4t5 tooshort" }
+    let(:example_passwords) { "VJHT29061987 1234567890 q1w2e3r4t5 tooshort" }
     let(:test_password_list_path) { Rails.root.join("tmp/common-passwords.txt") }
 
     before do
@@ -24,15 +23,15 @@ module Decidim
 
     describe "#passwords" do
       it "returns passwords" do
-        expect(subject.passwords).to be_kind_of(Array)
+        expect(described_class.instance.passwords).to be_kind_of(Array)
       end
     end
 
     describe "#update_passwords!" do
       it "opens file for writing" do
-        subject.update_passwords!
+        described_class.instance.update_passwords!
 
-        expect(subject.passwords).to eq(example_passwords.split.slice(0..-2))
+        expect(described_class.instance.passwords).to eq(example_passwords.split.slice(0..-2))
       end
     end
   end
