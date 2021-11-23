e8b2c4b9a4eaacc868190ee3905b48035cda0713
roxana.oprescu <roxana.oprescu@tremend.com>
2021-11-23 12:35:25 +0200

    Send notifications to global and space admins for official meetings

--- decidim-meetings/app/mailers/decidim/meetings/close_meeting_reminder_mailer.rb
+++ decidim-meetings/app/mailers/decidim/meetings/close_meeting_reminder_mailer.rb
@@ -4,6 +4,14 @@ module Decidim
   module Meetings
     # A custom mailer for sending notifications for overdue meetings
     class CloseMeetingReminderMailer < Decidim::ApplicationMailer
+      include Decidim::TranslationsHelper
+      include ActionView::Helpers::SanitizeHelper
+      include Decidim::ApplicationHelper
+
+      helper Decidim::ResourceHelper
+      helper Decidim::TranslationsHelper
+      helper Decidim::ApplicationHelper
+
       def first_notification(meeting, user)
         notify(meeting, user)
       end
@@ -15,17 +23,19 @@ module Decidim
       private
 
       def notify(meeting, user)
-        @user = user
-        @meeting = meeting
-        @locale = user.locale
-        @organization = user.organization
-        mail(
-          to: user.email,
-          subject: I18n.t(
-            "decidim.meetings.close_meeting_reminder_mailer.first_notification.subject",
-            organization_name: @organization.name
+        with_user(user) do
+          @user = user
+          @meeting = meeting
+          @organization = user.organization
+          mail(
+            to: user.email,
+            subject: I18n.t(
+              "decidim.meetings.close_meeting_reminder_mailer.first_notification.subject",
+              organization_name: @organization.name
+            ),
+            template_name: "first_notification"
           )
-        )
+        end
       end
     end
   end
