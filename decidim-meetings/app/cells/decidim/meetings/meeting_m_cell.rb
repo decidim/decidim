993bc9978750bec9fe39c6c04608966f7d89ba6e
roxanaopr <66411127+roxanaopr@users.noreply.github.com>
2021-12-10 15:36:02 +0200

    Backport: Fix HTML injection in comments and meeting's description (#8511)

--- decidim-meetings/app/cells/decidim/meetings/meeting_m_cell.rb
+++ decidim-meetings/app/cells/decidim/meetings/meeting_m_cell.rb
@@ -27,6 +27,10 @@ module Decidim
         present(model).title
       end
 
+      def description
+        present(model).description(strip_tags: true)
+      end
+
       delegate :online_meeting?, to: :model
 
       private
