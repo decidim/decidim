9c7c0f3a4b0939c9bee8eb273c3614a1abc17e42
lahdeero <eero.lahdenpera@gmail.com>
2021-12-16 12:03:11 +0200

    Update comment

--- decidim-core/lib/decidim/reminder_registry.rb
+++ decidim-core/lib/decidim/reminder_registry.rb
@@ -2,14 +2,17 @@
 
 module Decidim
   # This class acts as a registry for reminders. Each reminder needs a name,
-  # reminder times and a manager class, that will be used for generating reminders.
+  # and a manager class, that will be used for generating the actual sendable reminders.
   # Reminder times could be used to define intervals between reminders, so that user
-  # isn't reminded too often.
+  # isn't reminded too often. In addition, you can register form and command classes
+  # for additional features, such as order reminders can be sent manually from the admin panel.
   #
   # In order to register a reminder, you can follow this example:
   #
   #     Decidim.reminders_registry.register(:orders) do |reminder_registry|
-  #       reminder_registry.manager_class = "Decidim::Budgets::VoteReminderGenerator"
+  #       reminder_registry.manager_class_name = "Decidim::Budgets::OrderReminderGenerator"
+  #       reminder_registry.form_class_name = "Decidim::Budgets::Admin::OrderReminderForm"
+  #       reminder_registry.command_class_name = "Decidim::Budgets::Admin::CreateOrderReminders"
   #
   #       reminder_registry.settings do |settings|
   #         settings.attribute :reminder_times, type: :array, default: [2.hours, 1.week, 2.weeks]
