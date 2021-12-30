1acb25a944a78664b9758ff01313e80dd125dfba
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-12-30 20:11:27 +0200

    Add the symbol type

--- decidim-core/lib/decidim/attributes.rb
+++ decidim-core/lib/decidim/attributes.rb
@@ -5,10 +5,12 @@ module Decidim
     autoload :TimeWithZone, "decidim/attributes/time_with_zone"
     autoload :LocalizedDate, "decidim/attributes/localized_date"
     autoload :CleanString, "decidim/attributes/clean_string"
+    autoload :Symbol, "decidim/attributes/symbol"
 
     ActiveModel::Type.register(:"decidim/attributes/time_with_zone", Decidim::Attributes::TimeWithZone)
     ActiveModel::Type.register(:"decidim/attributes/localized_date", Decidim::Attributes::LocalizedDate)
     ActiveModel::Type.register(:"decidim/attributes/clean_string", Decidim::Attributes::CleanString)
+    ActiveModel::Type.register(:symbol, Decidim::Attributes::Symbol)
     ActiveModel::Type.register(:date_time, ActiveModel::Type::DateTime) # Synonym for :datetime
   end
 end
---
+++ decidim-core/lib/decidim/attributes/symbol.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Attributes
+    # Custom attributes value to represent a Symbol.
+    class Symbol < ActiveModel::Type::Value
+      def type # :nodoc:
+        :symbol
+      end
+
+      private
+
+      def cast_value(value)
+        return value unless value.is_a?(Symbol)
+
+        value.to_sym
+      end
+    end
+  end
+end
