9ad45758dcf6a2d1bb112e1fb7b9290d788b0d60
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-12-30 20:10:54 +0200

    Add synonym for the :date_time attribute type

--- decidim-core/lib/decidim/attributes.rb
+++ decidim-core/lib/decidim/attributes.rb
@@ -9,5 +9,6 @@ module Decidim
     ActiveModel::Type.register(:"decidim/attributes/time_with_zone", Decidim::Attributes::TimeWithZone)
     ActiveModel::Type.register(:"decidim/attributes/localized_date", Decidim::Attributes::LocalizedDate)
     ActiveModel::Type.register(:"decidim/attributes/clean_string", Decidim::Attributes::CleanString)
+    ActiveModel::Type.register(:date_time, ActiveModel::Type::DateTime) # Synonym for :datetime
   end
 end
