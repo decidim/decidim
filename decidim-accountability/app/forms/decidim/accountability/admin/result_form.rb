25e592272b18b0eb458d7eecf726b9e4eac7f17e
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-12-31 18:27:58 +0200

    Fix accountability result form after ActiveModel::Attributes migration

--- decidim-accountability/app/forms/decidim/accountability/admin/result_form.rb
+++ decidim-accountability/app/forms/decidim/accountability/admin/result_form.rb
@@ -59,7 +59,7 @@ module Decidim
         #
         # Returns a Decidim::Scope
         def scope
-          @scope ||= @decidim_scope_id ? current_component.scopes.find_by(id: @decidim_scope_id) : current_component.scope
+          @scope ||= attributes["decidim_scope_id"] ? current_component.scopes.find_by(id: attributes["decidim_scope_id"]) : current_component.scope
         end
 
         # Scope identifier
