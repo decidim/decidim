3edc5b613b13123d2b292ba9c6a5ab0b96a7387d
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-01-05 19:23:41 +0200

    Move respond_to? override to module body instead of included block

--- decidim-core/lib/decidim/attribute_object/model.rb
+++ decidim-core/lib/decidim/attribute_object/model.rb
@@ -28,19 +28,6 @@ module Decidim
 
         # Default attributes for all models
         attribute :id
-
-        # Override the ActiveModel::AttributeMethods#respond_to? method because
-        # otherwise this would fail in case it is called very early after the
-        # object initialization, such as the `map_model` methods in the form
-        # classes. We don't need the functionality provided by
-        # ActiveModel::AttributeMethods for the attribute methods which checks
-        # whether attributes[method] returns a value (which calls `attributes`
-        # that is problematic).
-        # rubocop:disable Style/OptionalBooleanParameter
-        def respond_to?(method, include_private_methods = false)
-          respond_to_without_attributes?(method, include_private_methods)
-        end
-        # rubocop:enable Style/OptionalBooleanParameter
       end
 
       class_methods do
@@ -206,6 +193,19 @@ module Decidim
       # The to_hash alias is needed for the as_json call which calls this method
       # if defined.
       alias to_hash to_h
+
+      # Override the ActiveModel::AttributeMethods#respond_to? method because
+      # otherwise this would fail in case it is called very early after the
+      # object initialization, such as the `map_model` methods in the form
+      # classes. We don't need the functionality provided by
+      # ActiveModel::AttributeMethods for the attribute methods which checks
+      # whether attributes[method] returns a value (which calls `attributes`
+      # that is problematic).
+      # rubocop:disable Style/OptionalBooleanParameter
+      def respond_to?(method, include_private_methods = false)
+        respond_to_without_attributes?(method, include_private_methods)
+      end
+      # rubocop:enable Style/OptionalBooleanParameter
     end
   end
 end
