f6b769003963c6816329f0c68bb57ae0a68cdd62
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-01-05 17:22:35 +0200

    Fix the scope fetching for the proposal forms

--- decidim-proposals/app/forms/decidim/proposals/proposal_form.rb
+++ decidim-proposals/app/forms/decidim/proposals/proposal_form.rb
@@ -54,14 +54,14 @@ module Decidim
       #
       # Returns a Decidim::Scope
       def scope
-        @scope ||= @scope_id ? current_component.scopes.find_by(id: @scope_id) : current_component.scope
+        @scope ||= @attributes["scope_id"].value ? current_component.scopes.find_by(id: @attributes["scope_id"].value) : current_component.scope
       end
 
       # Scope identifier
       #
       # Returns the scope identifier related to the proposal
       def scope_id
-        @scope_id || scope&.id
+        super || scope&.id
       end
 
       def geocoding_enabled?
