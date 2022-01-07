9842aa64d7c5c1991c25b9b48af9e0bc3b879b2a
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-01-07 21:40:21 +0200

    Make the symbol casting more robust

--- decidim-core/lib/decidim/attributes/symbol.rb
+++ decidim-core/lib/decidim/attributes/symbol.rb
@@ -13,6 +13,9 @@ module Decidim
       def cast_value(value)
         return value if value.is_a?(::Symbol)
 
+        value = value.to_s if !value.respond_to?(:to_sym) && value.respond_to?(:to_s)
+        return unless value.respond_to?(:to_sym)
+
         value.to_sym
       end
     end
