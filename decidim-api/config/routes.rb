f1939e449f7879d85a0af47ab0369ff2e43a04b7
Alexandru Emil Lupu <contact@alecslupu.ro>
2022-01-14 13:08:41 +0200

    Replace graphql-docs npm package with gem (#8631)
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    Co-authored-by: Leonardo Diez <leiodd@gmail.com>

--- decidim-api/config/routes.rb
+++ decidim-api/config/routes.rb
@@ -3,6 +3,7 @@
 Decidim::Api::Engine.routes.draw do
   get "/graphiql", to: "graphiql#show", graphql_path: "/api", as: :graphiql
   get "/docs", to: "documentation#show", as: :documentation
+  get "/docs/*path", to: "documentation#show"
   get "/", to: redirect("/api/docs")
   post "/" => "queries#create", as: :root
 end
