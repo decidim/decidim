ceb8e84c89ef9528850d37e8905420ff43266e0c
quino <quino@populate.tools>
2022-01-18 19:54:24 +0100

    Add fields in manifest to allow a2h button

--- decidim-core/app/packs/src/decidim/sw/loader.js
+++ decidim-core/app/packs/src/decidim/sw/loader.js
@@ -6,3 +6,37 @@ window.addEventListener("load", async () => {
     console.log("Your browser doesn't support service workers 🤷‍♀️");
   }
 });
+
+// Visits control to prompt the a2hs confirmation banner
+let visits = localStorage.getItem("visits_counter") || 0
+let visitsReached = false;
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  localStorage.setItem("visits_counter", parseInt(visits, 10) + 1)
+  window.deferredPrompt = event;
+
+  if (visits > 3)
+  {visitsReached = true}
+  else
+  {event.preventDefault();}
+
+});
+
+$(document).ready(function() {
+  window.addEventListener("appinstalled", () => {
+    localStorage.removeItem("visits_counter");
+    console.log("PWA was installed");
+  });
+
+  window.addEventListener("touchend", async () => {
+    if (visitsReached === true) {
+      const promptEvent = window.deferredPrompt;
+      if (!promptEvent) {
+        // The deferred prompt isn't available.
+        return;
+      }
+      promptEvent.prompt();
+      window.deferredPrompt = null;
+    }
+  });
+});
