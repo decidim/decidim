10a13c51a96c343df04b640cccff942d426ff4a4
Armand Fardeau <fardeauarmand@gmail.com>
2022-01-18 14:31:10 +0100

    Revert deletion of accessor

--- decidim-initiatives/lib/decidim/initiatives.rb
+++ decidim-initiatives/lib/decidim/initiatives.rb
@@ -14,6 +14,7 @@ module Decidim
     # Public setting that defines whether creation is allowed to any validated
     # user or not. Defaults to true.
     config_accessor :creation_enabled do
+      true
     end
 
     # Public Setting that defines the similarity minimum value to consider two
