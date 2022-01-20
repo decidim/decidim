f54c266b088231cfe93cd4fb7914baaa76146845
quino <quino@populate.tools>
2022-01-20 18:24:44 +0100

    Add push notifications request to user

--- decidim-core/app/packs/src/decidim/sw/loader.js
+++ decidim-core/app/packs/src/decidim/sw/loader.js
@@ -1,11 +1,44 @@
+function removeNotAllowedSettings () {
+  $(".push-notifications").remove();
+}
+
+function displayAllowedSettings () {
+  $(".push-notifications").removeClass("hide")
+}
+
+function toggleNotificationsReminder () {
+  if (Notification.permission === 'granted') {
+    $(".push-notifications__reminder").addClass("hide")
+  }else{
+    $(".push-notifications__reminder").removeClass("hide")
+  }
+}
+
+$(document).ready(function() {
+  $("#user_allow_push_notifications").on("change", function () {
+    if(this.checked && ("serviceWorker" in navigator)) {
+      Notification.requestPermission(async (permission) => {
+        toggleNotificationsReminder()
+      });
+    }
+  });
+});
+
+
 // check if the browser supports serviceWorker at all
 window.addEventListener("load", async () => {
   if ("serviceWorker" in navigator) {
-    await navigator.serviceWorker.register("/sw.js", { scope: "/" });
+    // eslint-disable-next-line no-unused-vars
+    toggleNotificationsReminder();
+    displayAllowedSettings();
+    const registration = await navigator.serviceWorker.register("/sw.js", { scope: "/" });
   } else {
+    removeNotAllowedSettings();
     console.log("Your browser doesn't support service workers 🤷‍♀️");
   }
 });
+
+
 
 // Visits control to prompt the a2hs confirmation banner
 let visits = localStorage.getItem("visits_counter") || 0
