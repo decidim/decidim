324824189d5899bd7f04c25479845b07a5ce3fe4
Andrés Pereira de Lucena <andreslucena@gmail.com>
2022-01-26 16:25:25 +0100

    Make the scope selection required on initiative's creation

--- decidim-initiatives/spec/system/create_initiative_spec.rb
+++ decidim-initiatives/spec/system/create_initiative_spec.rb
@@ -267,6 +267,14 @@ describe "Initiative", type: :system do
             end
           end
 
+          context "when the scope isn't selected" do
+            it "shows an error" do
+              select("Online", from: "Signature collection type")
+              find_button("Continue").click
+              expect(page).to have_content("There's an error in this field")
+            end
+          end
+
           context "when the initiative type does not enable custom signature end date" do
             it "does not show the signature end date" do
               expect(page).not_to have_content("End of signature collection period")
