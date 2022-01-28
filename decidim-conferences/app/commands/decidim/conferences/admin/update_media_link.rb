ecd66b34d85d336646072c47c76fbd786b1a3ac8
Alexandru-Emil Lupu <contact@alecslupu.ro>
2022-01-28 14:40:03 +0200

    Change Rectify::Command in Conferences

--- decidim-conferences/app/commands/decidim/conferences/admin/update_media_link.rb
+++ decidim-conferences/app/commands/decidim/conferences/admin/update_media_link.rb
@@ -5,7 +5,7 @@ module Decidim
     module Admin
       # A command with all the business logic when updating a conference
       # speaker in the system.
-      class UpdateMediaLink < Rectify::Command
+      class UpdateMediaLink < Decidim::Command
         # Public: Initializes the command.
         #
         # form - A form object with the params.
