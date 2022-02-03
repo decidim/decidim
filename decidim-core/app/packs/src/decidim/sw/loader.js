3bdb1ad9fbed84ec43613d16a178c24a6c9652a6
Hugoren Martinako <aumpfbahn@gmail.com>
2022-02-03 10:00:49 +0100

    saving possible non-existent dom code

--- decidim-core/app/packs/src/decidim/sw/loader.js
+++ decidim-core/app/packs/src/decidim/sw/loader.js
@@ -1,18 +1,27 @@
 // check if the browser supports serviceWorker at all
 window.addEventListener("load", async () => {
   if ("serviceWorker" in navigator) {
-    const vapidPublicKeyElement = JSON.parse(document.querySelector("#vapidPublicKey").value)
     const registration = await navigator.serviceWorker.register("/sw.js", { scope: "/" });
-    const subscription = await registration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: new Uint8Array(vapidPublicKeyElement)
-    });
 
-    await fetch("/subscribe_to_notifications", {
-      headers: { "Content-Type": "application/json" },
-      method: "POST",
-      body: JSON.stringify(subscription)
-    });
+    if (registration) {
+      const vapidElement = document.querySelector("#vapidPublicKey")
+      // element could not exist in DOM
+      if (vapidElement) {
+        const vapidPublicKeyElement = JSON.parse(vapidElement.value)
+        const subscription = await registration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: new Uint8Array(vapidPublicKeyElement)
+        });
+
+        if (subscription) {
+          await fetch("/subscribe_to_notifications", {
+            headers: { "Content-Type": "application/json" },
+            method: "POST",
+            body: JSON.stringify(subscription)
+          });
+        }
+      }
+    }
   } else {
     console.log("Your browser doesn't support service workers 🤷‍♀️");
   }
