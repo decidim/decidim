661ab7b5f476edd47ed5e8539ac1f45868cd1758
Alexandru-Emil Lupu <contact@alecslupu.ro>
2022-02-07 12:41:48 +0200

    Add importmaps to Conferences

--- decidim-conferences/app/packs/entrypoints/decidim_conferences_admin.js
+++ decidim-conferences/app/packs/entrypoints/decidim_conferences_admin.js
@@ -1,7 +1,4 @@
-import "src/decidim/conferences/admin/conferences"
-import "src/decidim/conferences/admin/conference_speakers"
-import "src/decidim/conferences/admin/conference_invite_form"
-import "src/decidim/slug_form"
+import "src/decidim/conferences/admin/index"
 
 // Images
 require.context("../images", true)
