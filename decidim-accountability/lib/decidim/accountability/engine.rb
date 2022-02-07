7fb4aec62e073259727fc90bd0e7bc57b4f910b8
Alexandru-Emil Lupu <contact@alecslupu.ro>
2022-02-07 11:52:15 +0200

    Add importmaps to accountability

--- decidim-accountability/lib/decidim/accountability/engine.rb
+++ decidim-accountability/lib/decidim/accountability/engine.rb
@@ -43,6 +43,15 @@ module Decidim
       initializer "decidim_accountability.webpacker.assets_path" do
         Decidim.register_assets_path File.expand_path("app/packs", root)
       end
+
+      initializer "decidim_accountability.importmap", before: "importmap" do |app|
+        app.config.importmap.paths << Engine.root.join("config/importmap.rb")
+        app.config.importmap.cache_sweepers << Engine.root.join("app/packs/src")
+      end
+
+      initializer "decidim_accountability.importmap.assets", before: "importmap.assets" do |app|
+        app.config.assets.paths << Engine.root.join("app/packs") if app.config.respond_to?(:assets)
+      end
     end
   end
 end
