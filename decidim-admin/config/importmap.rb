9efa9e73bf03044a5cd7f2656346c09e7e666267
Alexandru-Emil Lupu <contact@alecslupu.ro>
2022-02-07 08:25:37 +0200

    Preloading modules

--- decidim-admin/config/importmap.rb
+++ decidim-admin/config/importmap.rb
@@ -4,4 +4,4 @@ pin "@tarekraafat/autocomplete.js", to: "https://ga.jspm.io/npm:@tarekraafat/aut
 pin "html5sortable/dist/html5sortable.es", to: "https://ga.jspm.io/npm:html5sortable@0.10.0/dist/html5sortable.js"
 pin "moment", to: "https://ga.jspm.io/npm:moment@2.29.1/moment.js"
 
-pin_all_from File.expand_path("../app/packs/src/decidim", __dir__), under: "src/decidim"
+pin_all_from File.expand_path("../app/packs/src/decidim", __dir__), under: "src/decidim", preload: true
--- decidim-dev/config/importmap.rb
+++ decidim-dev/config/importmap.rb
@@ -5,4 +5,4 @@ pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.19/nodelibs/bro
 pin "axe-core", to: "https://ga.jspm.io/npm:axe-core@4.1.4/axe.js"
 
 pin "src/decidim/icon"
-pin_all_from File.expand_path("../app/packs/src/decidim/dev", __dir__), under: "src/decidim/dev"
+pin_all_from File.expand_path("../app/packs/src/decidim/dev", __dir__), under: "src/decidim/dev", preload: true
