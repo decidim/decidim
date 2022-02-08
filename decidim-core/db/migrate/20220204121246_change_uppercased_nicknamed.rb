44e0533b9225d9dee43198ba56c2dabc856e5979
eliegaboriau <elie@opensourcepolitics.eu>
2022-02-08 11:08:22 +0100

    linter

--- decidim-core/db/migrate/20220204121246_change_uppercased_nicknamed.rb
+++ decidim-core/db/migrate/20220204121246_change_uppercased_nicknamed.rb
@@ -9,11 +9,11 @@ class ChangeUppercasedNicknamed < ActiveRecord::Migration[6.0]
       # if already downcased, don't care
       next if user.nickname.downcase == user.nickname
 
-      Decidim::User.where("nickname ILIKE ?", user.nickname.downcase).order(:created_at).each_with_index do |similar_user,index|
+      Decidim::User.where("nickname ILIKE ?", user.nickname.downcase).order(:created_at).each_with_index do |similar_user, index|
         next if has_changed.include? similar_user
 
         # change his nickname to the lowercased one with -1 if it's the first, -2 if it's the second etc
-        similar_user.nickname.update!("#{similar_user.nickname.downcase}-#{index+1}")
+        similar_user.nickname.update!("#{similar_user.nickname.downcase}-#{index + 1}")
         has_changed.append(similar_user)
       end
 
