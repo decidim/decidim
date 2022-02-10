2afde5f3e33643d56f1fc725c690cde380ea4083
Alexandru-Emil Lupu <contact@alecslupu.ro>
2022-02-10 11:00:43 +0200

    Using Quill to own importmap

--- decidim-core/app/packs/entrypoints/decidim_core.js
+++ decidim-core/app/packs/entrypoints/decidim_core.js
@@ -1,6 +1,7 @@
 /* eslint no-unused-vars: 0 */
 /* eslint id-length: ["error", { "exceptions": ["$"] }] */
 
+import "src/decidim/core"
 // CSS
 import "entrypoints/decidim_core.scss"
 
--- decidim-core/config/importmap.rb
+++ decidim-core/config/importmap.rb
@@ -4,8 +4,6 @@ pin "core-js/stable", to: "https://ga.jspm.io/npm:core-js@3.1.3/stable/index.js"
 pin "regenerator-runtime/runtime", to: "https://ga.jspm.io/npm:regenerator-runtime@0.13.7/runtime.js"
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
 
-pin "buffer", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.19/nodelibs/browser/buffer.js"
-pin "quill", to: "https://ga.jspm.io/npm:quill@1.3.7/dist/quill.js"
 pin "lodash/defaultsDeep", to: "https://ga.jspm.io/npm:lodash@4.17.4/defaultsDeep.js"
 
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@6.0.0/lib/assets/compiled/rails-ujs.js"
