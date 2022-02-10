90c57ea7d7c4de46a0301c15a1bba85227b4f583
Alexandru-Emil Lupu <contact@alecslupu.ro>
2022-02-10 21:12:36 +0200

    Fix decidim-templates errors

--- decidim-admin/config/importmap.rb
+++ decidim-admin/config/importmap.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 pin "@tarekraafat/autocomplete.js", to: "https://ga.jspm.io/npm:@tarekraafat/autocomplete.js@10.2.6/dist/autoComplete.min.js"
-pin "html5sortable/dist/html5sortable.es", to: "https://ga.jspm.io/npm:html5sortable@0.10.0/dist/html5sortable.js"
+pin "html5sortable/dist/html5sortable.es", to: "https://ga.jspm.io/npm:html5sortable@0.10.0/dist/html5sortable.es.js"
 pin "moment", to: "https://ga.jspm.io/npm:moment@2.29.1/moment.js"
 
 pin_all_from File.expand_path("../app/packs/src/decidim", __dir__), under: "src/decidim", preload: true
