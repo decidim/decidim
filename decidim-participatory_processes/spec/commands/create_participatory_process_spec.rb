76e968eb6a100b654b633a31d97d69bd715bcf52
Andrés Pereira de Lucena <andreslucena@gmail.com>
2022-02-28 11:46:35 +0100

    Fix processes creation form with stats, metrics and announcements

--- decidim-participatory_processes/spec/commands/create_participatory_process_spec.rb
+++ decidim-participatory_processes/spec/commands/create_participatory_process_spec.rb
@@ -45,7 +45,10 @@ module Decidim::ParticipatoryProcesses
         area: area,
         errors: errors,
         related_process_ids: related_process_ids,
-        participatory_process_group: participatory_process_group
+        participatory_process_group: participatory_process_group,
+        show_statistics: false,
+        show_metrics: false,
+        announcement: { en: "message" },
       )
     end
     let(:invalid) { false }
@@ -114,6 +117,12 @@ module Decidim::ParticipatoryProcesses
         subject.call
         expect(process.steps.count).to eq(1)
         expect(process.steps.first).to be_active
+      end
+
+      it "doesn't enable by default stats and metrics" do
+        subject.call
+        expect(process.show_statistics).to eq(false)
+        expect(process.show_metrics).to eq(false)
       end
 
       it "adds the admins as followers" do
