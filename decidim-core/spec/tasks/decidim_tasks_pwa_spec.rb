db434a4dcb9e93aa7b1d065d628a77921f9fd483
quino <quino@populate.tools>
2022-03-01 10:45:48 +0100

    Add namespace to vapid generators

--- decidim-core/spec/tasks/decidim_tasks_pwa_spec.rb
+++ decidim-core/spec/tasks/decidim_tasks_pwa_spec.rb
@@ -6,7 +6,7 @@ describe "Executing Decidim Generators tasks" do
   describe "rake decidim:generate_vapid_keys", type: :task do
     context "when executing task" do
       it "shows the VAPID public and private keys" do
-        Rake::Task[:"decidim:generate_vapid_keys"].invoke
+        Rake::Task[:"decidim:pwa:generate_vapid_keys"].invoke
         expect($stdout.string).to include("VAPID keys correctly generated.")
         expect($stdout.string).to include("VAPID public key is")
         expect($stdout.string).to include("VAPID private key is")
