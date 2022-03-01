6e6733c8d0314abe280f6f1afd3a5d4b32b6cd23
amaliacardenas <amaliacardenas@gmail.com>
2022-03-01 13:51:24 +0100

    Fix tests in explore posts and add validation

--- decidim-blogs/app/forms/decidim/blogs/admin/post_form.rb
+++ decidim-blogs/app/forms/decidim/blogs/admin/post_form.rb
@@ -14,6 +14,7 @@ module Decidim
 
         validates :title, translatable_presence: true
         validates :body, translatable_presence: true
+        validate :can_set_author
 
         def user_or_group
           @user_or_group ||= Decidim::UserBaseEntity.find_by(
@@ -24,6 +25,19 @@ module Decidim
 
         def author
           user_or_group || current_organization
+        end
+
+        private
+
+        def can_set_author
+          return if author == current_user.organization
+          return if author == current_user
+
+          user_groups = Decidim::UserGroups::ManageableUserGroups.for(current_user).verified
+          return if user_groups.include? author
+          return if author == Post.find(id)&.author
+
+          errors.add(:decidim_author_id, :invalid)
         end
       end
     end
--- decidim-blogs/spec/system/explore_posts_spec.rb
+++ decidim-blogs/spec/system/explore_posts_spec.rb
@@ -54,10 +54,6 @@ describe "Explore posts", type: :system do
 
   describe "show" do
     let(:posts_count) { 1 }
-    # let(:current_organization) { create(:organization) }
-    # let(:current_user) { create :user, organization: current_organization }
-    # let(:user_group) { create(:user_group, :verified, organization: current_organization) }
-    # let(:decidim_author_id) { "" }
     let(:author) { organization }
     let!(:post) { create(:post, component: component, author: author) }
 
@@ -66,28 +62,30 @@ describe "Explore posts", type: :system do
     end
 
     context "when author is an organization" do
-      it "shows 'Official blog' as the author" do
-        expect(page).to have_content("Official debate") # error - should show Official blog
+      it "shows 'Official' as the author" do
+        within ".author__name" do
+          expect(page).to have_content("Official")
+        end
       end
     end
 
     context "when author is a user_group" do
-      let(:user_group) { create(:user_group, :verified, organization: organization) }
-      # let(:decidim_author_id) { user_group.id }
-      let!(:post) { create(:post, component: component, author: user_group) }
+      let(:author) { create(:user_group, :verified, organization: organization) }
 
       it "shows user group as the author" do
-        expect(page).to have_content(user_group.name)
+        within ".author__name" do
+          expect(page).to have_content(author.name)
+        end
       end
     end
 
     context "when author is a user" do
-      # let(:decidim_author_id) { current_user.id }
       let(:author) { user }
-      let!(:post) { create(:post, component: component, author: author) }
 
       it "shows user as the author" do
-        expect(page).to have_content(user.name)
+        within ".author__name" do
+          expect(page).to have_content(user.name)
+        end
       end
     end
 
