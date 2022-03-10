e176c1b4a62b5b39fb326eb414fe72f46dc25c7f
Ivan Vergés <ivan@platoniq.net>
2022-03-10 15:44:28 +0100

    Backport "Show signatures in answered initiatives" to v0.26 (#8991)
    Co-authored-by: amaliacardenas <amaliacardenas@gmail.com>

--- decidim-initiatives/spec/cells/decidim/initiatives/initiative_m_cell_spec.rb
+++ decidim-initiatives/spec/cells/decidim/initiatives/initiative_m_cell_spec.rb
@@ -10,7 +10,8 @@ module Decidim::Initiatives
 
     let(:my_cell) { cell("decidim/initiatives/initiative_m", initiative, context: { show_space: show_space }) }
     let(:cell_html) { my_cell.call }
-    let!(:initiative) { create(:initiative, hashtag: "my_hashtag") }
+    let(:state) { :published }
+    let!(:initiative) { create(:initiative, hashtag: "my_hashtag", state: state) }
     let(:user) { create :user, organization: initiative.organization }
 
     before do
@@ -24,8 +25,56 @@ module Decidim::Initiatives
         expect(subject).to have_css(".card--initiative")
       end
 
+      shared_examples_for "card shows signatures" do
+        it "shows signatures" do
+          expect(subject).to have_css(".progress__bar__number")
+          expect(subject).to have_css(".progress__bar__text")
+          expect(subject.to_s).to include("signatures")
+        end
+      end
+
+      shared_examples_for "card does not show signatures" do
+        it "does not show signatures" do
+          expect(subject).not_to have_css(".progress__bar__number")
+          expect(subject).not_to have_css(".progress__bar__text")
+          expect(subject.to_s).not_to include("signatures")
+        end
+      end
+
       it "renders the hashtag" do
         expect(subject).to have_content("#my_hashtag")
+      end
+
+      it_behaves_like "card shows signatures"
+
+      context "when initiative state is rejected" do
+        let(:state) { :rejected }
+
+        it_behaves_like "card shows signatures"
+      end
+
+      context "when initiative state is accepted" do
+        let(:state) { :accepted }
+
+        it_behaves_like "card shows signatures"
+      end
+
+      context "when initiative state is created" do
+        let(:state) { :created }
+
+        it_behaves_like "card does not show signatures"
+      end
+
+      context "when initiative state is validating" do
+        let(:state) { :validating }
+
+        it_behaves_like "card does not show signatures"
+      end
+
+      context "when initiative state is discarded" do
+        let(:state) { :discarded }
+
+        it_behaves_like "card does not show signatures"
       end
     end
   end
