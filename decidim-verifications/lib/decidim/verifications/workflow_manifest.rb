489dc64232745a0289b13fe5c33780e49fdd6a35
Alexandru-Emil Lupu <contact@alecslupu.ro>
2022-03-13 09:39:59 +0200

    Fixing Rails::Engine parameters

--- decidim-verifications/lib/decidim/verifications/workflow_manifest.rb
+++ decidim-verifications/lib/decidim/verifications/workflow_manifest.rb
@@ -30,7 +30,7 @@ module Decidim
       include Decidim::AttributeObject::Model
 
       attribute :engine, Rails::Engine
-      attribute :admin_engine, Rails::Engine
+      attribute :admin_engine, Rails::Engine, **{}
       attribute :form, String
       attribute :expires_in, ActiveSupport::Duration, default: 0.minutes
       attribute :action_authorizer, String
