c59818258bb279b2c70a0bf643005a4be37b694d
Andrés Pereira de Lucena <andreslucena@gmail.com>
2022-03-15 09:25:38 +0100

    Apply suggestions from code review

--- decidim-core/app/packs/src/decidim/back_to_list.js
+++ decidim-core/app/packs/src/decidim/back_to_list.js
@@ -2,7 +2,7 @@
  * Changes "Back to list" links to the one saved in sessionStorage API
  * To apply this to a link, at least one element must have the class "js-back-to-list".
  * For this to work it needs the filteredParams in SessionStorage, that's saved on FormFilterComponent.
- * @param {HTMLElement} links - Hyperlinks elements that point to the filters page that will use the fitererd params
+ * @param {NodeList} links - Hyperlinks elements that point to the filters page that will use the fitererd params
  * @returns {void}
  */
 export default function backToListLink(links) {
