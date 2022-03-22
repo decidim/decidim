d6904b4230d93f3a9fe3a4bf4201f6e02a6e27c6
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2022-03-22 11:12:50 +0100

    Fix Twitter hashtag search when it starts with a number (#9039)

---
+++ decidim-core/app/helpers/decidim/twitter_search_helper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Decidim
+  module TwitterSearchHelper
+    # Builds the URL for Twitter's hashtag search.
+    #
+    # @param hashtag [String] The hasthag to search
+    #
+    # @return [String]
+    def twitter_hashtag_url(hashtag)
+      format("https://twitter.com/hashtag/%{hashtag}?src=hash", hashtag: hashtag)
+    end
+  end
+end
