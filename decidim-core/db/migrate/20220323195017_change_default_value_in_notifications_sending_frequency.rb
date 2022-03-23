a7dcfff760cc814ad1bad50f452ee19e194fed2b
quino <quino@populate.tools>
2022-03-23 22:20:44 +0100

    Fix error in migration

--- decidim-core/db/migrate/20220323195017_change_default_value_in_notifications_sending_frequency.rb
+++ decidim-core/db/migrate/20220323195017_change_default_value_in_notifications_sending_frequency.rb
@@ -1,4 +1,4 @@
-class ChangeDefaultValueInsNotificationsSendingFrequency < ActiveRecord::Migration[6.0]
+class ChangeDefaultValueInNotificationsSendingFrequency < ActiveRecord::Migration[6.0]
   def change
     change_column_default :decidim_users, :notifications_sending_frequency, "none"
   end
