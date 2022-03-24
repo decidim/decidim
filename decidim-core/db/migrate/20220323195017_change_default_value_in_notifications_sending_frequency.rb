e23d6acc723eab16ed931977a7fb7284fdd34eb1
quino <quino@populate.tools>
2022-03-24 03:26:30 +0100

    Fix lint

--- decidim-core/db/migrate/20220323195017_change_default_value_in_notifications_sending_frequency.rb
+++ decidim-core/db/migrate/20220323195017_change_default_value_in_notifications_sending_frequency.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ChangeDefaultValueInNotificationsSendingFrequency < ActiveRecord::Migration[6.0]
   def change
     change_column_default :decidim_users, :notifications_sending_frequency, "none"
--- decidim-core/db/migrate/20220323195258_add_index_to_decidim_users_notifications_sending_frequency.rb
+++ decidim-core/db/migrate/20220323195258_add_index_to_decidim_users_notifications_sending_frequency.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddIndexToDecidimUsersNotificationsSendingFrequency < ActiveRecord::Migration[6.0]
   def change
     add_index :decidim_users, :notifications_sending_frequency
