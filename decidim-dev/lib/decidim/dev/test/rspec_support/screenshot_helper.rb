e8e045caaa13b9f0961a358e5ef4f945a74bcf8a
Andrés Pereira de Lucena <andreslucena@gmail.com>
2022-03-24 09:15:46 +0100

    Keep sanitization comment

--- decidim-dev/lib/decidim/dev/test/rspec_support/screenshot_helper.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/screenshot_helper.rb
@@ -13,6 +13,7 @@ module ActionDispatch
         # fails at GitHub actions. See the list of unallowed characters from:
         # https://github.com/actions/toolkit/blob/main/packages/artifact/docs/additional-information.md#non-supported-characters
         def image_name
+          # By default, this only cleans up the forward and backward slash characters.
           sanitized_method_name = method_name.tr("/\\()\":<>|*?", "-----------")
           name = "#{unique}_#{sanitized_method_name}"
           name[0...225]
