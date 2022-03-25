a80ed407e4789879d272d8ff56d186e4451fea88
Eero Lahdenperä <eero.lahdenpera@mainiotech.fi>
2022-03-25 12:05:50 +0200

    Dont add external link inside editor

--- decidim-core/app/packs/src/decidim/external_link.js
+++ decidim-core/app/packs/src/decidim/external_link.js
@@ -5,6 +5,9 @@ const EXCLUDE_CLASSES = [
   "footer-social__icon",
   "logo-cityhall"
 ];
+const EXCLUE_ANCESTORS_CLASSES = [
+  "editor-container"
+]
 const EXCLUDE_REL = ["license", "decidim"];
 
 const DEFAULT_MESSAGES = {
@@ -25,6 +28,9 @@ export default class ExternalLink {
 
   setup() {
     if (EXCLUDE_CLASSES.some((cls) => this.$link.hasClass(cls))) {
+      return;
+    }
+    if (EXCLUE_ANCESTORS_CLASSES.some((cls) => this.$link.parents().hasClass(cls))) {
       return;
     }
     if (
