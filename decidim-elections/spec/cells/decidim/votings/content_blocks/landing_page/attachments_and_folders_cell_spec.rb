80edb84b8c4ec0049ec4517630cf8b05cd314fc3
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2022-04-06 10:22:34 +0200

    Fix attachments when called from Cells (#9136)
    * Drop .html extension on partials filenames
    
    * Add spec for votings' AttachmentsAndFoldersCell

---
+++ decidim-elections/spec/cells/decidim/votings/content_blocks/landing_page/attachments_and_folders_cell_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Decidim::Votings::ContentBlocks::LandingPage::AttachmentsAndFoldersCell, type: :cell do
+  subject { cell(content_block.cell, content_block).call }
+
+  controller Decidim::Votings::VotingsController
+
+  let(:organization) { create(:organization) }
+  let(:voting) { create(:voting, :published, organization: organization) }
+  let(:content_block) { create :content_block, organization: organization, manifest_name: :attachments_and_folders, scope_name: :voting_landing_page }
+  let!(:attachment_pdf) { create(:attachment, :with_pdf, attached_to: voting) }
+  let!(:attachment_img) { create(:attachment, :with_image, attached_to: voting) }
+
+  before do
+    allow(controller).to receive(:current_participatory_space).and_return(voting)
+    allow(controller).to receive(:current_organization).and_return(organization)
+  end
+
+  context "when rendering attachments" do
+    it "shows image attachment" do
+      expect(subject).to have_selector(".attachments .thumbnail", count: 1)
+    end
+
+    it "shows pdf attachment" do
+      expect(subject).to have_selector(".documents .card__link", count: 1)
+    end
+  end
+end
