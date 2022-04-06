43e2010d5b71c150cf0373f3d8ce53ccdd737249
Ivan Vergés <ivan@platoniq.net>
2022-04-06 13:41:12 +0200

    setup a test for the helper

--- decidim-blogs/app/helpers/decidim/blogs/admin/posts_helper.rb
+++ decidim-blogs/app/helpers/decidim/blogs/admin/posts_helper.rb
@@ -24,11 +24,11 @@ module Decidim
             [current_organization.name, ""],
             [current_user.name, current_user.id]
           ]
-          select_options << [form.object.author.name, form.object.author.id] if form.object.author.is_a?(Decidim::User) && form.object.author.id != current_user.id
           if current_organization.user_groups_enabled? && Decidim::UserGroups::ManageableUserGroups.for(current_user).verified.any?
             user_groups = Decidim::UserGroups::ManageableUserGroups.for(current_user).verified
             select_options += user_groups.map { |g| [g.name, g.id] }
           end
+          select_options << [form.object.author.name, form.object.author.id] unless select_options.pluck(1).include?(form.object.author.id)
 
           form.select(name, select_options)
         end
--- decidim-blogs/spec/helpers/decidim/blogs/admin/post_helper_spec.rb
+++ decidim-blogs/spec/helpers/decidim/blogs/admin/post_helper_spec.rb
@@ -5,12 +5,96 @@ require "spec_helper"
 module Decidim::Blogs::Admin
   describe PostsHelper, type: :helper do
     describe "#post_author_select_field" do
-      let(:form) { nil }
+      let(:organization) { create :organization }
+      let(:user) { create :user, :admin, :confirmed, organization: organization }
+      let!(:another_user) { create :user, :admin, :confirmed, organization: organization }
+      let!(:user_group) { create :user_group, :verified, decidim_organization_id: organization.id, users: [user] }
+      let!(:another_user_group) { create :user_group, :verified, decidim_organization_id: organization.id, users: [another_user] }
+      let(:name) { "a-select-form-name" }
+      let(:author) { user }
+      let(:form) do
+        double(
+          object: double(author: author)
+          ) 
+      end
 
-      context "when option strip_tags is invoked" do
-        it "strips the tags from the target string" do
-          expect(helper.post_author_select_field(form, name)).eq all_fields
+      let(:all_fields) do
+        [
+          [organization.name, ""], 
+          [user.name, user.id],
+          [user_group.name, user_group.id]
+        ]
+      end
+
+      let(:extra_fields) do
+        [
+          [organization.name, ""], 
+          [user.name, user.id],
+          [user_group.name, user_group.id],
+          [another_user.name, another_user.id]
+        ]
+      end
+
+      let(:basic_fields) do
+        [
+          [organization.name, ""], 
+          [user.name, user.id]
+        ]
+      end
+
+      let(:groups_enabled) { true }
+
+      before do
+        allow(helper).to receive(:current_organization).and_return(organization)
+        allow(helper).to receive(:current_user).and_return(user)
+        allow(organization).to receive(:user_groups_enabled?).and_return(groups_enabled)
+        allow(form).to receive(:select) { |_, array| array }
+      end
+
+      context "when all actors are present" do
+        it "Returns all types of authors" do
+          expect(helper.post_author_select_field(form, name)).to eq(all_fields)
         end
+      end
+
+      context "when author is a user's user_group" do
+        let(:author) { user_group }
+
+        #todo
+      end
+
+      context "when organization has no groups_enabled" do
+        let(:groups_enabled) { false }
+
+        it "Returns all types of authors" do
+          expect(helper.post_author_select_field(form, name)).to eq(basic_fields)
+        end
+
+        context "when author is a user's user_group" do
+          let(:author) { user_group }
+
+          #todo
+        end
+      end
+
+      context "when author is another user" do
+        let(:author) { another_user }
+
+        it "Returns all types of authors plus the original author" do
+          expect(helper.post_author_select_field(form, name)).to eq(extra_fields)
+        end
+      end
+
+      context "when author is the organization" do
+        let(:author) { organization }
+
+        #todo
+      end
+
+      context "when author is another user_group" do
+        let(:author) { another_user_group }
+
+        #todo
       end
     end
   end
