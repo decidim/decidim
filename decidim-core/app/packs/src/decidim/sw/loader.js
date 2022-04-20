a6877082451ca97276eaa15331e1c82683024ba6
quino <quino@populate.tools>
2022-04-20 15:10:05 +0200

    Hide sw mandatory elements when there's no sw

--- decidim-core/app/packs/src/decidim/sw/loader.js
+++ decidim-core/app/packs/src/decidim/sw/loader.js
@@ -3,6 +3,7 @@ window.addEventListener("load", async () => {
   if ("serviceWorker" in navigator) {
     await navigator.serviceWorker.register("/sw.js", { scope: "/" });
   } else {
+    document.querySelector(".sw-mandatory").remove();
     console.log("Your browser doesn't support service workers 🤷‍♀️");
   }
 });
