df1377020e564d3ad15ba373d0d5200ef9cadf06
quino <quino@populate.tools>
2022-04-21 12:37:19 +0200

    Reverse the loding order for sw mandatory items

--- decidim-core/app/packs/src/decidim/sw/loader.js
+++ decidim-core/app/packs/src/decidim/sw/loader.js
@@ -1,13 +1,14 @@
 // check if the browser supports serviceWorker at all
 window.addEventListener("load", async () => {
   if ("serviceWorker" in navigator) {
-    await navigator.serviceWorker.register("/sw.js", { scope: "/" });
-  } else {
     const mandatoryElements = document.querySelector(".sw-mandatory")
 
     if (mandatoryElements) {
-      mandatoryElements.remove();
+      mandatoryElements.style.display = "block";
     }
+
+    await navigator.serviceWorker.register("/sw.js", { scope: "/" });
+  } else {
     console.log("Your browser doesn't support service workers 🤷‍♀️");
   }
 });
