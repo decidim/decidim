668fe98a6bf426ed4ac30bfad03a6e177d3a4449
quino <quino@populate.tools>
2022-04-21 12:50:49 +0200

    Fix await bug

--- decidim-core/app/packs/src/decidim/sw/loader.js
+++ decidim-core/app/packs/src/decidim/sw/loader.js
@@ -1,13 +1,13 @@
 // check if the browser supports serviceWorker at all
 window.addEventListener("load", async () => {
   if ("serviceWorker" in navigator) {
-    const mandatoryElements = document.querySelector(".sw-mandatory")
+    await navigator.serviceWorker.register("/sw.js", { scope: "/" });
+
+    const mandatoryElements = document.querySelector(".sw-mandatory");
 
     if (mandatoryElements) {
       mandatoryElements.style.display = "block";
     }
-
-    await navigator.serviceWorker.register("/sw.js", { scope: "/" });
   } else {
     console.log("Your browser doesn't support service workers 🤷‍♀️");
   }
