8ba2e287de03cee4da55d9da2f53e542800c0b20
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-04-29 12:48:42 +0200

    Improve RedesignLayout concern

--- decidim-core/app/controllers/decidim/application_controller.rb
+++ decidim-core/app/controllers/decidim/application_controller.rb
@@ -22,6 +22,7 @@ module Decidim
     include DisableRedirectionToExternalHost
 
     include RedesignLayout
+    redesign
 
     helper Decidim::MetaTagsHelper
     helper Decidim::DecidimFormHelper
