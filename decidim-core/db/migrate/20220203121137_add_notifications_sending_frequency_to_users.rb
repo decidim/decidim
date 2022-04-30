6f5899c84c9990a6ca889cf34d45d757af9e85d9
Fernando Blat <fernando@blat.es>
2022-04-30 05:34:20 +0200

    Simplify migrations

--- decidim-core/db/migrate/20220203121137_add_notifications_sending_frequency_to_users.rb
+++ decidim-core/db/migrate/20220203121137_add_notifications_sending_frequency_to_users.rb
@@ -2,6 +2,6 @@
 
 class AddNotificationsSendingFrequencyToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :decidim_users, :notifications_sending_frequency, :string, default: "daily"
+    add_column :decidim_users, :notifications_sending_frequency, :string, default: "none", index: true
   end
 end
--- decidim-core/db/migrate/20220323195017_change_default_value_in_notifications_sending_frequency.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeDefaultValueInNotificationsSendingFrequency < ActiveRecord::Migration[6.0]
-  def change
-    change_column_default :decidim_users, :notifications_sending_frequency, "none"
-  end
-end
