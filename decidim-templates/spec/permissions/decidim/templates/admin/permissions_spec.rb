030dfd51e33ce31b243b6fcfbc62eb10904c950e
Alexandru-Emil Lupu <contact@alecslupu.ro>
2022-05-02 11:45:44 +0300

    Fixing: RSpec/BeEq cop violation in decidim-templates

--- decidim-templates/spec/permissions/decidim/templates/admin/permissions_spec.rb
+++ decidim-templates/spec/permissions/decidim/templates/admin/permissions_spec.rb
@@ -43,7 +43,7 @@ describe Decidim::Templates::Admin::Permissions do
       { scope: scope, action: action, subject: subject }
     end
 
-    it { is_expected.to eq true }
+    it { is_expected.to be true }
   end
 
   context "when indexing templates" do
@@ -75,6 +75,6 @@ describe Decidim::Templates::Admin::Permissions do
       { scope: :admin, action: :foo, subject: :questionnaire }
     end
 
-    it { is_expected.to eq true }
+    it { is_expected.to be true }
   end
 end
