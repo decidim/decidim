622d73273ea9abdd5da6dde90d58828e68b9f107
Andrés Pereira de Lucena <andreslucena@gmail.com>
2022-05-03 11:11:43 +0200

    Fix spec

--- decidim-core/spec/content_parsers/decidim/hashtag_parser_spec.rb
+++ decidim-core/spec/content_parsers/decidim/hashtag_parser_spec.rb
@@ -90,8 +90,7 @@ module Decidim
       it_behaves_like "find and stores the hashtags references"
     end
 
-    context "when content contains an URL with a fragment (aka anchored link)" do
-      let(:hashtag) { Decidim::Hashtag.find_by(organization: organization, name: name) }
+    context "when content contains an URL with a fragment (aka anchor link)" do
       let(:content) { "You can add an URL and this shouldn't be parsed http://www.example.org/path##{hashtag.name}" }
       let(:parsed_content) { "You can add an URL and this shouldn't be parsed http://www.example.org/path#fragment" }
 
@@ -99,15 +98,15 @@ module Decidim
         expect(parser.metadata).to be_a(Decidim::ContentParsers::HashtagParser::Metadata)
         expect(parser.metadata.hashtags).to eq([])
       end
+    end
 
-      context "when written with an slash before the fragmnent" do
-        let(:content) { "You can add an URL and this shouldn't be parsed http://www.example.org/path/#fragment" }
-        let(:parsed_content) { "You can add an URL and this shouldn't be parsed http://www.example.org/path/#fragment" }
+    context "when written with an slash before the fragment" do
+      let(:content) { "You can add an URL and this shouldn't be parsed http://www.example.org/path/#fragment" }
+      let(:parsed_content) { "You can add an URL and this shouldn't be parsed http://www.example.org/path/#fragment" }
 
-        it "doesn't find the hashtag" do
-          expect(parser.metadata).to be_a(Decidim::ContentParsers::HashtagParser::Metadata)
-          expect(parser.metadata.hashtags).to eq([])
-        end
+      it "doesn't find the hashtag" do
+        expect(parser.metadata).to be_a(Decidim::ContentParsers::HashtagParser::Metadata)
+        expect(parser.metadata.hashtags).to eq([])
       end
     end
   end
