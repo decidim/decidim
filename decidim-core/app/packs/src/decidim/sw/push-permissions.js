3de37ce11387f32621ce86f1857d7211df6f3c95
Fernando Blat <fernando@blat.es>
2022-05-03 12:51:40 +0200

    Revert "Refactor JS to run only when scope node is present"
    This reverts commit 9966259a54c8e88c9e92c56da3f5226d3a5f9a9d.

--- decidim-core/app/packs/src/decidim/sw/push-permissions.js
+++ decidim-core/app/packs/src/decidim/sw/push-permissions.js
@@ -3,8 +3,8 @@ window.addEventListener("DOMContentLoaded", async () => {
     const toggle = document.getElementById("allow_push_notifications")
 
     if (toggle) {
-      const HIDE_CLASS = "hide"
       const reminder = document.querySelector("#push-notifications-reminder")
+      const HIDE_CLASS = "hide"
 
       const subKeys = JSON.parse(document.querySelector("#subKeys").value)
 
@@ -13,12 +13,12 @@ window.addEventListener("DOMContentLoaded", async () => {
 
       if (currentSubscription) {
         const auth = currentSubscription.toJSON().keys.auth
-        // Subscribed && browser notifications enabled
+        // Subscribed && browser notif enabled
         if (subKeys.includes(auth) && (window.Notification.permission === "granted")) {
           reminder.classList.add(HIDE_CLASS)
           toggle.checked = true
         }
-        // Not subscribed && browser notifications enabled
+        // Not Subscribed && browser notif enabled
         else if (!subKeys.includes(auth) && (window.Notification.permission === "granted")) {
           reminder.classList.add(HIDE_CLASS)
           toggle.checked = false
@@ -26,57 +26,57 @@ window.addEventListener("DOMContentLoaded", async () => {
         else {
           toggle.checked = false
         }
+      }
 
-        toggle.addEventListener("change", async ({ target }) => {
-          if (target.checked) {
-            const permission = await window.Notification.requestPermission();
+      toggle.addEventListener("change", async ({ target }) => {
+        if (target.checked) {
+          const permission = await window.Notification.requestPermission();
 
-            if (registration && permission === "granted") {
-              const vapidElement = document.querySelector("#vapidPublicKey")
-              // element could not exist in DOM
-              if (vapidElement) {
-                const vapidPublicKeyElement = JSON.parse(vapidElement.value)
-                const subscription = await registration.pushManager.subscribe({
-                  userVisibleOnly: true,
-                  applicationServerKey: new Uint8Array(vapidPublicKeyElement)
+          if (registration && permission === "granted") {
+            const vapidElement = document.querySelector("#vapidPublicKey")
+            // element could not exist in DOM
+            if (vapidElement) {
+              const vapidPublicKeyElement = JSON.parse(vapidElement.value)
+              const subscription = await registration.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: new Uint8Array(vapidPublicKeyElement)
+              });
+
+              if (subscription) {
+                await fetch("/notifications_subscriptions", {
+                  headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content
+                  },
+                  method: "POST",
+                  body: JSON.stringify(subscription)
                 });
-
-                if (subscription) {
-                  await fetch("/notifications_subscriptions", {
-                    headers: {
-                      "Content-Type": "application/json",
-                      "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content
-                    },
-                    method: "POST",
-                    body: JSON.stringify(subscription)
-                  });
-                }
               }
-              reminder.classList.add(HIDE_CLASS)
-            } else {
-              throw new Error("Permission not granted for Notification");
             }
-          }
-          else {
-            /* eslint-disable no-shadow */
-            const currentSubscription = await registration.pushManager.getSubscription()
-            const auth = currentSubscription.toJSON().keys.auth
-            await fetch(`/notifications_subscriptions/${auth}`, {
-              headers: {
-                "Content-Type": "application/json",
-                "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content
-              },
-              method: "DELETE"
-            });
-          }
-        })
-
-        if (toggle.checked) {
-          if (window.Notification.permission === "granted") {
             reminder.classList.add(HIDE_CLASS)
           } else {
-            toggle.checked = false
+            throw new Error("Permission not granted for Notification");
           }
+        }
+        else {
+          /* eslint-disable no-shadow */
+          const currentSubscription = await registration.pushManager.getSubscription()
+          const auth = currentSubscription.toJSON().keys.auth
+          await fetch(`/notifications_subscriptions/${auth}`, {
+            headers: {
+              "Content-Type": "application/json",
+              "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content
+            },
+            method: "DELETE"
+          });
+        }
+      })
+
+      if (toggle.checked) {
+        if (window.Notification.permission === "granted") {
+          reminder.classList.add(HIDE_CLASS)
+        } else {
+          toggle.checked = false
         }
       }
     }
