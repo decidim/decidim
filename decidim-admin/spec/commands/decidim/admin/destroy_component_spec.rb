857513a3734430444392711bd55c4a065d2877ae
Alexandru-Emil Lupu <contact@alecslupu.ro>
2022-05-04 19:32:07 +0300

    Attempt to fix last Review Recommendation

--- decidim-admin/spec/commands/decidim/admin/destroy_component_spec.rb
+++ decidim-admin/spec/commands/decidim/admin/destroy_component_spec.rb
@@ -37,9 +37,9 @@ module Decidim::Admin
 
         subject.call
 
-        component = results[:component]
-        expect(results[:component].id).to eq(component.id)
-        expect(results[:component]).not_to be_persisted
+        result_component = results[:component]
+        expect(result_component.id).to eq(component.id)
+        expect(result_component).not_to be_persisted
       end
     end
   end
