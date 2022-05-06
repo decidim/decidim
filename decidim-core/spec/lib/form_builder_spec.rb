38dd812e610de7322713463eae57cd48111ebb2e
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-05-06 12:30:53 +0200

    Override password_field helper

--- decidim-core/spec/lib/form_builder_spec.rb
+++ decidim-core/spec/lib/form_builder_spec.rb
@@ -344,6 +344,39 @@ module Decidim
       end
     end
 
+    describe "#password_field" do
+      let(:output) do
+        builder.password_field :password, options
+      end
+      let(:options) { {} }
+
+      it "renders the input type password" do
+        expect(output).to eq('<label for="resource_password">Password<input autocomplete="off" type="password" name="resource[password]" id="resource_password" /></label>')
+      end
+
+      context "when autocomplete attribute is defined" do
+        let(:options) { { autocomplete: "new-password" } }
+
+        it "renders the input type password with given autocomplete attribute" do
+          expect(output).to eq('<label for="resource_password">Password<input autocomplete="new-password" type="password" name="resource[password]" id="resource_password" /></label>')
+        end
+      end
+    end
+
+    describe "checkbox" do
+      let(:output) do
+        builder.check_box :name
+      end
+
+      it "renders the checkbox before the label text" do
+        expect(output).to eq(
+          '<label for="resource_name"><input name="resource[name]" type="hidden" value="0" autocomplete="off" />' \
+          '<input type="checkbox" value="1" name="resource[name]" id="resource_name" />Name' \
+          "</label>"
+        )
+      end
+    end
+
     describe "date_field" do
       context "when the resource has errors" do
         before do
