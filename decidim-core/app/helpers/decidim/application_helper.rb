f9d4db08894a6877810a4bb768cd11f2ea79488d
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-05-09 13:13:20 +0200

    Allow cells to use redesigned versions if redesign is enabled in controller and redesigned cell exists

--- decidim-core/app/helpers/decidim/application_helper.rb
+++ decidim-core/app/helpers/decidim/application_helper.rb
@@ -99,6 +99,9 @@ module Decidim
     # Renders the cell contents.
     def cell(name, model, options = {}, &block)
       options = { context: { current_user: current_user } }.deep_merge(options)
+
+      redesigned_name = controller.redesigned_layout(name)
+      name = redesigned_name if Object.const_defined?(redesigned_name.camelize)
       super(name, model, options, &block)
     end
 
