8eec23d757bf3bcd9240a0d6f593a498ed745825
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2022-05-10 15:51:42 +0200

    Fix initiative print link, margin, and organization logo (#9162) (#9263)
    * Fix initiative print when there's an organization logo
    
    * Add i18n string for members header
    
    * Add missing margin in initiative print page
    
    * Link to the public initiative print page from admin panel
    
    * Remove show/print action from initiatives' admin
    
    * Open initiative print page in new tab
    
    * Fix spec

--- decidim-initiatives/spec/system/print_initiative_spec.rb
+++ decidim-initiatives/spec/system/print_initiative_spec.rb
@@ -37,5 +37,17 @@ describe "User prints the initiative", type: :system do
         end
       end
     end
+
+    context "and the organization has a logo" do
+      let(:organization) { create :organization, logo: Decidim::Dev.test_file("avatar.jpg", "image/jpeg") }
+
+      it "shows a printable form when created" do
+        within "main" do
+          expect(page).to have_content(translated(initiative.title, locale: :en))
+          expect(page).to have_content(translated(initiative.type.title, locale: :en))
+          expect(page).to have_content(ActionView::Base.full_sanitizer.sanitize(translated(initiative.description, locale: :en), tags: []))
+        end
+      end
+    end
   end
 end
