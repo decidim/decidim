8ad4ad8f41048acee2ebeba9daba02036e5e8913
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2022-05-11 19:32:58 +0200

    Enforce password validation rules on system admins (#9207) (#9259)

--- decidim-system/lib/decidim/system/test/factories.rb
+++ decidim-system/lib/decidim/system/test/factories.rb
@@ -5,7 +5,7 @@ require "decidim/core/test/factories"
 FactoryBot.define do
   factory :admin, class: "Decidim::System::Admin" do
     sequence(:email) { |n| "admin#{n}@citizen.corp" }
-    password { "password1234" }
-    password_confirmation { "password1234" }
+    password { "decidim123456" }
+    password_confirmation { "decidim123456" }
   end
 end
