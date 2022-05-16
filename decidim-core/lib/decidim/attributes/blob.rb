393ba0230cda93eb1343549bad72c516e8217a2f
lahdeero <eero.lahdenpera@gmail.com>
2022-05-16 17:36:12 +0300

    Update comment

--- decidim-core/lib/decidim/attributes/blob.rb
+++ decidim-core/lib/decidim/attributes/blob.rb
@@ -2,10 +2,7 @@
 
 module Decidim
   module Attributes
-    # Custom attributes value to make conversion from signed id to ActiveStorage::Blob easier.
-    # Signed id is a string that we pass as a param so that we can locate the uploaded file.
-    # and blob is an instance of ActiveStorage::Blob which contains the metadata about
-    # a file and a key for where that file resides on the service.
+    # Custom attributes value to make conversion from signed id (String) to ActiveStorage::Blob easier.
     class Blob < ActiveModel::Type::Value
       def type
         :"decidim/attributes/blob"
