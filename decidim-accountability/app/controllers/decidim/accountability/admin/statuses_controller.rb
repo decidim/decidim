aa7ce5d1fb846c6ea901d5093fec2653fcda8500
eliegaboriau <elie@opensourcepolitics.eu>
2022-05-18 14:21:24 +0200

    add admin log when updating an accountability status

--- decidim-accountability/app/controllers/decidim/accountability/admin/statuses_controller.rb
+++ decidim-accountability/app/controllers/decidim/accountability/admin/statuses_controller.rb
@@ -42,7 +42,7 @@ module Decidim
 
           @form = form(StatusForm).from_params(params)
 
-          UpdateStatus.call(@form, status) do
+          UpdateStatus.call(@form, status, current_user) do
             on(:ok) do
               flash[:notice] = I18n.t("statuses.update.success", scope: "decidim.accountability.admin")
               redirect_to statuses_path
