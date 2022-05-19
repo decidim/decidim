68af1413f9d2a16274244deda4bc67d5afd3bc0b
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-05-19 15:34:16 +0200

    add colors & update typography

--- decidim-core/lib/decidim/webpacker/tailwind.config.js
+++ decidim-core/lib/decidim/webpacker/tailwind.config.js
@@ -3,8 +3,27 @@ module.exports = {
   content: ["./app/**/*.{html,erb,js,rb}", `${GEM_PATH}/**/decidim-*/app/**/*.{html,erb,js,rb}`],
   theme: {
     colors: {
-      primary: "var(--primary)",
-      secondary: "var(--secondary)"
+      primary: "var(--primary, #FF3333)",
+      secondary: "var(--secondary, #155ABF)",
+      tertiary: "var(--tertiary, #EBC34B)",
+      green: "var(--green, #28A745)",
+      red: "var(--red, #ED1C24)",
+      yellow: "var(--yellow, #FFB703)",
+      black: "#020203",
+      white: "#FFFFFF",
+      gray: {
+        DEFAULT: "#C0C6CC",
+        2: "#576075"
+      },
+      background: {
+        DEFAULT: "#FAFBFC",
+        2: "#F8F8F8",
+        3: "#F3F4F7"
+      },
+      border: {
+        DEFAULT: "#C0C6CC",
+        2: "#F3F4F7"
+      }
     },
     container: {
       center: true
@@ -13,18 +32,11 @@ module.exports = {
       "sans": ["Source Sans Pro", "ui-sans-serif", "system-ui", "sans-serif"]
     },
     fontSize: {
-      xs: ["0.813rem", "16px"],
-      sm: ["0.875rem", "18px"],
-      md: ["1rem", "20px"],
-      lg: ["1.125rem", "23px"],
-      xl: ["1.25rem", "25px"],
-      "2xl": ["1.5rem", "30px"],
-      "3xl": ["1.75rem", "35px"],
-      "4xl": ["2rem", "40px"],
-      "5xl": ["2.25rem", "45px"],
-      "6xl": ["2.625rem", "53px"],
-      "7xl": ["3rem", "60px"],
-      "8xl": ["3.375rem", "68px"]
+      xs: ["13px", "16px"],
+      sm: ["14px", "18px"],
+      md: ["16px", "20px"],
+      lg: ["18px", "23px"],
+      xl: ["20px", "25px"]
     }
   },
   plugins: [
