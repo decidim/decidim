e9f94d2ca707760048e804bfae69fac705bf3d5a
Fernando Blat <fernando@blat.es>
2022-05-20 05:48:22 +0200

    Helper to change network conditions

--- decidim-core/spec/system/pwa_features_spec.rb
+++ decidim-core/spec/system/pwa_features_spec.rb
@@ -16,17 +16,11 @@ describe "PWA features", type: :system do
       visit decidim.root_path
       expect(page).to have_content("Home")
 
-      page.driver.browser.network_conditions = { offline: true }
-
-      # Wait for the browser to be offline
-      sleep 1
-
-      visit decidim.root_path
-      expect(page).to have_content("Home")
-      expect(page).to have_content("Your network is offline. This is a previously cached version of the page you're visiting, perhaps the content is not up to date.")
-
-      # Restore the network conditions
-      page.driver.browser.network_conditions = { offline: false }
+      with_browser_in_offline_mode do
+        visit decidim.root_path
+        expect(page).to have_content("Home")
+        expect(page).to have_content("Your network is offline. This is a previously cached version of the page you're visiting, perhaps the content is not up to date.")
+      end
     end
   end
 end
