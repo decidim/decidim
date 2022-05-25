66150a4b9c5ffb96168bb38d7c62415cdc09e4d7
Fernando Blat <fernando@blat.es>
2022-05-25 15:02:15 +0200

    Revert "Simplify plugins"
    This reverts commit 1d53c003dcba2a700aaf3d4b5d1a395cc5d2118f.

--- decidim-core/lib/decidim/webpacker/postcss.config.js
+++ decidim-core/lib/decidim/webpacker/postcss.config.js
@@ -6,6 +6,13 @@ module.exports = {
     // postcss-import must be the very first plugin https://tailwindcss.com/docs/using-with-preprocessors#build-time-imports
     require('postcss-import'),
     require('tailwindcss'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3
+    }),
     require('autoprefixer'),
   ]
 }
