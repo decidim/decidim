722ea52bb38bae2323ca86c75b7db115470a3bdd
eliegaboriau <elie@opensourcepolitics.eu>
2022-05-31 11:27:24 +0200

    add cache for current_initiative_settings

--- decidim-initiatives/app/controllers/concerns/decidim/initiatives/orderable.rb
+++ decidim-initiatives/app/controllers/concerns/decidim/initiatives/orderable.rb
@@ -43,7 +43,7 @@ module Decidim
         end
 
         def current_initiatives_settings
-          Decidim::InitiativesSettings.find_or_create_by!(decidim_organization_id: current_organization.id)
+          @current_initiatives_settings ||= Decidim::InitiativesSettings.find_or_create_by!(decidim_organization_id: current_organization.id)
         end
       end
     end
