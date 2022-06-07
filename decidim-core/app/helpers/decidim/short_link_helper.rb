dfcba9916c65ed7cc7a616c44b187d2c3aa16976
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-06-07 11:03:26 +0300

    Fix few typos in the short link helper comments

--- decidim-core/app/helpers/decidim/short_link_helper.rb
+++ decidim-core/app/helpers/decidim/short_link_helper.rb
@@ -6,7 +6,7 @@ module Decidim
   module ShortLinkHelper
     # A helper method to get a short URL in the current context where this
     # method is called from. This helper automatically fetches the "target" for
-    # the short link, suck as the component or the participatory process. This
+    # the short link, such as the component or the participatory process. This
     # also resolves the current mounted route name to make it possible to refer
     # to the same context when redirecting the short URL to correct full URL.
     #
@@ -14,7 +14,7 @@ module Decidim
     #   link should link to
     # @option kwargs [Float] :params the URL query parameters that should be
     #   included in the URL where the short link redirects to
-    # @return [Decidim::ShortLink] The short link instance to link to.
+    # @return [String] The short URL
     def short_url(**kwargs)
       target = respond_to?(:current_component) && current_component
       target ||= respond_to?(:current_participatory_space) && current_participatory_space
