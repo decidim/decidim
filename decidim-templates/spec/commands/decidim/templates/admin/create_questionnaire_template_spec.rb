192e832b09fadbce4d56b26a09d70a174f31eedf
eliegaboriau <93646702+eliegaboriau@users.noreply.github.com>
2022-06-08 10:59:55 +0200

    Add admin log when creating, deleting, duplicating and updating templates (#9363)
    * add specs for commands
    
    * add presenter
    
    * add admin log when duplicating
    
    * normalize locale
    
    * normalize locale
    
    * Update decidim-templates/config/locales/en.yml
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

---
+++ decidim-templates/spec/commands/decidim/templates/admin/create_questionnaire_template_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  module Templates
+    module Admin
+      describe CreateQuestionnaireTemplate do
+        subject { described_class.new(form) }
+
+        let(:organization) { create :organization }
+        let(:user) { create :user, :admin, :confirmed, organization: organization }
+
+        let(:form) do
+          instance_double(
+            TemplateForm,
+            invalid?: invalid,
+            valid?: !invalid,
+            current_user: user,
+            current_organization: organization,
+            name: "name",
+            description: "description"
+          )
+        end
+        let(:invalid) { false }
+
+        context "when the form is not valid" do
+          let(:invalid) { true }
+
+          it "is not valid" do
+            expect { subject.call }.to broadcast(:invalid)
+          end
+        end
+
+        context "when the form is valid" do
+          it "broadcasts ok" do
+            expect { subject.call }.to broadcast(:ok)
+          end
+
+          it "create a new template for the organization" do
+            expect { subject.call }.to change { Decidim::Templates::Template.all.count }.by(1)
+          end
+
+          it "traces the action", versioning: true do
+            expect(Decidim.traceability)
+              .to receive(:perform_action!)
+              .with(:create, Decidim::Templates::Template, user, {})
+              .and_call_original
+
+            expect { subject.call }.to change(Decidim::ActionLog, :count)
+            action_log = Decidim::ActionLog.last
+            expect(action_log.version).to be_present
+          end
+        end
+      end
+    end
+  end
+end
