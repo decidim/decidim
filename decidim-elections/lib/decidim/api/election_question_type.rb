c176a71ff7bdc46b015edf02eabc795b176c557e
Ivan Vergés <ivan@pokecode.net>
2022-06-09 17:07:25 +0200

    fix schema

--- decidim-elections/lib/decidim/api/election_question_type.rb
+++ decidim-elections/lib/decidim/api/election_question_type.rb
@@ -11,7 +11,6 @@ module Decidim
 
       field :id, GraphQL::Types::ID, "The internal ID of this question", null: false
       field :title, Decidim::Core::TranslatedFieldType, "The title for this question", null: false
-      field :description, Decidim::Core::TranslatedFieldType, "The description for this question", null: false
       field :max_selections, GraphQL::Types::Int, "The maximum number of possible selections for this question", null: false
       field :weight, GraphQL::Types::Int, "The ordering weight for this question", null: true
       field :random_answers_order, GraphQL::Types::Boolean, "Should this question order answers in random order?", null: true
