4e3b06cd44704fd11226222cea49c34d1522a64c
Hugoren Martinako <aumpfbahn@gmail.com>
2022-06-15 11:13:33 +0200

    enhacement decidim paginate

--- decidim-core/app/helpers/decidim/paginate_helper.rb
+++ decidim-core/app/helpers/decidim/paginate_helper.rb
@@ -9,7 +9,15 @@ module Decidim
     # collection - a collection of elements that need to be paginated
     # paginate_params - a Hash with options to delegate to the pagination helper.
     def decidim_paginate(collection, paginate_params = {})
-      paginate collection, theme: "decidim", params: paginate_params
+      content_tag :div, class: "flex flex-col-reverse md:flex-row items-center justify-between gap-1 py-8 md:py-16" do
+        template = render partial: "decidim/shared/results_per_page.html"
+        template += paginate collection, window: 2, outer_window: 1, theme: "decidim", params: paginate_params
+        template.html_safe
+      end
+    end
+
+    def per_page
+      params[:per_page] || Decidim::Paginable::OPTIONS.first
     end
   end
 end
