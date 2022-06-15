3c0c26b02f6e7388d02f459e254f3f16385a36d4
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-06-15 19:03:13 +0200

    Redesign: Install tailwind (#9274)
    * [skip ci] testing commit
    
    * testing files
    
    * Copy tailwind configuration
    
    * clean files to run tailwind
    
    * remove the package-locks references
    
    * restore default import to override
    
    * Fix decidim gem path in tailwind configuration
    
    * testing files
    
    * Remove not required file
    
    * Update package-lock
    
    * Fix quotes
    
    * Update package-lock design app
    
    * Update plugin order
    
    * Simplify plugins
    
    * Fix import order
    
    * Define paths that don't include packs/ folder to prevent infinite loops
    
    * Revert "Simplify plugins"
    
    This reverts commit 1d53c003dcba2a700aaf3d4b5d1a395cc5d2118f.
    
    * Load node_modules styles with JS
    
    * JS offense
    
    * CSS offense
    
    * Detailed configuration
    
    * Use explicit list of paths
    
    Avoid using the star in the paths to prevent infinite loops
    
    * Add clarification
    
    * Remove package-lock file
    
    Co-authored-by: Eduardo Martinez Echevarria <eduardomech@gmail.com>
    Co-authored-by: Fernando Blat <fernando@blat.es>

--- decidim-core/lib/decidim/webpacker/postcss.config.js
+++ decidim-core/lib/decidim/webpacker/postcss.config.js
@@ -3,14 +3,16 @@
 module.exports = {
   syntax: 'postcss-scss',
   plugins: [
-    require('autoprefixer'),
+    // postcss-import must be the very first plugin https://tailwindcss.com/docs/using-with-preprocessors#build-time-imports
     require('postcss-import'),
+    require('tailwindcss'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
+    require('autoprefixer'),
   ]
 }
