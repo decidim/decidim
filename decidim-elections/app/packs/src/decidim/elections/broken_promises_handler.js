9f80554ddd3a990bfc15837305821dcc67306967
Ivan Vergés <ivan@pokecode.net>
2022-06-17 23:08:30 +0200

    provide a generic error if it is not a XHR failure

--- decidim-elections/app/packs/src/decidim/elections/broken_promises_handler.js
+++ decidim-elections/app/packs/src/decidim/elections/broken_promises_handler.js
@@ -2,6 +2,10 @@
 window.addEventListener("unhandledrejection", (event) => {
   console.log("broken", event)
   $("#server-failure .tech-info").html(event.reason);
+  if (event.reason.toString().indexOf("fetch") === -1) {
+    $("#server-failure .communication_error").addClass("hide");  
+    $("#server-failure .generic_error").removeClass("hide"); 
+  }
   $("#server-failure").foundation("open");
 });
 
