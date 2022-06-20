0091dd3d9903bf4d450ad3cbf55f66ba86a48d36
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-06-20 16:21:07 +0300

    Allow configuring the admin password configs through ENV vars

--- decidim-generators/lib/decidim/generators/app_templates/initializer.rb
+++ decidim-generators/lib/decidim/generators/app_templates/initializer.rb
@@ -364,6 +364,15 @@ Decidim.configure do |config|
   #   }
   # ]
 
+  # Admin admin password configurations
+  Rails.application.secrets.dig(:decidim, :admin_password, :strong).tap do |strong_pw|
+    # When the strong password is not configured, default to true
+    config.admin_password_strong_enable = strong_pw.nil? ? true : strong_pw.present?
+  end
+  config.admin_password_days_expiration = Rails.application.secrets.dig(:decidim, :admin_password, :expiration_days).presence || 90
+  config.admin_password_min_characters = Rails.application.secrets.dig(:decidim, :admin_password, :min_length).presence || 15
+  config.admin_password_repetition_times = Rails.application.secrets.dig(:decidim, :admin_password, :repetition_times).presence || 5
+
   # Additional optional configurations (see decidim-core/lib/decidim/core.rb)
   config.cache_key_separator = Rails.application.secrets.decidim[:cache_key_separator] if Rails.application.secrets.decidim[:cache_key_separator].present?
   config.expire_session_after = Rails.application.secrets.decidim[:expire_session_after].to_i.minutes if Rails.application.secrets.decidim[:expire_session_after].present?
