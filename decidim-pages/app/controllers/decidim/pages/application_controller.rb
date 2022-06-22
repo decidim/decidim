b492456be6df55d0a128b4fd6cc8acbe733c2b68
Hugoren Martinako <aumpfbahn@gmail.com>
2022-06-22 12:47:17 +0200

    participatory space page

--- decidim-pages/app/controllers/decidim/pages/application_controller.rb
+++ decidim-pages/app/controllers/decidim/pages/application_controller.rb
@@ -8,6 +8,9 @@ module Decidim
     # Note that it inherits from `Decidim::Components::Basecontroller`, which
     # override its layout and provide all kinds of useful methods.
     class ApplicationController < Decidim::Components::BaseController
+
+      redesign_participatory_space_layout
+
       def show
         @page = Page.find_by(component: current_component)
       end
