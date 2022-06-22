ace7450a8f7b6fc9492724108695ec26c2d52545
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-06-22 15:47:59 +0300

    Add system specs for the admin log filters

--- decidim-admin/spec/system/admin_checks_logs_spec.rb
+++ decidim-admin/spec/system/admin_checks_logs_spec.rb
@@ -12,15 +12,128 @@ describe "Admin checks logs", type: :system do
     switch_to_host(organization.host)
     login_as user, scope: :user
     visit decidim_admin.root_path
+    click_link "Admin activity log"
   end
 
   it "lists all recent logs" do
-    click_link "Admin activity log"
-
     expect(page).to have_content("Admin log")
 
-    within ".content" do
+    within ".content .logs.table" do
       expect(page).to have_selector("li", count: 3)
+    end
+  end
+
+  context "when filtering" do
+    context "with participatory space" do
+      it "lists only logs from that participatory space" do
+        within ".filters__section" do
+          select(translated(action_logs.first.participatory_space.title), from: "q_with_participatory_space")
+          find("*[type=submit]").click
+        end
+
+        within ".content .logs.table" do
+          expect(page).to have_selector("li", count: 1)
+        end
+      end
+    end
+
+    context "with time" do
+      let!(:action_logs) do
+        [].tap do |logs|
+          logs << create(:action_log, created_at: Time.zone.local(2022, 6, 22, 8, 9, 10), organization: organization)
+          logs << create(:action_log, created_at: Time.zone.local(2022, 6, 22, 9, 10, 11), organization: organization)
+          logs << create(:action_log, created_at: Time.zone.local(2022, 6, 22, 10, 11, 12), organization: organization)
+        end
+      end
+
+      it "lists only logs after the start time or at the same minute" do
+        within ".filters__section" do
+          find('input[id="q_created_at_dtgteq"]').set(Time.zone.local(2022, 6, 22, 9, 10))
+          find("*[type=submit]").click
+        end
+
+        within ".content .logs.table" do
+          expect(page).to have_selector("li", count: 2)
+        end
+      end
+
+      it "lists only logs before the end time or at the same minute" do
+        within ".filters__section" do
+          find('input[id="q_created_at_dtlteq"]').set(Time.zone.local(2022, 6, 22, 9, 10))
+          find("*[type=submit]").click
+        end
+
+        within ".content .logs.table" do
+          expect(page).to have_selector("li", count: 2)
+        end
+      end
+
+      it "lists only logs between the start time and the end time or at the same minutes" do
+        within ".filters__section" do
+          find('input[id="q_created_at_dtgteq"]').set(Time.zone.local(2022, 6, 22, 8, 9))
+          find('input[id="q_created_at_dtlteq"]').set(Time.zone.local(2022, 6, 22, 9, 10))
+          find("*[type=submit]").click
+        end
+
+        within ".content .logs.table" do
+          expect(page).to have_selector("li", count: 2)
+        end
+      end
+    end
+
+    context "with user" do
+      let(:admin1) { create(:user, :admin, organization: organization, name: "John Doe", nickname: "joe", email: "jdoe@example.org") }
+      let(:admin2) { create(:user, :admin, organization: organization, name: "Richard Roe", nickname: "roe", email: "rroe@example.org") }
+      let(:admin3) { create(:user, :admin, organization: organization, name: "Joe Schmoe", nickname: "schmoe", email: "jschmoe@example.org") }
+      let!(:action_logs) do
+        [].tap do |logs|
+          logs << create(:action_log, user: admin1, organization: organization)
+          logs << create(:action_log, user: admin2, organization: organization)
+          logs << create(:action_log, user: admin3, organization: organization)
+        end
+      end
+
+      it "lists only logs matching the user's name" do
+        within ".filters__section" do
+          fill_in(:q_user_name_or_user_nickname_or_user_email_cont, with: "john")
+          find("*[type=submit]").click
+        end
+
+        within ".content .logs.table" do
+          expect(page).to have_selector("li", count: 1)
+        end
+
+        within ".filters__section" do
+          fill_in(:q_user_name_or_user_nickname_or_user_email_cont, with: "doe")
+          find("*[type=submit]").click
+        end
+
+        within ".content .logs.table" do
+          expect(page).to have_selector("li", count: 1)
+        end
+      end
+
+      it "lists only logs matching the user's nickname" do
+        within ".filters__section" do
+          fill_in(:q_user_name_or_user_nickname_or_user_email_cont, with: "schmoe")
+          find("*[type=submit]").click
+        end
+
+        within ".content .logs.table" do
+          expect(page).to have_selector("li", count: 1)
+        end
+      end
+
+      it "lists only logs matching the user's email" do
+        within ".filters__section" do
+          fill_in(:q_user_name_or_user_nickname_or_user_email_cont, with: "rroe@example.org")
+          find("*[type=submit]").click
+        end
+
+        within ".content .logs.table" do
+          expect(page).to have_selector("li", count: 1)
+        end
+      end
     end
   end
 end
