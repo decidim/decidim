35ab5e8ffe2974e7785dc356195585d847f0e557
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-06-23 20:03:51 +0200

    Refactor user_activity cell to filter results

--- decidim-core/app/cells/decidim/user_activity_cell.rb
+++ decidim-core/app/cells/decidim/user_activity_cell.rb
@@ -4,11 +4,11 @@ module Decidim
   class UserActivityCell < Decidim::ViewModel
     include Cell::ViewModel::Partial
     include CellsPaginateHelper
-    include Decidim::LayoutHelper
     include Decidim::Core::Engine.routes.url_helpers
     include ActionView::Helpers::FormOptionsHelper
     include Decidim::FiltersHelper
     include Decidim::LayoutHelper
+    include Decidim::IconHelper
 
     def show
       render :show
@@ -20,6 +20,37 @@ module Decidim
 
     def resource_types
       context[:resource_types]
+    end
+
+    def resource_items
+      resource_types.map do |resource_type|
+        {
+          name: resource_type,
+          translation: I18n.t(resource_type.split("::").last.underscore, scope: "decidim.components.component_order_selector.order"),
+          filter: { resource_type: resource_type }
+        }
+      end
+    end
+
+    def filter_items
+      resource_items.prepend(
+        name: all_types_key,
+        translation: t("decidim.components.component_order_selector.order.all_types")
+      )
+    end
+
+    def filter
+      context[:filter]
+    end
+
+    def all_types_key
+      "all"
+    end
+
+    def current_resource_type
+      return all_types_key unless resource_types.include? filter.resource_type
+
+      filter.resource_type
     end
   end
 end
