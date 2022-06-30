512f597cd22330bacf41941e70fb01a4fa643cd6
Ivan Vergés <ivan@pokecode.net>
2022-06-30 12:05:40 +0200

    trim whitespace

--- decidim-elections/app/packs/src/decidim/elections/broken_promises_handler.js
+++ decidim-elections/app/packs/src/decidim/elections/broken_promises_handler.js
@@ -3,8 +3,8 @@ window.addEventListener("unhandledrejection", (event) => {
   console.log("broken", event)
   $("#server-failure .tech-info").html(event.reason);
   if (event.reason.toString().indexOf("fetch") === -1) {
-    $("#server-failure .communication_error").addClass("hide");  
-    $("#server-failure .generic_error").removeClass("hide"); 
+    $("#server-failure .communication_error").addClass("hide");
+    $("#server-failure .generic_error").removeClass("hide");
   }
   $("#server-failure").foundation("open");
 });
