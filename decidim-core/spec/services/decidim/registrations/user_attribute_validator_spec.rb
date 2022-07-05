8a7ae04230727eff61dedeac710c4bbf565bc40c
Ivan Vergés <ivan@pokecode.net>
2022-07-05 17:10:37 +0200

    fix test

--- decidim-core/spec/services/decidim/registrations/user_attribute_validator_spec.rb
+++ decidim-core/spec/services/decidim/registrations/user_attribute_validator_spec.rb
@@ -21,25 +21,6 @@ describe Decidim::Registrations::UserAttributeValidator do
   let(:user_value) { "zimbawe" }
   let(:group_value) { "africa" }
   let!(:organization) { create :organization }
-  let!(:user) { create :user, :organization => organization, attribute.to_sym => user_value }
-  let!(:user_group) { create :user_group, :organization => organization, attribute.to_sym => group_value }
-
-  shared_examples "suggests alternatives" do |expectation|
-    it { is_expected.not_to be_valid }
-
-    it "suggests the word with a next consecutive number" do
-      expect(subject.suggestion).to eq(expectation)
-    end
-
-    it "returns an error message" do
-      expect(subject.error).to include("already been taken")
-    end
-
-    it "returns an error message with a suggestion" do
-      expect(subject.error_with_suggestion).to include(subject.error)
-      expect(subject.error_with_suggestion).to include(expectation)
-    end
-  end
 
   it "responds to form" do
     expect(subject.form).to eq(form)
@@ -47,62 +28,25 @@ describe Decidim::Registrations::UserAttributeValidator do
 
   it { is_expected.to be_valid }
 
-  it "does not suggest an alternative word" do
-    expect(subject.suggestion).to eq(input_value)
+  it "does not have errors" do
+    expect(subject.error).to be_blank
   end
 
   context "when attribute already belongs to a user" do
+    let!(:user) { create :user, :organization => organization, attribute.to_sym => user_value }
     let(:input_value) { "zimbawe" }
 
-    it_behaves_like "suggests alternatives", "zimbawe1"
-
-    context "and ends with numbers" do
-      let(:input_value) { "zimbawe123" }
-      let(:user_value) { "zimbawe123" }
-
-      it_behaves_like "suggests alternatives", "zimbawe124"
+    it "returns an error message" do
+      expect(subject.error).to include("already been taken")
     end
   end
 
   context "when attribute already belongs to a group" do
+    let!(:user_group) { create :user_group, :organization => organization, attribute.to_sym => group_value }
     let(:input_value) { "africa" }
 
-    it_behaves_like "suggests alternatives", "africa1"
-
-    context "and ends with numbers" do
-      let(:input_value) { "africa123" }
-      let(:group_value) { "africa123" }
-
-      it_behaves_like "suggests alternatives", "africa124"
-    end
-  end
-
-  context "when attribute is not enabled for suggestions" do
-    let(:attribute) { "email" }
-    let(:input_value) { "some_email@example.org" }
-    let(:user_value) { "some_user_email@example.org" }
-    let(:group_value) { "some_group_email@example.org" }
-
-    it { is_expected.to be_valid }
-
-    context "when suggest an existing user email" do
-      let(:input_value) { user_value }
-
-      it { is_expected.not_to be_valid }
-
-      it "does not suggest an alternative word" do
-        expect(subject.suggestion).to eq(input_value)
-      end
-    end
-
-    context "when suggest an existing group email" do
-      let(:input_value) { group_value }
-
-      it { is_expected.not_to be_valid }
-
-      it "does not suggest an alternative word" do
-        expect(subject.suggestion).to eq(input_value)
-      end
+    it "returns an error message" do
+      expect(subject.error).to include("already been taken")
     end
   end
 
@@ -110,5 +54,35 @@ describe Decidim::Registrations::UserAttributeValidator do
     let(:attribute) { "about" }
 
     it { is_expected.not_to be_valid }
+
+    it "returns an error message" do
+      expect(subject.error).to include("Invalid attribute")
+    end
+  end
+
+  context "when attribute has dependant validations" do
+    let(:attribute) { :password }
+    let(:name) { "Africa Unite 1979" }
+    let(:input_value) { "africaunite1979" }
+    let(:params) do
+      {
+        name: name,
+        password: input_value,
+        password_confirmation: input_value,
+        attribute: attribute
+      }
+    end
+
+    it { is_expected.not_to be_valid }
+
+    it "returns an error message" do
+      expect(subject.error).to include("Is too similar to your name")
+    end
+
+    context "and depency is ok" do
+      let(:name) { "Survival" }
+
+      it { is_expected.to be_valid }
+    end
   end
 end
