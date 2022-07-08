90d0d984bc604e6b96bf6e6b88e69193bb5e766c
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2022-07-08 09:48:42 +0200

    Fix cache hash on Hightlighted spaces (#9474) (#9536)
    * Fix cache hash on Hightlighted spaces
    
    * Remove cache expiry time for content block cells

--- decidim-conferences/app/cells/decidim/conferences/content_blocks/highlighted_conferences_cell.rb
+++ decidim-conferences/app/cells/decidim/conferences/content_blocks/highlighted_conferences_cell.rb
@@ -31,6 +31,7 @@ module Decidim
         def cache_hash
           hash = []
           hash.push(I18n.locale)
+          hash.push(highlighted_conferences.map(&:cache_key_with_version))
           hash.join(Decidim.cache_key_separator)
         end
       end
