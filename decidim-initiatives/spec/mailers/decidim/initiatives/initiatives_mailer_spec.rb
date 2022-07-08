ee0d4488ac35442676678642e0d96a575c7c1d6f
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2022-07-08 17:56:47 +0200

    Use public link on initiatives mailer (#9542)
    * Use described_class instead of hardcoding class name
    
    * Add spec
    
    * Use public link instead of admin link on mailer

--- decidim-initiatives/spec/mailers/decidim/initiatives/initiatives_mailer_spec.rb
+++ decidim-initiatives/spec/mailers/decidim/initiatives/initiatives_mailer_spec.rb
@@ -5,10 +5,13 @@ require "spec_helper"
 module Decidim
   module Initiatives
     describe InitiativesMailer, type: :mailer do
-      let(:initiative) { create(:initiative) }
+      let(:organization) { create(:organization, host: "1.lvh.me") }
+      let(:initiative) { create(:initiative, organization: organization) }
+      let(:router) { Decidim::Initiatives::Engine.routes.url_helpers }
+      let(:admin_router) { Decidim::Initiatives::AdminEngine.routes.url_helpers }
 
       context "when notifies creation" do
-        let(:mail) { InitiativesMailer.notify_creation(initiative) }
+        let(:mail) { described_class.notify_creation(initiative) }
 
         it "renders the headers" do
           expect(mail.subject).to eq("Your initiative '#{initiative.title["en"]}' has been created")
@@ -18,10 +21,15 @@ module Decidim
         it "renders the body" do
           expect(mail.body.encoded).to match(initiative.title["en"])
         end
+
+        it "renders the correct link" do
+          expect(mail).to have_link(router.initiative_url(initiative, host: initiative.organization.host))
+          expect(mail).not_to have_link(admin_router.initiative_url(initiative, host: initiative.organization.host))
+        end
       end
 
       context "when notifies state change" do
-        let(:mail) { InitiativesMailer.notify_state_change(initiative, initiative.author) }
+        let(:mail) { described_class.notify_state_change(initiative, initiative.author) }
 
         it "renders the headers" do
           expect(mail.subject).to eq("The initiative #{initiative.title["en"]} has changed its status")
@@ -34,7 +42,7 @@ module Decidim
       end
 
       context "when notifies progress" do
-        let(:mail) { InitiativesMailer.notify_progress(initiative, initiative.author) }
+        let(:mail) { described_class.notify_progress(initiative, initiative.author) }
 
         it "renders the headers" do
           expect(mail.subject).to eq("Summary about the initiative: #{initiative.title["en"]}")
