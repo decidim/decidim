6adf4e00b79c519964dcfd96e994ae251fef948c
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-07-11 15:08:29 +0300

    Add specs for the unregister method at the block registry

--- decidim-core/spec/lib/block_registry_spec.rb
+++ decidim-core/spec/lib/block_registry_spec.rb
@@ -44,5 +44,34 @@ module Decidim
         subject.registrations[:foo].call(foo_argument)
       end
     end
+
+    describe "#unregister" do
+      before do
+        subject.register(:foo, &foo_block)
+        subject.register(:bar, &bar_block)
+      end
+
+      it "removes the provided blocks from the registry" do
+        subject.unregister(:foo)
+
+        expect(subject.registrations).to eq(bar: bar_block)
+      end
+
+      it "returns the originally registered block" do
+        expect(subject.unregister(:foo)).to be(foo_block)
+      end
+
+      context "when calling with multiple arguments" do
+        it "removes the provided blocks from the registry" do
+          subject.unregister(:foo, :bar)
+
+          expect(subject.registrations).to be_empty
+        end
+
+        it "returns an array of the originally registered blocks" do
+          expect(subject.unregister(:foo, :bar)).to eq([foo_block, bar_block])
+        end
+      end
+    end
   end
 end
