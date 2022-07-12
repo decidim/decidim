28583bdc620bcbd8c5acb4589a62ea455182f342
Sergei Krylov <sergei.krylov@yahoo.com>
2022-07-12 13:47:18 +0300

    Make the HERE map display in the currently selected language

--- decidim-core/lib/decidim/map/provider/dynamic_map/here.rb
+++ decidim-core/lib/decidim/map/provider/dynamic_map/here.rb
@@ -33,7 +33,7 @@ module Decidim
               )
             end
 
-            base_config.merge(api_key: api_key)
+            base_config.merge(api_key: api_key, language: language_code)
           end
 
           # A builder for the HERE maps which needs to be configured differently
@@ -44,6 +44,58 @@ module Decidim
               template.javascript_pack_tag("decidim_map_provider_here", defer: false)
             end
           end
+
+          private
+
+          # rubocop: disable Metrics/CyclomaticComplexity
+          # rubocop: disable Lint/DuplicateBranch
+          def language_code
+            case I18n.locale.to_s
+            when "ar" then "ara" # Arabic
+            when "eu" then "baq" # Basque
+            when "ca" then "cat" # Catalan
+            when "zh-cn" then "chi" # Chinese (simplified)
+            # when "" then "cht" # Chinese (traditional)
+            when "cs" then "cze" # Czech
+            when "da" then "dan" # Danish
+            when "nl" then "dut" # Dutch
+            when "en" then "eng" # English
+            when "fi" then "fin" # Finnish
+            when "fi-pl" then "fin"
+            when "fi-plain" then "fin"
+            when "fr" then "fre" # French
+            when "fr-ca" then "fre"
+            when "fr-lu" then "fre"
+            when "de" then "ger" # German
+            when "ga" then "gle" # Gaelic
+            when "el" then "gre" # Greek
+            # when "he" then "heb" # Hebrew
+            # when "hi" then "hin" # Hindi
+            when "id" then "ind" # Indonesian
+            when "it" then "ita" # Italian
+            when "no" then "nor" # Norwegian
+            # when "fa" then "per" # Persian
+            when "pl" then "pol" # Polish
+            when "pt" then "por"; # Portuguese
+            when "pt-br" then "por"
+            when "ru" then "rus" # Russian
+            when "si" then "sin" # Sinhalese
+            when "es" then "spa" # Spanish
+            when "es-mx" then "spa"
+            when "es-py" then "spa"
+            when "sv" then "swe" # Swedish
+            # when "th" then "tha" # Thai
+            when "tr" then "tur" # Turkish
+            when "uk" then "ukr" # Ukrainian
+            # when "ur" then "urd" # Urdu
+            when "vi" then "vie" # Vietnamese
+            # when "cy" then "wel" # Welsh
+            else
+              ""
+            end
+          end
+          # rubocop: enable Lint/DuplicateBranch
+          # rubocop: enable Metrics/CyclomaticComplexity
         end
       end
     end
