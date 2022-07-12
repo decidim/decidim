4b4888d55ed4f79b841db8c443592b6d8f0df04c
Sergei Krylov <sergei.krylov@yahoo.com>
2022-07-12 14:42:39 +0300

    Fix linters

--- decidim-admin/app/packs/src/decidim/admin/choose_language.js
+++ decidim-admin/app/packs/src/decidim/admin/choose_language.js
@@ -4,12 +4,11 @@
 export default function initLanguageChangeSelect(elements) {
   elements.forEach((select) => {
     select.addEventListener("click", () => {
-        let targetTabPaneSelector = select.value;
-        let tabsContent = select.parentElement.parentElement.nextElementSibling;
+      let targetTabPaneSelector = select.value;
+      let tabsContent = select.parentElement.parentElement.nextElementSibling;
 
-        tabsContent.querySelector(".is-active").classList.remove("is-active");
-        tabsContent.querySelector(targetTabPaneSelector).classList.add("is-active");
-      }
-    )
+      tabsContent.querySelector(".is-active").classList.remove("is-active");
+      tabsContent.querySelector(targetTabPaneSelector).classList.add("is-active");
+    })
   });
 }
