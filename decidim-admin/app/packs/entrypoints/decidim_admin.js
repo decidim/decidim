b0f04cb505ae505d55b37a78ab20707b93d65cc3
Sergei Krylov <sergei.krylov@yahoo.com>
2022-07-13 20:58:56 +0200

    Fix admin language selector with more than 4 locales
    * Fix language selector doesn't work for newly created fields.
    The field disappears when there are errors in the form and select a different language for this field.
    
    * Fix translations of language with errors for language selector.
    
    * Disable require-jsdoc
    
    * Revert "Fix translations of language with errors for language selector."
    
    This reverts commit cda01acf
    
    * Refactoring of onchange function calls, reimplement without jQuery.
    
    * Refactor select change event
    
    * Fix linters
    
    * Update decidim-admin/app/packs/src/decidim/admin/choose_language.js
    
    Typo. Change back "click" to "change"
    
    Co-authored-by: Antti Hukkanen <antti.hukkanen@mainiotech.fi>
    
    Co-authored-by: Antti Hukkanen <antti.hukkanen@mainiotech.fi>

--- decidim-admin/app/packs/entrypoints/decidim_admin.js
+++ decidim-admin/app/packs/entrypoints/decidim_admin.js
@@ -14,7 +14,7 @@ import "jquery.autocomplete"
 import "jquery-serializejson"
 
 import "src/decidim/admin/tab_focus"
-import "src/decidim/admin/choose_language"
+import initLanguageChangeSelect from "src/decidim/admin/choose_language"
 import "src/decidim/admin/application"
 import "src/decidim/admin/resources_permissions"
 import "src/decidim/admin/welcome_notification"
@@ -40,3 +40,7 @@ import "entrypoints/decidim_admin.scss";
 
 // This needs to be loaded after confirm dialog to bind properly
 Rails.start()
+
+window.addEventListener("DOMContentLoaded", () => {
+  initLanguageChangeSelect(document.querySelectorAll("select.language-change"));
+});
