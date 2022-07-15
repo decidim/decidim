54f5b104833cc618d28135cfb40715ccbb243d6b
Andrés Pereira de Lucena <andreslucena@gmail.com>
2022-07-15 14:10:14 +0200

    Fix HTML tags on spec

--- decidim-proposals/spec/lib/decidim/proposals/markdown_to_proposals_spec.rb
+++ decidim-proposals/spec/lib/decidim/proposals/markdown_to_proposals_spec.rb
@@ -142,16 +142,16 @@ module Decidim
             **bold text** is supported, *italics text* is supported, __underlined text__ is supported.
             As explained [here](https://daringfireball.net/projects/markdown/syntax#em) Markdown treats asterisks
             and underscores as indicators of emphasis.
-            Text wrapped with one asterisk or underscore will be wrapped with an HTML &lt;em> tag; double asterisks or underscores will be wrapped with an HTML &lt;strong> tag. E.g., this input:
+            Text wrapped with one asterisk or underscore will be wrapped with an HTML <em> tag; double asterisks or underscores will be wrapped with an HTML <strong> tag. E.g., this input:
             - *single asterisks*
             - _single underscores_
             - **double asterisks**
             - __double underscores__
             Will produce this oputput:
-            - &lt;em>single asterisks&lt;/em>
-            - &lt;u>single underscores&lt;/u>
-            - &lt;strong>double asterisks&lt;/strong>
-            - &lt;strong>double underscores&lt;/strong>
+            - <em>single asterisks</em>
+            - <u>single underscores</u>
+            - <strong>double asterisks</strong>
+            - <strong>double underscores</strong>
           EOPARAGRAPH
         end
 
@@ -168,16 +168,16 @@ module Decidim
             <strong>bold text</strong> is supported, <em>italics text</em> is supported, <strong>underlined text</strong> is supported.
             As explained <a href="https://daringfireball.net/projects/markdown/syntax#em">here</a> Markdown treats asterisks
             and underscores as indicators of emphasis.
-            Text wrapped with one asterisk or underscore will be wrapped with an HTML &lt;em> tag; double asterisks or underscores will be wrapped with an HTML &lt;strong> tag. E.g., this input:
+            Text wrapped with one asterisk or underscore will be wrapped with an HTML <em> tag; double asterisks or underscores will be wrapped with an HTML <strong> tag. E.g., this input:
             - <em>single asterisks</em>
             - <u>single underscores</u>
             - <strong>double asterisks</strong>
             - <strong>double underscores</strong>
             Will produce this oputput:
-            - &lt;em>single asterisks&lt;/em>
-            - &lt;u>single underscores&lt;/u>
-            - &lt;strong>double asterisks&lt;/strong>
-            - &lt;strong>double underscores&lt;/strong>
+            - <em>single asterisks</em>
+            - <u>single underscores</u>
+            - <strong>double asterisks</strong>
+            - <strong>double underscores</strong>
           EOEXPECTED
           expect(translated(proposal.body)).to eq(paragraph.strip)
           expect(proposal.position).to eq(1)
