428ff1f0853a9e91042f6ff665b0c83503bc057e
Fernando Blat <fernando@blat.es>
2022-07-15 10:45:19 +0200

    Strip tags keeping entity characters

--- decidim-core/app/presenters/decidim/push_notification_presenter.rb
+++ decidim-core/app/presenters/decidim/push_notification_presenter.rb
@@ -10,7 +10,8 @@ module Decidim
     end
 
     def body
-      ActionView::Base.full_sanitizer.sanitize(event_class_instance.notification_title)
+      # Not using Rails sanitizers here because they escape HTML entities (i.e &amp;) and we want to keep them
+      event_class_instance.notification_title.gsub(%r{</?[^>]*>}, "") if event_class_instance.notification_title.present?
     end
 
     def icon
---
+++ decidim-core/spec/presenters/decidim/push_notification_presenter_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  describe PushNotificationPresenter, type: :presenter do
+    let(:notification) { create(:notification) }
+    let(:np) { described_class.new(notification) }
+
+    subject { described_class.new(notification) }
+
+    context "with a valid notification" do
+      let(:event_class) { Decidim::Comments::CommentCreatedEvent }
+      let(:event_name) { "decidim.events.comments.comment_created" }
+      let(:extra) { { comment_id: create(:comment).id } }
+
+      let(:notification) { create(:notification, event_class: event_class, event_name: event_name, extra: extra) }
+
+      describe "#body" do
+        it "returns text without links and with HTML entities unescaped" do
+          # rubocop:disable RSpec/AnyInstance
+          allow_any_instance_of(event_class).to receive(:notification_title).and_return(
+            "There is a new comment from <a href='/path/to/profile'>Foo & Bar</a>"
+          )
+
+          expect(subject.body).to eq("There is a new comment from Foo & Bar")
+          # rubocop:enable RSpec/AnyInstance
+        end
+      end
+    end
+  end
+end
