605639b2e2727f65c7d20379a8c408ade7415d80
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-07-19 10:53:36 +0200

    Disable layout specific of participatory process (redesign pending)

--- decidim-pages/app/controllers/decidim/pages/application_controller.rb
+++ decidim-pages/app/controllers/decidim/pages/application_controller.rb
@@ -8,7 +8,8 @@ module Decidim
     # Note that it inherits from `Decidim::Components::Basecontroller`, which
     # override its layout and provide all kinds of useful methods.
     class ApplicationController < Decidim::Components::BaseController
-      redesign_participatory_space_layout only: [:show]
+
+      redesign_participatory_space_layout
 
       def show
         @page = Page.find_by(component: current_component)
