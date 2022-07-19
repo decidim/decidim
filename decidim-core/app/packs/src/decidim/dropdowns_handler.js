679aac67be79f024f5a34a0faae5897b63ec8c21
Hugoren Martinako <aumpfbahn@gmail.com>
2022-07-19 11:06:49 +0200

    accordion style & add bypass behaviour

--- decidim-core/app/packs/src/decidim/dropdowns_handler.js
+++ decidim-core/app/packs/src/decidim/dropdowns_handler.js
@@ -1,12 +1,18 @@
 /**
  * Handler to allow ONLY ONE dropdown (details HTML tag) open at once.
  * To click outside or open a different dropdown will close the others.
+ *
+ * Adding the HTML5 attribute autoclose="false" to the details tag, will bypass this behaviour
  */
 document.addEventListener("DOMContentLoaded", () => {
-  const details = [...document.querySelectorAll("details")];
+  const details = [...document.querySelectorAll('details:not([autoclose="false"])')];
 
   document.addEventListener("click", ({ target }) => {
-    if (details.some((element) => element.contains(target)).length !== 0) {
+    if (details.some((element) => element.contains(target))) {
+      // click inside handler: close everything but the cliked one
+      details.forEach((element) => !element.contains(target) && element.removeAttribute("open"));
+    } else {
+      // click outside handler: close everything
       details.forEach((element) => element.removeAttribute("open"));
     }
   });
