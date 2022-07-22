deedd44f170b44c76c7f67115b266a05e4beeeb9
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-07-22 21:54:30 +0200

    Allow to provide options to redesigned_follow_button from helper using it

--- decidim-core/app/helpers/decidim/followable_helper.rb
+++ decidim-core/app/helpers/decidim/followable_helper.rb
@@ -4,8 +4,11 @@ module Decidim
   # A Helper for views with Followable resources.
   module FollowableHelper
     # Invokes the decidim/shared/follow_button partial.
-    def follow_button_for(model, large = nil)
-      controller.view_context.render partial: "decidim/shared/follow_button", locals: { followable: model, large: }
+    def follow_button_for(model, large = nil, opts = {})
+      controller.view_context.render(
+        partial: "decidim/shared/follow_button",
+        locals: { followable: model, large: , opts: opts.slice(:button_classes, :text_classes, :icon_classes) }
+      )
     end
   end
 end
