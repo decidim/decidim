9f2b117046461027e9501c23073ebbda015df797
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2022-07-25 14:22:45 +0300

    Fix linting violations

--- decidim-accountability/app/packs/src/decidim/accountability/admin/imports.js
+++ decidim-accountability/app/packs/src/decidim/accountability/admin/imports.js
@@ -1,16 +1,16 @@
  const tuggleInfo = (val) => {
-    document.querySelectorAll(".help-text").forEach((toBeHidden) => {
-        if(val == toBeHidden.id && toBeHidden.style.display == "none"){
-          toBeHidden.style.display = null;
-          }else{
-            toBeHidden.style.display = "none";
-          }
+  document.querySelectorAll(".help-text").forEach((toBeHidden) => {
+    if (val === toBeHidden.id && toBeHidden.style.display === "none") {
+      toBeHidden.style.display = null;
+    } else {
+      toBeHidden.style.display = "none";
+    }
   });
-  };
-  export default () =>{
-    var item = document.querySelector("#result_import_projects_origin_component_id");
-      item.addEventListener(('change'),(event)=>{
-        tuggleInfo(`component_${event.target.value}`);
-      });
-  };
+};
+export default () => {
+  const item = document.querySelector("#result_import_projects_origin_component_id");
+  item.addEventListener(("change"), (event) => {
+    tuggleInfo(`component_${event.target.value}`);
+  });
+};
 
