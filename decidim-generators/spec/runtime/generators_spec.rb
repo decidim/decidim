1d8d189b1790f1011a502256d53ccbae5ca2791b
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-07-25 20:17:44 +0300

    Add spec for running db commands consecutively

--- decidim-generators/spec/runtime/generators_spec.rb
+++ decidim-generators/spec/runtime/generators_spec.rb
@@ -85,6 +85,18 @@ module Decidim
 
         it_behaves_like "a new development application"
         it_behaves_like "an application with extra configurable env vars"
+
+        context "when running the db commands consecutively" do
+          let(:command) { "decidim #{test_app} --demo --path #{repo_root}" }
+
+          let(:subcommand) { "bundle exec rails db:drop db:create db:migrate db:seed" }
+          let(:subresult) { GemManager.new(test_app).capture(subcommand, env: env) }
+
+          it "creates the app without errors" do
+            expect(result[1]).to be_success, result[0]
+            expect(subresult[1]).to be_success, subresult[0]
+          end
+        end
       end
 
       context "with a development application" do
