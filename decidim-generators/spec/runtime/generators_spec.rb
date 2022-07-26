94e3bfe3482a5b8c5313140c38fd66ef0827fd3b
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-07-26 10:20:22 +0300

    Revert "Try sub-command without bundle exec in CI"
    This reverts commit 66e4361df6a5e423773b6c1ed2316d209dbaccb4.

--- decidim-generators/spec/runtime/generators_spec.rb
+++ decidim-generators/spec/runtime/generators_spec.rb
@@ -89,7 +89,7 @@ module Decidim
         context "when running the db commands consecutively" do
           let(:command) { "decidim #{test_app} --demo --path #{repo_root}" }
 
-          let(:subcommand) { "rails db:drop db:create db:migrate db:seed" }
+          let(:subcommand) { "bundle exec rails db:drop db:create db:migrate db:seed" }
           let(:subresult) { GemManager.new(test_app).capture(subcommand, env: env) }
 
           it "creates the app without errors" do
