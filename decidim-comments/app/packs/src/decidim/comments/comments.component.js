81f0680e69dd8a6dd7fea54ba9a11b65060c9070
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-07-26 22:08:17 +0300

    Fix posting comments before the initial load has run

--- decidim-comments/app/packs/src/decidim/comments/comments.component.js
+++ decidim-comments/app/packs/src/decidim/comments/comments.component.js
@@ -215,6 +215,7 @@ export default class CommentsComponent {
    * @returns {Void} - Returns nothing
    */
   _fetchComments() {
+    $(".add-comment textarea", this.$element).prop("disabled", true);
     Rails.ajax({
       url: this.commentsUrl,
       type: "GET",
@@ -226,7 +227,10 @@ export default class CommentsComponent {
         ...(this.toggleTranslations && { "toggle_translations": this.toggleTranslations }),
         ...(this.lastCommentId && { "after": this.lastCommentId })
       }),
-      success: this._pollComments()
+      success: () => {
+        $(".add-comment textarea", this.$element).prop("disabled", false);
+        this._pollComments();
+      }
     })
   }
 
