a063c9defd77698fa071febac77ca4ccad8aa8e0
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-08-02 13:05:39 +0300

    Fix machine translations at the API

--- decidim-core/spec/types/localized_string_type_spec.rb
+++ decidim-core/spec/types/localized_string_type_spec.rb
@@ -27,6 +27,54 @@ module Decidim
           expect(response).to include("text" => "A test locale.")
         end
       end
+
+      describe "machineTranslated" do
+        let(:query) { "{ machineTranslated }" }
+
+        it "returns false by default" do
+          expect(response).to include("machineTranslated" => false)
+        end
+
+        context "when the model responds to machine_translated" do
+          let(:model) { OpenStruct.new(machine_translated: true) }
+
+          it "returns the correct value" do
+            expect(response).to include("machineTranslated" => true)
+          end
+
+          context "and the model returns false" do
+            let(:model) { OpenStruct.new(machine_translated: false) }
+
+            it "returns the correct value" do
+              expect(response).to include("machineTranslated" => false)
+            end
+          end
+
+          context "and the model returns a nil" do
+            let(:model) { OpenStruct.new(machine_translated: nil) }
+
+            it "returns false" do
+              expect(response).to include("machineTranslated" => false)
+            end
+          end
+
+          context "and the model returns a non-boolean evaluating as present" do
+            let(:model) { OpenStruct.new(machine_translated: "true") }
+
+            it "returns true" do
+              expect(response).to include("machineTranslated" => true)
+            end
+          end
+
+          context "and the model returns a non-boolean evaluating as blank" do
+            let(:model) { OpenStruct.new(machine_translated: "") }
+
+            it "returns false" do
+              expect(response).to include("machineTranslated" => false)
+            end
+          end
+        end
+      end
     end
   end
 end
