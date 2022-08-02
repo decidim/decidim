ace7c2ede2eaf0001a21f9eb3770b172d0ffa4ab
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-08-02 14:43:06 +0300

    Fix form answer attachments breaking the answer view

--- decidim-forms/lib/decidim/forms/test/shared_examples/manage_questionnaire_answers.rb
+++ decidim-forms/lib/decidim/forms/test/shared_examples/manage_questionnaire_answers.rb
@@ -10,8 +10,11 @@ shared_examples_for "manage questionnaire answers" do
   let!(:second) do
     create :questionnaire_question, questionnaire:, position: 2, question_type: "single_option"
   end
+  let!(:third) do
+    create :questionnaire_question, questionnaire:, position: 3, question_type: "files"
+  end
   let(:questions) do
-    [first, second]
+    [first, second, third]
   end
 
   context "when there are no answers" do
@@ -25,6 +28,7 @@ shared_examples_for "manage questionnaire answers" do
     let!(:answer1) { create :answer, questionnaire:, question: first }
     let!(:answer2) { create :answer, body: "second answer", questionnaire:, question: first }
     let!(:answer3) { create :answer, questionnaire:, question: second }
+    let!(:file_answer) { create :answer, :with_attachments, questionnaire:, question: third, body: nil, user: answer3.user, session_token: answer3.session_token }
 
     it "shows the answer admin link" do
       visit questionnaire_edit_path
@@ -45,7 +49,7 @@ shared_examples_for "manage questionnaire answers" do
       end
 
       it "shows the percentage" do
-        expect(page).to have_content("50%")
+        expect(page).to have_content("33%")
       end
 
       it "has a detail link" do
@@ -120,6 +124,25 @@ shared_examples_for "manage questionnaire answers" do
         click_link answer3.session_token, match: :first
         expect(page).not_to have_link("Next ›")
         expect(page).to have_link("‹ Prev")
+      end
+
+      it "third answer has download link for the attachments" do
+        click_link answer3.session_token, match: :first
+        expect(page).to have_content(translated(file_answer.attachments.first.title))
+        expect(page).to have_content(translated(file_answer.attachments.second.title))
+      end
+
+      context "when the file answer does not have a title for the attachment" do
+        let!(:file_answer) { create :answer, questionnaire:, question: third, body: nil, user: answer3.user, session_token: answer3.session_token }
+
+        before do
+          create :attachment, :with_image, attached_to: file_answer, title: {}, description: {}
+        end
+
+        it "third answer has download link for the attachments" do
+          click_link answer3.session_token, match: :first
+          expect(page).to have_content("Download attachment")
+        end
       end
     end
   end
