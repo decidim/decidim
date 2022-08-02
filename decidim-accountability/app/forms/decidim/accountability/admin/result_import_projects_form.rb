b0a81f5161ac14b181be2dd034fe3e36c49b88d6
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2022-08-02 12:07:46 +0300

    Add error when there are no projects to import

--- decidim-accountability/app/forms/decidim/accountability/admin/result_import_projects_form.rb
+++ decidim-accountability/app/forms/decidim/accountability/admin/result_import_projects_form.rb
@@ -8,9 +8,11 @@ module Decidim
       class ResultImportProjectsForm < Decidim::Form
         attribute :origin_component_id, Integer
         attribute :import_all_selected_projects, Boolean
+        attribute :to_be_added_projects, Integer
 
         validates :origin_component_id, presence: true
         validates :import_all_selected_projects, allow_nil: false, acceptance: true
+        validates :to_be_added_projects, numericality: { greater_than: 0 }, if: ->(form) { form.origin_component_id }
 
         def origin_component
           @origin_component ||= origin_components.find_by(id: origin_component_id)
@@ -26,11 +28,16 @@ module Decidim
           @budgets_component ||= current_participatory_space.components.where(manifest_name: "budgets")
         end
 
+        def to_be_added_projects
+          selceted_projects_count(origin_component_id)
+        end
+
         def selceted_projects_count(component)
           projects = Decidim::Budgets::Project.joins(:budget).selected.where(
             budget: { component: component }
           )
           projects.reject { |project| project_already_copied?(project) }.count
+          # errors[:origin_component_id] << "No projects are being added" if projects_count <= 0
         end
 
         def project_already_copied?(original_project)
