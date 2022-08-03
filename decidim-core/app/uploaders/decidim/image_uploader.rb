02650700568c545d53ff2f26423bc57cc0224f76
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-08-03 17:17:32 +0300

    Use MiniMime to get the mime type for the extension

--- decidim-core/app/uploaders/decidim/image_uploader.rb
+++ decidim-core/app/uploaders/decidim/image_uploader.rb
@@ -8,7 +8,7 @@ module Decidim
     end
 
     def content_type_allowlist
-      extension_allowlist.map { |ext| Rack::Mime.mime_type(".#{ext}") }.uniq
+      extension_allowlist.map { |ext| MiniMime.lookup_by_extension(ext).content_type }.uniq
     end
 
     # Fetches info about different variants, their processors and dimensions
