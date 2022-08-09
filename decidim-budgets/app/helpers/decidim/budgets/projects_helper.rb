2fe6e05ad173ed50d6efc48ce69018ca4772c16f
Fernando Blat <fernando@blat.es>
2022-08-09 04:37:19 +0200

    Display amounts and dynamic help text

--- decidim-budgets/app/helpers/decidim/budgets/projects_helper.rb
+++ decidim-budgets/app/helpers/decidim/budgets/projects_helper.rb
@@ -64,24 +64,49 @@ module Decidim
         end
       end
 
+      # Returns false if the current order does not have a rule for minimum budget
+      # Returns false if the current order has not reached the minimum budget
+      # Otherwhise returns true
+      def current_order_minimum_reached?
+        return false if current_order.minimum_budget.zero?
+
+        current_order.total > current_order.minimum_budget
+      end
+
+      def current_rule_call_for_action_text
+        return "" unless current_order
+
+        if current_order_minimum_reached?
+          t(".dynamic_help.minimum_reached")
+        elsif current_order.projects.empty?
+          t(".dynamic_help.start_adding_projects")
+        else
+          t(".dynamic_help.keep_adding_projects")
+        end
+      end
+
       def current_rule_description
         return unless current_order
 
-        if current_order.projects_rule?
-          if current_order.minimum_projects.positive? && current_order.minimum_projects < current_order.maximum_projects
-            t(
-              ".projects_rule.description",
-              minimum_number: current_order.minimum_projects,
-              maximum_number: current_order.maximum_projects
-            )
-          else
-            t(".projects_rule_maximum_only.description", maximum_number: current_order.maximum_projects)
-          end
-        elsif current_order.minimum_projects_rule?
-          t(".minimum_projects_rule.description", minimum_number: current_order.minimum_projects)
-        else
-          t(".vote_threshold_percent_rule.description", minimum_budget: budget_to_currency(current_order.minimum_budget))
-        end
+        rule_text = if current_order_minimum_reached?
+                      ""
+                    elsif current_order.projects_rule?
+                      if current_order.minimum_projects.positive? && current_order.minimum_projects < current_order.maximum_projects
+                        t(
+                          ".projects_rule.description",
+                          minimum_number: current_order.minimum_projects,
+                          maximum_number: current_order.maximum_projects
+                        )
+                      else
+                        t(".projects_rule_maximum_only.description", maximum_number: current_order.maximum_projects)
+                      end
+                    elsif current_order.minimum_projects_rule?
+                      t(".minimum_projects_rule.description", minimum_number: current_order.minimum_projects)
+                    else
+                      t(".vote_threshold_percent_rule.description", minimum_budget: budget_to_currency(current_order.minimum_budget))
+                    end
+
+        %(<strong>#{current_rule_call_for_action_text}</strong>. #{rule_text} #{cell("decidim/budgets/budget_information_modal", budget)}).html_safe
       end
 
       # Serialize a collection of geocoded projects to be used by the dynamic map component
