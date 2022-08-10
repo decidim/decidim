4461fa6fc0ca865c390f66320aa229dfc4e94727
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-08-10 18:26:18 +0300

    Update stylelint

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -156,5 +156,11 @@ module.exports = { // eslint-disable-line
     "value-list-comma-space-after": "always-single-line",
     "value-list-comma-space-before": "never",
     "value-list-max-empty-lines": 0
-  }
+  },
+  "overrides": [
+    {
+      "files": ["**/*.scss"],
+      "customSyntax": "postcss-scss"
+    }
+  ]
 }
