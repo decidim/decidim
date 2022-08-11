40e8fa0bc33ce64895aea54e41904f0be26d06b4
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-08-11 13:13:05 +0300

    Remove the overrides and solve the positioning using custom reference
    In order not to override too much from the emoji popup picker,
    create a specific reference element positioned exactly where we
    want the popup picker to appear.

--- decidim-core/app/packs/src/decidim/input_emoji.js
+++ decidim-core/app/packs/src/decidim/input_emoji.js
@@ -1,49 +1,4 @@
-import { PopupPickerController } from "@picmo/popup-picker";
-
-/**
- * This implements the mobile position handling based on a mobile breakpoint to
- * make the picker behave better on mobile.
- */
-const MOBILE_BREAKPOINT = 450;
-class PickerController extends PopupPickerController {
-  constructor(pickerOptions, popupOptions) {
-    super(pickerOptions, popupOptions);
-
-    this.configuredRootElement = popupOptions.rootElement || this.options.rootElement;
-    this.configuredPosition = {
-      desktop: this.options.position,
-      mobile: this.options.mobilePosition || this.options.position
-    }
-  }
-
-  async setPosition() {
-    if (this.isMobileView()) {
-      this.options.position = this.configuredPosition.mobile;
-    } else {
-      this.options.position = { inset: null, ...this.configuredPosition.desktop };
-    }
-
-    await Reflect.apply(PopupPickerController.prototype.setPosition, this, []);
-  }
-
-  async open() {
-    if (this.isOpen) {
-      return;
-    }
-
-    if (this.isMobileView()) {
-      this.options.rootElement = document.body;
-    } else {
-      this.options.rootElement = this.configuredRootElement;
-    }
-
-    await Reflect.apply(PopupPickerController.prototype.open, this, []);
-  }
-
-  isMobileView() {
-    return window.matchMedia(`screen and (max-width: ${MOBILE_BREAKPOINT}px)`).matches;
-  }
-}
+import { createPopup } from "@picmo/popup-picker";
 
 /**
  * Turns a deep messages object into a dictionary object with a single level and
@@ -68,14 +23,6 @@ const dictionary = (messages, prefix = "") => {
   return final;
 };
 
-const createPopup = (pickerOptions, popupOptions) => {
-  const popup = new PickerController({
-    autoFocus: "auto",
-    ...pickerOptions
-  }, popupOptions);
-  return popup;
-}
-
 // eslint-disable-next-line require-jsdoc
 export default function addInputEmoji() {
   const containers = document.querySelectorAll("[data-input-emoji]");
@@ -95,6 +42,7 @@ export default function addInputEmoji() {
         reveal.style.overflowY = "unset"
       }
 
+      const elemStyle = window.getComputedStyle(elem, null);
       const wrapper = document.createElement("div");
       wrapper.className = "emoji__container"
       const btnContainer = document.createElement("div");
@@ -103,11 +51,15 @@ export default function addInputEmoji() {
       btn.type = "button";
       btn.className = "emoji__button"
       btn.innerHTML = '<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="smile" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 448c-110.3 0-200-89.7-200-200S137.7 56 248 56s200 89.7 200 200-89.7 200-200 200zm-80-216c17.7 0 32-14.3 32-32s-14.3-32-32-32-32 14.3-32 32 14.3 32 32 32zm160 0c17.7 0 32-14.3 32-32s-14.3-32-32-32-32 14.3-32 32 14.3 32 32 32zm4 72.6c-20.8 25-51.5 39.4-84 39.4s-63.2-14.3-84-39.4c-8.5-10.2-23.7-11.5-33.8-3.1-10.2 8.5-11.5 23.6-3.1 33.8 30 36 74.1 56.6 120.9 56.6s90.9-20.6 120.9-56.6c8.5-10.2 7.1-25.3-3.1-33.8-10.1-8.4-25.3-7.1-33.8 3.1z"></path></svg>'
+      const referenceElement = document.createElement("div");
+      referenceElement.className = "emoji__reference";
+      referenceElement.style = `position: absolute; bottom: -${elemStyle["padding-bottom"]}; right: -${elemStyle["padding-right"]};`;
 
       const parent = elem.parentNode;
       parent.insertBefore(wrapper, elem);
       wrapper.appendChild(elem);
       wrapper.appendChild(btnContainer);
+      wrapper.appendChild(referenceElement);
       btnContainer.appendChild(btn);
 
       // The form errors need to be in the same container with the field they
@@ -119,15 +71,9 @@ export default function addInputEmoji() {
         locale: document.documentElement.getAttribute("lang"),
         i18n
       }, {
-        position: {
-          position: "absolute",
-          right: 0,
-          bottom: 0
-        },
-        mobilePosition: "bottom-end",
-        rootElement: wrapper,
-        referenceElement: elem,
-        triggerElement: btn
+        position: "bottom-end",
+        triggerElement: btn,
+        referenceElement
       });
 
       // Prevent the picker close button to submit the comment form
