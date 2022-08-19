b0fe7ffac86f05922982598767cfaffe5bd5c23f
Hugoren Martinako <aumpfbahn@gmail.com>
2022-08-19 12:42:24 +0200

    id_documents

--- decidim-core/app/cells/decidim/upload_modal_cell.rb
+++ decidim-core/app/cells/decidim/upload_modal_cell.rb
@@ -23,9 +23,7 @@ module Decidim
     end
 
     def button_class
-      "button small hollow add-field add-file" if has_title?
-
-      "button small add-file"
+      "#{options[:button_class]} add-file"
     end
 
     def label
