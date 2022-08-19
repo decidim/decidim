23ff50aef5c8efed1a1dfba99c2d5f32fe1e0f69
Hugoren Martinako <aumpfbahn@gmail.com>
2022-08-19 14:50:48 +0200

    add helper (with error) + edit view

---
+++ decidim-verifications/app/helpers/decidim/verifications/application_helper.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Verifications
+    module ApplicationHelper
+      def announcement_title(authorization)
+        return t("decidim.verifications.id_documents.authorizations.edit.being_reviewed") if authorization.rejected?
+
+        body = content_tag :ul do
+          [
+            content_tag :li, t("decidim.verifications.id_documents.authorizations.edit.rejection_correctness"),
+            content_tag :li, t("decidim.verifications.id_documents.authorizations.edit.rejection_clarity")
+          ].join
+        end.html_safe
+
+        {
+          title: t("decidim.verifications.id_documents.authorizations.edit.rejection_notice"),
+          body: body
+        }
+      end
+    end
+  end
+end
