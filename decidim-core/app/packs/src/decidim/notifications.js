9d44062b5420903bb9de4f3cf9bdab21e989fa5b
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-08-25 17:53:04 +0200

    Add a no notifications message using javascript if notifications are empty

--- decidim-core/app/packs/src/decidim/notifications.js
+++ decidim-core/app/packs/src/decidim/notifications.js
@@ -1,8 +1,10 @@
 /**
  * REDESIGN_PENDING: This file will be unnecessary once Turbo is implemented.
+ * The topbar__notifications element has changes after redesign
  * @returns {void}
  */
 export default function() {
+  const noNotificationsText = document.querySelector(".empty-notifications")
   const handleRemove = ({ currentTarget }) => currentTarget.remove()
   const handleFadeOut = (element) => {
     if (element) {
@@ -10,7 +12,17 @@ export default function() {
       element.style.opacity = 0
     }
   }
-  const handleClick = ({ currentTarget }) => handleFadeOut(currentTarget.closest("[data-notification]"))
+  const emptyNotifications = () => {
+    noNotificationsText.classList.remove("hidden")
+    noNotificationsText.classList.remove("hide")
+    document.querySelector(".topbar__notifications")?.classList?.remove("is-active")
+  }
+  const handleClick = ({ currentTarget }) => {
+    handleFadeOut(currentTarget.closest("[data-notification]"))
+    if (!document.querySelector("[data-notification]:not([style])")) {
+      emptyNotifications()
+    }
+  }
 
   const notifications = document.querySelectorAll("[data-notification]")
 
@@ -18,6 +30,11 @@ export default function() {
     notifications.forEach((btn) => btn.querySelector("[data-notification-read]").addEventListener("click", handleClick))
 
     document.querySelector("[data-notification-read-all]").
-      addEventListener("click", () => notifications.forEach((notification) => handleFadeOut(notification)))
+      addEventListener(
+        "click", () => {
+          notifications.forEach((notification) => handleFadeOut(notification))
+          emptyNotifications()
+        }
+      )
   }
 }
