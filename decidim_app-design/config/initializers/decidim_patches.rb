015d34ab6ca5c1eec5786014d824536ae8896cc9
Fernando Blat <fernando@blat.es>
2022-09-07 11:56:46 +0200

    Use decidim header and footer

--- decidim_app-design/config/initializers/decidim_patches.rb
+++ decidim_app-design/config/initializers/decidim_patches.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Rails.application.config.middleware.delete Decidim::Middleware::CurrentOrganization
+Rails.application.config.middleware.delete Decidim::Middleware::CurrentOrganizationDesignApp
 
 Decidim.configure do |config|
   config.redesign_active = true
