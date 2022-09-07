24b9cb006242a270f6a7bc172009831cd38d9547
Fernando Blat <fernando@blat.es>
2022-09-07 12:40:52 +0200

    Add missing require

--- decidim_app-design/config/initializers/decidim_patches.rb
+++ decidim_app-design/config/initializers/decidim_patches.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "decidim/middleware/current_organization_design_app"
+
 Rails.application.config.middleware.delete Decidim::Middleware::CurrentOrganizationDesignApp
 
 Decidim.configure do |config|
