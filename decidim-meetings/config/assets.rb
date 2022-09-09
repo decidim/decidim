f2d4dcb02508f88e10ca49aea29a6ab117b3fa2f
Hugoren Martinako <aumpfbahn@gmail.com>
2022-09-09 14:16:08 +0200

    prepare assets & index layout

--- decidim-meetings/config/assets.rb
+++ decidim-meetings/config/assets.rb
@@ -7,5 +7,3 @@ Decidim::Webpacker.register_entrypoints(
   decidim_meetings: "#{base_path}/app/packs/entrypoints/decidim_meetings.js",
   decidim_meetings_admin: "#{base_path}/app/packs/entrypoints/decidim_meetings_admin.js"
 )
-
-Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/meetings/meetings")
