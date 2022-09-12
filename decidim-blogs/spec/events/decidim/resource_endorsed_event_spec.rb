619c0cc194c506d099561e47939f6a8ac4297fe5
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-09-12 11:14:51 +0300

    Backport 'Fix resource endorsed notification with Debates' to v0.27 (#9722)
    * Add resource type in ResourceEndorsedEvent spec
    
    * Extract 'resource endorsed event' to a shared example
    
    * Extract resource_text so it can be defined on callers
    
    * Add failing spec
    
    * Fix when resource doesn't have a body
    
    * Add specs for Proposals and Blog Posts
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-blogs/spec/events/decidim/resource_endorsed_event_spec.rb
+++ decidim-blogs/spec/events/decidim/resource_endorsed_event_spec.rb
@@ -3,8 +3,8 @@
 require "spec_helper"
 
 describe Decidim::ResourceEndorsedEvent do
-  let(:resource) { create :dummy_resource, title: { en: "My super dummy resource" } }
-  let(:resource_type) { "Dummy resource" }
+  let(:resource) { create :post, title: { en: "My blog post" } }
+  let(:resource_type) { "Post" }
   let(:resource_text) { resource.body }
 
   it_behaves_like "resource endorsed event"
