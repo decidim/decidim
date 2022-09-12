3cf23305d8e0526b6ef9e51f5f6695acb2eb1fc9
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-09-12 12:52:53 +0300

    Backport 'Fix issues with daily and weekly notifications' to v0.27 (#9739)
    * Fix issues with daily and weekly notifications
    
    * Fix notification daily and weekly status
    
    * Fix notification digest sending decider to check from the end of the day
    
    * Rubocop
    
    * Fix syntax errors for Ruby 3.0

--- decidim-core/spec/services/decidim/notifications_digest_sending_decider_spec.rb
+++ decidim-core/spec/services/decidim/notifications_digest_sending_decider_spec.rb
@@ -26,7 +26,7 @@ describe Decidim::NotificationsDigestSendingDecider do
   end
 
   context "when frequency is set to daily" do
-    context "with a user who has receive a notifications digest mail in the same day" do
+    context "with a user who has received a notifications digest mail in the same day" do
       let(:current_time) { Time.now.utc }
       let(:user) { build :user, notifications_sending_frequency: :daily, digest_sent_at: current_time - 1.hour }
 
@@ -37,9 +37,25 @@ describe Decidim::NotificationsDigestSendingDecider do
       end
     end
 
-    context "with a user who has receive a notifications digest mail two days ago" do
+    context "with a user who has received a notifications digest mail two days ago" do
       let(:current_time) { Time.now.utc }
       let(:user) { build :user, notifications_sending_frequency: :daily, digest_sent_at: current_time - 2.days }
+
+      describe "must_notify?" do
+        it "returns true" do
+          expect(subject.must_notify?(user, time: current_time)).to be(true)
+        end
+      end
+    end
+
+    # This particular case shouldn't happen on daily runs but in case sending
+    # takes a long time, those users should be also included who were notified
+    # a bit later than the exact time when the scheduled task was run as we
+    # cannot know precicely on which second the scheduled task was run exactly
+    # and if it is always run at the same second.
+    context "with a user who has received a notification digest mail the previous day but less than a day ago" do
+      let(:current_time) { Time.now.utc }
+      let(:user) { build :user, notifications_sending_frequency: :daily, digest_sent_at: (current_time - 1.day).end_of_day }
 
       describe "must_notify?" do
         it "returns true" do
@@ -50,7 +66,7 @@ describe Decidim::NotificationsDigestSendingDecider do
   end
 
   context "when frequency is set to weekly" do
-    context "with a user who has receive a notifications digest mail in the same week" do
+    context "with a user who has received a notifications digest mail in the same week" do
       let(:current_time) { Time.now.utc }
       let(:user) { build :user, notifications_sending_frequency: :weekly, digest_sent_at: current_time - 1.day }
 
@@ -61,9 +77,25 @@ describe Decidim::NotificationsDigestSendingDecider do
       end
     end
 
-    context "with a user who has receive a notifications digest mail two weeks ago" do
+    context "with a user who has received a notifications digest mail two weeks ago" do
       let(:current_time) { Time.now.utc }
       let(:user) { build :user, notifications_sending_frequency: :weekly, digest_sent_at: current_time - 2.weeks }
+
+      describe "must_notify?" do
+        it "returns true" do
+          expect(subject.must_notify?(user, time: current_time)).to be(true)
+        end
+      end
+    end
+
+    # This particular case shouldn't happen on weekly runs but in case sending
+    # takes a long time, those users should be also included who were notified
+    # a bit later than the exact time when the scheduled task was run as we
+    # cannot know precicely on which second the scheduled task was run exactly
+    # and if it is always run at the same second.
+    context "with a user who has received a notifications the previous week but less than a exactly 7 days ago from yesterday" do
+      let(:current_time) { Time.now.utc }
+      let(:user) { build :user, notifications_sending_frequency: :weekly, digest_sent_at: (current_time - 1.day - 1.week).end_of_day }
 
       describe "must_notify?" do
         it "returns true" do
