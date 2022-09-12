baff4363b6388f09eb1db31d64e8ecadc44e1f61
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-09-12 11:05:17 +0300

    Backport 'Fix publish event on official proposals' to v0.27 (#9707)
    * Fix publish event on official proposals
    
    * Ruby offsense
    
    * Use official check method
    
    * Add missing translation and rewording
    
    * Remove period
    
    * Test proper scenario
    
    Co-authored-by: Fernando Blat <fernando@blat.es>

--- decidim-proposals/app/events/decidim/proposals/publish_proposal_event.rb
+++ decidim-proposals/app/events/decidim/proposals/publish_proposal_event.rb
@@ -13,6 +13,8 @@ module Decidim
       end
 
       def i18n_options
+        return super if author.blank?
+
         author_path = link_to("@#{author.nickname}", profile_path(author.nickname))
         author_string = "#{author.name} #{author_path}"
         super.merge({ author: author_string })
@@ -33,6 +35,12 @@ module Decidim
 
       def safe_resource_translated_text
         I18n.with_locale(I18n.locale) { translated_attribute(resource_text, nil, true).to_s.html_safe }
+      end
+
+      def notification_title
+        i18n_key = resource.official? ? "notification_title_official" : "notification_title"
+
+        I18n.t(i18n_key, **i18n_options).html_safe
       end
 
       private
