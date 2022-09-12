29251519fbaca11561847fdfb6753140d04e501e
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-09-12 11:41:04 +0300

    Backport 'Fix absolute urls on 'managed user error' event' to v0.26 (#9730)
    * Fix absolute urls on 'managed user error' event
    
    * Remove unecessary requires for simple_event examples
    
    * Fix bug on factory where two users could be on different organizations
    
    * Fix absolute urls on 'managed user error' event
    
    * Using EngineRouter for generating the conflict_url
    
    * Changing the event to other participant
    
    * Wrap only the EngineRouter for URLs
    
    * Change 'other participant' string in email subject too
    
    * Update specs with latest changes
    
    * Fix spec difference for 0.26 (link without port)
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-blogs/spec/events/decidim/blogs/create_post_event_spec.rb
+++ decidim-blogs/spec/events/decidim/blogs/create_post_event_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-require "decidim/core/test/shared_examples/simple_event"
 
 describe Decidim::Blogs::CreatePostEvent do
   let(:resource) { create :post }
