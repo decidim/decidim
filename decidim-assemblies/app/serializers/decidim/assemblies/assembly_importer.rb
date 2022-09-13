0299d22b170a50e12e0ab64d81842b67fcb5d955
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2022-09-13 11:45:30 +0200

    Fix import of images on spaces
    * Fix import hero and banner images in participatory process
    
    Fix assemblies and process images importation
    
    * Remove uneeded yarn.lock
    
    * TEST: Make tests succeed
    
    * Refactor bugfix
    
    * Rename helper test function
    
    Co-authored-by: Laura Jaime <laura.jv@coditramuntana.com>

--- decidim-assemblies/app/serializers/decidim/assemblies/assembly_importer.rb
+++ decidim-assemblies/app/serializers/decidim/assemblies/assembly_importer.rb
@@ -61,8 +61,9 @@ module Decidim
             meta_scope: attributes["meta_scope"],
             announcement: attributes["announcement"]
           )
-          @imported_assembly.remote_hero_image_url = attributes["remote_hero_image_url"] if remote_file_exists?(attributes["remote_hero_image_url"])
-          @imported_assembly.remote_banner_image_url = attributes["remote_banner_image_url"] if remote_file_exists?(attributes["remote_banner_image_url"])
+          @imported_assembly.attached_uploader(:hero_image).remote_url = attributes["remote_hero_image_url"] if attributes["remote_hero_image_url"].present?
+          @imported_assembly.attached_uploader(:banner_image).remote_url = attributes["remote_banner_image_url"] if attributes["remote_banner_image_url"].present?
+
           @imported_assembly.save!
           @imported_assembly
         end
