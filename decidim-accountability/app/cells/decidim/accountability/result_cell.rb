84d93e5e4dc546c0a29973b3703b03fb6e1c6548
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-09-19 20:03:12 +0200

    Define a cell to display results data

--- decidim-accountability/app/cells/decidim/accountability/result_cell.rb
+++ decidim-accountability/app/cells/decidim/accountability/result_cell.rb
@@ -5,16 +5,65 @@ require "cell/partial"
 module Decidim
   module Accountability
     # This cell renders the result card for an instance of a Result
-    # the default size is the Medium Card (:m)
     class ResultCell < Decidim::ViewModel
+      include Decidim::SanitizeHelper
+      include Decidim::TranslationsHelper
+      include ActiveSupport::NumberHelper
+
+      delegate :start_date, :end_date, :status, to: :model
+
       def show
-        cell card_size, model, options
+        render
       end
 
       private
 
-      def card_size
-        "decidim/accountability/result_m"
+      def items
+        [dates_item, status_item, status_description].compact
+      end
+
+      def dates_item
+        return if start_date.blank?
+
+        date_title = [
+          *(t("models.result.fields.start_date", scope: "decidim.accountability") if result.start_date),
+          *(t("models.result.fields.end_date", scope: "decidim.accountability") if result.end_date)
+        ].join(" / ")
+
+        date_values = [
+          *(l(result.start_date, format: :decidim_short_with_month_name_short) if result.start_date),
+          *(l(result.end_date, format: :decidim_short_with_month_name_short) if result.end_date)
+        ].join(" / ")
+
+        {
+          text: date_title,
+          icon: "calendar-todo-line",
+          value: date_values
+        }
+      end
+
+      def status_item
+        return if status.blank?
+
+        {
+          text: t("models.result.fields.status", scope: "decidim.accountability"),
+          icon: "focus-2-line",
+          value: translated_attribute(status.name)
+        }
+      end
+
+      def status_description
+        return unless status.present? && (description = translated_attribute(status.description)).present?
+
+        {
+          text: t("models.status.fields.description", scope: "decidim.accountability"),
+          icon: "file-text-line",
+          value: description
+        }
+      end
+
+      def has_dates?
+        start_date.present? && end_date.present?
       end
     end
   end
