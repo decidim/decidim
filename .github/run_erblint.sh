043de328c95c535e9ffd99dcce10aa1b4d00caf0
Hugoren Martinako <aumpfbahn@gmail.com>
2022-09-29 12:34:02 +0200

    Get back the initial command

--- .github/run_erblint.sh
+++ .github/run_erblint.sh
@@ -2,10 +2,10 @@
 
 shopt -s globstar
 
-DEFAULT_BRANCH=develop
+DEFAULT_BRANCH=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
 
 # Scan only the modified ERB files
-FILES=$(git diff --name-only "${DEFAULT_BRANCH}"..HEAD -- | grep '.erb$')
+FILES=$(git diff --name-only "${DEFAULT_BRANCH}" | grep '.erb$')
 [[ -n "$FILES" ]] && bundle exec erblint ${FILES} || echo "No ERB files changed"
 
 # Store the return code of the erblint execution
