dc357a66523a486708351b89b34f547908cba759
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-09-29 21:03:30 +0200

    Refactor comments_button cell to inherit from RedesignedButtonCell

--- decidim-core/app/cells/decidim/comments_button_cell.rb
+++ decidim-core/app/cells/decidim/comments_button_cell.rb
@@ -1,7 +1,28 @@
 # frozen_string_literal: true
 
 module Decidim
-  class CommentsButtonCell < Decidim::ViewModel
-    include LayoutHelper
+  class CommentsButtonCell < RedesignedButtonCell
+    private
+
+    def path
+      # REDESIGN_PENDING: Replace this path
+      "#comments"
+    end
+
+    def text
+      t("decidim.comments.comments_title")
+    end
+
+    def icon_classes
+      nil
+    end
+
+    def text_classes
+      nil
+    end
+
+    def icon_name
+      "chat-1-line"
+    end
   end
 end
