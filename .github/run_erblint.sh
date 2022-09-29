1c68a1b1a41c11720cfcb7937536425beae68e13
Hugoren Martinako <aumpfbahn@gmail.com>
2022-09-29 12:06:58 +0200

    handle no files exit code

--- .github/run_erblint.sh
+++ .github/run_erblint.sh
@@ -5,7 +5,8 @@ shopt -s globstar
 DEFAULT_BRANCH=develop
 
 # Scan only the modified ERB files
-bundle exec erblint $(git diff --name-only "${DEFAULT_BRANCH}"..HEAD -- | grep '.erb$')
+FILES=$(git diff --name-only "${DEFAULT_BRANCH}"..HEAD -- | grep '.erb$')
+[[ -n "$FILES" ]] && bundle exec erblint ${FILES} || echo "No ERB files changed"
 
 # Store the return code of the erblint execution
 EXIT_CODE=$?
