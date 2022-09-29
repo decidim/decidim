31ff3c3148272321e381685442f6bc9dcd6ba412
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-09-29 21:26:32 +0200

    Refactor share and flag buttons to inherit from RedesignedButtonCell

--- decidim-core/app/cells/decidim/report_button_cell.rb
+++ decidim-core/app/cells/decidim/report_button_cell.rb
@@ -1,7 +1,27 @@
 # frozen_string_literal: true
 
 module Decidim
-  class ReportButtonCell < Decidim::ViewModel
-    include LayoutHelper
+  class ReportButtonCell < RedesignedButtonCell
+    private
+
+    def button_classes
+      "button button__sm button__text-secondary"
+    end
+
+    def text
+      t("decidim.shared.flag_modal.report")
+    end
+
+    def icon_classes
+      nil
+    end
+
+    def text_classes
+      nil
+    end
+
+    def icon_name
+      "flag-line"
+    end
   end
 end
--- decidim-core/app/cells/decidim/share_button_cell.rb
+++ decidim-core/app/cells/decidim/share_button_cell.rb
@@ -1,7 +1,27 @@
 # frozen_string_literal: true
 
 module Decidim
-  class ShareButtonCell < Decidim::ViewModel
-    include LayoutHelper
+  class ShareButtonCell < RedesignedButtonCell
+    private
+
+    def button_classes
+      "button button__sm button__text-secondary"
+    end
+
+    def text
+      t("decidim.shared.share_modal.share")
+    end
+
+    def icon_classes
+      nil
+    end
+
+    def text_classes
+      nil
+    end
+
+    def icon_name
+      "share-line"
+    end
   end
 end
