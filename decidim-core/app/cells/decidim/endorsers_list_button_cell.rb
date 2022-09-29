8fdf750676b114e00d17d64a2f7595f9051af78e
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-09-29 20:38:04 +0200

    Refactor endorsers_list cell to inherit from RedesignedButtonCell

--- decidim-core/app/cells/decidim/endorsers_list_button_cell.rb
+++ decidim-core/app/cells/decidim/endorsers_list_button_cell.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 module Decidim
-  class EndorsersListButtonCell < Decidim::ViewModel
-    include LayoutHelper
+  class EndorsersListButtonCell < RedesignedButtonCell
+    private
+
+    def button_classes
+      "endorsers-list__trigger"
+    end
+
+    def html_options
+      { data: { component: "dropdown", target: "dropdown-menu-endorsers-list" } }
+    end
   end
 end
