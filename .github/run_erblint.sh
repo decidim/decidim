a6c33713bd98a587b636d18a25d9b04652d84629
Hugoren Martinako <aumpfbahn@gmail.com>
2022-09-29 11:22:30 +0200

    Test another syntax

--- .github/run_erblint.sh
+++ .github/run_erblint.sh
@@ -5,7 +5,7 @@ shopt -s globstar
 DEFAULT_BRANCH=develop
 
 # Scan only the modified ERB files
-bundle exec erblint $(git diff --name-only "${DEFAULT_BRANCH}" | grep '.erb$')
+bundle exec erblint $(git diff --name-only "${DEFAULT_BRANCH}"..HEAD -- | grep '.erb$')
 
 # Store the return code of the erblint execution
 EXIT_CODE=$?
