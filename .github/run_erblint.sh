9924f5994ee61b48404b04774cbb70d793cd2bd4
Hugoren Martinako <aumpfbahn@gmail.com>
2022-09-29 09:16:13 +0200

    boost the ERBlinter filtering the scanned files

--- .github/run_erblint.sh
+++ .github/run_erblint.sh
@@ -2,7 +2,10 @@
 
 shopt -s globstar
 
-bundle exec erblint decidim**/app/{cells,views}/**/*.erb
+DEFAULT_BRANCH=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+
+# Scan only the modified ERB files
+bundle exec erblint $(git diff --name-only "${DEFAULT_BRANCH}" | grep '.erb$')
 
 # Store the return code of the erblint execution
 EXIT_CODE=$?
