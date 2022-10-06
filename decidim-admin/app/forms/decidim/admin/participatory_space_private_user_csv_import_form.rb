1d5ad2edf9d8a48638515ea8c5f2a33f3253b738
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-10-06 09:53:20 +0300

    Add malformed file errors when CSV reading fails
    * Add malformed file errors when CSV reading fails
    
    * Exclude the introduced ISO 8859-1 encoded file from the i18n scan
    
    * Fix the formatting server error for the CSV census form

--- decidim-admin/app/forms/decidim/admin/participatory_space_private_user_csv_import_form.rb
+++ decidim-admin/app/forms/decidim/admin/participatory_space_private_user_csv_import_form.rb
@@ -19,9 +19,11 @@ module Decidim
       def validate_csv
         return if file.blank?
 
-        CSV.foreach(file.path) do |_email, user_name|
+        CSV.foreach(file.path, encoding: "BOM|UTF-8") do |_email, user_name|
           errors.add(:user_name, :invalid) unless user_name.match?(UserBaseEntity::REGEXP_NAME)
         end
+      rescue CSV::MalformedCSVError
+        errors.add(:file, :malformed)
       end
     end
   end
