f362e7e8f8b65d226cae79d8c07fb3a7c99abdb4
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-10-26 11:36:24 +0300

    Fix invalid rendering of meeting and proposal body texts
    * Remove the double sanitization from the meeting description
    
    * Fix the rendering of the safe content
    
    * Add proper styles for the quill lists
    
    * Fix rich text editor specs after class name change
    
    * Revert the edit spec change which was already correct
    
    * Fix typos in specs
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    * Remove ql-reset-decidim class
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-proposals/spec/helpers/application_helper_spec.rb
+++ decidim-proposals/spec/helpers/application_helper_spec.rb
@@ -5,13 +5,7 @@ require "spec_helper"
 module Decidim
   module Proposals
     describe ApplicationHelper do
-      let(:helper) do
-        Class.new.tap do |v|
-          v.extend(Decidim::Proposals::ApplicationHelper)
-        end
-      end
-
-      describe "humanize_proposal_state" do
+      describe "#humanize_proposal_state" do
         subject { helper.humanize_proposal_state(state) }
 
         context "when it is accepted" do
@@ -36,6 +30,47 @@ module Decidim
           let(:state) { "withdrawn" }
 
           it { is_expected.to eq("Withdrawn") }
+        end
+      end
+
+      describe "#render_proposal_body" do
+        subject { helper.render_proposal_body(proposal) }
+
+        before do
+          allow(helper).to receive(:present).with(proposal).and_return(Decidim::Proposals::ProposalPresenter.new(proposal))
+          allow(helper).to receive(:current_organization).and_return(proposal.organization)
+          helper.instance_variable_set(:@proposal, proposal)
+        end
+
+        let(:body) { "<ul><li>First</li><li>Second</li><li>Third</li></ul><script>alert('OK');</script>" }
+        let(:proposal_trait) { :participant_author }
+        let(:proposal) { create(:proposal, proposal_trait, body: { "en" => body }) }
+
+        it "renders a sanitized body" do
+          expect(subject).to eq(
+            <<~HTML.strip
+              <div>• First
+              <br />• Second
+              <br />• Third
+              </div>
+            HTML
+          )
+        end
+
+        context "with official meeting" do
+          let(:proposal_trait) { :official }
+
+          it "renders a sanitized body" do
+            expect(subject).to eq(
+              <<~HTML.strip
+                <div class="ql-editor-display"><ul>
+                <li>First</li>
+                <li>Second</li>
+                <li>Third</li>
+                </ul>alert('OK');</div>
+              HTML
+            )
+          end
         end
       end
     end
