0dfbfb0f12fc303b2410de1a095b0384f522a072
Andrés Pereira de Lucena <andreslucena@gmail.com>
2022-10-27 09:35:21 +0200

    Add omitted value in keyword argument

--- decidim-pages/spec/serializers/decidim/pages/data_importer_spec.rb
+++ decidim-pages/spec/serializers/decidim/pages/data_importer_spec.rb
@@ -7,8 +7,8 @@ module Decidim::Pages
     let(:importer) { described_class.new(component) }
 
     let(:organization) { create(:organization) }
-    let(:user) { create(:user, :admin, :confirmed, organization:) }
-    let(:participatory_process) { create(:participatory_process, organization:) }
+    let(:user) { create(:user, :admin, :confirmed, organization: organization) }
+    let(:participatory_process) { create(:participatory_process, organization: organization) }
     let(:component) { create(:component, manifest_name: "pages", participatory_space: participatory_process) }
 
     describe "#import" do
@@ -30,7 +30,7 @@ module Decidim::Pages
 
       it "imports the page" do
         expect(subject).to be_a(Decidim::Pages::Page)
-        expect(subject.id).to eq(Page.find_by(component:).id)
+        expect(subject.id).to eq(Page.find_by(component: component).id)
         expect(subject.body).to eq(
           {
             "en" => "English content",
