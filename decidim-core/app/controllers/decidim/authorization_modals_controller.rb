4839776d0e78936fc220fdfb22b7e1e44a3258d1
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-10-28 18:36:20 +0200

    Move parts of redesigned authorizations modal content to controller

--- decidim-core/app/controllers/decidim/authorization_modals_controller.rb
+++ decidim-core/app/controllers/decidim/authorization_modals_controller.rb
@@ -2,7 +2,7 @@
 
 module Decidim
   class AuthorizationModalsController < Decidim::ApplicationController
-    helper_method :authorizations, :authorize_action_path
+    helper_method :authorizations, :authorize_action_path, :status_messages, :status_fields, :status_cta
     layout false
 
     def show; end
@@ -30,6 +30,51 @@ module Decidim
 
     def authorizations
       @authorizations ||= action_authorized_to(authorization_action, resource:)
+    end
+
+    def status_messages(status)
+      [t(
+        "#{status.code}.explanation",
+        authorization: t("#{status.handler_name}.name", scope: "decidim.authorization_handlers"),
+        scope: "decidim.authorization_modals.content"
+      )].tap do |messages|
+        [status.data[:extra_explanation]].flatten.compact.each do |extra_explanation|
+          messages << t(extra_explanation[:key], **extra_explanation[:params])
+        end
+      end
+    end
+
+    def status_fields(status)
+      return [] if status.data[:fields].blank?
+
+      status.data[:fields].map do |field, value|
+        t(
+          "#{status.code}.invalid_field",
+          field: t("#{status.handler_name}.fields.#{field}", scope: "decidim.authorization_handlers"),
+          value: value ? "(#{value})" : "",
+          scope: "decidim.authorization_modals.content"
+        )
+      end
+    end
+
+    def status_cta(status)
+      if status.data[:action].present?
+        {
+          type: :a,
+          text: t(
+            "#{status.code}.#{status.data[:action]}",
+            authorization: t("#{status.handler_name}.name", scope: "decidim.authorization_handlers"),
+            scope: "decidim.authorization_modals.content"
+          ),
+          url: authorize_action_path(status.handler_name)
+        }
+      else
+        {
+          type: :button,
+          text: t("#{status.code}.ok", scope: "decidim.authorization_modals.content"),
+          data: { "dialog-close": "authorizationModal" }
+        }
+      end
     end
   end
 end
