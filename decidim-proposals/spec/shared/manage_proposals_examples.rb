85204b4494aa176ddaa76a5d2c0df2cc5e187f7c
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-11-04 08:32:09 +0200

    Backport 'Fix importing a page component without a body' to v0.26 (#10023)
    * Fix importing a page component without a body
    
    * Add page in AssemblyImporter JSON example
    
    * Add spec for pages imported without body
    
    * Add guard clause for pages imported without body
    
    * Fix proposals tests
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@gmail.com>

--- decidim-proposals/spec/shared/manage_proposals_examples.rb
+++ decidim-proposals/spec/shared/manage_proposals_examples.rb
@@ -337,8 +337,8 @@ shared_examples "manage proposals" do
         end
 
         proposal.reload
-        expect(proposal.answered_at).to be_within(1.second).of Time.zone.now
-        expect(proposal.state_published_at).to be_within(1.second).of Time.zone.now
+        expect(proposal.answered_at).to be_within(2.seconds).of Time.zone.now
+        expect(proposal.state_published_at).to be_within(2.seconds).of Time.zone.now
       end
 
       it "can accept a proposal" do
@@ -356,8 +356,8 @@ shared_examples "manage proposals" do
         end
 
         proposal.reload
-        expect(proposal.answered_at).to be_within(1.second).of Time.zone.now
-        expect(proposal.state_published_at).to be_within(1.second).of Time.zone.now
+        expect(proposal.answered_at).to be_within(2.seconds).of Time.zone.now
+        expect(proposal.state_published_at).to be_within(2.seconds).of Time.zone.now
       end
 
       it "can mark a proposal as evaluating" do
@@ -375,8 +375,8 @@ shared_examples "manage proposals" do
         end
 
         proposal.reload
-        expect(proposal.answered_at).to be_within(1.second).of Time.zone.now
-        expect(proposal.state_published_at).to be_within(1.second).of Time.zone.now
+        expect(proposal.answered_at).to be_within(2.seconds).of Time.zone.now
+        expect(proposal.state_published_at).to be_within(2.seconds).of Time.zone.now
       end
 
       it "can mark a proposal as 'not answered'" do
@@ -435,7 +435,7 @@ shared_examples "manage proposals" do
         end
 
         proposal.reload
-        expect(proposal.answered_at).to be_within(1.second).of Time.zone.now
+        expect(proposal.answered_at).to be_within(2.seconds).of Time.zone.now
       end
     end
 
