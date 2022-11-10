226cfa108722266b2fcf7edbb975338f988c5327
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-11-10 19:26:05 +0100

    Refactor meeting_month cell and update params

--- decidim-meetings/app/cells/decidim/meetings/meeting_month_cell.rb
+++ decidim-meetings/app/cells/decidim/meetings/meeting_month_cell.rb
@@ -3,12 +3,26 @@
 module Decidim
   module Meetings
     class MeetingMonthCell < Decidim::ViewModel
+      alias meetings model
+
       def month
-        options[:month]
+        @month ||= expanded_month(start_date || first_meeting.start_time)
+      end
+
+      def start_date
+        options[:start_date]
       end
 
       def events
-        options[:events] || []
+        options[:events] || meetings.select { |meeting| meeting.start_time.month == month[0] }.map { |meeting| meeting.start_time.to_date }
+      end
+
+      def first_meeting
+        @first_meeting ||= meetings.first
+      end
+
+      def expanded_month(date)
+        [date.month, date.to_date.all_month]
       end
     end
   end
