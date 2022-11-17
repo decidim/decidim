aa35e01f593c1777529edd66f05e1221b76041c6
Alexandru-Emil Lupu <contact@alecslupu.ro>
2022-11-17 15:02:35 +0200

    Apply clarified review recommendations

--- decidim-templates/db/migrate/20221006055954_add_field_values_to_decidim_templates.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddFieldValuesToDecidimTemplates < ActiveRecord::Migration[6.0]
-  def change
-    add_column :decidim_templates_templates, :field_values, :json, default: {}
-    add_column :decidim_templates_templates, :target, :string
-  end
-end
