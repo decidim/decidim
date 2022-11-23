da92b320e56e8fdd7f6952c6331eb7a1d6b67d84
Andrés Pereira de Lucena <andreslucena@gmail.com>
2022-11-23 09:23:08 +0100

    Fix rubocop offense

--- decidim-conferences/db/migrate/20221116084952_add_weight_to_conferences.rb
+++ decidim-conferences/db/migrate/20221116084952_add_weight_to_conferences.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddWeightToConferences < ActiveRecord::Migration[6.1]
   def change
     add_column :decidim_conferences, :weight, :integer, null: false, default: 0
