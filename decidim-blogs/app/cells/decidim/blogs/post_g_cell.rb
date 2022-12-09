905b0332d60957926d0d31cab143723779fde3d3
Hugoren Martinako <aumpfbahn@gmail.com>
2022-12-09 18:18:59 +0100

    create post_g cell

---
+++ decidim-blogs/app/cells/decidim/blogs/post_g_cell.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Blogs
+    # This cell renders the Medium (:m) post card
+    # for an given instance of a Post
+    class PostGCell < Decidim::CardMCell
+      private
+    end
+  end
+end
