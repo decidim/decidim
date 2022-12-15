c8971b8d81ce21bf53338bbbfa6c307f340adaf0
Fernando Blat <fernando@blat.es>
2022-12-15 10:24:51 +0100

    Use jQuery to load mentions

--- decidim-comments/app/packs/src/decidim/comments/comments.js
+++ decidim-comments/app/packs/src/decidim/comments/comments.js
@@ -12,5 +12,7 @@ const commentsInitializer = () => {
   });
 }
 
-document.addEventListener("DOMContentLoaded", () => commentsInitializer());
+// If no jQuery is used the Tribute feature used in comments to autocomplete
+// mentions stops working
+$(() => commentsInitializer());
 document.addEventListener("turbo:load", () => commentsInitializer());
