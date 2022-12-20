96c13d03b8f14458d654046dccea76a49971307a
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-12-20 11:53:30 +0100

    Fix tests

--- decidim-meetings/spec/shared/manage_invites_examples.rb
+++ decidim-meetings/spec/shared/manage_invites_examples.rb
@@ -83,7 +83,7 @@ shared_examples "manage invites" do
           end
 
           expect(page).to have_content "successfully"
-          expect(page).to have_css(".button", text: "CANCEL YOUR REGISTRATION")
+          expect(page).to have_css(".button", text: "Cancel your registration")
         end
 
         it "the invited user sign up into the application and declines the invitation" do
@@ -102,7 +102,7 @@ shared_examples "manage invites" do
           end
 
           expect(page).to have_content "declined the invitation successfully"
-          expect(page).to have_css(".button", text: "JOIN MEETING")
+          expect(page).to have_css(".button", text: "Join meeting")
         end
       end
 
@@ -116,7 +116,7 @@ shared_examples "manage invites" do
 
           visit last_email_link
 
-          expect(page).to have_css(".button", text: "CANCEL YOUR REGISTRATION")
+          expect(page).to have_css(".button", text: "Cancel your registration")
         end
 
         it "the invited user declines the invitation" do
@@ -126,7 +126,7 @@ shared_examples "manage invites" do
 
           visit last_email_first_link
 
-          expect(page).to have_css(".button", text: "JOIN MEETING")
+          expect(page).to have_css(".button", text: "Join meeting")
         end
       end
 
@@ -140,7 +140,7 @@ shared_examples "manage invites" do
 
           visit last_email_link
 
-          expect(page).to have_css(".button", text: "CANCEL YOUR REGISTRATION")
+          expect(page).to have_css(".button", text: "Cancel your registration")
         end
 
         it "the invited user declines the invitation" do
@@ -150,7 +150,7 @@ shared_examples "manage invites" do
 
           visit last_email_first_link
 
-          expect(page).to have_css(".button", text: "JOIN MEETING")
+          expect(page).to have_css(".button", text: "Join meeting")
         end
       end
     end
@@ -171,9 +171,9 @@ shared_examples "manage invites" do
       it "allows searching by text" do
         visit_meeting_invites_page
 
-        within "[data-filters]" do
+        within ".filters__search" do
           fill_in :q, with: invites.first.user.email
-          find('button[type="submit"]').click
+          find(".icon--magnifying-glass").click
         end
 
         within "#meeting-invites table tbody" do
