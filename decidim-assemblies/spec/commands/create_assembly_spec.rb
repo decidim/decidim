785b7670e8af0e3b1d719e6231021a002eed268f
Alexandru Emil Lupu <contact@alecslupu.ro>
2022-12-21 11:18:42 +0200

    Add order by in linked_participatory_space_resources
    * Add order by in linked_participatory_space_resources
    
    * Fixing Weight issue on Conferences
    
    * Add Migration for other participatory spaces
    
    * Apply review recommendations

--- decidim-assemblies/spec/commands/create_assembly_spec.rb
+++ decidim-assemblies/spec/commands/create_assembly_spec.rb
@@ -19,6 +19,8 @@ module Decidim::Assemblies
         organization: organization
       )
     end
+    let(:related_process_ids) { [participatory_processes.map(&:id)] }
+
     let(:form) do
       instance_double(
         Admin::AssemblyForm,
@@ -47,7 +49,7 @@ module Decidim::Assemblies
         parent: nil,
         private_space: false,
         errors: errors,
-        participatory_processes_ids: participatory_processes.map(&:id),
+        participatory_processes_ids: related_process_ids,
         show_statistics: false,
         purpose_of_action: { en: "purpose of action" },
         composition: { en: "composition of internal working groups" },
@@ -188,6 +190,19 @@ module Decidim::Assemblies
         subject.call
         linked_participatory_processes = assembly.linked_participatory_space_resources(:participatory_processes, "included_participatory_processes")
         expect(linked_participatory_processes).to match_array(participatory_processes)
+      end
+
+      context "when sorting by weight" do
+        let!(:process_one) { create :participatory_process, organization: organization, weight: 2 }
+        let!(:process_two) { create :participatory_process, organization: organization, weight: 1 }
+        let(:related_process_ids) { [process_one.id, process_two.id] }
+
+        it "links processes in right way" do
+          subject.call
+
+          linked_processes = assembly.linked_participatory_space_resources(:participatory_process, "included_participatory_processes")
+          expect(linked_processes.first).to eq(process_two)
+        end
       end
     end
   end
