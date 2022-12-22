949bafbb252f29bdf9586c4b3b25a1e72c9d71b9
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-12-22 20:56:40 +0100

    Add state item to proposal metadata card

--- decidim-proposals/app/cells/decidim/proposals/proposal_card_metadata_cell.rb
+++ decidim-proposals/app/cells/decidim/proposals/proposal_card_metadata_cell.rb
@@ -4,6 +4,10 @@ module Decidim
   module Proposals
     # This cell renders metadata for an instance of a Proposal
     class ProposalCardMetadataCell < Decidim::CardMetadataCell
+      include Decidim::Proposals::ApplicationHelper
+
+      delegate :state, to: :model
+
       def initialize(*)
         super
 
@@ -13,7 +17,26 @@ module Decidim
       private
 
       def proposal_items
-        [coauthors_item, comments_count_item, endorsements_count_item, emendation_item]
+        [coauthors_item, comments_count_item, endorsements_count_item, state_item, emendation_item]
+      end
+
+      def state_item
+        return if state.blank?
+
+        { text: content_tag(:span, humanize_proposal_state(state), class: state_class) }
+      end
+
+      def state_class
+        case state
+        when "accepted"
+          "success"
+        when "rejected", "withdrawn"
+          "alert"
+        when "evaluating"
+          "warning"
+        else
+          "muted"
+        end
       end
     end
   end
