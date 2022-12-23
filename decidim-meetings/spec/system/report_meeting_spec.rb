166d602223077b146dd4edb748a3775fc11febe4
Alexandru Emil Lupu <contact@alecslupu.ro>
2022-12-23 10:42:19 +0200

    Add ability to hide content from front page (#10024)
    * Add ability to hide content from front page
    
    * Apply review recommendations

--- decidim-meetings/spec/system/report_meeting_spec.rb
+++ decidim-meetings/spec/system/report_meeting_spec.rb
@@ -6,16 +6,15 @@ describe "Report Meeting", type: :system do
   include_context "with a component"
 
   let(:manifest_name) { "meetings" }
-  let!(:meetings) { create_list(:meeting, 3, :published, component:) }
+  let!(:meetings) { create_list(:meeting, 3, :not_official, :published, component:) }
   let(:reportable) { meetings.first }
   let(:reportable_path) { resource_locator(reportable).path }
-  let!(:user) { create :user, :confirmed, organization: }
 
   let!(:component) do
-    create(:proposal_component,
+    create(:meeting_component,
            manifest:,
            participatory_space: participatory_process)
   end
 
-  include_examples "reports"
+  include_examples "reports by user type"
 end
