8cc49802fb321bcfa84acf30c98fe7b390790049
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-12-27 16:23:45 +0200

    Add editor toolbar groups

--- decidim-core/app/packs/src/decidim/editor/toolbar.js
+++ decidim-core/app/packs/src/decidim/editor/toolbar.js
@@ -21,6 +21,13 @@ const createIcon = (iconName, options = {}) => {
   </svg>`;
 }
 
+const createEditorToolbarGroup = (editor, { items }) => {
+  const group = document.createElement("div")
+  group.classList.add("editor-toolbar-group");
+  items.forEach((item) => group.appendChild(item));
+  return group;
+}
+
 const createEditorToolbarToggle = (editor, { label, icon, action }) => {
   const ctrl = document.createElement("button");
   ctrl.classList.add("editor-toolbar-control");
@@ -82,121 +89,163 @@ export default function createEditorToolbar(editor) {
     }
   });
 
-  toolbar.appendChild(createEditorSelect(editor, {
-    label: "Text style",
-    options: [
-      { value: "normal", label: "Normal" },
-      { value: 2, label: "Heading 2" },
-      { value: 3, label: "Heading 3" },
-      { value: 4, label: "Heading 4" },
-      { value: 5, label: "Heading 5" },
-      { value: 6, label: "Heading 6" }
-    ],
-    action: (value) => {
-      if (value === "normal") {
-        editor.commands.setParagraph();
-      } else {
-        editor.commands.toggleHeading({ level: parseInt(value, 10) });
-      }
-    }
-  }));
-  toolbar.appendChild(createEditorToolbarToggle(editor, {
-    label: "Bold",
-    icon: "bold",
-    action: () => editor.commands.toggleBold()
-  }));
-  toolbar.appendChild(createEditorToolbarToggle(editor, {
-    label: "Italic",
-    icon: "italic",
-    action: () => editor.commands.toggleItalic()
-  }));
-  toolbar.appendChild(createEditorToolbarToggle(editor, {
-    label: "Underline",
-    icon: "underline",
-    action: () => editor.commands.toggleUnderline()
-  }));
-  toolbar.appendChild(createEditorToolbarToggle(editor, {
-    label: "Line break",
-    icon: "text-wrap",
-    action: () => editor.commands.setHardBreak()
-  }));
-  toolbar.appendChild(createEditorToolbarToggle(editor, {
-    label: "Ordered list",
-    icon: "list-ordered",
-    action: () => editor.commands.toggleOrderedList()
-  }));
-  toolbar.appendChild(createEditorToolbarToggle(editor, {
-    label: "Unordered list",
-    icon: "list-unordered",
-    action: () => editor.commands.toggleBulletList()
-  }));
-  toolbar.appendChild(createEditorToolbarToggle(editor, {
-    label: "Link",
-    icon: "link",
-    action: () => {
-      linkModal.toggle((state) => {
-        if (state === "save") {
-          editor.commands.setLink({ href: linkModal.getValue("href") })
-        } else {
-          editor.commands.unsetLink();
-        }
-      }, { href: editor.getAttributes("link").href });
-    }
-  }));
-  toolbar.appendChild(createEditorToolbarToggle(editor, {
-    label: "Erase styles",
-    icon: "eraser-line",
-    action: () => editor.chain().focus().clearNodes().unsetAllMarks().run()
-  }));
-  toolbar.appendChild(createEditorToolbarToggle(editor, {
-    label: "Code block",
-    icon: "code-line",
-    action: () => editor.commands.toggleCodeBlock()
-  }));
-  toolbar.appendChild(createEditorToolbarToggle(editor, {
-    label: "Blockquote",
-    icon: "double-quotes-l",
-    action: () => editor.commands.toggleBlockquote()
-  }));
-  toolbar.appendChild(createEditorToolbarToggle(editor, {
-    label: "Indent",
-    icon: "indent-increase",
-    action: () => editor.commands.indent()
-  }));
-  toolbar.appendChild(createEditorToolbarToggle(editor, {
-    label: "Outdent",
-    icon: "indent-decrease",
-    action: () => editor.commands.outdent()
-  }));
-  toolbar.appendChild(createEditorToolbarToggle(editor, {
-    label: "Video",
-    icon: "video-line",
-    action: () => {
-      videoModal.toggle((state) => {
-        if (state === "save") {
-          editor.commands.setYoutubeVideo({
-            src: videoModal.getValue("src"),
-            width: 640,
-            height: 480
-          });
-        }
-      })
-    }
-  }));
-  toolbar.appendChild(createEditorToolbarToggle(editor, {
-    label: "Image",
-    icon: "image-line",
-    action: () => {
-      imageModal.toggle((state) => {
-        if (state === "save") {
-          editor.commands.setImage({
-            src: imageModal.getValue("src"),
-            alt: imageModal.getValue("alt")
-          });
-        }
-      })
-    }
-  }));
+  toolbar.appendChild(
+    createEditorToolbarGroup(editor, {
+      items: [
+        createEditorSelect(editor, {
+          label: "Text style",
+          options: [
+            { value: "normal", label: "Normal" },
+            { value: 2, label: "Heading 2" },
+            { value: 3, label: "Heading 3" },
+            { value: 4, label: "Heading 4" },
+            { value: 5, label: "Heading 5" },
+            { value: 6, label: "Heading 6" }
+          ],
+          action: (value) => {
+            if (value === "normal") {
+              editor.commands.setParagraph();
+            } else {
+              editor.commands.toggleHeading({ level: parseInt(value, 10) });
+            }
+          }
+        })
+      ]
+    })
+  );
+  toolbar.appendChild(
+    createEditorToolbarGroup(editor, {
+      items: [
+        createEditorToolbarToggle(editor, {
+          label: "Bold",
+          icon: "bold",
+          action: () => editor.commands.toggleBold()
+        }),
+        createEditorToolbarToggle(editor, {
+          label: "Italic",
+          icon: "italic",
+          action: () => editor.commands.toggleItalic()
+        }),
+        createEditorToolbarToggle(editor, {
+          label: "Underline",
+          icon: "underline",
+          action: () => editor.commands.toggleUnderline()
+        }),
+        createEditorToolbarToggle(editor, {
+          label: "Line break",
+          icon: "text-wrap",
+          action: () => editor.commands.setHardBreak()
+        })
+      ]
+    })
+  );
+  toolbar.appendChild(
+    createEditorToolbarGroup(editor, {
+      items: [
+        createEditorToolbarToggle(editor, {
+          label: "Ordered list",
+          icon: "list-ordered",
+          action: () => editor.commands.toggleOrderedList()
+        }),
+        createEditorToolbarToggle(editor, {
+          label: "Unordered list",
+          icon: "list-unordered",
+          action: () => editor.commands.toggleBulletList()
+        })
+      ]
+    })
+  );
+  toolbar.appendChild(
+    createEditorToolbarGroup(editor, {
+      items: [
+        createEditorToolbarToggle(editor, {
+          label: "Link",
+          icon: "link",
+          action: () => {
+            linkModal.toggle((state) => {
+              if (state === "save") {
+                editor.commands.setLink({ href: linkModal.getValue("href") })
+              } else {
+                editor.commands.unsetLink();
+              }
+            }, { href: editor.getAttributes("link").href });
+          }
+        }),
+        createEditorToolbarToggle(editor, {
+          label: "Erase styles",
+          icon: "eraser-line",
+          action: () => editor.chain().focus().clearNodes().unsetAllMarks().run()
+        })
+      ]
+    })
+  );
+  toolbar.appendChild(
+    createEditorToolbarGroup(editor, {
+      items: [
+        createEditorToolbarToggle(editor, {
+          label: "Code block",
+          icon: "code-line",
+          action: () => editor.commands.toggleCodeBlock()
+        }),
+        createEditorToolbarToggle(editor, {
+          label: "Blockquote",
+          icon: "double-quotes-l",
+          action: () => editor.commands.toggleBlockquote()
+        })
+      ]
+    })
+  );
+  toolbar.appendChild(
+    createEditorToolbarGroup(editor, {
+      items: [
+        createEditorToolbarToggle(editor, {
+          label: "Indent",
+          icon: "indent-increase",
+          action: () => editor.commands.indent()
+        }),
+        createEditorToolbarToggle(editor, {
+          label: "Outdent",
+          icon: "indent-decrease",
+          action: () => editor.commands.outdent()
+        })
+      ]
+    })
+  );
+  toolbar.appendChild(
+    createEditorToolbarGroup(editor, {
+      items: [
+        createEditorToolbarToggle(editor, {
+          label: "Video",
+          icon: "video-line",
+          action: () => {
+            videoModal.toggle((state) => {
+              if (state === "save") {
+                editor.commands.setYoutubeVideo({
+                  src: videoModal.getValue("src"),
+                  width: 640,
+                  height: 480
+                });
+              }
+            })
+          }
+        }),
+        createEditorToolbarToggle(editor, {
+          label: "Image",
+          icon: "image-line",
+          action: () => {
+            imageModal.toggle((state) => {
+              if (state === "save") {
+                editor.commands.setImage({
+                  src: imageModal.getValue("src"),
+                  alt: imageModal.getValue("alt")
+                });
+              }
+            })
+          }
+        })
+      ]
+    })
+  );
 
   return toolbar;
 }
