ca05518d5769feac479fa25807f0dae0be3b934f
Hugoren Martinako <aumpfbahn@gmail.com>
2023-01-09 12:47:15 +0100

    tooltip variations

--- decidim-core/app/helpers/decidim/tooltip_helper.rb
+++ decidim-core/app/helpers/decidim/tooltip_helper.rb
@@ -4,9 +4,14 @@ module Decidim
   # This helper includes some methods to help with the inclusion of tooltips
   # on the layout.
   module TooltipHelper
+    # deprecated
     def with_tooltip(title, &)
       content_tag(:span, data: { tooltip: true, disable_hover: false, keep_on_hover: true, click_open: false },
                          title:, &)
+    end
+
+    def decidim_tooltip(opts = {}, &)
+      content_tag(:div, class: opts[:class] || "bottom", role: "tooltip", &)
     end
   end
 end
