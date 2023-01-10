7b5671af7fe9a8a6c170aeeb2bf5bf8c29dcca6f
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-01-10 20:44:11 +0100

    Define card_g cell

---
+++ decidim-core/app/cells/decidim/card_g_cell.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+module Decidim
+  # This cell is used a base for all Grid cards. It holds the basic layout
+  # so other cells only have to customize a few methods or overwrite views.
+  class CardGCell < Decidim::ViewModel
+    include Decidim::ApplicationHelper
+    include Decidim::SanitizeHelper
+
+    alias resource model
+
+    def show
+      render
+    end
+
+    private
+
+    def highlight?
+      options[:highlight] == true
+    end
+
+    def class_prefix
+      @class_prefix ||= "card__#{highlight? ? "highlight" : "grid"}"
+    end
+
+    def resource_id
+      "#{id_base_name}_#{resource.id}"
+    end
+
+    def resource_path
+      resource_locator(resource).path
+    end
+
+    def id_base_name
+      @id_base_name ||= resource.class.name.gsub(/\ADecidim::/, "").underscore.split("/").join("__")
+    end
+
+    def resource_image_path
+      nil
+    end
+
+    def has_image?
+      resource_image_path.present?
+    end
+
+    def has_description?
+      highlight?
+    end
+
+    def metadata_cell
+      "decidim/card_metadata"
+    end
+
+    def details_template
+      return :metadata if metadata_cell.present?
+      return :author if has_author?
+    end
+
+    def has_author?
+      false
+    end
+
+    def title
+      decidim_html_escape(translated_attribute(resource.title))
+    end
+
+    def title_tag
+      options[:title_tag] || :h3
+    end
+
+    def title_class
+      "h4 text-secondary"
+    end
+
+    def description
+      attribute = resource.try(:short_description) || resource.try(:body) || resource.description
+      text = translated_attribute(attribute)
+
+      decidim_sanitize_editor(html_truncate(text, length: 100))
+    end
+
+    def has_authors?
+      resource.is_a?(Decidim::Authorable) || resource.is_a?(Decidim::Coauthorable)
+    end
+  end
+end
