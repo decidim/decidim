3836231033f6919f681c883c19b667b4db54c6b8
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-01-11 10:34:51 +0100

    Define progress item in metadata

--- decidim-core/app/cells/decidim/card_metadata_cell.rb
+++ decidim-core/app/cells/decidim/card_metadata_cell.rb
@@ -52,5 +52,59 @@ module Decidim
         url: Decidim::ResourceLocatorPresenter.new(participatory_space).path
       }
     end
+
+    def start_date
+      nil
+    end
+
+    def end_date
+      nil
+    end
+
+    def dates_blank?
+      [start_date, end_date].all?(&:blank?)
+    end
+
+    def current_date
+      @current_date ||= Time.current.to_time
+    end
+
+    def progress_item
+      return if progress_value.blank?
+
+      {
+        text: "<div class=\"card__grid-loader\" style=\"--value:#{progress_value};\"></div>#{progress_text}".html_safe
+      }
+    end
+
+    def progress_value
+      return if dates_blank?
+
+      @progress_value ||= if start_date.present?
+                            if current_date <= start_date || end_date.blank?
+                              1
+                            elsif current_date <= end_date
+                              (end_date - current_date).to_f / (end_date - start_date).to_i
+                            else
+                              0
+                            end
+                          else
+                            current_date <= end_date ? 1 : 0
+                          end
+    end
+
+    def progress_text
+      return if progress_value.blank?
+
+      @progress_text ||= if start_date.present? && current_date < start_date
+                           t("not_started", scope: "decidim.metadata.progress")
+                         elsif end_date.blank?
+                           t("active", scope: "decidim.metadata.progress")
+                         elsif current_date < end_date
+                           t("remaining", time_distance: distance_of_time_in_words(current_date, end_date), scope: "decidim.metadata.progress")
+                         else
+                           t("finished", scope: "decidim.metadata.progress")
+                         end
+    end
   end
 end
