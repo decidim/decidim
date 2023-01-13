ef0d24d40098f8a1f98f32064e09bc868048995b
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-01-13 11:02:26 +0100

    Don't use sanitize_editor on description

--- decidim-core/app/cells/decidim/card_g_cell.rb
+++ decidim-core/app/cells/decidim/card_g_cell.rb
@@ -71,7 +71,7 @@ module Decidim
       attribute = resource.try(:short_description) || resource.try(:body) || resource.description
       text = translated_attribute(attribute)
 
-      decidim_sanitize_editor(html_truncate(text, length: 300))
+      decidim_sanitize(html_truncate(text, length: 300))
     end
 
     def has_authors?
