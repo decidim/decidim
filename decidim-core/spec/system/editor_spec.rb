27d425c7b29c17ab6febbe8749f0808e3570192e
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-01-17 11:38:10 +0200

    Add E2E tests for the legacy designed editor

--- decidim-core/spec/system/editor_spec.rb
+++ decidim-core/spec/system/editor_spec.rb
@@ -9,6 +9,7 @@ describe "Editor", type: :system do
   # Which features to enable for the toolbar: basic|full
   let(:features) { "basic" }
 
+  let(:redesigned) { true }
   let(:editor_content) { "" }
   let(:record) { OpenStruct.new(body: editor_content) }
   let(:form) { Decidim::FormBuilder.new(:record, record, template, {}) }
@@ -28,7 +29,45 @@ describe "Editor", type: :system do
         selfxssWarning: I18n.t("decidim.security.selfxss_warning")
       }
     }
-    editor_wrapper = form.editor(:body, toolbar: features, image_upload: { redesigned: true })
+    editor_wrapper = form.editor(:body, toolbar: features, image_upload: { redesigned: })
+    pack_prefix = ""
+    content_wrapper = ""
+    if redesigned
+      pack_prefix = "redesigned_"
+      content_wrapper = <<~HTML
+        <div data-content>
+          <main class="layout-1col">
+            <div class="cols-6">
+              <div class="text-center py-12">
+                <h1 class="h1 decorator inline-block text-left">Editor test</h1>
+              </div>
+              <div class="page__container">
+                #{editor_wrapper}
+              </div>
+            </div>
+          </main>
+        </div>
+      HTML
+    else
+      content_wrapper = <<~HTML
+        <main id="content">
+          <div class="wrapper">
+            <div class="row column">
+              <div class="page-title-wrapper">
+                <h1 class="heading1 page-title">Editor test</h1>
+              </div>
+              <div class="columns small-12">
+                <div class="card">
+                  <div class="card__content">
+                    #{editor_wrapper}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      HTML
+    end
     template.instance_eval do
       <<~HTML.strip
         <!doctype html>
@@ -41,27 +80,16 @@ describe "Editor", type: :system do
             protection.
           -->
           <meta name="csrf-token" content="abcdef0123456789">
-          #{stylesheet_pack_tag "redesigned_decidim_core", media: "all"}
+          #{stylesheet_pack_tag "#{pack_prefix}decidim_core", media: "all"}
           #{stylesheet_pack_tag "decidim_editor", media: "all"}
         </head>
         <body>
           <header>
             <a href="#content">Skip to main content</a>
           </header>
-          <div data-content>
-            <main class="layout-1col">
-              <div class="cols-6">
-                <div class="text-center py-12">
-                  <h1 class="h1 decorator inline-block text-left">Editor test</h1>
-                </div>
-                <div class="page__container">
-                  #{editor_wrapper}
-                </div>
-              </div>
-            </main>
-          </div>
+          #{content_wrapper}
           <footer>Decidim</footer>
-          #{javascript_pack_tag "redesigned_decidim_core", defer: false}
+          #{javascript_pack_tag "#{pack_prefix}decidim_core", defer: false}
           #{javascript_pack_tag "decidim_editor", defer: false}
           <script>
             Decidim.config.set(#{js_configs.to_json});
@@ -642,6 +670,130 @@ describe "Editor", type: :system do
     end
   end
 
+  # Note that the legacy design is necessary to maintain as long as we have
+  # the admin panel using the legacy design. This is also why we test that.
+  context "with legacy design" do
+    let(:redesigned) { false }
+
+    it_behaves_like "accessible page"
+
+    context "with basic toolbar controls" do
+      before do
+        prosemirror.native.send_keys "Hello, world!", [:enter], "Another paragraph."
+        prosemirror.native.send_keys [:shift, *Array.new(10).map { :left }]
+      end
+
+      it "link" do
+        click_toggle("link")
+        within "[data-reveal][aria-hidden='false']" do
+          fill_in "Link URL", with: "https://decidim.org"
+          select "New tab", from: "Target"
+          find("button[data-action='save']").click
+        end
+        expect_value(
+          <<~HTML
+            <p>Hello, world!</p>
+            <p>Another <a target="_blank" rel="noopener noreferrer nofollow" href="https://decidim.org">paragraph.</a></p>
+          HTML
+        )
+
+        click_toggle("link")
+        within "[data-reveal][aria-hidden='false']" do
+          fill_in "Link URL", with: "https://docs.decidim.org"
+          select "Default (same tab)", from: "Target"
+          find("button[data-action='save']").click
+        end
+        expect_value(
+          <<~HTML
+            <p>Hello, world!</p>
+            <p>Another <a rel="noopener noreferrer nofollow" href="https://docs.decidim.org">paragraph.</a></p>
+          HTML
+        )
+
+        click_toggle("link")
+        within "[data-reveal][aria-hidden='false']" do
+          find("button[data-action='remove']").click
+        end
+        expect_value(
+          <<~HTML
+            <p>Hello, world!</p>
+            <p>Another paragraph.</p>
+          HTML
+        )
+      end
+    end
+
+    context "with full toolbar controls" do
+      let(:features) { "full" }
+
+      before do
+        prosemirror.native.send_keys "Hello, world!"
+      end
+
+      it "videoEmbed" do
+        click_toggle("videoEmbed")
+        within "[data-reveal][aria-hidden='false']" do
+          fill_in "Video URL", with: "https://www.youtube.com/watch?v=f6JMgJAQ2tc"
+          fill_in "Title", with: "Decidim"
+          find("button[data-action='save']").click
+        end
+
+        expect_value(
+          <<~HTML
+            <p>Hello, world!</p>
+            <div class="editor-content-videoEmbed" data-video-embed="https://www.youtube.com/watch?v=f6JMgJAQ2tc">
+              <div>
+                <iframe src="https://www.youtube-nocookie.com/embed/f6JMgJAQ2tc?cc_load_policy=1&amp;modestbranding=1" title="Decidim" frameborder="0" allowfullscreen="true"></iframe>
+              </div>
+            </div>
+          HTML
+        )
+
+        click_toggle("videoEmbed")
+        within "[data-reveal][aria-hidden='false']" do
+          fill_in "Video URL", with: "https://www.youtube.com/watch?v=zhMMW0TENNA"
+          fill_in "Title", with: "Free Open-Source"
+          find("button[data-action='save']").click
+        end
+
+        expect_value(
+          <<~HTML
+            <p>Hello, world!</p>
+            <div class="editor-content-videoEmbed" data-video-embed="https://www.youtube.com/watch?v=zhMMW0TENNA">
+              <div>
+                <iframe src="https://www.youtube-nocookie.com/embed/zhMMW0TENNA?cc_load_policy=1&amp;modestbranding=1" title="Free Open-Source" frameborder="0" allowfullscreen="true"></iframe>
+              </div>
+            </div>
+          HTML
+        )
+      end
+
+      it "image" do
+        click_toggle("image")
+        within "[data-reveal][aria-hidden='false']" do
+          drop_file("city.jpeg", ".dropzone-container .dropzone")
+          fill_in "Alternative text for the image", with: "City landscape"
+
+          find("button.add-file-file").click
+        end
+
+        src = Decidim::EditorImage.last.attached_uploader(:file).path
+        expect_value(
+          <<~HTML
+            <p>Hello, world!</p>
+            <div class="editor-content-image" data-image="">
+              <img src="#{src}" alt="City landscape">
+            </div>
+          HTML
+        )
+      end
+    end
+
+    # it "displays the editor" do
+    #   expect(page).to have_content("FOOBAR")
+    # end
+  end
+
   def expect_value(html)
     expect(input.value).to eq(html.strip.gsub(/\n\s*/, ""))
   end
@@ -692,8 +844,12 @@ describe "Editor", type: :system do
     )
 
     # Wait for the file to be uploaded
-    within "[data-dropzone-items]" do
-      expect(page).to have_content(filename)
+    if redesigned
+      within "[data-dropzone-items]" do
+        expect(page).to have_content(filename)
+      end
+    else
+      expect(page).to have_css("img[alt='Uploaded file']")
     end
   end
 
