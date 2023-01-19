15df1b24316588eb76f99b649dc35ade11cecf58
Francisco Bolívar <francisco.bolivar@nazaries.com>
2023-01-19 19:08:07 +0100

    Add new tests for deleting content blocks and creating multiple with the same manifest on the same page

--- decidim-admin/spec/system/admin_manages_static_page_content_blocks_spec.rb
+++ decidim-admin/spec/system/admin_manages_static_page_content_blocks_spec.rb
@@ -29,6 +29,61 @@ describe "Admin manages static page content blocks", type: :system do
     end
   end
 
+  context "when including multiple blocks with the same manifest" do
+    let(:number_of_content_blocks) { Faker::Number.within(range: 2..5) }
+
+    it "creates all the content blocks" do
+      visit decidim_admin.edit_static_page_path(tos_page)
+      expect do
+        number_of_content_blocks.times do |content|
+          within ".edit_content_blocks" do
+            find("button", text: "Add content block").click
+            within ".add-components" do
+              find("a", text: "Section").click
+            end
+          end
+        end
+      end.to change(Decidim::ContentBlock, :count).by number_of_content_blocks
+    end
+  end
+
+  context "when the page has multiple content blocks with the same manifest" do
+    let(:content1) { Faker::Lorem.sentence }
+    let(:content2) { Faker::Lorem.sentence }
+    let!(:content_block1) { create :content_block, organization:, manifest_name: :section, scope_name: :static_page, scoped_resource_id: tos_page.id, settings: { content_en: content1 } }
+    let!(:content_block2) { create :content_block, organization:, manifest_name: :section, scope_name: :static_page, scoped_resource_id: tos_page.id, settings: { content_en: content2 } }
+
+    it "shows all of them" do
+      visit decidim.page_path(tos_page)
+      expect(page).to have_content(content1)
+      expect(page).to have_content(content2)
+    end
+  end
+
+  context "when deleting content block" do
+    let(:content) { Faker::Lorem.sentence }
+    let!(:content_block) { create :content_block, organization:, manifest_name: :section, scope_name: :static_page, scoped_resource_id: tos_page.id, settings: { content_en: content } }
+
+    it "the content block is no further visible on the page" do
+      visit decidim_admin.edit_static_page_path(tos_page)
+
+      within ".edit_content_blocks" do
+        within first("ul.js-list-actives li") do
+          find(".icon--x").click
+        end
+      end
+
+      within ".confirm-modal-footer" do
+        find("a.button[data-confirm-ok]").click
+      end
+
+      expect(page).to have_content("Content block successfully deleted")
+
+      visit decidim.page_path(tos_page)
+      expect(page).not_to have_content(content)
+    end
+  end
+
   context "when editing a persisted content block" do
     let!(:content_block) { create :content_block, organization:, manifest_name: :summary, scope_name: :static_page, scoped_resource_id: tos_page.id }
 
