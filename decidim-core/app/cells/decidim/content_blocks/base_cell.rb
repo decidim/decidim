182f382c8bd52f1922f1230e8a52c9e138493a82
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-01-19 21:37:13 +0100

    Define a cell to wrap all content blocks content

---
+++ decidim-core/app/cells/decidim/content_blocks/base_cell.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Decidim
+  module ContentBlocks
+    # Base cell to wrap each content block which identifies also the resource
+    # the block belongs to
+    class BaseCell < Decidim::ViewModel
+      def resource
+        @resource ||= base_relation.find(model.scoped_resource_id)
+      end
+
+      private
+
+      def base_relation
+        raise NotImplementedError, "Please, overwrite this method. Inheriting classes should define their own base relation"
+      end
+
+      def section_class
+        return "content-block" if extra_classes.blank?
+
+        "content-block #{extra_classes}"
+      end
+
+      def extra_classes; end
+    end
+  end
+end
