56a01350d2ec1351e005855607e7fa912744783a
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-01-23 13:33:02 +0100

    Register last activity cell in participatory processes

---
+++ decidim-participatory_processes/app/cells/decidim/participatory_processes/content_blocks/last_activity_cell.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Decidim
+  module ParticipatoryProcesses
+    module ContentBlocks
+      class LastActivityCell < Decidim::ContentBlocks::ParticipatorySpaceLastActivityCell
+        include ParticipatorySpaceContentBlocksHelper
+      end
+    end
+  end
+end
