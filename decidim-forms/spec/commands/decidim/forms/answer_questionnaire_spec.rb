5c28bb1124825b7d298b58457987cfb088fcdbfc
Elie Gaboriau <elie@opensourcepolitics.eu>
2023-01-24 16:07:51 +0100

    fix conditionnal questions

--- decidim-forms/spec/commands/decidim/forms/answer_questionnaire_spec.rb
+++ decidim-forms/spec/commands/decidim/forms/answer_questionnaire_spec.rb
@@ -193,6 +193,60 @@ module Decidim
             end
           end
         end
+
+        context "when display_conditions are not mandatory on the same question but are fulfilled" do
+          let(:questionnaire_conditionned) { create(:questionnaire, questionnaire_for: participatory_process) }
+          let!(:option1) { create :answer_option, question: }
+          let!(:option2) { create :answer_option, question: }
+          let!(:option3) { create :answer_option, question: }
+          let!(:question) do
+            create(
+              :questionnaire_question,
+              questionnaire: questionnaire_conditionned,
+              mandatory: false,
+              question_type: "single_option"
+            )
+          end
+          let!(:condition_question) { create(:questionnaire_question, questionnaire: questionnaire_conditionned, question_type: "short_answer") }
+          let!(:display_condition) { create(:display_condition, question:, condition_question:, condition_type: :equal, answer_option: option1) }
+          let!(:display_condition2) { create(:display_condition, question:, condition_question:, condition_type: :equal, answer_option: option3) }
+          let(:form_params) do
+            {
+              "responses" => [
+                {
+                  "choices" => [
+                    { "body" => option1.body, "answer_option_id" => option1.id }
+                  ],
+                  "question_id" => question.id
+                },
+                {
+                  "body" => "answer_test",
+                  "question_id" => condition_question.id
+                }
+              ],
+              "tos_agreement" => "1"
+            }
+          end
+          let(:command) { described_class.new(form, current_user, questionnaire_conditionned) }
+
+          it "broadcasts ok" do
+            expect { command.call }.to broadcast(:ok)
+          end
+
+          it "creates a questionnaire answer for each question answered" do
+            expect do
+              command.call
+            end.to change(Answer, :count).by(2)
+            expect(Answer.all.map(&:questionnaire)).to eq([questionnaire_conditionned, questionnaire_conditionned])
+          end
+
+          it "creates answers with the correct information" do
+            command.call
+
+            expect(Answer.first.choices.first.answer_option).to eq(option1)
+            expect(Answer.second.body).to eq("answer_test")
+          end
+        end
       end
 
       describe "when the user is unregistered" do
