97df14b65f32d02935194c59978cdf0990e9c800
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-01-27 19:21:01 +0100

    Register highlighted_results content block on processes

--- decidim-participatory_processes/lib/decidim/participatory_processes/engine.rb
+++ decidim-participatory_processes/lib/decidim/participatory_processes/engine.rb
@@ -121,6 +121,18 @@ module Decidim
           content_block.default!
         end
 
+        Decidim.content_blocks.register(:participatory_process_homepage, :highlighted_results) do |content_block|
+          content_block.cell = "decidim/accountability/content_blocks/highlighted_results"
+          content_block.settings_form_cell = "decidim/content_blocks/highlighted_elements_for_component_settings_form"
+          content_block.public_name_key = "decidim.accountability.content_blocks.highlighted_results.results"
+          content_block.component_manifest_name = "accountability"
+
+          content_block.settings do |settings|
+            settings.attribute :order, type: :enum, default: "random", choices: %w(random recent)
+            settings.attribute :component_id, type: :select, default: nil
+          end
+        end
+
         Decidim.content_blocks.register(:participatory_process_group_homepage, :metadata) do |content_block|
           content_block.cell = "decidim/participatory_process_groups/content_blocks/metadata"
           content_block.public_name_key = "decidim.participatory_process_groups.content_blocks.metadata.name"
