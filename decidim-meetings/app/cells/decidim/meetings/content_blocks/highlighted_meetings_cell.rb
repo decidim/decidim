1c33f0cb78c54c92b34fd617a601c03b45b44932
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-01-30 18:58:45 +0100

    Integrate meetings content block using highlighted_meetings_for_component cell

--- decidim-meetings/app/cells/decidim/meetings/content_blocks/highlighted_meetings_cell.rb
+++ decidim-meetings/app/cells/decidim/meetings/content_blocks/highlighted_meetings_cell.rb
@@ -3,31 +3,7 @@
 module Decidim
   module Meetings
     module ContentBlocks
-      class HighlightedMeetingsCell < Decidim::ContentBlocks::HighlightedElementsCell
-        def base_relation
-          Decidim::Meetings::Meeting
-            .except_withdrawn
-            .published
-            .not_hidden
-            .upcoming
-            .visible_for(current_user)
-            .where(component: published_components)
-        end
-
-        def elements
-          @elements ||= base_relation.order(start_time: :asc).limit(limit)
-        end
-
-        def geolocation_enabled?
-          Decidim::Map.available?(:geocoding)
-        end
-
-        private
-
-        def limit
-          geolocation_enabled? ? 4 : 8
-        end
-      end
+      class HighlightedMeetingsCell < Decidim::ContentBlocks::HighlightedElementsCell; end
     end
   end
 end
