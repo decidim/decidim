a65f98b05ae7bbc79c85ea6ab0af6e534ca6aa8a
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-01-30 18:47:51 +0100

    Use snippets to include assets in cell

--- decidim-meetings/app/cells/decidim/meetings/highlighted_meetings_for_component_cell.rb
+++ decidim-meetings/app/cells/decidim/meetings/highlighted_meetings_for_component_cell.rb
@@ -8,9 +8,12 @@ module Decidim
     # It is intended to be used in the `participatory_space_highlighted_elements`
     # view hook.
     class HighlightedMeetingsForComponentCell < Decidim::ViewModel
+      include ApplicationHelper
       include Decidim::ComponentPathHelper
       include Decidim::CardHelper
       include Decidim::LayoutHelper
+
+      delegate :snippets, to: :controller
 
       def show
         render unless meetings_count.zero?
