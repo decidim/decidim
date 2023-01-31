0849799ea91f33ede0b935f769caa955cae24df6
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-01-31 22:37:18 +0100

    Define content block to display documents in participatory spaces and define cell particularized to processes

---
+++ decidim-core/app/cells/decidim/content_blocks/participatory_space_documents_cell.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Decidim
+  module ContentBlocks
+    class ParticipatorySpaceDocumentsCell < BaseCell; end
+  end
+end
---
+++ decidim-participatory_processes/app/cells/decidim/participatory_processes/content_blocks/documents_cell.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Decidim
+  module ParticipatoryProcesses
+    module ContentBlocks
+      class DocumentsCell < Decidim::ContentBlocks::ParticipatorySpaceDocumentsCell
+        include ParticipatorySpaceContentBlocksHelper
+      end
+    end
+  end
+end
