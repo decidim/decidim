da57dc988c1d84c11ef1020bc6af6f40e6725ba0
Hugoren Martinako <aumpfbahn@gmail.com>
2023-02-06 16:50:37 +0100

    fix icon anchor position

--- decidim-core/app/packs/src/decidim/map/controller.js
+++ decidim-core/app/packs/src/decidim/map/controller.js
@@ -57,12 +57,12 @@ export default class MapController {
   }
 
   createIcon() {
+    const size = 36
     return L.divIcon({
       html: `
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em"><path fill="none" d="M0 0h24v24H0z"/><path fill="currentColor" d="M18.364 17.364L12 23.728l-6.364-6.364a9 9 0 1 1 12.728 0zM12 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm0-2a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/></svg>`,
-      iconSize: [-1, -1],
-      // for thex-axis: half of the displayed size
-      iconAnchor: [18, 0]
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="${size}px" height="${size}px"><path fill="none" d="M0 0h24v24H0z"/><path fill="currentColor" d="M18.364 17.364L12 23.728l-6.364-6.364a9 9 0 1 1 12.728 0zM12 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm0-2a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/></svg>`,
+      iconAnchor: [0.5 * size, size],
+      popupAnchor: [0, -0.5 * size]
     });
   }
 
