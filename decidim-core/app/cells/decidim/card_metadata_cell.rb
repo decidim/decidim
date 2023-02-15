c192aaeb6ceab4543b96c64aecc8c9eb29ce75a7
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-02-15 10:52:11 +0100

    Move dates and duration item to generic metadata cell

--- decidim-core/app/cells/decidim/card_metadata_cell.rb
+++ decidim-core/app/cells/decidim/card_metadata_cell.rb
@@ -88,6 +88,31 @@ module Decidim
       }
     end
 
+    def dates_item
+      return if dates_blank?
+
+      format = [start_date.year, end_date.year].any? { |year| year != Date.current.year } ? :decidim_short_with_month_name_short : :decidim_with_month_name_short
+      text = if start_date.to_date == end_date.to_date
+               "#{l(start_date.to_date, format:)} #{l(start_date, format: :time_of_day)} - #{l(end_date, format: :time_of_day)}"
+             else
+               "#{l(start_date.to_date, format:)} - #{l(end_date.to_date, format:)}"
+             end
+
+      {
+        text:,
+        icon: "timer-2-line"
+      }
+    end
+
+    def duration_item
+      return if dates_blank?
+
+      {
+        text: distance_of_time_in_words(start_date, end_date, scope: "datetime.distance_in_words.short"),
+        icon: "time-line"
+      }
+    end
+
     def enable_links?
       return true unless options.has_key?(:links)
 
--- decidim-meetings/app/cells/decidim/meetings/meeting_card_metadata_cell.rb
+++ decidim-meetings/app/cells/decidim/meetings/meeting_card_metadata_cell.rb
@@ -11,6 +11,9 @@ module Decidim
 
       delegate :type_of_meeting, :start_time, :end_time, :category, :withdrawn?, to: :meeting
 
+      alias start_date start_time
+      alias end_date end_time
+
       def initialize(*)
         super
 
@@ -20,11 +23,11 @@ module Decidim
       private
 
       def meeting_items
-        [type, category_item, duration, comments_count_item, official, withdrawn_item]
+        [type, category_item, duration_item, comments_count_item, official, withdrawn_item]
       end
 
       def meeting_items_for_map
-        [dates, type, official].compact_blank.map do |item|
+        [dates_item, type, official].compact_blank.map do |item|
           {
             text: item[:text],
             icon: icon(item[:icon]).html_safe
@@ -32,34 +35,10 @@ module Decidim
         end
       end
 
-      def dates
-        text = if start_time.year != end_time.year
-                 "#{l(start_time.to_date, format: :decidim_short_with_month_name_short)} - #{l(end_time.to_date, format: :decidim_short_with_month_name_short)}"
-               elsif start_time.month != end_time.month || start_time.day != end_time.day
-                 "#{l(start_time.to_date, format: :decidim_with_month_name_short)} - #{l(end_time.to_date, format: :decidim_with_month_name_short)}"
-               else
-                 "#{l(start_time, format: :time_of_day)} - #{l(end_time, format: :time_of_day)}"
-               end
-
-        {
-          text:,
-          icon: "timer-2-line"
-        }
-      end
-
       def type
         {
           text: t(type_of_meeting, scope: "decidim.meetings.meetings.filters.type_values"),
           icon: resource_type_icon_key(type_of_meeting)
-        }
-      end
-
-      def duration
-        return if [start_time, end_time].any?(&:blank?)
-
-        {
-          text: distance_of_time_in_words(start_time, end_time, scope: "datetime.distance_in_words.short"),
-          icon: "time-line"
         }
       end
 
