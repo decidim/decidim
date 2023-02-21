5a3c356eb81b463b7f8f68898f033581d002c453
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-02-21 21:35:52 +0100

    Add condition to display modal preventing vote because limit exceded

--- decidim-budgets/app/cells/decidim/budgets/project_vote_button_cell.rb
+++ decidim-budgets/app/cells/decidim/budgets/project_vote_button_cell.rb
@@ -5,6 +5,19 @@ module Decidim
     # This cell renders an authorized_action button
     # to vote a given instance of a Project in a budget list
     class ProjectVoteButtonCell < ProjectListItemCell
+      def modal_params
+        return {} if resource_added? || below_maximum?
+
+        { dialog_open: "budget-excess" }
+      end
+
+      def remaining_amount
+        model.budget.total_budget - (current_order&.total_budget || 0)
+      end
+
+      def below_maximum?
+        model.budget_amount <= remaining_amount
+      end
     end
   end
 end
