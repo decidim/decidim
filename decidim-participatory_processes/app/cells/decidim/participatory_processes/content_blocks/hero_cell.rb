87da4d9a1149f1002a4419f79c86c9c7f8584c69
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-02-23 17:06:16 +0100

    Add CTA button to hero content block if fully configured in active step of process

--- decidim-participatory_processes/app/cells/decidim/participatory_processes/content_blocks/hero_cell.rb
+++ decidim-participatory_processes/app/cells/decidim/participatory_processes/content_blocks/hero_cell.rb
@@ -4,11 +4,15 @@ module Decidim
   module ParticipatoryProcesses
     module ContentBlocks
       class HeroCell < Decidim::ContentBlocks::BaseCell
+        include Decidim::ApplicationHelper
         include Decidim::SanitizeHelper
         include Decidim::TranslationsHelper
         include Decidim::TwitterSearchHelper
+        include ParticipatorySpaceContentBlocksHelper
 
-        delegate :title, :subtitle, :attached_uploader, :hashtag, to: :resource
+        attr_reader :cta_text, :cta_path
+
+        delegate :title, :subtitle, :attached_uploader, :hashtag, :active_step, to: :resource
 
         def title_text
           translated_attribute(title)
@@ -24,6 +28,15 @@ module Decidim
 
         def has_hashtag?
           @has_hashtag ||= hashtag.present?
+        end
+
+        def has_cta?
+          return if active_step.blank?
+
+          @cta_text ||= translated_attribute(active_step.cta_text).presence
+          @cta_path ||= active_step.cta_path.presence && step_cta_url(resource)
+
+          [cta_text, cta_path].all?
         end
 
         def escaped_hashtag
