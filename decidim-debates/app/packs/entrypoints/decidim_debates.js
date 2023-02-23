1f633f7762358de54b9d287eba324c8ebc366f31
Jorge <jorgeatgu@gmail.com>
2023-02-23 16:35:43 +0100

    remove debates.scss

--- decidim-debates/app/packs/entrypoints/decidim_debates.js
+++ decidim-debates/app/packs/entrypoints/decidim_debates.js
@@ -1,4 +1,2 @@
 // Images
 require.context("../images", true)
-
-import "stylesheets/debates.scss"
--- decidim-debates/app/packs/entrypoints/decidim_debates_admin.js
+++ decidim-debates/app/packs/entrypoints/decidim_debates_admin.js
@@ -2,4 +2,3 @@ import "src/decidim/debates/admin/debates"
 
 // Images
 require.context("../images", true)
-import "stylesheets/debates.scss"
