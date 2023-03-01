6c9c5b1e11c8432b44323ef3464218b3a8240099
Jorge <jorgeatgu@gmail.com>
2023-03-01 13:41:10 +0100

    add orderable to budget_controller

--- decidim-budgets/app/controllers/decidim/budgets/budgets_controller.rb
+++ decidim-budgets/app/controllers/decidim/budgets/budgets_controller.rb
@@ -4,6 +4,7 @@ module Decidim
   module Budgets
     # Exposes the budget resources so users can participate on them
     class BudgetsController < Decidim::Budgets::ApplicationController
+      include Decidim::Budgets::Orderable
       def index
         redirect_to budget_projects_path(current_workflow.single) if current_workflow.single?
       end
