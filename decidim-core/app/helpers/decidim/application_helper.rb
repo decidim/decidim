88b665234bfe0522e94c71691dd3c68fca0e4ddb
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-03-01 23:36:22 +0200

    Patch rails cells to use the rails view context as a reference

--- decidim-core/app/helpers/decidim/application_helper.rb
+++ decidim-core/app/helpers/decidim/application_helper.rb
@@ -99,8 +99,7 @@ module Decidim
     #
     # Renders the cell contents.
     def cell(name, model, options = {}, &)
-      options = { context: { current_user: } }.deep_merge(options)
-
+      options = { context: { view_context: self, current_user: } }.deep_merge(options)
       super(redesigned_cell_name(name), model, options, &)
     end
 
