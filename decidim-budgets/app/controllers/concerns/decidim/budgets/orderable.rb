d0a05730efd9b37e4687e2156b35cb6277443653
Jorge <jorgeatgu@gmail.com>
2023-03-01 11:14:17 +0100

    order projects

--- decidim-budgets/app/controllers/concerns/decidim/budgets/orderable.rb
+++ decidim-budgets/app/controllers/concerns/decidim/budgets/orderable.rb
@@ -18,8 +18,7 @@ module Decidim
           @available_orders ||= begin
             available_orders = []
             available_orders << "random" if voting_open? || !votes_are_visible?
-            available_orders << "most_voted" if votes_are_visible?
-            available_orders += %w(highest_cost lowest_cost)
+            available_orders += %w(highest_cost)
             available_orders
           end
         end
@@ -36,15 +35,6 @@ module Decidim
           case order
           when "highest_cost"
             projects.order(budget_amount: :desc)
-          when "lowest_cost"
-            projects.order(budget_amount: :asc)
-          when "most_voted"
-            if votes_are_visible?
-              ids = projects.sort_by(&:confirmed_orders_count).map(&:id).reverse
-              projects.ordered_ids(ids)
-            else
-              projects
-            end
           when "random"
             projects.order_randomly(random_seed)
           else
