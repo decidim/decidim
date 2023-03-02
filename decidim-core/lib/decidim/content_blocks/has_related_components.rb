9e5bcd0a304db273795c7bc74eae05461f8b2341
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-03-02 14:06:36 +0100

    Use associations configuration of content blocks and manifest to not call models directly in has_related_components concern

--- decidim-core/lib/decidim/content_blocks/has_related_components.rb
+++ decidim-core/lib/decidim/content_blocks/has_related_components.rb
@@ -10,12 +10,16 @@ module Decidim
           return if content_block.blank?
 
           manifest_name = content_block.component_manifest_name
-          case content_block.scope_name
-          when "participatory_process_group_homepage"
-            participatory_space = Decidim::ParticipatoryProcessGroup.find(content_block.scoped_resource_id).participatory_processes
+          scope_class = Decidim::ContentBlocks::BaseCell::SCOPE_ASSOCIATIONS[content_block.scope_name]&.safe_constantize
+
+          participatory_space = if scope_class.respond_to?(:participatory_space_manifest)
+                                  scope_class.participatory_space_manifest.participatory_spaces.call(content_block.organization).find(content_block.scoped_resource_id)
+                                elsif scope_class.respond_to?(:participatory_spaces)
+                                  scope_class.participatory_spaces(content_block.scoped_resource_id)
+                                end
+
+          if participatory_space.present?
             Decidim::Component.where(participatory_space:, manifest_name:)
-          when "participatory_process_homepage"
-            Decidim::ParticipatoryProcess.find(content_block.scoped_resource_id).components.where(manifest_name:)
           else
             Decidim::PublicComponents.for(content_block.organization, manifest_name:)
           end
