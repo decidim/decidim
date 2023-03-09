1b6ab031efbea241ffdff8bc30bdd1fdc61ba717
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-03-09 21:10:30 +0100

    Make nil presenter return false to methods ending with "?"
    In this way the presenter returns false to methods like has_tooltip?,
    can_be_contacted? or can_follow?

--- decidim-core/app/presenters/decidim/nil_presenter.rb
+++ decidim-core/app/presenters/decidim/nil_presenter.rb
@@ -22,7 +22,9 @@ module Decidim
       true
     end
 
-    def method_missing(_method, *_args)
+    def method_missing(method, *_args)
+      return false if method.end_with?("?")
+
       ""
     end
   end
