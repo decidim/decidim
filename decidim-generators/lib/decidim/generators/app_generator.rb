68c62f1fc5ff7ddcdfb57205495a0992cadb1e40
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-03-10 11:08:02 +0200

    Fix generator

--- decidim-generators/lib/decidim/generators/app_generator.rb
+++ decidim-generators/lib/decidim/generators/app_generator.rb
@@ -93,8 +93,10 @@ module Decidim
                              default: false,
                              desc: "Don't add Puma development SSL configuration options"
 
-      # we disable the webpacker instalation as we will use shakapacker
-      def webpacker_gemfile_entry; end
+      # we disable the webpacker installation as we will use shakapacker
+      def webpacker_gemfile_entry
+        []
+      end
 
       def database_yml
         template "database.yml.erb", "config/database.yml", force: true
