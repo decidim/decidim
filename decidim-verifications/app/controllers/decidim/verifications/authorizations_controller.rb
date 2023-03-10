ecd7f11640e6920f087d4438d9f52f3aa6dd7979
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-03-10 19:24:44 +0200

    Standardize the format of the words "should not"

--- decidim-verifications/app/controllers/decidim/verifications/authorizations_controller.rb
+++ decidim-verifications/app/controllers/decidim/verifications/authorizations_controller.rb
@@ -3,7 +3,7 @@
 module Decidim
   module Verifications
     # This controller allows users to create and destroy their authorizations. It
-    # shouldn't be necessary to expand it to add new authorization schemes.
+    # should not be necessary to expand it to add new authorization schemes.
     class AuthorizationsController < Verifications::ApplicationController
       helper_method :handler, :unauthorized_methods, :authorization_method, :authorization
       before_action :valid_handler, only: [:new, :create]
