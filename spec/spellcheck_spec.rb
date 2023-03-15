d45b3800a858a8488d8cc50bea17acf955bd390f
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-03-15 14:16:09 +0200

    Refactor the spec for the spell check script
    - Extract the correct invalid lines for each check
    - Add testing also for matching the correct locale files and
      excluding other locales than `en.yml`

--- spec/spellcheck_spec.rb
+++ spec/spellcheck_spec.rb
@@ -13,11 +13,12 @@ describe "Spellcheck utility" do
   let(:config) { YAML.load_file("#{working_dir}/.spelling.yml") }
   let(:script) { "#{working_dir}/.github/run_spelling_check.sh" }
 
-  let(:test_directory) { "/tmp/spellcheck-utility-test-#{rand(1_000)}" }
+  let(:test_directory) { "/tmp/decidim-spellcheck-utility-test-#{rand(1_000)}" }
   let(:working_dir) { File.expand_path("..", __dir__) }
 
   before do
     FileUtils.mkdir_p(test_directory)
+    FileUtils.mkdir_p("#{test_directory}/decidim-test/config/locales")
     FileUtils.mkdir_p("#{test_directory}/decidim-test/lib")
     FileUtils.mkdir_p("#{test_directory}/docs")
     FileUtils.mkdir_p("#{test_directory}/lib")
@@ -52,9 +53,16 @@ describe "Spellcheck utility" do
         #{config["forbidden"].keys.map { |k| "#{k.capitalize} starts the string." }.join("\n")}
       DOC
     end
+    let(:invalid_locale) do
+      <<~LOCALE
+        en:
+          hello: Isn't it nice this file doesn't follow the rules?
+      LOCALE
+    end
 
     before do
       File.write("#{test_directory}/decidim-test/lib/invalid.rb", invalid_code)
+      File.write("#{test_directory}/decidim-test/config/locales/en.yml", invalid_locale)
       File.write("#{test_directory}/docs/documentation.md", invalid_doc)
       File.write("#{test_directory}/lib/invalid.rb", invalid_code) # This should be ignored
     end
@@ -63,21 +71,31 @@ describe "Spellcheck utility" do
       expect(exitstatus).to be(1)
 
       start_lines = [5, 5 + config["forbidden"].length]
+      invalid_code_lines = output_lines[0..(2 * config["forbidden"].length)]
       config["forbidden"].each_with_index do |(word, preferred), idx|
-        expect(output_lines[idx]).to eq(
+        expect(invalid_code_lines[idx]).to eq(
           %(::error file=decidim-test/lib/invalid.rb,line=#{start_lines[0] + idx},col=27,endColumn=#{27 + word.length}::Use "#{preferred}" instead of "#{word}".)
         )
-        expect(output_lines[idx + config["forbidden"].length]).to eq(
+        expect(invalid_code_lines[idx + config["forbidden"].length]).to eq(
           %(::error file=decidim-test/lib/invalid.rb,line=#{start_lines[1] + idx},col=5,endColumn=#{5 + word.length}::Use "#{preferred}" instead of "#{word}".)
         )
       end
 
+      invalid_locale_lines = output_lines[(2 * config["forbidden"].length)..((2 * config["forbidden"].length) + 2)]
+      expect(invalid_locale_lines[0]).to eq(
+        %(::error file=decidim-test/config/locales/en.yml,line=2,col=10,endColumn=15::Use "is not" instead of "isn't".)
+      )
+      expect(invalid_locale_lines[1]).to eq(
+        %(::error file=decidim-test/config/locales/en.yml,line=2,col=34,endColumn=41::Use "does not" instead of "doesn't".)
+      )
+
       start_lines = [5, 6 + config["forbidden"].length]
+      invalid_doc_lines = output_lines[((2 * config["forbidden"].length) + 2)..((4 * config["forbidden"].length) + 2)]
       config["forbidden"].each_with_index do |(word, preferred), idx|
-        expect(output_lines[idx + (2 * config["forbidden"].length)]).to eq(
+        expect(invalid_doc_lines[idx]).to eq(
           %(::error file=docs/documentation.md,line=#{start_lines[0] + idx},col=20,endColumn=#{20 + word.length}::Use "#{preferred}" instead of "#{word}".)
         )
-        expect(output_lines[idx + (3 * config["forbidden"].length)]).to eq(
+        expect(invalid_doc_lines[idx + config["forbidden"].length]).to eq(
           %(::error file=docs/documentation.md,line=#{start_lines[1] + idx},col=1,endColumn=#{1 + word.length}::Use "#{preferred}" instead of "#{word}".)
         )
       end
@@ -103,9 +121,16 @@ describe "Spellcheck utility" do
         Strings containing parts of forbidden strings such as "parent" should be ignored.
       DOC
     end
+    let(:invalid_locale) do
+      <<~LOCALE
+        fi:
+          hello: Isn't it nice this file doesn't follow the rules?
+      LOCALE
+    end
 
     before do
       File.write("#{test_directory}/decidim-test/lib/correct.rb", code)
+      File.write("#{test_directory}/decidim-test/config/locales/fi.yml", invalid_locale)
       File.write("#{test_directory}/docs/documentation.md", doc)
     end
 
