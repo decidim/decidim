f38245f7df89605634c41df038fb4273ef202957
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-03-15 09:41:36 +0200

    Include the docs folder for the grep

--- .github/run_spelling_check.sh
+++ .github/run_spelling_check.sh
@@ -50,6 +50,6 @@ while read match; do
       echo "::error file=${file},line=${line},col=${spos},endColumn=${epos}::Use \"$preferred\" instead of \"$word\"."
     done < <(echo "$text" | grep -owib "$word")
   done
-done < <(find decidim-* -type f | grep -vP "$exclude_paths_pattern" | xargs -n1000 grep -Hnwif $forbidden_words_file)
+done < <(find decidim-* docs/ -type f | grep -vP "$exclude_paths_pattern" | xargs -n1000 grep -Hnwif $forbidden_words_file)
 
 exit $status
