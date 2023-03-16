f1454ec64b59479a65c3ebcad83348b2927c06f7
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-03-16 14:47:52 +0200

    Apply shellcheck

--- .github/run_spelling_check.sh
+++ .github/run_spelling_check.sh
@@ -11,45 +11,47 @@ fi
 CONFIG_FILE=".spelling.yml"
 
 declare -A forbidden_words=()
-while read forbidden; do
-  word=$(echo $forbidden | cut -d'=' -f 1)
-  preferred=$(echo $forbidden | cut -d'=' -f 2-)
+# shellcheck disable=SC2002
+while read -r forbidden; do
+  word=$(echo "$forbidden" | cut -d'=' -f 1)
+  preferred=$(echo "$forbidden" | cut -d'=' -f 2-)
   forbidden_words["$word"]="$preferred"
 done < <(cat "$CONFIG_FILE" | yq '.forbidden | to_entries | map([.key, .value] | join("=")) | .[]')
 
 forbidden_words_file=$(mktemp)
 trap 'rm -f $forbidden_words_file' EXIT
 
-printf "%s\n" "${!forbidden_words[@]}" | sort > $forbidden_words_file
+printf "%s\n" "${!forbidden_words[@]}" | sort > "$forbidden_words_file"
 
-exclude_paths=($(cat "$CONFIG_FILE" | yq '.exclude_paths[]'))
+# shellcheck disable=SC2002
+mapfile -t exclude_paths < <(cat "$CONFIG_FILE" | yq '.exclude_paths[]')
 exclude_paths_pattern=$(printf "|(%s)" "${exclude_paths[@]}" | cut -c 2-)
 
 # Perform grep and iterate through all matches
 status=0
 
-while read match; do
+while read -r match; do
   status=1
 
   # Cut the relevant parts of the match
-  file=$(echo $match | cut -d':' -f 1)
-  line=$(echo $match | cut -d':' -f 2)
+  file=$(echo "$match" | cut -d':' -f 1)
+  line=$(echo "$match" | cut -d':' -f 2)
   text=$(sed "${line}!d" "$file")
 
   # Find the forbidden words
   for word in "${!forbidden_words[@]}"; do
-    len=$(expr length "$word")
+    len=${#word}
     preferred=${forbidden_words[$word]}
 
-    while read posmatch; do
-      spos=$(( $(echo $posmatch | cut -d':' -f 1) + 1 ))
-      epos=$(( $spos + $len ))
+    while read -r posmatch; do
+      spos=$(( $(echo "$posmatch" | cut -d':' -f 1) + 1 ))
+      epos=$(( spos + len ))
 
       # Print out the annotation messages
       # https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-error-message
       echo "::error file=${file},line=${line},col=${spos},endColumn=${epos}::Use \"$preferred\" instead of \"$word\"."
     done < <(echo "$text" | grep -owib "$word")
   done
-done < <(find decidim-* docs/ -type f | grep -vP "$exclude_paths_pattern" | xargs -n1000 grep -Hnwif $forbidden_words_file)
+done < <(find decidim-* docs/ -type f | grep -vP "$exclude_paths_pattern" | xargs -n1000 grep -Hnwif "$forbidden_words_file")
 
 exit $status
