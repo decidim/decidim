dc3e04e064f117ad8e9eb848b6612feb9805e476
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-03-16 17:21:49 +0200

    Fix more specs broken due to the changes in the locales

--- decidim-proposals/spec/controllers/decidim/proposals/proposals_controller_spec.rb
+++ decidim-proposals/spec/controllers/decidim/proposals/proposals_controller_spec.rb
@@ -270,7 +270,7 @@ module Decidim
 
               put :withdraw, params: params.merge(id: proposal.id)
 
-              expect(flash[:alert]).to eq("You are not authorized to perform this action")
+              expect(flash[:alert]).to eq("You are not authorized to perform this action.")
               expect(response).to have_http_status(:found)
               proposal.reload
               expect(proposal.withdrawn?).to be false
