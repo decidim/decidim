9e6c5efead87dd268ff7231d424e58e80577b623
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-03-20 16:49:57 +0200

    Refactor Admin Users in Participatory spaces (#10524)
    * Refactor Admin Users in Participatory spaces
    
    * Update decidim-core/lib/decidim/participatory_space_user.rb
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    * Update decidim-admin/app/forms/decidim/admin/participatory_space_admin_user_form.rb
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    * Update decidim-conferences/app/forms/decidim/conferences/admin/conference_user_role_form.rb
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    * Apply review recommendation
    
    * Lint
    
    ---------
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-participatory_processes/app/forms/decidim/participatory_processes/admin/participatory_process_user_role_form.rb
+++ decidim-participatory_processes/app/forms/decidim/participatory_processes/admin/participatory_process_user_role_form.rb
@@ -6,26 +6,10 @@ module Decidim
       # A form object used to create participatory process user roles from the
       # admin dashboard.
       #
-      class ParticipatoryProcessUserRoleForm < Form
+      class ParticipatoryProcessUserRoleForm < Decidim::Admin::ParticipatorySpaceAdminUserForm
         mimic :participatory_process_user_role
 
-        attribute :name, String
-        attribute :email, String
-        attribute :role, String
-
-        validates :name, :email, :role, presence: true
-        validates :role, inclusion: { in: Decidim::ParticipatoryProcessUserRole::ROLES }
-
-        validates :name, format: { with: UserBaseEntity::REGEXP_NAME }
-
-        def roles
-          Decidim::ParticipatoryProcessUserRole::ROLES.map do |role|
-            [
-              I18n.t(role, scope: "decidim.admin.models.participatory_process_user_role.roles"),
-              role
-            ]
-          end
-        end
+        def scope = "decidim.admin.models.participatory_process_user_role.roles"
       end
     end
   end
