4d7d9b5eb630b0f479d714825519a78c02b6f497
Fernando Blat <fernando@blat.es>
2023-03-22 06:46:42 +0100

    Restore specs

--- decidim-comments/spec/cells/decidim/comments/comment_thread_cell_spec.rb
+++ decidim-comments/spec/cells/decidim/comments/comment_thread_cell_spec.rb
@@ -19,6 +19,30 @@ module Decidim::Comments
       it "renders the thread" do
         expect(subject).to have_css(".comment-thread")
         expect(subject).to have_content(comment.body.values.first)
+        expect(subject).not_to have_css(".comment-reply")
+      end
+
+      context "with replies" do
+        let(:resource_locator) { Decidim::ResourceLocatorPresenter.new(commentable) }
+        let!(:replies) { create_list(:comment, 10, commentable: comment) }
+
+        before do
+          allow(Decidim::ResourceLocatorPresenter).to receive(:new).and_return(resource_locator)
+          allow(resource_locator).to receive(:path).and_return("/dummies")
+        end
+
+        it "renders the reply" do
+          expect(subject).to have_css(".comment-reply .comment", count: 10)
+        end
+
+        context "with a deleted user" do
+          let(:user) { create(:user, :deleted, organization: component.organization) }
+          let(:comment) { create(:comment, commentable:, author: user) }
+
+          it "renders the user as deleted" do
+            expect(subject).to have_css(".comment__header", text: "Participant deleted")
+          end
+        end
       end
     end
   end
