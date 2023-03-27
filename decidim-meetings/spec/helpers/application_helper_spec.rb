34fe83032ad1cee8ad1bd83df444d9fa66562afb
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-03-27 15:55:35 +0300

    Fix the application helper specs

--- decidim-meetings/spec/helpers/application_helper_spec.rb
+++ decidim-meetings/spec/helpers/application_helper_spec.rb
@@ -60,12 +60,14 @@ module Decidim
             it "renders the image and iframe embed" do
               expect(subject).to eq(
                 <<~HTML.strip
-                  <div class="ql-editor-display"><p><img src="/path/to/image.jpg" alt="Image"></p>
+                  <div class="rich-text-display">
+                  <p><img src="/path/to/image.jpg" alt="Image"></p>
                   <div class="editor-content-videoEmbed">
                     <div>
                       <div class="disabled-iframe"><!-- <iframe src="https://example.org/video/xyz" title="Video" frameborder="0" allowfullscreen="true"></iframe> --></div>
                     </div>
-                  </div></div>
+                  </div>
+                  </div>
                 HTML
               )
             end
