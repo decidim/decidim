e0c6e506a4dca24e2b0252be0cf716d93eaf586b
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-03-28 15:44:32 +0200

    Remove deprecated code

--- decidim-core/app/cells/decidim/members_cell.rb
+++ decidim-core/app/cells/decidim/members_cell.rb
@@ -13,13 +13,6 @@ module Decidim
       render :show
     end
 
-    # deprecated
-    def membership_cell_name
-      return "decidim/user_group_admin_membership_profile" if options[:from_admin].presence
-
-      "decidim/user_group_membership_profile"
-    end
-
     def memberships
       @memberships ||= case role
                        when "member"
