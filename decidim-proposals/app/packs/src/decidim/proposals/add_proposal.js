e22d5323d572de9392602a8cbfb61e345a34e3e3
Hugoren Martinako <aumpfbahn@gmail.com>
2023-03-31 17:52:40 +0200

    update proposals w/ geocoding (possible deprecated)

--- decidim-proposals/app/packs/src/decidim/proposals/add_proposal.js
+++ decidim-proposals/app/packs/src/decidim/proposals/add_proposal.js
@@ -2,10 +2,12 @@ import attachGeocoding from "src/decidim/geocoding/attach_input"
 import getCoordinateInputName from "src/decidim/geocoding/coordinate_input";
 
 $(() => {
-  const $checkbox = $("input:checkbox[name$='[has_address]']");
-  const $addressInput = $("#address_input");
-  const $addressInputField = $("input", $addressInput);
   const $map = $("#address_map");
+  const $addressInputField = $("[data-decidim-geocoding]");
+
+  $map.hide();
+  $addressInputField.on("geocoder-suggest-coordinates.decidim", () => $map.show());
+
   let latFieldName = "latitude";
   let longFieldName = "longitude";
 
@@ -14,42 +16,19 @@ $(() => {
     longFieldName = getCoordinateInputName("longitude", $addressInputField, {})
   }
 
-  $map.hide();
+  const ctrl = $("[data-decidim-map]").data("map-controller");
+  ctrl.setEventHandler("coordinates", (ev) => {
+    $(`input[name='${latFieldName}']`).val(ev.lat);
+    $(`input[name='${longFieldName}']`).val(ev.lng);
+  });
 
-  if ($checkbox.length > 0) {
-    const toggleInput = () => {
-      if ($checkbox[0].checked) {
-        $addressInput.show();
-        $addressInputField.prop("disabled", false);
-      } else {
-        $addressInput.hide();
-        $addressInputField.prop("disabled", true);
-      }
-    }
-    toggleInput();
-    $checkbox.on("change", toggleInput);
-  }
-
-  if ($addressInput.length > 0) {
-    if ($checkbox[0].checked) {
-      $map.show();
-    }
-
-    const ctrl = $("[data-decidim-map]").data("map-controller");
-    ctrl.setEventHandler("coordinates", (ev) => {
-      $(`input[name='${latFieldName}']`).val(ev.lat);
-      $(`input[name='${longFieldName}']`).val(ev.lng);
+  attachGeocoding($addressInputField, null, (coordinates) => {
+    // Remove previous marker when user updates address in address field
+    ctrl.removeMarker();
+    ctrl.addMarker({
+      latitude: coordinates[0],
+      longitude: coordinates[1],
+      address: $addressInputField.val()
     });
-
-    attachGeocoding($addressInputField, null, (coordinates) => {
-      $map.show();
-      // Remove previous marker when user updates address in address field
-      ctrl.removeMarker();
-      ctrl.addMarker({
-        latitude: coordinates[0],
-        longitude: coordinates[1],
-        address: $addressInput.val()
-      });
-    });
-  }
+  });
 });
