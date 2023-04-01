de9da2beed20af8ecfc86cfb98483c4c35e54283
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-04-01 23:19:53 +0300

    Add specs for hide content models

--- decidim-core/app/packs/src/decidim/change_report_form_behavior.js
+++ decidim-core/app/packs/src/decidim/change_report_form_behavior.js
@@ -2,6 +2,10 @@
  * These set of functions aims to change the behavior of the report modal forms
  * so that when checking various checkboxes, to change the label of the button
  * to either report or hide.
+ * Given the behavior is similar for report content and report user, we have the
+ * functionality grouped in one single function.
+ * It's important to note that the report user modal has a checkbox that allows
+ * the admin to block the user in the report user modal.
  */
 
 /**
