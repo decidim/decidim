b0389c0c0023bfbed458bb92fe3203e8d5d6c4b3
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-04-03 11:44:35 +0300

    Change I18n captions on moderation module (#10663)
    * Change I18n captions on moderation module
    
    * Implement review recommendations

--- decidim-admin/spec/system/space_admin_manages_global_moderations_spec.rb
+++ decidim-admin/spec/system/space_admin_manages_global_moderations_spec.rb
@@ -63,7 +63,7 @@ describe "Space admin manages global moderations", type: :system do
       visit decidim_admin.moderations_path
 
       within "body", wait: 2 do
-        expect(page).to have_content("Moderations")
+        expect(page).to have_content("Reported content")
         expect(page).to have_link("Visit URL")
 
         find_link("Visit URL").hover
@@ -94,7 +94,7 @@ describe "Space admin manages global moderations", type: :system do
       visit decidim_admin.moderations_path
 
       within ".container" do
-        expect(page).to have_content("Moderations")
+        expect(page).to have_content("Reported content")
 
         expect(page).to have_no_selector("table.table-list tbody tr")
       end
