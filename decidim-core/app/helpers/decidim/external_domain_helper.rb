7f557097339bc4304175afc8269b2ea2a83338c4
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-04-05 11:15:07 +0300

    Improve the link handling
    * Improve the link handling
    
    * Improve further the link handling
    
    * More iterations
    
    * Another iteration
    
    * Enforce http or https protocols
    
    * Temporary commit
    
    * Running linters
    
    * Refactor the links controller
    
    * Apply review recommendations
    
    * Add handling for invalid url
    
    * Further improve the controller

--- decidim-core/app/helpers/decidim/external_domain_helper.rb
+++ decidim-core/app/helpers/decidim/external_domain_helper.rb
@@ -3,10 +3,21 @@
 module Decidim
   module ExternalDomainHelper
     def highlight_domain
+      highlighted_domain = [
+        external_url.host,
+        (external_url.port && [80, 443].include?(external_url.port) ? "" : ":#{external_url.port}")
+      ].join
+
+      path = [
+        external_url.path,
+        (external_url.query ? "?#{external_url.query}" : ""),
+        (external_url.fragment ? "##{external_url.fragment}" : "")
+      ].join
+
       tag.div do
-        content_tag(:span, "#{@url_parts[:protocol]}//") +
-          content_tag(:span, @url_parts[:domain], class: "alert") +
-          content_tag(:span, @url_parts[:path])
+        content_tag(:span, "#{external_url.scheme}://") +
+          content_tag(:span, highlighted_domain, class: "text-alert") +
+          content_tag(:span, path)
       end
     end
   end
