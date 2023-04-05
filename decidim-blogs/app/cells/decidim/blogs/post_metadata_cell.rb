888f6681ac94f213a3d333cd3f51a7527f079c17
Hugoren Martinako <aumpfbahn@gmail.com>
2023-04-05 14:06:44 +0200

    include date in metadata

--- decidim-blogs/app/cells/decidim/blogs/post_metadata_cell.rb
+++ decidim-blogs/app/cells/decidim/blogs/post_metadata_cell.rb
@@ -4,8 +4,6 @@ module Decidim
   module Blogs
     # This cell renders metadata for an instance of a Proposal
     class PostMetadataCell < Decidim::CardMetadataCell
-      delegate :state, to: :model
-
       def initialize(*)
         super
 
@@ -15,7 +13,16 @@ module Decidim
       private
 
       def post_items
-        [author_item, comments_count_item, endorsements_count_item]
+        [author_item, creation_date, comments_count_item, endorsements_count_item]
+      end
+
+      def creation_date
+        date_at = model.try(:published_at) || model.try(:created_at)
+
+        {
+          text: l(date_at.to_date, format: :decidim_short_with_month_name_short),
+          icon: "calendar-todo-line"
+        }
       end
     end
   end
