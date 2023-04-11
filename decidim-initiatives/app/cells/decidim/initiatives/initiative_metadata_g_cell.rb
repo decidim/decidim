8f0eb6d2465497cd3fca1bc3f496b314b3549653
Jorge <jorgeatgu@gmail.com>
2023-04-11 14:43:47 +0200

    initiatives cards

---
+++ decidim-initiatives/app/cells/decidim/initiatives/initiative_metadata_g_cell.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Initiatives
+    # This cell renders the assembly metadata for g card
+    class InitiativeMetadataGCell < Decidim::CardMetadataCell
+      def items
+        ["test"]
+      end
+    end
+  end
+end
