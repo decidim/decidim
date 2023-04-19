5a359fe1bb09151f64d399f86495687eb47e076a
Hugoren Martinako <aumpfbahn@gmail.com>
2023-04-19 13:28:24 +0200

    fix links between versions

--- decidim-proposals/app/cells/decidim/proposals/collaborative_draft_link_to_proposal_cell.rb
+++ decidim-proposals/app/cells/decidim/proposals/collaborative_draft_link_to_proposal_cell.rb
@@ -16,35 +16,6 @@ module Decidim
         @proposal ||= model.linked_resources(:proposal, "created_from_collaborative_draft").first
       end
 
-      def link_to_resource
-        link_to resource_locator(proposal).path, class: "button secondary light expanded button--sc mt-s" do
-          t("published_proposal", scope: "decidim.proposals.collaborative_drafts.show")
-        end
-      end
-
-      def link_header
-        tag.strong(class: "text-large") do
-          t("final_proposal", scope: "decidim.proposals.collaborative_drafts.show")
-        end
-      end
-
-      def link_help_text
-        tag.span(class: "text-medium") do
-          t("final_proposal_help_text", scope: "decidim.proposals.collaborative_drafts.show")
-        end
-      end
-
-      def link_to_versions
-        @path ||= decidim_proposals.collaborative_draft_versions_path(
-          collaborative_draft_id: model.id
-        )
-        link_to @path, class: "text-medium" do
-          tag.u do
-            t("version_history", scope: "decidim.proposals.collaborative_drafts.show")
-          end
-        end
-      end
-
       def decidim
         Decidim::Core::Engine.routes.url_helpers
       end
