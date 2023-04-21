5a5c2cd0287cd1ba5f1651a396e7eb0417588a4a
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-04-21 13:59:10 +0200

    Enable map conditionally

--- decidim-proposals/app/packs/src/decidim/proposals/add_proposal.js
+++ decidim-proposals/app/packs/src/decidim/proposals/add_proposal.js
@@ -5,30 +5,32 @@ $(() => {
   const $map = $("#address_map");
   const $addressInputField = $("[data-decidim-geocoding]");
 
-  $map.hide();
-  $addressInputField.on("geocoder-suggest-coordinates.decidim", () => $map.show());
+  if ($map.length) {
+    $map.hide();
+    $addressInputField.on("geocoder-suggest-coordinates.decidim", () => $map.show());
 
-  let latFieldName = "latitude";
-  let longFieldName = "longitude";
+    let latFieldName = "latitude";
+    let longFieldName = "longitude";
 
-  if ($addressInputField.length > 0) {
-    latFieldName = getCoordinateInputName("latitude", $addressInputField, {})
-    longFieldName = getCoordinateInputName("longitude", $addressInputField, {})
-  }
+    if ($addressInputField.length > 0) {
+      latFieldName = getCoordinateInputName("latitude", $addressInputField, {})
+      longFieldName = getCoordinateInputName("longitude", $addressInputField, {})
+    }
 
-  const ctrl = $("[data-decidim-map]").data("map-controller");
-  ctrl.setEventHandler("coordinates", (ev) => {
-    $(`input[name='${latFieldName}']`).val(ev.lat);
-    $(`input[name='${longFieldName}']`).val(ev.lng);
-  });
-
-  attachGeocoding($addressInputField, null, (coordinates) => {
-    // Remove previous marker when user updates address in address field
-    ctrl.removeMarker();
-    ctrl.addMarker({
-      latitude: coordinates[0],
-      longitude: coordinates[1],
-      address: $addressInputField.val()
+    const ctrl = $("[data-decidim-map]").data("map-controller");
+    ctrl.setEventHandler("coordinates", (ev) => {
+      $(`input[name='${latFieldName}']`).val(ev.lat);
+      $(`input[name='${longFieldName}']`).val(ev.lng);
     });
-  });
+
+    attachGeocoding($addressInputField, null, (coordinates) => {
+      // Remove previous marker when user updates address in address field
+      ctrl.removeMarker();
+      ctrl.addMarker({
+        latitude: coordinates[0],
+        longitude: coordinates[1],
+        address: $addressInputField.val()
+      });
+    });
+  }
 });
