ef7856bdfcd53513309ce41b9dd70d5bc5245dd6
Hugoren Martinako <aumpfbahn@gmail.com>
2023-04-25 15:26:57 +0200

    Redesign: process & process group cards (#10716)
    * add metadata to the process group cards
    
    * add end_date to card and improve radial icon
    
    * Implement temporary processes count metadata
    
    * enforce grid item as a block
    
    * normalize locales
    
    ---------
    
    Co-authored-by: Eduardo Martinez Echevarria <eduardomech@gmail.com>

--- decidim-core/app/cells/decidim/card_metadata_cell.rb
+++ decidim-core/app/cells/decidim/card_metadata_cell.rb
@@ -175,8 +175,9 @@ module Decidim
 
     def progress_value
       return if dates_blank?
+      return 0 unless current_date <= end_date
 
-      @progress_value ||= (end_date - current_date).to_f / (end_date - start_date) if current_date <= end_date
+      @progress_value ||= (end_date - current_date).to_f / (end_date - start_date)
     end
 
     def progress_span
@@ -193,9 +194,9 @@ module Decidim
                          elsif end_date.blank?
                            t("active", scope: "decidim.metadata.progress")
                          elsif current_date < end_date
-                           t("remaining", time_distance: distance_of_time_in_words(current_date, end_date), scope: "decidim.metadata.progress")
+                           t("remaining", scope: "decidim.metadata.progress", time_distance: distance_of_time_in_words(current_date, end_date))
                          else
-                           t("finished", scope: "decidim.metadata.progress")
+                           t("finished", scope: "decidim.metadata.progress", end_date: l(end_date.to_date, format: :decidim_short))
                          end
     end
   end
