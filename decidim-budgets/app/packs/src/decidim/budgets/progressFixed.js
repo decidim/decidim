7bd3e6d073fc21f5d6f7ed30d5bf3b93b972a72a
Jorge <jorgeatgu@gmail.com>
2023-04-26 17:43:30 +0200

    update amounts

--- decidim-budgets/app/packs/src/decidim/budgets/progressFixed.js
+++ decidim-budgets/app/packs/src/decidim/budgets/progressFixed.js
@@ -1,26 +1,28 @@
 $(() => {
   const checkProgressPosition = () => {
 
-    let progressRef
-    let progressFix
+    const progressRef = document.querySelectorAll("[data-progress-reference]");
+    const progressFix = document.querySelectorAll("[data-progressbox-fixed]");
+    let selectedProgressRef
+    let selectedProgressFix
     let progressVisibleClass = "is-progressbox-visible";
     if(window.matchMedia('(min-width: 768px)').matches) {
-      progressFix = document.querySelector("[data-progressbox-fixed]");
-      progressRef = document.querySelector("[data-progress-reference]");
+      selectedProgressRef = progressRef[1];
+      selectedProgressFix = progressFix[1];
     } else {
-      progressFix = document.querySelector("[data-progressbox-fixed-responsive]");
-      progressRef = document.querySelector("[data-progress-reference-responsive]");
+      selectedProgressRef = progressRef[0];
+      selectedProgressFix = progressFix[0];
     }
 
     if (!progressRef) {
       return;
     }
 
-    let progressPosition = progressRef.getBoundingClientRect().bottom;
+    let progressPosition = selectedProgressRef.getBoundingClientRect().bottom;
     if (progressPosition > 0) {
-      progressFix.classList.remove(progressVisibleClass);
+      selectedProgressFix.classList.remove(progressVisibleClass);
     } else {
-      progressFix.classList.add(progressVisibleClass);
+      selectedProgressFix.classList.add(progressVisibleClass);
     }
   }
 
