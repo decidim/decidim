be141a434ac5f51e08ea7ba2f45f293d5eb5646b
Hugoren Martinako <aumpfbahn@gmail.com>
2023-04-28 17:10:49 +0200

    full refactor of elections assets

--- decidim-elections/app/packs/entrypoints/decidim_elections.js
+++ decidim-elections/app/packs/entrypoints/decidim_elections.js
@@ -1,2 +1,16 @@
+import "src/decidim/elections/election_log";
+import "src/decidim/elections/onboarding";
+import "src/decidim/elections/trustee/key_ceremony";
+import "src/decidim/elections/trustee/tally";
+import "src/decidim/elections/trustee/trustee_zone";
+import "src/decidim/elections/voter/casting-vote";
+import "src/decidim/elections/voter/new-vote";
+import "src/decidim/elections/voter/setup-preview";
+import "src/decidim/elections/voter/setup-vote";
+import "src/decidim/elections/voter/verify-vote";
+
 // Images
 require.context("../images", true)
+
+// CSS
+import "stylesheets/decidim/elections/elections.scss"
