69fd8928f7283277baac5d0d318b511eed2bb850
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-05-02 21:44:51 +0300

    Apply review recommendation

--- decidim-core/lib/decidim/core/test/shared_examples/editor_shared_examples.rb
+++ decidim-core/lib/decidim/core/test/shared_examples/editor_shared_examples.rb
@@ -3,11 +3,8 @@
 shared_examples_for "has embedded video in description" do |description_attribute_name, count: 1|
   let(description_attribute_name) { { en: %(Description <iframe class="ql-video" allowfullscreen="true" src="#{iframe_src}" frameborder="0"></iframe>) } }
   let(:iframe_src) { "http://www.example.org" }
-  let!(:cookie_warning) { "You need to enable all cookies in order to see this content" }
 
-  context "when cookies are accepted" do
-    it "shows iframe" do
-      expect(page).to have_selector("iframe", count: count)
-    end
+  it "shows iframe" do
+    expect(page).to have_selector("iframe", count: count)
   end
 end
