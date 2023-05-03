3088dc52595bc882809cbb62b97ee1af28f54378
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-05-03 12:44:41 +0300

    Fix proposals' valuators assignments not deleted when space admin is removed (#10607)
    * Add event handlers to destroy records
    
    * Trigger event from Assemblies
    
    * Trigger event from Processes
    
    * Trigger event from Conferences
    
    * Running linters
    
    * Add cleanup rake task
    
    * Add release notes
    
    * Apply suggestions from code review
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    * Check if modules are being installed
    
    * Running linters
    
    * More linting
    
    ---------
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-conferences/spec/commands/destroy_conference_admin_spec.rb
+++ decidim-conferences/spec/commands/destroy_conference_admin_spec.rb
@@ -25,6 +25,16 @@ module Decidim::Conferences
         expect { role.reload }.to raise_error(ActiveRecord::RecordNotFound)
       end
 
+      it "fires an event" do
+        expect(ActiveSupport::Notifications).to receive(:publish).with(
+          "decidim.system.participatory_space.admin.destroyed",
+          "Decidim::ConferenceUserRole",
+          role.id
+        )
+
+        subject.call
+      end
+
       it "traces the action" do
         expect(Decidim.traceability)
           .to receive(:perform_action!)
