7807dfc69aaa6650991c4d4c7afec5d9a54fe6db
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-05-05 14:12:34 +0200

    Add orders to budgets list

--- decidim-budgets/app/cells/decidim/budgets/budgets_list_cell.rb
+++ decidim-budgets/app/cells/decidim/budgets/budgets_list_cell.rb
@@ -4,7 +4,13 @@ module Decidim
   module Budgets
     # This cell renders the budgets list of a Budget component
     class BudgetsListCell < BaseCell
+      AVAILABLE_ORDERS = %w(random highest_cost lowest_cost).freeze
+
       include Decidim::CellsPaginateHelper
+      include Decidim::OrdersHelper
+      include Decidim::Orderable
+      include Cell::ViewModel::Partial
+
       alias current_workflow model
 
       delegate :allowed, :budgets, :highlighted, :voted, to: :current_workflow
@@ -41,6 +47,21 @@ module Decidim
       def budget_paginate
         @budget_paginate ||= Kaminari.paginate_array(budgets).page(params[:page]).per(10)
       end
+
+      def reorder(budgets)
+        case order
+        when "highest_cost"
+          budgets.order(total_budget: :desc)
+        when "lowest_cost"
+          budgets.order(total_budget: :asc)
+        when "random"
+          budgets.order_randomly(random_seed)
+        else
+          budgets
+        end
+      end
+
+      def available_orders = AVAILABLE_ORDERS
     end
   end
 end
