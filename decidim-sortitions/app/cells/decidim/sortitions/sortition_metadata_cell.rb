9b02c1814030743d9a0cef8d98bd15403a5b11b9
Jorge <jorgeatgu@gmail.com>
2023-05-09 12:21:29 +0200

    sortition metadata

--- decidim-sortitions/app/cells/decidim/sortitions/sortition_metadata_cell.rb
+++ decidim-sortitions/app/cells/decidim/sortitions/sortition_metadata_cell.rb
@@ -8,6 +8,37 @@ module Decidim
 
       delegate :state, to: :model
 
+      def initialize(*)
+        super
+
+        @items.prepend(*sortition_items)
+      end
+
+      def sortition_items
+        [count_item, seed_item, badge_item]
+      end
+
+      def count_item
+        {
+          text: "#{proposals_count} #{t("decidim.sortitions.sortitions.sortition.selected_proposals", count: proposals_count)}",
+          icon: "chat-new-line"
+        }
+      end
+
+      def seed_item
+        {
+          text: "#{t("decidim.sortitions.sortitions.sortition.random_seed")} #{model.seed}",
+          icon: "seedling-line"
+        }
+      end
+
+      #REDESIGN_PENDING: add a function to pass the status(alert, warning or success) as a class to the tag
+      def badge_item
+        {
+          text: content_tag(:span, class: "alert label") {badge_name}
+        }
+      end
+
       def has_badge?
         false
       end
