9158043ca09baf124666ed4964c3112929a56c88
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-05-09 19:58:35 +0200

    Add duration and closing date elements to assembly about page

--- decidim-assemblies/app/presenters/decidim/assemblies/assembly_presenter.rb
+++ decidim-assemblies/app/presenters/decidim/assemblies/assembly_presenter.rb
@@ -17,6 +17,18 @@ module Decidim
         Decidim::AreaPresenter.new(assembly.area).translated_name_with_type
       end
 
+      def duration
+        return I18n.t("indefinite_duration", scope: "decidim.assemblies.assemblies.description") if (date = assembly.duration).blank?
+
+        I18n.l(date, format: :decidim_short)
+      end
+
+      def closing_date
+        return if (date = assembly.closing_date).blank?
+
+        I18n.l(date, format: :decidim_short)
+      end
+
       def assembly
         __getobj__
       end
