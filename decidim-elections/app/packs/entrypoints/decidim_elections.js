1f8806118b48791457a6ff76021e68e81a7e1866
Hugoren Martinako <aumpfbahn@gmail.com>
2023-05-11 14:02:25 +0200

    fix scripts triggers in elections

--- decidim-elections/app/packs/entrypoints/decidim_elections.js
+++ decidim-elections/app/packs/entrypoints/decidim_elections.js
@@ -2,10 +2,13 @@ import "src/decidim/elections/election_log";
 import "src/decidim/elections/trustee/key_ceremony";
 import "src/decidim/elections/trustee/tally";
 import "src/decidim/elections/trustee/trustee_zone";
+
+// both setup-vote and setup-preview MUST LOAD BEFORE new-vote
+// as they're imported from the window object
+import "src/decidim/elections/voter/setup-vote";
+import "src/decidim/elections/voter/setup-preview";
 import "src/decidim/elections/voter/casting-vote";
 import "src/decidim/elections/voter/new-vote";
-import "src/decidim/elections/voter/setup-preview";
-import "src/decidim/elections/voter/setup-vote";
 import "src/decidim/elections/voter/verify-vote";
 
 // Images
