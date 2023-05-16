a611166cb6b92896e1cd9859aad96d690066df4c
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-05-16 18:09:45 +0300

    Improve the engine initializers

--- decidim-dev/lib/decidim/dev/test/rspec_support/engine_examples.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/engine_examples.rb
@@ -2,12 +2,14 @@
 
 shared_examples_for "clean engine" do
   described_class.initializers.each do |initializer|
-    let(:initializer_name_prefix) { described_class.to_s.sub(/::([A-Za-z]+)?Engine$/, "\\1").gsub("::", "").underscore }
+    describe "'#{initializer.name}' initializer" do
+      let(:initializer_name_prefix) { described_class.to_s.sub(/::([A-Za-z]+)?Engine$/, "\\1").gsub("::", "").underscore }
 
-    it "names the '#{initializer.name}' initializer correctly" do
-      expect(initializer.name).to start_with(initializer_name_prefix)
-      expect(initializer.name).to match(/^[a-z0-9_.]+$/)
-      expect(initializer.name).not_to start_with(".")
+      it "is named correctly" do
+        expect(initializer.name).to start_with(initializer_name_prefix)
+        expect(initializer.name).to match(/^[a-z0-9_.]+$/)
+        expect(initializer.name).not_to start_with(".")
+      end
     end
   end
 end
