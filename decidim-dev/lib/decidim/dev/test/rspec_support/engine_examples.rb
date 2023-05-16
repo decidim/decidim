b0706efb137a2da7a87b43de006c41a45fb52655
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-05-16 19:58:09 +0300

    Ensure a dot after the engine name in the initializer name

--- decidim-dev/lib/decidim/dev/test/rspec_support/engine_examples.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/engine_examples.rb
@@ -6,7 +6,7 @@ shared_examples_for "clean engine" do
       let(:initializer_name_prefix) { described_class.to_s.sub(/::([A-Za-z]+)?Engine$/, "\\1").gsub("::", "").underscore }
 
       it "is named correctly" do
-        expect(initializer.name).to start_with(initializer_name_prefix)
+        expect(initializer.name).to start_with("#{initializer_name_prefix}.")
         expect(initializer.name).to match(/^[a-z0-9_.]+$/)
         expect(initializer.name).not_to start_with(".")
       end
