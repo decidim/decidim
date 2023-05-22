874245054e6c13ab02f6e0db51b961a16eecefed
Jorge <jorgeatgu@gmail.com>
2023-05-22 14:11:47 +0200

    spacing

--- decidim-core/lib/decidim/core/engine.rb
+++ decidim-core/lib/decidim/core/engine.rb
@@ -183,6 +183,7 @@ module Decidim
           namespace :design do
             get "forms", to: "forms#index"
             get "cards", to: "cards#index"
+            get "spacing", to: "spacing#index"
           end
         end
       end
