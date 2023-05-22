29f2dfffdc3a2e96055eb3c7ae9258b8ba9bf5a8
Hugoren Martinako <aumpfbahn@gmail.com>
2023-05-22 18:58:09 +0200

    add steps wizard

--- decidim-elections/app/helpers/decidim/elections/votes_helper.rb
+++ decidim-elections/app/helpers/decidim/elections/votes_helper.rb
@@ -18,6 +18,23 @@ module Decidim
           answer.proposals.any? ||
           answer.photos.any?
       end
+
+      def election_wizard_steps
+        ["register", "election", "confirm", "ballot_decision"]
+      end
+
+      def wizard_steps(current_step)
+        scope = "decidim.elections.votes.header"
+        inactive_data = { data: { past: "" } }
+        content_tag(:ol, id: "wizard-steps", class: "wizard-steps") do
+          election_wizard_steps.map do |wizard_step|
+            active = current_step == wizard_step
+            inactive_data = {} if active
+            attributes = active ? { "aria-current": "step", "aria-label": "#{t("current_step", scope:)}: #{t(wizard_step, scope:)}", data: { active: "" } } : inactive_data
+            content_tag(:li, t(wizard_step, scope:), attributes)
+          end.join.html_safe
+        end
+      end
     end
   end
 end
