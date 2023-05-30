d02e38d56501a7f8db6161f1b59ae33cdddd9b01
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-05-30 22:57:35 +0300

    Fix Style/RedundantStringEscape rubocop rule

--- decidim-core/db/migrate/20190412131728_fix_user_names.rb
+++ decidim-core/db/migrate/20190412131728_fix_user_names.rb
@@ -8,7 +8,7 @@ class FixUserNames < ActiveRecord::Migration[5.2]
     characters_to_remove = "<>?%&^*\#@()[]=+:;\"{}\\|/"
 
     weird_characters.each do |character|
-      Decidim::UserBaseEntity.where(deleted_at: nil).where("name like '%#{character}%' escape '\' OR nickname like '%#{character}%' escape '\'").find_each do |entity|
+      Decidim::UserBaseEntity.where(deleted_at: nil).where("name like '%#{character}%' escape '' OR nickname like '%#{character}%' escape ''").find_each do |entity|
         Rails.logger.debug { "detected character: #{character}" }
         Rails.logger.debug { "UserBaseEntity ID: #{entity.id}" }
         Rails.logger.debug { "#{entity.name} => #{entity.name.delete(characters_to_remove).strip}" }
