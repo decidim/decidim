50da59196ea25b394bc1e74f483b8981343ef718
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-06-02 15:00:49 +0300

    Fix user and group related migrations calling the actual record classes
    * Fix user and group related migrations calling the actual records
    
    * Fix few more cases with possible references to the user model
    
    * Fix the sortition authors to polymorphic spec
    
    * Fix issues with the inheritance columns in the migration records

--- decidim-initiatives/db/migrate/20181003082010_fix_user_groups_ids_on_initiatives.rb
+++ decidim-initiatives/db/migrate/20181003082010_fix_user_groups_ids_on_initiatives.rb
@@ -1,16 +1,31 @@
 # frozen_string_literal: true
 
 class FixUserGroupsIdsOnInitiatives < ActiveRecord::Migration[5.2]
+  class UserGroup < ApplicationRecord
+    self.table_name = :decidim_users
+    self.inheritance_column = nil # disable the default inheritance
+
+    default_scope { where(type: "Decidim::UserGroup") }
+  end
+
+  class Initiative < ApplicationRecord
+    self.table_name = :decidim_initiatives
+  end
+
+  class InitiativesVote < ApplicationRecord
+    self.table_name = :decidim_initiatives_votes
+  end
+
   # rubocop:disable Rails/SkipsModelValidations
   def change
-    Decidim::UserGroup.find_each do |group|
+    UserGroup.find_each do |group|
       old_id = group.extended_data["old_user_group_id"]
       next unless old_id
 
-      Decidim::Initiative
+      Initiative
         .where(decidim_user_group_id: old_id)
         .update_all(decidim_user_group_id: group.id)
-      Decidim::InitiativesVote
+      InitiativesVote
         .where(decidim_user_group_id: old_id)
         .update_all(decidim_user_group_id: group.id)
     end
