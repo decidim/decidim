218b9d582bb2c9bbe3a9737edc2eb92a2feef5b7
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-06-02 18:25:27 +0200

    Include purpose_of_action, internal_organisation and composition in main data of assemblies

--- decidim-assemblies/app/cells/decidim/assemblies/content_blocks/main_data_cell.rb
+++ decidim-assemblies/app/cells/decidim/assemblies/content_blocks/main_data_cell.rb
@@ -8,7 +8,10 @@ module Decidim
         include Decidim::ComponentPathHelper
         include ActiveLinkTo
 
+        EXTRA_ATTRIBUTES = %w(purpose_of_action internal_organisation composition).freeze
+
         delegate :short_description, :description, to: :resource
+        delegate(*EXTRA_ATTRIBUTES, to: :resource)
 
         private
 
@@ -25,7 +28,18 @@ module Decidim
         end
 
         def description_text
-          decidim_sanitize_editor_admin translated_attribute(description)
+          [decidim_sanitize_editor_admin(translated_attribute(description)), extra_attributes].compact_blank.join("\n")
+        end
+
+        def extra_attributes
+          %w(purpose_of_action internal_organisation composition).filter_map do |attribute|
+            if (text = translated_attribute(send(attribute))).present?
+              [
+                content_tag(:h3, class: "h4") { t(attribute, scope: "activemodel.attributes.assembly") },
+                decidim_sanitize_editor_admin(text)
+              ].join("\n")
+            end
+          end
         end
 
         def nav_items
