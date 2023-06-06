c246bdf88a3af7669af365583c925c8e9327fa8c
Hugoren Martinako <aumpfbahn@gmail.com>
2023-06-06 11:23:58 +0200

    fix rubocop

--- decidim-core/app/cells/decidim/collapsible_authors_cell.rb
+++ decidim-core/app/cells/decidim/collapsible_authors_cell.rb
@@ -4,7 +4,6 @@ module Decidim
   # This cell renders a collapsible list of authors. Each element from the
   # array of Users will be rendered with the `:cell` cell.
   class CollapsibleAuthorsCell < Decidim::ViewModel
-
     MAX_ITEMS_STACKED = 3
 
     def show
