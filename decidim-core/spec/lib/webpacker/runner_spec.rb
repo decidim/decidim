d631fd2716928ea841fbd964db37f3dac4008c22
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-06-07 19:53:02 +0200

    Fix tests after import deprecation

--- decidim-core/spec/lib/webpacker/runner_spec.rb
+++ decidim-core/spec/lib/webpacker/runner_spec.rb
@@ -46,8 +46,6 @@ module Webpacker
         expect(runtime_config["default"]["stylesheet_imports"].keys).to include("imports")
         expect(runtime_config["default"]["stylesheet_imports"]["imports"].keys).to include("app")
         expect(runtime_config["default"]["stylesheet_imports"]["imports"]["app"]).to include(
-          "stylesheets/decidim/proposals/proposals",
-          "stylesheets/decidim/budgets/budgets",
           "stylesheets/decidim/consultations/consultations",
           "stylesheets/decidim/elections/elections",
           "stylesheets/decidim/votings/votings",
