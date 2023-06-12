c47a2a29bb4ef1af56fe399c673cc0759b364777
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-06-12 15:04:51 +0200

    Remove bootstrap-tagsinput npm package

--- decidim-core/app/packs/src/decidim/input_tags.js
+++ decidim-core/app/packs/src/decidim/input_tags.js
@@ -1,12 +1,5 @@
-import "bootstrap-tagsinput"
-
 $(() => {
   const $tagContainer = $(".js-tags-container");
 
-  // Initialize
-  $tagContainer.tagsinput({
-    tagClass: "input__tag",
-    trimValue: true
-  });
-
+  // TODO
 });
