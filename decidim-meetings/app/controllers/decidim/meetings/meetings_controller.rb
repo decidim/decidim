a1642ccbcbe1425e15e48f6e749f1efc1d2733d2
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-06-13 16:45:53 +0200

    Refactor tabbed content on meetings and proposals to use tab_panels cell

--- decidim-meetings/app/controllers/decidim/meetings/meetings_controller.rb
+++ decidim-meetings/app/controllers/decidim/meetings/meetings_controller.rb
@@ -14,8 +14,9 @@ module Decidim
       helper Decidim::WidgetUrlsHelper
       helper Decidim::ResourceVersionsHelper
       helper Decidim::ShortLinkHelper
+      include Decidim::AttachmentsHelper
 
-      helper_method :meetings, :meeting, :registration, :search, :nav_paths
+      helper_method :meetings, :meeting, :registration, :search, :nav_paths, :tab_panel_items
 
       redesign active: true
 
@@ -191,6 +192,43 @@ module Decidim
           with_any_origin: default_filter_origin_params,
           with_any_type: default_filter_type_params
         }
+      end
+
+      def tab_panel_items
+        @tab_panel_items ||= [
+          {
+            enabled: meeting.public_participants.any?,
+            id: "participants",
+            text: t("attending_participants", scope: "decidim.meetings.public_participants_list"),
+            icon: "group-line",
+            method: :cell,
+            args: ["decidim/meetings/public_participants_list", meeting]
+          },
+          {
+            enabled: !meeting.closed? && meeting.user_group_registrations.any?,
+            id: "organizations",
+            text: t("attending_organizations", scope: "decidim.meetings.public_participants_list"),
+            icon: "community-line",
+            method: :cell,
+            args: ["decidim/meetings/attending_organizations_list", meeting]
+          },
+          {
+            enabled: meeting.linked_resources(:proposals, "proposals_from_meeting").present?,
+            id: "included_proposals",
+            text: t("decidim/proposals/proposal", scope: "activerecord.models", count: 2),
+            icon: resource_type_icon_key("Decidim::Proposals::Proposal"),
+            method: :cell,
+            args: ["decidim/linked_resources_for", meeting, { type: :proposals, link_name: "proposals_from_meeting" }]
+          },
+          {
+            enabled: meeting.linked_resources(:results, "meetings_through_proposals").present?,
+            id: "included_meetings",
+            text: t("decidim/accountability/result", scope: "activerecord.models", count: 2),
+            icon: resource_type_icon_key("Decidim::Accountability::Result"),
+            method: :cell,
+            args: ["decidim/linked_resources_for", meeting, { type: :results, link_name: "meetings_through_proposals" }]
+          }
+        ] + attachments_tab_panel_items(@meeting)
       end
     end
   end
