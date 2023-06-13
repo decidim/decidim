bd0bdd9eccbdfc746a812a5a6ab3589ff77ee628
Hugoren Martinako <aumpfbahn@gmail.com>
2023-06-13 17:59:58 +0200

    fix js test

--- decidim-comments/app/packs/src/decidim/comments/comments.component.test.js
+++ decidim-comments/app/packs/src/decidim/comments/comments.component.test.js
@@ -49,7 +49,7 @@ describe("CommentsComponent", () => {
   const spyOnAddComment = (methodToSpy) => {
     addComment.each((i) => {
       addComment[i].$ = $(addComment[i]);
-      addComment[i].opinionToggles = $(".opinion-toggle .button", addComment[i].$);
+      addComment[i].opinionToggles = $(".opinion-toggle button", addComment[i].$);
       addComment[i].commentForm = $("form", addComment[i].$);
       addComment[i].commentTextarea = $("textarea", addComment[i].commentForm);
 
@@ -79,7 +79,7 @@ describe("CommentsComponent", () => {
       }
       const addCommentsArray = addComment.toArray();
       for (let i = 0; i < addCommentsArray.length; i += 1) {
-        if (jqSelector === ".opinion-toggle .button" && parent.is(addCommentsArray[i].$)) {
+        if (jqSelector === ".opinion-toggle button" && parent.is(addCommentsArray[i].$)) {
           return addCommentsArray[i].opinionToggles;
         } else if (jqSelector === "form" && parent.is(addCommentsArray[i].$)) {
           return addCommentsArray[i].commentForm;
