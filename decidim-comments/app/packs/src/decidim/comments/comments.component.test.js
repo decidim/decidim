5242d6aa0b58c1aadd9006be8fe1afd1d68e28af
Hugoren Martinako <aumpfbahn@gmail.com>
2023-06-13 17:43:33 +0200

    fix js test

--- decidim-comments/app/packs/src/decidim/comments/comments.component.test.js
+++ decidim-comments/app/packs/src/decidim/comments/comments.component.test.js
@@ -70,7 +70,7 @@ describe("CommentsComponent", () => {
         return orderLinks;
       } else if (jqSelector === ".add-comment" && parent.is(subject.$element)) {
         return addComment;
-      } else if (jqSelector === ".add-comment .opinion-toggle .button" && parent.is(subject.$element)) {
+      } else if (jqSelector === ".add-comment .opinion-toggle button" && parent.is(subject.$element)) {
         return allToggles;
       } else if (jqSelector === ".add-comment textarea" && parent.is(subject.$element)) {
         return allTextareas;
