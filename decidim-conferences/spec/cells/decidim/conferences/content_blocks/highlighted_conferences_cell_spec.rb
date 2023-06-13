80c7d82a47813c2fdadb6b8f1a045051bec102de
Jorge <jorgeatgu@gmail.com>
2023-06-13 15:47:26 +0200

    use the card__grid class for the home content-block tests

--- decidim-conferences/spec/cells/decidim/conferences/content_blocks/highlighted_conferences_cell_spec.rb
+++ decidim-conferences/spec/cells/decidim/conferences/content_blocks/highlighted_conferences_cell_spec.rb
@@ -21,7 +21,7 @@ describe Decidim::Conferences::ContentBlocks::HighlightedConferencesCell, type: 
   # Conferences do not have a max_results settings number selector yet, we might want this back when they do
   # context "when the content block has no settings" do
   #   it "shows 4 processes" do
-  #     expect(highlighted_conferences).to have_selector("a.card--conference", count: 4)
+  #     expect(highlighted_conferences).to have_selector("a.card__grid", count: 4)
   #   end
   # end
 
@@ -34,7 +34,7 @@ describe Decidim::Conferences::ContentBlocks::HighlightedConferencesCell, type: 
     end
 
     it "shows up to 8 conferences" do
-      expect(highlighted_conferences).to have_selector("a.card--conference", count: 5)
+      expect(highlighted_conferences).to have_selector("a.card__grid", count: 5)
     end
   end
 end
--- decidim-participatory_processes/spec/cells/decidim/participatory_processes/content_blocks/highlighted_processes_cell_spec.rb
+++ decidim-participatory_processes/spec/cells/decidim/participatory_processes/content_blocks/highlighted_processes_cell_spec.rb
@@ -10,7 +10,7 @@ describe Decidim::ParticipatoryProcesses::ContentBlocks::HighlightedProcessesCel
   let!(:processes) { create_list :participatory_process, 8, organization: }
   let(:settings) { {} }
 
-  let(:highlighted_processes) { subject.find("#highlighted_processes") }
+  let(:highlighted_processes) { subject.find("#highlighted-processes") }
 
   controller Decidim::PagesController
 
