d10b9c35c74d518b3796b2f760f530033175a210
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-06-14 07:43:05 +0300

    Fix "No activity" message in Last Activities isn't shown sometimes
    * Partial refactor of LastActivity query
    
    * Apply review recommendations
    
    * Fix last_activity log
    
    * Fix factory
    
    * Fix factory

--- decidim-core/app/cells/decidim/content_blocks/last_activity_cell.rb
+++ decidim-core/app/cells/decidim/content_blocks/last_activity_cell.rb
@@ -56,7 +56,7 @@ module Decidim
       end
 
       def activities
-        @activities ||= Decidim::LastActivity.new(current_organization).query.limit(activities_to_show * 6)
+        @activities ||= Decidim::LastActivity.new(current_organization, current_user: current_user).query.limit(activities_to_show * 6)
       end
 
       def activities_to_show
