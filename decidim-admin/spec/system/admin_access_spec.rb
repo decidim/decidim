0db6e5b309b48ec53a966b1acd9b8b0df2460183
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-06-14 07:55:40 +0200

    Add spec

---
+++ decidim-admin/spec/system/admin_access_spec.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "AdminAccess", type: :system do
+  let(:organization) { create(:organization) }
+  let(:participatory_process) { create(:participatory_process, organization:, title: { en: "My process" }) }
+  let(:other_participatory_process) { create(:participatory_process, organization:) }
+
+  before do
+    switch_to_host(organization.host)
+    login_as user, scope: :user
+  end
+
+  shared_examples "showing the page" do
+    it "shows the page" do
+      expect(page).to have_content("View public page")
+      expect(page).to have_content("My process")
+    end
+  end
+
+  shared_examples "showing the unauthorized error message" do
+    it "redirects the user to the core's root_page" do
+      expect(page).to have_content("You are not authorized to perform this action")
+      expect(page).to have_current_path("/")
+    end
+  end
+
+  context "when the user is a process admin" do
+    let(:user) { create(:process_admin, :confirmed, organization:, participatory_process:) }
+
+    context "and has permission" do
+      before do
+        visit decidim_admin_participatory_processes.edit_participatory_process_path(participatory_process)
+      end
+
+      it_behaves_like "showing the page"
+    end
+
+    context "and does not have permission" do
+      before do
+        visit decidim_admin_participatory_processes.edit_participatory_process_path(other_participatory_process)
+      end
+
+      it_behaves_like "showing the unauthorized error message"
+    end
+  end
+
+  context "when the user is a valuator" do
+    let(:user) { create(:process_valuator, :confirmed, participatory_process:) }
+
+    context "and has permission" do
+      before do
+        visit decidim_admin_participatory_processes.components_path(participatory_process)
+      end
+
+      it_behaves_like "showing the page"
+    end
+
+    context "and does not have permission" do
+      before do
+        visit decidim_admin_participatory_processes.components_path(other_participatory_process)
+      end
+
+      it_behaves_like "showing the unauthorized error message"
+    end
+  end
+end
