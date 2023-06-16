8d96cd8171793c2957e10c351cad77a644fc9430
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-06-16 10:20:19 +0300

    Backport 'Don't allow access to admin panel without ToS acceptance' to v0.27 (#11042)
    * Don't allow access to admin panel without ToS acceptance
    
    * Add redirection to previous page after accepting ToS
    
    * Use have_content instead of have_text
    
    * Running spellcheck linters
    
    * Fix specs
    
    * Fix permissions on Templates when user is not admin
    
    * Fix specs
    
    * Fix i18n string scope from merge
    
    * Workaround for admin ToS acceptance in Initiatives
    
    After #5736, the initiatives' authors and commitee members should not
    have access to the admin panel.
    
    The problem is that with the change of the Terms of Service acceptance
    in the admin panel this is changing, so there's still some leftovers in
    the initiatives' permissions.
    
    As I only want to focus on ToS acceptance for now, I'll skip these specs
    and fix the real problem (cleaning the leftovers from #5736) on another
    PR to keep this small.
    
    * Fix typo
    
    * Fix for possible Cookie overflow with a long list of URL params
    
    Detected by code review
    
    * Remove unecessary namespaces
    
    * Fix spec
    
    * Bring consistency to the spec messages
    
    * "has not accepted" sounds better than "did not accepted"
    * sometimes I was using "has a message" and other times "shows a
      message"
    * sometimes we were using ToS and other times TOS
    
    * Add missing specs for Templates' specs
    
    * Remove unecessary return
    
    Apply suggestions from code review
    
    
    
    * Fix the stored request.path to not mess with the frontend's stored location
    
    Apply suggestions from code review
    
    
    
    * Fetch from stored_location_for so the session value is cleaned
    
    Apply suggestions from code review
    
    
    
    * Fix traits usages in factories calls
    
    Apply suggestions from code review
    
    
    
    * Introduce "needs admin TOS accepted" shared example
    
    * Fix rubocop offenses
    
    * Fix rubocop offenses
    
    * Make the user configurable for "needs admin TOS accepted" shared example
    
    * Fix rubocop offense
    
    * Refactor spec to shared examples
    
    * Add example for roles that aren't admin
    
    ---------
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    Co-authored-by: Antti Hukkanen <antti.hukkanen@mainiotech.fi>

--- decidim-admin/spec/system/space_admin_manages_global_moderations_spec.rb
+++ decidim-admin/spec/system/space_admin_manages_global_moderations_spec.rb
@@ -24,14 +24,13 @@ describe "Space admin manages global moderations", type: :system do
     login_as user, scope: :user
   end
 
-  context "when the user didn't accepted the admin ToS" do
+  context "when the user has not accepted the admin TOS" do
     before do
       user.update(admin_terms_accepted_at: nil)
       visit decidim_admin.moderations_path
     end
 
     it "has a message that they need to accept the admin TOS" do
-      expect(page).to have_content("You are not authorized")
       expect(page).to have_content("Please take a moment to review Admin Terms of Use. Otherwise you won't be able to manage the platform")
     end
 
@@ -48,9 +47,7 @@ describe "Space admin manages global moderations", type: :system do
       end
 
       it "says that you're not authorized" do
-        within ".callout.alert" do
-          expect(page).to have_text("You are not authorized to perform this action")
-        end
+        expect(page).to have_text("Please take a moment to review Admin Terms of Use")
       end
     end
   end
