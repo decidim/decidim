115155f3769ad96b3da285860d607e1da2624e84
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-06-16 15:11:32 +0200

    Fix spelling

--- decidim-core/spec/system/user_profile_spec.rb
+++ decidim-core/spec/system/user_profile_spec.rb
@@ -232,7 +232,7 @@ describe "Profile", type: :system do
       let(:view_hook) { :user_profile_bottom }
 
       it "renders the view hook" do
-        skip "REDESIGN_PENDING - The user_profile_botton view hook is not considered in the redesign. Remove the test if it's correct"
+        skip "REDESIGN_PENDING - The user_profile_botton view hook is not considered in the redesign. Remove the test if it is correct"
 
         expect(Decidim.view_hooks).to have_received(:render).with(:user_profile_bottom, a_kind_of(Decidim::ProfileSidebarCell))
         expect(page).to have_content("Rendered from user_profile_bottom view hook")
