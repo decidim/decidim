440b40b05ec5ab30ee54e6689d2801cf8dc14c01
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-06-19 13:53:16 +0300

    Add hide_resource event notification

---
+++ decidim-core/lib/decidim/core/test/shared_examples/active_support_examples.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+shared_examples "fires an ActiveSupport::Notification event" do |fired_event|
+  it "subscribes to #{fired_event}" do
+    @tested_notification = false
+    subscriber = ActiveSupport::Notifications.subscribe(fired_event) { |_, _| @tested_notification = true }
+
+    command.call
+
+    expect(@tested_notification).to eq(true)
+  rescue RSpec::Expectations::ExpectationNotMetError => e
+    ActiveSupport::Notifications.unsubscribe(subscriber)
+    raise e
+  ensure
+    ActiveSupport::Notifications.unsubscribe(subscriber)
+  end
+end
