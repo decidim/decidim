4140e094bb46688ee2091ae402a6abd7d00e6081
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-06-20 11:13:22 +0200

    Use generic content block for highlighted processes in home

--- decidim-participatory_processes/app/cells/decidim/participatory_processes/content_blocks/highlighted_processes_cell.rb
+++ decidim-participatory_processes/app/cells/decidim/participatory_processes/content_blocks/highlighted_processes_cell.rb
@@ -3,45 +3,30 @@
 module Decidim
   module ParticipatoryProcesses
     module ContentBlocks
-      class HighlightedProcessesCell < Decidim::ViewModel
-        include Decidim::ApplicationHelper
-        include Decidim::SanitizeHelper
-        include Decidim::CardHelper
-        include Cell::ViewModel::Partial
-        include ParticipatoryProcessHelper
-        include Decidim::ParticipatoryProcesses::Engine.routes.url_helpers
+      class HighlightedProcessesCell < Decidim::ContentBlocks::HighlightedParticipatorySpacesCell
+        BLOCK_ID = "highlighted-processes"
 
         delegate :current_user, to: :controller
 
-        def show
-          if single_item?
-            render "single_process"
-          elsif highlighted_items.any?
-            render
-          end
-        end
-
-        def single_item?
-          highlighted_items.length == 1
-        end
-
-        def max_results
-          model.settings.max_results
-        end
-
-        def highlighted_items
-          @highlighted_items ||= promoted_groups + highlighted_processes
+        def highlighted_spaces
+          @highlighted_spaces ||= promoted_groups + highlighted_processes
         end
 
         def i18n_scope
           "decidim.participatory_processes.pages.home.highlighted_processes"
         end
 
-        def decidim_participatory_processes
-          Decidim::ParticipatoryProcesses::Engine.routes.url_helpers
+        def all_path
+          Decidim::ParticipatoryProcesses::Engine.routes.url_helpers.participatory_processes_path
+        end
+
+        def max_results
+          model.settings.max_results
         end
 
         private
+
+        def block_id = BLOCK_ID
 
         def highlighted_processes
           @highlighted_processes ||= if highlighted_processes_max_results.zero?
