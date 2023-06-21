3c47a4f8001213c52befb7261244b1c0910a806c
Alejandro Puerta <alejandro.puerta@nazaries.com>
2023-06-21 10:42:58 +0200

    rubocop lint

--- decidim-core/spec/system/authentication_spec.rb
+++ decidim-core/spec/system/authentication_spec.rb
@@ -602,7 +602,7 @@ describe "Authentication", type: :system do
         end
 
         context "when admin password is expired" do
-          let(:user) { create(:user, :confirmed, :admin, password_updated_at: 91.days.ago, organization: organization) }
+          let(:user) { create(:user, :confirmed, :admin, password_updated_at: 91.days.ago, organization:) }
 
           before do
             allow(Decidim.config).to receive(:admin_password_expiration_days).and_return(90)
