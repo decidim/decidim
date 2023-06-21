287585d7cc2cb8e0d9add4fec21daaafcb407572
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-06-21 15:22:29 +0200

    Enable 'remove button' feature

--- decidim-core/app/packs/src/decidim/input_tags.js
+++ decidim-core/app/packs/src/decidim/input_tags.js
@@ -1,8 +1,9 @@
-import TomSelect from "tom-select/dist/cjs/tom-select";
+import TomSelect from "tom-select/dist/cjs/tom-select.popular";
 
 document.addEventListener("DOMContentLoaded", () => {
   const tagContainers = document.querySelectorAll(".js-tags-container");
   const config = {
+    plugins: ["remove_button"],
     create: true,
     render: {
       no_results: null
