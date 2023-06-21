5c90018215f5fedfcc467ebf613dccd438c413a0
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-06-21 06:09:52 +0300

    Apply suggestions from code review
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-core/lib/decidim/core.rb
+++ decidim-core/lib/decidim/core.rb
@@ -527,7 +527,7 @@ module Decidim
   # List of additional content security policies to be appended to the default ones
   # This is useful for adding custom CSPs for external services like Here Maps, YouTube, etc.
   # Read more: https://docs.decidim.org/en/develop/configure/initializer/#_content-security-policy
-  config_accessor :additional_content_security_policies do
+  config_accessor :content_security_policies_extra do
     {}
   end
 
