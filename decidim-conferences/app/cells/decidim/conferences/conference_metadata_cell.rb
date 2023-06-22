009473728321c44569ccaf602134dd0ba24ad944
Hugoren Martinako <aumpfbahn@gmail.com>
2023-06-22 16:24:16 +0200

    add case when date & time are the same

--- decidim-conferences/app/cells/decidim/conferences/conference_metadata_cell.rb
+++ decidim-conferences/app/cells/decidim/conferences/conference_metadata_cell.rb
@@ -5,10 +5,16 @@ require "cell/partial"
 module Decidim
   module Conferences
     class ConferenceMetadataCell < Decidim::CardMetadataCell
-      delegate :start_date, :end_date, to: :model
-
       def items
         [dates_item].compact
+      end
+
+      def start_date
+        model.start_date.to_time
+      end
+
+      def end_date
+        model.end_date.to_time
       end
     end
   end
