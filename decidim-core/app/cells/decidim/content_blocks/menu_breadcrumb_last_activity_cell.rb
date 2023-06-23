376b7fb524aa19116bfccd5290fcd4bd4c3a53e8
Fernando Blat <fernando@blat.es>
2023-06-23 11:30:27 +0200

    Integrate last activity

--- decidim-core/app/cells/decidim/content_blocks/menu_breadcrumb_last_activity_cell.rb
+++ decidim-core/app/cells/decidim/content_blocks/menu_breadcrumb_last_activity_cell.rb
@@ -8,7 +8,13 @@ module Decidim
       private
 
       def activities
-        @activities ||= Decidim::ParticipatorySpaceLastActivity.new(model).query.limit(activities_to_show * 6)
+        @activities ||= begin
+          if model.is_a?(Decidim::Organization)
+            Decidim::LastActivity.new(model).query
+          else
+            Decidim::ParticipatorySpaceLastActivity.new(model).query
+          end.limit(activities_to_show * 6)
+        end
       end
 
       # A MD5 hash of model attributes is needed because
