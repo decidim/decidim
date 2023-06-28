d9309b211904fb36a60f0f0798f9f110a8c5773e
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-06-28 18:40:45 +0200

    Fix condition
    If resource does not respond to can_participate? its associated action
    log should be displayed

--- decidim-core/app/models/decidim/action_log.rb
+++ decidim-core/app/models/decidim/action_log.rb
@@ -253,7 +253,7 @@ module Decidim
         participatory_space_lazy.present? &&
         !resource_lazy.try(:deleted?) &&
         !resource_lazy.try(:hidden?) &&
-        (participatory_space_lazy.try(:is_transparent?) || resource_lazy.try(:can_participate?, user))
+        (participatory_space_lazy.try(:is_transparent?) || !resource_lazy.respond_to?(:can_participate?) || resource_lazy.try(:can_participate?, user))
     rescue NameError => e
       Rails.logger.warn "Failed resource for #{self.class.name}(id=#{id}): #{e.message}"
 
