28305e67f98cc22752ec82829b6758fee9ddc03a
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2023-07-04 12:02:30 +0200

    Backport 'Avoid password change to be requested when user registration mode is disabled' to v0.27 (#11120)
    * Avoid password change to be requested when user registration mode is disabled
    
    * avoid password change to be requested when user registration mode is disabled
    
    * admin_password_expiration_days accessor set to 90 days
    
    * rubocop lint
    
    * Add omitted value in keyword argument
    
    ---------
    
    Co-authored-by: Alejandro Puerta <71900287+apuertadelaguila@users.noreply.github.com>

--- decidim-core/app/models/decidim/user.rb
+++ decidim-core/app/models/decidim/user.rb
@@ -261,6 +261,7 @@ module Decidim
     end
 
     def needs_password_update?
+      return false if organization.users_registration_mode == "disabled"
       return false unless admin?
       return false unless Decidim.config.admin_password_strong
       return true if password_updated_at.blank?
