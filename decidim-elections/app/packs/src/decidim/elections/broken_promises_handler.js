ad5028c7e4c1f114b814e69d717104cdb441bb05
Hugoren Martinako <aumpfbahn@gmail.com>
2023-07-06 16:03:52 +0200

    enhance error modal

--- decidim-elections/app/packs/src/decidim/elections/broken_promises_handler.js
+++ decidim-elections/app/packs/src/decidim/elections/broken_promises_handler.js
@@ -1,11 +1,13 @@
 /* Fallback for non-handled failed promises */
 window.addEventListener("unhandledrejection", (event) => {
-  document.getElementById("tech-info").innerHTML = event.reason
+  if (window.Decidim.currentDialogs["server-failure"]) {
+    document.getElementById("tech-info").innerHTML = event.reason
 
-  if (event.reason.toString().indexOf("fetch") === -1) {
-    document.getElementById("communication_error").hidden = true
-    document.getElementById("generic_error").hidden = false
+    if (event.reason.toString().indexOf("fetch") === -1) {
+      document.getElementById("communication_error").hidden = true
+      document.getElementById("generic_error").hidden = false
+    }
+
+    window.Decidim.currentDialogs["server-failure"].open()
   }
-
-  window.Decidim.currentDialogs["server-failure"].open()
 });
