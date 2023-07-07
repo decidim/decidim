fd11664bc4debe285dd5ad2016c1a4de5fef865d
Hugoren Martinako <aumpfbahn@gmail.com>
2023-07-07 13:01:52 +0200

    do not load dynamic import on the entrypoint (they're called in new-vote.js)

--- decidim-elections/app/packs/entrypoints/decidim_elections.js
+++ decidim-elections/app/packs/entrypoints/decidim_elections.js
@@ -3,12 +3,6 @@ import "src/decidim/elections/trustee/key_ceremony";
 import "src/decidim/elections/trustee/tally";
 import "src/decidim/elections/trustee/trustee_zone";
 
-// REDESIGN_PENDING: setup-vote and setup-preview MUST NOT LOAD at the same time
-// they should be loaded asynchronously depending "preview_mode?" rails variable
-// thourgh "async imports", see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/import
-import "src/decidim/elections/voter/setup-vote";
-import "src/decidim/elections/voter/setup-preview";
-
 import "src/decidim/elections/voter/casting-vote";
 import "src/decidim/elections/voter/new-vote";
 import "src/decidim/elections/voter/verify-vote";
