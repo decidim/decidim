750b142b8025b8e8c7f9c174f7d1e89de18c44a2
Hugoren Martinako <aumpfbahn@gmail.com>
2023-07-11 14:10:24 +0200

    polling stations content block

--- decidim-elections/app/cells/decidim/votings/content_blocks/polling_stations_cell.rb
+++ decidim-elections/app/cells/decidim/votings/content_blocks/polling_stations_cell.rb
@@ -22,31 +22,8 @@ module Decidim
 
         private
 
-        def geolocation_enabled?
-          Decidim::Map.available?(:geocoding)
-        end
-
         def polling_stations
           @polling_stations ||= current_participatory_space.polling_stations
-        end
-
-        def polling_stations_geocoded
-          @polling_stations_geocoded ||= polling_stations.geocoded
-        end
-
-        def polling_stations_geocoded_data_for_map
-          polling_stations_geocoded.map do |polling_station|
-            polling_station_data_for_map(polling_station)
-          end
-        end
-
-        def polling_station_data_for_map(polling_station)
-          polling_station.slice(:latitude, :longitude, :address)
-                         .merge(
-                           title: translated_attribute(polling_station.title),
-                           location: translated_attribute(polling_station.location),
-                           locationHints: translated_attribute(polling_station.location_hints)
-                         )
         end
       end
     end
