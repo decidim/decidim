9a558c048bb61f3b4571c8b42e5d65bbfeb4cf31
Hugoren Martinako <aumpfbahn@gmail.com>
2023-07-11 14:30:09 +0200

    fix i18n & rubocop

--- decidim-elections/app/helpers/decidim/votings/map_helper.rb
+++ decidim-elections/app/helpers/decidim/votings/map_helper.rb
@@ -11,10 +11,10 @@ module Decidim
         polling_stations_geocoded = polling_stations.select(&:geocoded_and_valid?)
         polling_stations_geocoded.map do |polling_station|
           polling_station.slice(:latitude, :longitude, :address)
-                          .merge(
-                            title: translated_attribute(polling_station.title),
-                            items: [{ icon: icon("map-line").html_safe, text: polling_station.address }].to_json
-                          )
+                         .merge(
+                           title: translated_attribute(polling_station.title),
+                           items: [{ icon: icon("map-line").html_safe, text: polling_station.address }].to_json
+                         )
         end
       end
     end
