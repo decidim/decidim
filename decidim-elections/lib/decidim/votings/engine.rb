6d6da3f0ce964a9e4b4819323635f3109f0fc36a
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-07-11 18:32:39 +0200

    Recover dummy metrics content block on votings and inherit from redesigned metrics block for participatory spaces

--- decidim-elections/lib/decidim/votings/engine.rb
+++ decidim-elections/lib/decidim/votings/engine.rb
@@ -121,8 +121,8 @@ module Decidim
         end
 
         Decidim.content_blocks.register(:voting_landing_page, :metrics) do |content_block|
-          content_block.cell = "decidim/content_blocks/organization_metrics"
-          content_block.public_name_key = "decidim.content_blocks.metrics.name"
+          content_block.cell = "decidim/votings/content_blocks/metrics"
+          content_block.public_name_key = "decidim.votings.admin.content_blocks.metrics.name"
         end
 
         Decidim.content_blocks.register(:voting_landing_page, :html) do |content_block|
