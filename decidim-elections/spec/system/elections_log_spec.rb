96966a8906e0f8146cfd56580fa7a1c60f174354
Hugoren Martinako <aumpfbahn@gmail.com>
2023-07-12 13:14:47 +0200

    fix rubocop and i18n

--- decidim-elections/spec/system/elections_log_spec.rb
+++ decidim-elections/spec/system/elections_log_spec.rb
@@ -33,7 +33,7 @@ describe "Elections log", type: :system do
       expect(page).to have_content("The election log will show you all relevant information about each voting. For example, the status of the key ceremony or tally or if results are published already. Click on the election you want the log information about.")
       expect(page).to have_selector("[data-log-entry]").exactly(4).times
       expect(page).to have_content(translated(vote_election.title))
-      expect(page).to have_content(strip_tags translated(vote_election.description))
+      expect(page).to have_content(strip_tags(translated(vote_election.description)))
       expect(page).to have_content(vote_election.start_time)
       expect(page).to have_content(vote_election.bb_status.to_s.titlecase)
     end
