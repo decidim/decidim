da4082cfdb4b44cc153166952e0650375b660904
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-07-13 16:11:26 +0200

    Rename redesigned css files calls

--- decidim-core/app/packs/entrypoints/redesigned_decidim_core.js
+++ decidim-core/app/packs/entrypoints/redesigned_decidim_core.js
@@ -1,11 +1,11 @@
 // DO NOT include any javascript file here, but inside the following file instead
-import "src/decidim/redesigned_index"
+import "src/decidim/index"
 
 // Place for custom scripts
 import "src/decidim/decidim_application"
 
 // CSS
-import "entrypoints/redesigned_decidim_core.scss"
+import "entrypoints/decidim_core.scss"
 
 // Images
 require.context("../images", true)
