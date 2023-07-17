e80f679dea148c69577e064c3f88e391eec05b46
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-07-17 12:53:03 +0200

    Add status item to voting metadata cell

--- decidim-elections/app/cells/decidim/votings/voting_metadata_cell.rb
+++ decidim-elections/app/cells/decidim/votings/voting_metadata_cell.rb
@@ -4,6 +4,8 @@ module Decidim
   module Votings
     # This cell renders metadata for an instance of a Voting
     class VotingMetadataCell < Decidim::CardMetadataCell
+      delegate :voting_type, :period_status, :start_time, :end_time, to: :model
+
       def initialize(*)
         super
 
@@ -13,26 +15,41 @@ module Decidim
       private
 
       def voting_items
-        [dates_metadata_item, type_item]
+        [dates_metadata_item, type_item, status_item]
       end
 
       def type_item
         {
-          icon: resource_type_icon_key(model.voting_type),
-          text: t(model.voting_type, scope: "decidim.votings.votings_m.voting_type")
+          icon: resource_type_icon_key(voting_type),
+          text: t(voting_type, scope: "decidim.votings.votings_m.voting_type")
         }
       end
 
-      def start_date
-        return unless model.start_time
+      def status_item
+        return if period_status.blank?
 
-        model.start_time.to_date
+        { text: content_tag(:span, t(period_status, scope: "decidim.votings.votings_m.badge_name"), class: "label #{state_class}") }
+      end
+
+      def state_class
+        case period_status
+        when :ongoing
+          "success"
+        when :upcoming
+          "warning"
+        end
+      end
+
+      def start_date
+        return unless start_time
+
+        start_time.to_date
       end
 
       def end_date
-        return unless model.end_time
+        return unless end_time
 
-        model.end_time.to_date
+        end_time.to_date
       end
 
       def dates_metadata_item
