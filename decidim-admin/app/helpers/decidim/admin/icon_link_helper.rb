cfb68ffd17c2aeef31a32882f0b73ac7ac1a1611
Jorge <jorgeatgu@gmail.com>
2023-07-20 16:45:24 +0200

    add with_tooltip to icon_link_to

--- decidim-admin/app/helpers/decidim/admin/icon_link_helper.rb
+++ decidim-admin/app/helpers/decidim/admin/icon_link_helper.rb
@@ -15,15 +15,17 @@ module Decidim
       #             data   - This option can be used to add custom data attributes.
       #
       def icon_link_to(icon_name, link, title, options = {})
-        link_to(link,
-                method: options[:method],
-                class: "action-icon #{options[:class]}",
-                data: options[:data] || {},
-                title:,
-                target: options[:target]) do
-          content_tag(:span, data: { tooltip: true, disable_hover: false, click_open: false },
-                             title:) do
-            icon(icon_name, aria_label: title, role: "img")
+        with_tooltip(title, options.merge(class: "top")) do
+          link_to(link,
+                  method: options[:method],
+                  class: "action-icon #{options[:class]}",
+                  data: options[:data] || {},
+                  title:,
+                  target: options[:target]) do
+            content_tag(:span, data: { tooltip: true, disable_hover: false, click_open: false },
+                               title:) do
+              icon(icon_name, aria_label: title, role: "img")
+            end
           end
         end
       end
