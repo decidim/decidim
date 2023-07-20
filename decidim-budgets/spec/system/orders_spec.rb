6d76b3ede4e46735951ffe9894727c48498f6fb0
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-07-20 12:34:31 +0200

    Remove deprecated test
    The proposals card-l does not show the supports count in the metadata,
    so there is no difference when supports are enabled on proposals
    component or not

--- decidim-budgets/spec/system/orders_spec.rb
+++ decidim-budgets/spec/system/orders_spec.rb
@@ -656,38 +656,6 @@ describe "Orders", type: :system do
           expect(page).to have_content(proposal.votes.size)
         end
       end
-
-      context "with supports enabled" do
-        let(:proposal_component) do
-          create(:proposal_component, :with_votes_enabled, participatory_space: project.component.participatory_space)
-        end
-
-        let(:proposals) { create_list(:proposal, 1, :with_votes, component: proposal_component) }
-
-        it "shows the amount of supports" do
-          skip "REDESIGN_PENDING: This needs to be fixed after #10671 is being merged"
-          visit_budget
-          click_link translated(project.title)
-
-          expect(page.find('span[class="card--list__data__number"]')).to have_content("5")
-        end
-      end
-
-      context "with supports disabled" do
-        let(:proposal_component) do
-          create(:proposal_component, participatory_space: project.component.participatory_space)
-        end
-
-        let(:proposals) { create_list(:proposal, 1, :with_votes, component: proposal_component) }
-
-        it "does not show supports" do
-          skip "REDESIGN_PENDING: This needs to be fixed after #10671 is being merged"
-          visit_budget
-          click_link translated(project.title)
-
-          expect(page).not_to have_selector('span[class="card--list__data__number"]')
-        end
-      end
     end
   end
 
