376534c48c565b76946967c8607540e974843770
Jorge <jorgeatgu@gmail.com>
2023-07-20 17:44:46 +0200

    replace icons

--- decidim-blogs/app/helpers/decidim/blogs/admin/posts_helper.rb
+++ decidim-blogs/app/helpers/decidim/blogs/admin/posts_helper.rb
@@ -37,7 +37,7 @@ module Decidim
         def publish_data(published_at)
           data = {}
           if published_at > Time.current
-            data[:icon] = icon("clock", aria_label: t("decidim.blogs.admin.posts.index.not_published_yet"), role: "img")
+            data[:icon] = icon("time-line", aria_label: t("decidim.blogs.admin.posts.index.not_published_yet"), role: "img")
             data[:popup] = t("decidim.blogs.admin.posts.index.not_published_yet")
           end
           data
