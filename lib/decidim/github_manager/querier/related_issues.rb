033111e7803e53aa05fd7dc0fdf1893f5b0f7e3f
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2023-07-24 13:46:12 +0200

    Add script for checking the status of the backports (#11065)
    * Extract ByIssueId class on Decidim::GithubManager::Querier to introduce Base class
    
    * Introduce Decidim::GithubManager::Querier::ByLabel
    
    * Introduce Decidim::GithubManager::Querier::RelatedIssues
    
    * Add exclude_label feature in ByLabel class
    
    * Prefer usage of `id` key in RelatedIssues class for consistency with the other classes
    
    * Remove hardcoded days constant in ByLabel
    
    * Differentiate when a PR was merged or closed w/o merge
    
    * Clean-up spec
    
    * Fix spec
    
    * Fix yardocs
    
    * Move raising InvalidMetadataError exception logic to Base class
    
    * Add missing requires
    
    * Permit multiple excluded labels when querying by label
    
    * Add CSV and CLI backports reporters
    
    * Add CLI for backports_checker
    
    * Remove hardcoded version numbers in Reports
    
    * Remove CSV report from output
    
    * Fix rubocop offense for Rails/Date
    
    * Fix rubocop offense for Style/NegatedIfElseCondition
    
    * Fix rubocop offense for Layout/SpaceInsideArrayLiteralBrackets
    
    * Fix rubocop offense for Rails/TimeZoneAssignment
    
    * Add spec for checking that the backport branch is converted correctly
    
    * Prefer usage of subject instead of memoized manager variable for rspec example
    
    * Sanitize user-supplied parameter for preventing command-line injections
    
    * Check if the pull requests were merged before showing in the PRs to backport list
    
    * Improve docs on ByLabel class with all the conditions
    
    * Tune style of CLIReport
    
    * Fix bug when there are much events in the Timeline for RelatedIssues
    
    * Add colors explanations in CLI help
    
    * Fix documentation on :days_to_check_from
    
    It is on how many days since the PR was **updated**, not **created**,
    nor **merged**.
    
    * Add pagination support for GitHub querier
    
    * Check that the days_to_check_from complies with the merged_at date
    
    * Add instructions for how to use the auth token from the `gh` CLI tool
    
    * Fix rubocop offense for Style/PreferredHashMethods
    
    * Fix bug on CLI reporter when the title is really long
    
    * Improve specs for related_issues on CLI reporter
    
    * Fix bug on CLI reporter when the ID is short

---
+++ lib/decidim/github_manager/querier/related_issues.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require_relative "base"
+
+module Decidim
+  module GithubManager
+    module Querier
+      # Makes a GET request for the related issues of an Issue or Pull Request in GitHub
+      # Uses the Timeline events API endpoint
+      #
+      # @see https://docs.github.com/en/rest/issues/timeline?apiVersion=2022-11-28 GitHub API documentation
+      class RelatedIssues < Decidim::GithubManager::Querier::Base
+        def initialize(issue_id:, token:)
+          @issue_id = issue_id
+          @token = token
+        end
+
+        # Makes the GET request and parses the response of an Issue or Pull Request in GitHub
+        #
+        # @return [Hash]
+        def call
+          parse(json_response("https://api.github.com/repos/decidim/decidim/issues/#{@issue_id}/timeline"))
+        end
+
+        private
+
+        def headers
+          { per_page: 100 }
+        end
+
+        # Parses the response of an Issue or Pull Request in GitHub
+        #
+        # @return [Hash]
+        def parse(metadata)
+          references = metadata.select do |item|
+            item["event"] == "cross-referenced"
+          end
+          references.map do |item|
+            issue = item["source"]["issue"]
+
+            {
+              id: issue["number"],
+              title: issue["title"],
+              state: issue.dig("pull_request", "merged_at").nil? ? issue["state"] : "merged"
+            }
+          end
+        end
+      end
+    end
+  end
+end
