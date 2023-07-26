2525e17f00ae189f7408da2177f22226921e94c5
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2023-07-26 10:37:59 +0200

    Prepare v0.26.8 release (#11353)

--- decidim-comments/lib/decidim/comments/version.rb
+++ decidim-comments/lib/decidim/comments/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-comments version.
   module Comments
     def self.version
-      "0.26.7"
+      "0.26.8"
     end
   end
 end
--- decidim-meetings/lib/decidim/meetings/version.rb
+++ decidim-meetings/lib/decidim/meetings/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-meetings version.
   module Meetings
     def self.version
-      "0.26.7"
+      "0.26.8"
     end
   end
 end
