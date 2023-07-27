5fe95e1f7764b37dcc30a5f9dcfe616c6e51cbc7
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-07-27 16:19:27 +0200

    Allow passing an id to filter_text_for method

--- decidim-core/app/helpers/decidim/check_boxes_tree_helper.rb
+++ decidim-core/app/helpers/decidim/check_boxes_tree_helper.rb
@@ -130,8 +130,8 @@ module Decidim
       end
     end
 
-    def filter_text_for(translation)
-      content_tag(:span, translation).html_safe + content_tag(:span)
+    def filter_text_for(translation, id: nil)
+      content_tag(:span, translation, id:).html_safe + content_tag(:span)
     end
 
     private
