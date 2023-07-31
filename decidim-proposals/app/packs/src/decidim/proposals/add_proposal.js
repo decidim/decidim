84fb148b98adbf594a79c6e31095297e9ddce304
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-07-31 17:39:33 +0200

    Fix coordinates update in add_proposal js

--- decidim-proposals/app/packs/src/decidim/proposals/add_proposal.js
+++ decidim-proposals/app/packs/src/decidim/proposals/add_proposal.js
@@ -17,19 +17,22 @@ $(() => {
       longFieldName = getCoordinateInputName("longitude", $addressInputField, {})
     }
 
-    const ctrl = $("[data-decidim-map]").data("map-controller");
-    ctrl.setEventHandler("coordinates", (ev) => {
-      $(`input[name='${latFieldName}']`).val(ev.lat);
-      $(`input[name='${longFieldName}']`).val(ev.lng);
-    });
+    $("[data-decidim-map]").on("ready.decidim", (event) => {
+      const ctrl = $(event.target).data("map-controller");
 
-    attachGeocoding($addressInputField, null, (coordinates) => {
-      // Remove previous marker when user updates address in address field
-      ctrl.removeMarker();
-      ctrl.addMarker({
-        latitude: coordinates[0],
-        longitude: coordinates[1],
-        address: $addressInputField.val()
+      ctrl.setEventHandler("coordinates", (ev) => {
+        $(`input[name='${latFieldName}']`).val(ev.lat);
+        $(`input[name='${longFieldName}']`).val(ev.lng);
+      });
+
+      attachGeocoding($addressInputField, null, (coordinates) => {
+        // Remove previous marker when user updates address in address field
+        ctrl.removeMarker();
+        ctrl.addMarker({
+          latitude: coordinates[1],
+          longitude: coordinates[0],
+          address: $addressInputField.val()
+        });
       });
     });
   }
