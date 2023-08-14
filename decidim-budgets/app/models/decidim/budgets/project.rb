00ccc25896f2c97529887bbb7c0a284f944bca77
Fran Bolívar <francisco.bolivar@nazaries.com>
2023-08-14 11:18:25 +0200

    Fix ambiguous id column on projects query

--- decidim-budgets/app/models/decidim/budgets/project.rb
+++ decidim-budgets/app/models/decidim/budgets/project.rb
@@ -47,7 +47,7 @@ module Decidim
         # array. This is why we search for match ",2," instead to get the actual
         # position for ID 2.
         concat_ids = connection.quote(",#{ids.join(",")},")
-        order(Arel.sql("position(concat(',', id::text, ',') in #{concat_ids})"))
+        order(Arel.sql("position(concat(',', decidim_budgets_projects.id::text, ',') in #{concat_ids})"))
       end
 
       def self.log_presenter_class_for(_log)
--- decidim-budgets/spec/models/decidim/budgets/project_spec.rb
+++ decidim-budgets/spec/models/decidim/budgets/project_spec.rb
@@ -6,7 +6,7 @@ module Decidim::Budgets
   describe Project do
     subject { project }
 
-    let(:project) { create :project }
+    let(:project) { create(:project) }
 
     include_examples "has reference"
     include_examples "resourceable"
@@ -15,28 +15,39 @@ module Decidim::Budgets
     it { is_expected.to be_versioned }
 
     context "without a budget" do
-      let(:project) { build :project, budget: nil }
+      let(:project) { build(:project, budget: nil) }
 
       it { is_expected.not_to be_valid }
     end
 
     context "when the scope is from another organization" do
-      let(:scope) { create :scope }
-      let(:project) { build :project, scope: scope }
+      let(:scope) { create(:scope) }
+      let(:project) { build(:project, scope: scope) }
 
       it { is_expected.not_to be_valid }
     end
 
     context "when the category is from another organization" do
-      let(:category) { create :category }
-      let(:project) { build :project, category: category }
+      let(:category) { create(:category) }
+      let(:project) { build(:project, category: category) }
 
       it { is_expected.not_to be_valid }
     end
 
     describe ".ordered_ids" do
       let(:budget) { create(:budget, total_budget: 1_000_000) }
-      let(:projects) { create_list(:project, 50, budget: budget, budget_amount: 100_000) }
+      let(:category) { create(:category, participatory_space: budget.participatory_space) }
+      let(:projects) { create_list(:project, 50, budget: budget, budget_amount: 100_000, category: category) }
+      let(:test_ids) do
+        first = described_class.where(budget: budget).order(:id).pluck(:id)[0..3]
+        ids = described_class.where(budget: budget).pluck(:id).shuffle
+
+        # Put the first items at the end of the IDs array in order to get
+        # possibly "conflicting" matches for them at earlier array positions.
+        # As we have 50 projects, we should have IDs starting with 1, 2, 3 and 4
+        # which is why we put the first 4 items at the end.
+        (ids - first) + first
+      end
 
       before do
         # Reset the project IDs to start from 1 in order to get possibly
@@ -50,25 +61,20 @@ module Decidim::Budgets
       end
 
       it "returns the correctly ordered projects" do
-        first = described_class.where(budget: budget).order(:id).pluck(:id)[0..3]
-        ids = described_class.where(budget: budget).pluck(:id).shuffle
-
-        # Put the first items at the end of the IDs array in order to get
-        # possibly "conflicting" matches for them at earlier array positions.
-        # As we have 50 projects, we should have IDs starting with 1, 2, 3 and 4
-        # which is why we put the first 4 items at the end.
-        test_ids = (ids - first) + first
-
         expect(described_class.ordered_ids(test_ids).pluck(:id)).to eq(test_ids)
+      end
+
+      it "returns the correctly ordered projects after filtering by category" do
+        expect(described_class.with_any_category([category.id]).ordered_ids(test_ids).pluck(:id)).to eq(test_ids)
       end
     end
 
     describe "#orders_count" do
-      let(:project) { create :project, budget_amount: 75_000_000 }
-      let(:order) { create :order, budget: project.budget }
-      let(:unfinished_order) { create :order, budget: project.budget }
-      let!(:line_item) { create :line_item, project: project, order: order }
-      let!(:line_item_1) { create :line_item, project: project, order: unfinished_order }
+      let(:project) { create(:project, budget_amount: 75_000_000) }
+      let(:order) { create(:order, budget: project.budget) }
+      let(:unfinished_order) { create(:order, budget: project.budget) }
+      let!(:line_item) { create(:line_item, project: project, order: order) }
+      let!(:line_item_1) { create(:line_item, project: project, order: unfinished_order) }
 
       it "return number of finished orders for this project" do
         order.reload.update!(checked_out_at: Time.current)
@@ -85,7 +91,7 @@ module Decidim::Budgets
     end
 
     describe "#selected?" do
-      let(:project) { create :project, selected_at: selected_at }
+      let(:project) { create(:project, selected_at: selected_at) }
 
       context "when selected_at is blank" do
         let(:selected_at) { nil }
