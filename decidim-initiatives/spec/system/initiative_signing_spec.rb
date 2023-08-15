fb5a7405d7bc838f8f2845dc38de97c8c7482c5a
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-08-15 18:05:17 +0300

    Add date_of_birth to initiatives signatures

--- decidim-initiatives/spec/system/initiative_signing_spec.rb
+++ decidim-initiatives/spec/system/initiative_signing_spec.rb
@@ -185,9 +185,7 @@ describe "Initiative signing", type: :system do
     if has_content?("Complete your data")
       fill_in :initiatives_vote_name_and_surname, with: confirmed_user.name
       fill_in :initiatives_vote_document_number, with: "012345678X"
-      select 30.years.ago.year.to_s, from: :initiatives_vote_date_of_birth_1i
-      select "January", from: :initiatives_vote_date_of_birth_2i
-      select "1", from: :initiatives_vote_date_of_birth_3i
+      fill_in :initiatives_vote_date_of_birth, with: 30.years.ago.strftime("01/01/%Y")
       fill_in :initiatives_vote_postal_code, with: "01234"
 
       click_button "Continue"
