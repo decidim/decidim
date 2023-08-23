902f0a152943d5c745bb88942d0bc78e1fc968f4
Francisco Bolívar <francisco.bolivar@nazaries.com>
2023-08-23 09:58:30 +0200

    Show different text for the Register CTA when the user is already registered

--- decidim-conferences/spec/system/conference_registrations_spec.rb
+++ decidim-conferences/spec/system/conference_registrations_spec.rb
@@ -93,6 +93,14 @@ describe "Conference registrations", type: :system do
         login_as user, scope: :user
       end
 
+      it "shows register button" do
+        visit_conference
+
+        within ".conference__box" do
+          expect(page).to have_content "Register"
+        end
+      end
+
       it "they can join the conference" do
         visit_conference_registration_types
 
@@ -117,6 +125,14 @@ describe "Conference registrations", type: :system do
     before do
       create(:conference_registration, conference:, user:, registration_type:)
       login_as user, scope: :user
+    end
+
+    it "shows manage registration button" do
+      visit_conference
+
+      within ".conference__box" do
+        expect(page).to have_content "Manage registration"
+      end
     end
 
     it "they can leave the conference" do
