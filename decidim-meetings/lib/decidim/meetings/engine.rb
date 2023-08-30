22f65fa8a911898dbd3d490b0435d44894118898
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-08-30 10:30:36 +0300

    Remove unused view hook for `:upcoming_meeting_for_card` (#11543)

--- decidim-meetings/lib/decidim/meetings/engine.rb
+++ decidim-meetings/lib/decidim/meetings/engine.rb
@@ -65,22 +65,6 @@ module Decidim
           view_context.cell("decidim/meetings/highlighted_meetings", view_context.current_participatory_space)
         end
 
-        # This view hook is used in card cells. It renders the next upcoming
-        # meeting for the given participatory space.
-        Decidim.view_hooks.register(:upcoming_meeting_for_card, priority: Decidim::ViewHooks::LOW_PRIORITY) do |view_context|
-          published_components = Decidim::Component.where(participatory_space: view_context.current_participatory_space).published
-          upcoming_meeting = Decidim::Meetings::Meeting.where(component: published_components).published.upcoming.order(:start_time, :end_time).first
-
-          next unless upcoming_meeting
-
-          view_context.render(
-            partial: "decidim/participatory_spaces/upcoming_meeting_for_card.html",
-            locals: {
-              upcoming_meeting:
-            }
-          )
-        end
-
         Decidim.view_hooks.register(:conference_venues, priority: Decidim::ViewHooks::HIGH_PRIORITY) do |view_context|
           published_components = Decidim::Component.where(participatory_space: view_context.current_participatory_space).published
           meetings = Decidim::Meetings::Meeting.where(component: published_components).group_by(&:address)
