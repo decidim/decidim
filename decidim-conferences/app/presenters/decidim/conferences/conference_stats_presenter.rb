83167a881a9cd5849b99bf2edfeb8bd30f7a0861
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-08-31 08:26:18 +0200

    Remove duplication from stats presenters

--- decidim-conferences/app/presenters/decidim/conferences/conference_stats_presenter.rb
+++ decidim-conferences/app/presenters/decidim/conferences/conference_stats_presenter.rb
@@ -2,7 +2,7 @@
 
 module Decidim
   module Conferences
-    # A presenter to render statistics in the homepage.
+    # A presenter to render statistics in a Conference.
     class ConferenceStatsPresenter < Decidim::StatsPresenter
       include IconHelper
 
@@ -10,31 +10,12 @@ module Decidim
         __getobj__.fetch(:conference)
       end
 
-      # Public: Render a collection of primary stats.
-      def highlighted
-        highlighted_stats = component_stats(priority: StatsRegistry::HIGH_PRIORITY)
-        highlighted_stats.concat(component_stats(priority: StatsRegistry::MEDIUM_PRIORITY))
-        highlighted_stats.concat(comments_stats(:conferences))
-        highlighted_stats = highlighted_stats.reject(&:empty?)
-        highlighted_stats = highlighted_stats.reject { |_manifest, _name, data| data.zero? }
-        grouped_highlighted_stats = highlighted_stats.group_by(&:first)
-
-        statistics(grouped_highlighted_stats)
+      def participatory_space
+        conference
       end
 
-      private
-
-      def component_stats(conditions)
-        Decidim.component_manifests.map do |component_manifest|
-          component_manifest.stats
-                            .filter(conditions)
-                            .with_context(published_components)
-                            .map { |name, data| [component_manifest.name, name, data] }.flatten
-        end
-      end
-
-      def published_components
-        @published_components ||= Component.where(participatory_space: conference).published
+      def participatory_space_sym
+        :conferences
       end
     end
   end
