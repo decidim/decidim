03f7a30f230dc27a82cf174ecc94e3d5ff51b722
Jorge <jorgeatgu@gmail.com>
2023-09-01 20:14:07 +0200

    trigger click if the button contains "question-error"

--- decidim-forms/app/packs/src/decidim/forms/admin/collapsible_questions.js
+++ decidim-forms/app/packs/src/decidim/forms/admin/collapsible_questions.js
@@ -4,35 +4,16 @@
   setTimeout(() => {
     [...getButtons].forEach((button) => {
       if(button.classList.contains("question-error")){
-        button.setAttribute('aria-expanded','true')
-      }
-    })
-
-    document.querySelectorAll(".panel-question-card ").forEach((panel) => {
-      console.log("panel", panel);
-      if(panel.classList.contains("panel-error")){
-        panel.setAttribute('aria-hidden','false')
+        button.click()
       }
     })
   },100)
 
   $("button.collapse-all").on("click", () => {
-    [...getButtons].forEach((button) => {
-      button.setAttribute("aria-expanded", "false");
-    })
-
-    document.querySelectorAll(".panel-question-card ").forEach((panel) => {
-      panel.setAttribute("aria-hidden", "true");
-    })
+    $("[id$=field]").find("button.question--collapse[aria-expanded='true']").click()
   });
 
   $("button.expand-all").on("click", () => {
-    [...getButtons].forEach((button) => {
-      button.setAttribute("aria-expanded", "true");
-    })
-    document.querySelectorAll(".panel-question-card ").forEach((panel) => {
-      panel.setAttribute("aria-hidden", "false");
-    })
-
+    $("[id$=field]").find("button.question--collapse[aria-expanded='false']").click()
   });
 })(window);
