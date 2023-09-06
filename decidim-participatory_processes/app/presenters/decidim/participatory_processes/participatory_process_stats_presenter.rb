6ae6c507385fde4dba5bccec7a29ea14dd5ae8e2
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2023-09-06 17:42:00 +0200

    Remove duplication from stats presenters (#11551)
    * Remove duplication from stats presenters
    
    * Fix specs on change of main method
    
    With the refactor, the method with the collection in the case of
    conferences is being change from `highlighted` to `collections`, so
    we also change the specs accordingly.
    
    * Apply suggestions from code review
    
    * Remove unecessary method
    * Implement endless methods
    * Make them private
    
    * Remove unecessary method
    
    Suggested by code review
    
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>
    
    * Remove duplication from ParticipatoryProcessGroupStatsPresenter
    
    * Rename reference to participatory_space
    
    ---------
    
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>

--- decidim-participatory_processes/app/presenters/decidim/participatory_processes/participatory_process_stats_presenter.rb
+++ decidim-participatory_processes/app/presenters/decidim/participatory_processes/participatory_process_stats_presenter.rb
@@ -2,63 +2,15 @@
 
 module Decidim
   module ParticipatoryProcesses
-    # A presenter to render statistics in the homepage.
+    # A presenter to render statistics in a Participatory Process.
     class ParticipatoryProcessStatsPresenter < Decidim::StatsPresenter
       include Decidim::IconHelper
 
-      def participatory_process
-        __getobj__.fetch(:participatory_process)
-      end
-
-      # Public: returns a collection of stats (Hash) for the Process Home.
-      def collection
-        highlighted_stats = process_participants_stats
-        highlighted_stats.concat(process_followers_stats(priority: StatsRegistry::HIGH_PRIORITY))
-        highlighted_stats.concat(component_stats(priority: StatsRegistry::HIGH_PRIORITY))
-        highlighted_stats.concat(component_stats(priority: StatsRegistry::MEDIUM_PRIORITY))
-        highlighted_stats.concat(comments_stats(:participatory_processes))
-        highlighted_stats = highlighted_stats.reject(&:empty?)
-        highlighted_stats = highlighted_stats.reject { |_stat_manifest, _stat_title, stat_number| stat_number.zero? }
-        grouped_highlighted_stats = highlighted_stats.group_by(&:first)
-
-        statistics(grouped_highlighted_stats)
-      end
-
-      def card_collection
-        card_stats = Decidim.stats.only([:followers_count, :votings_count])
-                            .filter(priority: StatsRegistry::HIGH_PRIORITY)
-                            .with_context(participatory_process)
-                            .map { |stat_title, stat_number| [participatory_process.manifest.name, stat_title, stat_number] }
-
-        statistics(card_stats.group_by(&:first))
-      end
-
       private
 
-      def process_participants_stats
-        Decidim.stats.only([:participants_count]).with_context(participatory_process)
-               .map { |stat_title, stat_number| [participatory_process.manifest.name, stat_title, stat_number] }
-      end
+      def participatory_space = __getobj__.fetch(:participatory_process)
 
-      def component_stats(conditions)
-        Decidim.component_manifests.map do |component_manifest|
-          component_manifest.stats.except([:proposals_accepted])
-                            .filter(conditions)
-                            .with_context(published_components)
-                            .map { |stat_title, stat_number| [component_manifest.name, stat_title, stat_number] }.flatten
-        end
-      end
-
-      def process_followers_stats(conditions)
-        Decidim.stats.only([:followers_count])
-               .filter(conditions)
-               .with_context(participatory_process)
-               .map { |stat_title, stat_number| [participatory_process.manifest.name, stat_title, stat_number] }
-      end
-
-      def published_components
-        @published_components ||= Component.where(participatory_space: participatory_process).published
-      end
+      def participatory_space_sym = :participatory_processes
     end
   end
 end
