c767f2250e4b0f54200b9cd34d77c1cf0641880a
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-09-19 15:13:39 +0200

    Password confirm validation

--- decidim-core/app/forms/decidim/account_form.rb
+++ decidim-core/app/forms/decidim/account_form.rb
@@ -24,7 +24,7 @@ module Decidim
     validates :nickname, presence: true, format: { with: Decidim::User::REGEXP_NICKNAME }
 
     validates :nickname, length: { maximum: Decidim::User.nickname_max_length, allow_blank: true }
-    validates :password, confirmation: true
+    validates :password, confirmation: { message: '"Confirm your password" does not match Password' }
     validates :password, password: { name: :name, email: :email, username: :nickname }, if: -> { password.present? }
     validates :password_confirmation, presence: true, if: :password_present
     validates :avatar, passthru: { to: Decidim::User }
