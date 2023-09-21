17fd821e34b7de9fe2f04291ee618d795683881e
Hugoren Martinako <aumpfbahn@gmail.com>
2023-09-21 18:00:43 +0200

    fix spelling

--- decidim-dev/lib/decidim/dev/test/rspec_support/tom_select.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/tom_select.rb
@@ -5,9 +5,9 @@ module Capybara
   module TomSelect
     # A helper for Capyabara tests that need to set values from a tom-select.js input.
     #
-    # This is a really hacky approach using execute_javascript, but it works. Not sure if there's
+    # This is a really hacky approach using execute_javascript, but it works. Not sure if there is
     # a better way, we could try actually interacting with the on-screen tom-select-provided UI,
-    # but we're taking the easy way out for now.
+    # but we are taking the easy way out for now.
     #
     # @param option_id can be the `id` value of an option in the select, OR for select multiple inputs,
     #   can be an array of such IDs.
