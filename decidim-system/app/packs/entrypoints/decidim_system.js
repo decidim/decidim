7335b709760fa580c494cd671cb8d2cc983a50b8
Hugoren Martinako <aumpfbahn@gmail.com>
2023-09-28 18:03:37 +0200

    decidim-system CSS

--- decidim-system/app/packs/entrypoints/decidim_system.js
+++ decidim-system/app/packs/entrypoints/decidim_system.js
@@ -1,7 +1,7 @@
 import "src/decidim/direct_uploads/upload_field"
 
-// CSS
-import "stylesheets/decidim/system/redesigned_application.scss"
-
 // Images
 require.context("../images", true)
+
+// CSS
+import "stylesheets/decidim/system/application.scss"
