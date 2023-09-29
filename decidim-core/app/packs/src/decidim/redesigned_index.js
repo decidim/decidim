57799762a0a82959e2f8fb2ffb1ea2ca63c68149
Francisco Bolívar <francisco.bolivar@nazaries.com>
2023-09-29 16:04:53 +0200

    Scroll down to show dropdown menu

--- decidim-core/app/packs/src/decidim/redesigned_index.js
+++ decidim-core/app/packs/src/decidim/redesigned_index.js
@@ -36,6 +36,7 @@ import "./append_elements"
 import "./user_registrations"
 import "./account_form"
 // import "./dropdowns_menus" -- deprecated
+import "./dropdown_scrollable"
 import "./append_redirect_url_to_modals"
 import "./form_attachments"
 import "./form_remote"
