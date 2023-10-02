24d5e0ea160ae35c0725d85b985f968bffa5d6a6
Laura Jaime <laura.jv@coditramuntana.com>
2023-10-02 16:21:58 +0200

    Refactor with events

--- decidim-forms/app/commands/decidim/forms/answer_questionnaire.rb
+++ decidim-forms/app/commands/decidim/forms/answer_questionnaire.rb
@@ -24,7 +24,6 @@ module Decidim
 
         with_events do
           answer_questionnaire
-          send_confirmation_email
         end
 
         if @errors
@@ -43,8 +42,9 @@ module Decidim
         {
           resource: questionnaire,
           extra: {
-            event_author: form.current_user,
-            locale:
+            session_token: form.context.session_token,
+            questionnaire:,
+            event_author: current_user
           }
         }
       end
@@ -112,17 +112,6 @@ module Decidim
 
       def user_already_answered?
         questionnaire.answered_by?(current_user || form.context.session_token)
-      end
-
-      # This method send confirmation email with answers to user when a questionnaire is answered.
-      def send_confirmation_email
-        component = @questionnaire.questionnaire_for.component
-        answers = Decidim::Forms::QuestionnaireUserAnswers.for(@questionnaire)
-        user_answers = answers.select { |a| a.first.session_token == session_token }
-
-        if component.manifest_name == "surveys" && answers.present?
-          Decidim::Surveys::SurveyConfirmationMailer.confirmation(current_user, questionnaire, user_answers).deliver_later
-        end
       end
     end
   end
--- decidim-forms/spec/commands/decidim/forms/answer_questionnaire_spec.rb
+++ decidim-forms/spec/commands/decidim/forms/answer_questionnaire_spec.rb
@@ -5,32 +5,16 @@ require "spec_helper"
 module Decidim
   module Forms
     describe AnswerQuestionnaire do
-      def tokenize(id)
-        "fake-hash-for-#{id}"
-      end
-
-      let(:current_organization) { create(:organization) }
-      let(:current_user) { create(:user, organization: current_organization) }
-      let(:session_id) { "session-string" }
-      let(:session_token) { tokenize(current_user&.id || session_id) }
-      let(:remote_ip) { "1.1.1.1" }
-      let(:ip_hash) { tokenize(remote_ip) }
-      let(:request) do
-        double(
-          session: { session_id: session_id },
-          remote_ip: remote_ip
+      let(:command) { described_class.new(form, current_user, questionnaire) }
+      let(:form) do
+        QuestionnaireForm.from_params(
+          form_params
+        ).with_context(
+          current_organization:,
+          session_token:,
+          ip_hash:
         )
       end
-
-      let(:participatory_process) { create(:participatory_process, organization: current_organization) }
-      let(:questionnaire) { create(:questionnaire, questionnaire_for: participatory_process) }
-      let(:question_1) { create(:questionnaire_question, questionnaire: questionnaire) }
-      let(:question_2) { create(:questionnaire_question, questionnaire: questionnaire) }
-      let(:question_3) { create(:questionnaire_question, questionnaire: questionnaire) }
-      let(:answer_options) { create_list(:answer_option, 5, question: question_2) }
-      let(:answer_option_ids) { answer_options.pluck(:id).map(&:to_s) }
-      let(:matrix_rows) { create_list(:question_matrix_row, 3, question: question_2) }
-      let(:matrix_row_ids) { matrix_rows.pluck(:id).map(&:to_s) }
       let(:form_params) do
         {
           "responses" => [
@@ -57,16 +41,33 @@ module Decidim
           "tos_agreement" => "1"
         }
       end
-      let(:form) do
-        QuestionnaireForm.from_params(
-          form_params
-        ).with_context(
-          current_organization: current_organization,
-          session_token: session_token,
-          ip_hash: ip_hash
+      let(:matrix_row_ids) { matrix_rows.pluck(:id).map(&:to_s) }
+      let(:matrix_rows) { create_list(:question_matrix_row, 3, question: question2) }
+      let(:answer_option_ids) { answer_options.pluck(:id).map(&:to_s) }
+      let(:answer_options) { create_list(:answer_option, 5, question: question2) }
+      let(:question3) { create(:questionnaire_question, questionnaire:) }
+      let(:question2) { create(:questionnaire_question, questionnaire:) }
+      let(:question1) { create(:questionnaire_question, questionnaire:) }
+      let(:questionnaire) { create(:questionnaire, questionnaire_for: participatory_process) }
+      let(:participatory_process) { create(:participatory_process, organization: current_organization) }
+      let(:request) do
+        double(
+          session: { session_id: },
+          remote_ip:
         )
       end
-      let(:command) { described_class.new(form, current_user, questionnaire) }
+      let(:ip_hash) { tokenize(remote_ip) }
+      let(:remote_ip) { "1.1.1.1" }
+      let(:session_token) { tokenize(current_user&.id || session_id) }
+      let(:session_id) { "session-string" }
+      let(:current_user) { create(:user, organization: current_organization) }
+      let(:current_organization) { create(:organization) }
+
+      it_behaves_like "fires an ActiveSupport::Notification event", "decidim.forms.answer_questionnaire:after"
+
+      def tokenize(id)
+        "fake-hash-for-#{id}"
+      end
 
       describe "when the form is invalid" do
         before do
@@ -180,6 +181,93 @@ module Decidim
               expect { command.call }.to broadcast(:invalid)
             end
           end
+        end
+
+        context "when display_conditions are not mandatory on the same question but are fulfilled" do
+          let(:questionnaire_conditionned) { create(:questionnaire, questionnaire_for: participatory_process) }
+          let!(:option1) { create(:answer_option, question: condition_question) }
+          let!(:option2) { create(:answer_option, question: condition_question) }
+          let!(:option3) { create(:answer_option, question: condition_question) }
+          let!(:condition_question) do
+            create(
+              :questionnaire_question,
+              questionnaire: questionnaire_conditionned,
+              mandatory: false,
+              question_type: "single_option"
+            )
+          end
+          let!(:question) { create(:questionnaire_question, questionnaire: questionnaire_conditionned, question_type: "short_answer") }
+          let!(:display_condition) { create(:display_condition, question:, condition_question:, condition_type: :equal, answer_option: option1, mandatory: false) }
+          let!(:display_condition2) { create(:display_condition, question:, condition_question:, condition_type: :equal, answer_option: option3, mandatory: false) }
+          let(:form_params) do
+            {
+              "responses" => [
+                {
+                  "choices" => [
+                    { "body" => option1.body, "answer_option_id" => option1.id }
+                  ],
+                  "question_id" => condition_question.id
+                },
+                {
+                  "body" => "answer_test",
+                  "question_id" => question.id
+                }
+              ],
+              "tos_agreement" => "1"
+            }
+          end
+          let(:command) { described_class.new(form, current_user, questionnaire_conditionned) }
+
+          it "broadcasts ok" do
+            expect { command.call }.to broadcast(:ok)
+          end
+
+          it "creates a questionnaire answer for each question answered" do
+            expect do
+              command.call
+            end.to change(Answer, :count).by(2)
+            expect(Answer.all.map(&:questionnaire)).to eq([questionnaire_conditionned, questionnaire_conditionned])
+          end
+
+          it "creates answers with the correct information" do
+            command.call
+
+            expect(Answer.first.choices.first.answer_option).to eq(option1)
+            expect(Answer.second.body).to eq("answer_test")
+          end
+        end
+
+        context "when questionnaire component is a survey" do
+          let(:manifest_name) { "surveys" }
+          let(:manifest) { Decidim.find_component_manifest(manifest_name) }
+
+          let!(:component) do
+            create(:component,
+                   manifest:,
+                   participatory_space: participatory_process,
+                   published_at: nil)
+          end
+          let!(:survey) { create(:survey, component:, questionnaire:) }
+
+          let(:answers) do
+            survey.questionnaire.questions.map do |question|
+              create(:answer, questionnaire: survey.questionnaire, question:, user: current_user)
+            end
+          end
+
+          let(:event_arguments) do
+            {
+              resource: questionnaire,
+              extra: {
+                session_token:,
+                questionnaire:,
+                event_author: current_user
+              }
+            }
+          end
+          let(:mailer) { double :mailer }
+
+          it_behaves_like "fires an ActiveSupport::Notification event", "decidim.forms.answer_questionnaire:after"
         end
       end
 
