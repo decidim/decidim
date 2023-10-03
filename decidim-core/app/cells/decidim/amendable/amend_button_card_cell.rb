cda191610eb542843e7783a63bbc6ca619ec7b3f
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-10-03 11:41:03 +0200

    Recover removed method

--- decidim-core/app/cells/decidim/amendable/amend_button_card_cell.rb
+++ decidim-core/app/cells/decidim/amendable/amend_button_card_cell.rb
@@ -10,6 +10,10 @@ module Decidim
         model.model_name.human
       end
 
+      def current_component
+        model.component
+      end
+
       def new_amend_path
         decidim.new_amend_path(amendable_gid: model.to_sgid.to_s)
       end
