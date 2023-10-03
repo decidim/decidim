a461841ee05df9f1be47473664336a9018b662f8
Ivan Vergés <ivan@pokecode.net>
2023-10-03 14:41:21 +0200

    use latest bulletin packages

--- decidim_app-design/config/webpack/test.js
+++ decidim_app-design/config/webpack/test.js
@@ -2,6 +2,6 @@
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development"
 
-const webpackConfig = require("./base")
+const webpackConfig = require("./webpack.config")
 
 module.exports = webpackConfig
