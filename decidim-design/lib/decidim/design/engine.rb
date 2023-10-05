7e31b7c335d1523d08905cc02ad39d1aeae1b1ef
Hugoren Martinako <aumpfbahn@gmail.com>
2023-10-05 17:37:11 +0200

    routes does not work on its own

--- decidim-design/lib/decidim/design/engine.rb
+++ decidim-design/lib/decidim/design/engine.rb
@@ -2,7 +2,6 @@
 
 require "rails"
 require "active_support/all"
-
 require "decidim/core"
 
 module Decidim
@@ -13,25 +12,27 @@ module Decidim
     class Engine < ::Rails::Engine
       isolate_namespace Decidim::Design
 
-      routes do
-        namespace :design do
-          namespace :components do
-            get "forms", to: "forms#index"
-            get "cards", to: "cards#index"
-            get "spacing", to: "spacing#index"
+      initializer "decidim_design.mount_routes" do |_app|
+        Decidim::Core::Engine.routes do
+          namespace :design do
+            namespace :components do
+              get "forms", to: "forms#index"
+              get "cards", to: "cards#index"
+              get "spacing", to: "spacing#index"
+            end
+
+            namespace :foundations do
+              get "accessibility", to: "accessibility#index"
+              get "color", to: "color#index"
+              get "iconography", to: "iconography#index"
+              get "layout", to: "layout#index"
+              get "typography", to: "typography#index"
+            end
+
+            get "home", to: "home#index"
+
+            root to: "home#index"
           end
-
-          namespace :foundations do
-            get "accessibility", to: "accessibility#index"
-            get "color", to: "color#index"
-            get "iconography", to: "iconography#index"
-            get "layout", to: "layout#index"
-            get "typography", to: "typography#index"
-          end
-
-          get "home", to: "home#index"
-
-          root to: "home#index"
         end
       end
 
