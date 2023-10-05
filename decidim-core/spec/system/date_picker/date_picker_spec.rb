17280e18275115be52379b4ff3106c7d539ee430
Joonas <joonas.aapro@mainiotech.fi>
2023-10-05 08:32:30 +0300

    Remove extra app-design package

--- decidim-core/spec/system/date_picker/date_picker_spec.rb
+++ decidim-core/spec/system/date_picker/date_picker_spec.rb
@@ -2,8 +2,8 @@
 
 require "spec_helper"
 
-describe "Data consent scripts", type: :system do
-  let(:organization) { create(:organization) }
+describe "Datepicker", type: :system do
+  let!(:organization) { create(:organization) }
 
   let(:template_class) do
     Class.new(ActionView::Base) do
@@ -17,12 +17,11 @@ describe "Data consent scripts", type: :system do
   let(:html_document) do
     template.instance_eval do
       js_config = { icons_path: asset_pack_path("media/images/icons.svg") }
-
       <<~HTML.strip
         <!doctype html>
         <html lang="en">
         <head>
-          <title>Accessibility Test</title>
+          <title>Datepicker Test</title>
           #{stylesheet_pack_tag "redesigned_decidim_core"}
           #{stylesheet_pack_tag "decidim_dev"}
           #{javascript_pack_tag "redesigned_decidim_core", "decidim_dev", defer: false}
@@ -30,12 +29,12 @@ describe "Data consent scripts", type: :system do
         <!-- Add some line breaks so that the "WAI WCAG" notification does not block screenshots -->
         <br><br><br><br><br>
         <body>
-          <div class="footer">
+          <div>
             <input type="datetime-local" id="example_input">
             <button type="submit" name="commit" class="button button_sm md:button__lg button__secondary">Create</button>
           </div>
           <script>
-          Decidim.config.set(#{js_config.to_json});
+            Decidim.config.set(#{js_config.to_json});
           </script>
         </body>
         </html>
@@ -58,7 +57,7 @@ describe "Data consent scripts", type: :system do
     Rails.application.reload_routes!
   end
 
-  context "when using pickers" do
+  context "when using date picker" do
     it "fills input fields correctly" do
       find(".calendar_button").click
       find('span > input[name="year"]').set("1994")
@@ -75,6 +74,56 @@ describe "Data consent scripts", type: :system do
       expect(page).to have_field("example_input_time", with: "01:59")
       expect(page).to have_field("example_input_date", with: "20/01/1994")
       expect(page).to have_field("example_input", with: "1994-01-20T01:59", visible: :all)
+    end
+
+    context "when opening datepicker for the first time" do
+      it "has disabled select button" do
+        find(".calendar_button").click
+        expect(page).to have_button("Select", disabled: true)
+      end
+
+      context "when choosing a date" do
+        it "enables the select button" do
+          find(".calendar_button").click
+          find("td > span", text: "20", match: :first).click
+          expect(page).to have_button("Select", disabled: false)
+        end
+      end
+    end
+
+    context "when opening datepicker after having picked a date before" do
+      it "has the previously picked date selected" do
+        find(".calendar_button").click
+        find('span > input[name="year"]').set("1994")
+        select("January", from: "month").select_option
+        find("td > span", text: "20", match: :first).click
+        find(".pick_calendar").click
+        find(".calendar_button").click
+        element = find("td.wc-datepicker__date--selected")
+        expect(element).to have_content("20")
+        expect(page).to have_button("Select", disabled: true)
+      end
+    end
+
+    context "when changing month" do
+      it "disables select button even if date was picked" do
+        find(".calendar_button").click
+        find("td > span", text: "20", match: :first).click
+        expect(page).to have_button("Select", disabled: false)
+        select("January", from: "month").select_option
+        expect(page).to have_button("Select", disabled: true)
+      end
+    end
+  end
+
+  context "when using date input" do
+    context "when typing" do
+      it "only accepts numbers, slash and period" do
+        fill_in_datepicker :example_input_date, with: "sad221"
+        input = find("#example_input_date")
+        expect(input).not_to have_content("abktp")
+        expect(input).to have_content("1/2/4")
+      end
     end
   end
 end
