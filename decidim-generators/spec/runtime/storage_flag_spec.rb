1fc8d17aea3611ee0531e8ad62619de7970dc4fb
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-10-11 01:17:39 +0300

    Refactor CI pipelines

--- decidim-generators/spec/runtime/storage_flag_spec.rb
+++ decidim-generators/spec/runtime/storage_flag_spec.rb
@@ -54,38 +54,6 @@ module Decidim
 
       after { FileUtils.rm_rf(test_app) }
 
-      context "without flags" do
-        let(:command) { "decidim #{test_app}" }
-
-        it_behaves_like "a new production application"
-        it_behaves_like "an application with configurable env vars"
-      end
-
-      context "with --edge flag" do
-        let(:command) { "decidim --edge #{test_app}" }
-
-        it_behaves_like "a new production application"
-      end
-
-      context "with --branch flag" do
-        let(:default_branch) { Decidim::Generators.edge_git_branch }
-        let(:command) { "decidim --branch #{default_branch} #{test_app}" }
-
-        it_behaves_like "a new production application"
-      end
-
-      context "with --path flag" do
-        let(:command) { "decidim --path #{repo_root} #{test_app}" }
-
-        it_behaves_like "a new production application"
-      end
-
-      context "with a development application" do
-        let(:command) { "decidim --path #{repo_root} #{test_app} --recreate_db --seed_db --demo" }
-
-        it_behaves_like "a new development application"
-      end
-
       context "with wrong --storage providers" do
         let(:command) { "decidim #{test_app} --storage s3,gcs,assure" }
 
@@ -96,47 +64,6 @@ module Decidim
         let(:command) { "decidim #{test_app} --storage s3,gcs,azure" }
 
         it_behaves_like "an application with cloud storage gems"
-      end
-
-      context "with --queue providers" do
-        let(:command) { "decidim #{test_app} --storage s3 --queue sidekiq" }
-
-        it_behaves_like "an application with storage and queue gems"
-      end
-    end
-
-    context "with a component" do
-      let(:test_component) { "dummy_component" }
-      let(:command) { "decidim --component #{test_component}" }
-      let(:semver_friendly_version) { Decidim::GemManager.semver_friendly_version(Decidim.version) }
-      let(:npm_package_version) { "^#{semver_friendly_version}" }
-
-      after { FileUtils.rm_rf("decidim-module-#{test_component}") }
-
-      it "succeeds" do
-        expect(result[1]).to be_success, result[0]
-
-        expect(JSON.parse(File.read("decidim-module-#{test_component}/package.json"))).to eq(
-          "name" => "decidim-#{test_component}",
-          "version" => "0.0.1",
-          "description" => "",
-          "private" => true,
-          "license" => "AGPL-3.0",
-          "scripts" => {
-            "lint" => "eslint -c .eslintrc.json --no-error-on-unmatched-pattern --ignore-pattern app/packs/vendor --ext .js app/packs",
-            "stylelint" => "stylelint app/packs/**/*.scss"
-          },
-          "dependencies" => {
-            "@decidim/browserslist-config" => npm_package_version,
-            "@decidim/webpacker" => npm_package_version
-          },
-          "devDependencies" => {
-            "@decidim/dev" => npm_package_version,
-            "@decidim/eslint-config" => npm_package_version,
-            "@decidim/stylelint-config" => npm_package_version
-          },
-          "browserslist" => ["extends @decidim/browserslist-config"]
-        )
       end
     end
 
