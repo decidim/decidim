26626ae1950aad6870ccb8e12129e01f9d86b7fb
Elvia Benedith <ejbenedith@gmail.com>
2023-10-23 13:08:13 +0200

    add more scenarios to new proposals test

--- decidim-proposals/spec/shared/manage_proposals_settings_example.rb
+++ decidim-proposals/spec/shared/manage_proposals_settings_example.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 shared_examples "manage settings" do
-  include Decidim::SanitizeHelper
   let(:rich_text_editor_enabled) { true }
   let(:organization) { create(:organization, rich_text_editor_in_public_views: rich_text_editor_enabled) }
-  let(:body_template) { "<p>test</p>" }
   before do
-    component.settings[:new_proposal_body_template] = body_template
-    component.save!
     within_admin_menu do
       click_link "Components"
     end
@@ -30,11 +26,6 @@ shared_examples "manage settings" do
       within ".new_proposal_body_template_container" do
         expect(page).not_to have_css(".editor-toolbar")
       end
-    end
-
-    it "does not display string tags in the body template" do
-      expect(decidim_sanitize(body_template, strip_tags: true)).not_to include("<p>")
-      expect(decidim_sanitize(body_template, strip_tags: true)).not_to include("</p>")
     end
   end
 end
