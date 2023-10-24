14811b1e92e56edf2b461d10b2ad0f2809538ac0
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2023-10-24 10:52:36 +0200

    Remove legacy design app (#11697)
    * Remove legacy decidim_app-design
    
    * Remove leftover file from heroku
    
    * Add more removed files from #9670
    
    * Add GH Action for verifying labels in Pull Requests

--- decidim_app-design/packages/webpacker/index.js
+++
@@ -1,9 +0,0 @@
-const { generateWebpackConfig, ...restOpts } = require("shakapacker");
-const webpackConfig = generateWebpackConfig()
-
-const overrideConfig = require("./src/override-config");
-
-module.exports = {
-  webpackConfig: overrideConfig(webpackConfig),
-  ...restOpts
-};
