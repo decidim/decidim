5142fa8e21cae9ede0432a33ca201dd52984de72
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-10-26 17:06:41 +0200

    Add replies to comments in seeds

--- decidim-comments/app/models/decidim/comments/seed.rb
+++ decidim-comments/app/models/decidim/comments/seed.rb
@@ -6,25 +6,38 @@ module Decidim
     # to Seed those models in order to be able to use them in the development
     # app.
     class Seed
-      # Public: adds a random amount of comments for a given resource.
-      #
-      # resource - the resource to add the coments to.
-      #
-      # Returns nothing.
-      def self.comments_for(resource)
-        return unless resource.accepts_new_comments?
+      class << self
+        # Public: adds a random amount of comments for a given resource.
+        #
+        # resource - the resource to add the coments to.
+        #
+        # Returns nothing.
+        def comments_for(resource)
+          return unless resource.accepts_new_comments?
 
-        Decidim::Comments::Comment.reset_column_information
+          Decidim::Comments::Comment.reset_column_information
 
-        organization = resource.organization
+          rand(0..6).times do
+            comment = create_comment(resource)
+            create_comment(comment, resource) if [true, false].sample
+          end
+        end
 
-        2.times do
-          author = Decidim::User.where(organization:).all.sample
+        private
+
+        # Private - creates a comment for a given resource.
+        #
+        # resource - the resource to add the coments to.
+        # root_commentable - the root commentable resource. It's optional, used for making nested comments.
+        #
+        # Returns the created comment.
+        def create_comment(resource, root_commentable = nil)
+          author = Decidim::User.where(organization: resource.organization).all.sample
           user_group = [true, false].sample ? Decidim::UserGroups::ManageableUserGroups.for(author).verified.sample : nil
 
           params = {
             commentable: resource,
-            root_commentable: resource,
+            root_commentable: root_commentable || resource,
             body: { en: ::Faker::Lorem.sentence(word_count: 50) },
             author:,
             user_group:
