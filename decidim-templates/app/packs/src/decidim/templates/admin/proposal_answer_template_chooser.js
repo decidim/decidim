8954aaf5bd845c2abc6d92bb5e629a9d4551e9f5
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-10-27 09:02:44 +0300

    Check for existing element

--- decidim-templates/app/packs/src/decidim/templates/admin/proposal_answer_template_chooser.js
+++ decidim-templates/app/packs/src/decidim/templates/admin/proposal_answer_template_chooser.js
@@ -1,26 +1,31 @@
 // Choose a Proposal Answer template, get it by AJAX and add the Template in the Proposal Answer textarea
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("proposal_answer_template_chooser").addEventListener("change", () => {
-    const dropdown =  document.getElementById("proposal_answer_template_chooser");
-    const url = dropdown.getAttribute("data-url");
-    const templateId = dropdown.value;
-    const proposalId = dropdown.dataset.proposal;
+  const proposalAnswerTemplateChooser = document.getElementById("proposal_answer_template_chooser");
+  if (proposalAnswerTemplateChooser) {
 
-    if (templateId === "") {
-      return;
-    }
-    fetch(`${new URL(url).pathname}?${new URLSearchParams({ id: templateId, proposalId: proposalId })}`).
-      then((response) => response.json()).
-      then((data) => {
-        document.getElementById(`proposal_answer_internal_state_${data.state}`).click();
+    proposalAnswerTemplateChooser.addEventListener("change", () => {
+      const dropdown =  document.getElementById("proposal_answer_template_chooser");
+      const url = dropdown.getAttribute("data-url");
+      const templateId = dropdown.value;
+      const proposalId = dropdown.dataset.proposal;
 
-        let editorContainer = null;
-        for (const [key, value] of Object.entries(data.template)) {
-          editorContainer = document.querySelector(`[name="proposal_answer[answer_${key}]"]`).nextElementSibling;
-          let editor = editorContainer.querySelector(".ProseMirror").editor;
+      if (templateId === "") {
+        return;
+      }
+      fetch(`${new URL(url).pathname}?${new URLSearchParams({ id: templateId, proposalId: proposalId })}`).
+        then((response) => response.json()).
+        then((data) => {
+          document.getElementById(`proposal_answer_internal_state_${data.state}`).click();
 
-          editor.commands.setContent(value, true);
-        }
-      })
-  });
+          let editorContainer = null;
+          for (const [key, value] of Object.entries(data.template)) {
+            editorContainer = document.querySelector(`[name="proposal_answer[answer_${key}]"]`).nextElementSibling;
+            let editor = editorContainer.querySelector(".ProseMirror").editor;
+
+            editor.commands.setContent(value, true);
+          }
+        })
+    });
+
+  }
 });
