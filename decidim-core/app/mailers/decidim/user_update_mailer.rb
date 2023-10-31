1c889bd5f77ffbf28337c5fc0a98ffe9b5c94ccc
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-10-31 11:32:44 +0200

    Remove unnecessary translations from user_update_mailer

--- decidim-core/app/mailers/decidim/user_update_mailer.rb
+++ decidim-core/app/mailers/decidim/user_update_mailer.rb
@@ -20,8 +20,6 @@ module Decidim
       case updates.length
       when 1
         updates.first
-      when 2
-        I18n.t("decidim.user_update_mailer.notify.update_two_fields", updates: updates.first, last_update:, count: 2)
       else
         I18n.t("decidim.user_update_mailer.notify.update_fields", updates: updates[0..-2].join(", "), last_update:, count: updates.length)
       end
