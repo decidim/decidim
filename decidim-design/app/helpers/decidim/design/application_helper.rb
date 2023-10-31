ef85e44687e697525c2bd49c2e061a117a47a6cc
Hugoren Martinako <aumpfbahn@gmail.com>
2023-10-31 16:26:01 +0100

    Redesign: design buttons (#11781)
    * buttons site
    
    * replace uttons with tables
    
    * replace tailwind with css classes
    
    * Define a generic representation of buttons page
    
    * Fix toc links in buttons page
    
    * no need of css classes for paragraphs
    
    * include first iteration snippet for code display
    
    * update styles and add html
    
    * Enable cell snippet rendering
    
    * Refactor display design data in tables
    
    * Add card snippet exampe to buttons page
    
    * Improve html output format
    
    * Update decidim-design/app/helpers/decidim/design/buttons_helper.rb
    
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>
    
    ---------
    
    Co-authored-by: Eduardo Martinez Echevarria <eduardomech@gmail.com>
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>

---
+++ decidim-design/app/helpers/decidim/design/application_helper.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Design
+    module ApplicationHelper
+      # For the moment keep this as a constant and later decide where to move
+      # this
+      RELEASE_ID = "develop"
+
+      def section_title(section)
+        content_tag(:h2, section_text(section), class: "design__heading__3xl")
+      end
+
+      def section_text(section)
+        section[:title] || section[:id].titleize
+      end
+
+      def render_content(content)
+        case content[:type]
+        when :text
+          html = ""
+          content[:values].each do |value|
+            html += content_tag(:p, value, class: content[:class])
+          end
+          html.html_safe
+        when :table
+          render partial: "decidim/design/shared/table", locals: content.slice(:items).merge(content[:options] || {})
+        when :partial
+          render partial: content[:template], locals: content[:locals]
+        else
+          content[:values].to_s.html_safe
+        end
+      end
+
+      def render_cell_snippet(content)
+        return "" if content[:cell_snippet].blank?
+
+        render partial: "decidim/design/shared/cell_snippet", locals: cell_snippet_locals(content[:cell_snippet][:cell], content[:cell_snippet][:args])
+      end
+
+      def cell_snippet_locals(cell, args)
+        path = args.delete(:path) || File.join("decidim-core/app/cells/", cell)
+        {
+          text: path,
+          url: "https://github.com/decidim/decidim/blob/#{RELEASE_ID}/#{path}_cell.rb",
+          cell:,
+          args:
+        }
+      end
+
+      def render_row(row)
+        if row.is_a?(Array)
+          html = ""
+          row.each do |cell|
+            html += render_cell(cell)
+          end
+          html.html_safe
+        end
+      end
+
+      def render_cell(cell)
+        content_tag(:td) do
+          if cell.is_a?(Hash)
+            send(cell[:method], *cell[:args]).to_s
+          else
+            cell
+          end
+        end
+      end
+    end
+  end
+end
