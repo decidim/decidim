e6572c7b3bb8cf05e69b5d9bb14933d0a4c23b43
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2023-11-03 10:02:52 +0100

    Backport 'Fix Bootsnap configuration' to v0.26 (#11897)
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>

--- decidim-generators/lib/decidim/generators/app_generator.rb
+++ decidim-generators/lib/decidim/generators/app_generator.rb
@@ -130,22 +130,6 @@ module Decidim
         run "bundle install"
       end
 
-      def tweak_bootsnap
-        gsub_file "config/boot.rb", %r{require 'bootsnap/setup'.*$}, <<~RUBY.rstrip
-          require "bootsnap"
-
-          env = ENV["RAILS_ENV"] || "development"
-
-          Bootsnap.setup(
-            cache_dir: File.expand_path(File.join("..", "tmp", "cache"), __dir__),
-            development_mode: env == "development",
-            load_path_cache: true,
-            compile_cache_iseq: !ENV["SIMPLECOV"],
-            compile_cache_yaml: true
-          )
-        RUBY
-      end
-
       def add_ignore_uploads
         append_file ".gitignore", "\n# Ignore public uploads\npublic/uploads" unless options["skip_git"]
       end
