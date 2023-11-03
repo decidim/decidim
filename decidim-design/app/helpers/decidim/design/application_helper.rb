f3f8e604044d2eee4cec16547476bf58173f7fd7
Hugoren Martinako <aumpfbahn@gmail.com>
2023-11-03 12:28:23 +0100

    partial cherry-pick from cards

--- decidim-design/app/helpers/decidim/design/application_helper.rb
+++ decidim-design/app/helpers/decidim/design/application_helper.rb
@@ -8,11 +8,19 @@ module Decidim
       RELEASE_ID = "develop"
 
       def section_title(section)
-        content_tag(:h2, section_text(section), class: "design__heading__3xl")
+        content_tag(:h2, section_text(section))
+      end
+
+      def section_subtitle(section)
+        content_tag(:h3, section_text(section))
       end
 
       def section_text(section)
-        section[:title] || section[:id].titleize
+        title = section[:title] || section[:id]&.titleize
+        return title unless section[:label].present?
+
+        title += content_tag(:span, section[:label], class: "label")
+        title.html_safe
       end
 
       def render_content(content)
@@ -20,16 +28,34 @@ module Decidim
         when :text
           html = ""
           content[:values].each do |value|
-            html += content_tag(:p, value, class: content[:class])
+            html += content_tag(:p, value.html_safe, class: content[:class])
           end
           html.html_safe
         when :table
           render partial: "decidim/design/shared/table", locals: content.slice(:items).merge(content[:options] || {})
         when :partial
-          render partial: content[:template], locals: content[:locals]
+          partial = render_partial(content)
+
+          return partial unless content[:layout].present?
+
+          render layout: content[:layout] do
+            partial.html_safe
+          end
         else
           content[:values].to_s.html_safe
         end
+      end
+
+      def render_partial(content)
+        if content[:template].is_a?(Array)
+          templates = ""
+          content[:template].each do |value|
+            templates += render partial: value, locals: content[:locals]
+          end
+          return templates
+        end
+
+        render partial: content[:template], locals: content[:locals]
       end
 
       def render_cell_snippet(content)
