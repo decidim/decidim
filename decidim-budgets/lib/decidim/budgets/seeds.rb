224c0efc6620d3908c674e81d91c15659bab24b6
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2023-11-09 11:36:47 +0100

    Extract seeds to its own classes (#11968)
    * Extract accountability seeds to its own class
    
    * Extract participatory-processes seeds to its own class
    
    * Extract assemblies seeds to its own class
    
    * Extract blogs seeds to its own class
    
    * Extract budgets seeds to its own class
    
    * Extract conferences seeds to its own class
    
    * Extract debates seeds to its own class
    
    * Extract elections seeds to its own class
    
    * Extract votings seeds to its own class
    
    * Extract initiatives seeds to its own class
    
    * Extract pages seeds to its own class
    
    * Extract proposals seeds to its own class
    
    * Extract sortitions seeds to its own class
    
    * Extract surveys seeds to its own class
    
    * Extract meetings seeds to its own class
    
    * Fix rubocop offense for Lint/UselessAssignment
    
    * Ignore rubocop offenses for CyclomaticComplexity and PerceivedComplexity
    
    * Extract core seeds to its own class
    
    * Make consistent where we have the seeds images

---
+++ decidim-budgets/lib/decidim/budgets/seeds.rb
@@ -0,0 +1,106 @@
+# frozen_string_literal: true
+
+require "decidim/components/namer"
+
+module Decidim
+  module Budgets
+    class Seeds
+      attr_reader :participatory_space
+
+      def initialize(participatory_space:)
+        @participatory_space = participatory_space
+      end
+
+      def call
+        landing_page_content = Decidim::Faker::Localized.localized do
+          "<h2>#{::Faker::Lorem.sentence}</h2>" \
+            "<p>#{::Faker::Lorem.paragraph}</p>" \
+            "<p>#{::Faker::Lorem.paragraph}</p>"
+        end
+
+        component = Decidim::Component.create!(
+          name: Decidim::Components::Namer.new(participatory_space.organization.available_locales, :budgets).i18n_name,
+          manifest_name: :budgets,
+          published_at: Time.current,
+          participatory_space:,
+          settings: {
+            landing_page_content:,
+            more_information_modal: Decidim::Faker::Localized.paragraph(sentence_count: 4),
+            workflow: Decidim::Budgets.workflows.keys.sample
+          }
+        )
+
+        rand(1...3).times do
+          Decidim::Budgets::Budget.create!(
+            component:,
+            title: Decidim::Faker::Localized.sentence(word_count: 2),
+            description: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
+              Decidim::Faker::Localized.paragraph(sentence_count: 3)
+            end,
+            total_budget: ::Faker::Number.number(digits: 8)
+          )
+        end
+
+        Decidim::Budgets::Budget.where(component:).each do |budget|
+          rand(2...4).times do
+            project = Decidim::Budgets::Project.create!(
+              budget:,
+              scope: participatory_space.organization.scopes.sample,
+              category: participatory_space.categories.sample,
+              title: Decidim::Faker::Localized.sentence(word_count: 2),
+              description: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
+                Decidim::Faker::Localized.paragraph(sentence_count: 3)
+              end,
+              budget_amount: ::Faker::Number.between(from: Integer(budget.total_budget * 0.7), to: budget.total_budget)
+            )
+
+            attachment_collection = Decidim::AttachmentCollection.create!(
+              name: Decidim::Faker::Localized.word,
+              description: Decidim::Faker::Localized.sentence(word_count: 5),
+              collection_for: project
+            )
+
+            Decidim::Attachment.create!(
+              title: Decidim::Faker::Localized.sentence(word_count: 2),
+              description: Decidim::Faker::Localized.sentence(word_count: 5),
+              attachment_collection:,
+              attached_to: project,
+              content_type: "application/pdf",
+              file: ActiveStorage::Blob.create_and_upload!(
+                io: File.open(File.join(__dir__, "seeds", "Exampledocument.pdf")),
+                filename: "Exampledocument.pdf",
+                content_type: "application/pdf",
+                metadata: nil
+              )
+            )
+            Decidim::Attachment.create!(
+              title: Decidim::Faker::Localized.sentence(word_count: 2),
+              description: Decidim::Faker::Localized.sentence(word_count: 5),
+              attached_to: project,
+              content_type: "image/jpeg",
+              file: ActiveStorage::Blob.create_and_upload!(
+                io: File.open(File.join(__dir__, "seeds", "city.jpeg")),
+                filename: "city.jpeg",
+                content_type: "image/jpeg",
+                metadata: nil
+              )
+            )
+            Decidim::Attachment.create!(
+              title: Decidim::Faker::Localized.sentence(word_count: 2),
+              description: Decidim::Faker::Localized.sentence(word_count: 5),
+              attached_to: project,
+              content_type: "application/pdf",
+              file: ActiveStorage::Blob.create_and_upload!(
+                io: File.open(File.join(__dir__, "seeds", "Exampledocument.pdf")),
+                filename: "Exampledocument.pdf",
+                content_type: "application/pdf",
+                metadata: nil
+              )
+            )
+            Decidim::Comments::Seed.comments_for(project)
+          end
+        end
+      end
+    end
+  end
+end
