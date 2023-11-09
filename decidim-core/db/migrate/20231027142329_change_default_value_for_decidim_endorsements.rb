f18b68995ef5237e8221c9f7fae4fd394fd36914
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-11-09 14:02:02 +0100

    Fix rails version in migration file

--- decidim-core/db/migrate/20231027142329_change_default_value_for_decidim_endorsements.rb
+++ decidim-core/db/migrate/20231027142329_change_default_value_for_decidim_endorsements.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ChangeDefaultValueForDecidimEndorsements < ActiveRecord::Migration[6.1]
+class ChangeDefaultValueForDecidimEndorsements < ActiveRecord::Migration[6.0]
   def up
     change_column_default :decidim_endorsements, :decidim_user_group_id, 0
   end
