5c5ee7a50d75c10643dd8c495e2517641e4d74db
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2023-11-10 14:40:55 +0100

    Backport 'Fix duplicated endorsements' to v0.26 (#11973)
    * Fix duplicated endorsements
    
    * Add Migration to set user_group_id default value to 0
    
    * Fix Endorsement UserGroup interaction
    
    * Fix failing spec
    
    * Running linters
    
    * Add cleanup task
    
    * Apply suggestions from code review
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    * Fix rails version in migration file
    
    * Fix specs
    
    ---------
    
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>

---
+++ decidim-core/db/migrate/20231027142329_change_default_value_for_decidim_endorsements.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class ChangeDefaultValueForDecidimEndorsements < ActiveRecord::Migration[6.0]
+  def up
+    change_column_default :decidim_endorsements, :decidim_user_group_id, 0
+  end
+
+  def down
+    change_column_default :decidim_endorsements, :decidim_user_group_id, nil
+  end
+end
