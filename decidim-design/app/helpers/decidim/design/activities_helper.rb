6f1e03411e91d6d54839d75c592cf577cc3a22d6
Hugoren Martinako <aumpfbahn@gmail.com>
2023-11-10 12:27:11 +0100

    add more info about activities

--- decidim-design/app/helpers/decidim/design/activities_helper.rb
+++ decidim-design/app/helpers/decidim/design/activities_helper.rb
@@ -6,7 +6,7 @@ module Decidim
       def activities_sections
         [
           {
-            id: "Demo",
+            id: "demo",
             contents: [
               {
                 type: :text,
@@ -15,15 +15,34 @@ module Decidim
               {
                 type: :partial,
                 template: "decidim/design/components/activities/static-activities"
-              }
+              },
+              {
+                type: :text,
+                values: ["Used by the last activity page, in a content block and within the dropdowns."]
+              },
             ]
           },
+          {
+            id: "sourcecode",
+            contents: [
+              {
+                type: :table,
+                options: { headings: %w(Cell Code) },
+                items: activities_table(
+                  { name: "Activities", url: "https://github.com/decidim/decidim/blob/develop/decidim-core/app/cells/decidim/activities_cell.rb" },
+                  { name: "Activitiy", url: "https://github.com/decidim/decidim/blob/develop/decidim-core/app/cells/decidim/activity_cell.rb" }
+                )
+              }
+            ]
+          }
         ]
       end
 
       def activities_table(*table_rows, **_opts)
         table_rows.map do |table_cell|
           row = []
+          row << table_cell[:name]
+          row << link_to(table_cell[:url].split("/").last, table_cell[:url], target: "_blank", class: "text-secondary underline", rel: "noopener")
           row
         end
       end
