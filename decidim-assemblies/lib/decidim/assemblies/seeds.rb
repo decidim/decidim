ecd27897182469f6a3a164b5ace0837845e1e23f
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-11-15 12:31:32 +0100

    Extract create_assembly! to its own method

--- decidim-assemblies/lib/decidim/assemblies/seeds.rb
+++ decidim-assemblies/lib/decidim/assemblies/seeds.rb
@@ -5,70 +5,11 @@ require "decidim/seeds"
 module Decidim
   module Assemblies
     class Seeds < Decidim::Seeds
-      # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
       def call
         create_content_block!
 
         2.times do |n|
-          params = {
-            title: Decidim::Faker::Localized.sentence(word_count: 5),
-            slug: Decidim::Faker::Internet.unique.slug(words: nil, glue: "-"),
-            subtitle: Decidim::Faker::Localized.sentence(word_count: 2),
-            hashtag: "##{::Faker::Lorem.word}",
-            short_description: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
-              Decidim::Faker::Localized.sentence(word_count: 3)
-            end,
-            description: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
-              Decidim::Faker::Localized.paragraph(sentence_count: 3)
-            end,
-            organization:,
-            hero_image: ::Faker::Boolean.boolean(true_ratio: 0.5) ? hero_image : nil, # Keep after organization
-            banner_image: ::Faker::Boolean.boolean(true_ratio: 0.5) ? banner_image : nil, # Keep after organization
-            promoted: true,
-            published_at: 2.weeks.ago,
-            meta_scope: Decidim::Faker::Localized.word,
-            developer_group: Decidim::Faker::Localized.sentence(word_count: 1),
-            local_area: Decidim::Faker::Localized.sentence(word_count: 2),
-            target: Decidim::Faker::Localized.sentence(word_count: 3),
-            participatory_scope: Decidim::Faker::Localized.sentence(word_count: 1),
-            participatory_structure: Decidim::Faker::Localized.sentence(word_count: 2),
-            scope: n.positive? ? Decidim::Scope.reorder(Arel.sql("RANDOM()")).first : nil,
-            purpose_of_action: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
-              Decidim::Faker::Localized.paragraph(sentence_count: 3)
-            end,
-            composition: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
-              Decidim::Faker::Localized.paragraph(sentence_count: 3)
-            end,
-            assembly_type: Decidim::AssembliesType.create!(organization:, title: Decidim::Faker::Localized.word),
-            creation_date: 1.day.from_now,
-            created_by: "others",
-            created_by_other: Decidim::Faker::Localized.word,
-            duration: 2.days.from_now,
-            included_at: 5.days.from_now,
-            closing_date: 5.days.from_now,
-            closing_date_reason: Decidim::Faker::Localized.sentence(word_count: 3),
-            internal_organisation: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
-              Decidim::Faker::Localized.paragraph(sentence_count: 3)
-            end,
-            is_transparent: true,
-            special_features: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
-              Decidim::Faker::Localized.paragraph(sentence_count: 3)
-            end,
-            twitter_handler: ::Faker::Lorem.word,
-            facebook_handler: ::Faker::Lorem.word,
-            instagram_handler: ::Faker::Lorem.word,
-            youtube_handler: ::Faker::Lorem.word,
-            github_handler: ::Faker::Lorem.word
-          }
-
-          assembly = Decidim.traceability.perform_action!(
-            "publish",
-            Decidim::Assembly,
-            organization.users.first,
-            visibility: "all"
-          ) do
-            Decidim::Assembly.create!(params)
-          end
+          assembly = create_assembly!
 
           # Create users with specific roles
           Decidim::AssemblyUserRole::ROLES.each do |role|
@@ -168,7 +109,6 @@ module Decidim
           end
         end
       end
-      # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
       def organization = Decidim::Organization.first
 
@@ -180,6 +120,69 @@ module Decidim
           manifest_name: :highlighted_assemblies,
           published_at: Time.current
         )
+      end
+
+      def create_assembly!
+        n = rand(2)
+        params = {
+          title: Decidim::Faker::Localized.sentence(word_count: 5),
+          slug: Decidim::Faker::Internet.unique.slug(words: nil, glue: "-"),
+          subtitle: Decidim::Faker::Localized.sentence(word_count: 2),
+          hashtag: "##{::Faker::Lorem.word}",
+          short_description: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
+            Decidim::Faker::Localized.sentence(word_count: 3)
+          end,
+          description: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
+            Decidim::Faker::Localized.paragraph(sentence_count: 3)
+          end,
+          organization:,
+          hero_image: ::Faker::Boolean.boolean(true_ratio: 0.5) ? hero_image : nil, # Keep after organization
+          banner_image: ::Faker::Boolean.boolean(true_ratio: 0.5) ? banner_image : nil, # Keep after organization
+          promoted: true,
+          published_at: 2.weeks.ago,
+          meta_scope: Decidim::Faker::Localized.word,
+          developer_group: Decidim::Faker::Localized.sentence(word_count: 1),
+          local_area: Decidim::Faker::Localized.sentence(word_count: 2),
+          target: Decidim::Faker::Localized.sentence(word_count: 3),
+          participatory_scope: Decidim::Faker::Localized.sentence(word_count: 1),
+          participatory_structure: Decidim::Faker::Localized.sentence(word_count: 2),
+          scope: n.positive? ? Decidim::Scope.reorder(Arel.sql("RANDOM()")).first : nil,
+          purpose_of_action: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
+            Decidim::Faker::Localized.paragraph(sentence_count: 3)
+          end,
+          composition: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
+            Decidim::Faker::Localized.paragraph(sentence_count: 3)
+          end,
+          assembly_type: Decidim::AssembliesType.create!(organization:, title: Decidim::Faker::Localized.word),
+          creation_date: 1.day.from_now,
+          created_by: "others",
+          created_by_other: Decidim::Faker::Localized.word,
+          duration: 2.days.from_now,
+          included_at: 5.days.from_now,
+          closing_date: 5.days.from_now,
+          closing_date_reason: Decidim::Faker::Localized.sentence(word_count: 3),
+          internal_organisation: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
+            Decidim::Faker::Localized.paragraph(sentence_count: 3)
+          end,
+          is_transparent: true,
+          special_features: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
+            Decidim::Faker::Localized.paragraph(sentence_count: 3)
+          end,
+          twitter_handler: ::Faker::Lorem.word,
+          facebook_handler: ::Faker::Lorem.word,
+          instagram_handler: ::Faker::Lorem.word,
+          youtube_handler: ::Faker::Lorem.word,
+          github_handler: ::Faker::Lorem.word
+        }
+
+        Decidim.traceability.perform_action!(
+          "publish",
+          Decidim::Assembly,
+          organization.users.first,
+          visibility: "all"
+        ) do
+          Decidim::Assembly.create!(params)
+        end
       end
     end
   end
