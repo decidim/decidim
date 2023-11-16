840ad0186b1ac74b039066c16e9e883dfc9b002f
Sina Eftekhar <104360479+sinaeftekhar@users.noreply.github.com>
2023-11-16 09:58:27 +0200

    Preserve filters for data exports (#11788)
    * initial stage
    
    * Add export selection to the proposals
    
    * Add selection export for meetings
    
    * Add export selection for projects
    
    * Add selection export for accounatbility
    
    * Add tests for the exports_helper
    
    * Add system tests for decidim-accountability
    
    * Add system tests for projects export
    
    * Add system tests for exporting proposals
    
    * Add system tests for meetings exports
    
    * Add tests for correct query setting
    
    * fix lintin/test error
    
    * Fix linting errors
    
    * Fix a failing test
    
    * Fix a  bug in commentable selection export
    
    * Fix an issue

--- decidim-budgets/spec/shared/export_projects_examples.rb
+++ decidim-budgets/spec/shared/export_projects_examples.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-shared_examples "export meetings" do
-  let!(:meetings) { create_list(:meeting, 3, :published, component: current_component) }
+shared_examples "export projects" do
+  let!(:projects) { create_list(:project, 5, budget:) }
   let(:export_type) { "Export all" }
 
   it_behaves_like "export as CSV"
@@ -9,7 +9,7 @@ shared_examples "export meetings" do
 
   context "with query" do
     before do
-      fill_in "q[id_string_or_title_cont]", with: translated(meetings.last.title)
+      fill_in "q[id_string_or_title_cont]", with: translated(projects.last.title)
       find("button[aria-label='Search']").click
     end
 
@@ -23,23 +23,23 @@ end
 shared_examples "export as CSV" do
   it "exports a CSV" do
     find("span.exports", text: export_type).click
-    perform_enqueued_jobs { click_link "Meetings as CSV" }
+    perform_enqueued_jobs { click_link "Projects as CSV" }
 
     expect(page).to have_admin_callout "Your export is currently in progress. You will receive an email when it is complete."
-    expect(last_email.subject).to include("meetings", "csv")
+    expect(last_email.subject).to include("projects", "csv")
     expect(last_email.attachments.length).to be_positive
-    expect(last_email.attachments.first.filename).to match(/^meetings.*\.zip$/)
+    expect(last_email.attachments.first.filename).to match(/^projects.*\.zip$/)
   end
 end
 
 shared_examples "export as JSON" do
   it "exports a JSON" do
     find("span.exports", text: export_type).click
-    perform_enqueued_jobs { click_link "Meetings as JSON" }
+    perform_enqueued_jobs { click_link "Projects as JSON" }
 
     expect(page).to have_admin_callout "Your export is currently in progress. You will receive an email when it is complete."
-    expect(last_email.subject).to include("meetings", "json")
+    expect(last_email.subject).to include("projects", "json")
     expect(last_email.attachments.length).to be_positive
-    expect(last_email.attachments.first.filename).to match(/^meetings.*\.zip$/)
+    expect(last_email.attachments.first.filename).to match(/^projects.*\.zip$/)
   end
 end
