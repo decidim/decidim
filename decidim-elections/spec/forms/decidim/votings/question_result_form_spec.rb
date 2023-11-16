7902158c02c57220de899ae3325560ee606c6260
Ivan Vergés <ivan@pokecode.net>
2023-11-16 23:26:55 +0100

    remove wrong test

--- decidim-elections/spec/forms/decidim/votings/question_result_form_spec.rb
+++ decidim-elections/spec/forms/decidim/votings/question_result_form_spec.rb
@@ -7,12 +7,10 @@ describe Decidim::Votings::QuestionResultForm do
 
   let(:closure) { create(:ps_closure) }
   let(:question) { create(:question, election: closure.election, min_selections:, max_selections:) }
-  let!(:result) { create(:election_result, :blank_ballots, election: closure.election, closurable: closure, question:, value: blank_total) }
   let(:question_id) { question.id }
   let(:value) { 123 }
   let(:max_selections) { 3 }
   let(:min_selections) { 0 }
-  let(:blank_total) { 124 }
 
   let(:attributes) do
     {
@@ -40,11 +38,5 @@ describe Decidim::Votings::QuestionResultForm do
 
     # The value is cast to the correct type so "abcde" becomes 0 which is valid.
     it { is_expected.to be_valid }
-  end
-
-  context "when value is greater than the number of blank ballots" do
-    let(:blank_total) { 100 }
-
-    it { is_expected.to be_invalid }
   end
 end
