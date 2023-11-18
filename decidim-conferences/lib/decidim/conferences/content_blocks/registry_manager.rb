8824106cf728cc92171202c78755602df24796f1
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-11-18 13:48:45 +0100

    Fix rubocop offense

--- decidim-conferences/lib/decidim/conferences/content_blocks/registry_manager.rb
+++ decidim-conferences/lib/decidim/conferences/content_blocks/registry_manager.rb
@@ -7,15 +7,15 @@ module Decidim
     module ContentBlocks
       class RegistryManager
         def self.register!
-        Decidim.content_blocks.register(:homepage, :highlighted_conferences) do |content_block|
-          content_block.cell = "decidim/conferences/content_blocks/highlighted_conferences"
-          content_block.public_name_key = "decidim.conferences.content_blocks.highlighted_conferences.name"
-          content_block.settings_form_cell = "decidim/conferences/content_blocks/highlighted_conferences_settings_form"
+          Decidim.content_blocks.register(:homepage, :highlighted_conferences) do |content_block|
+            content_block.cell = "decidim/conferences/content_blocks/highlighted_conferences"
+            content_block.public_name_key = "decidim.conferences.content_blocks.highlighted_conferences.name"
+            content_block.settings_form_cell = "decidim/conferences/content_blocks/highlighted_conferences_settings_form"
 
-          content_block.settings do |settings|
-            settings.attribute :max_results, type: :integer, default: 6
+            content_block.settings do |settings|
+              settings.attribute :max_results, type: :integer, default: 6
+            end
           end
-        end
         end
       end
     end
