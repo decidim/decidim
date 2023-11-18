b3501486d3e8ff54ec9f3dbcfde849874b354052
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2023-11-18 12:04:31 +0100

    Add max results setting to highlighted conferences content block (#12028)

--- decidim-conferences/lib/decidim/conferences/engine.rb
+++ decidim-conferences/lib/decidim/conferences/engine.rb
@@ -70,6 +70,11 @@ module Decidim
         Decidim.content_blocks.register(:homepage, :highlighted_conferences) do |content_block|
           content_block.cell = "decidim/conferences/content_blocks/highlighted_conferences"
           content_block.public_name_key = "decidim.conferences.content_blocks.highlighted_conferences.name"
+          content_block.settings_form_cell = "decidim/conferences/content_blocks/highlighted_conferences_settings_form"
+
+          content_block.settings do |settings|
+            settings.attribute :max_results, type: :integer, default: 6
+          end
         end
       end
 
