4e061d6d72211ef507c7669e1670a6612e31a4ec
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-11-24 10:26:19 +0200

    Fixing test

--- decidim-proposals/app/helpers/decidim/proposals/admin/proposals_helper.rb
+++ decidim-proposals/app/helpers/decidim/proposals/admin/proposals_helper.rb
@@ -38,7 +38,7 @@ module Decidim
         end
 
         def proposal_complete_state(proposal)
-          return humanize_proposal_state("not_answered").html_safe if proposal.state.blank?
+          return humanize_proposal_state("not_answered").html_safe if proposal.proposal_state.nil?
 
           translated_attribute(proposal&.proposal_state&.title)
         end
