babd300bf5856256d7afa2ee02638bc735f59e1f
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2023-11-25 21:52:21 +0100

    Backport 'Fix dependency resolver trying to fetch gem paths from lazy specifications' to v0.26 (#12061)
    Co-authored-by: Antti Hukkanen <antti.hukkanen@mainiotech.fi>

--- decidim-core/spec/lib/dependency_resolver_spec.rb
+++ decidim-core/spec/lib/dependency_resolver_spec.rb
@@ -138,8 +138,8 @@ module Decidim
         describe "#lookup" do
           subject { resolver.lookup(gem) }
 
-          it "returns Bundler::LazySpecification" do
-            expect(subject).to be_instance_of(Bundler::LazySpecification)
+          it "returns Gem::Specification" do
+            expect(subject).to be_instance_of(Gem::Specification)
             expect(subject.name).to eq(gem)
           end
         end
