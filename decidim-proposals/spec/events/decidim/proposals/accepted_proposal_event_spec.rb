f30ce462d3bd96d1090753d8ffe388224a361354
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-11-28 12:19:15 +0100

    refactor accepted proposal event spec

--- decidim-proposals/spec/events/decidim/proposals/accepted_proposal_event_spec.rb
+++ decidim-proposals/spec/events/decidim/proposals/accepted_proposal_event_spec.rb
@@ -10,32 +10,10 @@ describe Decidim::Proposals::AcceptedProposalEvent do
   include_context "when a simple event"
   it_behaves_like "a simple event"
 
-  describe "email_subject" do
-    it "is generated correctly" do
-      expect(subject.email_subject).to eq("A proposal you are following has been accepted")
-    end
-  end
-
-  describe "email_intro" do
-    it "is generated correctly" do
-      expect(subject.email_intro)
-        .to eq("The proposal \"#{resource_title}\" has been accepted. You can read the answer in this page:")
-    end
-  end
-
-  describe "email_outro" do
-    it "is generated correctly" do
-      expect(subject.email_outro)
-        .to eq("You have received this notification because you are following \"#{resource_title}\". You can unfollow it from the previous link.")
-    end
-  end
-
-  describe "notification_title" do
-    it "is generated correctly" do
-      expect(subject.notification_title)
-        .to include("The <a href=\"#{resource_path}\">#{resource_title}</a> proposal has been accepted")
-    end
-  end
+  let(:email_subject) { "A proposal you are following has been accepted" }
+  let(:email_intro) { "The proposal \"#{resource_title}\" has been accepted. You can read the answer in this page:" }
+  let(:email_outro) { "You have received this notification because you are following \"#{resource_title}\". You can unfollow it from the previous link." }
+  let(:notification_title) { "The <a href=\"#{resource_path}\">#{resource_title}</a> proposal has been accepted" }
 
   describe "resource_text" do
     it "shows the proposal answer" do
