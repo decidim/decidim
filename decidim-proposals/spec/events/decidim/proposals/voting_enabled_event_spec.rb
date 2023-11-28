33224d5561dee48d60a1b0ec03449f1b137ea267
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-11-28 12:08:31 +0100

    refactor of voting enabled event spec

--- decidim-proposals/spec/events/decidim/proposals/voting_enabled_event_spec.rb
+++ decidim-proposals/spec/events/decidim/proposals/voting_enabled_event_spec.rb
@@ -16,32 +16,40 @@ module Decidim
 
       it_behaves_like "a simple event"
 
-      describe "email_subject" do
-        it "is generated correctly" do
-          expect(subject.email_subject).to eq("Proposal support has started for #{participatory_space.title["en"]}")
-        end
-      end
+      # describe "email_subject" do
+      #   it "is generated correctly" do
+      #     expect(subject.email_subject).to eq("Proposal support has started for #{participatory_space.title["en"]}")
+      #   end
+      # end
 
-      describe "email_intro" do
-        it "is generated correctly" do
-          expect(subject.email_intro)
-            .to eq("You can support proposals in #{participatory_space_title}! Start participating in this page:")
-        end
-      end
+      let(:email_subject) { "Proposal support has started for #{participatory_space.title["en"]}" }
 
-      describe "email_outro" do
-        it "is generated correctly" do
-          expect(subject.email_outro)
-            .to include("You have received this notification because you are following #{participatory_space.title["en"]}")
-        end
-      end
+      # describe "email_intro" do
+      #   it "is generated correctly" do
+      #     expect(subject.email_intro)
+      #       .to eq("You can support proposals in #{participatory_space_title}! Start participating in this page:")
+      #   end
+      # end
 
-      describe "notification_title" do
-        it "is generated correctly" do
-          expect(subject.notification_title)
-            .to eq("You can now start <a href=\"#{resource_path}\">supporting proposals</a> in <a href=\"#{participatory_space_url}\">#{participatory_space.title["en"]}</a>")
-        end
-      end
+      let(:email_intro) { "You can support proposals in #{participatory_space_title}! Start participating in this page:" }
+
+      # describe "email_outro" do
+      #   it "is generated correctly" do
+      #     expect(subject.email_outro)
+      #       .to include("You have received this notification because you are following #{participatory_space.title["en"]}")
+      #   end
+      # end
+
+      let(:email_outro) { "You have received this notification because you are following #{participatory_space.title["en"]}" }
+
+      # describe "notification_title" do
+      #   it "is generated correctly" do
+      #     expect(subject.notification_title)
+      #       .to eq("You can now start <a href=\"#{resource_path}\">supporting proposals</a> in <a href=\"#{participatory_space_url}\">#{participatory_space.title["en"]}</a>")
+      #   end
+      # end
+
+      let(:notification_title) { "You can now start <a href=\"#{resource_path}\">supporting proposals</a> in <a href=\"#{participatory_space_url}\">#{participatory_space.title["en"]}</a>" }
     end
   end
 end
