9cfd1b2eb5d56a0750ed3698f0a6a45e8f8fee95
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-11-28 11:29:35 +0100

    refactor collaborative_draft_wtihdrawn_spec

--- decidim-proposals/spec/events/decidim/proposals/collaborative_draft_withdrawn_event_spec.rb
+++ decidim-proposals/spec/events/decidim/proposals/collaborative_draft_withdrawn_event_spec.rb
@@ -21,31 +21,9 @@ describe Decidim::Proposals::CollaborativeDraftWithdrawnEvent do
   context "when the notification is for coauthor users" do
     it_behaves_like "a simple event"
 
-    describe "email_subject" do
-      it "is generated correctly" do
-        expect(subject.email_subject).to eq("#{author_name} #{author_nickname} withdrawn the #{decidim_sanitize(resource_title)} collaborative draft.")
-      end
-    end
-
-    describe "email_intro" do
-      it "is generated correctly" do
-        expect(subject.email_intro)
-          .to eq(%(<a href="#{author_url}">#{author_name} #{author_nickname}</a> withdrawn the <a href="#{resource_url}">#{resource_title}</a> collaborative draft.))
-      end
-    end
-
-    describe "email_outro" do
-      it "is generated correctly" do
-        expect(subject.email_outro)
-          .to eq(%(You have received this notification because you are a collaborator of <a href="#{resource_url}">#{resource_title}</a>.))
-      end
-    end
-
-    describe "notification_title" do
-      it "is generated correctly" do
-        expect(subject.notification_title)
-          .to include(%(<a href="#{author_path}">#{author_name} #{author_nickname}</a> <strong>withdrawn</strong> the <a href="#{resource_path}">#{resource_title}</a> collaborative draft.))
-      end
-    end
+    let(:email_subject) { "#{author_name} #{author_nickname} withdrawn the #{decidim_sanitize(resource_title)} collaborative draft." }
+    let(:email_intro) { %(<a href="#{author_url}">#{author_name} #{author_nickname}</a> withdrawn the <a href="#{resource_url}">#{resource_title}</a> collaborative draft.) }
+    let(:email_outro) { %(You have received this notification because you are a collaborator of <a href="#{resource_url}">#{resource_title}</a>.) }
+    let(:notificaiton_title) { %(<a href="#{author_path}">#{author_name} #{author_nickname}</a> <strong>withdrawn</strong> the <a href="#{resource_path}">#{resource_title}</a> collaborative draft.) }
   end
 end
