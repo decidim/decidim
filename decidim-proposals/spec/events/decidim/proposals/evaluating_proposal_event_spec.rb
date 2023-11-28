dff6137fef454e988fbe65d0eb36e43016d4c292
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-11-28 12:26:24 +0100

    refactor of evaluatingh proposal event spec

--- decidim-proposals/spec/events/decidim/proposals/evaluating_proposal_event_spec.rb
+++ decidim-proposals/spec/events/decidim/proposals/evaluating_proposal_event_spec.rb
@@ -9,30 +9,8 @@ describe Decidim::Proposals::EvaluatingProposalEvent do
   include_context "when a simple event"
   it_behaves_like "a simple event"
 
-  describe "email_subject" do
-    it "is generated correctly" do
-      expect(subject.email_subject).to eq("A proposal you are following is being evaluated")
-    end
-  end
-
-  describe "email_intro" do
-    it "is generated correctly" do
-      expect(subject.email_intro)
-        .to eq("The proposal \"#{translated(resource.title)}\" is currently being evaluated. You can check for an answer in this page:")
-    end
-  end
-
-  describe "email_outro" do
-    it "is generated correctly" do
-      expect(subject.email_outro)
-        .to eq("You have received this notification because you are following \"#{translated(resource.title)}\". You can unfollow it from the previous link.")
-    end
-  end
-
-  describe "notification_title" do
-    it "is generated correctly" do
-      expect(subject.notification_title)
-        .to include("The <a href=\"#{resource_path}\">#{translated(resource.title)}</a> proposal is being evaluated")
-    end
-  end
+  let(:email_subject) { "A proposal you are following is being evaluated" }
+  let(:email_intro) { "The proposal \"#{translated(resource.title)}\" is currently being evaluated. You can check for an answer in this page:" }
+  let(:email_outro) { "You have received this notification because you are following \"#{translated(resource.title)}\". You can unfollow it from the previous link." }
+  let(:notification_title) { "The <a href=\"#{resource_path}\">#{translated(resource.title)}</a> proposal is being evaluated" }
 end
