78d20627650a05d8d93e7c958e1dd78720a47f7d
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-11-28 12:05:06 +0100

    refactor of publish_proposal_event_spec

--- decidim-proposals/spec/events/decidim/proposals/publish_proposal_event_spec.rb
+++ decidim-proposals/spec/events/decidim/proposals/publish_proposal_event_spec.rb
@@ -78,31 +78,10 @@ module Decidim
         include_context "when a simple event"
         it_behaves_like "a simple event"
 
-        describe "email_subject" do
-          it "is generated correctly" do
-            expect(subject.email_subject).to eq("New proposal \"#{resource_title}\" added to #{participatory_space_title}")
-          end
-        end
-
-        describe "email_intro" do
-          it "is generated correctly" do
-            expect(subject.email_intro).to eq("The proposal \"A nice proposal\" has been added to \"#{participatory_space_title}\" that you are following.")
-          end
-        end
-
-        describe "email_outro" do
-          it "is generated correctly" do
-            expect(subject.email_outro)
-              .to include("You have received this notification because you are following \"#{participatory_space_title}\"")
-          end
-        end
-
-        describe "notification_title" do
-          it "is generated correctly" do
-            expect(subject.notification_title)
-              .to eq("The proposal <a href=\"#{resource_path}\">A nice proposal</a> has been added to #{participatory_space_title} by #{author.name} <a href=\"/profiles/#{author.nickname}\">@#{author.nickname}</a>.")
-          end
-        end
+        let(:email_subject) { "New proposal \"#{resource_title}\" added to #{participatory_space_title}" }
+        let(:email_intro) { "The proposal \"A nice proposal\" has been added to \"#{participatory_space_title}\" that you are following." }
+        let(:email_outro) { "You have received this notification because you are following \"#{participatory_space_title}\"" }
+        let(:notification_title) { "The proposal <a href=\"#{resource_path}\">A nice proposal</a> has been added to #{participatory_space_title} by #{author.name} <a href=\"/profiles/#{author.nickname}\">@#{author.nickname}</a>." }
       end
 
       describe "translated notifications" do
