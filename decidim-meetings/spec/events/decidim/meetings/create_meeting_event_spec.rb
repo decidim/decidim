242a919aa293988098822c785c8800877875aa2b
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-11-28 11:45:41 +0100

    refactor of create_meeting_event_spec

--- decidim-meetings/spec/events/decidim/meetings/create_meeting_event_spec.rb
+++ decidim-meetings/spec/events/decidim/meetings/create_meeting_event_spec.rb
@@ -12,33 +12,10 @@ describe Decidim::Meetings::CreateMeetingEvent do
   it_behaves_like "a simple event"
   it_behaves_like "a translated meeting event"
 
-  describe "email_subject" do
-    it "is generated correctly" do
-      expect(subject.email_subject).to eq("New meeting added to #{participatory_space_title}")
-    end
-  end
-
-  describe "email_intro" do
-    it "is generated correctly" do
-      meeting_title = decidim_html_escape(resource_title)
-      expect(subject.email_intro).to eq("The meeting \"#{meeting_title}\" has been added to \"#{participatory_space_title}\" that you are following.")
-    end
-  end
-
-  describe "email_outro" do
-    it "is generated correctly" do
-      expect(subject.email_outro)
-        .to include("You have received this notification because you are following \"#{participatory_space_title}\"")
-    end
-  end
-
-  describe "notification_title" do
-    it "is generated correctly" do
-      meeting_title = decidim_html_escape(resource_title)
-      expect(subject.notification_title)
-        .to eq("The meeting <a href=\"#{resource_path}\">#{meeting_title}</a> has been added to #{participatory_space_title}")
-    end
-  end
+  let(:email_subject) { "New meeting added to #{participatory_space_title}" }
+  let(:email_intro) { "The meeting \"#{meeting_title}\" has been added to \"#{participatory_space_title}\" that you are following." }
+  let(:email_outro) { "You have received this notification because you are following \"#{participatory_space_title}\"" }
+  let(:notificaiton_title) { "The meeting <a href=\"#{resource_path}\">#{meeting_title}</a> has been added to #{participatory_space_title}" }
 
   describe "resource_text" do
     it "returns the meeting description" do
