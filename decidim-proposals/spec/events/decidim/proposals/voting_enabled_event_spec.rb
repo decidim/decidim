2219a93b78565b119088a908a01daefa460cd1c8
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-11-29 13:57:15 +0100

    fix for voting_enabled_event_spec.rb

--- decidim-proposals/spec/events/decidim/proposals/voting_enabled_event_spec.rb
+++ decidim-proposals/spec/events/decidim/proposals/voting_enabled_event_spec.rb
@@ -12,18 +12,15 @@ module Decidim
       let(:event_name) { "decidim.events.proposals.voting_enabled" }
       let(:email_subject) { "Proposal support has started for #{participatory_space.title["en"]}" }
       let(:email_intro) { "You can support proposals in #{participatory_space_title}! Start participating in this page:" }
-      let(:email_outro) { "You have received this notification because you are following #{participatory_space.title["en"]}" }
+      let(:email_outro) { "You have received this notification because you are following #{participatory_space.title["en"]}. You can stop receiving notifications following the previous link." }
       let(:notification_title) { "You can now start <a href=\"#{resource_path}\">supporting proposals</a> in <a href=\"#{participatory_space_url}\">#{participatory_space.title["en"]}</a>" }
       let(:resource) { create(:proposal_component) }
       let(:participatory_space) { resource.participatory_space }
       let(:resource_path) { main_component_path(resource) }
 
-      let(:notification_title) { "You can now start <a href=\"#{resource_path}\">supporting proposals</a> in <a href=\"#{participatory_space_url}\">#{participatory_space.title["en"]}</a>" }
-      let(:email_outro) { "You have received this notification because you are following #{participatory_space.title["en"]}" }
-      let(:email_intro) { "You can support proposals in #{participatory_space_title}! Start participating in this page:" }
-      let(:email_subject) { "Proposal support has started for #{participatory_space.title["en"]}" }
-
       it_behaves_like "a simple event"
+      it_behaves_like "a simple event email"
+      it_behaves_like "a simple event notification"
     end
   end
 end
