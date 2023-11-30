0d9e2d3b0c75a79ca3820fb114dbddb64da7c5b2
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-11-30 12:31:57 +0100

    refactor of corrections in managed user error event spec

--- decidim-verifications/spec/events/decidim/verifications/managed_user_error_event_spec.rb
+++ decidim-verifications/spec/events/decidim/verifications/managed_user_error_event_spec.rb
@@ -16,4 +16,17 @@ describe Decidim::Verifications::ManagedUserErrorEvent do
   let(:email_subject) { "Failed verification attempt against another participant" }
   let(:email_intro) { "The participant <a href=\"http://#{organization_host}/profiles/#{resource.current_user.nickname}\">#{resource.current_user.name}</a> has tried to verify themself with the data of another participant (<a href=\"http://#{organization_host}/profiles/#{resource.managed_user.nickname}\">#{resource.managed_user.name}</a>)." }
   let(:email_outro) { "Check the <a href=\"http://#{organization_host}/admin/conflicts\">Verifications's conflicts list</a> and contact the participant to verify their details and solve the issue." }
+
+  it_behaves_like "a simple event email"
+  it_behaves_like "a simple event notification"
+
+  describe "default_i18n_options" do
+    it "includes managed_user_name" do
+      expect(subject.default_i18n_options[:managed_user_name]).to eq(resource.managed_user.name)
+    end
+
+    it "includes managed_user_profile" do
+      expect(subject.default_i18n_options[:managed_user_path]).to eq("/profiles/#{resource.managed_user.nickname}")
+    end
+  end
 end
