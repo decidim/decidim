e7ef3a6afda904d183724208b1d8c3c458888faf
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-12-04 16:14:00 +0100

    Refactor for update_proposal_scope_event_spec

--- decidim-proposals/spec/events/decidim/proposals/admin/update_proposal_scope_event_spec.rb
+++ decidim-proposals/spec/events/decidim/proposals/admin/update_proposal_scope_event_spec.rb
@@ -6,34 +6,13 @@ describe Decidim::Proposals::Admin::UpdateProposalScopeEvent do
   let(:resource) { create(:proposal, title: "It is my super proposal") }
   let(:resource_title) { translated(resource.title) }
   let(:event_name) { "decidim.events.proposals.proposal_update_scope" }
+  let(:email_subject) { "The #{decidim_sanitize(resource_title)} proposal scope has been updated" }
+  let(:email_intro) { "An admin has updated the scope of your proposal \"#{decidim_html_escape(resource_title)}\", check it out in this page:" }
+  let(:email_outro) { "You have received this notification because you are the author of the proposal." }
+  let(:notification_title) { "The <a href=\"#{resource_path}\">#{decidim_html_escape(resource_title)}</a> proposal scope has been updated by an admin." }
 
   include_context "when a simple event"
   it_behaves_like "a simple event"
-
-  describe "email_subject" do
-    it "is generated correctly" do
-      expect(subject.email_subject).to eq("The #{decidim_sanitize(resource_title)} proposal scope has been updated")
-    end
-  end
-
-  describe "email_intro" do
-    it "is generated correctly" do
-      expect(subject.email_intro)
-        .to eq("An admin has updated the scope of your proposal \"#{decidim_html_escape(resource_title)}\", check it out in this page:")
-    end
-  end
-
-  describe "email_outro" do
-    it "is generated correctly" do
-      expect(subject.email_outro)
-        .to eq("You have received this notification because you are the author of the proposal.")
-    end
-  end
-
-  describe "notification_title" do
-    it "is generated correctly" do
-      expect(subject.notification_title)
-        .to include("The <a href=\"#{resource_path}\">#{decidim_html_escape(resource_title)}</a> proposal scope has been updated by an admin.")
-    end
-  end
+  it_behaves_like "a simple event email"
+  it_behaves_like "a simple event notification"
 end
