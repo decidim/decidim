2918e47bc4b80baca84fc68aff624886cd8b6195
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-12-04 15:45:17 +0100

    Refactor of participatory_process_role_assigned_event_spec

--- decidim-participatory_processes/spec/events/decidim/participatory_process_role_assigned_event_spec.rb
+++ decidim-participatory_processes/spec/events/decidim/participatory_process_role_assigned_event_spec.rb
@@ -9,32 +9,12 @@ describe Decidim::ParticipatoryProcessRoleAssignedEvent do
   let(:event_name) { "decidim.events.participatory_process.role_assigned" }
   let(:role) { create(:participatory_process_user_role, user:, participatory_process: resource, role: :admin) }
   let(:extra) { { role: } }
+  let(:email_subject) { "You have been assigned as #{role} for \"#{resource.title["en"]}\"." }
+  let(:email_outro) { "You have received this notification because you are #{role} of the \"#{resource.title["en"]}\" participatory process." }
+  let(:email_intro) { "You have been assigned as #{role} for participatory process \"#{resource.title["en"]}\"." }
+  let(:notification_title) { "You have been assigned as #{role} for participatory process <a href=\"#{resource_url}\">#{resource.title["en"]}</a>." }
 
   it_behaves_like "a simple event"
-
-  describe "email_subject" do
-    it "is generated correctly" do
-      expect(subject.email_subject).to eq("You have been assigned as #{role} for \"#{resource.title["en"]}\".")
-    end
-  end
-
-  describe "email_outro" do
-    it "is generated correctly" do
-      expect(subject.email_outro)
-        .to eq("You have received this notification because you are #{role} of the \"#{resource.title["en"]}\" participatory process.")
-    end
-  end
-
-  describe "email_intro" do
-    it "is generated correctly" do
-      expect(subject.email_intro)
-        .to eq("You have been assigned as #{role} for participatory process \"#{resource.title["en"]}\".")
-    end
-  end
-
-  describe "notification_title" do
-    it "is generated correctly" do
-      expect(subject.notification_title).to include("You have been assigned as #{role} for participatory process <a href=\"#{resource_url}\">#{resource.title["en"]}</a>.")
-    end
-  end
+  it_behaves_like "a simple event email"
+  it_behaves_like "a simple event notification"
 end
