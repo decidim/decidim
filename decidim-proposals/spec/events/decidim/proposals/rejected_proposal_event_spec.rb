d1ae49073539143b0400c7eac33829bd7625c3fa
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-12-04 15:59:39 +0100

    Refactor of rejected_proposal_event_spec.rb

--- decidim-proposals/spec/events/decidim/proposals/rejected_proposal_event_spec.rb
+++ decidim-proposals/spec/events/decidim/proposals/rejected_proposal_event_spec.rb
@@ -6,40 +6,14 @@ describe Decidim::Proposals::RejectedProposalEvent do
   let(:resource) { create(:proposal, :with_answer, title: "It is my super proposal") }
   let(:resource_title) { translated(resource.title) }
   let(:event_name) { "decidim.events.proposals.proposal_rejected" }
+  let(:email_subject) { "A proposal you are following has been rejected" }
+  let(:email_intro) { "The proposal \"#{decidim_html_escape(resource_title)}\" has been rejected. You can read the answer in this page:" }
+  let(:email_outro) { "You have received this notification because you are following \"#{decidim_html_escape(resource_title)}\". You can unfollow it from the previous link." }
+  let(:notification_title) { "The <a href=\"#{resource_path}\">#{decidim_html_escape(resource_title)}</a> proposal has been rejected." }
+  let(:resource_text) { translated(resource.answer) }
 
   include_context "when a simple event"
   it_behaves_like "a simple event"
-
-  describe "email_subject" do
-    it "is generated correctly" do
-      expect(subject.email_subject).to eq("A proposal you are following has been rejected")
-    end
-  end
-
-  describe "email_intro" do
-    it "is generated correctly" do
-      expect(subject.email_intro)
-        .to eq("The proposal \"#{decidim_html_escape(resource_title)}\" has been rejected. You can read the answer in this page:")
-    end
-  end
-
-  describe "email_outro" do
-    it "is generated correctly" do
-      expect(subject.email_outro)
-        .to eq("You have received this notification because you are following \"#{decidim_html_escape(resource_title)}\". You can unfollow it from the previous link.")
-    end
-  end
-
-  describe "notification_title" do
-    it "is generated correctly" do
-      expect(subject.notification_title)
-        .to include("The <a href=\"#{resource_path}\">#{decidim_html_escape(resource_title)}</a> proposal has been rejected")
-    end
-  end
-
-  describe "resource_text" do
-    it "shows the proposal answer" do
-      expect(subject.resource_text).to eq translated(resource.answer)
-    end
-  end
+  it_behaves_like "a simple event email"
+  it_behaves_like "a simple event notification"
 end
