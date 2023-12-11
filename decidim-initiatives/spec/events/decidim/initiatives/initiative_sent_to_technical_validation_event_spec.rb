b39db55e778da60ccef5371990bd9f251cac370c
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-12-11 13:27:57 +0100

    Refactor of initiative_sent_to_technical_validation_event_spec

--- decidim-initiatives/spec/events/decidim/initiatives/initiative_sent_to_technical_validation_event_spec.rb
+++ decidim-initiatives/spec/events/decidim/initiatives/initiative_sent_to_technical_validation_event_spec.rb
@@ -16,34 +16,4 @@ describe Decidim::Initiatives::InitiativeSentToTechnicalValidationEvent do
   it_behaves_like "a simple event"
   it_behaves_like "a simple event email"
   it_behaves_like "a simple event notification"
-
-  # describe "email_subject" do
-  #   it "is generated correctly" do
-  #     initiative_title = decidim_sanitize(resource_title)
-  #     expect(subject.email_subject).to eq("Initiative \"#{initiative_title}\" was sent to technical validation.")
-  #   end
-  # end
-
-  # describe "email_intro" do
-  #   it "is generated correctly" do
-  #     initiative_title = decidim_html_escape(resource_title)
-  #     expect(subject.email_intro)
-  #       .to eq(%(The initiative "#{initiative_title}" has been sent to technical validation. Check it out at <a href="#{admin_initiative_url}">the admin panel</a>))
-  #   end
-  # end
-
-  # describe "email_outro" do
-  #   it "is generated correctly" do
-  #     expect(subject.email_outro)
-  #       .to eq("You have received this notification because you are an admin of the platform.")
-  #   end
-  # end
-
-  # describe "notification_title" do
-  #   it "is generated correctly" do
-  #     initiative_title = decidim_html_escape(resource_title)
-  #     expect(subject.notification_title)
-  #       .to include(%(The initiative "#{initiative_title}" has been sent to technical validation. Check it out at <a href="#{admin_initiative_path}">the admin panel</a>))
-  #   end
-  # end
 end
