1e6f7bc4180a1407d99a0c25ea80b5f3a568fcfb
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-12-11 10:02:47 +0200

    Update decidim-accountability/app/commands/decidim/accountability/admin/update_result.rb
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-accountability/app/commands/decidim/accountability/admin/update_result.rb
+++ decidim-accountability/app/commands/decidim/accountability/admin/update_result.rb
@@ -8,6 +8,7 @@ module Decidim
       class UpdateResult < Decidim::Commands::UpdateResource
         include Decidim::Accountability::Admin::HasResultCommand
 
+
         fetch_form_attributes :scope, :category, :parent_id, :title, :description, :start_date, :end_date,
                               :progress, :decidim_accountability_status_id, :external_id, :weight
 
