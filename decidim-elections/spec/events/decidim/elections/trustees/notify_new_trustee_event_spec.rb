5d52557a9c6c4a959bc461e74c2b83341b865e3b
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-12-11 14:45:26 +0100

    Refactor of notify_new_trustee_event_spec

--- decidim-elections/spec/events/decidim/elections/trustees/notify_new_trustee_event_spec.rb
+++ decidim-elections/spec/events/decidim/elections/trustees/notify_new_trustee_event_spec.rb
@@ -10,33 +10,12 @@ describe Decidim::Elections::Trustees::NotifyNewTrusteeEvent do
   let(:participatory_space_title) { resource.title["en"] }
   let(:resource_title) { resource.title["en"] }
   let(:trustee_zone_url) { "http://#{resource.organization.host}/trustee" }
+  let(:email_subject) { "You are a trustee for #{participatory_space_title}." }
+  let(:email_intro) { "An admin has added you as trustee for #{participatory_space_title}. You should create your public key <a href='#{trustee_zone_url}'>in your trustee zone</a>" }
+  let(:email_outro) { "You have received this notification because you have been added as trustee for #{participatory_space_title}." }
+  let(:notification_title) { "You are a trustee for <a href=\"#{resource_url}\">#{participatory_space_title}</a>." }
 
   it_behaves_like "a simple event"
-
-  describe "email_subject" do
-    it "is generated correctly" do
-      expect(subject.email_subject).to eq("You are a trustee for #{participatory_space_title}.")
-    end
-  end
-
-  describe "email_intro" do
-    it "is generated correctly" do
-      expect(subject.email_intro)
-        .to eq("An admin has added you as trustee for #{participatory_space_title}. You should create your public key <a href='#{trustee_zone_url}'>in your trustee zone</a>")
-    end
-  end
-
-  describe "email_outro" do
-    it "is generated correctly" do
-      expect(subject.email_outro)
-        .to eq("You have received this notification because you have been added as trustee for #{participatory_space_title}.")
-    end
-  end
-
-  describe "notification_title" do
-    it "is generated correctly" do
-      expect(subject.notification_title)
-        .to eq("You are a trustee for <a href=\"#{resource_url}\">#{participatory_space_title}</a>.")
-    end
-  end
+  it_behaves_like "a simple event email"
+  it_behaves_like "a simple event notification"
 end
