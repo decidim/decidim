99d5608bc619e312e6a3876a8ba7f0e49e997540
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-12-11 10:12:03 +0200

    Run linters

--- decidim-accountability/app/commands/decidim/accountability/admin/update_result.rb
+++ decidim-accountability/app/commands/decidim/accountability/admin/update_result.rb
@@ -8,7 +8,6 @@ module Decidim
       class UpdateResult < Decidim::Commands::UpdateResource
         include Decidim::Accountability::Admin::HasResultCommand
 
-
         fetch_form_attributes :scope, :category, :parent_id, :title, :description, :start_date, :end_date,
                               :progress, :decidim_accountability_status_id, :external_id, :weight
 
