c9af229aaede92ed22d3bb9878012461f6b0006f
Anna Topalidi <60363870+antopalidi@users.noreply.github.com>
2023-12-14 12:33:07 +0100

    change the comment text
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-elections/app/events/decidim/elections/trustees/notify_trustee_tally_process_event.rb
+++ decidim-elections/app/events/decidim/elections/trustees/notify_trustee_tally_process_event.rb
@@ -4,7 +4,7 @@ module Decidim
   module Elections
     module Trustees
       class NotifyTrusteeTallyProcessEvent < Decidim::Events::SimpleEvent
-        # This event sends a notification when an an admin started tally process.
+        # This event sends a notification when an an admin started the tally process.
         def resource_name
           @resource_name ||= translated_attribute(election.title)
         end
