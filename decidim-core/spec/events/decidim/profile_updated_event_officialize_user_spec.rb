db7480e2b38ea728c9df5eee8444ff16b0dced04
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-12-14 08:29:11 +0000

    reformating of profile_updated_event_officialize_user_spec

--- decidim-core/spec/events/decidim/profile_updated_event_officialize_user_spec.rb
+++ decidim-core/spec/events/decidim/profile_updated_event_officialize_user_spec.rb
@@ -16,30 +16,4 @@ describe Decidim::ProfileUpdatedEvent do
   it_behaves_like "a simple event", true
   it_behaves_like "a simple event email"
   it_behaves_like "a simple event notification"
-
-  # describe "email_subject" do
-  #   it "is generated correctly" do
-  #     expect(subject.email_subject).to eq("#{resource.name} has been officialized")
-  #   end
-  # end
-
-  # describe "email_intro" do
-  #   it "is generated correctly" do
-  #     expect(subject.email_intro)
-  #       .to eq("Participant #{resource.name} (@#{resource.nickname}) has been officialized.")
-  #   end
-  # end
-
-  # describe "email_outro" do
-  #   it "is generated correctly" do
-  #     expect(subject.email_outro)
-  #       .to eq("You have received this notification because you are an administrator of the organization.")
-  #   end
-  # end
-
-  # describe "notification_title" do
-  #   it "is generated correctly" do
-  #     expect(subject.notification_title).to eq("Participant #{resource.name} (@#{resource.nickname}) has been officialized.")
-  #   end
-  # end
 end
