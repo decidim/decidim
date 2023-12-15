20b71e4b36edab7d89aee0edbd3970d75cd07ecc
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-12-15 10:02:27 +0000

    Refactor of user_group_updated_event_spec (BROKEN)

--- decidim-core/spec/events/decidim/user_group_updated_event_spec.rb
+++ decidim-core/spec/events/decidim/user_group_updated_event_spec.rb
@@ -9,32 +9,12 @@ describe Decidim::UserGroupUpdatedEvent do
   let(:resource) { create(:user_group) }
   let(:user_group_name) { escaped_html(resource.name) }
   let(:admin_panel_url) { "http://#{organization.host}/admin/user_groups" }
+  let(:email_subject) { "A user group has updated its profile" }
+  let(:email_intro) { %(A user group with the name #{user_group_name} has updated its profile, leaving it unverified. You can now verify it in the <a href="#{admin_panel_url}">admin panel</a>.) }
+  let(:email_outro) { "You have received this notification because you are an admin of the platform." }
+  let(:notification_title) { %(The #{user_group_name} user group has updated its profile, leaving it unverified. You can now verify it in the <a href="/admin/user_groups">admin panel</a>.) }
 
   it_behaves_like "a simple event", true
-
-  describe "email_subject" do
-    it "is generated correctly" do
-      expect(subject.email_subject).to eq("A user group has updated its profile")
-    end
-  end
-
-  describe "email_intro" do
-    it "is generated correctly" do
-      expect(subject.email_intro)
-        .to eq(%(A user group with the name #{user_group_name} has updated its profile, leaving it unverified. You can now verify it in the <a href="#{admin_panel_url}">admin panel</a>.))
-    end
-  end
-
-  describe "email_outro" do
-    it "is generated correctly" do
-      expect(subject.email_outro)
-        .to eq("You have received this notification because you are an admin of the platform.")
-    end
-  end
-
-  describe "notification_title" do
-    it "is generated correctly" do
-      expect(subject.notification_title).to include(%(The #{user_group_name} user group has updated its profile, leaving it unverified. You can now verify it in the <a href="/admin/user_groups">admin panel</a>.))
-    end
-  end
+  it_behaves_like "a simple event email"
+  it_behaves_like "a simple event notification"
 end
