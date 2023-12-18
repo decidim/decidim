b950a8325dc8a3c00f3a7b443a07b9e18cc55101
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-12-18 14:54:08 +0200

    Remove code duplication in commands (part 6)

--- decidim-meetings/app/commands/decidim/meetings/admin/destroy_meeting.rb
+++ decidim-meetings/app/commands/decidim/meetings/admin/destroy_meeting.rb
@@ -5,44 +5,13 @@ module Decidim
     module Admin
       # This command is executed when the user destroys a Meeting from the admin
       # panel.
-      class DestroyMeeting < Decidim::Command
-        # Initializes a CloseMeeting Command.
-        #
-        # meeting - The current instance of the page to be closed.
-        # current_user - the user performing the action
-        def initialize(meeting, current_user)
-          @meeting = meeting
-          @current_user = current_user
-        end
-
-        # Destroys the meeting if valid.
-        #
-        # Broadcasts :ok if successful, :invalid otherwise.
-        def call
-          return broadcast(:invalid, proposals.size) if proposals.any?
-
-          destroy_meeting
-          broadcast(:ok)
-        end
-
+      class DestroyMeeting < Decidim::Commands::DestroyResource
         private
-
-        attr_reader :current_user, :meeting
-
-        def destroy_meeting
-          Decidim.traceability.perform_action!(
-            :delete,
-            meeting,
-            current_user
-          ) do
-            meeting.destroy!
-          end
-        end
 
         def proposals
           return [] unless Decidim::Meetings.enable_proposal_linking
 
-          @proposals ||= meeting.authored_proposals.load
+          @proposals ||= resource.authored_proposals.load
         end
       end
     end
