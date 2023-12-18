c5184048a0525188e8d2e0c17e1a369d7e480978
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-12-18 09:59:24 +0000

    Refactor of create_debate_event_spec (styling)

--- decidim-debates/spec/events/decidim/debates/create_debate_event_spec.rb
+++ decidim-debates/spec/events/decidim/debates/create_debate_event_spec.rb
@@ -76,36 +76,5 @@ describe Decidim::Debates::CreateDebateEvent do
     it_behaves_like "a simple event"
     it_behaves_like "a simple event email"
     it_behaves_like "a simple event notification"
-
-    # describe "email_subject" do
-    #   it "is generated correctly" do
-    #     expect(subject.email_subject).to eq("New debate \"#{decidim_sanitize(debate_title)}\" on #{translated(space.title)}")
-    #   end
-    # end
-
-    # describe "email_intro" do
-    #   it "is generated correctly" do
-    #     expect(subject.email_intro)
-    #       .to eq("Hi,\nA new debate \"#{debate_title}\" has been created on the #{translated(space.title)} participatory space, check it out and contribute:")
-    #   end
-    # end
-
-    # describe "email_outro" do
-    #   it "is generated correctly" do
-    #     expect(subject.email_outro)
-    #       .to eq("You have received this notification because you are following the #{translated(space.title)} participatory space. " \
-    #              "You can stop receiving notifications following the previous link.")
-    #   end
-    # end
-
-    # describe "notification_title" do
-    #   it "is generated correctly" do
-    #     expect(subject.notification_title)
-    #       .to include("The <a href=\"#{resource_path}\">#{debate_title}</a> debate was created on ")
-
-    #     expect(subject.notification_title)
-    #       .to include("<a href=\"#{space_path}\">#{translated(space.title)}</a>.")
-    #   end
-    # end
   end
 end
