de04fbe56c058b4a70d89876e31373b7b4b30f08
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-12-19 18:49:11 +0100

    Fix flash of inputs in assemblies' social networks

--- decidim-core/lib/decidim/form_builder.rb
+++ decidim-core/lib/decidim/form_builder.rb
@@ -164,7 +164,7 @@ module Decidim
       tabs_content = content_tag(:div, class: "tabs-content", data: { tabs_content: tabs_id }) do
         handlers.each_with_index.inject("".html_safe) do |string, (handler, index)|
           tab_content_id = sanitize_tabs_selector "#{tabs_id}-#{name}-panel-#{index}"
-          string + content_tag(:div, class: tab_element_class_for("panel", index), id: tab_content_id) do
+          string + content_tag(:div, class: tab_element_class_for("panel", index), id: tab_content_id, "aria-hidden": tab_attr_aria_hidden_for(index)) do
             send(type, "#{handler}_handler", options.merge(label: false))
           end
         end
