f7ba655d01b877f05ebc93b08aa3d0c4076a8a2b
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-12-19 09:47:06 +0000

    Refactor of profile_updated_event_spec

--- decidim-core/spec/events/decidim/profile_updated_event_spec.rb
+++ decidim-core/spec/events/decidim/profile_updated_event_spec.rb
@@ -16,30 +16,4 @@ describe Decidim::ProfileUpdatedEvent do
   it_behaves_like "a simple event", true
   it_behaves_like "a simple event email"
   it_behaves_like "a simple event notification"
-
-  # describe "email_subject" do
-  #   it "is generated correctly" do
-  #     expect(subject.email_subject).to eq("@#{resource.nickname} updated their profile")
-  #   end
-  # end
-
-  # describe "email_intro" do
-  #   it "is generated correctly" do
-  #     expect(subject.email_intro)
-  #       .to eq("The <a href=\"#{author_presenter.profile_url}\">profile page</a> of #{resource.name} (@#{resource.nickname}), who you are following, has been updated.")
-  #   end
-  # end
-
-  # describe "email_outro" do
-  #   it "is generated correctly" do
-  #     expect(subject.email_outro)
-  #       .to eq("You have received this notification because you are following @#{resource.nickname}. You can stop receiving notifications following the previous link.")
-  #   end
-  # end
-
-  # describe "notification_title" do
-  #   it "is generated correctly" do
-  #     expect(subject.notification_title).to include("<a href=\"#{author_presenter.profile_path}\">profile page</a>")
-  #   end
-  # end
 end
