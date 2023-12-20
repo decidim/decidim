25dd9f393540e00c61198242c90a5e0c98c1b069
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-12-20 12:57:54 +0000

    Refactor of decidim-comments/spec/events/decidim/comments/comment_by_followed_user_group_event_spec.rb (NEW)

--- decidim-comments/spec/events/decidim/comments/comment_by_followed_user_group_event_spec.rb
+++ decidim-comments/spec/events/decidim/comments/comment_by_followed_user_group_event_spec.rb
@@ -13,8 +13,8 @@ module Decidim
       let(:user_group_path) { Decidim::UserGroupPresenter.new(user_group).profile_path }
       let(:email_subject) { "There is a new comment by #{escaped_html(user_group.name)} in #{resource_title}" }
       let(:email_intro) { "The group #{escaped_html(user_group.name)} has left a comment in #{resource_title}. You can read it in this page:" }
-      let(:email_outro) { "You have received this notification because you are following #{escaped_html(user_group.name)}" }
-      let(:notification_title) { "There is a new comment by <a href=\"#{user_group_path}\">#{escaped_html(user_group.name)} @#{user_group.nickname}</a> in <a href=\"#{resource_path}?commentId=#{comment.id}#comment_#{comment.id}\">#{resource_title}</a>" }
+      let(:email_outro) { "You have received this notification because you are following #{escaped_html(user_group.name)}. You can unfollow this group from its profile page." }
+      let(:notification_title) { "There is a new comment by <a href=\"#{user_group_path}\">#{escaped_html(user_group.name)} @#{user_group.nickname}</a> in <a href=\"#{resource_path}?commentId=#{comment.id}#comment_#{comment.id}\">#{resource_title}</a>." }
 
       it_behaves_like "a simple event email"
       it_behaves_like "a simple event notification"
