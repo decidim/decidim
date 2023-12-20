e563c19f37682a37e025cf993769584cf1a9608a
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-12-20 12:51:38 +0000

    Refactor of decidim-accountability/spec/events/decidim/accountability/result_progress_updated_event_spec.rb (NEW)

--- decidim-accountability/spec/events/decidim/accountability/result_progress_updated_event_spec.rb
+++ decidim-accountability/spec/events/decidim/accountability/result_progress_updated_event_spec.rb
@@ -16,6 +16,8 @@ describe Decidim::Accountability::ResultProgressUpdatedEvent do
   let(:proposal_title) { translated(proposal.title) }
   let(:email_subject) { "An update to #{translated resource.title} progress" }
   let(:notification_title) { "The result <a href=\"#{resource_path}\">#{resource_title}</a>, which includes the proposal <a href=\"#{proposal_path}\">#{proposal_title}</a>, is now 95% complete." }
+  let(:email_outro) { "You have received this notification because you are following \"#{proposal_title}\", and this proposal is included in the result \"#{translated resource.title}\". You can stop receiving notifications following the previous link." }
+  let(:email_intro) { "The result \"#{translated resource.title}\", which includes the proposal \"#{proposal_title}\", is now 95% complete. You can see it from this page:" }
 
   before do
     resource.link_resources([proposal], "included_proposals")
@@ -44,14 +46,12 @@ describe Decidim::Accountability::ResultProgressUpdatedEvent do
 
   describe "email_outro" do
     it "is generated correctly" do
-      expect(subject.email_outro).to eq("You have received this notification because you are following \"#{proposal_title}\", and this proposal is included in the result \"#{translated resource.title}\". You can stop receiving notifications following the previous link.")
       expect(subject.email_outro).not_to include(proposal.title.to_s)
     end
   end
 
   describe "email_intro" do
     it "is generated correctly" do
-      expect(subject.email_intro).to eq("The result \"#{translated resource.title}\", which includes the proposal \"#{proposal_title}\", is now 95% complete. You can see it from this page:")
       expect(subject.email_intro).not_to include(proposal.title.to_s)
     end
   end
