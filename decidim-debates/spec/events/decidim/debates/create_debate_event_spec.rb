ff7faf088ccf9b1fbc38f2c58700408a1736c5a4
Tom <101816158+greenwoodt@users.noreply.github.com>
2023-12-21 01:03:22 +0000

    Update decidim-debates/spec/events/decidim/debates/create_debate_event_spec.rb
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-debates/spec/events/decidim/debates/create_debate_event_spec.rb
+++ decidim-debates/spec/events/decidim/debates/create_debate_event_spec.rb
@@ -33,36 +33,6 @@ describe Decidim::Debates::CreateDebateEvent do
     it_behaves_like "a simple event"
     it_behaves_like "a simple event email"
     it_behaves_like "a simple event notification"
-
-    # describe "email_subject" do
-    #   it "is generated correctly" do
-    #     expect(subject.email_subject).to eq("New debate \"#{decidim_sanitize(debate_title)}\" by @#{author.nickname}")
-    #   end
-    # end
-
-    # describe "email_intro" do
-    #   it "is generated correctly" do
-    #     expect(subject.email_intro)
-    #       .to eq("Hi,\n#{author.name} @#{author.nickname}, who you are following, has created a new debate \"#{debate_title}\". Check it out and contribute:")
-    #   end
-    # end
-
-    # describe "email_outro" do
-    #   it "is generated correctly" do
-    #     expect(subject.email_outro)
-    #       .to eq("You have received this notification because you are following @#{author.nickname}. You can stop receiving notifications following the previous link.")
-    #   end
-    # end
-
-    # describe "notification_title" do
-    #   it "is generated correctly" do
-    #     expect(subject.notification_title)
-    #       .to include("<a href=\"/profiles/#{author.nickname}\">#{author.name} @#{author.nickname}</a>")
-
-    #     expect(subject.notification_title)
-    #       .to include("created the <a href=\"#{resource_path}\">#{debate_title}</a> debate.")
-    #   end
-    # end
   end
 
   context "when the notification is for space followers" do
