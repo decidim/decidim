4229fe2a54133c5b85403c66674ede87e1c516d4
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-12-29 10:21:02 +0100

    Rollback relative import in comments.component_for_testing.js

--- decidim-comments/app/packs/src/decidim/comments/comments.component_for_testing.js
+++ decidim-comments/app/packs/src/decidim/comments/comments.component_for_testing.js
@@ -1,6 +1,7 @@
 /* eslint no-undef: 0 */
+/* eslint-disable no-relative-import-paths/no-relative-import-paths */
 
-import CommentsComponent from "src/decidim/comments/comments.component"
+import CommentsComponent from "./comments.component"
 
 // This module.exports is necessary for the tests to load
 // does not conflict with import/export
