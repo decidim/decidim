afe475edf67344d273ef8d87ba2b5d013901601b
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-01-08 12:02:18 +0100

    Fix sidebar active link bug in Scopes and Area types

--- decidim-admin/lib/decidim/admin/menu.rb
+++ decidim-admin/lib/decidim/admin/menu.rb
@@ -168,13 +168,22 @@ module Decidim
                         decidim_admin.scopes_path,
                         icon_name: "price-tag-3-line",
                         position: 1.3,
-                        if: allowed_to?(:read, :scope)
+                        if: allowed_to?(:read, :scope),
+                        active: [%w(
+                          decidim/admin/scopes
+                          decidim/admin/scope_types
+                        ), []]
+
           menu.add_item :areas,
                         I18n.t("menu.areas", scope: "decidim.admin"),
                         decidim_admin.areas_path,
                         icon_name: "layout-masonry-line",
                         position: 1.5,
-                        if: allowed_to?(:read, :area)
+                        if: allowed_to?(:read, :area),
+                        active: [%w(
+                          decidim/admin/areas
+                          decidim/admin/area_types
+                        ), []]
 
           menu.add_item :help_sections,
                         I18n.t("menu.help_sections", scope: "decidim.admin"),
