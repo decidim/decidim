b7ecc90e4f27a74d8d4f1395adc7248634066709
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-01-09 09:48:28 +0200

    Fix esbuild config

--- decidim-core/lib/decidim/webpacker/esbuild.config.js
+++ decidim-core/lib/decidim/webpacker/esbuild.config.js
@@ -1,16 +1,25 @@
-const esbuild = require("esbuild");
 const sassPlugin = require("esbuild-sass-plugin").sassPlugin;
+const { EsbuildPlugin } = require("esbuild-loader");
 
-esbuild.build({
+module.exports = {
+  options: {
+    tsconfigRaw: JSON.stringify({}),
+    target: "es2015"
+  },
+  // target: ["es2015", "chrome58", "edge16", "firefox57", "safari11"],
   bundle: true,
   sourcemap: true,
-  write: true,
   minify: true,
   outdir: "public",
-  target: ["es2015", "chrome58", "edge16", "firefox57", "safari11"],
-  loader: { ".scss": "css" },
   outExtension: {
     ".css": ".scss"
   },
-  plugins: [sassPlugin({})]
-});
+  plugins: [sassPlugin({})],
+  optimization: {
+    minimizer: [
+      new EsbuildPlugin({
+        target: "es2015"
+      })
+    ]
+  }
+};
--- packages/webpacker/index.js
+++ packages/webpacker/index.js
@@ -1,17 +1,6 @@
 const { generateWebpackConfig, ...restOpts } = require("shakapacker");
-const { EsbuildPlugin } = require("esbuild-loader")
 
-const options = {
-  optimization: {
-    minimizer: [
-      new EsbuildPlugin({
-        target: "es2015"
-      })
-    ]
-  }
-}
-
-const webpackConfig = generateWebpackConfig(options)
+const webpackConfig = generateWebpackConfig();
 const overrideConfig = require("./src/override-config");
 
 module.exports = {
