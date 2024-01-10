0b13379e519214a4f8c0aef34644084a36d55f77
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-01-10 12:23:37 +0100

    Create new decidim-maintainers_toolbox gem with the bundler skeleton

---
+++ decidim-maintainers_toolbox/lib/decidim/maintainers_toolbox.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "maintainers_toolbox/version"
+
+module Decidim
+  module MaintainersToolbox
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
