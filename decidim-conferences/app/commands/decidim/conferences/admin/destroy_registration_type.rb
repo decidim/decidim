58a211148983602851707b93a3564f51541ea5a9
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-01-12 13:51:23 +0200

    Remove code duplication in commands (part 5) (#12183)
    * Remove code duplication in commands (part 5)
    
    * Update resources
    
    * Fix failings specs
    
    * Fix specs
    
    * more specs
    
    * Fix failing spec
    
    * Apply suggestions from code review
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-conferences/app/commands/decidim/conferences/admin/destroy_registration_type.rb
+++ decidim-conferences/app/commands/decidim/conferences/admin/destroy_registration_type.rb
@@ -5,50 +5,18 @@ module Decidim
     module Admin
       # A command with all the business logic when destroying a conference
       # registration type in the system.
-      class DestroyRegistrationType < Decidim::Command
-        # Public: Initializes the command.
-        #
-        # registration_type - the Partner to destroy
-        # current_user - the user performing this action
-        def initialize(registration_type, current_user)
-          @registration_type = registration_type
-          @current_user = current_user
-        end
+      class DestroyRegistrationType < Decidim::Commands::DestroyResource
+        protected
 
-        # Executes the command. Broadcasts these events:
-        #
-        # - :ok when everything is valid.
-        # - :invalid if the form was not valid and we could not proceed.
-        #
-        # Returns nothing.
-        def call
-          destroy_registration_type!
-          broadcast(:ok)
-        end
-
-        private
-
-        attr_reader :registration_type, :current_user
-
-        def destroy_registration_type!
-          log_info = {
+        def extra_params
+          {
             resource: {
-              title: registration_type.title
+              title: resource.title
             },
             participatory_space: {
-              title: registration_type.conference.title
+              title: resource.conference.title
             }
           }
-
-          Decidim.traceability.perform_action!(
-            "delete",
-            registration_type,
-            current_user,
-            log_info
-          ) do
-            registration_type.destroy!
-            registration_type
-          end
         end
       end
     end
