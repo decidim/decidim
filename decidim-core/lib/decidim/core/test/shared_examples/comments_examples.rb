be048154ddc4759be50f42376568ce300e712ca1
Tom Greenwood <tom.t.greenwood@gmail.com>
2024-01-15 14:06:09 +0100

    Andres review changes 15/01 - comment__footer

--- decidim-core/lib/decidim/core/test/shared_examples/comments_examples.rb
+++ decidim-core/lib/decidim/core/test/shared_examples/comments_examples.rb
@@ -91,7 +91,7 @@ shared_examples "comments" do
       expect(page).not_to have_content(translated(deleted_comment.body))
       within "#comment_#{deleted_comment.id}" do
         expect(page).to have_content("Comment deleted on")
-        expect(page).not_to have_selector([data-comment-footer])
+        expect(page).not_to have_selector("div[data-comment-footer]")
       end
     end
 
