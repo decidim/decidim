e54a7768fef5b43f206c99afbcdc7bf56f7c9cbe
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-01-26 23:27:23 +0200

    Fix decidim participatory process output

--- decidim-participatory_processes/spec/events/decidim/participatory_process_role_assigned_event_spec.rb
+++ decidim-participatory_processes/spec/events/decidim/participatory_process_role_assigned_event_spec.rb
@@ -9,10 +9,10 @@ describe Decidim::ParticipatoryProcessRoleAssignedEvent do
   let(:event_name) { "decidim.events.participatory_process.role_assigned" }
   let(:role) { create(:participatory_process_user_role, user:, participatory_process: resource, role: :admin) }
   let(:extra) { { role: } }
-  let(:email_subject) { "You have been assigned as #{role} for \"#{resource.title["en"]}\"." }
-  let(:email_outro) { "You have received this notification because you are #{role} of the \"#{resource.title["en"]}\" participatory process." }
-  let(:email_intro) { "You have been assigned as #{role} for participatory process \"#{resource.title["en"]}\"." }
-  let(:notification_title) { "You have been assigned as #{role} for participatory process <a href=\"#{resource_url}\">#{resource.title["en"]}</a>." }
+  let(:email_subject) { "You have been assigned as #{role} for \"#{resource_title}\"." }
+  let(:email_outro) { "You have received this notification because you are #{role} of the \"#{resource_title}\" participatory process." }
+  let(:email_intro) { "You have been assigned as #{role} for participatory process \"#{resource_title}\"." }
+  let(:notification_title) { "You have been assigned as #{role} for participatory process <a href=\"#{resource_url}\">#{resource_title}</a>." }
 
   it_behaves_like "a simple event"
   it_behaves_like "a simple event email"
