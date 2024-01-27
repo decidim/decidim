6516f10d9ba5374325195310c6243fd54d6d6446
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-01-27 19:07:27 +0200

    Some refactor

--- decidim-admin/spec/events/decidim/resource_hidden_event_spec.rb
+++ decidim-admin/spec/events/decidim/resource_hidden_event_spec.rb
@@ -6,13 +6,13 @@ module Decidim
   describe ResourceHiddenEvent do
     include_context "when a simple event"
 
-    let(:comment) { create(:comment, body: { "en" => "a reported comment" }) }
+    let(:comment) { create(:comment, body: generate_localized_description(:comment_body)) }
     let(:moderation) { create(:moderation, reportable: comment) }
     let(:report) { create(:report, moderation:) }
     let(:resource) { comment }
     let(:event_name) { "decidim.events.reports.resource_hidden" }
     let(:extra) { { report_reasons: ["spam"] } }
-    let(:notification_title) { "An administrator removed your comment because it has been reported as spam.</br>\n<i>a reported comment</i>" }
+    let(:notification_title) { "An administrator removed your comment because it has been reported as spam.</br>\n<i>#{translated(resource.body)}</i>" }
     let(:email_subject) { "Your comment has been removed" }
     let(:email_outro) { "You have received this notification because you are an author of the removed content." }
     let(:email_intro) { "An administrator removed your comment because it has been reported as spam." }
@@ -22,7 +22,7 @@ module Decidim
 
     describe "resource_text" do
       it "is generated correctly" do
-        expect(subject.resource_text).to include("<i>#{comment.body["en"]}</i>")
+        expect(subject.resource_text).to include("<i>#{translated(resource.body)}</i>")
       end
     end
   end
