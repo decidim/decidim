bff300d5b366da23d7b6380bd253ef45705b76ea
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2024-01-29 20:38:35 +0100

    Fix bug in welcome notifications when the organization has weird characters
    * Fix bug in welcome notifications when the organization has weird characters
    
    * Remove unecessary mustache dependency
    
    * Make sure the formatting is displayed as HTML in the mail body
    
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>
    
    ---------
    
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>

---
+++ decidim-core/spec/events/decidim/welcome_notification_event_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Decidim::WelcomeNotificationEvent do
+  subject { event_instance }
+
+  let(:event_instance) do
+    described_class.new(
+      resource: user,
+      event_name:,
+      user:
+    )
+  end
+  let(:event_name) { "decidim.events.core.welcome_notification" }
+  let(:user) { create(:user, organization:) }
+  let(:organization) { create(:organization, name: organization_name) }
+
+  context "with a normal organization name" do
+    let(:organization_name) { "My Organization" }
+
+    describe "#email_subject" do
+      subject { event_instance.email_subject }
+
+      it { is_expected.to eq("Thanks for joining My Organization!") }
+    end
+
+    describe "#email_intro" do
+      subject { event_instance.email_intro }
+
+      it { is_expected.to match(%r{^<p>Hi #{CGI.escapeHTML(user.name)}, thanks for joining My Organization and welcome!</p>}) }
+    end
+  end
+
+  context "with an organization with an apostrophe" do
+    let(:organization_name) { "My ol'Organization" }
+
+    describe "#email_subject" do
+      subject { event_instance.email_subject }
+
+      it { is_expected.to eq("Thanks for joining My ol'Organization!") }
+    end
+
+    describe "#email_intro" do
+      subject { event_instance.email_intro }
+
+      it { is_expected.to match(%r{^<p>Hi #{CGI.escapeHTML(user.name)}, thanks for joining My ol'Organization and welcome!</p>}) }
+    end
+  end
+end
