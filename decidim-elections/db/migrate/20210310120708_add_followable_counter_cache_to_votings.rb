90e991e9de43450053a311b9f063600c53080e6f
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-01-29 15:00:45 +0100

    Fix voting data migration for AddFollowableCounterCacheToVotings

--- decidim-elections/db/migrate/20210310120708_add_followable_counter_cache_to_votings.rb
+++ decidim-elections/db/migrate/20210310120708_add_followable_counter_cache_to_votings.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
 class AddFollowableCounterCacheToVotings < ActiveRecord::Migration[5.2]
+  class Voting < ApplicationRecord
+    self.table_name = :decidim_votings_votings
+  end
+
   def change
     add_column :decidim_votings_votings, :follows_count, :integer, null: false, default: 0, index: true
 
     reversible do |dir|
       dir.up do
-        Decidim::Votings::Voting.reset_column_information
-        Decidim::Votings::Voting.find_each do |record|
+        Voting.reset_column_information
+        Voting.find_each do |record|
           record.class.reset_counters(record.id, :follows)
         end
       end
