e126cb15934cc4583fbbda43ba31d58a6d461da6
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-01-29 16:32:41 +0200

    Fix admin spec

--- decidim-admin/spec/events/decidim/resource_hidden_event_spec.rb
+++ decidim-admin/spec/events/decidim/resource_hidden_event_spec.rb
@@ -10,9 +10,10 @@ module Decidim
     let(:moderation) { create(:moderation, reportable: comment) }
     let(:report) { create(:report, moderation:) }
     let(:resource) { comment }
+    let(:resource_text) { "<i>#{decidim_sanitize(translated(resource.body), strip_tags: true)}</i>" }
     let(:event_name) { "decidim.events.reports.resource_hidden" }
     let(:extra) { { report_reasons: ["spam"] } }
-    let(:notification_title) { "An administrator removed your comment because it has been reported as spam.</br>\n<i>#{translated(resource.body)}</i>" }
+    let(:notification_title) { "An administrator removed your comment because it has been reported as spam.</br>\n#{resource_text}" }
     let(:email_subject) { "Your comment has been removed" }
     let(:email_outro) { "You have received this notification because you are an author of the removed content." }
     let(:email_intro) { "An administrator removed your comment because it has been reported as spam." }
@@ -22,7 +23,7 @@ module Decidim
 
     describe "resource_text" do
       it "is generated correctly" do
-        expect(subject.resource_text).to include("<i>#{translated(resource.body)}</i>")
+        expect(subject.resource_text).to include(resource_text)
       end
     end
   end
