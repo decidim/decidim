10a52c5c28434118a0e3f5e8033c05a93f7286ff
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-02-07 16:11:09 +0200

    Patch events on the new format
    * Patch events on the new format
    
    * Some refactor
    
    * Fix more specs
    
    * Fix specs
    
    * Running linters
    
    * Patch the specs
    
    * RUnning linters
    
    * Fix admin spec
    
    * Fixing more specs
    
    * patch test
    
    * Apply review recommendations
    
    * Fix failing specs
    
    * Fix failing specs on conferences
    
    * Update decidim-core/spec/lib/events/simple_event_spec.rb
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-debates/spec/events/decidim/debates/create_debate_event_spec.rb
+++ decidim-debates/spec/events/decidim/debates/create_debate_event_spec.rb
@@ -3,16 +3,12 @@
 require "spec_helper"
 
 describe Decidim::Debates::CreateDebateEvent do
-  include Decidim::SanitizeHelper
-
   include_context "when a simple event"
 
-  let(:resource) { create(:debate, :participant_author, title: { en: "It is my debate" }) }
+  let(:resource) { create(:debate, :participant_author, title: generate_localized_title(:debate_title)) }
   let(:space) { resource.participatory_space }
   let(:event_name) { "decidim.events.debates.debate_created" }
-  let(:space_path) { resource_locator(space).path }
   let(:extra) { { type: type.to_s } }
-  let(:debate_title) { decidim_html_escape(translated(resource.title)) }
 
   describe "resource_text" do
     let(:type) { "" }
@@ -25,10 +21,10 @@ describe Decidim::Debates::CreateDebateEvent do
   context "when the notification is for user followers" do
     let(:type) { :user }
     let(:i18n_scope) { "decidim.events.debates.create_debate_event.user_followers" }
-    let(:email_subject) { "New debate \"#{decidim_sanitize(debate_title)}\" by @#{author.nickname}" }
-    let(:email_intro) { "Hi,\n#{author.name} @#{author.nickname}, who you are following, has created a new debate \"#{debate_title}\". Check it out and contribute:" }
+    let(:email_subject) { "New debate \"#{resource_title}\" by @#{author.nickname}" }
+    let(:email_intro) { "Hi,\n#{author.name} @#{author.nickname}, who you are following, has created a new debate \"#{resource_title}\". Check it out and contribute:" }
     let(:email_outro) { "You have received this notification because you are following @#{author.nickname}. You can stop receiving notifications following the previous link." }
-    let(:notification_title) { "<a href=\"/profiles/#{author.nickname}\">#{author.name} @#{author.nickname}</a> created the <a href=\"#{resource_path}\">#{debate_title}</a> debate." }
+    let(:notification_title) { "<a href=\"/profiles/#{author.nickname}\">#{author.name} @#{author.nickname}</a> created the <a href=\"#{resource_path}\">#{resource_title}</a> debate." }
 
     it_behaves_like "a simple event"
     it_behaves_like "a simple event email"
@@ -38,10 +34,10 @@ describe Decidim::Debates::CreateDebateEvent do
   context "when the notification is for space followers" do
     let(:type) { :space }
     let(:i18n_scope) { "decidim.events.debates.create_debate_event.space_followers" }
-    let(:email_subject) { "New debate \"#{decidim_sanitize(debate_title)}\" on #{translated(space.title)}" }
-    let(:email_intro) { "Hi,\nA new debate \"#{debate_title}\" has been created on the #{translated(space.title)} participatory space, check it out and contribute:" }
-    let(:email_outro) { "You have received this notification because you are following the #{translated(space.title)} participatory space. You can stop receiving notifications following the previous link." }
-    let(:notification_title) { "The <a href=\"#{resource_path}\">#{debate_title}</a> debate was created on <a href=\"#{space_path}\">#{translated(space.title)}</a>." }
+    let(:email_subject) { "New debate \"#{resource_title}\" on #{participatory_space_title}" }
+    let(:email_intro) { "Hi,\nA new debate \"#{resource_title}\" has been created on the #{participatory_space_title} participatory space, check it out and contribute:" }
+    let(:email_outro) { "You have received this notification because you are following the #{participatory_space_title} participatory space. You can stop receiving notifications following the previous link." }
+    let(:notification_title) { "The <a href=\"#{resource_path}\">#{resource_title}</a> debate was created on <a href=\"#{participatory_space_url}\">#{participatory_space_title}</a>." }
 
     it_behaves_like "a simple event"
     it_behaves_like "a simple event email"
