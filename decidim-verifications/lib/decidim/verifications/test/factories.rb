2420f7622e78b41303c2ee2094f3ba011d430937
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-02-08 04:33:57 +0200

    Patch factories in decidim-verifications

--- decidim-verifications/lib/decidim/verifications/test/factories.rb
+++ decidim-verifications/lib/decidim/verifications/test/factories.rb
@@ -5,13 +5,19 @@ require "decidim/dev"
 
 FactoryBot.define do
   factory :csv_datum, class: "Decidim::Verifications::CsvDatum" do
+    transient do
+      skip_injection { false }
+    end
     email { generate(:email) }
     organization
   end
 
   factory :conflict, class: "Decidim::Verifications::Conflict" do
-    current_user { create(:user) }
-    managed_user { create(:user, managed: true, organization: current_user.organization) }
+    transient do
+      skip_injection { false }
+    end
+    current_user { create(:user, skip_injection:) }
+    managed_user { create(:user, managed: true, organization: current_user.organization, skip_injection:) }
     unique_id { "12345678X" }
     times { 1 }
   end
