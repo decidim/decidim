014b55c025e5bec3e07e86f8e5bd847cf4e46454
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2024-02-08 15:05:28 +0100

    Fix decidim_escape_translated call
    Suggested in code review
    
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>

--- decidim-core/app/helpers/decidim/check_boxes_tree_helper.rb
+++ decidim-core/app/helpers/decidim/check_boxes_tree_helper.rb
@@ -52,20 +52,20 @@ module Decidim
       organization = current_participatory_space.organization
 
       sorted_main_categories = current_participatory_space.categories.first_class.includes(:subcategories).sort_by do |category|
-        [category.weight, decidim_escape_translated(category.name, organization)]
+        [category.weight, decidim_escape_translated(category.name)]
       end
 
       categories_values = sorted_main_categories.flat_map do |category|
         sorted_descendant_categories = category.descendants.includes(:subcategories).sort_by do |subcategory|
-          [subcategory.weight, decidim_escape_translated(subcategory.name, organization)]
+          [subcategory.weight, decidim_escape_translated(subcategory.name)]
         end
 
         subcategories = sorted_descendant_categories.flat_map do |subcategory|
-          TreePoint.new(subcategory.id.to_s, decidim_escape_translated(subcategory.name, organization))
+          TreePoint.new(subcategory.id.to_s, decidim_escape_translated(subcategory.name))
         end
 
         TreeNode.new(
-          TreePoint.new(category.id.to_s, decidim_escape_translated(category.name, organization)),
+          TreePoint.new(category.id.to_s, decidim_escape_translated(category.name)),
           subcategories
         )
       end
