83edd30c0b41ea586b849331a29e97ab51ba9b45
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-02-09 01:13:04 -0500

    spelling: github
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- decidim-assemblies/lib/decidim/api/assembly_type.rb
+++ decidim-assemblies/lib/decidim/api/assembly_type.rb
@@ -55,7 +55,7 @@ module Decidim
       field :instagram_handler, String, "Instagram handler", null: true
       field :facebook_handler, String, "Facebook handler", null: true
       field :youtube_handler, String, "Youtube handler", null: true
-      field :github_handler, String, "Github handler", null: true
+      field :github_handler, String, "GitHub handler", null: true
       field :announcement, Decidim::Core::TranslatedFieldType, "Highlighted announcement for this assembly", null: true
 
       field :members, [Decidim::Assemblies::AssemblyMemberType, { null: true }], "Members of this assembly", null: false
