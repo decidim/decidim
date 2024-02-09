8e7875800d539128aea8b6982ae061331ca9f991
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-02-09 06:40:19 -0500

    spelling: selection
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- decidim-admin/spec/helpers/exports_helper_spec.rb
+++ decidim-admin/spec/helpers/exports_helper_spec.rb
@@ -46,7 +46,7 @@ module Decidim
             expect(subject).to have_css("#export-dropdown")
             expect(subject).to have_no_css("#export-selection-dropdown")
             expect(subject).to have_content("Export all")
-            expect(subject).to have_no_content("Export selectioin")
+            expect(subject).to have_no_content("Export selection")
             expect(helper).to have_received(:render_dropdown).with(component:, resource_id: nil, filters: {}).once
             expect(helper).not_to have_received(:render_dropdown).with(component:, resource_id: nil, filters: { id_in: [1] })
           end
