56843cf8fb1b0b5cc2f84dea5404dad52c3d038c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-02-09 05:11:45 -0500

    spelling: correctly
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- decidim-meetings/spec/cells/decidim/meetings/meeting_cell_spec.rb
+++ decidim-meetings/spec/cells/decidim/meetings/meeting_cell_spec.rb
@@ -27,7 +27,7 @@ module Decidim::Meetings
         meeting.reload
       end
 
-      it "escapes them correclty" do
+      it "escapes them correctly" do
         # as the `cell` test helper wraps content in a Capybara artifact that already converts html entities
         # we should compare with the expected visual result, as we were checking the DOM instead of the html
         expect(cell_html).to have_content("#{@original_title} &'<")
