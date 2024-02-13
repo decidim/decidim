d1c731c574f5f42ffd32b251f3e33fc303770936
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-02-13 18:55:27 +0200

    Apply suggestions from code review
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-core/spec/presenters/decidim/admin_log/oauth_application_presenter_spec.rb
+++ decidim-core/spec/presenters/decidim/admin_log/oauth_application_presenter_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper"
 
 describe Decidim::AdminLog::OAuthApplicationPresenter, type: :helper do
   include_examples "present admin log entry" do
-    let(:admin_log_resource) { create(:oauth_application, organization:) }
+    let(:admin_log_resource) { create(:oauth_application, organization: organization) }
     let(:action) { "update" }
   end
 end
