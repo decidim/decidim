f34610438e9b6ea9c10e9be25a189413ea9d8c27
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-02-21 15:16:43 +0200

    Skip authenticity token in questionnaire templates
    * Skip authenticity token in certain cases
    
    * Add spec for testing the fix is in place

---
+++ decidim-templates/spec/system/admin/admin_applies_questionnaire_template_to_component_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Admin applies questionnaire templates" do
+  let!(:organization) { create(:organization) }
+  let!(:user) { create(:user, :admin, :confirmed, organization:) }
+  let!(:participatory_space) { create(:participatory_process, organization:) }
+  let!(:component) { create(:component, participatory_space:) }
+  let!(:questionnaire_template) { create(:questionnaire_template, :with_all_questions, organization:, skip_injection: true) }
+
+  around do |example|
+    ActionController::Base.allow_forgery_protection = true
+    example.run
+    ActionController::Base.allow_forgery_protection = false
+  end
+
+  before do
+    switch_to_host(organization.host)
+    login_as user, scope: :user
+    visit decidim_admin_participatory_processes.components_path(participatory_space)
+  end
+
+  it "installs a survey component" do
+    find("button[data-toggle=add-component-dropdown]").click
+
+    within "#add-component-dropdown" do
+      find(".surveys").click
+    end
+
+    click_on "Add component"
+    click_on "Survey"
+
+    select(translated_attribute(questionnaire_template.name), from: "select-template")
+    expect(page).to have_content("If you are human, ignore this field")
+    click_on "Create from template"
+
+    expect(page).to have_content("Template applied successfully.")
+  end
+end
