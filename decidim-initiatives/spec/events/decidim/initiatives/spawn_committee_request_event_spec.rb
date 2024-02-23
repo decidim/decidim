28b2acf8d772a9c43c3cd739c6194be7fc5cea15
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-02-23 10:33:59 +0200

    Backport 'Refactor of events specs' to v0.27 (#12507)
    * Refactor of events specs
    
    * Refactor of event_spec in amendment_promoted_event_examples.rb
    
    * refactor of event_spec in created_event_examples. Adding of attributes to each modified file
    
    * misspelling of arguement test
    
    * partial completion of managed_user_error_event_spec
    
    * reverting to old version
    
    * fix to cli tests
    
    * fix to mixed up branch
    
    * fixing mix up of commits
    
    * refactor of the managed error user error event spec
    
    * refactor of create_sortition_event_spec
    
    * refactor collaborative_draft_wtihdrawn_spec
    
    * refactor of create_meeting_event_spec
    
    * refactor of collaborative_draft_access_requested_event_spec
    
    * refactor of publish_proposal_event_spec
    
    * refactor of voting enabled event spec
    
    * refactor of endorsing enbabled event spec
    
    * refactor accepted proposal event spec
    
    * refactor of evaluatingh proposal event spec
    
    * auto-correcting errors
    
    * refactor of accepted_proposal_event_spec
    
    * refactor for endorising enbabled event
    
    * fix for voting_enabled_event_spec.rb
    
    * Refactor made to publish proposal event spec
    
    * Refactor correction of collaborative draft proposal
    
    * refactor correction of create meeting event spec
    
    * refactor and corrction of collaborative draft withdrawn
    
    * refactor correction of sortitions event spec
    
    * refactor of corrections in managed user error event spec
    
    * correctable syntax spacing in test
    
    * refactor of participory process step changed event spec
    
    * Refactor of events for creation_enbabled_event_spec
    
    * Deletion of commented out code
    
    * Refactor of the collaborative_draft_access_rejected_event_spec
    
    * Refactor of collaborative_draft_access_accepted_event_spec
    
    * removal of commented out sections
    
    * Refactor of participatory_process_step_activated_event_spec
    
    * Refactor of participatory_process_role_assigned_event_spec
    
    * Refactor of rejected_proposal_event_spec.rb
    
    * Refactor of update_proposal_category_event_spec.rb
    
    * Refactor for update_proposal_scope_event_spec
    
    * Refactor for approve_membership_request_event_spec
    
    * Refactor of revoke_membership_request_event_spec
    
    * Refactor of support_threshold_reached_event_spec
    
    * Refactor of spawn_committee_request_event_spec
    
    * Refactor endorse_initiative_event_spec
    
    * Refactor of proposal_note_created_event_spec
    
    * Removal of :resource object as its used twice in proposal_note_created_event
    
    * Refactor of endorse_initiative_event_spec (minor change to test)
    
    * Update decidim-initiatives/spec/cells/decidim/initiatives/content_blocks/highlighted_initiatives_cell_spec.rb
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    * Add missing examples
    
    * Fix failing specs
    
    * Fix more specs
    
    * Fix spec on decidim-initiatives
    
    ---------
    
    Co-authored-by: Tom <101816158+greenwoodt@users.noreply.github.com>
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-initiatives/spec/events/decidim/initiatives/spawn_committee_request_event_spec.rb
+++ decidim-initiatives/spec/events/decidim/initiatives/spawn_committee_request_event_spec.rb
@@ -21,6 +21,10 @@ describe Decidim::Initiatives::SpawnCommitteeRequestEvent do
   let(:applicant_nickname) { Decidim::UserPresenter.new(applicant).nickname }
   let(:resource_url) { resource_locator(initiative).url }
   let(:resource_title) { translated(initiative.title) }
+  let(:email_subject) { "#{applicant_nickname} wants to join your initiative" }
+  let(:email_intro) { "#{applicant_nickname} applied for the promoter committee of your initiative #{resource_title}. To accept or reject the application, go to the edit form of your initiative." }
+  let(:email_outro) { "You received this notification because you are the author of this initiative: #{resource_title}" }
+  let(:notification_title) { "<a href=\"#{applicant_profile_url}\">#{applicant_nickname}</a> applied for the promoter committee of your initiative <a href=\"#{resource_url}\">#{resource_title}</a>. To accept or reject click <a href=\"#{resource_url}/edit\">here</a>." }
 
   describe "types" do
     subject { described_class }
@@ -34,29 +38,6 @@ describe Decidim::Initiatives::SpawnCommitteeRequestEvent do
     end
   end
 
-  describe "email_subject" do
-    it "is generated correctly" do
-      expect(subject.email_subject).to eq("#{applicant_nickname} wants to join your initiative")
-    end
-  end
-
-  describe "email_intro" do
-    it "is generated correctly" do
-      expect(subject.email_intro).to eq("#{applicant_nickname} applied for the promoter committee of your initiative #{resource_title}. To accept or reject the application, go to the edit form of your initiative.")
-    end
-  end
-
-  describe "email_outro" do
-    it "is generated correctly" do
-      expect(subject.email_outro)
-        .to eq("You received this notification because you are the author of this initiative: #{resource_title}")
-    end
-  end
-
-  describe "notification_title" do
-    it "is generated correctly" do
-      expect(subject.notification_title)
-        .to eq("<a href=\"#{applicant_profile_url}\">#{applicant_nickname}</a> applied for the promoter committee of your initiative <a href=\"#{resource_url}\">#{resource_title}</a>. To accept or reject click <a href=\"#{resource_url}/edit\">here</a>.")
-    end
-  end
+  it_behaves_like "a simple event email"
+  it_behaves_like "a simple event notification"
 end
