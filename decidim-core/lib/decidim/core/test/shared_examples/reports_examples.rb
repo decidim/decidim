1e35d0105ba2843296dd3a1cf0d388316e992968
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-02-28 12:34:37 +0200

    Use default rules of rubocop-capybara

--- decidim-core/lib/decidim/core/test/shared_examples/reports_examples.rb
+++ decidim-core/lib/decidim/core/test/shared_examples/reports_examples.rb
@@ -72,7 +72,7 @@ shared_examples "higher user role does not have hide" do
       expect(page).to have_css(".flag-modal", visible: :visible)
 
       within ".flag-modal" do
-        expect(page).not_to have_field(name: "report[hide]")
+        expect(page).to have_no_field(name: "report[hide]")
       end
     end
   end
@@ -83,7 +83,7 @@ shared_examples "reports" do
     it "gives the option to sign in" do
       visit reportable_path
 
-      expect(page).not_to have_css("html.is-disabled")
+      expect(page).to have_no_css("html.is-disabled")
 
       click_on "Report"
 
