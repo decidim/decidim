0fc0a95f23b81ea3f24955b41ccf8594796b765e
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-03-01 00:26:55 +0200

    Patch all project factories

--- decidim-templates/lib/decidim/templates/test/factories.rb
+++ decidim-templates/lib/decidim/templates/test/factories.rb
@@ -5,28 +5,32 @@ require "decidim/dev"
 
 FactoryBot.define do
   factory :template, class: "Decidim::Templates::Template" do
+    transient do
+      skip_injection { false }
+    end
     organization
-    templatable { build(:dummy_resource) }
+    name { generate_localized_title(:template_name, skip_injection: skip_injection) }
+    description { generate_localized_title(:template_description, skip_injection: skip_injection) }
     name { Decidim::Faker::Localized.sentence }
 
     ## Questionnaire templates
     factory :questionnaire_template do
       trait :with_questions do
-        after(:create) do |template|
-          template.templatable = create(:questionnaire, :with_questions, questionnaire_for: template)
+        after(:create) do |template, evaluator|
+          template.templatable = create(:questionnaire, :with_questions, questionnaire_for: template, skip_injection: evaluator.skip_injection)
           template.save!
         end
       end
 
       trait :with_all_questions do
-        after(:create) do |template|
-          template.templatable = create(:questionnaire, :with_all_questions, questionnaire_for: template)
+        after(:create) do |template, evaluator|
+          template.templatable = create(:questionnaire, :with_all_questions, questionnaire_for: template, skip_injection: evaluator.skip_injection)
           template.save!
         end
       end
 
-      after(:create) do |template|
-        template.templatable = create(:questionnaire, questionnaire_for: template)
+      after(:create) do |template, evaluator|
+        template.templatable = create(:questionnaire, questionnaire_for: template, skip_injection: evaluator.skip_injection)
         template.save!
       end
     end
