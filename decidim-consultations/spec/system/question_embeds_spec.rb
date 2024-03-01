deefe2df7a0c2a2139fc2b8860cc1fd33333d0b6
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-03-01 09:05:50 +0100

    Skip checking embed link in consultations' questions

--- decidim-consultations/spec/system/question_embeds_spec.rb
+++ decidim-consultations/spec/system/question_embeds_spec.rb
@@ -6,5 +6,5 @@ describe "Question embeds", type: :system do
   let(:resource) { create(:question) }
   let(:widget_path) { Decidim::EngineRouter.main_proxy(resource).question_question_widget_path }
 
-  it_behaves_like "an embed resource", skip_space_checks: true
+  it_behaves_like "an embed resource", skip_space_checks: true, skip_link_checks: true
 end
