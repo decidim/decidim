15c540e6c6281b922aa19df941c1978739e5fc0a
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-03-11 12:55:40 +0100

    Implement announcement preview

--- decidim-admin/app/packs/entrypoints/decidim_admin.js
+++ decidim-admin/app/packs/entrypoints/decidim_admin.js
@@ -36,7 +36,7 @@ window.addEventListener("DOMContentLoaded", () => {
     })
   })
 
-  document.querySelectorAll("input[data-text-copy=true]").forEach((element) => {
+  document.querySelectorAll("input[data-text-copy=true], textarea[data-text-copy=true]").forEach((element) => {
     element.addEventListener("change", (event) => {
       const target = document.querySelector(event.target.dataset.target);
       target.innerText = event.target.value;
