4ba862a01ea91b9af5a46ea3bf9e5dc873014d12
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-03-13 14:59:46 +0100

    Use same colors from the class in the migration

--- decidim-proposals/db/migrate/20240209092404_change_color_fields_on_proposals_states.rb
+++ decidim-proposals/db/migrate/20240209092404_change_color_fields_on_proposals_states.rb
@@ -1,17 +1,48 @@
 # frozen_string_literal: true
 
 class ChangeColorFieldsOnProposalsStates < ActiveRecord::Migration[6.1]
+  class ProposalState
+    def self.colors
+      {
+        gray: {
+          background: "#F6F8FA",
+          foreground: "#4B5058"
+        },
+        green: {
+          background: "#E3FCE9",
+          foreground: "#15602C"
+        },
+        orange: {
+          background: "#FFF1E5",
+          foreground: "#BC4C00"
+        },
+        red: {
+          background: "#FFEBE9",
+          foreground: "#D1242F"
+        }
+      }
+    end
+  end
+
   def up
-    add_column :decidim_proposals_proposal_states, :bg_color, :string, default: "#F3F4F7", null: false
-    add_column :decidim_proposals_proposal_states, :text_color, :string, default: "#3E4C5C", null: false
+    colors = Decidim::Proposals::ProposalState.colors
+
+    add_column :decidim_proposals_proposal_states, :bg_color, :string, default: colors[:gray][:background], null: false
+    add_column :decidim_proposals_proposal_states, :text_color, :string, default: colors[:gray][:foreground], null: false
     remove_column :decidim_proposals_proposal_states, :css_class
 
     Decidim::Proposals::ProposalState.reset_column_information
 
     # rubocop:disable Rails/SkipsModelValidations
-    Decidim::Proposals::ProposalState.where(token: :accepted).update_all(bg_color: "#c4ecd0", text_color: "#16592e")
-    Decidim::Proposals::ProposalState.where(token: :evaluating).update_all(bg_color: "#ffeebd", text_color: "#ad4910")
-    Decidim::Proposals::ProposalState.where(token: :rejected).update_all(bg_color: "#ffdee3", text_color: "#b9081b")
+    Decidim::Proposals::ProposalState.where(token: :accepted).update_all(
+      bg_color: colors[:green][:background], text_color: colors[:green][:foreground]
+    )
+    Decidim::Proposals::ProposalState.where(token: :evaluating).update_all(
+      bg_color: colors[:orange][:background], text_color: colors[:orange][:foreground]
+    )
+    Decidim::Proposals::ProposalState.where(token: :rejected).update_all(
+      bg_color: colors[:red][:background], text_color: colors[:red][:foreground]
+    )
     # rubocop:enable Rails/SkipsModelValidations
   end
 
