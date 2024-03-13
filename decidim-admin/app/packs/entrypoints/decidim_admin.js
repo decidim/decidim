10f970ca9e21c6b02ee862ba79e3344cbfd5ec56
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-03-13 18:03:56 +0100

    Extract 'css preview' JavaScript logic to its own file

--- decidim-admin/app/packs/entrypoints/decidim_admin.js
+++ decidim-admin/app/packs/entrypoints/decidim_admin.js
@@ -17,28 +17,13 @@ import "src/decidim/slug_form"
 import "src/decidim/admin/admin_autocomplete"
 import "src/decidim/admin/triadic_color_picker"
 import "src/decidim/admin/participatory_space_search"
+import "src/decidim/admin/css_preview"
 
 // CSS
 import "entrypoints/decidim_admin.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   initLanguageChangeSelect(document.querySelectorAll("select.language-change"));
-
-  document.querySelectorAll("input[data-css-preview=true]").forEach((element) => {
-    element.addEventListener("change", (event) => {
-      // All the update rules are separated by a semicolon, so we split them
-      // selector:property:value; selector:property:value;
-      //
-      // for example:
-      //   strong[data-label-preview]:color:#0000ff; strong[data-label-preview]:backgroundColor:#eeeeee;
-      const updateRules = event.target.dataset.cssPreviewUpdates.split(";");
-
-      updateRules.forEach((rule) => {
-        const [target, property, value] = rule.split(":");
-        document.querySelector(target).style[property.trim()] = value.trim();
-      });
-    })
-  })
 
   document.querySelectorAll("input[data-sync-radio-buttons=true]").forEach((element) => {
     element.addEventListener("change", (event) => {
