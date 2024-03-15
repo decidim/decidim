7416ab194276dfe2b352ca28e1ba0eda0cdaf411
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2024-03-15 14:18:40 +0100

    Backport 'Do not list hidden reported debates in the admin' to v0.28 (#12607)
    * Do not list hidden reported debates in the admin
    
    * extration of not_hidden method from proposal applied to debates includer method helper in controller
    
    * test
    
    * Fixed of rubocop spacing issue
    
    * Test added to admin manages debates
    
    * Update decidim-debates/app/controllers/decidim/debates/admin/debates_controller.rb
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    * Fix rubocop offense
    
    ---------
    
    Co-authored-by: Tom <101816158+greenwoodt@users.noreply.github.com>

--- decidim-debates/spec/shared/manage_debates_examples.rb
+++ decidim-debates/spec/shared/manage_debates_examples.rb
@@ -6,6 +6,20 @@ RSpec.shared_examples "manage debates" do
   before { visit_component_admin }
 
   describe "listing" do
+    context "with hidden debates" do
+      let!(:my_other_debate) { create(:debate, category:, component: current_component) }
+
+      before do
+        my_other_debate.update!(title: { en: "Debate <strong>title</strong>" })
+        create(:moderation, :hidden, reportable: my_other_debate)
+      end
+
+      it "does not list the hidden debates" do
+        visit current_path
+        expect(page).not_to have_content(translated(my_other_debate.title))
+      end
+    end
+
     context "with enriched content" do
       before do
         debate.update!(title: { en: "Debate <strong>title</strong>" })
