8a2e8fd957d4219273a757ca1c5e20d94c8fe32a
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-03-19 18:53:13 +0200

    Fix initiatives pipeline

--- decidim-initiatives/spec/presenters/decidim/resource_locator_presenter_spec.rb
+++ decidim-initiatives/spec/presenters/decidim/resource_locator_presenter_spec.rb
@@ -5,8 +5,8 @@ require "spec_helper"
 module Decidim
   describe ResourceLocatorPresenter, type: :helper do
     it_behaves_like "generates routes without query strings on slug" do
-      let(:route_fragment) { "conferences/#{participatory_space.slug}" }
-      let(:admin_route_fragment) { "conferences/#{participatory_space.slug}" }
+      let(:route_fragment) { "initiatives/#{participatory_space.slug}" }
+      let(:admin_route_fragment) { "initiatives/#{participatory_space.slug}" }
       let(:factory_name) { :initiative }
     end
   end
