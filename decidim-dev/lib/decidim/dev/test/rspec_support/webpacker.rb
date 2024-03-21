6d21514e79e49c5437a66097b21b9af924a076a2
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-03-21 12:15:00 +0100

    Add debugging message in Shakapacker compilation failure

--- decidim-dev/lib/decidim/dev/test/rspec_support/webpacker.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/webpacker.rb
@@ -7,5 +7,22 @@ RSpec.configure do |config|
     raise "Node modules directory does not exist" unless Rails.root.join("node_modules").exist?
 
     Dir.chdir(Rails.root) { Webpacker.compile }
+  rescue Errno::ENOENT
+    node_modules_contents = `ls #{Rails.root.join("node_modules")}`
+
+    message = <<~ERROR
+      There was an error during the Webpacker compilation
+      #{"=" * 80}
+      Node version: #{`node -v`}
+      #{"=" * 80}
+      NPM version: #{`npm -v`}
+      #{"=" * 80}
+      Node modules packages: #{`npm list`}
+      #{"=" * 80}
+      Node modules contents: #{node_modules_contents}
+      #{"=" * 80}
+    ERROR
+
+    raise message
   end
 end
