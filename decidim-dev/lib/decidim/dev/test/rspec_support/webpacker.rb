99a46a1b8404c455d0469e29b0e4a4d159ee5e56
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-03-21 11:52:32 +0100

    Compile shakapacker in all the spec types

--- decidim-dev/lib/decidim/dev/test/rspec_support/webpacker.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/webpacker.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
-  config.before(:all, type: :system) do
-    Dir.chdir(Rails.root) { Webpacker.compile }
-  end
-  config.before(:all, type: :mailer) do
-    Dir.chdir(Rails.root) { Webpacker.compile }
-  end
-  config.before(:all, type: :cell) do
+  config.before(:all) do
     Dir.chdir(Rails.root) { Webpacker.compile }
   end
 end
