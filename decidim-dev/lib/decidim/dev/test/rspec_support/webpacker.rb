dc2cdea57de21c5cfd1f71a7ce3d9bd72082b8c4
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-03-21 11:56:33 +0100

    Add extra controls to ease up debugging flaky spec

--- decidim-dev/lib/decidim/dev/test/rspec_support/webpacker.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/webpacker.rb
@@ -2,6 +2,10 @@
 
 RSpec.configure do |config|
   config.before(:all) do
+    raise "Rails.root directory does not exist" unless Rails.root.exist?
+    raise "package.json file does not exist" unless Rails.root.join("package.json").exist?
+    raise "Node modules directory does not exist" unless Rails.root.join("node_modules").exist?
+
     Dir.chdir(Rails.root) { Webpacker.compile }
   end
 end
