7877523f5fc5eeb23b3f9a236baa02135540bad7
Tom Greenwood <tom.t.greenwood@gmail.com>
2024-03-26 17:57:09 +0100

    rubocop issues fixed

--- decidim-core/db/migrate/20240326104215_remove_show_in_footer_in_static_pages.rb
+++ decidim-core/db/migrate/20240326104215_remove_show_in_footer_in_static_pages.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveShowInFooterInStaticPages < ActiveRecord::Migration[6.1]
   def change
     remove_column :decidim_static_pages, :show_in_footer, :Boolean
