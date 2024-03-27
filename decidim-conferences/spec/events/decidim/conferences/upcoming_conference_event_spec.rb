6145854ae6c5d17664190043562b24c450f5c9f9
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-03-27 23:04:09 +0200

    Revert some changes

--- decidim-conferences/spec/events/decidim/conferences/upcoming_conference_event_spec.rb
+++ decidim-conferences/spec/events/decidim/conferences/upcoming_conference_event_spec.rb
@@ -5,7 +5,7 @@ require "spec_helper"
 describe Decidim::Conferences::UpcomingConferenceEvent do
   include_context "when a simple event"
 
-  let(:resource) { create(:conference) }
+  let(:resource) { create(:conference, title: generate_localized_title(:conference_title)) }
   let(:participatory_space) { resource }
   let(:event_name) { "decidim.events.conferences.upcoming_conference" }
 
