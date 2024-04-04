056e7aad3984edd0c7e886b994ca6f53ff0081c5
Tom Greenwood <tom.t.greenwood@gmail.com>
2024-04-04 10:41:11 +0200

    changes based on second review

--- decidim-core/db/migrate/20240326104215_remove_show_in_footer_in_static_pages.rb
+++ decidim-core/db/migrate/20240326104215_remove_show_in_footer_in_static_pages.rb
@@ -2,6 +2,6 @@
 
 class RemoveShowInFooterInStaticPages < ActiveRecord::Migration[6.1]
   def change
-    remove_column :decidim_static_pages, :show_in_footer, :Boolean
+    remove_column :decidim_static_pages, :show_in_footer, :boolean
   end
 end
