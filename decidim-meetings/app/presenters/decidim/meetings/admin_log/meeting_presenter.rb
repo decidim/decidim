e875100643f99fe0f8c256f4ceec6668915d7ad5
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-04-17 15:07:21 +0300

    More cleanup

--- decidim-meetings/app/presenters/decidim/meetings/admin_log/meeting_presenter.rb
+++ decidim-meetings/app/presenters/decidim/meetings/admin_log/meeting_presenter.rb
@@ -15,10 +15,6 @@ module Decidim
       class MeetingPresenter < Decidim::Log::BasePresenter
         private
 
-        def resource_presenter
-          @resource_presenter ||= Decidim::Meetings::Log::ResourcePresenter.new(action_log.resource, h, action_log.extra["resource"])
-        end
-
         def diff_fields_mapping
           {
             address: :string,
