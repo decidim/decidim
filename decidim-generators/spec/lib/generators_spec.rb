eea85254814c47a352b6955399b680bbcfe52c99
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2024-04-24 12:07:21 +0200

    Revert changes to generator's branches

--- decidim-generators/spec/lib/generators_spec.rb
+++ decidim-generators/spec/lib/generators_spec.rb
@@ -17,7 +17,7 @@ module Decidim
         let(:test_version) { "0.27.0.dev" }
 
         it "returns the develop branch" do
-          expect(subject.edge_git_branch).to eq("deps/rails-7")
+          expect(subject.edge_git_branch).to eq("develop")
         end
       end
 
