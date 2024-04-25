b1b68267d07543bc7389658cf083834d0a32eb6a
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2024-04-25 11:24:48 +0200

    Accessibility fixes regarding links that open in new tabs (#12739)
    * Accessibility fixes regarding links that open in new tabs
    
    - Add external link text to the external links that were missing
      it
    - Add a new text "Opens in a new tab" to the internal links that
      open a new tab
    - Fix the translations on the external links
    
    * Fix the external link translations when running under jest
    
    * Add documentation for the  configuration
    
    * Do not force the external link warning when it is disabled
    
    * Update the external link documentation regarding disabled warning
    
    * Disable the external link warning for the join meeting links
    
    * Fix ERBLint violation for "mixed styles" where styles were not mixed
    
    Co-authored-by: Antti Hukkanen <antti.hukkanen@mainiotech.fi>

--- decidim-core/app/packs/src/decidim/external_domain_warning.js
+++ decidim-core/app/packs/src/decidim/external_domain_warning.js
@@ -1,11 +1,24 @@
 /* eslint-disable require-jsdoc */
 
+/**
+ * If you want to disable the warning but indicate that the link is an external
+ * link, please define the `data-external-link-warning="false"` attribute for
+ * the link,
+ * e.g. <a href="https://..." target="_blank" data-external-link="text-only" data-external-domain-link="false">...</a>
+ *
+ * @param {HTMLElement} element The element for which to replace the link href for.
+ * @returns {void} Nothing
+ */
 export default function updateExternalDomainLinks(element) {
   if (window.location.pathname === "/link") {
     return;
   }
 
   if (!element.hasAttribute("href") || element.closest(".editor-container")) {
+    return;
+  }
+
+  if (element.dataset.externalDomainLink === "false") {
     return;
   }
 
