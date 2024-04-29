2fdb64de585f579108ac27e5a1205d7b188bb352
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2024-04-29 07:41:10 +0200

    Backport 'Add votes count to comment caches' to v0.27 (#12782)
    * Add votes count to comment caches
    
    * Add votes count to comment caches
    
    * Use size instead of count method
    
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>
    
    * Implement counter_cache for comment's votes
    
    We need to handle the counter_cache manually as we have an scoped
    association (i.e. with the lambda that does the where in the relation)
    
    > has_many :up_votes, -> { where(weight: 1) }
    
    So, for this reason we need to do handle the update with a callback in
    CommentVote and can't use the reset_counters in the migration.
    
    * Explicitly use the counter_cache attributes in comment cell
    
    ---------
    
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>
    
    * Fix syntax for ruby version
    
    ---------
    
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>

--- decidim-comments/app/cells/decidim/comments/comment_cell.rb
+++ decidim-comments/app/cells/decidim/comments/comment_cell.rb
@@ -47,6 +47,8 @@ module Decidim
         hash.push(model.must_render_translation?(current_organization) ? 1 : 0)
         hash.push(model.authored_by?(current_user) ? 1 : 0)
         hash.push(model.reported_by?(current_user) ? 1 : 0)
+        hash.push(model.up_votes_count)
+        hash.push(model.down_votes_count)
         hash.push(model.cache_key_with_version)
         hash.push(model.author.cache_key_with_version)
         @hash = hash.join(Decidim.cache_key_separator)
@@ -146,11 +148,11 @@ module Decidim
       end
 
       def up_votes_count
-        model.up_votes.count
+        model.up_votes_count
       end
 
       def down_votes_count
-        model.down_votes.count
+        model.down_votes_count
       end
 
       def root_depth
