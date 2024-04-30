225579e16355f4cf185559c9968fac5ac67a1459
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-04-30 15:03:09 +0200

    Prepare 0.27.6 release

--- decidim-initiatives/lib/decidim/initiatives/version.rb
+++ decidim-initiatives/lib/decidim/initiatives/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-initiatives version.
   module Initiatives
     def self.version
-      "0.27.5"
+      "0.27.6"
     end
   end
 end
--- decidim-templates/lib/decidim/templates/version.rb
+++ decidim-templates/lib/decidim/templates/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-templates version.
   module Templates
     def self.version
-      "0.27.5"
+      "0.27.6"
     end
   end
 end
