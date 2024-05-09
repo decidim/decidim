a1355d361b1b8d4a3b7f695e4540a42ee42e72a5
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-05-09 07:20:05 +0300

    User finding consistency

--- decidim-participatory_processes/lib/decidim/participatory_processes/seeds.rb
+++ decidim-participatory_processes/lib/decidim/participatory_processes/seeds.rb
@@ -26,8 +26,8 @@ module Decidim
         2.times do |_n|
           process = create_process!(process_group: process_groups.sample, process_type: process_types.sample)
 
-          create_follow!(admin_user, process)
-          create_follow!(Decidim::User.where(admin: false).first, process)
+          create_follow!(Decidim::User.where(organization:, admin: true).first, process)
+          create_follow!(Decidim::User.where(organization:, admin: false).first, process)
 
           create_process_step!(process:)
 
