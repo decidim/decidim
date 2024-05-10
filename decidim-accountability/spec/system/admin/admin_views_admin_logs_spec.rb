9147a802cdb1077d9d672f0e9e13f4270600204a
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-05-10 11:07:36 +0300

    Add admin log entries

---
+++ decidim-accountability/spec/system/admin/admin_views_admin_logs_spec.rb
@@ -0,0 +1,154 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Admin views admin logs" do
+  let(:manifest_name) { "accountability" }
+
+  include_context "when managing a component as an admin"
+
+  before do
+    switch_to_host(organization.host)
+    login_as user, scope: :user
+    visit_component_admin
+  end
+
+  describe "timeline entry" do
+    let!(:result) { create(:result, component: current_component) }
+    let(:attributes) { attributes_for(:timeline_entry, result:) }
+    let!(:timeline_entries) { create(:timeline_entry, result:) }
+
+    before do
+      visit_component_admin
+      within "tr", text: translated(result.title) do
+        click_on "Project evolution"
+      end
+    end
+
+    it "updates a timeline entry" do
+      click_on "Edit", match: :first
+
+      within ".edit_timeline_entry" do
+        fill_in :timeline_entry_entry_date_date, with: Date.current.strftime("%d/%m/%Y")
+        fill_in_i18n(:timeline_entry_title, "#timeline_entry-title-tabs", **attributes[:title].except("machine_translations"))
+        fill_in_i18n_editor(:timeline_entry_description, "#timeline_entry-description-tabs", **attributes[:description].except("machine_translations"))
+
+        find("*[type=submit]").click
+      end
+
+      expect(page).to have_admin_callout("successfully")
+
+      visit decidim_admin.root_path
+      expect { accept_alert }.to raise_error(Capybara::ModalNotFound)
+      expect(page).to have_content("foobar")
+    end
+
+    it "creates a timeline entry" do
+      click_on "New timeline entry", match: :first
+
+      within ".new_timeline_entry" do
+        fill_in :timeline_entry_entry_date_date, with: Date.current.strftime("%d/%m/%Y")
+        fill_in_i18n(:timeline_entry_title, "#timeline_entry-title-tabs", **attributes[:title].except("machine_translations"))
+        fill_in_i18n_editor(:timeline_entry_description, "#timeline_entry-description-tabs", **attributes[:description].except("machine_translations"))
+
+        find("*[type=submit]").click
+      end
+
+      expect(page).to have_admin_callout("successfully")
+
+      visit decidim_admin.root_path
+      expect { accept_alert }.to raise_error(Capybara::ModalNotFound)
+      expect(page).to have_content("foobar")
+    end
+  end
+
+  describe "result" do
+    let(:attributes) { attributes_for(:result, component: current_component) }
+    let!(:result) { create(:result, component: current_component) }
+
+    let!(:proposal_component) { create(:proposal_component, participatory_space:) }
+    let!(:proposals) { create_list(:proposal, 5, component: proposal_component) }
+
+    it "updates a result" do
+      visit_component_admin
+      within "tr", text: translated(result.title) do
+        click_on "Edit"
+      end
+
+      within ".edit_result" do
+        fill_in_i18n(:result_title, "#result-title-tabs", **attributes[:title].except("machine_translations"))
+        tom_select("#proposals_list", option_id: proposals.first(2).map(&:id))
+
+        find("*[type=submit]").click
+      end
+
+      expect(page).to have_admin_callout("successfully")
+
+      visit decidim_admin.root_path
+      expect { accept_alert }.to raise_error(Capybara::ModalNotFound)
+    end
+
+    it "creates a new result", :slow do
+      click_on "New result", match: :first
+
+      within ".new_result" do
+        fill_in_i18n(:result_title, "#result-title-tabs", **attributes[:title].except("machine_translations"))
+        fill_in_i18n_editor(:result_description, "#result-description-tabs", **attributes[:description].except("machine_translations"))
+        tom_select("#proposals_list", option_id: proposals.first(2).map(&:id))
+
+        find("*[type=submit]").click
+      end
+
+      expect(page).to have_admin_callout("successfully")
+
+      visit decidim_admin.root_path
+      expect { accept_alert }.to raise_error(Capybara::ModalNotFound)
+    end
+  end
+
+  describe "statuses" do
+    let(:attributes) { attributes_for(:status) }
+    let!(:status) { create(:status, component: current_component) }
+
+    before do
+      click_on "Statuses"
+    end
+
+    it "updates a status" do
+      within "tr", text: status.key do
+        click_on "Edit"
+      end
+
+      within ".edit_status" do
+        fill_in_i18n(:status_name, "#status-name-tabs", **attributes[:name].except("machine_translations"))
+
+        find("*[type=submit]").click
+      end
+
+      expect(page).to have_admin_callout("successfully")
+
+      visit decidim_admin.root_path
+      expect { accept_alert }.to raise_error(Capybara::ModalNotFound)
+    end
+
+    it "creates a new status" do
+      click_on "New status"
+
+      within ".new_status" do
+        fill_in :status_key, with: "status_key_1"
+
+        fill_in_i18n(:status_name, "#status-name-tabs", **attributes[:name].except("machine_translations"))
+        fill_in_i18n(:status_description, "#status-description-tabs", **attributes[:description].except("machine_translations"))
+
+        fill_in :status_progress, with: 75
+
+        find("*[type=submit]").click
+      end
+
+      expect(page).to have_admin_callout("successfully")
+
+      visit decidim_admin.root_path
+      expect { accept_alert }.to raise_error(Capybara::ModalNotFound)
+    end
+  end
+end
---
+++ decidim-meetings/spec/system/admin/admin_views_admin_logs_spec.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Admin views admin logs" do
+  let(:manifest_name) { "meetings" }
+
+  include_context "when managing a component as an admin"
+
+  before do
+    switch_to_host(organization.host)
+    login_as user, scope: :user
+  end
+
+  describe "meetings" do
+    let(:attributes) { attributes_for(:meeting, component: current_component) }
+    let(:base_date) { Time.new.utc }
+    let(:meeting_start_date) { base_date.strftime("%d/%m/%Y") }
+    let(:meeting_start_time) { base_date.utc.strftime("%H:%M") }
+    let(:meeting_end_date) { ((base_date + 2.days) + 1.month).strftime("%d/%m/%Y") }
+    let(:meeting_end_time) { (base_date + 4.hours).strftime("%H:%M") }
+    let!(:meeting) { create(:meeting, :published, scope:, services: [], component: current_component) }
+    let(:latitude) { 40.1234 }
+    let(:longitude) { 2.1234 }
+    let(:address) { attributes[:address] }
+
+    before do
+      stub_geocoding(address, [latitude, longitude])
+      visit_component_admin
+    end
+
+    it "updates a meeting" do
+      within "tr", text: Decidim::Meetings::MeetingPresenter.new(meeting).title do
+        click_on "Edit"
+      end
+
+      within ".edit_meeting" do
+        fill_in_i18n(:meeting_title, "#meeting-title-tabs", **attributes[:title].except("machine_translations"))
+
+        fill_in_i18n(:meeting_location, "#meeting-location-tabs", **attributes[:location].except("machine_translations"))
+        fill_in_i18n(:meeting_location_hints, "#meeting-location_hints-tabs", **attributes[:location_hints].except("machine_translations"))
+        fill_in_i18n_editor(:meeting_description, "#meeting-description-tabs", **attributes[:description].except("machine_translations"))
+
+        fill_in :meeting_address, with: address
+        find("*[type=submit]").click
+      end
+
+      expect(page).to have_admin_callout("successfully")
+
+      visit decidim_admin.root_path
+      expect { accept_alert }.to raise_error(Capybara::ModalNotFound)
+    end
+
+    it "creates a new meeting" do
+      click_on "New meeting"
+      fill_in_i18n(:meeting_title, "#meeting-title-tabs", **attributes[:title].except("machine_translations"))
+
+      select "In person", from: :meeting_type_of_meeting
+
+      fill_in_i18n(:meeting_location, "#meeting-location-tabs", **attributes[:location].except("machine_translations"))
+      fill_in_i18n(:meeting_location_hints, "#meeting-location_hints-tabs", **attributes[:location_hints].except("machine_translations"))
+      fill_in_i18n_editor(:meeting_description, "#meeting-description-tabs", **attributes[:description].except("machine_translations"))
+
+      fill_in :meeting_address, with: address
+
+      select "Registration disabled", from: :meeting_registration_type
+
+      fill_in_datepicker :meeting_start_time_date, with: meeting_start_date
+      fill_in_timepicker :meeting_start_time_time, with: meeting_start_time
+      fill_in_datepicker :meeting_end_time_date, with: meeting_end_date
+      fill_in_timepicker :meeting_end_time_time, with: meeting_end_time
+
+      within ".new_meeting" do
+        find("*[type=submit]").click
+      end
+
+      expect(page).to have_admin_callout("successfully")
+
+      visit decidim_admin.root_path
+      expect { accept_alert }.to raise_error(Capybara::ModalNotFound)
+    end
+  end
+end
