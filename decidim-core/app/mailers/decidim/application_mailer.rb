a76b38cb466a4cc24b7f4f6700cf16c080e9d19d
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2024-05-15 12:06:07 +0200

    Add organization or application name in emails' From header
    * Fix "From" header in emails sent by the application
    
    - Add organization name in emails' from in core and admin modules
    - Add application name in emails' from in system module
    
    * Change gotham.gov to example.org in specs
    
    * Remove unecessary ApplicationMailer for admin module
    
    * Improve how we generate the mail from
    
    While reviewing, it was found that if the from was defined in the
    System settings, then the From header wasn't set correctly.
    
    To prevent this bug we set-up the from only once in the after_action,
    and make some checks if we should actually redefine this From or not.
    
    * Remove unecessary ApplicationMailer for system module
    
    * Remove unecessary parameter and rename methods
    
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>
    
    * Fix bug when there's defined a name in the mailer_sender config_accessor
    
    * Make the organization an attr_reader
    
    * Fix rubocop offenses
    
    * Fix spec
    
    ---------
    
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>

--- decidim-core/app/mailers/decidim/application_mailer.rb
+++ decidim-core/app/mailers/decidim/application_mailer.rb
@@ -7,23 +7,57 @@ module Decidim
     include LocalisedMailer
     include MultitenantAssetHost
     after_action :set_smtp
+    after_action :set_from
 
     default from: Decidim.config.mailer_sender
     layout "decidim/mailer"
 
     private
 
-    def set_smtp
-      return if @organization.nil? || @organization.smtp_settings.blank?
+    attr_reader :organization
 
-      mail.from = @organization.smtp_settings["from"].presence || mail.from
+    def set_smtp
+      return if organization.nil? || organization.smtp_settings.blank?
+
       mail.reply_to = mail.reply_to || Decidim.config.mailer_reply
       mail.delivery_method.settings.merge!(
-        address: @organization.smtp_settings["address"],
-        port: @organization.smtp_settings["port"],
-        user_name: @organization.smtp_settings["user_name"],
-        password: Decidim::AttributeEncryptor.decrypt(@organization.smtp_settings["encrypted_password"])
+        address: organization.smtp_settings["address"],
+        port: organization.smtp_settings["port"],
+        user_name: organization.smtp_settings["user_name"],
+        password: Decidim::AttributeEncryptor.decrypt(organization.smtp_settings["encrypted_password"])
       ) { |_k, o, v| v.presence || o }.compact_blank!
+    end
+
+    def set_from
+      return if organization.nil?
+      return if already_defined_name_in_mail?(mail.from.first)
+
+      mail.from = sender
+    end
+
+    def sender
+      return Decidim.config.mailer_sender if return_mailer_sender?
+      return default_sender if organization.smtp_settings.blank?
+      return default_sender if organization.smtp_settings["from"].nil?
+      return default_sender if organization.smtp_settings["from"].empty?
+
+      smtp_settings_from = organization.smtp_settings["from"]
+      return smtp_settings_from if already_defined_name_in_mail?(smtp_settings_from)
+
+      email_address_with_name(smtp_settings_from, organization.name)
+    end
+
+    def default_sender
+      email_address_with_name(Decidim.config.mailer_sender, organization.name)
+    end
+
+    def already_defined_name_in_mail?(mail_address)
+      # if there is an space, there is already a name in the address
+      mail_address.match?(/ /)
+    end
+
+    def return_mailer_sender?
+      already_defined_name_in_mail?(Decidim.config.mailer_sender) && organization.smtp_settings.present?
     end
   end
 end
