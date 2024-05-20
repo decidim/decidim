ede1275172bfc96f0faa74976168bf6ddcd08386
stephanie rousset <strabon@club-internet.fr>
2024-05-20 14:46:57 +0200

    fix: deletes the follows of destroyed private user for assembly that is not transparent and its components

--- decidim-admin/app/commands/decidim/admin/destroy_participatory_space_private_user.rb
+++ decidim-admin/app/commands/decidim/admin/destroy_participatory_space_private_user.rb
@@ -13,6 +13,32 @@ module Decidim
           }
         }
       end
+
+      def run_after_hooks
+        # A hook to destroy the follows of user on private non transparent assembly and its children
+        # when private user is destroyed
+        return unless resource.privatable_to_type == "Decidim::Assembly"
+
+        assembly = Decidim::Assembly.find(resource.privatable_to_id)
+        return unless assembly.private_space == true && assembly.is_transparent == false
+
+        user = Decidim::User.find(resource.decidim_user_id)
+        ids = []
+        ids << Decidim::Follow.where(user:)
+                              .where(decidim_followable_type: "Decidim::Assembly")
+                              .where(decidim_followable_id: assembly.id)
+                              .first.id
+        children_ids = Decidim::Follow.where(user:)
+                                      .select { |follow| find_object(follow).respond_to?("decidim_component_id") }
+                                      .select { |follow| assembly.components.ids.include?(find_object(follow).decidim_component_id) }
+                                      .map(&:id)
+        ids << children_ids
+        Decidim::Follow.where(user:).where(id: ids.flatten).destroy_all if ids.present?
+      end
+
+      def find_object(follow)
+        follow.decidim_followable_type.constantize.find(follow.decidim_followable_id)
+      end
     end
   end
 end
