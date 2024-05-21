309f49f9c53ed4efd5104ed824cb77d099ac8dfe
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2024-05-21 09:42:04 +0200

    Fix rubocop offense

--- decidim-conferences/spec/system/conferences_spec.rb
+++ decidim-conferences/spec/system/conferences_spec.rb
@@ -280,7 +280,7 @@ describe "Conferences" do
     end
 
     it "has no sidebar" do
-      expect(page).to have_no_css(".conference__nav-container")
+      expect(page).not_to have_css(".conference__nav-container")
     end
   end
 end
