9e55f2dc87fd35dbea73b3ce85b4ba7b638e3303
Tom <101816158+greenwoodt@users.noreply.github.com>
2024-05-23 10:54:23 +0200

    Update decidim-system/app/commands/decidim/system/create_default_help_pages.rb
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-system/app/commands/decidim/system/create_default_help_pages.rb
+++ decidim-system/app/commands/decidim/system/create_default_help_pages.rb
@@ -28,7 +28,7 @@ module Decidim
 
           Decidim::StaticPage.create!(
             slug: "help",
-            title: multi_translation("decidim.help.main_topic.default_page.title", organization: @organization.name),
+            title: multi_translation("decidim.help.main_topic.default_page.title", organization: organization.name),
             content: multi_translation("decidim.help.main_topic.default_page.content", organization: @organization.name),
             topic:,
             organization: @organization,
