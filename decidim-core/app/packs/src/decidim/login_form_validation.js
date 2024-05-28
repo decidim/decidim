101d5ffe59368a88770b46055865bcddf5d472db
Anna Topalidi <t.opalidisanna@gmail.com>
2024-05-28 11:50:35 +0200

    fix js

--- decidim-core/app/packs/src/decidim/login_form_validation.js
+++ decidim-core/app/packs/src/decidim/login_form_validation.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
   const emailInput = document.getElementById("session_user_email");
   const form = document.getElementById("session_new_user");
-  const emailError = emailInput.closest("label").querySelector(".form-error");
 
-  form.removeAttribute("data-live-validate");
+  if (emailInput && form) {
+    const emailError = emailInput.closest("label").querySelector(".form-error");
 
-  emailInput.addEventListener("input", function () {
-    emailError.classList.remove("is-visible");
-  });
+    form.removeAttribute("data-live-validate");
+
+    emailInput.addEventListener("input", function () {
+      emailError.classList.remove("is-visible");
+    });
+  }
 });
