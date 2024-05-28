330664b6c38d4e43e16c2e7d9596e966c6c5d6dd
Anna Topalidi <t.opalidisanna@gmail.com>
2024-05-28 11:20:36 +0200

    remove extra code

--- decidim-core/app/packs/src/decidim/login_form_validation.js
+++ decidim-core/app/packs/src/decidim/login_form_validation.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("DOMContentLoaded event fired");
-
   const emailInput = document.getElementById("session_user_email");
-  const passwordInput = document.getElementById("session_user_password");
   const form = document.getElementById("session_new_user");
   const emailError = emailInput.closest("label").querySelector(".form-error");
 
@@ -10,17 +7,5 @@ document.addEventListener("DOMContentLoaded", function() {
 
   emailInput.addEventListener("input", function () {
     emailError.classList.remove("is-visible");
-  });
-
-  emailInput.addEventListener("blur", function() {
-    if (emailInput.value.trim() === "") {
-      emailError.classList.add("is-visible");
-    } else {
-      emailError.classList.remove("is-visible");
-    }
-  });
-
-  passwordInput.addEventListener("focus", function() {
-    emailInput.dispatchEvent(new Event("blur"));
   });
 });
