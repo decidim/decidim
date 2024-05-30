44b7ccb5d09b9fc390adbd7672f76f6481425a77
elviabth <ejbenedith@gmail.com>
2024-05-30 13:04:28 +0200

    change spelling

--- decidim-core/app/packs/src/decidim/sticky_header.js
+++ decidim-core/app/packs/src/decidim/sticky_header.js
@@ -1,17 +1,17 @@
-let prevScrollpos = window.scrollY;
+let prevScroll = window.scrollY;
 const mainBar = document.getElementById("main-bar");
 
 window.onscroll = function() {
   // if a subelement is not visible it has no offsetParent
   const header = document.getElementById("main-dropdown-summary-mobile").offsetParent;
   if (header) {
-    let currentScrollPos = window.scrollY;
+    let currentScroll = window.scrollY;
 
-    if (prevScrollpos > currentScrollPos) {
+    if (prevScrollpos > currentScroll) {
       mainBar.style.top = 0;
     } else {
       mainBar.style.top = `-${header.offsetHeight}px`;
     }
-    prevScrollpos = currentScrollPos;
+    prevScroll = currentScroll;
   }
 };
