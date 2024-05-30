aa29a535f8052809726152c2493feb50d58fc039
Maxim Colls <collsmaxim@gmail.com>
2024-05-30 12:32:59 +0200

    Move migration to decidim-participatory_processes engine

--- decidim-participatory_processes/db/migrate/20240529161054_add_link_to_decidim_attachments.rb
+++ decidim-participatory_processes/db/migrate/20240529161054_add_link_to_decidim_attachments.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddLinkToAttachments < ActiveRecord::Migration[7.0]
+class AddLinkToDecidimAttachments < ActiveRecord::Migration[6.1]
   def change
     add_column :decidim_attachments, :link, :string
   end
