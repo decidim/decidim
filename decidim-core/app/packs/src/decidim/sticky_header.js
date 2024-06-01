d7c8da7ae8c6f2cda8157f07b842ad9b217865ba
elviabth <ejbenedith@gmail.com>
2024-06-01 22:07:08 +0200

    add guard clause for sticky header

--- decidim-core/app/packs/src/decidim/sticky_header.js
+++ decidim-core/app/packs/src/decidim/sticky_header.js
@@ -1,17 +1,18 @@
 let prevScroll = window.scrollY;
 const stickyHeader = document.getElementById("sticky-header");
 
-document.addEventListener("scroll", () => {
-  // if a subelement is not visible it has no offsetParent
-  const header = document.getElementById("main-bar").offsetParent;
-  if (header) {
-    let currentScroll = window.scrollY;
-    if (prevScroll > currentScroll || currentScroll < stickyHeader.offsetHeight) {
-      stickyHeader.style.top = 0;
-    } else {
-      stickyHeader.style.top = `-${stickyHeader.offsetHeight}px`;
-      console.log(header.offsetHeight);
+if (stickyHeader) {
+  document.addEventListener("scroll", () => {
+    // if a subelement is not visible it has no offsetParent
+    const header = document.getElementById("main-bar").offsetParent;
+    if (header) {
+      let currentScroll = window.scrollY;
+      if (prevScroll > currentScroll || currentScroll < stickyHeader.offsetHeight) {
+        stickyHeader.style.top = 0;
+      } else {
+        stickyHeader.style.top = `-${stickyHeader.offsetHeight}px`;
+      }
+      prevScroll = currentScroll;
     }
-    prevScroll = currentScroll;
-  }
-});
+  });
+};
