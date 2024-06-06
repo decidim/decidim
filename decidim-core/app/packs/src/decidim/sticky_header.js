4793fb410f5856ff64a05b6f44da0519b9c16761
elviabth <ejbenedith@gmail.com>
2024-06-06 10:18:52 +0200

    add a couple of documentaion lines for sticky_header.js file

--- decidim-core/app/packs/src/decidim/sticky_header.js
+++ decidim-core/app/packs/src/decidim/sticky_header.js
@@ -1,24 +1,20 @@
-// Initialize the previous scroll position
+// This script implements a sticky header that hides when participants scroll down and shows when they scroll up.
+// Sticky headers allow users to quickly access the navigation, search, and utility-navigation elements without scrolling up to the top of the page.
+// They increase the discoverability of the elements in the header.
 let prevScroll = window.scrollY;
-
-// Get the sticky header element by its ID
 const stickyHeader = document.getElementById("js-sticky-header");
 
-// Check if the sticky header element exists
 if (stickyHeader) {
   document.addEventListener("scroll", () => {
     // if a subelement is not visible it has no offsetParent
     const header = document.getElementById("main-bar").offsetParent;
     if (header) {
       let currentScroll = window.scrollY;
-      // Determine the position of the sticky header based on scroll direction
       if (prevScroll > currentScroll || currentScroll < stickyHeader.offsetHeight) {
         stickyHeader.style.top = 0;
       } else {
-        // If scrolling down, hide the header by setting its top position to negative its height
         stickyHeader.style.top = `-${stickyHeader.offsetHeight}px`;
       }
-      // Update the previous scroll position to the current scroll position
       prevScroll = currentScroll;
     }
   });
