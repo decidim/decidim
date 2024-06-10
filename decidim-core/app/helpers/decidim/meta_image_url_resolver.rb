c14e9cf70dfd4b0eca163aabd6f0ef89a3956b78
Anna Topalidi <t.opalidisanna@gmail.com>
2024-06-10 18:59:02 +0200

    change a hierarchy

--- decidim-core/app/helpers/decidim/meta_image_url_resolver.rb
+++ decidim-core/app/helpers/decidim/meta_image_url_resolver.rb
@@ -12,28 +12,38 @@ module Decidim
       @helper = helper
     end
 
+    # Resolves the image URL to be used for meta tags.
+    #
+    # @return [String] - The resolved image URL or the provided image URL.
     def resolve
       determine_image_url.presence || @image_url
     end
 
     private
 
-    # Determines the image URL to be used for meta tags.
+    # Determines the image URL to be used for meta tags by following a hierarchy.
     #
     # @return [String] - A String of the absolute URL of the image.
     def determine_image_url
       return add_base_url_to(@image_url) if @image_url.present?
       return unless @resource
 
-      url = fetch_attachment_image_url || fetch_image_from_description || fetch_participatory_space_image_url
+      url = fetch_attachment_image_url || fetch_image_from_description || fetch_participatory_space_image_url || fetch_default_image_url
       add_base_url_to(url) if url.present?
+    end
+
+    # Fetches the default hero image URL if available.
+    #
+    # @return [String, nil] - The URL of the hero image or nil if not found.
+    def fetch_default_image_url
+      @resource.try(:attached_uploader, :hero_image).try(:path)
     end
 
     # Fetches the URL of the first attachment image.
     #
-    # @return [String] - A String of the absolute URL of the attachment image.
+    # @return [String, nil] - The URL of the attachment image or nil if not found.
     def fetch_attachment_image_url
-      attachment = find_image_attachment(@resource) || find_image_attachment(@resource.try(:participatory_space))
+      attachment = find_image_attachment(@resource)
       return unless attachment
 
       if attachment.file&.attached?
@@ -44,13 +54,18 @@ module Decidim
       end
     end
 
+    # Finds the first image attachment for a given entity.
+    #
+    # @param [Object] entity - The entity to search for attachments.
+    #
+    # @return [Decidim::Attachment, nil] - The first image attachment or nil if not found.
     def find_image_attachment(entity)
       entity.try(:attachments)&.find_by(content_type: %w(image/jpeg image/png))
     end
 
     # Extracts the first image URL from the resource description.
     #
-    # @return [String] - A String of the relative URL of the first image found in the body.
+    # @return [String] - A String of the relative URL of the first image found in the body or description.
     def fetch_image_from_description
       html = @resource.try(:body) || @resource.try(:description) || @resource.try(:short_description)
       return unless html
@@ -65,10 +80,22 @@ module Decidim
 
     # Fetches the URL of the participatory space image.
     #
-    # @return [String] - A String of the absolute URL of the participatory space image.
+    # @return [String, nil] - The URL of the participatory space image or nil if not found.
     def fetch_participatory_space_image_url
-      image_path = @resource.participatory_space&.attached_uploader(:hero_image)&.path
-      resize_image_url(image_path) if image_path.present?
+      participatory_space = @resource.try(:participatory_space)
+      return unless participatory_space
+
+      attachment = find_image_attachment(participatory_space)
+      return unless attachment
+
+      if attachment.file&.attached?
+        Rails.application.routes.url_helpers.rails_representation_url(
+          attachment.file.variant(resize_to_limit: [1200, 630]).processed,
+          only_path: true
+        )
+      else
+        participatory_space.try(:attached_uploader, :hero_image).try(:path)
+      end
     end
 
     # Resizes the image URL to the specified dimensions (1200x630) if it's larger.
