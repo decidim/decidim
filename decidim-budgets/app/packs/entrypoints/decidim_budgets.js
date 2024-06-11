fa91965a0244682a6bcc39d9551c0322a3bf2902
elviabth <ejbenedith@gmail.com>
2024-06-11 19:03:37 +0200

    add budget-summary_content_header.js file

--- decidim-budgets/app/packs/entrypoints/decidim_budgets.js
+++ decidim-budgets/app/packs/entrypoints/decidim_budgets.js
@@ -1,6 +1,7 @@
 import "src/decidim/budgets/projects"
 import "src/decidim/budgets/progressFixed"
 import "src/decidim/budgets/exit_handler"
+import "src/decidim/budgets/budget_summary_content_header"
 
 // Images
 require.context("../images", true)
