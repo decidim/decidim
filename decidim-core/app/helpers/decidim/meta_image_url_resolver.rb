9d688b140a8714bebf0ef02cd0ae97c4cb44633e
Anna Topalidi <t.opalidisanna@gmail.com>
2024-06-12 17:41:20 +0200

    refactoring

--- decidim-core/app/helpers/decidim/meta_image_url_resolver.rb
+++ decidim-core/app/helpers/decidim/meta_image_url_resolver.rb
@@ -4,8 +4,6 @@ module Decidim
   class MetaImageUrlResolver
     include TranslatableAttributes
 
-    # delegate :add_base_url_to, to: :@helper
-
     def initialize(resource, current_organization)
       @resource = resource
       @current_organization = current_organization
@@ -13,24 +11,25 @@ module Decidim
 
     # Resolves the image URL to be used for meta tags.
     #
-    # @return [String] - The resolved image URL or the provided image URL.
+    # @return [String, nil] - The resolved image URL or nil if no image is found.
     def resolve
-      determine_image_url.presence
+      blob = determine_image_blob.presence
+      resize_image_blob(blob) if blob.present?
     end
 
     private
 
-    # Determines the image URL to be used for meta tags by following a hierarchy.
+    # Determines the image blob to be used for meta tags by following a hierarchy.
     #
-    # @return [String] - A String of the absolute URL of the image.
-    def determine_image_url
-      fetch_attachment_image_url || fetch_image_from_description || fetch_participatory_space_image_url || fetch_default_image_url
+    # @return [ActiveStorage::Blob, nil] - The image blob or nil if no image is found.
+    def determine_image_blob
+      blob_from_attachment_image || blob_from_description || blob_from_participatory_space || blob_from_content_blocks
     end
 
-    # Fetches the default homepage image URL.
+    # Fetches the default homepage image blob.
     #
-    # @return [String, nil] - The URL of the homepage image or nil if not found.
-    def fetch_default_image_url
+    # @return [ActiveStorage::Blob, nil] - The image blob or nil if not found.
+    def blob_from_content_blocks
       content_block = Decidim::ContentBlock.find_by(
         organization: @current_organization,
         manifest_name: :hero,
@@ -42,22 +41,17 @@ module Decidim
       attachment = content_block.attachments.first
       return unless attachment&.file&.attached?
 
-      Rails.application.routes.url_helpers.rails_blob_path(attachment.file, only_path: true)
+      attachment.file.blob
     end
 
-    # Fetches the URL of the first attachment image.
+    # Fetches the first attachment image blob.
     #
-    # @return [String, nil] - The URL of the attachment image or nil if not found.
-    def fetch_attachment_image_url
+    # @return [ActiveStorage::Blob, nil] - The image blob or nil if not found.
+    def blob_from_attachment_image
       attachment = find_image_attachment(@resource)
-      return unless attachment
+      return unless attachment&.file&.attached?
 
-      if attachment.file&.attached?
-        Rails.application.routes.url_helpers.rails_representation_url(
-          attachment.file.variant(resize_to_limit: [1200, 630]).processed,
-          only_path: true
-        )
-      end
+      attachment.file.blob
     end
 
     # Finds the first image attachment for a given entity.
@@ -69,10 +63,10 @@ module Decidim
       entity.try(:attachments)&.find_by(content_type: %w(image/jpeg image/png))
     end
 
-    # Extracts the first image URL from the resource description.
+    # Extracts the first image blob from the resource description.
     #
-    # @return [String] - A String of the relative URL of the first image found in the body or description.
-    def fetch_image_from_description
+    # @return [ActiveStorage::Blob, nil] - The image blob or nil if not found.
+    def blob_from_description
       html = @resource.try(:body) || @resource.try(:description) || @resource.try(:short_description)
       return unless html
 
@@ -81,45 +75,32 @@ module Decidim
 
       doc = Nokogiri::HTML(html)
       image_url = doc.css("img").map { |img| img["src"] }.first
-      resize_image_url(image_url) if image_url.present?
+      return unless image_url
+
+      blob_key = image_url.split("/").second_to_last
+      ActiveStorage::Blob.find_signed(blob_key)
     end
 
-    # Fetches the URL of the participatory space image.
+    # Fetches the image blob from the participatory space.
     #
-    # @return [String, nil] - The URL of the participatory space image or nil if not found.
-    def fetch_participatory_space_image_url
+    # @return [ActiveStorage::Blob, nil] - The image blob or nil if not found.
+    def blob_from_participatory_space
       participatory_space = @resource&.participatory_space
       return unless participatory_space
 
-      attachment = participatory_space.hero_image.attachment || participatory_space.banner_image.attachment
-      return unless attachment
-
-      if participatory_space.hero_image.attached? || participatory_space.banner_image.attached?
-        Rails.application.routes.url_helpers.rails_representation_url(
-          attachment.variant(resize_to_limit: [1200, 630]).processed,
-          only_path: true
-        )
-      end
+      participatory_space.hero_image.blob || participatory_space.banner_image.blob
     end
 
-    # Resizes the image URL to the specified dimensions (1200x630) if it's larger.
+    # Resizes the image blob to the specified dimensions (1200x630) if it's larger.
     #
-    # @param [String] image_url - The URL of the image to be resized.
+    # @param [ActiveStorage::Blob] blob - The image blob to be resized.
     #
     # @return [String] - The URL of the resized image.
-    def resize_image_url(image_url)
-      blob_key = image_url.split("/").second_to_last
-      blob = ActiveStorage::Blob.find_signed(blob_key)
+    def resize_image_blob(blob)
+      return unless blob
 
-      if blob
-        resized_variant = blob.variant(resize_to_limit: [1200, 630]).processed
-        Rails.application.routes.url_helpers.rails_representation_url(resized_variant, only_path: true)
-      else
-        image_url
-      end
-    rescue StandardError => e
-      Rails.logger.error("Error processing image: #{e.message}")
-      image_url
+      resized_variant = blob.variant(resize_to_limit: [1200, 630]).processed
+      Rails.application.routes.url_helpers.rails_representation_url(resized_variant, only_path: true)
     end
   end
 end
