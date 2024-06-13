f33e7ccd4600d10e3f1415e08a1274d69a666322
Anna Topalidi <t.opalidisanna@gmail.com>
2024-06-13 15:24:58 +0200

    fiz test

--- decidim-proposals/spec/system/proposal_show_spec.rb
+++ decidim-proposals/spec/system/proposal_show_spec.rb
@@ -136,7 +136,7 @@ describe "Show a Proposal" do
           proposal.update!(body: { en: "This is my proposal" })
         end
 
-        it_behaves_like "meta image url examples", "hero_image.jpeg" do
+        it_behaves_like "meta image url examples", "default_hero_image.jpeg" do
           let(:resource) { proposal }
         end
       end
