c5bef130c85b88f188059d0775de1f8c36664f7a
Maxim Colls <collsmaxim@gmail.com>
2024-06-13 10:13:37 +0200

    Restore unrelated changes done by prettier in proposals.js

--- decidim-proposals/app/packs/src/decidim/proposals/admin/proposals.js
+++ decidim-proposals/app/packs/src/decidim/proposals/admin/proposals.js
@@ -5,22 +5,19 @@
 import TomSelect from "tom-select/dist/cjs/tom-select.popular";
 
 $(() => {
-  const selectedProposalsCount = function () {
-    return $(".table-list .js-check-all-proposal:checked").length;
-  };
+  const selectedProposalsCount = function() {
+    return $(".table-list .js-check-all-proposal:checked").length
+  }
 
-  const selectedProposalsNotPublishedAnswerCount = function () {
-    return $(
-      ".table-list [data-published-state=false] .js-check-all-proposal:checked"
-    ).length;
-  };
+  const selectedProposalsNotPublishedAnswerCount = function() {
+    return $(".table-list [data-published-state=false] .js-check-all-proposal:checked").length
+  }
 
-  const selectedProposalsCountUpdate = function () {
+  const selectedProposalsCountUpdate = function() {
     const selectedProposals = selectedProposalsCount();
-    const selectedProposalsNotPublishedAnswer =
-      selectedProposalsNotPublishedAnswerCount();
+    const selectedProposalsNotPublishedAnswer = selectedProposalsNotPublishedAnswerCount();
     if (selectedProposals === 0) {
-      $("#js-selected-proposals-count").text("");
+      $("#js-selected-proposals-count").text("")
     } else {
       $("#js-selected-proposals-count").text(selectedProposals);
     }
@@ -33,49 +30,46 @@ $(() => {
 
     if (selectedProposalsNotPublishedAnswer > 0) {
       $('button[data-action="publish-answers"]').parent().show();
-      $("#js-form-publish-answers-number").text(
-        selectedProposalsNotPublishedAnswer
-      );
+      $("#js-form-publish-answers-number").text(selectedProposalsNotPublishedAnswer);
     } else {
       $('button[data-action="publish-answers"]').parent().hide();
     }
-  };
+  }
 
-  const showBulkActionsButton = function () {
+  const showBulkActionsButton = function() {
     if (selectedProposalsCount() > 0) {
       $("#js-bulk-actions-button").removeClass("hide");
     }
-  };
+  }
 
-  const hideBulkActionsButton = function (force = false) {
+  const hideBulkActionsButton = function(force = false) {
     if (selectedProposalsCount() === 0 || force === true) {
       $("#js-bulk-actions-button").addClass("hide");
       $("#js-bulk-actions-dropdown").removeClass("is-open");
     }
-  };
+  }
 
-  const resetForms = function () {
-    $("#js-bulk-actions-dropdown button").each(function () {
+  const resetForms = function() {
+    $("#js-bulk-actions-dropdown button").each(function() {
       $(`#js-form-${$(this).data("action")}`)[0].reset();
-    });
-  };
+    })
+  }
 
-  const showOtherActionsButtons = function () {
+  const showOtherActionsButtons = function() {
     $("#js-other-actions-wrapper").removeClass("hide");
-  };
+  }
 
-  const hideOtherActionsButtons = function () {
+  const hideOtherActionsButtons = function() {
     $("#js-other-actions-wrapper").addClass("hide");
-  };
+  }
 
-  const hideBulkActionForms = function () {
+  const hideBulkActionForms = function() {
     $(".js-bulk-action-form").addClass("hide");
-  };
+  }
 
   // Expose functions to make them available in .js.erb templates
   window.selectedProposalsCount = selectedProposalsCount;
-  window.selectedProposalsNotPublishedAnswerCount =
-    selectedProposalsNotPublishedAnswerCount;
+  window.selectedProposalsNotPublishedAnswerCount = selectedProposalsNotPublishedAnswerCount;
   window.selectedProposalsCountUpdate = selectedProposalsCountUpdate;
   window.showBulkActionsButton = showBulkActionsButton;
   window.hideBulkActionsButton = hideBulkActionsButton;
@@ -119,7 +113,7 @@ $(() => {
     });
 
     // select all checkboxes
-    $(".js-check-all").change(function () {
+    $(".js-check-all").change(function() {
       $(".js-check-all-proposal").prop("checked", $(this).prop("checked"));
 
       if ($(this).prop("checked")) {
@@ -135,18 +129,15 @@ $(() => {
 
     // proposal checkbox change
     $(".table-list").on("change", ".js-check-all-proposal", function (e) {
-      let proposalId = $(this).val();
-      let checked = $(this).prop("checked");
+      let proposalId = $(this).val()
+      let checked = $(this).prop("checked")
 
       // uncheck "select all", if one of the listed checkbox item is unchecked
       if ($(this).prop("checked") === false) {
         $(".js-check-all").prop("checked", false);
       }
       // check "select all" if all checkbox proposals are checked
-      if (
-        $(".js-check-all-proposal:checked").length ===
-        $(".js-check-all-proposal").length
-      ) {
+      if ($(".js-check-all-proposal:checked").length === $(".js-check-all-proposal").length) {
         $(".js-check-all").prop("checked", true);
         showBulkActionsButton();
       }
@@ -163,14 +154,12 @@ $(() => {
         hideBulkActionsButton();
       }
 
-      $(".js-bulk-action-form")
-        .find(`.js-proposal-id-${proposalId}`)
-        .prop("checked", checked);
+      $(".js-bulk-action-form").find(`.js-proposal-id-${proposalId}`).prop("checked", checked);
       selectedProposalsCountUpdate();
     });
 
     $(".js-cancel-bulk-action").on("click", function (e) {
-      hideBulkActionForms();
+      hideBulkActionForms()
       showBulkActionsButton();
       showOtherActionsButtons();
     });
