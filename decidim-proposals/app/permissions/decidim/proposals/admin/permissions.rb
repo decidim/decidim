e32dcbf10464666ad3fca56175d8f425d6a7c2ea
Maxim Colls <collsmaxim@gmail.com>
2024-06-14 10:00:11 +0200

    Revert "Allow valuators assign other valuators to proposal"
    This reverts commit da11fc390a34e011e1c80e2b3fa0f7682c107fb9.

--- decidim-proposals/app/permissions/decidim/proposals/admin/permissions.rb
+++ decidim-proposals/app/permissions/decidim/proposals/admin/permissions.rb
@@ -15,11 +15,7 @@ module Decidim
               can_create_proposal_answer?
             end
             can_export_proposals?
-
-            if user_is_context_valuator?
-              can_unassign_valuator_from_proposals?
-              can_assign_valuator_to_proposals?
-            end
+            valuator_can_unassign_valuator_from_proposals?
 
             return permission_action
           end
@@ -53,7 +49,7 @@ module Decidim
           allow! if permission_action.subject == :proposals && permission_action.action == :split
 
           # Every user allowed by the space can assign proposals to a valuator
-          can_assign_valuator_to_proposals?
+          allow! if permission_action.subject == :proposals && permission_action.action == :assign_to_valuator
 
           # Every user allowed by the space can unassign a valuator from proposals
           can_unassign_valuator_from_proposals?
@@ -149,20 +145,12 @@ module Decidim
           toggle_allow(admin_proposal_answering_is_enabled?) if permission_action.subject == :proposal_answer
         end
 
-        def can_assign_valuator_to_proposals?
-          allow! if permission_action.subject == :proposals && permission_action.action == :assign_to_valuator
-        end
-
         def can_unassign_valuator_from_proposals?
           allow! if permission_action.subject == :proposals && permission_action.action == :unassign_from_valuator
         end
 
         def valuator_can_unassign_valuator_from_proposals?
-          can_unassign_valuator_from_proposals?
-        end
-
-        def user_is_context_valuator?
-          user == context.fetch(:valuator, nil)
+          can_unassign_valuator_from_proposals? if user == context.fetch(:valuator, nil)
         end
 
         def can_export_proposals?
