48cc172177d4cda1aa34c2a23458b7fd1d2310ca
Maxim Colls <collsmaxim@gmail.com>
2024-06-19 10:59:16 +0200

    Fix valuator being able to assign/unassign themselves in the proposals list

--- decidim-proposals/app/helpers/decidim/proposals/admin/proposal_bulk_actions_helper.rb
+++ decidim-proposals/app/helpers/decidim/proposals/admin/proposal_bulk_actions_helper.rb
@@ -9,11 +9,15 @@ module Decidim
         end
 
         # find the valuators for the current space.
-        def find_valuators_for_select(participatory_space)
+        def find_valuators_for_select(participatory_space, current_user)
           valuator_roles = participatory_space.user_roles(:valuator)
           valuators = Decidim::User.where(id: valuator_roles.pluck(:decidim_user_id)).to_a
 
-          valuator_roles.map do |role|
+          filtered_valuator_roles = valuator_roles.filter do |role|
+            role.decidim_user_id != current_user.id
+          end
+
+          filtered_valuator_roles.map do |role|
             valuator = valuators.find { |user| user.id == role.decidim_user_id }
 
             [valuator.name, role.id]
