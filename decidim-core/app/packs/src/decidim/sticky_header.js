dfcd6d39c2bce209ff54bdc6a7e204a4689636c8
elviabth <ejbenedith@gmail.com>
2024-06-21 13:21:14 +0200

    resolve requested changes for sticky_header.js

--- decidim-core/app/packs/src/decidim/sticky_header.js
+++ decidim-core/app/packs/src/decidim/sticky_header.js
@@ -1,10 +1,11 @@
-// This script implements a sticky header that hides when participants scroll down and shows when they scroll up.
-// Sticky headers allow users to quickly access the navigation, search, and utility-navigation elements without scrolling up to the top of the page.
-// They increase the discoverability of the elements in the header.
+// This script implements the sticky header and the sticky buttons. The sticky header hides when participants scroll down and shows when they scroll up.
+// Sticky headers allow users to quickly access the navigation, search, and utility-navigation elements without scrolling up to the top of the page. They increase the discoverability of the elements in the header.
+// The sticky buttons in the other hand, those are some of the main Call to Actions (CTAs) that remain accessible on the screen as the user scrolls through the detailed view of the Meetings, Proposals, Surveys, and Budgets components.
+
 let prevScroll = window.scrollY;
 const stickyHeader = document.querySelector("[data-sticky-header]");
 const footer = document.querySelector("footer");
-const ctasButtons = document.querySelectorAll("[data-ctas-buttons]");
+const stickyButtons = document.querySelectorAll("[data-sticky-buttons]");
 
 const isElementInViewport = (element) => {
   const rect = element.getBoundingClientRect();
@@ -12,11 +13,11 @@ const isElementInViewport = (element) => {
 };
 
 const adjustCtasButtons = () => {
-  if (!ctasButtons || !ctasButtons.length) {
+  if (!stickyButtons || !stickyButtons.length) {
     return;
   }
 
-  let visibleButtons = Array.from(ctasButtons).filter(isElementInViewport);
+  let visibleButtons = Array.from(stickyButtons).filter(isElementInViewport);
 
   if (visibleButtons.length > 0) {
     const marginBottom = Math.max(...visibleButtons.map((ctasButton) => ctasButton.offsetHeight));
