63a7b78486377ed18ffbd85054cbae44026354ec
Tom Greenwood <tom.t.greenwood@gmail.com>
2024-06-21 10:47:31 +0200

    update account delegated and spec fixed

--- decidim-core/app/commands/decidim/update_account.rb
+++ decidim-core/app/commands/decidim/update_account.rb
@@ -3,12 +3,13 @@
 module Decidim
   # This command updates the user's account.
   class UpdateAccount < Decidim::Command
+    delegate :current_user, to: :form
+
     # Updates a user's account.
     #
     # user - The user to be updated.
     # form - The form with the data.
-    def initialize(user, form)
-      @user = user
+    def initialize(form)
       @form = form
     end
 
@@ -19,15 +20,15 @@ module Decidim
       update_avatar
       update_password
 
-      if @user.valid?
-        changes = @user.changed
-        @user.save!
+      if current_user.valid?
+        changes = current_user.changed
+        current_user.save!
         notify_followers
         send_update_summary!(changes)
-        broadcast(:ok, @user.unconfirmed_email.present?)
+        broadcast(:ok, current_user.unconfirmed_email.present?)
       else
         [:avatar, :password].each do |key|
-          @form.errors.add key, @user.errors[key] if @user.errors.has_key? key
+          @form.errors.add key, current_user.errors[key] if current_user.errors.has_key? key
         end
         broadcast(:invalid, @form.password)
       end
@@ -35,38 +36,40 @@ module Decidim
 
     private
 
+    attr_reader :form
+
     def update_personal_data
-      @user.locale = @form.locale
-      @user.name = @form.name
-      @user.nickname = @form.nickname
-      @user.email = @form.email
-      @user.personal_url = @form.personal_url
-      @user.about = @form.about
+      current_user.locale = @form.locale
+      current_user.name = @form.name
+      current_user.nickname = @form.nickname
+      current_user.email = @form.email
+      current_user.personal_url = @form.personal_url
+      current_user.about = @form.about
     end
 
     def update_avatar
       if @form.avatar.present?
-        @user.avatar.attach(@form.avatar)
+        current_user.avatar.attach(@form.avatar)
       elsif @form.remove_avatar
-        @user.avatar = nil
+        current_user.avatar = nil
       end
     end
 
     def update_password
       return if @form.password.blank?
 
-      @user.password = @form.password
-      @user.password_updated_at = Time.current
+      current_user.password = @form.password
+      current_user.password_updated_at = Time.current
     end
 
     def notify_followers
-      return unless @user.previous_changes.keys.intersect?(%w(about personal_url))
+      return unless current_user.previous_changes.keys.intersect?(%w(about personal_url))
 
       Decidim::EventsManager.publish(
         event: "decidim.events.users.profile_updated",
         event_class: Decidim::ProfileUpdatedEvent,
-        resource: @user,
-        followers: @user.followers
+        resource: current_user,
+        followers: current_user.followers
       )
     end
 
@@ -78,7 +81,7 @@ module Decidim
 
         I18n.t("activemodel.attributes.user.#{attr}")
       end
-      UserUpdateMailer.notify(@user, updates).deliver_later
+      UserUpdateMailer.notify(current_user, updates).deliver_later
     end
 
     def attr_set
