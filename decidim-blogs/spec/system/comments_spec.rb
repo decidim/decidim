09cc3d528dd7a2221a8787c0a22530e4b42ea40d
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2024-06-21 11:24:01 +0200

    Fix users tooltip display after clicking Endorse/Like/Dislike  (#12845)
    Changes to be committed:
    	modified:   decidim-core/app/views/decidim/endorsements/update_buttons_and_counters.js.erb

--- decidim-blogs/spec/system/comments_spec.rb
+++ decidim-blogs/spec/system/comments_spec.rb
@@ -3,15 +3,15 @@
 require "spec_helper"
 
 describe "Comments", perform_enqueued: true do
-  let!(:component) { create(:debates_component, organization:) }
-  let!(:commentable) { create(:debate, :open_ama, component:) }
+  let!(:component) { create(:post_component, organization:) }
+  let!(:commentable) { create(:post, component:) }
 
   let(:resource_path) { resource_locator(commentable).path }
 
   include_examples "comments"
 
   context "with comments blocked" do
-    let!(:component) { create(:debates_component, participatory_space:, organization:) }
+    let!(:component) { create(:post_component, participatory_space:, organization:) }
     let(:participatory_space) { create(:participatory_process, :with_steps, organization:) }
 
     include_examples "comments blocked"
--- decidim-comments/spec/system/comments_spec.rb
+++ decidim-comments/spec/system/comments_spec.rb
@@ -4,8 +4,7 @@ require "spec_helper"
 
 describe "Comments" do
   let!(:component) { create(:component, manifest_name: :dummy, organization:) }
-  let!(:author) { create(:user, :confirmed, organization:) }
-  let!(:commentable) { create(:dummy_resource, component:, author:) }
+  let!(:commentable) { create(:dummy_resource, component:) }
 
   let(:resource_path) { resource_locator(commentable).path }
 
