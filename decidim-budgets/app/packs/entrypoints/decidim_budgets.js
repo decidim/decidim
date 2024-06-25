a5ccdfdfed31fc694bdc76d34b45e151ece1ec99
elviabth <ejbenedith@gmail.com>
2024-06-25 09:16:33 +0200

    apply review

--- decidim-budgets/app/packs/entrypoints/decidim_budgets.js
+++ decidim-budgets/app/packs/entrypoints/decidim_budgets.js
@@ -1,7 +1,6 @@
 import "src/decidim/budgets/projects"
 import "src/decidim/budgets/progressFixed"
 import "src/decidim/budgets/exit_handler"
-import "src/decidim/budgets/budget_summary_content_header"
 
 // Images
 require.context("../images", true)
