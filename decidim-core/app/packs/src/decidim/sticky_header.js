41607b50cc659e72cbc18cc1e89a95dc88a0c0fd
Elvia Benedith <116598037+ElviaBth@users.noreply.github.com>
2024-06-25 11:52:12 +0200

    Implement sticky buttons for main CTAs in Mobile UI (#13002)
    * remove stiky menu mobile
    
    * remove sticky menu mobile view
    
    * remove main-bar__links-mobile from spec
    
    * fix spec lint error
    
    * add login next to mobile menu
    
    * apply new design for header_defautl and header_login_ photo
    
    * install stylelint
    
    * create mobile_header_spec file
    
    * add mobile search block
    
    * add trailing newline
    
    * remove dupliacate id
    
    * add missing tag
    
    * add search to the menu
    
    * hide last svg when dropdown is close
    
    * add language-chooser to mobile-menu
    
    * hide login when dropdown open
    
    * add spec to test header features
    
    * add trailing newlines
    
    * configure sticky header
    
    * change spelling
    
    * change spelling
    
    * remove duplicate id
    
    * resolve review comments
    
    * display flash messages
    
    * remove unused code
    
    * fix duplicated ids
    
    * change ids name
    
    * check spelling
    
    * check spelling
    
    * add dinamics ids
    
    * separate desktop and mobile dropdowns
    
    * fix specs
    
    * add guard clause for sticky header
    
    * adjust header width
    
    * switch of logo to favicon on mobile
    
    * display favicon when mobile
    
    * add user_initials helper spec check
    
    * include application helper in devise_controller
    
    * refactor text_initials method
    
    * custom cta-button for meeting
    
    * change smile-line for initials when no image
    
    * custom meeting register button for mobile
    
    * run linter
    
    * add sticky cta proposal button
    
    * add proposals with threshold sticky-cta
    
    * add survey-cta sticky button
    
    * align survey sections buttons
    
    * add more-info dropdown for mobile
    
    * custom budget fixed-cta-button
    
    * custom participatory budget view for mobile
    
    * fix orders_spec
    
    * add dinamic footer margin
    
    * remove footer margin from desktop
    
    * refactor sticky_header script
    
    * add sticky button in progressbar for desktop
    
    * removed rubocop changed files
    
    * removed rubocop changed files
    
    * position cta-button for desktop
    
    * checkout tailwind.config.js.erb file to develop version
    
    * checkout tailwind.config.js.erb file to develop version
    
    * check for multiple ctas buttons
    
    * refactor adjustCtasButtons function
    
    * display progressbox-buttons
    
    * add media queries with tailwind classes
    
    * remove single quotes
    
    * remove extra-margin in budget view
    
    * add budget-summary_content_header.js file
    
    * fix broke design on desktop
    
    * remove trailling whitespaces
    
    * resolve requested changes for sticky_header.js
    
    * fix regressions in desktop meetings
    
    * resolve requested changes
    
    * apply review
    
    ---------
    
    Co-authored-by: Ivan Vergés <ivan@pokecode.net>

--- decidim-core/app/packs/src/decidim/sticky_header.js
+++ decidim-core/app/packs/src/decidim/sticky_header.js
@@ -1,14 +1,37 @@
-// This script implements a sticky header that hides when participants scroll down and shows when they scroll up.
-// Sticky headers allow users to quickly access the navigation, search, and utility-navigation elements without scrolling up to the top of the page.
-// They increase the discoverability of the elements in the header.
+// This script implements the sticky header and the sticky buttons. The sticky header hides when participants scroll down and shows when they scroll up.
+// Sticky headers allow users to quickly access the navigation, search, and utility-navigation elements without scrolling up to the top of the page. They increase the discoverability of the elements in the header.
+// The sticky buttons in the other hand, those are some of the main Call to Actions (CTAs) that remain accessible on the screen as the user scrolls through the detailed view of the Meetings, Proposals, Surveys, and Budgets components.
+
 let prevScroll = window.scrollY;
 const stickyHeader = document.querySelector("[data-sticky-header]");
+const footer = document.querySelector("footer");
+const stickyButtons = document.querySelectorAll("[data-sticky-buttons]");
+
+const isElementInViewport = (element) => {
+  const rect = element.getBoundingClientRect();
+  return rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight);
+};
+
+const adjustCtasButtons = () => {
+  if (!stickyButtons || !stickyButtons.length) {
+    return;
+  }
+
+  let visibleButtons = Array.from(stickyButtons).filter(isElementInViewport);
+
+  if (visibleButtons.length > 0) {
+    const marginBottom = Math.max(...visibleButtons.map((ctasButton) => ctasButton.offsetHeight));
+    footer.style.marginBottom = `${marginBottom}px`;
+  } else {
+    footer.style.marginBottom = 0;
+  }
+};
 
 if (stickyHeader) {
   document.addEventListener("scroll", () => {
     // if a subelement is not visible it has no offsetParent
     const header = document.getElementById("main-bar").offsetParent;
-    if (header) {
+    if (header && window.getComputedStyle(stickyHeader).position === "fixed") {
       let currentScroll = window.scrollY;
       let goingDown = prevScroll > currentScroll;
       let change = Math.abs(prevScroll - currentScroll);
@@ -20,6 +43,12 @@ if (stickyHeader) {
         }
         prevScroll = currentScroll;
       }
+
+      adjustCtasButtons();
     }
+  });
+
+  document.addEventListener("on:toggle", () => {
+    adjustCtasButtons();
   });
 };
