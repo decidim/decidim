2f3f16f2ca7d0ac4e8259158a4afbc0e9fe29166
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-06-26 13:24:14 +0200

    Extract process collaborator examples specs to generic space

--- decidim-participatory_processes/spec/system/admin/invite_process_collaborator_spec.rb
+++ decidim-participatory_processes/spec/system/admin/invite_process_collaborator_spec.rb
@@ -2,69 +2,17 @@
 
 require "spec_helper"
 
+require "decidim/admin/test/invite_participatory_space_collaborators_shared_examples"
+
 describe "Invite process collaborator" do
-  include_context "when inviting process users"
+  let(:participatory_space) { create(:participatory_process) }
+  let(:participatory_space_user_roles_path) { decidim_admin_participatory_processes.participatory_process_user_roles_path(participatory_space) }
+  let(:new_button_label) { "New process admin" }
+
+  include_context "when inviting participatory space users"
+
+  let(:space_sidebar_label) { "Processes" }
   let(:role) { "Collaborator" }
 
-  before do
-    switch_to_host organization.host
-  end
-
-  context "when the user does not exist" do
-    before do
-      perform_enqueued_jobs { invite_user }
-    end
-
-    it "asks for a password and nickname and redirects to the admin dashboard" do
-      visit last_email_link
-
-      within "form.new_user" do
-        fill_in :invitation_user_nickname, with: "caballo_loco"
-        fill_in :invitation_user_password, with: "decidim123456789"
-        check :invitation_user_tos_agreement
-        find("*[type=submit]").click
-      end
-
-      expect(page).to have_current_path "/admin/admin_terms/show"
-
-      visit decidim_admin.admin_terms_show_path
-      find_button("I agree with the terms").click
-
-      click_on "Processes"
-
-      within "div.table-scroll" do
-        expect(page).to have_i18n_content(participatory_process.title)
-      end
-    end
-  end
-
-  context "when the user already exists" do
-    let(:email) { "collaborator@example.org" }
-
-    let!(:collaborator) do
-      create(:user, :confirmed, :admin_terms_accepted, email:, organization:)
-    end
-
-    before do
-      perform_enqueued_jobs { invite_user }
-    end
-
-    it "redirects the collaborator to the admin dashboard" do
-      login_as collaborator, scope: :user
-
-      visit decidim_admin.root_path
-
-      click_on "Processes"
-
-      within "div.table-scroll" do
-        expect(page).to have_i18n_content(participatory_process.title)
-      end
-    end
-
-    it "selects the user role in the form" do
-      edit_user(collaborator.name)
-
-      expect(page).to have_select("Role", selected: "Collaborator")
-    end
-  end
+  it_behaves_like "inviting participatory space collaborators"
 end
