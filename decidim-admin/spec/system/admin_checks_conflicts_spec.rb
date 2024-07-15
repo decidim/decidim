6e0fb02c44c1d2e94d5f72fe8f04c2e486089ea6
Alejandro Rueda <alejandro.rueda@nazaries.com>
2024-07-15 13:42:22 +0200

    Added test

---
+++ decidim-admin/spec/system/admin_checks_conflicts_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Admin checks conflicts" do
+  let(:organization) { create(:organization) }
+  let(:resource_controller) { Decidim::Admin::ConflictsController }
+
+  let!(:user) { create(:user, :admin, :confirmed, organization:) }
+  let!(:first_conflictive_user) { create(:user, :admin, :confirmed, organization:) }
+  let!(:second_conflictive_user) { create(:user, :admin, :confirmed, organization:) }
+
+  let!(:first_user_conflicts) { create_list(:conflict, 15, current_user: first_conflictive_user) }
+  let!(:second_user_conflicts) { create_list(:conflict, 15, current_user: second_conflictive_user) }
+
+  before do
+    switch_to_host(organization.host)
+    login_as user, scope: :user
+    visit decidim_admin.root_path
+    click_on "Participants"
+    click_on "Verification conflicts"
+  end
+
+  include_context "with filterable context"
+
+  context "when listing conflicts" do
+    before { visit current_path }
+
+    it_behaves_like "paginating a collection" do
+      let!(:collection) { create_list(:conflict, 50, current_user: first_conflictive_user) }
+    end
+  end
+
+  context "when searching by current user name" do
+    let(:conflict_user) { first_user_conflicts.first.current_user }
+
+    before { visit current_path }
+
+    it "can be searched by name" do
+      search_by_text(conflict_user.name)
+
+      expect(page).to have_content(conflict_user.name)
+    end
+
+    it "can be searched by nickname" do
+      search_by_text(conflict_user.nickname)
+
+      expect(page).to have_content(conflict_user.name)
+    end
+
+    it "can be searched by email" do
+      search_by_text(conflict_user.email)
+
+      expect(page).to have_content(conflict_user.name)
+    end
+  end
+end
