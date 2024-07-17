164cd3bd4536baecb2dad57598b4e57b51e6ee44
elviabth <ejbenedith@gmail.com>
2024-07-17 12:36:55 +0200

    add more checks to share_tokens_form spec

--- decidim-admin/spec/forms/share_token_form_spec.rb
+++ decidim-admin/spec/forms/share_token_form_spec.rb
@@ -13,7 +13,8 @@ module Decidim::Admin
         token:,
         automatic_token:,
         expires_at:,
-        no_expiration:
+        no_expiration:,
+        registered_only:
       ).with_context(
         current_user:,
         current_organization: organization,
@@ -25,6 +26,11 @@ module Decidim::Admin
     let(:automatic_token) { false }
     let(:expires_at) { Time.zone.today + 3.days }
     let(:no_expiration) { false }
+    let(:registered_only) { true }
+
+    it "returns registered only true" do
+      expect(form.registered_only).to be(true)
+    end
 
     context "when automatic_token validation is false" do
       let(:automatic_token) { false }
@@ -33,6 +39,14 @@ module Decidim::Admin
         form.token = nil
         expect(form).to be_invalid
         expect(form.errors[:token]).to include("cannot be blank")
+      end
+    end
+
+    context "when expires_at is nil" do
+      let!(:expires_at) { nil }
+
+      it "does not expires" do
+        expect(form).to be_valid
       end
     end
 
