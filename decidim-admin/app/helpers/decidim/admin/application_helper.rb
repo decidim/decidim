6c94ffeea53e1d3bed3519306aef6743645a6aa5
JoonasAapro <110532525+JoonasAapro@users.noreply.github.com>
2024-07-25 13:08:10 +0300

    Overwrite admin "Cell" -helper method

--- decidim-admin/app/helpers/decidim/admin/application_helper.rb
+++ decidim-admin/app/helpers/decidim/admin/application_helper.rb
@@ -16,6 +16,19 @@ module Decidim
       include Decidim::Admin::ResourceScopeHelper
       include Decidim::Admin::SearchFormHelper
 
+      # Public: Overwrites the `cell` helper method to automatically set some
+      # common context.
+      #
+      # name - the name of the cell to render
+      # model - the cell model
+      # options - a Hash with options
+      #
+      # Renders the cell contents.
+      def cell(name, model, options = {}, &)
+        options = { context: { view_context: self, current_user: } }.deep_merge(options)
+        super
+      end
+
       def participatory_space_active_link?(component)
         endpoints = component.manifest.admin_engine.try(:participatory_space_endpoints)
         endpoints && is_active_link?(decidim_admin_participatory_processes.components_path(current_participatory_space), %r{/\d+/manage/(#{endpoints.join("|")})\b})
