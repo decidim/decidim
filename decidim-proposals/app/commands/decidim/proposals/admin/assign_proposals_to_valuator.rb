ca4e26d99c2395118fd3d6b8afe6dea0fe71584c
Màxim Colls <collsmaxim@gmail.com>
2024-07-26 10:38:03 +0200

    Add notification to valuators when a proposal is assigned (#13022)
    * Add notification to valuators when a proposal is assigned to them. Also adds a new notifications setting to disable this notification
    
    * Update decidim-core/spec/commands/decidim/update_notifications_settings_spec.rb
    
    Co-authored-by: Eduardo Martínez <eduardomech@gmail.com>
    
    ---------
    
    Co-authored-by: Eduardo Martínez <eduardomech@gmail.com>

--- decidim-proposals/app/commands/decidim/proposals/admin/assign_proposals_to_valuator.rb
+++ decidim-proposals/app/commands/decidim/proposals/admin/assign_proposals_to_valuator.rb
@@ -37,6 +37,7 @@ module Decidim
             form.proposals.flat_map do |proposal|
               form.valuator_roles.each do |valuator_role|
                 find_assignment(proposal, valuator_role) || assign_proposal(proposal, valuator_role)
+                notify_valuator(proposal, valuator_role)
               end
             end
           end
@@ -56,6 +57,19 @@ module Decidim
             proposal:,
             valuator_role:
           )
+        end
+
+        def notify_valuator(proposal, valuator_role)
+          return unless valuator_role.user.email_on_assigned_proposals?
+
+          data = {
+            event: "decidim.events.proposals.admin.proposal_assigned_to_valuator",
+            event_class: Decidim::Proposals::Admin::ProposalAssignedToValuatorEvent,
+            resource: proposal,
+            affected_users: [valuator_role.user]
+          }
+
+          Decidim::EventsManager.publish(**data)
         end
       end
     end
