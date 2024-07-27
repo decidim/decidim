44ec9b2137d44b54c93ab8868936e0f66c1492b0
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-07-27 13:04:14 +0300

    Patch rb file

--- decidim-core/config/initializers/secrets_to_credentials.rb
+++ decidim-core/config/initializers/secrets_to_credentials.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-source = ERB.new(IO.read("config/secrets.yml")).result
+source = ERB.new(IO.read(Rails.root.join("config/secrets.yml"))).result
 secrets = YAML.respond_to?(:unsafe_load) ? YAML.unsafe_load(source) : YAML.load(source)
 Rails.application.credentials.deep_merge!(secrets[Rails.env].deep_symbolize_keys)
