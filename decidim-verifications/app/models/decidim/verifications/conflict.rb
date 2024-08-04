d0c2fdc58457f23d364f81a6eb9c2988f077d93a
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-08-04 17:21:21 +0300

    Fix Conflicts

--- decidim-verifications/app/models/decidim/verifications/conflict.rb
+++ decidim-verifications/app/models/decidim/verifications/conflict.rb
@@ -4,5 +4,14 @@ module Decidim::Verifications
   class Conflict < ApplicationRecord
     belongs_to :current_user, class_name: "User"
     belongs_to :managed_user, class_name: "User"
+
+    def self.ransackable_attributes(_auth_object = nil)
+      %w[current_user_id id managed_user_id solved times]
+    end
+
+    def self.ransackable_associations(_auth_object = nil)
+      %w[current_user managed_user]
+    end
+
   end
 end
