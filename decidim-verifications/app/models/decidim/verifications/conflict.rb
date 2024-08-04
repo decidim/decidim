032cd02d214cbae1efac1271ae9bada83b75a6f5
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-08-04 19:26:24 +0300

    Run lints

--- decidim-verifications/app/models/decidim/verifications/conflict.rb
+++ decidim-verifications/app/models/decidim/verifications/conflict.rb
@@ -6,12 +6,11 @@ module Decidim::Verifications
     belongs_to :managed_user, class_name: "User"
 
     def self.ransackable_attributes(_auth_object = nil)
-      %w[current_user_id id managed_user_id solved times]
+      %w(current_user_id id managed_user_id solved times)
     end
 
     def self.ransackable_associations(_auth_object = nil)
-      %w[current_user managed_user]
+      %w(current_user managed_user)
     end
-
   end
 end
