a7a5226d765abdc4e1cd728b5f61b4b66adc82fd
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-08-16 17:49:38 +0300

    Fix accessibility issues

--- decidim-core/app/packs/src/decidim/a11y.js
+++ decidim-core/app/packs/src/decidim/a11y.js
@@ -59,7 +59,6 @@ const createDropdown = (component) => {
   const dropdownOptions = {};
   dropdownOptions.dropdown = component.dataset.target;
   dropdownOptions.hover = component.dataset.hover === "true";
-  dropdownOptions.isOpen = component.dataset.open === "true";
   dropdownOptions.autoClose = component.dataset.autoClose === "true";
 
   // This snippet allows to disable the dropdown based on the current viewport
@@ -77,6 +76,17 @@ const createDropdown = (component) => {
   if (isDisabled) {
     return
   }
+
+  dropdownOptions.isOpen = component.dataset.open === "true";
+
+  const isOpen = Object.keys(screens).some((key) => {
+    if (!isScreenSize(key)) {
+      return false;
+    }
+    return Boolean(component.dataset[`open-${key}`.replace(/-([a-z])/g, (str) => str[1].toUpperCase())]);
+  });
+
+  dropdownOptions.isOpen = dropdownOptions.isOpen || isOpen;
 
   if (!component.id) {
     // when component has no id, we enforce to have it one
