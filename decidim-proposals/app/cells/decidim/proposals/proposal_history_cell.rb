c604b40ca18f2728b502640bfcbcac60f6b606ab
elviabth <ejbenedith@gmail.com>
2024-08-22 12:07:56 +0200

    refactor proposal_history cell

--- decidim-proposals/app/cells/decidim/proposals/proposal_history_cell.rb
+++ decidim-proposals/app/cells/decidim/proposals/proposal_history_cell.rb
@@ -12,7 +12,7 @@ module Decidim
     #
     #   cell(
     #     "decidim/proposal_history",
-    #     meeting
+    #     proposal
     #   )
     class ProposalHistoryCell < Decidim::ViewModel
       def show
@@ -21,18 +21,19 @@ module Decidim
 
       private
 
-      def proposal_history_items
-        @proposal_history_items ||= [].tap do |items|
-          add_proposal_creation_item(items)
-          add_linked_resources_item(items, :projects, "included_proposals", "decidim/budgets/project/text", "Decidim::Budgets::Project")
-          add_linked_resources_item(items, :results, "included_proposals", "decidim/accountability/result/text", "Decidim::Accountability::Result")
-          add_linked_resources_item(items, :meetings, "proposals_from_meeting", "decidim/meetings/meeting/text", "Decidim::Meetings::Meeting")
-          add_linked_resources_item(items, :proposals, "copied_from_component", "decidim/proposals/proposal/text", "Decidim::Proposals::Proposal")
-          add_proposal_state_item(items) if proposal_state(@model).present?
-          add_proposal_withdraw_item(items) if proposal_withdrawn_state(@model).present?
-        end
+      def history_items
+        return @history_items if @history_items.present?
 
-        @proposal_history_items.sort_by! { |item| item[:date] }
+        @history_items = []
+        add_proposal_creation_item(@history_items)
+        add_linked_resources_item(@history_items, :projects, "included_proposals", "decidim/budgets/project/text", "Decidim::Budgets::Project")
+        add_linked_resources_item(@history_items, :results, "included_proposals", "decidim/accountability/result/text", "Decidim::Accountability::Result")
+        add_linked_resources_item(@history_items, :meetings, "proposals_from_meeting", "decidim/meetings/meeting/text", "Decidim::Meetings::Meeting")
+        add_linked_resources_item(@history_items, :proposals, "copied_from_component", "decidim/proposals/proposal/text", "Decidim::Proposals::Proposal")
+        add_proposal_state_item(@history_items) if proposal_state(@model).present?
+        add_proposal_withdraw_item(@history_items) if proposal_withdrawn_state(@model).present?
+
+        @history_items.sort_by! { |item| item[:date] }
       end
 
       def add_linked_resources_item(items, resource_type, link_name, text_key, icon_key)
@@ -59,24 +60,22 @@ module Decidim
       end
 
       def add_proposal_state_item(items)
-        state = proposal_state(@model)
         items << {
           id: "proposal_state",
           date: @model.updated_at,
           text: t("decidim.proposals.state.text"),
           icon: resource_type_icon_key("Decidim::Proposals::Proposal"),
-          state:
+          state: proposal_state(@model)
         }
       end
 
       def add_proposal_withdraw_item(items)
-        state = proposal_withdrawn_state(@model)
         items << {
           id: "proposal_withdraw",
           date: @model.updated_at,
           text: t("decidim.proposals.withdraw.text"),
           icon: resource_type_icon_key("Decidim::Proposals::Proposal"),
-          state:
+          state: proposal_withdrawn_state(@model)
         }
       end
 
