74f9a2943be43cdcfe83f17add579c3ed88744ea
Anna Topalidi <t.opalidisanna@gmail.com>
2024-08-25 22:27:07 +0200

    fix migration. fix erb lint

--- decidim-core/lib/decidim/soft_deletable.rb
+++ decidim-core/lib/decidim/soft_deletable.rb
@@ -3,45 +3,29 @@
 require "active_support/concern"
 
 module Decidim
-  # This concern handles the implementation of soft deletion for models.
-  # It provides methods to mark records as deleted without actually removing them from the database,
-  # and includes scopes to filter out or retrieve trashed records.
+  # This concern contains the logic related to soft deletion (trashing).
   module SoftDeletable
     extend ActiveSupport::Concern
 
     included do
-      # NOTE: This is a temporary solution due to problems with migrations that use models
-      # with a `deleted_at` column. The current approach checks the existence of the column
-      # before defining the soft delete functionality. This should be refactored once
-      # the migration issues are resolved to avoid using `column_names.include?`.
-      @soft_deletable_available ||= column_names.include?("deleted_at")
+      # Scope to return only records that have not been trashed (soft deleted).
+      scope :not_deleted, -> { where(deleted_at: nil) }
 
-      if @soft_deletable_available
-        # Scope to return only non-deleted records.
-        scope :not_deleted, -> { where(deleted_at: nil) }
-
-        # Scope to return only trashed (soft deleted) records.
-        scope :trashed, -> { where.not(deleted_at: nil) }
-      else
-        def self.trashed
-          none
-        end
-      end
+      # Scope to return only trashed (soft deleted) records.
+      scope :trashed, -> { where.not(deleted_at: nil) }
     end
 
     # Public: Checks whether the record has been trashed (soft deleted) or not.
     #
     # Returns true if trashed, false otherwise.
     def trashed?
-      self.class.instance_variable_get(:@soft_deletable_available) && deleted_at.present?
+      deleted_at.present?
     end
 
     # Public: Soft delete this record by setting `deleted_at`.
     #
     # Returns true if the record was properly saved, false otherwise.
     def trash!
-      return false unless self.class.instance_variable_get(:@soft_deletable_available)
-
       update!(deleted_at: Time.current)
     end
 
@@ -49,8 +33,6 @@ module Decidim
     #
     # Returns true if the record was properly saved, false otherwise.
     def restore!
-      return false unless self.class.instance_variable_get(:@soft_deletable_available)
-
       update!(deleted_at: nil)
     end
   end
