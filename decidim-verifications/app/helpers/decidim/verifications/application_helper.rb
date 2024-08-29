d33e7bca76019aa952dea3f5d0400d966d4ee182
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2024-08-29 13:58:53 +0200

    Remame first_login with onboarding_pending

--- decidim-verifications/app/helpers/decidim/verifications/application_helper.rb
+++ decidim-verifications/app/helpers/decidim/verifications/application_helper.rb
@@ -90,17 +90,17 @@ module Decidim
       def onboarding_sections(onboarding_manager, redirect_url: nil, granted_authorizations: nil, pending_authorizations: nil, unauthorized_methods: nil)
         [
           [
-            t("granted_verifications", scope: "decidim.verifications.authorizations.first_login"),
+            t("granted_verifications", scope: "decidim.verifications.authorizations.onboarding_pending"),
             onboarding_manager.filter_authorizations(granted_authorizations),
             :granted_authorization_display_data
           ],
           [
-            t("pending_admin_approval_verifications", scope: "decidim.verifications.authorizations.first_login"),
+            t("pending_admin_approval_verifications", scope: "decidim.verifications.authorizations.onboarding_pending"),
             onboarding_manager.filter_authorizations(pending_authorizations),
             :pending_authorization_display_data
           ],
           [
-            t("pending_verifications", scope: "decidim.verifications.authorizations.first_login"),
+            t("pending_verifications", scope: "decidim.verifications.authorizations.onboarding_pending"),
             onboarding_manager.filter_authorizations(unauthorized_methods),
             :unauthorized_method_display_data
           ]
