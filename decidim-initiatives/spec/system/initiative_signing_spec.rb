f320405e86044a0da5900cafcafe1c09b3a4ba8d
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2024-08-29 17:49:19 +0200

    Update test

--- decidim-initiatives/spec/system/initiative_signing_spec.rb
+++ decidim-initiatives/spec/system/initiative_signing_spec.rb
@@ -89,10 +89,12 @@ describe "Initiative signing" do
           visit decidim_initiatives.initiative_path(initiative)
 
           within ".initiative__aside" do
-            expect(page).to have_content("Verify your account to sign the initiative")
+            expect(page).to have_content("Verify your account to sign")
           end
-          click_on "Verify your account to sign the initiative"
-          expect(page).to have_content("Authorization required")
+          click_on "Verify your account to sign"
+
+          expect(page).to have_content("You are almost ready to sign in the initiative")
+          expect(page).to have_css("a[data-verification]", count: 2)
         end
       end
 
