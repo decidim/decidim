09dde3311298efc6926c15547ae52ccd140a6b81
elviabth <ejbenedith@gmail.com>
2024-08-29 22:19:13 +0200

    fix decidim-proposals spec failures

--- decidim-proposals/spec/system/proposals_fields_spec.rb
+++ decidim-proposals/spec/system/proposals_fields_spec.rb
@@ -312,8 +312,8 @@ describe "Proposals" do
             click_on "Publish"
 
             expect(page).to have_content("successfully")
+            expect(page).to have_content("Images")
 
-            click_on "Images"
             within "#panel-images" do
               expect(page).to have_css("img[src*=\"city.jpeg\"]", count: 1)
             end
