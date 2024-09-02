d2edfda2db6d0cc22aaf20323b5d25ec231c8a33
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2024-09-02 16:59:10 +0200

    Backport 'Fix deleting a component which has reminders associated with it' to v0.28 (#13329)
    * Fix deleting a component which has reminders associated with it
    
    * Fix deleting a component which has reminders associated with it
    
    * Change the spec to use the  test factory
    
    * Fix bad merge
    
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>
    
    ---------
    
    Co-authored-by: Antti Hukkanen <antti.hukkanen@mainiotech.fi>
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>

--- decidim-admin/app/commands/decidim/admin/destroy_component.rb
+++ decidim-admin/app/commands/decidim/admin/destroy_component.rb
@@ -44,6 +44,7 @@ module Decidim
       end
 
       def run_before_hooks
+        Decidim::Reminder.where(component: @component).destroy_all
         @component.manifest.run_hooks(:before_destroy, @component)
       end
 
