060b0d61686a53a42fc90b5434692bd272c6820c
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-09-04 16:18:26 +0200

    Fix rubocop offenses

--- decidim-core/lib/decidim/core/test/shared_examples/download_open_data_shared_context.rb
+++ decidim-core/lib/decidim/core/test/shared_examples/download_open_data_shared_context.rb
@@ -4,7 +4,7 @@ RSpec.shared_context "when downloading open data files" do
   def download_open_data_file
     # Prevent using the same cached file
     open_data_file = Rails.root.join("tmp/#{organization.open_data_file_path}")
-    File.delete(open_data_file) if File.exist?(open_data_file)
+    FileUtils.rm_f(open_data_file)
     Decidim::OpenDataJob.perform_now(organization)
     switch_to_host(organization.host)
     visit decidim.root_path
@@ -14,7 +14,7 @@ RSpec.shared_context "when downloading open data files" do
 
   def extract_content_from_zip(download_path, file_name)
     Zip::File.open(download_path) do |zipfile|
-      entry = zipfile.select { |entry| entry.name.match?(/.*#{file_name}/) }.first
+      entry = zipfile.select { |an_entry| an_entry.name.match?(/.*#{file_name}/) }.first
       content = entry.get_input_stream.read
 
       return content
