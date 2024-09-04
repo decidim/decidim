ff26ba9a3e86194de24e2e5a4d976083fc27df0d
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-09-04 19:10:42 +0200

    Add OpenDataInitiativeSerializer

---
+++ decidim-initiatives/app/serializers/decidim/initiatives/open_data_initiative_serializer.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Initiatives
+    class OpenDataInitiativeSerializer < Decidim::Exporters::Serializer
+      # Serializes an initiative
+      def serialize
+        {
+          id: resource.id,
+          title: resource.title,
+          description: resource.description,
+          state: resource.state,
+          created_at: resource.created_at,
+          published_at: resource.published_at,
+          signature_end_date: resource.signature_end_date,
+          signature_type: resource.signature_type,
+          signatures: resource.supports_count,
+          scope: {
+            name: resource.scope&.name
+          },
+          type: {
+            title: resource.type&.title
+          },
+          authors: {
+            id: resource.author_users.map(&:id),
+            name: resource.author_users.map(&:name)
+          },
+          area: {
+            name: resource.area&.name
+          }
+        }
+      end
+    end
+  end
+end
