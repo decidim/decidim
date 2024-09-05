db7a9848659b14c0e8dacb1d1a3e6496e81e4d6e
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-09-05 20:37:51 +0300

    Attempt to fix specs

--- decidim-core/spec/tasks/upgrade/clean_action_logs_spec.rb
+++ decidim-core/spec/tasks/upgrade/clean_action_logs_spec.rb
@@ -25,6 +25,7 @@ describe "rake decidim:upgrade:clean:action_logs", type: :task do
           Decidim::ActionLog.where(id: invalid_entries).update_all(resource_type: "Decidim::Component") # rubocop:disable Rails/SkipsModelValidations
 
           expect(Decidim::ActionLog.count).to eq(8)
+          expect(Decidim::ActionLog.where(resource_type: "Decidim::Component").count).to eq(4)
 
           expect { task.execute }.to change(Decidim::ActionLog, :count).by(-invalid_entries.size)
 
@@ -38,6 +39,7 @@ describe "rake decidim:upgrade:clean:action_logs", type: :task do
             Decidim::ActionLog.where(id: invalid_entries).update_all(resource_type: "Decidim::Dev::MissingResource") # rubocop:disable Rails/SkipsModelValidations
 
             expect(Decidim::ActionLog.count).to eq(8)
+            expect(Decidim::ActionLog.where(resource_type: "Decidim::Dev::MissingResource").count).to eq(4)
 
             expect { task.execute }.to change(Decidim::ActionLog, :count).by(-invalid_entries.size)
 
@@ -50,6 +52,7 @@ describe "rake decidim:upgrade:clean:action_logs", type: :task do
             Decidim::ActionLog.where(id: invalid_entries).update_all(participatory_space_type: "Decidim::Dev::MissingSpace") # rubocop:disable Rails/SkipsModelValidations
 
             expect(Decidim::ActionLog.count).to eq(8)
+            expect(Decidim::ActionLog.where(participatory_space_type: "Decidim::Dev::MissingSpace").count).to eq(4)
 
             expect { task.execute }.to change(Decidim::ActionLog, :count).by(-invalid_entries.size)
 
