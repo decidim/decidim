cdcc076ddeccef36b8fee0b7c4203f242cf5c196
elviabth <ejbenedith@gmail.com>
2024-09-05 15:12:06 +0200

    add proposal exit notification modal

--- decidim-proposals/app/packs/entrypoints/decidim_proposals.js
+++ decidim-proposals/app/packs/entrypoints/decidim_proposals.js
@@ -1,6 +1,7 @@
 import "src/decidim/proposals/utils"
 import "src/decidim/proposals/add_proposal"
 import "src/decidim/proposals/choose_proposals"
+import "src/decidim/proposals/exit_handler"
 
 // Images
 require.context("../images", true)
---
+++ decidim-proposals/app/packs/src/decidim/proposals/exit_handler.js
@@ -0,0 +1,63 @@
+
+const allowExitFrom = ($el) => {
+  if ($el.attr("id") === "exit-proposal-notification-link") {
+    return true;
+  } 
+
+  return false;
+}
+
+$(() => {
+  const $exitNotification = $("#exit-proposal-notification");
+  const $exitLink = $("#exit-proposal-notification-link");
+  const defaultExitUrl = $exitLink.attr("href");
+  const defaultExitLinkText = $exitLink.text();
+  const signOutPath = window.Decidim.config.get("sign_out_path");
+  let exitLinkText = defaultExitLinkText;
+
+  if ($exitNotification.length < 1) {
+    // Do not apply when not inside the voting pipeline
+    return;
+  }
+
+  const openExitNotification = (url, method = null) => {
+    if (method && method !== "get") {
+      $exitLink.attr("data-method", method);
+    } else {
+      $exitLink.removeAttr("data-method");
+    }
+
+    $exitLink.attr("href", url);
+    $exitLink.text(exitLinkText);
+    window.Decidim.currentDialogs["exit-proposal-notification"].open();
+  };
+
+  $(document).on("click", "a", (event) => {
+    exitLinkText = defaultExitLinkText;
+
+    const $link = $(event.currentTarget);
+    if (!allowExitFrom($link)) {
+      event.preventDefault();
+      openExitNotification($link.attr("href"), $link.data("method"));
+    }
+  });
+  // Custom handling for the header sign out so that it will not trigger the
+  // logout form submit and so that it changes the exit link text. This does
+  // not trigger the document link click listener because it has the
+  // data-method attribute to trigger a form submit event.
+  $(`[href='${signOutPath}']`).on("click", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const $link = $(event.currentTarget);
+    exitLinkText = $link.text();
+    openExitNotification($link.attr("href"), $link.data("method"));
+  });
+  // Custom handling for the exit link which needs to change the exit link
+  // text to the default text as this is not handled by the document click
+  // listener.
+  $("a[data-dialog-open='exit-proposal-notification']").on("click", () => {
+    exitLinkText = defaultExitLinkText;
+    openExitNotification(defaultExitUrl);
+  });
+});
