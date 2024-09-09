01d3a5fd64c01e71ea8b7d2a240915f2e1faf3d3
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-09-09 23:53:27 +0300

    Refactor decidim mounting routes option

--- decidim-conferences/lib/decidim/conferences/admin_engine.rb
+++ decidim-conferences/lib/decidim/conferences/admin_engine.rb
@@ -15,86 +15,94 @@ module Decidim
       paths["lib/tasks"] = nil
 
       routes do
-        resources :conferences, param: :slug, except: [:show, :destroy] do
-          resource :publish, controller: "conference_publications", only: [:create, :destroy]
-          resources :copies, controller: "conference_copies", only: [:new, :create]
-          resources :speakers, controller: "conference_speakers" do
-            member do
-              put :publish
-              put :unpublish
+        constraints(->(request) { Decidim::Admin::OrganizationDashboardConstraint.new(request).matches? }) do
+          resources :conferences, param: :slug, except: [:show, :destroy] do
+            resource :publish, controller: "conference_publications", only: [:create, :destroy]
+            resources :copies, controller: "conference_copies", only: [:new, :create]
+            resources :speakers, controller: "conference_speakers" do
+              member do
+                put :publish
+                put :unpublish
+              end
             end
+            resources :partners, controller: "partners", except: [:show]
+            resources :media_links, controller: "media_links"
+            resources :registration_types, controller: "registration_types" do
+              resource :publish, controller: "registration_type_publications", only: [:create, :destroy]
+              collection do
+                get :conference_meetings
+              end
+            end
+            resources :conference_invites, only: [:index, :new, :create]
+            resources :conference_registrations, only: :index do
+              member do
+                post :confirm
+              end
+              collection do
+                get :export
+              end
+            end
+            resource :diploma, only: [:edit, :update] do
+              member do
+                post :send, to: "diplomas#send_diplomas"
+              end
+            end
+            resources :user_roles, controller: "conference_user_roles" do
+              member do
+                post :resend_invitation, to: "conference_user_roles#resend_invitation"
+              end
+            end
+
+            resources :attachment_collections, controller: "conference_attachment_collections", except: [:show]
+            resources :attachments, controller: "conference_attachments", except: [:show]
           end
-          resources :partners, controller: "partners", except: [:show]
-          resources :media_links, controller: "media_links"
-          resources :registration_types, controller: "registration_types" do
-            resource :publish, controller: "registration_type_publications", only: [:create, :destroy]
-            collection do
-              get :conference_meetings
+
+          scope "/conferences/:conference_slug" do
+            resources :categories, except: [:show]
+
+            resources :components do
+              collection do
+                put :reorder
+              end
+              resource :permissions, controller: "component_permissions"
+              member do
+                put :publish
+                put :unpublish
+                get :share
+                put :hide
+              end
+              resources :exports, only: :create
+              resources :imports, only: [:new, :create] do
+                get :example, on: :collection
+              end
+              resources :reminders, only: [:new, :create]
             end
-          end
-          resources :conference_invites, only: [:index, :new, :create]
-          resources :conference_registrations, only: :index do
-            member do
-              post :confirm
-            end
-            collection do
-              get :export
-            end
-          end
-          resource :diploma, only: [:edit, :update] do
-            member do
-              post :send, to: "diplomas#send_diplomas"
-            end
-          end
-          resources :user_roles, controller: "conference_user_roles" do
-            member do
-              post :resend_invitation, to: "conference_user_roles#resend_invitation"
+
+            resources :moderations do
+              member do
+                put :unreport
+                put :hide
+                put :unhide
+              end
+              resources :reports, controller: "moderations/reports", only: [:index, :show]
             end
           end
 
-          resources :attachment_collections, controller: "conference_attachment_collections", except: [:show]
-          resources :attachments, controller: "conference_attachments", except: [:show]
+          scope "/conferences/:conference_slug/components/:component_id/manage" do
+            Decidim.component_manifests.each do |manifest|
+              next unless manifest.admin_engine
+
+              constraints CurrentComponent.new(manifest) do
+                mount manifest.admin_engine, at: "/", as: "decidim_admin_conference_#{manifest.name}"
+              end
+            end
+          end
         end
+      end
 
-        scope "/conferences/:conference_slug" do
-          resources :categories, except: [:show]
-
-          resources :components do
-            collection do
-              put :reorder
-            end
-            resource :permissions, controller: "component_permissions"
-            member do
-              put :publish
-              put :unpublish
-              get :share
-              put :hide
-            end
-            resources :exports, only: :create
-            resources :imports, only: [:new, :create] do
-              get :example, on: :collection
-            end
-            resources :reminders, only: [:new, :create]
-          end
-
-          resources :moderations do
-            member do
-              put :unreport
-              put :hide
-              put :unhide
-            end
-            resources :reports, controller: "moderations/reports", only: [:index, :show]
-          end
-        end
-
-        scope "/conferences/:conference_slug/components/:component_id/manage" do
-          Decidim.component_manifests.each do |manifest|
-            next unless manifest.admin_engine
-
-            constraints CurrentComponent.new(manifest) do
-              mount manifest.admin_engine, at: "/", as: "decidim_admin_conference_#{manifest.name}"
-            end
-          end
+      initializer "decidim_conferences_admin.mount_routes" do
+        Decidim::Core::Engine.routes do
+          mount Decidim::Conferences::AdminEngine, at: "/admin", as: "decidim_admin_conferences"
         end
       end
 
--- decidim-participatory_processes/lib/decidim/participatory_processes/admin_engine.rb
+++ decidim-participatory_processes/lib/decidim/participatory_processes/admin_engine.rb
@@ -16,91 +16,99 @@ module Decidim
       paths["lib/tasks"] = nil
 
       routes do
-        resources :participatory_process_groups do
-          resource :landing_page, only: [:edit, :update], controller: "participatory_process_group_landing_page" do
-            resources :content_blocks, only: [:edit, :update, :destroy, :create], controller: "participatory_process_group_landing_page_content_blocks"
+        constraints(->(request) { Decidim::Admin::OrganizationDashboardConstraint.new(request).matches? }) do
+          resources :participatory_process_groups do
+            resource :landing_page, only: [:edit, :update], controller: "participatory_process_group_landing_page" do
+              resources :content_blocks, only: [:edit, :update, :destroy, :create], controller: "participatory_process_group_landing_page_content_blocks"
+            end
           end
-        end
-        resources :participatory_process_types
-        resources :participatory_process_filters, except: [:show]
-        resources :participatory_processes, param: :slug, except: [:show, :destroy] do
-          resource :publish, controller: "participatory_process_publications", only: [:create, :destroy]
-          resources :copies, controller: "participatory_process_copies", only: [:new, :create]
+          resources :participatory_process_types
+          resources :participatory_process_filters, except: [:show]
+          resources :participatory_processes, param: :slug, except: [:show, :destroy] do
+            resource :publish, controller: "participatory_process_publications", only: [:create, :destroy]
+            resources :copies, controller: "participatory_process_copies", only: [:new, :create]
 
-          resources :steps, controller: "participatory_process_steps" do
-            resource :activate, controller: "participatory_process_step_activations", only: [:create, :destroy]
+            resources :steps, controller: "participatory_process_steps" do
+              resource :activate, controller: "participatory_process_step_activations", only: [:create, :destroy]
+              collection do
+                post :ordering, to: "participatory_process_step_ordering#create"
+              end
+            end
+            resources :user_roles, controller: "participatory_process_user_roles" do
+              member do
+                post :resend_invitation, to: "participatory_process_user_roles#resend_invitation"
+              end
+            end
+            resources :attachment_collections, controller: "participatory_process_attachment_collections", except: [:show]
+            resources :attachments, controller: "participatory_process_attachments", except: [:show]
+
+            resource :export, controller: "participatory_process_exports", only: :create
+
             collection do
-              post :ordering, to: "participatory_process_step_ordering#create"
+              resources :imports, controller: "participatory_process_imports", only: [:new, :create]
+            end
+            resource :landing_page, only: [:edit, :update], controller: "participatory_process_landing_page" do
+              resources :content_blocks, only: [:edit, :update, :destroy, :create], controller: "participatory_process_landing_page_content_blocks"
             end
           end
-          resources :user_roles, controller: "participatory_process_user_roles" do
-            member do
-              post :resend_invitation, to: "participatory_process_user_roles#resend_invitation"
+
+          scope "/participatory_processes/:participatory_process_slug" do
+            resources :categories, except: [:show]
+
+            resources :components do
+              collection do
+                put :reorder
+              end
+              resource :permissions, controller: "component_permissions"
+              member do
+                put :publish
+                put :unpublish
+                get :share
+                put :hide
+              end
+              resources :exports, only: :create
+              resources :imports, only: [:new, :create] do
+                get :example, on: :collection
+              end
+              resources :reminders, only: [:new, :create]
+            end
+
+            resources :moderations do
+              member do
+                put :unreport
+                put :hide
+                put :unhide
+              end
+              resources :reports, controller: "moderations/reports", only: [:index, :show]
+            end
+
+            resources :participatory_space_private_users, controller: "participatory_space_private_users" do
+              member do
+                post :resend_invitation, to: "participatory_space_private_users#resend_invitation"
+              end
+              collection do
+                resource :participatory_space_private_users_csv_imports, only: [:new, :create], path: "csv_import" do
+                  delete :destroy_all
+                end
+              end
             end
           end
-          resources :attachment_collections, controller: "participatory_process_attachment_collections", except: [:show]
-          resources :attachments, controller: "participatory_process_attachments", except: [:show]
 
-          resource :export, controller: "participatory_process_exports", only: :create
+          scope "/participatory_processes/:participatory_process_slug/components/:component_id/manage" do
+            Decidim.component_manifests.each do |manifest|
+              next unless manifest.admin_engine
 
-          collection do
-            resources :imports, controller: "participatory_process_imports", only: [:new, :create]
-          end
-          resource :landing_page, only: [:edit, :update], controller: "participatory_process_landing_page" do
-            resources :content_blocks, only: [:edit, :update, :destroy, :create], controller: "participatory_process_landing_page_content_blocks"
-          end
-        end
-
-        scope "/participatory_processes/:participatory_process_slug" do
-          resources :categories, except: [:show]
-
-          resources :components do
-            collection do
-              put :reorder
-            end
-            resource :permissions, controller: "component_permissions"
-            member do
-              put :publish
-              put :unpublish
-              get :share
-              put :hide
-            end
-            resources :exports, only: :create
-            resources :imports, only: [:new, :create] do
-              get :example, on: :collection
-            end
-            resources :reminders, only: [:new, :create]
-          end
-
-          resources :moderations do
-            member do
-              put :unreport
-              put :hide
-              put :unhide
-            end
-            resources :reports, controller: "moderations/reports", only: [:index, :show]
-          end
-
-          resources :participatory_space_private_users, controller: "participatory_space_private_users" do
-            member do
-              post :resend_invitation, to: "participatory_space_private_users#resend_invitation"
-            end
-            collection do
-              resource :participatory_space_private_users_csv_imports, only: [:new, :create], path: "csv_import" do
-                delete :destroy_all
+              constraints CurrentComponent.new(manifest) do
+                mount manifest.admin_engine, at: "/", as: "decidim_admin_participatory_process_#{manifest.name}"
               end
             end
           end
         end
+      end
 
-        scope "/participatory_processes/:participatory_process_slug/components/:component_id/manage" do
-          Decidim.component_manifests.each do |manifest|
-            next unless manifest.admin_engine
-
-            constraints CurrentComponent.new(manifest) do
-              mount manifest.admin_engine, at: "/", as: "decidim_admin_participatory_process_#{manifest.name}"
-            end
-          end
+      initializer "decidim_participatory_processes_admin.mount_routes" do
+        Decidim::Core::Engine.routes do
+          mount Decidim::ParticipatoryProcesses::AdminEngine, at: "/admin", as: "decidim_admin_participatory_processes"
         end
       end
 
