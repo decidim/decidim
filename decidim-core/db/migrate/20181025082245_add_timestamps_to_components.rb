2875303b400eb3355555115f298040b2d4b52c63
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-09-09 09:14:25 +0300

    Add manifest name check for spaces and components
    * Add manifest name check for spaces and components
    
    * Unscoping migrations
    
    * Fixing failin specs
    
    * Fix specs
    
    * Add cleanup tasks for invalid records
    
    * Add release notes
    
    * Fix spelling
    
    * Apply review recommendations
    
    * Remove commented code
    
    * Add specs
    
    * Fix spelling
    
    * Small refactor
    
    * Attempt to fix specs
    
    * Another attempt
    
    * Yet another fix attempt

--- decidim-core/db/migrate/20181025082245_add_timestamps_to_components.rb
+++ decidim-core/db/migrate/20181025082245_add_timestamps_to_components.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class AddTimestampsToComponents < ActiveRecord::Migration[5.2]
+  class Component < ApplicationRecord
+    self.table_name = :decidim_components
+  end
+
   def change
     add_timestamps :decidim_components, null: true
     # rubocop:disable Rails/SkipsModelValidations
-    Decidim::Component.update_all(created_at: Time.current, updated_at: Time.current)
+    Component.update_all(created_at: Time.current, updated_at: Time.current)
     # rubocop:enable Rails/SkipsModelValidations
     change_column_null :decidim_components, :created_at, false
     change_column_null :decidim_components, :updated_at, false
