9ffe52012571014c40d05ef317a49bbc239254ab
elviabth <ejbenedith@gmail.com>
2024-09-10 15:14:21 +0200

    remove send method

--- decidim-budgets/app/controllers/decidim/budgets/projects_controller.rb
+++ decidim-budgets/app/controllers/decidim/budgets/projects_controller.rb
@@ -74,7 +74,7 @@ module Decidim
       def items
         @items ||= [
           {
-            enabled: BudgetHistoryCell.new(@project).send(:history_items).any?,
+            enabled: BudgetHistoryCell.new(@project).history_items.any?,
             id: "included_history",
             text: t("decidim/history/history", scope: "activerecord.models", count: 2),
             icon: resource_type_icon_key("history"),
