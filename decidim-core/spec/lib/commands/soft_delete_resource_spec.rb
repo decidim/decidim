5fe98766dd8eb169210b025da1f0f6bff1f6f397
Anna Topalidi <t.opalidisanna@gmail.com>
2024-09-11 14:39:29 +0200

    add spec to decidim-core

---
+++ decidim-core/spec/lib/commands/soft_delete_resource_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  describe Commands::SoftDeleteResource do
+    subject { described_class.new(resource, user) }
+
+    let(:resource) { create(:dummy_resource) }
+    let(:organization) { resource.component.organization }
+    let(:user) { create(:user, organization:) }
+
+    context "when everything is ok" do
+      it "soft deletes the resource" do
+        subject.call
+
+        expect(resource.reload.trashed?).to be(true)
+      end
+
+      it "traces the action", versioning: true do
+        expect(Decidim.traceability)
+          .to receive(:perform_action!)
+          .with("soft_delete", resource, user)
+          .and_call_original
+
+        expect { subject.call }.to change(Decidim::ActionLog, :count)
+        action_log = Decidim::ActionLog.last
+        expect(action_log.version).to be_present
+        expect(action_log.action).to eq("soft_delete")
+      end
+    end
+
+    context "when the resource is invalid" do
+      before do
+        allow(subject).to receive(:invalid?).and_return(true)
+      end
+
+      it "does not soft delete the resource and broadcasts :invalid" do
+        expect(resource).not_to receive(:trash!)
+        expect { subject.call }.to broadcast(:invalid)
+      end
+    end
+
+    context "when a hook raises an error" do
+      before do
+        allow(subject).to receive(:run_before_hooks).and_raise(Decidim::Commands::HookError)
+      end
+
+      it "broadcasts :invalid" do
+        expect { subject.call }.to broadcast(:invalid)
+      end
+    end
+  end
+end
---
+++ decidim-core/spec/lib/soft_deletable_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  describe SoftDeletable do
+    let(:resource) { create(:dummy_resource) }
+
+    describe "#trashed?" do
+      context "when deleted_at is nil" do
+        it "returns false" do
+          expect(resource.trashed?).to be false
+        end
+      end
+
+      context "when deleted_at is set" do
+        before do
+          resource.update!(deleted_at: Time.current)
+        end
+
+        it "returns true" do
+          expect(resource.trashed?).to be true
+        end
+      end
+    end
+
+    describe "#trash!" do
+      it "sets deleted_at to current time" do
+        expect { resource.trash! }.to change(resource, :deleted_at).from(nil)
+      end
+    end
+
+    describe "#restore!" do
+      before do
+        resource.update!(deleted_at: Time.current)
+      end
+
+      it "clears the deleted_at field" do
+        expect { resource.restore! }.to change(resource, :deleted_at).to(nil)
+      end
+    end
+
+    describe ".not_deleted" do
+      let!(:resource1) { create(:dummy_resource) }
+      let!(:resource2) { create(:dummy_resource, deleted_at: Time.current) }
+
+      it "returns only resources that are not deleted" do
+        expect(Decidim::Dev::DummyResource.not_deleted).to include(resource1)
+        expect(Decidim::Dev::DummyResource.not_deleted).not_to include(resource2)
+      end
+    end
+
+    describe ".trashed" do
+      let!(:resource1) { create(:dummy_resource, deleted_at: Time.current) }
+      let!(:resource2) { create(:dummy_resource) }
+
+      it "returns only trashed resources" do
+        expect(Decidim::Dev::DummyResource.trashed).to include(resource1)
+        expect(Decidim::Dev::DummyResource.trashed).not_to include(resource2)
+      end
+    end
+  end
+end
