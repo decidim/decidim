08b8c5769ea7493dd6aaa090e469a33b401d6c5a
Elvia Benedith <116598037+ElviaBth@users.noreply.github.com>
2024-09-17 13:39:27 +0200

    Implement history for related components (#13290)
    * modify tab_panel name
    
    * add history tab_panel icon
    
    * register history-line icon
    
    * add history tab panel
    
    * remove no needed panel tabs in Proposal
    
    * add static design for proposal history
    
    * refactor static design for proposal history
    
    * create proposal_history_items method
    
    * run bundle exec i18n-tasks normalize --locales en
    
    * sort proposal_history by date
    
    * fix styles
    
    * add proposal state
    
    * add a url for resources
    
    * fix history line styles
    
    * refactor proposal_history cell
    
    * create proposal_history.scss file
    
    * refactor view styles
    
    * fix stylelint errors
    
    * fix explore_results_spec
    
    * fix manage_proposals_examples_spec
    
    * add decidim_sanitize_translated method
    
    * fix proposal failed specs
    
    * create proposal_history_cell_spec
    
    * add more cheks to proposal_spec
    
    * add more checks to proposal_history_cell_spec
    
    * add checks to spec
    
    * refactor proposal_history_cell
    
    * run bundle exec i18n-tasks unused --locales en
    
    * fix decidim-proposals spec failures
    
    * add history tab for budget
    
    * include result_history_cell
    
    * fix spelling error
    
    * run npm run lint
    
    * fix explore_results_spec file
    
    * refactor budget_history files
    
    * update budget history styles
    
    * fix styles in budget item[:text]
    
    * add history_cell specs
    
    * add import, merge and split proposals to history
    
    * distinct between from/to
    
    * add proposal history translations
    
    * add import_proposals linked text
    
    * fix proposal_history styles
    
    * apply DRY and refactor code
    
    * refactor add_linked_resources_items method
    
    * add extra_class
    
    * add 100% timeline height
    
    * use sanitize
    
    * remove send method
    
    * refactor history cell
    
    * rename cell
    
    * hide History tab when no resources present
    
    * fix preject_history_cell_spec
    
    * apply review comments feedback
    
    * apply feedback
    
    ---------
    
    Co-authored-by: Ivan Vergés <ivan@pokecode.net>

--- decidim-accountability/app/cells/decidim/accountability/project_cell.rb
+++ decidim-accountability/app/cells/decidim/accountability/project_cell.rb
@@ -15,6 +15,35 @@ module Decidim
         render template
       end
 
+      def tab_panel_items
+        [
+          {
+            enabled: ResultHistoryCell.new(result).render?,
+            id: "included_history",
+            text: t("decidim.history", scope: "activerecord.models", count: 2),
+            icon: resource_type_icon_key("history"),
+            method: :cell,
+            args: ["decidim/accountability/result_history", result]
+          },
+          {
+            enabled: timeline_entries.any?,
+            id: "timeline_entries",
+            text: t("decidim.accountability.results.timeline.title"),
+            icon: "route-line",
+            method: :cell,
+            args: ["decidim/accountability/project", result, { template: :timeline }]
+          },
+          {
+            enabled: children.any?,
+            id: "included_results",
+            text: t("activemodel.attributes.result.subresults"),
+            icon: "briefcase-2-line",
+            method: :cell,
+            args: ["decidim/accountability/results", result.children]
+          }
+        ] + attachments_tab_panel_items(result)
+      end
+
       private
 
       def template
@@ -31,51 +60,6 @@ module Decidim
 
       def scope
         current_scope.presence
-      end
-
-      def tab_panel_items
-        [
-          {
-            enabled: timeline_entries.any?,
-            id: "timeline_entries",
-            text: t("decidim.accountability.results.timeline.title"),
-            icon: "route-line",
-            method: :cell,
-            args: ["decidim/accountability/project", result, { template: :timeline }]
-          },
-          {
-            enabled: children.any?,
-            id: "included_results",
-            text: t("activemodel.attributes.result.subresults"),
-            icon: "briefcase-2-line",
-            method: :cell,
-            args: ["decidim/accountability/results", result.children]
-          },
-          {
-            enabled: result.linked_resources(:proposals, "included_proposals").present?,
-            id: "included_proposals",
-            text: t("activemodel.attributes.result.proposals"),
-            icon: "chat-new-line",
-            method: :cell,
-            args: ["decidim/linked_resources_for", result, { type: :proposals, link_name: "included_proposals" }]
-          },
-          {
-            enabled: result.linked_resources(:projects, "included_projects").present?,
-            id: "included_projects",
-            text: t("activemodel.attributes.result.project_ids"),
-            icon: "git-pull-request-line",
-            method: :cell,
-            args: ["decidim/linked_resources_for", result, { type: :projects, link_name: "included_projects" }]
-          },
-          {
-            enabled: result.linked_resources(:meetings, "meetings_through_proposals").present?,
-            id: "included_meetings",
-            text: t("activemodel.attributes.result.meetings_ids"),
-            icon: "treasure-map-line",
-            method: :cell,
-            args: ["decidim/linked_resources_for", result, { type: :meetings, link_name: "meetings_through_proposals" }]
-          }
-        ] + attachments_tab_panel_items(result)
       end
     end
   end
