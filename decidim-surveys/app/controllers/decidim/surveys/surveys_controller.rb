14fc8c674d9c60c3520fe50766739edc780ab4e7
elviabth <ejbenedith@gmail.com>
2024-09-20 17:58:12 +0200

    put index method

--- decidim-surveys/app/controllers/decidim/surveys/surveys_controller.rb
+++ decidim-surveys/app/controllers/decidim/surveys/surveys_controller.rb
@@ -14,6 +14,8 @@ module Decidim
 
       before_action :check_permissions
 
+      def index; end
+
       def check_permissions
         render :no_permission unless action_authorized_to(:answer, resource: survey).ok?
       end
