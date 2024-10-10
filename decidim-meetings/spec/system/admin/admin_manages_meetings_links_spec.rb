9498bcdbb55c42f96f85b2ca690f77033f2d00b9
Màxim Colls <collsmaxim@gmail.com>
2024-10-11 01:13:37 +0200

    Add Linked Meetings feature to allow linking meetings to other spaces (#13169)
    * Add linked meetings feature to allow linking meetings to other participatory spaces (MeetingLink model)
    
    * Update meeting components selctor to not show the current_component and show non published components
    
    * Fix rubocop offenses
    
    * Fix eslint offenses
    
    * Fix erblint offenses
    
    * Fix spelling linter
    
    * Normalise locales
    
    * Fix several admin/meetings specs
    
    * Fix specs
    
    * Add specs
    
    * Fix rubocop offenses
    
    * Disable assign button with private non transparent meeting
    
    * Add tooltip to unlink action button
    
    * Update meeting redirection message
    
    * Fix linking a meeting to private and transparent spaces
    
    * Fix unlink tooltip in dynamic elements
    
    * Normalise locales
    
    * Fix redirect callout, avoid using hash syntax
    
    * Move redirect notice to the frontend
    
    * Fix meeting cell when shown outside of a component
    
    * Escape space names
    
    * Fix meeting link in accountability results
    
    * Fix meeting cell when url extra params are passed
    
    * Unify private_non_transparent logic
    
    * Fix rubocop offense
    
    * Fix method
    
    * Add specs for private_non_transparent_space? method in component

---
+++ decidim-meetings/spec/system/admin/admin_manages_meetings_links_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "decidim/dev/test/rspec_support/tom_select"
+
+describe "Admin manages meetings" do
+  let(:manifest_name) { "meetings" }
+  let!(:meeting) { create(:meeting, :published, scope:, component: current_component) }
+
+  let!(:other_participatory_space) { create(:participatory_process, organization: current_component.organization) }
+  let!(:other_component) { create(:meeting_component, participatory_space: other_participatory_space) }
+  let!(:other_meeting) { create(:meeting, :published, component: other_component) }
+
+  include_context "when managing a component as an admin"
+
+  describe "listing meeting links" do
+    before do
+      create(:meeting_link, meeting: other_meeting, component: current_component)
+      visit current_path
+    end
+
+    it "shows the meeting links" do
+      expect(page).to have_css("tbody tr:first-child", text: Decidim::Meetings::MeetingPresenter.new(other_meeting).title)
+      expect(page).to have_css("tbody tr:last-child", text: Decidim::Meetings::MeetingPresenter.new(meeting).title)
+    end
+  end
+
+  describe "linking a meeting" do
+    it "creates a new link" do
+      within "tr", text: Decidim::Meetings::MeetingPresenter.new(meeting).title do
+        click_on "Edit"
+      end
+
+      within "#accordion-linked-spaces" do
+        click_on "Linked spaces"
+        tom_select("#add_component_select", option_id: other_meeting.component.id)
+        click_on "Assign"
+      end
+
+      within ".js-components" do
+        expect(page).to have_content(other_meeting.component.manifest.name)
+      end
+
+      expect do
+        click_on "Update"
+      end.to change { meeting.meeting_links.count }.by(1)
+    end
+  end
+end
