f2400812108bfc993111e997dc6e25b8ff36fadf
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-10-11 18:17:37 +0300

    Running linters

--- decidim-core/spec/system/external_domain_warning_spec.rb
+++ decidim-core/spec/system/external_domain_warning_spec.rb
@@ -57,7 +57,7 @@ describe "ExternalDomainWarning" do
 
     it "does not show invalid url alert" do
       visit url
-      expect(page).to have_no_content("Invalid URL")
+      expect(page).not_to have_content("Invalid URL")
       expect(page).to have_content("Me,myself,and,I")
     end
   end
