479efeb1f61de4608a8fbd0bc615d1909446e7a1
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-10-11 15:07:16 +0200

    Skip empty items

--- decidim-core/app/serializers/decidim/schema_org_breadcrumb_list_serializer.rb
+++ decidim-core/app/serializers/decidim/schema_org_breadcrumb_list_serializer.rb
@@ -17,6 +17,8 @@ module Decidim
     # @see https://schema.org/BreadcrumbList
     # @see https://developers.google.com/search/docs/appearance/structured-data/breadcrumb?hl=en
     def serialize
+      return {} if breadcrumb_items.none? { |item| item.has_key?(:url) }
+
       {
         "@context": "https://schema.org",
         "@type": "BreadcrumbList",
@@ -32,6 +34,8 @@ module Decidim
       all_items = []
 
       breadcrumb_items.each_with_index do |item, index|
+        next if item.empty?
+
         all_items << {
           "@type": "ListItem",
           position: index + 1,
--- decidim-core/spec/serializers/decidim/schema_org_breadcrumb_list_serializer_spec.rb
+++ decidim-core/spec/serializers/decidim/schema_org_breadcrumb_list_serializer_spec.rb
@@ -21,7 +21,6 @@ module Decidim
           dropdown_cell: "decidim/participatory_processes/process_dropdown_metadata",
           resource: participatory_process
         }
-
       ]
     end
 
@@ -46,6 +45,48 @@ module Decidim
           { "@type": "ListItem", position: 2, name: "Hello world", item: "https://example.org/processes/hello-world" }
         ]
         expect(serialized[:itemListElement]).to eq(expected_items_elements)
+      end
+
+      context "when there are empty items" do
+        let(:breadcrumb_items) do
+          [
+            {
+              label: "Processes",
+              url: "/processes",
+              active: true
+            },
+            {
+              label: { ca: "Hola mon", es: "Hola mundo", en: "Hello world" },
+              url: "/processes/hello-world",
+              dropdown_cell: "decidim/participatory_processes/process_dropdown_metadata",
+              resource: participatory_process
+            },
+            {}
+          ]
+        end
+
+        it "ignores them" do
+          expected_items_elements = [
+            { "@type": "ListItem", position: 1, name: "Processes", item: "https://example.org/processes" },
+            { "@type": "ListItem", position: 2, name: "Hello world", item: "https://example.org/processes/hello-world" }
+          ]
+          expect(serialized[:itemListElement]).to eq(expected_items_elements)
+        end
+      end
+
+      context "when there are only items without URLs" do
+        let(:breadcrumb_items) do
+          [
+            {
+              label: "Profile",
+              active: true
+            }
+          ]
+        end
+
+        it "returns an empty JSON" do
+          expect(serialized).to eq({})
+        end
       end
     end
   end
