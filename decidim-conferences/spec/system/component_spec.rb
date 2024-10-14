55fe143d417c05652d95f8a74d0f1dd61b849afd
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-10-14 13:01:55 +0300

    Fix usage of private_non_transparent_space?

---
+++ decidim-conferences/spec/system/component_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Initiative Components" do
+  describe "private_non_transparent_space?" do
+    subject { component }
+
+    let!(:organization) { create(:organization) }
+
+    let(:participatory_space) { create(:conference, organization:) }
+    let(:component) { create(:component, manifest_name: "another_dummy", participatory_space:) }
+
+    context "when the component belongs to a space that does not respond to private_space?" do
+      it "returns false" do
+        expect(subject.private_non_transparent_space?).to be false
+      end
+    end
+  end
+end
