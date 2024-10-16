a74e5e23cdbcffd7f9742a9c709b645d31cf69dd
Ivan Vergés <ivan@pokecode.net>
2024-10-16 08:33:04 +0200

    Improve components and spaces sharing with administrable tokens (#13221)
    * remove share_tokens from admin/components/_form
    
    * add routes
    
    * add component name on index title
    
    * add form and new views
    
    * fix index styles
    
    * refactor share_token form
    
    * refactor share_token form
    
    * add locales keys
    
    * add automatic_token attribute
    
    * refactor createsharetoken command
    
    * remove target_blank
    
    * add more methods to sharetokenform
    
    * refactor token_for definition
    
    * add edit and update method to controller
    
    * fix routing error
    
    * add create and update command specs
    
    * fix spelling error
    
    * fix spelling error
    
    * add share_token_form spec
    
    * remove target_ blank from edit action
    
    * remove set_default_expiration method
    
    * add collection_radio_buttonts
    
    * fix no expitration bug
    
    * add registered_only to decidim_share_tokens migration
    
    * add more checks to share_tokens_form spec
    
    * add more checks to commands create and update share_token spec
    
    * add copy clipboard functionality
    
    * fix lint errors
    
    * add is_active_link for share_tokens_path
    
    * remove space detected
    
    * fix noMethodError on user_can_preview_component? method
    
    * add enforce_permission to controller
    
    * fix manage_components_share_tokens spec
    
    * fix manage_process_components_examples spec
    
    * fix share_token_spec
    
    * add share_tokens routes on initiatives-module
    
    * remove unused i18n keys
    
    * add more checks to manage_component_share_tokens
    
    * fix has to edit a share token case check
    
    * add spec check allows copying the share link from the share token
    
    * save clipboard-copy-label-original
    
    * fix clipboard js
    
    * fix validations and views
    
    * add specs
    
    * update permissions
    
    * update documentation
    
    * add help text
    
    * allow to manage participatory spaces share tokens
    
    * add space specs
    
    * add preview specs
    
    * fix clipboard
    
    * fix specs
    
    * fix new minimum page items
    
    * trailing spaces
    
    * use standard datepicker
    
    * fix surveys component actions
    
    * make spec deterministic
    
    * fix specs
    
    * debug
    
    * prevent token repetition in parallel tests
    
    * apply corrections
    
    * fix typo
    
    * fix typo
    
    * harmonize copies
    
    * apply review
    
    * lint
    
    * change test
    
    * apply review
    
    * add td sizes
    
    * normalize sizes
    
    * add action logs
    
    * add model specs
    
    * fix title interpolation
    
    * fix creat command spec
    
    ---------
    
    Co-authored-by: elviabth <ejbenedith@gmail.com>

--- decidim-meetings/spec/system/preview_meetings_with_share_token_spec.rb
+++ decidim-meetings/spec/system/preview_meetings_with_share_token_spec.rb
@@ -2,9 +2,9 @@
 
 require "spec_helper"
 
-describe "Preview meetings with share token" do
+describe "preview meetings with a share token" do
   let(:manifest_name) { "meetings" }
 
   include_context "with a component"
-  it_behaves_like "preview component with share_token"
+  it_behaves_like "preview component with a share_token"
 end
