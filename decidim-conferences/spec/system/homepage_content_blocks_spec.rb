9994f08425c7fef5ff02ac0cb18b6ae1e6a2f836
David Igón <david.igon@riseup.net>
2024-10-18 18:16:28 +0200

    improve homepage_content_block_spec for conferences

--- decidim-conferences/spec/system/homepage_content_blocks_spec.rb
+++ decidim-conferences/spec/system/homepage_content_blocks_spec.rb
@@ -8,9 +8,9 @@ describe "Homepage conferences content blocks" do
   let!(:promoted_conference) { create(:conference, :promoted, organization:) }
   let!(:unpromoted_conference) { create(:conference, organization:) }
   let!(:promoted_external_conference) { create(:conference, :promoted) }
+  let!(:highlighted_conferences_content_block) { create(:content_block, organization:, scope_name: :homepage, manifest_name: :highlighted_conferences) }
 
   before do
-    create(:content_block, organization:, scope_name: :homepage, manifest_name: :highlighted_conferences)
     switch_to_host(organization.host)
   end
 
@@ -21,6 +21,33 @@ describe "Homepage conferences content blocks" do
       expect(page).to have_i18n_content(promoted_conference.title)
       expect(page).to have_i18n_content(unpromoted_conference.title)
       expect(page).not_to have_i18n_content(promoted_external_conference.title)
+
+      expect(page).to have_css("a.card__grid", count: 3)
+    end
+  end
+
+  include_context "when admin administrating a conference"
+
+  before do
+    switch_to_host(organization.host)
+    login_as user, scope: :user
+  end
+
+  it "updates the number of highlighted conferences with a number input field" do
+    visit decidim_admin.edit_organization_homepage_content_block_path(highlighted_conferences_content_block)
+
+    expect(find("input[type='number'][name='content_block[settings][max_results]']").value).to eq("6")
+
+    fill_in "content_block[settings][max_results]", with: "1"
+    click_on "Update"
+
+    expect(page).to have_content("Highlighted conferences")
+    expect(highlighted_conferences_content_block.reload.settings["max_results"]).to eq(1)
+
+    visit decidim.root_path
+
+    within "#highlighted-conferences" do
+      expect(page).to have_css("a.card__grid", count: 1)
     end
   end
 end
