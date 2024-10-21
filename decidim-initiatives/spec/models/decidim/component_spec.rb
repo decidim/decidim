b6c3901c6b10fa6e770cf4a416dd622a5641beb1
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-10-21 17:06:54 +0300

    Refactor specs

---
+++ decidim-initiatives/spec/models/decidim/component_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  describe Component do
+    subject { component }
+
+    let!(:organization) { create(:organization) }
+    let!(:participatory_space) { create(:conference, organization:) }
+    let(:component) { build(:component, manifest_name: "dummy", participatory_space:) }
+
+    describe ".private_non_transparent_space?" do
+      # since the conferences don't respond to private_space? we are testing and mae sure it doesn't fail
+      it { expect(subject.private_non_transparent_space?).to be_faley }
+    end
+  end
+end
