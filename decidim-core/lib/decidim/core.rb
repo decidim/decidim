0ea38a6b93422d51c2bd833bd1ce55bca20a16cb
Anna Topalidi <60363870+antopalidi@users.noreply.github.com>
2024-10-31 10:39:59 +0100

    Update decidim-core/lib/decidim/core.rb
    Co-authored-by: Ivan Vergés <ivan@pokecode.net>

--- decidim-core/lib/decidim/core.rb
+++ decidim-core/lib/decidim/core.rb
@@ -846,7 +846,7 @@ module Decidim
   end
 
   # Public Setting that allows to configure which component will have "Use my current location" button
-  # in a geocoded address field. Accepts an array of component manifest names
+  # in a geocoded address field. Accepts an array of component manifest names or simply the value ":all" to apply everywhere (default value)
   config_accessor :show_my_location_button do
     [:proposals]
   end
