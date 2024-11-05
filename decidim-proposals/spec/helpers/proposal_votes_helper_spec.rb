0650a5f277ba99236ce81eeca7d56cf41c025627
elviabth <ejbenedith@gmail.com>
2024-11-05 22:16:25 +0100

    add more checks to specs

--- decidim-proposals/spec/helpers/proposal_votes_helper_spec.rb
+++ decidim-proposals/spec/helpers/proposal_votes_helper_spec.rb
@@ -42,6 +42,57 @@ module Decidim
           expect(helper.remaining_votes_count_for(user)).to eq(9)
         end
       end
+
+      describe "#remaining_minimum_votes_count_for" do
+        subject { helper.remaining_minimum_votes_count_for(user) }
+
+        let(:minimum_votes) { 5 }
+
+        before do
+          allow(helper).to receive(:vote_limit_enabled?).and_return(vote_limit_enabled)
+          allow(helper).to receive(:component_settings).and_return(double(minimum_votes_per_user: minimum_votes))
+        end
+
+        context "when the vote limit is not enabled" do
+          let(:vote_limit_enabled) { false }
+
+          it "returns 0" do
+            expect(subject).to eq(0)
+          end
+        end
+
+        context "when the vote limit is enabled" do
+          let(:vote_limit_enabled) { true }
+
+          context "when the user has not cast any votes" do
+            it "returns the minimum votes per user" do
+              expect(subject).to eq(minimum_votes)
+            end
+          end
+
+          context "when the user has cast some votes" do
+            before do
+              proposals = create_list(:proposal, 3, component: proposal_component)
+              proposals.each { |proposal| create(:proposal_vote, author: user, proposal:) }
+            end
+
+            it "returns the remaining minimum votes" do
+              expect(subject).to eq(2)
+            end
+          end
+
+          context "when the user has cast enough votes to meet or exceed the minimum" do
+            before do
+              proposals = create_list(:proposal, 5, component: proposal_component)
+              proposals.each { |proposal| create(:proposal_vote, author: user, proposal:) }
+            end
+
+            it "returns 0, as the minimum has been met" do
+              expect(subject).to eq(0)
+            end
+          end
+        end
+      end
     end
   end
 end
