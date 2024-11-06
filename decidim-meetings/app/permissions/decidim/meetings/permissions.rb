e56b017f6b6da6f27bc7a23b5f2344fc423be0c4
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-11-06 22:32:00 +0200

    Fix permission

--- decidim-meetings/app/permissions/decidim/meetings/permissions.rb
+++ decidim-meetings/app/permissions/decidim/meetings/permissions.rb
@@ -8,7 +8,10 @@ module Decidim
         return Decidim::Meetings::Admin::Permissions.new(user, permission_action, context).permissions if permission_action.scope == :admin
         return permission_action if permission_action.scope != :public
 
-        return toggle_allow(!meeting&.hidden? && meeting&.current_user_can_visit_meeting?(user)) if permission_action.subject == :meeting && permission_action.action == :read
+        if permission_action.subject == :meeting && permission_action.action == :read
+          toggle_allow(!meeting&.hidden? && meeting&.current_user_can_visit_meeting?(user))
+          return permission_action
+        end
 
         return permission_action unless user
 
