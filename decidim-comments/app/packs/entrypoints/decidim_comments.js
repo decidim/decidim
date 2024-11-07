c9f5985e45d92befabb725ba2426b07fbff18b47
elviabth <ejbenedith@gmail.com>
2024-11-07 12:16:23 +0100

    add dropdown JS

--- decidim-comments/app/packs/entrypoints/decidim_comments.js
+++ decidim-comments/app/packs/entrypoints/decidim_comments.js
@@ -3,3 +3,4 @@ import "stylesheets/comments.scss"
 
 // JavaScript
 import "src/decidim/comments/comments"
+import "src/decidim/comments/comments_user_group"
---
+++ decidim-comments/app/packs/src/decidim/comments/comments_user_group.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("[data-comments-dropdown]");
+  const dropdownId = button.getAttribute("data-target");
+  const dropdownMenu = document.getElementById(dropdownId);
+
+  dropdownMenu.querySelectorAll("input[type='radio']").forEach((input) => {
+    input.addEventListener("click", () => {
+      const authorInfo = input.closest(".comment__as-author-container").querySelector(".comment__as-author-info");
+
+      if (authorInfo) {
+        const authorContent = authorInfo.innerHTML;
+
+        button.querySelector("span").innerHTML = authorContent;
+        const selectedLi = input.closest("li");
+        selectedLi.style.display = "none";
+
+        dropdownMenu.querySelectorAll("li").forEach(li => {
+          if (li !== selectedLi) {
+              li.style.display = "";
+          }
+        });
+
+        dropdownMenu.style.display = "none";
+      }
+    });
+  });
+
+  button.addEventListener("click", () => {
+    dropdownMenu.style.display = dropdownMenu.style.display === "none" ? "" : "none";
+  });
+});
+
