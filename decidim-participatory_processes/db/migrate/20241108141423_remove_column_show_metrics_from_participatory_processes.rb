dfbc04dc2e4fe58a94d8393480cdebc97ba5cd05
Tom Greenwood <tom.t.greenwood@gmail.com>
2024-11-12 11:28:07 +0100

    Revert "Remove useless fields from processes and assemblies (#13639)"
    This reverts commit e6b3a050a2c20cbaaceaefbfaedad246fcc803b3.

--- decidim-participatory_processes/db/migrate/20241108141423_remove_column_show_metrics_from_participatory_processes.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveColumnShowMetricsFromParticipatoryProcesses < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :decidim_participatory_processes, :show_metrics, :boolean
-  end
-end
--- decidim-participatory_processes/db/migrate/20241108141605_remove_column_show_statistics_from_participatory_processes.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveColumnShowStatisticsFromParticipatoryProcesses < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :decidim_participatory_processes, :show_statistics, :string
-  end
-end
