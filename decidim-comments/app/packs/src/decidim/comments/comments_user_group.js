1d10e7283b3567945a992b5f446b9d2420c762c9
elviabth <ejbenedith@gmail.com>
2024-11-12 11:52:34 +0100

    add fixed button to modal

--- decidim-comments/app/packs/src/decidim/comments/comments_user_group.js
+++ decidim-comments/app/packs/src/decidim/comments/comments_user_group.js
@@ -1,36 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const button = document.querySelector("[data-comments-dropdown]");
-  const dropdownId = button.getAttribute("data-target");
-  const dropdownMenu = document.getElementById(dropdownId);
+  document.querySelectorAll("[data-comments-dropdown]").forEach((button) => {
+    const dropdownId = button.getAttribute("data-target");
+    const dropdownMenu = document.getElementById(dropdownId);
 
-  const firstLi = dropdownMenu.querySelector("li");
-  const firstAuthorInfo = firstLi.querySelector(".comment__as-author-info");
-  
-  if (firstAuthorInfo) {
-    button.querySelector("span").innerHTML = firstAuthorInfo.innerHTML;
+    const firstLi = dropdownMenu.querySelector("li");
+    const firstAuthorInfo = firstLi?.querySelector(".comment__as-author-info");
+    if (firstAuthorInfo) {
+      button.querySelector("span").innerHTML = firstAuthorInfo.innerHTML;
+      firstLi.style.display = "none";
+    }
 
-    firstLi.style.display = "none";
-  }
+    dropdownMenu.querySelectorAll("input[type='radio']").forEach((input) => {
+      input.addEventListener("click", () => {
+        const authorInfo = input.closest(".comment__as-author-container").querySelector(".comment__as-author-info");
 
-  dropdownMenu.querySelectorAll("input[type='radio']").forEach((input) => {
-    input.addEventListener("click", () => {
-      const authorInfo = input.closest(".comment__as-author-container").querySelector(".comment__as-author-info");
+        if (authorInfo) {
+          const authorContent = authorInfo.innerHTML;
 
-      if (authorInfo) {
-        const authorContent = authorInfo.innerHTML;
+          setTimeout(() => {
+            button.querySelector("span").innerHTML = authorContent;
 
-        setTimeout(() => {
-          button.querySelector("span").innerHTML = authorContent;
-          const selectedLi = input.closest("li");
-          selectedLi.style.display = "none";
-            
-          dropdownMenu.querySelectorAll("li").forEach((li) => {
-            if (li !== selectedLi) {
-              li.style.display = "";
-            }
-          });
-        }, 2000);
-      }
+            const selectedLi = input.closest("li");
+            selectedLi.style.display = "none";
+            dropdownMenu.querySelectorAll("li").forEach((li) => {
+              if (li !== selectedLi) {
+                li.style.display = "";
+              }
+            });
+          }, 2000);
+        }
+      });
     });
   });
 });
