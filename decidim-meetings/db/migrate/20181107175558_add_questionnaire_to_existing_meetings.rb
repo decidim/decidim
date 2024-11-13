850bd6a84b58b411a366ca8e9bd0414585e58d2b
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-11-13 22:58:13 +0200

    Patch migrations

--- decidim-meetings/db/migrate/20181107175558_add_questionnaire_to_existing_meetings.rb
+++ decidim-meetings/db/migrate/20181107175558_add_questionnaire_to_existing_meetings.rb
@@ -3,7 +3,7 @@
 class AddQuestionnaireToExistingMeetings < ActiveRecord::Migration[5.2]
   def change
     Decidim::Meetings::Meeting.transaction do
-      Decidim::Meetings::Meeting.find_each do |meeting|
+      Decidim::Meetings::Meeting.unscoped.find_each do |meeting|
         if meeting.component.present? && meeting.questionnaire.blank?
           meeting.update!(
             questionnaire: Decidim::Forms::Questionnaire.new
