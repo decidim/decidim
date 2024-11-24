517ed080e6209cf4aa3554e4aa93f59d445fa816
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-11-24 22:45:46 +0100

    Generate attributes documentation for 'conference_invitations'

---
+++ decidim-conferences/spec/services/decidim/download_your_data_exporter_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "decidim/core/test/shared_examples/download_your_data_shared_examples"
+
+module Decidim
+  describe DownloadYourDataExporter do
+    subject { DownloadYourDataExporter.new(user, "download-your-data", "CSV") }
+
+    let(:user) { create(:user, organization:) }
+    let(:organization) { create(:organization) }
+
+    describe "#readme" do
+      context "when the user has a conference invite" do
+        let!(:conference_invite) { create(:conference_invite, user:) }
+        let(:help_definition_string) { "The date when this conference invitation was sent" }
+
+        it_behaves_like "a download your data entity"
+      end
+    end
+  end
+end
