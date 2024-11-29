43ddbf1c73378d28bc3eb043987a9389e9aeb914
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-11-29 23:50:41 +0200

    Fix function description

--- decidim-core/app/packs/src/decidim/remote_tooltips.js
+++ decidim-core/app/packs/src/decidim/remote_tooltips.js
@@ -1,10 +1,17 @@
 import createTooltip from "src/decidim/tooltips"
 
 /**
- * This function is parsing the DOM and tries to attach and initialize a tooltip starting from an element
- * containing the following structure:
+ * Given the following HTML structure,
  * <span data-remote-tooltip="true" tooltip-url="some url" data-author="true">
  *   <span></span>
+ * </span>
+ * 
+ * This function will check if the HTMLElement where is attached to has a child, and will add a data tooltip attribute
+ * to the respective child in order to attach the fetched HTML content fetched under a json key as the content of the
+ * HTML tooltip. The DOM structure is expected to be like follows:
+ *
+ * <span data-remote-tooltip="true" tooltip-url="some url" data-author="true">
+ *   <span data-tooltip="HTML content from json data field"></span>
  * </span>
  *
  * @param {HTMLElement} node The element holding the initialization data
