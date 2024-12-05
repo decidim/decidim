640cd0aba697bc8266c24b2f7d893016b341cf76
elviabth <ejbenedith@gmail.com>
2024-12-05 12:16:36 +0100

    fix spec

--- decidim-core/lib/decidim/core/test/shared_examples/admin_log_presenter_examples.rb
+++ decidim-core/lib/decidim/core/test/shared_examples/admin_log_presenter_examples.rb
@@ -32,53 +32,5 @@ shared_examples "present admin log entry" do
         expect(subject).not_to include("class=\"logs__log__diff\"")
       end
     end
-
-    context "when the logged action has extra data for the diff" do
-      let(:admin_log_extra_data) do
-        {
-          "extra" => {
-            "reportable_type" => "DummyResource",
-            "reported_count" => 3,
-            "reported_content" => { "title" => { "en" => "Example Title" } }
-          }
-        }
-      end
-
-      it "includes the reported content in the diff" do
-        expect(subject).to include("Example Title")
-      end
-    end
-  end
-
-  describe "#action_string" do
-    subject { presenter.send(:action_string) }
-
-    context "when the action is 'unreport'" do
-      let(:action) { "unreport" }
-
-      it "returns the correct I18n key" do
-        expect(subject).to eq("decidim.admin_log.moderation.unreport")
-      end
-    end
-  end
-
-  describe "#has_diff?" do
-    subject { presenter.send(:has_diff?) }
-
-    context "when the action is in the diff actions and changeset has data" do
-      let(:action) { "unreport" }
-
-      before do
-        allow(presenter).to receive(:changeset).and_return({ key: "value" })
-      end
-
-      it { is_expected.to be true }
-    end
-
-    context "when the action is not in the diff actions" do
-      let(:action) { "create" }
-
-      it { is_expected.to be false }
-    end
   end
 end
