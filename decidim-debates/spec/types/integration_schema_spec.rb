35a759d378724fa268006f0de57e74b6b0e6101d
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-12-11 11:27:15 +0200

    Add authorization to GraphQL
    * Temp commit
    
    * Adding authorization layer
    
    * Add more permission checks
    
    * Running linters
    
    * Add Commentable tests
    
    * Fix spec
    
    * Do not show the moderated or deleted comments
    
    * Add conditions for comments, users and user groups
    
    * Fixing specs
    
    * Fix latest round of specs
    
    * Fix organization spec
    
    * Apply suggestions from code review
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    * Add Praticipatory space is private but transparent
    
    * Add Praticipatory space is private but transparent for accountability
    
    * Add Praticipatory space is private but transparent for debates
    
    * Add Praticipatory space is private but transparent for meetings
    
    * Add Praticipatory space is private but transparent for forms
    
    * Add Praticipatory space is private but transparent for proposals
    
    * Add Praticipatory space is private but transparent for sortitions
    
    * Linting
    
    * Fix visibility on assemblies
    
    * Refactor
    
    * Fix specs
    
    * Fix specs
    
    * Fix specs, refactor the name of specs
    
    * Additional GraphQL fixes (#13602)
    
    * Additional GraphQL fixes
    
    * More fixes
    
    * Fix spec
    
    * Add specs
    
    * Fix specs
    
    * Fix permission
    
    * Apply review recommendations
    
    * Apply suggestions from code review
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    * Apply review recommendations
    
    * Refactor specs
    
    * Apply review recommendations
    
    * Running linters
    
    ---------
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    * Refactor context to avoid codeclimate
    
    * running lint
    
    * Fix specs
    
    ---------
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-debates/spec/types/integration_schema_spec.rb
+++ decidim-debates/spec/types/integration_schema_spec.rb
@@ -2,58 +2,9 @@
 
 require "spec_helper"
 require "decidim/api/test/component_context"
-require "decidim/budgets/test/factories"
 
 describe "Decidim::Api::QueryType" do
-  include_context "with a graphql decidim component"
-  let(:component_type) { "Debates" }
-
-  let!(:current_component) { create(:debates_component, participatory_space: participatory_process) }
-  let!(:debate) { create(:debate, :participant_author, component: current_component, category:) }
-
-  let(:debate_single_result) do
-    {
-      "acceptsNewComments" => debate.accepts_new_comments?,
-      "author" => { "id" => debate.author.id.to_s },
-      "category" => { "id" => debate.category.id.to_s },
-      "comments" => [],
-      "commentsHaveAlignment" => debate.comments_have_alignment?,
-      "commentsHaveVotes" => debate.comments_have_votes?,
-      "createdAt" => debate.created_at.iso8601.to_s.gsub("Z", "+00:00"),
-      "description" => { "translation" => debate.description[locale] },
-      "endTime" => debate.end_time,
-      "hasComments" => debate.comment_threads.size.positive?,
-      "id" => debate.id.to_s,
-      "image" => nil,
-      "informationUpdates" => { "translation" => debate.information_updates[locale] },
-      "instructions" => { "translation" => debate.instructions[locale] },
-      "reference" => debate.reference,
-      "startTime" => debate.start_time,
-      "title" => { "translation" => debate.title[locale] },
-      "totalCommentsCount" => debate.comments_count,
-      "type" => "Decidim::Debates::Debate",
-      "updatedAt" => debate.updated_at.iso8601.to_s.gsub("Z", "+00:00"),
-      "userAllowedToComment" => debate.user_allowed_to_comment?(current_user)
-    }
-  end
-
-  let(:debates_data) do
-    {
-      "__typename" => "Debates",
-      "id" => current_component.id.to_s,
-      "name" => { "translation" => translated(current_component.name) },
-      "debates" => {
-        "edges" => [
-          {
-            "node" => debate_single_result
-          }
-        ]
-      },
-      "weight" => 0
-    }
-  end
-
-  describe "valid connection query" do
+  include_context "with a graphql decidim component" do
     let(:component_fragment) do
       %(
       fragment fooComponent on Debates {
@@ -101,6 +52,77 @@ describe "Decidim::Api::QueryType" do
       }
 )
     end
+  end
+  let(:component_type) { "Debates" }
+
+  let!(:current_component) { create(:debates_component, participatory_space: participatory_process) }
+  let!(:debate) { create(:debate, :participant_author, component: current_component, category:) }
+  let(:author) { build(:user, :confirmed, organization: current_component.organization) }
+
+  let(:debate_single_result) do
+    {
+      "acceptsNewComments" => debate.accepts_new_comments?,
+      "author" => { "id" => debate.author.id.to_s },
+      "category" => { "id" => debate.category.id.to_s },
+      "comments" => [],
+      "commentsHaveAlignment" => debate.comments_have_alignment?,
+      "commentsHaveVotes" => debate.comments_have_votes?,
+      "createdAt" => debate.created_at.iso8601.to_s.gsub("Z", "+00:00"),
+      "description" => { "translation" => debate.description[locale] },
+      "endTime" => debate.end_time,
+      "hasComments" => debate.comment_threads.size.positive?,
+      "id" => debate.id.to_s,
+      "image" => nil,
+      "informationUpdates" => { "translation" => debate.information_updates[locale] },
+      "instructions" => { "translation" => debate.instructions[locale] },
+      "reference" => debate.reference,
+      "startTime" => debate.start_time,
+      "title" => { "translation" => debate.title[locale] },
+      "totalCommentsCount" => debate.comments_count,
+      "type" => "Decidim::Debates::Debate",
+      "updatedAt" => debate.updated_at.iso8601.to_s.gsub("Z", "+00:00"),
+      "userAllowedToComment" => debate.user_allowed_to_comment?(current_user)
+    }
+  end
+
+  let(:debates_data) do
+    {
+      "__typename" => "Debates",
+      "id" => current_component.id.to_s,
+      "name" => { "translation" => translated(current_component.name) },
+      "debates" => {
+        "edges" => [
+          {
+            "node" => debate_single_result
+          }
+        ]
+      },
+      "weight" => 0
+    }
+  end
+
+  describe "commentable" do
+    let(:component_fragment) { nil }
+
+    let(:participatory_process_query) do
+      %(
+        commentable(id: "#{debate.id}", type: "Decidim::Debates::Debate", locale: "en", toggleTranslations: false) {
+          __typename
+        }
+      )
+    end
+
+    it "executes successfully" do
+      expect { response }.not_to raise_error
+    end
+
+    it do
+      expect(response).to eq({ "commentable" => { "__typename" => "Debate" } })
+    end
+  end
+
+  describe "valid connection query" do
+
 
     it "executes successfully" do
       expect { response }.not_to raise_error
@@ -112,50 +134,6 @@ describe "Decidim::Api::QueryType" do
   end
 
   describe "valid query" do
-    let(:component_fragment) do
-      %(
-      fragment fooComponent on Debates {
-        debate(id: #{debate.id}){
-          acceptsNewComments
-          author {
-            id
-          }
-          category {
-            id
-          }
-          comments {
-            id
-          }
-          commentsHaveAlignment
-          commentsHaveVotes
-          createdAt
-          description {
-            translation(locale: "#{locale}")
-          }
-          endTime
-          hasComments
-          id
-          image
-          informationUpdates {
-            translation(locale: "#{locale}")
-          }
-          instructions {
-            translation(locale: "#{locale}")
-          }
-          reference
-          startTime
-          title {
-            translation(locale: "#{locale}")
-          }
-          totalCommentsCount
-          type
-          updatedAt
-          userAllowedToComment
-        }
-      }
-)
-    end
-
     it "executes successfully" do
       expect { response }.not_to raise_error
     end
@@ -163,5 +141,11 @@ describe "Decidim::Api::QueryType" do
     it do
       expect(response["participatoryProcess"]["components"].first["debate"]).to eq(debate_single_result)
     end
+  end
+
+  include_examples "with resource visibility" do
+    let(:component_factory) { :debates_component }
+    let(:lookout_key) { "debate" }
+    let(:query_result) { debate_single_result }
   end
 end
