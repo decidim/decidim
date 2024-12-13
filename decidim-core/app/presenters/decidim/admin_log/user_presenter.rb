86b15d076c75c7e3849928dc4ad78cf5c329ec6c
elviabth <ejbenedith@gmail.com>
2024-12-13 13:37:35 +0100

    add new class base_user_presenter

--- decidim-core/app/presenters/decidim/admin_log/user_presenter.rb
+++ decidim-core/app/presenters/decidim/admin_log/user_presenter.rb
@@ -11,7 +11,7 @@ module Decidim
     #    action_log = Decidim::ActionLog.last
     #    view_helpers # => this comes from the views
     #    UserPresenter.new(action_log, view_helpers).present
-    class UserPresenter < Decidim::Log::BasePresenter
+    class UserPresenter < BaseUserPresenter
       private
 
       def action_string
@@ -26,23 +26,8 @@ module Decidim
 
       def i18n_params
         super.merge(
-          role: I18n.t("models.user.fields.roles.#{user_role}", scope: "decidim.admin"),
-          blocked_count: blocked_users.count,
-          unblocked_count: unblocked_users.count,
-          unreported_users_count: unreported_users.count
+          role: I18n.t("models.user.fields.roles.#{user_role}", scope: "decidim.admin")
         )
-      end
-
-      def blocked_users
-        action_log.extra.dig("extra", "blocked")&.values || []
-      end
-
-      def unblocked_users
-        action_log.extra.dig("extra", "unblocked")&.values || []
-      end
-
-      def unreported_users
-        action_log.extra.dig("extra", "unreported")&.values || []
       end
 
       def user_role
@@ -59,47 +44,6 @@ module Decidim
 
       def previous_justification
         action_log.extra.dig("extra", "previous_justification") || ""
-      end
-
-      def current_justification
-        action_log.extra.dig("extra", "current_justification") || Hash.new("")
-      end
-
-      # Overwrite the changeset for officialization,block and more actions.
-      def changeset
-        config = changeset_config[action.to_s]
-        return {} unless config
-
-        original = config[:original].call
-        return {} if original.values.flatten.all?(&:empty?)
-
-        Decidim::Log::DiffChangesetCalculator.new(original, config[:fields], i18n_labels_scope).changeset
-      end
-
-      def changeset_config
-        {
-          "block" => {
-            original: -> { { justification: [previous_justification, current_justification] } },
-            fields: { justification: :string }
-          },
-          "bulk_block" => {
-            original: -> { { justification: [blocked_users.join(", "), current_justification] } },
-            fields: { justification: :string }
-          },
-          "bulk_unblock" => {
-            original: -> { { unblocked_users: ["", unblocked_users.join(", ")] } },
-            fields: { unblocked_users: :string }
-          },
-          "bulk_ignore" => {
-            original: -> { { unreported_users: ["", unreported_users.join(", ")] } },
-            fields: { unreported_users: :string }
-          }
-        }
-      end
-
-      # override this as it not depend on the old version
-      def has_diff?
-        diff_actions.include?(action.to_s) && changeset.any?
       end
 
       def diff_actions
