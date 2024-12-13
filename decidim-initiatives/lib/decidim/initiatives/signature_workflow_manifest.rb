c4591a8c18a5119131ab5eaf32ff1973ef804ebd
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2024-12-13 13:30:10 +0100

    Use signature workflow and handler from signatures controller

--- decidim-initiatives/lib/decidim/initiatives/signature_workflow_manifest.rb
+++ decidim-initiatives/lib/decidim/initiatives/signature_workflow_manifest.rb
@@ -1,97 +1,82 @@
 # frozen_string_literal: true
 
-require "decidim/settings_manifest"
+# require "decidim/settings_manifest"
+require "decidim/initiatives/default_signature_authorizer"
 
 module Decidim
   module Initiatives
-    # TODO - This is a copy from verifications. Pending to adapt to signatures
-    # in initiatives
-    autoload :DefaultActionAuthorizer, "decidim/verifications/default_action_authorizer"
+    autoload :DefaultSignatureAuthorizer, "decidim/initiatives/default_signature__authorizer"
 
     #
-    # This class serves as a DSL to declaratively specify a verification method.
+    # This class serves as a DSL to declaratively specify a signature method.
     #
-    # To define a direct verification method, you need to specify the `form`
-    # attribute as a `Decidim::Form` that will be valid if the authorization is
-    # valid.
+    # To define a direct signature method, you need to specify the `form`
+    # attribute as a `Decidim::Form` that will be valid if the signature
+    # process is valid.
     #
-    # To define a deferred verification method, you need specify the `engine`
-    # attribute as a full `Rails::Engine` (and optionally another engine for the
-    # admin side as the `admin_engine` attribute). The verification will succeed
-    # once the engine creates a valid authorization for the method with the
-    # `granted_at` column set in DB.
+    # An example of declaration of a workflow:
     #
-    # Note that whereas direct methods can be used for "on the fly"
-    # verification, deferred methods require the authorization status to be
-    # persisted into DB. That is the reason why only direct methods can be used
-    # for user impersonation, for example, since they require continuous "on the
-    # fly" verification by the impersonating user.
+    # Decidim::Initiatives::Signatures.register_workflow(:dummy_signature_handler) do |workflow|
+    #   workflow.form = "DummySignatureHandler"
+    #   workflow.authorization_handler_form = "DummyAuthorizationHandler"
+    #   workflow.action_authorizer = "DummySignatureHandler::DummySignatureActionAuthorizer"
+    #   workflow.promote_authorization_validation_errors = true
+    #   workflow.sms_verification = true
+    # end
     #
     class SignatureWorkflowManifest
       include ActiveModel::Model
       include Decidim::AttributeObject::Model
 
-      attribute :engine, Rails::Engine, **{}
-      attribute :admin_engine, Rails::Engine, **{}
+      # For the moment no engines are going to be defined
+      # attribute :engine, Rails::Engine, **{}
+      # attribute :admin_engine, Rails::Engine, **{}
+      attribute :name, String
       attribute :form, String
-      attribute :expires_in, ActiveSupport::Duration, default: 0.minutes
       attribute :action_authorizer, String
-      attribute :renewable, Boolean, default: true
-      attribute :time_between_renewals, ActiveSupport::Duration, default: 1.day
-      attribute :metadata_cell, String, default: "decidim/verifications/authorization_metadata"
-      attribute :icon, String
+      attribute :authorization_handler_form, String, default: nil
+      attribute :save_authorizations, Boolean, default: true
+      attribute :promote_authorization_validation_errors, Boolean, default: false
+      attribute :ephemeral, Boolean, default: false
       attribute :sms_verification, Boolean, default: true
 
-      validate :engine_or_form
-
-      attribute :name, String
       validates :name, presence: true
-
-      def engine_or_form
-        engine || form
-      end
-
-      def type
-        form ? "direct" : "multistep"
-      end
+      validates :form, presence: true
 
       alias key name
 
       def fullname
-        I18n.t("#{key}.name", scope: "decidim.authorization_handlers", default: name.humanize)
-      end
-
-      def description
-        "#{fullname} (#{I18n.t(type, scope: "decidim.authorization_handlers")})"
+        I18n.t("#{key}.name", scope: "decidim.signature_workflows", default: name.humanize)
       end
 
       def action_authorizer_class
         if action_authorizer.present?
           action_authorizer.constantize
         else
-          DefaultActionAuthorizer
+          DefaultSignatureAuthorizer
         end
       end
 
-      # Public: Adds configurable settings for this verification workflow. It
-      # uses the DSL specified under `Decidim::SettingsManifest`.
-      #
-      # &block - The DSL present on `Decidim::SettingsManifest`
-      #
-      # Examples:
-      #
-      #   workflow.options do |options|
-      #     options.attribute :minimum_age, type: :integer, default: 18
-      #   end
-      #
-      # Returns nothing.
-      def options
-        @options ||= SettingsManifest.new
+      # For the moment this will not be used
+      # # Public: Adds configurable settings for this verification workflow. It
+      # # uses the DSL specified under `Decidim::SettingsManifest`.
+      # #
+      # # &block - The DSL present on `Decidim::SettingsManifest`
+      # #
+      # # Examples:
+      # #
+      # #   workflow.options do |options|
+      # #     options.attribute :minimum_age, type: :integer, default: 18
+      # #   end
+      # #
+      # # Returns nothing.
+      # def options
+      #   @options ||= SettingsManifest.new
 
-        yield(@options) if block_given?
+      #   yield(@options) if block_given?
 
-        @options
-      end
+      #   @options
+      # end
     end
   end
 end
