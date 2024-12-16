c1a0b510f2141a74e3c64d0dde09c228a369b7cb
Anna Topalidi <t.opalidisanna@gmail.com>
2024-12-16 18:35:24 +0100

    fix resize

--- decidim-comments/app/packs/src/decidim/comments/comments.js
+++ decidim-comments/app/packs/src/decidim/comments/comments.js
@@ -1,30 +1,38 @@
-import CommentsComponent from "src/decidim/comments/comments.component"
-import { screens } from "tailwindcss/defaultTheme"
+import CommentsComponent from "src/decidim/comments/comments.component";
+import { screens } from "tailwindcss/defaultTheme";
 
 window.Decidim.CommentsComponent = CommentsComponent;
 
 const commentsInitializer = () => {
-  const isMobileScreen = () => window.innerWidth < parseInt(screens.md, 10);
+  const smBreakpoint = parseInt(screens.md.replace("px", ""), 10);
+  const isMobileScreen = () => window.matchMedia(`(max-width: ${smBreakpoint}px)`).matches;
 
   // Mount comments component
   $("[data-decidim-comments]").each((_i, el) => {
     const $el = $(el);
     let comments = $(el).data("comments");
     let wasMobileScreen = isMobileScreen();
+
     if (!comments) {
       comments = new CommentsComponent($el, $el.data("decidim-comments"));
+      comments.mountComponent();
+      $(el).data("comments", comments);
     }
-    comments.mountComponent();
-    $(el).data("comments", comments);
+
     window.addEventListener("resize", () => {
-      if ((wasMobileScreen && !isMobileScreen()) || (!wasMobileScreen && isMobileScreen())) {
-        console.log("Resizing comments component", comments);
-        wasMobileScreen = isMobileScreen();
-        comments.reloadAllComments();
+      const isNowMobileScreen = isMobileScreen();
+      if ((wasMobileScreen && !isNowMobileScreen) || (!wasMobileScreen && isNowMobileScreen)) {
+        if (typeof comments.unmountComponent === "function") {
+          comments.unmountComponent();
+        }
+        comments = new CommentsComponent($el, $el.data("decidim-comments"));
+        comments.mountComponent();
+        wasMobileScreen = isNowMobileScreen;
+        $(el).data("comments", comments);
       }
     });
   });
-}
+};
 
 // If no jQuery is used the Tribute feature used in comments to autocomplete
 // mentions stops working
