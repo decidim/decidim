933b7a6ed9f2bd4b63c48e01a751cd5523fd69c5
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2024-12-16 14:53:20 +0100

    Fix flaky spec when changing the locale of the visitor

--- decidim-core/spec/system/locales_spec.rb
+++ decidim-core/spec/system/locales_spec.rb
@@ -42,6 +42,8 @@ describe "Locales" do
         click_on "Català"
       end
 
+      # Prevent flaky spec, where sometimes the language is not changed before the visit
+      sleep 2
       visit decidim_admin.root_path
 
       expect(page).to have_content("Cal iniciar sessió o crear un compte abans de continuar.")
@@ -68,6 +70,9 @@ describe "Locales" do
 
       before do
         login_as user, scope: :user
+
+        # Prevent flaky spec, where sometimes the language is not changed before the visit
+        sleep 2
         visit decidim.root_path
       end
 
