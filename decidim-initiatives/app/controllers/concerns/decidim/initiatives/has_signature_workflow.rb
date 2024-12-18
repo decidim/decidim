5c911da8b40ad085cf46440873414d4bb7481a37
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2024-12-18 18:15:36 +0100

    Allow to define forms and commands related with sms step

--- decidim-initiatives/app/controllers/concerns/decidim/initiatives/has_signature_workflow.rb
+++ decidim-initiatives/app/controllers/concerns/decidim/initiatives/has_signature_workflow.rb
@@ -11,18 +11,21 @@ module Decidim
       extend ActiveSupport::Concern
 
       included do
+
+        delegate :sms_mobile_phone_form_class, :sms_mobile_phone_validator_class, :sms_code_validator_class, to: :signature_workflow_manifest
+
         private
 
         def signature_workflow_manifest
           @signature_workflow_manifest ||= begin
             # XXX - This is a temporary trick
             handler_name = current_initiative.type.document_number_authorization_handler.gsub("authorization", "signature")
-            Decidim::Initiatives::Signatures.find_workflow_manifest(handler_name)
+            Decidim::Initiatives::Signatures.find_workflow_manifest(handler_name) || Decidim::Initiatives::SignatureWorkflowManifest.new
           end
         end
 
         def signature_form_class
-          return Decidim::Initiatives::SignatureHandler if signature_workflow_manifest.blank?
+          return Decidim::Initiatives::SignatureHandler if signature_workflow_manifest.form.blank?
 
           signature_workflow_manifest.form.constantize
         end
