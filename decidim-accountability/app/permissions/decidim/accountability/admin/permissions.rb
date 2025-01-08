420e0707acce11f7854a86c3432e7960df41f7e5
Màxim Colls <collsmaxim@gmail.com>
2025-01-08 21:49:35 +0100

    Accountability bulk actions (#13730)

--- decidim-accountability/app/permissions/decidim/accountability/admin/permissions.rb
+++ decidim-accountability/app/permissions/decidim/accountability/admin/permissions.rb
@@ -11,6 +11,7 @@ module Decidim
           permission_action.allow! if can_perform_actions_on?(:status, status)
           permission_action.allow! if can_perform_actions_on?(:timeline_entry, timeline_entry)
           permission_action.allow! if can_perform_actions_on?(:import_component, nil)
+          permission_action.allow! if can_perform_actions_on?(:bulk_update, nil)
 
           permission_action
         end
