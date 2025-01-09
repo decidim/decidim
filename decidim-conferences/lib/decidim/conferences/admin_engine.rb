20ebdde22605e9d5d920650302c4e20a0c0f3953
Ivan Vergés <ivan@pokecode.net>
2025-01-09 17:32:24 +0100

    remove categories controllers

--- decidim-conferences/lib/decidim/conferences/admin_engine.rb
+++ decidim-conferences/lib/decidim/conferences/admin_engine.rb
@@ -68,8 +68,6 @@ module Decidim
         end
 
         scope "/conferences/:conference_slug" do
-          resources :categories, except: [:show]
-
           resources :components do
             collection do
               put :reorder
--- decidim-participatory_processes/lib/decidim/participatory_processes/admin_engine.rb
+++ decidim-participatory_processes/lib/decidim/participatory_processes/admin_engine.rb
@@ -58,8 +58,6 @@ module Decidim
         end
 
         scope "/participatory_processes/:participatory_process_slug" do
-          resources :categories, except: [:show]
-
           resources :components do
             collection do
               put :reorder
