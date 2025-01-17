473408554c4494dc54c21f2144dd90e5e8105854
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-01-17 23:09:09 +0200

    Add ActiveJOb config

--- decidim-dev/lib/decidim/dev/test/rspec_support/active_job.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/active_job.rb
@@ -2,6 +2,11 @@
 
 RSpec.configure do |config|
   config.include ActiveJob::TestHelper
+
+  config.after do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
 end
 
 ActiveJob::Base.queue_adapter = :test
