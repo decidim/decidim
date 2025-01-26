4b803f1818e52e550418e93670a16fcc11580733
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-01-26 16:05:10 +0200

    Fix specs

--- decidim-core/lib/decidim/core/test/shared_examples/etiquette_validator_examples.rb
+++ decidim-core/lib/decidim/core/test/shared_examples/etiquette_validator_examples.rb
@@ -2,29 +2,22 @@
 
 shared_examples "etiquette validator" do |options|
   options[:fields].map(&:to_sym).each do |field|
+    let!(field) { options[:i18n] ? { en: string } : string }
+
     context "when #{field} contains too many caps" do
-      let(field) do
-        string = "#{"A" * 50}#{"a" * 49}"
-        options[:i18n] == false ? string : { en: string }
-      end
+      let(:string) { "#{"A" * 50}#{"a" * 49}" }
 
       it { is_expected.to be_invalid }
     end
 
     context "when #{field} not starting with caps" do
-      let(field) do
-        string = "aa#{"A" * 50}#{"a" * 49}"
-        options[:i18n] == false ? string : { en: string }
-      end
+      let(:string) { "aa#{"A" * 50}#{"a" * 49}" }
 
       it { is_expected.to be_invalid }
     end
 
     context "when #{field} containing too many marks" do
-      let(field) do
-        string = "#{"A" * 5}#{"a" * 49}!!??"
-        options[:i18n] == false ? string : { en: string }
-      end
+      let(:string) { "#{"A" * 5}#{"a" * 49}!!??" }
 
       it { is_expected.to be_invalid }
     end
