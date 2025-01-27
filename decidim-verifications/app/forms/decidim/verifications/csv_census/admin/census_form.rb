4d49a1cb37cc7dd30f5cf20950b01a922ad44984
elviabth <ejbenedith@gmail.com>
2025-01-27 10:35:24 +0100

    add census_form spec

--- decidim-verifications/app/forms/decidim/verifications/csv_census/admin/census_form.rb
+++ decidim-verifications/app/forms/decidim/verifications/csv_census/admin/census_form.rb
@@ -9,6 +9,19 @@ module Decidim
           attribute :email
 
           validates :email, presence: true
+          validate :unique_email
+
+          private
+
+          def unique_email
+            return true if Decidim::User.where(
+              organization: context.current_organization,
+              email:
+            ).where.not(id: context.current_user.id).empty?
+
+            errors.add :email, :taken
+            false
+          end
         end
       end
     end
