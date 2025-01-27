111823562850a558f7f58fea6b80e35170286dc9
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-01-27 11:23:09 +0100

    Remove trailing whitespaces

--- decidim-core/app/packs/src/decidim/sticky_footer.js
+++ decidim-core/app/packs/src/decidim/sticky_footer.js
@@ -1,10 +1,10 @@
 // This script add margin to the footer when the sticky CTA Buttons are shown on mobile devices,
 // so the footer is always visible.
-// 
+//
 // The sticky buttons show some of the main Call to Actions (CTAs) so that they remain accessible on the
 // screen as the participant scrolls through the detailed view of the Meetings, Proposals, Surveys, and Budgets
 // components.
-// 
+//
 
 const footer = document.querySelector("footer");
 const stickyButtons = document.querySelector("[data-sticky-buttons]");
