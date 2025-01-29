460fe646665990f954c5f2e9faaae5854fc97320
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-01-29 12:15:20 +0100

    Refactor invite and registration serializer classes and DRY them up

--- decidim-meetings/app/serializers/decidim/meetings/download_your_data_invite_serializer.rb
+++ decidim-meetings/app/serializers/decidim/meetings/download_your_data_invite_serializer.rb
@@ -2,33 +2,14 @@
 
 module Decidim
   module Meetings
-    class DownloadYourDataInviteSerializer < Decidim::Exporters::Serializer
+    class DownloadYourDataInviteSerializer < BaseDownloadYourDataSerializer
       # Serializes an invite for download your data
       def serialize
-        {
-          id: resource.id,
+        super.merge({
           sent_at: resource.sent_at,
           accepted_at: resource.accepted_at,
           rejected_at: resource.rejected_at,
-          created_at: resource.created_at,
-          updated_at: resource.updated_at,
-          meeting: {
-            title: resource.meeting.title,
-            url: Decidim::ResourceLocatorPresenter.new(resource.meeting).url,
-            description: resource.meeting.description,
-            start_time: resource.meeting.start_time,
-            end_time: resource.meeting.end_time,
-            address: resource.meeting.address,
-            location: resource.meeting.location,
-            location_hints: resource.meeting.location_hints,
-            reference: resource.meeting.reference,
-            attendees_count: resource.meeting.attendees_count,
-            attending_organizations: resource.meeting.attending_organizations,
-            closed_at: resource.meeting.closed_at,
-            closing_report: resource.meeting.closing_report,
-            published_at: resource.meeting.published_at
-          }
-        }
+        })
       end
     end
   end
--- decidim-meetings/app/serializers/decidim/meetings/download_your_data_registration_serializer.rb
+++ decidim-meetings/app/serializers/decidim/meetings/download_your_data_registration_serializer.rb
@@ -2,33 +2,14 @@
 
 module Decidim
   module Meetings
-    class DownloadYourDataRegistrationSerializer < Decidim::Exporters::Serializer
+    class DownloadYourDataRegistrationSerializer < BaseDownloadYourDataSerializer
       # Serializes a registration for download your data
       def serialize
-        {
-          id: resource.id,
+        super.merge({
           code: resource.code,
-          created_at: resource.created_at,
-          updated_at: resource.updated_at,
           validated_at: resource.validated_at,
           public_participation: resource.public_participation,
-          meeting: {
-            title: resource.meeting.title,
-            url: Decidim::ResourceLocatorPresenter.new(resource.meeting).url,
-            description: resource.meeting.description,
-            start_time: resource.meeting.start_time,
-            end_time: resource.meeting.end_time,
-            address: resource.meeting.address,
-            location: resource.meeting.location,
-            location_hints: resource.meeting.location_hints,
-            reference: resource.meeting.reference,
-            attendees_count: resource.meeting.attendees_count,
-            attending_organizations: resource.meeting.attending_organizations,
-            closed_at: resource.meeting.closed_at,
-            closing_report: resource.meeting.closing_report,
-            published_at: resource.meeting.published_at
-          }
-        }
+        })
       end
     end
   end
