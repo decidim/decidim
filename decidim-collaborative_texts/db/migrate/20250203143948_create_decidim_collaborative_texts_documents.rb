0c5ae62a7a6a916eeb27d1d4eecb9e8ca6250842
elviabth <ejbenedith@gmail.com>
2025-02-03 15:59:46 +0100

    fix lint error

--- decidim-collaborative_texts/db/migrate/20250203143948_create_decidim_collaborative_texts_documents.rb
+++ decidim-collaborative_texts/db/migrate/20250203143948_create_decidim_collaborative_texts_documents.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateDecidimCollaborativeTextsDocuments < ActiveRecord::Migration[7.0]
   def change
     create_table :decidim_collaborative_texts_documents do |t|
