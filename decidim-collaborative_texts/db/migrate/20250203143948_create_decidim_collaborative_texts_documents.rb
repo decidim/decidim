0af023a9c1ff475b2c61d9c6fe12d55b5a6af613
elviabth <ejbenedith@gmail.com>
2025-02-03 15:44:53 +0100

    create documents table

---
+++ decidim-collaborative_texts/db/migrate/20250203143948_create_decidim_collaborative_texts_documents.rb
@@ -0,0 +1,12 @@
+class CreateDecidimCollaborativeTextsDocuments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :decidim_collaborative_texts_documents do |t|
+      t.integer :decidim_component_id
+      t.string :title
+      t.timestamp :published_at
+      t.boolean :accepting_suggestions
+
+      t.timestamps
+    end
+  end
+end
