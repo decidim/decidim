39268efb6cf5e83ad29ff462cd4fde7c9725a7ed
elviabth <ejbenedith@gmail.com>
2025-02-04 23:33:57 +0100

    fix test

--- decidim-collaborative_texts/spec/controllers/decidim/collaborative_texts/admin/collaborative_texts_controller_spec.rb
+++ decidim-collaborative_texts/spec/controllers/decidim/collaborative_texts/admin/collaborative_texts_controller_spec.rb
@@ -2,21 +2,32 @@
 
 require "spec_helper"
 
-describe Decidim::CollaborativeTexts::Admin::CollaborativeTextsController do
-  routes { Decidim::CollaborativeTexts::AdminEngine.routes }
+module Decidim
+  module CollaborativeTexts
+    module Admin
+      describe CollaborativeTextsController do
+        routes { Decidim::CollaborativeTexts::AdminEngine.routes }
 
-  let(:user) { create(:user, :admin, :confirmed, organization:) }
-  let(:organization) { create(:organization) }
+        let(:user) { create(:user, :admin, :confirmed, organization:) }
+        let(:organization) { create(:organization) }
+        let(:participatory_space) { create(:participatory_process, organization:) }
+        let!(:component) { create(:collaborative_texts_component, participatory_space:) }
 
-  before do
-    request.env["decidim.current_organization"] = user.organization
-    sign_in user, scope: :user
-  end
+        before do
+          request.env["decidim.current_organization"] = user.organization
+          sign_in user, scope: :user
+          allow(controller).to receive(:current_participatory_space).and_return(participatory_space)
+          allow(controller).to receive(:current_component).and_return(component)
+        end
 
-  describe "GET #index" do
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template(:index)
+        describe "GET #index" do
+          it "renders the index template" do
+            get :index
+            expect(response).to have_http_status(:ok)
+            expect(response).to render_template(:index)
+          end
+        end
+      end
     end
   end
 end
