0a721a74efa48e53d1eef3f15d6db99ab0ee0b3f
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-02-06 16:09:39 +0200

    Fix more GraphQL rubocop offenses (part 2) (#13995)
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-comments/lib/decidim/api/commentable_mutation_type.rb
+++ decidim-comments/lib/decidim/api/commentable_mutation_type.rb
@@ -6,8 +6,8 @@ module Decidim
       description "A commentable which includes its available mutations"
 
       field :add_comment, Decidim::Comments::CommentType, description: "Add a new comment to a commentable", null: true do
-        argument :body, GraphQL::Types::String, "The comments's body", required: true
         argument :alignment, GraphQL::Types::Int, "The comment's alignment. Can be 0 (neutral), 1 (in favor) or -1 (against)'", default_value: 0, required: false
+        argument :body, GraphQL::Types::String, "The comments's body", required: true
         argument :user_group_id, GraphQL::Types::ID, "The comment's user group id. Replaces the author.", required: false
       end
       field :id, GraphQL::Types::ID, "The Commentable's unique ID", null: false
