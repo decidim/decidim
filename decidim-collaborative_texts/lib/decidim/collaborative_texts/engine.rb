99c101f01cb02e8987a6215753d11834dfddfffb
Ivan Vergés <ivan@pokecode.net>
2025-02-06 10:42:44 +0100

    add migrations in the engine

--- decidim-collaborative_texts/lib/decidim/collaborative_texts/engine.rb
+++ decidim-collaborative_texts/lib/decidim/collaborative_texts/engine.rb
@@ -4,12 +4,6 @@ module Decidim
   module CollaborativeTexts
     class Engine < ::Rails::Engine
       isolate_namespace Decidim::CollaborativeTexts
-
-      paths["db/migrate"] = nil
-
-      def load_seed
-        nil
-      end
     end
   end
 end
