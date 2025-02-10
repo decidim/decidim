545afac77b74d89cff56657a57b75fe618742d0b
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-02-10 09:14:20 +0100

    Fix rubocop offense

--- decidim-core/app/helpers/decidim/map_helper.rb
+++ decidim-core/app/helpers/decidim/map_helper.rb
@@ -36,7 +36,11 @@ module Decidim
           }.merge(map_html_options)
           return link_to(map_url, html_options) do
             # We also add the latitude and the longitude to prevent the Workbox cache to be overly aggressive when updating a map
-            image_tag decidim.static_map_path(sgid: resource.to_sgid.to_s, latitude: resource.latitude, longitude: resource.longitude), alt: "#{map_service_brand} - #{address_text}"
+            image_tag decidim.static_map_path(
+              sgid: resource.to_sgid.to_s,
+              latitude: resource.latitude,
+              longitude: resource.longitude
+            ), alt: "#{map_service_brand} - #{address_text}"
           end
         end
       end
