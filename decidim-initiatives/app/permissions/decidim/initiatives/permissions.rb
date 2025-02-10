313dcecfbe12c0857e2a221f273040d3062a28e8
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-02-10 13:16:17 +0100

    Change permissions to permit ephemeral users signatures

--- decidim-initiatives/app/permissions/decidim/initiatives/permissions.rb
+++ decidim-initiatives/app/permissions/decidim/initiatives/permissions.rb
@@ -15,6 +15,7 @@ module Decidim
         read_public_initiative?
         search_initiative_types_and_scopes?
         request_membership?
+        ephemeral_vote_initiative?
 
         return permission_action unless user
 
@@ -143,6 +144,14 @@ module Decidim
         toggle_allow(can_vote?)
       end
 
+      def ephemeral_vote_initiative?
+        return unless permission_action.action == :vote &&
+                      permission_action.subject == :initiative &&
+                      user.blank?
+
+        toggle_allow(ephemeral_signature_workflow?)
+      end
+
       def authorized?(permission_action, resource: nil, permissions_holder: nil)
         return unless resource || permissions_holder
 
@@ -186,7 +195,7 @@ module Decidim
         initiative.votes_enabled? &&
           initiative.organization&.id == user.organization&.id &&
           initiative.votes.where(author: user).empty? &&
-          authorized?(:vote, resource: initiative, permissions_holder: initiative.type)
+          vote_authorized?
       end
 
       def can_user_support?(initiative)
@@ -234,6 +243,16 @@ module Decidim
 
       def authorship_or_admin?
         initiative&.has_authorship?(user) || user.admin?
+      end
+
+      def vote_authorized?
+        return true if user.ephemeral? && ephemeral_signature_workflow?
+
+        authorized?(:vote, resource: initiative, permissions_holder: initiative.type)
+      end
+
+      def ephemeral_signature_workflow?
+        initiative.type.signature_workflow_manifest&.ephemeral?
       end
     end
   end
