5e1d5b4867f8506c8eac4e42a60eb97c2882f169
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-02-10 14:17:12 +0100

    added the frozen string literal in proposal migration 202501211100014
    Signed-off-by: Alexandru Emil Lupu <contact@alecslupu.ro>

--- decidim-proposals/db/migrate/20250121110014_rename_proposal_valuation_assignments_to_evaluation_assignments.rb
+++ decidim-proposals/db/migrate/20250121110014_rename_proposal_valuation_assignments_to_evaluation_assignments.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameProposalValuationAssignmentsToEvaluationAssignments < ActiveRecord::Migration[7.0]
   def change
     rename_table :decidim_proposals_valuation_assignments, :decidim_proposals_evaluation_assignments
