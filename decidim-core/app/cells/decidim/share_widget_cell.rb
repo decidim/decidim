e82bb6acbef3dcd9b0b76569f8457f7bedab44d5
andra-panaite <135139066+andra-panaite@users.noreply.github.com>
2025-02-12 11:26:21 +0200

    Short link sharing (#13847)
    * Added short link on share modal
    
    * Fixed tests
    
    * Small fix
    
    * Insert short_link dependency
    
    * Revert helpers
    
    * Apply review recommendations
    
    * Fix pipeline
    
    * Rubocops
    
    * Apply review recommendation
    
    ---------
    
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>

---
+++ decidim-core/app/cells/decidim/share_widget_cell.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Decidim
+  class ShareWidgetCell < Decidim::ViewModel
+    include Decidim::ShortLinkHelper
+    include Decidim::SocialShareButtonHelper
+
+    def show
+      render
+    end
+
+    private
+
+    def resource_name
+      return "budget_project" if model.is_a?(Decidim::Budgets::Project)
+
+      model.class.name.demodulize.underscore
+    end
+  end
+end
