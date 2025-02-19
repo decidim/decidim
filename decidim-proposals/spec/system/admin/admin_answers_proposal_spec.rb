aa2486bf861af882241ee138d714bdc03da1e6d9
Hadrien Froger <hadrien@octree.ch>
2025-02-19 10:27:09 +0000

    fix: rubocop lints

--- decidim-proposals/spec/system/admin/admin_answers_proposal_spec.rb
+++ decidim-proposals/spec/system/admin/admin_answers_proposal_spec.rb
@@ -16,7 +16,7 @@ describe "Admin answers proposals" do
   end
 
   context "when proposals answering is enabled" do
-    before do 
+    before do
       component.update!(
         settings: { proposal_answering_enabled: true },
         step_settings: {
@@ -37,7 +37,7 @@ describe "Admin answers proposals" do
         "#proposal_answer-answer-tabs",
         en: "An accepted answer"
       )
-      find("button[name=commit]", match: :first).click
+      find("*[type=submit][name=commit]", match: :first).click
       expect(page).to have_css(".flash", text: "Proposal successfully answered.")
     end
 
@@ -52,11 +52,10 @@ describe "Admin answers proposals" do
           "#proposal_answer-answer-tabs",
           en: "An accepted answer"
         )
-        find("button[name=commit]", match: :first).click
+        find("*[type=submit][name=commit]", match: :first).click
         expect(find("label[for=proposal_answer_cost_report]")).to have_content("Required field")
         expect(find("label[for=proposal_answer_cost]")).to have_content("Required field")
         expect(page).to have_css(".flash", text: "There was a problem answering this proposal.")
-
       end
 
       it "rejects without any cost value or cost report" do
@@ -67,7 +66,7 @@ describe "Admin answers proposals" do
           "#proposal_answer-answer-tabs",
           en: "A Rejected answer"
         )
-        find("button[name=commit]", match: :first).click
+        find("*[type=submit][name=commit]", match: :first).click
         expect(page).to have_css(".flash", text: "Proposal successfully answered.")
       end
 
@@ -86,14 +85,9 @@ describe "Admin answers proposals" do
         )
         fill_in :proposal_answer_cost, with: "50"
 
-        find("button[name=commit]", match: :first).click
+        find("*[type=submit][name=commit]", match: :first).click
         expect(page).to have_css(".flash", text: "Proposal successfully answered.")
       end
     end
-
-    
   end
-
-
-
 end
