cd111fb8ae5c3e21cda84a5c9c73928eb446dd13
elviabth <ejbenedith@gmail.com>
2025-02-19 11:41:44 +0100

    add more checks to system test

--- decidim-collaborative_texts/spec/system/admin/admin_creates_document_spec.rb
+++ decidim-collaborative_texts/spec/system/admin/admin_creates_document_spec.rb
@@ -19,4 +19,18 @@ describe "Admin creates documents" do
     expect(page).to have_admin_callout "Document successfully created"
     expect(page).to have_css(".table-list tbody tr", count: 1)
   end
+
+  context "when title is invalid" do
+    before do
+      click_on "New text"
+
+      fill_in "Title", with: "this title is invalid"
+      click_on "Create"
+    end
+
+    it "displays an error message" do
+      expect(page).to have_admin_callout "There was a problem creating the document"
+      expect(page).to have_admin_callout "must start with a capital letter"
+    end
+  end
 end
