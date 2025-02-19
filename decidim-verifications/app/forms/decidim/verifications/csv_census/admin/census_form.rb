fd1bb0ed72dd113e90acd88109e2bcb32f02ded6
elviabth <ejbenedith@gmail.com>
2025-02-19 14:50:00 +0100

    remove validation

--- decidim-verifications/app/forms/decidim/verifications/csv_census/admin/census_form.rb
+++ decidim-verifications/app/forms/decidim/verifications/csv_census/admin/census_form.rb
@@ -6,24 +6,9 @@ module Decidim
       module Admin
         # A form to temporarily upload csv census data
         class CensusForm < Form
-          # include Decidim::HasUploadValidations
-
           attribute :email, String
 
           validates :email, presence: true, "valid_email_2/email": { disposable: true }
-          validate :unique_email
-
-          private
-
-          def unique_email
-            return true if Decidim::UserBaseEntity.where(
-              organization: context.current_organization,
-              email:
-            ).where.not(id: context.current_user.id).empty?
-
-            errors.add :email, :taken
-            false
-          end
         end
       end
     end
