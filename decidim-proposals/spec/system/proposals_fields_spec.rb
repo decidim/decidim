1788a36eef86c5efb13a3cfebbb5ab4e1ce4b72c
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-02-20 21:37:13 +0100

    Adapt proposals tests

--- decidim-proposals/spec/system/proposals_fields_spec.rb
+++ decidim-proposals/spec/system/proposals_fields_spec.rb
@@ -11,6 +11,7 @@ describe "Proposals" do
   let(:taxonomy_filter) { create(:taxonomy_filter, root_taxonomy:) }
   let!(:taxonomy_filter_item) { create(:taxonomy_filter_item, taxonomy_filter:, taxonomy_item: taxonomy) }
   let!(:user) { create(:user, :confirmed, organization:) }
+  let!(:user_group) { create(:user_group, :confirmed, :verified, organization:) }
   let(:taxonomy_filter_ids) { [taxonomy_filter.id] }
 
   let(:address) { "Some address" }
@@ -190,12 +191,10 @@ describe "Proposals" do
           end
         end
 
-        context "when the user has verified organizations" do
-          let(:user_group) { create(:user_group, :verified, organization:) }
-          let(:user_group_proposal_draft) { create(:proposal, :draft, users: [user], component:, title: "More sidewalks and less roads", body: "Cities need more people, not more cars") }
-
-          before do
-            create(:user_group_membership, user:, user_group:)
+        context "when the user is a verified organization" do
+          let!(:user) { user_group }
+          let(:user_group_proposal_draft) do
+            create(:proposal, :draft, users: [user], component:, title: "More sidewalks and less roads", body: "Cities need more people, not more cars")
           end
 
           it "creates a new proposal as a user group", :slow do
@@ -205,7 +204,6 @@ describe "Proposals" do
               fill_in :proposal_title, with: "More sidewalks and less roads"
               fill_in :proposal_body, with: "Cities need more people, not more cars"
               select decidim_sanitize_translated(taxonomy.name), from: "taxonomies-#{taxonomy_filter.id}"
-              select user_group.name, from: :proposal_user_group_id
 
               find("*[type=submit]").click
             end
@@ -241,7 +239,6 @@ describe "Proposals" do
                 fill_in :proposal_body, with: "Cities need more people, not more cars"
                 fill_in :proposal_address, with: address
                 select decidim_sanitize_translated(taxonomy.name), from: "taxonomies-#{taxonomy_filter.id}"
-                select user_group.name, from: :proposal_user_group_id
 
                 find("*[type=submit]").click
               end
@@ -312,7 +309,9 @@ describe "Proposals" do
                    participatory_space: participatory_process)
           end
 
-          let(:proposal_draft) { create(:proposal, :draft, users: [user], component:, title: "Proposal with attachments", body: "This is my proposal and I want to upload attachments.") }
+          let(:proposal_draft) do
+            create(:proposal, :draft, users: [user], component:, title: "Proposal with attachments", body: "This is my proposal and I want to upload attachments.")
+          end
 
           it "creates a new proposal with attachments" do
             visit edit_draft_proposal_path(component, proposal_draft)
@@ -422,7 +421,9 @@ describe "Proposals" do
                  participatory_space: participatory_process)
         end
 
-        let!(:proposal_first) { create(:proposal, users: [user], component:, title: "Creating my first and only proposal", body: "This is my only proposal's body and I am using it unwisely.") }
+        let!(:proposal_first) do
+          create(:proposal, users: [user], component:, title: "Creating my first and only proposal", body: "This is my only proposal's body and I am using it unwisely.")
+        end
 
         before do
           visit_component
