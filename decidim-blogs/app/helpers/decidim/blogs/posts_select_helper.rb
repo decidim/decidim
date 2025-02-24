25770caa4ccdb8195bee7ba876dd7c20c687ca11
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-02-24 13:53:14 +0100

    Change author select in blogs to remove user groups

--- decidim-blogs/app/helpers/decidim/blogs/posts_select_helper.rb
+++ decidim-blogs/app/helpers/decidim/blogs/posts_select_helper.rb
@@ -12,20 +12,11 @@ module Decidim
           [current_user.name, current_user.id]
         ]
 
-        select_options += user_groups
         select_options << [form.object.author.name, form.object.author.id] unless !form.object.author || select_options.pluck(1).include?(form.object.author.id)
 
         return form.select(name, select_options) if select_options.size > 1
 
         form.hidden_field(name, value: select_options.first[1])
-      end
-
-      private
-
-      def user_groups
-        return [] unless current_organization.user_groups_enabled?
-
-        Decidim::UserGroups::ManageableUserGroups.for(current_user).verified.map { |user_g| [user_g.name, user_g.id] }
       end
     end
   end
