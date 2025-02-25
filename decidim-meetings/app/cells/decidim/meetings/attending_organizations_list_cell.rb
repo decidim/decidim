e64d4186160277a792a227ff4012d9085c4baa1f
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-02-25 17:37:20 +0100

    Refactor participants scopes and cells

--- decidim-meetings/app/cells/decidim/meetings/attending_organizations_list_cell.rb
+++ decidim-meetings/app/cells/decidim/meetings/attending_organizations_list_cell.rb
@@ -13,19 +13,11 @@ module Decidim
     class AttendingOrganizationsListCell < PublicParticipantsListCell
       private
 
-      def user_group_ids
-        model.user_group_registrations.pluck(:decidim_user_id)
-      end
-
-      def user_groups
-        Decidim::User.where(id: user_group_ids)
-      end
-
       # Finds the public organizations (as user groups) of meeting
       #
       # Returns an Array of presented UserGroups
       def public_participants
-        @public_participants ||= user_groups.map { |user_group| present(user_group) }
+        @public_participants ||= model.public_user_groups.map { |user_group| present(user_group) }
       end
     end
   end
