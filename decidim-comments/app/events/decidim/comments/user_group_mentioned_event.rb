b9ee3e6666aefdb66cdaf5b662913d7a82794a21
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-02-27 15:47:58 +0100

    Remove UserGroupEvent and change notification

--- decidim-comments/app/events/decidim/comments/user_group_mentioned_event.rb
+++ decidim-comments/app/events/decidim/comments/user_group_mentioned_event.rb
@@ -4,7 +4,6 @@ module Decidim
   module Comments
     class UserGroupMentionedEvent < Decidim::Events::SimpleEvent
       include Decidim::Comments::CommentEvent
-      include Decidim::Events::UserGroupEvent
     end
   end
 end
