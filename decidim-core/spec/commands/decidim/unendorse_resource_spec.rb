075d7d4fe084528316626291f3826fc4ec0332d8
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-02-27 19:54:00 +0100

    Adapt endorsements commands specs

--- decidim-core/spec/commands/decidim/unendorse_resource_spec.rb
+++ decidim-core/spec/commands/decidim/unendorse_resource_spec.rb
@@ -32,7 +32,7 @@ module Decidim
 
     describe "user group unendorses resource" do
       let(:endorsement) { create(:user_group_endorsement) }
-      let(:command) { described_class.new(endorsement.resource, endorsement.author, endorsement.user_group) }
+      let(:command) { described_class.new(endorsement.resource, endorsement.author) }
 
       it "broadcasts ok" do
         expect(endorsement).to be_valid
@@ -53,36 +53,6 @@ module Decidim
         resource = endorsement.resource
         resource.reload
         expect(resource.endorsements_count).to be_zero
-      end
-
-      context "when it is from other user" do
-        let(:other_user) { create(:user, organization: endorsement.resource.organization) }
-        let(:command) { described_class.new(endorsement.resource, other_user, endorsement.user_group) }
-
-        before do
-          create(:user_group_membership, user: other_user, user_group: endorsement.user_group, role: "admin")
-        end
-
-        it "broadcasts ok" do
-          expect(endorsement).to be_valid
-          expect { command.call }.to broadcast :ok
-        end
-
-        it "removes the endorsement" do
-          expect(endorsement).to be_valid
-          expect do
-            command.call
-          end.to change(Endorsement, :count).by(-1)
-        end
-
-        it "do not decreases the endorsement counter by one" do
-          expect(endorsement).to be_valid
-          command.call
-
-          resource = endorsement.resource
-          resource.reload
-          expect(resource.endorsements_count).to be_zero
-        end
       end
     end
   end
