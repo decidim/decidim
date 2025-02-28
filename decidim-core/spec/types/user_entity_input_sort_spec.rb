cd0d480953d46ed11aa8bf170a85c3684fe16c3b
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-02-28 13:21:34 +0100

    Remove test about users type sorting

--- decidim-core/spec/types/user_entity_input_sort_spec.rb
+++ decidim-core/spec/types/user_entity_input_sort_spec.rb
@@ -27,26 +27,6 @@ module Decidim
       context "when sorting by user nickname" do
         include_examples "collection has input sort", "users", "nickname"
       end
-
-      context "when sorting by user type" do
-        describe "ASC" do
-          let(:query) { %[{ users(order: { type: "ASC" }) { id } }] }
-
-          it "returns alphabetical order" do
-            expect(response["users"].first["id"]).to eq(user.id.to_s)
-            expect(response["users"].last["id"]).to eq(user_group.id.to_s)
-          end
-        end
-
-        describe "DESC" do
-          let(:query) { %[{ users(order: { type: "DESC" }) { id } }] }
-
-          it "returns revered alphabetical order" do
-            expect(response["users"].first["id"]).to eq(user_group.id.to_s)
-            expect(response["users"].last["id"]).to eq(user.id.to_s)
-          end
-        end
-      end
     end
   end
 end
