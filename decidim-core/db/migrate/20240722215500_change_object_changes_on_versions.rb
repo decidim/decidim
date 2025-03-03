5b97e724ddeb3c01b29a060c3bf2aa9dde7ca96a
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-03-03 17:38:21 +0200

    Apply review recommendation

--- decidim-core/db/migrate/20240722215500_change_object_changes_on_versions.rb
+++ decidim-core/db/migrate/20240722215500_change_object_changes_on_versions.rb
@@ -5,15 +5,14 @@ class ChangeObjectChangesOnVersions < ActiveRecord::Migration[6.1]
     rename_column :versions, :object_changes, :old_object_changes
     add_column :versions, :object_changes, :jsonb # or :json
 
+    PaperTrail::Version.reset_column_information
+
     PaperTrail::Version.where.not(old_object_changes: nil).find_each do |version|
       # we set a time interval to ensure that deployment has succeeded
       Decidim::Migrate::PaperTrailJob.set(wait: 10.minutes).perform_later(version.id)
     rescue NameError
       Rails.logger.info "Skipping History of #{version.item_type} with id #{version.item_id}"
     end
-
-    PaperTrail::Version.reset_column_information
-    remove_column :versions, :old_object_changes
   end
 
   def down
