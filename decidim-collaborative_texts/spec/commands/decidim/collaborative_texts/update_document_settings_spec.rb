4a83d2ebba550c39e43facd5ed779e54d204ecca
elviabth <ejbenedith@gmail.com>
2025-03-04 13:25:11 +0100

    fix lint errors

--- decidim-collaborative_texts/spec/commands/decidim/collaborative_texts/update_document_settings_spec.rb
+++ decidim-collaborative_texts/spec/commands/decidim/collaborative_texts/update_document_settings_spec.rb
@@ -5,15 +5,6 @@ require "spec_helper"
 module Decidim
   module CollaborativeTexts
     module Admin
-      describe UpdateDocumentSettings, type: :command do
-        let(:organization) { create(:organization, available_locales: [:en]) }
-        let(:document) { create(:collaborative_text_document, published_at: nil, title: "This is and original document test title") }
-        let(:current_user) { create(:user, :admin, :confirmed, organization:) }
-
-        # TODO: check updating a document updates a new document with the correct :accepting_suggestions and :announcement
-        # todo: check updating a document ignores title and body
-        # todo: check traceability (new entry for document only)
-      end
     end
   end
 end
