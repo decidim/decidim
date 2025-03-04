f75c19c12e4ad04c6f4c507d3bc8aaef31c853dc
robertluca-publicis <robert.luca@publicissapient.com>
2025-03-04 18:15:33 +0200

    Fix proposal state when importing from another component
    * fix proposal state when importing from another component
    
    * add batch size
    
    * update fix proposals rake and spec
    
    * use proposal_state instead of state when copying
    
    * change spe context description
    
    * Apply review recommendations
    
    * Fix specs
    
    ---------
    
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>

--- decidim-proposals/app/jobs/decidim/proposals/admin/import_proposals_job.rb
+++ decidim-proposals/app/jobs/decidim/proposals/admin/import_proposals_job.rb
@@ -76,7 +76,7 @@ module Decidim
         def proposal_answer_attributes(original_proposal)
           return {} unless @form["keep_answers"]
 
-          state = Decidim::Proposals::ProposalState.where(component: target_component, token: original_proposal.state).first
+          state = Decidim::Proposals::ProposalState.where(component: target_component, token: original_proposal.proposal_state&.token).first
 
           {
             answer: original_proposal.answer,
