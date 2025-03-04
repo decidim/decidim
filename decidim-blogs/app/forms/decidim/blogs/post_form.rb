8bb3db0c7bb14a95d4fc6d4ca8b70d23bcb6876e
Màxim Colls <collsmaxim@gmail.com>
2025-03-04 17:04:11 +0100

    Add taxonomies and attachments to posts (#14085)
    * Add taxonomies to blog posts
    
    * Add attachments in blog posts form in frontend
    
    * Fix notification visibility after creating post
    
    * Fix permissions creating and managing posts
    
    * Add spec creating post with attachment
    
    * Fix specs
    
    * Fix specs
    
    * Improve taxonom presenter code
    
    * Add setting in Blog component to allow/disallow attachments
    
    * Fix attachment specs
    
    * Fix spec
    
    * Fix spec
    
    * Fix spec

--- decidim-blogs/app/forms/decidim/blogs/post_form.rb
+++ decidim-blogs/app/forms/decidim/blogs/post_form.rb
@@ -5,22 +5,30 @@ module Decidim
     # This class holds a Form to update pages from Decidim's admin panel.
     class PostForm < Decidim::Form
       include TranslatableAttributes
+      include Decidim::HasTaxonomyFormAttributes
+      include Decidim::AttachmentAttributes
+      include Decidim::HasUploadValidations
 
       translatable_attribute :title, String
       translatable_attribute :body, Decidim::Attributes::RichText
 
       attribute :decidim_author_id, Integer
 
+      attachments_attribute :documents
+
       validates :body, translatable_presence: true
       validates :title, translatable_presence: true
 
       validate :can_set_author
+
+      alias component current_component
 
       def map_model(model)
         presenter = PostPresenter.new(model)
 
         self.title = presenter.title
         self.body = presenter.body
+        self.documents = model.attachments
       end
 
       def author
@@ -46,6 +54,10 @@ module Decidim
 
       def user_groups
         @user_groups ||= Decidim::UserGroups::ManageableUserGroups.for(current_user).verified
+      end
+
+      def participatory_space_manifest
+        @participatory_space_manifest ||= current_component.participatory_space.manifest.name
       end
     end
   end
