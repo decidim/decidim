0335ef08ddb96ff444b4992dfd658c76bd6f5ca1
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-03-11 17:06:28 +0100

    Define registrations attendees management from admin panel

---
+++ decidim-meetings/app/presenters/decidim/meetings/registration_presenter.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Meetings
+    #
+    # Decorator for meeting registrations
+    #
+    class RegistrationPresenter < SimpleDelegator
+      delegate :name, :email, to: :user
+
+      def status
+        return I18n.t("attended", scope: "decidim.meetings.models.registration.status") if attendee?
+
+        I18n.t("not_attended", scope: "decidim.meetings.models.registration.status")
+      end
+
+      def status_html_class
+        return "success" if attendee?
+
+        "alert"
+      end
+
+      def attendee?
+        @attendee ||= validated?
+      end
+    end
+  end
+end
