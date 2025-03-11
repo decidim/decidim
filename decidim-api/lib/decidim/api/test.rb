ef59e786516e38c4bc1e51920736b51558181c4d
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2025-03-11 14:34:13 +0200

    Fix FactoryBot error

--- decidim-api/lib/decidim/api/test.rb
+++ decidim-api/lib/decidim/api/test.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
-Dir["#{__dir__}/test/**/*.rb"].each { |f| require f }
+Dir["#{__dir__}/test/**/*.rb"]
+  .reject { |f| f == "#{__dir__}/test/factories.rb" }
+  .each { |f| require f }
