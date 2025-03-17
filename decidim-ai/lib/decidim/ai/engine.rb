5b47990196950dfc7d5e1c69b28ec7c842502b15
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-03-17 12:03:36 +0200

    Add delay of  queue
    * Add delay of spam_analysis queue
    
    * Apply suggestions from code review

--- decidim-ai/lib/decidim/ai/engine.rb
+++ decidim-ai/lib/decidim/ai/engine.rb
@@ -22,7 +22,9 @@ module Decidim
       initializer "decidim_ai.events.subscribe_profile" do
         config.to_prepare do
           Decidim::EventsManager.subscribe("decidim.update_account:after") do |_event_name, data|
-            Decidim::Ai::SpamDetection::UserSpamAnalyzerJob.perform_later(data[:resource])
+            Decidim::Ai::SpamDetection::UserSpamAnalyzerJob
+              .set(wait: Decidim::Ai::SpamDetection.spam_detection_delay)
+              .perform_later(data[:resource])
           end
           Decidim::EventsManager.subscribe("decidim.update_user_group:after") do |_event_name, data|
             Decidim::Ai::SpamDetection::UserSpamAnalyzerJob.perform_later(data[:resource])
@@ -36,10 +38,16 @@ module Decidim
       initializer "decidim_ai.events.subscribe_comments" do
         config.to_prepare do
           ActiveSupport::Notifications.subscribe("decidim.comments.create_comment:after") do |_event_name, data|
-            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob.perform_later(data[:resource], data.dig(:extra, :event_author), data.dig(:extra, :locale), [:body])
+            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob
+              .set(wait: Decidim::Ai::SpamDetection.spam_detection_delay)
+              .perform_later(data[:resource],
+                             data.dig(:extra, :event_author), data.dig(:extra, :locale), [:body])
           end
           ActiveSupport::Notifications.subscribe("decidim.comments.update_comment:after") do |_event_name, data|
-            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob.perform_later(data[:resource], data.dig(:extra, :event_author), data.dig(:extra, :locale), [:body])
+            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob
+              .set(wait: Decidim::Ai::SpamDetection.spam_detection_delay)
+              .perform_later(data[:resource],
+                             data.dig(:extra, :event_author), data.dig(:extra, :locale), [:body])
           end
         end
       end
@@ -47,12 +55,16 @@ module Decidim
       initializer "decidim_ai.events.subscribe_meeting" do
         config.to_prepare do
           ActiveSupport::Notifications.subscribe("decidim.meetings.create_meeting:after") do |_event_name, data|
-            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob.perform_later(data[:resource], data.dig(:extra, :event_author), data.dig(:extra, :locale),
-                                                                             [:description, :title, :location_hints, :registration_terms])
+            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob
+              .set(wait: Decidim::Ai::SpamDetection.spam_detection_delay)
+              .perform_later(data[:resource], data.dig(:extra, :event_author), data.dig(:extra, :locale),
+                             [:description, :title, :location_hints, :registration_terms])
           end
           ActiveSupport::Notifications.subscribe("decidim.meetings.update_meeting:after") do |_event_name, data|
-            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob.perform_later(data[:resource], data.dig(:extra, :event_author), data.dig(:extra, :locale),
-                                                                             [:description, :title, :location_hints, :registration_terms])
+            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob
+              .set(wait: Decidim::Ai::SpamDetection.spam_detection_delay)
+              .perform_later(data[:resource], data.dig(:extra, :event_author), data.dig(:extra, :locale),
+                             [:description, :title, :location_hints, :registration_terms])
           end
         end
       end
@@ -60,10 +72,16 @@ module Decidim
       initializer "decidim_ai.events.subscribe_debate" do
         config.to_prepare do
           ActiveSupport::Notifications.subscribe("decidim.debates.create_debate:after") do |_event_name, data|
-            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob.perform_later(data[:resource], data.dig(:extra, :event_author), data.dig(:extra, :locale), [:description, :title])
+            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob
+              .set(wait: Decidim::Ai::SpamDetection.spam_detection_delay)
+              .perform_later(data[:resource],
+                             data.dig(:extra, :event_author), data.dig(:extra, :locale), [:description, :title])
           end
           ActiveSupport::Notifications.subscribe("decidim.debates.update_debate:after") do |_event_name, data|
-            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob.perform_later(data[:resource], data.dig(:extra, :event_author), data.dig(:extra, :locale), [:description, :title])
+            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob
+              .set(wait: Decidim::Ai::SpamDetection.spam_detection_delay)
+              .perform_later(data[:resource],
+                             data.dig(:extra, :event_author), data.dig(:extra, :locale), [:description, :title])
           end
         end
       end
@@ -71,10 +89,16 @@ module Decidim
       initializer "decidim_ai.events.subscribe_initiatives" do
         config.to_prepare do
           ActiveSupport::Notifications.subscribe("decidim.initiatives.create_initiative:after") do |_event_name, data|
-            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob.perform_later(data[:resource], data.dig(:extra, :event_author), data.dig(:extra, :locale), [:description, :title])
+            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob
+              .set(wait: Decidim::Ai::SpamDetection.spam_detection_delay)
+              .perform_later(data[:resource],
+                             data.dig(:extra, :event_author), data.dig(:extra, :locale), [:description, :title])
           end
           ActiveSupport::Notifications.subscribe("decidim.initiatives.update_initiative:after") do |_event_name, data|
-            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob.perform_later(data[:resource], data.dig(:extra, :event_author), data.dig(:extra, :locale), [:description, :title])
+            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob
+              .set(wait: Decidim::Ai::SpamDetection.spam_detection_delay)
+              .perform_later(data[:resource],
+                             data.dig(:extra, :event_author), data.dig(:extra, :locale), [:description, :title])
           end
         end
       end
@@ -82,16 +106,28 @@ module Decidim
       initializer "decidim_ai.events.subscribe_proposals" do
         config.to_prepare do
           ActiveSupport::Notifications.subscribe("decidim.proposals.create_proposal:after") do |_event_name, data|
-            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob.perform_later(data[:resource], data.dig(:extra, :event_author), data.dig(:extra, :locale), [:body, :title])
+            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob
+              .set(wait: Decidim::Ai::SpamDetection.spam_detection_delay)
+              .perform_later(data[:resource],
+                             data.dig(:extra, :event_author), data.dig(:extra, :locale), [:body, :title])
           end
           ActiveSupport::Notifications.subscribe("decidim.proposals.update_proposal:after") do |_event_name, data|
-            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob.perform_later(data[:resource], data.dig(:extra, :event_author), data.dig(:extra, :locale), [:body, :title])
+            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob
+              .set(wait: Decidim::Ai::SpamDetection.spam_detection_delay)
+              .perform_later(data[:resource],
+                             data.dig(:extra, :event_author), data.dig(:extra, :locale), [:body, :title])
           end
           ActiveSupport::Notifications.subscribe("decidim.proposals.create_collaborative_draft:after") do |_event_name, data|
-            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob.perform_later(data[:resource], data.dig(:extra, :event_author), data.dig(:extra, :locale), [:body, :title])
+            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob
+              .set(wait: Decidim::Ai::SpamDetection.spam_detection_delay)
+              .perform_later(data[:resource],
+                             data.dig(:extra, :event_author), data.dig(:extra, :locale), [:body, :title])
           end
           ActiveSupport::Notifications.subscribe("decidim.proposals.update_collaborative_draft:after") do |_event_name, data|
-            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob.perform_later(data[:resource], data.dig(:extra, :event_author), data.dig(:extra, :locale), [:body, :title])
+            Decidim::Ai::SpamDetection::GenericSpamAnalyzerJob
+              .set(wait: Decidim::Ai::SpamDetection.spam_detection_delay)
+              .perform_later(data[:resource],
+                             data.dig(:extra, :event_author), data.dig(:extra, :locale), [:body, :title])
           end
         end
       end
