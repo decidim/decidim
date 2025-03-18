07e674e5fa753913ebb657bf403c0cad91871753
Maxim Colls <collsmaxim@gmail.com>
2025-03-18 14:42:18 +0100

    Address feedback

--- decidim-core/app/cells/decidim/share_text_widget_cell.rb
+++ decidim-core/app/cells/decidim/share_text_widget_cell.rb
@@ -13,11 +13,15 @@ module Decidim
     end
 
     def title
-      t(:share_text, scope: "decidim.budgets.order.status", space_name:)
+      t(:share_text, scope: "decidim.budgets.order.status", space_name:, component_url:)
     end
 
     def space_name
       translated_attribute(resource.participatory_space.title)
+    end
+
+    def component_url
+      Decidim::ResourceLocatorPresenter.new(resource).url
     end
   end
 end
