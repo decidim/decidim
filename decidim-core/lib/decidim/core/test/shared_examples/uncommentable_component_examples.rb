5e547da521e4b99cfa3dd89ee949c09777621622
Laura Jaime <laura.jv@coditramuntana.com>
2025-03-19 08:07:09 +0100

    Filter searches when results are uncommentable

--- decidim-core/lib/decidim/core/test/shared_examples/uncommentable_component_examples.rb
+++ decidim-core/lib/decidim/core/test/shared_examples/uncommentable_component_examples.rb
@@ -18,4 +18,20 @@ shared_examples "an uncommentable component" do
       expect(page).to have_no_link(resource_locator(resource).path)
     end
   end
+
+  describe "when search a comment in the global search" do
+    let(:comment) { create :comment }
+
+    it "does not displays the comment" do
+      component.update!(settings: { comments_enabled: false })
+      visit decidim.root_path
+
+      within ".main-bar__search" do
+        fill_in "term", with: comment.body["en"]
+        find("input#input-search").native.send_keys :enter
+      end
+
+      expect(page).to have_content("0 Results for the search")
+    end
+  end
 end
