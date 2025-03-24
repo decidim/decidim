7ec3a1275e1b02672e04025da2bc775aa4bec4df
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2025-03-24 14:29:07 +0200

    Make last reviews changes

--- decidim-core/db/migrate/20250221085317_add_user_type_to_action_logs.rb
+++ decidim-core/db/migrate/20250221085317_add_user_type_to_action_logs.rb
@@ -1,22 +1,12 @@
 # frozen_string_literal: true
 
 class AddUserTypeToActionLogs < ActiveRecord::Migration[7.0]
-  class ActionLog < ApplicationRecord
-    self.table_name = :decidim_action_logs
-  end
-
-  def up
+  def change
     add_column :decidim_action_logs, :user_type, :string, default: "Decidim::User", null: false
 
     rename_column :decidim_action_logs, :decidim_user_id, :user_id
     add_index :decidim_action_logs,
               [:user_id, :user_type],
               name: "index_decidim_action_log_on_users"
-  end
-
-  def down
-    remove_index :decidim_action_logs, name: "index_decidim_action_log_on_users"
-    rename_column :decidim_action_logs, :user_id, :decidim_user_id
-    remove_column :decidim_action_logs, :user_type
   end
 end
