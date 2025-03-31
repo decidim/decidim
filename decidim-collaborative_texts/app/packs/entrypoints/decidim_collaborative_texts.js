d3cec2c1ec2ecd8c7049e8dee39cf1e2c3d75859
Ivan Vergés <ivan@pokecode.net>
2025-03-31 14:06:53 +0200

    remove empty files

--- decidim-collaborative_texts/app/packs/entrypoints/decidim_collaborative_texts.js
+++ decidim-collaborative_texts/app/packs/entrypoints/decidim_collaborative_texts.js
@@ -1,7 +1,2 @@
-import "src/decidim/collaborative_texts/documents"
-
 // Images
 require.context("../images", true)
-
-// CSS
-// import "stylesheets/decidim/collaborative_texts/collaborative_texts.scss"
