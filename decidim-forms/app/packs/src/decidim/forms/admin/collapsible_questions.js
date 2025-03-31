a8bd9fb2769e33d1d9fc73aec976e68e8700f34f
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-03-31 13:33:30 +0200

    Fix eslint offenses

--- decidim-forms/app/packs/src/decidim/forms/admin/collapsible_questions.js
+++ decidim-forms/app/packs/src/decidim/forms/admin/collapsible_questions.js
@@ -10,12 +10,12 @@
   }, 100);
 
   document.querySelector("button.collapse-all")?.addEventListener("click", () => {
-    document.querySelectorAll("[id$=field] button.question--collapse[aria-expanded='true']")
-      .forEach(button => button.click());
+    document.querySelectorAll("[id$=field] button.question--collapse[aria-expanded='true']").
+      forEach((button) => button.click());
   });
 
   document.querySelector("button.expand-all")?.addEventListener("click", () => {
-    document.querySelectorAll("[id$=field] button.question--collapse[aria-expanded='false']")
-      .forEach(button => button.click());
+    document.querySelectorAll("[id$=field] button.question--collapse[aria-expanded='false']").
+      forEach((button) => button.click());
   });
 })();
