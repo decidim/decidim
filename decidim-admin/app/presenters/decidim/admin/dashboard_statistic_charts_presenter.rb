d66bafa67a05fa7217b11124cc5521b1a7f73a4e
elviabth <ejbenedith@gmail.com>
2025-04-03 11:37:44 +0200

    refactor highlighted method

--- decidim-admin/app/presenters/decidim/admin/dashboard_statistic_charts_presenter.rb
+++ decidim-admin/app/presenters/decidim/admin/dashboard_statistic_charts_presenter.rb
@@ -11,7 +11,15 @@ module Decidim
         high_priority_stats = collection(priority: StatsRegistry::HIGH_PRIORITY)
         medium_priority_stats = collection(priority: StatsRegistry::MEDIUM_PRIORITY)
 
-        (high_priority_stats + medium_priority_stats).uniq
+        filtered_high_priority = high_priority_stats.reject { |stat| duplicated_stat?(stat) }
+
+        (filtered_high_priority + medium_priority_stats).uniq
+      end
+
+      private
+
+      def duplicated_stat?(stat)
+        [:proposals_count, :meetings_count].include?(stat[:name].to_sym)
       end
     end
   end
