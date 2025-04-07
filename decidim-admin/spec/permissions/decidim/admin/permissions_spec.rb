12dd4fad06cfb8a9039ce33ff4d922ec73617862
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-04-07 13:50:33 +0200

    removed metrics block, not present in develop

--- decidim-admin/spec/permissions/decidim/admin/permissions_spec.rb
+++ decidim-admin/spec/permissions/decidim/admin/permissions_spec.rb
@@ -118,18 +118,6 @@ describe Decidim::Admin::Permissions do
     end
   end
 
-  describe "metrics" do
-    let(:action_subject) { :metrics }
-
-    it_behaves_like "permission is not set"
-
-    context "when reading" do
-      let(:action_name) { :read }
-
-      it { is_expected.to be true }
-    end
-  end
-
   describe "editor image upload" do
     let(:action_subject) { :editor_image }
     let(:action_name) { :create }
