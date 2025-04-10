b8836854e318fb301ceca2322d5902a95e9510a1
Ivan Vergés <ivan@pokecode.net>
2025-04-10 11:09:57 +0200

    remove setting

--- decidim-collaborative_texts/app/commands/decidim/collaborative_texts/admin/update_document_settings.rb
+++ decidim-collaborative_texts/app/commands/decidim/collaborative_texts/admin/update_document_settings.rb
@@ -6,7 +6,7 @@ module Decidim
       # This command is executed when the user changes a Document from the admin
       # panel.
       class UpdateDocumentSettings < Decidim::Commands::UpdateResource
-        fetch_form_attributes :accepting_suggestions, :announcement
+        fetch_form_attributes :announcement
       end
     end
   end
