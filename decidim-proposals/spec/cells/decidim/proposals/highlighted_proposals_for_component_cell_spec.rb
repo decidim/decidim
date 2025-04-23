eedbf675f11c24da399c170dc20a41645bdad034
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-04-23 20:12:27 +0300

    Remove the label

--- decidim-proposals/spec/cells/decidim/proposals/highlighted_proposals_for_component_cell_spec.rb
+++ decidim-proposals/spec/cells/decidim/proposals/highlighted_proposals_for_component_cell_spec.rb
@@ -24,8 +24,6 @@ describe Decidim::Proposals::HighlightedProposalsForComponentCell, type: :cell d
 
     it "renders nothing" do
       expect(subject).to have_no_content("A nice title")
-      expect(subject).to have_no_content("Last proposals")
-      expect(subject).to have_no_content("Random")
       expect(subject).to have_no_css(".card__list-title", count: 1)
     end
   end
@@ -35,8 +33,6 @@ describe Decidim::Proposals::HighlightedProposalsForComponentCell, type: :cell d
 
     it "renders the proposals" do
       expect(subject).to have_no_content("A nice title")
-      expect(subject).to have_no_content("Last proposals")
-      expect(subject).to have_no_content("Random")
       expect(subject).to have_no_css(".card__list-title", count: 1)
     end
   end
@@ -46,8 +42,6 @@ describe Decidim::Proposals::HighlightedProposalsForComponentCell, type: :cell d
 
     it "renders the proposals" do
       expect(subject).to have_no_content("A nice title")
-      expect(subject).to have_no_content("Last proposals")
-      expect(subject).to have_no_content("Random")
       expect(subject).to have_no_css(".card__list-title", count: 1)
     end
   end
@@ -58,7 +52,6 @@ describe Decidim::Proposals::HighlightedProposalsForComponentCell, type: :cell d
     it "renders the proposals in random" do
       expect(subject).to have_content("A nice title")
       expect(subject).to have_content("Another nice title")
-      expect(subject).to have_content("Random")
       expect(subject).to have_css(".card__list-title", count: 2)
     end
 
@@ -68,8 +61,6 @@ describe Decidim::Proposals::HighlightedProposalsForComponentCell, type: :cell d
       it "renders the proposals" do
         expect(subject).to have_content("A nice title")
         expect(subject).to have_content("Another nice title")
-        expect(subject).to have_content("Last proposals")
-        expect(subject).to have_no_content("Random")
         expect(subject).to have_css(".card__list-title", count: 2)
       end
     end
@@ -80,18 +71,7 @@ describe Decidim::Proposals::HighlightedProposalsForComponentCell, type: :cell d
     let!(:proposal) { create(:proposal, :with_votes, title: { en: "A nice title" }, component: model) }
 
     it "renders the proposals" do
-      expect(subject).to have_content("A nice title")
-      expect(subject).to have_content("Random")
       expect(subject).to have_css(".card__list-title", count: 1)
-    end
-
-    context "when order is set to most recent" do
-      let!(:proposal2) { create(:proposal, title: { en: "Another nice title" }, component: model, created_at: 1.minute.ago) }
-      let(:options) { { order: "recent" } }
-
-      it "renders the proposals" do
-        expect(subject).to have_content("Last proposals")
-      end
     end
   end
 end
