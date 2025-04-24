250482a90f8b0abde9202c6b15da5c5f277388cd
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-04-24 13:26:11 +0200

    further refactoring of meetings

--- decidim-meetings/app/permissions/decidim/meetings/permissions.rb
+++ decidim-meetings/app/permissions/decidim/meetings/permissions.rb
@@ -8,8 +8,8 @@ module Decidim
         return Decidim::Meetings::Admin::Permissions.new(user, permission_action, context).permissions if permission_action.scope == :admin
         return permission_action if permission_action.scope != :public
 
-        if permission_action.subject == :meeting && permission_action.action == :read
-          toggle_allow(user_has_any_role?(user, meeting.participatory_space, broad_check: true) || (!meeting&.hidden? && meeting&.current_user_can_visit_meeting?(user)))
+        if subject == :meeting && permission_action.action == :read
+          toggle_allow(!meeting&.hidden? && meeting&.current_user_can_visit_meeting?(user))
           return permission_action
         end
 
