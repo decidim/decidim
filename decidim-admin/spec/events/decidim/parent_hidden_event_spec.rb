6548aa5a6c8d9a4141c45f09eb0db2a4a65978dd
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-04-24 13:14:37 +0300

    Fix notification

--- decidim-admin/spec/events/decidim/parent_hidden_event_spec.rb
+++ decidim-admin/spec/events/decidim/parent_hidden_event_spec.rb
@@ -13,13 +13,11 @@ module Decidim
     let(:resource_text) { "<i>#{decidim_sanitize(translated(resource.body), strip_tags: true)}</i>" }
     let(:event_name) { "decidim.events.reports.parent_hidden" }
     let(:extra) { { report_reasons: ["spam"] } }
-    let(:notification_title) { "A comment you have created was hidden because its parent resource was also hidden</br>\n#{resource_text}" }
     let(:email_subject) { "Your comment is no longer visible" }
     let(:email_outro) { "You have received this notification because you are an author of the affected comment." }
-    let(:email_intro) { "A comment you have created was hidden because its parent resource was also hidden." }
+    let(:email_intro) { "#{email_subject}.<br>\nThis happened because the proposal, meeting, debate or comment you replied to has been moderated. If it becomes available again, your comment will be automatically restored." }
 
     it_behaves_like "a simple event email"
-    it_behaves_like "a simple event notification"
 
     describe "resource_text" do
       it "is generated correctly" do
