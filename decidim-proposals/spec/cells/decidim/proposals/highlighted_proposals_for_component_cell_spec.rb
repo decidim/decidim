095516d21b2f62a11f683127660e86cec70751dc
robertluca-publicis <robert.luca@publicissapient.com>
2025-04-24 14:16:27 +0300

    Fix label on proposals landing page block
    * fix label on proposals landing page block
    
    * add spec
    
    * Add extra spec
    
    * Remove the label
    
    * Remove the new button
    
    * Update decidim-proposals/config/locales/en.yml
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-proposals/spec/cells/decidim/proposals/highlighted_proposals_for_component_cell_spec.rb
+++ decidim-proposals/spec/cells/decidim/proposals/highlighted_proposals_for_component_cell_spec.rb
@@ -7,12 +7,13 @@ describe Decidim::Proposals::HighlightedProposalsForComponentCell, type: :cell d
 
   subject { my_cell.call }
 
-  let(:my_cell) { cell("decidim/proposals/highlighted_proposals_for_component", model) }
+  let(:options) { { order: "random" } }
+  let(:my_cell) { cell("decidim/proposals/highlighted_proposals_for_component", model, options) }
   let!(:official_proposal) { create(:proposal, :official) }
   let!(:user_proposal) { create(:proposal) }
   let!(:current_user) { create(:user, :confirmed, organization: model.participatory_space.organization) }
   let(:model) { create(:proposal_component) }
-  let!(:proposal) { create(:proposal, title: { en: "A nice title" }, component: model) }
+  let!(:proposal) { create(:proposal, title: { en: "A nice title" }, component: model, created_at: 2.hours.ago) }
 
   before do
     allow(controller).to receive(:current_user).and_return(current_user)
@@ -23,7 +24,24 @@ describe Decidim::Proposals::HighlightedProposalsForComponentCell, type: :cell d
 
     it "renders nothing" do
       expect(subject).to have_no_content("A nice title")
-      expect(subject).to have_no_content("Last proposals")
+      expect(subject).to have_no_css(".card__list-title", count: 1)
+    end
+  end
+
+  context "when no published proposals" do
+    let!(:proposal) { create(:proposal, :unpublished, title: { en: "A nice title" }, component: model) }
+
+    it "renders the proposals" do
+      expect(subject).to have_no_content("A nice title")
+      expect(subject).to have_no_css(".card__list-title", count: 1)
+    end
+  end
+
+  context "when proposals are hidden" do
+    let!(:proposal) { create(:proposal, :hidden, title: { en: "A nice title" }, component: model) }
+
+    it "renders the proposals" do
+      expect(subject).to have_no_content("A nice title")
       expect(subject).to have_no_css(".card__list-title", count: 1)
     end
   end
@@ -31,11 +49,20 @@ describe Decidim::Proposals::HighlightedProposalsForComponentCell, type: :cell d
   context "with proposals" do
     let!(:proposal2) { create(:proposal, title: { en: "Another nice title" }, component: model) }
 
-    it "renders the proposals" do
+    it "renders the proposals in random" do
       expect(subject).to have_content("A nice title")
       expect(subject).to have_content("Another nice title")
-      expect(subject).to have_content("Last proposals")
       expect(subject).to have_css(".card__list-title", count: 2)
+    end
+
+    context "when order is set to most recent" do
+      let(:options) { { order: "recent" } }
+
+      it "renders the proposals" do
+        expect(subject).to have_content("A nice title")
+        expect(subject).to have_content("Another nice title")
+        expect(subject).to have_css(".card__list-title", count: 2)
+      end
     end
   end
 end
