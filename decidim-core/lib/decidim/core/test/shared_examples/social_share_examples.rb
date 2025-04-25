91a173b8a3d39673eea954a82d2f4e682cc660d2
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-04-25 12:01:08 +0300

    Fix codeclimate

--- decidim-core/lib/decidim/core/test/shared_examples/social_share_examples.rb
+++ decidim-core/lib/decidim/core/test/shared_examples/social_share_examples.rb
@@ -64,7 +64,7 @@ shared_examples "a social share via QR code" do
     it_behaves_like "a 404 page" do
       let(:target_path) { decidim.qr_path(resource: "Missing") }
     end
-  end 
+  end
 
   it "has the QR code" do
     visit_resource
