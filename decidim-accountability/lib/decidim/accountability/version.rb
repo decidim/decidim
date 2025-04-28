98579d6c6b76de924c9ba5666ffff24aba32d710
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-04-28 10:20:48 +0200

    Prepare 0.28.6 release

--- decidim-accountability/lib/decidim/accountability/version.rb
+++ decidim-accountability/lib/decidim/accountability/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds decidim-accountability version.
   module Accountability
     def self.version
-      "0.28.5"
+      "0.28.6"
     end
   end
 end
--- decidim-debates/lib/decidim/debates/version.rb
+++ decidim-debates/lib/decidim/debates/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-debates version.
   module Debates
     def self.version
-      "0.28.5"
+      "0.28.6"
     end
   end
 end
