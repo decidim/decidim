7dc02f03b499ba7d670dba0c9eb5017ae8bf1539
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-04-28 11:41:29 +0200

    Prepare 0.30.0 release

--- decidim-comments/lib/decidim/comments/version.rb
+++ decidim-comments/lib/decidim/comments/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-comments version.
   module Comments
     def self.version
-      "0.30.0.rc3"
+      "0.30.0"
     end
   end
 end
--- decidim-forms/lib/decidim/forms/version.rb
+++ decidim-forms/lib/decidim/forms/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-forms version.
   module Forms
     def self.version
-      "0.30.0.rc3"
+      "0.30.0"
     end
   end
 end
