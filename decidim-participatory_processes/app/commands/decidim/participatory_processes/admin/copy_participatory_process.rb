07881948b23296d1fdbecbca8f32754e70b26630
stephanierousset <61418966+Stef-Rousset@users.noreply.github.com>
2025-04-30 02:33:19 +0200

    Fix copy blocks when duplicating assemblies or spaces
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>

--- decidim-participatory_processes/app/commands/decidim/participatory_processes/admin/copy_participatory_process.rb
+++ decidim-participatory_processes/app/commands/decidim/participatory_processes/admin/copy_participatory_process.rb
@@ -32,6 +32,7 @@ module Decidim
               copy_participatory_process_steps if @form.copy_steps?
               copy_participatory_process_categories if @form.copy_categories?
               copy_participatory_process_components if @form.copy_components?
+              copy_landing_page_blocks if @form.copy_landing_page_blocks?
             end
           end
 
@@ -127,6 +128,39 @@ module Decidim
         def map_step_settings(step_settings)
           step_settings.each_with_object({}) do |(step_id, settings), acc|
             acc.update(@steps_relationship[step_id.to_s] => settings)
+          end
+        end
+
+        def copy_landing_page_blocks
+          blocks = Decidim::ContentBlock.where(scoped_resource_id: @participatory_process.id, scope_name: "participatory_process_homepage",
+                                               organization: @participatory_process.organization)
+          return if blocks.blank?
+
+          blocks.each do |block|
+            new_block = Decidim::ContentBlock.create!(
+              organization: @copied_process.organization,
+              scope_name: "participatory_process_homepage",
+              scoped_resource_id: @copied_process.id,
+              manifest_name: block.manifest_name,
+              settings: block.settings,
+              weight: block.weight,
+              published_at: block.published_at.present? ? @copied_process.created_at : nil # determine if block is active/inactive
+            )
+            copy_block_attachments(block, new_block) if block.attachments.present?
+          end
+        end
+
+        def copy_block_attachments(block, new_block)
+          block.attachments.map(&:name).each do |name|
+            original_image = block.images_container.send(name).blob
+            next if original_image.blank?
+
+            new_block.images_container.send("#{name}=", ActiveStorage::Blob.create_and_upload!(
+                                                          io: StringIO.new(original_image.download),
+                                                          filename: "image.png",
+                                                          content_type: block.images_container.background_image.blob.content_type
+                                                        ))
+            new_block.save!
           end
         end
       end
