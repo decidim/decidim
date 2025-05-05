75829213b5e8424cf6bc6d044ec9c972e32db6dc
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-05-05 17:28:45 +0200

    removed rolechecker from default permissions

--- decidim-core/app/permissions/decidim/default_permissions.rb
+++ decidim-core/app/permissions/decidim/default_permissions.rb
@@ -5,8 +5,6 @@ module Decidim
   # actions by any kind of user. Also works as a default implementation so other
   # components can inherit from it and get some convenience methods.
   class DefaultPermissions
-    include Decidim::UserRoleChecker
-
     def initialize(user, permission_action, context = {})
       @user = user
       @permission_action = permission_action
