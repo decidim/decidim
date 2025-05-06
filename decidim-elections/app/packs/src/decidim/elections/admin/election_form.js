b8154cbb1f55317800e71e36306cee1fa5c6972d
Anna Topalidi <t.opalidisanna@gmail.com>
2025-05-06 20:50:38 +0200

    npm lint

--- decidim-elections/app/packs/src/decidim/elections/admin/election_form.js
+++ decidim-elections/app/packs/src/decidim/elections/admin/election_form.js
@@ -2,10 +2,9 @@ $(() => {
   const $manualStart = $("#election_manual_start");
   const $datepickerRow = $(".election_start_time");
 
-  console.log("Checkbox found:", $manualStart.length > 0);
-  console.log("Datepicker row found:", $datepickerRow.length > 0);
-
-  if ($manualStart.length === 0 || $datepickerRow.length === 0) return;
+  if ($manualStart.length === 0 || $datepickerRow.length === 0) {
+    return;
+  }
 
   const toggleVisibility = () => {
     if ($manualStart.is(":checked")) {
