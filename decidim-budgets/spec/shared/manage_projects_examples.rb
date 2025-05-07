2390684e58aec8374c3bc9d5c0aa11fe022ebb2c
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-05-07 18:03:20 +0200

    reverted example in manage projects to added and_call_original method

--- decidim-budgets/spec/shared/manage_projects_examples.rb
+++ decidim-budgets/spec/shared/manage_projects_examples.rb
@@ -63,6 +63,19 @@ shared_examples "manage projects" do
         end
       end
     end
+
+    context "when proposal linking is disabled" do
+      before do
+        allow(Decidim).to receive(:module_installed?).and_call_original
+
+        # Reload the page with the updated settings
+        visit current_path
+      end
+
+      it "does not display the proposal picker" do
+        expect(page).to have_no_content "Choose proposals"
+      end
+    end
   end
 
   it "updates a project" do
