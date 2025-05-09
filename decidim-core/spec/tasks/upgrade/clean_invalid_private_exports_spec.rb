f3dc6dceb5d1f5cb684764f1dcd72e037b8e3855
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-05-09 13:42:00 +0300

    Update decidim-core/spec/tasks/upgrade/clean_invalid_private_exports_spec.rb

--- decidim-core/spec/tasks/upgrade/clean_invalid_private_exports_spec.rb
+++ decidim-core/spec/tasks/upgrade/clean_invalid_private_exports_spec.rb
@@ -21,7 +21,7 @@ describe "rake decidim:upgrade:clean:invalid_private_exports", type: :task do
 
   context "when there are errored entries" do
     let!(:exports) { create_list(:private_export, 5) }
-    let!(:invalid_entries) { create(:private_export, export_type: "survey_user_responses_88") }
+    let!(:invalid_entries) { create(:private_export, export_type: "survey_user_responses_88ca03fab3b71024a7d97639bc10d62b771d69ae2c89d748055762e6b41d3e4d") }
 
     it "removes the entries" do
       expect { task.execute }.to change(Decidim::PrivateExport, :count).by(-1)
