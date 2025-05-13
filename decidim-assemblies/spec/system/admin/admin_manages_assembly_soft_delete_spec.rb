bf4586b0ec7b655a7c728f662e7318f995258875
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-05-13 13:37:22 +0200

    added test for assemblies. fixed structure of index

--- decidim-assemblies/spec/system/admin/admin_manages_assembly_soft_delete_spec.rb
+++ decidim-assemblies/spec/system/admin/admin_manages_assembly_soft_delete_spec.rb
@@ -12,4 +12,26 @@ describe "Admin manages assembly soft delete" do
 
   it_behaves_like "manage soft deletable component or space", "assembly"
   it_behaves_like "manage trashed resource", "assembly"
+
+  context "when a user is collaborator" do
+    let!(:assembly) { create(:assembly, organization: organization) }
+    let!(:collaborator_user) { create(:user, :admin_terms_accepted, :confirmed, organization: organization) }
+    let!(:collaborator_role) do
+      create(:assembly_user_role,
+             user: collaborator_user,
+             assembly: assembly,
+             role: :collaborator)
+    end
+
+    before do
+      switch_to_host(organization.host)
+      login_as collaborator_user, scope: :user
+      visit admin_resource_path
+    end
+
+    it "doesn't allow collaborators to view deleted assemblies" do
+      expect(page).to have_content("Assemblies")
+      expect(page).to have_no_content("View deleted assemblies")
+    end
+  end
 end
