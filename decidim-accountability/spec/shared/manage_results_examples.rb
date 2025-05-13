dca78d779c64cfa54e88f315913934e3fa903fae
Tom Greenwood <101816158+greenwoodt@users.noreply.github.com>
2025-05-13 11:49:29 +0200

    Update decidim-accountability/spec/shared/manage_results_examples.rb
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-accountability/spec/shared/manage_results_examples.rb
+++ decidim-accountability/spec/shared/manage_results_examples.rb
@@ -11,7 +11,7 @@ shared_examples "manage results" do
 
   context "when the proposal module is installed" do
     before do
-      allow(Decidim).to receive(:module_installed?).and_call_original
+      allow(Decidim).to receive(:module_installed?).and_return(false)
 
       # Reload the page with the updated settings
       visit current_path
