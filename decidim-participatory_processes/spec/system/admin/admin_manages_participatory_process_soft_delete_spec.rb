85ea7c3de4552b2aead5c265d1a7f44ac3b300de
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-05-16 13:57:52 +0200

    processes trash resources test added for other roles

--- decidim-participatory_processes/spec/system/admin/admin_manages_participatory_process_soft_delete_spec.rb
+++ decidim-participatory_processes/spec/system/admin/admin_manages_participatory_process_soft_delete_spec.rb
@@ -31,7 +31,73 @@ describe "Admin manages participatory process soft delete" do
 
     it "does not allow collaborators to view deleted processes" do
       expect(page).to have_content("Processes")
-      expect(page).to have_no_content("View deleted processes")
+      expect(page).to have_no_link("View deleted processes", href: /.*processes.*trash.*/)
+    end
+  end
+
+  context "when a user is evaluator" do
+    let!(:participatory_process) { create(:participatory_process, organization: organization) }
+    let!(:evaluator_user) { create(:user, :admin_terms_accepted, :confirmed, organization: organization) }
+    let!(:evaluator_role) do
+      create(:participatory_process_user_role,
+             user: evaluator_user,
+             participatory_process: participatory_process,
+             role: :evaluator)
+    end
+
+    before do
+      switch_to_host(organization.host)
+      login_as evaluator_user, scope: :user
+      visit admin_resource_path
+    end
+
+    it "does not allow evaluators to view deleted processes" do
+      expect(page).to have_content("Processes")
+      expect(page).to have_no_link("View deleted processes", href: /.*processes.*trash.*/)
+    end
+  end
+
+  context "when a user is moderator" do
+    let!(:participatory_process) { create(:participatory_process, organization: organization) }
+    let!(:moderator_user) { create(:user, :admin_terms_accepted, :confirmed, organization: organization) }
+    let!(:moderator_role) do
+      create(:participatory_process_user_role,
+             user: moderator_user,
+             participatory_process: participatory_process,
+             role: :moderator)
+    end
+
+    before do
+      switch_to_host(organization.host)
+      login_as moderator_user, scope: :user
+      visit admin_resource_path
+    end
+
+    it "does not allow moderators to view deleted processes" do
+      expect(page).to have_content("Processes")
+      expect(page).to have_no_link("View deleted processes", href: /.*processes.*trash.*/)
+    end
+  end
+
+  context "when a user is a space admin" do
+    let!(:participatory_process) { create(:participatory_process, organization: organization) }
+    let!(:admin_user) { create(:user, :admin_terms_accepted, :confirmed, organization: organization) }
+    let!(:admin_role) do
+      create(:participatory_process_user_role,
+             user: admin_user,
+             participatory_process: participatory_process,
+             role: :admin)
+    end
+
+    before do
+      switch_to_host(organization.host)
+      login_as admin_user, scope: :user
+      visit admin_resource_path
+    end
+
+    it "does not allow space admins to view deleted processes" do
+      expect(page).to have_content("Processes")
+      expect(page).to have_no_link("View deleted processes", href: /.*processes.*trash.*/)
     end
   end
 end
