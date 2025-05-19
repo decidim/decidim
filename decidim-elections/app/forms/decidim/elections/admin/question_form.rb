ff901ec4248ad057dc05aa7a0261c53c6f955f14
Anna Topalidi <t.opalidisanna@gmail.com>
2025-05-19 12:46:25 +0200

    draggble

--- decidim-elections/app/forms/decidim/elections/admin/question_form.rb
+++ decidim-elections/app/forms/decidim/elections/admin/question_form.rb
@@ -19,6 +19,8 @@ module Decidim
 
         validates :body, translatable_presence: true
         validates :position, numericality: { greater_than_or_equal_to: 0 }
+        validates :question_type, inclusion: { in: Decidim::Elections::Question::QUESTION_TYPES }, if: :editable?
+        validates :response_options, presence: true, if: :editable?
 
         def election
           @election ||= context[:election]
