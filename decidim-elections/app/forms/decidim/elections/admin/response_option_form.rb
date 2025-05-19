f33929c2fb394d1e7db7e5754114ce9ad56b9052
Anna Topalidi <t.opalidisanna@gmail.com>
2025-05-19 15:18:00 +0200

    add forms spec

--- decidim-elections/app/forms/decidim/elections/admin/response_option_form.rb
+++ decidim-elections/app/forms/decidim/elections/admin/response_option_form.rb
@@ -4,8 +4,6 @@ module Decidim
   module Elections
     module Admin
       class ResponseOptionForm < Decidim::Form
-        mimic :answer
-
         include TranslatableAttributes
 
         attribute :deleted, Boolean, default: false
@@ -13,14 +11,6 @@ module Decidim
         translatable_attribute :body, String
 
         validates :body, translatable_presence: true, unless: :deleted
-
-        def election
-          @election ||= context[:election]
-        end
-
-        def question
-          @question ||= context[:question]
-        end
 
         def to_param
           return id if id.present?
