c90e5890187b3c37bce8204a7db33710a96e0f3c
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-05-20 12:12:43 +0200

    removed shared example spec checking proposal hashtags

--- decidim-proposals/spec/shared/proposal_form_examples.rb
+++ decidim-proposals/spec/shared/proposal_form_examples.rb
@@ -233,52 +233,6 @@ shared_examples "a proposal form" do |options|
       end
     end
   end
-
-  describe "#extra_hashtags" do
-    subject { form.extra_hashtags }
-
-    let(:component) do
-      create(
-        :proposal_component,
-        :with_extra_hashtags,
-        participatory_space:,
-        suggested_hashtags: component_suggested_hashtags,
-        automatic_hashtags: component_automatic_hashtags
-      )
-    end
-    let(:component_automatic_hashtags) { "" }
-    let(:component_suggested_hashtags) { "" }
-
-    it { is_expected.to eq([]) }
-
-    context "when there are auto hashtags" do
-      let(:component_automatic_hashtags) { "HashtagAuto1 HashtagAuto2" }
-
-      it { is_expected.to eq(%w(HashtagAuto1 HashtagAuto2)) }
-    end
-
-    context "when there are some suggested hashtags checked" do
-      let(:component_suggested_hashtags) { "HashtagSuggested1 HashtagSuggested2 HashtagSuggested3" }
-      let(:suggested_hashtags) { %w(HashtagSuggested1 HashtagSuggested2) }
-
-      it { is_expected.to eq(%w(HashtagSuggested1 HashtagSuggested2)) }
-    end
-
-    context "when there are invalid suggested hashtags checked" do
-      let(:component_suggested_hashtags) { "HashtagSuggested1 HashtagSuggested2" }
-      let(:suggested_hashtags) { %w(HashtagSuggested1 HashtagSuggested3) }
-
-      it { is_expected.to eq(%w(HashtagSuggested1)) }
-    end
-
-    context "when there are both suggested and auto hashtags" do
-      let(:component_automatic_hashtags) { "HashtagAuto1 HashtagAuto2" }
-      let(:component_suggested_hashtags) { "HashtagSuggested1 HashtagSuggested2" }
-      let(:suggested_hashtags) { %w(HashtagSuggested2) }
-
-      it { is_expected.to eq(%w(HashtagAuto1 HashtagAuto2 HashtagSuggested2)) }
-    end
-  end
 end
 
 shared_examples "a proposal form with meeting as author" do |options|
