a881b093b50664eee33bc181be1d1c12999faf64
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-05-22 21:22:44 +0300

    Fix specs for wrap_parameters_by_default

--- decidim-core/config/initializers/new_framework_defaults_7_0.rb
+++ decidim-core/config/initializers/new_framework_defaults_7_0.rb
@@ -76,7 +76,7 @@ Rails.application.config.action_mailer.smtp_timeout = 5
 # Enable parameter wrapping for JSON.
 # Previously this was set in an initializer. It is fine to keep using that initializer if you have customized it.
 # To disable parameter wrapping entirely, set this config to `false`.
-# Rails.application.config.action_controller.wrap_parameters_by_default = true
+Rails.application.config.action_controller.wrap_parameters_by_default = true
 
 # Specifies whether generated namespaced UUIDs follow the RFC 4122 standard for namespace IDs provided as a
 # `String` to `Digest::UUID.uuid_v3` or `Digest::UUID.uuid_v5` method calls.
