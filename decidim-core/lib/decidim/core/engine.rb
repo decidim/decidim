c971cc86208e5a2b7b51ddf52a54afb271b0cb9a
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-05-29 23:31:32 +0300

    Fix: NoMethodError: undefined method `filter_map' for nil

--- decidim-core/lib/decidim/core/engine.rb
+++ decidim-core/lib/decidim/core/engine.rb
@@ -239,6 +239,21 @@ module Decidim
         app.config.active_storage.variant_processor = :mini_magick
       end
 
+      initializer "decidim_core.active_storage_method_patch" do |_app|
+        if Rails::VERSION::MAJOR < 8
+          # This is a manual bugfix of https://github.com/rails/rails/pull/51931
+          module Attachment
+            def named_variants
+              record.attachment_reflections[name]&.named_variants || {}
+            end
+          end
+
+          ActiveSupport.on_load(:active_storage_attachment) { prepend Attachment }
+        else
+          ActiveSupport::Deprecation.warn("Remove decidim_core.active_storage_method_patch initializer from #{__FILE__}")
+        end
+      end
+
       initializer "decidim_core.action_controller" do |_app|
         config.to_prepare do
           ActiveSupport.on_load :action_controller do
