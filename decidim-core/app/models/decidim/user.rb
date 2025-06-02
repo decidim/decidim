0dad0085c74b094c398a0e339bc9b9c126cfe2ee
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-06-02 22:01:16 +0300

    Running rubocops

--- decidim-core/app/models/decidim/user.rb
+++ decidim-core/app/models/decidim/user.rb
@@ -58,7 +58,7 @@ module Decidim
     scope :org_admins_except_me, ->(user) { where(organization: user.organization, admin: true).where.not(id: user.id) }
 
     scope :ephemeral, -> { where("extended_data @> ?", { ephemeral: true }.to_json) }
-    
+
     scope :with_inactivity_notification, lambda {
       where("extended_data ? 'inactivity_notification'")
     }
