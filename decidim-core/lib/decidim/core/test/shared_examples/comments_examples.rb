9692752d852272fd5c1e06142cdd70f34f4cc447
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-06-03 13:55:03 +0300

    Attempt to fix the Comments specs

--- decidim-core/lib/decidim/core/test/shared_examples/comments_examples.rb
+++ decidim-core/lib/decidim/core/test/shared_examples/comments_examples.rb
@@ -192,9 +192,9 @@ shared_examples "comments" do
       end
 
       it "shows a modal with the comment form" do
-        sleep 1
+        expect(page).to have_content("Add comment")
         click_on "Add comment"
-        sleep 1
+        
         expect(page).to have_content("Add comment")
         expect(page).to have_content("1000 characters left")
         expect(page).to have_css(".add-comment form")
