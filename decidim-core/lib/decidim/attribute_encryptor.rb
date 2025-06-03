7aad2167ba6f61f2bb2d1f4010188c81191ff50e
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-06-03 18:09:26 +0300

    Patch Decidim field encryptors

--- decidim-core/lib/decidim/attribute_encryptor.rb
+++ decidim-core/lib/decidim/attribute_encryptor.rb
@@ -2,11 +2,20 @@
 
 module Decidim
   class AttributeEncryptor
-    def self.encrypt(string)
-      cryptor.encrypt_and_sign(string) if string.present?
+    attr_reader :secret, :hash_digest_class
+
+    def initialize(secret: "attribute", hash_digest_class: OpenSSL::Digest::SHA1)
+      @secret = secret
+      @hash_digest_class = hash_digest_class
     end
 
-    def self.decrypt(string_encrypted)
+    def encrypt(string)
+      return if string.blank?
+
+      encryptor.encrypt_and_sign(string)
+    end
+
+    def decrypt(string_encrypted)
       return if string_encrypted.blank?
 
       # `ActiveSupport::MessageEncryptor` expects all values passed to the
@@ -15,16 +24,31 @@ module Decidim
       # If something else is passed, just return the value as is.
       return string_encrypted unless string_encrypted.is_a?(String)
 
-      cryptor.decrypt_and_verify(string_encrypted)
+      encryptor.decrypt_and_verify(string_encrypted)
+    end
+
+    def self.encrypt(string)
+      cryptor.encrypt(string)
+    end
+
+    def self.decrypt(string_encrypted)
+      cryptor.decrypt(string_encrypted)
     end
 
     def self.cryptor
-      @cryptor ||= begin
-        key = ActiveSupport::KeyGenerator.new("attribute", hash_digest_class: OpenSSL::Digest::SHA1).generate_key(
-          Rails.application.secret_key_base, ActiveSupport::MessageEncryptor.key_len
-        )
-        ActiveSupport::MessageEncryptor.new(key)
-      end
+      @cryptor ||= new(secret: "attribute")
+    end
+
+    private
+
+    def encryptor
+      @encryptor ||= ActiveSupport::MessageEncryptor.new(key)
+    end
+
+    def key
+      @key ||= ActiveSupport::KeyGenerator.new(secret, hash_digest_class:).generate_key(
+        Rails.application.secret_key_base, ActiveSupport::MessageEncryptor.key_len
+      )
     end
   end
 end
--- decidim-initiatives/app/services/decidim/initiatives/data_encryptor.rb
+++ decidim-initiatives/app/services/decidim/initiatives/data_encryptor.rb
@@ -3,23 +3,9 @@
 module Decidim
   module Initiatives
     # Service to encrypt and decrypt metadata
-    class DataEncryptor
-      attr_reader :secret
-
+    class DataEncryptor < Decidim::AttributeEncryptor
       def initialize(args = {})
-        @secret = args.fetch(:secret) || "default"
-        @key = ActiveSupport::KeyGenerator.new(secret, hash_digest_class: OpenSSL::Digest::SHA1).generate_key(
-          Rails.application.secret_key_base, ActiveSupport::MessageEncryptor.key_len
-        )
-        @encryptor = ActiveSupport::MessageEncryptor.new(@key)
-      end
-
-      def encrypt(data)
-        @encryptor.encrypt_and_sign(data)
-      end
-
-      def decrypt(encrypted_data)
-        @encryptor.decrypt_and_verify(encrypted_data)
+        super(secret: args.fetch(:secret, "default"))
       end
     end
   end
