fd6de9f7eccb5a09f28263d191e550f1d371f93a
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-06-04 00:37:45 +0300

    Fix description text

--- decidim-proposals/lib/decidim/api/proposal_state_type.rb
+++ decidim-proposals/lib/decidim/api/proposal_state_type.rb
@@ -8,7 +8,7 @@ module Decidim
       field :announcement_title, Decidim::Core::TranslatedFieldType, "The announcement for this proposal state", null: true
       field :bg_color, GraphQL::Types::String, description: "The background color of proposal state label", null: true
       field :id, GraphQL::Types::ID, "The id of the proposal state", null: false
-      field :proposals_count, GraphQL::Types::Int, "The announcement for this proposal state", null: true
+      field :proposals_count, GraphQL::Types::Int, "The number of proposals having this state", null: true
       field :text_color, GraphQL::Types::String, description: "The text color of this proposal state label", null: true
       field :title, Decidim::Core::TranslatedFieldType, "The title for this proposal state", null: true
     end
