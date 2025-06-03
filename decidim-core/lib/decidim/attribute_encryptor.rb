c51c456461f42b29b8d42f45b2db66d8c1137646
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-06-03 22:01:52 +0300

    Refactor arguments of Decidim::AttributeEncryptor

--- decidim-core/lib/decidim/attribute_encryptor.rb
+++ decidim-core/lib/decidim/attribute_encryptor.rb
@@ -4,7 +4,7 @@ module Decidim
   class AttributeEncryptor
     attr_reader :secret, :hash_digest_class, :secret_key_base, :key_len
 
-    def initialize(secret: "attribute", options: {})
+    def initialize(secret: "attribute", **options)
       @secret = secret
       @hash_digest_class = options.fetch(:hash_digest_class, Rails.application.config.active_support.hash_digest_class)
       @secret_key_base = options.fetch(:secret_key_base, Rails.application.secret_key_base)
@@ -27,9 +27,9 @@ module Decidim
       return string_encrypted unless string_encrypted.is_a?(String)
 
       encryptor.decrypt_and_verify(string_encrypted)
-    rescue ActiveSupport::MessageVerifier::InvalidSignature
-      # Since we have migrated from SHA1 to SHA256, we need to ensure that eny forgotten string is still being decodable
-      new(secret:, hash_digest_class: OpenSSL::Digest::SHA1).decrypt(string_encrypted)
+    rescue ActiveSupport::MessageEncryptor::InvalidMessage
+      # Since we have migrated from SHA1 to SHA256, we need to ensure that eny encrypted string not migrated is still being decrypted successfully.
+      self.class.new(secret:, hash_digest_class: OpenSSL::Digest::SHA1).decrypt(string_encrypted)
     end
 
     def self.encrypt(string)
