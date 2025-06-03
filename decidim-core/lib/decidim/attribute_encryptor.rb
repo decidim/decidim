9138e2cdd76f419a0586630bdf5c6277b9f8b6e3
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-06-03 21:09:27 +0300

    Refactor Decidim::AttributeEncryptor

--- decidim-core/lib/decidim/attribute_encryptor.rb
+++ decidim-core/lib/decidim/attribute_encryptor.rb
@@ -2,11 +2,13 @@
 
 module Decidim
   class AttributeEncryptor
-    attr_reader :secret, :hash_digest_class
+    attr_reader :secret, :hash_digest_class, :secret_key_base, :key_len
 
-    def initialize(secret: "attribute", hash_digest_class: OpenSSL::Digest::SHA1)
+    def initialize(secret: "attribute", options: {})
       @secret = secret
-      @hash_digest_class = hash_digest_class
+      @hash_digest_class = options.fetch(:hash_digest_class, Rails.application.config.active_support.hash_digest_class)
+      @secret_key_base = options.fetch(:secret_key_base, Rails.application.secret_key_base)
+      @key_len = options.fetch(:key_len, ActiveSupport::MessageEncryptor.key_len)
     end
 
     def encrypt(string)
@@ -25,6 +27,9 @@ module Decidim
       return string_encrypted unless string_encrypted.is_a?(String)
 
       encryptor.decrypt_and_verify(string_encrypted)
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+      # Since we have migrated from SHA1 to SHA256, we need to ensure that eny forgotten string is still being decodable
+      new(secret:, hash_digest_class: OpenSSL::Digest::SHA1).decrypt(string_encrypted)
     end
 
     def self.encrypt(string)
@@ -46,9 +51,11 @@ module Decidim
     end
 
     def key
-      @key ||= ActiveSupport::KeyGenerator.new(secret, hash_digest_class:).generate_key(
-        Rails.application.secret_key_base, ActiveSupport::MessageEncryptor.key_len
-      )
+      @key ||= key_generator.generate_key(secret_key_base, key_len)
+    end
+
+    def key_generator
+      @key_generator ||= ActiveSupport::KeyGenerator.new(secret, hash_digest_class:)
     end
   end
 end
