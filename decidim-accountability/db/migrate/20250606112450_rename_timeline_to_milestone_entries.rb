ad9b36337d88a4a9e82a659c49d00dd19451b1e7
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-06-06 13:44:39 +0200

    migration renaming of time to milestone in accountablity

---
+++ decidim-accountability/db/migrate/20250606112450_rename_timeline_to_milestone_entries.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class RenameTimelineToMilestoneEntries < ActiveRecord::Migration[7.0]
+  def change
+    def up
+      rename_table :decidim_accountability_timeline_entries, :decidim_accountability_milestone_entries
+
+      rename_index :decidim_accountability_milestone_entries,
+                   :index_decidim_accountability_timeline_entries_on_results_id,
+                   :index_decidim_accountability_milestone_entries_on_results_id
+
+      rename_index :decidim_accountability_milestone_entries,
+                   :index_decidim_accountability_timeline_entries_on_entry_date,
+                   :index_decidim_accountability_milestone_entries_on_entry_date
+    end
+
+    def down
+      rename_index :decidim_accountability_milestone_entries,
+                   :index_decidim_accountability_milestone_entries_on_results_id,
+                   :index_decidim_accountability_timeline_entries_on_results_id
+
+      rename_index :decidim_accountability_milestone_entries,
+                   :index_decidim_accountability_milestone_entries_on_entry_date,
+                   :index_decidim_accountability_timeline_entries_on_entry_date
+
+      rename_table :decidim_accountability_milestone_entries, :decidim_accountability_timeline_entries
+    end
+  end
+end
