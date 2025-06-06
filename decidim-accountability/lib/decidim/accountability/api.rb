6f380d0bd97ef06cbdda47861f636aeaf7db814a
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-06-06 14:10:22 +0200

    api path miss-spelling

--- decidim-accountability/lib/decidim/accountability/api.rb
+++ decidim-accountability/lib/decidim/accountability/api.rb
@@ -5,6 +5,6 @@ module Decidim
     autoload :AccountabilityType, "decidim/api/accountability_type"
     autoload :ResultType, "decidim/api/result_type"
     autoload :StatusType, "decidim/api/status_type"
-    autoload :MilestoneEntryType, "decidim/api/milestone_type"
+    autoload :MilestoneEntryType, "decidim/api/milestone_entry_type"
   end
 end
