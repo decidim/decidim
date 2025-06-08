e79834ceaf65c068da2c17bb19a908526da60040
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-06-08 23:25:44 +0300

    Fix deprecator warn

--- decidim-initiatives/app/services/decidim/initiatives/data_encryptor.rb
+++ decidim-initiatives/app/services/decidim/initiatives/data_encryptor.rb
@@ -5,7 +5,7 @@ module Decidim
     # Service to encrypt and decrypt metadata
     class DataEncryptor < Decidim::AttributeEncryptor
       def initialize(secret: "default", **)
-        ActiveSupport::Deprecation.warn("Decidim::Initiatives::DataEncryptor is deprecated, and we will remove it in Decidim 0.33. Please use Decidim::AttributeEncryptor instead.")
+        Decidim.deprecator.warn("Decidim::Initiatives::DataEncryptor is deprecated, and we will remove it in Decidim 0.33. Please use Decidim::AttributeEncryptor instead.")
         super(secret: secret || "default", **)
       end
     end
