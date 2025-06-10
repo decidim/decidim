209d2a9a28f88c3792c7ae9bfb3c7d8ab6505d9e
Tom Greenwood <101816158+greenwoodt@users.noreply.github.com>
2025-06-10 12:50:31 +0200

    Update decidim-participatory_processes/db/migrate/20250605114415_remove_hashtag_from_participatory_processes_groups.rb
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-participatory_processes/db/migrate/20250605114415_remove_hashtag_from_participatory_processes_groups.rb
+++ decidim-participatory_processes/db/migrate/20250605114415_remove_hashtag_from_participatory_processes_groups.rb
@@ -2,6 +2,6 @@
 
 class RemoveHashtagFromParticipatoryProcessesGroups < ActiveRecord::Migration[7.1]
   def change
-    remove_column :decidim_participatory_process_groups, :hashtag, :string
+    remove_column :decidim_participatory_process_groups, :hashtag, :string, if_exists: true
   end
 end
