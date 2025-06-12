db905c39387b63a1a30da7642ee2cd89577c25bf
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-06-12 10:06:37 +0200

    Fix exception when cell does not have the settings defined

--- decidim-core/app/cells/decidim/content_blocks/highlighted_content_banner_cell.rb
+++ decidim-core/app/cells/decidim/content_blocks/highlighted_content_banner_cell.rb
@@ -4,6 +4,8 @@ module Decidim
   module ContentBlocks
     class HighlightedContentBannerCell < Decidim::ViewModel
       def show
+        return unless render?
+
         render
       end
 
@@ -25,6 +27,13 @@ module Decidim
 
       def background_image
         model.images_container.attached_uploader(:background_image).variant_url(:big)
+      end
+
+      private
+
+      def render?
+        required_keys = %i[title short_description action_button_title action_button_subtitle action_button_url]
+        required_keys.all? { |key| model.settings.public_send(key).present? }
       end
     end
   end
