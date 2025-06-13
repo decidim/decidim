6e16acb21dd3cb3356cf1050473fa30944beaad5
stephanie rousset <strabon@club-internet.fr>
2025-06-13 09:29:24 +0200

    refactor: arrow function in dropdown menu

--- decidim-core/app/packs/src/decidim/dropdown_menu.js
+++ decidim-core/app/packs/src/decidim/dropdown_menu.js
@@ -1,6 +1,6 @@
 // changes the value "menu" of role attribute set by a11y on div dropdown-menu-account and
 // dropdown-menu-account-mobile which are inappropriate for accessibility
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", () =>  {
   setTimeout(() => {
     document.querySelector("#dropdown-menu-account").setAttribute("role", "dialog")
     document.querySelector("#dropdown-menu-account-mobile").setAttribute("role", "dialog")
