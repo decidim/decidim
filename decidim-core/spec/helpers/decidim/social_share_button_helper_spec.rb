e7f12f5b8c0bfe108ef2dcd4499554415abe3dc0
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-06-13 16:55:24 +0200

    fixed social share button spec

--- decidim-core/spec/helpers/decidim/social_share_button_helper_spec.rb
+++ decidim-core/spec/helpers/decidim/social_share_button_helper_spec.rb
@@ -69,7 +69,7 @@ module Decidim
 
         it "renders the correct HTML" do
           expect(result).to include("Share to X")
-          expect(result).to include("https://twitter.com/intent/tweet?url=http%3A%2F%2Fexample.org&amp;text=Hello&amp;via=Decidim")
+          expect(result).to include("https://twitter.com/intent/tweet?url=http%3A%2F%2Fexample.org&amp;text=Hello")
           expect(result).to include(".svg")
         end
       end
