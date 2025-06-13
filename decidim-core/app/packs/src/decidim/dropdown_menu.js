66dc1c7988dcb4d29d151cf4022a003a2d50245c
stephanie rousset <strabon@club-internet.fr>
2025-06-13 10:12:11 +0200

    refactor: add condition in dropdown_menu js file

--- decidim-core/app/packs/src/decidim/dropdown_menu.js
+++ decidim-core/app/packs/src/decidim/dropdown_menu.js
@@ -1,8 +1,12 @@
 // changes the value "menu" of role attribute set by a11y on div dropdown-menu-account and
 // dropdown-menu-account-mobile which are inappropriate for accessibility
-document.addEventListener("DOMContentLoaded", () =>  {
-  setTimeout(() => {
-    document.querySelector("#dropdown-menu-account").setAttribute("role", "dialog")
-    document.querySelector("#dropdown-menu-account-mobile").setAttribute("role", "dialog")
-  }, 300)
+document.addEventListener("DOMContentLoaded", () => {
+  const dropdownDiv = document.querySelector("#dropdown-menu-account");
+  const dropdownMobileDiv = document.querySelector("#dropdown-menu-account-mobile");
+  if (dropdownDiv) {
+    setTimeout(() => {
+      dropdownDiv.setAttribute("role", "dialog")
+      dropdownMobileDiv.setAttribute("role", "dialog")
+    }, 300)
+  }
 });
