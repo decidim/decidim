035c396d4b37fc0d753405f545cbc718237b1978
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-06-13 13:17:58 +0200

    reverting changes to mentions in editor_context test

--- decidim-dev/lib/decidim/dev/test/rspec_support/editor_context.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/editor_context.rb
@@ -4,6 +4,19 @@ shared_context "with editor content containing mentions" do
   let(:user) { create(:user, :confirmed, organization:) }
   let(:user2) { create(:user, :confirmed, organization:) }
 
+  let(:html) do
+    <<~HTML
+      <p>Paragraph with some information</p>
+      <p>Paragraph with a user mention #{user.to_global_id} and another user mention #{user2.to_global_id}</p>
+    HTML
+  end
+  let(:editor_html) do
+    <<~HTML
+      <p>Paragraph with some information</p>
+      <p>Paragraph with a user mention #{html_mention(user)} and another user mention #{html_mention(user2)}</p>
+    HTML
+  end
+
   def html_mention(mentionable)
     mention = "@#{mentionable.nickname}"
     label = "#{mention} (#{CGI.escapeHTML(mentionable.name)})"
