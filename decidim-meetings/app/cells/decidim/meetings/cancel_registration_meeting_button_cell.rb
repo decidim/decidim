14dc568b92081e599dc8b22b2432ea47ea0e9cf1
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-06-14 09:59:59 +0300

    Fix i18n-tasks errors

--- decidim-meetings/app/cells/decidim/meetings/cancel_registration_meeting_button_cell.rb
+++ decidim-meetings/app/cells/decidim/meetings/cancel_registration_meeting_button_cell.rb
@@ -22,7 +22,11 @@ module Decidim
       def registration_status
         model.registrations.find_by(user: current_user)&.status
       end
-
+      
+      # i18n-tasks-use t('decidim.meetings.meetings.show.leave')
+      # i18n-tasks-use t('decidim.meetings.meetings.show.leave_confirmation')
+      # i18n-tasks-use t('decidim.meetings.meetings.show.leave_waitlist')
+      # i18n-tasks-use t('decidim.meetings.meetings.show.leave_waitlist_confirmation')
       def action_keys
         if registration_status == "waiting_list"
           {
