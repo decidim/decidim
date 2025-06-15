eb42c17a4c9e4cbe6449028aa958505cd9f1a720
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-06-15 12:07:23 +0200

    reworked description in input_mentions.js

--- decidim-core/app/packs/src/decidim/input_mentions.js
+++ decidim-core/app/packs/src/decidim/input_mentions.js
@@ -5,8 +5,7 @@ const mentionsInitializer = () => {
   const $mentionContainer = $(".js-mentions");
   const nodatafound = $mentionContainer.attr("data-noresults");
 
-  // The editor implements hashtags functionality by itself so it is not needed
-  // to attach tribute to the rich text editor.
+  // Used to render mentions in the rich text editor.
   if ($mentionContainer.parent().hasClass("editor")) {
     return;
   }
