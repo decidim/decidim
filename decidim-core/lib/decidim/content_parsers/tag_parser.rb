166b8d5941871589040e94cf3f981cd25419f579
Tom Greenwood <101816158+greenwoodt@users.noreply.github.com>
2025-06-16 10:32:22 +0200

    Update decidim-core/lib/decidim/content_parsers/tag_parser.rb
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-core/lib/decidim/content_parsers/tag_parser.rb
+++ decidim-core/lib/decidim/content_parsers/tag_parser.rb
@@ -12,7 +12,7 @@ module Decidim
       # Replaces tags name with new or existing tags models global ids.
       #
       # The actual tags depend on the context, these can be user
-      # mentions, etc.
+      # mentions (with a "@"), resource mentions (with a "/"), etc.
       #
       # @return [String] the content with the tags replaced by global ids
       def rewrite
