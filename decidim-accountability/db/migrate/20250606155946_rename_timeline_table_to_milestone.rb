f59c96bd464a57dd53cba44a5d5a276d2debc4a7
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-06-19 09:58:02 +0200

    updated migration file to account for index results id and entry date

--- decidim-accountability/db/migrate/20250606155946_rename_timeline_table_to_milestone.rb
+++ decidim-accountability/db/migrate/20250606155946_rename_timeline_table_to_milestone.rb
@@ -3,5 +3,10 @@
 class RenameTimelineTableToMilestone < ActiveRecord::Migration[7.2]
   def change
     rename_table :decidim_accountability_timeline_entries, :decidim_accountability_milestone_entries
+
+    rename_index :decidim_accountability_milestone_entries, "index_decidim_accountability_timeline_entries_on_results_id",
+                 "index_decidim_accountability_milestone_entries_on_results_id"
+    rename_index :decidim_accountability_milestone_entries, "index_decidim_accountability_timeline_entries_on_entry_date",
+                 "index_decidim_accountability_milestone_entries_on_entry_date"
   end
 end
