5d0ec6aaea68d64a8e458da58ed16e685af45d9f
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-06-19 11:53:54 +0300

    Update API for core module

--- decidim-core/lib/decidim/api/types/attachment_collection_type.rb
+++ decidim-core/lib/decidim/api/types/attachment_collection_type.rb
@@ -5,10 +5,11 @@ module Decidim
     class AttachmentCollectionType < Decidim::Api::Types::BaseObject
       description "A file attachment collection"
 
-      field :description, Decidim::Core::TranslatedFieldType, "The description of this AttachmentCollection.", null: false
-      field :name, Decidim::Core::TranslatedFieldType, "The name of this AttachmentCollection.", null: false
-
       field :attachments, [Decidim::Core::AttachmentType, { null: true }], "This object's attachments", null: false
+      field :description, Decidim::Core::TranslatedFieldType, "The description of this AttachmentCollection.", null: false
+      field :id, GraphQL::Types::ID, "Internal ID for this AttachmentCollection", null: false
+      field :name, Decidim::Core::TranslatedFieldType, "The name of this AttachmentCollection.", null: false
+      field :weight, GraphQL::Types::Int, "The weight of this AttachmentCollection", null: true
     end
   end
 end
