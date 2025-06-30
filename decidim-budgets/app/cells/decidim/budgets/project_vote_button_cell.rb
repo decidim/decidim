997e9118d75fbd990a1b40ec27c8a31d382db26f
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-06-30 09:26:37 +0200

    Do not rely on arguments and always link to the authorized link for projects

--- decidim-budgets/app/cells/decidim/budgets/project_vote_button_cell.rb
+++ decidim-budgets/app/cells/decidim/budgets/project_vote_button_cell.rb
@@ -72,7 +72,7 @@ module Decidim
       end
 
       def authorization_redirect_path
-        options[:authorization_redirect_path] || resource_path
+        budget_focus_projects_path(budget, start_voting: true)
       end
 
       def vote_button_label
