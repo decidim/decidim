84592fd57d823fd59eb4dbefdb92e14a81497598
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-07-07 09:52:10 +0200

    Remove reference to `resource_permissions_link`

--- decidim-admin/app/helpers/decidim/admin/resource_permissions_helper.rb
+++ decidim-admin/app/helpers/decidim/admin/resource_permissions_helper.rb
@@ -20,22 +20,6 @@ module Decidim
         end
       end
 
-      # Public: Render a link to the permissions page for the resource.
-      #
-      # resource - The resource which permissions are going to be modified
-      def resource_permissions_link(resource)
-        return unless resource.allow_resource_permissions? && allowed_to?(:update, :component, component: resource.component)
-
-        current_participatory_space_admin_proxy = ::Decidim::EngineRouter.admin_proxy(current_participatory_space)
-        icon_link_to "key-2-line",
-                     current_participatory_space_admin_proxy.edit_component_permissions_path(
-                       current_component.id,
-                       resource_name: resource.resource_manifest.name,
-                       resource_id: resource.id
-                     ),
-                     t("actions.permissions", scope: "decidim.admin")
-      end
-
       # Public: Render a link to the permissions page for a resource not
       # related with a component and participatory space.
       #
