6080b01b3ff987ac76c4311eafac608ebd9bc15f
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-07-07 18:04:04 +0300

    Fix spell checks

--- decidim-demographics/app/controllers/decidim/demographics/admin/publish_responses_controller.rb
+++ decidim-demographics/app/controllers/decidim/demographics/admin/publish_responses_controller.rb
@@ -3,7 +3,7 @@
 module Decidim
   module Demographics
     module Admin
-      # Mind that even though the name of the controller, currently we don't allow the publication.
+      # Mind that even though the name of the controller, currently we do not allow the publication.
       # This is only for showing to the admin the visualization of the responses
       class PublishResponsesController < Admin::ApplicationController
         include Decidim::Forms::Admin::Concerns::HasQuestionnaireResponsesUrlHelper
