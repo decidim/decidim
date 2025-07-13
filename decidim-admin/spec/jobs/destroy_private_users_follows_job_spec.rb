670d374c4f7bc5fb7723b4db834c4f62e1fe803d
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-07-13 18:20:02 +0300

    Add more specs

--- decidim-admin/spec/jobs/destroy_private_users_follows_job_spec.rb
+++ decidim-admin/spec/jobs/destroy_private_users_follows_job_spec.rb
@@ -6,45 +6,55 @@ module Decidim
   module Admin
     describe DestroyPrivateUsersFollowsJob do
       let(:organization) { create(:organization) }
-      let(:user) { create(:user, :admin, :confirmed, organization:) }
-      let(:participatory_space_private_user) { create(:participatory_space_private_user, user:) }
+      let!(:user) { create(:user, :admin, :confirmed, organization:) }
+      let!(:normal_user) { create(:user, organization:) }
+      let!(:follow) { create(:follow, followable: participatory_space, user: normal_user) }
+      let(:component) { create(:dummy_component, participatory_space: ) }
+      let(:resource) { create(:dummy_resource, component: component, author: user) }
+      let!(:followed_resource) { create(:follow, followable: resource, user: normal_user) }
 
-      context "when assembly is not transparent and user follows assembly and one meeting belonging to assembly" do
-        let(:normal_user) { create(:user, organization:) }
-        let(:assembly) { create(:assembly, :private, :opaque, :published, organization: user.organization) }
-        let!(:participatory_space_private_user) { create(:participatory_space_private_user, user: normal_user, privatable_to: assembly) }
-        let!(:follow) { create(:follow, followable: assembly, user: normal_user) }
-        let(:component) { create(:dummy_component, participatory_space: assembly) }
-        let(:resource) { create(:dummy_resource, component: component, author: user) }
-        let!(:followed_resource) { create(:follow, followable: resource, user: normal_user) }
 
-        it "destroys 2 follows" do
-          expect(Decidim::Follow.where(user: normal_user).count).to eq(2)
-          expect do
-            described_class.perform_now(normal_user.id, assembly)
-          end.to change(Decidim::Follow, :count).by(-2)
+      context "when assembly is private and non transparent" do
+        let(:participatory_space) { create(:assembly, :private, :published, :opaque, organization: user.organization) }
+
+        it "deletes follows of non private users" do
+          # we have 2 follows, one for assembly, and one for a "child" resource
+          expect { described_class.perform_now(normal_user.id, participatory_space) }.to change(Decidim::Follow, :count).by(-2)
         end
       end
 
-      context "when participatory process is private" do
-        let(:normal_user) { create(:user, organization:) }
-        let(:participatory_process) { create(:participatory_process, :private, :published, organization: user.organization) }
-        let!(:participatory_space_private_user) { create(:participatory_space_private_user, user: normal_user, privatable_to: participatory_process) }
+      context "when assembly is private but transparent" do
+        let(:participatory_space) { create(:assembly, :private, :published, organization: user.organization) }
 
-        context "and user follows process and follows meeting belonging to process" do
-          let!(:follow) { create(:follow, followable: participatory_process, user: normal_user) }
-          let(:component) { create(:dummy_component, participatory_space: participatory_process) }
-          let(:resource) { create(:dummy_resource, component: component, author: user) }
-          let!(:followed_resource) { create(:follow, followable: resource, user: normal_user) }
+        it "preserves follows of non private users" do
+          # we have 2 follows, one for assembly, and one for a "child" resource
+          expect { described_class.perform_now(normal_user.id, participatory_space) }.not_to change(Decidim::Follow, :count)
+        end
+      end
 
-          let(:meetings_component) { create(:component, manifest_name: "meetings", participatory_space: participatory_process) }
+      context "when assembly is public" do
+        let(:participatory_space) { create(:assembly, :published, organization: user.organization) }
 
-          it "destroys 2 follows" do
-            expect(Decidim::Follow.where(user: normal_user).count).to eq(2)
-            expect do
-              described_class.perform_now(normal_user.id, participatory_process)
-            end.to change(Decidim::Follow, :count).by(-2)
-          end
+        it "preserves follows of non private users" do
+          # we have 2 follows, one for assembly, and one for a "child" resource
+          expect { described_class.perform_now(normal_user.id, participatory_space) }.not_to change(Decidim::Follow, :count)
+        end
+      end
+
+      context "when process is private" do
+        let(:participatory_space) { create(:participatory_process, :private, :published, organization: user.organization) }
+
+        it "deletes follows of non private users" do
+          # we have 2 follows, one for process, and one for a "child" resource
+          expect { described_class.perform_now(normal_user.id, participatory_space) }.to change(Decidim::Follow, :count).by(-2)
+        end
+      end
+
+      context "when process is public" do
+        let(:participatory_space) { create(:participatory_process, :published, organization: user.organization) }
+
+        it "preserves follows of non private users" do
+          expect { described_class.perform_now(normal_user.id, participatory_space) }.not_to change(Decidim::Follow, :count)
         end
       end
     end
