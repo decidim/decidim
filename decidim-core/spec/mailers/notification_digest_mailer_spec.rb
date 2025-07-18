0331768e1889cb8355f970c712120917e6dc81ce
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-07-18 16:42:45 +0300

    Filter invalid notifications from emails being sent

--- decidim-core/spec/mailers/notification_digest_mailer_spec.rb
+++ decidim-core/spec/mailers/notification_digest_mailer_spec.rb
@@ -8,8 +8,8 @@ module Decidim
     let(:user) { create(:user, name: "Sarah Connor", organization:) }
     let(:notification_ids) { [notification.id] }
     let(:notification) { create(:notification, user:, resource:) }
-    let(:component) { create(:component, manifest_name: "dummy", organization:) }
-    let(:resource) { create(:dummy_resource, title: { en: %(Testing <a href="/resource">resource</a>) }, component:) }
+    let(:component) { create(:component, :published, manifest_name: "dummy", organization:) }
+    let(:resource) { create(:dummy_resource, :published, title: { en: %(Testing <a href="/resource">resource</a>) }, component:) }
 
     describe "digest_mail" do
       subject { described_class.digest_mail(user, notification_ids) }
@@ -31,6 +31,80 @@ module Decidim
           expect(subject.body).to include("This is a comment")
         end
       end
+
+      context "resource of notification is not visible" do
+        context "when is moderated" do
+          let(:moderation) { create(:moderation, reportable: resource, participatory_space: component.participatory_space, report_count: 1, hidden_at: Time.current) }
+          let!(:report) { create(:report, moderation:, user:) }
+
+          it "hides the notification" do
+            expect(subject.body).not_to include(
+                                          %(<a href="#{::Decidim::ResourceLocatorPresenter.new(resource).url}">Testing resource</a>)
+                                        )
+          end
+        end
+
+        context "when is deleted" do
+          it "hides the notification" do
+            resource.update(deleted_at: Time.current)
+            expect(subject.body).not_to include(
+                                          %(<a href="#{::Decidim::ResourceLocatorPresenter.new(resource).url}">Testing resource</a>)
+                                        )
+          end
+        end
+
+        context "when the component is not visible" do
+          it "hides the notification" do
+            component.update(published_at: nil)
+            expect(subject.body).not_to include(
+                                          %(<a href="#{::Decidim::ResourceLocatorPresenter.new(resource).url}">Testing resource</a>)
+                                        )
+          end
+        end
+
+        context "when the space is not published" do
+          it "hides the notification" do
+            component.participatory_space.update(published_at: nil)
+            expect(subject.body).not_to include(
+                                          %(<a href="#{::Decidim::ResourceLocatorPresenter.new(resource).url}">Testing resource</a>)
+                                        )
+          end
+        end
+
+        context "when the space is private" do
+          it "hides the notification" do
+            component.participatory_space.update(private_space: true)
+            expect(subject.body).not_to include(
+                                          %(<a href="#{::Decidim::ResourceLocatorPresenter.new(resource).url}">Testing resource</a>)
+                                        )
+          end
+        end
+      end
+
+      context "when the space is private and user has access" do
+        let!(:participatory_space) { create(:participatory_process, :private, organization: ) }
+        let(:component) { create(:component, :published, manifest_name: "dummy", participatory_space:) }
+        let!(:participatory_space_private_user) { create(:participatory_space_private_user, privatable_to: participatory_space, user: user) }
+
+        it "displays the notification" do
+          expect(subject.body).to include(
+                                        %(<a href="#{::Decidim::ResourceLocatorPresenter.new(resource).url}">Testing resource</a>)
+                                      )
+        end
+      end
+      
+      context "when the space is transparent and user has access" do
+        let!(:participatory_space) { create(:assembly, :transparent, :private, organization: ) }
+        let(:component) { create(:component, :published, manifest_name: "dummy", participatory_space:) }
+        let!(:participatory_space_private_user) { create(:participatory_space_private_user, privatable_to: participatory_space, user: user) }
+
+        it "displays the notification" do
+          expect(subject.body).to include(
+                                        %(<a href="#{::Decidim::ResourceLocatorPresenter.new(resource).url}">Testing resource</a>)
+                                      )
+        end
+      end
+
 
       context "when the notification does not send emails" do
         let(:notification) { create(:notification, user:, resource:, event_class: "Decidim::Dev::DummyNotificationOnlyResourceEvent") }
