22446acc5ce1b3a94be3aef091a0827467a3374e
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-07-29 14:22:43 +0300

    Fix element

--- decidim-core/app/packs/src/decidim/index.js
+++ decidim-core/app/packs/src/decidim/index.js
@@ -185,8 +185,7 @@ const initializer = (element = document) => {
   element.querySelectorAll('[data-component="accordion"]').forEach((component) => createAccordion(component))
 
 
-  element.querySelectorAll('[data-controller="dropdown"]').forEach((component) => createAccordion(component))
-
+  element.querySelectorAll('[data-controller="dropdown"]').forEach((component) => createDropdown(component))
   element.querySelectorAll('[data-component="dropdown"]').forEach((component) => {
     alert(`${window.location.href} Using dropdwon component`);
   })
