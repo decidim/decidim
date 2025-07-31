da6de4e697a181976486466e7d9c2f4db6c31eac
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2025-07-31 13:36:34 +0300

    Make results API attachable + tests

--- decidim-accountability/lib/decidim/api/mutations/result_mutation_type.rb
+++ decidim-accountability/lib/decidim/api/mutations/result_mutation_type.rb
@@ -6,6 +6,9 @@ module Decidim
       graphql_name "ResultMutation"
       description "Accountability result mutations"
 
+      include Decidim::Core::AttachableMutations
+      include Decidim::Core::AttachableCollectionMutations
+
       field :create_milestone, mutation: CreateMilestoneType, description: "create milestone"
       field :delete_milestone, mutation: Decidim::Accountability::DeleteMilestoneType, description: "delete milestone"
       field :update_milestone, mutation: UpdateMilestoneType, description: "update milestone"
