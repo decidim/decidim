1626f79e57d17d76bd5426103a4e29f7706ad0ea
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-08-04 10:29:45 +0200

    inclusiveness adapted and added

--- decidim-participatory_processes/app/cells/decidim/participatory_processes/content_blocks/democratic_quality_stats_settings_form_cell.rb
+++ decidim-participatory_processes/app/cells/decidim/participatory_processes/content_blocks/democratic_quality_stats_settings_form_cell.rb
@@ -18,30 +18,30 @@ module Decidim
         def settings_options
           {
             functional_diversity_invited: [
-              [t("inclusiveness.cultural_origins_participation.options.one", scope: option_scope), 0],
-              [t("inclusiveness.cultural_origins_participation.options.two", scope: option_scope), 1],
-              [t("inclusiveness.cultural_origins_participation.options.three", scope: option_scope), 2.5],
-              [t("inclusiveness.cultural_origins_participation.options.four", scope: option_scope), 5]
+              [t("inclusiveness.functional_diversity_invited.options.zero", scope: option_scope), 0],
+              [t("inclusiveness.functional_diversity_invited.options.one", scope: option_scope), 1],
+              [t("inclusiveness.functional_diversity_invited.options.two_five", scope: option_scope), 2.5],
+              [t("inclusiveness.functional_diversity_invited.options.five", scope: option_scope), 5]
             ],
             languages_communicated: [
-              [t("inclusiveness.cultural_origins_participation.options.one", scope: option_scope), 1],
-              [t("inclusiveness.cultural_origins_participation.options.two", scope: option_scope), 2.5],
-              [t("inclusiveness.cultural_origins_participation.options.three", scope: option_scope), 5]
+              [t("inclusiveness.languages_communicated.options.one", scope: option_scope), 1],
+              [t("inclusiveness.languages_communicated.options.two_five", scope: option_scope), 2.5],
+              [t("inclusiveness.languages_communicated.options.five", scope: option_scope), 5]
             ],
             mobility_meeting_access: [
-              [t("inclusiveness.varied_participation_times.options.zero", scope: option_scope), 0],
-              [t("inclusiveness.varied_participation_times.options.two_five", scope: option_scope), 2.5],
-              [t("inclusiveness.varied_participation_times.options.five", scope: option_scope), 5]
+              [t("inclusiveness.mobility_meeting_access.options.zero", scope: option_scope), 0],
+              [t("inclusiveness.mobility_meeting_access.options.two_five", scope: option_scope), 2.5],
+              [t("inclusiveness.mobility_meeting_access.options.five", scope: option_scope), 5]
             ],
             participation_scheduling_times: [
-              [t("inclusiveness.varied_participation_times.options.zero", scope: option_scope), 0],
-              [t("inclusiveness.varied_participation_times.options.two_five", scope: option_scope), 2.5],
-              [t("inclusiveness.varied_participation_times.options.five", scope: option_scope), 5]
+              [t("inclusiveness.participation_scheduling_times.options.zero", scope: option_scope), 0],
+              [t("inclusiveness.participation_scheduling_times.options.two_five", scope: option_scope), 2.5],
+              [t("inclusiveness.participation_scheduling_times.options.five", scope: option_scope), 5]
             ],
             digital_support_offered: [
-              [t("inclusiveness.varied_participation_times.options.zero", scope: option_scope), 0],
-              [t("inclusiveness.varied_participation_times.options.two_five", scope: option_scope), 2.5],
-              [t("inclusiveness.varied_participation_times.options.five", scope: option_scope), 5]
+              [t("inclusiveness.digital_support_offered.options.zero", scope: option_scope), 0],
+              [t("inclusiveness.digital_support_offered.options.two_five", scope: option_scope), 2.5],
+              [t("inclusiveness.digital_support_offered.options.five", scope: option_scope), 5]
             ],
             relevance_percentage: [
               [t("relevance.relevance_percentage.options.one", scope: option_scope), 1],
