76a1b8c6b340c4b152fd6df4161e2b095419536c
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-08-12 11:33:56 +0200

    refactored meetings permissions

--- decidim-meetings/app/permissions/decidim/meetings/admin/permissions.rb
+++ decidim-meetings/app/permissions/decidim/meetings/admin/permissions.rb
@@ -8,13 +8,11 @@ module Decidim
           return permission_action unless user
           return permission_action unless permission_action.scope == :admin
 
+          return Decidim::Meetings::Admin::MeetingPermissions.new(user, permission_action, context).permissions if subject == :meeting
+          return Decidim::Meetings::Admin::QuestionnairePermissions.new(user, permission_action, context).permissions if subject == :questionnaire
           return Decidim::Meetings::Admin::AgendaPermissions.new(user, permission_action, context).permissions if subject == :agenda
 
           toggle_allow(poll.present? && meeting.present?) if subject == :poll && action == :update
-
-          allowed_registration_form_action?
-          allowed_meeting_action?
-          allowed_poll_action?
 
           permission_action
         end
@@ -27,42 +25,6 @@ module Decidim
 
         def poll
           @poll ||= context.fetch(:poll, nil)
-        end
-
-        def registration_form
-          @registration_form ||= context.fetch(:questionnaire, nil)
-        end
-
-        def allowed_meeting_action?
-          return unless permission_action.subject == :meeting
-
-          return disallow! if meeting && !meeting.official?
-
-          case permission_action.action
-          when :close, :copy, :export_registrations, :update, :read_invites
-            toggle_allow(meeting.present?)
-          when :invite_attendee
-            toggle_allow(meeting.present? && meeting.registrations_enabled?)
-          when :validate_registration_code
-            toggle_allow(
-              meeting.present? &&
-              meeting.registrations_enabled? &&
-              meeting.component.settings.registration_code_enabled
-            )
-          when :create
-            allow!
-          end
-        end
-
-        def allowed_registration_form_action?
-          return unless permission_action.subject == :questionnaire
-
-          case permission_action.action
-          when :update
-            toggle_allow(registration_form.present?)
-          when :export_responses
-            permission_action.allow!
-          end
         end
       end
     end
