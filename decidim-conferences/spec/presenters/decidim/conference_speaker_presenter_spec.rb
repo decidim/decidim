cead527ff524050c8a6fa603ef784863fa947af0
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2025-08-13 14:06:30 +0300

    Fix conference speaker presenter spec

--- decidim-conferences/spec/presenters/decidim/conference_speaker_presenter_spec.rb
+++ decidim-conferences/spec/presenters/decidim/conference_speaker_presenter_spec.rb
@@ -4,8 +4,9 @@ require "spec_helper"
 
 module Decidim
   describe ConferenceSpeakerPresenter, type: :helper do
+    let(:conference) { create(:conference, skip_injection: true) }
     let(:conference_speaker) do
-      build(:conference_speaker, full_name: "Full name")
+      build(:conference_speaker, full_name: "Full name", conference:)
     end
 
     describe "name" do
@@ -14,7 +15,7 @@ module Decidim
       it { is_expected.to eq "Full name" }
 
       context "when speaker is an existing user" do
-        let(:user) { build(:user, :confirmed, name: "Julia G.", nickname: "julia_g") }
+        let(:user) { build(:user, :confirmed, name: "Julia G.", nickname: "julia_g", organization: conference.organization) }
         let(:conference_speaker) { build(:conference_speaker, full_name: "Full name", user:) }
 
         it { is_expected.to eq "Julia G." }
@@ -27,7 +28,7 @@ module Decidim
       it { is_expected.to be_nil }
 
       context "when speaker is an existing user" do
-        let(:user) { build(:user, :confirmed, name: "Julia G.", nickname: "julia_g") }
+        let(:user) { build(:user, :confirmed, name: "Julia G.", nickname: "julia_g", organization: conference.organization) }
         let(:conference_speaker) { build(:conference_speaker, full_name: "Full name", user:) }
 
         it { is_expected.to eq "@julia_g" }
