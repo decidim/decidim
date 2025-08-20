32b9f9b408c1d5bb9bb97a88c8553a33e22ca910
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-08-20 16:41:43 +0300

    Fix tooltip display on mobile

--- decidim-core/app/packs/src/decidim/controllers/tooltip/controller.js
+++ decidim-core/app/packs/src/decidim/controllers/tooltip/controller.js
@@ -259,6 +259,7 @@ export default class extends Controller {
         this.tooltip.setAttribute("aria-hidden", false)
       })
       this.tooltip.addEventListener("mouseleave", () => this.hideTooltip())
+      this.element.addEventListener("click", (event) => this.toggleTooltip(event))
     }
   }
 
