588a4a0fe31a04435fba178329d8d1e87cbcad2b
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-09-08 15:55:29 +0300

    Apply review recommendations

--- decidim-api/lib/decidim/api/types/base_object.rb
+++ decidim-api/lib/decidim/api/types/base_object.rb
@@ -10,23 +10,6 @@ module Decidim
         field_class Types::BaseField
 
         required_scopes "api:read"
-
-        # Creates the permission chain arrau that contains all the permission classes required to authorize a certain resource
-        # We are using unshift as we need the Admin and base permissions to be last in the chain
-        # @param object [ActiveModel::Base] The object that is being represented.
-        #
-        # @return [Decidim::DefaultPermissions]
-        def self.permission_chain(object)
-          permissions = [
-            Decidim::Admin::Permissions,
-            Decidim::Permissions
-          ]
-
-          permissions.unshift(object.participatory_space.manifest.permissions_class) if object.respond_to?(:participatory_space)
-          permissions.unshift(object.component.manifest.permissions_class) if object.respond_to?(:component) && object.component.present?
-
-          permissions
-        end
       end
     end
   end
