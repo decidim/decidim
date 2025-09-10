e3bdc40536bd417ee265645c912a68607b67c586
MariaDascaluPublicis <maria.dascalu@publicissapient.com>
2025-09-10 11:15:43 +0300

    fix review suggestions

--- decidim-core/app/packs/src/decidim/dropdown_menu.js
+++ decidim-core/app/packs/src/decidim/dropdown_menu.js
@@ -17,43 +17,52 @@ document.addEventListener("turbo:load", () => {
   }
 });
 
+
+const setMenuOpacity = (opacity) => {
+  const content = document.getElementById("content");
+  const footer = document.querySelector("footer");
+  const menuBar = document.getElementById("menu-bar-container");
+
+  if (content) {
+    content.style.opacity = opacity;
+  }
+  if (footer) {
+    footer.style.opacity = opacity;
+  }
+  if (menuBar) {
+    menuBar.style.opacity = opacity;
+  }
+}
+
 const menuContainer = document.getElementById("dropdown-menu-main-desktop");
 const menuButton = document.getElementById("main-dropdown-summary-desktop");
-const content = document.getElementById("content");
-const footer = document.querySelector("footer");
-const menuBar = document.getElementById("menu-bar-container");
 
-if (menuButton !== null) {
+if (menuButton && menuContainer) {
   menuButton.addEventListener("click", function () {
-    if (menuContainer === null) {
+    const isHidden = menuContainer.getAttribute("aria-hidden") === "true";
+    if (!isHidden) {
       return;
     }
-
-    const isHidden = menuContainer.getAttribute("aria-hidden") === "true";
-
-    if (isHidden) {
+    setTimeout(() => {
+      setMenuOpacity("0.3");
+      document.body.style.overflow = "hidden";
       menuContainer.setAttribute("aria-hidden", "false");
-      content.style.opacity = "0.3";
-      footer.style.opacity = "0.3";
-      menuBar.style.opacity = "0.3";
-    } else {
-      menuContainer.setAttribute("aria-hidden", "true");
-      content.style.opacity = "1";
-      footer.style.opacity = "1";
-      menuBar.style.opacity = "1";
-    }
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }, 50);
   });
 }
 
-if (menuContainer !== null) {
+if (menuContainer) {
   document.addEventListener("click", function (event) {
     const isOpen = menuContainer.getAttribute("aria-hidden") === "false";
-    const clickedInsideMenu = menuContainer.contains(event.target) || menuButton.contains(event.target);
+    const closeMenuButton = document.getElementById("main-dropdown-summary-desktop-close");
+    const clickedInsideMenu = menuContainer.contains(event.target);
+    const clickCloseButton = closeMenuButton && closeMenuButton.contains(event.target);
 
-    if (isOpen && !clickedInsideMenu) {
+    if (isOpen && (!clickedInsideMenu || clickCloseButton)) {
       menuContainer.setAttribute("aria-hidden", "true");
-      content.style.opacity = "1";
-      footer.style.opacity = "1";
+      setMenuOpacity("1");
+      document.body.style.overflow = "scroll";
     }
   });
 }
