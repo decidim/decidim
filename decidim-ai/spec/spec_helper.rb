2e7a2372d43e30631247b565e86291802fcaa8b0
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-09-15 11:13:53 +0200

    Clean-up usage of the DECIDIM_SPAM_DETECTION_BACKEND env vars

--- decidim-ai/spec/spec_helper.rb
+++ decidim-ai/spec/spec_helper.rb
@@ -3,8 +3,6 @@
 require "decidim/dev"
 
 ENV["ENGINE_ROOT"] = File.dirname(__dir__)
-ENV["DECIDIM_SPAM_DETECTION_BACKEND_USER"] ||= "memory"
-ENV["DECIDIM_SPAM_DETECTION_BACKEND_RESOURCE"] ||= "memory"
 
 Decidim::Dev.dummy_app_path = File.expand_path(File.join("..", "spec", "decidim_dummy_app"))
 
