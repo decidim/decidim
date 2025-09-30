376b87010dad34c5e5786428894d56c26981e2c6
Alejandro Rueda <alejandro.rueda@nazaries.com>
2025-09-30 16:59:51 +0300

    Restrict vote_comment action

--- decidim-proposals/lib/decidim/proposals/commentable_proposal.rb
+++ decidim-proposals/lib/decidim/proposals/commentable_proposal.rb
@@ -25,11 +25,6 @@ module Decidim
           followers
         end
 
-        def user_allowed_to_vote_comment?(user)
-          return unless can_participate_in_space?(user)
-
-          ActionAuthorizer.new(user, "vote_comment", component, self).authorize.ok?
-        end
       end
     end
   end
