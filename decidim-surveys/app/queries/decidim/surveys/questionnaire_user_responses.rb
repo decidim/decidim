7f55aaf1dd846e61c400128de3904ad7dee7af3b
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-10-03 15:30:06 +0200

    path for question responses

---
+++ decidim-surveys/app/queries/decidim/surveys/questionnaire_user_responses.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Surveys
+    # A class used to collect user responses for a questionnaire
+    class QuestionnaireUserResponses < Decidim::Query
+      # Syntactic sugar to initialize the class and return the queried objects.
+      #
+      # questionnaire - a Questionnaire object
+      def self.for(questionnaire)
+        new(questionnaire).query
+      end
+
+      # Initializes the class.
+      #
+      # questionnaire = a Questionnaire object
+      def initialize(questionnaire)
+        @questionnaire = questionnaire
+      end
+
+      # Returns grouped user responses for published surveys only
+      def query
+        return [] unless @questionnaire.questionnaire_for&.published_at.present?
+
+        responses = Decidim::Forms::Response.joins(:question)
+                                           .where(questionnaire: @questionnaire)
+                                           .includes(:question, :user)
+                                           .order("decidim_forms_questions.position")
+
+        # Group by user and return as an enumerable that supports find_in_batches
+        UserResponseCollection.new(responses.group_by(&:user).values)
+      end
+    end
+
+    # A wrapper class to make grouped responses work with ActiveRecord batch processing
+    class UserResponseCollection
+      include Enumerable
+
+      def initialize(grouped_responses)
+        @grouped_responses = grouped_responses
+      end
+
+      def each(&block)
+        @grouped_responses.each(&block)
+      end
+
+      def find_in_batches(batch_size: 1000)
+        @grouped_responses.each_slice(batch_size) do |batch|
+          yield batch
+        end
+      end
+
+      def find_each(&block)
+        @grouped_responses.each(&block)
+      end
+    end
+  end
+end
