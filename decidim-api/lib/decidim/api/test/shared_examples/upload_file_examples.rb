7532870f37e93d8edf3de95003145012245e96c0
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2025-10-07 15:07:14 +0300

    Add shared examples for api file upload

---
+++ decidim-api/lib/decidim/api/test/shared_examples/upload_file_examples.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+require "tempfile"
+
+shared_examples "API uploadable file" do
+  it_behaves_like "handle form error"
+end
+
+shared_examples "handle form error" do
+  context "when file not found" do
+    let!(:file_path) { "/path/to/nonexistent/file.jpg" }
+
+    it "raises execution error" do
+      expect do
+        execute_query(query, variables)
+      end.to raise_error(StandardError)
+    end
+  end
+
+  context "when file extension is not supported" do
+    let!(:file) { Tempfile.create(["foo", ".xyz"]) }
+
+    let!(:file_path) { file.path }
+
+    it "raises execution error" do
+      expect do
+        execute_query(query, variables)
+      end.to raise_error(StandardError, match(/File extension is not suppoerted./))
+    end
+  end
+
+  context "when content type is not supported" do
+    let!(:file) { Tempfile.create("fake_file") }
+    let!(:file_path) { file.path }
+
+    before do
+      allow(Marcel::MimeType).to receive(:for).and_return("unsupported/type")
+    end
+
+    it "raises execution error" do
+      expect do
+        execute_query(query, variables)
+      end.to raise_error(StandardError, match(/File type is not supported./))
+    end
+  end
+
+  context "when everything is ok" do
+    let!(:file) { Tempfile.create(["valid_file", ".jpeg"]) }
+    before do
+      allow(Marcel::MimeType).to receive(:for).and_return("image/jpeg")
+    end
+
+    it "uploads the file and returns the blob" do
+      result = nil
+
+      expect {
+        result = execute_query(query, variables)
+      }.to change(ActiveStorage::Blob, :count).by(1)
+
+      blob = ActiveStorage::Blob.last
+
+      expect(result["uploadFile"]["blob"]).to include(
+        "id" => blob.id.to_s,
+        "signedId" => blob.signed_id,
+        "filename" => blob.filename,
+        "contentType" => "image/jpeg"
+      )
+    end
+  end
+end
