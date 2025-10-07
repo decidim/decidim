cf069372f974a4e6e9e06ae8a7047f4ea3afac5e
Anna Topalidi <t.opalidisanna@gmail.com>
2025-10-07 15:24:39 +0200

    add max choices limit

--- decidim-elections/app/helpers/decidim/elections/application_helper.rb
+++ decidim-elections/app/helpers/decidim/elections/application_helper.rb
@@ -39,7 +39,11 @@ module Decidim
 
       def question_title(question, tag = :h3, **options)
         content_tag(tag, **options) do
-          translated_attribute(question.body)
+          title = translated_attribute(question.body)
+          if question.max_choices.present? && question.question_type == "multiple_option"
+            title += " (#{t("decidim.elections.votes.question.max_choices", count: question.max_choices)})"
+          end
+          title.html_safe
         end
       end
 
---
+++ decidim-elections/app/packs/src/decidim/elections/elections.js
@@ -0,0 +1,27 @@
+document.addEventListener("turbo:load", () => {
+  const responseContainers = document.querySelectorAll(".response[data-max-choices]");
+
+  responseContainers.forEach((container) => {
+    const maxChoices = parseInt(container.dataset.maxChoices, 10);
+    if (!maxChoices) return;
+
+    const checkboxes = container.querySelectorAll("input[type=checkbox]");
+    const alertElement = container.querySelector(".max-choices-alert");
+
+    const checkLimit = () => {
+      const checkedCount = container.querySelectorAll("input[type=checkbox]:checked").length;
+
+      if (checkedCount > maxChoices) {
+        alertElement.style.display = "block";
+      } else {
+        alertElement.style.display = "none";
+      }
+    };
+
+    checkboxes.forEach((checkbox) => {
+      checkbox.addEventListener("change", checkLimit);
+    });
+
+    checkLimit();
+  });
+});
