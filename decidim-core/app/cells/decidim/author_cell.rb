1d7dfc9b50e2aa8291f9979f79ea419411226b83
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2025-10-09 10:02:43 +0200

    Show badges next to user nicknames

--- decidim-core/app/cells/decidim/author_cell.rb
+++ decidim-core/app/cells/decidim/author_cell.rb
@@ -186,5 +186,15 @@ module Decidim
 
       model.has_tooltip?
     end
+
+    def show_badge?
+      return false unless model.respond_to? :officialized?
+
+      model.officialized?
+    end
+
+    def officialization_text
+      translated_attribute(model.officialized_as).presence || t("decidim.profiles.show.officialized")
+    end
   end
 end
