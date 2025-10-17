64cd0c608f99b1ead2ed0701f73d0763d20b1030
Alejandro Puerta <71900287+apuertadelaguila@users.noreply.github.com>
2025-10-17 14:56:13 +0200

    Add format validation for name and nickname in UserGroupForm (#15368)

--- decidim-core/spec/system/user_group_creation_spec.rb
+++ decidim-core/spec/system/user_group_creation_spec.rb
@@ -36,4 +36,40 @@ describe "User group creation" do
       expect(page).to have_content(user.name)
     end
   end
+
+  context "when nickname has invalid format" do
+    it "shows validation error in the form instead of raising exception" do
+      click_on "Create group"
+
+      fill_in "Name", with: "Valid Group Name"
+      fill_in "Nickname", with: "Invalid Nickname"
+      fill_in "Email", with: "user_group@decidim.org"
+      fill_in "Document number", with: "12345678X"
+      fill_in "Phone", with: "12345678"
+
+      click_on "Create group"
+
+      expect(page).to have_content("There was a problem creating the group")
+      expect(page).to have_css("small.form-error.is-visible", text: "is invalid")
+      expect(page).to have_css(".is-invalid-input#group_nickname")
+    end
+  end
+
+  context "when name has invalid format" do
+    it "shows validation error in the form instead of raising exception" do
+      click_on "Create group"
+
+      fill_in "Name", with: "<Invalid Name>"
+      fill_in "Nickname", with: "valid_nickname"
+      fill_in "Email", with: "user_group@decidim.org"
+      fill_in "Document number", with: "12345678X"
+      fill_in "Phone", with: "12345678"
+
+      click_on "Create group"
+
+      expect(page).to have_content("There was a problem creating the group")
+      expect(page).to have_css("small.form-error.is-visible", text: "is invalid")
+      expect(page).to have_css(".is-invalid-input#group_name")
+    end
+  end
 end
