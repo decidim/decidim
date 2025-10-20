4636bc96b4b7550927238b4fca774215f241e02f
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-10-20 08:26:28 +0200

    serializer added

--- decidim-surveys/lib/decidim/surveys/user_responses_serializer.rb
+++ decidim-surveys/lib/decidim/surveys/user_responses_serializer.rb
@@ -8,15 +8,26 @@ module Decidim
 
       # Public: Exports a hash with the serialized data for the user response.
       def serialize
-        {
-          response_translated_attribute_name(:id) => [resource.id, resource.user&.id].compact.join("_"),
-          response_translated_attribute_name(:created_at) => resource.created_at,
-          response_translated_attribute_name(:user_status) => response_translated_attribute_name(resource.decidim_user_id.present? ? "registered" : "unregistered"),
-          response_translated_attribute_name(:body) => normalize_body(resource)
-        }
+        responses_hash = hash_for(@responses.first)
+        responses_hash.merge!(questions_hash)
+
+        @responses.each do |response|
+          responses_hash[translated_question_key(response.question.position, response.question.body)] = normalize_body(response)
+        end
+
+        responses_hash
       end
 
       private
+
+      def hash_for(response)
+        {
+          response_translated_attribute_name(:id) => response&.session_token,
+          response_translated_attribute_name(:created_at) => response&.created_at,
+          response_translated_attribute_name(:ip_hash) => response&.ip_hash,
+          response_translated_attribute_name(:user_status) => response_translated_attribute_name(response&.decidim_user_id.present? ? "registered" : "unregistered")
+        }
+      end
 
       def normalize_body(response)
         return response.body if response.body.present?
@@ -29,7 +40,7 @@ module Decidim
       end
 
       def response_translated_attribute_name(attribute)
-        I18n.t(attribute.to_sym, scope: "decidim.open_data.help.survey_user_responses")
+        I18n.t(attribute.to_sym, scope: "decidim.forms.user_responses_serializer")
       end
     end
   end
