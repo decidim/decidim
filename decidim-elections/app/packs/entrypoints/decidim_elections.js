1a5bb05fa8cd1b607f6c898ad8a2e6d5b9167362
Ivan Vergés <ivan@pokecode.net>
2025-10-21 10:39:54 +0200

    Squashed commit of the following:
    commit 6cc62f36552c2ab27d76df75d5d339320f85d9a7
    Merge: 31e09c73d3 82c93ce522
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Tue Oct 21 10:37:46 2025 +0200
    
        Merge branch 'develop' into feature/elections-census-check-before-start
    
    commit 31e09c73d35ee1721e1899f547d454278f96b6f6
    Author: Anna Topalidi <t.opalidisanna@gmail.com>
    Date:   Tue Oct 21 07:10:47 2025 +0200
    
        fix linter
    
    commit 85f1a257e9254824dc83ff1edd4253774be43f25
    Author: Anna Topalidi <t.opalidisanna@gmail.com>
    Date:   Mon Oct 20 22:08:55 2025 +0200
    
        Allow users to check if they can vote before the election starts
    
    commit e503bec945ba658533a6cfa9fed5f7f605cc5efd
    Author: Anna Topalidi <t.opalidisanna@gmail.com>
    Date:   Fri Oct 10 15:36:16 2025 +0200
    
        xhange controller, add tests
    
    commit 408a17494d15b41fd9783e7634b7a9485edf6184
    Author: Anna Topalidi <t.opalidisanna@gmail.com>
    Date:   Fri Oct 10 14:52:00 2025 +0200
    
        change text
    
    commit a1ed69073915df3416c8307e65ba9772283ebe8e
    Author: Anna Topalidi <t.opalidisanna@gmail.com>
    Date:   Fri Oct 10 13:55:51 2025 +0200
    
        change test
    
    commit 242f0a66dccbcea940bde10b70f5b58859d7fccd
    Author: Anna Topalidi <t.opalidisanna@gmail.com>
    Date:   Fri Oct 10 13:33:59 2025 +0200
    
        add check before start

--- decidim-elections/app/packs/entrypoints/decidim_elections.js
+++ decidim-elections/app/packs/entrypoints/decidim_elections.js
@@ -1,5 +1,6 @@
 import "src/decidim/elections/waiting_room.js"
 import "src/decidim/elections/live_results_update.js";
+import "src/decidim/elections/census_check_visibility.js";
 
 // Images
 require.context("../images", true)
