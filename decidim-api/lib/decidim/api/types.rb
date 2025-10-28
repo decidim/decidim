332e7f0710db6d4564d528793a3283894bed43d9
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2025-10-28 12:35:09 +0200

    Fix merging confilcts

--- decidim-api/lib/decidim/api/types.rb
+++ decidim-api/lib/decidim/api/types.rb
@@ -8,6 +8,7 @@ module Decidim
     autoload :RequiredScopes, "decidim/api/required_scopes"
     autoload :GraphqlPermissions, "decidim/api/graphql_permissions"
     autoload :ComponentMutationType, "decidim/api/component_mutation_type"
+    autoload :SoftDeleteResourceType, "decidim/api/soft_delete_resource_type"
 
     module Types
       autoload :BaseArgument, "decidim/api/types/base_argument"
