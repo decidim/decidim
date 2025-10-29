1704c44130f93a534046a802e54cf15bd31d4e9b
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-10-29 07:00:36 +0100

    Add debugging messages for error (part 3)

--- decidim-dev/lib/decidim/dev/test/rspec_support/shakapacker_patch.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/shakapacker_patch.rb
@@ -5,9 +5,11 @@ require "shakapacker/env"
 module Shakapacker
   class Env
     def current
+      puts "*" * 80
       puts config_path.to_s
       puts File.exist?(config_path.to_s)
-      puts File.read(config_path.to_s)
+      puts File.read(config_path.to_s).split.count
+      puts "*" * 80
 
       envs = available_environments || {}
       Rails.env.presence_in(envs) || Shakapacker::DEFAULT_ENV
