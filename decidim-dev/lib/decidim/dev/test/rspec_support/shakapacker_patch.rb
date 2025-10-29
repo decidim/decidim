df579cdcfb5608fc497664180476bf40c7848f4e
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-10-29 12:01:16 +0100

    Add debugging messages for error (part 4)

--- decidim-dev/lib/decidim/dev/test/rspec_support/shakapacker_patch.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/shakapacker_patch.rb
@@ -9,6 +9,7 @@ module Shakapacker
       puts config_path.to_s
       puts File.exist?(config_path.to_s)
       puts File.read(config_path.to_s).split.count
+      puts YAML.load_file(config_path.to_s, aliases: true).inspect
       puts "*" * 80
 
       envs = available_environments || {}
