3d9023acbebd955fccf1e1f8aa863413f680ffe1
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-10-31 08:07:48 +0100

    Disable spec on decidim-api that removes the views directory

--- decidim-api/spec/tasks/decidim_api_generate_docs_spec.rb
+++ decidim-api/spec/tasks/decidim_api_generate_docs_spec.rb
@@ -2,15 +2,15 @@
 
 require "spec_helper"
 
-describe "rake decidim_api:generate_docs", type: :task do
-  it "creates the static docs files" do
-    static = Rails.root.join("app/views/static/api/docs")
-    FileUtils.rm_rf(static)
+# describe "rake decidim_api:generate_docs", type: :task do
+#   it "creates the static docs files" do
+#     static = Rails.root.join("app/views/static/api/docs")
+#     FileUtils.rm_rf(static)
 
-    task.execute
+#     task.execute
 
-    index = File.read("#{static}/index.html")
-    expect(index).to include("About the GraphQL API")
-    expect(index).to include("GraphQL Reference")
-  end
-end
+#     index = File.read("#{static}/index.html")
+#     expect(index).to include("About the GraphQL API")
+#     expect(index).to include("GraphQL Reference")
+#   end
+# end
