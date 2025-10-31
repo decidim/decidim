783dde3964dd2a2b8cf13b20663cfc2424778d69
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-10-31 09:41:27 +0100

    Add debugging messages for error (part 8)

--- decidim-dev/lib/decidim/dev/test/rspec_support/shakapacker_patch.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/shakapacker_patch.rb
@@ -20,3 +20,32 @@ module Shakapacker
     end
   end
 end
+
+class Shakapacker::Configuration
+  def load
+    config = begin
+      YAML.load_file(config_path.to_s, aliases: true)
+    rescue ArgumentError
+      YAML.load_file(config_path.to_s)
+    end
+
+    puts "===> Shakapacker env: #{env.inspect}"
+    puts "===> Shakapacker YAML top-level keys: #{config.keys.inspect}"
+
+    symbolized_config = config[env].deep_symbolize_keys
+
+    return symbolized_config
+  rescue Errno::ENOENT => e
+    if self.class.installing
+      {}
+    else
+      raise "Shakapacker configuration file not found #{config_path}. " \
+            "Please run rails shakapacker:install " \
+            "Error: #{e.message}"
+    end
+  rescue Psych::SyntaxError => e
+    raise "YAML syntax error occurred while parsing #{config_path}. " \
+          "Please note that YAML must be consistently indented using spaces. Tabs are not allowed. " \
+          "Error: #{e.message}"
+  end
+end
