59353811e90ab276d43778f738b40d29da9f0353
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-10-31 15:37:21 +0100

    spelling error

--- decidim-surveys/lib/decidim/surveys.rb
+++ decidim-surveys/lib/decidim/surveys.rb
@@ -5,7 +5,7 @@ require "decidim/surveys/api"
 require "decidim/surveys/engine"
 require "decidim/surveys/admin_engine"
 require "decidim/surveys/component"
-require "decidim/surveys/batches array"
+require "decidim/surveys/batches_array"
 
 module Decidim
   # This namespace holds the logic of the `Surveys` component. This component
