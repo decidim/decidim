0e4af640c834857bc87846759b7f2fcea5250db2
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-11-11 12:27:15 +0200

    Implement changes to the GraphQL attachments types

--- decidim-core/lib/decidim/api/types/attachment_type.rb
+++ decidim-core/lib/decidim/api/types/attachment_type.rb
@@ -4,12 +4,18 @@ module Decidim
   module Core
     class AttachmentType < Decidim::Api::Types::BaseObject
       description "A file attachment"
+      implements Decidim::Core::TimestampsInterface
 
+      field :content_type, GraphQL::Types::String, "The content type of this attachment", null: true
       field :description, Decidim::Core::TranslatedFieldType, "The description of this attachment.", null: false
+      field :file_size, GraphQL::Types::String, "The file size of this attachment", null: true
+      field :id, GraphQL::Types::ID, "Internal ID of this attachment", null: false
+      field :link, GraphQL::Types::String, "The attached link of this attachment", null: true
       field :thumbnail, GraphQL::Types::String, "A thumbnail of this attachment, if it is an image.", method: :thumbnail_url, null: true
       field :title, Decidim::Core::TranslatedFieldType, "The title of this attachment.", null: false
       field :type, GraphQL::Types::String, "The type of this attachment", method: :file_type, null: false
       field :url, GraphQL::Types::String, "The url of this attachment", null: false
+      field :weight, GraphQL::Types::Int, "The weight of this attachment", null: true
     end
   end
 end
--- decidim-core/spec/types/attachment_type_spec.rb
+++ decidim-core/spec/types/attachment_type_spec.rb
@@ -8,61 +8,104 @@ module Decidim
     describe AttachmentType do
       include_context "with a graphql class type"
 
-      let(:title) { { en: "Participation guidelines", es: "Pautas de participación", ca: "Pautes de participació" } }
-      let(:description) { { en: "Read through these guidelines carefully.", es: "Lea atentamente estas pautas.", ca: "Llegiu atentament aquestes directrius." } }
-      let(:url) { "https://foo.bar/baz" }
-      let(:file_type) { "image" }
-      let(:thumbnail) { "https://foo.bar/baz.thumb" }
+      let(:model) { create(:attachment) }
 
-      let(:model) do
-        double(title:, description:, url:, file_type:, thumbnail_url: thumbnail)
-      end
+      include_examples "timestamps interface"
 
-      describe "title" do
-        let(:query) { "{ title { translations { locale text } } }" }
+      describe "content_type" do
+        let(:query) { "{ contentType }" }
 
-        it "returns the attachment's url" do
-          expect(response).to eq("title" => { "translations" => title.map { |locale, text| { "locale" => locale.to_s, "text" => text } } })
+        it "returns the content type of this attachment" do
+          expect(response["contentType"]).to eq(model.content_type)
         end
       end
 
       describe "description" do
-        let(:query) { "{ description { translations { locale text } } }" }
+        let(:query) { '{ description { translation(locale: "en")}}' }
 
-        it "returns the attachment's url" do
-          expect(response).to eq("description" => { "translations" => description.map { |locale, text| { "locale" => locale.to_s, "text" => text } } })
+        it "returns the description of this attachment" do
+          expect(response["description"]["translation"]).to eq(translated(model.description))
         end
       end
 
-      describe "url" do
-        let(:query) { "{ url }" }
+      describe "file_size" do
+        let(:query) { "{ fileSize }" }
 
-        it "returns the attachment's url" do
-          expect(response).to eq("url" => url)
+        it "returns the file size of this attachment" do
+          expect(response["fileSize"]).to eq(model.file_size)
         end
       end
 
-      describe "type" do
-        let(:query) { "{ type }" }
+      describe "id" do
+        let(:query) { "{ id }" }
 
-        it "returns the attachment's type" do
-          expect(response).to eq("type" => file_type)
+        it "returns the id of this attachment" do
+          expect(response).to eq("id" => model.id.to_s)
+        end
+      end
+
+      describe "link" do
+        let(:query) { "{ link }" }
+        let(:model) { create(:attachment, :with_link) }
+
+        it "returns the link of this attachment" do
+          expect(response).to eq("link" => model.link)
+        end
+
+        context "when not available" do
+          let(:model) { create(:attachment, :with_pdf) }
+
+          it "returns nil" do
+            expect(response).to eq("link" => nil)
+          end
         end
       end
 
       describe "thumbnail" do
         let(:query) { "{ thumbnail }" }
 
-        it "returns the attachment's thumbnail" do
-          expect(response).to eq("thumbnail" => thumbnail)
+        it "returns the thumbnail of this attachment" do
+          expect(response).to eq("thumbnail" => model.thumbnail_url)
         end
 
         context "when not available" do
-          let(:thumbnail) { nil }
+          let(:model) { create(:attachment, :with_pdf) }
 
           it "returns nil" do
             expect(response).to eq("thumbnail" => nil)
           end
+        end
+      end
+
+      describe "title" do
+        let(:query) { '{ title { translation(locale: "en")}}' }
+
+        it "returns the title of this attachment" do
+          expect(response["title"]["translation"]).to eq(translated(model.title))
+        end
+      end
+
+      describe "type" do
+        let(:query) { "{ type }" }
+
+        it "returns the type of this attachment" do
+          expect(response).to eq("type" => model.file_type)
+        end
+      end
+
+      describe "url" do
+        let(:query) { "{ url }" }
+
+        it "returns the url of this attachment" do
+          expect(response).to eq("url" => model.url)
+        end
+      end
+
+      describe "weight" do
+        let(:query) { "{ weight }" }
+
+        it "returns the weight of this attachment" do
+          expect(response).to eq("weight" => model.weight)
         end
       end
     end
