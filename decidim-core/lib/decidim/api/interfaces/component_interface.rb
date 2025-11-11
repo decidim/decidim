4e6fc8c386b10a224bb02c9344a63973b3382263
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-11-11 23:54:58 +0200

    Apply suggestions from code review
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-core/lib/decidim/api/interfaces/component_interface.rb
+++ decidim-core/lib/decidim/api/interfaces/component_interface.rb
@@ -9,10 +9,10 @@ module Decidim
       implements Decidim::Core::TimestampsInterface
 
       field :id, ID, "The unique id of this component", null: false
-      field :name, TranslatedFieldType, "The name of this component.", null: false
+      field :name, TranslatedFieldType, "The name of this component", null: false
       field :participatory_space, ParticipatorySpaceType, "The participatory space of this component", null: false
       field :published_at, Decidim::Core::DateTimeType, "The publication timestamp of this component", null: false
-      field :url, String, "The URL of this component.", null: false
+      field :url, String, "The URL of this component", null: false
       field :visible, GraphQL::Types::Boolean, "the visibility status of this component", null: true, method: :visible?
       field :weight, Integer, "The weight of this component", null: false
 
