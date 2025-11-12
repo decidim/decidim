b330bcd1dd379c49affc5ce2adb249889594a867
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2025-11-12 15:42:42 +0100

    Remove trailing whitespace

--- decidim-participatory_processes/db/migrate/20251112113613_remove_legacy_images_from_participatory_processes_module.rb
+++ decidim-participatory_processes/db/migrate/20251112113613_remove_legacy_images_from_participatory_processes_module.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
- 
 class RemoveLegacyImagesFromParticipatoryProcessesModule < ActiveRecord::Migration[7.2]
   def change
     remove_column :decidim_participatory_processes, :hero_image, :string
