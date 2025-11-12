e33f5a221a66f855b9afd45e826b29fda0412a63
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-11-12 12:17:14 +0200

    Fix test suite

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+/* eslint id-length: ["error", { "exceptions": ["d"] }] */
+
+const fs = require("fs");
+const path = require("path");
+
+const root = __dirname;
+const moduleDirs = ["node_modules"];
+fs.readdirSync(root, { withFileTypes: true }).
+  filter((d) => d.isDirectory() && (/^decidim-(.*)$/).test(d.name)).
+  forEach((d) => moduleDirs.push(path.join("<rootDir>", d.name, "app/packs")));
+
+module.exports = {
+  testEnvironment: "jsdom",
+  testEnvironmentOptions: {
+    "url": "https://decidim.dev/"
+  },
+  setupFiles: [
+    "<rootDir>/decidim-core/spec/js/entry_test.js",
+    "raf/polyfill"
+  ],
+  moduleFileExtensions: [
+    "js"
+  ],
+  moduleNameMapper: {
+    "\\.(scss|css|less)$": "identity-obj-proxy"
+  },
+  transform: {
+    "\\.yml$": "yaml-jest",
+    "\\.js$": "babel-jest"
+  },
+  testRegex: "\\.(test|spec)\\.js$",
+  moduleDirectories: moduleDirs
+};
