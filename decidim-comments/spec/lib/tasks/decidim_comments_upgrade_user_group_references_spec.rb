0133c7faac90a99a3cb0aeddfd970312bdc8e3d6
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-11-13 15:05:52 +0100

    Refactor examples to make them clearer and easy to work with the Hashes

--- decidim-comments/spec/lib/tasks/decidim_comments_upgrade_user_group_references_spec.rb
+++ decidim-comments/spec/lib/tasks/decidim_comments_upgrade_user_group_references_spec.rb
@@ -4,35 +4,38 @@ require "spec_helper"
 require "decidim/proposals/test/factories"
 
 describe "rake decidim_comments:upgrade:update_user_group_references", type: :task do
-  let!(:comment) { create(:comment, body: { en: body }) }
+  let!(:comment) { create(:comment, body:) }
 
   describe "with a normal body" do
-    let(:body) { "This is a normal body" }
+    let(:body) { { "en" => "This is a normal body" } }
+    let(:result) { { "en" => "This is a normal body" } }
 
     it "does not do anything" do
-      expect(comment.reload.body).to eq({ "en" => "This is a normal body" })
+      expect(comment.reload.body).to eq(body)
       task.execute
-      expect(comment.reload.body).to eq({ "en" => "This is a normal body" })
+      expect(comment.reload.body).to eq(result)
     end
   end
 
   describe "with a User reference in the body" do
-    let(:body) { "This is a body mentioning gid://decidim-test/Decidim::User/9" }
+    let(:body) { { "en" => "This is a body mentioning gid://decidim-test/Decidim::User/9" } }
+    let(:result) { { "en" => "This is a body mentioning gid://decidim-test/Decidim::User/9" } }
 
     it "does not do anything" do
-      expect(comment.reload.body).to eq({ "en" => "This is a body mentioning gid://decidim-test/Decidim::User/9" })
+      expect(comment.reload.body).to eq(body)
       task.execute
-      expect(comment.reload.body).to eq({ "en" => "This is a body mentioning gid://decidim-test/Decidim::User/9" })
+      expect(comment.reload.body).to eq(result)
     end
   end
 
   describe "with a UserGroup reference in the body" do
-    let(:body) { "This is a body mentioning gid://decidim-development-app/Decidim::UserGroup/5" }
+    let(:body) { { "en" => "This is a body mentioning gid://decidim-development-app/Decidim::UserGroup/5" } }
+    let(:result) { { "en" => "This is a body mentioning gid://decidim-development-app/Decidim::User/5" } }
 
     it "changes the reference to User" do
-      expect(comment.reload.body).to eq({ "en" => "This is a body mentioning gid://decidim-development-app/Decidim::UserGroup/5" })
+      expect(comment.reload.body).to eq(body)
       task.execute
-      expect(comment.reload.body).to eq({ "en" => "This is a body mentioning gid://decidim-development-app/Decidim::User/5" })
+      expect(comment.reload.body).to eq(result)
     end
   end
 end
