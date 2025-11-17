63aa61f5b13b51870f4eaee534ed9e46fe9bfc6a
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-11-17 08:31:11 +0100

    Fix rubocop offenses

--- decidim-comments/db/data/20251117070404_update_user_groups_references.rb
+++ decidim-comments/db/data/20251117070404_update_user_groups_references.rb
@@ -2,7 +2,7 @@
 
 class UpdateUserGroupsReferences < ActiveRecord::Migration[7.2]
   def up
-    puts "Starting comment body updates..."
+    Rails.logger.info "Starting comment body updates..."
 
     updated_count = 0
     skipped_count = 0
@@ -11,62 +11,72 @@ class UpdateUserGroupsReferences < ActiveRecord::Migration[7.2]
     Decidim::Comments::Comment.find_each do |comment|
       next if comment.body.blank?
 
-      body_changed = false
-      updated_body = {}
-
-      # Process each locale in the body hash
-      comment.body.each do |locale, text|
-        if locale == "machine_translations" && text.is_a?(Hash)
-          # Handle machine_translations nested hash
-          updated_translations = {}
-          text.each do |translation_locale, translation_text|
-            next if translation_text.blank?
-
-            updated_translation = translation_text.gsub(
-              %r{gid://([^/]+)/Decidim::UserGroup/(\d+)},
-              'gid://\1/Decidim::User/\2'
-            )
-
-            updated_translations[translation_locale] = updated_translation
-            body_changed = true if updated_translation != translation_text
-          end
-          updated_body[locale] = updated_translations
-        else
-          # Handle regular locale strings
-          next if text.blank?
-
-          updated_text = text.gsub(
-            %r{gid://([^/]+)/Decidim::UserGroup/(\d+)},
-            'gid://\1/Decidim::User/\2'
-          )
-
-          updated_body[locale] = updated_text
-          body_changed = true if updated_text != text
-        end
-      end
+      updated_body = process_comment_body(comment.body)
+      body_changed = updated_body != comment.body
 
       if body_changed
         comment.update_column(:body, updated_body) # rubocop:disable Rails/SkipsModelValidations
         updated_count += 1
-        puts "✓ Updated comment ##{comment.id}"
+        Rails.logger.info "✓ Updated comment ##{comment.id}"
       else
         skipped_count += 1
       end
     rescue StandardError => e
       error_count += 1
-      puts "✗ Error updating comment ##{comment.id}: #{e.message}"
+      Rails.logger.error "✗ Error updating comment ##{comment.id}: #{e.message}"
     end
 
-    puts "=" * 50
-    puts "Comment body update completed!"
-    puts "=" * 50
-    puts "Updated: #{updated_count}"
-    puts "Skipped: #{skipped_count}"
-    puts "Errors: #{error_count}"
-    puts "=" * 50
+    log_summary(updated_count, skipped_count, error_count)
   end
 
   def down
     raise ActiveRecord::IrreversibleMigration
+  end
+
+  private
+
+  def process_comment_body(body)
+    updated_body = {}
+
+    body.each do |locale, text|
+      updated_body[locale] = if locale == "machine_translations" && text.is_a?(Hash)
+                               process_machine_translations(text)
+                             else
+                               replace_user_group_references(text)
+                             end
+    end
+
+    updated_body
+  end
+
+  def process_machine_translations(translations)
+    updated_translations = {}
+
+    translations.each do |translation_locale, translation_text|
+      next if translation_text.blank?
+
+      updated_translations[translation_locale] = replace_user_group_references(translation_text)
+    end
+
+    updated_translations
+  end
+
+  def replace_user_group_references(text)
+    return text if text.blank?
+
+    text.gsub(
+      %r{gid://([^/]+)/Decidim::UserGroup/(\d+)},
+      'gid://\1/Decidim::User/\2'
+    )
+  end
+
+  def log_summary(updated_count, skipped_count, error_count)
+    Rails.logger.info "=" * 50
+    Rails.logger.info "Comment body update completed!"
+    Rails.logger.info "=" * 50
+    Rails.logger.info "Updated: #{updated_count}"
+    Rails.logger.info "Skipped: #{skipped_count}"
+    Rails.logger.info "Errors: #{error_count}"
+    Rails.logger.info "=" * 50
   end
 end
