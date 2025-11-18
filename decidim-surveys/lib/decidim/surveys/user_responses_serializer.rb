f1ec158b4cf8c48c61fed215afe7bdd98cdf71b5
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-11-18 10:42:07 +0100

    translations for registered and unregistered added

--- decidim-surveys/lib/decidim/surveys/user_responses_serializer.rb
+++ decidim-surveys/lib/decidim/surveys/user_responses_serializer.rb
@@ -13,7 +13,7 @@ module Decidim
           id: response.session_token,
           created_at: response.created_at,
           ip_hash: response.ip_hash,
-          user_status: (response.decidim_user_id.present? ? "Registered" : "Unregistered"),
+          user_status: translated_attribute(response.decidim_user_id.present? ? "Registered" : "Unregistered"),
           question: question_text(response),
           body: normalize_body(response)
         }
