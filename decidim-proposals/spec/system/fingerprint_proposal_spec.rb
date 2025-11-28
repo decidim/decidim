0da4730d162e2e785f6abc34f62548ddac807baa
JoonasAapro <110532525+JoonasAapro@users.noreply.github.com>
2025-11-28 13:38:09 +0200

    Prevent whitespace collapse in fingerprint source in fingerprint modal

--- decidim-proposals/spec/system/fingerprint_proposal_spec.rb
+++ decidim-proposals/spec/system/fingerprint_proposal_spec.rb
@@ -10,4 +10,12 @@ describe "Fingerprint proposal" do
   end
 
   include_examples "fingerprint"
+
+  context "when proposal body text has multiple spaces in a row" do
+    let!(:fingerprintable) do
+      create(:proposal, component:, body: "Body text with extra    space")
+    end
+
+    include_examples "consistent fingerprint"
+  end
 end
