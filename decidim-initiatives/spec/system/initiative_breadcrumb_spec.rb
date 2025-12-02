ce20777c4734136e08de262e7850d86041d5bf66
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-12-02 14:01:20 +0200

    Show processes groups in breadcrumb with components
    * Fix Assembly breadcrumb
    
    * Fix Assembly breadcrumb
    
    * Add specs + Groups breadcrumbs
    
    * Add initiatives breadcrumb specs
    
    * Apply suggestions from code review
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

---
+++ decidim-initiatives/spec/system/initiative_breadcrumb_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Initiative Breadcrumb" do
+  let(:organization) { create(:organization) }
+  let(:participatory_space) { create(:initiative, :open, organization:) }
+  let(:component) { create(:meeting_component, :published, participatory_space:) }
+  let(:router) { Decidim::EngineRouter.main_proxy(component) }
+  let!(:meeting) { create(:meeting, :published, component:) }
+
+  before do
+    switch_to_host(organization.host)
+  end
+
+  scenario "shows breadcrumb with only initiative" do
+    visit decidim_initiatives.initiative_path(participatory_space)
+
+    within ".menu-bar" do
+      expect(page).to have_content("Initiatives")
+      expect(page).to have_content(translated(participatory_space.title))
+    end
+  end
+
+  scenario "shows breadcrumb with initiative and component" do
+    visit router.root_path
+
+    within ".menu-bar" do
+      expect(page).to have_content("Initiatives")
+      expect(page).to have_content(translated(participatory_space.title))
+      expect(page).to have_content(translated(component.name))
+    end
+  end
+end
