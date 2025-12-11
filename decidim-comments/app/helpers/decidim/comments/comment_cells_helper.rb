48a1d04565e05d893b3f0ae013ea2f81905b9f5e
Leo <leo.storey@mainiotech.fi>
2025-12-11 09:40:06 +0200

    reverting to see test outcome

--- decidim-comments/app/helpers/decidim/comments/comment_cells_helper.rb
+++ decidim-comments/app/helpers/decidim/comments/comment_cells_helper.rb
@@ -10,7 +10,8 @@ module Decidim
       end
 
       def resource_link_text
-        translated_attribute(comment.body)
+        # translated_attribute(comment.body)
+        comment.formatted_body
       end
 
       def resource_link_path
