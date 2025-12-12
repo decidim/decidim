6d1a7ec69f1f4f1e13b49f4cb099313736bcc8ac
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-12-12 09:38:08 +0100

    Fix specs

--- decidim-core/spec/services/decidim/download_your_data_exporter_spec.rb
+++ decidim-core/spec/services/decidim/download_your_data_exporter_spec.rb
@@ -16,7 +16,7 @@ module Decidim
         decidim-identities-
         decidim-messaging-conversations-
         decidim-notifications-
-        decidim-members-
+        decidim-participatoryspace-members-
         decidim-reports-
         decidim-users-
         decidim-meetings-registrations-
