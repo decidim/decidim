467430ca6dc5e46d1065704f1df28409b5e3ae4d
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-12-12 13:56:47 +0200

    Extract elections

--- decidim-elections/spec/system/user_votes_in_an_election_spec.rb
+++ decidim-elections/spec/system/user_votes_in_an_election_spec.rb
@@ -94,13 +94,6 @@ describe "Dashboard" do
     context "when the election has finished" do
       let(:election) { create(:election, :published, :finished, :with_internal_users_census) }
 
-      it "shows the correct information in breadcrumb" do
-        within(".menu-bar") do
-          expect(page).to have_content(translated(election.component.name))
-          expect(page).to have_content(translated(election.title))
-        end
-      end
-
       it "does not allow to vote" do
         expect(page).to have_no_link("Vote")
         expect(page).to have_no_content("You have already voted.")
