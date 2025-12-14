2ad1f69c6b67cf48266b782cebb79c37aa496dda
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-12-14 06:59:38 +0200

    Add Decidim::Api::Errors::AttributeValidationError

--- decidim-api/lib/decidim/api/types.rb
+++ decidim-api/lib/decidim/api/types.rb
@@ -10,6 +10,7 @@ module Decidim
     autoload :ComponentMutationType, "decidim/api/component_mutation_type"
 
     module Errors
+      autoload :AttributeValidationError, "decidim/api/errors/attribute_validation_error"
       autoload :MutationNotAuthorizedError, "decidim/api/errors/mutation_not_authorized_error"
       autoload :NotFoundError, "decidim/api/errors/not_found_error"
       autoload :PermissionNotSetError, "decidim/api/errors/permission_not_set_error"
