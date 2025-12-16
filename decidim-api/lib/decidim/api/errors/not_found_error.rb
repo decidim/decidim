69b061f3ca39c08fbd279fba949c8666dff0eb91
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-12-16 11:44:04 +0200

    Fix error class tokens

--- decidim-api/lib/decidim/api/errors/not_found_error.rb
+++ decidim-api/lib/decidim/api/errors/not_found_error.rb
@@ -6,7 +6,7 @@ module Decidim
       # i18n-tasks-use t("decidim.api.errors.not_found")
       class NotFoundError < GraphQL::ExecutionError
         def to_h
-          super.merge({ "extensions" => { "code" => "NOT_FOUND" } })
+          super.merge({ "extensions" => { "code" => "NOT_FOUND_ERROR" } })
         end
       end
     end
