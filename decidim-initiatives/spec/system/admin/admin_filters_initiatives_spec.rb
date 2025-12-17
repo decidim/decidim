4b859a148ce4bb59b01a7da758dcfd22c68df6d7
Tom Greenwood <tom.t.greenwood@gmail.com>
2025-12-17 12:30:34 +0100

    new spec to filter creation in initiatives

---
+++ decidim-initiatives/spec/system/admin/admin_filters_initiatives_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Admin sorting initiatives" do
+  let(:organization) { create(:organization) }
+  let(:user) { create(:user, :admin, :confirmed, organization:) }
+
+  let!(:old_initiative) { create(:initiative, title: { en: "Old initiative" }, created_at: 3.weeks.ago, organization:) }
+  let!(:recent_initiative) { create(:initiative, title: { en: "Recent initiative" }, created_at: 1.day.ago, organization:) }
+  let!(:newest_initiative) { create(:initiative, title: { en: "Newest initiative" }, created_at: Time.current, organization:) }
+
+  before do
+    switch_to_host(organization.host)
+    login_as user, scope: :user
+    visit decidim_admin_initiatives.initiatives_path
+  end
+
+  context "when sorting initiatives by their creation" do
+    it "sorts by created_at descending by default" do
+      within "table thead" do
+        click_link "Created at"
+      end
+
+      titles = page.all("table tbody tr td:first-child")
+      expect(titles[0].text).to include("Newest initiative")
+      expect(titles[1].text).to include("Recent initiative")
+      expect(titles[2].text).to include("Old initiative")
+    end
+
+    it "sorts by created_at ascending when clicked again" do
+      within "table thead" do
+        click_link "Created at"
+        click_link "Created at"
+      end
+
+      titles = page.all("table tbody tr td:first-child")
+      expect(titles[0].text).to include("Old initiative")
+      expect(titles[1].text).to include("Recent initiative")
+      expect(titles[2].text).to include("Newest initiative")
+    end
+  end
+end
