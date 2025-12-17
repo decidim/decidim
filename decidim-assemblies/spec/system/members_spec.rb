435e1567eebdc932998e4d6c7a03c06e39761ce4
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-12-17 10:14:31 +0100

    Refactor spec for future extraction

--- decidim-assemblies/spec/system/members_spec.rb
+++ decidim-assemblies/spec/system/members_spec.rb
@@ -8,6 +8,10 @@ describe "Assembly members" do
   let(:participatory_space) { assembly }
   let(:blocks_manifests) { [] }
 
+  let(:participatory_space_homepage_path) { decidim_assemblies.assembly_path(participatory_space, locale: I18n.locale) }
+  let(:members_path) { decidim_assemblies.assembly_members_path(participatory_space, locale: I18n.locale) }
+  let(:unexisting_participatory_space_members_path) { decidim_assemblies.assembly_members_path(assembly_slug: 999_999_999, locale: I18n.locale) }
+
   let(:user) { create(:user, organization: participatory_space.organization) }
   let(:ceased_user) { create(:user, organization: participatory_space.organization) }
 
@@ -15,16 +19,16 @@ describe "Assembly members" do
     switch_to_host(organization.host)
   end
 
-  context "when there are no assembly members and directly accessing from URL" do
+  context "when there are no members and directly accessing from URL" do
     it_behaves_like "a 404 page" do
-      let(:target_path) { decidim_assemblies.assembly_members_path(assembly, locale: I18n.locale) }
+      let(:target_path) { members_path }
     end
   end
 
-  context "when there are no assembly members and accessing from the assembly homepage" do
+  context "when there are no members and accessing from the space homepage" do
     context "and the main data content block is disabled" do
       it "the menu nav is not shown" do
-        visit decidim_assemblies.assembly_path(assembly, locale: I18n.locale)
+        visit participatory_space_homepage_path
 
         expect(page).to have_no_css(".participatory-space__nav-container")
       end
@@ -34,34 +38,34 @@ describe "Assembly members" do
       let(:blocks_manifests) { ["main_data"] }
 
       it "the menu link is not shown" do
-        visit decidim_assemblies.assembly_path(assembly, locale: I18n.locale)
+        visit participatory_space_homepage_path
 
         expect(page).to have_no_content("Members")
       end
     end
   end
 
-  context "when the assembly does not exist" do
+  context "when the participatory space does not exist" do
     it_behaves_like "a 404 page" do
-      let(:target_path) { decidim_assemblies.assembly_members_path(assembly_slug: 999_999_999, locale: I18n.locale) }
+      let(:target_path) { unexisting_participatory_space_members_path }
     end
   end
 
-  context "when there are some assembly members and all are unpublished" do
+  context "when there are some members and all are unpublished" do
     before do
       create(:member, user:, participatory_space:, published: false)
     end
 
     context "and directly accessing from URL" do
       it_behaves_like "a 404 page" do
-        let(:target_path) { decidim_assemblies.assembly_members_path(assembly, locale: I18n.locale) }
+        let(:target_path) { members_path }
       end
     end
 
     context "and accessing from the homepage" do
       context "and the main data content block is disabled" do
         it "the menu nav is not shown" do
-          visit decidim_assemblies.assembly_path(assembly, locale: I18n.locale)
+          visit participatory_space_homepage_path
 
           expect(page).to have_no_css(".participatory-space__nav-container")
         end
@@ -71,7 +75,7 @@ describe "Assembly members" do
         let(:blocks_manifests) { ["main_data"] }
 
         it "the menu link is not shown" do
-          visit decidim_assemblies.assembly_path(assembly, locale: I18n.locale)
+          visit participatory_space_homepage_path
 
           expect(page).to have_no_content("Members")
         end
@@ -79,18 +83,18 @@ describe "Assembly members" do
     end
   end
 
-  context "when there are some published assembly members" do
+  context "when there are some published members" do
     let!(:member) { create(:member, user:, participatory_space:, published: true) }
     let!(:ceased_member) { create(:member, user: ceased_user, participatory_space:, published: false) }
 
     before do
-      visit decidim_assemblies.assembly_members_path(assembly, locale: I18n.locale)
+      visit members_path
     end
 
-    context "and accessing from the assembly homepage" do
+    context "and accessing from the space homepage" do
       context "and the main data content block is disabled" do
         it "the menu nav is not shown" do
-          visit decidim_assemblies.assembly_path(assembly, locale: I18n.locale)
+          visit participatory_space_homepage_path
 
           expect(page).to have_no_css(".participatory-space__nav-container")
         end
@@ -100,18 +104,18 @@ describe "Assembly members" do
         let(:blocks_manifests) { ["main_data"] }
 
         it "the menu link is shown" do
-          visit decidim_assemblies.assembly_path(assembly, locale: I18n.locale)
+          visit participatory_space_homepage_path
 
           within ".participatory-space__nav-container" do
             expect(page).to have_content("Members")
             click_on "Members"
           end
 
-          expect(page).to have_current_path decidim_assemblies.assembly_members_path(assembly, locale: I18n.locale)
+          expect(page).to have_current_path members_path
         end
       end
 
-      it "lists all the non ceased assembly members" do
+      it "lists all the non ceased members" do
         within "#assembly_members-grid" do
           expect(page).to have_css(".profile__user", count: 1)
 
