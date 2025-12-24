c482a261fc3939a3b12b308f7793bdfa4aa947fd
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-12-24 10:07:56 +0200

    Fix attribute retrieval

--- decidim-proposals/lib/decidim/api/mutations/create_proposal_type.rb
+++ decidim-proposals/lib/decidim/api/mutations/create_proposal_type.rb
@@ -15,9 +15,7 @@ module Decidim
       def resolve(attributes:, locale:, toggle_translations:)
         set_locale(locale:, toggle_translations:)
 
-        params = attributes.to_h.slice(:title, :body, :address, :latitude, :longitude, :taxonomies)
-
-        params[:taxonomies] = Decidim::Taxonomy.where(organization: current_organization, id: params[:taxonomies]).pluck(:id) if params[:taxonomies]
+        params = extract_from(attributes)
 
         form = form(Decidim::Proposals::ProposalForm).from_params(params)
 
@@ -46,6 +44,19 @@ module Decidim
         end
 
         true
+      end
+
+      private
+
+      def extract_from(attributes)
+        title = attributes.to_h.fetch(:title, translated_attribute(object.title))
+        body = attributes.to_h.fetch(:title, translated_attribute(object.body))
+        taxonomies = Decidim::Taxonomy.where(organization: current_organization, id: attributes.to_h.fetch(:taxonomies, [])).pluck(:id)
+        address = attributes.to_h.fetch(:address, object.address)
+        latitude = attributes.to_h.fetch(:latitude, object.latitude)
+        longitude = attributes.to_h.fetch(:longitude, object.longitude)
+
+        { title:, body:, address:, latitude:, longitude:, taxonomies: }
       end
     end
   end
