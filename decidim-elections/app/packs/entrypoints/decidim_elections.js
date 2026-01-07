1447dbc6d6a9db478681054be423cb46e2ae1dc5
Anna Topalidi <60363870+antopalidi@users.noreply.github.com>
2026-01-07 16:12:31 +0100

    Fix auto-redirect when question voting closes

--- decidim-elections/app/packs/entrypoints/decidim_elections.js
+++ decidim-elections/app/packs/entrypoints/decidim_elections.js
@@ -1,5 +1,6 @@
 import "src/decidim/elections/waiting_room.js"
 import "src/decidim/elections/live_results_update.js";
+import "src/decidim/elections/question_status_checker.js";
 
 // Images
 require.context("../images", true)
