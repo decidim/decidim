99b1a91281e7b00cc1383ba9b61657b2b36ae9a4
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-01-08 16:13:29 +0100

    Disabled warning of link & underline decleared twice by assigning them false within the Decidim kit.

--- decidim-core/app/packs/src/decidim/editor/extensions/decidim_kit/index.js
+++ decidim-core/app/packs/src/decidim/editor/extensions/decidim_kit/index.js
@@ -43,7 +43,9 @@ export default Extension.create({
         heading: false,
         bold: false,
         orderedList: false,
-        codeBlock: false
+        codeBlock: false,
+        link: false,
+        underline: false
       }),
       CharacterCount.configure(this.options.characterCount),
       Link.configure({ openOnClick: false, ...this.options.link }),
