3784613f4f479e6eaa9a94adf9f135c655ad3b71
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-01-13 01:20:49 +0200

    Apply suggestions from code review
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-accountability/spec/system/admin/admin_publishes_component_spec.rb
+++ decidim-accountability/spec/system/admin/admin_publishes_component_spec.rb
@@ -2,7 +2,7 @@
 
 require "spec_helper"
 
-describe "AdminAccess" do
+describe "Admin publishes component" do
   let(:manifest_name) { "accountability" }
   let!(:result) { create(:result, component:) }
 
