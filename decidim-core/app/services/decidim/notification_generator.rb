1fd2ce0826559fb87190a8bbe412c8272d9e4b09
Renato <renato@octree.ch>
2026-01-15 11:35:24 -0300

    fix rubocop review

--- decidim-core/app/services/decidim/notification_generator.rb
+++ decidim-core/app/services/decidim/notification_generator.rb
@@ -42,6 +42,7 @@ module Decidim
 
       affected_users.each do |recipient|
         next unless recipient.respond_to?(:notification_types)
+
         generate_notification_for(recipient, user_role: :affected_user) if ["all", "own-only"].include?(recipient.notification_types)
       end
     end
