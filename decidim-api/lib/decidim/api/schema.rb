6871e4fccd7bccd11c23dc6137909f5ae60ca52f
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-01-20 18:40:23 +0200

    Fix specs

--- decidim-api/lib/decidim/api/schema.rb
+++ decidim-api/lib/decidim/api/schema.rb
@@ -7,12 +7,14 @@ module Decidim
       mutation(MutationType)
       query(QueryType)
 
+      introspection IntrospectionAnalyzer
       query_analyzer RecursionAnalyzer
+      query_analyzer AliasAnalyzer
 
       default_max_page_size Decidim::Api.schema_max_per_page
       max_depth Decidim::Api.schema_max_depth
       max_complexity Decidim::Api.schema_max_complexity
-      
+
       orphan_types(Api.orphan_types)
     end
   end
