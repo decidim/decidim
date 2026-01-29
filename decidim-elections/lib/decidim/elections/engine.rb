fe826884c2cd8efb1376234d7959bbdf8080cc5c
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-01-29 16:45:17 +0100

    Reverted engine path back to elections instead of core

--- decidim-elections/lib/decidim/elections/engine.rb
+++ decidim-elections/lib/decidim/elections/engine.rb
@@ -29,7 +29,7 @@ module Decidim
       end
 
       initializer "decidim_elections.register_icons" do
-        Decidim.icons.register(name: "Decidim::Elections::Election", icon: "file-paper-2-line", description: "Elections", category: "activity", engine: :core)
+        Decidim.icons.register(name: "Decidim::Elections::Election", icon: "file-paper-2-line", description: "Elections", category: "activity", engine: :elections)
       end
 
       initializer "decidim_elections.add_cells_view_paths" do
