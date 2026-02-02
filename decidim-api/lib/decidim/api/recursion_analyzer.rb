85c350579811baa2b1fa471f9509a1d2dc396919
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-02-02 14:04:26 +0200

    Fix recursion error on comments

--- decidim-api/lib/decidim/api/recursion_analyzer.rb
+++ decidim-api/lib/decidim/api/recursion_analyzer.rb
@@ -10,7 +10,7 @@
 module Decidim
   module Api
     class RecursionAnalyzer < GraphQL::Analysis::AST::Analyzer
-      IGNORED_FIELDS = %w(node edges nodes ofType).freeze
+      IGNORED_FIELDS = %w(node edges nodes ofType comments).freeze
       RECURSION_THRESHOLD = 2
 
       def initialize(query)
