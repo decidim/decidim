8cf6fbceb1d9de32b68a07876ee68e2c8cd57018
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-02-03 21:24:20 +0200

    Add jsDoc for header account menu

--- decidim-core/app/packs/src/decidim/controllers/header_account_menu/controller.js
+++ decidim-core/app/packs/src/decidim/controllers/header_account_menu/controller.js
@@ -1,5 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
+/**
+ * changes the value "menu" of role attribute set by a11y on div dropdown-menu-account and
+ * dropdown-menu-account-mobile which are inappropriate for accessibility
+ */
 export default class extends Controller {
   connect() {
     const dropdownMobileDiv = document.querySelector("#dropdown-menu-account-mobile");
