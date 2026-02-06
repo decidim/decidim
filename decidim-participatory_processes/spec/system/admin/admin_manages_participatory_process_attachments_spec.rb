bfb11f348da80c3ef9cbef6f22bdd1636dabf4f9
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-02-06 11:54:52 +0200

    Fix can_participate? into ParticipatoryProcessStep
    * Fix can_participate into ParticipatoryProcessStep
    
    * Add spec for attachments
    
    * Revert "Add spec for attachments"
    
    This reverts commit 53b6cd6ec349ba3ee8429c2f0e85e9a06c9b606e.
    
    * Refactor attachment specs
    
    * Update decidim-participatory_processes/spec/system/participatory_process_steps_spec.rb
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-participatory_processes/spec/system/admin/admin_manages_participatory_process_attachments_spec.rb
+++ decidim-participatory_processes/spec/system/admin/admin_manages_participatory_process_attachments_spec.rb
@@ -5,5 +5,44 @@ require "spec_helper"
 describe "Admin manages participatory process attachments" do
   include_context "when admin administrating a participatory process"
 
-  it_behaves_like "manage process attachments examples"
+  it_behaves_like "manage process attachments examples" do
+    context "when checking notifications" do
+      it "successfully displays the notification" do
+        create(:follow, user:, followable: attached_to)
+
+        click_on "New attachment"
+
+        within ".new_attachment" do
+          fill_in_i18n(
+            :attachment_title,
+            "#attachment-title-tabs",
+            en: "Very Important Document",
+            es: "Documento Muy Importante",
+            ca: "Document Molt Important"
+          )
+
+          fill_in_i18n(
+            :attachment_description,
+            "#attachment-description-tabs",
+            en: "This document contains important information",
+            es: "Este documento contiene información importante",
+            ca: "Aquest document conté informació important"
+          )
+        end
+
+        dynamically_attach_file(:attachment_file, Decidim::Dev.asset("Exampledocument.pdf"))
+
+        within ".new_attachment" do
+          find("*[type=submit]").click
+        end
+
+        expect(page).to have_admin_callout("successfully")
+
+        wait_enqueued_jobs do
+          visit decidim.notifications_path
+          expect(page).to have_content("A new document has been added to")
+        end
+      end
+    end
+  end
 end
