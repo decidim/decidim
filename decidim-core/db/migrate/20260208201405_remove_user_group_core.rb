54e2b030147c27d6242b8c795c29f710496d5367
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-02-08 21:12:39 +0200

    Remove UserGroups fields in Core

--- decidim-core/db/migrate/20260208201405_remove_user_group_core.rb
+++ decidim-core/db/migrate/20260208201405_remove_user_group_core.rb
@@ -4,10 +4,14 @@ class RemoveUserGroupCore < ActiveRecord::Migration[7.0]
   def up
     remove_index :decidim_coauthorships, :decidim_user_group_id
     remove_column :decidim_coauthorships, :decidim_user_group_id
+    remove_index :decidim_likes, :decidim_user_group_id
+    remove_column :decidim_likes, :decidim_user_group_id
   end
 
   def down
     add_column :decidim_coauthorships, :decidim_user_group_id, :integer
     add_index :decidim_coauthorships, :decidim_user_group_id
+    add_column :decidim_likes, :decidim_user_group_id, :integer
+    add_index :decidim_likes, :decidim_user_group_id
   end
 end
