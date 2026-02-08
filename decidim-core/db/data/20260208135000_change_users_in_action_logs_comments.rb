a56e532615d28e3353fe966138fb4c7efdd49bdd
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-02-08 15:42:04 +0200

    Fix ActionLogs for Comments when author is a group

---
+++ decidim-core/db/data/20260208135000_change_users_in_action_logs_comments.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class ChangeUsersInActionLogsComments < ActiveRecord::Migration[7.2]
+  class ActionLog < ApplicationRecord
+    self.table_name = :decidim_action_logs
+
+    belongs_to :user, class_name: "Decidim::UserBaseEntity"
+
+    belongs_to :resource,
+               polymorphic: true,
+               optional: true
+  end
+
+  class Comment < ApplicationRecord
+    self.table_name = :decidim_comments_comments
+
+    belongs_to :author, polymorphic: true, foreign_key: "decidim_author_id", foreign_type: "decidim_author_type"
+  end
+
+  def up
+    ActionLog.where(resource_type: "Decidim::Comments::Comment").find_each do |action_log|
+      author = action_log.resource.author
+
+      next unless author.is_a?(Decidim::User)
+      next unless author.group?
+
+      action_log.user_id = author.id
+      action_log.extra["user"].merge!("name" => author.name, :nickname => author.nickname)
+      action_log.save!
+    end
+  end
+end
