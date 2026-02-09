d7d3c3b9e8a4868bc41505af9a6c7fd78e5b6f5b
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-02-09 02:09:00 +0200

    Patch the migration

--- decidim-core/db/migrate/20260208201406_remove_user_group_memberships.rb
+++ decidim-core/db/migrate/20260208201406_remove_user_group_memberships.rb
@@ -6,17 +6,25 @@ class RemoveUserGroupMemberships < ActiveRecord::Migration[7.0]
   end
 
   def down
-    create_table "decidim_user_group_memberships", id: :serial, force: :cascade do |t|
-      t.integer "decidim_user_id", null: false
-      t.integer "decidim_user_group_id", null: false
-      t.datetime "created_at", precision: nil, null: false
-      t.datetime "updated_at", precision: nil, null: false
-      t.string "role", default: "requested", null: false
-      t.index %w(decidim_user_group_id decidim_user_id), name: "index_user_group_memberships_group_id_user_id"
-      t.index ["decidim_user_group_id"], name: "index_decidim_user_group_memberships_on_decidim_user_group_id"
-      t.index %w(decidim_user_id decidim_user_group_id), name: "decidim_user_group_memberships_unique_user_and_group_ids", unique: true
-      t.index ["decidim_user_id"], name: "index_decidim_user_group_memberships_on_decidim_user_id"
-      t.index %w(role decidim_user_group_id), name: "decidim_group_membership_one_creator_per_group", unique: true, where: "((role)::text = 'creator'::text)"
+    create_table :decidim_user_group_memberships do |t|
+      t.references :decidim_user, null: false, index: true
+      t.references :decidim_user_group, null: false, index: true
+
+      t.timestamps
     end
+
+    add_index :decidim_user_group_memberships, [:decidim_user_id, :decidim_user_group_id], unique: true, name: "decidim_user_group_memberships_unique_user_and_group_ids"
+    add_column :decidim_user_group_memberships, :role, :string, default: "requested"
+    execute("UPDATE decidim_user_group_memberships SET role = 'creator'")
+    change_column_null :decidim_user_group_memberships, :role, false
+    add_index(
+      :decidim_user_group_memberships,
+      %w(role decidim_user_group_id),
+      where: "(role = 'creator')",
+      name: "decidim_group_membership_one_creator_per_group",
+      unique: true
+    )
+
+    add_index :decidim_user_group_memberships, [:decidim_user_group_id, :decidim_user_id], name: "index_user_group_memberships_group_id_user_id"
   end
 end
