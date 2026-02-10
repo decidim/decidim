f2559536820f984571b1383f24e9c6728277342c
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-02-10 14:53:47 +0100

    Fix the Stimulus controller loading from decidim-meetings

--- decidim-meetings/app/packs/entrypoints/decidim_meetings.js
+++ decidim-meetings/app/packs/entrypoints/decidim_meetings.js
@@ -1,10 +1,5 @@
-import "src/decidim/meetings/meetings_form"
-import "src/decidim/meetings/meetings_polls"
+import "src/decidim/meetings/meetings"
 import "src/decidim/forms/forms"
-import { definitionsFromContext } from "src/decidim/refactor/support/stimulus"
-
-const context = require.context("../src/decidim/meetings/controllers", true, /controller\.js$/)
-window.Stimulus.load(definitionsFromContext(context))
 
 // Images
 require.context("../images", true)
