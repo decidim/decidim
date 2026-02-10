4363c84d8ba770603ed1f0224055a0d929e4f359
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-02-10 18:07:40 +0100

    Clean-up specs

--- decidim-meetings/spec/system/show_spec.rb
+++ decidim-meetings/spec/system/show_spec.rb
@@ -57,39 +57,37 @@ describe "show", js: true do
 
     context "when meeting has many public participants" do
       let!(:users) { create_list(:user, 15, organization:) }
+      let(:show_more_text) { I18n.t("show_more", scope: "decidim.meetings.public_participants_list") }
+      let(:show_less_text) { I18n.t("show_less", scope: "decidim.meetings.public_participants_list") }
 
       before do
         users.each do |user|
           create(:registration, meeting:, user:, public_participation: true)
         end
-
-        visit resource_locator(meeting).path
-      end
-
-      it "shows a limited list with a toggle" do
-        within "#panel-participants" do
-          expect(page).to have_css("[data-participants-item]", count: 15, visible: :all)
-          expect(page).to have_text(I18n.t("show_more", scope: "decidim.meetings.public_participants_list"))
-        end
-      end
-
-      it "expands and collapses the list" do
-        within "#panel-participants" do
-          toggle = find("[data-participants-toggle]", visible: :all)
-          expect(page).to have_css(".meeting__public-participants-item:not(.hidden)", count: 12)
-          toggle.click
-          expect(page).to have_css(".meeting__public-participants-item:not(.hidden)", count: 15)
-          expect(toggle).to have_text(I18n.t("show_less", scope: "decidim.meetings.public_participants_list"))
-          toggle.click
-          expect(page).to have_css(".meeting__public-participants-item:not(.hidden)", count: 12)
-          expect(toggle).to have_text(I18n.t("show_more", scope: "decidim.meetings.public_participants_list"))
-        end
       end
 
       context "with a desktop" do
-        it "limits visible participants by viewport" do
+        before do
+          visit resource_locator(meeting).path
+        end
+
+        it "shows a limited list with a toggle" do
           within "#panel-participants" do
-            expect(page).to have_css(".meeting__public-participants-item:not(.hidden)", count: 12)
+            expect(page).to have_css("[data-participants-item]", count: 12)
+            expect(page).to have_css("[data-participants-item]", count: 15, visible: :all)
+          end
+        end
+
+        it "expands and collapses the list" do
+          within "#panel-participants" do
+            expect(page).to have_css("[data-participants-item]", count: 12)
+            expect(page).to have_text(show_more_text)
+            click_toggle_button
+            expect(page).to have_css("[data-participants-item]", count: 15)
+            expect(page).to have_text(show_less_text)
+            click_toggle_button
+            expect(page).to have_css("[data-participants-item]", count: 12)
+            expect(page).to have_text(show_more_text)
           end
         end
       end
@@ -102,23 +100,33 @@ describe "show", js: true do
 
         it "limits visible participants by viewport" do
           within "#panel-participants" do
-            expect(page).to have_css(".meeting__public-participants-item:not(.hidden)", count: 8)
+            expect(page).to have_css("[data-participants-item]", count: 8)
+            expect(page).to have_css("[data-participants-item]", count: 15, visible: :all)
           end
         end
 
         it "expands and collapses the list" do
           within "#panel-participants" do
-            toggle = find("[data-participants-toggle]", visible: :all)
-            expect(page).to have_css(".meeting__public-participants-item:not(.hidden)", count: 8)
-            toggle.click
-            expect(page).to have_css(".meeting__public-participants-item:not(.hidden)", count: 15)
-            expect(toggle).to have_text(I18n.t("show_less", scope: "decidim.meetings.public_participants_list"))
-            toggle.click
-            expect(page).to have_css(".meeting__public-participants-item:not(.hidden)", count: 8)
-            expect(toggle).to have_text(I18n.t("show_more", scope: "decidim.meetings.public_participants_list"))
+            expect(page).to have_css("[data-participants-item]", count: 8)
+            expect(page).to have_text(show_more_text)
+            click_toggle_button
+            expect(page).to have_css("[data-participants-item]", count: 15)
+            expect(page).to have_text(show_less_text)
+            click_toggle_button
+            expect(page).to have_css("[data-participants-item]", count: 8)
+            expect(page).to have_text(show_more_text)
           end
         end
       end
     end
+  end
+
+  private
+
+  def click_toggle_button
+    # Seems like the :iphone driver does not play nicely with a
+    # click_on("Show more") as the button has an icon inside
+    # as a workardound, we do this equivalent that works correctly with this driver
+    find("[data-participants-toggle]", visible: :all).send_keys(:enter)
   end
 end
