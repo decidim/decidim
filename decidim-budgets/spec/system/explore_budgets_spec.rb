386a735624d7ca37ecd4aad733664b63a78dd233
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2026-02-11 14:03:55 +0200

    Fix budgets list ordering

--- decidim-budgets/spec/system/explore_budgets_spec.rb
+++ decidim-budgets/spec/system/explore_budgets_spec.rb
@@ -37,9 +37,12 @@ describe "Explore Budgets", :slow do
     let!(:budgets) do
       1.upto(6).to_a.map { |x| create(:budget, component:, weight: x, total_budget: x * 10_000_000, description: { en: "This is budget #{x}" }) }
     end
+    let(:component_path) do
+      Decidim::EngineRouter.main_proxy(component).root_path(locale: I18n.locale, order: "lowest_cost")
+    end
 
     before do
-      visit_component
+      visit component_path
     end
 
     it "shows the component name in the sidebar" do
