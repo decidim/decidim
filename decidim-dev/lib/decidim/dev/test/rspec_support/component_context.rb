c19d0581a7b255de386eee2bb1449ac5b3c9185a
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-02-13 13:22:30 +0200

    Make sure resources are are when component is marked as marked as visible

--- decidim-dev/lib/decidim/dev/test/rspec_support/component_context.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/component_context.rb
@@ -164,7 +164,9 @@ shared_context "when publishing and unpublishing the component" do
         click_on "Hide from menu"
       end
 
-      expect(Decidim::SearchableResource.where(resource:).count).to be_zero
+      perform_enqueued_jobs
+
+      expect(Decidim::SearchableResource.where(resource:).count).to be_positive
 
       within "tr", text: title do
         find("button[data-controller='dropdown']").click
