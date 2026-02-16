49b8595d97a4ba011d4fad772c5d172c10715781
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-02-16 14:23:08 +0200

    Apply codeRabbit recommendations

--- decidim-forms/app/packs/src/decidim/forms/admin/forms.js
+++ decidim-forms/app/packs/src/decidim/forms/admin/forms.js
@@ -389,9 +389,12 @@ export default function createEditableForm() {
       autoLabelByPosition.run();
       autoButtonsByPosition.run();
 
-      document.querySelectorAll("select.language-change").forEach((container) => {
-        window.deprecate(container, "language-change", "select.language-change")
-      });
+      const fieldElement = $field[0];
+      if (fieldElement) {
+        fieldElement.querySelectorAll("select.language-change").forEach((container) => {
+          window.deprecate(container, "language-change", "select.language-change")
+        });
+      }
 
       // instead of initialize specific stuff, we send an event, with the DOM fragment we wanna update/refresh/bind
       document.dispatchEvent(new CustomEvent("ajax:loaded", { detail: $field[0] }));
