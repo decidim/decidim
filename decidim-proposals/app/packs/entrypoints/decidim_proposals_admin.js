d8a1e686c73eb37363c299f59cb0b6ff0e5d33a2
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-02-20 12:42:20 +0100

    Imported js entry point of import_proposals.

--- decidim-proposals/app/packs/entrypoints/decidim_proposals_admin.js
+++ decidim-proposals/app/packs/entrypoints/decidim_proposals_admin.js
@@ -1,3 +1,4 @@
 import "src/decidim/proposals/admin/proposals"
 import "src/decidim/proposals/admin/proposals_form"
 import "src/decidim/proposals/admin/proposals_merge"
+import "src/decidim/proposals/admin/import_proposals"
