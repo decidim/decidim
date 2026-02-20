70513fabc82b222f1a5015e42d6495721de37d9b
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-02-20 14:01:15 +0100

    Moved the proposals_merge resource route.

--- decidim-proposals/lib/decidim/proposals/admin_engine.rb
+++ decidim-proposals/lib/decidim/proposals/admin_engine.rb
@@ -23,12 +23,6 @@ module Decidim
             post :publish_answers
             post :update_multiple_answers, controller: "proposal_answers"
             get :manage_trash, controller: "proposals"
-            resource :proposals_import, only: [:new, :create] do
-              collection do
-                get :component_states
-              end
-            end
-            resource :proposals_merge, only: [:new, :create]
             resource :proposals_split, only: [:create]
             resource :evaluation_assignment, only: [:create, :destroy]
           end
@@ -39,6 +33,13 @@ module Decidim
             end
           end
         end
+
+        resource :proposals_import, only: [:new, :create] do
+          collection do
+            get :component_states
+          end
+        end
+        resource :proposals_merge, only: [:new, :create]
 
         resources :proposal_states
 
