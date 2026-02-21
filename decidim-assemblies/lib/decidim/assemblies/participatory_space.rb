84994b402c60882302eb14f661377cbeadffc01e
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2026-02-21 23:35:32 +0100

    Fix export for private spaces

--- decidim-assemblies/lib/decidim/assemblies/participatory_space.rb
+++ decidim-assemblies/lib/decidim/assemblies/participatory_space.rb
@@ -32,8 +32,8 @@ Decidim.register_participatory_space(:assemblies) do |participatory_space|
   end
 
   participatory_space.exports :assemblies do |export|
-    export.collection do |space, _user|
-      Decidim::Assembly.public_spaces.includes(:attachment_collections).where(id: space)
+    export.collection do |participatory_space, user|
+      Decidim::Assembly.visible_for(user).includes(:attachment_collections).where(id: participatory_space)
     end
 
     export.include_in_open_data = true
