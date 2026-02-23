be75a29bf219d391e04e3a8b493e00a582424a10
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-02-23 14:22:07 +0100

    Fix migrations

--- decidim-proposals/db/migrate/20210310120812_add_followable_counter_cache_to_collaborative_drafts.rb
+++ decidim-proposals/db/migrate/20210310120812_add_followable_counter_cache_to_collaborative_drafts.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
 class AddFollowableCounterCacheToCollaborativeDrafts < ActiveRecord::Migration[5.2]
+  class CollaborativeDraft < ApplicationRecord
+    self.table_name = "decidim_proposals_collaborative_drafts"
+  end
+
   def change
     add_column :decidim_proposals_collaborative_drafts, :follows_count, :integer, null: false, default: 0, index: true
 
     reversible do |dir|
       dir.up do
-        Decidim::Proposals::CollaborativeDraft.reset_column_information
-        Decidim::Proposals::CollaborativeDraft.find_each do |record|
+        CollaborativeDraft.reset_column_information
+        CollaborativeDraft.find_each do |record|
           record.class.reset_counters(record.id, :follows)
         end
       end
