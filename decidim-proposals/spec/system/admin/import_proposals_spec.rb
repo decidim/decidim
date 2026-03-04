28c6d1da6a1bc46249291155ce20b9eb0e8cb89b
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-03-04 09:18:53 +0100

    Updated spec to account for custom states to be detected in drop down

--- decidim-proposals/spec/system/admin/import_proposals_spec.rb
+++ decidim-proposals/spec/system/admin/import_proposals_spec.rb
@@ -103,5 +103,47 @@ describe "Import proposals" do
         expect(Decidim::Proposals::Proposal.where(component:).count).to eq(2)
       end
     end
+
+    context "with a custom state on the origin component" do
+      let(:custom_title) { { "en" => "Under review" } }
+      let!(:custom_state) do
+        create(:proposal_state, component: origin_component, token: "under_review", title: custom_title)
+      end
+
+      it "shows the custom state alongside the default ones" do
+        within ".import_proposals" do
+          select origin_component.name["en"], from: "Origin component"
+        end
+
+        within "#states-container" do
+          expect(page).to have_unchecked_field("Under review")
+          expect(page).to have_unchecked_field("Accepted")
+          expect(page).to have_unchecked_field("Not answered")
+        end
+      end
+
+      context "when importing proposals with the custom state" do
+        let!(:custom_state_on_target) do
+          create(:proposal_state, component:, token: "under_review", title: custom_title)
+        end
+        let!(:custom_proposals) { create_list(:proposal, 2, component: origin_component, state: "under_review") }
+        let!(:accepted_proposals) { create_list(:proposal, 1, :accepted, component: origin_component) }
+
+        it "only imports proposals in the custom state" do
+          within ".import_proposals" do
+            select origin_component.name["en"], from: "Origin component"
+            check "Under review"
+          end
+
+          click_on "Import proposals"
+
+          expect(page).to have_content("The import process has started. We will let you know once it has finished.")
+          perform_enqueued_jobs
+          visit current_path
+
+          expect(Decidim::Proposals::Proposal.where(component:).count).to eq(2)
+        end
+      end
+    end
   end
 end
