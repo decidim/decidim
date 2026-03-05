ea1bf0423ee4d9a1259ab00a6a6fb39ee05f7528
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2026-03-05 09:04:56 +0100

    Move conditinonal check inside of the class method (part 2)
    Co-authored-by: Alexandru Emil Lupu <contact@alecslupu.ro>

--- decidim-proposals/app/models/decidim/proposals/proposal.rb
+++ decidim-proposals/app/models/decidim/proposals/proposal.rb
@@ -186,6 +186,8 @@ module Decidim
       end
 
       def self.most_liked_available?(component)
+        return false unless component.current_settings.likes_enabled?
+
         where(component:)
           .published
           .not_hidden
