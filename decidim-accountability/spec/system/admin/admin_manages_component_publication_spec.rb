9688b3df0383a7fb2f4594600610011e0e7feada
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-03-05 12:50:53 +0200

    Add tests

--- decidim-accountability/spec/system/admin/admin_manages_component_publication_spec.rb
+++ decidim-accountability/spec/system/admin/admin_manages_component_publication_spec.rb
@@ -4,24 +4,49 @@ require "spec_helper"
 
 describe "Admin manages component publication" do
   include_context "when managing a component as an admin" do
-    let!(:resource) { create(:result, component:) }
+    context "when there are no children" do
+      let!(:resource) { create(:result, component:) }
 
-    context "when cycling through publication states" do
-      let!(:component) { create(:accountability_component, participatory_space:) }
+      context "when cycling through publication states" do
+        let!(:component) { create(:accountability_component, participatory_space:) }
 
-      include_examples "cycling through publication states"
+        include_examples "cycling through publication states"
+      end
+
+      context "when component is unpublished, and admin publishes" do
+        let!(:component) { create(:accountability_component, :unpublished, participatory_space:) }
+
+        include_examples "add component resources to search index"
+      end
+
+      context "when component is published, and admin unpublishes" do
+        let!(:component) { create(:accountability_component, :published, participatory_space:) }
+
+        include_examples "removes component resources from search index"
+      end
     end
 
-    context "when component is unpublished, and admin publishes" do
-      let!(:component) { create(:accountability_component, :unpublished, participatory_space:) }
+    context "when there are children" do
+      let!(:resource) { create(:result, component:) }
+      let(:children) { create(:result, component:, parent: resource) }
 
-      include_examples "add component resources to search index"
-    end
+      context "when cycling through publication states" do
+        let!(:component) { create(:accountability_component, participatory_space:) }
 
-    context "when component is published, and admin unpublishes" do
-      let!(:component) { create(:accountability_component, :published, participatory_space:) }
+        include_examples "cycling through publication states"
+      end
 
-      include_examples "removes component resources from search index"
+      context "when component is unpublished, and admin publishes" do
+        let!(:component) { create(:accountability_component, :unpublished, participatory_space:) }
+
+        include_examples "add component resources to search index"
+      end
+
+      context "when component is published, and admin unpublishes" do
+        let!(:component) { create(:accountability_component, :published, participatory_space:) }
+
+        include_examples "removes component resources from search index"
+      end
     end
   end
 end
