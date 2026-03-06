e4168ea893ea38c0aace8d6a64b11c658622d3d1
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-03-06 10:31:08 +0200

    Apply suggestions from code review
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-pages/spec/system/admin_manages_component_publication_spec.rb
+++ decidim-pages/spec/system/admin_manages_component_publication_spec.rb
@@ -5,6 +5,8 @@ require "spec_helper"
 describe "Admin manages component publication" do
   let!(:resource) { create(:page, component:) }
 
+  # Note: other components also handle search index updates (additions/removals) when publishing or
+  # unpublishing. This component is excluded from general search, so those operations are not implemented here.
   include_context "when managing a component as an admin" do
     context "when cycling through publication states" do
       let!(:component) { create(:page_component, participatory_space:) }
