c59feacf54e3cd884a13985bf350bb52a9e9b223
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-03-10 09:39:07 +0100

    Add and consolidate specs

---
+++ decidim-forms/lib/decidim/forms/test/shared_examples/manage_questionnaires/draggable_behavior.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+shared_examples_for "manage questionnaire draggable behavior" do
+  let!(:question) { create(:questionnaire_question, :title_and_description, questionnaire:) }
+
+  context "when questionnaire has no responses (editable)" do
+    it "shows draggable data attributes for questions list" do
+      visit_manage_questions
+
+      expect(page).to have_selector("[data-draggable-table]")
+      expect(page).to have_selector("[data-draggable-handle]")
+    end
+
+    describe "when hovering over card divider" do
+      it "shows resize cursor for editable questions" do
+        visit_manage_questions
+
+        within first(".questionnaire-question") do
+          expect(page).to have_css(".card-divider.hover\\:cursor-ns-resize")
+        end
+      end
+    end
+  end
+
+  context "when questionnaire has responses (not editable)" do
+    let!(:response) { create(:response, questionnaire:) }
+
+    it "does not show draggable data attributes for questions list" do
+      visit_manage_questions
+
+      expect(page).not_to have_selector("[data-draggable-table]")
+      expect(page).not_to have_selector("[data-draggable-handle]")
+    end
+
+    describe "when hovering over card divider" do
+      it "does not show resize cursor for non-editable questions" do
+        visit_manage_questions
+
+        within first(".questionnaire-question") do
+          expect(page).to have_no_css(".card-divider.hover\\:cursor-ns-resize")
+        end
+      end
+    end
+  end
+
+  private
+
+  def visit_manage_questions
+    click_on "Questions"
+  end
+end
