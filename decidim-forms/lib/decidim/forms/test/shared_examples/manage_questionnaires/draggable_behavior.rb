5d08a3cfdf1a254def3ba787f144db9eb73dc95e
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-03-11 11:15:53 +0000

    Remove unecessary method in spec

--- decidim-forms/lib/decidim/forms/test/shared_examples/manage_questionnaires/draggable_behavior.rb
+++ decidim-forms/lib/decidim/forms/test/shared_examples/manage_questionnaires/draggable_behavior.rb
@@ -7,16 +7,12 @@ shared_examples_for "manage questionnaire draggable behavior" do
 
   context "when questionnaire has no responses (editable)" do
     it "shows draggable data attributes for questions list" do
-      visit_manage_questions
-
       expect(page).to have_css("[data-draggable-table]")
       expect(page).to have_css("[data-draggable-handle]")
     end
 
     describe "when hovering over card divider" do
       it "shows resize cursor for editable questions" do
-        visit_manage_questions
-
         within first(".questionnaire-question") do
           expect(page).to have_css(".card-divider.hover\\:cursor-grab")
         end
@@ -28,26 +24,16 @@ shared_examples_for "manage questionnaire draggable behavior" do
     let!(:response) { create(:response, questionnaire:) }
 
     it "does not show draggable data attributes for questions list" do
-      visit_manage_questions
-
       expect(page).to have_no_css("[data-draggable-table]")
       expect(page).to have_no_css("[data-draggable-handle]")
     end
 
     describe "when hovering over card divider" do
       it "does not show resize cursor for non-editable questions" do
-        visit_manage_questions
-
         within first(".questionnaire-question") do
           expect(page).to have_no_css(".card-divider.hover\\:cursor-grab")
         end
       end
     end
-  end
-
-  private
-
-  def visit_manage_questions
-    click_on "Questions"
   end
 end
