44a3fcee798b957be72b078781011bd29f68fca1
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-03-16 17:19:23 +0200

    fix specs

--- decidim-proposals/spec/system/admin/admin_edits_proposal_spec.rb
+++ decidim-proposals/spec/system/admin/admin_edits_proposal_spec.rb
@@ -125,7 +125,7 @@ describe "Admin edits proposals" do
       it "can edit a proposal with an attachment" do
         visit_component_admin
         within "tr[data-id='#{proposal.id}']" do
-          find("button[data-controller='dropdown']").click
+          find("button[data-component='dropdown']").click
           click_on "Edit proposal"
         end
 
@@ -140,7 +140,7 @@ describe "Admin edits proposals" do
 
         visit_component_admin
         within "tr[data-id='#{proposal.id}']" do
-          find("button[data-controller='dropdown']").click
+          find("button[data-component='dropdown']").click
           click_on "Edit proposal"
         end
 
