a0014ade0e5524f247744bfde78dc1f87ac1d279
Tom Greenwood <101816158+greenwoodt@users.noreply.github.com>
2026-03-16 11:13:08 +0100

    Show only component specific states in proposal to proposal import
    * JS import configured to select states based on component
    
    * Route configured for collection on proposal import new
    
    * Imported js entry point of import_proposals.
    
    * Configured a new iteration from select on the component origin within the form
    
    * Created component_states method within the import controller
    
    * Updated some lint issues with the JS script
    
    * Updated import controller to retrieve the origin_id instead of component id
    
    * Moved the proposals_merge resource route.
    
    * Added attribute to select states based on the selected component
    
    * Added selectedStates which parses the selected states based on the component
    
    * Removed validate :valid_states because pluck token wasn't reterving states on its own componet not from that selected
    
    * Fixed the tercary operator on the selected states checkbox
    
    * Aligned the method call on the new form to fix lint error
    
    * not answered missing from the component states maped to be selected from the componet
    
    * Extracted the enforce_permissions method to component_states to parse users who dont have permissions to run the import actions
    
    * clean up of unused methods within the proposal import form
    
    * Updated object that retrieves the states via the proposal_import
    
    * Escaped the html by interpolating the state.token and state.title within the script to encode all handles
    
    * reverted private method of valid_states checking the proposal state and plucking the not_answered state server side validation
    
    * System spec added to check for dynamically added states from origin component
    
    * Updated spec to account for custom states to be detected in drop down
    
    * refactored the proposal import change to import from a stimulus controller
    
    * modified the new page to call the data attribute of the dynamic states
    
    * update to controller based on lint errors
    
    * refactored to use the mordern way of stimulus of calling the controller and an absolute path
    
    * jS docs added to the controller fetching the states dynamically

--- decidim-proposals/spec/system/admin/import_proposals_spec.rb
+++ decidim-proposals/spec/system/admin/import_proposals_spec.rb
@@ -14,12 +14,12 @@ describe "Import proposals" do
     let!(:component) { create(:proposal_component, participatory_space:) }
   end
 
-  before do
-    page.find(".imports").click
-    click_on "Import proposals from a file"
-  end
+  describe "import from a file" do
+    before do
+      click_on "Import"
+      click_on "Import proposals from a file"
+    end
 
-  describe "import from a file page" do
     it "has start import button" do
       expect(page).to have_content("Import")
     end
@@ -30,7 +30,7 @@ describe "Import proposals" do
     end
 
     it "does not change proposal amount if one imported row fails" do
-      dynamically_attach_file(:proposals_file_import_file, Decidim::Dev.asset("import_proposals_broken.csv"))
+      dynamically_attach_file(:import_file, Decidim::Dev.asset("import_proposals_broken.csv"))
 
       click_on "Import"
       expect(page).to have_content("Found an error in the import file on line 4")
@@ -38,34 +38,112 @@ describe "Import proposals" do
     end
 
     it "creates proposals after successfully import" do
-      dynamically_attach_file(:proposals_file_import_file, Decidim::Dev.asset("import_proposals.csv"))
+      dynamically_attach_file(:import_file, Decidim::Dev.asset("import_proposals.csv"))
       click_on "Import"
       expect(page).to have_content("3 proposals successfully imported")
       expect(Decidim::Proposals::Proposal.count).to eq(3)
     end
   end
 
-  context "when the user is in user group" do
-    let(:user_group) { create(:user_group, :confirmed, :verified, organization:) }
-    let!(:membership) { create(:user_group_membership, user:, user_group:) }
+  describe "import from another component" do
+    let!(:origin_component) { create(:proposal_component, participatory_space:) }
 
     before do
-      visit "#{current_path}?name=proposals"
-    end
-
-    it "has create import as dropdown" do
-      page.find_by_id("proposals_file_import_user_group_id").click
-      expect(page).to have_content(user_group.name)
-    end
-
-    it "links proposal to user group during the import" do
-      page.find_by_id("proposals_file_import_user_group_id").click
-      select user_group.name, from: "proposals_file_import_user_group_id"
-      dynamically_attach_file(:proposals_file_import_file, Decidim::Dev.asset("import_proposals.csv"))
       click_on "Import"
-      expect(page).to have_content("3 proposals successfully imported")
-      expect(Decidim::Proposals::Proposal.last.user_groups.count).to eq(1)
-      expect(Decidim::Proposals::Proposal.last.user_groups.first.name).to eq(user_group.name)
+      click_on "Import proposals from another component"
+    end
+
+    it "does not show state checkboxes before a component is selected" do
+      expect(page).to have_no_css("#states-container input[type='checkbox']")
+    end
+
+    it "dynamically loads state checkboxes after selecting an origin component" do
+      within ".import_proposals" do
+        select origin_component.name["en"], from: "Origin component"
+      end
+
+      within "#states-container" do
+        expect(page).to have_unchecked_field("Accepted")
+        expect(page).to have_unchecked_field("Rejected")
+        expect(page).to have_unchecked_field("Evaluating")
+        expect(page).to have_unchecked_field("Not answered")
+      end
+    end
+
+    it "hides state checkboxes when the component selection is cleared" do
+      within ".import_proposals" do
+        select origin_component.name["en"], from: "Origin component"
+      end
+
+      expect(page).to have_css("#states-container input[type='checkbox']")
+
+      within ".import_proposals" do
+        select "Please select a component", from: "Origin component"
+      end
+
+      expect(page).to have_no_css("#states-container input[type='checkbox']")
+    end
+
+    context "when importing proposals filtered by state" do
+      let!(:accepted_proposals) { create_list(:proposal, 2, :accepted, component: origin_component) }
+      let!(:rejected_proposals) { create_list(:proposal, 1, :rejected, component: origin_component) }
+
+      it "only imports proposals matching the selected state" do
+        within ".import_proposals" do
+          select origin_component.name["en"], from: "Origin component"
+          check "Accepted"
+        end
+
+        click_on "Import proposals"
+
+        expect(page).to have_content("The import process has started. We will let you know once it has finished.")
+        perform_enqueued_jobs
+        visit current_path
+
+        expect(Decidim::Proposals::Proposal.where(component:).count).to eq(2)
+      end
+    end
+
+    context "with a custom state on the origin component" do
+      let(:custom_title) { { "en" => "Under review" } }
+      let!(:custom_state) do
+        create(:proposal_state, component: origin_component, token: "under_review", title: custom_title)
+      end
+
+      it "shows the custom state alongside the default ones" do
+        within ".import_proposals" do
+          select origin_component.name["en"], from: "Origin component"
+        end
+
+        within "#states-container" do
+          expect(page).to have_unchecked_field("Under review")
+          expect(page).to have_unchecked_field("Accepted")
+          expect(page).to have_unchecked_field("Not answered")
+        end
+      end
+
+      context "when importing proposals with the custom state" do
+        let!(:custom_state_on_target) do
+          create(:proposal_state, component:, token: "under_review", title: custom_title)
+        end
+        let!(:custom_proposals) { create_list(:proposal, 2, component: origin_component, state: "under_review") }
+        let!(:accepted_proposals) { create_list(:proposal, 1, :accepted, component: origin_component) }
+
+        it "only imports proposals in the custom state" do
+          within ".import_proposals" do
+            select origin_component.name["en"], from: "Origin component"
+            check "Under review"
+          end
+
+          click_on "Import proposals"
+
+          expect(page).to have_content("The import process has started. We will let you know once it has finished.")
+          perform_enqueued_jobs
+          visit current_path
+
+          expect(Decidim::Proposals::Proposal.where(component:).count).to eq(2)
+        end
+      end
     end
   end
 end
