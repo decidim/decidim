edfb3e1871877999d568c49800149ebf2593ab37
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-03-18 09:37:32 +0100

    Fix example descriptions for documentation

--- decidim-proposals/spec/system/admin/import_proposals_from_file_spec.rb
+++ decidim-proposals/spec/system/admin/import_proposals_from_file_spec.rb
@@ -12,34 +12,32 @@ describe "Import proposals from a file" do
     let!(:component) { create(:proposal_component, participatory_space:) }
   end
 
-  describe "import from a file" do
-    before do
-      click_on "Import"
-      click_on "Import proposals from a file"
-    end
+  before do
+    click_on "Import"
+    click_on "Import proposals from a file"
+  end
 
-    it "has start import button" do
-      expect(page).to have_content("Import")
-    end
+  it "has start import button" do
+    expect(page).to have_content("Import")
+  end
 
-    it "returns error without a file" do
-      click_on "Import"
-      expect(page).to have_content("There is an error in this field")
-    end
+  it "returns error without a file" do
+    click_on "Import"
+    expect(page).to have_content("There is an error in this field")
+  end
 
-    it "does not change proposal amount if one imported row fails" do
-      dynamically_attach_file(:import_file, Decidim::Dev.asset("import_proposals_broken.csv"))
+  it "does not change proposal amount if one imported row fails" do
+    dynamically_attach_file(:import_file, Decidim::Dev.asset("import_proposals_broken.csv"))
 
-      click_on "Import"
-      expect(page).to have_content("Found an error in the import file on line 4")
-      expect(Decidim::Proposals::Proposal.count).to eq(0)
-    end
+    click_on "Import"
+    expect(page).to have_content("Found an error in the import file on line 4")
+    expect(Decidim::Proposals::Proposal.count).to eq(0)
+  end
 
-    it "creates proposals after successfully import" do
-      dynamically_attach_file(:import_file, Decidim::Dev.asset("import_proposals.csv"))
-      click_on "Import"
-      expect(page).to have_content("3 proposals successfully imported")
-      expect(Decidim::Proposals::Proposal.count).to eq(3)
-    end
+  it "creates proposals after successfully import" do
+    dynamically_attach_file(:import_file, Decidim::Dev.asset("import_proposals.csv"))
+    click_on "Import"
+    expect(page).to have_content("3 proposals successfully imported")
+    expect(Decidim::Proposals::Proposal.count).to eq(3)
   end
 end
