18413fdc9bafd94abeedff2befbcc97e1567f8be
Ivan Vergés <ivan@pokecode.net>
2026-03-19 16:07:13 +0100

    move helper to its owwn module

---
+++ decidim-core/spec/helpers/decidim/mailer_helper_spec.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  describe MailerHelper do
+    describe "#decidim_transform_image_urls" do
+      let(:host) { "example.org" }
+
+      let(:user_input) do
+        %{(<p>Hello, %{name}</p>
+        <a href="https://meta.decidim.org">Link</a>
+        <img src="/rails/active_storage/blobs/redirect/12345.JPG" alt="image" />
+        <a href="https://meta.decidim.org/">Link</a>
+        <img src="/rails/active_storage/blobs/redirect/56789.JPG" alt="second image" />)}
+      end
+
+      context "when decidim_transform_image_urls is invoked" do
+        subject { helper.send(:decidim_transform_image_urls, user_input, host) }
+
+        it "transforms image URLs with the host" do
+          root_url = Decidim::EngineRouter.new("decidim", {}).root_url(host:)[0..-2]
+          expect(subject).to include(%(<img src="#{root_url}/rails/active_storage/blobs/redirect/12345.JPG"))
+          expect(subject).to include(%(<img src="#{root_url}/rails/active_storage/blobs/redirect/56789.JPG"))
+        end
+
+        context "when a relative src matches the suffix of an absolute URL" do
+          let(:user_input) do
+            %(<img src="/image.jpg" alt="relative" /><img src="https://example.com/image.jpg" alt="absolute" />)
+          end
+
+          it "transforms only the relative URL" do
+            root_url = Decidim::EngineRouter.new("decidim", {}).root_url(host:)[0..-2]
+
+            expect(subject).to include(%(<img src="#{root_url}/image.jpg" alt="relative" />))
+            expect(subject).to include(%(<img src="https://example.com/image.jpg" alt="absolute" />))
+          end
+        end
+
+        context "when src uses data/protocol-relative/cid URLs" do
+          let(:user_input) do
+            %(<img src="data:image/png;base64,AAAA" alt="data" />
+<img src="//cdn.example.org/image.jpg" alt="protocol-relative" />
+<img src="cid:logo@example.org" alt="cid" />)
+          end
+
+          it "keeps them unchanged" do
+            expect(subject).to include(%(src="data:image/png;base64,AAAA"))
+            expect(subject).to include(%(src="//cdn.example.org/image.jpg"))
+            expect(subject).to include(%(src="cid:logo@example.org"))
+          end
+        end
+
+        context "when src attribute is single-quoted" do
+          let(:user_input) { "<img src='/image.jpg' alt='relative' />" }
+
+          it "transforms the URL preserving single quotes" do
+            root_url = Decidim::EngineRouter.new("decidim", {}).root_url(host:).chomp("/")
+            expect(subject).to include(%(<img src='#{root_url}/image.jpg' alt='relative' />))
+          end
+        end
+
+        context "when host is not present" do
+          subject { helper.send(:decidim_transform_image_urls, user_input, nil) }
+
+          it "returns the full content" do
+            expect(subject).to eq user_input
+          end
+        end
+      end
+    end
+  end
+end
