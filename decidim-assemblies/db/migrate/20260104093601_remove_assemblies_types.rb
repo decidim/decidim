54af362674b8ba36864ae968d13d93a1e39151ee
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-03-28 08:29:34 +0200

    Apply codereview recommendations

--- decidim-assemblies/db/migrate/20260104093601_remove_assemblies_types.rb
+++ decidim-assemblies/db/migrate/20260104093601_remove_assemblies_types.rb
@@ -2,8 +2,8 @@
 
 class RemoveAssembliesTypes < ActiveRecord::Migration[7.0]
   def up
-    remove_column :decidim_assemblies, :decidim_assemblies_type_id
-    drop_table :decidim_assemblies_types
+    remove_column :decidim_assemblies, :decidim_assemblies_type_id, if_exists: true
+    drop_table :decidim_assemblies_types, if_exists: true
   end
 
   def down
