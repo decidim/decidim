d6ebafce6883d683b43b9d9650bb02803f326f05
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-04-14 12:31:25 +0200

    Fix locale namespace on decidim-templates routes

--- decidim-templates/lib/decidim/templates/admin_engine.rb
+++ decidim-templates/lib/decidim/templates/admin_engine.rb
@@ -13,51 +13,51 @@ module Decidim
 
       routes do
         constraints(->(request) { Decidim::Admin::OrganizationDashboardConstraint.new(request).matches? }) do
-          scope "/:locale", defaults: { locale: Decidim.default_locale } do
-            resources :proposal_answer_templates do
-              member do
-                post :copy
-              end
-              collection do
-                get :fetch
-              end
+          resources :proposal_answer_templates do
+            member do
+              post :copy
             end
-
-            ## Routes for Questionnaire Templates
-            resources :questionnaire_templates do
-              member do
-                post :copy
-                get :edit_questions
-                patch :update_questions
-                resource :questionnaire, module: :questionnaire_templates # To manage the templatable resource
-              end
-
-              collection do
-                post :apply # To use when creating an object from a template
-                post :skip # To use when creating an object without a template
-                get :preview # To provide a preview for the template in the object creation view
-              end
+            collection do
+              get :fetch
             end
-
-            resources :block_user_templates do
-              member do
-                post :copy
-              end
-              collection do
-                get :fetch
-              end
-            end
-
-            get "/questionnaire_template/questionnaire/response_options", to: "questionnaire_templates/questionnaires#response_options", as: "response_options_template"
-
-            root to: "questionnaire_templates#index"
           end
+
+          ## Routes for Questionnaire Templates
+          resources :questionnaire_templates do
+            member do
+              post :copy
+              get :edit_questions
+              patch :update_questions
+              resource :questionnaire, module: :questionnaire_templates # To manage the templatable resource
+            end
+
+            collection do
+              post :apply # To use when creating an object from a template
+              post :skip # To use when creating an object without a template
+              get :preview # To provide a preview for the template in the object creation view
+            end
+          end
+
+          resources :block_user_templates do
+            member do
+              post :copy
+            end
+            collection do
+              get :fetch
+            end
+          end
+
+          get "/questionnaire_template/questionnaire/response_options", to: "questionnaire_templates/questionnaires#response_options", as: "response_options_template"
+
+          root to: "questionnaire_templates#index"
         end
       end
 
       initializer "decidim_templates_admin.mount_routes" do
         Decidim::Core::Engine.routes do
-          mount Decidim::Templates::AdminEngine, at: "/admin/templates", as: "decidim_admin_templates"
+          scope "/:locale", defaults: { locale: Decidim.default_locale } do
+            mount Decidim::Templates::AdminEngine, at: "/admin/templates", as: "decidim_admin_templates"
+          end
         end
       end
 
