1410f41f6061dbe6ee9807ef87815ac396cc09fe
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-04-14 12:19:22 +0200

    Adjusted test block to expect a valid value for proposal import

--- decidim-proposals/spec/forms/decidim/proposals/admin/proposals_import_form_spec.rb
+++ decidim-proposals/spec/forms/decidim/proposals/admin/proposals_import_form_spec.rb
@@ -40,7 +40,7 @@ module Decidim
         context "when there are no states" do
           let(:states) { [] }
 
-          it { is_expected.to be_invalid }
+          it { is_expected.to invalid }
         end
 
         context "when there is no target component" do
