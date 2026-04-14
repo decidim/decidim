1acf4e26ab48b29873410a02f2d95442990a83e7
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-04-14 12:21:38 +0200

    Removed validation on states within the proposals import form

--- decidim-proposals/app/forms/decidim/proposals/admin/proposals_import_form.rb
+++ decidim-proposals/app/forms/decidim/proposals/admin/proposals_import_form.rb
@@ -16,11 +16,10 @@ module Decidim
         attribute :states, Array[String]
 
         validates :origin_component_id, :origin_component, :current_component, presence: true
-        validates :states, presence: true
         validate :valid_states
 
-        def states
-          super.compact_blank
+        def states=(values)
+          super(Array(values).compact_blank)
         end
 
         def origin_component
