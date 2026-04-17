3a2907aff1bc02f04dd959489a30219c86b162dd
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-04-17 12:19:48 +0300

    Remove debug message

--- decidim-core/app/packs/entrypoints/decidim_editor.js
+++ decidim-core/app/packs/entrypoints/decidim_editor.js
@@ -4,6 +4,5 @@ import "stylesheets/decidim/editor.scss"
 import EditorController from "src/decidim/controllers/editor/controller";
 
 document.addEventListener("stimulus:load", () => {
-  console.log("Editor controller registered");
   window.Stimulus.register("editor", EditorController);
 })
