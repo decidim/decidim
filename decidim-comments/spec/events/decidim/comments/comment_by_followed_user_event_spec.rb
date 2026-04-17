94e70b2f81396772f32d880c65c43dddd1f265a3
andra-panaite <andra.panaite@tremend.com>
2026-04-17 12:41:28 +0300

    Updated test files

--- decidim-comments/spec/events/decidim/comments/comment_by_followed_user_event_spec.rb
+++ decidim-comments/spec/events/decidim/comments/comment_by_followed_user_event_spec.rb
@@ -12,7 +12,7 @@ module Decidim
       let(:email_subject) { "There is a new comment by #{author_name} in #{resource_title}" }
       let(:email_intro) { "#{author_name} has left a comment in #{resource_title}. You can read it in this page:" }
       let(:email_outro) { "You have received this notification because you are following #{author_name}. You can unfollow this user from their profile page." }
-      let(:notification_title) { "There is a new comment by <a href=\"#{author_path}\">#{author_name} #{author_nickname}</a> in <a href=\"#{resource_path}?commentId=#{comment.id}#comment_#{comment.id}\">#{resource_title}</a>." }
+      let(:notification_title) { "There is a new comment by <a href=\"#{author_path}\">#{author_name}</a> in <a href=\"#{resource_path}?commentId=#{comment.id}#comment_#{comment.id}\">#{resource_title}</a>." }
 
       it_behaves_like "a simple event email"
       it_behaves_like "a simple event notification"
