eaa9cc21bdc367d2e8587cc0a1ede480e2b7acb1
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2026-04-21 17:03:26 +0200

    Remove the roles menu on footer
    Co-authored-by: stephanie rousset <strabon@club-internet.fr>

--- decidim-core/app/presenters/decidim/menu_item_presenter.rb
+++ decidim-core/app/presenters/decidim/menu_item_presenter.rb
@@ -26,7 +26,7 @@ module Decidim
     delegate :content_tag, :safe_join, :link_to, :active_link_to_class, :is_active_link?, :icon, to: :@view
 
     def render
-      content_tag :li, role: :menuitem, class: link_wrapper_classes do
+      content_tag :li, role: menuitem_role, class: link_wrapper_classes do
         output = if url == "#"
                    [content_tag(:span, composed_label, class: "sidebar-menu__item-disabled")]
                  else
@@ -60,6 +60,12 @@ module Decidim
       return @options.element_class unless is_active_link?(url, active)
 
       [@options.element_class, active_class].compact.join(" ")
+    end
+
+    def menuitem_role
+      return if @options.role == false
+
+      @options.role || :menuitem
     end
 
     def active_class
