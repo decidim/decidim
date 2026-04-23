63f7e37128abf731cd5b8aa5afdd218add3439d3
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-04-23 08:09:31 +0200

    Fix specs

--- decidim-comments/spec/events/decidim/comments/user_mentioned_event_spec.rb
+++ decidim-comments/spec/events/decidim/comments/user_mentioned_event_spec.rb
@@ -10,9 +10,13 @@ describe Decidim::Comments::UserMentionedEvent do
   let(:event_name) { "decidim.events.comments.user_mentioned" }
   let(:ca_comment_content) { "<div><p>Un commentaire pour #{author_link}</p></div>" }
   let(:en_comment_content) { "<div><p>Comment mentioning some user, #{author_link}</p></div>" }
-  let(:author_link) { "<a href=\"http://#{organization.host}:#{Capybara.server_port}/#{I18n.locale}/profiles/#{author.nickname}\" data-external-link=\"false\" target=\"_blank\" rel=\"nofollow noopener noreferrer ugc\">@#{author.nickname}</a>" }
+  let(:author_link) { "<a href=\"http://#{organization.host}:#{Capybara.server_port}/en/profiles/#{author.nickname}\" data-external-link=\"false\" target=\"_blank\" rel=\"nofollow noopener noreferrer ugc\">@#{author.nickname}</a>" }
   let(:parsed_body) { Decidim::ContentProcessor.parse("Comment mentioning some user, @#{author.nickname}", current_organization: organization) }
-  let(:parsed_ca_body) { Decidim::ContentProcessor.parse("Un commentaire pour @#{author.nickname}", current_organization: organization) }
+  let(:parsed_ca_body) do
+    I18n.with_locale(:ca) do
+      Decidim::ContentProcessor.parse("Un commentaire pour @#{author.nickname}", current_organization: organization)
+    end
+  end
   let(:body) { { en: parsed_body.rewrite, machine_translations: { ca: parsed_ca_body.rewrite } } }
 
   let(:participatory_process) { create(:participatory_process, organization:) }
@@ -51,7 +55,7 @@ describe Decidim::Comments::UserMentionedEvent do
     let(:machine_translated) { ca_comment_content }
     let(:translatable) { true }
 
-    let(:untranslated_content) { "<div><p>Comment mentioning some user, #{ca_author_link}</p></div>" }
+    let(:untranslated_content) { "<div><p>Comment mentioning some user, #{author_link}</p></div>" }
     let(:ca_author_link) { "<a href=\"http://#{organization.host}:#{Capybara.server_port}/ca/profiles/#{author.nickname}\" data-external-link=\"false\" target=\"_blank\" rel=\"nofollow noopener noreferrer ugc\">@#{author.nickname}</a>" }
 
     it_behaves_like "a translated event"
