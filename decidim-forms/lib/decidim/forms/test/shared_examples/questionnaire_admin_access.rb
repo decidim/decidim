b8fc1dc40bf10b3e099653ffade57d62645e0bd1
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-04-29 10:34:26 +0200

    Fix error with `manage_questions_path` and 404 shared example

--- decidim-forms/lib/decidim/forms/test/shared_examples/questionnaire_admin_access.rb
+++ decidim-forms/lib/decidim/forms/test/shared_examples/questionnaire_admin_access.rb
@@ -3,15 +3,32 @@
 require "spec_helper"
 
 shared_examples_for "questionnaire admin access" do
-  context "when the user is not an admin" do
+  context "when the user is not an admin", driver: :rack_test do
     let(:regular_user) { create(:user, :confirmed, organization:) }
+    let(:target_path) { manage_questions_path }
 
     before do
       login_as regular_user, scope: :user
     end
 
-    it_behaves_like "a 404 page" do
-      visit manage_questions_path
+    before do
+      allow(Rails.application).to \
+        receive(:env_config).with(no_args).and_wrap_original do |m, *|
+          m.call.merge(
+            "action_dispatch.show_exceptions" => true,
+            "action_dispatch.show_detailed_exceptions" => false
+          )
+        end
+
+      visit target_path
+    end
+
+    it "leads to a 404" do
+      expect(page).to have_content("The page you are looking for cannot be found")
+
+      expect(page).to have_http_status(:not_found)
+
+      expect(page).to have_current_path(target_path)
     end
   end
 
