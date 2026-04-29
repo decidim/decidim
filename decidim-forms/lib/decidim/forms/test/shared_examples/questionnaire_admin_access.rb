3f84c3be37bd9ed681b0511edbb9ed76b3d2b78a
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-04-29 10:13:33 +0200

    Implement specs

--- decidim-forms/lib/decidim/forms/test/shared_examples/questionnaire_admin_access.rb
+++ decidim-forms/lib/decidim/forms/test/shared_examples/questionnaire_admin_access.rb
@@ -3,33 +3,24 @@
 require "spec_helper"
 
 shared_examples_for "questionnaire admin access" do
-  context "when the user is not authenticated" do
-    before do
-      sign_out user
-    end
-
-    it "redirects to the sign in page" do
-      expect do
-        visit manage_questions_path
-      end.to redirect_to("/users/sign_in")
-    end
-  end
-
   context "when the user is not an admin" do
-    let(:user) { create(:user, :confirmed, organization:) }
+    let(:regular_user) { create(:user, :confirmed, organization:) }
 
-    it "redirects to the root page" do
-      expect do
-        visit manage_questions_path
-      end.to redirect_to("/")
+    before do
+      login_as regular_user, scope: :user
+    end
+
+    it_behaves_like "a 404 page" do
+      visit manage_questions_path
     end
   end
 
   context "when the user is a process admin" do
-    let(:user) { create(:user, :confirmed, organization:) }
-    let(:role) { create(:participatory_process_user_role, role: :admin, user:, participatory_process:) }
+    let(:process_admin) { create(:user, :confirmed, organization:) }
+    let(:role) { create(:participatory_process_user_role, role: :admin, user: process_admin, participatory_process:) }
 
     it "allows access to the questionnaire" do
+      login_as process_admin, scope: :user
       visit manage_questions_path
 
       expect(page).to have_current_path(manage_questions_path)
@@ -37,9 +28,10 @@ shared_examples_for "questionnaire admin access" do
   end
 
   context "when the user is an admin" do
-    let(:user) { create(:user, :admin, :confirmed, organization:) }
+    let(:admin) { create(:user, :admin, :confirmed, organization:) }
 
     it "allows access to the questionnaire" do
+      login_as admin, scope: :user
       visit manage_questions_path
 
       expect(page).to have_current_path(manage_questions_path)
