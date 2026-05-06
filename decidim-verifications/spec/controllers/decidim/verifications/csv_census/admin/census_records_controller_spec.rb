5ceace8e812aaba444081e98076f0c6c03ec555c
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-05-06 12:15:31 +0300

    Enforce permission check on admins' CSV census records

--- decidim-verifications/spec/controllers/decidim/verifications/csv_census/admin/census_records_controller_spec.rb
+++ decidim-verifications/spec/controllers/decidim/verifications/csv_census/admin/census_records_controller_spec.rb
@@ -6,7 +6,6 @@ module Decidim::Verifications::CsvCensus::Admin
   describe CensusRecordsController do
     routes { Decidim::Verifications::CsvCensus::AdminEngine.routes }
 
-    let(:user) { create(:user, :admin, :confirmed, organization:) }
     let(:organization) { create(:organization) }
     let(:csv_datum) { create(:csv_datum, organization:) }
 
@@ -15,19 +14,47 @@ module Decidim::Verifications::CsvCensus::Admin
       sign_in user, scope: :user
     end
 
-    describe "GET #new_record" do
-      it "renders the new_record template" do
-        get :new_record
-        expect(response).to render_template(:new_record)
-        expect(assigns(:form)).to be_a(Decidim::Verifications::CsvCensus::Admin::CensusForm)
+    context "when user is admin" do
+      let(:user) { create(:user, :admin, :confirmed, organization:) }
+
+      describe "GET #new_record" do
+        it "renders the new_record template" do
+          expect(controller).to receive(:enforce_permission_to).with(:create, :authorization).and_call_original
+
+          get :new_record
+          expect(response).to render_template(:new_record)
+          expect(assigns(:form)).to be_a(Decidim::Verifications::CsvCensus::Admin::CensusForm)
+        end
+      end
+
+      describe "GET #edit_record" do
+        it "renders the edit_record template" do
+          expect(controller).to receive(:enforce_permission_to).with(:update, :authorization).and_call_original
+
+          get :edit_record, params: { id: csv_datum.id }
+          expect(response).to render_template(:edit_record)
+          expect(assigns(:form)).to be_a(Decidim::Verifications::CsvCensus::Admin::CensusForm)
+        end
       end
     end
 
-    describe "GET #edit_record" do
-      it "renders the edit_record template" do
-        get :edit_record, params: { id: csv_datum.id }
-        expect(response).to render_template(:edit_record)
-        expect(assigns(:form)).to be_a(Decidim::Verifications::CsvCensus::Admin::CensusForm)
+    context "when user is NOT admin" do
+      let(:user) { create(:user, :confirmed, organization:) }
+
+      describe "GET #new_record" do
+        it "renders the new_record template" do
+          get :new_record
+
+          expect(response).to have_http_status(:redirect)
+        end
+      end
+
+      describe "GET #edit_record" do
+        it "renders the edit_record template" do
+          get :edit_record, params: { id: csv_datum.id }
+
+          expect(response).to have_http_status(:redirect)
+        end
       end
     end
   end
