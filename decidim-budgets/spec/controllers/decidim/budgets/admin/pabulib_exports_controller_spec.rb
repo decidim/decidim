3e71b1e0b0cea5cb1f3989cad1b623b8b42918f3
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2026-05-19 12:18:21 +0300

    Add the controller specs for Pabulib exports

--- decidim-budgets/spec/controllers/decidim/budgets/admin/pabulib_exports_controller_spec.rb
+++ decidim-budgets/spec/controllers/decidim/budgets/admin/pabulib_exports_controller_spec.rb
@@ -5,7 +5,7 @@ require "spec_helper"
 module Decidim
   module Budgets
     module Admin
-      describe BudgetsController do
+      describe PabulibExportsController do
         let(:current_user) { create(:user, :confirmed, :admin, organization: component.organization) }
         let(:component) { create(:budgets_component) }
         let(:budget) { create(:budget, component:) }
@@ -19,20 +19,55 @@ module Decidim
 
         describe "GET show" do
           it "returns the correct status code" do
-            # TODO
+            get :show, params: { budget_id: budget.id }
+
+            expect(response).to have_http_status(:ok)
           end
         end
 
         describe "POST create" do
           context "when the form is valid" do
+            let(:export_params) do
+              {
+                description: "City PB voting",
+                country: "Finland",
+                unit: "Southern region",
+                instance: "2026",
+                vote_type: "approval",
+                min_length: 1,
+                max_length: 5
+              }
+            end
+
             it "returns the correct status code" do
-              # TODO
+              post :create, params: { budget_id: budget.id, pabulib_export: export_params }
+
+              expect(response).to have_http_status(:ok)
+
+              expect(response.content_type).to eq("text/csv; charset=utf-8")
+              expect(response.headers["Content-Disposition"]).to match(/\Aattachment; filename="decidim-budget-#{budget.id}-results-[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}.pb"\z/)
+              expect(response.headers["Cache-Control"]).to eq("no-cache, no-store")
+              expect(Time.zone.parse(response.headers["Last-Modified"])).to be_between(2.seconds.ago, Time.current)
             end
           end
 
           context "when the form is invalid" do
+            before do
+              form_builder = double
+              allow(controller).to receive(:form).with(PabulibExportForm).and_return(form_builder)
+              allow(form_builder).to receive(:from_params) do |params|
+                PabulibExportForm.from_params(params).tap do |form|
+                  allow(form).to receive(:valid?).and_return(false)
+                end
+              end
+            end
+
             it "returns the correct status code" do
-              # TODO
+              post :create, params: { budget_id: budget.id }
+
+              expect(response).to have_http_status(:ok)
+              expect(response.content_type).to eq("text/html; charset=utf-8")
+              expect(flash[:alert]).not_to be_empty
             end
           end
         end
