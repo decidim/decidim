a78228413d84b9862ddba3c9838bed5b83b9feca
Alejandro Rueda <alejandro.rueda@nazaries.com>
2026-05-20 11:21:31 +0300

    Improve performance on taxonomy filter form

--- decidim-admin/app/forms/decidim/admin/taxonomy_filter_form.rb
+++ decidim-admin/app/forms/decidim/admin/taxonomy_filter_form.rb
@@ -44,7 +44,7 @@ module Decidim
       def items_collection
         return [] unless root_taxonomy
 
-        @items_collection ||= map_items_collection(root_taxonomy)
+        @items_collection ||= build_items_collection
       end
 
       def root_taxonomy
@@ -62,12 +62,17 @@ module Decidim
 
       private
 
-      def map_items_collection(taxonomy)
-        taxonomy.children.map do |item|
+      def build_items_collection
+        children_by_parent_id = root_taxonomy.all_children.group_by(&:parent_id)
+        build_subtree(root_taxonomy.id, children_by_parent_id)
+      end
+
+      def build_subtree(parent_id, children_by_parent_id)
+        Array(children_by_parent_id[parent_id]).map do |item|
           Item.new(
             name: translated_attribute(item.name),
             value: item.id,
-            children: map_items_collection(item)
+            children: build_subtree(item.id, children_by_parent_id)
           )
         end
       end
