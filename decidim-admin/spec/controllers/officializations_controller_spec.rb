1e4406c0f9969df9aa0817dfb12e61747c397d2f
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-05-21 14:40:54 +0300

    Apply review recommendation

---
+++ decidim-admin/spec/controllers/officializations_controller_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  module Admin
+    describe OfficializationsController do
+      routes { Decidim::Admin::Engine.routes }
+      render_views
+
+      let(:organization) { create(:organization) }
+      let(:current_user) { create(:user, :admin, :confirmed, organization:) }
+
+      before do
+        request.env["decidim.current_organization"] = organization
+        sign_in current_user, scope: :user
+      end
+
+      describe "show_email" do
+        context "when user is not found" do
+          it "sets a not found message" do
+            get :show_email, params: { user_id: 999_999_999 }
+
+            expect(response.body).to have_content("No user found")
+          end
+        end
+      end
+    end
+  end
+end
