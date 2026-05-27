08341a0c49ca693712bf34be2530acbe1575c8bd
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-05-27 12:15:10 +0200

    Add the VALIDATOR_HTML_URI env var to the WebMock configuration

--- decidim-dev/lib/decidim/dev/test/rspec_support/webmock.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/webmock.rb
@@ -2,10 +2,22 @@
 
 require "webmock/rspec"
 
+class CleanUpValidatorHtmlUri
+  def self.host
+    validator_html_uri = ENV["VALIDATOR_HTML_URI"]
+    return if validator_html_uri.to_s.empty?
+
+    URI.parse(validator_html_uri).host
+  rescue URI::InvalidURIError
+    nil
+  end
+end
+
 WebMock.disable_net_connect!(
   allow_localhost: true,
   allow: [
     %r{https://validator\.w3\.org/},
+    CleanUpValidatorHtmlUri.host,
     Decidim::Dev::Test::MapServer.host
-  ]
+  ].compact
 )
