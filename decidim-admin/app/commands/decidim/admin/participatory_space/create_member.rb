95648e61b6b8a5efa44895f2fe55d9d382e2996a
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-06-01 15:53:57 +0200

    Fix rubocop offenses (automatic)

--- decidim-admin/app/commands/decidim/admin/participatory_space/create_member.rb
+++ decidim-admin/app/commands/decidim/admin/participatory_space/create_member.rb
@@ -63,14 +63,14 @@ module Decidim
         def existing_user
           return @existing_user if defined?(@existing_user)
 
-          if form.existing_user
-            @existing_user = form.user
-          else
-            @existing_user = User.find_by(
-              email: form.email.downcase,
-              organization: member_to.organization
-            )
-          end
+          @existing_user = if form.existing_user
+                             form.user
+                           else
+                             User.find_by(
+                               email: form.email.downcase,
+                               organization: member_to.organization
+                             )
+                           end
 
           InviteUserAgain.call(@existing_user, invitation_instructions) if @existing_user&.invitation_pending?
 
