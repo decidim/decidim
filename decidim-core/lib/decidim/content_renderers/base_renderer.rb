9ab4aa80fa9a467c1d93c3fbbe41cab63fcef497
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-06-01 22:05:09 +0200

    Fix rubocop offense

--- decidim-core/lib/decidim/content_renderers/base_renderer.rb
+++ decidim-core/lib/decidim/content_renderers/base_renderer.rb
@@ -12,7 +12,7 @@ module Decidim
     class BaseRenderer
       include Decidim::ContentProcessor::Common
 
-      ReplacementContext = Struct.new(:placement, :node_name, :attribute_name, :ancestor_names, keyword_init: true) do
+      ReplacementContext = Struct.new(:placement, :node_name, :attribute_name, :ancestor_names) do
         def text?
           placement == :text
         end
