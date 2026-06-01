4bf5dd43102057843f02d79bee1ef976c76b82a4
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-06-01 15:29:00 +0300

    Fix specs

--- decidim-meetings/spec/system/admin/admin_filters_meeting_invites_spec.rb
+++ decidim-meetings/spec/system/admin/admin_filters_meeting_invites_spec.rb
@@ -49,11 +49,9 @@ describe "Admin filters invites" do
     let(:meeting) { create(:meeting, :with_registrations_enabled) }
 
     it "successfully handles the error" do
-      within "#new_meeting_registration_invite" do
-        fill_in :meeting_registration_invite_name, with: user1.name
-        fill_in :meeting_registration_invite_email, with: user1.email
-        click_on "Invite"
-      end
+      fill_in :meeting_registration_invite_name, with: user1.name
+      fill_in :meeting_registration_invite_email, with: user1.email
+      click_on "Invite"
 
       expect(page).to have_callout("There was a problem inviting the participant to join the meeting.")
     end
@@ -65,11 +63,9 @@ describe "Admin filters invites" do
     it "successfully handles the invite" do
       invited = build(:user, organization:)
 
-      within "#new_meeting_registration_invite" do
-        fill_in :meeting_registration_invite_name, with: invited.name
-        fill_in :meeting_registration_invite_email, with: invited.email
-        click_on "Invite"
-      end
+      fill_in :meeting_registration_invite_name, with: invited.name
+      fill_in :meeting_registration_invite_email, with: invited.email
+      click_on "Invite"
 
       expect(page).to have_callout("Participant successfully invited to join the meeting.")
     end
