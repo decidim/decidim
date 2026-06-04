0365e0d1a0611b75a8e1019e5105a5751c0f8f59
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2026-06-04 10:23:11 +0200

    Fix data attribute format

--- decidim-core/app/packs/src/decidim/controllers/mention/controller.js
+++ decidim-core/app/packs/src/decidim/controllers/mention/controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
   connect() {
     this.options = {
       noDataFoundMessage: this.element.getAttribute("data-no_results") || "No results found",
-      searchPromptMessage: this.element.getAttribute("data-search_prompt") || "Type to search participants",
+      searchPromptMessage: this.element.getAttribute("data-search-prompt") || "Type to search participants",
       debounceDelay: 250,
       menuItemLimit: 5
     };
