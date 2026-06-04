0a4fd10eabd6491b10a8143200cd8f25814825ec
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-06-04 10:40:57 +0200

    Remove unecessary InlineImagesParser call
    This is a legacy task that isn't being call correctly (it should provide
    the user in the context too). I prefer to drop it.

--- decidim-debates/app/commands/decidim/debates/create_debate.rb
+++ decidim-debates/app/commands/decidim/debates/create_debate.rb
@@ -58,13 +58,7 @@ module Decidim
 
       def attributes
         parsed_title = Decidim::ContentProcessor.parse(form.title, current_organization: form.current_organization).rewrite
-        parsed_description = Decidim::ContentProcessor
-                             .parse_with_processor(
-                               :inline_images,
-                               Decidim::ContentProcessor.parse(form.description, current_organization: form.current_organization),
-                               current_organization: form.current_organization
-                             )
-                             .rewrite
+        parsed_description = Decidim::ContentProcessor.parse(form.description, current_organization: form.current_organization).rewrite
 
         super.merge({
                       author: form.current_user,
--- decidim-meetings/app/commands/decidim/meetings/admin/update_meeting.rb
+++ decidim-meetings/app/commands/decidim/meetings/admin/update_meeting.rb
@@ -23,13 +23,7 @@ module Decidim
 
         def attributes
           parsed_title = Decidim::ContentProcessor.parse(form.title, current_organization: form.current_organization).rewrite
-          parsed_description = Decidim::ContentProcessor
-                               .parse_with_processor(
-                                 :inline_images,
-                                 Decidim::ContentProcessor.parse(form.description, current_organization: form.current_organization),
-                                 current_organization: form.current_organization
-                               )
-                               .rewrite
+          parsed_description = Decidim::ContentProcessor.parse(form.description, current_organization: form.current_organization).rewrite
 
           super.merge({
                         title: parsed_title,
