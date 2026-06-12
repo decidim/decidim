2f3c102839412492385a4a8699ffef7366538d49
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-06-12 12:36:17 +0300

    Running rubocops

--- decidim-core/app/mailers/decidim/delete_user_mailer.rb
+++ decidim-core/app/mailers/decidim/delete_user_mailer.rb
@@ -2,7 +2,6 @@
 
 module Decidim
   class DeleteUserMailer < ApplicationMailer
-
     # This email is being sent when a user deletes his own account, or when the user was inactive for too long.
     def delete(user_email:, user_name:, locale:, organization:)
       I18n.with_locale(locale) do
