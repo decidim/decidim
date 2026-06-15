7510a119b74adc9d045f3b44eda0250070ce3531
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-06-15 14:17:07 +0200

    Fix specs in elections

--- decidim-elections/spec/system/admin/admin_manages_elections_spec.rb
+++ decidim-elections/spec/system/admin/admin_manages_elections_spec.rb
@@ -147,7 +147,7 @@ describe "Admin manages elections" do
         expect(page).to have_field("election_end_at_date", disabled: true)
         expect(page).to have_field("election_end_at_time", disabled: true)
       end
-      dynamically_attach_file(:election_documents, Decidim::Dev.asset("city2.jpeg"))
+      dynamically_attach_file(:election_attachments, Decidim::Dev.asset("city2.jpeg"))
 
       click_on "Save and continue"
       expect(page).to have_callout "Election updated successfully"
