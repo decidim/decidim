d3b907a588f9f9bb68a166eaf1db94bff0b3438b
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-06-15 14:33:46 +0200

    Fix miss spelling of attachments in all debate examples

--- decidim-debates/spec/shared/manage_debates_examples.rb
+++ decidim-debates/spec/shared/manage_debates_examples.rb
@@ -260,8 +260,8 @@ RSpec.shared_examples "manage debates" do
           choose "Open"
         end
 
-        dynamically_attach_file(:debate_attachmets, image_path)
-        dynamically_attach_file(:debate_attachmets, document_path)
+        dynamically_attach_file(:debate_attachments, image_path)
+        dynamically_attach_file(:debate_attachments, document_path)
 
         within ".new_debate" do
           find("*[type=submit]").click
@@ -279,7 +279,7 @@ RSpec.shared_examples "manage debates" do
       end
 
       it "shows validation error when format is not accepted" do
-        dynamically_attach_file(:debate_attachmets, invalid_document, keep_modal_open: true) do
+        dynamically_attach_file(:debate_attachments, invalid_document, keep_modal_open: true) do
           expect(page).to have_text("Accepted formats: #{Decidim::OrganizationSettings.for(organization).upload_allowed_file_extensions.join(", ")}")
         end
         expect(page).to have_text("Validation error!")
@@ -301,8 +301,8 @@ RSpec.shared_examples "manage debates" do
           fill_in_i18n_editor(:debate_instructions, "#debate-instructions-tabs", **attributes[:instructions].except("machine_translations"))
         end
 
-        dynamically_attach_file(:debate_attachmets, image_path)
-        dynamically_attach_file(:debate_attachmets, document_path)
+        dynamically_attach_file(:debate_attachments, image_path)
+        dynamically_attach_file(:debate_attachments, document_path)
 
         within ".edit_debate" do
           find("*[type=submit]").click
