1d5c59406838f5b201a80a6a23d962df24a12add
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-06-22 10:00:58 +0300

    Backport 'Make the fields at the "highlighted content banner" content block required' to v0.31 (#17125)
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-admin/spec/system/admin_manages_organization_homepage_spec.rb
+++ decidim-admin/spec/system/admin_manages_organization_homepage_spec.rb
@@ -137,6 +137,86 @@ describe "Admin manages organization homepage" do
         expect(page).to have_text("Custom HTML text!")
       end
     end
+
+    context "and edits highlighted content banner settings" do
+      let!(:content_block) { create(:content_block, organization:, manifest_name: :highlighted_content_banner, scope_name: :homepage) }
+
+      it "updates the settings of the content block" do
+        visit decidim_admin.edit_organization_homepage_content_block_path(content_block)
+
+        fill_in(:content_block_settings_title_en, with: "Custom title text!")
+        fill_in(:content_block_settings_action_button_title_en, with: "Custom action title!")
+        fill_in(:content_block_settings_action_button_subtitle_en, with: "Custom action subtitle!")
+        fill_in(:content_block_settings_action_button_url, with: "http://example.org")
+
+        fill_in_i18n_editor :content_block_settings_short_description, "#content_block-settings--short_description-tabs",
+                            en: "<p>The Short description</p>"
+
+        click_on "Update"
+        sleep 1
+        visit decidim.root_path
+        expect(page).to have_text("Custom title text!")
+        expect(page).to have_text("The Short description")
+        expect(page).to have_link("Custom action subtitle!", href: "http://example.org")
+      end
+
+      it "updates the images of the content block" do
+        visit decidim_admin.edit_organization_homepage_content_block_path(content_block)
+
+        fill_in(:content_block_settings_title_en, with: "Custom title text!")
+        fill_in(:content_block_settings_action_button_title_en, with: "Custom action title!")
+        fill_in(:content_block_settings_action_button_subtitle_en, with: "Custom action subtitle!")
+        fill_in(:content_block_settings_action_button_url, with: "http://example.org")
+
+        fill_in_i18n_editor :content_block_settings_short_description, "#content_block-settings--short_description-tabs",
+                            en: "<p>The Short description</p>"
+
+        dynamically_attach_file(:content_block_images_background_image, Decidim::Dev.asset("city2.jpeg"))
+
+        click_on "Update"
+
+        sleep 1
+        visit decidim.root_path
+        expect(page.html).to include("city2.jpeg")
+      end
+
+      it "displays the 'Resolution is too large' error message when image is invalid" do
+        visit decidim_admin.edit_organization_homepage_content_block_path(content_block)
+
+        fill_in(:content_block_settings_title_en, with: "Custom title text!")
+        fill_in(:content_block_settings_action_button_title_en, with: "Custom action title!")
+        fill_in(:content_block_settings_action_button_subtitle_en, with: "Custom action subtitle!")
+        fill_in(:content_block_settings_action_button_url, with: "http://example.org")
+
+        dynamically_attach_file(:content_block_images_background_image, Decidim::Dev.asset("8001x4000.png"))
+
+        click_on "Update"
+
+        expect(page).to have_text("File resolution is too large")
+      end
+
+      it "displays validation errors when required fields are empty" do
+        visit decidim_admin.edit_organization_homepage_content_block_path(content_block)
+
+        click_on "Update"
+
+        expect(page).to have_text("There is an error in this field.")
+      end
+    end
+
+    context "and edits html settings" do
+      let!(:content_block) { create(:content_block, organization:, manifest_name: :html, scope_name: :homepage) }
+
+      it "updates the settings of the content block" do
+        visit decidim_admin.edit_organization_homepage_content_block_path(content_block)
+
+        fill_in(:content_block_settings_html_content_en, with: "Custom HTML text!")
+        click_on "Update"
+        sleep 1
+        visit decidim.root_path
+        expect(page).to have_text("Custom HTML text!")
+      end
+    end
   end
 
   context "when loading non-existing content blocks" do
