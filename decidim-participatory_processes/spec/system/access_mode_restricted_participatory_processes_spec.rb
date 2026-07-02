46dbc372493d35cb552f530d7d65f40da7378a18
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-07-02 16:10:02 +0300

    Apply suggestions from code review
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-participatory_processes/spec/system/access_mode_restricted_participatory_processes_spec.rb
+++ decidim-participatory_processes/spec/system/access_mode_restricted_participatory_processes_spec.rb
@@ -20,7 +20,7 @@ describe "Access Mode Restricted Participatory Processes" do
   it_behaves_like "access mode restricted participatory spaces"
   it_behaves_like "access mode restricted participatory spaces comments"
 
-  context "when accessing restricted assembly and user has space roles assigned" do
+  context "when accessing restricted participatory process and user has space roles assigned" do
     context "when user is a space admin" do
       it_behaves_like "access mode restricted participatory spaces" do
         let!(:admin) { create(:process_admin, :confirmed, participatory_process: restricted_participatory_space) }
