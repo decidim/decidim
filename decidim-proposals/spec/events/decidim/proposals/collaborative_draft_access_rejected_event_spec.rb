c48491e5f353b8c6c09e73acf112e4cc3df11585
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-07-02 09:17:26 +0200

    Fix specs

--- decidim-proposals/spec/events/decidim/proposals/collaborative_draft_access_rejected_event_spec.rb
+++ decidim-proposals/spec/events/decidim/proposals/collaborative_draft_access_rejected_event_spec.rb
@@ -14,12 +14,10 @@ describe Decidim::Proposals::CollaborativeDraftAccessRejectedEvent do
   let(:author_presenter) { Decidim::UserPresenter.new(author) }
   let(:author_path) { author_presenter.profile_path }
   let(:author_name) { author_presenter.name }
-  let(:author_nickname) { author_presenter.nickname }
   let(:requester) { create(:user, :confirmed, organization: resource.organization) }
   let(:requester_presenter) { Decidim::UserPresenter.new(requester) }
   let(:requester_id) { requester.id }
   let(:requester_name) { requester.name }
-  let(:requester_nickname) { requester_presenter.nickname }
   let(:requester_path) { requester_presenter.profile_path }
   let(:extra) { { requester_id: } }
 
@@ -27,7 +25,7 @@ describe Decidim::Proposals::CollaborativeDraftAccessRejectedEvent do
     let(:email_subject) { "#{requester_name} has been rejected to access as a contributor of the #{translated(resource.title)} collaborative draft." }
     let(:email_intro) { %(#{requester_name} has been rejected to access as a contributor of the <a href="#{resource_url}">#{resource_title}</a> collaborative draft.) }
     let(:email_outro) { %(You have received this notification because you are a collaborator of <a href="#{resource_url}">#{resource_title}</a>.) }
-    let(:notification_title) { %(<a href="#{requester_path}">#{requester_name} #{requester_nickname}</a> has been <strong>rejected to access as a contributor</strong> of the <a href="#{resource_path}">#{resource_title}</a> collaborative draft.) }
+    let(:notification_title) { %(<a href="#{requester_path}">#{requester_name}</a> has been <strong>rejected to access as a contributor</strong> of the <a href="#{resource_path}">#{resource_title}</a> collaborative draft.) }
 
     it_behaves_like "a simple event"
     it_behaves_like "a simple event email"
