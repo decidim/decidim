20aff9ce044224c30ae7bc5a42f5b4f40faf3d5f
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-07-06 13:54:11 +0300

    Apply suggestions from code review
    Co-authored-by: Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>

--- decidim-core/app/packs/src/decidim/controllers/tabs/controller.js
+++ decidim-core/app/packs/src/decidim/controllers/tabs/controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus";
  * This JavaScript is taken from the following link:
  * https://www.w3.org/WAI/ARIA/apg/patterns/tabs/examples/tabs-automatic/
  *
- * To which I have converted it to a Stimulus controller, trying to keep decidim structure.
+ * To which we have converted it to a Stimulus controller, trying to keep decidim structure.
  * We keep the same HTML structure as on foundation, but we are replacing with a truly accessible tabs implementation.
  */
 
