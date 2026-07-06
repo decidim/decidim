71e669b8f2a2f599f95a11b833faa678ad8d1c4a
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-07-06 17:26:56 +0200

    Fix bullet error by documenting the eager loading
    (cherry picked from commit 1014b524af6c3a67290c56fc08ed8d1a09c7fdc5)

--- decidim-pages/spec/commands/copy_page_spec.rb
+++ decidim-pages/spec/commands/copy_page_spec.rb
@@ -42,6 +42,18 @@ module Decidim
           end
 
           context "when the page has attachments" do
+            before do
+              # We do not optimize the eager loading here, as these associations are loaded by
+              # .with_attached_file and .includes(:attachment_collection) and used internally
+              # by ActiveStorage callbacks or the machine_translation callback, which Bullet does not track.
+              Bullet.add_safelist type: :unused_eager_loading, class_name: "Decidim::Attachment", association: :file_attachment
+              Bullet.add_safelist type: :unused_eager_loading, class_name: "ActiveStorage::Attachment", association: :blob
+              Bullet.add_safelist type: :unused_eager_loading, class_name: "ActiveStorage::Blob", association: :variant_records
+              Bullet.add_safelist type: :unused_eager_loading, class_name: "ActiveStorage::Blob", association: :preview_image_attachment
+              Bullet.add_safelist type: :unused_eager_loading, class_name: "ActiveStorage::Attachment", association: :record
+              Bullet.add_safelist type: :unused_eager_loading, class_name: "Decidim::Attachment", association: :attachment_collection
+            end
+
             let!(:document) { create(:attachment, :with_pdf, attached_to: page) }
             let!(:photo) { create(:attachment, :with_image, attached_to: page) }
 
