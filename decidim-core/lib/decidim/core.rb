c167548617005b92a57a76373858c2c80a561fc6
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2026-07-24 22:25:13 +0300

    Revert the action logger overrides after the seeding process
    To support the specs or any consecutive commands.

--- decidim-core/lib/decidim/core.rb
+++ decidim-core/lib/decidim/core.rb
@@ -209,7 +209,7 @@ module Decidim
     # Make sure the overrides applied by the seeds are no longer present after
     # the seeding process completes. This can be a problem when running multiple
     # tests consecutively.
-    Rails.application.reloader.reload!
+    load File.expand_path("#{Decidim::Core::Engine.root}/app/services/decidim/action_logger.rb")
   end
 
   def self.seed_contextual_help_sections!(manifest)
