0df82d51c8f71a94b99828684e79dd602b2ab345
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-07-28 02:19:27 +0300

    Dsiabling bullet for logs controller

--- decidim-admin/app/controllers/decidim/admin/logs_controller.rb
+++ decidim-admin/app/controllers/decidim/admin/logs_controller.rb
@@ -9,6 +9,14 @@ module Decidim
 
       helper_method :logs, :no_logs_available?
 
+      # The action logs are too complicated to be eagerly loaded
+      # we may have a Decidim::Meetings::Meeting resource to which we can eagerly load component: { participatory_space: :organization }
+      # We may have a Decidim::Comments::Comment resource to which we caan eagerly load :commentable, :root_commentable with associated children
+      # We may have a Decidim::Component resource that
+      # We may have a Decidim::Parocess resource that doesn't have an collection
+      # Calling .includes() with all the possible combinations, could raise a `ActiveRecord::AssociationNotFoundError`
+      around_action :skip_bullet, if: -> { defined?(::Bullet) }
+
       def index
         enforce_permission_to :read, :admin_log
       end
@@ -24,15 +32,27 @@ module Decidim
       end
 
       def base_query
-        root_query.includes(
-          :component, :participatory_space, :user, version: :item, resource: [:organization, { component: :participatory_space }]
-        )
+        root_query.includes(:organization, :component, :user, :resource, :participatory_space, version: :item)
       end
 
       def root_query
         Decidim::ActionLog.where(
           organization: current_organization
         ).for_admin
+      end
+
+      private
+
+      # This method is presented in Bullet readme, but is not yet available in Bullet 8.1.3.
+      # https://github.com/flyerhzm/bullet/issues/505
+      def skip_bullet
+        return Bullet.skip { yield } if Bullet.respond_to?(:skip)
+
+        previous_value = Bullet.enable?
+        Bullet.enable = false
+        yield
+      ensure
+        Bullet.enable = previous_value
       end
     end
   end
