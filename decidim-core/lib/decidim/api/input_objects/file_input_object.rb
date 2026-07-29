eeaeeab5843403552dd7437e33df28ac25add694
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-07-29 19:16:03 +0300

    Apply suggestion from @alecslupu

--- decidim-core/lib/decidim/api/input_objects/file_input_object.rb
+++ decidim-core/lib/decidim/api/input_objects/file_input_object.rb
@@ -14,7 +14,7 @@ module Decidim
         return unless blob_id
 
         blob = ActiveStorage::Blob.find(blob_id)
-        raise Api::Errors::UnauthorizedObjectError, "You don't have permission to access this blob" unless context[:current_user]&.admin?
+        raise Api::Errors::UnauthorizedObjectError, "You do not have permission to access this blob" unless context[:current_user]&.admin?
 
         blob
       end
