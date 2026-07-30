dde53f3fa5b514a83e814706586a5f6dfb7dad89
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-07-30 16:37:20 +0200

    Fix offense for CensusController (verifications)
    Prompt:
    """
    decidim-verifications/app/controllers/decidim/verifications/csv_census/admin/census_controller.rb:99:26
    """

--- decidim-verifications/app/controllers/decidim/verifications/csv_census/admin/census_controller.rb
+++ decidim-verifications/app/controllers/decidim/verifications/csv_census/admin/census_controller.rb
@@ -96,9 +96,9 @@ module Decidim
 
             return { icon: nil, text: t(".no_user"), last_sign_in: nil } unless user
 
-            authorized = Decidim::Authorization.where(name: "csv_census", user:)
-                                               .where.not(granted_at: nil)
-                                               .exists?
+            authorized = Decidim::Authorization.where(name: "csv_census", user:) # rubocop:disable Decidim/OrganizationScopedFinder -- user is already scoped to current_organization (line 95)
+                                                .where.not(granted_at: nil)
+                                                .exists?
 
             icon = authorized ? "checkbox-circle-line" : "close-circle-line"
             text = authorized ? t("index.authorized", scope: "decidim.verifications.csv_census.admin") : t("index.no_authorized", scope: "decidim.verifications.csv_census.admin")
