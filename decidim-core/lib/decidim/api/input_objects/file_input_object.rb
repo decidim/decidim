66f6136cc8331e5da17a99fdcaac4994fbb4cd16
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-08-03 15:44:56 +0300

    Fix: Use signed_id instead of a raw sequential blob_id.

--- decidim-core/lib/decidim/api/input_objects/file_input_object.rb
+++ decidim-core/lib/decidim/api/input_objects/file_input_object.rb
@@ -8,12 +8,12 @@ module Decidim
     class FileInputObject < Decidim::Api::Types::BaseInputObject
       description "Attributes for attaching files to objects"
 
-      argument :blob_id, GraphQL::Types::Int, description: "The file blob ID to attach to the object", required: true
+      argument :signed_id, GraphQL::Types::String, description: "The signed ID of the file blob to attach to the object", required: true
 
       def blob
-        return unless blob_id
+        return unless signed_id
 
-        blob = ActiveStorage::Blob.find(blob_id)
+        blob = ActiveStorage::Blob.find_signed(signed_id)
         raise Api::Errors::UnauthorizedObjectError, "You do not have permission to access this blob" unless context[:current_user]&.admin?
 
         blob
