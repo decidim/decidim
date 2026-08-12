b8be31bcc83bb48a03eeebd583e9383ce2646401
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-08-12 15:01:39 +0200

    Fix tests for disabler

--- decidim-core/spec/services/decidim/iframe_disabler_spec.rb
+++ decidim-core/spec/services/decidim/iframe_disabler_spec.rb
@@ -15,40 +15,40 @@ describe Decidim::IframeDisabler do
 
       it "converts the iframe to a disabled div element" do
         expect(subject).to eq(
-          %(<div class="disabled-iframe"><!-- #{text} --></div>)
+          %(<div class="disabled-iframe"><!-- <iframe class="testing" src="https://www.youtube-nocookie.com/embed/f6JMgJAQ2tc" title="Decidim video" allowfullscreen scrolling="no"></iframe> --></div>)
         )
       end
     end
 
     context "when the iframe does not define the scrolling attribute" do
       let(:iframe) { %(<iframe src="https://www.youtube.com/embed/f6JMgJAQ2tc" title="Decidim video"></iframe>) }
-      let(:text) { %(<iframe src="https://www.youtube.com/embed/f6JMgJAQ2tc" title="Decidim video" scrolling="no"></iframe>) }
+      let(:text) { iframe }
 
       it "adds the scrolling attribute to the iframe node" do
         expect(subject).to eq(
-          %(<div class="disabled-iframe"><!-- #{text} --></div>)
+          %(<div class="disabled-iframe"><!-- <iframe src="https://www.youtube-nocookie.com/embed/f6JMgJAQ2tc" title="Decidim video" scrolling="no"></iframe> --></div>)
         )
       end
     end
 
     context "when the iframe does not define the title attribute" do
       let(:iframe) { %(<iframe src="https://www.youtube.com/embed/f6JMgJAQ2tc" scrolling="no"></iframe>) }
-      let(:text) { %(<iframe src="https://www.youtube.com/embed/f6JMgJAQ2tc" title="Embedded video content" scrolling="no"></iframe>) }
+      let(:text) { iframe }
 
       it "adds the default title to the iframe node" do
         expect(subject).to eq(
-          %(<div class="disabled-iframe"><!-- #{text} --></div>)
+          %(<div class="disabled-iframe"><!-- <iframe src="https://www.youtube-nocookie.com/embed/f6JMgJAQ2tc" scrolling="no" title="Embedded video content"></iframe> --></div>)
         )
       end
     end
 
     context "when the iframe's title attribute is empty" do
       let(:iframe) { %(<iframe src="https://www.youtube.com/embed/f6JMgJAQ2tc" title="" scrolling="no"></iframe>) }
-      let(:text) { %(<iframe src="https://www.youtube.com/embed/f6JMgJAQ2tc" title="Embedded video content" scrolling="no"></iframe>) }
+      let(:text) { iframe }
 
       it "adds the default title to the iframe node" do
         expect(subject).to eq(
-          %(<div class="disabled-iframe"><!-- #{text} --></div>)
+          %(<div class="disabled-iframe"><!-- <iframe src="https://www.youtube-nocookie.com/embed/f6JMgJAQ2tc" title="Embedded video content" scrolling="no"></iframe> --></div>)
         )
       end
     end
@@ -58,7 +58,18 @@ describe Decidim::IframeDisabler do
 
       it "converts the iframe to a disabled div element" do
         expect(subject).to eq(
-          %(<div><div><div class="disabled-iframe"><!-- #{iframe} --></div></div></div>)
+          %(<div><div><div class="disabled-iframe"><!-- <iframe class="testing" src="https://www.youtube-nocookie.com/embed/f6JMgJAQ2tc" title="Decidim video" allowfullscreen scrolling="no"></iframe> --></div></div></div>)
+        )
+      end
+    end
+
+    context "when the iframe source contains an embed code" do
+      let(:iframe) { %(<iframe src="&lt;iframe width=&quot;560&quot; height=&quot;315&quot; src=&quot;https://www.youtube.com/embed/y8CcxALf4PQ&quot; title=&quot;YouTube video player&quot; frameborder=&quot;0&quot; allowfullscreen&gt;&lt;/iframe&gt;" title="video url"></iframe>) }
+      let(:text) { iframe }
+
+      it "extracts the embed URL from the source" do
+        expect(subject).to eq(
+          %(<div class="disabled-iframe"><!-- <iframe src="https://www.youtube-nocookie.com/embed/y8CcxALf4PQ" title="video url" scrolling="no"></iframe> --></div>)
         )
       end
     end
