fe40bf5cafd853060435e87f47795c97ee2a841a
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-08-12 15:01:39 +0200

    Revert change to disabler
    Normalize youtube embed src to youtube-nocookie.com so iframes are not blocked by the Content-Security-Policy when restored after cookie consent.

--- decidim-core/app/services/decidim/iframe_disabler.rb
+++ decidim-core/app/services/decidim/iframe_disabler.rb
@@ -22,6 +22,7 @@ module Decidim
         node["title"] = I18n.t("decidim.shared.embed.title") if node["title"].blank?
         # Disable scrollbar for some embed services
         node["scrolling"] = "no"
+        normalize_src(node)
         orig_node = node.to_s
         node.replace(%(<div class="disabled-iframe"><!-- #{orig_node} --></div>))
       end
@@ -29,6 +30,17 @@ module Decidim
       node.children.each do |child|
         disable_iframes(child)
       end
+    end
+
+    def normalize_src(node)
+      src = node["src"]
+      return if src.blank?
+
+      # Extracts the embedded URL from iframe src attribute
+      embedded = src.match(/src\s*=\s*["']([^"']+)["']/i)&.captures&.first
+      src = embedded if embedded
+
+      node["src"] = src.sub(%r{\Ahttps?://(?:www\.|music\.)?youtube\.com/embed/}, "https://www.youtube-nocookie.com/embed/")
     end
   end
 end
