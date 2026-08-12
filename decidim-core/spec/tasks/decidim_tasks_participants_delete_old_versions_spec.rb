9f4056658762de01338e9664610486171d0d1ab4
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2026-08-12 13:07:03 +0300

    Refine the spec description

--- decidim-core/spec/tasks/decidim_tasks_participants_delete_old_versions_spec.rb
+++ decidim-core/spec/tasks/decidim_tasks_participants_delete_old_versions_spec.rb
@@ -52,7 +52,7 @@ describe "rake decidim:participants:delete_old_versions", type: :task do
   end
 
   context "when a valid days argument is provided" do
-    it "executes the jobs" do
+    it "enqueues the cleanup jobs" do
       ActiveJob::Base.queue_adapter = :test
 
       expect do
