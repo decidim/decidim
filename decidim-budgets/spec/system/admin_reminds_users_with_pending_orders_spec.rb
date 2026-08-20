316d9773312dc645f1ea26d939b9997665daaad5
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-08-20 21:25:03 +0300

    Backport 'Fix flaky specs at admin_reminds_users_with_pending_orders_spec.rb' to v0.32 (#17537)
    Co-authored-by: Antti Hukkanen <antti.hukkanen@mainiotech.fi>

--- decidim-budgets/spec/system/admin_reminds_users_with_pending_orders_spec.rb
+++ decidim-budgets/spec/system/admin_reminds_users_with_pending_orders_spec.rb
@@ -34,8 +34,10 @@ describe "Admin reminds users with pending orders" do
     end
 
     it "sends reminders" do
-      perform_enqueued_jobs { click_on "Send" }
-      expect(page).to have_content("2 users will be reminded")
+      perform_enqueued_jobs do
+        click_on "Send"
+        expect(page).to have_content("2 users will be reminded")
+      end
 
       expect(emails.count).to eq(2)
       emails.each do |email|
@@ -46,11 +48,15 @@ describe "Admin reminds users with pending orders" do
     end
 
     it "does not send reminders twice" do
-      perform_enqueued_jobs { click_on "Send" }
-      expect(page).to have_content("2 users will be reminded")
+      perform_enqueued_jobs do
+        click_on "Send"
+        expect(page).to have_content("2 users will be reminded")
+      end
       click_on "Send voting reminders"
-      perform_enqueued_jobs { click_on "Send" }
-      expect(page).to have_content("0 users will be reminded")
+      perform_enqueued_jobs do
+        click_on "Send"
+        expect(page).to have_content("0 users will be reminded")
+      end
     end
   end
 end
