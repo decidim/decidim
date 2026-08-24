d48fdfd1df0980cbc4012497b8c00af863052155
takahashim <takahashimm@gmail.com>
2026-08-24 20:14:15 +0900

    Fix survey questions partially lost on space import

--- decidim-surveys/app/serializers/decidim/surveys/data_serializer.rb
+++ decidim-surveys/app/serializers/decidim/surveys/data_serializer.rb
@@ -3,7 +3,8 @@
 module Decidim
   module Surveys
     # This class serializes the specific data in each Survey.
-    # This is `Questionnaire->questions->response_options` but not `responses`
+    # This is `Questionnaire->questions` with their `response_options`,
+    # `matrix_rows` and `display_conditions`, but not `responses`
     # and `response_choices`.
     class DataSerializer < Decidim::Exporters::Serializer
       # Returns: Array of Decidim::Forms::Questionnaire as a json hash,
@@ -22,7 +23,9 @@ module Decidim
       def serialize_survey(survey)
         questionnaire = survey.questionnaire
         questionnaire_json = questionnaire.attributes.as_json
-        questionnaire_json[:questions] = serialize_questions(questionnaire.questions.order(:position))
+        questionnaire_json[:questions] = serialize_questions(
+          questionnaire.questions.includes(:response_options, :matrix_rows, :display_conditions).order(:position)
+        )
         json = survey.attributes.as_json
         json[:questionnaire] = questionnaire_json
         json
@@ -32,6 +35,8 @@ module Decidim
         questions.collect do |question|
           json = question.attributes.as_json
           json[:response_options] = serialize_response_options(question.response_options)
+          json[:matrix_rows] = serialize_matrix_rows(question.matrix_rows)
+          json[:display_conditions] = serialize_display_conditions(question.display_conditions)
           json
         end
       end
@@ -39,6 +44,18 @@ module Decidim
       def serialize_response_options(response_options)
         response_options.collect do |option|
           option.attributes.as_json
+        end
+      end
+
+      def serialize_matrix_rows(matrix_rows)
+        matrix_rows.collect do |matrix_row|
+          matrix_row.attributes.as_json
+        end
+      end
+
+      def serialize_display_conditions(display_conditions)
+        display_conditions.collect do |display_condition|
+          display_condition.attributes.as_json
         end
       end
     end
