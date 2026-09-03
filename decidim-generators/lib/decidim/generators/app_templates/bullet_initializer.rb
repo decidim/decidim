c57140d3bc5cc9c4cece37dbf6e0fb9f183f230f
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-09-03 18:08:36 +0300

    Set defaults to BULLET variables to true, so that it would reise errors

--- decidim-generators/lib/decidim/generators/app_templates/bullet_initializer.rb
+++ decidim-generators/lib/decidim/generators/app_templates/bullet_initializer.rb
@@ -11,9 +11,9 @@ if defined?(Bullet) && !Rails.application.config.try(:boost_performance)
     Bullet.stacktrace_includes = %w(decidim-)
 
     # Detect N+1 queries
-    Bullet.n_plus_one_query_enable = Decidim::Env.new("DECIDIM_BULLET_N_PLUS_ONE", "false").present?
+    Bullet.n_plus_one_query_enable = Decidim::Env.new("DECIDIM_BULLET_N_PLUS_ONE", "true").present?
     # Detect eager-loaded associations which are not used
-    Bullet.unused_eager_loading_enable = Decidim::Env.new("DECIDIM_BULLET_UNUSED_EAGER", "false").present?
+    Bullet.unused_eager_loading_enable = Decidim::Env.new("DECIDIM_BULLET_UNUSED_EAGER", "true").present?
     # Detect unnecessary COUNT queries which could be avoided with a counter_cache
     Bullet.counter_cache_enable = Decidim::Env.new("DECIDIM_BULLET_COUNTER_CACHE", "true").present?
   end
