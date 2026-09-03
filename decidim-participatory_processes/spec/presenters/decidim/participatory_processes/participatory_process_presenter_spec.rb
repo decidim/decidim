713526bba6095f5c9e06dc00f886754a1569b742
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2026-09-03 21:46:41 +0300

    Fix broken specs

--- decidim-participatory_processes/spec/presenters/decidim/participatory_processes/participatory_process_presenter_spec.rb
+++ decidim-participatory_processes/spec/presenters/decidim/participatory_processes/participatory_process_presenter_spec.rb
@@ -20,9 +20,28 @@ module Decidim
       end
 
       context "when image is attached" do
-        it "returns an URL including the organization domain" do
-          expect(subject.hero_image_url).to include(process.organization.host)
+        it "returns an URL including with the default domain" do
+          # expect(subject.hero_image_url).to include(process.organization.host)
+          expect(subject.hero_image_url).to match(%r{\Ahttp://localhost:#{Capybara.server_port}})
           expect(subject.hero_image_url).to be_blob_url(process.hero_image.blob)
+        end
+
+        context "when the current URL options have been set" do
+          before do
+            ActiveStorage::Current.url_options = { host: "example.org" }
+          end
+
+          it "returns an URL including the configured domain" do
+            expect(subject.hero_image_url).to match(%r{\Ahttp://example.org/})
+          end
+        end
+
+        context "when the default domain cannot be resolved" do
+          before { allow(Rails.env).to receive(:local?).and_return(false) }
+
+          it "returns an URL including the organization domain" do
+            expect(subject.hero_image_url).to match(%r{\Ahttp://#{process.organization.host}:#{Capybara.server_port}})
+          end
         end
       end
     end
