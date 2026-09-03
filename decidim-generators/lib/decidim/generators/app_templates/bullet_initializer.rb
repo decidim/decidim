d46a79a67e23cc990525319888869e6ffa9a47a8
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-09-03 18:39:43 +0300

    Disabling the DECIDIM_BULLET_UNUSED_EAGER by default, as it may trigger on pages that may have 1 result

--- decidim-generators/lib/decidim/generators/app_templates/bullet_initializer.rb
+++ decidim-generators/lib/decidim/generators/app_templates/bullet_initializer.rb
@@ -14,7 +14,7 @@ if defined?(Bullet) && !Rails.application.config.try(:boost_performance)
     # Detect N+1 queries
     Bullet.n_plus_one_query_enable = Decidim::Env.new("DECIDIM_BULLET_N_PLUS_ONE", "true").present?
     # Detect eager-loaded associations which are not used
-    Bullet.unused_eager_loading_enable = Decidim::Env.new("DECIDIM_BULLET_UNUSED_EAGER", "true").present?
+    Bullet.unused_eager_loading_enable = Decidim::Env.new("DECIDIM_BULLET_UNUSED_EAGER", "false").present?
     # Detect unnecessary COUNT queries which could be avoided with a counter_cache
     Bullet.counter_cache_enable = Decidim::Env.new("DECIDIM_BULLET_COUNTER_CACHE", "true").present?
   end
