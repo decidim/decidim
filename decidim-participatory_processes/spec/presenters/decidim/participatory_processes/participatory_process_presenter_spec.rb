f2b5aa20b5e639eba73cb6c64f1bb070672df284
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2026-09-04 18:18:00 +0300

    Fix broken spec

--- decidim-participatory_processes/spec/presenters/decidim/participatory_processes/participatory_process_presenter_spec.rb
+++ decidim-participatory_processes/spec/presenters/decidim/participatory_processes/participatory_process_presenter_spec.rb
@@ -21,7 +21,6 @@ module Decidim
 
       context "when image is attached" do
         it "returns an URL including with the default domain" do
-          # expect(subject.hero_image_url).to include(process.organization.host)
           expect(subject.hero_image_url).to match(%r{\Ahttp://localhost:#{Capybara.server_port}})
           expect(subject.hero_image_url).to be_blob_url(process.hero_image.blob)
         end
@@ -32,7 +31,7 @@ module Decidim
           end
 
           it "returns an URL including the configured domain" do
-            expect(subject.hero_image_url).to match(%r{\Ahttp://example.org/})
+            expect(subject.hero_image_url).to match(%r{\Ahttp://example.org:#{Capybara.server_port}/})
           end
         end
 
