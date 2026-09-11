096c1479742efc352a69c3190c6104bf48fbcda4
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-09-11 09:34:08 +0300

    Touch session, so that we know is active

--- decidim-core/app/controllers/concerns/decidim/headers/session_updater.rb
+++ decidim-core/app/controllers/concerns/decidim/headers/session_updater.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
-module SessionUpdater
+module Decidim
+  module Headers
+    # Since we are moving the sessions in the database, we are using this particular snippet to make sure the sessions
+    # are updated on every request from user, so that we allow the server to know if a session is active or not.
+    module SessionUpdater
+      extend ActiveSupport::Concern
+
+      included do
+        before_action :touch_session
+      end
+
+      private
+
+      def touch_session
+        session[:last_seen_at] = Time.current
+      end
+    end
+  end
 end
