21d665d838843c690de3cd33b1c127585b3bcc47
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-09-12 10:44:56 +0300

    Update decidim-conferences/spec/shared/manage_conference_components_examples.rb
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- decidim-conferences/spec/shared/manage_conference_components_examples.rb
+++ decidim-conferences/spec/shared/manage_conference_components_examples.rb
@@ -179,7 +179,7 @@ shared_examples "manage conference components" do
           click_on "Publish"
         end
 
-        expect(page).to have_callout("The component has been successfully published.")
+        expect(page).to have_admin_callout("The component has been successfully published.")
 
         expect(Decidim::EventPublisherJob).to(have_been_enqueued.with(
                                                 "decidim.events.components.component_published", {
