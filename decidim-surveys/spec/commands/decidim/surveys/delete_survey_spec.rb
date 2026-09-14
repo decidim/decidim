f73598ef34431c6454bf42afff162c12b69f78fc
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-09-14 09:31:28 +0200

    Add command spec for delete survey

---
+++ decidim-surveys/spec/commands/decidim/surveys/delete_survey_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  module Surveys
+    module Admin
+      describe DeleteSurvey, type: :command do
+        let(:survey) { create(:survey, published_at: Time.current) }
+        let(:current_user) { create(:user, :admin, :confirmed, organization: survey.component.organization) }
+        let(:command) { described_class.new(survey, current_user) }
+
+        let!(:responses) do
+          survey.questionnaire.questions.map do |question|
+            create(:response, questionnaire: survey.questionnaire, question:, user: current_user)
+          end
+        end
+
+        describe "call" do
+          it "broadcasts ok" do
+            expect(command).to broadcast(:ok)
+          end
+
+          it "deletes all the survey responses" do
+            expect { command.call }.to change(Decidim::Forms::Response, :count).by(-responses.size)
+            expect(survey.questionnaire.reload.responses).to be_empty
+          end
+
+          it "traces the action" do
+            expect(Decidim.traceability).to receive(:perform_action!).with(
+              "delete_all_responses",
+              survey.questionnaire,
+              current_user
+            ).and_call_original
+
+            command.call
+          end
+        end
+      end
+    end
+  end
+end
