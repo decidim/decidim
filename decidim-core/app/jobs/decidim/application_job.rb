0ac7772be34f3c88ebdfa3138325244f9a280643
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-09-16 11:34:11 +0300

    Use the boolean value instead of :always

--- decidim-core/app/jobs/decidim/application_job.rb
+++ decidim-core/app/jobs/decidim/application_job.rb
@@ -2,7 +2,7 @@
 
 module Decidim
   class ApplicationJob < ActiveJob::Base
-    self.enqueue_after_transaction_commit = :always
+    self.enqueue_after_transaction_commit = true
     # Automatically retry jobs that encountered a deadlock
     retry_on ActiveRecord::Deadlocked
 
