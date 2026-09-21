b60bb52f936bf4f1ad7f26e33355b915ec533347
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-09-21 09:29:27 +0200

    * Add specs for choices and attachments deletion
    Prompt:
    
    """
    Add specs to account for choices and attachments deletion
    """
    
    Assisted by: BigPickle (OpenCode)

--- decidim-surveys/spec/commands/decidim/surveys/delete_survey_responses_spec.rb
+++ decidim-surveys/spec/commands/decidim/surveys/delete_survey_responses_spec.rb
@@ -45,6 +45,34 @@ module Decidim
 
             command.call
           end
+
+          context "when responses have choices" do
+            let(:question_with_options) { survey.questionnaire.questions.find_by(question_type: :single_option) }
+            let!(:choice_response) do
+              create(:response, questionnaire: survey.questionnaire, question: question_with_options, user: current_user)
+            end
+            let!(:choice) do
+              create(:response_choice,
+                     response: choice_response,
+                     response_option: question_with_options.response_options.first,
+                     matrix_row: nil)
+            end
+
+            it "destroys the associated response choices" do
+              expect { command.call }.to change(Decidim::Forms::ResponseChoice, :count).by(-1)
+              expect { choice.reload }.to raise_error(ActiveRecord::RecordNotFound)
+            end
+          end
+
+          context "when responses have attachments" do
+            let!(:response_with_attachments) do
+              create(:response, :with_attachments, questionnaire: survey.questionnaire, question: survey.questionnaire.questions.first, user: current_user)
+            end
+
+            it "destroys the associated attachments" do
+              expect { command.call }.to change(Decidim::Attachment, :count).by(-2)
+            end
+          end
         end
       end
     end
