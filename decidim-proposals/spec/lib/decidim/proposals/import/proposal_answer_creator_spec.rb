15a6514dac75b66343378c9e6baa4e770c8a8c14
Ruben <ruben.bg@coditramuntana.com>
2026-09-22 10:37:19 +0200

    Assert persisted answer

--- decidim-proposals/spec/lib/decidim/proposals/import/proposal_answer_creator_spec.rb
+++ decidim-proposals/spec/lib/decidim/proposals/import/proposal_answer_creator_spec.rb
@@ -151,7 +151,7 @@ describe Decidim::Proposals::Import::ProposalAnswerCreator do
 
       subject.finish_without_notify!
 
-      expect(record.new_record?).to be(false)
+      expect(proposal.reload.answer["en"]).to eq(data[:"answer/en"])
       expect(Decidim::Proposals::Admin::NotifyProposalAnswer).to have_received(:call).with(record, proposal.proposal_state)
     end
   end
