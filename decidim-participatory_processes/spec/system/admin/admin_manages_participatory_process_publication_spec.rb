d7f78f3881595f4579b1c81d876519e27c6b189f
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2026-09-25 10:52:15 +0300

    Fix potentially flaky publish specs

--- decidim-participatory_processes/spec/system/admin/admin_manages_participatory_process_publication_spec.rb
+++ decidim-participatory_processes/spec/system/admin/admin_manages_participatory_process_publication_spec.rb
@@ -28,6 +28,8 @@ describe "Admin manages participatory process publication" do |_options|
       click_on "Publish"
     end
 
+    expect(page).to have_callout(publish_callout_message)
+
     visit decidim.root_path
     visit decidim.last_activities_path
 
